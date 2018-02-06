c33abfb47704f733206b0a648322608977188aac
Daktar-AeriePeak <norm0005@hotmail.com>
2018-02-05 22:07:17 -0600

    - Added Comments to Blackwing Lair and made the drops match the Dungeon Journal.

--- contrib/Parser/DATAS/1 - Dungeons Raids/1 - Classic/1 - Raids/2.0 Blackwing Lair.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/1 - Classic/1 - Raids/2.0 Blackwing Lair.lua
@@ -2,7 +2,7 @@
 --   D U N G E O N S  &  R A I D S  M O D U L E    --
 -----------------------------------------------------
 
-_.Instances = 
+_.Instances =
 {
 	{ -- Classic
 		["groups"] = {
@@ -20,197 +20,197 @@ _.Instances =
 					}),
 					e(1529, { -- Razoregore the Untamed
 						["groups"] = {
-							i(19334),
-							i(19335),
-							i(19370),
-							i(16911),
-							i(16959),
-							i(16926),
-							i(16943),
-							i(16934),
-							i(16904),
-							i(16951),
-							i(16918),
-							i(16935),
-							i(19369),
-							i(93036),
+							i(93036),	-- Unscathed Egg (Pet)
+							i(19334),	-- The Untamed Blade
+							i(19335),	-- Spineshatter
+							i(19370),	-- Mantle of the Blackwing Cabal
+							i(16926),	-- Bindings of Transcendence
+							i(16911),	-- Bloodfang Bracers
+							i(16959),	-- Bracelets of Wrath
+							i(16943),	-- Bracers of Ten Storms
+							i(16935),	-- Dragonstalker's Bracers
+							i(16951),	-- Judgment Bindings
+							i(16934),	-- Nemesis Bracers
+							i(16918),	-- Netherwind Bindings
+							i(16904),	-- Stormrage Bracers
+							i(19369),	-- Gloves of Rapid Evolution
 						},
 						["creatureID"] = 12435,
 					}),
 					e(1530, { -- Vaelastrasz the Corrupt
 						["groups"] = {
-							i(19346),
-							i(19348),
-							i(19372),
-							i(16933),
-							i(16925),
-							i(16936),
-							i(16818),
-							i(16944),
-							i(16910),
-							i(16952),
-							i(16960),
-							i(16903),
+							i(19346),	-- Dragonfang Blade
+							i(19348),	-- Red Dragonscale Protector
+							i(19372),	-- Helm of Endless Rage
+							i(16944),	-- Belt of Ten Storms
+							i(16925),	-- Belt of Transcendence
+							i(16910),	-- Bloodfang Belt
+							i(16936),	-- Dragonstalker's Belt
+							i(16952),	-- Judgment Belt
+							i(16933),	-- Nemesis Belt
+							i(16818),	-- Netherwind Belt
+							i(16903),	-- Stormrage Belt
+							i(16960),	-- Waistband of Wrath
 						},
 						["creatureID"] = 13020,
 					}),
 					e(1531, { -- Broodlord Lashlayer
 						["groups"] = {
-							i(19351),
-							i(19350),
-							i(19373),
-							i(19374),
-							i(16927),
-							i(16949),
-							i(16965),
-							i(16919),
-							i(16957),
-							i(16906),
-							i(16941),
-							i(16912),
-							i(16898),
-							i(93037),
+							i(93037),	-- Blackwing Banner (Pet)
+							i(19351),	-- Maladath, Runed Blade of the Black Flight
+							i(19350),	-- Heartstriker
+							i(19373),	-- Black Brood Pauldrons
+							i(19374),	-- Bracers of Arcane Accuracy
+							i(16906),	-- Bloodfang Boots
+							i(16919),	-- Boots of Transcendence
+							i(16941),	-- Dragonstalker's Greaves
+							i(16949),	-- Greaves of Ten Storms
+							i(16957),	-- Judgment Sabatons
+							i(16927),	-- Nemesis Boots
+							i(16912),	-- Netherwind Boots
+							i(16965),	-- Sabatons of Wrath
+							i(16898),	-- Stormrage Boots
 						},
 						["creatureID"] = 12017,
 					}),
 					e(1532, { -- Firemaw
 						["groups"] = {
-							i(19353),
-							i(19355),
-							i(19365),
-							i(19394),
-							i(19398),
-							i(19399),
-							i(16907),
-							i(16928),
-							i(16964),
-							i(16920),
-							i(16948),
-							i(16940),
-							i(16956),
-							i(16913),
-							i(16899),
-							i(19396),
-							i(19400),
-							i(19402),
-							i(19401),
+							i(19353),	-- Drake Talon Cleaver
+							i(19355),	-- Shadow Wing Focus Staff
+							i(19365),	-- Claw of the Black Drake
+							i(19394),	-- Drake Talon Pauldrons
+							i(19398),	-- Cloak of Firemaw
+							i(19399),	-- Black Ash Robe
+							i(16907),	-- Bloodfang Gloves
+							i(16940),	-- Dragonstalker's Gauntlets
+							i(16928),	-- Nemesis Gloves
+							i(16948),	-- Gauntlets of Ten Storms
+							i(16964),	-- Gauntlets of Wrath
+							i(16920),	-- Handguards of Transcendence
+							i(16956),	-- Judgment Gauntlets
+							i(16913),	-- Netherwind Gloves
+							i(16899),	-- Stormrage Handguards
+							i(19400),	-- Firemaw's Clutch
+							i(19396),	-- Taut Dragonhide Belt
+							i(19402),	-- Legguards of the Fallen Crusader
+							i(19401),	-- Primalist's Linked Legguards
 						},
 						["creatureID"] = 11983,
 					}),
 					e(1533, { -- Ebonroc
 						["groups"] = {
-							i(19353),
-							i(19355),
-							i(19368),
-							i(19394),
-							i(19405),
-							i(16907),
-							i(16920),
-							i(16928),
-							i(16899),
-							i(19407),
-							i(16964),
-							i(16948),
-							i(16956),
-							i(16913),
-							i(16940),
-							i(19396),
+							i(19353),	-- Drake Talon Cleaver
+							i(19355),	-- Shadow Wing Focus Staff
+							i(19368),	-- Dragonbreath Hand Cannon
+							i(19394),	-- Drake Talon Pauldrons
+							i(19405),	-- Malfurion's Blessed Bulwark
+							i(16907),	-- Bloodfang Gloves
+							i(16940),	-- Dragonstalker's Gauntlets
+							i(19407),	-- Ebony Flame Gloves
+							i(16948),	-- Gauntlets of Ten Storms
+							i(16964),	-- Gauntlets of Wrath
+							i(16920),	-- Handguards of Transcendence
+							i(16956),	-- Judgment Gauntlets
+							i(16928),	-- Nemesis Gloves
+							i(16913),	-- Netherwind Gloves
+							i(16899),	-- Stormrage Handguards
+							i(19396),	-- Taut Dragonhide Belt
 						},
 						["creatureID"] = 14601,
 					}),
 					e(1534, { -- Flamegor
 						["groups"] = {
-							i(19355),
-							i(19353),
-							i(19357),
-							i(19367),
-							i(19394),
-							i(16964),
-							i(16948),
-							i(16907),
-							i(16920),
-							i(16899),
-							i(16913),
-							i(16956),
-							i(16928),
-							i(16940),
-							i(19396),
-							i(19433),
-							i(19430),
+							i(19353),	-- Drake Talon Cleaver
+							i(19357),	-- Herald of Woe
+							i(19355),	-- Shadow Wing Focus Staff
+							i(19367),	-- Dragon's Touch
+							i(19394),	-- Drake Talon Pauldrons
+							i(19430),	-- Shroud of Pure Thought
+							i(16907),	-- Bloodfang Gloves
+							i(16940),	-- Dragonstalker's Gauntlets
+							i(16948),	-- Gauntlets of Ten Storms
+							i(16964),	-- Gauntlets of Wrath
+							i(16920),	-- Handguards of Transcendence
+							i(16956),	-- Judgment Gauntlets
+							i(16928),	-- Nemesis Gloves
+							i(16913),	-- Netherwind Gloves
+							i(16899),	-- Stormrage Handguards
+							i(19396),	-- Taut Dragonhide Belt
+							i(19433),	-- Emberweave Leggings
 						},
 						["creatureID"] = 11981,
 					}),
 					e(1535, { -- Chromaggus
 						["groups"] = {
-							i(19352),
-							i(19347),
-							i(19361),
-							i(19349),
-							i(16961),
-							i(16945),
-							i(16932),
-							i(16832),
-							i(16902),
-							i(19389),
-							i(16937),
-							i(16917),
-							i(16924),
-							i(16953),
-							i(19386),
-							i(19390),
-							i(19392),
-							i(19388),
-							i(19393),
-							i(19385),
-							i(19387),
-							i(19391),
-							i(93038),
+							i(93038),	-- Whistle of Chromatic Bone
+							i(19352),	-- Chromatically Tempered Sword
+							i(19347),	-- Claw of Chromaggus
+							i(19361),	-- Ashjre'thul, Crossbow of Smiting
+							i(19349),	-- Elementium Reinforced Bulwark
+							i(16832),	-- Bloodfang Spaulders
+							i(16937),	-- Dragonstalker's Spaulders
+							i(16945),	-- Epaulets of Ten Storms
+							i(16953),	-- Judgment Spaulders
+							i(16932),	-- Nemesis Spaulders
+							i(16917),	-- Netherwind Mantle
+							i(16924),	-- Pauldrons of Transcendence
+							i(16961),	-- Pauldrons of Wrath
+							i(16902),	-- Stormrage Pauldrons
+							i(19389),	-- Taut Dragonhide Shoulderpads
+							i(19386),	-- Elementium Threaded Cloak
+							i(19390),	-- Taut Dragonhide Gloves
+							i(19388),	-- Angelista's Grasp
+							i(19392),	-- Girdle of the Fallen Crusader
+							i(19393),	-- Primalist's Linked Waistguard
+							i(19385),	-- Empowered Leggings
+							i(19387),	-- Chromatic Boots
+							i(19391),	-- Shimmering Geta
 						},
 						["creatureID"] = 14020,
 					}),
 					e(1536, { -- Nefarian
 						["groups"] = {
-							i(19003, {
-								qa(7781, {
-									qa(7782, {
-										i(19366), -- ["classes"] {2,5,7,8,9,10,11}
+							i(19003, {	-- Head of Nefarian
+								qa(7781, {	-- The Lord of Blackrock
+									qa(7782, {	-- THe Lord of Blackrock
+										i(19366), -- Master Dragonslayer's Orb
 									}),
 								}),
-							}),	
-							
-							i(19002, {
-								qh(7783, {
-									qh(7784, {
-										i(19366), -- ["classes"] {2,5,7,8,9,10,11}
+							}),
+
+							i(19002, {	-- Head of Nefarian
+								qh(7783, {	-- The Lord of Blackrock
+									qh(7784, {	-- The Lord of Blackrock
+										i(19366), -- Master Dragonslayer's Orb
 									}),
 								}),
-							}),	
-							i(19364),
-							i(19356),
-							i(19360),
-							i(19363),
-							i(19375),
-							i(16963),
-							i(16900),
-							i(16921),
-							i(16955),
-							i(16939),
-							i(16947),
-							i(16914),
-							i(16908),
-							i(16929),
-							i(19378),
-							i(16923),
-							i(16966),
-							i(16950),
-							i(16958),
-							i(16897),
-							i(16905),
-							i(16931),
-							i(16942),
-							i(16916),
-							i(19380),
-							i(19381),
+							}),
+							i(19364),	-- Ashkandi, Greatsword of the Brotherhood
+							i(19356),	-- Staff of the Shadow Flame
+							i(19363),	-- Crul'shorukh, Edge of Chaos
+							i(19360),	-- Lok'amir il Romathis
+							i(16908),	-- Bloodfang Hood
+							i(16939),	-- Dragonstalker's Helm
+							i(16921),	-- Halo of Transcendence
+							i(16963),	-- Helm of Wrath
+							i(16947),	-- Helmet of Ten Storms
+							i(16955),	-- Judgment Crown
+							i(19375),	-- Mish'undare, Circlet of the Mind Flayer
+							i(16929),	-- Nemesis Skullcap
+							i(16914),	-- Netherwind Crown
+							i(16900),	-- Stormrage Cover
+							i(19378),	-- Cloak of the Brood Lord
+							i(16905),	-- Bloodfang Chestpiece
+							i(16950),	-- Breastplate of Ten Storms
+							i(16966),	-- Breastplate of Wrath
+							i(16942),	-- Dragonstalker's Breastplate
+							i(16958),	-- Judgment Breastplate
+							i(16931),	-- Nemesis Robes
+							i(16916),	-- Netherwind Robes
+							i(16923),	-- Robes of Transcendence
+							i(16897),	-- Stormrage Chestguard
+							i(19380),	-- Therazane's Link
+							i(19381),	-- Boots of the Shadow Flame
 						},
 						["creatureID"] = 11583,
 					})
@@ -220,7 +220,7 @@ _.Instances =
 				["Lvl"] = 50,
 				["mapID"] = 755
 			}),
-		},					
+		},
 		["tierID"] = 1
-	},	
+	},
 };
