39f537e020b8901c761b07a7eada88d54d19e082
Lucetia-Detheroc <lucetribunal@gmail.com>
2018-02-07 00:59:11 -0800

    - Added contents to Satchel of Helpful Goods (Looks like they combined all of the 15-60 bags into one)  [NOTE: Some items are currently bugged] - Harvested Items

--- contrib/Parser/DATAS/4 - Events/13 Special/WorldEvents_Special.lua
+++ contrib/Parser/DATAS/4 - Events/13 Special/WorldEvents_Special.lua
@@ -7,18 +7,50 @@ _.WorldEvents =
 		["groups"] = {
 			n(-300, { -- Satchel of Helpful Goods
 				["groups"] = {
-					i(156683, { -- Satchel of Helpful Goods [Level 15-25]
-						["groups"] = {
-							i(51994), -- Tumultuous Cloak
-							i(51968), -- Enumerated Wrap
-							i(51964), -- Vigorous Belt
-							--i(51978), -- Earthbound Girdle; Probably unobtainable.
-							i(143580), -- Earthbound Girdle
-						 	i(143582), -- Stalwart Belt
-						},
-						["description"] = "|cff66ccffRewarded for completing a random dungeon in the level 15-25 range.|r",
-						["Lvl"] = 15,
-					}),
+					i(156683, { -- Satchel of Helpful Goods [Level 15-60]			
+						["groups"] = {		
+							i(51994),	-- Tumultuous Cloak
+							i(51993),	-- Turbulent Cloak
+							i(51974),	-- Enumerated Shoulderpads
+							i(51974),	-- Enumerated Shoulderpads
+							i(51972),	-- Enumerated Bracers
+							i(51968),	-- Enumerated Wrap
+							i(51970),	-- Enumerated Gloves
+							i(51973),	-- Enumerated Handwraps
+							i(51971),	-- Enumerated Belt
+							i(51967),	-- Enumerated Sandals
+							i(51984),	-- Stalwart Shoulderpads
+							i(51984),	-- Stalwart Shoulderpads
+							i(51997),	-- Stalwart Spaulders
+							i(51989),	-- Stalwart Bands
+							i(143583),	-- Stalwart Grips
+							i(51987),	-- Stalwart Grips
+							i(143582),	-- Stalwart Belt
+							i(51985),	-- Stalwart Belt
+							i(51990),	-- Stalwart Treads
+							i(51976),	-- Earthbound Shoulderguards
+							i(51976),	-- Earthbound Shoulderguards
+							i(51981),	-- Earthbound Wristguards
+							i(143581),	-- Earthbound Grips
+							i(51979),	-- Earthbound Grips
+							un(2, i(51980)),	-- Earthbound Handgrips [Probably unobtainable]
+							i(143580),	-- Earthbound Girdle
+							i(51977),	-- Earthbound Girdle
+							un(2, i(51978)),	-- Earthbound Girdle [Probably unobtainable]
+							i(51982),	-- Earthbound Boots
+							i(51966),	-- Vigorous Spaulders
+							i(51998),	-- Vigorous Spaulders
+							i(51966),	-- Vigorous Spaulders
+							i(51962),	-- Vigorous Bracers
+							i(51960),	-- Vigorous Gloves
+							i(51965),	-- Vigorous Handguards
+							i(51964),	-- Vigorous Belt
+							i(51959),	-- Vigorous Belt
+							i(51963),	-- Vigorous Stompers
+						},		
+						["description"] = "|cff66ccffRewarded for completing a random dungeon in the level 15-60 range.|r",		
+						["Lvl"] = 15,		
+					}),	
 			--[[
 			   i(51999, { -- Satchel of Helpful Goods 15-25
 				["groups"] = {
