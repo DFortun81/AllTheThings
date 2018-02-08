3b97e3035dfc0ae8e9db91012d981a904659da91
Lucetia-Detheroc <lucetribunal@gmail.com>
2018-02-08 00:14:00 -0800

    - Added file for starter sets [Will be filling it out soon]

---
+++ contrib/Parser/DATAS/6 - Gear Sets/Subgroups/GearSets_Starter.lua
@@ -0,0 +1,28 @@
+--[[
+-----------------------------------------------------
+--       G E A R     S E T S     M O D U L E       --
+-----------------------------------------------------
+_.GearSets = 
+{
+	n(-211, { -- New Characters
+		n(-209, { -- Highmountain Tauren
+			i(157631),	-- Maul of Smiting
+		}),
+		n(-207, { -- Lightforged Draenei
+			i(157631),	-- Maul of Smiting
+			i(157698),	-- Lightsoul Battleplate
+			i(157705),	-- Lightsoul Vambraces
+			i(157700),	-- Lightsoul Gauntlets
+			i(157704),	-- Lightsoul Girdle
+			i(157702),	-- Lightsoul Legplates
+			i(157699),	-- Lightsoul Sabatons
+		}),
+		n(-210, { -- Nightborne
+			i(157631),	-- Maul of Smiting
+		}),
+		n(-208, { -- Void Elf
+			i(157631),	-- Maul of Smiting
+		}),
+	}),
+};
+--]]
