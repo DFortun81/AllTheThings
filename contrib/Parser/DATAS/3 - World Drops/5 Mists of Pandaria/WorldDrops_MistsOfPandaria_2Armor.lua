db0ccd907a7a6272be01c502f82505db056606fe
Avella-Velen <purple.spot@gmail.com>
2018-02-13 06:27:12 -0600

    -- Commented all of MoP World Drops. Wheee....

--- contrib/Parser/DATAS/3 - World Drops/5 Mists of Pandaria/WorldDrops_MistsOfPandaria_2Armor.lua
+++ contrib/Parser/DATAS/3 - World Drops/5 Mists of Pandaria/WorldDrops_MistsOfPandaria_2Armor.lua
@@ -7,137 +7,137 @@ _.WorldDrops =
 		["groups"] = {
 			n(-318, { -- Armor
 				n(-320, { -- Head
-					i(87450),
-					i(87451),
-					i(87452),
-					i(87453),
-					i(87454),
-					i(87455),
-					i(87456),
-					i(90573),
+					i(87450),	-- Hood of Alchemical Vapors
+					i(87451),	-- Helm of Elemental Binding
+					i(87452),	-- Zom's Misty Hood
+					i(87453),	-- Endwyn's Toothy Visage
+					i(87454),	-- Light-Sealed Casque
+					i(87455),	-- Davidian's All-Seeing Eyes
+					i(87456),	-- Hexu's Amplifying Helm
+					i(90573),	-- Wang's Unshakable Smile
 				}),
 				n(-321, { -- Shoulder
-					i(87489),
-					i(87490),
-					i(87491),
-					i(87492),
-					i(87493),
-					i(87494),
-					i(90576),
+					i(87489),	-- Fletcher's Sharpening Shoulders
+					i(87490),	-- Orb-Bound Shoulders
+					i(87491),	-- Spaulders of Five Rings
+					i(87492),	-- Mantle of Bestial Force
+					i(87493),	-- Enokian Growth Shoulderguards
+					i(87494),	-- Shoulderguards of Honest Labor
+					i(90576),	-- Spaulders of the Scorned
 				}),
 				n(-322, { -- Back
-					i(116693),
-					i(82017),
-					i(82061),
-					i(82116),
-					i(82171),
-					i(82226),
-					i(82281),
-					i(87427),
-					i(87428),
-					i(87429),
-					i(87430),
-					i(87431),
-					i(90578),
-					i(90580),
-					i(90586),
-					i(90589),
+					i(116693),	-- Coldsinger Cloak
+					i(82017),	-- Intricate Cloak
+					i(82061),	-- Gemmed Cloak
+					i(82116),	-- Shamanic Cloak
+					i(82171),	-- Grummle Cloak
+					i(82226),	-- Bejeweled Cloak
+					i(82281),	-- Immaculate Cloak
+					i(87427),	-- Blade-Dulling Greatcloak
+					i(87428),	-- Aquino's Cloak of Unity
+					i(87429),	-- Drape of Flowing Gauze
+					i(87430),	-- Brilliant Cloak of Wu the Younger
+					i(87431),	-- Roffle's Flowing Raiment
+					i(90578),	-- Zom's Rain-Stained Cloak
+					i(90580),	-- Cristof's Crushing Cloak
+					i(90586),	-- Dory's Pageantry
+					i(90589),	-- Dirl's Drafty Drape
 				}),
 				n(-323, { -- Chest
-					i(87425),
-					i(87426),
-					i(87424),
-					i(87484),
-					i(87485),
-					i(90577),
-					i(90585),
-					i(90587),
+					i(87424),	-- Cryptwarden's Breastplate
+					i(87425),	-- Chestguard of the Solicitious Pandaren
+					i(87426),	-- Deathspite Breastplate
+					i(87484),	-- Robes of the Quiet Isle
+					i(87485),	-- Robes of Forgone Hope
+					i(90577),	-- Boblet's Bouncing Hauberk
+					i(90585),	-- Vulajin's Vicious Breastplate
+					i(90587),	-- Natli's Fireheart Robe
 				}),
 				n(-326, { -- Wrist
-					i(86183),
-					i(86184),
-					i(86185),
-					i(86186),
-					i(86187),
-					i(86188),
-					i(86189),
-					i(86190),
-					i(86191),
-					i(86192),
-					i(86841),
-					i(86842),
-					i(86843),
-					i(86844),
-					i(86845),
-					i(86846),
-					i(86847),
-					i(86848),
-					i(86849),
-					i(86850),
-					i(86992),
-					i(86993),
-					i(86994),
-					i(86995),
-					i(86996),
-					i(86997),
-					i(86998),
-					i(86999),
-					i(87000),
-					i(87001),
-					i(87517),
-					i(87518),
-					i(87519),
-					i(87520),
-					i(87521),
-					i(87522),
+					i(86183),	-- Shining Cicada Bracers
+					i(86184),	-- Luminescent Firefly Wristguards
+					i(86185),	-- Smooth Beetle Wristbands
+					i(86186),	-- Gleaming Moth Cuffs
+					i(86187),	-- Pearlescent Butterfly Wristbands
+					i(86188),	-- Inlaid Cricket Bracers
+					i(86189),	-- Jagged Hornet Bracers
+					i(86190),	-- Serrated Wasp Bracers
+					i(86191),	-- Plated Locust Bracers
+					i(86192),	-- Darting Damselfly Cuffs
+					i(86841),	-- Shining Cicada Bracers
+					i(86842),	-- Luminescent Firefly Wristguards
+					i(86843),	-- Smooth Beetle Wristbands
+					i(86844),	-- Gleaming Moth Cuffs
+					i(86845),	-- Pearlescent Butterfly Wristbands
+					i(86846),	-- Inlaid Cricket Bracers
+					i(86847),	-- Jagged Hornet Bracers
+					i(86848),	-- Serrated Wasp Bracers
+					i(86849),	-- Plated Locust Bracers
+					i(86850),	-- Darting Damselfly Cuffs
+					i(86992),	-- Shining Cicada Bracers
+					i(86993),	-- Darting Damselfly Cuffs
+					i(86994),	-- Gleaming Moth Cuffs
+					i(86995),	-- Smooth Beetle Wristbands
+					i(86996),	-- Pearlescent Butterfly Wristbands
+					i(86997),	-- Jagged Hornet Bracers
+					i(86998),	-- Luminescent Firefly Wristguards
+					i(86999),	-- Inlaid Cricket Bracers
+					i(87000),	-- Plated Locust Bracers
+					i(87001),	-- Serrated Wasp Bracers
+					i(87517),	-- Bracers of Rolling Fields
+					i(87518),	-- Bracers of the Neux Dawn
+					i(87519),	-- Misty Morning Wristguards
+					i(87520),	-- Glowing Wind Bracers
+					i(87521),	-- Zoid's Scorched Bracers
+					i(87522),	-- Star-Carrier Bracers
 				}),
 				n(-327, { -- Hands
-					i(87443),
-					i(87444),
-					i(87445),
-					i(87446),
-					i(87447),
-					i(87448),
-					i(87449),
-					i(90574),
-					i(90588),
+					i(87443),	-- Syrio's Gloves of Tutelage
+					i(87444),	-- Wind-Kissed Handwraps
+					i(87445),	-- Gauntlets of Blurring Fingers
+					i(87446),	-- Gauntlets of the Solemn Charge
+					i(87447),	-- Toad-Slime Gloves
+					i(87448),	-- Toremu's Crackling Grips
+					i(87449),	-- Errick's Woundbinder Gauntlets
+					i(90574),	-- Etoshia's Elegant Gloves
+					i(90588),	-- Rittsyn's Ruinblasters
 				}),
 				n(-328, { -- Waist
-					i(87501),
-					i(87502),
-					i(87503),
-					i(87504),
-					i(87505),
-					i(87506),
-					i(87507),
+					i(87501),	-- Unbreakable Waistplate
+					i(87502),	-- Repression Belt
+					i(87503),	-- Repin's Ammo-Laden Belt
+					i(87504),	-- Waistguard of Flaming Words
+					i(87505),	-- Spirit-Speaking Belt
+					i(87506),	-- Cheng's Irrepressible Girdle
+					i(87507),	-- Melador's Mercurial Wrap
 				}),
 				n(-329, { -- Legs
-					i(87459),
-					i(87460),
-					i(87461),
-					i(87462),
-					i(87463),
-					i(87464),
-					i(90570),
-					i(90572),
-					i(90579),
-					i(90717),
-					i(90718),
-					i(90719),
-					i(90720),
-					i(90721),
-					i(90722),
-					i(90723),
-					i(90724),
-					i(90725),
+					i(87459),	-- Dills' Primal Leggings
+					i(87460),	-- Legplates of the Wandering Warrior
+					i(87461),	-- Greatheart Pants
+					i(87462),	-- Leggings of Perfect Balance
+					i(87463),	-- Very Manly Leggings
+					i(87464),	-- Leggings of the Scorched Man
+					i(90570),	-- Legguards of Sleeting Arrows
+					i(90572),	-- Kilt of Pandaren Promises
+					i(90579),	-- Legplates of Durable Dreams
+					i(90717),	-- Qu'nas' Apocryphal Legplates
+					i(90718),	-- Torik-Ethis' Bloodied Legguards
+					i(90719),	-- Go-Kan's Golden Trousers
+					i(90720),	-- Silent Leggings of the Ghostpaw
+					i(90721),	-- Cournith Waterstrider's Silken Finery
+					i(90722),	-- Torik-Ethis' Gilded Legplates
+					i(90723),	-- Arness's Scaled Leggings
+					i(90724),	-- Spriggin's Sproggin' Leggin'
+					i(90725),	-- Gaarn's Leggings of Infestation
 				}),
 				n(-330, { -- Feet
-					i(87432),
-					i(87433),
-					i(87434),
-					i(87435),
-					i(87436),
-					i(87695),
+					i(87432),	-- Tesslah's Ticking Treads
+					i(87433),	-- Blackfooted Greatboots
+					i(87434),	-- Sandals of Sympathetic Healing
+					i(87435),	-- Treads of Exotic Mastery
+					i(87436),	-- Sollerets of Immovability
+					i(87695),	-- Treads of Edward the Odd
 				}),
 			}),
 		},
