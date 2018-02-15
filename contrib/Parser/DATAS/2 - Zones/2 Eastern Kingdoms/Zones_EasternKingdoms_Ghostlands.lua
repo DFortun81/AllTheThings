3b1f23c29aa7d4d4e235d16fd3b05adcfc4a72fb
slumber <slumber@love.com>
2018-02-15 02:35:18 -0600

    - commented drops in iron docks and reorganized them to match dungeon journal order - started ghostlands questline (unfinished)

--- contrib/Parser/DATAS/2 - Zones/2 Eastern Kingdoms/Zones_EasternKingdoms_Ghostlands.lua
+++ contrib/Parser/DATAS/2 - Zones/2 Eastern Kingdoms/Zones_EasternKingdoms_Ghostlands.lua
@@ -97,30 +97,332 @@ _.Zones =
 				n(-17, { 	-- Quests
 				
 				
-				qh( 9327, {	-- The Forsaken
-					["groups"] = {
-					},
-					["qg"] = 16197,	-- Arcanist Vandril
-					["races"] = {10} -- Blood Elf
-				}),
-				qh( 9329, {	-- The Forsaken
-					["groups"] = {
-					},
-					["qg"] = 16197,	-- Arcanist Vandril					
-					["races"] = { 2, 5, 6, 8, 9, 26, 27, 28 }, -- Horde, not Blood Elf
-				}),
+					qh( 9275, {	-- A Little Dash of Seasoning
+						["groups"] = {
+						},
+						["qg"] = 16464,	-- Apothecary Venustus
+						["sourceQuestID"] = { 9276 },	-- Attack on Zeb'Tela
+					}),
 				
 				
-				
-				
-				
-				
+					qh( 9315, {	-- Anok'suten
+							["groups"] = {
+								i( 24341),	-- Fortified Oven Mitts
+								i( 24339),	-- Stung
+								i( 24340),	-- Vandril's Hand Me Down Pants
+								i( 28149),	-- Tranquillien Breeches
+								i(131251)	-- Mix-Match Linked Pants
+							},
+							["qg"] = 16601,	-- Dying Blood Elf
+							["sourceQuestID"] = { 9327, 9329 },	-- The Forsaken (either version)
+					}),
+					
+					
+					qh( 9277, {	-- Assault on Zeb'Nowa
+						["groups"] = {
+							i( 23406),	-- Sentry Bracers
+							i( 23407),	-- Supple Cotton Bracers
+							i( 23408),	-- Farstrider Bracers
+							i(131249),	-- Farstrider Cuffs
+						},
+						["qg"] = 16463,	-- Farstrider Solanna
+						["sourceQuestID"] = { 9276 },	-- Attack on Zeb'tela
+					}),
+					
+					
+					qh( 9276, {	-- Attack on Zeb'Tela
+						["groups"] = {
+						},
+						["qg"] = 16463,	-- Farstrider Solanna
+						["sourceQuestID"] = { 9146 },	-- Report to Captain Helios
+					}),
+					
+					
+					qh( 9158, {	-- Bearers of the Plague
+						["groups"] = {
+						},
+						["qg"] = 16202,	-- Farstrider Sedina
+					}),
+					
+					
+					o(181153, {	-- Wanted Poster: Kel'gash the Wicked
+						qh(9215, { -- Bring Me Kel'gash's Head!
+							["groups"] = {
+								i(23409),	-- Well Crafted Long Bow
+								i(23410),	-- Well Crafted Sword
+								i(23411),	-- Well Crafted Staff
+							},
+							["sourceQuestID"] = { 9276 },	-- Attack on Zeb'tela
+						}),
+					}),
+					
+					
+					qh( 9171, {	-- Culinary Crunch
+						["groups"] = {
+						},
+						["qg"] = 16253,	-- Master Chef Mouldier
+						["sourceQuestID"] = { 9138 },	-- Suncrown Village
+					}),
+					
+					
+					qh( 9159, {	-- Curbing the Plague
+						["groups"] = {
+							i( 28141),	-- Ranger's Sash
+							i( 28142),	-- Farstrider's Belt
+							i( 28143),	-- Rusted Plate Girdle
+							i(131244),	-- Farstrider's Chain
+							i( 23415)	-- Survival Knife
+						},
+						["qg"] = 16202,	-- Farstrider Sedina
+						["sourceQuestID"] = { 9158 },	-- Bearers of the Plague
+					}),
+					
+					
+					qh( 9143, {	-- Dealing with Zeb'Sora
+						["groups"] = {
+						},
+						["qg"] = 16219,	-- Ranger Valanna
+						["sourceQuestID"] = { 9145 },	-- Helo Ranger Valanna!
+					}),
+					
+					
+					qh( 9155, {	-- Down the Dead Scar
+						["groups"] = {
+						},
+						["qg"] = 16200,	-- Deathstalker Rathiel
+						["sourceQuestID"] = { 9138 },	-- Suncrown Village
+					}),
+					
+					
+					qh( 9212, {	-- Escape from the Catacombs
+						["groups"] = {
+							i( 23412),	-- Troll Kickers (Plate)
+							i( 23413),	-- Troll Kickers (Cloth)
+							i( 23414),	-- Troll Kickers (Leather)
+							i(131248),	-- Troll Kickers (Mail)
+						},
+						["qg"] = 16295,	-- Ranger Lilatha
+						["sourceQuestID"] = { 9193 },	-- Investigate the Amani Catacombs
+					}),
+					
+					
+					qh( 9157, {	-- Forgotten Rituals
+						["groups"] = {
+						},
+						["qg"] = 16201,	-- Geranis Whitemorn
+						["sourceQuestID"] = { 9274 },	-- Spirits of the Drowned (NOTE: I was able to pick up this while I was on this quest, but it may have procced after Report to Captain Helios - when I pulled up Wholly to check what info it had, it only listed a level requirement, but it was not available to me at that level or above until somewhere around this point)
+					}),
+					
+					
+					qh( 9139, {	-- Goldenmist Village
+						["groups"] = {
+							i(28152)	-- Quel'Thalas Recurve
+						},
+						["qg"] = 16197,	-- Arcanist Vandril
+						["sourceQuestID"] = { 9138 }	-- Suncrown Village
+					}),
+					
+					
+					qh( 9145, {	-- Help Ranger Valanna!
+						["groups"] = {
+						},
+						["qg"] = 16213,	-- Ranger Lethvalin
+						["sourceQuestID"] = { 9327, 9329 },	-- The Forsaken (either version)
+					}),
+
+					
+					qh( 9163, {	-- Into Occupied Territory
+						["groups"] = {
+						},
+						["qg"] = 16231,	-- Dame Auriferous
+						["sourceQuestID"] = { 9160 },	-- Investigate An'daroth
+					}),
+					
+					
+					qh( 9193, {	-- Investigate the Amani Catacombs
+						["groups"] = {
+						},
+						["qg"] = 16203,	-- Ranger Vynna
+						["sourceQuestID"] = { 9146 },	-- Report to Captain Helios
+					}),
+					
+					
+					qh( 9160, {	-- Investigate An'daroth
+						["groups"] = {
+						},
+						["qg"] = 16231,	-- Dame Auriferous
+						["sourceQuestID"] = { 9138 },	-- Suncrown Village
+					}),
+					
+					
+					qh( 9146, {	-- Report to Captain Helios
+						["groups"] = {
+							i( 23405),	-- Farstrider's Tunic
+							i( 28144),	-- Troll Handler Gloves
+							i( 28153),	-- Farstrider's Shield
+							i(131242),	-- Farstrider's Chainmail
+						},
+						["qg"] = 16219,	-- Ranger Valanna
+						["sourceQuestID"] = { 9143 },	-- Dealing with Zeb'Sora
+					}),
+					
+					
+					qh( 9173, {	-- Retaking Windrunner Spire
+						["groups"] = {
+						},
+						["qg"] = 16252,	-- High Executor Mavren
+						["sourceQuestID"] = { 9139 },	-- Goldenmist Village
+					}),
+					
+					
+					qh( 9758, {	-- Return to Arcanist Vandril
+						["groups"] = {
+						},
+						["qg"] = 16252,	-- High Executor Mavren
+						["sourceQuestID"] = { 9327, 9329 },	-- The Forsaken (either version)
+					}),
+					
+					
+					qh( 9216, {	-- Rotting Hearts
+						["groups"] = {
+						},
+						["qg"] = 16205,	-- Magistrix Aminel
+						["sourceQuestID"] = { 9155 },	-- Down the Dead Scar
+					}),
+					
+					
+					qh( 9150, {	-- Salvaging the Past
+						["groups"] = {
+						},
+						["qg"] = 16199,	-- Magister Darenis
+						["sourceQuestID"] = { 9138 },	-- Suncrown Village
+					}),
+					
+					
+					qh( 9214, {	-- Shadowpine Weaponry
+						["groups"] = {
+						},
+						["qg"] = 16220,	-- Captain Helios
+						["sourceQuestID"] = { 9146 },	-- Report to Captain Helios
+					}),
+					
+					
+					qh( 9218, {	-- Spinal Dust
+						["groups"] = {
+						},
+						["qg"] = 16205,	-- Magistrix Aminel
+						["sourceQuestID"] = { 9155 },	-- Down the Dead Scar
+					}),
+					
+					
+					qh( 9274, {	-- Spirits of the Drowned
+						["groups"] = {
+						},
+						["qg"] = 16462,	-- Ranger Krenn'an
+						["sourceQuestID"] = { 9146 },	-- Report to Captain Helios
+					}),
+					
+					
+					qh( 9138, {	-- Suncrown Village
+						["groups"] = {
+						},
+						["qg"] = 16197,	-- Arcanist Vandril
+						["sourceQuestID"] = { 9758 },	-- Return to Arcanist Vandril
+					}),
+
+					
+					qh( 9327, {	-- The Forsaken
+						["groups"] = {
+						},
+						["qg"] = 16197,	-- Arcanist Vandril
+						["races"] = {10} -- Blood Elf
+					}),
+					qh( 9329, {	-- The Forsaken
+						["groups"] = {
+						},
+						["qg"] = 16197,	-- Arcanist Vandril					
+						["races"] = { 2, 5, 6, 8, 9, 26, 27, 28 }, -- Horde, not Blood Elf
+					}),
+					
+					
+					qh( 9149, {	-- The Plagued Coast
+						["groups"] = {
+							i(28150)	-- Renzithen's Dusty Cloak
+						},
+						["qg"] = 16198,	-- Apothecary Renzithen
+						["sourceQuestID"] = { 9138 }	-- Suncrown Village
+					}),
+					
+					
+					qh( 9152, {	-- Tomber's Supplies
+						["groups"] = {
+						},
+						["qg"] = 16224,	-- Rathis Tomber
+						["sourceQuestID"] = { 9138 },	-- Suncrown Village
+					}),
+					
+					
+					qh( 9199, {	-- Troll Juju
+						["groups"] = {
+							i( 28156),	-- Rotting Handwraps
+							i( 28159),	-- Undertaker's Gloves
+							i( 28163),	-- Maltendis' Handguards
+							i(131247)	-- Gravedigger's Grips
+						},
+						["qg"] = 16464,	-- Apothecary Venustus
+						["sourceQuestID"] = { 9146 },	-- Report to Captain Helios
+					}),
+					
+
+					qh( 9192, {	-- Trouble at the Underlight Mines
+						["groups"] = {
+						},
+						["qg"] = 16251,	-- Deathstalker Maltendis
+						["sourceQuestID"] = { 9138 },	-- Suncrown Village
+					}),
+					
+					
+					qh( 9207, {	-- Underlight Ore Samples
+						["groups"] = {
+						},
+						["qg"] = 16293,	-- Apprentice Shatharia
+					}),
+					
+					
+					qh( 9174, {	-- Vanquishing Aquantion
+						["groups"] = {
+						},
+						["qg"] = 16201,	-- Geranis Whitemorn
+						["sourceQuestID"] = { 9157 },	-- Forgotten Rituals
+					}),
+
+					
+					o(181147, {	-- Wanted Poster
+						["groups"] = {
+							qh(9156),	-- Wanted: Knucklerot and Luzran
+						},
+						["sourceQuestID"] = { 9138 }	-- Suncrown Village
+					}),
+
+
+					qh( 9140, {	-- Windrunner Village
+						["groups"] = {
+							i( 23402),	-- Arcanist's Dagger
+							i( 28151),	-- Arcanist's Wand
+							i( 23403),	-- Salvaged Plate Leggings
+							i( 23404),	-- Padded Running Shoes
+							i(131241),	-- Relinked Chain Boots
+						},
+						["qg"] = 16197,	-- Arcanist Vandril
+						["sourceQuestID"] = { 9139 },	-- Goldenmist Village
+					}),
+					
 --[[							
-					["groups"] = {
-					},
-					["qg"] = ,	-- 
-					["sourceQuestID"] = {  },	-- 
-				}),
+						["groups"] = {
+						},
+						["qg"] = ,	-- 
+						["sourceQuestID"] = {  },	-- 
+					}),
 --]]
 				
 				
@@ -131,7 +433,6 @@ _.Zones =
 				
 				
 				
---					qg(16464, qh( 9275)),	-- A Little Dash of Seasoning
 --					qh( 9877),	-- A Restorative Draught (LEGACY)
 					qg(16197, qh( 9488, {	-- A Simple Robe
 						["groups"] = {
@@ -140,13 +441,6 @@ _.Zones =
 						["races"] = {10}, -- Blood Elf
 						["classes"] = {8}, -- Mage
 					})),
-					qg(16601, qh( 9315, {	-- Anok'suten
-						i( 24341),	-- Fortified Oven Mitts
-						i( 24339),	-- Stung
-						i( 24340),	-- Vandril's Hand Me Down Pants
-						i( 28149),	-- Tranquillien Breeches
-						i(131251)	-- Mix-Match Linked Pants
-					})),
 --[[
 					qg(16197, qh( 9487, {	-- Arcane Reavers
 						["groups"] = {
@@ -154,36 +448,9 @@ _.Zones =
 						["races"] = {10}, -- Blood Elf
 						["classes"] = {8}, -- Mage
 					})),
---]]
-					qg(16463, qh( 9277, {	-- Assault on Zeb'Nowa
-						i( 23406),	-- Sentry Bracers
-						i( 23407),	-- Supple Cotton Bracers
-						i( 23408),	-- Farstrider Bracers
-						i(131249),	-- Farstrider Cuffs
-					})),
---[[					
-					qg(16463, qh( 9276)),	-- Attack on Zeb'Tela
-					qg(16202, qh( 9158)),	-- Bearers of the Plague
---]]
-					o(181153, {	-- Wanted Poster: Kel'gash the Wicked
-						qh(9215, { -- Bring Me Kel'gash's Head!
-							i(23409),	-- Well Crafted Long Bow
-							i(23410),	-- Well Crafted Sword
-							i(23411),	-- Well Crafted Staff
-						}),
-					}),
---[[					
 					qg(16198, qh( 9164)),	-- Captives at Deatholme
 					qg(16480, qh( 9281)),	-- Clearing the Way
-					qg(16253, qh( 9171)),	-- Culinary Crunch
 --]]
-					qg(16202, qh( 9159, {	-- Curbing the Plague
-						i( 28141),	-- Ranger's Sash
-						i( 28142),	-- Farstrider's Belt
-						i( 28143),	-- Rusted Plate Girdle
-						i(131244),	-- Farstrider's Chain
-						i( 23415)	-- Survival Knife
-					})),
 					qg(16204, qh( 9170, {	-- Dar'Khan's Lieutenants
 						i( 22996),	-- Reforged Quel'dorei Crest
 						i( 22997),	-- Ley-Keeper's Wand
@@ -197,7 +464,6 @@ _.Zones =
 						i(28160),	-- An'telas Plate Shirt
 					})),
 --[[					
-					qg(16219, qh( 9143)),	-- Dealing with Zeb'Sora
 					qg(16231, qh( 9166)),	-- Deliver the Plans to An'telas
 --]]
 					qg(16183, qh( 9148, {	-- Delivery to Tranquillien
@@ -206,22 +472,8 @@ _.Zones =
 						i( 28148),
 						i(131243),
 					})),
---					qg(16200, qh( 9155)),	-- Down the Dead Scar
-					qg(16295, qh( 9212, {	-- Escape from the Catacombs
-						i( 23412),	-- Troll Kickers...
-						i( 23413),
-						i( 23414),
-						i(131248)
-					})),
 --[[					
-					qg(16201, qh( 9157)),	-- Forgotten Rituals
 					qg(16239, qh( 9811)),	-- Friend of the Sin'dorei
---]]
-					qg(16197, qh( 9139, {	-- Goldenmist Village
-						i(28152)	-- Quel'Thalas Recurve
-					})),
---[[					
-					qg(16213, qh( 9145)),	-- Help Ranger Valanna!
 					qg(16239, qh( 9328, {	-- Hero of the Sin'dorei
 						["groups"] = {
 						},
@@ -230,9 +482,6 @@ _.Zones =
 					o(181150, {	-- Dusty Journal
 						qh(9162),	-- Hints of the Past
 					}),
-					qg(16231, qh( 9163)),	-- Into Occupied Territory
-					qg(16231, qh( 9160)),	-- Investigate An'daroth
-					qg(16289, qh( 9193)),	-- Investigate the Amani Catacombs
 					qg(16252, qh( 9180, {	-- Journey to Undercity
 						["groups"] = {
 						},
@@ -255,14 +504,6 @@ _.Zones =
 						i(131246),	-- Dark Chain Shirt
 					})),
 --[[					
-					qg(16252, qh( 9173)),	-- Retaking Windrunner Spire
-					qg(16252, qh( 9758)),	-- Return to Arcanist Vandril
-					qg(16205, qh( 9216)),	-- Rotting Hearts
-					qg(16199, qh( 9150)),	-- Salvaging the Past
-					qg(16220, qh( 9214)),	-- Shadowpine Weaponry
-					qg(16205, qh( 9218)),	-- Spinal Dust
-					qg(16462, qh( 9274)),	-- Spirits of the Drowned
-					qg(16197, qh( 9138)),	-- Suncrown Village
 					qg(16196, qh( 9147)),	-- The Fallen Courier
 					qg(16239, qh( 9282)),	-- The Farstrider Enclave
 					n(16314, {	-- Fallen Ranger
@@ -275,10 +516,7 @@ _.Zones =
 							qh(9175),	-- The Lady's Necklace
 						}),
 					}),
---]]					
-					qg(16198, qh( 9149, {	-- The Plagued Coast
-						i(28150)	-- Renzithen's Dusty Cloak
-					})),					
+--]]										
 --					qg(16199, qh( 9151)),	-- The Sanctum of the Sun
 --[[
 					qh( 9529, {	-- The Stone (LEGACY?  no information on where to pick up and most recent wowhead comment speculates it has been removed)
@@ -299,29 +537,9 @@ _.Zones =
 						i(22783), 	-- Sunwell Blade
 						i(22784)	-- Sunwell Orb
 					})),
---					qg(16224, qh( 9152)),	-- Tomber's Supplies
-					qg(16251, qh( 9199, {	-- Troll Juju
-						i( 28156),	-- Rotting Handwraps
-						i( 28159),	-- Undertaker's Gloves
-						i( 28163),	-- Maltendis' Handguards
-						i(131247)	-- Gravedigger's Grips
-					})),
 --[[					
-					qg(16251, qh( 9192)),	-- Trouble at the Underlight Mines
-					qg(16293, qh( 9207)),	-- Underlight Ore Samples
-					qg(16201, qh( 9174)),	-- Vanquishing Aquantion
-					o(181147, {	-- Wanted Poster
-						qh(9156),	-- Wanted: Knucklerot and Luzran
-					}),
 					qg(16204, qh( 9220)),	-- War on Deatholme
 --]]
-					qg(16197, qh( 9140, {	-- Windrunner Village
-						i( 23402),	-- Arcanist's Dagger
-						i( 28151),	-- Arcanist's Wand
-						i( 23403),	-- Salvaged Plate Leggings
-						i( 23404),	-- Padded Running Shoes
-						i(131241),	-- Relinked Chain Boots
-					})),					
 				}),
 				n(-16, { 	-- Rares	
 					n(22062, { -- Dr. Whitherlimb
