934563ae814a7ba1f8f0f2992e67c808f79764d1
Lucetia-Detheroc <lucetribunal@gmail.com>
2018-02-17 15:20:32 -0800

    - All 84 Elders have been added to the World Events -> Lunar Festival category [Currently not supported in each zone]

--- contrib/Parser/DATAS/4 - Events/11 Holidays/WorldEvents_Holidays_02LunarFestival.lua
+++ contrib/Parser/DATAS/4 - Events/11 Holidays/WorldEvents_Holidays_02LunarFestival.lua
@@ -18,6 +18,258 @@ _.WorldEvents =
 						q(8868, { -- Elune's Blessing
 							un(17, i(21540)), -- Elune's Lantern
 						}),
+						q(13033, {	-- Arp the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13013, {	-- Beldak the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8647, {	-- Bellowrage the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8715, {	-- Bladeleaf the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8719, {	-- Bladesing the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8718, {	-- Bladeswift the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8673, {	-- Bloodhoof the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13026, {	-- Bluewolf the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8726, {	-- Brightspear the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8866, {	-- Bronzebeard the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13067, {	-- Chogan'gada the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8648, {	-- Darkcore the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(29736, {	-- Darkfeather the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8677, {	-- Darkhorn the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8683, {	-- Dawnstrider the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(29734, {	-- Deepforge the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8684, {	-- Dreamseer the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(29740, {	-- Evershade the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13015, {	-- Fargal the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8727, {	-- Farwhisper the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(29737, {	-- Firebeard the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8653, {	-- Goldwell the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8652, {	-- Graveborn the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13028, {	-- Graymane the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8679, {	-- Grimtotem the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8646, {	-- Hammershout the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8686, {	-- High Mountain the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8643, {	-- Highpeak the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13021, {	-- Igasho the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8651, {	-- Ironband the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13017, {	-- Jarten the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13023, {	-- Kilias the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13025, {	-- Lunaro the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8722, {	-- Meadowrun the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(29742, {	-- Menkhaf the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8685, {	-- Mistwalker the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(29738, {	-- Moonlance the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8714, {	-- Moonstrike the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8717, {	-- Moonwarden the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8619, {	-- Morndeep the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8724, {	-- Morningdew the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13014, {	-- Morthie the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13032, {	-- Muraco the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8723, {	-- Nightwind the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13016, {	-- Northal the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13022, {	-- Nurgen the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8645, {	-- Obsidian the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13065, {	-- Ohanzee the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13029, {	-- Pamuya the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8654, {	-- Primestone the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8671, {	-- Ragetotem the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8725, {	-- Riversong the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8636, {	-- Rumblerock the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8670, {	-- Runetotem the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13018, {	-- Sandrene the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13012, {	-- Sardis the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(29741, {	-- Sekhemi the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8642, {	-- Silvervein the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8675, {	-- Skychaser the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8720, {	-- Skygleam the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8682, {	-- Skyseer the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13031, {	-- Skywarden the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8650, {	-- Snowcrown the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8635, {	-- Splitrock the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8716, {	-- Starglade the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8713, {	-- Starsong the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8721, {	-- Starweave the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13020, {	-- Stonebeard the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(29735, {	-- Stonebrand the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8644, {	-- Stonefort the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8672, {	-- Stonespire the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8649, {	-- Stormbrow the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13027, {	-- Tauros the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13019, {	-- Thoim the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8681, {	-- Thunderhorn the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13024, {	-- Wanikaya the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8678, {	-- Wheathoof the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13030, {	-- Whurain the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8676, {	-- Wildmane the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8688, {	-- Windrun the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(29739, {	-- Windsong the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8680, {	-- Windtotem the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(8674, {	-- Winterhoof the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
+						q(13066, {	-- Yurauk the Elder
+							-- i(21100), -- Coin of Ancestry
+						}),
 					}),
 					n(-2, { -- Vendors
 						n(15909, { -- Fariel Starsong <Coin of Ancestry Collector>
