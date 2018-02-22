c4faefa2573a6f8172e7c6ef5d7857a08b22dbd6
Lucetia-Detheroc <lucetribunal@gmail.com>
2018-02-22 09:41:03 -0800

    - Added Lunar Festival Elders to Maraudon, Sunken Temple and Zul'Farrak (Haven't parsed, wanted to push the changes before I left so slumbers didn't overlap)

--- contrib/Parser/DATAS/1 - Dungeons Raids/1 - Classic/2 - Dungeons/The Temple of Atal'hakkar.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/1 - Classic/2 - Dungeons/The Temple of Atal'hakkar.lua
@@ -8,6 +8,25 @@ _.Instances =
 		["groups"] = {
 			c(237, { 	-- The Temple of Atal'hakkar
 				["groups"] = {
+					n(-47, {	-- Lunar Festival						
+						["groups"] = {						
+							n(-17, {	-- Quests				
+								q(8713, {	-- Starsong the Elder			
+									["groups"] = {			
+										-- i(21100),	-- Coin of Ancestry	
+										-- i(21746, {	-- Lucky Red Envelope	
+											--i(21745),	-- Elder's Moonstone
+											--i(21744),	-- Lucky Rocket Cluster
+										--}),		
+									},			
+									["description"] = "Location: 62.4, 34.4",			
+									["qg"] = 15593,	-- Elder Starsong		
+									["u"] = 17,	-- Lunar Festival Holiday Filter		
+								}),				
+							}),					
+						},						
+						["achievementID"] = 913, -- To Honor One's Elders						
+					}),							
 					n(-17, {    		-- Quests          
 						qg(46077, q(27605)),	-- Eranikus
 						qg(46077, q(27604)),	-- Jammal'an the Prophet
