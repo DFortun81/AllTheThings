55021276d75ddb4a16c8359d4468070feb4fd3c8
CRIEVE-SARGERAS <dylanfortune@live.com>
2018-03-06 12:18:57 -0700

    Fixed the bug where completing a quest would increase the counter by 1 if you aren't treating Quests as Collectible.

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -4130,30 +4130,33 @@ function app.QuestCompletionHelper(questID)
 	-- Search ATT for the related quests.
 	local searchResults = SearchForQuestID(questID);
 	if searchResults and #searchResults > 0 then
-		-- Attempt to cleanly refresh the data.
-		for i,result in ipairs(searchResults) do
-			if result.visible and result.parent and result.parent.total then
-				result.marked = true;
+		-- Only increase progress for Quests as Collectible users.
+		if GetDataMember("TreatIncompleteQuestsAsCollectible") then
+			-- Attempt to cleanly refresh the data.
+			for i,result in ipairs(searchResults) do
+				if result.visible and result.parent and result.parent.total then
+					result.marked = true;
+				end
 			end
-		end
-		for i,result in ipairs(searchResults) do
-			if result.marked then
-				result.marked = nil;
-				if result.total then
-					-- This is an item that has a relative set of groups
-					UpdateParentProgress(result);
-					
-					-- If this is NOT a group...
-					if not result.groups and result.collectible then
-						-- If we've collected the item, use the "Show Collected Items" filter.
-						result.visible = app.CollectedItemVisibilityFilter(result);
-					end
-				else	
-					UpdateParentProgress(result.parent);
-					
-					if result.collectible then
-						-- If we've collected the item, use the "Show Collected Items" filter.
-						result.visible = app.CollectedItemVisibilityFilter(result);
+			for i,result in ipairs(searchResults) do
+				if result.marked then
+					result.marked = nil;
+					if result.total then
+						-- This is an item that has a relative set of groups
+						UpdateParentProgress(result);
+						
+						-- If this is NOT a group...
+						if not result.groups and result.collectible then
+							-- If we've collected the item, use the "Show Collected Items" filter.
+							result.visible = app.CollectedItemVisibilityFilter(result);
+						end
+					else
+						UpdateParentProgress(result.parent);
+						
+						if result.collectible then
+							-- If we've collected the item, use the "Show Collected Items" filter.
+							result.visible = app.CollectedItemVisibilityFilter(result);
+						end
 					end
 				end
 			end
