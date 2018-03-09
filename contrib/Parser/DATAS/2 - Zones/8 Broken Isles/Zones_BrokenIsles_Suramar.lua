21bf37ba6c1dd8979f7101e7d26a4ff2a266fbab
Lucetia-Detheroc <lucetribunal@gmail.com>
2018-03-09 12:48:35 -0800

    - Fixed Suramar's Emissary

--- contrib/Parser/DATAS/2 - Zones/8 Broken Isles/Zones_BrokenIsles_Suramar.lua
+++ contrib/Parser/DATAS/2 - Zones/8 Broken Isles/Zones_BrokenIsles_Suramar.lua
@@ -132,7 +132,333 @@ _.Zones =
 					p(1807), 	-- Vicious Broodling
 					p(1591), 	-- Violet Firefly
 				}),
-				
+				n(-169, { -- Emissary Quests
+					q(42421, { -- The Nightfallen
+						i(157826, { -- Nightfallen Hoard
+							["groups"] = {
+								i(140495, { -- Torn Invitation [Fox Mount Quest]
+									i(137573), -- Reins of the Llothien Prowler
+								}),
+								n(-322, { -- Cloak
+									i(121798), -- Moon Guard Drape
+									i(134290), -- Nightborne Noble's Cloak
+								}),
+								n(-3243, { -- Manawracker Set
+									["groups"] = {
+										i(134304),	-- Manawracker Crown
+										i(134309),	-- Manawracker Shoulders
+										i(134307),	-- Manawracker Vestments
+										i(134310),	-- Manawracker Bindings
+										i(134305),	-- Manawracker Gloves
+										i(134303),	-- Manawracker Waistcord
+										i(134306),	-- Manawracker Pants
+										i(134308),	-- Manawracker Sandals
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_cloth_legionendgame_c_01",
+								}),
+								n(-3262, { -- Swordsinger Set
+									["groups"] = {
+										i(134284),	-- Swordsinger's Hood
+										i(134286),	-- Swordsinger's Shoulders
+										i(134285),	-- Swordsinger's Vest
+										i(134280),	-- Swordsinger's Wristguards
+										i(134283),	-- Swordsinger's Grips
+										i(134287),	-- Swordsinger's Belt
+										i(134282),	-- Swordsinger's Leggings
+										i(134281),	-- Swordsinger's Footpads
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_leather_legionendgame_c_01",
+								}),
+								n(-3274, { -- Ley Dragoon Set
+									["groups"] = {
+										i(134299),	-- Ley Dragoon's Headgear
+										i(134301),	-- Ley Dragoon's Spaulders
+										i(134302),	-- Ley Dragoon's Hauberk
+										i(134296),	-- Ley Dragoon's Wristbraces
+										i(134297),	-- Ley Dragoon's Gloves
+										i(134295),	-- Ley Dragoon's Belt
+										i(134300),	-- Ley Dragoon's Legguards
+										i(134298),	-- Ley Dragoon's Stompers
+									},
+									["icon"] = "Interface\\Icons\\inv_helmet_mail_legionendgame_c_01",
+								}),
+								n(-3299, { -- Arcane Defender Set
+									["groups"] = {
+										i(134270),	-- Arcane Defender's Helm
+										i(134272),	-- Arcane Defender's Pauldrons
+										i(134267),	-- Arcane Defender's Breastplate
+										i(134274),	-- Arcane Defender's Wristguards
+										i(134268),	-- Arcane Defender's Gauntlets
+										i(134269),	-- Arcane Defender's Belt
+										i(134271),	-- Arcane Defender's Pants
+										i(134273),	-- Arcane Defender's Boots
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_plate_legionendgame_c_01",
+								}),
+							},
+						}),
+						un(2, i(154907, { -- Nightfallen Hoard
+							["groups"] = {
+								i(140495, { -- Torn Invitation [Fox Mount Quest]
+									i(137573), -- Reins of the Llothien Prowler
+								}),
+								n(-322, { -- Cloak
+									i(121798), -- Moon Guard Drape
+									i(134290), -- Nightborne Noble's Cloak
+								}),
+								n(-3243, { -- Manawracker Set
+									["groups"] = {
+										i(134304),	-- Manawracker Crown
+										i(134309),	-- Manawracker Shoulders
+										i(134307),	-- Manawracker Vestments
+										i(134310),	-- Manawracker Bindings
+										i(134305),	-- Manawracker Gloves
+										i(134303),	-- Manawracker Waistcord
+										i(134306),	-- Manawracker Pants
+										i(134308),	-- Manawracker Sandals
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_cloth_legionendgame_c_01",
+								}),
+								n(-3262, { -- Swordsinger Set
+									["groups"] = {
+										i(134284),	-- Swordsinger's Hood
+										i(134286),	-- Swordsinger's Shoulders
+										i(134285),	-- Swordsinger's Vest
+										i(134280),	-- Swordsinger's Wristguards
+										i(134283),	-- Swordsinger's Grips
+										i(134287),	-- Swordsinger's Belt
+										i(134282),	-- Swordsinger's Leggings
+										i(134281),	-- Swordsinger's Footpads
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_leather_legionendgame_c_01",
+								}),
+								n(-3274, { -- Ley Dragoon Set
+									["groups"] = {
+										i(134299),	-- Ley Dragoon's Headgear
+										i(134301),	-- Ley Dragoon's Spaulders
+										i(134302),	-- Ley Dragoon's Hauberk
+										i(134296),	-- Ley Dragoon's Wristbraces
+										i(134297),	-- Ley Dragoon's Gloves
+										i(134295),	-- Ley Dragoon's Belt
+										i(134300),	-- Ley Dragoon's Legguards
+										i(134298),	-- Ley Dragoon's Stompers
+									},
+									["icon"] = "Interface\\Icons\\inv_helmet_mail_legionendgame_c_01",
+								}),
+								n(-3299, { -- Arcane Defender Set
+									["groups"] = {
+										i(134270),	-- Arcane Defender's Helm
+										i(134272),	-- Arcane Defender's Pauldrons
+										i(134267),	-- Arcane Defender's Breastplate
+										i(134274),	-- Arcane Defender's Wristguards
+										i(134268),	-- Arcane Defender's Gauntlets
+										i(134269),	-- Arcane Defender's Belt
+										i(134271),	-- Arcane Defender's Pants
+										i(134273),	-- Arcane Defender's Boots
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_plate_legionendgame_c_01",
+								}),
+							},
+						})),
+						un(2, i(151468, { -- Nightfallen Hoard
+							["groups"] = {
+								i(140495, { -- Torn Invitation [Fox Mount Quest]
+									i(137573), -- Reins of the Llothien Prowler
+								}),
+								n(-322, { -- Cloak
+									i(121798), -- Moon Guard Drape
+									i(134290), -- Nightborne Noble's Cloak
+								}),
+								n(-3243, { -- Manawracker Set
+									["groups"] = {
+										i(134304),	-- Manawracker Crown
+										i(134309),	-- Manawracker Shoulders
+										i(134307),	-- Manawracker Vestments
+										i(134310),	-- Manawracker Bindings
+										i(134305),	-- Manawracker Gloves
+										i(134303),	-- Manawracker Waistcord
+										i(134306),	-- Manawracker Pants
+										i(134308),	-- Manawracker Sandals
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_cloth_legionendgame_c_01",
+								}),
+								n(-3262, { -- Swordsinger Set
+									["groups"] = {
+										i(134284),	-- Swordsinger's Hood
+										i(134286),	-- Swordsinger's Shoulders
+										i(134285),	-- Swordsinger's Vest
+										i(134280),	-- Swordsinger's Wristguards
+										i(134283),	-- Swordsinger's Grips
+										i(134287),	-- Swordsinger's Belt
+										i(134282),	-- Swordsinger's Leggings
+										i(134281),	-- Swordsinger's Footpads
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_leather_legionendgame_c_01",
+								}),
+								n(-3274, { -- Ley Dragoon Set
+									["groups"] = {
+										i(134299),	-- Ley Dragoon's Headgear
+										i(134301),	-- Ley Dragoon's Spaulders
+										i(134302),	-- Ley Dragoon's Hauberk
+										i(134296),	-- Ley Dragoon's Wristbraces
+										i(134297),	-- Ley Dragoon's Gloves
+										i(134295),	-- Ley Dragoon's Belt
+										i(134300),	-- Ley Dragoon's Legguards
+										i(134298),	-- Ley Dragoon's Stompers
+									},
+									["icon"] = "Interface\\Icons\\inv_helmet_mail_legionendgame_c_01",
+								}),
+								n(-3299, { -- Arcane Defender Set
+									["groups"] = {
+										i(134270),	-- Arcane Defender's Helm
+										i(134272),	-- Arcane Defender's Pauldrons
+										i(134267),	-- Arcane Defender's Breastplate
+										i(134274),	-- Arcane Defender's Wristguards
+										i(134268),	-- Arcane Defender's Gauntlets
+										i(134269),	-- Arcane Defender's Belt
+										i(134271),	-- Arcane Defender's Pants
+										i(134273),	-- Arcane Defender's Boots
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_plate_legionendgame_c_01",
+								}),
+							},
+						})),
+						un(2, i(146751, { -- Nightfallen Hoard
+							["groups"] = {
+								i(140495, { -- Torn Invitation [Fox Mount Quest]
+									i(137573), -- Reins of the Llothien Prowler
+								}),
+								n(-322, { -- Cloak
+									i(121798), -- Moon Guard Drape
+									i(134290), -- Nightborne Noble's Cloak
+								}),
+								n(-3243, { -- Manawracker Set
+									["groups"] = {
+										i(134304),	-- Manawracker Crown
+										i(134309),	-- Manawracker Shoulders
+										i(134307),	-- Manawracker Vestments
+										i(134310),	-- Manawracker Bindings
+										i(134305),	-- Manawracker Gloves
+										i(134303),	-- Manawracker Waistcord
+										i(134306),	-- Manawracker Pants
+										i(134308),	-- Manawracker Sandals
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_cloth_legionendgame_c_01",
+								}),
+								n(-3262, { -- Swordsinger Set
+									["groups"] = {
+										i(134284),	-- Swordsinger's Hood
+										i(134286),	-- Swordsinger's Shoulders
+										i(134285),	-- Swordsinger's Vest
+										i(134280),	-- Swordsinger's Wristguards
+										i(134283),	-- Swordsinger's Grips
+										i(134287),	-- Swordsinger's Belt
+										i(134282),	-- Swordsinger's Leggings
+										i(134281),	-- Swordsinger's Footpads
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_leather_legionendgame_c_01",
+								}),
+								n(-3274, { -- Ley Dragoon Set
+									["groups"] = {
+										i(134299),	-- Ley Dragoon's Headgear
+										i(134301),	-- Ley Dragoon's Spaulders
+										i(134302),	-- Ley Dragoon's Hauberk
+										i(134296),	-- Ley Dragoon's Wristbraces
+										i(134297),	-- Ley Dragoon's Gloves
+										i(134295),	-- Ley Dragoon's Belt
+										i(134300),	-- Ley Dragoon's Legguards
+										i(134298),	-- Ley Dragoon's Stompers
+									},
+									["icon"] = "Interface\\Icons\\inv_helmet_mail_legionendgame_c_01",
+								}),
+								n(-3299, { -- Arcane Defender Set
+									["groups"] = {
+										i(134270),	-- Arcane Defender's Helm
+										i(134272),	-- Arcane Defender's Pauldrons
+										i(134267),	-- Arcane Defender's Breastplate
+										i(134274),	-- Arcane Defender's Wristguards
+										i(134268),	-- Arcane Defender's Gauntlets
+										i(134269),	-- Arcane Defender's Belt
+										i(134271),	-- Arcane Defender's Pants
+										i(134273),	-- Arcane Defender's Boots
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_plate_legionendgame_c_01",
+								}),
+							},
+						})),
+						un(2, i(137564, { -- Nightfallen Hoard
+							["groups"] = {
+								i(140495, { -- Torn Invitation [Fox Mount Quest]
+									i(137573), -- Reins of the Llothien Prowler
+								}),
+								n(-322, { -- Cloak
+									i(121798), -- Moon Guard Drape
+									i(134290), -- Nightborne Noble's Cloak
+								}),
+								n(-3243, { -- Manawracker Set
+									["groups"] = {
+										i(134304),	-- Manawracker Crown
+										i(134309),	-- Manawracker Shoulders
+										i(134307),	-- Manawracker Vestments
+										i(134310),	-- Manawracker Bindings
+										i(134305),	-- Manawracker Gloves
+										i(134303),	-- Manawracker Waistcord
+										i(134306),	-- Manawracker Pants
+										i(134308),	-- Manawracker Sandals
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_cloth_legionendgame_c_01",
+								}),
+								n(-3262, { -- Swordsinger Set
+									["groups"] = {
+										i(134284),	-- Swordsinger's Hood
+										i(134286),	-- Swordsinger's Shoulders
+										i(134285),	-- Swordsinger's Vest
+										i(134280),	-- Swordsinger's Wristguards
+										i(134283),	-- Swordsinger's Grips
+										i(134287),	-- Swordsinger's Belt
+										i(134282),	-- Swordsinger's Leggings
+										i(134281),	-- Swordsinger's Footpads
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_leather_legionendgame_c_01",
+								}),
+								n(-3274, { -- Ley Dragoon Set
+									["groups"] = {
+										i(134299),	-- Ley Dragoon's Headgear
+										i(134301),	-- Ley Dragoon's Spaulders
+										i(134302),	-- Ley Dragoon's Hauberk
+										i(134296),	-- Ley Dragoon's Wristbraces
+										i(134297),	-- Ley Dragoon's Gloves
+										i(134295),	-- Ley Dragoon's Belt
+										i(134300),	-- Ley Dragoon's Legguards
+										i(134298),	-- Ley Dragoon's Stompers
+									},
+									["icon"] = "Interface\\Icons\\inv_helmet_mail_legionendgame_c_01",
+								}),
+								n(-3299, { -- Arcane Defender Set
+									["groups"] = {
+										i(134270),	-- Arcane Defender's Helm
+										i(134272),	-- Arcane Defender's Pauldrons
+										i(134267),	-- Arcane Defender's Breastplate
+										i(134274),	-- Arcane Defender's Wristguards
+										i(134268),	-- Arcane Defender's Gauntlets
+										i(134269),	-- Arcane Defender's Belt
+										i(134271),	-- Arcane Defender's Pants
+										i(134273),	-- Arcane Defender's Boots
+									},
+									["icon"] = "Interface\\Icons\\inv_helm_plate_legionendgame_c_01",
+								}),
+							},
+						})),
+					}),
+					n(-206, { -- Paragon
+						q(46748, { -- Supplies From the Nightfallen
+							i(152105), -- Leywoven Flying Carpet
+							i(140495, { -- Torn Invitation [Fox Mount Quest]
+								i(137573), -- Reins of the Llothien Prowler
+							}),
+						}),
+					}),
+				}),
 				n(-17, { -- Quests			
 					n(-34, {	-- World Quests
 --[[
