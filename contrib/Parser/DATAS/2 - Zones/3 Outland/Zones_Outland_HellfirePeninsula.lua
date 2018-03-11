9590233d822dc02cf1e3c33117d03061640d33fc
Lucetia-Detheroc <lucetribunal@gmail.com>
2018-03-11 00:53:44 -0800

    -  Hellfire Peninsula has all of it's Holiday quests available - Part of the quests for the Achievement Critera "Disrupting the Burning Legion" [Horde] side has now been added with it's proper chains.  (Work in Progress).

--- contrib/Parser/DATAS/2 - Zones/3 Outland/Zones_Outland_HellfirePeninsula.lua
+++ contrib/Parser/DATAS/2 - Zones/3 Outland/Zones_Outland_HellfirePeninsula.lua
@@ -5,117 +5,212 @@ _.Zones =
 {
 	m(466, { 	-- Outland
 		m(465, {	-- Hellfire Peninsula			
-			["groups"] = {			
-				n(18728, { 	-- Doom Lord Kazzak
-					["groups"] = {
-						i(30735),	-- Ancient Spellcloak of the Highborne
-						i(30734),	-- Leggings of the Seventh Circle
-						i(30737),	-- Gold-Leaf Wildboots
-						i(30739),	-- Scaled Greaves of the Marksman
-						i(30740),	-- Ripfiend Shoulderplates
-						i(30741),	-- Topaz-Studded Battlegrips
-						i(30733),	-- Hope Ender
-						i(30732),	-- Exodar Life-Staff
-					},
-					["description"] = "|cff66ccffLocated at the Throne of Kil'jaeden, Hellfire Penninsula|r",
-					["isRaid"] = true,
-				}),			
-				n(-3, { 	-- Holidays					
---[[					
-					n(-47, {     -- Lunar Festival
+			["groups"] = {
+				n(-7, { -- World Bosses
+					n(18728, { 	-- Doom Lord Kazzak
 						["groups"] = {
-							n(-17, { 	-- Quests
-							}),
+							i(30735),	-- Ancient Spellcloak of the Highborne
+							i(30734),	-- Leggings of the Seventh Circle
+							i(30737),	-- Gold-Leaf Wildboots
+							i(30739),	-- Scaled Greaves of the Marksman
+							i(30740),	-- Ripfiend Shoulderplates
+							i(30741),	-- Topaz-Studded Battlegrips
+							i(30733),	-- Hope Ender
+							i(30732),	-- Exodar Life-Staff
 						},
-						["achievementID"] = 913, -- To Honor One's Elders
-						["u"] = 17,
+						["description"] = "|cff66ccffLocated at the Throne of Kil'jaeden, Hellfire Penninsula|r",
+						["isRaid"] = true,
 					}),
-					n(-50, {     -- Love is in the Air
-						["groups"] = {
-							n(-17, { 	-- Quests
-							}),
-						},
-						["achievementID"] = 1693,    -- Fool For Love
-						["u"] = 18,
-					}),
-					n(-51, {     -- Noblegarden
-						["groups"] = {
-							n(-17, { 	-- Quests
-							}),
-						},
-						["achievementID"] = 2798, -- Noble Gardener
-						["u"] = 19,
-					}),
-					n(-52, {     -- Children's Week
-						["groups"] = {
-							n(-17, { 	-- Quests
-							}),
-						},
-						["achievementID"] = 1793, -- For The Children
-						["u"] = 20,
-					}),
---]]					
+				}),
+				n(-3, { 	-- Holidays	
 					n(-53, {     -- Midsummer Fire Festival
 						["groups"] = {
 							n(-17, { 	-- Quests
-								qg(25994, qh( 11915)),	-- Playing with Fire
-								qg(25962, qa( 11882)),	-- Playing with Fire
+								qa(11775, { -- Desecrate this Fire!
+									["groups"] = {
+									},
+									["description"] = "Thrallmar 57.3, 41,8", -- Location
+									["u"] = 21, -- Midsummer Fire Festival
+								}),
+								qh(11747, { -- Desecrate this Fire!
+									["groups"] = {
+									},
+									["description"] = "Honor Hold 61.9, 58.5", -- Location
+									["u"] = 21, -- Midsummer Fire Festival
+								}),
+								qa(11818, { -- Honor the Flame
+									["groups"] = {
+									},
+									["qg"] = 25900, -- Hellfire Peninsulaa Flame Warden
+									["u"] = 21, -- Midsummer Fire Festival
+								}),
+								qh(11851, { -- Honor the Flame
+									["groups"] = {
+									},
+									["qg"] = 25934, -- Hellfire Peninsulaa Flame Warden
+									["u"] = 21, -- Midsummer Fire Festival
+								}),
+								qa(11882, { -- Playing With Fire
+									["groups"] = {
+									},
+									["qg"] = 25962, -- Flame Eater
+									["u"] = 21, -- Midsummer Fire Festival
+								}),
+								qh(11915, { -- Playing With Fire
+									["groups"] = {
+									},
+									["qg"] = 25994, -- Flame Eater
+									["u"] = 21, -- Midsummer Fire Festival
+								}),
 							}),
 						},
 						["achievementID"] = 1038, -- The Flame Warden
 						["u"] = 21,
 					}),
---[[					
-					n(-56, {     -- Brewfest
-						["groups"] = {
-							n(-17, { 	-- Quests
-							}),
-						},
-						["achievementID"] = 1683, -- Brewmaster
-						["u"] = 24,
-					}),
 					n(-58, {     -- Hallow's End
 						["groups"] = {
 							n(-17, { 	-- Quests
+								qa(12352, { -- Candy Bucket
+									["groups"] = {
+									},
+									["description"] = "Honor Hold", -- Location
+									["u"] = 26, -- Hallow's End
+								}),
+								qa(12353, { -- Candy Bucket
+									["groups"] = {
+									},
+									["description"] = "Temple of Telhamat", -- Location
+									["u"] = 26, -- Hallow's End
+								}),
+								qh(12389, { -- Candy Bucket
+									["groups"] = {
+									},
+									["description"] = "Falcon Watch", -- Location
+									["u"] = 26, -- Hallow's End
+								}),
+								qh(12388, { -- Candy Bucket
+									["groups"] = {
+									},
+									["description"] = "Thrallmar", -- Location
+									["u"] = 26, -- Hallow's End
+								}),
 							}),
 						},
 						["achievementID"] = 1656, -- Hallowed Be Thy Name
-						["u"] = 26,
 					}),	
-					n(-60, {     -- Pilgrim's Bounty
-						["groups"]  = {
-							n(-17, { 	-- Quests
-							}),
-						},
-						["achievementID"] = 3478, -- Pilgrim
-						["u"] = 28,
-					}),
-					n(-61, {     -- Feast of Winter Veil
-						["groups"] = {
-							n(-17, { 	-- Quests
-							}),
-						},
-						["achievementID"] = 1691, -- Merrymaker
-						["u"] = 29,
-					}),		
---]]				
 				}),								
 				n(-25, { 	-- Pet Battle
 					p(635), -- Adder
 					p(414), -- Scorpid
 				}),
-				n(-17, {  	-- Quests 
---[[				
+			n(-17, {  	-- Quests
+				ach(1271, { -- To Hellfire and Back [Horde Version]
+					crit(1, { -- Disrupt the Burning Legion
+						qh(49816, { -- To Outland! [Breadcrumb]
+							["groups"] = {
+							},
+						}),
+						qh(10120, { -- Arrival in Outland
+							["groups"] = {
+							},
+							["qg"] = 19253, -- Lieutenant General Orion
+							["sourceQuestID"] = { 49816 }, -- To Outland [Breadcrumb]
+						}),
+						qh(10289, { -- Journey to Thrallmar
+							["groups"] = {
+							},
+							["qg"] = 18930, -- Vlagga Freyfeather
+							["sourceQuestID"] = { 10120 }, -- Arrival in Outlanad
+						}),
+						qh(10291, { -- Report to Nazgrel
+							["groups"] = {
+							},
+							["qg"] = 19255, -- General Krakork
+							["sourceQuestID"] = { 10289 }, -- Journey to Thrallmar
+						}),
+						qh(10086, { -- I Work... For the Horde!
+							["groups"] = {
+								i(29931), -- Phantasmal Headdress
+								i(29938), -- Battle Seeker Chesguard
+								i(29943), -- Legionnaire's Studded Helm
+								i(29945), -- Magistrate's Greaves
+							},
+							["qg"] = 21283, -- Megzeg Nukklebust
+						}),
+						qh(10121, { -- Eradicate the Burning Legion
+							["groups"] = {
+							},
+							["qg"] = 3230, -- Nazgrel
+							["sourceQuestID"] = { 10291 }, -- Report to Nazgrel
+						}),
+						qh(10450, { -- Bonechewer Blood
+							["groups"] = {
+								i(29915), -- Desolation Rod
+								i(29919), -- Adamantine Kite Shield
+								i(29917), -- Landslide Buckler
+								i(29914), -- Hellfire Skiver
+								i(29916), -- Ironstar Repeater
+							},
+							["qg"] = 21256, -- Vurtok Axebreaker
+							["sourceQuestID"] = { 10291 }, -- Report to Nazgrel
+						}),
+						qh(10087, { -- Burn It Up... For the Horde!
+							["groups"] = {
+								i(29928), -- Wanderer's Stitched Trousers
+								i(29930), -- Nature-Stitched Kilt
+								i(29934), -- Helm of Affinity
+								i(29941), -- Scale Brand Breastplate
+							},
+							["qg"] = 21283, -- Megzeg Nukklebust
+							["sourceQuestID"] = { 10086 }, -- Work For the Horde
+						}),
+						qh(10123, { -- Felspark Ravine
+							["groups"] = {
+								i(29908), -- Rage Reaver
+								i(29909), -- Screaming Dagger
+								i(29910), -- The Staff of Twin Worlds
+								i(29911), -- Agamaggan's Quill
+								i(29913), -- Foe Reaver
+							},
+							["qg"] = 19256, -- Sergeant Shatterskull
+							["sourceQuestID"] = { 10121 }, -- Eradicate the Burning Legion
+						}),
+						qh(10449, { -- Apothecary Zelana
+							["groups"] = {
+							},
+							["qg"] = 21256, -- Vurtok Axebreaker
+							["sourceQuestID"] = { 10450 }, -- Bonechewer Blood
+						}),
+						q(9498, { -- Falcon Watch [Breadcrumb]
+							["groups"] = {
+							},
+							["qg"] = 21256, -- Vurtok Axebreaker
+							["sourceQuestID"] = { 10450 }, -- Bonechewer Blood
+							["races"] = { 2, 5, 6, 8, 9, 26, 27, 28 }, -- Every race but Blood Elves
+						}),
+						q(9499, { -- Falcon Watch [Breadcrumb]
+							["groups"] = {
+							},
+							["qg"] = 21256, -- Vurtok Axebreaker
+							["sourceQuestID"] = { 10450 }, -- Bonechewer Blood
+							["races"] = { 10 }, -- Blood Elves
+						}),
+						qh(10124, { -- Forward Base: Reaver's Fall
+							["groups"] = {
+							},
+							["qg"] = 19256, -- Sergeeant Shatterskull
+							["sourceQuestID"] = { 10123 }, -- Felspark Ravine
+						}),
+					}),
+				}),
 					qh( 10864),	-- A Burden of Souls
 					qh(  9442),	-- A Debilitating Sickness
 					qa(  9355),	-- A Job for an Intelligent Man
---]]					
 					qh(9376,  {  -- A Pilgrim's Plight
-						i(25783),
-						i(25782),
-						i(25781),
+						i(25783), -- Pilgrim's Cover
+						i(25782), -- Sunstrider Legguards
+						i(25781), -- Segmented Breastplate
 					}),
---[[					
 					qh(  9410),	-- A Spirit Guide
 					qh(  9401),	-- A Strange Weapon
 					q(10367),	-- A Traitor Among Us
@@ -132,7 +227,6 @@ _.Zones =
 					qa( 10288),	-- Arrival in Outland
 					q(10369),	-- Arzeth's Demise
 					qa(  9543),	-- Atonement
---]]					
 					q(9418,  {  -- Avruu's Orb
 						i(25489),
 					}),
@@ -141,7 +235,6 @@ _.Zones =
 						i(30855),
 						i(30857),
 					}),
---[[					
 					q(45415),	-- Between Worlds
 					qh(  9397),	-- Birds of a Feather
 					q(11516),	-- Blast the Gateway
@@ -149,28 +242,12 @@ _.Zones =
 					qa( 47025),	-- Blood: Aid of the Illidari
 					qh( 10250),	-- Bloody Vengeance
 					qh( 10538),	-- Boiling Blood
---]]					
-					qh(10450,  {  -- Bonechewer Blood
-						i(29915),
-						i(29919),
-						i(29917),
-						i(29914),
-						i(29916),
-					}),
 					qa( 47024),	-- Brewmaster: Aid of the Illidari
-					qh(10087,  {  -- Burn It Up... For the Horde!
-						i(29928),
-						i(29930),
-						i(29934),
-						i(29941),
-					}),
---[[					
 					qh( 12388),	-- Candy Bucket
 					qh( 12389),	-- Candy Bucket
 					qa( 12352),	-- Candy Bucket
 					qa( 12353),	-- Candy Bucket
 					qh( 10391),	-- Cannons of Rage
---]]					
 					qa(9427, {  -- Cleansing the Waters
 						i(25486), 
 					}),
@@ -178,7 +255,6 @@ _.Zones =
 						i(28062),
 						i(28063),
 					}),
---[[					
 					q(45414),	-- Confirming Suspicions
 					q(10134),	-- Crimson Crystal Clue
 					qa(  9399),	-- Cruel Taskmasters
@@ -199,7 +275,6 @@ _.Zones =
 					qh( 11581),	-- Desecrate this Fire!
 					qh( 11745),	-- Desecrate this Fire!
 					qa( 10916),	-- Digging for Prayer Beads
---]]					
 					qa(10144, {  -- Disrupt Their Reinforcements
 						i(29927),
 						i(29939),
@@ -214,26 +289,19 @@ _.Zones =
 						i(29946),
 						i(29935),
 					}),	
---[[					
 					qa( 10394),	-- Disruption - Forge Camp: Mageddon
 					qh( 10392),	-- Doorway to the Abyss
---]]					
 					qa(10937,  {  -- Drill the Drillmaster
 						i(31719),
 						i(31718),
 						i(31717),
 						i(31720),
 					}),
---[[					
 					qa( 10763),	-- Dumphry's Request
 					qa( 10396),	-- Enemy of my Enemy...
 					qa( 10754),	-- Entry Into the Citadel
-					qh( 10121),	-- Eradicate the Burning Legion
 					qa( 10143),	-- Expedition Point
-					qh(  9499),	-- Falcon Watch
-					qh(  9498),	-- Falcon Watch
 					qa( 10919),	-- Fei Fei's Treat
---]]					
 					qa(10482,  {  -- Fel Orc Scavengers
 						i(29915),
 						i(29919),
@@ -241,31 +309,17 @@ _.Zones =
 						i(29914),
 						i(29916),
 					}),
---[[					
 					qa( 10909),	-- Fel Spirits
---]]
-					qh(10123,  {  -- Felspark Ravine
-						i(29908),
-						i(29909),
-						i(29910),
-						i(29911),
-						i(29913),
-					}),
---[[					
 					qa( 10254),	-- Force Commander Danath
 					qh( 10390),	-- Forge Camp: Mageddon
-					qh( 10124),	-- Forward Base: Reaver's Fall
---]]
 					qh(10295,  {  -- From the Abyss
 						i(29399),
 						i(29400),
 					}),
---[[					
 					qa(  9563),	-- Gaining Mirren's Trust
 					q(45413),	-- Gathering Information
 					qa( 10382),	-- Go to the Front
 					qa( 10762),	-- Grand Master Dumphry
---]]					
 					qh(10834,  {  -- Grillok "Darkeye"
 						i(28057),
 						i(28052),
@@ -273,7 +327,6 @@ _.Zones =
 						i(28055),
 						i(28051),
 					}),
---[[					
 					qa( 47023),	-- Guardian: Aid of the Illidari
 					q(9361),	-- Helboar, the Other White Meat
 					qa( 10106),	-- Hellfire Fortifications
@@ -285,30 +338,20 @@ _.Zones =
 					qh( 10442),	-- Helping the Cenarion Post
 					qa( 10443),	-- Helping the Cenarion Post
 					qa( 28708),	-- Hero's Call: Outland!
---]]					
 					qh(10258,  {  -- Honor the Fallen
 						i(29108),
 						i(29109),
 					}),
---[[					
 					qa( 11818),	-- Honor the Flame
 					qh( 11851),	-- Honor the Flame
 					qa( 10764),	-- Hotter than Hell					
 					q(10238),	-- How to Serve Goblins
---]]					
-					qh(10086,  {  -- I Work... For The Horde!
-						i(29931),
-						i(29938),
-						i(29943),
-						i(29945),
-					}),						
---					qa( 10483),	-- Ill Omens
+					qa( 10483),	-- Ill Omens
 					q(10161,  {  -- In Case of Emergency...
 						i(25981),
 						i(25980),
 						i(25979),
 					}),
---[[					
 					qh(  9366),	-- In Need of Felblood
 					qa(  9390),	-- In Search of Sedai
 					qa( 11964),	-- Incense for the Summer Scorchlings
@@ -320,19 +363,16 @@ _.Zones =
 					qh( 10289),	-- Journey to Thrallmar
 					q(10159),	-- Keep Thornfang Hill Clear!
 					qa( 10160),	-- Know your Enemy
---]]					
 					qa(10078,  {  -- Laying Waste to the Unwanted
 						i(29928),
 						i(29930),
 						i(29934),
 						i(29941),
 					}),
---[[					
 					qh(  9483),	-- Life's Finer Pleasures
 					qa( 10057),	-- Looking to the Leadership
 					qh(  9396),	-- Magic of the Arakkoa
 					qh( 10220),	-- Make Them Listen
---]]					
 					qa(9424,  {  -- Makuru's Vengeance
 						i(25920),
 					}),
@@ -341,7 +381,7 @@ _.Zones =
 						i(25502),
 						i(25504),
 					}),
---					q(9373),	-- Missing Missive
+					q(9373),	-- Missing Missive
 					qh(10129,  {  -- Mission: Gateways Murketh and Shaadraz
 						i(29942),
 						i(29937),
@@ -373,7 +413,6 @@ _.Zones =
 						i(28069),
 						i(28070),
 					}),
---[[					
 					q(31922),	-- Nicki Tinytech
 					q(10236),	-- Outland Sucks!
 					qa( 10400),	-- Overlord
@@ -398,38 +437,33 @@ _.Zones =
 					qa( 10340),	-- Shatter Point
 					q(10629),	-- Shizz Work
 					q(9356),	-- Smooth as Butter
---]]					
 					qh(9387,  {  -- Source of the Corruption
 						i(25915),
 					}),
---					qh( 10242),	-- Spinebreaker Post
+					qh( 10242),	-- Spinebreaker Post
 					q(10255,  {  -- Testing the Antidote
 						i(25986),
 						i(25987),
 						i(25985),
 					}),
---[[					
 					qh( 10389),	-- The Agony and the Darkness
 					qa(  9417),	-- The Arakkoa Threat
 					qh(  9400),	-- The Assassin
 					qh( 10230),	-- The Battle Horn
 					q(9912),	-- The Cenarion Expedition
---]]					
 					qh(9370,  {  -- The Cleansing Must Be Stopped
 						i(25501),
 					}),
---					qa( 10395),	-- The Dark Missive
+					qa( 10395),	-- The Dark Missive
 					qh(10838,  {  -- The Demoniac Scryer
 						i(31715),
 					}),
---[[					
 					q(10368),	-- The Dreghood Elders
 					q(10349),	-- The Earthbinder
 					qa( 10935),	-- The Exorcism of Colonel Jules
 					qh( 10813),	-- The Eyes of Grillok
 					qh( 11003),	-- The Fall of Magtheridon
 					qa( 11002),	-- The Fall of Magtheridon
---]]					
 					qa(9420,  {  -- The Finest Down
 						i(23587),
 					}),
@@ -439,12 +473,10 @@ _.Zones =
 						i(31718),
 						i(31720),
 					}),	
---[[					
 					qh(  9340),	-- The Great Fissure
 					qa( 10399),	-- The Heart of Darkness
 					qa( 10141),	-- The Legion Reborn
 					qa(  9558),	-- The Longbeards
---]]					
 					qh(9406,  {  -- The Mag'har
 						i(25510),
 						i(25512),
@@ -456,10 +488,8 @@ _.Zones =
 						i(25982),
 						i(25983),
 					}),
---[[					
 					q(11526),	-- The Missing Magistrix
 					qh( 10287),	-- The Mistress Revealed
---]]					
 					qa(10142,  {  -- The Path of Anguish
 						i(29908),
 						i(29909),
@@ -467,11 +497,9 @@ _.Zones =
 						i(29911),
 						i(29913),
 					}),
---[[					
 					qa( 10047),	-- The Path of Glory
 					qa(  9426),	-- The Pools of Aggonar
 					qh(  9375),	-- The Road to Falcon Watch
---]]
 					qa(9490,  {  -- The Rock Flayer Matriarch
 						i(25480),
 						i(25479),
@@ -482,7 +510,6 @@ _.Zones =
 						i(25506),
 						i(25507),
 					}),
---[[					
 					qa( 10093),	-- The Temple of Telhamat
 					qh(  9405),	-- The Warchief's Mandate
 					qh( 10278),	-- The Warp Rifts
@@ -495,28 +522,23 @@ _.Zones =
 					qh( 10393),	-- Vile Plans
 					qh( 10294),	-- Void Ridge
 					q(9351),	-- Voidwalkers Gone Wild
---]]					
 					qh(9466,  { -- Wanted: Blacktalon the Savage
 						i(25483),
 						i(25482),
 						i(25481),
 					}),
---[[					
 					qh( 10809),	-- Wanted: Worg Master Kruush					
 					qh( 28705),	-- Warchief's Command: Outland!
 					qa( 10485),	-- Warlord of the Bleeding Hollow
 					q(9724),	-- Warning the Cenarion Circle
---]]
 					qa(10055,  {  -- Waste Not, Want Not
 						i(29931),
 						i(29938),
 						i(29943),
 						i(29945),
 					}),
---[[					
 					qa( 10079),	-- When This Mine's a-Rockin'
 					qa( 10344),	-- Wing Commander Gryphongar
---]]
 					qa(10895,  {  -- Zeth'Gor Must Burn!
 						i(28057),
 						i(28052),
