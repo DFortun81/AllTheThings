79c3101f84ccb0c2cd6ce871cf263bbcb89aa6bb
Lucetia-Detheroc <lucetribunal@gmail.com>
2018-03-18 18:39:44 -0700

    - Adding in PvP section [Not Active]

---
+++ contrib/Parser/DATAS/14 - PvP/PvP.lua
@@ -0,0 +1,168 @@
+-----------------------------------------------
+--     P L A Y E R   V S   P L A Y E R       --
+-----------------------------------------------
+_.PvP =
+{
+	n(-94, { -- Black Market AH
+		i(121001),	-- Rhut'van Breastplate
+			i(121002),	-- Rhut'van Gauntlets
+			i(121003),	-- Rhut'van Girdle
+			i(121004),	-- Rhut'van Helm
+			i(121005),	-- Rhut'van Legplates
+			i(121006),	-- Rhut'van Pauldrons
+			i(121007),	-- Rhut'van Sabatons
+			i(121008),	-- Rhut'van Vambraces
+			i(121017),	-- Llothien Bindings
+			i(121018),	-- Llothien Boots
+			i(121019),	-- Llothien Britches
+			i(121020),	-- Llothien Gloves
+			i(121021),	-- Llothien Hood
+			i(121022),	-- Llothien Jerkin
+			i(121023),	-- Llothien Shoulders
+			i(121024),	-- Llothien Waistband
+			i(121040),	-- Faronaar Chain Belt
+			i(121041),	-- Faronaar Chain Bracers
+			i(121042),	-- Faronaar Chain Gauntlets
+			i(121043),	-- Faronaar Chain Greaves
+			i(121044),	-- Faronaar Chain Helm
+			i(121045),	-- Faronaar Chain Legguards
+			i(121046),	-- Faronaar Chain Spaulders
+			i(121047),	-- Faronaar Chain Vest
+			i(121048),	-- Temporal Scholar's Cord
+			i(121049),	-- Temporal Scholar's Cowl
+			i(121050),	-- Temporal Scholar's Handwraps
+			i(121051),	-- Temporal Scholar's Leggings
+			i(121052),	-- Temporal Scholar's Robe
+			i(121053),	-- Temporal Scholar's Sandals
+			i(121054),	-- Temporal Scholar's Shoulderpads
+			i(121055),	-- Temporal Scholar's Wristwraps
+			i(121056),	-- Ettinbone Breastplate
+			i(121057),	-- Ettinbone Gauntlets
+			i(121058),	-- Ettinbone Girdle
+			i(121059),	-- Ettinbone Helm
+			i(121060),	-- Ettinbone Legplates
+			i(121061),	-- Ettinbone Pauldrons
+			i(121062),	-- Ettinbone Sabatons
+			i(121063),	-- Ettinbone Vambraces
+			i(121072),	-- Icepine Bindings
+			i(121073),	-- Icepine Boots
+			i(121074),	-- Icepine Britches
+			i(121075),	-- Icepine Gloves
+			i(121076),	-- Icepine Hood
+			i(121077),	-- Icepine Jerkin
+			i(121078),	-- Icepine Shoulders
+			i(121079),	-- Icepine Waistband
+			i(121095),	-- Hillstride Belt
+			i(121096),	-- Hillstride Bracers
+			i(121097),	-- Hillstride Gauntlets
+			i(121098),	-- Hillstride Greaves
+			i(121099),	-- Hillstride Helm
+			i(121100),	-- Hillstride Legguards
+			i(121101),	-- Hillstride Spaulders
+			i(121102),	-- Hillstride Vest
+			i(121103),	-- Hagfeather Cord
+			i(121104),	-- Hagfeather Cowl
+			i(121105),	-- Hagfeather Handwraps
+			i(121106),	-- Hagfeather Leggings
+			i(121107),	-- Hagfeather Robe
+			i(121108),	-- Hagfeather Sandals
+			i(121109),	-- Hagfeather Shoulderpads
+			i(121110),	-- Hagfeather Wristwraps
+			i(121111),	-- Valarsmidd Breastplate
+			i(121112),	-- Valarsmidd Gauntlets
+			i(121113),	-- Valarsmidd Girdle
+			i(121114),	-- Valarsmidd Helm
+			i(121115),	-- Valarsmidd Legplates
+			i(121116),	-- Valarsmidd Pauldrons
+			i(121117),	-- Valarsmidd Sabatons
+			i(121118),	-- Valarsmidd Vambraces
+			i(121127),	-- Haustvelt Bindings
+			i(121128),	-- Haustvelt Boots
+			i(121129),	-- Haustvelt Britches
+			i(121130),	-- Haustvelt Gloves
+			i(121131),	-- Haustvelt Hood
+			i(121132),	-- Haustvelt Jerkin
+			i(121133),	-- Haustvelt Shoulders
+			i(121134),	-- Haustvelt Waistband
+			i(121150),	-- Valisdall Belt
+			i(121151),	-- Valisdall Bracers
+			i(121152),	-- Valisdall Gauntlets
+			i(121153),	-- Valisdall Greaves
+			i(121154),	-- Valisdall Helm
+			i(121155),	-- Valisdall Legguards
+			i(121156),	-- Valisdall Spaulders
+			i(121157),	-- Valisdall Vest
+			i(121158),	-- Hrydshal Cord
+			i(121159),	-- Hrydshal Cowl
+			i(121160),	-- Hrydshal Handwraps
+			i(121161),	-- Hrydshal Leggings
+			i(121162),	-- Hrydshal Robe
+			i(121163),	-- Hrydshal Sandals
+			i(121164),	-- Hrydshal Shoulderpads
+			i(121165),	-- Hrydshal Wristwraps
+			i(121166),	-- Kal'delar Breastplate
+			i(121167),	-- Kal'delar Gauntlets
+			i(121168),	-- Kal'delar Girdle
+			i(121169),	-- Kal'delar Helm
+			i(121170),	-- Kal'delar Legplates
+			i(121171),	-- Kal'delar Pauldrons
+			i(121172),	-- Kal'delar Sabatons
+			i(121173),	-- Kal'delar Vambraces
+			i(121182),	-- Smolderhide Bindings
+			i(121183),	-- Smolderhide Boots
+			i(121184),	-- Smolderhide Britches
+			i(121185),	-- Smolderhide Gloves
+			i(121186),	-- Smolderhide Hood
+			i(121187),	-- Smolderhide Jerkin
+			i(121188),	-- Smolderhide Shoulders
+			i(121189),	-- Smolderhide Waistband
+			i(121205),	-- Dreadroot Linked Belt
+			i(121206),	-- Dreadroot Linked Bracers
+			i(121207),	-- Dreadroot Linked Gauntlets
+			i(121208),	-- Dreadroot Linked Greaves
+			i(121209),	-- Dreadroot Linked Helm
+			i(121210),	-- Dreadroot Linked Legguards
+			i(121211),	-- Dreadroot Linked Spaulders
+			i(121212),	-- Dreadroot Linked Vest
+			i(121213),	-- Crescent Vale Cord
+			i(121214),	-- Crescent Vale Cowl
+			i(121215),	-- Crescent Vale Handwraps
+			i(121216),	-- Crescent Vale Leggings
+			i(121217),	-- Crescent Vale Robe
+			i(121218),	-- Crescent Vale Sandals
+			i(121219),	-- Crescent Vale Shoulderpads
+			i(121220),	-- Crescent Vale Wristwraps
+			i(121221),	-- Jandvik Breastplate
+			i(121222),	-- Jandvik Gauntlets
+			i(121223),	-- Jandvik Girdle
+			i(121224),	-- Jandvik Helm
+			i(121225),	-- Jandvik Legplates
+			i(121226),	-- Jandvik Pauldrons
+			i(121227),	-- Jandvik Sabatons
+			i(121228),	-- Jandvik Vambraces
+			i(121237),	-- Ambervale Bindings
+			i(121238),	-- Ambervale Boots
+			i(121239),	-- Ambervale Britches
+			i(121240),	-- Ambervale Gloves
+			i(121241),	-- Ambervale Hood
+			i(121242),	-- Ambervale Jerkin
+			i(121243),	-- Ambervale Shoulders
+			i(121244),	-- Ambervale Waistband
+			i(121260),	-- Warpwind Belt
+			i(121261),	-- Warpwind Bracers
+			i(121262),	-- Warpwind Gauntlets
+			i(121263),	-- Warpwind Greaves
+			i(121264),	-- Warpwind Helm
+			i(121265),	-- Warpwind Legguards
+			i(121266),	-- Warpwind Spaulders
+			i(121267),	-- Warpwind Vest
+			i(121268),	-- Moonfall Cord
+			i(121269),	-- Moonfall Cowl
+			i(121270),	-- Moonfall Handwraps
+			i(121271),	-- Moonfall Leggings
+			i(121272),	-- Moonfall Robe
+			i(121273),	-- Moonfall Sandals
+			i(121274),	-- Moonfall Shoulderpads
+			i(121275),	-- Moonfall Wristwraps
+	}),
+};
