00da8094bc58c5098161c05d0db3a0ce49b715b1
Lucetia-Detheroc <lucetribunal@gmail.com>
2018-04-07 17:51:32 -0700

    - Added Timewarped Badge Currency information

--- contrib/Parser/DATAS/2 - Zones/1 Kalimdor/Zones_Kalimdor_Orgrimmar.lua
+++ contrib/Parser/DATAS/2 - Zones/1 Kalimdor/Zones_Kalimdor_Orgrimmar.lua
@@ -2659,25 +2659,27 @@ Legacy				qh( 1436),	-- Alliance Relations
 					}),
 					n(101759, {	-- Kiatke <Timewalking Vendor>
 						["groups"] = {
-							i(133525),	-- Bones of the Damned
-							i(133526),	-- Lightwarper Vestments
-							i(133527),	-- Ghostworld Chestguard
-							i(133528),	-- Decaying Herbalist's Robes
-							i(133529),	-- Belt of Hidden Keys
-							i(133530),	-- Cord of Dragon Sinew
-							i(133531),	-- Blinding Girdle of Truth
-							i(133532),	-- Vestal's Irrepressible Girdle
-							i(133533),	-- Flashing Bracers of Warmth
-							i(133534),	-- Dragonbelly Bracers
-							i(133535),	-- Bracers of Manifold Pockets
-							i(133536),	-- Bracers of the Black Dream
-							i(133511),	-- Gurboggle's Gleaming Bauble Toy
-							i(133542),	-- Tosselwrench's Mega-Accurate Simulation Viewfinder Toy
-							i(67414, {	-- Bag of Fishing Treasures
-								dr(1.1, i(44983)),	-- Strand Crawler Pet
-								dr(1.1, i(33820)),	-- Weather-Beaten Fishing Hat
-								--dr(0.5, i(45991)),	-- Bone Fishing Pole - not moggable
-								--dr(0.5, i(45992)),	-- Jeweled Fishing Pole - not Moggable
+							currency(1166, { -- Timewarped Badge
+								i(133525),	-- Bones of the Damned
+								i(133526),	-- Lightwarper Vestments
+								i(133527),	-- Ghostworld Chestguard
+								i(133528),	-- Decaying Herbalist's Robes
+								i(133529),	-- Belt of Hidden Keys
+								i(133530),	-- Cord of Dragon Sinew
+								i(133531),	-- Blinding Girdle of Truth
+								i(133532),	-- Vestal's Irrepressible Girdle
+								i(133533),	-- Flashing Bracers of Warmth
+								i(133534),	-- Dragonbelly Bracers
+								i(133535),	-- Bracers of Manifold Pockets
+								i(133536),	-- Bracers of the Black Dream
+								i(133511),	-- Gurboggle's Gleaming Bauble Toy
+								i(133542),	-- Tosselwrench's Mega-Accurate Simulation Viewfinder Toy
+								i(67414, {	-- Bag of Fishing Treasures
+									dr(1.1, i(44983)),	-- Strand Crawler Pet
+									dr(1.1, i(33820)),	-- Weather-Beaten Fishing Hat
+									--dr(0.5, i(45991)),	-- Bone Fishing Pole - not moggable
+									--dr(0.5, i(45992)),	-- Jeweled Fishing Pole - not Moggable
+								}),
 							}),
 						},
 						["description"] = "Vendor only available during Timewalking(Cataclysm).|r",
