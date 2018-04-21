fc9b42c4ab07806978ea77cdf2216c8779a15dfa
Lucetia-Detheroc <aceso2896@gmail.com>
2018-04-20 19:35:06 -0700

    - Season 2 PvP Item Sets now have all spec related sets listed (to match Season 1) Fixes #50

--- contrib/Parser/DATAS/6 - Gear Sets/Subgroups/GearSets_PvP.lua
+++ contrib/Parser/DATAS/6 - Gear Sets/Subgroups/GearSets_PvP.lua
@@ -234,15 +234,27 @@ _.GearSets =
 		gsh(380, {	-- Season 2
 			["groups"] = {
 				gs(380, {	-- Merciless Gladiator's Satin Armor		
-					["groups"] = {		
-						i(32035),	-- Merciless Gladiator's Satin Hood
-						i(32037),	-- Merciless Gladiator's Satin Mantle
-						i(32038),	-- Merciless Gladiator's Satin Robe
-						i(32980),	-- Veteran's Mooncloth Cuffs
-						i(32034),	-- Merciless Gladiator's Satin Gloves
-						i(32979),	-- Veteran's Mooncloth Belt
-						i(32036),	-- Merciless Gladiator's Satin Leggings
-						i(147609),	-- Veteran's Mooncloth Slippers
+					["groups"] = {
+						n(-9951, { -- Discipline / Holy Priest
+							i(32016),	-- Merciless Gladiator's Mooncloth Hood
+							i(32018),	-- Merciless Gladiator's Mooncloth Mantle
+							i(32019),	-- Merciless Gladiator's Mooncloth Robe
+							i(32980),	-- Veteran's Mooncloth Cuffs
+							i(32015),	-- Merciless Gladiator's Mooncloth Gloves
+							i(32979),	-- Veteran's Mooncloth Belt
+							i(32017),	-- Merciless Gladiator's Mooncloth Leggings
+							i(147609),	-- Veteran's Mooncloth Slippers
+						}),
+						n(-9917, {	-- Shadow Priest
+							i(32035),	-- Merciless Gladiator's Satin Hood
+							i(32037),	-- Merciless Gladiator's Satin Mantle
+							i(32038),	-- Merciless Gladiator's Satin Robe
+							i(32980),	-- Veteran's Mooncloth Cuffs
+							i(32034),	-- Merciless Gladiator's Satin Gloves
+							i(32979),	-- Veteran's Mooncloth Belt
+							i(32036),	-- Merciless Gladiator's Satin Leggings
+							i(147609),	-- Veteran's Satin Slippers
+						}),
 					},		
 					["classes"] = { 5 },		
 				}),			
@@ -273,15 +285,37 @@ _.GearSets =
 					["classes"] = { 9 },		
 				}),			
 				gs(969, {	-- Merciless Gladiator's Dragonhide Armor		
-					["groups"] = {		
-						i(31968),	-- Merciless Gladiator's Dragonhide Helm
-						i(31971),	-- Merciless Gladiator's Dragonhide Spaulders
-						i(31972),	-- Merciless Gladiator's Dragonhide Tunic
-						i(32810),	-- Veteran's Dragonhide Bracers
-						i(31967),	-- Merciless Gladiator's Dragonhide Gloves
-						i(32798),	-- Veteran's Dragonhide Belt
-						i(31969),	-- Merciless Gladiator's Dragonhide Trousers
-						i(32786),	-- Veteran's Dragonhide Boots
+					["groups"] = {
+						n(-9952, {	-- Guardian / Feral Spec
+							i(31988),	-- Merciless Gladiator's Kodohide Helm
+							i(31990),	-- Merciless Gladiator's Kodohide Spaulders
+							i(31991),	-- Merciless Gladiator's Kodohide Tunic
+							i(32812),	-- Veteran's Kodohide Bracers
+							i(31987),	-- Merciless Gladiator's Kodohide Gloves
+							i(32800),	-- Veteran's Kodohide Belt
+							i(31989),	-- Merciless Gladiator's Kodohide Legguards
+							i(32788),	-- Veteran's Kodohide Boots
+						}),
+						n(-9926, {	-- Balance
+							i(32057),	-- Merciless Gladiator's Wyrmhide Helm
+							i(32059),	-- Merciless Gladiator's Wyrmhide Spaulders
+							i(32060),	-- Merciless Gladiator's Wyrmhide Tunic
+							i(32821),	-- Veteran's Wyrmhide Bracers
+							i(32056),	-- Merciless Gladiator's Wyrmhide Gloves
+							i(32808),	-- Veteran's Wyrmhide Belt
+							i(32058),	-- Merciless Gladiator's Wyrmhide Legguards
+							i(32796),	-- Veteran's Wyrmhide Boots
+						}),
+						n(-9927, {	-- Restoration
+							i(31968),	-- Merciless Gladiator's Dragonhide Helm
+							i(31971),	-- Merciless Gladiator's Dragonhide Spaulders
+							i(31972),	-- Merciless Gladiator's Dragonhide Tunic
+							i(32810),	-- Veteran's Dragonhide Bracers
+							i(31967),	-- Merciless Gladiator's Dragonhide Gloves
+							i(32798),	-- Veteran's Dragonhide Belt
+							i(31969),	-- Merciless Gladiator's Dragonhide Trousers
+							i(32786),	-- Veteran's Dragonhide Boots
+						}),
 					},		
 					["classes"] = { 11 },		
 				}),			
@@ -312,28 +346,72 @@ _.GearSets =
 					["classes"] = { 3 },		
 				}),			
 				gs(964, {	-- Merciless Gladiator's Ringmail Armor		
-					["groups"] = {		
-						i(32031),	-- Merciless Gladiator's Ringmail Helm
-						i(32033),	-- Merciless Gladiator's Ringmail Spaulders
-						i(32029),	-- Merciless Gladiator's Ringmail Armor
-						i(32816),	-- Merciless Gladiator's Ringmail Bracers
-						i(32030),	-- Merciless Gladiator's Ringmail Gauntlets
-						i(32803),	-- Merciless Gladiator's Ringmail Girdle
-						i(32032),	-- Merciless Gladiator's Ringmail Leggings
-						i(32791),	-- Merciless Gladiator's Ringmail Sabatons
+					["groups"] = {
+						n(-9939, {	-- Elemental
+							i(32006),	-- Merciless Gladiator's Linked Helm
+							i(32008),	-- Merciless Gladiator's Linked Spaulders
+							i(32004),	-- Merciless Gladiator's Linked Armor
+							i(32816),	-- Veteran's Linked Bracers
+							i(32005),	-- Merciless Gladiator's Linked Gauntlets
+							i(32803),	-- Veteran's Linked Belt
+							i(32007),	-- Merciless Gladiator's Linked Leggings
+							i(32791),	-- Veteran's Linked Sabatons
+						}),
+						n(-9940, {	-- Enhancement
+							i(32031),	-- Merciless Gladiator's Ringmail Helm
+							i(32033),	-- Merciless Gladiator's Ringmail Spaulders
+							i(32029),	-- Merciless Gladiator's Ringmail Armor
+							i(32816),	-- Merciless Gladiator's Ringmail Bracers
+							i(32030),	-- Merciless Gladiator's Ringmail Gauntlets
+							i(32803),	-- Merciless Gladiator's Ringmail Girdle
+							i(32032),	-- Merciless Gladiator's Ringmail Leggings
+							i(32791),	-- Merciless Gladiator's Ringmail Sabatons
+						}),
+						n(-9941, {	-- Restoration
+							i(32011),	-- Merciless Gladiator's Mail Helm
+							i(32013),	-- Merciless Gladiator's Mail Spaulders
+							i(32009),	-- Merciless Gladiator's Mail Armor
+							i(32817),	-- Veteran's Mail Bracers
+							i(32010),	-- Merciless Gladiator's Mail Gauntlets
+							i(32804),	-- Veteran's Mail Girdle
+							i(32012),	-- Merciless Gladiator's Mail Leggings
+							i(32792),	-- Veteran's Mail Sabatons
+						}),						
 					},		
 					["classes"] = { 7 },		
 				}),			
 				gs(966, {	-- Merciless Gladiator's Lamellar Armor		
-					["groups"] = {		
-						i(31997),	-- Merciless Gladiator's Lamellar Helm
-						i(31996),	-- Merciless Gladiator's Lamellar Shoulders
-						i(31992),	-- Merciless Gladiator's Lamellar Chestpiece
-						i(32813),	-- Merciless Gladiator's Lamellar Bracers
-						i(31993),	-- Merciless Gladiator's Lamellar Gauntlets
-						i(32801),	-- Merciless Gladiator's Lamellar Belt
-						i(31995),	-- Merciless Gladiator's Lamellar Legguards
-						i(32789),	-- Merciless Gladiator's Lamellar Greaves
+					["groups"] = {
+						n(-9942, {	-- Holy
+							i(32022),	-- Merciless Gladiator's Ornamented Headcover
+							i(32024),	-- Merciless Gladiator's Ornamented Spaulders
+							i(32020),	-- Merciless Gladiator's Ornamented Chestguard
+							i(32989),	-- Veteran's Ornamented Bracers
+							i(32021),	-- Merciless Gladiator's Ornamented Gloves
+							i(32988),	-- Veteran's Ornamented Belt
+							i(32023),	-- Merciless Gladiator's Ornamented Legplates
+							i(32990),	-- Veteran's Ornamented Greaves
+						}),
+						n(-9943, {	-- Protection
+							i(32041),	-- Merciless Gladiator's Scaled Helm
+							i(32043),	-- Merciless Gladiator's Scaled Shoulders
+							i(32039),	-- Merciless Gladiator's Scaled Armor
+							i(32819),	-- Veteran's Scaled Bracers
+							i(32040),	-- Merciless Gladiator's Scaled Gauntlets
+							i(32806),	-- Veteran's Scaled Belt
+							i(32042),	-- Merciless Gladiator's Scaled Legguards
+							i(32794),	-- Veteran's Scaled Greaves
+						}),
+						n(-9944, {	-- Retribtion
+							i(31997),	-- Merciless Gladiator's Lamellar Helm
+							i(31996),	-- Merciless Gladiator's Lamellar Shoulders
+							i(31992),	-- Merciless Gladiator's Lamellar Chestpiece
+							i(32813),	-- Merciless Gladiator's Lamellar Bracers
+							i(31993),	-- Merciless Gladiator's Lamellar Gauntlets
+							i(32801),	-- Merciless Gladiator's Lamellar Belt
+							i(31995),	-- Merciless Gladiator's Lamellar Legguards
+							i(32789),	-- Merciless Gladiator's Lamellar Greaves
+						}),	
 					},		
 					["classes"] = { 2 },		
 				}),			
