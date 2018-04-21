8bca84501e5347c60e5b8f15548d9eca66fdec53
Lucetia-Detheroc <aceso2896@gmail.com>
2018-04-20 18:51:22 -0700

    - Fixes #49 PvP Season 3 Ensembles added to Kitzie Crankshot in Netherstorm (Thanks slumber for helping speed it up!)

--- contrib/Parser/DATAS/2 - Zones/3 Outland/Zones_Outland_Netherstorm.lua
+++ contrib/Parser/DATAS/2 - Zones/3 Outland/Zones_Outland_Netherstorm.lua
@@ -2417,6 +2417,123 @@ _.Zones =
 							i(33766),	-- Vengeful Gladiator's War Staff
 							i(34014),	-- Vengeful Gladiator's Waraxe
 						}),
+						i(146635, {	-- Ensemble: Vengeful Gladiator's Satin Armor		
+							["groups"] = {		
+								i(33718),	-- Vengeful Gladiator's Mooncloth Hood
+								i(33720),	-- Vengeful Gladiator's Mooncloth Mantle
+								i(33721),	-- Vengeful Gladiator's Mooncloth Robe
+								i(33901),	-- Vindicator's Mooncloth Cuffs
+								i(33717),	-- Vengeful Gladiator's Mooncloth Gloves
+								i(33900),	-- Vindicator's Mooncloth Belt
+								i(33719),	-- Vengeful Gladiator's Mooncloth Leggings
+								i(33902),	-- Vindicator's Mooncloth Slippers
+							},		
+							["classes"] = 5,	-- Priest	
+						}),			
+						i(146637, {	-- Ensemble: Vengeful Gladiator's Silk Armor		
+							["groups"] = {		
+								i(33758),	-- Vengeful Gladiator's Silk Cowl
+								i(33757),	-- Vengeful Gladiator's Silk Amice
+								i(33760),	-- Vengeful Gladiator's Silk Raiment
+								i(33913),	-- Vindicator's Silk Cuffs
+								i(33759),	-- Vengeful Gladiator's Silk Handguards
+								i(33912),	-- Vindicator's Silk Belt
+								i(33761),	-- Vengeful Gladiator's Silk Trousers
+								i(33914),	-- Vindicator's Silk Footguards
+							},		
+							["classes"] = 8,	-- Mage	
+						}),			
+						i(146631, {	-- Ensemble: Vengeful Gladiator's Felweave Armor		
+							["groups"] = {		
+								i(33677),	-- Vengeful Gladiator's Dreadweave Hood
+								i(33679),	-- Vengeful Gladiator's Dreadweave Mantle
+								i(33680),	-- Vengeful Gladiator's Dreadweave Robe
+								i(33883),	-- Vindicator's Dreadweave Cuffs
+								i(33676),	-- Vengeful Gladiator's Dreadweave Gloves
+								i(33882),	-- Vindicator's Dreadweave Belt
+								i(33678),	-- Vengeful Gladiator's Dreadweave Leggings
+								i(33884),	-- Vindicator's Dreadweave Stalkers
+							},		
+							["classes"] = 9,	-- Warlock	
+						}),			
+						i(146629, {	-- Ensemble: Vengeful Gladiator's Dragonhide Armor		
+							["groups"] = {		
+								i(33672),	-- Vengeful Gladiator's Dragonhide Armor
+								i(33674),	-- Vengeful Gladiator's Dragonhide Spaulders
+								i(33675),	-- Vengeful Gladiator's Dragonhide Tunic
+								i(33881),	-- Vindicator's Dragonhide Bracers
+								i(33690),	-- Vengeful Gladiator's Kodohide Gloves
+								i(33879),	-- Vindicator's Dragonhide Belt
+								i(33673),	-- Vengeful Gladiator's Dragonhide Legguards
+								i(33880),	-- Vindicator's Dragonhide Boots
+							},		
+							["classes"] = 11,	-- Druid	
+						}),			
+						i(146632, {	-- Ensemble: Vengeful Gladiator's Leather Armor		
+							["groups"] = {		
+								i(33701),	-- Vengeful Gladiator's Leather Helm
+								i(33703),	-- Vengeful Gladiator's Leather Spaulders
+								i(33704),	-- Vengeful Gladiator's Leather Tunic
+								i(147670),	-- Vindicator's Leather Wristguards
+								i(33700),	-- Vengeful Gladiator's Leather Gloves
+								i(33891),	-- Vindicator's Leather Belt
+								i(33702),	-- Vengeful Gladiator's Leather Legguards
+								i(33892),	-- Vindicator's Leather Boots
+							},		
+							["classes"] = 4,	-- Rogue	
+						}),			
+						i(146628, {	-- Ensemble: Vengeful Gladiator's Chain Armor		
+							["groups"] = {		
+								i(33666),	-- Vengeful Gladiator's Chain Helm
+								i(33668),	-- Vengeful Gladiator's Chain Spaulders
+								i(33664),	-- Vengeful Gladiator's Chain Armor
+								i(33876),	-- Vindicator's Chain Bracers
+								i(33665),	-- Vengeful Gladiator's Chain Gauntlets
+								i(33877),	-- Vindicator's Chain Girdle
+								i(33667),	-- Vengeful Gladiator's Chain Leggings
+								i(33878),	-- Vindicator's Chain Sabatons
+							},		
+							["classes"] = 3,	-- Hunter	
+						}),			
+						i(146634, {	-- Ensemble: Vengeful Gladiator's Ringmail Armor		
+							["groups"] = {		
+								i(33740),	-- Vengeful Gladiator's Ringmail Helm
+								i(33742),	-- Vengeful Gladiator's Ringmail Spaulders
+								i(33738),	-- Vengeful Gladiator's Ringmail Armor
+								i(33894),	-- Vindicator's Linked Bracers
+								i(33739),	-- Vengeful Gladiator's Ringmail Gauntlets
+								i(33898),	-- Vindicator's Mail Girdle
+								i(33741),	-- Vengeful Gladiator's Ringmail Leggings
+								i(33896),	-- Vindicator's Linked Sabatons
+							},		
+							["classes"] = 7,	-- Shaman	
+						}),			
+						i(146636, {	-- Ensemble: Vengeful Gladiator's Scaled Armor		
+							["groups"] = {		
+								i(33697),	-- Vengeful Gladiator's Lamellar Helm
+								i(33753),	-- Vengeful Gladiator's Scaled Shoulders
+								i(33695),	-- Vengeful Gladiator's Lamellar Chestpiece
+								i(33889),	-- Vindicator's Lamellar Bracers
+								i(33696),	-- Vengeful Gladiator's Lamellar Gauntlets
+								i(33888),	-- Vindicator's Lamellar Belt
+								i(33698),	-- Vengeful Gladiator's Lamellar Legguards
+								i(33890),	-- Vindicator's Lamellar Greaves
+							},		
+							["classes"] = 2,	-- Paladin	
+						}),			
+						i(146633, {	-- Ensemble: Vengeful Gladiator's Plate Armor		
+							["groups"] = {		
+								i(33730),	-- Vengeful Gladiator's Plate Helm
+								i(33732),	-- Vengeful Gladiator's Plate Shoulders
+								i(33728),	-- Vengeful Gladiator's Plate Chestpiece
+								i(33813),	-- Vindicator's Plate Bracers
+								i(33729),	-- Vengeful Gladiator's Plate Gauntlets
+								i(33811),	-- Vindicator's Plate Belt
+								i(33731),	-- Vengeful Gladiator's Plate Legguards
+								i(33812),	-- Vindicator's Plate Greaves
+							},		
+							["classes"] = 1,	-- Warrior	
+						}),
 						i(33661),	-- Vengeful Gladiator's Barrier
 						i(34059),	-- Vengeful Gladiator's Baton of Light
 						i(34540),	-- Vengeful Gladiator's Battle Staff
