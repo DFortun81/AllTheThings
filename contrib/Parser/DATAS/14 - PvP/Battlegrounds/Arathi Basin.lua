5b70e51bf785b61e0f123330a8946b75ac5f57d7
ada <slumber@love.com>
2018-04-22 01:04:47 -0500

    - commented/organized AB/WSG vendor items, added accessories

--- contrib/Parser/DATAS/14 - PvP/Battlegrounds/Arathi Basin.lua
+++ contrib/Parser/DATAS/14 - PvP/Battlegrounds/Arathi Basin.lua
@@ -10,92 +10,96 @@ _.PVP =
 			["description"] = "Arathi Basin is a 15v15 battleground located in Arathi Highlands. Players fight over five resources (Stables, Mines, Blacksmith, Lumber Mill, Farm) which rewards teams with points. The more resources a team controls, the faster they accumulate points. A team with all five nodes captured will gain 30 resources per second.\n\nThe game is won when one team reaches 1,600 points. The most common way of winning is to hold three nodes and defend, with the Blacksmith being a key node.",
 			["groups"] = {
 				ach(1169, {	-- Master of Arathi Basin
-					ach(583),	-- Arathi Basin All-Star
-					ach(584),	-- Arathi Basin Assassin
-					ach(165),	-- Arathi Basin Perfection
-					ach(155),	-- Arathi Basin Veteran
-					ach(73),	-- Disgracin' The Basin
-					ach(159),	-- Let's Get This Done
-					ach(158),	-- Me and the Cappin' Makin' it Happen
+					ach( 583),	-- Arathi Basin All-Star
+					ach( 584),	-- Arathi Basin Assassin
+					ach( 165),	-- Arathi Basin Perfection
+					ach( 155),	-- Arathi Basin Veteran
+					ach(  73),	-- Disgracin' The Basin
+					ach( 159),	-- Let's Get This Done
+					ach( 158),	-- Me and the Cappin' Makin' it Happen
 					ach(1153),	-- Overly Defensive
-					ach(161),	-- Resilient Victory
-					ach(156),	-- Territorial Dominance
-					ach(157),	-- To The Rescue!
-					ach(162),	-- We Had It All Along *cough*
+					ach( 161),	-- Resilient Victory
+					ach( 156),	-- Territorial Dominance
+					ach( 157),	-- To The Rescue!
+					ach( 162),	-- We Had It All Along *cough*
 				}),
 				h(faction(510, {
 					["groups"] = {
 						ach(710), 	-- The Defiler
-						nh(15126, {	-- Rutherford Twing -- TODO: Comment these in and organize + add rings/trinkets https://www.wowhead.com/npc=15126/rutherford-twing#sells
+						nh(15126, {	-- Rutherford Twing
 							["itemID"] = 137642,	-- Mark of Honor
 							["groups"] = {
-								i(20220),	-- Ironbark Staff
-								i(20068),	-- Deathguard's Cloak
-								i(20158),	-- Defiler's Chain Pauldrons
-								i(20175),	-- Defiler's Lizardhide Shoulders
 								i(20176),	-- Defiler's Epaulets
-								i(20184),	--
-								i(20194),	--
-								i(20203),	--
-								i(20212),	--
-								i(20214),	--
-								i(20150),	--
-								i(20154),	--
-								i(20159),	--
-								i(20163),	--
-								i(20167),	--
-								i(20171),	--
-								i(20177),	--
-								i(20181),	--
-								i(20186),	--
-								i(20190),	--
-								i(20195),	--
-								i(20199),	--
-								i(20204),	--
-								i(20208),	--
-								i(20151),	--
-								i(20155),	--
-								i(20160),	--
-								i(20165),	--
-								i(20170),	--
-								i(20174),	--
-								i(20179),	--
-								i(20185),	--
-								i(20189),	--
-								i(20193),	--
-								i(20196),	--
-								i(20202),	--
-								i(20205),	--
-								i(20211), 	--
-								i(20153),	--
-								i(20156),	--
-								i(20161),	--
-								i(20166),	--
-								i(20168),	--
-								i(20173),	--
-								i(20180),	--
-								i(20183),	--
-								i(20187),	--
-								i(20192),	--
-								i(20198),	--
-								i(20200),	--
-								i(20206),	--
-								i(20209),	--
-								i(20152),	--
-								i(20157),	--
-								i(20162),	--
-								i(20164),	--
-								i(20169),	--
-								i(20172),	--
-								i(20178),	--
-								i(20182),	--
-								i(20188),	--
-								i(20191),	--
-								i(20197),	--
-								i(20201),	--
-								i(20207),	--
-								i(20210),	--
-								i(20131),	--
+								i(20163),	-- Defiler's Cloth Girdle
+								i(20164),	-- Defiler's Cloth Girdle
+								i(20165),	-- Defiler's Cloth Girdle
+								i(20166),	-- Defiler's Cloth Girdle
+								i(20159),	-- Defiler's Cloth Boots
+								i(20160),	-- Defiler's Cloth Boots
+								i(20161),	-- Defiler's Cloth Boots
+								i(20162),	-- Defiler's Cloth Boots
+								i(20194),	-- Defiler's Leather Shoulders
+								i(20190),	-- Defiler's Leather Girdle
+								i(20191),	-- Defiler's Leather Girdle
+								i(20192),	-- Defiler's Leather Girdle
+								i(20193),	-- Defiler's Leather Girdle
+								i(20186),	-- Defiler's Leather Boots
+								i(20187),	-- Defiler's Leather Boots
+								i(20188),	-- Defiler's Leather Boots
+								i(20189),	-- Defiler's Leather Boots
+								i(20175),	-- Defiler's Lizardhide Shoulders
+								i(20171),	-- Defiler's Lizardhide Girdle
+								i(20172),	-- Defiler's Lizardhide Girdle
+								i(20173),	-- Defiler's Lizardhide Girdle
+								i(20174),	-- Defiler's Lizardhide Girdle
+								i(20167),	-- Defiler's Lizardhide Boots
+								i(20168),	-- Defiler's Lizardhide Boots
+								i(20169),	-- Defiler's Lizardhide Boots
+								i(20170),	-- Defiler's Lizardhide Boots
+								i(20158),	-- Defiler's Chain Pauldrons
+								i(20150),	-- Defiler's Chain Girdle
+								i(20151),	-- Defiler's Chain Girdle
+								i(20152),	-- Defiler's Chain Girdle
+								i(20153),	-- Defiler's Chain Girdle
+								i(20154),	-- Defiler's Chain Greaves
+								i(20155),	-- Defiler's Chain Greaves
+								i(20156),	-- Defiler's Chain Greaves
+								i(20157),	-- Defiler's Chain Greaves
+								i(20203),	-- Defiler's Mail Pauldrons
+								i(20195),	-- Defiler's Mail Girdle
+								i(20196),	-- Defiler's Mail Girdle
+								i(20197),	-- Defiler's Mail Girdle
+								i(20198),	-- Defiler's Mail Girdle
+								i(20199),	-- Defiler's Mail Greaves
+								i(20200),	-- Defiler's Mail Greaves
+								i(20201),	-- Defiler's Mail Greaves
+								i(20202),	-- Defiler's Mail Greaves
+								i(20184),	-- Defiler's Lamellar Spaulders
+								i(20177),	-- Defiler's Lamellar Girdle
+								i(20178),	-- Defiler's Lamellar Girdle
+								i(20179),	-- Defiler's Lamellar Girdle
+								i(20180),	-- Defiler's Lamellar Girdle
+								i(20181),	-- Defiler's Lamellar Greaves
+								i(20182),	-- Defiler's Lamellar Greaves
+								i(20183),	-- Defiler's Lamellar Greaves
+								i(20185),	-- Defiler's Lamellar Greaves
+								i(20212),	-- Defiler's Plate Spaulders
+								i(20204),	-- Defiler's Plate Girdle
+								i(20205),	-- Defiler's Plate Girdle
+								i(20206),	-- Defiler's Plate Girdle
+								i(20207),	-- Defiler's Plate Girdle
+								i(20208),	-- Defiler's Plate Greaves
+								i(20209),	-- Defiler's Plate Greaves
+								i(20210),	-- Defiler's Plate Greaves
+								i(20211), 	-- Defiler's Plate Greaves
+								i(20220),	-- Ironbark Staff
+								i(20214),	-- Mindfang
+								i(20131),	-- Battle Tabard of the Defilers
+								i(20068),	-- Deathguard's Cloak
+								i(20072),	-- Defiler's Talisman
+								i(21115),	-- Defiler's Talisman
+								i(21116),	-- Defiler's Talisman
+								i(21120),	-- Defiler's Talisman
 							},
 						}),
 					},
@@ -104,76 +108,80 @@ _.PVP =
 				a(faction(509, {
 					["groups"] = {
 						ach(711), 	-- Knight of Arathor
-						na(15127, {	-- Samual Hawke	-- TODO: Comment these in and organize + add rings/trinkets https://www.wowhead.com/npc=15127/samuel-hawke
+						na(15127, {	-- Samual Hawke
 							["itemID"] = 137642,	-- Mark of Honor
 							["groups"] = {
-								i(20069),	-- Ironbark Staff
+								i(20061),	-- Highlander's Epaulets
+								i(20047),	-- Highlander's Cloth Girdle
+								i(20097),	-- Highlander's Cloth Girdle
+								i(20098),	-- Highlander's Cloth Girdle
+								i(20099),	-- Highlander's Cloth Girdle
+								i(20054),	-- Highlander's Cloth Boots
+								i(20094),	-- Highlander's Cloth Boots
+								i(20095),	-- Highlander's Cloth Boots
+								i(20096),	-- Highlander's Cloth Boots
+								i(20059),	-- Highlander's Leather Shoulders
+								i(20045),	-- Highlander's Leather Girdle
+								i(20115),	-- Highlander's Leather Girdle
+								i(20116),	-- Highlander's Leather Girdle
+								i(20117),	-- Highlander's Leather Girdle
+								i(20052),	-- Highlander's Leather Boots
+								i(20112),	-- Highlander's Leather Boots
+								i(20113),	-- Highlander's Leather Boots
+								i(20114),	-- Highlander's Leather Boots
+								i(20060),	-- Highlander's Lizardhide Shoulders
+								i(20046),	-- Highlander's Lizardhide Girdle
+								i(20103),	-- Highlander's Lizardhide Girdle
+								i(20104),	-- Highlander's Lizardhide Girdle
+								i(20105),	-- Highlander's Lizardhide Girdle
+								i(20053),	-- Highlander's Lizardhide Boots
+								i(20100),	-- Highlander's Lizardhide Boots
+								i(20101),	-- Highlander's Lizardhide Boots
+								i(20102),	-- Highlander's Lizardhide Boots
 								i(20055),	-- Highlander's Chain Pauldrons
+								i(20043),	-- Highlander's Chain Girdle
+								i(20088),	-- Highlander's Chain Girdle
+								i(20089),	-- Highlander's Chain Girdle
+								i(20090),	-- Highlander's Chain Girdle
+								i(20050),	-- Highlander's Chain Greaves
+								i(20091),	-- Highlander's Chain Greaves
+								i(20092),	-- Highlander's Chain Greaves
+								i(20093),	-- Highlander's Chain Greaves
 								i(20056),	-- Highlander's Mail Pauldrons
+								i(20044),	-- Highlander's Mail Girdle
+								i(20118),	-- Highlander's Mail Girdle
+								i(20119),	-- Highlander's Mail Girdle
+								i(20120),	-- Highlander's Mail Girdle
+								i(20051),	-- Highlander's Mail Greaves
+								i(20121),	-- Highlander's Mail Greaves
+								i(20122),	-- Highlander's Mail Greaves
+								i(20123),	-- Highlander's Mail Greaves
 								i(20057),	-- Highlander's Plate Spaulders
+								i(20041),	-- Highlander's Plate Girdle
+								i(20124),	-- Highlander's Plate Girdle
+								i(20125),	-- Highlander's Plate Girdle
+								i(20126),	-- Highlander's Plate Girdle
+								i(20048),	-- Highlander's Plate Greaves
+								i(20127),	-- Highlander's Plate Greaves
+								i(20128),	-- Highlander's Plate Greaves
+								i(20129),	-- Highlander's Plate Greaves
 								i(20058),	-- Highlander's Lamellar Spaulers
-								i(20059),	--
-								i(20060),	--
-								i(20061),	--
-								i(20070),	--
-								i(20073),	--
-								i(20041),	--
-								i(20042),	--
-								i(20043),	--
-								i(20044),	--
-								i(20045),	--
-								i(20046),	--
-								i(20047),	--
-								i(20048),	--
-								i(20049),	--
-								i(20050),	--
-								i(20051),	--
-								i(20052),	--
-								i(20053),	--
-								i(20054),	--
-								i(20088),	--
-								i(20091),	--
-								i(20094),	--
-								i(20097),	--
-								i(20100),	--
-								i(20103),	--
-								i(20106),	--
-								i(20109),	--
-								i(20112),	--
-								i(20115),	--
-								i(20118),	--
-								i(20121),	--
-								i(20124),	--
-								i(20127),	--
-								i(20089),	--
-								i(20092),	--
-								i(20095),	--
-								i(20098),	--
-								i(20101),	--
-								i(20104),	--
-								i(20107),	--
-								i(20110),	--
-								i(20113),	--
-								i(20116),	--
-								i(20119),	--
-								i(20122),	--
-								i(20125),	--
-								i(20128),	--
-								i(20090),	--
-								i(20093),	--
-								i(20096),	--
-								i(20099),	--
-								i(20102),	--
-								i(20105),	--
-								i(20108),	--
-								i(20111),	--
-								i(20114),	--
-								i(20117),	--
-								i(20120),	--
-								i(20123),	--
-								i(20126),	--
-								i(20129),	--
-								i(20132),	--
+								i(20042),	-- Highlander's Lamellar Girdle
+								i(20106),	-- Highlander's Lamellar Girdle
+								i(20107),	-- Highlander's Lamellar Girdle
+								i(20108),	-- Highlander's Lamellar Girdle
+								i(20049),	-- Highlander's Lamellar Greaves
+								i(20109),	-- Highlander's Lamellar Greaves
+								i(20110),	-- Highlander's Lamellar Greaves
+								i(20111),	-- Highlander's Lamellar Greaves
+								i(20069),	-- Ironbark Staff
+								i(20070),	-- Sageclaw
+								i(20132),	-- Arathor Battle Tabard
+								i(20073),	-- Cloak of the Honor Guard
+								i(20071),	-- Talisman of Arathor
+								i(21117),	-- Talisman of Arathor
+								i(21118),	-- Talisman of Arathor
+								i(21119),	-- Talisman of Arathor
 							},
 						}),
 					},
@@ -183,5 +191,3 @@ _.PVP =
 		}),
 	}),
 };
-
-
