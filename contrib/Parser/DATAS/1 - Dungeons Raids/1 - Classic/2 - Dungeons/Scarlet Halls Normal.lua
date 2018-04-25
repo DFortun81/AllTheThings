4600a4c12bf74e7bdb02b71b5bc1781afa4f4521
ada <slumber@love.com>
2018-04-25 17:35:22 -0500

    - adding accessories to dungeons.  masochism!

--- contrib/Parser/DATAS/1 - Dungeons Raids/1 - Classic/2 - Dungeons/Scarlet Halls Normal.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/1 - Classic/2 - Dungeons/Scarlet Halls Normal.lua
@@ -18,19 +18,23 @@ _.Instances =
 							i(88268),	-- Canine Commander's Breastplate
 							i(88267),	-- Commanding Bracers
 							i(88266),	-- Hound Trainer's Gloves
+							i(88265),	-- Beastbinder Ring
 						})),
 						cr(58632, e(654, {	-- Armsmaster Harlan
-							i(23192),	-- Tabard of the Scarlet Crusade
-							i(88272),	-- The Gleaming Ravager
-							i(88271),	-- Harlan's Shoulders
-							i(88270),	-- Lightblade Bracer
-							i(88269),	-- Scarlet Sandals
+							i( 23192),	-- Tabard of the Scarlet Crusade
+							i( 88272),	-- The Gleaming Ravager
+							i( 88273),	-- Armsmaster's Sealed Locket
+							i( 88271),	-- Harlan's Shoulders
+							i( 88270),	-- Lightblade Bracer
+							i( 88269),	-- Scarlet Sandals
 							i(132550),	-- Scarlet Chain Footpads
 						})),
 						cr(59150, e(656, {	-- Flameweaver Koegler
 							i(88278),	-- Mograine's Immaculate Might
 							i(88274),	-- Koegler's Ritual Knife
 							i(88280),	-- Melted Hypnotic Blade
+							i(88275),	-- Scorched Scarlet Key
+							i(88281),	-- Temperature-Sensing Necklace
 							i(88279),	-- Robes of Koegler
 							i(88282),	-- Vellum-Ripper Gloves
 							i(88276),	-- Bindburner Belt
