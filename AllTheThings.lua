e5df9a8531b8a732cf21c53d1db04e279efc1f6d
CRIEVE-SARGERAS <dylanfortune@live.com>
2018-04-28 15:40:37 -0700

    Harvested

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -6046,7 +6046,7 @@ function app:GetDataCache()
 			table.insert(g, db);
 		end
 		
-		--[[--]]
+		--[[
 		-- Uncomment for harvesting
 		-- Never Implemented
 		if app.Categories.NeverImplemented then
@@ -6077,7 +6077,7 @@ function app:GetDataCache()
 		db.expanded = false;
 		db.text = "Factions (Dynamic)";
 		table.insert(g, db);
-		--[[]]--
+		]]--
 		
 		-- Illusions (Dynamic)
 		--[[
