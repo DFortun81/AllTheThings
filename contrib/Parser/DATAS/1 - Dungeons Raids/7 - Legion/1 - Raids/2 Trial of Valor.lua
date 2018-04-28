8915055a65567626fe18a710832bcd19769b1445
Lucetia-Detheroc <aceso2896@gmail.com>
2018-04-28 12:38:43 -0700

    - Trial of Valor ensemble tokens now show the appropriate difficulty on the token itself rather than each version showing "Normal" Fixes #57

--- contrib/Parser/DATAS/1 - Dungeons Raids/7 - Legion/1 - Raids/2 Trial of Valor.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/7 - Legion/1 - Raids/2 Trial of Valor.lua
@@ -71,69 +71,67 @@ _.Instances =
 					d(17, {		-- LFR
 						["ids"] = { 3470 }, 
 						["groups"] = {
-							n(-1, {		-- Shared Boss Loot
-								i(143506, {	-- Ensemble: Vestment of the Chosen Dead
-									["groups"] = {
-										i(143346),	-- Circlet of the Chosen Dead
-										i(143356),	-- Mantle of the Chosen Dead
-										i(143369),	-- Drape of the Chosen Dead
-										i(143353),	-- Robe of the Chosen Dead
-										i(143362),	-- Bracers of the Chosen Dead
-										i(143343),	-- Gloves of the Chosen Dead
-										i(143359),	-- Sash of the Chosen Dead
-										i(143350),	-- Pantaloons of the Chosen Dead
-										i(143338),	-- Sandals of the Chosen Dead
-									},
-									["classes"] = { 5, 8, 9 }, -- Priest, Mage, Warock
-								}),		
-								i(143510, {	-- Ensemble: Garb of the Chosen Dead
-									["groups"] = {	
-										i(143348),	-- Hood of the Chosen Dead
-										i(143354),	-- Shoulderpads of the Chosen Dead
-										i(143366),	-- Wrap of the Chosen Dead
-										i(143336),	-- Tunic of the Chosen Dead
-										i(143365),	-- Wristguards of the Chosen Dead
-										i(143341),	-- Grips of the Chosen Dead
-										i(143361),	-- Strap of the Chosen Dead
-										i(143352),	-- Leggings of the Chosen Dead
-										i(143340),	-- Boots of the Chosen Dead
-									},
-									["classes"] = { 11, 4, 10, 12 }, -- Druid, Rogue, Monk, Demon Hunter
-								}),		
-								i(143514, {	-- Ensemble: Chains of the Chosen Dead	
-									["groups"] = {
-										i(143347),	-- Coif of the Chosen Dead
-										i(143357),	-- Pauldrons of the Chosen Dead
-										i(143368),	-- Cloak of the Chosen Dead
-										i(143335),	-- Chain Shirt of the Chosen Dead
-										i(143364),	-- Armbands of the Chosen Dead
-										i(143344),	-- Gauntlets of the Chosen Dead
-										i(143360),	-- Girdle of the Chosen Dead
-										i(143351),	-- Chain Leggings of the Chosen Dead
-										i(143339),	-- Sabatons of the Chosen Dead
-									},
-									["classes"] = { 3, 7 }, -- Hunter, Shaman
-								}),		
-								i(143518, {	-- Ensemble: Funerary Plate of the Chosen Dead	
-									["groups"] = {
-										i(143345),	-- Visor of the Chosen Dead
-										i(143355),	-- Shoulderplates of the Chosen Dead
-										i(143367),	-- Greatcloak of the Chosen Dead
-										i(143334),	-- Chestplate of the Chosen Dead
-										i(143363),	-- Wristclamps of the Chosen Dead
-										i(143342),	-- Handguards of the Chosen Dead
-										i(143358),	-- Waistplate of the Chosen Dead
-										i(143349),	-- Legplates of the Chosen Dead
-										i(143337),	-- Treads of the Chosen Dead
-									},
-									["classes"] = { 2, 1, 6 }, -- Paladin, Warrior, Death Knight
-								}),			
-							}),
 							n(0, { 		-- Zone Drop
-								i(142541),
+								i(142541), -- Drape of the Forgotten Souls
 							}),
 							ach(11394, { 	-- Trial of Valor
 								cr(114263, e(1819, {	-- Odyn
+									i(143509, {	-- Ensemble: Vestment of the Chosen Dead
+										["groups"] = {
+											i(143346),	-- Circlet of the Chosen Dead
+											i(143356),	-- Mantle of the Chosen Dead
+											i(143369),	-- Drape of the Chosen Dead
+											i(143353),	-- Robe of the Chosen Dead
+											i(143362),	-- Bracers of the Chosen Dead
+											i(143343),	-- Gloves of the Chosen Dead
+											i(143359),	-- Sash of the Chosen Dead
+											i(143350),	-- Pantaloons of the Chosen Dead
+											i(143338),	-- Sandals of the Chosen Dead
+										},
+										["classes"] = { 5, 8, 9 }, -- Priest, Mage, Warock
+									}),		
+									i(143513, {	-- Ensemble: Garb of the Chosen Dead
+										["groups"] = {	
+											i(143348),	-- Hood of the Chosen Dead
+											i(143354),	-- Shoulderpads of the Chosen Dead
+											i(143366),	-- Wrap of the Chosen Dead
+											i(143336),	-- Tunic of the Chosen Dead
+											i(143365),	-- Wristguards of the Chosen Dead
+											i(143341),	-- Grips of the Chosen Dead
+											i(143361),	-- Strap of the Chosen Dead
+											i(143352),	-- Leggings of the Chosen Dead
+											i(143340),	-- Boots of the Chosen Dead
+										},
+										["classes"] = { 11, 4, 10, 12 }, -- Druid, Rogue, Monk, Demon Hunter
+									}),		
+									i(143517, {	-- Ensemble: Chains of the Chosen Dead	
+										["groups"] = {
+											i(143347),	-- Coif of the Chosen Dead
+											i(143357),	-- Pauldrons of the Chosen Dead
+											i(143368),	-- Cloak of the Chosen Dead
+											i(143335),	-- Chain Shirt of the Chosen Dead
+											i(143364),	-- Armbands of the Chosen Dead
+											i(143344),	-- Gauntlets of the Chosen Dead
+											i(143360),	-- Girdle of the Chosen Dead
+											i(143351),	-- Chain Leggings of the Chosen Dead
+											i(143339),	-- Sabatons of the Chosen Dead
+										},
+										["classes"] = { 3, 7 }, -- Hunter, Shaman
+									}),		
+									i(143521, {	-- Ensemble: Funerary Plate of the Chosen Dead	
+										["groups"] = {
+											i(143345),	-- Visor of the Chosen Dead
+											i(143355),	-- Shoulderplates of the Chosen Dead
+											i(143367),	-- Greatcloak of the Chosen Dead
+											i(143334),	-- Chestplate of the Chosen Dead
+											i(143363),	-- Wristclamps of the Chosen Dead
+											i(143342),	-- Handguards of the Chosen Dead
+											i(143358),	-- Waistplate of the Chosen Dead
+											i(143349),	-- Legplates of the Chosen Dead
+											i(143337),	-- Treads of the Chosen Dead
+										},
+										["classes"] = { 2, 1, 6 }, -- Paladin, Warrior, Death Knight
+									}),	
 									i(142540),
 									i(142410),
 									i(142431),
@@ -145,6 +143,62 @@ _.Instances =
 									i(142422)
 								})),
 								cr(114344, e(1830, {	-- Guarm
+									i(143509, {	-- Ensemble: Vestment of the Chosen Dead
+										["groups"] = {
+											i(143346),	-- Circlet of the Chosen Dead
+											i(143356),	-- Mantle of the Chosen Dead
+											i(143369),	-- Drape of the Chosen Dead
+											i(143353),	-- Robe of the Chosen Dead
+											i(143362),	-- Bracers of the Chosen Dead
+											i(143343),	-- Gloves of the Chosen Dead
+											i(143359),	-- Sash of the Chosen Dead
+											i(143350),	-- Pantaloons of the Chosen Dead
+											i(143338),	-- Sandals of the Chosen Dead
+										},
+										["classes"] = { 5, 8, 9 }, -- Priest, Mage, Warock
+									}),		
+									i(143513, {	-- Ensemble: Garb of the Chosen Dead
+										["groups"] = {	
+											i(143348),	-- Hood of the Chosen Dead
+											i(143354),	-- Shoulderpads of the Chosen Dead
+											i(143366),	-- Wrap of the Chosen Dead
+											i(143336),	-- Tunic of the Chosen Dead
+											i(143365),	-- Wristguards of the Chosen Dead
+											i(143341),	-- Grips of the Chosen Dead
+											i(143361),	-- Strap of the Chosen Dead
+											i(143352),	-- Leggings of the Chosen Dead
+											i(143340),	-- Boots of the Chosen Dead
+										},
+										["classes"] = { 11, 4, 10, 12 }, -- Druid, Rogue, Monk, Demon Hunter
+									}),		
+									i(143517, {	-- Ensemble: Chains of the Chosen Dead	
+										["groups"] = {
+											i(143347),	-- Coif of the Chosen Dead
+											i(143357),	-- Pauldrons of the Chosen Dead
+											i(143368),	-- Cloak of the Chosen Dead
+											i(143335),	-- Chain Shirt of the Chosen Dead
+											i(143364),	-- Armbands of the Chosen Dead
+											i(143344),	-- Gauntlets of the Chosen Dead
+											i(143360),	-- Girdle of the Chosen Dead
+											i(143351),	-- Chain Leggings of the Chosen Dead
+											i(143339),	-- Sabatons of the Chosen Dead
+										},
+										["classes"] = { 3, 7 }, -- Hunter, Shaman
+									}),		
+									i(143521, {	-- Ensemble: Funerary Plate of the Chosen Dead	
+										["groups"] = {
+											i(143345),	-- Visor of the Chosen Dead
+											i(143355),	-- Shoulderplates of the Chosen Dead
+											i(143367),	-- Greatcloak of the Chosen Dead
+											i(143334),	-- Chestplate of the Chosen Dead
+											i(143363),	-- Wristclamps of the Chosen Dead
+											i(143342),	-- Handguards of the Chosen Dead
+											i(143358),	-- Waistplate of the Chosen Dead
+											i(143349),	-- Legplates of the Chosen Dead
+											i(143337),	-- Treads of the Chosen Dead
+										},
+										["classes"] = { 2, 1, 6 }, -- Paladin, Warrior, Death Knight
+									}),	
 									i(142412),
 									i(142432),
 									i(142423),
@@ -156,6 +210,62 @@ _.Instances =
 									i(142417)
 								})),
 								cr(114537, e(1829, {	-- Helya
+									i(143509, {	-- Ensemble: Vestment of the Chosen Dead
+										["groups"] = {
+											i(143346),	-- Circlet of the Chosen Dead
+											i(143356),	-- Mantle of the Chosen Dead
+											i(143369),	-- Drape of the Chosen Dead
+											i(143353),	-- Robe of the Chosen Dead
+											i(143362),	-- Bracers of the Chosen Dead
+											i(143343),	-- Gloves of the Chosen Dead
+											i(143359),	-- Sash of the Chosen Dead
+											i(143350),	-- Pantaloons of the Chosen Dead
+											i(143338),	-- Sandals of the Chosen Dead
+										},
+										["classes"] = { 5, 8, 9 }, -- Priest, Mage, Warock
+									}),		
+									i(143513, {	-- Ensemble: Garb of the Chosen Dead
+										["groups"] = {	
+											i(143348),	-- Hood of the Chosen Dead
+											i(143354),	-- Shoulderpads of the Chosen Dead
+											i(143366),	-- Wrap of the Chosen Dead
+											i(143336),	-- Tunic of the Chosen Dead
+											i(143365),	-- Wristguards of the Chosen Dead
+											i(143341),	-- Grips of the Chosen Dead
+											i(143361),	-- Strap of the Chosen Dead
+											i(143352),	-- Leggings of the Chosen Dead
+											i(143340),	-- Boots of the Chosen Dead
+										},
+										["classes"] = { 11, 4, 10, 12 }, -- Druid, Rogue, Monk, Demon Hunter
+									}),		
+									i(143517, {	-- Ensemble: Chains of the Chosen Dead	
+										["groups"] = {
+											i(143347),	-- Coif of the Chosen Dead
+											i(143357),	-- Pauldrons of the Chosen Dead
+											i(143368),	-- Cloak of the Chosen Dead
+											i(143335),	-- Chain Shirt of the Chosen Dead
+											i(143364),	-- Armbands of the Chosen Dead
+											i(143344),	-- Gauntlets of the Chosen Dead
+											i(143360),	-- Girdle of the Chosen Dead
+											i(143351),	-- Chain Leggings of the Chosen Dead
+											i(143339),	-- Sabatons of the Chosen Dead
+										},
+										["classes"] = { 3, 7 }, -- Hunter, Shaman
+									}),		
+									i(143521, {	-- Ensemble: Funerary Plate of the Chosen Dead	
+										["groups"] = {
+											i(143345),	-- Visor of the Chosen Dead
+											i(143355),	-- Shoulderplates of the Chosen Dead
+											i(143367),	-- Greatcloak of the Chosen Dead
+											i(143334),	-- Chestplate of the Chosen Dead
+											i(143363),	-- Wristclamps of the Chosen Dead
+											i(143342),	-- Handguards of the Chosen Dead
+											i(143358),	-- Waistplate of the Chosen Dead
+											i(143349),	-- Legplates of the Chosen Dead
+											i(143337),	-- Treads of the Chosen Dead
+										},
+										["classes"] = { 2, 1, 6 }, -- Paladin, Warrior, Death Knight
+									}),	
 									i(142521),
 									i(142433),
 									i(142427),
@@ -239,7 +349,7 @@ _.Instances =
 								i(142413)
 							})),
 							cr(114537, e(1829, {	-- Helya
-								i(143506, {	-- Ensemble: Vestment of the Chosen Dead
+								i(143507, {	-- Ensemble: Vestment of the Chosen Dead
 									["groups"] = {
 										i(143346),	-- Circlet of the Chosen Dead
 										i(143356),	-- Mantle of the Chosen Dead
@@ -253,7 +363,7 @@ _.Instances =
 									},
 									["classes"] = { 5, 8, 9 }, -- Priest, Mage, Warock
 								}),		
-								i(143510, {	-- Ensemble: Garb of the Chosen Dead
+								i(143511, {	-- Ensemble: Garb of the Chosen Dead
 									["groups"] = {	
 										i(143348),	-- Hood of the Chosen Dead
 										i(143354),	-- Shoulderpads of the Chosen Dead
@@ -267,7 +377,7 @@ _.Instances =
 									},
 									["classes"] = { 11, 4, 10, 12 }, -- Druid, Rogue, Monk, Demon Hunter
 								}),		
-								i(143514, {	-- Ensemble: Chains of the Chosen Dead	
+								i(143515, {	-- Ensemble: Chains of the Chosen Dead	
 									["groups"] = {
 										i(143347),	-- Coif of the Chosen Dead
 										i(143357),	-- Pauldrons of the Chosen Dead
@@ -281,7 +391,7 @@ _.Instances =
 									},
 									["classes"] = { 3, 7 }, -- Hunter, Shaman
 								}),		
-								i(143518, {	-- Ensemble: Funerary Plate of the Chosen Dead	
+								i(143519, {	-- Ensemble: Funerary Plate of the Chosen Dead	
 									["groups"] = {
 										i(143345),	-- Visor of the Chosen Dead
 										i(143355),	-- Shoulderplates of the Chosen Dead
@@ -338,7 +448,7 @@ _.Instances =
 							cr(114537, e(1829, {	-- Helya
 								ach(11387, { -- The Chosen
 									["groups"] = {
-										i(143506, {	-- Ensemble: Vestment of the Chosen Dead
+										i(143508, {	-- Ensemble: Vestment of the Chosen Dead
 											["groups"] = {
 												i(143346),	-- Circlet of the Chosen Dead
 												i(143356),	-- Mantle of the Chosen Dead
@@ -352,7 +462,7 @@ _.Instances =
 											},
 											["classes"] = { 5, 8, 9 }, -- Priest, Mage, Warock
 										}),		
-										i(143510, {	-- Ensemble: Garb of the Chosen Dead
+										i(143512, {	-- Ensemble: Garb of the Chosen Dead
 											["groups"] = {	
 												i(143348),	-- Hood of the Chosen Dead
 												i(143354),	-- Shoulderpads of the Chosen Dead
@@ -366,7 +476,7 @@ _.Instances =
 											},
 											["classes"] = { 11, 4, 10, 12 }, -- Druid, Rogue, Monk, Demon Hunter
 										}),		
-										i(143514, {	-- Ensemble: Chains of the Chosen Dead	
+										i(143516, {	-- Ensemble: Chains of the Chosen Dead	
 											["groups"] = {
 												i(143347),	-- Coif of the Chosen Dead
 												i(143357),	-- Pauldrons of the Chosen Dead
@@ -380,7 +490,7 @@ _.Instances =
 											},
 											["classes"] = { 3, 7 }, -- Hunter, Shaman
 										}),		
-										i(143518, {	-- Ensemble: Funerary Plate of the Chosen Dead	
+										i(143520, {	-- Ensemble: Funerary Plate of the Chosen Dead	
 											["groups"] = {
 												i(143345),	-- Visor of the Chosen Dead
 												i(143355),	-- Shoulderplates of the Chosen Dead
