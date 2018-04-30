d2f4c7e683f0bc3cf8236d271b69eb3cb9e7616d
Lucetia-Detheroc <aceso2896@gmail.com>
2018-04-29 18:31:16 -0700

    - Invasions now show up in the minilist for Azsuna, Stormheim, Val'sharah and Highmountain - NPC's have now been tied to each invasion so you can hover and see what is missing. - Invasion World Quest gear  has been added

--- contrib/Parser/DATAS/4 - Events/05 Legion Assaults/WorldEvents_LegionInvasions.lua
+++ contrib/Parser/DATAS/4 - Events/05 Legion Assaults/WorldEvents_LegionInvasions.lua
@@ -5,22 +5,113 @@ _.WorldEvents =
 {
 	n(-543, { 	-- Broken Isles: Legion Invasions
 		["groups"] = {
-			m(1007, { -- Broken Isles
-				i(138762), -- Niskaran Morning Star
-				i(139158), -- Battle Mace of the Niskaran Guard
-				i(139163), -- Star of Niskara
-				i(139155), -- Legion's Edge
-				i(139157), -- Dread Vanquisher's Hacker
-				i(139156), -- Argus Decapitator
-				i(139159), -- Face of Ruin
-				i(139160), -- Eredar Battle Staff
-				i(139161), -- Soulmage's Spire
-				i(139162), -- Blood Guard's Tooth
-				i(139164), -- Wrathblade
-				i(139165), -- Bloodseeker's Bulwark
-				i(139166), -- Soulstealer's Barrier
+			n(118180, { -- Dread Vizier Gra'tork <Legion Commander>
+				["groups"] = {
+					i(138762), -- Niskaran Morning Star
+					i(139158), -- Battle Mace of the Niskaran Guard
+					i(139163), -- Star of Niskara
+					i(139155), -- Legion's Edge
+					i(139157), -- Dread Vanquisher's Hacker
+					i(139156), -- Argus Decapitator
+					i(139159), -- Face of Ruin
+					i(139160), -- Eredar Battle Staff
+					i(139161), -- Soulmage's Spire
+					i(139162), -- Blood Guard's Tooth
+					i(139164), -- Wrathblade
+					i(139165), -- Bloodseeker's Bulwark
+					i(139166), -- Soulstealer's Barrier
+				},
+			}),
+			n(119579, { -- Fel Commandere Erixtol
+				["groups"] = {
+					i(138762), -- Niskaran Morning Star
+					i(139158), -- Battle Mace of the Niskaran Guard
+					i(139163), -- Star of Niskara
+					i(139155), -- Legion's Edge
+					i(139157), -- Dread Vanquisher's Hacker
+					i(139156), -- Argus Decapitator
+					i(139159), -- Face of Ruin
+					i(139160), -- Eredar Battle Staff
+					i(139161), -- Soulmage's Spire
+					i(139162), -- Blood Guard's Tooth
+					i(139164), -- Wrathblade
+					i(139165), -- Bloodseeker's Bulwark
+					i(139166), -- Soulstealer's Barrier
+				},
+			}),
+			n(118840, { -- Lord Commander Alexius
+				["groups"] = {
+					i(138762), -- Niskaran Morning Star
+					i(139158), -- Battle Mace of the Niskaran Guard
+					i(139163), -- Star of Niskara
+					i(139155), -- Legion's Edge
+					i(139157), -- Dread Vanquisher's Hacker
+					i(139156), -- Argus Decapitator
+					i(139159), -- Face of Ruin
+					i(139160), -- Eredar Battle Staff
+					i(139161), -- Soulmage's Spire
+					i(139162), -- Blood Guard's Tooth
+					i(139164), -- Wrathblade
+					i(139165), -- Bloodseeker's Bulwark
+					i(139166), -- Soulstealer's Barrier
+				},
+			}),
+			n(118975, { -- Xeritas
+				["groups"] = {
+					i(138762), -- Niskaran Morning Star
+					i(139158), -- Battle Mace of the Niskaran Guard
+					i(139163), -- Star of Niskara
+					i(139155), -- Legion's Edge
+					i(139157), -- Dread Vanquisher's Hacker
+					i(139156), -- Argus Decapitator
+					i(139159), -- Face of Ruin
+					i(139160), -- Eredar Battle Staff
+					i(139161), -- Soulmage's Spire
+					i(139162), -- Blood Guard's Tooth
+					i(139164), -- Wrathblade
+					i(139165), -- Bloodseeker's Bulwark
+					i(139166), -- Soulstealer's Barrier
+				},
+			}),
+			n(-34, { 	-- World Quests
+				["groups"] = {
+					i(146766), -- Nethercaller Cloak
+					i(146786),	-- Vileweave Cap
+					i(146791),	-- Vileweave Amice
+					i(146789),	-- Vileweave Robes
+					i(146792),	-- Vileweave Cuffs
+					i(146787),	-- Vileweave Mitts
+					i(146785),	-- Vileweave Sash
+					i(146788),	-- Vileweave Leggings
+					i(146790),	-- Vileweave Slippers
+					i(146772),	-- Netherfiend Headgear
+					i(146774),	-- Netherfiend Mantle
+					i(146773),	-- Netherfiend Chestpiece
+					i(146768),	-- Netherfiend Armbands
+					i(146771),	-- Netherfiend Grips
+					i(146775),	-- Netherfiend Belt
+					i(146770),	-- Netherfiend Trousers
+					i(146769),	-- Netherfiend Treads
+					i(146781),	-- Ered'ruin Coif
+					i(146783),	-- Ered'ruin Spaulders
+					i(146784),	-- Ered'ruin Chestguard
+					i(146778),	-- Ered'ruin Bracers
+					i(146779),	-- Ered'ruin Gloves
+					i(146777),	-- Ered'ruin Girdle
+					i(146782),	-- Ered'ruin Legguards
+					i(146780),	-- Ered'ruin Boots
+					i(146761),	-- Xorothian Helmet
+					i(146763),	-- Xorothian Pauldrons
+					i(146758),	-- Xorothian Breastplate
+					i(146765),	-- Xorothian Vambraces
+					i(146759),	-- Xorothian Gauntlets
+					i(146760),	-- Xorothian Waistplate
+					i(146762),	-- Xorothian Greaves
+					i(146764),	-- Xorothian Sabatons
+				},
 			}),
 		},
-		["achievementID"] = 11544. -- Defender of the Broken Isles
+		["achievementID"] = 11544, -- Defender of the Broken Isles
+		["maps"] = { 1018, 1024, 1017, 1015 },
 	}),
 };
