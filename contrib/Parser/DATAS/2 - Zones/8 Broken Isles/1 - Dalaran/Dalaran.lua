5cbaba9bd47bc6cab8fac2be89a7f0861f7de5fa
Lucetia-Detheroc <aceso2896@gmail.com>
2018-05-01 02:47:59 -0700

    - Purified Titan Essence no longer shows the legendary craftable or Insignia of the Grand Army

--- contrib/Parser/DATAS/2 - Zones/8 Broken Isles/1 - Dalaran/Dalaran.lua
+++ contrib/Parser/DATAS/2 - Zones/8 Broken Isles/1 - Dalaran/Dalaran.lua
@@ -2167,246 +2167,6 @@ _.Zones =
 					})),
 					n(106655, { -- Arcanomancer Vridiel <Blacksmithing Trainer>
 						currency(1533, { -- Wakening Essences
-							--[[ LEAVE FOR TESTING LATER
-							i(132376),	-- Acherus Drapes
-							i(137014),	-- Achor, the Eternal Hunger
-							i(140846),	-- Aegisjalmur, the Armguards of Awe
-							i(132443),	-- Aggramar's Stride (Paladin, Warrior, Death Knight)
-							i(137024),	-- Ailuro Pouncers
-							i(137084),	-- Akainu's Absolute Justice
-							i(137102),	-- Al'Akir's Acrimony
-							i(132445),	-- Al'maiesh, the Cord of Hope
-							i(132460),	-- Alythess's Pyrogenics
-							i(154172),	-- Aman'Thul's Vision
-							i(137072),	-- Aman'Thul's Wisdom
-							i(137038),	-- Anger of the Half-Giants
-							i(132409),	-- Anund's Seared Shackles
-							i(144277),	-- Anvil-Hardened Wristwraps
-							i(151822),	-- Ararat's Bloodmirror
-							i(137060),	-- Archavon's Heavy Hand
-							i(144249),	-- Archimonde's Hatred Reborn
-							i(144358),	-- Ashes to Dust
-							i(137052),	-- Ayala's Stone Heart
-							i(151801),	-- Behemoth Headdress
-							i(133977),	-- Belo'vir's Final Stand
-							i(137017),	-- Breastplate of the Golden Val'kyr
-							i(144361),	-- Butcher's Bone Apron
-							i(137101),	-- Call of the Wild
-							i(137088),	-- Ceann-Ar Charger
-							i(151803),	-- Celerity of the Windrunners
-							i(146666),	-- Celumbra, the Night's Dichotomy (Mage, Priest, Lock)
-							i(137019),	-- Cenedril, Reflector of Hatred
-							i(137086),	-- Chain of Thrayn
-							i(151783),	-- Chameleon Song
-							i(151798),	-- Chaos Theory
-							i(137040),	-- Chatoyant Signet
-							i(133976),	-- Cinidaria, the Symbiote (Druid, Rogue, Monk, Demon Hunter)
-							i(137066),	-- Cloak of Fel Flames
-							i(151796),	-- Cold Heart
-							i(144293),	-- Consort's Cold Core
-							i(151809),	-- Contained Infernal Core
-							i(132442),	-- Cord of Infinity
-							i(133800),	-- Cord of Maiev, Priestess of the Moon
-							i(132863),	-- Darckli's Dragonfire Diadem
-							i(144280),	-- Death March
-							i(144279),	-- Delusions of Grandeur
-							i(137100),	-- Denial of the Half-Giants
-							i(137018),	-- Destiny Driver
-							i(151784),	-- Doorway to Nowhere
-							i(132441),	-- Draugr, Girdle of the Everlasting King
-							i(137097),	-- Drinking Horn Cover
-							i(137041),	-- Dual Determination
-							i(137030),	-- Duskwalker's Footpads
-							i(137074),	-- Echoes of the Great Sundering
-							i(137095),	-- Edraith, Bonds of Aglaya
-							i(137028),	-- Ei'thas, Lunar Glides of Eramas
-							i(137015),	-- Ekowraith, Creator of Worlds
-							i(137036),	-- Elemental Rebalancers
-							i(137067),	-- Elize's Everlasting Encasement
-							i(137616),	-- Emalon's Charged Core
-							i(132447),	-- Entrancing Trousers of An'juna
-							i(137026),	-- Essence of Infusion
-							i(132861),	-- Estel, Dejahna's Inspiration
-							i(137050),	-- Eye of the Twisting Nether
-							i(137045),	-- Eyee of the Collidus the Warp-Watcher
-							i(132456),	-- Feretory of Souls
-							i(144354),	-- Fiery Red Maimers
-							i(151785),	-- Fire in the Deep
-							i(137027),	-- Firestone Walkers
-							i(137051),	-- Focuser of Jonat, the Elder
-							i(138854),	-- Fragment of the Betrayer's Prison
-							i(137043),	-- Frizzo's Fingertrap
-							i(137063),	-- Fundamental Observation
-							i(151802),	-- Fury of Nature
-							i(137079),	-- Gai Plin's Soothing Sash
-							i(144274),	-- Gravity Seal
-							i(137099),	-- Greenskin's Waterlogged Wristcuffs
-							i(151814),	-- Heart of the Void
-							i(137047),	-- Heathcliff's Immortality
-							i(137082),	-- Hellbrine, Rope of the Mist Marauder
-							i(137057),	-- Hidden Master's Forbidden Touch
-							i(132394),	-- Hood of Eternal Disdain
-							i(144260),	-- Ice Time
-							i(137039),	-- Impeccable Fel Essence
-							i(151786),	-- Inner Hallation
-							i(137049),	-- Insignia of Ravenholdt
-							i(152626),	-- Insignia of the Grand Army
-							i(137085),	-- Intact Nazjatar Molting
-							i(137046),	-- Iterendi, Crown Jewel of Silvermoon
-							i(137044),	-- Jewel of the Lost Abbey
-							i(137065),	-- Justice Gaze
-							i(137108),	-- Kakushan's Stormscale Gauntlets
-							i(144244),	-- Kam Xi'raff
-							i(137029),	-- Katsuo's Eclipse
-							i(132374),	-- Kazzak's Final Curse
-							i(137053),	-- Kazzalax, Fujieda's Fury
-							i(144259),	-- Kil'jaeden's Burning Wish
-							i(138949),	-- Kirel Narak
-							i(132366),	-- Koltira's Newfound Will
-							i(132454),	-- Koralon's Burning Touch
-							i(144295),	-- Lady and the Child
-							i(132411),	-- Lady Vashj's Grasp
-							i(133974),	-- Lana'thel's Lament
-							i(137068),	-- Leggings of the Black Flame
-							i(144369),	-- Lessons of Space-Time
-							i(137048),	-- Liadrin's Fury Unleashed
-							i(137022),	-- Loramus Thalipedes' Sacrifice
-							i(137056),	-- Luffa Wrappings
-							i(132407),	-- Magistrike Restraits
-							i(141353),	-- Magnetized Blasting Cap Launcher
-							i(138140),	-- Magtheridon's Banished Bracers
-							i(132864),	-- Mangaza's Madness
-							i(137107),	-- Mannoroth's Bloodletting Manacles
-							i(151808),	-- Mantle of the First Kirin Tor
-							i(144236),	-- Mantle of the Master Assassin
-							i(144273), 	-- Maraad's Dying Breath
-							i(137220),	-- March of the Legion
-							i(132406),	-- Marquee Bindings of the Sun King
-							i(144303),	-- MKII  Gyroscopic Stabilizer
-							i(137090),	-- Mo'arg Bionic Stabilizers
-							i(132437),	-- Mother Shahraz's Seduction
-							i(132450),	-- Muze's Unwavering Will
-							i(132451),	-- Mystic Kilt of the Rune Master
-							i(137087),	-- Naj'entus's Vertebrae
-							i(137276),	-- N'ero, Band of Promises
-							i(137034),	-- Nesingwary's Trapping Treads
-							i(137104),	-- Nobundo's Redemption
-							i(132455),	-- Norgannon's Foresigh (Priest, Mage, Lock)
-							i(144432),	-- Oakheart's Puny Quods
-							i(151799),	-- Obilivion's Embrace
-							i(137076),	-- Obsidian Stone Spaulders
-							i(132375),	-- Odr, Shawl of the Ymirjar
-							i(137092),	-- Oneth's Intuition
-							i(138879),	-- Ovyd's Winter Wrap
-							i(151805),	-- Parsel's Tongue
-							i(132459),	-- Perseverance of the Ebon Martyr
-							i(137096),	-- Petrichor Lagniappe
-							i(151812),	-- Pillars of Inmost Light
-							i(132357),	-- Pillars of the Dark Portal
-							i(132457),	-- Power Cord of Lethtendris
-							i(137058),	-- Praetorian's Tidecallers
-							i(137083),	-- Pristine Prot-Scale Girdle
-							i(137023),	-- Promise of Elune, the Moon Goddess
-							i(132444),	-- Prydaz, Xavric's Magnum Opus
-							i(144355),	-- Pyrotex Ignition Cloth
-							i(137227),	-- Qa'pla, Eredun War Order
-							i(137061),	-- Raddon's Cascading Eyes
-							i(151800),	-- Radiant Moonlight
-							i(144247),	-- Rammal's Ulterior Motive
-							i(132453),	-- Rattlegore Bone Legplates
-							i(144364),	-- Reap and Sow
-							i(132393),	-- Recurrent Ritual
-							i(146667),	-- Rethu's Incessant Courage (Paladin, Warrior, Death Knight)
-							i(132413),	-- Rhonin's Assaulting Armwraps
-							i(137080),	-- Roar of the Seven Lions
-							i(132466),	-- Roots of Shaladrassil (Hunter, Shaman)
-							i(137071),	-- Runemaster's Pauldrons
-							i(132378),	-- Sacrolash's Dark Strike
-							i(144275),	-- Saruan's Resolve
-							i(151813),	-- Scarlet Inquisitor's Expurgation
-							i(137223),	-- Seal of Necrofantasia
-							i(132452),	-- Sephuz's Secret
-							i(132367),	-- Service of Gorefiend
-							i(132365),	-- Shackles of Brynadaor
-							i(137032),	-- Shadow Satyr's Walk
-							i(132410),	-- Shard of the Exodar
-							i(151810),	-- Shattered Fragments of Sindragosa
-							i(141321),	-- Shivarran Symmetry
-							i(132379),	-- Sin'dorei Spite
-							i(132436),	-- Skjoldr, Sanctuary of Ivangont
-							i(144281),	-- Skullflower's Haemostasis
-							i(137025),	-- Skysec's Hold
-							i(151819),	-- Smoldering Heart
-							i(151636),	-- Soul of the Archdruid
-							i(151642),	-- Soul of the Archmage
-							i(151640),	-- Soul of the Deahtlord
-							i(151647),	-- Soul of the Farseer
-							i(151643),	-- Soul of the Grandmaster
-							i(151646),	-- Soul of the High Priest
-							i(151644),	-- Soul of the Highlord
-							i(151641),	-- Soul of the Huntmaster
-							i(151649),	-- Soul of the Netherlord
-							i(150936),	-- Soul of the Shadowblade
-							i(151639),	-- Soul of the Slayer
-							i(151795),	-- Soulflayer's Corruption
-							i(144292),	-- Spirit of the Darkness Flame
-							i(138117),	-- Spiritual Journey
-							i(137103),	-- Storm Tempests
-							i(151788),	-- Stormstout's Last Gasp
-							i(132381),	-- Streten's Sleepless Shackles
-							i(137075),	-- Tak'theritrix's Shoulderpads
-							i(137042),	-- Tearstone of Elune
-							i(151787),	-- the Alabaster Lady
-							i(137382),	-- The Apex Predator's Claw
-							i(151817),	-- the Curse of Restlessness
-							i(137078),	-- The Dark Titan's Advice
-							i(137035),	-- The Deceiver's Blood Pact
-							i(137091),	-- The Defiler's Lost Vambraces
-							i(137021),	-- The Dreadlord's Deceit
-							i(137062),	-- the Emerald Dreamcatch
-							i(151815),	-- the Empty Crown
-							i(151818),	-- the Fist of the Dead
-							i(151823),	-- The Great Storm's Eye
-							i(132448),	-- The Instructor's Fourth Lesson
-							i(144326),	-- the Mantle of Command
-							i(151821),	-- the Master Harvester
-							i(146669),	-- The Sentinel's Eternal Refuge (Druid, Rogue, Monk, Demon Hunter)
-							i(137064),	-- The Shadow Hunter's Voodoo Mask
-							i(151782),	-- the Topless Tower
-							i(133973),	-- The Twins' Painful Touch
-							i(137054),	-- the Walls Fell
-							i(137094),	-- The Wildshaper's Clutch
-							i(151811),	-- the Wind Blows
-							i(137031),	-- Thraxi's Tricksy Treads
-							i(137089),	-- Thundergod's Vigor 
-							i(143728),	-- Timeless Stratagem
-							i(132458),	-- Toravon's Whiteout Bindings
-							i(137070),	-- Tyelca, Ferren Marcus's Stature
-							i(137059),	-- Tyr's Hand of Faith
-							i(137033),	-- Ulr's Feather Snowshoes
-							i(143732),	-- Uncertain Reminder
-							i(137073),	-- Unison Spaulders
-							i(151807),	-- Unseen Predator's Cloak
-							i(137105),	-- Uther's Guard
-							i(137037),	-- Uvanimor, the Unbeautiful
-							i(151824),	-- Valarjar Berserkers
-							i(144258),	-- Velen's Future Sight
-							i(146668),	-- Vigilance Perch (Hunter, Shaman)
-							i(144385),	-- Wakener's Loyalty
-							i(137081),	-- War Belt of the Sentinel Army
-							i(137077),	-- Weight of the Earth
-							i(137020),	-- Whisper  of the Nathrezim
-							i(132369),	-- Wilfred's Sigil of Superior Summoning
-							i(137069),	-- Will of Valeera
-							i(132461),	-- Xalan the Feared's Clench
-							i(137109),	-- X'anshi, Shroud of Archbishop Benedictus
-							i(144242),	-- X'oni's Caress
-							i(133970),	-- Zann'esu Journey
-							i(144438),	-- Zeks Exterminatus
-							i(133971),	-- Zenk'aram, Iridi's Anadem
-							i(137055),	-- Zevrim's Hunger
-							i(137098),	-- Zoldyck Family Training Shackles
-							--]]
 							cl(12, { -- Demon Hunter
 								i(157796, { -- Purified Titan Essence
 									["groups"] = {
@@ -2424,11 +2184,9 @@ _.Zones =
 										i(151799), -- Obilivion's Embrace
 										i(138949), -- Kirel Narak
 										i(137022), -- Loramus Thalipedes' Sacrifice
-										i(146669), -- The Sentinel's Eternal Refuge (Druid, Rogue, Monk, Demon Hunter)
 										i(137038), -- Anger of the Half-Giants
 										i(138854), -- Fragment of the Betrayer's Prison
 										i(151639), -- Soul of the Slayer
-										i(152626), -- Insignia of the Grand Army
 										i(132452), -- Sephuz's Secret
 										i(154172), -- Aman'Thul's Vision
 										i(144249), -- Archimonde's Hatred Reborn
@@ -2452,7 +2210,6 @@ _.Zones =
 									i(132458), -- Toravon's Whiteout Bindings
 									i(132441), -- Draugr, Girdle of the Everlasting King
 									i(132366), -- Koltira's Newfound Will
-									i(146667), -- Rethu's Incessant Courage (Paladin, Warrior, Death Knight)
 									i(132367), -- Service of Gorefiend
 									i(144293), -- Consort's Cold Core
 									i(132453), -- Rattlegore Bone Legplates
@@ -2461,7 +2218,6 @@ _.Zones =
 									i(137037), -- Uvanimor, the Unbeautiful
 									i(137223), -- Seal of Necrofantasia
 									i(151640), -- Soul of the Deahtlord
-									i(152626), -- Insignia of the Grand Army
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144249), -- Archimonde's Hatred Reborn
@@ -2492,8 +2248,6 @@ _.Zones =
 									i(137026), -- Essence of Infusion
 									i(137023), -- Promise of Elune, the Moon Goddess
 									i(137025), -- Skysec's Hold
-									i(146669), -- The Sentinel's Eternal Refuge (Druid, Rogue, Monk, Demon Hunter)
-									i(152626), -- Insignia of the Grand Army
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144249), -- Archimonde's Hatred Reborn
@@ -2517,7 +2271,6 @@ _.Zones =
 									i(137101), -- Call of the Wild
 									i(141353), -- Magnetized Blasting Cap Launcher
 									i(144303), -- MKII  Gyroscopic Stabilizer
-									i(146668), -- Vigilance Perch (Hunter, Shaman)
 									i(137082), -- Hellbrine, Rope of the Mist Marauder
 									i(137080), -- Roar of the Seven Lions
 									i(137081), -- War Belt of the Sentinel Army
@@ -2525,7 +2278,6 @@ _.Zones =
 									i(137034), -- Nesingwary's Trapping Treads
 									i(137227), -- Qa'pla, Eredun War Order
 									i(137033), -- Ulr's Feather Snowshoes
-									i(152626), -- Insignia of the Grand Army
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144259), -- Kil'jaeden's Burning Wish
@@ -2541,7 +2293,6 @@ _.Zones =
 									i(132863), -- Darckli's Dragonfire Diadem
 									i(144274), -- Gravity Seal
 									i(151810), -- Shattered Fragments of Sindragosa
-									i(146666), -- Celumbra, the Night's Dichotomy (Mage, Priest, Lock)
 									i(151809), -- Contained Infernal Core
 									i(144260), -- Ice Time
 									i(151808), -- Mantle of the First Kirin Tor
@@ -2555,8 +2306,7 @@ _.Zones =
 									i(132454), -- Koralon's Burning Touch
 									i(133970), -- Zann'esu Journey
 									i(132451), -- Mystic Kilt of the Rune Master
-									i(132455), -- Norgannon's Foresigh (Priest, Mage, Lock)
-									i(152626), -- Insignia of the Grand Army
+									i(132455), -- Norgannon's Foresight (Priest, Mage, Lock)
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144259), -- Kil'jaeden's Burning Wish
@@ -2584,8 +2334,6 @@ _.Zones =
 									i(137028), -- Ei'thas, Lunar Glides of Eramas
 									i(137027), -- Firestone Walkers
 									i(137029), -- Katsuo's Eclipse
-									i(146669), -- The Sentinel's Eternal Refuge (Druid, Rogue, Monk, Demon Hunter)
-									i(152626), -- Insignia of the Grand Army
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144249), -- Archimonde's Hatred Reborn
@@ -2614,10 +2362,8 @@ _.Zones =
 									i(137105), -- Uther's Guard
 									i(137059), -- Tyr's Hand of Faith
 									i(137086), -- Chain of Thrayn
-									i(146667), -- Rethu's Incessant Courage (Paladin, Warrior, Death Knight)
 									i(137070), -- Tyelca, Ferren Marcus's Stature
 									i(132443), -- Aggramar's Stride (Paladin, Warrior, Death Knight)
-									i(152626), -- Insignia of the Grand Army
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144249), -- Archimonde's Hatred Reborn
@@ -2635,7 +2381,6 @@ _.Zones =
 									i(144244), -- Kam Xi'raff
 									i(151787), -- The Alabaster Lady
 									i(133971), -- Zenk'aram, Iridi's Anadem
-									i(146666), -- Celumbra, the Night's Dichotomy (Mage, Priest, Lock)
 									i(151786), -- Inner Hallation
 									i(132437), -- Mother Shahraz's Seduction
 									i(137109), -- X'anshi, Shroud of Archbishop Benedictus
@@ -2651,8 +2396,7 @@ _.Zones =
 									i(133800), -- Cord of Maiev, Priestess of the Moon
 									i(132864), -- Mangaza's Madness
 									i(132447), -- Entrancing Trousers of An'juna
-									i(132455), -- Norgannon's Foresigh (Priest, Mage, Lock)
-									i(152626), -- Insignia of the Grand Army
+									i(132455), -- Norgannon's Foresight (Priest, Mage, Lock)
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144259), -- Kil'jaeden's Burning Wish
@@ -2678,9 +2422,7 @@ _.Zones =
 									i(137069), -- Will of Valeera
 									i(137030), -- Duskwalker's Footpads
 									i(137032), -- Shadow Satyr's Walk
-									i(146669), -- The Sentinel's Eternal Refuge (Druid, Rogue, Monk, Demon Hunter)
 									i(137031), -- Thraxi's Tricksy Treads
-									i(152626), -- Insignia of the Grand Army
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144259), -- Kil'jaeden's Burning Wish
@@ -2700,7 +2442,6 @@ _.Zones =
 									i(137104), -- Nobundo's Redemption
 									i(137058), -- Praetorian's Tidecallers
 									i(151819), -- Smoldering Heart
-									i(146668), -- Vigilance Perch (Hunter, Shaman)
 									i(137085), -- Intact Nazjatar Molting
 									i(137083), -- Pristine Prot-Scale Girdle
 									i(137103), -- Storm Tempests
@@ -2708,7 +2449,6 @@ _.Zones =
 									i(137036), -- Elemental Rebalancers
 									i(138117), -- Spiritual Journey
 									i(137035), -- The Deceiver's Blood Pact
-									i(152626), -- Insignia of the Grand Army
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144259), -- Kil'jaeden's Burning Wish
@@ -2723,7 +2463,6 @@ _.Zones =
 								i(157796, { -- Purified Titan Essence
 									i(132394), -- Hood of Eternal Disdain
 									i(144385), -- Wakener's Loyalty
-									i(146666), -- Celumbra, the Night's Dichotomy (Mage, Priest, Lock)
 									i(144369), -- Lessons of Space-Time
 									i(132393), -- Recurrent Ritual
 									i(132375), -- Odr, Shawl of the Ymirjar
@@ -2736,8 +2475,7 @@ _.Zones =
 									i(132374), -- Kazzak's Final Curse
 									i(132457), -- Power Cord of Lethtendris
 									i(132357), -- Pillars of the Dark Portal
-									i(132455), -- Norgannon's Foresigh (Priest, Mage, Lock)
-									i(152626), -- Insignia of the Grand Army
+									i(132455), -- Norgannon's Foresight (Priest, Mage, Lock)
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144259), -- Kil'jaeden's Burning Wish
@@ -2759,13 +2497,11 @@ _.Zones =
 									i(137060), -- Archavon's Heavy Hand
 									i(137108), -- Kakushan's Stormscale Gauntlets
 									i(137087), -- Naj'entus's Vertebrae
-									i(146667), -- Rethu's Incessant Courage (Paladin, Warrior, Death Knight)
 									i(137089), -- Thundergod's Vigor 
 									i(151822), -- Ararat's Bloodmirror
 									i(151824), -- Valarjar Berserkers
 									i(137077), -- Weight of the Earth
 									i(132443), -- Aggramar's Stride (Paladin, Warrior, Death Knight)
-									i(152626), -- Insignia of the Grand Army
 									i(132452), -- Sephuz's Secret
 									i(154172), -- Aman'Thul's Vision
 									i(144249), -- Archimonde's Hatred Reborn
