2f7231c97841344af2af0b6eda1b02484a98d7a4
ada <slumber@love.com>
2018-05-02 00:47:36 -0500

    - more BC BS

--- contrib/Parser/DATAS/1 - Dungeons Raids/2 - Burning Crusade/1 - Raids/3 Magtheridon's Lair.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/2 - Burning Crusade/1 - Raids/3 Magtheridon's Lair.lua
@@ -9,63 +9,65 @@ _.Instances =
 			inst(747, { 	-- Magtheridon's Lair
 				["groups"] = {
 					cr(17257, e(1566, {
-							a(i(32385, {	-- Magtheridon's Head (Alliance)
-								a(q(11002)),	-- The Fall of Magtheridon (Alliance)
-							})),
-							h(i(32386, {	-- Magtheridon's Head (Horde)
-								h(q(11003)),	-- The Fall of Magtheridon (Horde)
-							})),
-							i(29754, {	-- Chestguard of the Fallen Champion
-								i(25831),	-- Gladiator's Leather Tunic
-								i(25997),	-- Gladiator's Linked Armor
-								i(27469),	-- Gladiator's Mail Armor
-								i(27702),	-- Gladiator's Lamellar Chestpiece
-								i(27879),	-- Gladiator's Scaled Chestpiece
-								i(31396),	-- Gladiator's Ringmail Armor
-								i(31613),	-- Gladiator's Ornamented Chestguard
-								i(29029),	-- Cyclone Hauberk
-								i(29033),	-- Cyclone Chestguard
-								i(29038),	-- Cyclone Breastplate
-								i(29045),	-- Netherblade Chestpiece
-								i(29062),	-- Justicar Chestpiece
-								i(29066),	-- Justicar Chestguard
-								i(29071)	-- Justicar Breastplate
-							}),
-							i(29753, {	-- Chestguard of the Fallen Defender
-								i(24544),	-- Gladiator's Plate Chestpiece
-								i(27711),	-- Gladiator's Satin Robe
-								i(28130),	-- Gladiator's Dragonhide Tunic
-								i(28140),	-- Gladiator's Wyrmhide Tunic
-								i(31379),	-- Gladiator's Kodohide Tunic
-								i(31413),	-- Gladiator's Mooncloth Robe
-								i(29056),	-- Shroud of the Incarnate
-								i(29087),	-- Chestguard of Malorne
-								i(29091),	-- Chestpiece of Malorne
-								i(29096),	-- Breastplate of Malorne
-								i(29012),	-- Warbringer Chestguard
-								i(29019),	-- Warbringer Breastplate
-								i(29050)	-- Robes of the Incarnate
-							}),
-							i(29755, {	-- Chestguard of the Fallen Hero
-								i(30200),	-- Gladiator's Felweave Raiment
-								i(24552),	-- Gladiator's Dreadweave Robe
-								i(25856),	-- Gladiator's Silk Raiment
-								i(28334),	-- Gladiator's Chain Armor
-								i(28964),	-- Voidheart Robe
-								i(29077),	-- Vestments of the Aldor
-								i(29082),	-- Demon Stalker Harness
-							}),
-							i(28782),	-- Crystalheart Pulse-Staff
-							i(28774),	-- Glaive of the Pit
-							i(28783),	-- Eredar Wand of Obliteration
-							i(29458),	-- Aegis of the Vindicator
-							i(28781),	-- Karaborian Talisman
-							i(28775),	-- Thundering Greathelm
-							i(28777),	-- Cloak of the Pit Stalker
-							i(28776),	-- Liar's Tongue Gloves
-							i(28780),	-- Soul-Eater's Handwraps
-							i(28779),	-- Girdle of the Endless Pit
-							i(28778),	-- Terror Pit Girdle
+						a(i(32385, {	-- Magtheridon's Head (Alliance)
+							a(q(11002)),	-- The Fall of Magtheridon (Alliance)
+						})),
+						h(i(32386, {	-- Magtheridon's Head (Horde)
+							h(q(11003)),	-- The Fall of Magtheridon (Horde)
+						})),
+						i(29754, {		-- Chestguard of the Fallen Champion
+							i(27702),	-- Gladiator's Lamellar Chestpiece
+							i(31613),	-- Gladiator's Ornamented Chestguard
+							i(27879),	-- Gladiator's Scaled Chestpiece
+							i(29071),	-- Justicar Breastplate
+							i(29066),	-- Justicar Chestguard
+							i(29062),	-- Justicar Chestpiece
+							i(25831),	-- Gladiator's Leather Tunic
+							i(29045),	-- Netherblade Chestpiece
+							i(29038),	-- Cyclone Breastplate
+							i(29033),	-- Cyclone Chestguard
+							i(29029),	-- Cyclone Hauberk
+							i(25997),	-- Gladiator's Linked Armor
+							i(27469),	-- Gladiator's Mail Armor
+							i(31396),	-- Gladiator's Ringmail Armor
+						}),
+						i(29753, {	-- Chestguard of the Fallen Defender
+							i(29096),	-- Breastplate of Malorne
+							i(29087),	-- Chestguard of Malorne
+							i(29091),	-- Chestpiece of Malorne
+							i(28130),	-- Gladiator's Dragonhide Tunic
+							i(31379),	-- Gladiator's Kodohide Tunic
+							i(28140),	-- Gladiator's Wyrmhide Tunic
+							i(31413),	-- Gladiator's Mooncloth Robe
+							i(27711),	-- Gladiator's Satin Robe
+							i(29050),	-- Robes of the Incarnate
+							i(29056),	-- Shroud of the Incarnate
+							i(24544),	-- Gladiator's Plate Chestpiece
+							i(29019),	-- Warbringer Breastplate
+							i(29012),	-- Warbringer Chestguard
+						}),
+						i(29755, {	-- Chestguard of the Fallen Hero
+							i(29082),	-- Demon Stalker Harness
+							i(28334),	-- Gladiator's Chain Armor
+							i(25856),	-- Gladiator's Silk Raiment
+							i(29077),	-- Vestments of the Aldor
+							i(24552),	-- Gladiator's Dreadweave Robe
+							i(30200),	-- Gladiator's Felweave Raiment
+							i(28964),	-- Voidheart Robe
+						}),
+						i(34845),	-- Pit Lord's Satchel
+						i(28782),	-- Crystalheart Pulse-Staff
+						i(28774),	-- Glaive of the Pit
+						i(28783),	-- Eredar Wand of Obliteration
+						i(29458),	-- Aegis of the Vindicator
+						i(28781),	-- Karaborian Talisman
+						i(28775),	-- Thundering Greathelm
+						i(28777),	-- Cloak of the Pit Stalker
+						i(28776),	-- Liar's Tongue Gloves
+						i(28780),	-- Soul-Eater's Handwraps
+						i(28779),	-- Girdle of the Endless Pit
+						i(28778),	-- Terror Pit Girdle
+						i(28789),	-- Eye of Magtheridon
 					})),
 				},
 				["isRaid"] = true,
