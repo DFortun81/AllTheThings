6c1936a31c6159a9ba2cb63dc2ecba0b2d96662f
ada <slumber@love.com>
2018-05-03 15:29:19 -0500

    - added blues to nef's loot table (will add more as i run into them)

--- contrib/Parser/DATAS/1 - Dungeons Raids/1 - Classic/1 - Raids/2.0 Blackwing Lair.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/1 - Classic/1 - Raids/2.0 Blackwing Lair.lua
@@ -272,6 +272,9 @@ _.Instances = { tier(1, {	-- Classic
 					i(19376),	-- Archimtiros' Ring of Reckoning
 					i(19382),	-- Pure Elementium Band
 					i(19379),	-- Neltharion's Tear
+				 -- BLUES NOT IN DUNGEON JOURNAL/WOWHEAD
+					i(13114),	-- Serenity Belt
+					i(13146),	-- Shell Launcher Shotgun
 				},
 				["creatureID"] = 11583,
 			})
