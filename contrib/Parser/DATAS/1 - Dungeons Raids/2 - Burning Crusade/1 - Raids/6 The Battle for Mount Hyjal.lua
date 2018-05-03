59c1cc1b71a7b7aa42e939337557c68e4c5f0850
ada <slumber@love.com>
2018-05-03 03:31:34 -0500

    - finished accessorizing/reorganizing/commenting hyjal, working on black temple

--- contrib/Parser/DATAS/1 - Dungeons Raids/2 - Burning Crusade/1 - Raids/6 The Battle for Mount Hyjal.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/2 - Burning Crusade/1 - Raids/6 The Battle for Mount Hyjal.lua
@@ -8,7 +8,7 @@ _.Instances =
 		["groups"] = {
 			inst(750, { 	-- The Battle for Mount Hyjal
 				["groups"] = {
-					n(-2, {		-- Vendors
+					n(-2, {	-- Vendors
 						n(23381, {	-- Tydormu
 							-- Tier Gloves
 							i(31092, {	-- Gloves of the Forgotten Conqueror
@@ -75,64 +75,64 @@ _.Instances =
 								i(30979),	-- Onslaught Shoulderblades
 								i(30980),	-- Onslaught Shoulderguards
 							}),
-							i(31102, {	-- 
-								i(31047),	-- 
-								i(31049),	-- 
-								i(31048),	-- 
-								i(31030),	-- 
-								i(31059),	-- 
+							i(31102, {	-- Pauldrons of the Forgotten Vanquisher
+								i(31048),	-- Thunderheart Pauldrons
+								i(31049),	-- Thunderheart Shoulderpads
+								i(31047),	-- Thunderheart Spaulders
+								i(31059),	-- Mantle of the Tempest
+								i(31030),	-- Slayer's Shoulderpads
 							}),
 							-- The Illidari Council Tokens
-							i(31098, {	-- 
-								i(30995),	-- 
-								i(30994),	-- 
-								i(30993),	-- 
-								i(31053),	-- 
-								i(31067),	-- 
-								i(31068),	-- 
+							i(31098, {	-- Leggings of the Forgotten Conqueror
+								i(30993),	-- Lightbringer Greaves
+								i(30994),	-- Lightbringer Leggings
+								i(30995),	-- Lightbringer Legguards
+								i(31068),	-- Breeches of Absolution
+								i(31067),	-- Leggings of Absolution
+								i(31053),	-- Leggings of the Malefic
 							}),
-							i(31100, {	-- 
-								i(31021),	-- 
-								i(31020),	-- 
-								i(31019),	-- 
-								i(30978),	-- 
-								i(30977),	-- 
-								i(31005),	-- 
+							i(31100, {	-- Leggings of the Forgotten Protector
+								i(31005),	-- Gronnstalker's Leggings
+								i(31019),	-- Skyshatter Leggings
+								i(31020),	-- Skyshatter Legguards
+								i(31021),	-- Skyshatter Pants
+								i(30977),	-- Onslaught Greaves
+								i(30978),	-- Onslaught Legguards
 							}),
-							i(31099, {	-- 
-								i(31046),	-- 
-								i(31045),	-- 
-								i(31044),	-- 
-								i(31029),	-- 
-								i(31058),	-- 
+							i(31099, {	-- Leggings of the Forgotten Vanquisher
+								i(31044),	-- Thunderheart Leggings
+								i(31045),	-- Thunderheart Legguards
+								i(31046),	-- Thunderheart Pants
+								i(31058),	-- Leggings of the Tempest
+								i(31029),	-- Slayer's Legguards
 							}),
 							-- Illidan Stormrage Tokens
-							i(31089, {	-- 
-								i(31066),	-- 
-								i(31065),	-- 
-								i(31052),	-- 
-								i(30992),	-- 
-								i(30991),	-- 
-								i(30990),	-- 
+							i(31089, {	-- Chestguard of the Forgotten Conqueror
+								i(30990),	-- Lightbringer Breastplate
+								i(30991),	-- Lightbringer Chestguard
+								i(30992),	-- Lightbringer Chestpiece
+								i(31065),	-- Shroud of Absolution
+								i(31066),	-- Vestments of Absolution
+								i(31052),	-- Robe of the Malefic
 							}),
-							i(31091, {	-- 
-								i(31018),	-- 
-								i(31016),	-- 
-								i(31017),	-- 
-								i(30976),	-- 
-								i(30975),	-- 
-								i(31004),	-- 
+							i(31091, {	-- Chestguard of the Forgotten Protector
+								i(31004),	-- Gronnstalker's Chestguard
+								i(31017),	-- Skyshatter Breastplate
+								i(31016),	-- Skyshatter Chestguard
+								i(31018),	-- Skyshatter Tunic
+								i(30975),	-- Onslaught Breastplate
+								i(30976),	-- Onslaught Chestguard
 							}),
-							i(31090, {	-- 
-								i(31043),	-- 
-								i(31041),	-- 
-								i(31042),	-- 
-								i(31028),	-- 
-								i(31057),	-- 
+							i(31090, {	-- Chestguard of the Forgotten Vanquisher
+								i(31042),	-- Thunderheart Chestguard
+								i(31041),	-- Thunderheart Tunic
+								i(31043),	-- Thunderheart Vest
+								i(31057),	-- Robes of the Tempest
+								i(31028),	-- Slayer's Chestguard
 							}),
 						})
 					}),
-					n(0, {		-- Zone Drop
+					n( 0, {	-- Zone Drop
 						i(32590),	-- Nethervoid Cloak
 						i(34010),	-- Pepe's Shroud of Pacification
 						i(32609),	-- Boots of the Divine Light
@@ -140,184 +140,183 @@ _.Instances =
 						i(34009),	-- Hammer of Judgement 
 						i(32946),	-- Claw of Molten Fury
 						i(32945),	-- Fist of Molten Fury
-						
 						-- Recipes
-						--i(32738, { i(32571), }),	-- Dawnsteel Bracers (BT ONLY)
-						i(32739, { i(32573), }),	-- Dawnsteel Shoulders (Hyjal ONLY)
-						i(32736, { i(32568), }),	-- Swiftsteel Bracers (Hyjal ONLY)
-						--i(32737, { i(32570), }),	-- Swiftsteel Shoulders (BT ONLY)
-						i(32748, { i(32574), }),	-- Bindings of Lightning Reflexes (Hyjal ONLY)
-						--i(32744, { i(32582), }),	-- Bracers of Renewed Life (BT ONLY)
-						--i(32750, { i(32577), }),	-- Living Earth Bindings (BT ONLY)
-						i(32751, { i(32579), }),	-- Living Earth Shoulders (Hyjal ONLY)
-						--i(32749, { i(32575), }),	-- Shoulders of Lightning Reflexes (BT ONLY)
-						i(32745, { i(32583), }),	-- Shoulderpads of Renewed Life (Hyjal ONLY)
-						i(32746, { i(32580), }),	-- Swiftstrike Bracers (Hyjal ONLY)
-						--i(32747, { i(32581), }),	-- Swiftstrike Shoulders (BT ONLY)
-						--i(32754, { i(32586), }),	-- Bracers of Nimble Thought (BT ONLY)
-						i(32755, { i(32587), }),	-- Mantle of Nimble Thought (Hyjal ONLY)
-						--i(32753, { i(32585), }),	-- Swiftheal Mantle(BT ONLY)
-						i(32752, { i(32584), }),	-- Swiftheal Wraps (Hyjal ONLY)
+						i(32748),	-- Pattern: Bindings of Lightning Reflexes (Hyjal ONLY)
+					 -- i(32754),	-- Pattern: Bracers of Nimble Thought (BT ONLY)
+					 -- i(32744),	-- Pattern: Bracers of Renewed Life (BT ONLY)
+					 -- i(32750),	-- Pattern: Living Earth Bindings (BT ONLY)
+						i(32751),	-- Pattern: Living Earth Shoulders (Hyjal ONLY)
+						i(32755),	-- Pattern: Mantle of Nimble Thought (Hyjal ONLY)
+						i(32745),	-- Pattern: Shoulderpads of Renewed Life (Hyjal ONLY)
+					 -- i(32749),	-- Pattern: Shoulders of Lightning Reflexes (BT ONLY)
+					 -- i(32753),	-- Pattern: Swiftheal Mantle(BT ONLY)
+						i(32752),	-- Pattern: Swiftheal Wraps (Hyjal ONLY)
+						i(32746),	-- Pattern: Swiftstrike Bracers (Hyjal ONLY)
+					 -- i(32747),	-- Pattern: Swiftstrike Shoulders (BT ONLY)
+					 -- i(32738),	-- Plans: Dawnsteel Bracers (BT ONLY)
+						i(32739),	-- Plans: Dawnsteel Shoulders (Hyjal ONLY)
+						i(32736),	-- Plans: Swiftsteel Bracers (Hyjal ONLY)
+					 -- i(32737),	-- Plans: Swiftsteel Shoulders (BT ONLY)
 					}),
-					e(1577, {
+					e(1577, {	-- Rage Winterchill
 						["groups"] = {
-							i(30865),
-							i(30872),
-							i(30866),
-							i(30864),
-							i(30869),
-							i(30870),
-							i(30861),
-							i(30871),
-							i(30863),
-							i(30868),
-							i(30862),
-							i(30873),
+							i(30865),	-- Tracker's Blade
+							i(30872),	-- Chronicle of Dark Secrets
+							i(30866),	-- Blood-Stained Pauldrons
+							i(30862),	-- Blessed Adamantite Bracers
+							i(30871),	-- Bracers of Martyrdom
+							i(30864),	-- Bracers of the Pathfinder
+							i(30870),	-- Cuffs of Devastation
+							i(30863),	-- Deadly Cuffs
+							i(30861),	-- Furious Shackles
+							i(30869),	-- Howling Wind Bracers
+							i(30868),	-- Rejuvenating Bracers
+							i(30873),	-- Stillwater Boots
 						},
 						["creatureID"] = 17767,
 					}),
-					e(1578, {
+					e(1578, {	-- Anetheron
 						["groups"] = {
-							i(30883),
-							i(30874),
-							i(30881),
-							i(30882),
-							i(30878),
-							i(30884),
-							i(30887),
-							i(30879),
-							i(30888),
-							i(30885),
-							i(30880),
-							i(30886),
-							i(122111),
+							i(122111),	-- Smelly Gravestone (PET!)
+							i( 30883),	-- Pillar of Ferocity
+							i( 30881),	-- Blade of Infamy
+							i( 30874),	-- The Unbreakable Will
+							i( 30882),	-- Bastion of Light
+							i( 30878),	-- Glimmering Steel Mantle
+							i( 30884),	-- Hatefury Mantle
+							i( 30887),	-- Golden Links of Restoration
+							i( 30888),	-- Anetheron's Noose
+							i( 30879),	-- Don Alejandro's Money Belt
+							i( 30885),	-- Archbishop's Slippers
+							i( 30886),	-- Enchanted Leather Sandals
+							i( 30880),	-- Quickstrider Moccasins
 						},
 						["creatureID"] = 17808,
 					}),
-					e(1579, {
+					e(1579, {	-- Kaz'rogal
 						["groups"] = {
-							i(30918),
-							i(30889),
-							i(30892),
-							i(30917),
-							i(30915),
-							i(30895),
-							i(30919),
-							i(30914),
-							i(30916),
-							i(30893),
-							i(30894),
-							i(30891),
+							i(30918),	-- Hammer of Atonement
+							i(30889),	-- Kaz'rogal's Hardened Heart
+							i(30892),	-- Beast-Tamer's Shoulders
+							i(30917),	-- Razorfury Mantle
+							i(30895),	-- Angelista's Sash
+							i(30915),	-- Belt of Seething Fury
+							i(30914),	-- Belt of the Crescent Moon
+							i(30919),	-- Valestalker Girdle
+							i(30916),	-- Leggings of Channeled Elements
+							i(30893),	-- Sun-Touched Chain Leggings
+							i(30891),	-- Black Featherlight Boots
+							i(30894),	-- Blue Suede Shoes
 						},
 						["creatureID"] = 17888,
 					}),
-					e(1580, {
+					e(1580, {	-- Azgalor
 						["groups"] = {
-							i(30901),
-							i(30899),
-							i(30896),
-							i(30897),
-							i(30898),
-							i(30900),
-							i(31092, {
-								i(32040),
-								i(32034),
-								i(32021),
-								i(32015),
-								i(31993),
-								i(31981),
-								i(31973),
-								i(30985),
-								i(30983),
-								i(30982),
-								i(31061),
-								i(31050),
-								i(31060)
+							i( 31092, {	-- Gloves of the Forgotten Conqueror
+								i(32040),	-- 
+								i(32034),	-- 
+								i(32021),	-- 
+								i(32015),	-- 
+								i(31993),	-- 
+								i(31981),	-- 
+								i(31973),	-- 
+								i(30985),	-- 
+								i(30983),	-- 
+								i(30982),	-- 
+								i(31061),	-- 
+								i(31050),	-- 
+								i(31060),	-- 
 							}),
-							i(31094, {
-								i(31011),
-								i(31007),
-								i(31008),
-								i(30970),
-								i(30969),
-								i(32030),
-								i(30487),
-								i(32010),
-								i(32005),
-								i(31961),
-								i(31001)
+							i( 31094, {	-- Gloves of the Forgotten Protector
+								i(31011),	-- 
+								i(31007),	-- 
+								i(31008),	-- 
+								i(30970),	-- 
+								i(30969),	-- 
+								i(32030),	-- 
+								i(30487),	-- 
+								i(32010),	-- 
+								i(32005),	-- 
+								i(31961),	-- 
+								i(31001),	-- 
 							}),
-							i(31093, {
-								i(31035),
-								i(31032),
-								i(31034),
-								i(31026),
-								i(32056),
-								i(32049),
-								i(31998),
-								i(31987),
-								i(31967),
-								i(31055)
+							i( 31093, {	-- Gloves of the Forgotten Vanquisher
+								i(31035),	-- 
+								i(31032),	-- 
+								i(31034),	-- 
+								i(31026),	-- 
+								i(32056),	-- 
+								i(32049),	-- 
+								i(31998),	-- 
+								i(31987),	-- 
+								i(31967),	-- 
+								i(31055),	-- 
 							}),
-							i(122105),
+							i(122105),	-- Grotesque Statue (PET!)
+							i( 30901),	-- Boundless Agony
+							i( 30899),	-- Don Rodrigo's Poncho
+							i( 30896),	-- Glory of the Defender
+							i( 30897),	-- Girdle of Hope
+							i( 30900),	-- Bow-Stitched Leggings
+							i( 30898),	-- Shady Dealer's Pantaloons
 						},
 						["creatureID"] = 17842,
 					}),
-					e(1581, {
+					e(1581, {	-- Archimonde
 						["groups"] = {
-							i(30902),
-							i(30908),
-							i(30910),
-							i(30906),
-							i(30909),
-							i(30911),
-							i(30907),
-							i(30913),
-							i(30905),
-							i(30903),
-							i(30912),
-							i(30904),
-							i(31097, {
-								i(32041),
-								i(32035),
-								i(32022),
-								i(32016),
-								i(31997),
-								i(31980),
-								i(31974),
-								i(30989),
-								i(30988),
-								i(30987),
-								i(31051),
-								i(31064),
-								i(31063)
+							i( 31097, {	-- Helm of the Forgotten Conqueror
+								i(30987),	-- Lightbringer Faceguard
+								i(30988),	-- Lightbringer Greathelm
+								i(30989),	-- Lightbringer War-Helm
+								i(31997),	-- Merciless Gladiator's Lamellar Helm
+								i(32022),	-- Merciless Gladiator's Ornamented Headcover
+								i(32041),	-- Merciless Gladiator's Scaled Helm
+								i(31063),	-- Cowl of Absolution
+								i(31064),	-- Hood of Absolution
+								i(32016),	-- Merciless Gladiator's Mooncloth Hood
+								i(32035),	-- Merciless Gladiator's Satin Hood
+								i(31051),	-- Hood of the Malefic
+								i(31974),	-- Merciless Gladiator's Dreadweave Hood
+								i(31980),	-- Merciless Gladiator's Felweave Cowl
 							}),
-							i(31095, {
-								i(31012),
-								i(31014),
-								i(31015),
-								i(30974),
-								i(30972),
-								i(32031),
-								i(30488),
-								i(32011),
-								i(32006),
-								i(31962),
-								i(31003)
+							i( 31095, {	-- Helm of the Forgotten Protector
+								i(31003),	-- Gronnstalker's Helmet
+								i(31962),	-- Merciless Gladiator's Chain Helm
+								i(32006),	-- Merciless Gladiator's Linked Helm
+								i(32011),	-- Merciless Gladiator's Mail Helm
+								i(32031),	-- Merciless Gladiator's Ringmail Helm
+								i(31015),	-- Skyshatter Cover
+								i(31014),	-- Skyshatter Headguard
+								i(31012),	-- Skyshatter Helmet
+								i(30488),	-- Merciless Gladiator's Plate Helm
+								i(30972),	-- Onslaught Battle-Helm
+								i(30974),	-- Onslaught Greathelm
 							}),
-							i(31096, {
-								i(31037),
-								i(31040),
-								i(31039),
-								i(31027),
-								i(32057),
-								i(32048),
-								i(31999),
-								i(31988),
-								i(31968),
-								i(31056)
+							i( 31096, {	-- Helm of the Forgotten Vanquisher
+								i(31968),	-- Merciless Gladiator's Dragonhide Helm
+								i(31988),	-- Merciless Gladiator's Kodohide Helm
+								i(32057),	-- Merciless Gladiator's Wyrmhide Helm
+								i(31039),	-- Thunderheart Cover
+								i(31040),	-- Thunderheart Headguard
+								i(31037),	-- Thunderheart Helmet
+								i(31056),	-- Cowl of the Tempest
+								i(32048),	-- Merciless Gladiator's Silk Cowl
+								i(31999),	-- Merciless Gladiator's Leather Helm
+								i(31027),	-- Slayer's Helm
 							}),
-							i(122112),
+							i(122112),	-- Hyjal Wisp (PET!)
+							i( 30908),	-- Apostle of Argus
+							i( 30902),	-- Cataclysm's Edge
+							i( 30910),	-- Tempest of Chaos
+							i( 30906),	-- Bristleblitz Striker
+							i( 30909),	-- Antonidas' Aegis of Rapt Concentration
+							i( 30911),	-- Scepter of Purification
+							i( 30907),	-- Mail of Fevered Pursuit
+							i( 30905),	-- Midnight Chestguard
+							i( 30913),	-- Robes of Rhonin
+							i( 30904),	-- Savior's Grasp
+							i( 30912),	-- Leggings of Eternity
+							i( 30903),	-- Legguards of Endless Rage
 						},
 						["creatureID"] = 17968,
 					})
