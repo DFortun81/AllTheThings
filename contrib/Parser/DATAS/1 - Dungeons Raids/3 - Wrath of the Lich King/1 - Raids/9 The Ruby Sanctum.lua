79f8fdc85165d5beba199d2353f29d6814fc7295
ada <slumber@love.com>
2018-05-16 05:19:18 -0500

    - reorganized ruby sanctum and added accessories

--- contrib/Parser/DATAS/1 - Dungeons Raids/3 - Wrath of the Lich King/1 - Raids/9 The Ruby Sanctum.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/3 - Wrath of the Lich King/1 - Raids/9 The Ruby Sanctum.lua
@@ -10,64 +10,84 @@ _.Instances =
 				["groups"] = {
 					desc(n(39751), "Must be killed before you can fight General Zarithrian or talk to the quest giver."),	-- Baltharus the Warborn
 					{
-						["questID"] = 26034,	-- The Twilight Destroyer
-						["qg"] = 40429,			-- Quest Giver: Krasus <Consort of the Queen>
-						["sourceQuest"] = 26013,	-- Source Quest: Assault on the Sanctum
+						["questID"] = 26034,		-- The Twilight Destroyer
+						["qg"] = 40429,				-- Krasus <Consort of the Queen>
+						["sourceQuest"] = 26013,	-- Assault on the Sanctum
 					},
 					desc(n(39747), "Must be killed before you can fight General Zarithrian."),	-- Saviana Ragefire
 					desc(n(39746), "Must be killed before you can fight Halion."),	-- General Zarithrian
-					d(3, {
-						cr(39863, e(1652, {
-							i(53113),
-							i(53115),
-							i(53114),
-							i(53112),
-							i(53117),
-							i(53121),
-							i(53118),
-							i(53111),
-							i(53119)
-						}))
+					d(3, {	-- 10-Player
+						cr(39863, e(1652, {	-- Halion
+							i(53103),	-- Baltharus' Gift
+							i(53113),	-- Twilight Scale Shoulders
+							i(53115),	-- Abduction's Cover
+							i(53114),	-- Gloaming Sark
+							i(53112),	-- Bracers of the Heir
+							i(53117),	-- Changeling Gloves
+							i(53118),	-- Misbegotten Belt
+							i(53121),	-- Surrogate Belt
+							i(53119),	-- Boots of Divided Being
+							i(53111),	-- Scion's Treads
+							i(53116),	-- Saviana's Tribute
+							i(53110),	-- Zarithrian's Offering
+						})),
 					}),
-					d(5, {
-						cr(39863, e(1652, {
-							i(54566),
-							i(54556),
-							i(54561),
-							i(54559),
-							i(54560),
-							i(54565),
-							i(54562),
-							i(54558),
-							i(54564)
-						}))
+					d(5, {	-- 10-Player (Heroic)
+						cr(39863, e(1652, {	-- Halion
+							i(54557),	-- Baltharus' Gift
+							i(54566),	-- Twilight Scale Shoulders
+							i(54556),	-- Abduction's Cover
+							i(54561),	-- Gloaming Sark
+							i(54559),	-- Bracers of the Heir
+							i(54560),	-- Changeling Gloves
+							i(54562),	-- Misbegotten Belt
+							i(54565),	-- Surrogate Belt
+							i(54558),	-- Boots of Divided Being
+							i(54564),	-- Scion's Treads
+							i(54563),	-- Saviana's Tribute
+							i(54567),	-- Zarithrian's Offering
+						})),
 					}),
-					d(4, {
-						cr(39863, e(1652, {
-							i(53489),
-							i(53486),
-							i(53134),
-							i(53126),
-							i(53488),
-							i(53125),
-							i(53129),
-							i(53487),
-							i(53127)
-						}))
+					d(4, {	-- 25-Player
+						cr(39863, e(1652, {	-- Halion
+							i(53132),	-- Penumbra Pendant
+							i(53489),	-- Cloak of Burning Dusk
+							i(53486),	-- Bracers of Fiery Night
+							i(53134),	-- Phaseshifter's Bracers
+							i(53126),	-- Umbrage Armbands
+							i(53488),	-- Split Shape Belt
+							i(53125),	-- Apocalypse's Advance
+							i(53487),	-- Foreshadow Steps
+							i(53127),	-- Returning Footfalls
+							i(53129),	-- Treads of Impending Resurrection
+							i(53490),	-- Ring of Phased Regeneration
+							i(53133),	-- Signet of Twilight
+							i(54572),	-- Charred Twilight Scale
+							i(54573),	-- Glowing Twilight Scale
+							i(54571),	-- Petrified Twilight Scale
+							i(54569),	-- Sharpened Twilight Scale
+						})),
 					}),
-					d(6, {
-						cr(39863, e(1652, {
-							i(54583),
-							i(54584),
-							i(54582),
-							i(54580),
-							i(54587),
-							i(54577),
-							i(54579),
-							i(54586),
-							i(54578)
-						}))
-					})
+					d(6, {	-- 25-Player (Heroic)
+						cr(39863, e(1652, {	-- Halion
+							i(54581),	-- Penumbra Pendant
+							i(54583),	-- Cloak of Burning Dusk
+							i(54582),	-- Bracers of Fiery Night
+							i(54584),	-- Phaseshifter's Bracers
+							i(54580),	-- Umbrage Armbands
+							i(54587),	-- Split Shape Belt
+							i(54578),	-- Apocalypse's Advance
+							i(54586),	-- Foreshadow Steps
+							i(54577),	-- Returning Footfalls
+							i(54579),	-- Treads of Impending Resurrection
+							i(54585),	-- Ring of Phased Regeneration
+							i(54576),	-- Signet of Twilight
+							i(54588),	-- Charred Twilight Scale
+							i(54589),	-- Glowing Twilight Scale
+							i(54591),	-- Petrified Twilight Scale
+							i(54590),	-- Sharpened Twilight Scale
+						})),
+					}),
 				},
 				["isRaid"] = true,
 				["sharedLockout"] = 1,
