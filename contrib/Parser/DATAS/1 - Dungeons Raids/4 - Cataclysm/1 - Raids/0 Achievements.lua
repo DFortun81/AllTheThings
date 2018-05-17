d5e5ccec2e93d3c007d9547e407f3f56b2883dfa
Laycel\Iyanden <carrie.a.newman@gmail.com>
2018-05-16 19:36:15 -0500

    - Adding achievements for Cataclysm raids (WIP)

--- contrib/Parser/DATAS/1 - Dungeons Raids/4 - Cataclysm/1 - Raids/0 Achievements.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/4 - Cataclysm/1 - Raids/0 Achievements.lua
@@ -3,6 +3,33 @@
 -----------------------------------------------------
 
 _.Instances = { tier(4, {	-- Cataclysm
+	ach(4853, {	-- Glory of the Cataclysm Raider
+		ach(5094),	-- Heroic: Magmaw
+		ach(5108),	-- Heroic: Maloriak
+		ach(5115),	-- Heroic: Chimaeron
+		ach(5118),	-- Heroic: Halfus Wyrmbreaker
+		ach(5119),	-- Heroic: Ascendant Council
+		ach(5122),	-- Heroic: Conclave of Wind
+		ach(5306),	-- Parasite Evening
+		ach(5308),	-- Silence is Golden
+		ach(5310),	-- Aberrant Behavior
+		ach(5300),	-- The Only Escape
+		ach(5311),	-- Elementary
+		ach(5304),	-- Stay Chill
+		ach(5107),	-- Heroic: Omnotron Defense System
+		ach(5109),	-- Heroic: Atramedes
+		ach(5116),	-- Heroic: Nefarian
+		ach(5117),	-- Heroic: Valiona and Theralion
+		ach(5120),	-- Heroic: Cho'gall
+		ach(5123),	-- Heroic: Al'Akir
+		ach(5307),	-- Achieve-a-tron
+		ach(5309),	-- Full of Sound and Fury
+		ach(4849),	-- Keeping it in the Family
+		ach(4852),	-- Double Dragon
+		ach(5312),	-- The Abyss Will Gaze Back Into You
+		ach(5305),	-- Four Play
+		i(62901),	-- Reins of the Drake of the East Wind
+		}),
 	ach(12079, { -- Raiding with Leashes V: Cuteaclysm
 		i(152963), -- Amalgam of Destruction
 	}),
