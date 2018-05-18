05890791e3ae2039b002efde4f69c77439dbf5f9
Lucetia-Detheroc <aceso2896@gmail.com>
2018-05-17 22:03:54 -0700

    - Added at least 1 achievement to each category to give an idea of how the base category goes.  Legacy is the big section we want to make sure we mark unobtainable since they can't be gotten anymore.

--- contrib/Parser/DATAS/3.2 - Achievements/Achievements.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/Achievements.lua
@@ -5,97 +5,312 @@
 _.Achievements = 
 {
 	n(-9955, { -- General
-		ach(10671, { -- Level 110
-			ach(6), -- Level 10
-			ach(7), -- Level 20
-			ach(8), -- Level 30
-			ach(9), -- Level 40
-			ach(10), -- Level 50
-			ach(11), -- Level 60
-			ach(12), -- Level 70
-			ach(13), -- Level 80
-			ach(4826), -- Level 85
-			ach(6193), -- Level 90
-			ach(9060), -- Level 100
-		}),
-		ach(5753, {	-- Cataclysmically Delicious
-			crit(1), -- 
-			crit(2), -- 
-			crit(3), -- 
-			crit(4), -- 
-			crit(5), -- 
-			crit(6), -- 
-			crit(7), -- 
-			crit(8), -- 
-			crit(9), -- 
-			crit(10), -- 
-			crit(11), -- 
-			crit(12), -- 
-			crit(13), -- 
-			crit(14), -- 
-			crit(15), -- 
-			crit(16), -- 
-			crit(17), -- 
-			crit(18), -- 
-			crit(19), -- 
-			crit(20), -- 
-			crit(21), -- 
-			crit(22), -- 
-			crit(23), -- 
-			crit(24), -- 
-			crit(25), -- 
-			crit(26), -- 
-			crit(27), -- 
-			crit(28), -- 
-			crit(29), -- 
-			crit(30), -- 
-			crit(31), -- 
-			crit(32), -- 
-			crit(33), -- 
-			crit(34), -- 
-			crit(35), -- 
-			crit(36), -- 
-			crit(37), -- 
-			crit(38), -- 
-			crit(39), -- 
-			crit(40), -- 
-			crit(41), -- 
-			crit(42), -- 
-			crit(43), -- 
-			crit(44), -- 
-			crit(45), -- 
-			crit(46), -- 
-			crit(47), -- 
-			crit(48), -- 
-		}),	
-		
+		["groups"] = {
+			ach(10671, { -- Level 110
+				ach(6), -- Level 10
+				ach(7), -- Level 20
+				ach(8), -- Level 30
+				ach(9), -- Level 40
+				ach(10), -- Level 50
+				ach(11), -- Level 60
+				ach(12), -- Level 70
+				ach(13), -- Level 80
+				ach(4826), -- Level 85
+				ach(6193), -- Level 90
+				ach(9060), -- Level 100
+			}),
+			ach(5753, {	-- Cataclysmically Delicious
+				crit(1), -- 
+				crit(2), -- 
+				crit(3), -- 
+				crit(4), -- 
+				crit(5), -- 
+				crit(6), -- 
+				crit(7), -- 
+				crit(8), -- 
+				crit(9), -- 
+				crit(10), -- 
+				crit(11), -- 
+				crit(12), -- 
+				crit(13), -- 
+				crit(14), -- 
+				crit(15), -- 
+				crit(16), -- 
+				crit(17), -- 
+				crit(18), -- 
+				crit(19), -- 
+				crit(20), -- 
+				crit(21), -- 
+				crit(22), -- 
+				crit(23), -- 
+				crit(24), -- 
+				crit(25), -- 
+				crit(26), -- 
+				crit(27), -- 
+				crit(28), -- 
+				crit(29), -- 
+				crit(30), -- 
+				crit(31), -- 
+				crit(32), -- 
+				crit(33), -- 
+				crit(34), -- 
+				crit(35), -- 
+				crit(36), -- 
+				crit(37), -- 
+				crit(38), -- 
+				crit(39), -- 
+				crit(40), -- 
+				crit(41), -- 
+				crit(42), -- 
+				crit(43), -- 
+				crit(44), -- 
+				crit(45), -- 
+				crit(46), -- 
+				crit(47), -- 
+				crit(48), -- 
+			}),	
+		},
 	}),
 	n(-9956, { -- Quests
+		["groups"] = {
+			ach(31), -- A Simple Re-Quest
+		},
 	}),
 	n(-9957, { -- Exploration
+		["groups"] = {
+			ach(9924, { -- Field Photographer
+				style(0, title(307)), -- Field Photographer
+				crit(1), -- 
+				crit(2), -- 
+				crit(3), -- 
+				crit(4), -- 
+				crit(5), -- 
+				crit(6), -- 
+				crit(7), -- 
+				crit(8), -- 
+				crit(9), -- 
+				crit(10), -- 
+				crit(11), -- 
+				crit(12), -- 
+				crit(13), -- 
+				crit(14), -- 
+				crit(15), -- 
+				crit(16), -- 
+				crit(17), -- 
+				crit(18), -- 
+				crit(19), -- 
+				crit(20), -- 
+				crit(21), -- 
+				crit(22), -- 
+				crit(23), -- 
+				crit(24), -- 
+				crit(25), -- 
+				crit(26), -- 
+				crit(27), -- 
+				crit(28), -- 
+				crit(29), -- 
+				crit(30), -- 
+				crit(31), -- 
+				crit(32), -- 
+				crit(33), -- 
+				crit(34), -- 
+				crit(35), -- 
+				crit(36), -- 
+				crit(37), -- 
+				crit(38), -- 
+				crit(39), -- 
+				crit(40), -- 
+				crit(41), -- 
+				crit(42), -- 
+				crit(43), -- 
+				crit(44), -- 
+			}),
+		},
 	}),
 	n(-9958, { -- Player vs Player
+		["groups"] = {
+			ach(10561), -- Honorable Medallion
+		},
 	}),
 	n(-9959, { -- Dungeons & Raids
+		["groups"] = {
+			ach(4478, { -- Looking For Multitudes
+				i(49912), -- Perky Pug
+				ach(4476), -- Looking For More
+				ach(4477, { -- Looking For Many
+					style(1, title(137)), -- the Patient <title>
+				}),
+			}),
+		},
 	}),
 	n(-9960, { -- Professions
+		["groups"] = {
+			ach(10587), -- Hot Swapper
+			ach(9071), -- Inspector Gagetzan
+			ach(10588), -- The Shortest Distance
+		},
 	}),
 	n(-9961, { -- Reputation
+		["groups"] = {
+			ach(5794), -- Time Flies When You're Having Fun
+		},
 	}),
 	n(-9962, { -- World Events
+		["groups"] = {
+			ach(9426), -- To The Afterlife
+		},
 	}),
 	n(-9963, { -- Pet Battles
+		["groups"] = {
+			ach(9712), -- Pet Charmer
+		},
 	}),
 	n(-9964, { -- Collections
+		["groups"] = {
+			ach(8728, {	-- Going To Need A Bigger Bag
+				crit(1), -- 
+				crit(2), -- 
+				crit(3), -- 
+				crit(4), -- 
+				crit(5), -- 
+				crit(6), -- 
+				crit(7), -- 
+				crit(8), -- 
+				crit(9), -- 
+				crit(10), -- 
+				crit(11), -- 
+				crit(12), -- 
+				crit(13), -- 
+				crit(14), -- 
+				crit(15), -- 
+				crit(16), -- 
+				crit(17), -- 
+				crit(18), -- 
+				crit(19), -- 
+				crit(20), -- 
+				crit(21), -- 
+				crit(22), -- 
+				crit(23), -- 
+				crit(24), -- 
+				crit(25), -- 
+				crit(26), -- 
+				crit(27), -- 
+				crit(28), -- 
+				crit(29), -- 
+				crit(30), -- 
+				crit(31), -- 
+				crit(32), -- 
+				crit(33), -- 
+				crit(34), -- 
+				crit(35), -- 
+				crit(36), -- 
+				crit(37), -- 
+				crit(38), -- 
+				crit(39), -- 
+				crit(40), -- 
+				crit(41), -- 
+				crit(42), -- 
+				crit(43), -- 
+				crit(44), -- 
+				crit(45), -- 
+				crit(46), -- 
+				crit(47), -- 
+				crit(48), -- 
+				crit(49), -- 
+				crit(50), -- 
+				crit(51), -- 
+				crit(52), -- 
+				crit(53), -- 
+				crit(54), -- 
+				crit(55), -- 
+				crit(56), -- 
+			}),	
+			ach(9909, { -- Heirloom Hoarder
+				i(120968), -- Chauffeured Chopper
+				i(122703), -- Chauffeured Chopper
+				ach(9911), -- Where's the Mailbox?
+				ach(9906), -- Alt-ernative Lifestyle
+				ach(9908), -- Ready for Powerleveling
+			}),
+		},
 	}),
 	n(-9965, { -- Class Hall
+		["groups"] = {
+			ach(11136, { -- An Epic Campaign
+				ach(10994), -- A Glorious Campaign
+				ach(11135), -- A Heroic Campaign
+			}),
+			ach(11223),  -- Legendary Research
+		},
 	}),
 	n(-9966, { -- Draenor Garrison
+		["groups"] = {
+			ach(9132), -- Garrison Buddies
+			ach(8933), -- Staying Regular
+		},
 	}),
 	n(-9967, { -- Scenarios
+		["groups"] = {
+			ach(6943), -- Complete 100 Scenarios
+		},
 	}),
-	n(-9968, { -- Legacy
+	n(-9968, { -- Legacy		NOTE: MOST WILL NEED THE un(2) TAG
+		["groups"] = {
+			un(2, ach(2358)), -- Charger
+			un(2, ach(16)), -- Did Somebody Order a Knuckle Sandwich?
+			un(2, ach(2716)), -- Dual Talent Specialization
+			un(2, ach(7315, { -- Eternally in the Vale
+				un(2, crit(1)), -- The Golden Gate
+				un(2, crit(2)), -- The Jade Army
+				un(2, crit(3)), -- Welcome Home
+				un(2, crit(4)), -- Remember to Close the Door
+				un(2, crit(5)), -- Weapons of the Thunder King
+			})),
+			un(2, ach(705)), -- Master of Arms
+			un(2, ach(2018, { -- Timear Foresees
+				un(2, crit(1)), -- Timear Foresees Centrifuge Constructs in your Future!
+				un(2, crit(2)), -- Timear Foresees Ymirjar Berserkers in your Future!
+				un(2, crit(3)), -- Timear Foresees Infinite Agents in your Future!
+				un(2, crit(4)), -- Timear Foresees Titanium Vanguards in your Future!
+			})),
+		},
 	}),
 	n(-9969, { -- Feats of Strength
+		["groups"] = {
+			ach(11137, { -- A Legendary Campaign
+				crit(1), -- Death Knight
+				crit(2), -- Demon Hunter
+				crit(3), -- Druid
+				crit(4), -- Hunter
+				crit(5), -- Mage
+				crit(6), -- Monk
+				crit(7), -- Paladin
+				crit(8), -- Priest
+				crit(9), -- Rogue
+				crit(10), -- Shaman
+				crit(11), -- Warlock
+				crit(12), -- Warrior
+			}),
+			ach(12245, { -- Allied Races: Highmountain Tauren
+				ach(10059), -- Ain't No Mountain High Enough
+				ach(12292), -- Highmountain Tribe
+				qh(48433), -- Together We Are the Horde!
+			}),
+			ach(12243, { -- Allied Races: Lightforged Draenei
+				ach(12076), -- Protector of the Argussian Reach
+				ach(12066), -- You Are Now Prepared!
+				qa(50071), -- For the Light!
+			}),
+			ach(12244, { -- Allied Races: Nightborne
+				ach(11340), -- Insurrection
+				ach(10778), -- The Nightfallen
+				qh(49614), -- The Nightborne
+			}),
+			ach(12242, { -- Allied Races: Void Elf
+				ach(12076), -- Protector of the Argussian Reach
+				ach(12066), -- You Are Now Prepared!
+				qa(48962), -- Telogrus Rift
+			}),
+		},
 	}),
 };
