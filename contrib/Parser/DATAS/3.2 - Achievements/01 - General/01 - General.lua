4aba7340342e6740209f06bd3ff9d2f044e31c96
Lucetia-Detheroc <aceso2896@gmail.com>
2018-05-18 12:25:43 -0700

    - General, Exploration and Quests hiearchy setup for achievements - Removed the main achievement file (parser can handle it from here in organizing things)

---
+++ contrib/Parser/DATAS/3.2 - Achievements/01 - General/01 - General.lua
@@ -0,0 +1,74 @@
+--------------------------------------------------
+--   G R O U P    F I N D E R    M O D U L E    --
+--------------------------------------------------
+
+_.Achievements = 
+{
+	n(-9955, { -- General
+		["groups"] = {
+			ach(10671, { -- Level 110
+				ach(6), -- Level 10
+				ach(7), -- Level 20
+				ach(8), -- Level 30
+				ach(9), -- Level 40
+				ach(10), -- Level 50
+				ach(11), -- Level 60
+				ach(12), -- Level 70
+				ach(13), -- Level 80
+				ach(4826), -- Level 85
+				ach(6193), -- Level 90
+				ach(9060), -- Level 100
+			}),
+			ach(5753, {	-- Cataclysmically Delicious
+				crit(1), -- Baked Rockfish
+				crit(2), -- Basilisk Liverdog
+				crit(3), -- Beer-Basted Crocolisk
+				crit(4), -- Blackbelly Sushi
+				crit(5), -- Blackened Surprise
+				crit(6), -- Broiled Mountain Trout
+				crit(7), -- Buttery Wheat Roll
+				crit(8), -- Chocolate Cookie
+				crit(9), -- Crocolisk Au Gratin
+				crit(10), -- Delicious Sagefish Tail
+				crit(11), -- Fish Fry
+				crit(12), -- Fortune Cookie
+				crit(13), -- Gilnean White
+				crit(14), -- Grilled Dragon
+				crit(15), -- Grilled Shark
+				crit(16), -- Hardtack
+				crit(17), -- Hearty Seafood Soup
+				crit(18), -- Highland Pomegranate
+				crit(19), -- Highland Sheep Cheese
+				crit(20), -- Lavascale Fillet
+				crit(21), -- Lavascale Minestrone
+				crit(22), -- Lightly Fried Lurker
+				crit(23), -- Lurker Lunch
+				crit(24), -- Massive Turkey Leg
+				crit(25), -- Mushroom Sauce Mudfish
+				crit(26), -- Oily Giblets
+				crit(27), -- Pickled Guppy
+				crit(28), -- Pine Nut Bread
+				crit(29), -- Roasted Beef
+				crit(30), -- Rock-Hard Biscuit
+				crit(31), -- Salted Eye
+				crit(32), -- Scalding Murglesnout
+				crit(33), -- Scarlet Polypore
+				crit(34), -- Seasoned Crab
+				crit(35), -- Severed Sagefish Head
+				crit(36), -- Simmered Squid
+				crit(37), -- Skewered Eel
+				crit(38), -- Sliced Raw Billfish
+				crit(39), -- Smoked String Cheese
+				crit(40), -- Sour Green Apple
+				crit(41), -- Stewed Rabbit
+				crit(42), -- Tasty Puffball
+				crit(43), -- Tender Baked Turtle
+				crit(44), -- Tropical Sunfruit
+				crit(45), -- Unidentifiable Meat Dish
+				crit(46), -- Vile Purple Fungus
+				crit(47), -- Violet Morel
+				crit(48), -- Whitecrest Gumbo
+			}),	
+		},
+	}),
+};
