38d3a661b6d69c1046d9a63299051ca2628b0991
Lucetia-Detheroc <aceso2896@gmail.com>
2018-05-18 15:33:08 -0700

    - Added Warsong Gulch achievements to the PVP -> Warsong area

--- contrib/Parser/DATAS/3.2 - Achievements/04 - Player vs Player/03 - Warsong Gulch.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/04 - Player vs Player/03 - Warsong Gulch.lua
@@ -8,6 +8,22 @@ _.Achievements =
 		["groups"] = {
 			n(-9984, { -- Warsong Gulch
 				["groups"] = {
+					ach(1172, { -- Master of Warsong Gulch	
+						ach(199), -- Capture the Flag
+						ach(872), -- Frenzied Defender
+						ach(204), -- Ironman
+						ach(1251), -- Not In My House
+						ach(200), -- Persistent Defender
+						ach(1502), -- Quick Cap
+						ach(207), -- Save the Day
+						ach(1252), -- Supreme Defender
+						ach(201), -- Warsong Expedience
+						ach(168), -- Warsong Gulch Perfection
+						ach(167), -- Warsong Gulch Veteran
+					}),	
+					ach(1259), -- Not So Fast	
+					ach(166), -- Warsong Gulch Victory	
+					ach(712), -- Warsong Outrider	
 				},
 			}),
 		},
