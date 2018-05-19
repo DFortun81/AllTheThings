01bbae05af5863f41d6e52aa2d8532164cb52c79
Laycel\Iyanden <carrie.a.newman@gmail.com>
2018-05-19 10:22:22 -0500

    - Added Strand of the Ancients achievements to Achivements -> PvP -> Strand of the Ancients

--- contrib/Parser/DATAS/3.2 - Achievements/04 - Player vs Player/08 - Strand of the Ancients.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/04 - Player vs Player/08 - Strand of the Ancients.lua
@@ -7,7 +7,76 @@ _.Achievements =
 	n(-9958, { -- Player vs Player
 		["groups"] = {
 			n(-9989, { -- Strand of the Ancients
+				["description"] = "WARNING! All Strand of the Ancients achievements will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH THEM BEFORE THEN!",
 				["groups"] = {
+					ach(1309, {	-- Strand of the Ancients Veteran
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+						["groups"] = {
+							ach(1308, {	-- Strand of the Ancients Victory
+								["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+							}),
+						},
+					}),
+					ach(1310, {	-- Storm the Beach
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					ach(1765, {	-- Steady Hands
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					a(ach(2200, {	-- Defense of the Ancients (Alliance)
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					})),
+					h(ach(1757, {	-- Defense of the Ancients (Horde)
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					})),
+					ach(1761, {	-- The Dapper Sapper
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					ach(2193, {	-- Explosives Expert
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					a(ach(1762, {	-- Not Even a Scratch (Alliance)
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					})),
+					h(ach(2192, {	-- Not Even a Scratch (Horde)
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					})),
+					ach(1763, {	-- Artillery Veteran
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					ach(2189, {	-- Artillery Expert
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					ach(1764, {	-- Drop It!
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					ach(2190, {	-- Drop It Now!
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					ach(1766, {	-- Ancient Protector
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					ach(2191, {	-- Ancient Courtyard Protector
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+					}),	
+					ach(2194, {	-- Master of Strand of the Ancients
+						["description"] = "WARNING! This achievement will be removed with the release of Battle For Azeroth. MAKE SURE TO FINISH IT BEFORE THEN!",
+						["groups"] = {
+							crit(1),	-- Strand of the Ancients Veteran
+							crit(2),	-- Storm the Beach
+							crit(3),	-- Steady Hands
+							crit(4),	-- The Dapper Sapper
+							crit(5),	-- Explosives Expert
+							crit(6),	-- Not Even a Scratch (Alliance);Defense of the Ancients (Horde)
+							crit(7),	-- Artillery Veteran
+							crit(8),	-- Artillery Expert
+							crit(9),	-- Drop It!
+							crit(10),	-- Drop It Now!
+							crit(11),	-- Ancient Protector
+							crit(12),	-- Ancient Courtyard Protector
+							crit(13),	-- Defense of the Ancients (Alliance);Defense of the Ancients (Horde)
+						},
+					}),
 				},
 			}),
 		},
