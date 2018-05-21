dcf4ffd61e1b8266139731b5a9eee52165d0494c
Laycel\Iyanden <carrie.a.newman@gmail.com>
2018-05-21 16:02:26 -0500

    - Added general profession achivements to Achievements -> Professions -> General

--- contrib/Parser/DATAS/3.2 - Achievements/06 - Professions/01 - General.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/06 - Professions/01 - General.lua
@@ -8,8 +8,140 @@ _.Achievements =
 		["groups"] = {
 			n(-9970, { -- General
 				["groups"] = {
+					ach(10583,	{	-- Legion Master of All (800)
+						crit(1),	-- Alchemy
+						crit(2),	-- Blacksmithing
+						crit(3),	-- Enchanting
+						crit(4),	-- Engineering
+						crit(5),	-- Herbalism
+						crit(6),	-- Inscription
+						crit(7),	-- Jewelcrafting
+						crit(8),	-- Leatherworking
+						crit(9),	-- Mining
+						crit(10),	-- Skinning
+						crit(11),	-- Tailoring
+						ach(9087, {	-- Grand Master of All (700)
+							crit(1),	-- Alchemy
+							crit(2),	-- Blacksmithing
+							crit(3),	-- Enchanting
+							crit(4),	-- Engineering
+							crit(5),	-- Herbalism
+							crit(6),	-- Inscription
+							crit(7),	-- Jewelcrafting
+							crit(8),	-- Leatherworking
+							crit(9),	-- Mining
+							crit(10),	-- Skinning
+							crit(11),	-- Tailoring
+						}),
+						ach(7379, {	-- Master of All (600)
+							crit(1),	-- Alchemy
+							crit(2),	-- Blacksmithing
+							crit(3),	-- Enchanting
+							crit(4),	-- Engineering
+							crit(5),	-- Herbalism
+							crit(6),	-- Inscription
+							crit(7),	-- Jewelcrafting
+							crit(8),	-- Leatherworking
+							crit(9),	-- Mining
+							crit(10),	-- Skinning
+							crit(11),	-- Tailoring
+						}),
+						ach(7378, {	-- Jack of All Trades (600)
+							crit(1),	-- Alchemy
+							crit(2),	-- Blacksmithing
+							crit(3),	-- Enchanting
+							crit(4),	-- Engineering
+							crit(5),	-- Herbalism
+							crit(6),	-- Inscription
+							crit(7),	-- Jewelcrafting
+							crit(8),	-- Leatherworking
+							crit(9),	-- Mining
+							crit(10),	-- Skinning
+							crit(11),	-- Tailoring
+						}),
+					}),
+					ach(10582, {	-- Professional Legion Master (800)
+						ach(9464, {	-- Draenor Master (700)
+							title(305),	--	Artisan
+						}),
+						ach(6830),	-- Professional Zen Master (600)
+						ach(4924),	-- Professional Illustrious Grand Master (525)
+						ach(734),	-- Professional Grand Master (450)
+						ach(733),	-- Professional Master (375)
+						ach(732),	-- Professional Artisan (300)
+						ach(731),	-- Professional Expert (225)
+						ach(116),	-- Professional Journeyman (150)
+					}),
+					ach(10581, {	-- Working the Isles (800)
+						ach(9507),	-- Working in Draenor (700)
+						ach(6835),	-- Working For a Living (600)
+						ach(4914),	-- Working Around the Clock (525)
+						ach(735),	-- Working Day and Night (450)
+					}),
+					ach(10580, {	-- Heroic Skills to Pay the Bills(800)
+						crit(1),	-- Legion Fisherman
+						crit(2),	-- Legion Medic
+						crit(3),	-- Legion Cook
+						crit(4),	-- Legion Archaeologist
+						ach(9506, {	-- Savage Skills to Pay the Bills (700)
+							crit(1),	-- Draenor Fisherman
+							crit(2),	-- Draenor Medic
+							crit(3),	-- Draenor Cook
+							crit(4),	-- Draenor Archaeologist
+						}),
+						ach(6836, {	-- Serious Skills to Pay the Bills (600)
+							crit(1),	-- Zen Master Fisherman
+							crit(2),	-- Zen Master Medic
+							crit(3),	-- Zen Master Cook
+							crit(4),	-- Zen Master Archaeologist
+						}),
+						ach(4915, {	-- More Skills to Pay the Bills (525)
+							crit(1),	-- Illustrious Grand Master Fisherman
+							crit(2),	-- Illustrious Grand Master Medic
+							crit(3),	-- Illustrious Grand Master Cook
+							crit(4),	-- Illustrious Grand Master Archaeologist
+						}),
+						ach(730, {	-- Skills to Pay the Bills (450)
+							crit(1),	-- Grand Master Fisherman
+							crit(2),	-- Grand Master First Aid
+							crit(3),	-- Grand Master Cook
+						}),
+					}),
+					ach(10761, {	-- Resourceful
+						crit(1),	-- Alchemy
+						crit(2),	-- Blacksmithing
+						crit(3),	-- Enchanting
+						crit(4),	-- Engineering
+						crit(5),	-- Herbalism
+						crit(6),	-- Inscription
+						crit(7),	-- Jewelcrafting
+						crit(8),	-- Leatherworking
+						crit(9),	-- Mining
+						crit(10),	-- Skinning
+						crit(11),	-- Tailoring
+					}),
+					ach(10586),	-- Mass Obliteration
+					ach(10585, {	-- Fel-Smelter
+						crit(1),	-- Firing Up the Forge
+					}),
+					ach(9454, {	-- Draenic Seed Collector
+						i(111997),	-- Herb Garden, Level 3
+					}),
+					ach(9453, {	-- Draenic Stone Collector
+						a(i(111996)),	-- Lunarfall Excavation, Level 3
+						h(i(116249)),	-- Frostwall Mines, Level 3
+					}),
 					ach(10587), -- Hot Swapper
-					ach(9071),	-- Inspector Gagetzan
+					ach(9071, {	-- Inspector Gagetzan
+						crit(1),	-- Decoded Message 001-003
+						crit(2),	-- Decoded Message 001-014
+						crit(3),	-- Decoded Message 001-107
+						crit(4),	-- Decoded Message 001-111
+						crit(5),	-- Decoded Message 001-119
+						crit(6),	-- Decoded Message 001-150
+						crit(7),	-- Decoded Message 001-159
+						crit(8),	-- Decoded Message 001-168
+					}),
 					ach(10588), -- The Shortest Distance
 				},
 			}),
