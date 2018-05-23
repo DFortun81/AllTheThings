e76a796da4096b4943d2e1cb6740af5208501e96
Lucetia-Detheroc <aceso2896@gmail.com>
2018-05-23 10:30:01 -0700

    - Dungeons & Raids Achievement Area [Work in Progress]

--- contrib/Parser/DATAS/3.2 - Achievements/05 - Dungeons and Raids/10 - Draenor Dungeons.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/05 - Dungeons and Raids/10 - Draenor Dungeons.lua
@@ -7,13 +7,6 @@ _.Achievements =
 	n(-9959, { -- Dungeons & Raids
 		["groups"] = {
 			n(-9955, { -- General
-				ach(4478, { -- Looking For Multitudes
-					i(49912), -- Perky Pug
-					ach(4476), -- Looking For More
-					ach(4477, { -- Looking For Many
-						style(1, title(137)), -- the Patient <title>
-					}),
-				}),
 			}),
 		},
 	}),
