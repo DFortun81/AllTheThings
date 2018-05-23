4d9555ff02834ebaf86f3103fb22626f6fd80167
Lucetia-Detheroc <aceso2896@gmail.com>
2018-05-23 10:51:42 -0700

    - Added Collections hierarchy

--- contrib/Parser/DATAS/3.2 - Achievements/10 - Collections/01 - General.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/10 - Collections/01 - General.lua
@@ -6,70 +6,72 @@ _.Achievements =
 {
 	n(-9964, { -- Collections
 		["groups"] = {
-			ach(8728, {	-- Going To Need A Bigger Bag
-				crit(1), -- Cursed Swabby Helmet
-				crit(2), -- Warped Warning Sign
-				crit(3), -- Giant Purse of Timeless Coins
-				crit(4), -- Crystal of Insanity
-				crit(5), -- Battle Horn
-				crit(6), -- Forager's Gloves
-				crit(7), -- Big Bag of Herbs
-				crit(8), -- Overgrown Lilypad
-				crit(9), -- Hardened Shell
-				crit(10), -- Bubbling Pi'jiu Brew
-				crit(11), -- Thick Pi'jiu Brew
-				crit(12), -- Misty Pi'jiu Brew
-				crit(13), -- Warning Sign
-				crit(14), -- Ash-Covered Horn
-				crit(15), -- Cauterizing Core
-				crit(16), -- Captain Zvezdan's Lost Leg
-				crit(17), -- Cursed Talisman
-				crit(18), -- Golden Moss
-				crit(19), -- Strange Glowing Mushroom
-				crit(20), -- Eternal Kiln
-				crit(21), -- Jadefire Spirit
-				crit(22), -- Sunset Stone
-				crit(23), -- Ashen Stone
-				crit(24), -- Blizzard Stone
-				crit(25), -- Rain Stone
-				crit(26), -- Blackflame Daggers
-				crit(27), -- Falling Flame
-				crit(28), -- Ordon Death Chime
-				crit(29), -- Blazing Sigil of Ordos
-				crit(30), -- Ordon Ceremonial Robes
-				crit(31), -- Rime of the Time-Lost Mariner
-				crit(32), -- Scuttler's Shell
-				crit(33), -- Partially-Digested Meal
-				crit(34), -- Swarmling of Gu'chi
-				crit(35), -- Sticky Silkworm Goo
-				crit(36), -- Faintly-Glowing Herb
-				crit(37), -- Condensed Jademist
-				crit(38), -- Windfeather Plume
-				crit(39), -- Quivering Firestorm Egg
-				crit(40), -- Reins of the Thundering Onyx Cloud Serpent
-				crit(41), -- Pristine Stalker Hide
-				crit(42), -- Glinting Pile of Stone
-				crit(43), -- Odd Polished Stone
-				crit(44), -- Glowing Blue Ash
-				crit(45), -- Glowing Green Ash
-				crit(46), -- Bonkers
-				crit(47), -- Gulp Froglet
-				crit(48), -- Spineclaw Crab
-				crit(49), -- Skunky Alemental
-				crit(50), -- Ominous Flame
-				crit(51), -- Jademist Dancer
-				crit(52), -- Death Adder Hatchling
-				crit(53), -- Dandelion Frolicker
-				crit(54), -- Ruby Droplet
-				crit(55), -- Azure Crane Chick
-				crit(56), -- Ashleaf Spriteling
-			}),	
-			ach(9909, { -- Heirloom Hoarder
-				i(120968), -- Chauffeured Chopper
-				i(122703), -- Chauffeured Chopper
-				ach(9911), -- Where's the Mailbox?
-				ach(9906), -- Alt-ernative Lifestyle
-				ach(9908), -- Ready for Powerleveling
+			n(-9955, { -- General
+				ach(8728, {	-- Going To Need A Bigger Bag
+					crit(1), -- Cursed Swabby Helmet
+					crit(2), -- Warped Warning Sign
+					crit(3), -- Giant Purse of Timeless Coins
+					crit(4), -- Crystal of Insanity
+					crit(5), -- Battle Horn
+					crit(6), -- Forager's Gloves
+					crit(7), -- Big Bag of Herbs
+					crit(8), -- Overgrown Lilypad
+					crit(9), -- Hardened Shell
+					crit(10), -- Bubbling Pi'jiu Brew
+					crit(11), -- Thick Pi'jiu Brew
+					crit(12), -- Misty Pi'jiu Brew
+					crit(13), -- Warning Sign
+					crit(14), -- Ash-Covered Horn
+					crit(15), -- Cauterizing Core
+					crit(16), -- Captain Zvezdan's Lost Leg
+					crit(17), -- Cursed Talisman
+					crit(18), -- Golden Moss
+					crit(19), -- Strange Glowing Mushroom
+					crit(20), -- Eternal Kiln
+					crit(21), -- Jadefire Spirit
+					crit(22), -- Sunset Stone
+					crit(23), -- Ashen Stone
+					crit(24), -- Blizzard Stone
+					crit(25), -- Rain Stone
+					crit(26), -- Blackflame Daggers
+					crit(27), -- Falling Flame
+					crit(28), -- Ordon Death Chime
+					crit(29), -- Blazing Sigil of Ordos
+					crit(30), -- Ordon Ceremonial Robes
+					crit(31), -- Rime of the Time-Lost Mariner
+					crit(32), -- Scuttler's Shell
+					crit(33), -- Partially-Digested Meal
+					crit(34), -- Swarmling of Gu'chi
+					crit(35), -- Sticky Silkworm Goo
+					crit(36), -- Faintly-Glowing Herb
+					crit(37), -- Condensed Jademist
+					crit(38), -- Windfeather Plume
+					crit(39), -- Quivering Firestorm Egg
+					crit(40), -- Reins of the Thundering Onyx Cloud Serpent
+					crit(41), -- Pristine Stalker Hide
+					crit(42), -- Glinting Pile of Stone
+					crit(43), -- Odd Polished Stone
+					crit(44), -- Glowing Blue Ash
+					crit(45), -- Glowing Green Ash
+					crit(46), -- Bonkers
+					crit(47), -- Gulp Froglet
+					crit(48), -- Spineclaw Crab
+					crit(49), -- Skunky Alemental
+					crit(50), -- Ominous Flame
+					crit(51), -- Jademist Dancer
+					crit(52), -- Death Adder Hatchling
+					crit(53), -- Dandelion Frolicker
+					crit(54), -- Ruby Droplet
+					crit(55), -- Azure Crane Chick
+					crit(56), -- Ashleaf Spriteling
+				}),	
+				ach(9909, { -- Heirloom Hoarder
+					i(120968), -- Chauffeured Chopper
+					i(122703), -- Chauffeured Chopper
+					ach(9911), -- Where's the Mailbox?
+					ach(9906), -- Alt-ernative Lifestyle
+					ach(9908), -- Ready for Powerleveling
+				}),
 			}),
 		},
 	}),
