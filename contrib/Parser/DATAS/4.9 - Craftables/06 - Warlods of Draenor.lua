655beeba17ea359efc17044aca56fa40ece1ccde
Lucetia-Detheroc <aceso2896@gmail.com>
2018-05-23 20:46:33 -0700

    - Added Tailoring Craftables (minus Classic)

--- contrib/Parser/DATAS/4.9 - Craftables/06 - Warlods of Draenor.lua
+++ contrib/Parser/DATAS/4.9 - Craftables/06 - Warlods of Draenor.lua
@@ -21,7 +21,20 @@ _.Craftables =
 		n(-187, { -- Leatherworking
 		}),
 		n(-190, { -- Tailoring
-			
+			WOD_CRAFTED_ITEM(114819),	-- Brilliant Hexweave Cloak
+			WOD_CRAFTED_ITEM(114816),	-- Hexweave Belt
+			WOD_CRAFTED_ITEM(114814),	-- Hexweave Bracers
+			WOD_CRAFTED_ITEM(114810),	-- Hexweave Cowl
+			WOD_CRAFTED_ITEM(114812),	-- Hexweave Gloves
+			WOD_CRAFTED_ITEM(114811),	-- Hexweave Leggings
+			WOD_CRAFTED_ITEM(114809),	-- Hexweave Mantle
+			WOD_CRAFTED_ITEM(114813),	-- Hexweave Robe
+			WOD_CRAFTED_ITEM(114815),	-- Hexweave Slippers
+			WOD_CRAFTED_ITEM(114818),	-- Nimble Hexweave Cloak
+			WOD_CRAFTED_ITEM(114817),	-- Powerful Hexweave Cloak
+			i(114828),	-- Sumptuous Cowl
+			i(114831),	-- Sumptuous Leggings
+			i(114829),	-- Sumptuous Robes
 		}),
 	}),
 };
--- contrib/Parser/DATAS/4.9 - Craftables/07 -  Legion.lua
+++ contrib/Parser/DATAS/4.9 - Craftables/07 -  Legion.lua
@@ -21,7 +21,36 @@ _.Craftables =
 		n(-187, { -- Leatherworking
 		}),
 		n(-190, { -- Tailoring
-			
+			i(146666),	-- Celumbra, the Night's Dichotomy
+			i(151771),	-- Festival Dress
+			i(151772),	-- Festival Suit
+			i(151792),	-- Green Winter Clothes
+			i(127002),	-- Imbued Silkweave Bracers
+			i(127001),	-- Imbued Silkweave Cinch
+			i(127019),	-- Imbued Silkweave Cover
+			i(127020),	-- Imbued Silkweave Drape
+			i(127000),	-- Imbued Silkweave Epaulets
+			i(127034),	-- Imbued Silkweave Flourish
+			i(126997),	-- Imbued Silkweave Gloves
+			i(126998),	-- Imbued Silkweave Hood
+			i(126999),	-- Imbued Silkweave Pantaloons
+			i(126995),	-- Imbued Silkweave Robe
+			i(127033),	-- Imbued Silkweave Shade
+			i(126996),	-- Imbued Silkweave Slippers
+			i(151571),	-- Lightweave Breeches
+			i(151790),	-- Red Winter Clothes
+			i(126994),	-- Silkweave Bracers
+			i(126993),	-- Silkweave Cinch
+			i(127016),	-- Silkweave Cover
+			i(127017),	-- Silkweave Drape
+			i(126992),	-- Silkweave Epaulets
+			i(127032),	-- Silkweave Flourish
+			i(126989),	-- Silkweave Gloves
+			i(126990),	-- Silkweave Hood
+			i(126991),	-- Silkweave Pantaloons
+			i(126987),	-- Silkweave Robe
+			i(127031),	-- Silkweave Shade
+			i(126988),	-- Silkweave Slippers
 		}),
 	}),
 };
