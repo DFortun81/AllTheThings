429ea39c3b23a159d50f155b1bc4b3cc48e69d6b
Lucetia-Detheroc <aceso2896@gmail.com>
2018-05-23 20:37:07 -0700

    - Setting up Craftables Hierarchy

---
+++ contrib/Parser/DATAS/4.9 - Craftables/01 - Classic.lua
@@ -0,0 +1,20 @@
+-----------------------------------------------------
+--     W O R L D   E V E N T S   M O D U L E       --
+-----------------------------------------------------
+_.Craftables =
+{
+	n(-153, { -- Classic
+		n(-180, { -- Alchemy
+		}),
+		n(-181, { -- Blacksmithing
+		}),
+		n(-182, { -- Enchanting
+		}),
+		n(-183, { -- Engineering
+		}),
+		n(-187, { -- Leatherworking
+		}),
+		n(-190, { -- Tailoring
+		}),
+	}),
+};
---
+++ contrib/Parser/DATAS/4.9 - Craftables/02 - The Burning  Crusade.lua
@@ -0,0 +1,22 @@
+-----------------------------------------------------
+--     W O R L D   E V E N T S   M O D U L E       --
+-----------------------------------------------------
+_.Craftables =
+{
+	n(-154, { -- The Burning Crusade
+		n(-180, { -- Alchemy
+		}),
+		n(-181, { -- Blacksmithing
+		}),
+		n(-182, { -- Enchanting
+		}),
+		n(-183, { -- Engineering
+		}),
+		n(-186, { -- Jewelcrafting
+		}),
+		n(-187, { -- Leatherworking
+		}),
+		n(-190, { -- Tailoring
+		}),
+	}),
+};
