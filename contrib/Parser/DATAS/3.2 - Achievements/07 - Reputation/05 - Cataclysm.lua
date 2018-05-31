b8db2d76bbce23a0f5de276cfb2973c9cf8bb02e
Lucetia-Detheroc <aceso2896@gmail.com>
2018-05-31 03:59:00 -0700

    - Added Cataclysm Reputations to the achievements

--- contrib/Parser/DATAS/3.2 - Achievements/07 - Reputation/05 - Cataclysm.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/07 - Reputation/05 - Cataclysm.lua
@@ -7,6 +7,34 @@ _.Achievements =
 	n(-9961, { -- Reputation
 		["groups"] = {
 			n(-9979, { -- Cataclysm
+				ach(5827, { -- Avengers of Hyjal	
+					faction(1204), -- Avengers of Hyjal
+					style(2, title(188)), -- <Name>, Avenger of Hyjal
+				}),	
+				a(ach(5375, { -- Bardin's Wardens	
+					faction(1177), -- Bardin's Wardens
+				})),	
+				h(ach(4886, { -- Dragonmaw Clan	
+					faction(1172), -- Dragonmaw Clan
+				})),	
+				h(ach(5376, { -- Hellscream's Reach	
+					faction(1178), -- Hellscream's Reach
+				})),	
+				ach(4884, { -- Ramkahen	
+					faction(1173), -- Ramkahen
+				}),	
+				ach(4881, { -- The Earthen Ring	
+					faction(1135), -- The Earthen Ring
+				}),	
+				ach(4882, { -- The Guardians of Hyjal	
+					faction(1158), -- The Guardians of Hyjal
+				}),	
+				ach(4883, { -- Therazane	
+					faction(1171), -- Therazane
+				}),	
+				a(ach(4885, { -- Wildhammer Clan	
+					faction(1174), -- Wildhammer Clan
+				})),	
 			}),
 		},
 	}),
