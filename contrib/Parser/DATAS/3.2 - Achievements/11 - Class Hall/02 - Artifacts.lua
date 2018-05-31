b01103ff88960df257bd17bca436107aa2d4403c
Laycel\Iyanden <carrie.a.newman@gmail.com>
2018-05-31 11:04:04 -0500

    - Added all Pet Battle achievements to Achievements -> Pet Battles -> etc - Added groups syntax to a couple of early files that I missed

--- contrib/Parser/DATAS/3.2 - Achievements/11 - Class Hall/02 - Artifacts.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/11 - Class Hall/02 - Artifacts.lua
@@ -7,55 +7,57 @@ _.Achievements =
 	n(-9965, { -- Class Hall
 		["groups"] = {
 			n(-10033, { -- Artifacts
-				ach(10461, {	-- Fighting with Style: Classic
-					crit(1),	-- Recover one of the Pillars of Creation
-					crit(2),	-- Complete the quest, "Light's Charge"
-					crit(3),	-- Complete the first order campaign effort
-				}),
-				ach(10746),	-- Forged for Battle
-				ach(11772, {	-- Power Ascended
-					ach(11144),	-- Power Realized
-				}),
-				ach(10853, {	-- Part of History
-					ach(10852),	-- Artifact or Artifiction
-				}),
-				ach(10747, {	-- Fighting with Style: Upgraded
-					crit(1),	-- Forged for Battle
-					crit(2),	-- Power Realized
-					crit(3),	-- Part of History
-					crit(4),	-- This Side Up
-				}),
-				ach(10459),	-- Improving on History
-				ach(10748, {	-- Fighting with Style: Valorous
-					crit(1),	-- Improving on History
-					crit(2),	-- Unleashed Monstrosities
-					crit(3),	-- Keystone Master
-					crit(4),	-- Glory of the Legion Hero
-				}),
-				a(ach(10743)),	-- The Prestige (Alliance)
-				h(ach(10745)),	-- The Prestige (Horde)
-				a(ach(10749, {	-- Fighting with Style: War-torn (Alliance)
-					crit(1),	-- The Prestige
-					crit(2),	-- Crest of Heroism
-					crit(3),	-- Crest of Carnage
-					crit(4),	-- Crest of Devastation
-				})),
-				h(ach(11173, {	-- Fighting with Style: War-torn (Horde)
-					crit(1),	-- The Prestige
-					crit(2),	-- Crest of Heroism
-					crit(3),	-- Crest of Carnage
-					crit(4),	-- Crest of Devastation
-				})),
-				ach(10460),	-- Hidden Potential
-				ach(10750),	-- Fighting with Style: Hidden
-				ach(11611),	-- A Challenging Look
-				ach(11612),	-- Fighting with Style: Challenging
-				ach(11610, {	-- Power Unleashed
-					ach(11609),	-- Power Unbound
-				}),
-				ach(11171),	-- Arsenal of Power
-				ach(12072),	-- Now You're Cooking with Netherlight
-				ach(12071),	-- Crucible's Promise
+				["groups"] = {
+					ach(10461, {	-- Fighting with Style: Classic
+						crit(1),	-- Recover one of the Pillars of Creation
+						crit(2),	-- Complete the quest, "Light's Charge"
+						crit(3),	-- Complete the first order campaign effort
+					}),
+					ach(10746),	-- Forged for Battle
+					ach(11772, {	-- Power Ascended
+						ach(11144),	-- Power Realized
+					}),
+					ach(10853, {	-- Part of History
+						ach(10852),	-- Artifact or Artifiction
+					}),
+					ach(10747, {	-- Fighting with Style: Upgraded
+						crit(1),	-- Forged for Battle
+						crit(2),	-- Power Realized
+						crit(3),	-- Part of History
+						crit(4),	-- This Side Up
+					}),
+					ach(10459),	-- Improving on History
+					ach(10748, {	-- Fighting with Style: Valorous
+						crit(1),	-- Improving on History
+						crit(2),	-- Unleashed Monstrosities
+						crit(3),	-- Keystone Master
+						crit(4),	-- Glory of the Legion Hero
+					}),
+					a(ach(10743)),	-- The Prestige (Alliance)
+					h(ach(10745)),	-- The Prestige (Horde)
+					a(ach(10749, {	-- Fighting with Style: War-torn (Alliance)
+						crit(1),	-- The Prestige
+						crit(2),	-- Crest of Heroism
+						crit(3),	-- Crest of Carnage
+						crit(4),	-- Crest of Devastation
+					})),
+					h(ach(11173, {	-- Fighting with Style: War-torn (Horde)
+						crit(1),	-- The Prestige
+						crit(2),	-- Crest of Heroism
+						crit(3),	-- Crest of Carnage
+						crit(4),	-- Crest of Devastation
+					})),
+					ach(10460),	-- Hidden Potential
+					ach(10750),	-- Fighting with Style: Hidden
+					ach(11611),	-- A Challenging Look
+					ach(11612),	-- Fighting with Style: Challenging
+					ach(11610, {	-- Power Unleashed
+						ach(11609),	-- Power Unbound
+					}),
+					ach(11171),	-- Arsenal of Power
+					ach(12072),	-- Now You're Cooking with Netherlight
+					ach(12071),	-- Crucible's Promise
+				},
 			}),
 		},
 	}),
--- contrib/Parser/DATAS/3.2 - Achievements/11 - Class Hall/03 - Missions.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/11 - Class Hall/03 - Missions.lua
@@ -7,21 +7,23 @@ _.Achievements =
 	n(-9965, { -- Class Hall
 		["groups"] = {
 			n(-10034, { -- Missions
-				ach(11213, {	-- Lead a Legion (100)
-					ach(11212),	-- Raise an Army (20)
-					ach(10706),	-- Training the Troops (5)
-				}),
-				ach(11217, {	-- Many Many Missions, Handle It! (500)
-					ach(11216),	-- So Many Missions (100)
-					ach(11215),	-- Quite a Few Missions (50)
-					ach(11214),	-- Many Missions (10)
-				}),
-				ach(11218),	-- There's a Boss In There
-				ach(11219),	-- Need Backup
-				ach(11222, {	-- Champions of Power
-					ach(11221),	-- Champions Rise
-					ach(11220),	-- Roster of Champions
-				}),
+				["groups"] = {
+					ach(11213, {	-- Lead a Legion (100)
+						ach(11212),	-- Raise an Army (20)
+						ach(10706),	-- Training the Troops (5)
+					}),
+					ach(11217, {	-- Many Many Missions, Handle It! (500)
+						ach(11216),	-- So Many Missions (100)
+						ach(11215),	-- Quite a Few Missions (50)
+						ach(11214),	-- Many Missions (10)
+					}),
+					ach(11218),	-- There's a Boss In There
+					ach(11219),	-- Need Backup
+					ach(11222, {	-- Champions of Power
+						ach(11221),	-- Champions Rise
+						ach(11220),	-- Roster of Champions
+					}),
+				},
 			}),
 		},
 	}),
