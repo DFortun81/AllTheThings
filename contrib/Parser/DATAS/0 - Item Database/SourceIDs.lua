1ec51ed486c1f82407b303057e6f762c8ede43a2
Lucetia-Detheroc <aceso2896@gmail.com>
2018-06-01 15:08:37 -0700

    - Added missing sourceID's for speciality items (Brewfest Mugs, etc); This should eliminate some of the missing sourceID text that appears ingame for some - Adjusted Cathedral BoE's (Blizzard be consistent on a format please)

--- contrib/Parser/DATAS/0 - Item Database/SourceIDs.lua
+++ contrib/Parser/DATAS/0 - Item Database/SourceIDs.lua
@@ -25,11 +25,29 @@ i(139300, 1, 80594);	-- Finely-Tailored Green Holiday Hat
 i(139299, 1, 80593);	-- Finely-Tailored Red Holiday Hat
 
 i(19022, 1, 7546); 		-- Nat Pagle's Extreme Angler FC-5000
+
 i(37892, 1, 18084);		-- Green Brewfest Stein
+i(37893, 1, 18085);		-- Filled Green Brewfest Stein
+i(37894, 1, 18086);		-- Filled Green Brewfest Stein
+i(37895, 1, 18087);		-- Filled Green Brewfest Stein
+i(37896, 1, 18088);		-- Filled Green Brewfest Stein
+i(37897, 1, 18089);		-- Filled Green Brewfest Stein
+i(33016, 1, 15238);		-- Blue Brewfest Stein
+i(33017, 1, 15239);		-- Filled Blue Brewfest Stein
+i(33018, 1, 15240);		-- Filled Blue Brewfest Stein
+i(33019, 1, 15241);		-- Filled Blue Brewfest Stein
+i(33020, 1, 15242);		-- Filled Blue Brewfest Stein
+i(33021, 1, 15243);		-- Filled Blue Brewfest Stein
+i(32912, 1, 15167);		-- Yellow Brewfest Stein
+i(32915, 1, 15169);		-- Filled Yellow Brewfest Stein
+i(32917, 1, 15170);		-- Filled Yellow Brewfest Stein
+i(32918, 1, 15171);		-- Filled Yellow Brewfest Stein
+i(32919, 1, 15172);		-- Filled Yellow Brewfest Stein
+i(32920, 1, 15173);		-- Filled Yellow Brewfest Stein
 i(41755, 1, 20156);		-- The Fire Extinguisher
 i(45991, 1, 22166);		-- Bone Fishing Pole
 i(45992, 1, 22167);		-- Jeweled Fishing Pole
 i(56836, 1, 27941);		-- Overflowing Purple Brewfest Stein
 i(88535, 1, 45845);		-- Sharpened Tuskarr Spear
 
-
+i(37597, 1, 17888);		-- Direbrew's Shanker
