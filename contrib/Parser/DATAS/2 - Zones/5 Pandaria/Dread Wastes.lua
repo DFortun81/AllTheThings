be45cfd6ee82135343e40ef482d4d3d3586191b9
Lucetia-Detheroc <aceso2896@gmail.com>
2018-06-02 14:45:06 -0700

    - Fixed Sha'tar's rep faction (was using Mag'har)

--- contrib/Parser/DATAS/2 - Zones/5 Pandaria/Dread Wastes.lua
+++ contrib/Parser/DATAS/2 - Zones/5 Pandaria/Dread Wastes.lua
@@ -638,74 +638,63 @@ _.Zones =
 				}),
 				n(-2, {		-- Vendors
 					n(64599, {	-- Ambersmith Zikk <Klaxxi Quartermaster>
+						i(89396),	-- Amber Espada of Klaxxi'vess
+						i(89398),	-- Amber Flammard of Klaxxi'vess
+						i(89397),	-- Amber Saber of Klaxxi'vess
+						i(89395),	-- Amber Scythe of Klaxxi'vess
+						i(89400),	-- Amber Sledge of Klaxxi'vess
+						i(89393),	-- Amber Slicer of Klaxxi'vess
+						i(89394),	-- Amber Spear of Klaxxi'vess
+						i(89392),	-- Amber Spine of Klaxxi'vess
+						i(89399),	-- Amber Sprayer of Klaxxi'vess
+						i(89094),	-- Ambersmith Legplates
+						i(89064),	-- Bloodseeker's Solitaire
+						i(89671),	-- Chestplate of the Stone Lion
+						i(89065),	-- Choker of the Klaxxi'va
+						i(89645),	-- Cruel Mercy Bracers
+						i(89090),	-- Dreadsworn Slayer Legs
+						i(89650),	-- Emperor's Riding Gloves
+						i(89647),	-- Entombed Traitor's Wristguards
 						i(92522),	-- Grand Commendation of the Klaxxi
-						i(89093),	-- Kovok's Riven Legguards
-						i(89089),	-- Wind-Reaver Greaves
+						i(89670),	-- Inner Serenity Chestplate
+						i(89060),	-- Klaxxi Lash of the Borrower
 						i(89056),	-- Klaxxi Lash of the Consumer
 						i(89057),	-- Klaxxi Lash of the Doubter
-						i(89058),	-- Klaxxi Lash of the Winnower
-						i(89059),	-- Klaxxi Lash of the Precursor
-						i(89060),	-- Klaxxi Lash of the Borrower
 						i(89061),	-- Klaxxi Lash of the Harbinger
 						i(89062),	-- Klaxxi Lash of the Orator
-						i(89063),	-- Klaxxi Lash of the Seeker
-						i(89087),	-- Poisoncrafter's Kilt
-						i(89088),	-- Leggings of the Poisoned Soul
-						i(89090),	-- Dreadsworn Slayer Legs
-						i(89091),	-- Swarmkeeper's Leggings
-						i(89092),	-- Locust Swarm Legguards
+						i(89059),	-- Klaxxi Lash of the Precursor
 						i(89055),	-- Klaxxi Lash of the Rescinder
-						i(89094),	-- Ambersmith Legplates
-						i(89095),	-- Legguards of the Unscathed
-						i(89400),	-- Amber Sledge of Klaxxi'vess
-						i(89392),	-- Amber Spine of Klaxxi'vess
-						i(89394),	-- Amber Spear of Klaxxi'vess
-						i(89395),	-- Amber Scythe of Klaxxi'vess
-						i(89396),	-- Amber Espada of Klaxxi'vess
-						i(89397),	-- Amber Saber of Klaxxi'vess
-						i(89398),	-- Amber Flammard of Klaxxi'vess
-						i(89399),	-- Amber Sprayer of Klaxxi'vess
-						i(89393),	-- Amber Slicer of Klaxxi'vess
-						i(89644),	-- Sudden Insight Bracers
-						i(89645),	-- Cruel Mercy Bracers
-						i(89646),	-- Runoff Wristguards
-						i(89647),	-- Entombed Traitor's Wristguards
-						i(89650),	-- Emperor's Riding Gloves
-						i(89651),	-- Krompf's Fine-Tuning Gloves
-						i(89670),	-- Inner Serenity Chestplate
-						i(89671),	-- Chestplate of the Stone Lion
+						i(89063),	-- Klaxxi Lash of the Seeker
+						i(89058),	-- Klaxxi Lash of the Winnower
 						i(89798),	-- Klaxxi Tabard
-						i(85262),	-- Reins of the Amber Scorpion Mount
-						i(83787, {	-- Plans: Ghost Reaver's Breastplate
-							i(82975),	-- Ghost Reaver's Breastplate
-						}),
-						i(83788, {	-- Plans: Ghost Reaver's Gauntlets
-							i(82976),	-- Ghost Reaver's Gauntlets
-						}),
-						i(83789, {	-- Plans: Living Steel Breastplate
-							i(82977),	-- Living Steel Breastplate
-						}),
-						i(83790, {	-- Plans: Living Steel Gauntlets
-							i(82978),	-- Living Steel Gauntlets
-						}),
-						i(83791, {	-- Plans: Breastplate of Ancient Steel
-							i(82979),	-- Breastplate of Ancient Steel
-						}),
-						i(83792, {	-- Plans: Gauntlets of Ancient Steel
-							i(82980),	-- Gauntlets of Ancient Steel
-						}),
-						i(84197, {	-- Plans: Masterwork Forgewire Axe
-							i(82970),	-- Masterwork Forgewire Axe
-						}),
-						i(84198, {	-- Plans: Masterwork Ghost Shard
-							i(82974),	-- Masterwork Ghost Shard
-						}),
-						i(84217, {	-- Plans: Masterwork Phantasmal Hammer
-							i(82972),	-- Masterwork Phantasmal Hammer
-						}),
-						i(84218, {	-- Plans: Masterwork Spiritblade Decimator
-							i(82973),	-- Masterwork Spiritblade Decimator
-						}),
+						i(89093),	-- Kovok's Riven Legguards
+						i(89651),	-- Krompf's Fine-Tuning Gloves
+						i(89088),	-- Leggings of the Poisoned Soul
+						i(89095),	-- Legguards of the Unscathed
+						i(89067),	-- Links of the Lucid
+						i(89092),	-- Locust Swarm Legguards
+						i(89066),	-- Paragon's Pale Pendant
+						i(83791),	-- Plans: Breastplate of Ancient Steel
+						i(83792),	-- Plans: Gauntlets of Ancient Steel
+						i(90531),	-- Plans: Ghost Iron Shield Spike
+						i(83787),	-- Plans: Ghost Reaver's Breastplate
+						i(83788),	-- Plans: Ghost Reaver's Gauntlets
+						i(84196),	-- Plans: Living Steel Belt Buckle
+						i(83789),	-- Plans: Living Steel Breastplate
+						i(83790),	-- Plans: Living Steel Gauntlets
+						i(90532),	-- Plans: Living Steel Weapon Chain
+						i(84197),	-- Plans: Masterwork Forgewire Axe
+						i(84198),	-- Plans: Masterwork Ghost Shard
+						i(84200),	-- Plans: Masterwork Ghost-Forged Blade
+						i(84217),	-- Plans: Masterwork Phantasmal Hammer
+						i(84218),	-- Plans: Masterwork Spiritblade Decimator
+						i(89087),	-- Poisoncrafter's Kilt
+						i(85262),	-- Reins of the Amber Scorpion
+						i(89646),	-- Runoff Wristguards
+						i(89644),	-- Sudden Insight Bracers
+						i(89091),	-- Swarmkeeper's Leggings
+						i(89089),	-- Wind-Reaver Greaves
+						i(89068),	-- Wire of the Wakener
 					}),
 				}),
 				n(-38, { -- Profession
