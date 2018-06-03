e7cfd70eb4380699c8708e5e59f3aa823fe01013
Laycel\Iyanden <carrie.a.newman@gmail.com>
2018-06-02 20:09:44 -0500

    - Added remaining garrison achievements to Achievements - Draenor Garrisons -> etc sections - May need to adjust filter on Admiral's Compass un Shipyard -> Admiral -> Seaman

--- contrib/Parser/DATAS/3.2 - Achievements/12 - Draenor Garrisons/03 - Followers.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/12 - Draenor Garrisons/03 - Followers.lua
@@ -8,6 +8,32 @@ _.Achievements =
 		["groups"] = {
 			n(-10036, { -- Followers
 				["groups"] = {
+					ach(9494, {	-- Commander (40)
+						ach(9109),	-- Fearless Leader (25)
+						ach(9108),	-- Where You Go, They Will Follow (10)
+						ach(9107),	-- Follow the Leader (5)
+					}),
+					ach(9129, {	-- Filling the Ranks (20 - lvl 100)
+						a(i(111967, {	-- Dwarven Bunker, Level 3
+							["groups"] = {
+							},
+							["f"] = 200, 	-- Recipe
+						})),
+						h(i(116186, {	-- War Mill, Level 3
+							["groups"] = {
+							},
+							["f"] = 200,	-- Recipe
+						})),
+						ach(9111),	-- Raising the Bar (10 - lvl 100)
+						ach(9110),	-- Following Up (1 - lvl 100)
+					}),
+					ach(9130),	-- A Rare Friend
+					ach(9131),	-- An Epic Buddy
+					ach(9243, {	-- Item Level Force (20 - lvl 650)
+						ach(9213),	-- Item Level Army (10 - lvl 650)
+						ach(9212),	-- My Item Level Is Way Higher than Yours (1 - lvl 650)
+						ach(9211),	-- My Item Level Is Higher than Yours (1 - lvl 625)
+					}),
 				},
 			}),
 		},
