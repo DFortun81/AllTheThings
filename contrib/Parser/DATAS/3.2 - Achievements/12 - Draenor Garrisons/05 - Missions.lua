58cdcd1d3b57ea5bcbbc665dc07b5db63abe497c
Laycel\Iyanden <carrie.a.newman@gmail.com>
2018-06-02 20:09:44 -0500

    - Added remaining garrison achievements to Achievements - Draenor Garrisons -> etc sections - May need to adjust filter on Admiral's Compass un Shipyard -> Admiral -> Seaman

--- contrib/Parser/DATAS/3.2 - Achievements/12 - Draenor Garrisons/05 - Missions.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/12 - Draenor Garrisons/05 - Missions.lua
@@ -8,6 +8,47 @@ _.Achievements =
 		["groups"] = {
 			n(-10038, { -- Missions
 				["groups"] = {
+					ach(9140, {	-- On a Metric Ton of Missions (1000)
+						ach(9139),	-- On a Massive Number of Missions (500)
+						ach(9138),	-- On a Lot of Missions (100)
+						ach(9134),	-- On a Few Missions (50)
+						ach(9133),	-- On a Mission (10)
+					}),
+					ach(9143, {	-- The Rarer the Better (300)
+						ach(9142),	-- Keeping It Rare (50)
+						ach(9141),	-- A Rare Mission (1)
+					}),
+					ach(9524, {	-- Treasure Master (50)
+						ach(9145),	-- Treasure Mission Specialist (1)
+					}),
+					ach(9523, {	-- Patrolling Draenor (50)
+						i(111971, {	-- Barracks, Level 3
+							["groups"] = {
+							},
+							["f"] = 200,	-- Recipe
+						}),
+						ach(9146),	-- Patrol Mission Specialist (1)
+					}),
+					ach(9147, {	-- Mission Specialist
+						crit(1),	-- Patrol Mission Specialist
+						crit(2),	-- Treasure Mission Specialist
+					}),
+					ach(9900, {	-- Exploration Mission Master
+						crit(1),	-- Exploration: Oshu'gun Dig Site
+						crit(2),	-- Exploration: Razed Warsong Outpost Dig Site
+						crit(3),	-- Exploration: Stonecrag Excavation Dig Site
+						crit(4),	-- Exploration: Sethekk Hollow Dig Sites
+						crit(5),	-- Exploration: Apexis Excavation Dig Site
+						crit(6),	-- Exploration: Ango'rosh Dig Site
+						crit(7),	-- Exploration: Veil Shadar Dig Site
+						crit(8),	-- Exploration: Overlook Ruins Dig Site
+						crit(9),	-- Exploration: The Broken Spine Dig Site
+						crit(10),	-- Exploration: Daggermaw Flows Dig Site
+						crit(11),	-- Exploration: Colossal's Fall Dig Site
+						crit(12),	-- Exploration: Burial Fields
+						crit(13),	-- Exploration: Umbrafen Dig Site
+						ach(9150),	-- Exploration Mission Specialist
+					}),
 				},
 			}),
 		},
--- contrib/Parser/DATAS/3.2 - Achievements/12 - Draenor Garrisons/07 - Shipyard.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/12 - Draenor Garrisons/07 - Shipyard.lua
@@ -8,6 +8,93 @@ _.Achievements =
 		["groups"] = {
 			n(-10040, { -- Shipyard
 				["groups"] = {
+					ach(10168, {	-- Naval Armada
+						ach(10169),	-- Naval Fleet
+						ach(10177),	-- Set Sail!
+					}),
+					a(ach(10174, {	-- Admiral (Alliance)
+						ach(10173),	-- Fleet Commander
+						ach(10172, {	-- Petty Officer
+							i(128362),	-- Captain's Hat
+						}),
+						ach(10170, {	-- Seaman
+							i(128353),	-- Admiral's Compass
+						}),
+					})),
+					h(ach(10276, {	-- Admiral (Horde)
+						ach(10275),	-- Fleet Commander
+						ach(10255, {	-- Petty Officer
+							i(128363),	-- Captain's Hat
+						}),
+						ach(10170, {	-- Seaman
+							i(128353),	-- Admiral's Compass
+						}),
+					})),
+					ach(10154, {	-- Naval Combat Expert
+						ach(10156),	-- Naval Combat Specialist
+					}),
+					ach(10160, {	-- Naval Treasure Expert
+						ach(10159),	-- Naval Treasure Specialist
+					}),
+					ach(10155, {	-- Naval Siege Expert
+						ach(10161),	-- Naval Siege Specialist
+					}),
+					ach(10163, {	-- Naval Training Expert
+						ach(10162),	-- Naval Training Specialist
+					}),
+					ach(10036, {	-- Naval Bonus Expert
+						ach(10017),	-- Naval Bonus Specialist
+					}),
+					ach(10164, {	-- Master of the Seas
+						title(317),	-- Captain
+						crit(1),	-- Naval Combat Expert
+						crit(2),	-- Naval Treasure Expert
+						crit(3),	-- Naval Training Expert
+						crit(4),	-- Naval Bonus Expert
+						crit(5),	-- Naval Siege Expert
+					}),
+					ach(10165, {	-- Ironsides
+						crit(1),	-- Destroyer
+						crit(2),	-- Submarine
+						crit(3),	-- Carrier
+						crit(4),	-- Transport
+						crit(5),	-- Battleship
+					}),
+					ach(10166, {	-- Naval Mechanics
+						crit(1),	-- Tuskarr Fishing Net
+						crit(2),	-- Felsmoke Launchers
+						crit(3),	-- Unsinkable
+						crit(4),	-- High Intensity Fog Lights
+						crit(5),	-- True Iron Rudder
+						crit(6),	-- Trained Shark Tank
+						crit(7),	-- Ghostly Spyglass
+						crit(8),	-- Ice Cutter
+						crit(9),	-- Gyroscopic Internal Stabilizer
+						crit(10),	-- Bilge Pump
+						crit(11),	-- Blast Furnace
+					}),
+					a(ach(10167, {	-- Exploring the High Seas (Alliance)
+						crit(1),	-- The House Always Wins
+						crit(2),	-- For Hate's Sake
+						crit(3),	-- Black Market Journal
+						crit(4),	-- Orphaned Aquatic Animal Rescue
+						crit(5),	-- The Wave Mistress
+						crit(6),	-- It's a Boat, It's a Plane, It's... Just a Riverbeast.
+						ach(10256, {	-- Charting a Course
+							i(128365),	-- Fleet Commander's Hat
+						}),
+					})),
+					h(ach(10307, {	-- Exploring the High Seas (Horde)
+						crit(1),	-- The House Always Wins
+						crit(2),	-- For Hate's Sake
+						crit(3),	-- Black Market Journal
+						crit(4),	-- Orphaned Aquatic Animal Rescue
+						crit(5),	-- The Wave Mistress
+						crit(6),	-- It's a Boat, It's a Plane, It's... Just a Riverbeast.
+						ach(10258, {	-- Charting a Course
+							i(128364),	-- Fleet Commander's Hat
+						}),
+					})),
 				},
 			}),
 		},
