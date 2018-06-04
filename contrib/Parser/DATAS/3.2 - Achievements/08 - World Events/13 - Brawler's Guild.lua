19dfaf0cf762cff5c95be0bdbf1b7ee46547a676
Laycel\Iyanden <carrie.a.newman@gmail.com>
2018-06-04 15:44:44 -0500

    - Added remaining world event achievements to Achievements -> World Events -> etc sections

--- contrib/Parser/DATAS/3.2 - Achievements/08 - World Events/13 - Brawler's Guild.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/08 - World Events/13 - Brawler's Guild.lua
@@ -8,6 +8,34 @@ _.Achievements =
 		["groups"] = {
 			n(-10026, { -- Brawler's Guidl
 				["groups"] = {
+					a(ach(11565, {	-- King of the Guild (Alliance)
+						title(209),	-- Brawler
+						ach(11563, {	-- The Second Rule of Brawler's Guild
+							i(93195),	-- Brawler's Pass
+						}),
+						ach(11560),	-- You Are Not Your $#*@! Legplates
+						ach(11558),	-- The First Rule of Brawler's Guild
+					})),
+					h(ach(11566, {	-- King of the Guild (Horde)
+						title(209),	-- Brawler
+						ach(11564, {	-- The Second Rule of Brawler's Guild
+							i(93228),	-- Brawler's Pass
+						}),
+						ach(11561),	-- You Are Not Your $#*@! Legplates
+						ach(11559),	-- The First Rule of Brawler's Guild
+					})),
+					ach(11567),	-- You Are Not The Contents Of Your Wallet
+					ach(11570),	-- Educated Guesser
+					ach(11572),	-- I Am Thrall's Complete Lack Of Surprise
+					ach(11573, {	-- Rumble Club
+						crit(1),	-- Mazhareen
+						crit(2),	-- Grief Warden
+						crit(3),	-- Battle of the Brew
+						crit(4),	-- Mindbreaker Gzzaj
+						crit(5),	-- Senya
+						crit(6),	-- Penguin Stampede
+						crit(7),	-- Stranglethorn Streak
+					}),
 				},
 			}),
 		},
