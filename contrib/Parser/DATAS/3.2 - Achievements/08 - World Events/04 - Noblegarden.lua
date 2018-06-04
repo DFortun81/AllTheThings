c98447069cc9aa005dc333d89254cb540f477e19
Laycel\Iyanden <carrie.a.newman@gmail.com>
2018-06-03 19:46:41 -0500

    - Added Lunar Festival, Love is in the Air, and Noblegarden achievements to Achievements -> World Events -> etc sections, more to come, still WIP

--- contrib/Parser/DATAS/3.2 - Achievements/08 - World Events/04 - Noblegarden.lua
+++ contrib/Parser/DATAS/3.2 - Achievements/08 - World Events/04 - Noblegarden.lua
@@ -8,6 +8,52 @@ _.Achievements =
 		["groups"] = {
 			n(-10017, { -- Noblegarden
 				["groups"] = {
+					ach(2676),	-- I Found One!
+					ach(2418, {	-- Chocoholic (100)
+						ach(2417),	-- Chocolate Lover (25)
+					}),
+					ach(248, {	-- Sunday's Finest
+						crit(1),	-- White Tuxedo Shirt
+						crit(2),	-- Black Tuxedo Pants
+					}),
+					ach(249),	-- Dressed for the Occasion
+					ach(2416),	-- Hard Boiled (Un'Goro Crater)
+					a(ach(2419, {	-- Spring Fling (Alliance)
+						crit(1),	-- Azure Watch
+						crit(2),	-- Dolanaar
+						crit(3),	-- Goldshire
+						crit(4),	-- Kharanos
+					})),	
+					h(ach(2497, {	-- Spring Fling (Horde)
+						crit(1),	-- Bloodhoof Village
+						crit(2),	-- Brill
+						crit(3),	-- Falconwing Square
+						crit(4),	-- Razor Hill
+					})),
+					a(ach(2421)),	-- Noble Garden (Stormwind City) (Alliance)
+					h(ach(2420)),	-- Noble Garden (Silvermoon City) (Horde)
+					ach(2422, {	-- Shake Your Bunny-Maker
+						crit(1),	-- Bloof Elf
+						crit(2),	-- Draenei
+						crit(3),	-- Dwarf
+						crit(4),	-- Gnome
+						crit(5),	-- Goblin
+						crit(6),	-- Human
+						crit(7),	-- Night Elf
+						crit(8),	-- Orc
+						crit(9),	-- Tauren
+						crit(10),	-- Troll
+						crit(11),	-- Undead
+						crit(12),	-- Worgen
+					}),
+					ach(2436, {	-- Desert Rose
+						crit(1),	-- The Badlands
+						crit(2),	-- Desolace
+						crit(3),	-- Silithus
+						crit(4),	-- Tanaris
+						crit(5),	-- Thousand Needles
+					}),
+					ach(2576),	-- Blushing Bride
 				},
 			}),
 		},
