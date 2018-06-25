d1ea01a8a40dc715df1600a209c5e5b85b67a68c
ada <slumber@love.com>
2018-06-25 16:20:51 -0500

    - added accessories & reorganized MSV

--- contrib/Parser/DATAS/1 - Dungeons Raids/5 - Mists of Pandaria/1 - Raids/1 Mogu'shan Vaults.lua
+++ contrib/Parser/DATAS/1 - Dungeons Raids/5 - Mists of Pandaria/1 - Raids/1 Mogu'shan Vaults.lua
@@ -178,20 +178,20 @@ _.Instances =
 									i(86790),	-- Vial of Dragon's Blood
 								})),
 								cr(60400, e(677, {	-- Will of the Emperor (60399)
-									i(86802),	-- Lei Shen's Final Orders
-									i(86805),	-- Qin-xi's Polarizing Seal
-									i(86810),	-- Worldwaker Cabochon
-									i(86809),	-- Hood of Focused Energy
-									i(86804),	-- Crown of Opportunistic Strikes
-									i(86808),	-- Magnetized Leggings
-									i(89975),	-- Dreadeye Gaze
-									i(89977),	-- Enameled Grips of Solemnity
-									i(87826),	-- Grips of Terra Cotta
-									i(86807),	-- Spaulders of the Emperor's Rage
-									i(89976),	-- Chestguard of Eternal Vigilance
-									i(86803),	-- Jang-xi's Devastating Legplates
 									i(86806),	-- Tihan, Scepter of the Sleeping Emperor
 									i(86801),	-- Fang Kung, Spark of Titans
+									i(86804),	-- Crown of Opportunistic Strikes
+									i(89975),	-- Dreadeye Gaze
+									i(86809),	-- Hood of Focused Energy
+									i(86810),	-- Worldwaker Cabochon
+									i(86807),	-- Spaulders of the Emperor's Rage
+									i(89976),	-- Chestguard of Eternal Vigilance
+									i(89977),	-- Enameled Grips of Solemnity
+									i(87826),	-- Grips of Terra Cotta
+									i(86803),	-- Jang-xi's Devastating Legplates
+									i(86808),	-- Magnetized Leggings
+									i(86802),	-- Lei Shen's Final Orders
+									i(86805),	-- Qin-xi's Polarizing Seal
 								})),
 							}),
 						},
@@ -203,22 +203,22 @@ _.Instances =
 						["groups"] = {
 							n(-0, { -- Common Zone Drop
 								["groups"] = {
+									i(86238),	-- Pattern: Chestguard of Nemeses
 									i(86272),	-- Pattern: Fists of Lightning
 									i(86380),	-- Pattern: Imperial Silk Gloves
+									i(86381),	-- Pattern: Legacy of the Emperor
 									i(86279),	-- Pattern: Liferuned Leather Gloves
 									i(86280),	-- Pattern: Murderer's Gloves
-									i(86284),	-- Pattern: Raven Lord's Gloves
-									i(86382),	-- Pattern: Touch of the Light
-									i(86238),	-- Pattern: Chestguard of Nemeses
-									i(86381),	-- Pattern: Legacy of the Emperor
 									i(86281),	-- Pattern: Nightfire Robe
 									i(86283),	-- Pattern: Raiment of Blood and Bone
+									i(86284),	-- Pattern: Raven Lord's Gloves
 									i(86379),	-- Pattern: Robe of Eternal Rule
 									i(86297),	-- Pattern: Stormbreaker Chestguard
+									i(86382),	-- Pattern: Touch of the Light
 									i(87411),	-- Plans: Bloodforged Warfists
+									i(87412),	-- Plans: Chestplate of Limitless Faith
 									i(87409),	-- Plans: Gauntlets of Battle Command
 									i(87413),	-- Plans: Gauntlets of Unbound Devotion
-									i(87412),	-- Plans: Chestplate of Limitless Faith
 									i(87410),	-- Plans: Ornate Battleplate of the Master
 									i(87408),	-- Plans: Unyielding Bloodplate
 								},
@@ -257,19 +257,20 @@ _.Instances =
 							}),
 							cr(60047, e(679, {	-- The Stone Guard
 								["groups"] = {
-									i(86134),	-- 
-									i(85925),	-- 
-									i(85926),	-- 
-									i(85924),	-- 
-									i(85979),	-- 
-									i(85977),	-- 
-									i(85976),	-- 
-									i(89766),	-- 
-									i(85923),	-- 
-									i(89768),	-- 
-									i(89767),	-- 
-									i(85978),	-- 
-									i(85975),	-- 
+									i(85924),	-- Dagger of the Seven Stars
+									i(85976),	-- Sixteen-Fanged Crown
+									i(85922),	-- Beads of the Mogu'shi
+									i(85979),	-- Cape of Three Lanterns
+									i(89766),	-- Stonefang Chestguard
+									i(85923),	-- Stonemaw Armguards
+									i(89768),	-- Claws of Amethyst
+									i(89767),	-- Ruby-Linked Girdle
+									i(86134),	-- Star-Stealer Waistguard
+									i(85977),	-- Stonebound Cinch
+									i(85978),	-- Jade Dust Leggings
+									i(85926),	-- Stoneflesh Leggings
+									i(85975),	-- Heavenly Jade Greatboots
+									i(85925),	-- Jasper Clawfeet
 								},
 								["crs"] = {
 									60047, -- Amethyst Guardian
@@ -279,45 +280,53 @@ _.Instances =
 								},
 							})),
 							cr(60009, e(689, {	-- Feng the Accursed
-								i(85984),	-- 
-								i(85985),	-- 
-								i(85983),	-- 
-								i(86082),	-- 
-								i(85982),	-- 
-								i(89424),	-- 
-								i(85989),	-- 
-								i(85990),	-- 
-								i(85987),	-- 
-								i(85988),	-- 
-								i(85980),	-- 
+								i(89424),	-- Fan of Fiery Winds
+								i(85989),	-- Hood of Cursed Dreams
+								i(85984),	-- Nullification Greathelm
+								i(85986),	-- Amulet of Seven Curses
+								i(86082),	-- Arrow Breaking Windcloak
+								i(85985),	-- Cloak of Peacock Feathers
+								i(85990),	-- Imperial Ghostbinder's Robes
+								i(85983),	-- Bracers of Six Oxen
+								i(85987),	-- Chain of Shadow
+								i(85982),	-- Tomb Raider's Girdle
+								i(85988),	-- Legplates of Sagacious Shadows
+								i(85980),	-- Wildfire Worldwalkers
+								i(89803),	-- Feng's Ring of Dreams
+								i(89802),	-- Feng's Seal of Binding
 							})),
 							cr(60143, e(682, {	-- Gara'jal the Spiritbinder
-								i(89817),	-- 
-								i(85992),	-- 
-								i(85994),	-- 
-								i(85995),	-- 
-								i(86039),	-- 
-								i(85996),	-- 
-								i(86041),	-- 
-								i(86027),	-- 
-								i(85993),	-- 
-								i(86040),	-- 
-								i(85997),	-- 
+								i(85994),	-- Gara'kal, Fist of the Spiritbinder
+								i(85996),	-- Eye of the Ancient Spirit
+								i(85991),	-- Soulgrasp Choker
+								i(85995),	-- Netherrealm Shoulderpads
+								i(86041),	-- Shadowsummoner Spaulders
+								i(86039),	-- Spaulders of the Divided Mind
+								i(86027),	-- Bindings of Ancient Spirits
+								i(89817),	-- Bonded Soul Bracers
+								i(85993),	-- Fetters of Death
+								i(86040),	-- Leggings of Imprisoned Will
+								i(85997),	-- Sandals of the Severed Soul
+								i(85992),	-- Sollerets of Spirit Splitting
+								i(86038),	-- Circuit of the Frail Soul
+
 							})),
 							cr(60701, e(687, {	-- The Spirit Kings
 								["groups"] = {
-									i(86071),	-- 
-									i(86080),	-- 
-									i(86076),	-- 
-									i(89818),	-- 
-									i(89819),	-- 
-									i(86127),	-- 
-									i(86075),	-- 
-									i(86129),	-- 
-									i(86128),	-- 
-									i(86086),	-- 
-									i(86081),	-- 
-									i(86084),	-- 
+									i(86071),	-- Screaming Tiger, Qiang's Unbreakable Polearm
+									i(86075),	-- Steelskin, Qiang's Impervious Shield
+									i(86129),	-- Hood of Blind Eyes
+									i(86047),	-- Amulet of the Hidden Kings
+									i(86083),	-- Zian's Choker of Coalesced Shadow
+									i(86080),	-- Shoulderguards of the Unflanked
+									i(89819),	-- Mindshard Drape
+									i(86076),	-- Breastplate of the Kings' Guard
+									i(86127),	-- Bracers of Dark Thoughts
+									i(89818),	-- Bracers of Violent Meditation
+									i(86128),	-- Undying Shadow Grips
+									i(86086),	-- Girdle of Delirious Visions
+									i(86081),	-- Subetai's Pillaging Leggings
+									i(86084),	-- Meng's Treads of Insanity
 								},
 								["crs"] = {
 									60701, -- Zian of the Endless Shadow <Sorcer King>
@@ -327,32 +336,39 @@ _.Instances =
 								},
 							})),
 							cr(60410, e(726, {	-- Elegon
-								i(86140),	-- 
-								i(86130),	-- 
-								i(86135),	-- 
-								i(86136),	-- 
-								i(86138),	-- 
-								i(86137),	-- 
-								i(89821),	-- 
-								i(86141),	-- 
-								i(89822),	-- 
-								i(86139),	-- 
-								i(87777),	-- 
+								i(87777),	-- Reins of the Astral Cloud Serpent (MOUNT!)
+								i(86140),	-- Starshatter
+								i(86130),	-- Elegion, the Fanged Crescent
+								i(86137),	-- Torch of the Celestial Spark
+								i(89821),	-- Crown of Keening Stars
+								i(86141),	-- Shoulders of Empyreal Focus
+								i(86136),	-- Chestguard of Total Annihilation
+								i(86135),	-- Starcrusher Gauntlets
+								i(89822),	-- Galaxyfire Girdle
+								i(86139),	-- Orbital Belt
+								i(86138),	-- Phasewalker Striders
+								i(89824),	-- Band of Bursting Novas
+								i(86132),	-- Bottle of Infinite Stars
+								i(86133),	-- Light of the Cosmos
+								i(86131),	-- Vial of Dragon's Blood
 							})),
 							cr(60400, e(677, {	-- Will of the Emperor
 								["groups"] = {
-									i(138804),	-- 
-									i(86148),	-- 
-									i(89823),	-- 
-									i(86145),	-- 
-									i(86146),	-- 
-									i(86150),	-- 
-									i(86142),	-- 
-									i(89820),	-- 
-									i(86151),	-- 
-									i(86149),	-- 
-									i(89825),	-- 
-									i(87827),	-- 
+									i(138804),	-- Illusion: Colossus
+									i( 86148),	-- Tihan, Scepter of the Sleeping Emperor
+									i( 86142),	-- Fang Kung, Spark of Titans
+									i( 86146),	-- Crown of Opportunistic Strikes
+									i( 89820),	-- Dreadeye Gaze
+									i( 86151),	-- Hood of Focused Energy
+									i( 86152),	-- Worldwaker Cabochon
+									i( 86149),	-- Spaulders of the Emperor's Rage
+									i( 89823),	-- Chestguard of Eternal Vigilance
+									i( 89825),	-- Enameled Grips of Solemnity
+									i( 87827),	-- Grips of Terra Cotta
+									i( 86145),	-- Jang-xi's Devastating Legplates
+									i( 86150),	-- Magnetized Leggings
+									i( 86144),	-- Lei Shen's Final Orders
+									i( 86147),	-- Qin-xi's Polarizing Seal
 								},
 								["crs"] = {
 									60400, -- Jan-xi <Emperor's Open Hand>
@@ -367,22 +383,22 @@ _.Instances =
 						["groups"] = {
 							n(0, { -- Zone Drop
 								["groups"] = {
+									i(86238),	-- Pattern: Chestguard of Nemeses
 									i(86272),	-- Pattern: Fists of Lightning
 									i(86380),	-- Pattern: Imperial Silk Gloves
+									i(86381),	-- Pattern: Legacy of the Emperor
 									i(86279),	-- Pattern: Liferuned Leather Gloves
 									i(86280),	-- Pattern: Murderer's Gloves
-									i(86284),	-- Pattern: Raven Lord's Gloves
-									i(86382),	-- Pattern: Touch of the Light
-									i(86238),	-- Pattern: Chestguard of Nemeses
-									i(86381),	-- Pattern: Legacy of the Emperor
 									i(86281),	-- Pattern: Nightfire Robe
 									i(86283),	-- Pattern: Raiment of Blood and Bone
+									i(86284),	-- Pattern: Raven Lord's Gloves
 									i(86379),	-- Pattern: Robe of Eternal Rule
 									i(86297),	-- Pattern: Stormbreaker Chestguard
+									i(86382),	-- Pattern: Touch of the Light
 									i(87411),	-- Plans: Bloodforged Warfists
+									i(87412),	-- Plans: Chestplate of Limitless Faith
 									i(87409),	-- Plans: Gauntlets of Battle Command
 									i(87413),	-- Plans: Gauntlets of Unbound Devotion
-									i(87412),	-- Plans: Chestplate of Limitless Faith
 									i(87410),	-- Plans: Ornate Battleplate of the Master
 									i(87408),	-- Plans: Unyielding Bloodplate
 								},
@@ -421,19 +437,20 @@ _.Instances =
 							}),
 							cr(60047, e(679, {	-- The Stone Guard
 								["groups"] = {
-									i(87060),	-- 
-									i(87015),	-- 
-									i(87013),	-- 
-									i(87012),	-- 
-									i(87018),	-- 
-									i(87019),	-- 
-									i(87020),	-- 
-									i(89929),	-- 
-									i(87014),	-- 
-									i(89931),	-- 
-									i(89930),	-- 
-									i(87017),	-- 
-									i(87021),	-- 
+									i(87012),	-- Dagger of the Seven Stars
+									i(87020),	-- Sixteen-Fanged Crown
+									i(87016),	-- Beads of the Mogu'shi
+									i(87018),	-- Cape of Three Lanterns
+									i(89929),	-- Stonefang Chestguard
+									i(87014),	-- Stonemaw Armguards
+									i(89931),	-- Claws of Amethyst
+									i(89930),	-- Ruby-Linked Girdle
+									i(87060),	-- Star-Stealer Waistguard
+									i(87019),	-- Stonebound Cinch
+									i(87017),	-- Jade Dust Leggings
+									i(87013),	-- Stoneflesh Leggings
+									i(87021),	-- Heavenly Jade Greatboots
+									i(87015),	-- Jasper Clawfeet
 								},
 								["crs"] = {
 									60047, -- Amethyst Guardian
@@ -443,45 +460,52 @@ _.Instances =
 								},
 							})),
 							cr(60009, e(689, {	-- Feng the Accursed
-								i(87024),	-- 
-								i(87026),	-- 
-								i(87025),	-- 
-								i(87044),	-- 
-								i(87022),	-- 
-								i(89425),	-- 
-								i(87029),	-- 
-								i(87027),	-- 
-								i(87030),	-- 
-								i(87031),	-- 
-								i(87023),	-- 
+								i(89425),	-- Fan of Fiery Winds
+								i(87029),	-- Hood of Cursed Dreams
+								i(87024),	-- Nullification Greathelm
+								i(87028),	-- Amulet of Seven Curses
+								i(87044),	-- Arrow Breaking Windcloak
+								i(87026),	-- Cloak of Peacock Feathers
+								i(87027),	-- Imperial Ghostbinder's Robes
+								i(87025),	-- Bracers of Six Oxen
+								i(87030),	-- Chain of Shadow
+								i(87022),	-- Tomb Raider's Girdle
+								i(87031),	-- Legplates of Sagacious Shadows
+								i(87023),	-- Wildfire Worldwalkers
+								i(89933),	-- Feng's Ring of Dreams
+								i(89932),	-- Feng's Seal of Binding
 							})),
 							cr(60143, e(682, {	-- Gara'jal the Spiritbinder
-								i(89934),	-- 
-								i(87035),	-- 
-								i(87032),	-- 
-								i(87033),	-- 
-								i(87041),	-- 
-								i(87039),	-- 
-								i(87038),	-- 
-								i(87043),	-- 
-								i(87034),	-- 
-								i(87042),	-- 
-								i(87037),	-- 
+								i(87032),	-- Gara'kal, Fist of the Spiritbinder
+								i(87039),	-- Eye of the Ancient Spirit
+								i(87036),	-- Soulgrasp Choker
+								i(87033),	-- Netherrealm Shoulderpads
+								i(87038),	-- Shadowsummoner Spaulders
+								i(87041),	-- Spaulders of the Divided Mind
+								i(87043),	-- Bindings of Ancient Spirits
+								i(89934),	-- Bonded Soul Bracers
+								i(87034),	-- Fetters of Death
+								i(87042),	-- Leggings of Imprisoned Will
+								i(87037),	-- Sandals of the Severed Soul
+								i(87035),	-- Sollerets of Spirit Splitting
+								i(87040),	-- Circuit of the Frail Soul
 							})),
 							cr(60701, e(687, {	-- The Spirit Kings
 								["groups"] = {
-									i(87046),	-- 
-									i(87049),	-- 
-									i(87048),	-- 
-									i(89935),	-- 
-									i(89936),	-- 
-									i(87054),	-- 
-									i(87050),	-- 
-									i(87051),	-- 
-									i(87052),	-- 
-									i(87056),	-- 
-									i(87047),	-- 
-									i(87055),	-- 
+									i(87046),	-- Screaming Tiger, Qiang's Unbreakable Polearm
+									i(87050),	-- Steelskin, Qiang's Impervious Shield
+									i(87051),	-- Hood of Blind Eyes
+									i(87045),	-- Amulet of the Hidden Kings
+									i(87053),	-- Zian's Choker of Coalesced Shadow
+									i(87049),	-- Shoulderguards of the Unflanked
+									i(89936),	-- Mindshard Drape
+									i(87048),	-- Breastplate of the Kings' Guard
+									i(87054),	-- Bracers of Dark Thoughts
+									i(89935),	-- Bracers of Violent Meditation
+									i(87052),	-- Undying Shadow Grips
+									i(87056),	-- Girdle of Delirious Visions
+									i(87047),	-- Subetai's Pillaging Leggings
+									i(87055),	-- Meng's Treads of Insanity
 								},
 								["crs"] = {
 									60701, -- Zian of the Endless Shadow <Sorcer King>
@@ -491,32 +515,39 @@ _.Instances =
 								},
 							})),
 							cr(60410, e(726, {	-- Elegon
-								i(87061),	-- 
-								i(87062),	-- 
-								i(87059),	-- 
-								i(87058),	-- 
-								i(87067),	-- 
-								i(87066),	-- 
-								i(89939),	-- 
-								i(87068),	-- 
-								i(89938),	-- 
-								i(87064),	-- 
-								i(87777),	-- 
+								i(87777),	-- Reins of the Astral Cloud Serpent (MOUNT!)
+								i(87061),	-- Starshatter
+								i(87062),	-- Elegion, the Fanged Crescent
+								i(87066),	-- Torch of the Celestial Spark
+								i(89939),	-- Crown of Keening Stars
+								i(87068),	-- Shoulders of Empyreal Focus
+								i(87058),	-- Chestguard of Total Annihilation
+								i(87059),	-- Starcrusher Gauntlets
+								i(89938),	-- Galaxyfire Girdle
+								i(87064),	-- Orbital Belt
+								i(87067),	-- Phasewalker Striders
+								i(89937),	-- Band of Bursting Novas
+								i(87057),	-- Bottle of Infinite Stars
+								i(87065),	-- Light of the Cosmos
+								i(87063),	-- Vial of Dragon's Blood
 							})),
 							cr(60400, e(677, {	-- Will of the Emperor
 								["groups"] = {
-									i(138804),	-- 
-									i(87074),	-- 
-									i(89941),	-- 
-									i(87071),	-- 
-									i(87070),	-- 
-									i(87077),	-- 
-									i(87069),	-- 
-									i(89940),	-- 
-									i(87073),	-- 
-									i(87078),	-- 
-									i(89942),	-- 
-									i(87825),	-- 
+									i(138804),	-- Illusion: Colossus
+									i( 87074),	-- Tihan, Scepter of the Sleeping Emperor
+									i( 87069),	-- Fang Kung, Spark of Titans
+									i( 87070),	-- Crown of Opportunistic Strikes
+									i( 89940),	-- Dreadeye Gaze
+									i( 87073),	-- Hood of Focused Energy
+									i( 87076),	-- Worldwaker Cabochon
+									i( 87078),	-- Spaulders of the Emperor's Rage
+									i( 89941),	-- Chestguard of Eternal Vigilance
+									i( 89942),	-- Enameled Grips of Solemnity
+									i( 87825),	-- Grips of Terra Cotta
+									i( 87071),	-- Jang-xi's Devastating Legplates
+									i( 87077),	-- Magnetized Leggings
+									i( 87072),	-- Lei Shen's Final Orders
+									i( 87075),	-- Qin-xi's Polarizing Seal
 								},
 								["crs"] = {
 									60400, -- Jan-xi <Emperor's Open Hand>
