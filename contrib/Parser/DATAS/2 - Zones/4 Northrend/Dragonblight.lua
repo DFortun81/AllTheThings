190c907c45ba2c27657df0a57af810c4109f12cd
Lucetia-Detheroc <aceso2896@gmail.com>
2018-06-30 23:53:48 -0700

    Revert "Forgot to clean up a bit"
    This reverts commit c28060c1e3e259108d86aa5870397b721e1bb67e.

--- contrib/Parser/DATAS/2 - Zones/4 Northrend/Dragonblight.lua
+++ contrib/Parser/DATAS/2 - Zones/4 Northrend/Dragonblight.lua
@@ -1644,6 +1644,8 @@ _.Zones =
 						-- 	},
 						-- 	["qg"] = 105081,	-- Archmage Kalec
 						-- }),
+					}),
+					n(-494, {	-- Also Miscellaneous (These are unorganized)
 						q(13077, {	--  The Touch of an Aspect
 							-- This is part of the Icecrown Achiev...because Blizz
 							["groups"] = {
