d7525d80ab66b98f884efcabb8c14d439e5a76f3
Oxlotus <alex.metz1024@gmail.com>
2018-07-01 18:49:40 -0500

    Pirate's Day was organized into headers to maintain the Holiday structure standard.

--- contrib/Parser/DATAS/4 - Events/11 Holidays/WorldEvents_Holidays_07Pirate'sDay.lua
+++ contrib/Parser/DATAS/4 - Events/11 Holidays/WorldEvents_Holidays_07Pirate'sDay.lua
@@ -7,23 +7,42 @@ _.WorldEvents =
 		["groups"] = {
 			n(-55, { 	-- Pirate's Day
 				["groups"] = {
-					n(122396, { -- Edward Techt
+					n(-4, { -- Achievements
+						ach(3457, { -- The Captain's Booty
+							["groups"] = {
+							},
+							["u"] = 23, -- Pirate's Day
+						}),
+					}),
+					n(-17, { -- Quests
+						q(42758, { -- You're Gonna Need A Bigger Boat!
+							["qg"] = 28048, -- Dread Captain DeMeza
+							["maps"] = { 673, }, -- The Cape of Stranglethorn
+							["u"] = 23, -- Pirate's Day
+						}),
+					}),
+					n(-16, { -- Rares
+						n(108715, {  -- Ol' Eary
+							["groups"] = {
+								un(23, i(138415)), -- Slightly-Chewed Insult Book
+							},
+							["displayID"] = 70668,
+							["u"] = 23, -- Pirate's Day
+						}),
+					}),
+					n(-2, { -- Vendors
+						n(122396, { -- Edward Techt
 						["groups"] =  {
 							ach(871, { -- Avast Ye, Admiral!
-								un(23, i(150547)), -- Jolly Roger Toy
+								un(23, i(150547)), -- Jolly Roger (Toy)
 							}),
 						},
-						["description"] = "Purchasing the toy |cFFFFD700Jolly Roger|r requires you to have the achievement |cFFFFD700Avast Ye, Admiral|r.  He is located at |cFFFFFFFF39.67 84.29|r in |cFFFFD700The Cape of Stranglethorn|r",
-					}),
-					n(108715, {  -- Ol' Eary
-						["groups"] = {
-							un(23, i(138415)), -- Slightly-Chewed Insult Book
-						},
-						["displayID"] = 70668,
+						["description"] = "To purchase the toy, |cFFFFD700Jolly Roger|r, you must have the achievement, |cFFFFD700Avast Ye, Admiral|r. Edward Techt is located at |cFFFFD70039.67|r, |cFFFFD70084.29|r in |cFFFFD700The Cape of Stranglethorn|r.",
+						["u"] = 23, -- Pirate's Day
+						}),
 					}),
 				},
-				["achievementID"] = 3457, -- The Captain's Booty
-				["u"] = 23,
+				["u"] = 23, -- Pirate's Day
 			}),
 		},
 		["icon"] = "Interface\\Icons\\INV_Helm_Cloth_WitchHat_B_01",
