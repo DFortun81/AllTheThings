dd6cc9010f01932ae731e7a1d80ea857c6f69aff
Lucetia-Detheroc <aceso2896@gmail.com>
2018-07-01 20:20:13 -0700

    - Added some missing Stein ID's

--- contrib/Parser/DATAS/0 - Item Database/SourceIDs.lua
+++ contrib/Parser/DATAS/0 - Item Database/SourceIDs.lua
@@ -8,11 +8,6 @@ end
 
 i(7509, 1, 2859);		-- Manaweave Robe (Ghostlands)
 i(24339, 1, 9500);		-- Stung
-i(18044, 1, 7222); 		-- Hurley's Tankard
-i(12791, 1, 4544); 		-- Barman Shanker
-i(117378, 1, 65960); 	-- Direbrew's Bloodied Shanker
-i(71331, 1, 36595); 	-- Direbrew's Bloodied Shanker
-i(107217, 1, 57985); 	-- Direbrew's Bloodied Shanker
 i(49888, 1, 24432); 	-- Shadow's Edge
 i(128827, 1, 76173);	-- Xal'atath, Blade of the Black Empire
 i(20637, 1, 8133);		-- Acid Inscribed Pauldrons
@@ -20,6 +15,19 @@ i(5197, 1, 1989);		-- Cookie's Tenderizer
 i(130064, 1, 74444);	-- Deadeye Monocle	
 i(139300, 1, 80594);	-- Finely-Tailored Green Holiday Hat
 i(139299, 1, 80593);	-- Finely-Tailored Red Holiday Hat
+i(41755, 1, 20156);		-- The Fire Extinguisher
+i(86559, 1, 44498);		-- Frying Pan
+
+------------------------------
+-- S T E I N S   /   M U G S--
+------------------------------
+
+i(18044, 1, 7222); 		-- Hurley's Tankard
+i(12791, 1, 4544); 		-- Barman Shanker
+i(117378, 1, 65960); 	-- Direbrew's Bloodied Shanker
+i(71331, 1, 36595); 	-- Direbrew's Bloodied Shanker
+i(107217, 1, 57985); 	-- Direbrew's Bloodied Shanker
+i(37597, 1, 17888);		-- Direbrew's Shanker
 i(37892, 1, 18084);		-- Green Brewfest Stein
 i(37893, 1, 18085);		-- Filled Green Brewfest Stein
 i(37894, 1, 18086);		-- Filled Green Brewfest Stein
@@ -38,11 +46,9 @@ i(32917, 1, 15170);		-- Filled Yellow Brewfest Stein
 i(32918, 1, 15171);		-- Filled Yellow Brewfest Stein
 i(32919, 1, 15172);		-- Filled Yellow Brewfest Stein
 i(32920, 1, 15173);		-- Filled Yellow Brewfest Stein
-i(41755, 1, 20156);		-- The Fire Extinguisher
 i(56836, 1, 27941);		-- Overflowing Purple Brewfest Stein
-i(37597, 1, 17888);		-- Direbrew's Shanker
-i(86559, 1, 44498);		-- Frying Pan
-
+i(15932, 1, 6515);		-- Disciple's Stein
+i(4696, 1, 1802);		-- Lapidis Tankard of Tidesippe
 
 ----------------------------------
 -- F I S H I N G      P O L E S --
