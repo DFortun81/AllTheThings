6db8b1681b481f28c8cab4ac8a538d0f1e41b05c
CRIEVE-SARGERAS <dylanfortune@live.com>
2018-07-23 21:58:13 -0700

    Added summoning items to the rares section for Terrok in Outland.

--- contrib/Parser/DATAS/2 - Zones/3 Outland/Terokkar Forest.lua
+++ contrib/Parser/DATAS/2 - Zones/3 Outland/Terokkar Forest.lua
@@ -425,23 +425,21 @@ _.Zones =
 						i(31228),
 						i(31229),
 					}), 
-					--i(32720, {	-- Time-Lost Offering
-						n(21838, {	-- Terokk
-							["itemID"] = 32720,
-							["groups"] = {	
-								dr(	13	, i(	31564	)), --	Skystalker's Leggings
-								dr(	12	, i(	31580	)), --	Slatesteel Leggings
-								dr(	12	, i(	31556	)), --	Windchanneller's Leggings
-								dr(	11	, i(	31572	)), --	Mistshroud Pants
-								dr(	10	, i(	32536	)), --	Terokk's Gavel
-								dr(	9	, i(	32537	)), --	Terokk's Gavel
-								dr(	9	, i(	32782	)), --	Time-Lost Figurine
-								dr(	6	, i(	32540	)), --	Terokk's Might
-								dr(	5	, i(	32541	)), --	Terokk's Wisdom
-							},
-							["description"] = "The process to summon this boss is as follows.\n\n1. Collect Shadow Dust from mobs in Skettis.\n2. Turn in 6 Shadow Dust to Severin for the quest More Shadow Dust to obtain Elixir of Shadows.\n3. Drink the elixir to gain a 20 min buff that allows you to see Time-Lost mobs around Skettis.\n4. Kill these mobs to obtain Time-Lost Scrolls (40 required per summon).\n5. Make sure you are on the Adversarial Blood quest if this is your first time, it begins with the quest Ishaal's Almanac.\n6. Go to a skull pile and use 10 Time-Lost Scrolls to summon boss. (Darkscreecher Akkarai [Akkarai's Talons], Karrog [Karrog's Spine], Gezzarak the Huntress [Gezzarak's Claws], Vakkiz the Windrager [Vakkiz's Scale]).\n7. Take these 4 items to Hazzik to complete Adversarial Blood which rewards a Time-Lost Offering.\n8. Use the Time-Lost Offering at the Ancient Skull Pile on middle island to summon Terokk. (It has about 15 min spawn timer)",
-						}),
-					--}),
+					n(21838, {	-- Terokk
+						["itemID"] = 32720,	-- -- Time-Lost Offering
+						["groups"] = {	
+							dr(	13	, i(	31564	)), --	Skystalker's Leggings
+							dr(	12	, i(	31580	)), --	Slatesteel Leggings
+							dr(	12	, i(	31556	)), --	Windchanneller's Leggings
+							dr(	11	, i(	31572	)), --	Mistshroud Pants
+							dr(	10	, i(	32536	)), --	Terokk's Gavel
+							dr(	9	, i(	32537	)), --	Terokk's Gavel
+							dr(	9	, i(	32782	)), --	Time-Lost Figurine
+							dr(	6	, i(	32540	)), --	Terokk's Might
+							dr(	5	, i(	32541	)), --	Terokk's Wisdom
+						},
+						["description"] = "The process to summon this boss is as follows.\n\n1. Collect Shadow Dust from mobs in Skettis.\n2. Turn in 6 Shadow Dust to Severin for the quest More Shadow Dust to obtain Elixir of Shadows.\n3. Drink the elixir to gain a 20 min buff that allows you to see Time-Lost mobs around Skettis.\n4. Kill these mobs to obtain Time-Lost Scrolls (40 required per summon).\n5. Make sure you are on the Adversarial Blood quest if this is your first time, it begins with the quest Ishaal's Almanac.\n6. Go to a skull pile and use 10 Time-Lost Scrolls to summon boss. (Darkscreecher Akkarai [Akkarai's Talons], Karrog [Karrog's Spine], Gezzarak the Huntress [Gezzarak's Claws], Vakkiz the Windrager [Vakkiz's Scale]).\n7. Take these 4 items to Hazzik to complete Adversarial Blood which rewards a Time-Lost Offering.\n8. Use the Time-Lost Offering at the Ancient Skull Pile on middle island to summon Terokk. (It has about 15 min spawn timer)",
+					}),
 					i(32620, {	-- Time-Lost Scroll
 						["description"] = "Use 10 Time-Lost Scrolls at the skull piles to summon the rares below. See Terokk description for more info.",
 						["groups"] = {
@@ -456,6 +454,7 @@ _.Zones =
 									dr(6, i(31558)),	-- Windchanneller's Bindings
 									dr(6, i(31555)),	-- Windchanneller's Ceinture
 									dr(5, i(31563)),	-- Skystalker's Cord
+									i(32715),	-- Akkarai's Talons
 								},
 								["description"] = "Summoned at skull piles with 10 Time-Lost Scrolls. See Terokk description for more info.",
 							}),
@@ -468,7 +467,8 @@ _.Zones =
 									dr(6, i(31582)),	-- Slatesteel Bracers
 									dr(6, i(31579)),	-- Slatesteel Girdle
 									dr(6, i(31558)),	-- Windchanneller's Bindings
-									dr(6, i(31555)),	-- Windchanneller's Ceinture							
+									dr(6, i(31555)),	-- Windchanneller's Ceinture
+									i(32716),	-- Gazzarak's Claws
 								},
 								["description"] = "Summoned at skull piles with 10 Time-Lost Scrolls. See Terokk description for more info.",					
 							}),
@@ -483,6 +483,7 @@ _.Zones =
 									dr(6, i(31579)),	-- Slatesteel Girdle
 									dr(6, i(31558)),	-- Windchanneller's Bindings
 									dr(6, i(31555)),	-- Windchanneller's Ceinture
+									i(32717),	-- Karro's Spine
 								},
 								["description"] = "Summoned at skull piles with 10 Time-Lost Scrolls. See Terokk description for more info.",					
 							}),
@@ -496,7 +497,8 @@ _.Zones =
 									dr(6, i(31579)),	-- Slatesteel Girdle
 									dr(6, i(31558)),	-- Windchanneller's Bindings
 									dr(6, i(31555)),	-- Windchanneller's Ceinture	
-									dr(5, i(31563)),	-- Skystalker's Cord							
+									dr(5, i(31563)),	-- Skystalker's Cord
+									i(32718),	-- Vakkiz's Scale
 								},
 								["description"] = "Summoned at skull piles with 10 Time-Lost Scrolls. See Terokk description for more info.",
 							}),
