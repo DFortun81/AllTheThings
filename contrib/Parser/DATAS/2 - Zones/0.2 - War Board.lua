5119603fe189dc1962b5768cb269a61030e6caf0
Lucetia-Detheroc <aceso2896@gmail.com>
2018-07-23 22:36:09 -0700

    -  Hero's Call: Redridge Mountains now displays under Special Duty Assignments (future ones will go here so they stand out more, especially if rewards are available)

--- contrib/Parser/DATAS/2 - Zones/0.2 - War Board.lua
+++ contrib/Parser/DATAS/2 - Zones/0.2 - War Board.lua
@@ -13,7 +13,16 @@ _.Zones =
 	})),
 	a(i(156474, { -- Special Duty Assignments
 		["groups"] = {
-			-- Quests Here
+			qa(26365, { 		-- Hero's Call: Redridge Mountains! (add'l QG 29611)
+				["groups"] = {
+					i(60689),	-- Belt of Unsolvable Problems
+					i(60690),	-- Doody Boots
+					i(131517),	-- Doody Shoes
+					i(60688),	-- Parker's Yardstick
+					i(60691),	-- Unfortunate Treads
+				},
+				["qg"] = 107574, -- Anduin Wrynn <King of Stormwind> 
+			}),
 		},
 		["description"] = "These can be obtained from any city or town.  Once you start questing in a zone then you can no longer pick these up.",
 		["collectible"] = false,
