249211d963bec3427b9a9b65d422b5bf8afbf394
Lucetia-Detheroc <aceso2896@gmail.com>
2018-07-27 14:11:20 -0700

    - Freehold Dungeon Migration Complete

---
+++ contrib/Parser/DATAS/1 - Dungeons Raids/8 - Battle For Azeroth/2 - Dungeons/Freehold.lua
@@ -0,0 +1,226 @@
+---------------------------------------------------
+--          Z O N E S       M O D U L E          --
+---------------------------------------------------
+
+_.Instances =
+{
+	{
+		["groups"] = {
+			inst(1001, { -- Freehold
+				["groups"] = {
+					d(1, { -- Normal
+						["groups"] = {
+							e(2102, { -- Skycap'n Kragg
+								["groups"] = {
+									i(159633), -- Sharkbait's Fishhook
+									i(155884), -- Parrotfeather Cloak
+									i(159227), -- Silk Cuffs of the Skycap'n
+									i(159353), -- Chain-Linked Safety Cord
+									i(158360), -- Sharkbait Harneses Girdle
+									i(155862), -- Kragg's Rigging Scalers
+								},
+								["crs"] = {
+									129732, -- Skycap'n Kragg
+									--  Shark Bait
+								},
+							}),
+							e(2093, { -- Council o' Captains
+								["groups"] = {
+									i(159132), -- Jolly's Boot Daggeer
+									i(159130), -- Captain's Diplomacy
+									i(158311), -- Concealed Fencing Plates
+									i(159356), -- Raoul's Barrelhook Bracers
+									i(158346), -- Sailcloth Waistband
+									i(159297), -- Silver-Trimmed Breeches
+									i(158351), -- Dashing Bilge Rat Shoes
+									i(158314), -- Seal of Questionable 
+								},
+								["crs"] = {
+									129431, -- Capttain Eudora
+									129432, -- Captain Jolly
+									126847, -- Captain Raoul
+								},
+							}),
+							e(2094, { -- Ring of Booty
+								["groups"] = {
+									i(159634), -- Jeweled Sharksplitter
+									i(158305), -- Sea Dog's Cuffs
+									i(155892), -- Bite-Resistant Chain Gloves
+									i(155891), -- Greasy Bacon-Grabbers
+									i(155889), -- Sharkhide Grips
+									i(155890), -- Sharktooth-Knuckled Grips
+									i(158302), -- Chum-Coated Leggings
+									i(158361), -- Sharkwater Waders
+									i(158356), -- Shell-Kickers
+								},
+								["crs"] = {
+									126969, -- Trothak <The Shark Puncher>
+								},
+							}),
+							e(2095, { -- Harlan Sweete
+								["groups"] = {
+									i(159635), -- Bloody Tideturner
+									i(155888), -- Irontide Captain's Hat
+									i(155885), -- Sea-Brawler's Greathelm
+									i(155886), -- Smartly Plumed Cap
+									i(155887), -- Sweete's Jeweled Headgear
+									i(159352), -- Gaping Maw Shoulderguard
+									i(159299), -- Gold-Tasseled Epautlets
+									i(159407), -- Lockjaw Shoulderplate
+									i(158301), -- Ruffled Poet Blouse
+									i(155881), -- Harlan's Loaded Dice
+								},
+								["crs"] = {
+									129440, -- Harlan Sweete
+								},
+							}),
+						},
+					}),
+					
+					d(2, { -- Heroic
+						["groups"] = {
+							e(2102, { -- Skycap'n Kragg
+								["groups"] = {
+									i(159633), -- Sharkbait's Fishhook
+									i(155884), -- Parrotfeather Cloak
+									i(159227), -- Silk Cuffs of the Skycap'n
+									i(159353), -- Chain-Linked Safety Cord
+									i(158360), -- Sharkbait Harneses Girdle
+									i(155862), -- Kragg's Rigging Scalers
+								},
+								["crs"] = {
+									129732, -- Skycap'n Kragg
+									--  Shark Bait
+								},
+							}),
+							e(2093, { -- Council o' Captains
+								["groups"] = {
+									i(159132), -- Jolly's Boot Daggeer
+									i(159130), -- Captain's Diplomacy
+									i(158311), -- Concealed Fencing Plates
+									i(159356), -- Raoul's Barrelhook Bracers
+									i(158346), -- Sailcloth Waistband
+									i(159297), -- Silver-Trimmed Breeches
+									i(158351), -- Dashing Bilge Rat Shoes
+									i(158314), -- Seal of Questionable 
+								},
+								["crs"] = {
+									129431, -- Capttain Eudora
+									129432, -- Captain Jolly
+									126847, -- Captain Raoul
+								},
+							}),
+							e(2094, { -- Ring of Booty
+								["groups"] = {
+									i(159634), -- Jeweled Sharksplitter
+									i(158305), -- Sea Dog's Cuffs
+									i(155892), -- Bite-Resistant Chain Gloves
+									i(155891), -- Greasy Bacon-Grabbers
+									i(155889), -- Sharkhide Grips
+									i(155890), -- Sharktooth-Knuckled Grips
+									i(158302), -- Chum-Coated Leggings
+									i(158361), -- Sharkwater Waders
+									i(158356), -- Shell-Kickers
+								},
+								["crs"] = {
+									126969, -- Trothak <The Shark Puncher>
+								},
+							}),
+							e(2095, { -- Harlan Sweete
+								["groups"] = {
+									i(159635), -- Bloody Tideturner
+									i(155888), -- Irontide Captain's Hat
+									i(155885), -- Sea-Brawler's Greathelm
+									i(155886), -- Smartly Plumed Cap
+									i(155887), -- Sweete's Jeweled Headgear
+									i(159352), -- Gaping Maw Shoulderguard
+									i(159299), -- Gold-Tasseled Epautlets
+									i(159407), -- Lockjaw Shoulderplate
+									i(158301), -- Ruffled Poet Blouse
+									i(155881), -- Harlan's Loaded Dice
+								},
+								["crs"] = {
+									129440, -- Harlan Sweete
+								},
+							}),
+						},
+					}),
+					d(23, { -- Mythic
+						["groups"] = {
+							e(2102, { -- Skycap'n Kragg
+								["groups"] = {
+									i(159633), -- Sharkbait's Fishhook
+									i(155884), -- Parrotfeather Cloak
+									i(159227), -- Silk Cuffs of the Skycap'n
+									i(159353), -- Chain-Linked Safety Cord
+									i(158360), -- Sharkbait Harneses Girdle
+									i(155862), -- Kragg's Rigging Scalers
+								},
+								["crs"] = {
+									129732, -- Skycap'n Kragg
+									--  Shark Bait
+								},
+							}),
+							e(2093, { -- Council o' Captains
+								["groups"] = {
+									i(159132), -- Jolly's Boot Daggeer
+									i(159130), -- Captain's Diplomacy
+									i(158311), -- Concealed Fencing Plates
+									i(159356), -- Raoul's Barrelhook Bracers
+									i(158346), -- Sailcloth Waistband
+									i(159297), -- Silver-Trimmed Breeches
+									i(158351), -- Dashing Bilge Rat Shoes
+									i(158314), -- Seal of Questionable 
+								},
+								["crs"] = {
+									129431, -- Capttain Eudora
+									129432, -- Captain Jolly
+									126847, -- Captain Raoul
+								},
+							}),
+							e(2094, { -- Ring of Booty
+								["groups"] = {
+									i(159634), -- Jeweled Sharksplitter
+									i(158305), -- Sea Dog's Cuffs
+									i(155892), -- Bite-Resistant Chain Gloves
+									i(155891), -- Greasy Bacon-Grabbers
+									i(155889), -- Sharkhide Grips
+									i(155890), -- Sharktooth-Knuckled Grips
+									i(158302), -- Chum-Coated Leggings
+									i(158361), -- Sharkwater Waders
+									i(158356), -- Shell-Kickers
+								},
+								["crs"] = {
+									126969, -- Trothak <The Shark Puncher>
+								},
+							}),
+							e(2095, { -- Harlan Sweete
+								["groups"] = {
+									i(159842), -- Sharkbait's Favorite Crackers
+									i(159635), -- Bloody Tideturner
+									i(155888), -- Irontide Captain's Hat
+									i(155885), -- Sea-Brawler's Greathelm
+									i(155886), -- Smartly Plumed Cap
+									i(155887), -- Sweete's Jeweled Headgear
+									i(159352), -- Gaping Maw Shoulderguard
+									i(159299), -- Gold-Tasseled Epautlets
+									i(159407), -- Lockjaw Shoulderplate
+									i(158301), -- Ruffled Poet Blouse
+									i(155881), -- Harlan's Loaded Dice
+								},
+								["crs"] = {
+									129440, -- Harlan Sweete
+								},
+							}),
+						},
+					}),
+				},
+				["lvl"] = 98,
+				["maps"] = {
+					936, -- Freehold
+				},
+			}),
+		},
+		["tierID"] = 8,
+	},
+};
