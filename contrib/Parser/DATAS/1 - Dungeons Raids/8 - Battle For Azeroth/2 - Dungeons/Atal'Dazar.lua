bc907ddcc1bab4c9f453a3e5a11661c683cdff0b
Lucetia-Detheroc <aceso2896@gmail.com>
2018-07-27 14:01:12 -0700

    - Atal'Dazar Dungeon Migration Complete

---
+++ contrib/Parser/DATAS/1 - Dungeons Raids/8 - Battle For Azeroth/2 - Dungeons/Atal'Dazar.lua
@@ -0,0 +1,218 @@
+---------------------------------------------------
+--          Z O N E S       M O D U L E          --
+---------------------------------------------------
+
+_.Instances =
+{
+	{
+		["groups"] = {
+			inst(968, { -- Atal'Dazar
+				["groups"] = {
+					d(1, { -- Normal
+						["groups"] = {
+							e(2082, { -- Priestess Alun'za
+								["groups"] = {
+									i(158321), -- Wand of Zealous Purification
+									i(158322), -- Aureus Vessel
+									i(158309), -- Wristlinks of Alchemical Transfusion
+									i(158306), -- Belt of Gleaming Determination
+									i(158347), -- Cincture of Glittering Gold
+									i(158313), -- Legplates of Beaten Gold
+									i(155861), -- Embellished Ritual Sabatons
+									i(158319), -- My'das Talisman
+								},
+								["crs"] = {
+									129614, -- Priestess Alun'za
+								},
+							}),
+							e(2036, { -- Vol'kaal
+								["groups"] = {
+									i(159632), -- Adulation Enforcer
+									i(158375), -- Drape of the Loyal Vassal
+									i(158348), -- Wraps of Everliving Fealty
+									i(158317), -- Gauntlets of Eternal Service
+									i(159445), -- Grips of the Everlasting Guardian
+									i(155869), -- Shambling Berserker's Leggings
+									i(158320), -- Revitalizing Voodoo Totem
+								},
+								["crs"] = {
+									129399, -- Vol'kaal
+								},
+							}),
+							e(2083, { -- Rezan
+								["groups"] = {
+									i(160269), -- Soulrending Claw
+									i(158711), -- Hallowed Ossein Longbow
+									i(158713), -- Disc of Indomitable Will
+									i(160214), -- Venerated Raptorhide Bindings
+									i(155868), -- Kilt of Fanatical Consumption
+									i(158303), -- Devilsaur Worshiper's Sandals
+									i(159458), -- Seal of the Regal Loa
+									i(158712), -- Rezan's Gleaning Eye
+								},
+								["crs"] = {
+									143577, -- Rezan
+								},
+							}),
+							e(2030, { -- Yazma
+								["groups"] = {
+									i(158323), -- Soulrender's Fang
+									i(159358), -- Coif of the Court Spider
+									i(158315), -- Secret Spinner's Miteer
+									i(155866), -- Soulspun Casque
+									i(158304), -- Mantle of Fastidious Machinations
+									i(159233), -- Loa Betrayer's Vestments
+									i(155860), -- Spymaster's  Wrap
+									i(160212), -- Shadowshroud Vambraces
+									i(158308), -- Souldrifting Sabatons
+									i(159610), -- Vessel of Skittering Shadows
+								},
+								["crs"] = {
+									129412, -- Yazma
+								},
+							}),
+						},
+					}),
+					
+					d(2, { -- Heroic
+						["groups"] = {
+							e(2082, { -- Priestess Alun'za
+								["groups"] = {
+									i(158321), -- Wand of Zealous Purification
+									i(158322), -- Aureus Vessel
+									i(158309), -- Wristlinks of Alchemical Transfusion
+									i(158306), -- Belt of Gleaming Determination
+									i(158347), -- Cincture of Glittering Gold
+									i(158313), -- Legplates of Beaten Gold
+									i(155861), -- Embellished Ritual Sabatons
+									i(158319), -- My'das Talisman
+								},
+								["crs"] = {
+									129614, -- Priestess Alun'za
+								},
+							}),
+							e(2036, { -- Vol'kaal
+								["groups"] = {
+									i(159632), -- Adulation Enforcer
+									i(158375), -- Drape of the Loyal Vassal
+									i(158348), -- Wraps of Everliving Fealty
+									i(158317), -- Gauntlets of Eternal Service
+									i(159445), -- Grips of the Everlasting Guardian
+									i(155869), -- Shambling Berserker's Leggings
+									i(158320), -- Revitalizing Voodoo Totem
+								},
+								["crs"] = {
+									129399, -- Vol'kaal
+								},
+							}),
+							e(2083, { -- Rezan
+								["groups"] = {
+									i(160269), -- Soulrending Claw
+									i(158711), -- Hallowed Ossein Longbow
+									i(158713), -- Disc of Indomitable Will
+									i(160214), -- Venerated Raptorhide Bindings
+									i(155868), -- Kilt of Fanatical Consumption
+									i(158303), -- Devilsaur Worshiper's Sandals
+									i(159458), -- Seal of the Regal Loa
+									i(158712), -- Rezan's Gleaning Eye
+								},
+								["crs"] = {
+									143577, -- Rezan
+								},
+							}),
+							e(2030, { -- Yazma
+								["groups"] = {
+									i(158323), -- Soulrender's Fang
+									i(159358), -- Coif of the Court Spider
+									i(158315), -- Secret Spinner's Miteer
+									i(155866), -- Soulspun Casque
+									i(158304), -- Mantle of Fastidious Machinations
+									i(159233), -- Loa Betrayer's Vestments
+									i(155860), -- Spymaster's  Wrap
+									i(160212), -- Shadowshroud Vambraces
+									i(158308), -- Souldrifting Sabatons
+									i(159610), -- Vessel of Skittering Shadows
+								},
+								["crs"] = {
+									129412, -- Yazma
+								},
+							}),
+						},
+					}),
+					d(23, { -- Mythic
+						["groups"] = {
+							e(2082, { -- Priestess Alun'za
+								["groups"] = {
+									i(158321), -- Wand of Zealous Purification
+									i(158322), -- Aureus Vessel
+									i(158309), -- Wristlinks of Alchemical Transfusion
+									i(158306), -- Belt of Gleaming Determination
+									i(158347), -- Cincture of Glittering Gold
+									i(158313), -- Legplates of Beaten Gold
+									i(155861), -- Embellished Ritual Sabatons
+									i(158319), -- My'das Talisman
+								},
+								["crs"] = {
+									129614, -- Priestess Alun'za
+								},
+							}),
+							e(2036, { -- Vol'kaal
+								["groups"] = {
+									i(159632), -- Adulation Enforcer
+									i(158375), -- Drape of the Loyal Vassal
+									i(158348), -- Wraps of Everliving Fealty
+									i(158317), -- Gauntlets of Eternal Service
+									i(159445), -- Grips of the Everlasting Guardian
+									i(155869), -- Shambling Berserker's Leggings
+									i(158320), -- Revitalizing Voodoo Totem
+								},
+								["crs"] = {
+									129399, -- Vol'kaal
+								},
+							}),
+							e(2083, { -- Rezan
+								["groups"] = {
+									i(159841), -- Tome of Hex: Skeletal Hatchling
+									i(160269), -- Soulrending Claw
+									i(158711), -- Hallowed Ossein Longbow
+									i(158713), -- Disc of Indomitable Will
+									i(160214), -- Venerated Raptorhide Bindings
+									i(155868), -- Kilt of Fanatical Consumption
+									i(158303), -- Devilsaur Worshiper's Sandals
+									i(159458), -- Seal of the Regal Loa
+									i(158712), -- Rezan's Gleaning Eye
+								},
+								["crs"] = {
+									143577, -- Rezan
+								},
+							}),
+							e(2030, { -- Yazma
+								["groups"] = {
+									i(158323), -- Soulrender's Fang
+									i(159358), -- Coif of the Court Spider
+									i(158315), -- Secret Spinner's Miteer
+									i(155866), -- Soulspun Casque
+									i(158304), -- Mantle of Fastidious Machinations
+									i(159233), -- Loa Betrayer's Vestments
+									i(155860), -- Spymaster's  Wrap
+									i(160212), -- Shadowshroud Vambraces
+									i(158308), -- Souldrifting Sabatons
+									i(159610), -- Vessel of Skittering Shadows
+								},
+								["crs"] = {
+									129412, -- Yazma
+								},
+							}),
+						},
+					}),
+				},
+				["lvl"] = 98,
+				["maps"] = {
+					934, -- Atal'Dazar
+					935, -- Sacrificial Pits
+				},
+			}),
+		},
+		["tierID"] = 8,
+	},
+};
