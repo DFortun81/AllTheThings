2452a3a2a222efd270c64c6710550f181766d22b
Lucetia-Detheroc <aceso2896@gmail.com>
2018-07-27 15:23:22 -0700

    - Harvested more BfA items - Kings' Rest Dungeon Migrated - Uldir migrated, pushing for Crieve.  Currently broken in-game.  See chat.

---
+++ contrib/Parser/DATAS/1 - Dungeons Raids/8 - Battle For Azeroth/1 - Raids/1 Uldir.lua
@@ -0,0 +1,402 @@
+-----------------------------------------------------
+--   D U N G E O N S  &  R A I D S  M O D U L E    --
+-----------------------------------------------------
+
+_.Instances = 
+{
+	{ -- Battle For Azeroth
+		["groups"] = {
+			inst(1031, { -- Uldir
+				["groups"] = {
+					d(17, {		-- LFR 
+						["groups"] = {
+							ach(12521, { -- Halls of Containment
+								e(2168, { -- Taloc
+									["groups"] = {
+										
+									},
+									["crs"] = {
+										137119, -- Taloc
+										138959, -- Coalesced Blood
+										138530, -- Volatile Droplet
+									},
+								}),
+								e(2167, { -- MOTHER
+									["groups"] = {
+										
+									},
+									["crs"] = {
+										140853, -- MOTHER
+										136315, -- Remnant of Corruption
+										143067, -- Resistant Bacterium
+										143065, -- Viral Contagion
+									},
+								}),
+								e(2169, { -- Zek'voz, Herald  of N'zoth
+									["groups"] = {
+										
+									},
+									["crs"] = {
+										134445, -- Zek'voz <Herald of N'zoth>
+										135824, -- Nerubian Voidweaver
+										134503, -- Silithid Warrior
+									},
+								}),
+							}),
+							ach(12522, { -- Crimson Descent
+								e(2166, { -- Vectis
+									["groups"] = {
+										
+									},
+									["crs"] = {
+										134442, -- Vectis
+									},
+								}),
+								e(2146, { -- Fetid Devourer
+									["groups"] = {
+										
+									},
+									["crs"] = {
+										133298, -- Fetid Devourer
+										133492, -- Corruption Corpuscle
+									},
+								}),
+								e(2195, { -- Zul, Reborn
+									["groups"] = {
+										
+									},
+									["crs"] = {
+										138967, -- Zul
+										139059, -- Bloodthirsty Crawg
+										139057, -- Nazmani Bloodhexer
+										139051, -- Nazmani Crusher
+									},
+								}),
+							}),
+							ach(12523, { -- Heart of Corruption
+								e(2194, { -- Mythrax the Unraveler
+									["groups"] = {
+										
+									},
+									["crs"] = {
+										136383, -- Mythrax the Unraveler
+									},
+								}),
+								e(2147, { -- G'huun
+									["groups"] = {
+										i(160693), -- Lancet of the Deft Hand
+										i(160694), -- Re-Origination Pulse Rifle
+										i(160699), -- Bararicade of Purifying Resolve
+										i(160732), -- Helm of the Defiled Laboratorium
+										i(160726), -- Amice of Corrupting Horror
+										i(160731), -- Spaulders of Coagulated Viscera
+										i(160728), -- Tunic of the Sanguine Deity
+										i(160727), -- Cord of Septic Envelopment
+										i(160733), -- Hematocyst Stompers
+										i(160729), -- Striders of the Putrescent Path
+										i(160654), -- Vanquisheed Tendril of G'huun
+									},
+									["crs"] = {
+										132998, -- G'huun
+										134590, -- Blightspreader Tendril
+										138531, -- Cyclopean Terror
+										138529, -- Dark Young
+									},
+								}),							
+							}),
+						},
+					}),
+					d(14, {		-- Normal
+						["groups"] = {
+							e(2168, { -- Taloc
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									137119, -- Taloc
+									138959, -- Coalesced Blood
+									138530, -- Volatile Droplet
+								},
+							}),
+							e(2167, { -- MOTHER
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									140853, -- MOTHER
+									136315, -- Remnant of Corruption
+									143067, -- Resistant Bacterium
+									143065, -- Viral Contagion
+								},
+							}),
+							e(2146, { -- Fetid Devourer
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									133298, -- Fetid Devourer
+									133492, -- Corruption Corpuscle
+								},
+							}),
+							e(2169, { -- Zek'voz, Herald  of N'zoth
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									134445, -- Zek'voz <Herald of N'zoth>
+									135824, -- Nerubian Voidweaver
+									134503, -- Silithid Warrior
+								},
+							}),
+							e(2166, { -- Vectis
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									134442, -- Vectis
+								},
+							}),
+							e(2195, { -- Zul, Reborn
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									138967, -- Zul
+									139059, -- Bloodthirsty Crawg
+									139057, -- Nazmani Bloodhexer
+									139051, -- Nazmani Crusher
+								},
+							}),
+							e(2194, { -- Mythrax the Unraveler
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									136383, -- Mythrax the Unraveler
+								},
+							}),
+							e(2147, { -- G'huun
+								["groups"] = {
+									i(160693), -- Lancet of the Deft Hand
+									i(160694), -- Re-Origination Pulse Rifle
+									i(160699), -- Bararicade of Purifying Resolve
+									i(160732), -- Helm of the Defiled Laboratorium
+									i(160726), -- Amice of Corrupting Horror
+									i(160731), -- Spaulders of Coagulated Viscera
+									i(160728), -- Tunic of the Sanguine Deity
+									i(160727), -- Cord of Septic Envelopment
+									i(160733), -- Hematocyst Stompers
+									i(160729), -- Striders of the Putrescent Path
+									i(160654), -- Vanquisheed Tendril of G'huun
+								},
+								["crs"] = {
+									132998, -- G'huun
+									134590, -- Blightspreader Tendril
+									138531, -- Cyclopean Terror
+									138529, -- Dark Young
+								},
+							}),	
+						},
+					}),
+					d(15, {		-- Heroic
+						["groups"] = {
+							e(2168, { -- Taloc
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									137119, -- Taloc
+									138959, -- Coalesced Blood
+									138530, -- Volatile Droplet
+								},
+							}),
+							e(2167, { -- MOTHER
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									140853, -- MOTHER
+									136315, -- Remnant of Corruption
+									143067, -- Resistant Bacterium
+									143065, -- Viral Contagion
+								},
+							}),
+							e(2146, { -- Fetid Devourer
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									133298, -- Fetid Devourer
+									133492, -- Corruption Corpuscle
+								},
+							}),
+							e(2169, { -- Zek'voz, Herald  of N'zoth
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									134445, -- Zek'voz <Herald of N'zoth>
+									135824, -- Nerubian Voidweaver
+									134503, -- Silithid Warrior
+								},
+							}),
+							e(2166, { -- Vectis
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									134442, -- Vectis
+								},
+							}),
+							e(2195, { -- Zul, Reborn
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									138967, -- Zul
+									139059, -- Bloodthirsty Crawg
+									139057, -- Nazmani Bloodhexer
+									139051, -- Nazmani Crusher
+								},
+							}),
+							e(2194, { -- Mythrax the Unraveler
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									136383, -- Mythrax the Unraveler
+								},
+							}),
+							e(2147, { -- G'huun
+								["groups"] = {
+									i(160693), -- Lancet of the Deft Hand
+									i(160694), -- Re-Origination Pulse Rifle
+									i(160699), -- Bararicade of Purifying Resolve
+									i(160732), -- Helm of the Defiled Laboratorium
+									i(160726), -- Amice of Corrupting Horror
+									i(160731), -- Spaulders of Coagulated Viscera
+									i(160728), -- Tunic of the Sanguine Deity
+									i(160727), -- Cord of Septic Envelopment
+									i(160733), -- Hematocyst Stompers
+									i(160729), -- Striders of the Putrescent Path
+									i(160654), -- Vanquisheed Tendril of G'huun
+								},
+								["crs"] = {
+									132998, -- G'huun
+									134590, -- Blightspreader Tendril
+									138531, -- Cyclopean Terror
+									138529, -- Dark Young
+								},
+							}),	
+						},
+					}),
+					d(16, {		-- Mythic
+						["groups"] = {
+							e(2168, { -- Taloc
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									137119, -- Taloc
+									138959, -- Coalesced Blood
+									138530, -- Volatile Droplet
+								},
+							}),
+							e(2167, { -- MOTHER
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									140853, -- MOTHER
+									136315, -- Remnant of Corruption
+									143067, -- Resistant Bacterium
+									143065, -- Viral Contagion
+								},
+							}),
+							e(2146, { -- Fetid Devourer
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									133298, -- Fetid Devourer
+									133492, -- Corruption Corpuscle
+								},
+							}),
+							e(2169, { -- Zek'voz, Herald  of N'zoth
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									134445, -- Zek'voz <Herald of N'zoth>
+									135824, -- Nerubian Voidweaver
+									134503, -- Silithid Warrior
+								},
+							}),
+							e(2166, { -- Vectis
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									134442, -- Vectis
+								},
+							}),
+							e(2195, { -- Zul, Reborn
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									138967, -- Zul
+									139059, -- Bloodthirsty Crawg
+									139057, -- Nazmani Bloodhexer
+									139051, -- Nazmani Crusher
+								},
+							}),
+							e(2194, { -- Mythrax the Unraveler
+								["groups"] = {
+									
+								},
+								["crs"] = {
+									136383, -- Mythrax the Unraveler
+								},
+							}),
+							e(2147, { -- G'huun
+								["groups"] = {
+									i(160693), -- Lancet of the Deft Hand
+									i(160694), -- Re-Origination Pulse Rifle
+									i(160699), -- Bararicade of Purifying Resolve
+									i(160732), -- Helm of the Defiled Laboratorium
+									i(160726), -- Amice of Corrupting Horror
+									i(160731), -- Spaulders of Coagulated Viscera
+									i(160728), -- Tunic of the Sanguine Deity
+									i(160727), -- Cord of Septic Envelopment
+									i(160733), -- Hematocyst Stompers
+									i(160729), -- Striders of the Putrescent Path
+									i(160654), -- Vanquisheed Tendril of G'huun
+								},
+								["crs"] = {
+									132998, -- G'huun
+									134590, -- Blightspreader Tendril
+									138531, -- Cyclopean Terror
+									138529, -- Dark Young
+								},
+							}),	
+						},
+					})
+				},
+				["isRaid"] = true,
+				["lvl"] = 120,
+				["mapID"] = 1148, -- Ruin's Descent
+				["maps"] = { 
+					1149, -- Hall of Sanitation
+					1150, -- Ring of Containment
+					1151, -- Archives of Eternity
+					1152, -- Plague Vault
+					1153, -- Gallery of Failures
+					1154, -- The Oblivion Door
+					1155, -- The Festering Core
+				},
+			}),
+		},					
+		["tierID"] = 8,
+	},	
+};
---
+++ contrib/Parser/DATAS/1 - Dungeons Raids/8 - Battle For Azeroth/2 - Dungeons/Kings Rest.lua
@@ -0,0 +1,88 @@
+---------------------------------------------------
+--          Z O N E S       M O D U L E          --
+---------------------------------------------------
+
+_.Instances =
+{
+	{
+		["groups"] = {
+			inst(1041, { -- Kings' Rest
+				["groups"] = {
+					d(23, { -- Mythic
+						["groups"] = {
+							e(2165, { -- The Golden Serpent
+								["groups"] = {
+									i(159137), -- Gilded Serpent's Tooth
+									i(159413), -- Gauntlets of the Avian Sentinel
+									i(159369), -- Belt of the Consecrateed Tomb
+									i(159313), -- Breechees of the Sacred Hall
+									i(159234), -- Down-Lined Breeches
+									i(159412), -- Auric Puddle Stompers
+									i(159304), -- Goldfeather Boots
+									i(159617), -- Lustrous Golden Plumage
+								},
+								["crs"] = {
+									135322, -- The Golden Serpent
+								},
+							}),
+							e(2171, { -- Mchimba the Embalmer
+								["groups"] = {
+									i(159642), -- Royal Purifier's Spaade
+									i(159667), -- Vessel of Last Rites
+									i(159409), -- Embalmer's Steadying Bracers
+									i(159312), -- Desiccator's Blessed Gloves
+									i(160213), -- Sepulchral Construct's Gloves
+									i(159459), -- Ritual Binder's Ring
+									i(159618), -- Mchimba's Ritual Bandages
+								},
+								["crs"] = {
+									134993, -- Mchimba thee Embalmer
+								},
+							}),
+							e(2170, { -- The Council of Tribes
+								["groups"] = {
+									i(160216), -- Crackling Jade Kilij
+									i(159136), -- Jeweled Dagger of Subjugation
+									i(159643), -- Crossbow of Forgotten Majesty
+									i(159288), -- Cloak of the Restless Tribes
+									i(159300), -- Kula's Butchering Wristwraps
+									i(159418), -- Girdle  of Pestilent Purification
+									i(159371), -- Boots of the Headlong Conqueror
+									i(159243), -- Sandals of Wise Voodoo
+								},
+								["crs"] = {
+									135470, -- Aka'ali  the Conqueror
+									135475, -- Kula the Butcher
+									135472, -- Zanazal the Wise
+								},
+							}),
+							e(2172, { -- Dazar, The First King
+								["groups"] = {
+									i(159921), -- Mummified Raptor Skull
+									i(159644), -- Geti'ikku, Cut of Death
+									i(159645), -- Headcracker of Supplication
+									i(159236), -- Hheaddress of the First Empire
+									i(159422), -- Helm of the Raptor King
+									i(158344), -- Mantle of Ceremonial Ascension
+									i(159423), -- Pauldron's of the Great Unifier
+									i(159368), -- Spaulders of Prime Emperor
+									i(158355), -- Loa-Blessed Chestguard
+									i(159303), -- Vest of Reeverent Adoration
+									i(159301), -- Primal Dinomancer's Belt
+								},
+								["crs"] = {
+									136160, -- King Dazar <The First>
+								},
+							}),
+						},
+					}),
+				},
+				["lvl"] = 98,
+				["maps"] = {
+					1004, -- Kings' Rest
+				},
+			}),
+		},
+		["tierID"] = 8,
+	},
+};
