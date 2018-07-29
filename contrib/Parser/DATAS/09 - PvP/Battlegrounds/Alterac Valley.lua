682b3d1a2d3e22adaa630fcc7c04f7e52776365d
Lucetia-Detheroc <aceso2896@gmail.com>
2018-07-28 22:15:57 -0700

    - Added Cold Forged Blade (Blizz lists in UI, but been gone since 1.6)

---
+++ contrib/Parser/DATAS/09 - PvP/Battlegrounds/Alterac Valley.lua
@@ -0,0 +1,29 @@
+-----------------------------------------------
+--     P L A Y E R   V S   P L A Y E R       --
+-----------------------------------------------
+_.PVP =
+{
+	n(-304, {	-- Battlegrounds
+		m(91 , {	-- Alterac Valley
+			["groups"] = {
+				n(13419, { -- Ivus the Forest Lord
+					["groups"] = {
+						un(2, i(19110, { -- Cold Forged Blade
+							["description"] = "This item was removed in Patch 1.6",
+						})),
+					},
+				}),
+				n(13256, { -- Lokholar the Ice Lord
+					["groups"] = {
+						un(2, i(19110, { -- Cold Forged Blade
+							["description"] = "This item was removed in Patch 1.6",
+						})),
+					},
+				}),
+			},
+			["maps"] = { 27 },	-- Hillsbrad Foothills
+			["achievementID"] = 218, -- Alterac Valley Victory
+			["description"] = "Arathi Basin is a 15v15 battleground located in Arathi Highlands. Players fight over five bases (Stables, Mines, Blacksmith, Lumber Mill, Farm) which reward teams with resources. The more bases a team controls, the faster they accumulate resources. A team with all five bases captured will gain 30 resources per second.\n\nThe game is won when one team reaches 1,600 resources. The most common way of winning is to hold three bases and defend, with the Blacksmith being a key base.",
+		}),
+	}),
+};
