534a778c089c91c157b5eed08061b06f8d43d073
DrOxlotus <alex.metz1024@gmail.com>
2018-08-09 15:29:12 -0500

    Organized Archaeology into sections. Implemented BfA sections (but disabled... for now.)

--- contrib/Parser/DATAS/11 - Professions/Archaeology.lua
+++ contrib/Parser/DATAS/11 - Professions/Archaeology.lua
@@ -1,68 +1,170 @@
 -- Archaeology - Skill ID 794 / Spell ID 158762
 profession(794, { -- Archaeology
-	n(-160, { -- Mounts
-		sp(90619, { i(60954) }),	-- Fossilized Raptor
-		sp(92148, { i(64883) }),	-- Scepter of Azj'Aqir
-	}),
-	n(-25, { -- Pet Battles
-		sp(172460, { i(117354) }),	-- Ancient Nest Guardian
-		sp(90521, { i(64372) }),	-- Clockwork Gnome
-		sp(92137, { i(60847) }),	-- Crawling Claw
-		sp(89693, { i(60955) }),	-- Fossilized Hatchling
-		sp(172466, { i(117380) }),	-- Frostwolf Ghostpup
-		sp(98582, { i(69821) }),	-- Pterrordax Hatchling
-		sp(98588, { i(69824) }),	-- Voodoo Figurine
-	}),
-	n(-17, { -- Quests
-		q(41157, {					-- Corrupted Studies
-			i(136362, {				-- Ancient War Remnants
-				i(131724)			-- Crystalline Eye of Undravius
-			})
-		}),
-		q(41163, {					-- The Apocalypse Bringer
-			i(136922)				-- Wyrmy Tunkins
-		}),
-		q(41188, {					-- Neltharion's Lair: Misdirected
-			i(131733)				-- Spear of Rethu
-		}),
-		q(41193, {					-- Laying to Rest
-			i(131734)				-- Spirit of Eche'ro
-		}),
-	}),
-	n(-161, { -- Toys
-		sp(113993, { i(89614) }),	-- Anatomical Dummy
-		sp(98560, { i(69776) }),	-- Ancient Amber
-		sp(90983, { i(64456) }),	-- Arrival of the Naaru
-		sp(91214, { i(64481) }),	-- Blessing of the Old God
-		sp(91761, { i(64646) }),	-- Bones of Transformation
-		sp(90553, { i(64373) }),	-- Chalice of the Mountain Kings
-		sp(90493, { i(64361) }),	-- Druid and Priest Statue Set
-		sp(98556, { i(69777) }),	-- Haunted War Drum
-		sp(90464, { i(64358) }),	-- Highborne Soul Mirror
-		sp(90614, { i(64383) }),	-- Kaldorei Wind Chimes
-		sp(92145, { i(64881) }),	-- Pendant of the Scarab Storm
-		sp(91215, { i(64482) }),	-- Puzzle Box of Yogg-Saron
-		sp(91226, { i(64488) }),	-- The Innkeeper's Daughter
-		sp(98569, { i(69775) }),	-- Vrykul Drinking Horn
-		sp(91773, { i(64651) }),	-- Wisp Amulet
-	}),
-	n(-163, { -- Armor
-		sp(90843, { i(64644) }),	-- Headdress of the First Shaman
-		sp(172459, { i(116985) }),	-- Headdress of the First Shaman
-		sp(90616, { i(64643) }),	-- Queen Azshara's Dressing Gown
-	}),
-	n(-319, { -- Weapons	
-		sp(168331, { i(117382) }),	-- Beakbreaker of Terokk
-		sp(98533, { i(69764) }),	-- Extinct Turtle Shell
-		sp(139786, { i(95391) }),	-- Mantid Sky Reaver
-		sp(90997, { i(64460) }),	-- Nifflevar Bearded Axe
-		sp(92163, { i(64885) }),	-- Scimitar of the Sirocco
-		sp(139787, { i(95392) }),	-- Sonic Pulse Generator
-		sp(113981, { i(89685) }),	-- Spear of Xuen
-		sp(92139, { i(64880) }),	-- Staff of Ammunae
-		sp(91227, { i(64489) }),	-- Staff of Sorcerer-Thane Thaurissan
-		sp(113980, { i(89684) }),	-- Umbrella of Chi-Ji
-		sp(168320, { i(117384) }),	-- Warmaul of the Warmaul Chieftain
-		sp(90608, { i(64377) }),	-- Zin'rokh, Destroyer of Worlds
-	}),
+  n(-583, { -- Arakkoa
+    n(-25, { -- Pet Battles
+      sp(172460, { i(117354) }), -- Ancient Nest Guardian
+    }),
+    n(-319, { -- Weapons
+      sp(168331, { i(117382) }), -- Beakbreaker of Terokk
+    }),
+  }),
+  n(-584, { -- Demonic
+    n(-17, { -- Quests
+      q(41157, { -- Corrupted Studies
+        i(136362, { -- Ancient War Remnants
+          i(131724), -- Crystalline Eye of Undravius
+        })
+      }),
+      q(41163, { -- The Apocalypse Bringer
+        i(136922), -- Wyrmy Tunkins
+      }),
+    }),
+  }),
+  n(-585, { -- Draenei
+    n(-161, { -- Toys
+      sp(90983, { i(64456) }), -- Arrival of the Naaru
+    }),
+  }),
+  n(-586, { -- Draenor Clans
+    n(-318, { -- Armor
+      sp(172459, { i(116985) }), -- Headdress of the First Shaman
+    }),
+    n(-25, { -- Pet Battles
+      sp(172466, { i(117380) }), -- Frostwolf Ghostpup
+    }),
+  }),
+  --[[ NOT YET IMPLEMENTED: Battle for Azeroth
+  n(-587, { -- Drustvar
+    n(-161, { -- Toys
+      sp(273852, { i(160751) }), -- Dance of the Dead
+    }),
+  }),
+  ]]--
+  n(-588, { -- Dwarf
+    n(-25, { -- Pet Battles
+      sp(90521, { i(64372) }), -- Clockwork Gnome
+    }),
+    n(-161, { -- Toys
+      sp(90553, { i(64373) }), -- Chalice of the Mountain Kings
+      sp(91226, { i(64488) }), -- The Innkeeper's Daughter
+    }),
+    n(-319, { -- Weapons
+      sp(91227, { i(64489) }), -- Staff of Sorcerer-Thane Thaurissan
+    }),
+  }),
+  n(-589, { -- Fossil
+    n(-318, { -- Armor
+      sp(98533, { i(69764) }), -- Extinct Turtle Shell
+    }),
+    n(-160, { -- Mounts
+      sp(90619, { i(60954) }), -- Fossilized Raptor
+    }),
+    n(-25, { -- Pet Battles
+      sp(89693, { i(60955) }), -- Fossilized Hatchling
+      sp(98582, { i(69821) }), -- Pterrordax Hatchling
+    }),
+    n(-161, { -- Toys
+      sp(98560, { i(69776) }), -- Ancient Amber
+    }),
+  }),
+  --n(-590, {}), -- Highborne (No collectible rewards. :/)
+  n(-591, { -- Highmountain Tauren
+    n(-17, { -- Quests
+      q(41193, { -- Laying to Rest
+        i(131734), -- Spirit of Eche'ro
+      }),
+      q(41188, { -- Neltharion's Lair: Misdirected
+        i(131733), -- Spear of Rethu
+      }),
+    }),
+  }),
+  n(-592, { -- Mantid
+    n(-319, { -- Weapons
+      sp(139786, { i(95391) }), -- Mantid Sky Reaver
+      sp(139787, { i(95392) }),	-- Sonic Pulse Generator
+    }),
+  }),
+  n(-593, { -- Mogu
+    n(-161, { -- Toys
+      sp(113993, { i(89614) }),	-- Anatomical Dummy
+    }),
+  }),
+  n(-594, { -- Nerubian
+    n(-161, { -- Toys
+      sp(91214, { i(64481) }), -- Blessing of the Old God
+      sp(91215, { i(64482) }), -- Puzzle Box of Yogg-Saron
+    }),
+  }),
+  n(-595, { -- Night Elf
+    n(-318, { -- Armor
+      sp(90616, { i(64643) }), -- Queen Azshara's Dressing Gown
+    }),
+    n(-161, { -- Toys
+      sp(91761, { i(64646) }), -- Bones of Transformation
+      sp(90493, { i(64361) }), -- Druid and Priest Statue Set
+      sp(90464, { i(64358) }), -- Highborne Soul Mirror
+      sp(90614, { i(64383) }), -- Kaldorei Wind Chimes
+      sp(91773, { i(64651) }), -- Wisp Amulet
+    }),
+  }),
+  n(-596, { -- Ogre
+    n(-319, { -- Weapon
+      sp(168320, { i(117384) }), -- Warmaul of the Warmaul Chieftain
+    }),
+  }),
+  n(-597, { -- Orc
+    n(-318, { -- Armor
+      sp(90843, { i(64644) }), -- Headdress of the First Shaman
+    }),
+  }),
+  n(-598, { -- Pandaren
+    n(-319, { -- Weapons
+      sp(113981, { i(89685) }), -- Spear of Xuen
+      sp(113980, { i(89684) }),	-- Umbrella of Chi-Ji
+    }),
+  }),
+  n(-599, { -- Tol'vir
+    n(-160, { -- Mounts
+      sp(92148, { i(64883) }), -- Scepter of Azj'Aqir
+    }),
+    n(-25, { -- Pet Battles
+      sp(92137, { i(60847) }), -- Crawling Claw
+    }),
+    n(-161, { -- Toys
+      sp(92145, { i(64881) }), -- Pendant of the Scarab Storm
+    }),
+    n(-319, { -- Weapons
+      sp(92163, { i(64885) }), -- Scimitar of the Sirocco
+      sp(92139, { i(64880) }), -- Staff of Ammunae
+    }),
+  }),
+  n(-600, { -- Troll
+    n(-25, { -- Pet Battles
+      sp(98588, { i(69824) }), -- Voodoo Figurine
+    }),
+    n(-161, { -- Toys
+      sp(98556, { i(69777) }), -- Haunted War Drum
+    }),
+    n(-319, { -- Weapons
+      sp(90608, { i(64377) }), -- Zin'rokh, Destroyer of Worlds
+    }),
+  }),
+  n(-601, { -- Vrykul
+    n(-161, { -- Toys
+      sp(98569, { i(69775) }), -- Vrykul Drinking Horn
+    }),
+    n(-319, { -- Weapons
+      sp(90997, { i(64460) }), -- Nifflevar Bearded Axe
+    }),
+  }),
+  --[[ NOT YET IMPLEMENTED: Battle for Azeroth
+  n(-602, { -- Zandalari
+    n(-25, { -- Pet Battles
+      sp(273817, { i(161080) }), -- Intact Direhorn Egg
+    }),
+    n(-161, { -- Toys
+      sp(273815, { i(160740) }), -- Croak Crock
+    }),
+  }),
+  ]]--
 });
