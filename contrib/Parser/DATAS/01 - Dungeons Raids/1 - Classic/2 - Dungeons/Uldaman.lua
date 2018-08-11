46be13562e42eaf926b609e53458b31ea2d974f6
DrOxlotus <alex.metz1024@gmail.com>
2018-08-10 20:48:54 -0500

    Corrected an issue that caused Baelog and Eric 'The Swift' to be marked for death... for collection purposes... when playing on the Alliance faction.

--- contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/2 - Dungeons/Uldaman.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/2 - Dungeons/Uldaman.lua
@@ -108,7 +108,6 @@ _.Instances =
 						}),
 						i(9386, {	-- Excavator's Brand
 							["crs"] = {
-								6907,	-- Eric "The Swift"
 								7012,	-- Earthen Sculptor
 								7030,	-- Shadowforge Geologist
 								4847,	-- Shadowforge Relic Hunter
@@ -210,16 +209,18 @@ _.Instances =
 							},
 						}),
 					}),
-					cr( 6906, h(e(468, {	-- The Lost Dwarves (Baelog)
-						i(  9401),	-- Nordic Longshank
-						i(  9400),	-- Baelog's Shortbow
-						i(  9403),	-- Battered Viking Shield
-						i(  9404),	-- Olaf's All Purpose Shield
-						i(  9394),	-- Horned Viking Helmet
-						i(151396),	-- Erik's High-Performance Armbands
-						i(132734),	-- Viking Chain Boots
-						i(  9398),	-- Worn Running Boots
-					}))),
+					cr(6906, e(468, {	-- The Lost Dwarves (Baelog)
+            ["g"] = bubbleDown({["races"] = HORDE_ONLY}, {
+              i(9400), -- Baelog's Shortbow
+              i(9403), -- Battered Viking Shield
+              i(151396), -- Erik's High-Performance Armbands
+              i(9394),	-- Horned Viking Helmet
+              i(9401),	-- Nordic Longshank
+              i(9404),	-- Olaf's All Purpose Shield
+              i(132734),	-- Viking Chain Boots
+              i(9398),	-- Worn Running Boots
+            }),
+					})),
 					cr( 6910, e(  467, {	-- Revelosh
 						i(132736),	-- Revelosh's Pauldrons
 						i(  9389),	-- Revelosh's Spaulders
