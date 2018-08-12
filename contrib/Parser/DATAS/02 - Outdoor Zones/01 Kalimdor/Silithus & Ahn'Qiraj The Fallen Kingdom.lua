d1a647c4f719386e31cbf5fda915814a513b25ab
Pr3vention <Pr3vention@gmail.com>
2018-08-11 19:46:03 -0500

    Moved quests out of the Ruins of Ahn'Qiraj raid file since the quests exist outside the raid
    - Updated their quest formats

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Silithus & Ahn'Qiraj The Fallen Kingdom.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Silithus & Ahn'Qiraj The Fallen Kingdom.lua
@@ -1031,6 +1031,197 @@ END OF MICRO-HOLIDAY --]]
 							desc(p(512), "Can be found near the gates of Ahn'Qiraj, or in the un-instanced zone south of Silithus."),-- Scarab Hatchling
 							p(511), 	-- Sidewinder
 						}),
+						n(-17, {	-- Quests
+							q(8700, {	-- Band of Unending Life
+								["groups"] = {
+									i(21408),	-- Band of Unending Life
+								},
+								["qg"] = 15498,	-- Windcaller Yessendra
+								["classes"] = { 11 },	-- Druid
+							}),
+							q(8699, {	-- Band of Vaulted Secrets
+								["groups"] = {
+									i(21414),	-- Band of Vaulted Secrets
+								},
+								["qg"] = 15498,	-- Windcaller Yessendra
+								["classes"] = { 8 },	-- Mage
+							}),
+							q(8701, {	-- Band of Veiled Shadows
+								["groups"] = {
+									i(21405),	-- Band of Veiled Shadows
+								},
+								["qg"] = 15498,	-- Windcaller Yessendra
+								["classes"] = { 4 },	-- Rogue
+							}),
+							q(8711, {	-- Blade of Eternal Justice
+								["groups"] = {
+									i(21395),	-- Blade of Eternal Justice
+								},
+								["qg"] = 15499,	-- Warden Haro
+								["classes"] = { 2 },	-- Paladin
+							}),
+							q(8707, {	-- Blade of Vaulted Secrets
+								["groups"] = {
+									i(21413),	-- Blade of Vaulted Secrets
+								},
+								["qg"] = 15499,	-- Warden Haro
+								["classes"] = { 8 },	-- Mage
+							}),
+							q(8695, {	-- Cape of Eternal Justice
+								["groups"] = {
+									i(21397),	-- Cape of Eternal Justice
+								},
+								["qg"] = 15500,	-- Keyl Swiftclaw
+								["classes"] = { 2 },	-- Paladin
+							}),
+							q(8690, {	-- Cloak kof the Gathering Storm
+								["groups"] = {
+									i(21400),	-- Cloak of the Gathering Storm
+								},
+								["qg"] = 15500,	-- Keyl Swiftclaw
+								["classes"] = { 7 },	-- Shaman
+							}),
+							q(8696, {	-- Cloak of the Unseen Path
+								["groups"] = {
+									i(21403),	-- Cloak of the Unseen Path
+								},
+								["qg"] = 15500,	-- Keyl Swiftclaw
+								["classes"] = { 3 },	-- Huntter
+							}),
+							q(8692, {	-- Cloak of Unending Life
+								["groups"] = {
+									i(21409),	-- Cloak of Unending Life
+								},
+								["qg"] = 15500,	-- Keyl Swiftclaw
+								["classes"] = { 11 },	-- Druid
+							}),
+							q(8693, {	-- Cloak of Veiled Shadows
+								["groups"] = {
+									i(21406),	-- Cloak of Veiled Shadows
+								},
+								["qg"] = 15500,	-- Keyl Swiftclaw
+								["classes"] = { 4 },	-- Rogue
+							}),
+							q(8709, {	-- Dagger of Veiled Shadows
+								["groups"] = {
+									i(21404),	-- Dagger of Veiled Shadows
+								},
+								["qg"] = 15499,	-- Warden Haro
+								["classes"] = { 4 },	-- Rogue
+							}),
+							q(8557, {	-- Drape of Unyielding Strength
+								["groups"] = {
+									i(21394),	-- Drape of Unyielding Strength
+								},
+								["qg"] = 15500,	-- Keyl Swiftclaw
+								["classes"] = { 1 },	-- 
+							}),
+							q(8691, {	-- Drape of Vaulted Secrets
+								["groups"] = {
+									i(21415),	-- Drape of Vaulted Secrets
+								},
+								["qg"] = 15500,	-- Keyl Swiftclaw
+								["classes"] = { 8 },	-- Mage
+							}),
+							q(8705, {	-- Gavel of Infinite Wisdom
+								["groups"] = {
+									i(21410),	-- Gavel of Infinite Wisdom
+								},
+								["qg"] = 15499,	-- Warden Haro
+								["classes"] = { 5 },	-- Priest
+							}),
+							q(8706, {	-- Hammer of the Gathering Storm
+								["groups"] = {
+									i(21398),	-- Hammer of the Gathering Storm
+								},
+								["qg"] = 15499,	-- Warden Haro
+								["classes"] = { 7 },	-- Shaman
+							}),
+							q(8710, {	-- Kris of Unspoken Names
+								["groups"] = {
+									i(21416),	-- Kris of Unspoken Names
+								},
+								["qg"] = 15499,	-- Warden Haro
+								["classes"] = { 9 },	-- Warlock
+							}),
+							q(8708, {	-- Mace of Unending Life
+								["groups"] = {
+									i(21407),	-- Mace of Unending Life
+								},
+								["qg"] = 15499,	-- Warden Haro
+								["classes"] = { 11 },	-- Druid
+							}),
+							q(8703, {	-- Ring of Eternal Justice
+								["groups"] = {
+									i(21396),	-- Ring of Eternal Justice
+								},
+								["qg"] = 15498,	-- Windcaller Yessendra
+								["classes"] = { 2 },	-- Paladin
+							}),
+							q(8697, {	-- Ring of Infinite Wisdom
+								["groups"] = {
+									i(21411),	-- Ring of Infinite Wisdom
+								},
+								["qg"] = 15498,	-- Windcaller Yessendra
+								["classes"] = { 5 },	-- Priest
+							}),
+							q(8698, {	-- Ring of the Gathering Storm
+								["groups"] = {
+									i(21399),	-- Ring of the Gathering Storm
+								},
+								["qg"] = 15498,	-- Windcaller Yessendra
+								["classes"] = { 7 },	-- Shaman
+							}),
+							q(8702, {	-- Ring of Unspoken Names
+								["groups"] = {
+									i(21417),	-- Ring of Unspoken Names
+								},
+								["qg"] = 15498,	-- Windcaller Yessendra
+								["classes"] = { 9 },	-- Warlock
+							}),
+							q(8712, {	-- Scythe of the Unseen Path
+								["groups"] = {
+									i(21401),	-- Scythe of the Unseen Path
+								},
+								["qg"] = 15499,	--Warden Haro
+								["classes"] = { 3 },	-- Hunter
+							}),
+							q(8689, {	-- Shroud of Infinite Wisdom
+								["groups"] = {
+									i(21412),	-- 
+								},
+								["qg"] = 15500,	-- Keyl Swiftclaw
+								["classes"] = { 5 },	-- Priest
+							}),
+							q(8694, {	-- Shroud of Unspoken Names
+								["groups"] = {
+									i(21418),	-- Shroud of Unspoken Names
+								},
+								["qg"] = 15500,	-- Keyl Swiftclaw
+								["classes"] = { 9 },	-- Warlock
+							}),
+							q(8558, {	-- Sickle of Unyielding Strength
+								["groups"] = {
+									i(21392),	-- Sickle of Unyielding Strength
+								},
+								["qg"] = 15499,	-- Warden Haro
+								["classes"] = { 1 },	-- Warrior
+							}),
+							q(8704, {	-- Signet of the Unseen path
+								["groups"] = {
+									i(21402),	-- Signet of the unseen Path
+								},
+								["qg"] = 15498,	-- Windcaller Yessendra
+								["classes"] = { 3 },	-- Hunter
+							}),
+							q(8556, {	-- Signet of Unyielding Strength
+								["groups"] = {
+									i(21393),	-- Signet of Unyielding Strength
+								},
+								["qg"] = 15498,	-- Windcaller Yessendra
+								["classes"] = { 1 },	-- Warrior
+							}),
+						}),
 						n(-16, { 	-- Rares
 							n(50747, { 	-- Tix
 								dr(	14	, i(	10377	)), --	Commander's Vambraces
