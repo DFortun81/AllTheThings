c5a43848b19f89cfb8c2719bcdff3e9e792e59b2
Lucetia-Detheroc <aceso2896@gmail.com>
2018-08-13 12:51:00 -0700

    - Vol'dun quests done dirtyly but work for now

--- contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/Vol'dun.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/Vol'dun.lua
@@ -69,8 +69,377 @@ _.Zones =
 					},
 				}),
 				n(-17, { -- Quests
+				---- TODO THESE QUESTS WILL BE SOURCED / ORGANIZED.  I HAVE THE DATA ALREADY PLEASE DO NOT ALTER.  THANKS!!!
 					["groups"] = {
+						q(47320),	-- A Balm to Calm
+						q(50817),	-- A Charming Tail
+						q(48322),	-- A Goldtusk Greeting
+						q(50818),	-- A Lost Flute
+						q(50755),	-- A Meal for Birds
+						q(49334, {	-- A Powerful Prisoner
+							i(155457), -- Devoted Defender's Cloak
+							i(155454),
+						}),
+						q(47860),	-- A Rude Awakening
+						q(48327),	-- A Strange Delivery
+						q(47322, {	-- Aided Escape
+							i(155388),
+							i(155389),
+							i(155390),
+							i(155391),
+							i(155409),
+							i(155410),
+							i(155411),
+							i(155412),
+						}),
+						q(48715),	-- Akunda Awaits
+						q(48994),	-- Akunda the Liar
+						q(50913),	-- Akunda's Blessing
+						q(47574),	-- All Webbed Up
+						q(49664),	-- Allies in Anarchy
+						q(48532),	-- Alpacas Gone Wild
+						q(49139),	-- An Army's Arsenal
+						q(51364, {	-- An Explosive Exit
+							i(161188),
+							i(155443),
+							i(161189),
+							i(161190),
+							i(161191),
+							i(161192),
+							i(161193),
+							i(161194),
+							i(161195),
+						}),
+						q(49331),	-- An Interesting Find
+						q(47671),	-- Ancient Lore
+						q(47672),	-- Ancient Lore
+						q(47327),	-- Answering Their Attacks
+						q(51357),	-- Armed and Ready
+						q(48847),	-- Arming the Tribe
+						q(50617),	-- Atul'Aman
+						q(50812),	-- Awakened Elements
+						q(51602),	-- Bandit Blades
+						q(48329),	-- Beaten But Not Broken
+						q(47581, {	-- Blessing of Kimbul
+							i(155450),
+						}),
+						q(50641),	-- Break Their Ranks
+						q(49333),	-- Building Our Arsenal
+						q(50771),	-- Calldown: Cleaner
+						q(51775),	-- Camp Lastwind
+						q(48845),	-- Cannon Cremation
+						q(49138),	-- Captain Gulnaku's Treasure
+						q(51810),	-- Captain Hartford
+						q(51991),	-- Charging the Batteries
+						q(48887),	-- Cleanse the Mind
+						q(47874),	-- Clearing the Fog
+						q(47943),	-- Crab Trapping
+						q(49261),	-- Crabby Crew Stew
+						q(50746),	-- Crater Conquered
+						q(48321, {	-- Creative Marketing
+							i(160021),
+							i(160022),
+							i(160023),
+							i(160024),
+							i(160009),
+							i(160010),
+							i(160011),
+							i(160012),
+						}),
+						q(48314),	-- Creeping Death
+						q(51145),	-- Curse of Jani
+						q(47870, {	-- Dead Men Tell No Tales
+							i(161168),
+							i(161169),
+							i(161170),
+							i(161171),
+							i(160000),
+							i(160001),
+							i(160002),
+							i(160003),
+						}),
+						q(50702),	-- Defeat Jakra'zet
+						q(48995),	-- Defeat Xenxuju
+						q(49141),	-- Diplomacy and Dominance
+						q(47501),	-- Dirty Work for Dirty Drinks
+						q(47313),	-- Discreet Discussions
+						q(50748),	-- Don't Drop It... Yet
+						q(49676),	-- Dressed for Battle
+						q(50770),	-- Effective Antivenom
+						q(48996),	-- Ending the Madness
+						q(51062),	-- Escaping Zem'lan
+						q(48872),	-- Expedite the Excavation
+						q(50596),	-- Exterminate the Vermin
+						q(49040),	-- Fond Farewells
+						q(49731),	-- Fond Farewells
+						q(49002, {	-- Forced Grounding
+							i(161166),
+							i(161167),
+						}),
+						q(50749),	-- Free Ride
+						q(51574),	-- Freshly Squeezed
+						q(47318),	-- Fruit of the Desert
+						q(49262),	-- Gang Bustin'
+						q(50775),	-- Get Us Some Beach
+						q(47503),	-- Gozda'kun the Slaver
+						q(48549),	-- Grozztok the Blackheart
+						q(51718),	-- Harvesting "Honey"
+						q(47570),	-- Hidden Motives
+						q(48315),	-- Hollow, Empty Eyes
+						q(48529),	-- Hungry Mouths To Feed
+						q(48530),	-- I Heard You Lost the Herd
+						q(51573),	-- I've Got Your Back
+						q(47939),	-- If the Key Fits...
+						q(49001),	-- Inconvenient Spirits
+						q(50745),	-- Infiltrating the Empire
+						q(52024),	-- Informing the Horde
+						q(50703),	-- Informing the Horde
+						q(52023),	-- Informing the Horde
+						q(50750),	-- Infuriating the Emperor
+						q(47315),	-- Into the Dunes
+						q(48888),	-- It Springs Eternal
+						q(47573),	-- Jungleweb Infestation
+						q(50979),	-- Just a Nip
+						q(50834),	-- Keep It Down!
+						q(47321),	-- Knickknack Takeback
+						q(48896),	-- Knowledge of the Past
+						q(48775),	-- Last Respects
+						q(48553),	-- Let it Flow
+						q(49668),	-- Light Up the Gulch
+						q(48846),	-- Liquid Motivation
+						q(48324),	-- Lost in Zem'lan
+						q(50536),	-- Magic Decoder Device
+						q(49666),	-- Make Them Fear Us
+						q(47578),	-- Mark of the Loa
+						q(51057),	-- Maroon 'em with Fire
+						q(49337),	-- Matters of the Heart
+						q(47497),	-- Meet the Goldtusk Gang
+						q(48988),	-- Memory Breach
+						q(50739),	-- Missing Business
+						q(51668, {	-- Mojambo
+							i(155424),
+							i(155419),
+							i(160029),
+							i(160030),
+							i(160031),
+							i(155441),
+							i(155429),
+							i(155442),
+							i(155420),
+						}),
+						q(47647),	-- Monsters of Zem'lan
+						q(50980, {	-- My Hungry Neighbor
+							i(160005),
+							i(160006),
+							i(160007),
+							i(160008),
+							i(160004),
+						}),
+						q(51056),	-- My Last Day Alive
+						q(48531),	-- Mystery Meat
+						q(48313),	-- Nature's Remedy
+						q(47928),	-- Offering for the Loa
+						q(48684),	-- On the Move
+						q(51060),	-- Our Share of the Plunder
+						q(51054),	-- Overdue Mutiny
+						q(51142),	-- Pests
+						q(49336),	-- Piecing Together the Past
+						q(49677),	-- Plans for Attack
+						q(48989),	-- Poisoned Intentions
+						q(50535),	-- Power of the Overseer
+						q(52129),	-- Power Problems
+						q(48993, {	-- Powerful Conductors
+							i(159979),
+							i(155451),
+						}),
+						q(47638),	-- Powerful Spirits
+						q(50590),	-- Preface the Pitch
+						q(49327),	-- Push Them Back!
+						q(51829),	-- Ranah's Wrench
+						q(48332),	-- Ranishu Are Resources
+						q(49665),	-- Ready to Riot
+						q(47325),	-- Reckless Vengeance
+						q(50752),	-- Relics of Sethraliss
+						q(48889),	-- Repairing the Past
+						q(48871),	-- Rescue the Relics
+						q(47564),	-- Restocking the Buffet
+						q(47319),	-- Restorative Venom
+						q(47498),	-- Rhan'ka's Lost Friend
+						q(50656),	-- Risky Rescue
+						q(48840),	-- Ruins-Level Marketing
+						q(47314),	-- Rumors of Exile
+						q(48992),	-- Sacred Remains
+						q(50751),	-- Sanctuary Under Siege
+						q(50901),	-- Saurid Surprise
+						q(48656),	-- Savage Saurolisks
+						q(47871),	-- Seafaring Necessities
+						q(47317),	-- Searching for Survivors
+						q(47716),	-- Searching the Ruins
+						q(47316),	-- Secrets in the Sand
+						q(50794),	-- Seeking Shelter
+						q(49672),	-- Servant of the Loa
+						q(47410),	-- Sethrak Sabotage
+						q(50951),	-- Sharing the Blessings
+						q(49005, {	-- Shattered and Broken
+							i(155455),
+							i(161268),
+							i(161270),
+							i(161272),
+							i(161274),
+						}),
+						q(48331),	-- Siphoning Souls
+						q(50687),	-- Skycaller Carcanet
+						q(50805),	-- Skycaller Shutdown
+						q(49335),	-- Skycaller Slaughter
+						q(48534),	-- Snarltooth's Last Laugh
+						q(48790),	-- Stolen Goods
+						q(48550, {	-- Stolen Satchels
+							i(155400),
+							i(155401),
+							i(155402),
+							i(155403),
+							i(161269),
+							i(161271),
+							i(161273),
+							i(161275),
+						}),
+						q(48307),	-- Strange Totems
+						q(50561, {	-- Sulthis' Stone
+							i(159984),
+							i(159985),
+							i(159986),
+							i(159987),
+							i(159988),
+							i(159989),
+							i(159990),
+							i(159991),
+						}),
+						q(49437),	-- Tattered Note
+						q(50904),	-- The Abandoned Passage
+						q(51773),	-- The Ashvane Threat
+						q(51717),	-- The Best Honey In Vol'dun
+						q(48320),	-- The Best Kill is Overkill
+						q(47873),	-- The Captain's Cache
+						q(48655),	-- The Chef's Apprentice
+						q(47580),	-- The Curse of Mepjila
+						q(51053),	-- The Day the Port Fell
+						q(47571),	-- The Elder's Wisdom
+						q(50550, {	-- The Fall of Emperor Korthek
+							i(155440),
+							i(155434),
+							i(160033),
+							i(160034),
+							i(160032),
+							i(155417),
+							i(155418),
+							i(115544),
+						}),
+						q(51061),	-- The First Time I Died
+						q(51059),	-- The Golden Isle
+						q(47502, {	-- The Great Cranium Caper
+							i(161176),
+							i(161177),
+							i(161178),
+							i(161179),
+							i(161180),
+						}),
+						q(47499, {	-- The Grinning Idols
+							i(155456),
+							i(159976),
+						}),
+						q(49340),	-- The Keepers' Keys
+						q(48990),	-- The Leader Calls
+						q(49667, {	-- The Little Ones
+							i(160017),
+							i(160018),
+							i(160019),
+							i(160020),
+							i(159992),
+							i(159993),
+							i(159994),
+							i(159995),
+						}),
+						q(49227),	-- The Master Key
+						q(49662),	-- The Missing Key
+						q(48895),	-- The Perfect Offering
+						q(47965),	-- The Ruined Temple
+						q(50539),	-- The Secrets of Zul'Ahjin
+						q(48554, {	-- The Source of the Problem
+							i(155392),
+i(155393),
+i(155394),
+i(155395),
+i(155448),
+i(159975),
+}),
+						q(48335),	-- The Strongest Rope in Vol'dun
+						q(51772),	-- The Tortaka Tribe
+						q(47959),	-- The Warguard's Trail
+						q(51055),	-- The Yard Arm of the Law
+						q(47577),	-- They Came From The Sea
+						q(48657, {	-- They Might Be Delicious
+						i(159978),
+i(159996),
+i(159997),
+i(159998),
+i(159999),
+}),
+						q(48334, {	-- They've Got Golems
+							i(161181),
+							i(159977),
+						}),
+						q(48326, {	-- This Be Mutiny
+						i(161172),
+i(161173),
+i(161174),
+i(161175),
+i(160013),
+i(160014),
+i(160015),
+i(160016),
+
+}),
+						q(48850),	-- Tongo
+						q(48894),	-- Trial of Truth
+						q(50328),	-- Unconventional Aromatics
+						q(49669, {	-- Unleash the Beasts
+							i(161182),
+						}),
+						q(47324),	-- Unlikely Allies
+						q(50757),	-- Untame Slaughter
+						q(48987),	-- Valley of Sorrows
+						q(49003),	-- Vengeance From Above
+						q(48991),	-- Vile Infestation
+						q(48533),	-- Vol'duni Fried Chicken
+						q(51164),	-- WANTED: Cobra Excursion Participants
+						q(51165),	-- WANTED: Sandscout Vesarik
+						q(51162),	-- WANTED: Taz'raka the Traitor
+						q(51161),	-- WANTED: Za'roco
+						q(48585),	-- Wasteland Survivor
+						q(48838),	-- Water Reclamation
+						q(48555),	-- We Can Salvage the Seeds
+						q(48551),	-- Wither Without Water
+						q(47576, {	-- Wrath of the Tiger
+							i(161183),
+i(161184),
+i(161185),
+i(161186),
+i(161187),
+}),
+						q(48330, {	-- Zandalari Treasure Trove
+							i(155445),
+i(155431),
+i(155423),
+i(155421),
+i(155438),
+i(155427),
+i(155432),
+i(160028),
+i(155433),
+i(155430),
+}),
 					},
+					["races"] = HORDE_ONLY,
 				}),
 				n(-16, { -- Rares
 					-- Note: While these are all technically Achievement Criteria we don't want to use that in the infomration on whether you have killed it or not.  These is due to once you killing it on one character it would auto-complete your alts.
