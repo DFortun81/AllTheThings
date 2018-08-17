0abdda70e2d0f3ef6818f911deef3d52c1a6572e
Lucetia-Detheroc <aceso2896@gmail.com>
2018-08-17 10:33:13 -0700

    Fixed Underrot

--- contrib/Parser/DATAS/01 - Dungeons Raids/8 - Battle For Azeroth/2 - Dungeons/The Underrot.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/8 - Battle For Azeroth/2 - Dungeons/The Underrot.lua
@@ -17,11 +17,12 @@ _.Instances =
 						["groups"] = {
 							e(2157, { -- Elder Leaxa
 								["groups"] = bubbleDown({["bonusID"] = 4776, ["lvl"] = 110}, {
-									i(159324), -- Blood Elder's Bindings
-									i(159402), -- Waistguard of Sanguine Fervor
-									i(159443), -- Legplates of Profane Sacrifice
-									i(159463), -- Loop of Pulsing Veins
-									i(159624), -- Rotcrusted Voodoo Doll
+									i(159652),	-- Leaxa's Thought-Piercer
+									i(159324),	-- Blood Elder's Bindings
+									i(159402),	-- Waistguard of Sanguine Fervor
+									i(159443),	-- Legplates of Profane Sacrifice
+									i(159463),	-- Loop of Pulsing Veins
+									i(159624),	-- Rotcrusted Voodoo Doll
 								}),
 								["crs"] = {
 									131318, -- Elder Leaxa <Voice of G'huun>
@@ -29,16 +30,16 @@ _.Instances =
 							}),
 							e(2131,  { -- Cragmaw the Infested
 								["groups"] = bubbleDown({["bonusID"] = 4776, ["lvl"] = 110}, {
-									i(159653), -- Bile-Stained Crawg Tusks
-									i(159134), -- Heart-Piercing Stalacite
-									i(159433), -- Phosphorescent Armplates
-									i(159275), -- Wristwraps of Twined Morels
-									i(159382), -- Blood Tick Crushers
-									i(159344), -- Underrot Grotto Tenders
-									i(159325), -- Bloodfeaster Belt
-									i(159269), -- Darklight Legwarmers
-									i(159436), -- Fluorescent Flora Stompers
-									i(159396), -- Waders of the Infested
+									i(159653),	-- Bile-Stained Crawg Tusks
+									i(159134),	-- Heart-Piercing Stalacite
+									i(159433),	-- Phosphorescent Armplates
+									i(159275),	-- Wristwraps of Twined Morels
+									i(159382),	-- Blood Tick Crushers
+									i(159344),	-- Underrot Grotto Tenders
+									i(159325),	-- Bloodfeaster Belt
+									i(159269),	-- Darklight Legwarmers
+									i(159436),	-- Fluorescent Flora Stompers
+									i(159396),	-- Waders of the Infested
 								}),
 								["crs"] = {
 									131817, -- Cragmaw the Infested
@@ -46,14 +47,14 @@ _.Instances =
 							}),
 							e(2130, { -- Sporecaller Zancha
 								["groups"] =  bubbleDown({["bonusID"] = 4776, ["lvl"] = 110}, {
-									i(159654), -- Corruption Borne Headlopper
-									i(159665), -- Targee of the Ancient Warder
-									i(159292), -- Sporecaller's Shroud
-									i(159410), -- Zancha's Venerated Greatbelt
-									i(159384), -- Corpuscular Greaves
-									i(159338), -- Pustule Bearer's Pants
-									i(159270), -- Blood Warder's Moccasins
-									i(159262), -- Lingering Sporepods
+									i(159654),	-- Corruption Borne Headlopper
+									i(159665),	-- Targee of the Ancient Warder
+									i(159292),	-- Sporecaller's Shroud
+									i(159410),	-- Zancha's Venerated Greatbelt
+									i(159384),	-- Corpuscular Greaves
+									i(159338),	-- Pustule Bearer's Pants
+									i(159270),	-- Blood Warder's Moccasins
+									i(159262),	-- Lingering Sporepods
 								}),
 								["crs"] = {
 									131383, -- Sporecaller Zancha
@@ -61,18 +62,18 @@ _.Instances =
 							}),
 							e(2158, { -- Unbound Abomination
 								["groups"] =  bubbleDown({["bonusID"] = 4776, ["lvl"] = 110}, {
-									ach(12500), -- Underrot
-									i(159655), -- Vile Exasanguinator
-									i(159446), -- Greathelm of the Putrid Path
-									i(159381), -- Visage of Bloody  Horrors
-									i(159385), -- Amalgamated Abomination Spaulders
-									i(159267), -- Pauldrons of Vile Corruption
-									i(159323), -- Shoulders of the Sanguine Monstrosity
-									i(159241), -- Blood-Drenched Robes
-									i(159432), -- Breastplate of Arterial Protection
-									i(159330), -- Gore-Splattered Vest
-									i(159253), -- Gloves of Staunched Wounds
-									i(159625), -- Vial of Animated Blood
+									ach(12500),	-- Underrot
+									i(159655),	-- Vile Exasanguinator
+									i(159446),	-- Greathelm of the Putrid Path
+									i(159381),	-- Visage of Bloody  Horrors
+									i(159385),	-- Amalgamated Abomination Spaulders
+									i(159267),	-- Pauldrons of Vile Corruption
+									i(159323),	-- Shoulders of the Sanguine Monstrosity
+									i(159241),	-- Blood-Drenched Robes
+									i(159432),	-- Breastplate of Arterial Protection
+									i(159330),	-- Gore-Splattered Vest
+									i(159253),	-- Gloves of Staunched Wounds
+									i(159625),	-- Vial of Animated Blood
 								}),
 								["crs"] = {
 									133007, -- Unbound Abomination
@@ -84,11 +85,12 @@ _.Instances =
 						["groups"] = {
 							e(2157, { -- Elder Leaxa
 								["groups"] = {
-									i(159324), -- Blood Elder's Bindings
-									i(159402), -- Waistguard of Sanguine Fervor
-									i(159443), -- Legplates of Profane Sacrifice
-									i(159463), -- Loop of Pulsing Veins
-									i(159624), -- Rotcrusted Voodoo Doll
+									i(159652),	-- Leaxa's Thought-Piercer
+									i(159324),	-- Blood Elder's Bindings
+									i(159402),	-- Waistguard of Sanguine Fervor
+									i(159443),	-- Legplates of Profane Sacrifice
+									i(159463),	-- Loop of Pulsing Veins
+									i(159624),	-- Rotcrusted Voodoo Doll
 								},
 								["crs"] = {
 									131318, -- Elder Leaxa <Voice of G'huun>
@@ -96,16 +98,16 @@ _.Instances =
 							}),
 							e(2131, { -- Cragmaw the Infested
 								["groups"] = {
-									i(159653), -- Bile-Stained Crawg Tusks
-									i(159134), -- Heart-Piercing Stalacite
-									i(159433), -- Phosphorescent Armplates
-									i(159275), -- Wristwraps of Twined Morels
-									i(159382), -- Blood Tick Crushers
-									i(159344), -- Underrot Grotto Tenders
-									i(159325), -- Bloodfeaster Belt
-									i(159269), -- Darklight Legwarmers
-									i(159436), -- Fluorescent Flora Stompers
-									i(159396), -- Waders of the Infested
+									i(159653),	-- Bile-Stained Crawg Tusks
+									i(159134),	-- Heart-Piercing Stalacite
+									i(159433),	-- Phosphorescent Armplates
+									i(159275),	-- Wristwraps of Twined Morels
+									i(159382),	-- Blood Tick Crushers
+									i(159344),	-- Underrot Grotto Tenders
+									i(159325),	-- Bloodfeaster Belt
+									i(159269),	-- Darklight Legwarmers
+									i(159436),	-- Fluorescent Flora Stompers
+									i(159396),	-- Waders of the Infested
 								},
 								["crs"] = {
 									131817, -- Cragmaw the Infested
@@ -113,14 +115,14 @@ _.Instances =
 							}),
 							e(2130, { -- Sporecaller Zancha
 								["groups"] = {
-									i(159654), -- Corruption Borne Headlopper
-									i(159665), -- Targee of the Ancient Warder
-									i(159292), -- Sporecaller's Shroud
-									i(159410), -- Zancha's Venerated Greatbelt
-									i(159384), -- Corpuscular Greaves
-									i(159338), -- Pustule Bearer's Pants
-									i(159270), -- Blood Warder's Moccasins
-									i(159262), -- Lingering Sporepods
+									i(159654),	-- Corruption Borne Headlopper
+									i(159665),	-- Targee of the Ancient Warder
+									i(159292),	-- Sporecaller's Shroud
+									i(159410),	-- Zancha's Venerated Greatbelt
+									i(159384),	-- Corpuscular Greaves
+									i(159338),	-- Pustule Bearer's Pants
+									i(159270),	-- Blood Warder's Moccasins
+									i(159262),	-- Lingering Sporepods
 								},
 								["crs"] = {
 									131383, -- Sporecaller Zancha
@@ -128,18 +130,18 @@ _.Instances =
 							}),
 							e(2158, { -- Unbound Abomination
 								["groups"] = {
-									ach(12501), -- Heroic: Underrot
-									i(159655), -- Vile Exasanguinator
-									i(159446), -- Greathelm of the Putrid Path
-									i(159381), -- Visage of Bloody  Horrors
-									i(159385), -- Amalgamated Abomination Spaulders
-									i(159267), -- Pauldrons of Vile Corruption
-									i(159323), -- Shoulders of the Sanguine Monstrosity
-									i(159241), -- Blood-Drenched Robes
-									i(159432), -- Breastplate of Arterial Protection
-									i(159330), -- Gore-Splattered Vest
-									i(159253), -- Gloves of Staunched Wounds
-									i(159625), -- Vial of Animated Blood
+									ach(12501),	-- Heroic: Underrot
+									i(159655),	-- Vile Exasanguinator
+									i(159446),	-- Greathelm of the Putrid Path
+									i(159381),	-- Visage of Bloody  Horrors
+									i(159385),	-- Amalgamated Abomination Spaulders
+									i(159267),	-- Pauldrons of Vile Corruption
+									i(159323),	-- Shoulders of the Sanguine Monstrosity
+									i(159241),	-- Blood-Drenched Robes
+									i(159432),	-- Breastplate of Arterial Protection
+									i(159330),	-- Gore-Splattered Vest
+									i(159253),	-- Gloves of Staunched Wounds
+									i(159625),	-- Vial of Animated Blood
 								},
 								["crs"] = {
 									133007, -- Unbound Abomination
@@ -152,13 +154,13 @@ _.Instances =
 						["groups"] = {
 							e(2157, { -- Elder Leaxa
 								["groups"] = {
-									ach(12498), -- Taint Nobody Got Time For That
-									i(159324), -- Blood Elder's Bindings
-									i(159652), -- Leaxa's Thought-Piercer
-									i(159443), -- Legplates of Profane Sacrifice
-									i(159463), -- Loop of Pulsing Veins
-									i(159624), -- Rotcrusted Voodoo Doll
-									i(159402), -- Waistguard of Sanguine Fervor									
+									ach(12498),	-- Taint Nobody Got Time For That
+									i(159652),	-- Leaxa's Thought-Piercer
+									i(159324),	-- Blood Elder's Bindings
+									i(159443),	-- Legplates of Profane Sacrifice
+									i(159463),	-- Loop of Pulsing Veins
+									i(159624),	-- Rotcrusted Voodoo Doll
+									i(159402),	-- Waistguard of Sanguine Fervor									
 								},
 								["crs"] = {
 									131318, -- Elder Leaxa <Voice of G'huun>
@@ -166,16 +168,16 @@ _.Instances =
 							}),
 							e(2131, { -- Cragmaw the Infested
 								["groups"] = {
-									i(159653), -- Bile-Stained Crawg Tusks
-									i(159134), -- Heart-Piercing Stalacite
-									i(159433), -- Phosphorescent Armplates
-									i(159275), -- Wristwraps of Twined Morels
-									i(159382), -- Blood Tick Crushers
-									i(159344), -- Underrot Grotto Tenders
-									i(159325), -- Bloodfeaster Belt
-									i(159269), -- Darklight Legwarmers
-									i(159436), -- Fluorescent Flora Stompers
-									i(159396), -- Waders of the Infested
+									i(159653),	-- Bile-Stained Crawg Tusks
+									i(159134),	-- Heart-Piercing Stalacite
+									i(159433),	-- Phosphorescent Armplates
+									i(159275),	-- Wristwraps of Twined Morels
+									i(159382),	-- Blood Tick Crushers
+									i(159344),	-- Underrot Grotto Tenders
+									i(159325),	-- Bloodfeaster Belt
+									i(159269),	-- Darklight Legwarmers
+									i(159436),	-- Fluorescent Flora Stompers
+									i(159396),	-- Waders of the Infested
 								},
 								["crs"] = {
 									131817, -- Cragmaw the Infested
@@ -183,15 +185,15 @@ _.Instances =
 							}),
 							e(2130, { -- Sporecaller Zancha
 								["groups"] = {
-									ach(12549), -- Not a Fun Guy
-									i(159654), -- Corruption Borne Headlopper
-									i(159665), -- Targee of the Ancient Warder
-									i(159292), -- Sporecaller's Shroud
-									i(159410), -- Zancha's Venerated Greatbelt
-									i(159384), -- Corpuscular Greaves
-									i(159338), -- Pustule Bearer's Pants
-									i(159270), -- Blood Warder's Moccasins
-									i(159262), -- Lingering Sporepods
+									ach(12549),	-- Not a Fun Guy
+									i(159654),	-- Corruption Borne Headlopper
+									i(159665),	-- Targee of the Ancient Warder
+									i(159292),	-- Sporecaller's Shroud
+									i(159410),	-- Zancha's Venerated Greatbelt
+									i(159384),	-- Corpuscular Greaves
+									i(159338),	-- Pustule Bearer's Pants
+									i(159270),	-- Blood Warder's Moccasins
+									i(159262),	-- Lingering Sporepods
 								},
 								["crs"] = {
 									131383, -- Sporecaller Zancha
@@ -199,20 +201,20 @@ _.Instances =
 							}),
 							e(2158, { -- Unbound Abomination
 								["groups"] = {
-									ach(12502), -- Mythic: Underrot
-									ach(12499), -- Sporely Alive
-									i(160829), -- Underrot Crawg Harness
-									i(159655), -- Vile Exasanguinator
-									i(159446), -- Greathelm of the Putrid Path
-									i(159381), -- Visage of Bloody  Horrors
-									i(159385), -- Amalgamated Abomination Spaulders
-									i(159267), -- Pauldrons of Vile Corruption
-									i(159323), -- Shoulders of the Sanguine Monstrosity
-									i(159241), -- Blood-Drenched Robes
-									i(159432), -- Breastplate of Arterial Protection
-									i(159330), -- Gore-Splattered Vest
-									i(159253), -- Gloves of Staunched Wounds
-									i(159625), -- Vial of Animated Blood
+									ach(12502),	-- Mythic: Underrot
+									ach(12499),	-- Sporely Alive
+									i(160829),	-- Underrot Crawg Harness
+									i(159655),	-- Vile Exasanguinator
+									i(159446),	-- Greathelm of the Putrid Path
+									i(159381),	-- Visage of Bloody  Horrors
+									i(159385),	-- Amalgamated Abomination Spaulders
+									i(159267),	-- Pauldrons of Vile Corruption
+									i(159323),	-- Shoulders of the Sanguine Monstrosity
+									i(159241),	-- Blood-Drenched Robes
+									i(159432),	-- Breastplate of Arterial Protection
+									i(159330),	-- Gore-Splattered Vest
+									i(159253),	-- Gloves of Staunched Wounds
+									i(159625),	-- Vial of Animated Blood
 								},
 								["crs"] = {
 									133007, -- Unbound Abomination
