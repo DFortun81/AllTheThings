fb483fd076281bf9b310fd9527e0c9f10f12e010
CRIEVE-SARGERAS <dylanfortune@live.com>
2018-08-18 16:18:48 -0700

    Derp missed one.

--- contrib/Parser/DATAS/11 - Professions/Engineering/Recipes/Custom Recipes.lua
+++ contrib/Parser/DATAS/11 - Professions/Engineering/Recipes/Custom Recipes.lua
@@ -1,12 +1,14 @@
 profession(202, { -- Engineering
-	cat(709, {	-- Kul Tiran Engineering
-		cat(1096, {	-- Belt Attachments
-			--[[
-			{
-				-- This was just an example of how to do hierarchy things.
-				["spellID"] = 115767,	-- Deep Wounds
-			}
-			]]--
+	filter(200, {	-- Recipes
+		cat(709, {	-- Kul Tiran Engineering
+			cat(1096, {	-- Belt Attachments
+				--[[
+				{
+					-- This was just an example of how to do hierarchy things.
+					["spellID"] = 115767,	-- Deep Wounds
+				}
+				]]--
+			}),
 		}),
 	}),
 });
