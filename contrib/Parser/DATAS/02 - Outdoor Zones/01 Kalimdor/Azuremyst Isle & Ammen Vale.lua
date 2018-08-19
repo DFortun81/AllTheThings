99bd6e5305f39211c9d9e02dacaaf9d1221ca58f
Pr3vention <Pr3vention@gmail.com>
2018-08-19 16:05:40 -0500

    Ammen Vale sourcing and coords

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Azuremyst Isle & Ammen Vale.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Azuremyst Isle & Ammen Vale.lua
@@ -14,22 +14,22 @@ _.Zones =
 					n(63077, {	-- Lehna
 						["groups"] = {
 							p(138, {	-- Blue Moth
-								["races"] = { 11 }	-- Draenei
+								["races"] = { 11 },	-- Draenei
 							}),
 							p(630, {	-- Gilnean Raven
-								["races"] = { 22 }	-- Worgan
+								["races"] = { 22 },	-- Worgan
 							}),
 							p(68, {		-- Great Horned Owl
-								["races"] = { 4 }		-- Night Elf
+								["races"] = { 4 },		-- Night Elf
 							}),
 							p(792, {	-- Jade Crane Chick
-								["races"] = { 25 }	-- Pandaren
+								["races"] = { 25 },	-- Pandaren
 							}),
 							p(43, {		-- Orange Tabby Cat
-								["races"] = { 1 }		-- Human
+								["races"] = { 1 },		-- Human
 							}),
 							p(72, {      -- Snowshoe Rabbit
-								["races"] = { 3, 7} 	-- Gnome, Dwarf
+								["races"] = { 3, 7 }, 	-- Gnome, Dwarf
 							}),
 						},
 						["description"] = "You may only learn this pet once per character from a Battle Pet Trainer.|r"
@@ -273,7 +273,7 @@ _.Zones =
 					qa(9622, {	-- Warn Your People
 						["qg"] = 17440,	-- High Chief Stillpine
 					}),
-					qa(9314, {	-- Word from Azure Watch
+					qa(9314, {	-- Word from Azure Watch (unlocks at lvl 5)
 						["qg"] = 16554,	-- Aeun
 					}),
 				}),
@@ -299,21 +299,32 @@ _.Zones =
 				}),
 				m(468, {	-- Ammen Vale
 					["groups"] = {
-						n( -25, {	-- Pet Battle
+						n(-25, {	-- Pet Battle
 							p(464), -- Grey Moth
 						}),
 						n(-17, {	-- Quests 	
 							n(-64, { 	-- Main Questline
-								qa(9279, {	-- You Survived!
+								qa(9279, {	-- You Survived! -- breadcrumb for 9280
 									["qg"] = 16475,	-- Megelon
+									["coords"] = {
+										{ 61.2, 29.5 },
+									},
 									["races"] = { 11 },	-- Draenei
+									["isBreadcrumb"] = true,
 								}),
-								qa(9280, {	-- Replenishing the Healing Crystals
+								q(9280, {	-- Replenishing the Healing Crystals
 									["qg"] = 16477,	-- Proenitus
+									["coords"] = {
+										{ 52.7, 35.9 },
+									},
 									["races"] = { 11 },	-- Draenei
+									["sourceQuests"] = { 9279 },
 								}),
-								qa(9369, {	-- Replenishing the Healing Crystals
+								q(9369, {	-- Replenishing the Healing Crystals
 									["qg"] = 16477,	-- Proenitus
+									["coords"] = {
+										{ 52.7, 35.9 },
+									},
 									["races"] = {
 										1,	-- Human
 										4,	-- Night Elf
@@ -324,67 +335,142 @@ _.Zones =
 								}),					
 								qa(9409, {	-- Urgent Delivery!
 									["qg"] = 16477,	-- Proenitus
+									["coords"] = {
+										{ 52.7, 35.9 },
+									},
+									["sourceQuests"] = {
+										9369,	-- Replenishing the Healing Crystals (All other ally races)
+										9280,	-- Replenishing the Healing Crystals (Draenei)
+									},
 								}),
 								qa(9283, {	-- Rescue the Survivors!
 									["qg"] = 16502,	-- Zalduun
+									["coords"] = {
+										{ 52.1, 42.4 },
+									},
 									["races"] = { 11 },	-- Draenei
+									["sourceQuests"] = { 9409 },	-- Urgent Delivery!
 								}),
-								qa(9371, {	-- Botanist Taerix (can't get if 9409 is done)
+								qa(9371, {	-- Botanist Taerix 
 									["qg"] = 16477,	-- Proenitus
+									["coords"] = {
+										{ 52.7, 35.9 },
+									},
+									["sourceQuests"] = { 9409 },	-- Urgent Delivery!
 									["isBreadcrumb"] = true,
 								}),
 								qa(10302, {	-- Volatile Mutations
 									["qg"] = 16514,	-- Botanist Taerix
-									["sourceQuests"] = { 9371 },	-- Botanist Taerix
+									["coords"] = {
+										{ 49.9, 37.3 },
+									},
+									["sourceQuests"] = {
+										9369,	-- Replenishing the Healing Crystal (All other ally races)
+										9280,	-- Replenishing the Healing Crystal (Draenei)
+										9371,	-- Botanist Taerix (breadcrumb)
+									},
 								}),
 								qa(9293, {	-- What Must Be Done...
 									["qg"] = 16514,	-- Botanist Taerix
+									["coords"] = {
+										{ 49.9, 37.3 },
+									},
 									["sourceQuests"] = { 10302 },	-- Volatile Mutations
 								}),
 								qa(9294, {	-- Healing the Lake	
 									["qg"] = 16514,	-- Botanist Taerix
-								}),						
+									["coords"] = {
+										{ 49.9, 37.3 },
+									},
+									["sourceQuests"] = { 9293 },	-- What Must Be Done...
+								}),
 								qa(10304, {	-- Vindicator Aldar (can't get if 37444 is done)
 									["qg"] = 16514,	-- Botanist Taerix
+									["coords"] = {
+										{ 49.9, 37.3 },
+									},
 									["isBreadcrumb"] = true,
 								}),
 								qa(9303, {	-- Inoculation
 									["qg"] = 16535,	-- Vindicator Aldar
+									["coords"] = {
+										{ 50.6, 48.7 },
+									},
 									["description"] = "This quest is given if you turn-in Vindicator Aldar quest first.",
+									["sourceQuests"] = {
+										10302,	-- Volatile Mutations
+										10304,	-- Vindicator Aldar
+									},
 								}),
 								qa(37444, {	-- Inoculation
 									["qg"] = 16535,	-- Vindicator Aldar
-									["description"] = "This quest is given if you pick-up before getting or turning in Vindicator Aldar quest.",
+									["coords"] = {
+										{ 50.6, 48.7 },
+									},
+									["description"] = "This quest is given if you pick-up before getting or turning in the 'Vindicator Aldar' quest.",
+									["sourceQuests"] = { 10302 },	-- Volatile Mutations
 								}),
 								qa(9309, {	-- The Missing Scout
 									["qg"] = 16535,	-- Vindicator Aldar
+									["coords"] = {
+										{ 50.6, 48.7 },
+									},
+									["sourceQuests"] = {
+										9303,	-- Inoculation
+										37444,	-- Inoculation
+									},
 								}),
 								qa(10303, {	-- The Blood Elves
 									["qg"] = 16546,	-- Tolaan
+									["coords"] = {
+										{ 33.9, 69.4 },
+									},
 									["sourceQuests"] = { 9309 },	-- The Missing Scout
 								}),
 								qa(9311, {	-- Blood Elf Spy
 									["qg"] = 16546,	-- Tolaan
+									["coords"] = {
+										{ 33.9, 69.4 },
+									},
 									["sourceQuests"] = { 10303 },	-- The Blood Elves
 								}),
 								qa(9312, {	-- The Emitter	
 									["qg"] = 16535,	-- Vindicator Aldar
+									["coords"] = {
+										{ 50.6, 48.8 },
+									},
+									["sourceQuests"] = { 9311 },	-- Blood Elf Spy
 								}),
 								qa(9313, {	-- Travel to Azure Watch
 									["qg"] = 17071,	-- Technician Zhanaa
+									["coords"] = {
+										{ 50.5, 47.9 },
+									},
+									["sourceQuests"] = { 9312 }, -- The Emitter
 								}),
 							}),
 							n(-168, { 	-- Other Quests	
 								i(24414, {	-- Blood Elf Plans
 									qa(9798, {	-- Blood Elf Plans
 										["qg"] = 16522,	-- Surveyor Candress
+										["coords"] = {
+											{ 27.8, 80.4 },
+										},
 									}),
 								}),
 								qa(9799, {	-- Botanical Legwork
 									["qg"] = 20233,	-- Apprentice Vishael
+									["coords"] = {
+										{ 49.7, 37.5 },
+									},
+									["sourceQuests"] = { 10302 },	-- Volatile Mutations
 								}),
 								qa(37445, {	-- Spare Parts 
 									["qg"] = 17071,	-- Technician Zhanaa
+									["coords"] = {
+										{ 50.7, 48.0 },
+									},
+									["sourceQuests"] = { 10302 },	-- Volatile Mutations
 								}),
 							}),
 						}),
