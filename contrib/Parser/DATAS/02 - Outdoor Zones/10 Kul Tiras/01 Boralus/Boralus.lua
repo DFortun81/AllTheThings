43eeb65f18d72550dbcd68b69105c206f9c66928
Laycel\Iyanden <carrie.a.newman@gmail.com>
2018-08-25 17:52:24 -0500

    - Added various expansion arcing achievements to Boralus and Dazar'alor

--- contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/01 Boralus/Boralus.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/01 Boralus/Boralus.lua
@@ -7,13 +7,18 @@ _.Zones =
 	m(876, { 	-- Kul'Tiras
 		m(1161, {	-- Boralus
 			["groups"] = {
-				n(-4, {	-- Achievement
+				n(-4, {		-- Achievements
 					["groups"] = {
 						a(ach(12891, {	-- A Nation United
 							crit(3),		-- A Nation United
 							crit(1),		-- Kul Tourist
 							crit(2),		-- The Pride of Kul Tiras
 						})),
+						ach(12740, {	-- Full of Scrap!
+							ach(12739),		-- Scraptastic!
+							ach(12738),		-- Holy Scrap!
+						}),
+						a(ach(12741)),	-- Giving a Scrap (Alliance)
 						a(ach(12593, {	-- Kul Tourist
 							crit(1),		-- A Sound Plan
 							crit(2),		-- Drust Do It.
@@ -78,6 +83,63 @@ _.Zones =
 				}),
 				n(-38, {	-- Professions
 					["groups"] = {
+						ach(12757),		-- Angling for Battle
+						h(ach(12759)),	-- Baiting the Enemy
+						ach(12747),		-- Catering for Combat
+						ach(12990),		-- Catchin' Some Rays
+						ach(12765, {	-- Exotic Discoveries
+							crit(1),		-- Ceremonial Bonesaw
+							crit(2),		-- Ancient Runebound Tome
+							crit(3),		-- Disembowling Sickle
+							crit(4),		-- Jagged Blade of the Drust
+							crit(5),		-- Ritual Fetish
+							crit(6),		-- Soul Coffer
+							crit(7),		-- Akun'Jar Vase
+							crit(8),		-- Urn of Passage
+							crit(9),		-- Rezan Idol
+							crit(10),		-- High Apothecary's Hood
+							crit(11),		-- Bwonsamdi Voodoo Mask
+							crit(12),		-- Blowgun of the Sethra
+						}),
+						ach(12756),		-- Fish Me In the Moonlight
+						a(ach(12760)),	-- Kul Tiran Archaeologist
+						a(ach(12742)),	-- Kul Tiran Cook
+						a(ach(12762, {	-- Kul Tiran Curator
+							crit(1),		-- Pristine Ceremonial Bonesaw
+							crit(2),		-- Pristine Ancient Runebound Tome
+							crit(3),		-- Pristine Disembowling Sickle
+							crit(4),		-- Pristine Jagged Blade of the Drust
+							crit(5),		-- Pristine Ritual Fetish
+							crit(6),		-- Pristine Soul Coffer
+							crit(7),		-- Pristine Akun'Jar Vase
+							crit(8),		-- Pristine Urn of Passage
+							crit(9),		-- Pristine Rezan Idol
+							crit(10),		-- Pristine High Apothecary's Hood
+							crit(11),		-- Pristine Bwonsamdi Voodoo Mask
+							crit(12),		-- Pristine Blowgun of the Sethrak
+						})),
+						a(ach(12753)),	-- Kul Tiran Fisherman
+						a(ach(12736)),	-- Kul Tiran Master of All
+						ach(12770, {	-- Lengthy Legwork
+							ach(12769),		-- Light Travel
+						}),
+						a(ach(12731)),	-- Professional Kul Tiran Master
+						ach(12755),		-- Scent of the Sea
+						a(ach(12744, {	-- The Kul Tiran Menu
+							crit(1),		-- Bountiful Captain's Feast
+							crit(2),		-- Galley Banquet
+							crit(3),		-- Grilled Catfish
+							crit(4),		-- Honey-Glazed Haunches
+							crit(5),		-- Kul Tiramisu
+							crit(6),		-- Loa Loaf
+							crit(7),		-- Mon'Dazi
+							crit(8),		-- Ravenberry Tarts
+							crit(9),		-- Sailor's Pie
+							crit(10),		-- Seasoned Loins
+							crit(11),		-- Spiced Snapper
+							crit(12),		-- Swamp Fish 'n Chips
+						})),
+						a(ach(12734)),	-- Working in Kul Tiras
 						q(48752, {	-- A Need For Coal
 							["groups"] = {
 								recipe(253334),	-- Monelite Deposit Rank 2
--- contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/01 Dazar'alor/Dazar'alor.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/01 Dazar'alor/Dazar'alor.lua
@@ -7,8 +7,13 @@ _.Zones =
 		m(1163, {	-- Dazar'alor
 			-- Note: This expansion we do NOT want to mark this city as Horde Only because both factions will have various World Quests here
 			["groups"] = {
-				n(-4, {	-- Achievements
+				n(-4, {		-- Achievements
 					["groups"] = {
+						ach(12740, {	-- Full of Scrap!
+							ach(12739),		-- Scraptastic!
+							ach(12738),		-- Holy Scrap!
+						}),
+						h(ach(13056)),	-- Giving a Scrap (Horde)
 						ach(12851, {	-- Treasures of Zuldazar
 							["groups"] = {
 								o(284454, {	-- Da White Shark's Bounty	
@@ -44,6 +49,63 @@ _.Zones =
 				}),
 				n(-38, {	-- Professions
 					["groups"] = {
+						ach(12757),		-- Angling for Battle
+						a(ach(12758)),	-- Baiting the Enemy
+						ach(12747),		-- Catering for Combat
+						ach(12990),		-- Catchin' Some Rays
+						ach(12765, {	-- Exotic Discoveries
+							crit(1),		-- Ceremonial Bonesaw
+							crit(2),		-- Ancient Runebound Tome
+							crit(3),		-- Disembowling Sickle
+							crit(4),		-- Jagged Blade of the Drust
+							crit(5),		-- Ritual Fetish
+							crit(6),		-- Soul Coffer
+							crit(7),		-- Akun'Jar Vase
+							crit(8),		-- Urn of Passage
+							crit(9),		-- Rezan Idol
+							crit(10),		-- High Apothecary's Hood
+							crit(11),		-- Bwonsamdi Voodoo Mask
+							crit(12),		-- Blowgun of the Sethra
+						}),
+						ach(12756),		-- Fish Me In the Moonlight
+						ach(12770, {	-- Lengthy Legwork
+							ach(12769),		-- Light Travel
+						}),
+						h(ach(12733)),	-- Professional Zandalari Master
+						ach(12755),		-- Scent of the Sea
+						h(ach(12746, {	-- The Zandalari Menu
+							crit(1),		-- Bountiful Captain's Feast
+							crit(2),		-- Galley Banquet
+							crit(3),		-- Grilled Catfish
+							crit(4),		-- Honey-Glazed Haunches
+							crit(5),		-- Kul Tiramisu
+							crit(6),		-- Loa Loaf
+							crit(7),		-- Mon'Dazi
+							crit(8),		-- Ravenberry Tarts
+							crit(9),		-- Sailor's Pie
+							crit(10),		-- Seasoned Loins
+							crit(11),		-- Spiced Snapper
+							crit(12),		-- Swamp Fish 'n Chips
+						})),
+						h(ach(12735)),	-- Working in Zandalar
+						h(ach(12761)),	-- Zandalari Archaeologist
+						h(ach(12743)),	-- Zandalari Cook
+						h(ach(12764, {	-- Zandalari Curator
+							crit(1),		-- Pristine Ceremonial Bonesaw
+							crit(2),		-- Pristine Ancient Runebound Tome
+							crit(3),		-- Pristine Disembowling Sickle
+							crit(4),		-- Pristine Jagged Blade of the Drust
+							crit(5),		-- Pristine Ritual Fetish
+							crit(6),		-- Pristine Soul Coffer
+							crit(7),		-- Pristine Akun'Jar Vase
+							crit(8),		-- Pristine Urn of Passage
+							crit(9),		-- Pristine Rezan Idol
+							crit(10),		-- Pristine High Apothecary's Hood
+							crit(11),		-- Pristine Bwonsamdi Voodoo Mask
+							crit(12),		-- Pristine Blowgun of the Sethrak
+						})),	
+						h(ach(12754)),	-- Zandalari Fisherman
+						h(ach(12737)),	-- Zandalari Master of All
 						q(51962, {	-- Lumbering Away
 							["groups"] = {
 								recipe(253334),	-- Monelite Deposit Rank 2
