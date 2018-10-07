fa225aecbd7d7a451f1c01ee3a7de8be160969b8
Pr3vention <Pr3vention@gmail.com>
2018-10-07 16:03:38 -0500

    Added achievements to Heart of Fear

--- contrib/Parser/DATAS/01 - Dungeons Raids/5 - Mists of Pandaria/1 - Raids/2 Heart of Fear.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/5 - Mists of Pandaria/1 - Raids/2 Heart of Fear.lua
@@ -325,6 +325,7 @@ _.Instances =
 								},
 							}),
 							cr(62980, e(745, {	-- Imperial Vizier Zor'lok
+								ach(6937),	-- Overzealous
 								i(89827),	-- Pheromone-Coated Choker
 								i(86154),	-- Hisek's Chrysanthemum Cape
 								i(86158),	-- Chestplate of the Forbidden Tower
@@ -341,6 +342,7 @@ _.Instances =
 								i(86155),	-- Vizier's Ruby Signet
 							})),
 							cr(62543, e(744, {  -- Blade Lord Ta'yak
+								ach(6936),	-- Candle in the Wind
 								i(86171),	-- Tornado-Summoning Censer
 								i(89831),	-- Hood of Stilled Winds
 								i(86166),	-- Choker of the Unleashed Storm
@@ -357,6 +359,7 @@ _.Instances =
 								i(86162),	-- Ring of the Bladed Tempest
 							})),
 							cr(62164, e(713, {	-- Garalon
+								ach(6553),	-- Like an Arrow to the Face
 								i(86174),	-- Garlon's Hollow Skull
 								i(86181),	-- Xaril's Hood of Intoxicating Vapors
 								i(86177),	-- Necklace of Congealed Weaknesses
@@ -373,6 +376,7 @@ _.Instances =
 								i(86172),	-- Ring of the Shattered Shell
 							})),
 							cr(62397, e(741, {	-- Wind Lord Mel'jarak
+								ach(6683),	-- Less Than Three
 								i(89240, {	-- Gauntlets of the Shadowy Conqueror
 									i(85342),	-- White Tiger Gauntlets
 									i(85347),	-- White Tiger Gloves
@@ -411,6 +415,7 @@ _.Instances =
 								i(86200),	-- Painful Thorned Ring
 							})),
 							cr(62511, e(737, {	-- Amber-Shaper Un'sok
+								ach(6518),	-- I Heard You Like Amber...
 								i(89243, {	-- Leggings of the Shadowy Conqueror
 									i(85345),	-- White Tiger Greaves
 									i(85320),	-- White Tiger Legguards
@@ -449,6 +454,7 @@ _.Instances =
 								i(86211),	-- Seal of the Profane
 							})),
 							cr(62837, e(743, {	-- Grand Empress Shek'zeer
+								ach(6922),	-- Timing is Everything
 								i(89237, {	-- Chest of the Shadowy Conqueror
 									i(85343),	-- White Tiger Battleplate
 									i(85348),	-- White Tiger Breastplate
@@ -555,6 +561,7 @@ _.Instances =
 								},
 							}),						
 							cr(62980, e(745, {	-- Imperial Vizier Zor'lok
+								ach(6725),	-- Heroic: Imperial Vizier Zor'lok
 								i(89917),	-- Pheromone-Coated Choker
 								i(86945),	-- Hisek's Chrysanthemum Cape
 								i(86952),	-- Chestplate of the Forbidden Tower
@@ -571,6 +578,7 @@ _.Instances =
 								i(86946),	-- Vizier's Ruby Signet
 							})),
 							cr(62543, e(744, {  -- Blade Lord Ta'yak
+								ach(6726),	-- Heroic: Blade Lord Ta'yak
 								i(86960),	-- Tornado-Summoning Censer
 								i(89922),	-- Hood of Stilled Winds
 								i(86953),	-- Choker of the Unleashed Storm
@@ -587,6 +595,7 @@ _.Instances =
 								i(86957),	-- Ring of the Bladed Tempest
 							})),
 							cr(62164, e(713, {	-- Garalon
+								ach(6727),	-- Heroic: Garalon
 								i(86966),	-- Garalon's Hollow Skull
 								i(86970),	-- Xaril's Hood of Intoxicating Vapors
 								i(86967),	-- Necklace of Congealed Weaknesses
@@ -603,6 +612,7 @@ _.Instances =
 								i(86968),	-- Ring of the Shattered Shell
 							})),
 							cr(62397, e(741, {	-- Wind Lord Mel'jarak
+								ach(6728),	-- Heroic: Wind Lord Mel'jarak
 								i(89256, {	-- Gauntlets of the Shadowy Conqueror
 									i(87100),	-- White Tiger Gauntlets
 									i(87105),	-- White Tiger Gloves
@@ -641,6 +651,7 @@ _.Instances =
 								i(86974),	-- Painful Thorned Ring
 							})),
 							cr(62511, e(737, {	-- Amber-Shaper Un'sok
+								ach(6729),	-- Heroic: Amber-Shaper Un'sok
 								i(89253, {	-- Leggings of the Shadowy Conqueror
 									i(87107),	-- White Tiger Greaves
 									i(87112),	-- White Tiger Legguards
@@ -679,6 +690,7 @@ _.Instances =
 								i(86982),	-- Seal of the Profane
 							})),
 							cr(62837, e(743, {	-- Grand Empress Shek'zeer
+								ach(6730),	-- Heroic: Grand Empress Shek'zeer
 								i(89250, {	-- Chest of the Shadowy Conqueror
 									i(87099),	-- White Tiger Battleplate
 									i(87104),	-- White Tiger Breastplate
