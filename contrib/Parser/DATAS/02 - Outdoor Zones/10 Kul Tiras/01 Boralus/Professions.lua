212dc22acbba26000a8bae62a7d978efdeb13f3c
Lucetia-Detheroc <aceso2896@gmail.com>
2018-10-09 21:06:38 -0700

    Alliance Platinum Deposit Rank 2, Monelite Seam 3 quest added

--- contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/01 Boralus/Professions.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/01 Boralus/Professions.lua
@@ -9,66 +9,6 @@ _.Zones =
 			["groups"] = {
 				n(-38, {	-- Professions
 					["groups"] = {
-						--[[
-						-- Note!! Commenting out the achievements for now.  They aren't specific to the city and are already under achievements section in ATT.  Will analyze later and see if any can be reconfigured.
-						ach(12757),		-- Angling for Battle
-						h(ach(12759)),	-- Baiting the Enemy
-						ach(12747),		-- Catering for Combat
-						ach(12990),		-- Catchin' Some Rays
-						ach(12765, {	-- Exotic Discoveries
-							crit(1),		-- Ceremonial Bonesaw
-							crit(2),		-- Ancient Runebound Tome
-							crit(3),		-- Disembowling Sickle
-							crit(4),		-- Jagged Blade of the Drust
-							crit(5),		-- Ritual Fetish
-							crit(6),		-- Soul Coffer
-							crit(7),		-- Akun'Jar Vase
-							crit(8),		-- Urn of Passage
-							crit(9),		-- Rezan Idol
-							crit(10),		-- High Apothecary's Hood
-							crit(11),		-- Bwonsamdi Voodoo Mask
-							crit(12),		-- Blowgun of the Sethra
-						}),
-						ach(12756),		-- Fish Me In the Moonlight
-						a(ach(12760)),	-- Kul Tiran Archaeologist
-						a(ach(12742)),	-- Kul Tiran Cook
-						a(ach(12762, {	-- Kul Tiran Curator
-							crit(1),		-- Pristine Ceremonial Bonesaw
-							crit(2),		-- Pristine Ancient Runebound Tome
-							crit(3),		-- Pristine Disembowling Sickle
-							crit(4),		-- Pristine Jagged Blade of the Drust
-							crit(5),		-- Pristine Ritual Fetish
-							crit(6),		-- Pristine Soul Coffer
-							crit(7),		-- Pristine Akun'Jar Vase
-							crit(8),		-- Pristine Urn of Passage
-							crit(9),		-- Pristine Rezan Idol
-							crit(10),		-- Pristine High Apothecary's Hood
-							crit(11),		-- Pristine Bwonsamdi Voodoo Mask
-							crit(12),		-- Pristine Blowgun of the Sethrak
-						})),
-						a(ach(12753)),	-- Kul Tiran Fisherman
-						a(ach(12736)),	-- Kul Tiran Master of All
-						ach(12770, {	-- Lengthy Legwork
-							ach(12769),		-- Light Travel
-						}),
-						a(ach(12731)),	-- Professional Kul Tiran Master
-						ach(12755),		-- Scent of the Sea
-						a(ach(12744, {	-- The Kul Tiran Menu
-							crit(1),		-- Bountiful Captain's Feast
-							crit(2),		-- Galley Banquet
-							crit(3),		-- Grilled Catfish
-							crit(4),		-- Honey-Glazed Haunches
-							crit(5),		-- Kul Tiramisu
-							crit(6),		-- Loa Loaf
-							crit(7),		-- Mon'Dazi
-							crit(8),		-- Ravenberry Tarts
-							crit(9),		-- Sailor's Pie
-							crit(10),		-- Seasoned Loins
-							crit(11),		-- Spiced Snapper
-							crit(12),		-- Swamp Fish 'n Chips
-						})),
-						a(ach(12734)),	-- Working in Kul Tiras
-						--]]
 						{	-- A Need For Coal [Mine -- Monelite Deposit II]
 							["questID"] = 48752, 	-- A Need For Coal
 							["qg"] = 136091,	-- Myra Cabot
@@ -222,6 +162,22 @@ _.Zones =
 							["description"] = "This drops from \"Anchor Weed\" and can drop at any herbalism level.  You can only turn it in at Level 150 Kul Tiran Herbalism.",
 							-- NOTE!! Also grants credit for Horde Equivalent == 51480
 						},
+						{	-- Exceptional Platinum Shard [Mine -- Platinum Deposit II]
+							["itemID"] = 161078,	-- Exceptional Platinum Shard
+							["questID"] = 52044,	-- An Exceptional Platinum Shard
+							["qg"] = 136091,		-- Myra Cabot
+							["requireSkill"] = 186, -- Mining
+							["coords"] = {
+								{ 75.01, 07.61 },
+							},
+							["races"] = ALLIANCE_ONLY,
+							["description"] = "Requires 150 Kul'Tiran Mining",
+							["g"] = {
+								{	-- Platinum Deposit [Rank 2]
+									["recipeID"] = 253340,	-- Platinum Deposit [Rank 2]
+								},
+							},
+						},
 						{	-- Fully Charged Lightning Pod
 							["itemID"] = 160117,	-- Fully Charged Lightning Pod
 							["questID"] = 51404,	-- What Happens Next
@@ -366,6 +322,21 @@ _.Zones =
 								},
 							},
 						},
+						{	-- Seams Familiar [Mine -- Monelite Seam III]
+							["questID"] = 48767,	-- Seams Familiar
+							["requireSkill"] = 186, -- Mining
+							["sourceQuests"] = {
+								48764, 	-- Don't Pick a Fight
+							},
+							["races"] = ALLIANCE_ONLY,
+							["description"] = "This drops from mining \"Monelite Seam\".  You can only turn it in at Level 150 Kul Tiran Mining.",
+							-- NOTE!! Also grants credit for Horde Equivalent == 
+							["g"] = {
+								{	-- Monelite Seam Rank 3
+									["recipeID"] = 253344,	-- Monelite Seam Rank 3
+								},
+							},
+						},
 						{	-- Seeking More Knowledge [Herb -- Anchor Weed II]
 							["questID"] = 51016,	-- Seeking More Knowledge
 							["qg"] = 136096,	-- Declan Senal
@@ -439,7 +410,66 @@ _.Zones =
 								},
 							},
 						},
-						
+						--[[
+						-- Note!! Commenting out the achievements for now.  They aren't specific to the city and are already under achievements section in ATT.  Will analyze later and see if any can be reconfigured.
+						ach(12757),		-- Angling for Battle
+						h(ach(12759)),	-- Baiting the Enemy
+						ach(12747),		-- Catering for Combat
+						ach(12990),		-- Catchin' Some Rays
+						ach(12765, {	-- Exotic Discoveries
+							crit(1),		-- Ceremonial Bonesaw
+							crit(2),		-- Ancient Runebound Tome
+							crit(3),		-- Disembowling Sickle
+							crit(4),		-- Jagged Blade of the Drust
+							crit(5),		-- Ritual Fetish
+							crit(6),		-- Soul Coffer
+							crit(7),		-- Akun'Jar Vase
+							crit(8),		-- Urn of Passage
+							crit(9),		-- Rezan Idol
+							crit(10),		-- High Apothecary's Hood
+							crit(11),		-- Bwonsamdi Voodoo Mask
+							crit(12),		-- Blowgun of the Sethra
+						}),
+						ach(12756),		-- Fish Me In the Moonlight
+						a(ach(12760)),	-- Kul Tiran Archaeologist
+						a(ach(12742)),	-- Kul Tiran Cook
+						a(ach(12762, {	-- Kul Tiran Curator
+							crit(1),		-- Pristine Ceremonial Bonesaw
+							crit(2),		-- Pristine Ancient Runebound Tome
+							crit(3),		-- Pristine Disembowling Sickle
+							crit(4),		-- Pristine Jagged Blade of the Drust
+							crit(5),		-- Pristine Ritual Fetish
+							crit(6),		-- Pristine Soul Coffer
+							crit(7),		-- Pristine Akun'Jar Vase
+							crit(8),		-- Pristine Urn of Passage
+							crit(9),		-- Pristine Rezan Idol
+							crit(10),		-- Pristine High Apothecary's Hood
+							crit(11),		-- Pristine Bwonsamdi Voodoo Mask
+							crit(12),		-- Pristine Blowgun of the Sethrak
+						})),
+						a(ach(12753)),	-- Kul Tiran Fisherman
+						a(ach(12736)),	-- Kul Tiran Master of All
+						ach(12770, {	-- Lengthy Legwork
+							ach(12769),		-- Light Travel
+						}),
+						a(ach(12731)),	-- Professional Kul Tiran Master
+						ach(12755),		-- Scent of the Sea
+						a(ach(12744, {	-- The Kul Tiran Menu
+							crit(1),		-- Bountiful Captain's Feast
+							crit(2),		-- Galley Banquet
+							crit(3),		-- Grilled Catfish
+							crit(4),		-- Honey-Glazed Haunches
+							crit(5),		-- Kul Tiramisu
+							crit(6),		-- Loa Loaf
+							crit(7),		-- Mon'Dazi
+							crit(8),		-- Ravenberry Tarts
+							crit(9),		-- Sailor's Pie
+							crit(10),		-- Seasoned Loins
+							crit(11),		-- Spiced Snapper
+							crit(12),		-- Swamp Fish 'n Chips
+						})),
+						a(ach(12734)),	-- Working in Kul Tiras
+						--]]
 					},
 				}),
 			},
