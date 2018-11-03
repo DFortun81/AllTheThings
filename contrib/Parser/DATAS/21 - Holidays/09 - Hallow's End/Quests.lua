c8a94849d6b100feb83baa96e31afe45448bbc54
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-03 12:56:42 -0700

    - Hallow's End Quests now filter

--- contrib/Parser/DATAS/21 - Holidays/09 - Hallow's End/Quests.lua
+++ contrib/Parser/DATAS/21 - Holidays/09 - Hallow's End/Quests.lua
@@ -6,7 +6,6 @@ _.Holidays =
 {
 	{	-- Hallow's End
 		["npcID"] = -58,	-- Hallow's End
-		["u"] = 26, -- Temp
 		["g"] = {
 			{	-- Quests
 				["npcID"] = -17,	-- Quests
@@ -161,6 +160,7 @@ _.Holidays =
 						["coords"] = {
 							{ 62.42, 66.71 },
 						},
+						["u"] = 26,				-- Hallow's End Filter
 						["maps"] = {
 							18,	-- Tirisfal Glades
 						},
@@ -312,41 +312,48 @@ _.Holidays =
 					q(29430, { -- A Friend in Need (Alliance)
 						["qg"] = 51934,	-- Gretchen Fenlow
 						["maps"] = { 37 }, -- Elwynn Forest
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = ALLIANCE_ONLY,
 					}),
 					q(29392, { -- Missing Heirlooms (Alliance)
 						["qg"] = 53949,	-- Anson Hastings
 						["maps"] = { 84 }, -- Stormwind City
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = ALLIANCE_ONLY,
 						["sourceQuests"] = { 29430 }, -- A Friend in Need (Alliance)
 					}),
 					q(29398, { -- Fencing the Goods (Alliance)
 						["qg"] = 53950,	-- Hired Courier
 						["maps"] = { 84 }, -- Stormwind City
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = ALLIANCE_ONLY,
 						["sourceQuests"] = { 29392 }, -- Missing Heirlooms (Alliance)
 					}),
 					q(29399, { -- Shopping Around (Alliance)
 						["qg"] = 8719,	-- Auctioneer Fitch
 						["maps"] = { 84 }, -- Stormwind City
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = ALLIANCE_ONLY,
 						["sourceQuests"] = { 29398 }, -- Fencing the Goods (Alliance)
 					}),
 					q(29402, { -- Taking Precautions (Alliance)
 						["qg"] = 54021,	-- Hudson Barnes
 						["maps"] = { 84 }, -- Stormwind City
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = ALLIANCE_ONLY,
 						["sourceQuests"] = { 29399 }, -- Shopping Around (Alliance)
 					}),
 					q(29403, { -- The Collector's Agent (Alliance)
 						["qg"] = 54021,	-- Hudson Barnes
 						["maps"] = { 84 }, -- Stormwind City
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = ALLIANCE_ONLY,
 						["sourceQuests"] = { 29402 }, -- Taking Precautions (Alliance)
 					}),
 					o(209072, { -- Stolen Crate
 						q(29411, { -- What Now? (Alliance)
 							["maps"] = { 84 }, -- Stormwind City
+						["u"] = 26,				-- Hallow's End Filter
 							["races"] = ALLIANCE_ONLY,
 							["sourceQuests"] = { 29403 }, -- The Collector's Agent (Alliance)
 						}),
@@ -357,6 +364,7 @@ _.Holidays =
 								un(26, i(71076)), -- Creepy Crate (Pet)
 							},
 							["maps"] = { 84 }, -- Stormwind City
+						["u"] = 26,				-- Hallow's End Filter
 							["races"] = ALLIANCE_ONLY,
 							["sourceQuests"] = { 29411 }, -- What Now? (Alliance)
 						}),
@@ -364,35 +372,41 @@ _.Holidays =
 					q(29431, { -- A Friend in Need (Horde)
 						["qg"] = 53763,	-- Gretchen Fenlow
 						["maps"] = { 90 }, -- Undercity
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = HORDE_ONLY,
 					}),
 					q(29415, { -- Missing Heirlooms (Horde)
 						["qg"] = 54141,	-- Edgar Goodwin
 						["maps"] = { 85 }, -- Orgrimmar
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = HORDE_ONLY,
 						["sourceQuests"] = { 29431 }, -- A Friend in Need (Horde)
 					}),
 					q(29416, { -- Fencing the Goods (Horde)
 						["qg"] = 54142,	-- Hired Courier
 						["maps"] = { 85 }, -- Orgrimmar
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = HORDE_ONLY,
 						["sourceQuests"] = { 29415 }, -- Missing Heirlooms (Horde)
 					}),
 					q(29425, { -- Shopping Around (Horde)
 						["qg"] = 44866,	-- Auctioneer Drezmit
 						["maps"] = { 85 }, -- Orgrimmar
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = HORDE_ONLY,
 						["sourceQuests"] = { 29416 }, -- Fencing the Goods (Horde)
 					}),
 					q(29426, { -- Taking Precautions (Horde)
 						["qg"] = 54146,	-- Delian Sunshade
 						["maps"] = { 85 }, -- Orgrimmar
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = HORDE_ONLY,
 						["sourceQuests"] = { 29425 }, -- Shopping Around (Horde)
 					}),
 					q(29427, { -- The Collector's Agent (Horde)
 						["qg"] = 54146,	-- Delian Sunshade
 						["maps"] = { 85 }, -- Orgrimmar
+						["u"] = 26,				-- Hallow's End Filter
 						["races"] = HORDE_ONLY,
 						["sourceQuests"] = { 29426 }, -- Taking Precautions (HORDE)
 					}),
@@ -400,6 +414,7 @@ _.Holidays =
 						q(29428, { -- What Now? (Horde)
 							["maps"] = { 85 }, -- Orgrimmar
 							["races"] = HORDE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 							["sourceQuests"] = { 29427 }, -- The Collector's Agent (Horde)
 						}),
 					}),
@@ -410,6 +425,7 @@ _.Holidays =
 							},
 							["maps"] = { 85 }, -- Orgrimmar
 							["races"] = HORDE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 							["sourceQuests"] = { 29428 }, -- What Now? (Horde)
 						}),
 					}),
@@ -417,60 +433,70 @@ _.Holidays =
 						["qg"] = 6740,	-- Innkeeper Allison
 						["maps"] = { 84 }, -- Stormwind City
 						["races"] = ALLIANCE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8311 }, -- Hallow's End Treats for Jesper!
 					}),
 					q(8355, { -- Incoming Gumdrop (Alliance)
 						["qg"] = 5111,	-- Innkeeper Firebrew
 						["maps"] = { 87 }, -- Ironforge
 						["races"] = ALLIANCE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8311 }, -- Hallow's End Treats for Jesper!
 					}),
 					q(8353, { -- Chicken Clucking for a Mint (Alliance)
 						["qg"] = 6740,	-- Innkeeper Allison
 						["maps"] = { 84 }, -- Stormwind City
 						["races"] = ALLIANCE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8311 }, -- Hallow's End Treats for Jesper!
 					}),
 					q(8357, { -- Dancing for Marzipan (Alliance)
 						["qg"] = 6735,	-- Innkeeper Saelienne
 						["maps"] = { 89 }, -- Darnassus
 						["races"] = ALLIANCE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8311 }, -- Hallow's End Treats for Jesper!
 					}),
 					q(8311, { -- Hallow's End Treats for Jesper!
 						["qg"] = 15310,	-- Jesper
 						["maps"] = { 37 }, -- Elwynn Forest
 						["races"] = ALLIANCE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8356, 8355, 8353, 8357 }, -- Flexing for Nougat, Incoming Gumdrop, Chicken Clucking for a Mint, Dancing for Marzipan (Alliance)
 					}),
 					q(8359, { -- Flexing for Nougat (Horde)
 						["qg"] = 6929,	-- Innkeeper Gryshka
 						["maps"] = { 85 }, -- Orgrimmar
 						["races"] = HORDE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8312 }, -- Hallow's End Treats for Spoops!
 					}),
 					q(8358, { -- Incoming Gumdrop (Horde)
 						["qg"] = 11814,	-- Kali Remik
 						["maps"] = { 85 }, -- Orgrimmar
 						["races"] = HORDE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8312 }, -- Hallow's End Treats for Spoops!
 					}),
 					q(8354, { -- Chicken Clucking for a Mint (Horde)
 						["qg"] = 6741,	-- Innkeeper Norman
 						["maps"] = { 90 }, -- Undercity
 						["races"] = HORDE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8312 }, -- Hallow's End Treats for Spoops!
 					}),
 					q(8360, { -- Dancing for Marzipan (Horde)
 						["qg"] = 6746,	-- Innkeeper Pala
 						["maps"] = { 88 }, -- Thunder Bluff
 						["races"] = HORDE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8312 }, -- Hallow's End Treats for Spoops!
 					}),
 					q(8312, { -- Hallow's End Treats for Spoops!
 						["qg"] = 15309,	-- Spoops
 						["maps"] = { 90 }, -- Undercity
 						["races"] = HORDE_ONLY,
+						["u"] = 26,				-- Hallow's End Filter
 						["sourceQuests"] = { 8359, 8358, 8354, 8360 }, -- Flexing for Nougat, Incoming Gumdrop, Chicken Clucking for a Mint, Dancing for Marzipan (Horde)
 					}),
 				},
