cbda6c1429c5de884c4bad125ccba60aa9621f54
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-04 17:16:43 -0800

    - Fixed battle pets in Frostfire Ridge

--- contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Frostfire Ridge/Pet Battles.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Frostfire Ridge/Pet Battles.lua
@@ -12,15 +12,40 @@ _.Zones =
 				["achievementID"] = 8937,
 				["description"] = "|cff66ccffFrostfire Ridge is a zone in north-western Draenor. It is home to the Frostwolf clan and the Thunderlord clan. The ogre stronghold-turned Horde capital, Bladespire Citadel is located here. When Draenor shattered and became Outland, Frostfire Ridge and Gorgrond were pressed together to become the Blade's Edge Mountains. Several recognizable landmarks in Blade's Edge Mountains can be found in Frostfire, such as the Circle of Blood and the rock spires jutting from the earth.|r",
 				["g"] = {
-					n(-25, { 	-- Pet Battle
-						p(1427), 	-- Frostfur Rat
-						p(1578), 	-- Frostshell Pincer
-						p(1457), 	-- Icespine Hatchling
-						p(1579), 	-- Ironclaw Scuttler
-						p(417), 	-- Rat
-						p(560), 	-- Sea Gull
-						p(1464), 	-- Twilight Wasp
-					}),
+					{	-- Pet Battles
+						["npcID"] = -25,	-- Pet Battles
+						["filterID"] = 101,	-- Battle Pets
+						["g"] = {
+							{	-- Frostfur Rat
+								["npcID"] = 82715,		-- Frostfur Rat
+								["speciesID"] = 1427,	-- Frostfur Rat
+							},
+							{	-- Frostshell Pincher
+								["npcID"] = 88480,		-- Frostshell Pincher
+								["speciesID"] = 1578,	-- Frostshell Pincher
+							},
+							{	-- Icespine Hatchling
+								["npcID"] = 85003,		-- Icespine Hatchling
+								["speciesID"] = 1457,	-- Icespine Hatchling
+							},
+							{	-- Ironclaw Scuttler
+								["npcID"] = 88474,		-- Ironclaw Scuttler
+								["speciesID"] = 1579,	-- Ironclaw Scuttler
+							},
+							{	-- Rat
+								["npcID"] = 61366,		-- Rat
+								["speciesID"] = 417,	-- Rat
+							},
+							{	-- Sea Gull
+								["npcID"] = 62953,		-- Sea Gull
+								["speciesID"] = 560,	-- Sea Gull
+							},
+							{	-- Twilight Wasp
+								["npcID"] = 85253,		-- Twilight Wasp
+								["speciesID"] = 1464,	-- Twilight Wasp
+							},
+						},
+					},
 				},
 			},
 		},
