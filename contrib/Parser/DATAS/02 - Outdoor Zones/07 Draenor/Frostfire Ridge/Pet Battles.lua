6f9020bb645c681ee590ebd383c02c87f0581275
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-04 17:39:09 -0800

    Cleanup from split

--- contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Frostfire Ridge/Pet Battles.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Frostfire Ridge/Pet Battles.lua
@@ -8,9 +8,6 @@ _.Zones =
 		["g"] = {
 			{	-- Frostfire Ridge
 				["mapID"] = 525,	-- Frostfire Ridge
-				["lvl"] = 90,
-				["achievementID"] = 8937,
-				["description"] = "|cff66ccffFrostfire Ridge is a zone in north-western Draenor. It is home to the Frostwolf clan and the Thunderlord clan. The ogre stronghold-turned Horde capital, Bladespire Citadel is located here. When Draenor shattered and became Outland, Frostfire Ridge and Gorgrond were pressed together to become the Blade's Edge Mountains. Several recognizable landmarks in Blade's Edge Mountains can be found in Frostfire, such as the Circle of Blood and the rock spires jutting from the earth.|r",
 				["g"] = {
 					{	-- Pet Battles
 						["npcID"] = -25,	-- Pet Battles
--- contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Talador/Pet Battle.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Talador/Pet Battle.lua
@@ -8,9 +8,6 @@ _.Zones =
 		["g"] = {
 			{	-- Talador
 				["mapID"] = 535,	-- Talador
-				["lvl"] = 94,	
-				["achievementID"] = 8940,
-				["description"] = "|cff66ccffTalador is the sanctuary of the draenei people. At its heart rests Shattrath City, the great metropolis, and the spiritual well of Auchindoun, where the souls of departed draenei reside. Bathed in a perpetual autumn hue, its beautiful olemba forests, gardens and settlements are at risk of decimation by the Iron Horde and the Burning Legion.|r",	
 				["g"] = {
 					n(-25, { 	-- Pet Battle
 						p(427), 	-- Ash Spiderling
