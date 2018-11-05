7fd16f1e2c531e32d544d7bef30788db30be7d14
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-04 18:15:51 -0800

    Talador Pet Battles Redone Coordinates added to Crimsonwing Moth

--- contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Talador/Pet Battle.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Talador/Pet Battle.lua
@@ -9,23 +9,140 @@ _.Zones =
 			{	-- Talador
 				["mapID"] = 535,	-- Talador
 				["g"] = {
-					n(-25, { 	-- Pet Battle
-						p(427), 	-- Ash Spiderling
-						p(1572), 	-- Brilliant Bloodfeather
-						{			-- Crimsonwing Moth
-						["speciesID"] = 1589, 
-						["description"] = "The Crimsonwing Moth has a unique spawning mechanic. It has multiple spawn points, yet only a single spawn will appear in Talador at any given time. There will always be one spawn available, though. |nOnce the Crimsonwing Moth has been captured or defeated, a new one will immediately pop up in the zone. It can appear at the same location in which it was captured, or it can respawn elsewhere.",
+					{	-- Pet Battle
+						["npcID"] = -25,	-- Pet Battle
+						["filterID"] = 101,	-- Battle Pets
+						["g"] = {
+							{	--  Ash Spiderling
+								["npcID"] = 61420,		--  Ash Spiderling
+								["speciesID"] = 427,	--  Ash Spiderling
+							},
+							{	--  Brilliant Bloodfeather
+								["npcID"] = 88385,		--  Brilliant Bloodfeather
+								["speciesID"] = 1572,	--  Brilliant Bloodfeather
+							},
+							{	-- Crimsonwing Moth
+								["npcID"] = 88413,		-- Crimsonwing Moth
+								["speciesID"] = 1589,	-- Crimsonwing Moth
+								["coords"] = {
+									{ 32.4, 63.6 },
+									{ 32.6, 63.2 },
+									{ 32.6, 63.8 },
+									{ 37.0, 63.8 },
+									{ 38.8, 91.2 },
+									{ 39.0, 78.2 },
+									{ 39.0, 79.0 },
+									{ 39.0, 91.6 },
+									{ 41.0, 69.2 },
+									{ 41.4, 69.8 },
+									{ 41.6, 70.0 },
+									{ 42.4, 92.4 },
+									{ 42.6, 58.0 },
+									{ 42.6, 91.8 },
+									{ 42.6, 92.6 },
+									{ 43.0, 57.2 },
+									{ 46.4, 86.0 },
+									{ 46.6, 85.2 },
+									{ 46.6, 86.0 },
+									{ 47.2, 57.0 },
+									{ 47.8, 56.8 },
+									{ 49.4, 85.8 },
+									{ 49.6, 86.0 },
+									{ 51.4, 31.2 },
+									{ 51.4, 39.4 },
+									{ 51.8, 39.2 },
+									{ 51.8, 39.8 },
+									{ 53.0, 24.2 },
+									{ 53.0, 24.6 },
+									{ 53.2, 34.4 },
+									{ 53.2, 34.8 },
+									{ 53.2, 87.0 },
+									{ 53.4, 59.2 },
+									{ 53.6, 58.8 },
+									{ 53.6, 86.8 },
+									{ 53.6, 87.6 },
+									{ 55.8, 17.4 },
+									{ 56.4, 36.4 },
+									{ 56.6, 36.2 },
+									{ 56.6, 36.6 },
+									{ 57.0, 70.0 },
+									{ 57.2, 46.4 },
+									{ 57.2, 54.2 },
+									{ 57.2, 70.6 },
+									{ 57.4, 46.6 },
+									{ 57.4, 55.0 },
+									{ 57.6, 46.6 },
+									{ 57.6, 55.0 },
+									{ 60.6, 13.4 },
+									{ 60.6, 14.0 },
+									{ 61.4, 56.0 },
+									{ 63.8, 60.6 },
+									{ 64.0, 60.2 },
+									{ 65.8, 51.0 },
+									{ 66.0, 36.0 },
+									{ 66.2, 36.6 },
+									{ 66.2, 51.8 },
+									{ 66.6, 17.4 },
+									{ 66.6, 51.8 },
+									{ 66.8, 17.8 },
+									{ 67.4, 22.6 },
+									{ 67.4, 27.2 },
+									{ 67.4, 28.2 },
+									{ 67.6, 27.4 },
+									{ 67.6, 27.8 },
+									{ 70.0, 39.2 },
+									{ 70.2, 38.2 },
+									{ 70.4, 54.2 },
+									{ 70.6, 54.2 },
+									{ 71.6, 50.6 },
+									{ 71.8, 50.0 },
+									{ 72.0, 36.6 },
+									{ 72.2, 41.0 },
+									{ 72.4, 35.8 },
+									{ 72.6, 36.6 },
+									{ 74.0, 32.2 },
+									{ 74.0, 32.8 },
+									{ 75.0, 17.8 },
+								},
+								["description"] = "Only one is up at a time.  Once captured or kill it immediately respawns.",
+							},							{	--  Flat-Tooth Calf
+								["npcID"] = 88572,		--  Flat-Tooth Calf
+								["speciesID"] = 1595,	--  Flat-Tooth Calf
+							},
+							{	--  Kelp Scuttler
+								["npcID"] = 88465,		--  Kelp Scuttler
+								["speciesID"] = 1583,	--  Kelp Scuttler
+							},
+							{	--  Mud Jumper
+								["npcID"] = 83642,		--  Mud Jumper
+								["speciesID"] = 1441,	--  Mud Jumper
+							},
+							{	--  Red-Tailed Chipmunk
+								["npcID"] = 61757,		--  Red-Tailed Chipmunk
+								["speciesID"] = 452,	--  Red-Tailed Chipmunk
+							},
+							{	--  Royal Moth
+								["npcID"] = 88417,		--  Royal Moth
+								["speciesID"] = 1587,	--  Royal Moth
+							},
+							{	--  Sea Gull
+								["npcID"] = 62953,		--  Sea Gull
+								["speciesID"] = 560,	--  Sea Gull
+							},
+							{	--  Shadow Sporebat
+								["npcID"] = 88576,		--  Shadow Sporebat
+								["speciesID"] = 1599,	--  Shadow Sporebat
+							},
+							{	--  Silkbed Snail
+								["npcID"] = 63001,		--  Silkbed Snail
+								["speciesID"] = 568,	--  Silkbed Snail
+							},
+							{	--  Waterfly
+								["npcID"] = 88355,		--  Waterfly
+								["speciesID"] = 1593,	--  Waterfly
+							},
 						},
-						p(1595), 	-- Flat-Tooth Calf
-						p(1583), 	-- Kelp Scuttler
-						p(1441), 	-- Mud Jumper
-						p(452), 	-- Red-Tailed Chipmunk
-						p(1587), 	-- Royal Moth
-						p(560), 	-- Sea Gull
-						p(1599), 	-- Shadow Sporebat
-						p(568), 	-- Silkbed Snail
-						p(1593), 	-- Waterfly
-					}),
+					},
 				},
 			},		
 		},	
