2a7650cd1988b0fc86c5d024ca258db23ca391f5
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-04 18:25:46 -0800

    Tanaan Jungle Pet Battles Redone

--- contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Tanaan Jungle/Pet Battle.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Tanaan Jungle/Pet Battle.lua
@@ -9,28 +9,66 @@ _.Zones =
 			{	-- Tanaan Jungle
 				["mapID"] = 534,	-- Tanaan Jungle
 				["g"] = {
-					n(-25, {	-- Pet Battle
-						p(1468),	-- Bloodbeak
-						p(1586),	-- Cerulean Moth
-						p(519),		-- Fel Flame
-						p(1581),	-- Fen Crab
-						p(483),		-- Horny Toad
-						p(417),		-- Rat
-						p(405),		-- Tree Python
-						p(1591),	-- Violet Firefly
-						p(1593),	-- Waterfly
-						ach(10052, {	-- Tiny Terrors in Tanaan
-							["groups"] = {
-								i(127751, {	-- Fel-Touched Pet Supplies
-									i(127753),	-- Nightmare Bell
-									i(127754),	-- Periwinkle Calf
-									i(118105),	-- Seaborne Spore
-									i(118101),	-- Zangar Spore
-								}),
+					{	-- Pet Battle
+						["npcID"] = -25,	-- Pet Battle
+						["filterID"] = 101,	-- Battle Pets
+						["g"] = {
+							{	-- Fel-Touched Pet Supplies
+								["itemID"] = 127751,	-- Fel-Touched Pet Supplies
+								["description"] = "Fel-Touched Pet Supplies is the reward for defeating any of the Tiny Terrors in Tanaan. You can defeat each Tiny Terror once per character per day.|r",
+								["g"] = {
+									{	-- Nightmare Bell
+										["itemID"] = 127753,	-- Nightmare Bell
+									},
+									{	-- Periwinkle Calf
+										["itemID"] = 127754,	-- Periwinkle Calf
+									},
+									{	-- Seaborne Spore
+										["itemID"] = 118105,	-- Seaborne Spore
+									},
+									{	-- Zangar Spore
+										["itemID"] = 118101,	-- Zangar Spore
+									},
+								},
 							},
-							["description"] = "Fel-Touched Pet Supplies is the reward for defeating any of the Tiny Terrors in Tanaan. You can defeat each Tiny Terror once per character per day.|r",
-						}),
-					}),
+							{	--  Bloodbeak
+								["npcID"] = 85388,		--  Bloodbeak
+								["speciesID"] = 1468,	--  Bloodbeak
+							},
+							{	--  Cerulean Moth
+								["npcID"] = 88422,		--  Cerulean Moth
+								["speciesID"] = 1586,	--  Cerulean Moth
+							},
+							{	--  Fel Flame
+								["npcID"] = 62621,		--  Fel Flame
+								["speciesID"] = 519,	--  Fel Flame
+							},
+							{	--  Fen Crab
+								["npcID"] = 88473,		--  Fen Crab
+								["speciesID"] = 1581,	--  Fen Crab
+							},
+							{	--  Horny Toad
+								["npcID"] = 62185,		--  Horny Toad
+								["speciesID"] = 483,	--  Horny Toad
+							},
+							{	--  Rat
+								["npcID"] = 61366,		--  Rat
+								["speciesID"] = 417,	--  Rat
+							},
+							{	--  Tree Python
+								["npcID"] = 61318,		--  Tree Python
+								["speciesID"] = 405,	--  Tree Python
+							},
+							{	--  Violet Firefly
+								["npcID"] = 88357,		--  Violet Firefly
+								["speciesID"] = 1591,	--  Violet Firefly
+							},
+							{	--  Waterfly
+								["npcID"] = 88355,		--  Waterfly
+								["speciesID"] = 1593,	--  Waterfly
+							},
+						},
+					},
 				},
 			},
 		},
