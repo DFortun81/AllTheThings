04a33398adb4fe5b5eac274fd76ff6609ea422da
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-10 20:24:50 -0800

    Crieve Request Part 1 -- Uldum dungeons now have locations

---
+++ contrib/Parser/DATAS/01 - Dungeons Raids/4 - Cataclysm/2 - Dungeons/Entrances.lua
@@ -0,0 +1,36 @@
+-----------------------------------------------------
+--   D U N G E O N S  &  R A I D S  M O D U L E    --
+-----------------------------------------------------
+
+_.Instances = 
+{
+	{ -- Cataclysm
+		["tierID"] = 4,	-- Cataclysm
+		["g"] = {
+			{	-- Halls of Origination
+				["instanceID"] = 70,	-- Halls of Origination
+				["coords"] = {
+					{ 71.81, 52.16, 249 },	-- Halls of Origination, Uldum
+				},
+			},
+			{	-- Lost City of Tol'vir
+				["instanceID"] = 69,	-- Lost City of Tol'vir
+				["coords"] = {
+					{ 60.53, 64.29, 249 },	-- Lost City of Tol'vir, Uldum
+				},
+			},
+			{	-- Throne of the Four Winds
+				["instanceID"] = 74,	-- Throne of the Four Winds
+				["coords"] = {
+					{ 38.37, 80.63, 249 },	-- Throne of the Four Winds, Uldum
+				},
+			},
+			{	-- Vortex Pinnacle
+				["instanceID"] = 68,	-- Vortex Pinnacle
+				["coords"] = {
+					{ 76.76, 84.44, 249 },	-- Vortex Pinnacle, Uldum
+				},
+			},
+		},
+	},
+};
