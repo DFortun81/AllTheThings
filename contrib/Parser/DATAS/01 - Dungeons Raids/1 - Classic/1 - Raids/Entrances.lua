fc30b6b86f09e3b4a6bff3e1f2cc9f80de96624d
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-11 02:40:02 -0800

    - Classic Dungeon and Raids now have their coordinates plotted for their entrance

--- contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/1 - Raids/Entrances.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/1 - Raids/Entrances.lua
@@ -10,13 +10,31 @@ _.Instances =
 			{	-- Molten Core
 				["instanceID"] = 741,	-- Molten Core
 				["coords"] = {
-					{ 54.18, 83.25, 35 },	-- Molten Core, Blackrock Mountain
+					{ 54.18, 83.25, 35 },	-- Molten Core, Blackrock Mountain [Blackrock Depths]
+				},
+			},
+			{	-- Onyxia's Lair
+				["instanceID"] = 760,	-- Onyxia's Lair
+				["coords"] = {
+					{ 56.52, 71.41, 12 },	-- Onyxia's Lair, Kalimdor [Dustwallow Marsh]
 				},
 			},
 			{	-- Blackwing Lair
 				["instanceID"] = 742,	-- Blackwing Lair
 				["coords"] = {
-					{ 64.29, 71.02, 33 },	-- Blackwing Lair, Blackrock Spire
+					{ 64.29, 71.02, 33 },	-- Blackwing Lair, Blackrock Mountain [Blackrock Spire]
+				},
+			},
+			{	-- Ruins of Ahn'Qiraj
+				["instanceID"] = 743,	-- Ruins of Ahn'Qiraj
+				["coords"] = {
+					{ 58.93, 14.26, 327 },	-- Ruins of Ahn'Qiraj, Ahn Qiraj: The Fallen Kingdom
+				},
+			},
+			{	-- Temple of Ahn'Qiraj
+				["instanceID"] = 744,	-- Temple of Ahn'Qiraj
+				["coords"] = {
+					{ 46.76, 7.53, 327 },	-- Temple of Ahn'Qiraj, Ahn Qiraj: The Fallen Kingdom
 				},
 			},
 		},
--- contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/2 - Dungeons/Entrances.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/2 - Dungeons/Entrances.lua
@@ -7,16 +7,124 @@ _.Instances =
 	{ -- Classic
 		["tierID"] = 1,	-- Classic
 		["g"] = {
+			{	-- Blackfathom Deeps
+				["instanceID"] = 227,	-- Blackfathom Deeps
+				["coords"] = {
+					{ 16.53, 11.01, 63 },	-- Blackfathom Deeps, Ashenvale
+				},
+			},
 			{	-- Blackrock Depths
 				["instanceID"] = 228,	-- Blackrock Depths
 				["coords"] = {
-					{ 39.06, 18.12, 35 },	-- Blackrock Depths, Blackrock Mountain
+					{ 39.06, 18.12, 35 },	-- Blackrock Depths, Blackrock Mountain [Blackrock Depths]
+				},
+			},
+			{	-- Deadmines
+				["instanceID"] = 63,	-- Deadmines
+				["coords"] = {
+					{ 25.59, 50.97, 55 },	-- Deadmines, The Deadmines [Westfall]
+				},
+			},
+			{	-- Dire Maul
+				["instanceID"] = 230,	-- Dire Maul
+				["coords"] = {
+					{ 60.32, 30.17, 69 },	-- Dire Maul, Feralas
 				},
 			},
 			{	-- Lower Blackrock Spire
 				["instanceID"] = 229,	-- Lower Blackrock Spire
 				["coords"] = {
-					{ 80.46, 40.65, 33 },	-- Lower Blackrock Spire, Blackrock Spire
+					{ 80.46, 40.65, 33 },	-- Lower Blackrock Spire, Blackrock Mountain [Blackrock Spire]
+				},
+			},
+			{	-- Gnomeregan
+				["instanceID"] = 231,	-- Gnomeregan
+				["coords"] = {
+					{ 30.11, 74.64, 30 },	-- Gnomeregan, Gnomeregan [Dun Morogh]
+				},
+			},
+			{	-- Maraudon
+				["instanceID"] = 232,	-- Maraudon
+				["coords"] = {
+					{ 78.01, 55.68, 67 },	-- Maraudon, The Wicked Grotto
+				},
+			},
+			{	-- Ragefire Chasm
+				["instanceID"] = 226,	-- Ragefire Chasm
+				["coords"] = {
+					{ 52.52, 57.83, 86 },	-- Ragefire Chasm, Orgrimmar [Cleft of Shadow]
+				},
+			},
+			{	-- Razorfen Downs
+				["instanceID"] = 233,	-- Razorfen Downs
+				["coords"] = {
+					{ 53.24, 71.17, 12 },	-- Razorfen Downs, Kalimdor [Southern Barrens]
+				},
+			},
+			{	-- Razorfen Kraul
+				["instanceID"] = 234,	-- Razorfen Kraul
+				["coords"] = {
+					{ 50.91, 70.37, 12 },	-- Razorfen Kraul, Kalimdor [Southern Barrens]
+				},
+			},
+			{	-- Scarlet Halls
+				["instanceID"] = 311,	-- Scarlet Halls
+				["coords"] = {
+					{ 69.06, 24.98, 19 },	-- Scarlet Halls, Scarlet Monastery
+				},
+			},
+			{	-- Scarlet Monastery
+				["instanceID"] = 316,	-- Scarlet Monastery
+				["coords"] = {
+					{ 78.36, 58.04, 19 },	-- Scarlet Monastery, Scarlet Monastery
+				},
+			},
+			{	-- Scholomance
+				["instanceID"] = 246,	-- Scholomance
+				["coords"] = {
+					{ 69.07, 72.96, 22 },	-- Scholomance, Western Plaguelands
+				},
+			},
+			{	-- Shadowfang Keep
+				["instanceID"] = 64,	-- Shadowfang Keep
+				["coords"] = {
+					{ 44.79, 67.82, 21 },	-- Shadowfang Keep, Silverpine Forest
+				},
+			},
+			{	-- Stratholme
+				["instanceID"] = 236,	-- Stratholme
+				["coords"] = {
+					{ 27.66, 11.64, 23 },	-- Stratholme, Eastern Plaguelands
+				},
+			},
+			{	-- The Stockade
+				["instanceID"] = 238,	-- The Stockade
+				["coords"] = {
+					{ 50.48, 66.46, 84 },	-- The Stockade, Stormwind City
+				},
+			},
+			{	-- The Temple of Atal'hakkar
+				["instanceID"] = 237,	-- The Temple of Atal'hakkar
+				["coords"] = {
+					{ 76.04, 45.21, 51 },	-- The Temple of Atal'hakkar, Swamp of Sorrows
+				},
+			},
+			{	-- Uldaman
+				["instanceID"] = 239,	-- Uldaman
+				["coords"] = {
+					{ 36.63, 29.42, 16 },	-- Uldaman, Uldaman [Badlands]
+				},
+			},
+			{	-- Wailing Caverns
+				["instanceID"] = 240,	-- Wailing Caverns
+				["coords"] = {
+					{ 55.19, 66.09, 11 },	-- Wailing Caverns, Wailing Caverns [Northern Barrens]
+				},
+			},
+			{	-- Zul'Farrak
+				["instanceID"] = 241,	-- Zul'Farrak
+				["coords"] = {
+					{ 39.21, 21.29, 71 },	-- Zul'Farrak, Tanaris
 				},
 			},
 		},
