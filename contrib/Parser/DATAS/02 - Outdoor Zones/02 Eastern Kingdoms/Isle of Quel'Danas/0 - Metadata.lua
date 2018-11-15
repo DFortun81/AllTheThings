e0e5679288b76a33e32c33aa7ed879a9e6b54db1
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-14 16:06:38 -0800

    - Isle of Quel'Danas was split apart by the Lich King - Started vendor rework with helms being done on "Karynna" to fit a newer system than originally designed back in the day

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Isle of Quel'Danas/0 - Metadata.lua
@@ -0,0 +1,17 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	{	-- Eastern Kingdoms
+		["mapID"] = 13,	-- Eastern Kingdoms
+		["g"] = {
+			{	-- Isle of Quel'Danas
+				["mapID"] = 122,	-- Isle of Quel'Danas
+				["lvl"] = 70,	
+				["achievementID"] = 868,
+				--["description"] = "|cff66ccffThe Isle of Quel'Danas is an island located north of Eversong Woods. It is most commonly reached by the direct portal from Shattrath City. It is the major daily quest hub for the Shattered Sun Offensive, a group of Aldor and Scryers working together to reclaim the island from Kael'thas, who reactivated the Sunwell.|r",	
+			},
+		},
+	},
+};
---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Isle of Quel'Danas/Flight Paths.lua
@@ -0,0 +1,27 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	{	-- Eastern Kingdoms
+		["mapID"] = 13,	-- Eastern Kingdoms
+		["g"] = {
+			{	-- Isle of Quel'Danas
+				["mapID"] = 122,	-- Isle of Quel'Danas
+				["g"] = {
+					{	-- Flight Paths
+						["npcID"] = -228,	-- Flight Paths
+						["g"] = {
+							{	-- Shattered Sun Stage Area
+								["flightPathID"] = 213, -- Shattered Sun Stage Area
+								["coords"] = {
+									{ 48.4, 25.2 },
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+};
