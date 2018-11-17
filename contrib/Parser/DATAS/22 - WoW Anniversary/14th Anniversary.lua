3bc8cad590332f9f6d34dd61bf498e968c4b861f
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-16 21:24:55 -0800

    - Added the slightly urgent item to 14th anniversary (looks like you can get it if you didn't do it last year)

--- contrib/Parser/DATAS/22 - WoW Anniversary/14th Anniversary.lua
+++ contrib/Parser/DATAS/22 - WoW Anniversary/14th Anniversary.lua
@@ -26,6 +26,18 @@ _.Anniversary =
 								["itemID"] = 162636,	-- Celebration Package
 								["u"] = 31,				-- WoW Anniversary Filter
 							},
+							{	-- A Slightly More Urgent Letter from the Timewalkers [H]
+								["itemID"] = 147881,	-- A Slightly More Urgent Letter from the Timewalkers
+								["questID"] = 47251,	-- Interesting Things
+								["races"] = HORDE_ONLY,
+								["u"] = 31,				-- WoW Anniversary Filter
+							},
+							{	-- A Slightly More Urgent Letter from the Timewalkers [A]
+								["itemID"] = 147880,	-- A Slightly More Urgent Letter from the Timewalkers
+								["questID"] = 47252,	-- Interesting Things
+								["races"] = ALLIANCE_ONLY,
+								["u"] = 31,				-- WoW Anniversary Filter
+							},
 						},
 					},
 				},
