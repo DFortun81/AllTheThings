8b7a8c1c1f3285c1ec1daf27c4be07e9acdfd722
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-23 04:47:20 -0800

    - Added "Timeless Legends" to Timless Isles along with all the possible spawnpoints

--- contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Timeless Isle/Achievements.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Timeless Isle/Achievements.lua
@@ -18,6 +18,48 @@ _.Zones =
 							{	-- Rolo's Riddle
 								["achievementID"] = 8730,	-- Rolo's Riddle
 							},
+							{	-- Timeless Legends
+								["achievementID"] = 8784,	-- Timeless Legends
+								["coords"] = {
+									{ 43, 55, 554 },
+									{ 25, 72, 554 },
+									{ 50, 72, 554 },
+									{ 56, 59, 554 },
+									{ 47, 81, 554 },
+									{ 55, 50, 554 },
+									{ 63, 45, 554 },
+									{ 33, 55, 554 },
+									{ 55, 72, 554 },
+									{ 68, 60, 554 },
+									{ 39, 77, 554 },
+									{ 32, 32, 554 },
+									{ 65, 52, 554 },
+									{ 32, 62, 554 },
+									{ 48, 51, 554 },
+									{ 67, 72, 554 },
+									{ 37, 40, 554 },
+									{ 52, 62, 554 },
+									{ 22, 38, 554 },
+								},
+								["g"] = {
+									{	-- Cloudstrike Family Helm
+										["achievementID"] = 8784,	-- Timeless Legends
+										["criteriaID"] = 1,			-- Cloudstrike Family Helm
+									},
+									{	-- Flameheart Shawl
+										["achievementID"] = 8784,	-- Timeless Legends
+										["criteriaID"] = 2,			-- Flameheart Shawl
+									},
+									{	-- Riverspeaker's Trident
+										["achievementID"] = 8784,	-- Timeless Legends
+										["criteriaID"] = 3,			-- Riverspeaker's Trident
+									},
+									{	-- Snowdrift Tiger Talons
+										["achievementID"] = 8784,	-- Timeless Legends
+										["criteriaID"] = 4,			-- Snowdrift Tiger Talons
+									},
+								},
+							},
 						},
 					},
 				},
