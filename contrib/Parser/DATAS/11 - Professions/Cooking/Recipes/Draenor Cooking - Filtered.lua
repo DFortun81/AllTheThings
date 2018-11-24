0727698d2839df3d3b699d619a639dd0667e29ec
Pr3vention <Pr3vention@gmail.com>
2018-11-24 11:43:28 -0600

    Fixes for grand noodle carts in MoP and Elekk recipes in WoD

---
+++ contrib/Parser/DATAS/11 - Professions/Cooking/Recipes/Draenor Cooking - Filtered.lua
@@ -0,0 +1,43 @@
+profession(185, {	-- Cooking
+	filter(200, {	-- Recipes
+		{
+			["categoryID"] = 1013--[[Food of Draenor - Header]],
+			["g"] = {
+				{
+					["categoryID"] = 342--[[Food of Draenor]],
+					["g"] = {
+						{
+							["categoryID"] = 345--[[Feasts]],
+						},
+						{
+							["categoryID"] = 346--[[Delicacies]],
+						},
+						{
+							["categoryID"] = 343--[[Meat Dishes]],
+						},
+						{
+							["categoryID"] = 344--[[Fish Dishes]],
+						},
+						{
+							["recipeID"] = 169696,--[[Marinated Elekk Steak]]
+							["u"] = 1,
+						},
+						{
+							["recipeID"] = 169699,--[[Seasoned Elekk Ribeye]]
+							["u"] = 1,
+						},
+						{
+							["recipeID"] = 169701,--[[Seasoned Elekk Ribeye]]
+							["u"] = 1,
+						},
+						{
+							["recipeID"] = 169693,--[[Whole Pot-Roasted Elekk]]
+							["u"] = 1,
+						}
+					}
+				}
+			}
+		},
+		
+	}),
+});
