270be1b6c8394c56e96f6718eb35895052205075
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-25 05:21:02 -0800

    - Added missing Pandaria maps

--- contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Timeless Isle/0 - Metadata.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Timeless Isle/0 - Metadata.lua
@@ -8,6 +8,9 @@ _.Zones =
 		["g"] = {
 			{	-- Timeless Isle
 				["mapID"] = 554,	-- Timeless Isle
+				["maps"] = {
+					555,	-- Cavern of Lost Spirits
+				},
 				["lvl"] = 85,	
 				["icon"] = "Interface\\Icons\\inv_pet_scorchedstone",
 				["description"] = "|cff66ccffA true enigma, the Timeless Isle has drifted in and out of Pandaria's mists for thousands of years. Here, time has no meaning, and the sun neither rises nor sets. Its unique characteristics have brought the Isle under the scrutiny of the bronze dragonflight, pandaren explorers, and the black dragon Wrathion.|r",				
