95f42d6823cb6a76bec752dda926ebc96a89968e
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-25 05:03:15 -0800

    - Zandalari Warbringer now have coordinates attached to them

--- contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Townlong Steppes.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Townlong Steppes.lua
@@ -1006,126 +1006,255 @@ _.Zones =
 						dr(19, i(87625)),	-- Congealed Mist Amulet
 						dr(9, i(87224)),	-- Big Bag of Wonders
 					}),
-					n(69769, {	-- Zandalari Warbringer
-						dr(30, {
-							i(94158, {
-								dr(2, {
-									i(87643),	-- Fangcracker Battlemace
-									i(87652),	-- Ook-Breaker Mace
-									i(87650),	-- Fishsticker Crossbow
-									i(90721),	-- Cournith Waterstrider's Silken Finery
-									i(87646),	-- Needlefang Throatripper
-									i(90723),	-- Arness's Scaled Leggings
-									i(87642),	-- Darkstaff of Annihilation
-									i(90719),	-- Go-Han's Golden Trousers
-									i(87651),	-- Pathwalker Greatstaff
-									i(90717),	-- Qu'nas' Apocryphal Legplates
-									i(87641),	-- Yaungol Battle Barrier
-									i(87649),	-- Pool-Stirrer
-									i(90724),	-- Spriggin's Sproggin' Leggin'
-									i(90718),	-- Torik-Ethis' Bloodied Legguards
-									i(90722),	-- Torik-Ethis' Gilded Legplates
-								}), 
-								dr(1, {
-									i(90725),	-- Gaarn's Leggings of Infestation
-									i(90720),	-- Silent Leggings of the Ghostpaw
-								}),
-							}),
-						}),
-						dr(4, i(94229)), 	-- Reins of the Slate Primordial Direhorn
-						{	-- Stolen Celestial Insignia
-							["itemID"] = 94225,	-- Stolen Celestial Insignia
+					{	-- Zandalari Warbringer
+						["npcID"] = 69841,	-- Zandalari Warbringer
+						["coords"] = {
+							{ 47.42, 61.54, 422 },	-- Dread Wastes
+							{ 38.81, 67.38, 418 },	-- Krasarang Wilds
+							{ 75.11, 67.47, 379 },	-- Kun-Lai Summit
+							{ 52.56, 18.85, 371 },	-- The Jade Forest
+							{ 36.58, 85.67, 388 },	-- Townlong Steppes
 						},
-						{	-- Stolen Golden Lotus Insignia
-							["itemID"] = 94227,	-- Stolen Golden Lotus Insignia
+						["g"] = {
+							{	-- Reins of the Amber Primordial Direhorn
+								["itemID"] = 94230,	-- Reins of the Amber Primordial Direhorn
+							},
+							{	-- Big Bag of Zandalari Supplies
+								["itemID"] = 94158,	-- Big Bag of Zandalari Supplies
+								["g"] = {
+									{	-- Arness's Scaled Leggings
+										["itemID"] = 90723,	-- Arness's Scaled Leggings
+									},
+									{	-- Cournith Waterstrider's Silken Finery
+										["itemID"] = 90721,	-- Cournith Waterstrider's Silken Finery
+									},
+									{	-- Darkstaff of Annihilation
+										["itemID"] = 87642,	-- Darkstaff of Annihilation
+									},
+									{	-- Fangcracker Battlemace
+										["itemID"] = 87643,	-- Fangcracker Battlemace
+									},
+									{	-- Fishsticker Crossbow
+										["itemID"] = 87650,	-- Fishsticker Crossbow
+									},
+									{	-- Gaarn's Leggings of Infestation
+										["itemID"] = 90725,	-- Gaarn's Leggings of Infestation
+									},
+									{	-- Go-Han's Golden Trousers
+										["itemID"] = 90719,	-- Go-Han's Golden Trousers
+									},
+									{	-- Needlefang Throatripper
+										["itemID"] = 87646,	-- Needlefang Throatripper
+									},
+									{	-- Ook-Breaker Mace
+										["itemID"] = 87652,	-- Ook-Breaker Mace
+									},
+									{	-- Pathwalker Greatstaff
+										["itemID"] = 87651,	-- Pathwalker Greatstaff
+									},
+									{	-- Pool-Stirrer
+										["itemID"] = 87649,	-- Pool-Stirrer
+									},
+									{	-- Qu'nas' Apocryphal Legplates
+										["itemID"] = 90717,	-- Qu'nas' Apocryphal Legplates
+									},
+									{	-- Silent Leggings of the Ghostpaw
+										["itemID"] = 90720,	-- Silent Leggings of the Ghostpaw
+									},
+									{	-- Spriggin's Sproggin' Leggin'
+										["itemID"] = 90724,	-- Spriggin's Sproggin' Leggin'
+									},
+									{	-- Torik-Ethis' Bloodied Legguards
+										["itemID"] = 90718,	-- Torik-Ethis' Bloodied Legguards
+									},
+									{	-- Torik-Ethis' Gilded Legplates
+										["itemID"] = 90722,	-- Torik-Ethis' Gilded Legplates
+									},
+									{	-- Yaungol Battle Barrier
+										["itemID"] = 87641,	-- Yaungol Battle Barrier
+									},
+								},
+							},
+							{	-- Stolen Celestial Insignia
+								["itemID"] = 94225,	-- Stolen Celestial Insignia
+							},
+							{	-- Stolen Golden Lotus Insignia
+								["itemID"] = 94227,	-- Stolen Golden Lotus Insignia
+							},
+							{	-- Stolen Klaxxi Insignia
+								["itemID"] = 94226,	-- Stolen Klaxxi Insignia
+							},
+							{	-- Stolen Shado-Pan Insignia
+								["itemID"] = 94223,	-- Stolen Shado-Pan Insignia
+							},
 						},
-						{	-- Stolen Klaxxi Insignia
-							["itemID"] = 94226,	-- Stolen Klaxxi Insignia
+					},
+					{	-- Zandalari Warbringer
+						["npcID"] = 69842,	-- Zandalari Warbringer
+						["coords"] = {
+							{ 47.42, 61.54, 422 },	-- Dread Wastes
+							{ 38.81, 67.38, 418 },	-- Krasarang Wilds
+							{ 75.11, 67.47, 379 },	-- Kun-Lai Summit
+							{ 52.56, 18.85, 371 },	-- The Jade Forest
+							{ 36.58, 85.67, 388 },	-- Townlong Steppes
 						},
-						{	-- Stolen Shado-Pan Insignia
-							["itemID"] = 94223,	-- Stolen Shado-Pan Insignia
+						["g"] = {
+							{	-- Reins of the Jade Primordial Direhorn
+								["itemID"] = 94231,	-- Reins of the Jade Primordial Direhorn
+							},
+							{	-- Big Bag of Zandalari Supplies
+								["itemID"] = 94158,	-- Big Bag of Zandalari Supplies
+								["g"] = {
+									{	-- Arness's Scaled Leggings
+										["itemID"] = 90723,	-- Arness's Scaled Leggings
+									},
+									{	-- Cournith Waterstrider's Silken Finery
+										["itemID"] = 90721,	-- Cournith Waterstrider's Silken Finery
+									},
+									{	-- Darkstaff of Annihilation
+										["itemID"] = 87642,	-- Darkstaff of Annihilation
+									},
+									{	-- Fangcracker Battlemace
+										["itemID"] = 87643,	-- Fangcracker Battlemace
+									},
+									{	-- Fishsticker Crossbow
+										["itemID"] = 87650,	-- Fishsticker Crossbow
+									},
+									{	-- Gaarn's Leggings of Infestation
+										["itemID"] = 90725,	-- Gaarn's Leggings of Infestation
+									},
+									{	-- Go-Han's Golden Trousers
+										["itemID"] = 90719,	-- Go-Han's Golden Trousers
+									},
+									{	-- Needlefang Throatripper
+										["itemID"] = 87646,	-- Needlefang Throatripper
+									},
+									{	-- Ook-Breaker Mace
+										["itemID"] = 87652,	-- Ook-Breaker Mace
+									},
+									{	-- Pathwalker Greatstaff
+										["itemID"] = 87651,	-- Pathwalker Greatstaff
+									},
+									{	-- Pool-Stirrer
+										["itemID"] = 87649,	-- Pool-Stirrer
+									},
+									{	-- Qu'nas' Apocryphal Legplates
+										["itemID"] = 90717,	-- Qu'nas' Apocryphal Legplates
+									},
+									{	-- Silent Leggings of the Ghostpaw
+										["itemID"] = 90720,	-- Silent Leggings of the Ghostpaw
+									},
+									{	-- Spriggin's Sproggin' Leggin'
+										["itemID"] = 90724,	-- Spriggin's Sproggin' Leggin'
+									},
+									{	-- Torik-Ethis' Bloodied Legguards
+										["itemID"] = 90718,	-- Torik-Ethis' Bloodied Legguards
+									},
+									{	-- Torik-Ethis' Gilded Legplates
+										["itemID"] = 90722,	-- Torik-Ethis' Gilded Legplates
+									},
+									{	-- Yaungol Battle Barrier
+										["itemID"] = 87641,	-- Yaungol Battle Barrier
+									},
+								},
+							},
+							{	-- Stolen Celestial Insignia
+								["itemID"] = 94225,	-- Stolen Celestial Insignia
+							},
+							{	-- Stolen Golden Lotus Insignia
+								["itemID"] = 94227,	-- Stolen Golden Lotus Insignia
+							},
+							{	-- Stolen Klaxxi Insignia
+								["itemID"] = 94226,	-- Stolen Klaxxi Insignia
+							},
+							{	-- Stolen Shado-Pan Insignia
+								["itemID"] = 94223,	-- Stolen Shado-Pan Insignia
+							},
 						},
-					}),
-					n(69841, {	-- Zandalari Warbringer
-						dr(30, {
-							i(94158, {
-								dr(2, {
-									i(87643),	-- Fangcracker Battlemace
-									i(87652),	-- Ook-Breaker Mace
-									i(87650),	-- Fishsticker Crossbow
-									i(90721),	-- Cournith Waterstrider's Silken Finery
-									i(87646),	-- Needlefang Throatripper
-									i(90723),	-- Arness's Scaled Leggings
-									i(87642),	-- Darkstaff of Annihilation
-									i(90719),	-- Go-Han's Golden Trousers
-									i(87651),	-- Pathwalker Greatstaff
-									i(90717),	-- Qu'nas' Apocryphal Legplates
-									i(87641),	-- Yaungol Battle Barrier
-									i(87649),	-- Pool-Stirrer
-									i(90724),	-- Spriggin's Sproggin' Leggin'
-									i(90718),	-- Torik-Ethis' Bloodied Legguards
-									i(90722),	-- Torik-Ethis' Gilded Legplates
-								}), 
-								dr(1, {
-									i(90725),	-- Gaarn's Leggings of Infestation
-									i(90720),	-- Silent Leggings of the Ghostpaw
-								}),
-							}),
-						}),
-						dr(4, i(94230)),	-- Reins of the Amber Primordial Direhorn
-						{	-- Stolen Celestial Insignia
-							["itemID"] = 94225,	-- Stolen Celestial Insignia
+					},
+					{	-- Zandalari Warbringer
+						["npcID"] = 69769,	-- Zandalari Warbringer
+						["coords"] = {
+							{ 47.42, 61.54, 422 },	-- Dread Wastes
+							{ 38.81, 67.38, 418 },	-- Krasarang Wilds
+							{ 75.11, 67.47, 379 },	-- Kun-Lai Summit
+							{ 52.56, 18.85, 371 },	-- The Jade Forest
+							{ 36.58, 85.67, 388 },	-- Townlong Steppes
 						},
-						{	-- Stolen Golden Lotus Insignia
-							["itemID"] = 94227,	-- Stolen Golden Lotus Insignia
+						["g"] = {
+							{	-- Reins of the Slate Primordial Direhorn
+								["itemID"] = 94229,	-- Reins of the Slate Primordial Direhorn
+							},
+							{	-- Big Bag of Zandalari Supplies
+								["itemID"] = 94158,	-- Big Bag of Zandalari Supplies
+								["g"] = {
+									{	-- Arness's Scaled Leggings
+										["itemID"] = 90723,	-- Arness's Scaled Leggings
+									},
+									{	-- Cournith Waterstrider's Silken Finery
+										["itemID"] = 90721,	-- Cournith Waterstrider's Silken Finery
+									},
+									{	-- Darkstaff of Annihilation
+										["itemID"] = 87642,	-- Darkstaff of Annihilation
+									},
+									{	-- Fangcracker Battlemace
+										["itemID"] = 87643,	-- Fangcracker Battlemace
+									},
+									{	-- Fishsticker Crossbow
+										["itemID"] = 87650,	-- Fishsticker Crossbow
+									},
+									{	-- Gaarn's Leggings of Infestation
+										["itemID"] = 90725,	-- Gaarn's Leggings of Infestation
+									},
+									{	-- Go-Han's Golden Trousers
+										["itemID"] = 90719,	-- Go-Han's Golden Trousers
+									},
+									{	-- Needlefang Throatripper
+										["itemID"] = 87646,	-- Needlefang Throatripper
+									},
+									{	-- Ook-Breaker Mace
+										["itemID"] = 87652,	-- Ook-Breaker Mace
+									},
+									{	-- Pathwalker Greatstaff
+										["itemID"] = 87651,	-- Pathwalker Greatstaff
+									},
+									{	-- Pool-Stirrer
+										["itemID"] = 87649,	-- Pool-Stirrer
+									},
+									{	-- Qu'nas' Apocryphal Legplates
+										["itemID"] = 90717,	-- Qu'nas' Apocryphal Legplates
+									},
+									{	-- Silent Leggings of the Ghostpaw
+										["itemID"] = 90720,	-- Silent Leggings of the Ghostpaw
+									},
+									{	-- Spriggin's Sproggin' Leggin'
+										["itemID"] = 90724,	-- Spriggin's Sproggin' Leggin'
+									},
+									{	-- Torik-Ethis' Bloodied Legguards
+										["itemID"] = 90718,	-- Torik-Ethis' Bloodied Legguards
+									},
+									{	-- Torik-Ethis' Gilded Legplates
+										["itemID"] = 90722,	-- Torik-Ethis' Gilded Legplates
+									},
+									{	-- Yaungol Battle Barrier
+										["itemID"] = 87641,	-- Yaungol Battle Barrier
+									},
+								},
+							},
+							{	-- Stolen Celestial Insignia
+								["itemID"] = 94225,	-- Stolen Celestial Insignia
+							},
+							{	-- Stolen Golden Lotus Insignia
+								["itemID"] = 94227,	-- Stolen Golden Lotus Insignia
+							},
+							{	-- Stolen Klaxxi Insignia
+								["itemID"] = 94226,	-- Stolen Klaxxi Insignia
+							},
+							{	-- Stolen Shado-Pan Insignia
+								["itemID"] = 94223,	-- Stolen Shado-Pan Insignia
+							},
 						},
-						{	-- Stolen Klaxxi Insignia
-							["itemID"] = 94226,	-- Stolen Klaxxi Insignia
-						},
-						{	-- Stolen Shado-Pan Insignia
-							["itemID"] = 94223,	-- Stolen Shado-Pan Insignia
-						},
-					}),
-					n(69842, {	-- Zandalari Warbringer
-						dr(30, {
-							i(94158, {
-								dr(2, {
-									i(87643),	-- Fangcracker Battlemace
-									i(87652),	-- Ook-Breaker Mace
-									i(87650),	-- Fishsticker Crossbow
-									i(90721),	-- Cournith Waterstrider's Silken Finery
-									i(87646),	-- Needlefang Throatripper
-									i(90723),	-- Arness's Scaled Leggings
-									i(87642),	-- Darkstaff of Annihilation
-									i(90719),	-- Go-Han's Golden Trousers
-									i(87651),	-- Pathwalker Greatstaff
-									i(90717),	-- Qu'nas' Apocryphal Legplates
-									i(87641),	-- Yaungol Battle Barrier
-									i(87649),	-- Pool-Stirrer
-									i(90724),	-- Spriggin's Sproggin' Leggin'
-									i(90718),	-- Torik-Ethis' Bloodied Legguards
-									i(90722),	-- Torik-Ethis' Gilded Legplates
-								}), 
-								dr(1, {
-									i(90725),	-- Gaarn's Leggings of Infestation
-									i(90720),	-- Silent Leggings of the Ghostpaw
-								}),
-							}),
-						}),
-						dr(4, i(94231)),	-- Reins of the Jade Primordial Direhorn
-						{	-- Stolen Celestial Insignia
-							["itemID"] = 94225,	-- Stolen Celestial Insignia
-						},
-						{	-- Stolen Golden Lotus Insignia
-							["itemID"] = 94227,	-- Stolen Golden Lotus Insignia
-						},
-						{	-- Stolen Klaxxi Insignia
-							["itemID"] = 94226,	-- Stolen Klaxxi Insignia
-						},
-						{	-- Stolen Shado-Pan Insignia
-							["itemID"] = 94223,	-- Stolen Shado-Pan Insignia
-						},
-					}),
+					},
 					n(69768, {	-- Zandalari Warscout
 						dr(15, {
 							i(94158, {
