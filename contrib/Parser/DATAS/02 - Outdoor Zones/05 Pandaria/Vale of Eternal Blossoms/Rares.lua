594151e88fae429367a910d66f0183fd48e46c03
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-25 03:37:58 -0800

    - All rares located in "Vale of Eternal Blossom" now has coordinates!  Mobs located in the Halls also plot to their correct location rather than at random places in the map and have the entrance to the halls plotted as well.

--- contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Vale of Eternal Blossoms/Rares.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Vale of Eternal Blossoms/Rares.lua
@@ -30,160 +30,435 @@ _.Zones =
 								i(76061),	-- Spirit of Harmony
 							},
 						}),
-						n(58778, {	-- Aetha
-							dr(46, i(103624)),	-- Treasures of the Vale
-						}),
-						n(50822, {	-- Ai-Ran the Shifting Cloud
-							dr(20, {
-								i(87638),	-- Cloak of Tranquil Clouds
-								i(86590),	-- Essence of the Breeze
-								i(87637),	-- Jade Harbinger's Cloak
-								i(87640),	-- Softfoot's Drape
-							}),
-							dr(19, {
-								i(87636),	-- Cloak of the Forgotten Emperor
-								i(87639),	-- Tattered Guo-Lai Dynasty Cloak
-							}),
-							dr(18, i(87217)),	-- Small Bag of Goods
-						}), 
-						n(64403, {	-- Alani
-							["groups"] = {
-								dr(100, i(90655)),	-- Reins of the Thundering Ruby Cloud Serpent
+						{	-- Aetha
+							["npcID"] = 58778,	-- Aetha
+							["coords"] = {
+								{ 35.05, 89.99, 390 },
 							},
-							["description"] = "|cff66ccffRequires 10 Skyshards to create a Sky Crystal to remove the Nimbus Shroud Debuff. Personal loot now so everyone in party will get the mount.|r",	
-						}),
-						n(58949, {	-- Bai'Jin the Butcher
-							dr(24, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(63695, {	-- Baolai the Immolator
-							dr(43, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(58474, {	-- Bloodtip
-							dr(19, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(58768, {	-- Cracklefang
-							dr(26, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(62881, {	-- Gaohun the Soul-Severer
-							dr(40, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(63101, {	-- General Temuja
-							dr(36, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(62880, {	-- Gochao the Ironfist
-							dr(35, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(63691, {	-- Huo-Shuang
-							dr(10, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(50749, {	-- Kal'tik the Blight
-							dr(21, i(87637)),	-- Jade Harbinger's Cloak
-							dr(20, i(87217)),	-- Small Bag of Goods
-							dr(19, {
-								i(87636),	-- Cloak of the Forgotten Emperor
-								i(87638),	-- Cloak ok Tranquil Clouds
-								i(87640),	-- Softfoot's Drape
-								i(87639),	-- Tattered Guo-Lai Dynasty Cloak
-							}),
-							dr(18, i(134023)),	-- Bottled Tornado
-						}), 
-						n(50349, {	-- Kang the Soul Thief
-							dr(21, i(87638)),	-- Cloak of Tranquil Clouds
-							dr(20, i(87639)),	-- Tattered Guo-Lai Dynasty Cloak
-							dr(19, {
-								i(87636),	-- Cloak of the Forgotten Emperor
-								i(87637),	-- Jade Harbinger's Cloak
-								i(87217),	-- Small Bag of Goods
-							}),
-							dr(18, {
-								i(86571),	-- Kang's Bindstone
-								i(87640),	-- Softfoot's Drape
-							}),
-						}), 
-						n(63978, {	-- Kri'chon
-							dr(29, i(103624)),	-- Treasures of the Vale
-						}),
-						n(50840, {	-- Major Nanners
-							dr(22, i(87636)),	-- Cloak of the Forgotten Emperor
-							dr(21, i(87639)),	-- Tattered Guo-lai Dynasty Cloak
-							dr(20, {
-								i(86594),	-- Helpful Wikky's Whistle
-								i(87217),	-- Small Bag of Goods
-							}),
-							dr(19, {
-								i(87638),	-- Cloak of Tranquil Clouds
-								i(87640),	-- Softfoot's Drape
-							}),
-							dr(18, i(87637)),	-- Jade Harbinger's Cloak
-						}), 
-						n(50806, {	-- Moldo One-Eye
-							dr(27, i(87639)),	-- Tattered Guo-Lai Dynasty Cloak
-							dr(19, {
-								i(87640),	-- Softfoot's Drape
-								i(87217),	-- Small Bag of Goods
-							}),
-							dr(18, {
-								i(87636),	-- Cloak of the Forgotten Emperor
-								i(87638),	-- Cloak ok Tranquil Clouds
-								i(86586),	-- Panflute of Pandaria
-							}),
-							dr(17, i(87637)),	-- Jade Harbinger's Cloak
-						}), 
-						n(58771, {	-- Quid
-							dr(45, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(50780, {	-- Sahn Tidehunter
-							dr(21, i(87217)),	-- Small Bag of Goods
-							dr(20, {
-								i(87636),	-- Cloak of the Forgotten Emperor
-								i(87637),	-- Jade Harbinger's Cloak
-								i(87639),	-- Tattered Guo-Lai Dynasty Cloak
-							}),
-							dr(19, {
-								i(87638),	-- Cloak ok Tranquil Clouds
-								i(87640),	-- Softfoot's Drape
-							}),
-							dr(11, i(86582)),	-- Aqua Jewel
-						}), 
-						n(63240, {	-- Shadowmaster Sydow
-							dr(37, i(103624)),	-- Treasures of the Vale
-						}),  
-						n(58817, {	-- Spirit of Lao-Fe
-							dr(26, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(50359, {	-- Urgolax
-							dr(29, i(87638)),	-- Cloak of Tranquil Clouds
-							dr(18, {
-								i(87637),	-- Jade Harbinger's Cloak
-								i(87217),	-- Small Bag of Goods
-								i(87639),	-- Tattered Guo-Lai Dynasty Cloak
-							}),
-							dr(17, i(87636)),	-- Cloak of the Forgotten Emperor
-							dr(16, {
-								i(87640),	-- Softfoot's Drape
-								i(86575),	-- Chalice of Secrets
-							}),
-						}), 
-						n(58769, {	-- Vicejaw
-							dr(34, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(63977, {	-- Vyraxxis
-							dr(31, i(103624)),	-- Treasures of the Vale
-						}), 
-						n(63510, {	-- Wulon
-							dr(20, i(103624)),	-- Treasures of the Vale
-						}),
-						n(50336, {	-- Yorik Sharpeye
-							dr(22, i(87638)),	-- Cloak of Tranquil Clouds
-							dr(20, i(87640)),	-- Softfoot's Drape
-							dr(19, {
-								i(87636),	-- Cloak of the Forgotten Emperor
-								i(87637),	-- Jade Harbinger's Cloak
-								i(87217),	-- Small Bag of Goods
-								i(87639),	-- Tattered Guo-Lai Dynasty Cloak
-							}),
-							dr(11, i(86568)),	-- Mr. Smite's Brass Compass
-						}), 
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Ai-Ran the Shifting Cloud
+							["npcID"] = 50822,	-- Ai-Ran the Shifting Cloud
+							["coords"] = {
+								{ 42.84, 69.26, 390 },
+							},
+							["g"] = {
+								{	-- Cloak of Tranquil Clouds
+									["itemID"] = 87638,	-- Cloak of Tranquil Clouds
+								},
+								{	-- Essence of the Breeze
+									["itemID"] = 86590,	-- Essence of the Breeze
+								},
+								{	-- Jade Harbinger's Cloak
+									["itemID"] = 87637,	-- Jade Harbinger's Cloak
+								},
+								{	-- Softfoot's Drape
+									["itemID"] = 87640,	-- Softfoot's Drape
+								},
+								{	-- Cloak of the Forgotten Emperor
+									["itemID"] = 87636,	-- Cloak of the Forgotten Emperor
+								},
+								{	-- Tattered Guo-Lai Dynasty Cloak
+									["itemID"] = 87639,	-- Tattered Guo-Lai Dynasty Cloak
+								},
+								{	-- Small Bag of Goods
+									["itemID"] = 87217,	-- Small Bag of Goods
+								},
+							},
+						},
+						{	-- Alani
+							["npcID"] = 64403,	-- Alani
+							["coords"] = {
+								{ 56.21, 42.61, 390 },
+								{ 35.61, 26.01, 390 },
+								{ 38.61, 64.61, 390 },
+							},
+							["description"] = "|cff66ccffRequires 10 Skyshards to create a Sky Crystal to remove the Nimbus Shroud Debuff. Personal loot now so everyone in party will get the mount.|r",
+							["g"] = {
+								{	-- Reins of the Thundering Ruby Cloud Serpent
+									["itemID"] = 90655,	-- Reins of the Thundering Ruby Cloud Serpent
+								},
+							},
+						},
+						{	-- Bai-Jin the Butcher
+							["npcID"] = 58949,	-- Bai-Jin the Butcher
+							["coords"] = {
+								{ 17.05, 48.73, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Baolai the Immolator
+							["npcID"] = 63695,	-- Baolai the Immolator
+							["coords"] = {
+								{ 28.71, 43.32, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Bloodtip
+							["npcID"] = 58474,	-- Bloodtip
+							["coords"] = {
+								{ 22.43, 26.75, 395 },	-- Entrance
+								{ 75.87, 47.5, 395 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Cracklefang
+							["npcID"] = 58768,	-- Cracklefang
+							["coords"] = {
+								{ 46.42, 59.37, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Gaohun the Soul-Severer
+							["npcID"] = 62881,	-- Gaohun the Soul-Severer
+							["coords"] = {
+								{ 22.43, 26.75, 395 },	-- Entrance
+								{ 53.16, 59.31, 395 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- General Temuja
+							["npcID"] = 63101,	-- General Temuja
+							["coords"] = {
+								{ 26.37, 51.11, 390 },
+								{ 28.96, 56.39, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Gochao the Ironfist
+							["npcID"] = 62880,	-- Gochao the Ironfist
+							["coords"] = {
+								{ 26.86, 13.08, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Huo-Shuang
+							["npcID"] = 63691,	-- Huo-Shuang
+							["coords"] = {
+								{ 22.43, 26.75, 395 },	-- Entrance
+								{ 63.92, 19.07, 395 },
+								{ 63.98, 19.19, 395 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Kal'tik the Blight
+							["npcID"] = 50749,	-- Kal'tik the Blight
+							["coords"] = {
+								{ 14.01, 58.61, 390 },
+							},
+							["g"] = {
+								{	-- Jade Harbinger's Cloak
+									["itemID"] = 87637,	-- Jade Harbinger's Cloak
+								},
+								{	-- Small Bag of Goods
+									["itemID"] = 87217,	-- Small Bag of Goods
+								},
+								{	-- Cloak of the Forgotten Emperor
+									["itemID"] = 87636,	-- Cloak of the Forgotten Emperor
+								},
+								{	-- Cloak ok Tranquil Clouds
+									["itemID"] = 87638,	-- Cloak ok Tranquil Clouds
+								},
+								{	-- Softfoot's Drape
+									["itemID"] = 87640,	-- Softfoot's Drape
+								},
+								{	-- Tattered Guo-Lai Dynasty Cloak
+									["itemID"] = 87639,	-- Tattered Guo-Lai Dynasty Cloak
+								},
+								{	-- Bottled Tornado
+									["itemID"] = 134023,	-- Bottled Tornado
+								},
+							},
+						},
+						{	-- Kang the Soul Thief
+							["npcID"] = 50349,	-- Kang the Soul Thief
+							["coords"] = {
+								{ 15.26, 35.21, 390 },
+							},
+							["g"] = {
+								{	-- Cloak of Tranquil Clouds
+									["itemID"] = 87638,	-- Cloak of Tranquil Clouds
+								},
+								{	-- Tattered Guo-Lai Dynasty Cloak
+									["itemID"] = 87639,	-- Tattered Guo-Lai Dynasty Cloak
+								},
+								{	-- Cloak of the Forgotten Emperor
+									["itemID"] = 87636,	-- Cloak of the Forgotten Emperor
+								},
+								{	-- Jade Harbinger's Cloak
+									["itemID"] = 87637,	-- Jade Harbinger's Cloak
+								},
+								{	-- Small Bag of Goods
+									["itemID"] = 87217,	-- Small Bag of Goods
+								},
+								{	-- Kang's Bindstone
+									["itemID"] = 86571,	-- Kang's Bindstone
+								},
+								{	-- Softfoot's Drape
+									["itemID"] = 87640,	-- Softfoot's Drape
+								},
+							},
+						},
+						{	-- Kri'chon
+							["npcID"] = 63978,	-- Kri'chon
+							["coords"] = {
+								{ 6.27, 58.58, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Major Nanners
+							["npcID"] = 50840,	-- Major Nanners
+							["coords"] = {
+								{ 30.75, 91.52, 390 },
+							},
+							["g"] = {
+								{	-- Cloak of the Forgotten Emperor
+									["itemID"] = 87636,	-- Cloak of the Forgotten Emperor
+								},
+								{	-- Tattered Guo-Lai Dynasty Cloak
+									["itemID"] = 87639,	-- Tattered Guo-Lai Dynasty Cloak
+								},
+								{	-- Helpful Wikky's Whistle
+									["itemID"] = 86594,	-- Helpful Wikky's Whistle
+								},
+								{	-- Small Bag of Goods
+									["itemID"] = 87217,	-- Small Bag of Goods
+								},
+								{	-- Cloak of Tranquil Clouds
+									["itemID"] = 87638,	-- Cloak of Tranquil Clouds
+								},
+								{	-- Softfoot's Drape
+									["itemID"] = 87640,	-- Softfoot's Drape
+								},
+								{	-- Jade Harbinger's Cloak
+									["itemID"] = 87637,	-- Jade Harbinger's Cloak
+								},
+							},
+						},
+						{	-- Moldo One-Eye
+							["npcID"] = 50806,	-- Moldo One-Eye
+							["coords"] = {
+								{ 39.18, 54.05, 390 },
+								{ 34.89, 60.78, 390 },
+							},
+							["g"] = {
+								{	-- Tattered Guo-Lai Dynasty Cloak
+									["itemID"] = 87639,	-- Tattered Guo-Lai Dynasty Cloak
+								},
+								{	-- Softfoot's Drape
+									["itemID"] = 87640,	-- Softfoot's Drape
+								},
+								{	-- Small Bag of Goods
+									["itemID"] = 87217,	-- Small Bag of Goods
+								},
+								{	-- Cloak of the Forgotten Emperor
+									["itemID"] = 87636,	-- Cloak of the Forgotten Emperor
+								},
+								{	-- Cloak ok Tranquil Clouds
+									["itemID"] = 87638,	-- Cloak ok Tranquil Clouds
+								},
+								{	-- Panflute of Pandaria
+									["itemID"] = 86586,	-- Panflute of Pandaria
+								},
+								{	-- Jade Harbinger's Cloak
+									["itemID"] = 87637,	-- Jade Harbinger's Cloak
+								},
+							},
+						},
+						{	-- Quid
+							["npcID"] = 58771,	-- Quid
+							["coords"] = {
+								{ 66.47, 39.31, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Sahn Tidehunter
+							["npcID"] = 50780,	-- Sahn Tidehunter
+							["coords"] = {
+								{ 69.46, 30.57, 390 },
+							},
+							["g"] = {
+								{	-- Small Bag of Goods
+									["itemID"] = 87217,	-- Small Bag of Goods
+								},
+								{	-- Cloak of the Forgotten Emperor
+									["itemID"] = 87636,	-- Cloak of the Forgotten Emperor
+								},
+								{	-- Jade Harbinger's Cloak
+									["itemID"] = 87637,	-- Jade Harbinger's Cloak
+								},
+								{	-- Tattered Guo-Lai Dynasty Cloak
+									["itemID"] = 87639,	-- Tattered Guo-Lai Dynasty Cloak
+								},
+								{	-- Cloak ok Tranquil Clouds
+									["itemID"] = 87638,	-- Cloak ok Tranquil Clouds
+								},
+								{	-- Softfoot's Drape
+									["itemID"] = 87640,	-- Softfoot's Drape
+								},
+								{	-- Aqua Jewel
+									["itemID"] = 86582,	-- Aqua Jewel
+								},
+							},
+						},
+						{	-- Shadowmaster Sydow
+							["npcID"] = 63240,	-- Shadowmaster Sydow
+							["coords"] = {
+								{ 30.56, 78.44, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Spirit of Lao-Fe
+							["npcID"] = 58817,	-- Spirit of Lao-Fe
+							["coords"] = {
+								{ 47.43, 65.61, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Urgolax
+							["npcID"] = 50359,	-- Urgolax
+							["coords"] = {
+								{ 39.54, 25.17, 390 },
+							},
+							["g"] = {
+								{	-- Cloak of Tranquil Clouds
+									["itemID"] = 87638,	-- Cloak of Tranquil Clouds
+								},
+								{	-- Jade Harbinger's Cloak
+									["itemID"] = 87637,	-- Jade Harbinger's Cloak
+								},
+								{	-- Small Bag of Goods
+									["itemID"] = 87217,	-- Small Bag of Goods
+								},
+								{	-- Tattered Guo-Lai Dynasty Cloak
+									["itemID"] = 87639,	-- Tattered Guo-Lai Dynasty Cloak
+								},
+								{	-- Cloak of the Forgotten Emperor
+									["itemID"] = 87636,	-- Cloak of the Forgotten Emperor
+								},
+								{	-- Softfoot's Drape
+									["itemID"] = 87640,	-- Softfoot's Drape
+								},
+								{	-- Chalic of Secrets
+									["itemID"] = 86575,	-- Chalice of Secrets
+								},
+							},
+						},
+						{	-- Vicejaw
+							["npcID"] = 58769,	-- Vicejaw
+							["coords"] = {
+								{ 37.42, 50.97, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Vyraxxis
+							["npcID"] = 63977,	-- Vyraxxis
+							["coords"] = {
+								{ 7.92, 33.81, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Wulon
+							["npcID"] = 63510,	-- Wulon
+							["coords"] = {
+								{ 45.38, 76.31, 390 },
+								{ 40.25, 77.14, 390 },
+							},
+							["g"] = {
+								{	-- Treasures of the Vale
+									["itemID"] = 103624,	-- Treasures of the Vale
+								},
+							},
+						},
+						{	-- Yorik Sharpeye
+							["npcID"] = 50336,	-- Yorik Sharpeye
+							["coords"] = {
+								{ 88.08, 44.34, 390 },
+							},
+							["g"] = {
+								{	-- Cloak of Tranquil Clouds
+									["itemID"] = 87638,	-- Cloak of Tranquil Clouds
+								},
+								{	-- Softfoot's Drape
+									["itemID"] = 87640,	-- Softfoot's Drape
+								},
+								{	-- Cloak of the Forgotten Emperor
+									["itemID"] = 87636,	-- Cloak of the Forgotten Emperor
+								},
+								{	-- Jade Harbinger's Cloak
+									["itemID"] = 87637,	-- Jade Harbinger's Cloak
+								},
+								{	-- Small Bag of Goods
+									["itemID"] = 87217,	-- Small Bag of Goods
+								},
+								{	-- Tattered Guo-Lai Dynasty Cloak
+									["itemID"] = 87639,	-- Tattered Guo-Lai Dynasty Cloak
+								},
+								{	-- Mr. Smite's Brass Compass
+									["itemID"] = 86568,	-- Mr. Smite's Brass Compass
+								},
+							},
+						},
 						--n(50843, { 		-- Portent	}), 
 					}),
 				},
