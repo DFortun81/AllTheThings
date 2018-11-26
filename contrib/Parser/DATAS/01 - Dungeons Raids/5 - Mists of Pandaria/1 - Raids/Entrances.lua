5c2993f899ef2a8848c3088a4ade04fa2aff3a7c
Lucetia-Detheroc <aceso2896@gmail.com>
2018-11-26 14:53:01 -0800

    - MoP World Bosses now have their coordinates attached

---
+++ contrib/Parser/DATAS/01 - Dungeons Raids/5 - Mists of Pandaria/1 - Raids/Entrances.lua
@@ -0,0 +1,53 @@
+-----------------------------------------------------
+--   D U N G E O N S  &  R A I D S  M O D U L E    --
+-----------------------------------------------------
+
+_.Instances = 
+{
+	{ -- Mists of Pandaria
+		["tierID"] = 5,	-- Mists of Pandaria
+		["g"] = {
+			{	-- World Bosses
+				["npcID"] = -7,	-- World Bosses
+				["g"] = {
+					{	-- Nalak, The Storm Lord
+						["encounterID"] = 814,	-- Nalak, The Storm Lord
+						["coords"] = {
+							{ 60.01, 37.73, 504 },
+						},
+					},
+					{	-- Oondasta
+						["encounterID"] = 826,	-- Oondasta
+						["coords"] = {
+							{ 49.90, 56.85, 507 },
+						},
+					},
+					{	-- Ordos, Fire-God of the Yaungol
+						["encounterID"] = 861,	-- Ordos, Fire-God of the Yaungol
+						["coords"] = {
+							{ 54.98, 17.36, 554},
+						},
+					},
+					{	-- Saylis's Warband [Galleon]
+						["encounterID"] = 725,	-- Saylis's Warband
+						["coords"] = {
+							{ 70.79, 63.51, 376 },
+						},
+					},
+					{	-- Sha of Anger
+						["encounterID"] = 691,	-- Sha of Anger
+						["coords"] = {
+							{ 53.53, 65.26, 379 },
+						},
+					},
+					{	-- The Four Celestials
+						["npcID"] = -10060,	-- The Four Celestials
+						["coords"] = {
+							{ 38.87, 55.29, 554 },
+						},
+					},
+				},
+			},
+		},
+	},
+};
