aaacb993bcdb37fc29d7974867086f174accfa42
Crieve-Sargeras <dylanfortune@live.com>
2018-12-08 15:22:34 -0700

    Added achievements to Trial of the Champion.

--- contrib/Parser/DATAS/01 - Dungeons Raids/3 - Wrath of the Lich King/2 - Dungeons/Trial of the Champion.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/3 - Wrath of the Lich King/2 - Dungeons/Trial of the Champion.lua
@@ -16,6 +16,80 @@ _.Instances =
 					}),
 					d(1, { -- Normal
 						["groups"] = {
+							a({
+								["achievementID"] = 4296, 	-- Trial of the Champion [Achievement]
+								["groups"] = {
+									{
+										["criteriaID"] = 6,	-- Argent Confessor Paletress
+										["creatureID"] = 34928,	-- Argent Confessor Paletress
+									},
+									{
+										["criteriaID"] = 2,	-- Deathstalker Visceri
+										["creatureID"] = 35617,	-- Deathstalker Visceri
+									},
+									{
+										["criteriaID"] = 7,	-- Eadric the Pure
+										["creatureID"] = 35119,	-- Eadric the Pure
+									},
+									{
+										["criteriaID"] = 4,	-- Eressea Dawnsinger
+										["creatureID"] = 35569,	-- Eressea Dawnsinger
+									},
+									{
+										["criteriaID"] = 1,	-- Mokra the Skullcrusher
+										["creatureID"] = 35572,	-- Mokra the Skullcrusher
+									},
+									{
+										["criteriaID"] = 3,	-- Runok Wildmane
+										["creatureID"] = 35571,	-- Runok Wildmane
+									},
+									{
+										["criteriaID"] = 8,	-- The Black Knight
+										["creatureID"] = 35451,	-- The Black Knight
+									},
+									{
+										["criteriaID"] = 5,	-- Zul'tore
+										["creatureID"] = 35570,	-- Zul'tore
+									},
+								},
+							}),
+							h({
+								["achievementID"] = 3778, 	-- Trial of the Champion [Achievement]
+								["groups"] = {
+									{
+										["criteriaID"] = 4,	-- Ambrose Boltspark
+										["creatureID"] = 34702,	-- Ambrose Boltspark
+									},
+									{
+										["criteriaID"] = 6,	-- Argent Confessor Paletress
+										["creatureID"] = 34928,	-- Argent Confessor Paletress
+									},
+									{
+										["criteriaID"] = 3,	-- Colosos
+										["creatureID"] = 34701,	-- Colosos
+									},
+									{
+										["criteriaID"] = 7,	-- Eadric the Pure
+										["creatureID"] = 35119,	-- Eadric the Pure
+									},
+									{
+										["criteriaID"] = 5,	-- Jaelyne Evensong
+										["creatureID"] = 34657,	-- Jaelyne Evensong
+									},
+									{
+										["criteriaID"] = 2,	-- Lana Stouthammer
+										["creatureID"] = 34703,	-- Lana Stouthammer
+									},
+									{
+										["criteriaID"] = 1,	-- Marshal Jacob Alerius
+										["creatureID"] = 34705,	-- Marshal Jacob Alerius
+									},
+									{
+										["criteriaID"] = 8,	-- The Black Knight
+										["creatureID"] = 35451,	-- The Black Knight
+									},
+								},
+							}),
 							e(634, {	-- Grand Champions
 								["groups"] = {							
 									i(47493), -- Edge of Ruin
@@ -134,6 +208,80 @@ _.Instances =
 					}),
 					d(2, { -- Heroic
 						["groups"] = {
+							a({
+								["achievementID"] = 4298, 	-- Heroic: Trial of the Champion [Achievement]
+								["groups"] = {
+									{
+										["criteriaID"] = 6,	-- Argent Confessor Paletress
+										["creatureID"] = 34928,	-- Argent Confessor Paletress
+									},
+									{
+										["criteriaID"] = 2,	-- Deathstalker Visceri
+										["creatureID"] = 35617,	-- Deathstalker Visceri
+									},
+									{
+										["criteriaID"] = 7,	-- Eadric the Pure
+										["creatureID"] = 35119,	-- Eadric the Pure
+									},
+									{
+										["criteriaID"] = 4,	-- Eressea Dawnsinger
+										["creatureID"] = 35569,	-- Eressea Dawnsinger
+									},
+									{
+										["criteriaID"] = 1,	-- Mokra the Skullcrusher
+										["creatureID"] = 35572,	-- Mokra the Skullcrusher
+									},
+									{
+										["criteriaID"] = 3,	-- Runok Wildmane
+										["creatureID"] = 35571,	-- Runok Wildmane
+									},
+									{
+										["criteriaID"] = 8,	-- The Black Knight
+										["creatureID"] = 35451,	-- The Black Knight
+									},
+									{
+										["criteriaID"] = 5,	-- Zul'tore
+										["creatureID"] = 35570,	-- Zul'tore
+									},
+								},
+							}),
+							h({
+								["achievementID"] = 4297, 	-- Heroic: Trial of the Champion [Achievement]
+								["groups"] = {
+									{
+										["criteriaID"] = 4,	-- Ambrose Boltspark
+										["creatureID"] = 34702,	-- Ambrose Boltspark
+									},
+									{
+										["criteriaID"] = 6,	-- Argent Confessor Paletress
+										["creatureID"] = 34928,	-- Argent Confessor Paletress
+									},
+									{
+										["criteriaID"] = 3,	-- Colosos
+										["creatureID"] = 34701,	-- Colosos
+									},
+									{
+										["criteriaID"] = 7,	-- Eadric the Pure
+										["creatureID"] = 35119,	-- Eadric the Pure
+									},
+									{
+										["criteriaID"] = 5,	-- Jaelyne Evensong
+										["creatureID"] = 34657,	-- Jaelyne Evensong
+									},
+									{
+										["criteriaID"] = 2,	-- Lana Stouthammer
+										["creatureID"] = 34703,	-- Lana Stouthammer
+									},
+									{
+										["criteriaID"] = 1,	-- Marshal Jacob Alerius
+										["creatureID"] = 34705,	-- Marshal Jacob Alerius
+									},
+									{
+										["criteriaID"] = 8,	-- The Black Knight
+										["creatureID"] = 35451,	-- The Black Knight
+									},
+								},
+							}),
 							e(634, {	-- Grand Champions
 								["groups"] = {
 									currency(241),	-- Champion's Seal
@@ -170,6 +318,10 @@ _.Instances =
 								},
 							}),
 							cr(35119, e(635, {	-- Eadric the Pure
+								{
+									["achievementID"] = 3803, 	-- The Faceroller [Achievement]
+									["description"] = "If you're soloing it in Legion, it's VERY easy to do, regardless of class:\n\nKill the adds, and when he gets attackable, don't attack in any way (no pets, no trinkets that activates, etc). In other words, do NO damage to him, you'll likely one-shot him and fail.\n\nJust face away from him until he casts Hammer of Justice. You'll catch the hamer, and your action bar changes. Now face him, and use the hammer. It will one-shot him, and you'll get the achievement.\n\nCredit to Netspook on Wowhead",
+								},
 								currency(241),	-- Champion's Seal
 								i(47508), -- Aledar's Battlestar
 								i(47509), -- Mariel's Sorrow
@@ -197,6 +349,111 @@ _.Instances =
 								i(47213), -- Abyssal Rune
 							})),
 							cr(34928, e(636, {	-- Argent Confessor Paletress
+								{
+									["achievementID"] = 3802, 	-- Argent Confessor [Achievement]
+									["groups"] = {
+										{
+											["criteriaID"] = 25,	-- Algalon
+											["creatureID"] = 35052,	-- Memory of Algalon
+										},
+										{
+											["criteriaID"] = 14,	-- Archimonde
+											["creatureID"] = 35041,	-- Memory of Archimonde
+										},
+										{
+											["criteriaID"] = 7,	-- Chromaggus
+											["creatureID"] = 35033,	-- Memory of Chromaggus
+										},
+										{
+											["criteriaID"] = 19,	-- Cyanigosa
+											["creatureID"] = 35046,	-- Memory of Cyanigosa
+										},
+										{
+											["criteriaID"] = 16,	-- Delrissa
+											["creatureID"] = 35043,	-- Memory of Delrissa
+										},
+										{
+											["criteriaID"] = 20,	-- Eck
+											["creatureID"] = 35047,	-- Memory of Eck
+										},
+										{
+											["criteriaID"] = 12,	-- Gruul
+											["creatureID"] = 35039,	-- Memory of Gruul
+										},
+										{
+											["criteriaID"] = 8,	-- Hakkar
+											["creatureID"] = 35034,	-- Memory of Hakkar
+										},
+										{
+											["criteriaID"] = 22,	-- Heigan
+											["creatureID"] = 35049,	-- Memory of Heigan
+										},
+										{
+											["criteriaID"] = 4,	-- Herod
+											["creatureID"] = 35030,	-- Memory of Herod
+										},
+										{
+											["criteriaID"] = 1,	-- Hogger
+											["creatureID"] = 34942,	-- Memory of Hogger
+										},
+										{
+											["criteriaID"] = 15,	-- Illidan
+											["creatureID"] = 35042,	-- Memory of Illidan
+										},
+										{
+											["criteriaID"] = 23,	-- Ignis
+											["creatureID"] = 35050,	-- Memory of Ignis
+										},
+										{
+											["criteriaID"] = 18,	-- Ingvar
+											["creatureID"] = 35045,	-- Memory of Ingvar
+										},
+										{
+											["criteriaID"] = 10,	-- Kalithresh
+											["creatureID"] = 35037,	-- Memory of Kalithresh
+										},
+										{
+											["criteriaID"] = 5,	-- Lucifron
+											["creatureID"] = 35031,	-- Memory of Lucifron
+										},
+										{
+											["criteriaID"] = 17,	-- M'uru (Entropius)
+											["creatureID"] = 35044,	-- Memory of Entropius
+										},
+										{
+											["criteriaID"] = 11,	-- Malchezaar
+											["creatureID"] = 35038,	-- Memory of Malchezaar
+										},
+										{
+											["criteriaID"] = 3,	-- Mutanus
+											["creatureID"] = 35029,	-- Memory of Mutanus
+										},
+										{
+											["criteriaID"] = 21,	-- Onyxia
+											["creatureID"] = 35048,	-- Memory of Onyxia
+										},
+										{
+											["criteriaID"] = 6,	-- Thunderaan
+											["creatureID"] = 35032,	-- Memory of Thunderaan
+										},
+										{
+											["criteriaID"] = 2,	-- VanCleef
+											["creatureID"] = 35028,	-- Memory of VanCleef
+										},
+										{
+											["criteriaID"] = 13,	-- Vashj
+											["creatureID"] = 35040,	-- Memory of Vashj
+										},
+										{
+											["criteriaID"] = 9,	-- Vek'nilash
+											["creatureID"] = 35036,	-- Memory of Vek'nilash
+										},
+										{
+											["criteriaID"] = 24,	-- Vezax
+											["creatureID"] = 35051,	-- Memory of Vezax
+										},
+									},
+								},
 								currency(241),	-- Champion's Seal
 								i(47522), -- Marrowstrike
 								i(47500), -- Peacekeeper Blade
@@ -224,6 +481,9 @@ _.Instances =
 								i(47214), -- Banner of Victory
 							})),
 							cr(35451, e(637, {	-- The Black Knight
+								{
+									["achievementID"] = 3804, 	-- I've Had Worse [Achievement]
+								},
 								currency(241),	-- Champion's Seal
 								i(49682), -- Black Knight's Rondel
 								i(47569), -- Spectral Kris
