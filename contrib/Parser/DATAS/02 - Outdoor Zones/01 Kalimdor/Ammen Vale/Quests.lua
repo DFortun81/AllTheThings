312abe2d38fb9bcb7d7529395d8f3ed918301274
Pr3vention <Pr3vention@gmail.com>
2018-12-20 00:45:28 -0600

    Ammen Vale format consistency, legacy consolidation

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Ammen Vale/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Ammen Vale/Quests.lua
@@ -3,24 +3,27 @@
 ---------------------------------------------------
 _.Zones =
 {
-	m(12, { 	-- Kalimdor
+	m(12, {	-- Kalimdor
 		m(468, {	-- Ammen Vale
 			["groups"] = {
-				n(-17, {	-- Quests 	
-					n(-64, { 	-- Main Questline
-						q(9279, {	-- You Survived! -- breadcrumb for 9280
+				n(-17, {	-- Quests
+					n(-64, {	-- Main Questline
+						{	-- You Survived!
+							["questID"] = 9279,
 							["qg"] = 16475,	-- Megelon
 							["coord"] = { 61.2, 29.5 },
 							["races"] = { 11 },	-- Draenei
 							["isBreadcrumb"] = true,
-						}),
-						q(9280, {	-- Replenishing the Healing Crystals
+						},
+						{	-- Replenishing the Healing Crystals
+							["questID"] = 9280,
 							["qg"] = 16477,	-- Proenitus
 							["coord"] = { 52.7, 35.9 },
 							["races"] = { 11 },	-- Draenei
-							["sourceQuests"] = { 9279 },
-						}),
-						q(9369, {	-- Replenishing the Healing Crystals
+							["sourceQuest"] = 9279,
+						},
+						{	-- Replenishing the Healing Crystals
+							["questID"] = 9369,
 							["qg"] = 16477,	-- Proenitus
 							["coord"] = { 52.7, 35.9 },
 							["races"] = {
@@ -30,112 +33,244 @@ _.Zones =
 								22,	-- Worgen
 								25,	-- Pandaren
 							},
-						}),					
-						qa(9409, {	-- Urgent Delivery!
+						},					
+						{	-- Urgent Delivery!
+							["questID"] = 9409,
 							["qg"] = 16477,	-- Proenitus
 							["coord"] = { 52.7, 35.9 },
+							["races"] = ALLIANCE_ONLY,
 							["sourceQuests"] = {
 								9369,	-- Replenishing the Healing Crystals (All other ally races)
 								9280,	-- Replenishing the Healing Crystals (Draenei)
 							},
-						}),
-						q(9283, {	-- Rescue the Survivors!
+						},
+						{	-- Rescue the Survivors!
+							["questID"] = 9283,
 							["qg"] = 16502,	-- Zalduun
 							["coord"] = { 52.1, 42.4 },
 							["races"] = { 11 },	-- Draenei
-							["sourceQuests"] = { 9409 },	-- Urgent Delivery!
-						}),
-						qa(9371, {	-- Botanist Taerix 
+							["sourceQuest"] = 9409,	-- Urgent Delivery!
+						},
+						{	-- Botanist Taerix 
+							["questID"] = 9371,
 							["qg"] = 16477,	-- Proenitus
 							["coord"] = { 52.7, 35.9 },
-							["sourceQuests"] = { 9409 },	-- Urgent Delivery!
+							["races"] = ALLIANCE_ONLY,
+							["sourceQuest"] = 9409,	-- Urgent Delivery!
 							["isBreadcrumb"] = true,
-						}),
-						qa(10302, {	-- Volatile Mutations
+						},
+						{	-- Volatile Mutations
+							["questID"] = 10302,
 							["qg"] = 16514,	-- Botanist Taerix
 							["coord"] = { 49.9, 37.3 },
+							["races"] = ALLIANCE_ONLY,
 							["sourceQuests"] = {
 								9369,	-- Replenishing the Healing Crystal (All other ally races)
 								9280,	-- Replenishing the Healing Crystal (Draenei)
 								9371,	-- Botanist Taerix (breadcrumb)
 							},
-						}),
-						qa(9293, {	-- What Must Be Done...
+						},
+						{	-- What Must Be Done...
+							["questID"] = 9293,
 							["qg"] = 16514,	-- Botanist Taerix
 							["coord"] = { 49.9, 37.3 },
-							["sourceQuests"] = { 10302 },	-- Volatile Mutations
-						}),
-						qa(9294, {	-- Healing the Lake	
+							["races"] = ALLIANCE_ONLY,
+							["sourceQuest"] = 10302,	-- Volatile Mutations
+						},
+						{	-- Healing the Lake	
+							["questID"] = 9294,
 							["qg"] = 16514,	-- Botanist Taerix
 							["coord"] = { 49.9, 37.3 },
-							["sourceQuests"] = { 9293 },	-- What Must Be Done...
-						}),
-						qa(10304, {	-- Vindicator Aldar (can't get if 37444 is done)
+							["races"] = ALLIANCE_ONLY,
+							["sourceQuest"] = 9293,	-- What Must Be Done...
+						},
+						{	-- Vindicator Aldar
+							["questID"] = 10304,
 							["qg"] = 16514,	-- Botanist Taerix
 							["coord"] = { 49.9, 37.3 },
+							["races"] = ALLIANCE_ONLY,
 							["isBreadcrumb"] = true,
-						}),
-						qa(9303, {	-- Inoculation
+						},
+						{	-- Inoculation
+							["questID"] = 9303,
 							["qg"] = 16535,	-- Vindicator Aldar
 							["coord"] = { 50.6, 48.7 },
+							["races"] = ALLIANCE_ONLY,
 							["description"] = "This quest is given if you turn-in Vindicator Aldar quest first.",
 							["sourceQuests"] = {
 								10302,	-- Volatile Mutations
 								10304,	-- Vindicator Aldar
 							},
-						}),
-						qa(37444, {	-- Inoculation
+						},
+						{	-- Inoculation
+							["questID"] = 37444,
 							["qg"] = 16535,	-- Vindicator Aldar
 							["coord"] = { 50.6, 48.7 },
+							["races"] = ALLIANCE_ONLY,
 							["description"] = "This quest is given if you pick-up before getting or turning in the 'Vindicator Aldar' quest.",
-							["sourceQuests"] = { 10302 },	-- Volatile Mutations
-						}),
-						qa(9309, {	-- The Missing Scout
+							["sourceQuest"] = 10302,	-- Volatile Mutations
+						},
+						{	-- The Missing Scout
+							["questID"] = 9309,
 							["qg"] = 16535,	-- Vindicator Aldar
 							["coord"] = { 50.6, 48.7 },
+							["races"] = ALLIANCE_ONLY,
 							["sourceQuests"] = {
 								9303,	-- Inoculation
 								37444,	-- Inoculation
 							},
-						}),
-						qa(10303, {	-- The Blood Elves
+						},
+						{	-- The Blood Elves
+							["questID"] = 10303,
 							["qg"] = 16546,	-- Tolaan
 							["coord"] = { 33.9, 69.4 },
-							["sourceQuests"] = { 9309 },	-- The Missing Scout
-						}),
-						qa(9311, {	-- Blood Elf Spy
+							["races"] = ALLIANCE_ONLY,
+							["sourceQuest"] = 9309,	-- The Missing Scout
+						},
+						{	-- Blood Elf Spy
+							["questID"] = 9311,
 							["qg"] = 16546,	-- Tolaan
 							["coord"] = { 33.9, 69.4 },
-							["sourceQuests"] = { 10303 },	-- The Blood Elves
-						}),
-						qa(9312, {	-- The Emitter	
+							["races"] = ALLIANCE_ONLY,
+							["sourceQuest"] = 10303,	-- The Blood Elves
+						},
+						{	-- The Emitter
+							["questID"] = 9312,
 							["qg"] = 16535,	-- Vindicator Aldar
 							["coord"] = { 50.6, 48.8 },
-							["sourceQuests"] = { 9311 },	-- Blood Elf Spy
-						}),
-						qa(9313, {	-- Travel to Azure Watch
+							["races"] = ALLIANCE_ONLY,
+							["sourceQuest"] = 9311,	-- Blood Elf Spy
+						},
+						{	-- Travel to Azure Watch
+							["questID"] = 9313,
 							["qg"] = 17071,	-- Technician Zhanaa
 							["coord"] = { 50.5, 47.9 },
-							["sourceQuests"] = { 9312 }, -- The Emitter
-						}),
+							["races"] = ALLIANCE_ONLY,
+							["sourceQuest"] = 9312, -- The Emitter
+						},
 					}),
 					n(-168, { 	-- Other Quests	
-						i(24414, {	-- Blood Elf Plans
-							qa(9798, {	-- Blood Elf Plans
-								["qg"] = 16522,	-- Surveyor Candress
-								--["coord"] = { 27.8, 80.4 },
-							}),
-						}),
-						qa(9799, {	-- Botanical Legwork
+						{	-- Blood Elf Plans
+							["itemID"] = 24414,
+							["groups"] = {
+								{	-- Blood Elf Plans
+									["questID"] = 9798,
+									["qg"] = 16522,	-- Surveyor Candress
+									["races"] = ALLIANCE_ONLY,
+								},
+							},
+						},
+						{	-- Botanical Legwork
+							["questID"] = 9799,
 							["qg"] = 20233,	-- Apprentice Vishael
 							["coord"] = { 49.7, 37.5 },
-							["sourceQuests"] = { 10302 },	-- Volatile Mutations
-						}),
-						qa(37445, {	-- Spare Parts 
+							["races"] = ALLIANCE_ONLY,
+							["sourceQuest"] = 10302,	-- Volatile Mutations
+						},
+						{	-- Frost Nova
+							["questID"] = 26968,
+							["u"] = 40,
+							["qg"] = 16500,	-- Valaatu
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 8 },	-- Mage
+						},
+						{	-- Hunter Training
+							["questID"] = 9288,
+							["u"] = 40,
+							["qg"] = 16499,	-- Keilnei
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 3 },	-- Hunter
+						},
+						{	-- Learning the Word
+							["questID"] = 26970,
+							["u"] = 40,
+							["qg"] = 16502,	-- Zalduun
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 5 },	-- Priest
+						},
+						{	-- Mage Training
+							["questID"] = 9290,
+							["u"] = 40,
+							["qg"] = 16500,	-- Valaatu
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 8 },	-- Mage
+						},
+						{	-- Paladin Training
+							["questID"] = 9287,
+							["u"] = 40,
+							["qg"] = 16501,	-- Aurelon
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 2 },	-- Paladin
+						},
+						{	-- Priest Training
+							["questID"] = 9291,
+							["u"] = 40,
+							["qg"] = 16502,	-- Zalduun
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 5 },	-- Priest
+						},
+						{	-- Primal Strike
+							["questID"] = 26969,
+							["u"] = 40,
+							["qg"] = 17089,	-- Firmanvaar
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 7 },	-- Shaman
+						},
+						{	-- Shaman Training
+							["questID"] = 9421,
+							["u"] = 40,
+							["qg"] = 17089,	-- Firmanvaar
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 7 },	-- Shaman
+						},
+						{	-- Spare Parts
+							["questID"] = 9305,
+							["u"] = 40,
+							["qg"] = 17071,	-- Technician Zhanaa
+							["races"] = ALLIANCE_ONLY,
+						},
+						{	-- Spare Parts 
+							["questID"] = 37445,
 							["qg"] = 17071,	-- Technician Zhanaa
 							["coord"] = { 50.7, 48.0 },
-							["sourceQuests"] = { 10302 },	-- Volatile Mutations
-						}),
+							["races"] = ALLIANCE_ONLY,
+							["sourceQuest"] = 10302,	-- Volatile Mutations
+						},
+						{	-- Steadying Your Shot
+							["questID"] = 26963,
+							["u"] = 40,
+							["qg"] = 16499,	-- Keilnei
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 3 },	-- Hunter
+						},
+						{	-- The Light's Power
+							["questID"] = 26966,
+							["u"] = 40,
+							["qg"] = 16501,	-- Aurelon
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 2 },	-- Paladin
+						},
+						{	-- The Tiger Palm
+							["questID"] = 31173,
+							["u"] = 40,
+							["qg"] = 63335,	-- Mojo Stormstout
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 10 },	-- Monk
+						},
+						{	-- Warrior Training
+							["questID"] = 9289,
+							["u"] = 40,
+							["qg"] = 16503,	-- Kore
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 1 },	-- Warrior
+						},
+						{	-- Your First Lesson
+							["questID"] = 26958,
+							["u"] = 40,
+							["qg"] = 16503,	-- Kore
+							["races"] = ALLIANCE_ONLY,
+							["classes"] = { 10 },	-- Monk
+						},
 					}),
 				}),
 			},
