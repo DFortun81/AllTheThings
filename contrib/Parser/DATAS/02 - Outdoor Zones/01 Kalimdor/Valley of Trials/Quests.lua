5d963be3e9f64f81ad46527a1826210947c332fc
Pr3vention <Pr3vention@gmail.com>
2018-12-22 23:08:07 -0600

    Valley of Trials legacy consolidation, formatting
    Format now consistent with other completed Kalimdor zones

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Valley of Trials/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Valley of Trials/Quests.lua
@@ -9,72 +9,90 @@ _.Zones =
 				["groups"] = {
 					n(-17, {	-- Quests
 						["groups"] = {
-							q(25152, {	-- Your Place In The World
+							{	-- Your Place In The World
+								["questID"] = 25152,
 								["qg"] = 10176, -- Kaltunk
 								["coord"] = { 45.2, 68.4 },
-							}),
-							q(25126, {	-- Cutting Teeth
+							},
+							{	-- Cutting Teeth
+								["questID"] = 25126,
 								["qg"] = 3143, -- Gornek
 								["coord"] = { 44.9, 66.4 },
-								["sourceQuests"] = { 25152 }, -- Your Place In The World
-							}),
-							q(25172, {	-- Invaders In Our Home
+								["sourceQuest"] = 25152, -- Your Place In The World
+							},
+							{	-- Invaders In Our Home
+								["questID"] = 25172,
 								["qg"] = 3143, -- Gornek
 								["coord"] = { 44.9, 66.4 },
-								["sourceQuests"] = { 25126 }, -- Cutting Teeth
-							}),
-							q(25136, {	-- Galgar's Cactus Apple Surprise
+								["sourceQuest"] = 25126, -- Cutting Teeth
+							},
+							{	-- Galgar's Cactus Apple Surprise
+								["questID"] = 25136,
 								["qg"] = 9796, -- Galgar
 								["coord"] = { 43.0, 62.4 },
-								["sourceQuests"] = { 25126 }, -- Cutting Teeth
-							}),
-							q(25127, {	-- Sting of the Scorpid
+								["sourceQuest"] = 25126, -- Cutting Teeth
+							},
+							{	-- Sting of the Scorpid
+								["questID"] = 25127,
 								["qg"] = 3143, -- Gornek
 								["coord"] = { 44.9, 66.4 },
-								["sourceQuests"] = { 25172 }, -- Invaders In Our Home
-							}),
-							q(37446, {	-- Lazy Peons
+								["sourceQuest"] = 25172, -- Invaders In Our Home
+							},
+							{	-- Lazy Peons (replaced by 37446)
+								["questID"] = 25134,
+								["u"] = 40,
+								["qg"] = 11378,	-- Foreman Thazz'ril
+								["races"] = HORDE_ONLY,
+							},
+							{	-- Lazy Peons
+								["questID"] = 37446,
 								["qg"] = 11378, -- Foreman Thazz'ril
 								["coord"] = { 46.2, 63.3 },
-								["sourceQuests"] = { 25127 }, -- Sting of the Scorpid
-							}),
-							q(25135, {	-- Thazz'ril's Pick
+								["sourceQuest"] = 25127, -- Sting of the Scorpid
+							},
+							{	-- Thazz'ril's Pick
+								["questID"] = 25135,
 								["qg"] = 11378, -- Foreman Thazz'ril
 								["coord"] = { 46.2, 63.3 },
-								["sourceQuests"] = { 37446 }, -- Lazy Peons
-							}),
-							q(25131, {	-- Vile Familiars
+								["sourceQuest"] = 37446, -- Lazy Peons
+							},
+							{	-- Vile Familiars
+								["questID"] = 25131,
 								["qg"] = 3145, -- Zureetha Fargaze
 								["coord"] = { 45.8, 63.5 },
-								["sourceQuests"] = { 25127 }, -- Sting of the Scorpid
-							}),
-							q(25132, {	-- Burning Blade Medallion
+								["sourceQuest"] = 25127, -- Sting of the Scorpid
+							},
+							{	-- Burning Blade Medallion
+								["questID"] = 25132,
 								["qg"] = 3145, -- Zureetha Fargaze
 								["coord"] = { 45.8, 63.5 },
-								["sourceQuests"] = { 25131 }, -- Vile Familiars
-							}),
-							q(25128, {	-- Hana'zua
+								["sourceQuest"] = 25131, -- Vile Familiars
+							},
+							{	-- Hana'zua
+								["questID"] = 25128,
 								["qg"] = 5887, -- Canaga Earthcaller
 								["coord"] = { 41.7, 70.0 },
 								["isBreadcrumb"] = true,
-							}),
-							q(25129, {	-- Sarkoth
+							},
+							{	-- Sarkoth
+								["questID"] = 25129,
 								["qg"] = 3287, -- Hana'zua
 								["coord"] = { 34.6, 44.2 },
-								["sourceQuests"] = { 25128 }, -- Hana'zua
-							}),
-							q(25130, {	-- Back to the Den
+								["sourceQuest"] = 25128, -- Hana'zua
+							},
+							{	-- Back to the Den
+								["questID"] = 25130,
 								["qg"] = 3287, -- Hana'zua
 								["coord"] = { 34.6, 44.2 },
-								["sourceQuests"] = { 25129 }, -- Sarkoth
-							}),
-							q(25133, {	-- Report to Sen'jin Village
+								["sourceQuest"] = 25129, -- Sarkoth
+							},
+							{	-- Report to Sen'jin Village
+								["questID"] = 25133,
 								["qg"] = 3145, -- Zureetha Fargaze
 								["coord"] = { 45.8, 63.5 },
-								["sourceQuests"] = { 25132 }, -- Burning Blade Medallion
+								["sourceQuest"] = 25132, -- Burning Blade Medallion
 								["isBreadcrumb"] = true,
-							}),
-							
+							},
 						},
 						["races"] = HORDE_ONLY,
 					}),
