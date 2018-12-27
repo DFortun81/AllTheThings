c618764b3afcaa2102fd854e722c440edaa2e666
Gold-DieAldor <goldenshacal@gmail.com>
2018-12-27 18:40:06 +0100

    itemid fix

--- contrib/Parser/DATAS/17 - NYI/NYI.lua
+++ contrib/Parser/DATAS/17 - NYI/NYI.lua
@@ -640,9 +640,9 @@ _.NeverImplemented =
 		un(1, i(166436)),	-- Sandy Nightsaber (only on ptr)
 		un(1, i(163063)),	-- Reins of the spectral phoenix (old trash 100 rep award)
 		un(1, i(163188)),	-- Surf Jelly (testing version)
-		un(1, i(163879)),	-- Reins of the Proudmoore Sea Scout
-		un(1, i(163908)),	-- Reins of the Dusky Waycrest Gryphon
-		un(1, i(163909)),	-- Reins of the Stormsong Coastwatcher
+		un(1, i(161879)),	-- Reins of the Proudmoore Sea Scout
+		un(1, i(161908)),	-- Reins of the Dusky Waycrest Gryphon
+		un(1, i(161909)),	-- Reins of the Stormsong Coastwatcher
 		un(1, i(163186)),	-- Tempestuous Skystallion (Old 400 Mounts Reward)
 		
 	}),
