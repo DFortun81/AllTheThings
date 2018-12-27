a7e29594c252f398463eb016f0f565590f41068e
Crieve-Sargeras <dylanfortune@live.com>
2018-12-27 00:29:46 -0700

    Added Artifact Relics, Quests, and extended some information for achievements for Deadwind Pass and Return to Karazhan.

--- contrib/Parser/DATAS/01 - Dungeons Raids/7 - Legion/2 - Dungeons/Cathedral of Eternal Night.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/7 - Legion/2 - Dungeons/Cathedral of Eternal Night.lua
@@ -8,6 +8,7 @@ _.Instances =
 		["groups"] = {
 			inst(900, {	-- Cathedral of Eternal Night
 				["groups"] = {
+					--[[
 					n(0, { -- Zone Drops
 						["groups"] = {
 							i(147517, { -- Inquisitor's Battle Cowl
@@ -37,111 +38,142 @@ _.Instances =
 							120717, -- Mistress Dominix
 						},
 					}),
+					]]--
 					d(2, {	-- Heroic
-						["groups"] = {
-							cr(117193, e(1905, {	-- Agronox
-								i(144469),	-- Caretaker's Budding Mantle
-								i(144491),	-- Choking Vine Bindings
-								i(144490),	-- Corruption-Fused Stompers
-								i(144468),	-- Felpruner's Shroud
-								i(144470),	-- Grovetender's Handwraps
-								i(144497),	-- Petrified Hust Girdle
-								i(144477),	-- Splinters of Agronox
-								i(144483),	-- Spore-Coated Vest
-								i(144498),	-- Vambraces of Fel Crust
-							})),
-							cr(117194, e(1906, {	-- Thrashbite the Scornful
-								i(144492),	-- Chain-Draped Gauntlets
-								i(144482),	-- Fel-Oiled Infernal Machine
-								i(144493),	-- Link-Lashed Spaulders
-								i(144479),	-- Master Thrasher's Lockcollar
-								i(144485),	-- Mo'arg Heavyweight's Wristwraps
-								i(144471),	-- Pulverizing Felsoles
-								i(144484),	-- Tattered Scornful Leggings
-								i(144499),	-- Tenderized Breastplate
-								i(144472),	-- Thrashbite's Cummerbund
-							})),
-							cr(119542, e(1904, {	-- Domatrax
-								i(144496),	-- Burning Chain Waistguard
-								i(144476),	-- Cloak of the Unending Assault
-								i(144475),	-- Dark Mistress' Cuffs
-								i(144495),	-- Domatrax's Chain Hauberk
-								i(144488),	-- Feaster Hide Gloves
-								i(144503),	-- Helm of Domatrax
-								i(144481),	-- Ring of Fel Domination
-								i(144474),	-- Sheer Felwoven Hosiery
-								i(144489),	-- Shoulderguards of Legion Domination
-								i(144500),	-- Two-Toed Trashstompers
-							})),
-							cr(120793, e(1878, {	-- Mephistroth
-								ach(11700),	-- Cathedral of Eternal Night
-								ach(11701),	-- Heroic: Cathedral of Eternal Night
-								i(141591),	-- Technique: Codex of the Tranquil Mind (Rank 2)
-								i(141592),	-- Technique: Codex of the Tranquil Mind (Rank 3)
-								i(144478),	-- Band of Dark Solitude
-								i(144486),	-- Cerulean Shadowfade Belt
-								i(144501),	-- Dread Commander's Pauldrons
-								i(144480),	-- Dreadstone of Endless Shadows
-								i(144494),	-- Imposing Dreadsoul Headgear
-								i(144473),	-- Nathrezim Acolyte's Robes
-								i(144502),	-- Shadowed Grips of the Nathrezim
-								i(144487),	-- Treads of Creeping Shadows
-							})),
-						},
+						n(0, { -- Zone Drops
+							["groups"] = {
+								i(147517),	-- Inquisitor's Battle Cowl
+								i(147516),	-- Legion Stalker's Hood
+								i(147515),	-- Hellblaze Invader's Greaves
+								i(147514),	-- Felguard Destroyer's Legplates
+							},
+							["crs"] = {
+								118704, -- Dul'zak
+								118723, -- Gazerax
+								118705, -- Nal'asha
+								120713, -- Wa'glur
+								120716, -- Dreadspeaker Serilis
+								120715, -- Raga'yut
+								120717, -- Mistress Dominix
+							},
+						}),
+						cr(117193, e(1905, {	-- Agronox
+							i(144469),	-- Caretaker's Budding Mantle
+							i(144491),	-- Choking Vine Bindings
+							i(144490),	-- Corruption-Fused Stompers
+							i(144468),	-- Felpruner's Shroud
+							i(144470),	-- Grovetender's Handwraps
+							i(144497),	-- Petrified Hust Girdle
+							i(144477),	-- Splinters of Agronox
+							i(144483),	-- Spore-Coated Vest
+							i(144498),	-- Vambraces of Fel Crust
+						})),
+						cr(117194, e(1906, {	-- Thrashbite the Scornful
+							i(144492),	-- Chain-Draped Gauntlets
+							i(144482),	-- Fel-Oiled Infernal Machine
+							i(144493),	-- Link-Lashed Spaulders
+							i(144479),	-- Master Thrasher's Lockcollar
+							i(144485),	-- Mo'arg Heavyweight's Wristwraps
+							i(144471),	-- Pulverizing Felsoles
+							i(144484),	-- Tattered Scornful Leggings
+							i(144499),	-- Tenderized Breastplate
+							i(144472),	-- Thrashbite's Cummerbund
+						})),
+						cr(119542, e(1904, {	-- Domatrax
+							i(144496),	-- Burning Chain Waistguard
+							i(144476),	-- Cloak of the Unending Assault
+							i(144475),	-- Dark Mistress' Cuffs
+							i(144495),	-- Domatrax's Chain Hauberk
+							i(144488),	-- Feaster Hide Gloves
+							i(144503),	-- Helm of Domatrax
+							i(144481),	-- Ring of Fel Domination
+							i(144474),	-- Sheer Felwoven Hosiery
+							i(144489),	-- Shoulderguards of Legion Domination
+							i(144500),	-- Two-Toed Trashstompers
+						})),
+						cr(120793, e(1878, {	-- Mephistroth
+							ach(11700),	-- Cathedral of Eternal Night
+							ach(11701),	-- Heroic: Cathedral of Eternal Night
+							i(141591),	-- Technique: Codex of the Tranquil Mind (Rank 2)
+							i(141592),	-- Technique: Codex of the Tranquil Mind (Rank 3)
+							i(144478),	-- Band of Dark Solitude
+							i(144486),	-- Cerulean Shadowfade Belt
+							i(144501),	-- Dread Commander's Pauldrons
+							i(144480),	-- Dreadstone of Endless Shadows
+							i(144494),	-- Imposing Dreadsoul Headgear
+							i(144473),	-- Nathrezim Acolyte's Robes
+							i(144502),	-- Shadowed Grips of the Nathrezim
+							i(144487),	-- Treads of Creeping Shadows
+						})),
 					}),
 					d(23, {	-- Mythic
-						["groups"] = {
-							cr(117193, e(1905, {	-- Agronox
-								ach(11768),	-- Boom Boom
-								i(144469),	-- Caretaker's Budding Mantle
-								i(144491),	-- Choking Vine Bindings
-								i(144490),	-- Corruption-Fused Stompers
-								i(144468),	-- Felpruner's Shroud
-								i(144470),	-- Grovetender's Handwraps
-								i(144497),	-- Petrified Hust Girdle
-								i(144477),	-- Splinters of Agronox
-								i(144483),	-- Spore-Coated Vest
-								i(144498),	-- Vambraces of Fel Crust
-							})),
-							cr(117194, e(1906, {	-- Thrashbite the Scornful
-								ach(11769),	-- A Steamy Romance Saga
-								i(144492),	-- Chain-Draped Gauntlets
-								i(144482),	-- Fel-Oiled Infernal Machine
-								i(144493),	-- Link-Lashed Spaulders
-								i(144479),	-- Master Thrasher's Lockcollar
-								i(144485),	-- Mo'arg Heavyweight's Wristwraps
-								i(144471),	-- Pulverizing Felsoles
-								i(144484),	-- Tattered Scornful Leggings
-								i(144499),	-- Tenderized Breastplate
-								i(144472),	-- Thrashbite's Cummerbund
-							})),
-							cr(119542, e(1904, {	-- Domatrax
-								i(144496),	-- Burning Chain Waistguard
-								i(144476),	-- Cloak of the Unending Assault
-								i(144475),	-- Dark Mistress' Cuffs
-								i(144495),	-- Domatrax's Chain Hauberk
-								i(144488),	-- Feaster Hide Gloves
-								i(144503),	-- Helm of Domatrax
-								i(144481),	-- Ring of Fel Domination
-								i(144474),	-- Sheer Felwoven Hosiery
-								i(144489),	-- Shoulderguards of Legion Domination
-								i(144500),	-- Two-Toed Trashstompers
-							})),
-							cr(120793, e(1878, {	-- Mephistroth
-								ach(11703),	-- Master of Shadows
-								ach(11702),	-- Mythic: Cathedral of Eternal Night
-								i(141591),	-- Technique: Codex of the Tranquil Mind (Rank 2)
-								i(141592),	-- Technique: Codex of the Tranquil Mind (Rank 3)
-								i(144478),	-- Band of Dark Solitude
-								i(144486),	-- Cerulean Shadowfade Belt
-								i(144501),	-- Dread Commander's Pauldrons
-								i(144480),	-- Dreadstone of Endless Shadows
-								i(144494),	-- Imposing Dreadsoul Headgear
-								i(144473),	-- Nathrezim Acolyte's Robes
-								i(144502),	-- Shadowed Grips of the Nathrezim
-								i(144487),	-- Treads of Creeping Shadows
-							})),
-						},
+						n(0, { -- Zone Drops
+							["groups"] = {
+								i(147517),	-- Inquisitor's Battle Cowl
+								i(147516),	-- Legion Stalker's Hood
+								i(147515),	-- Hellblaze Invader's Greaves
+								i(147514),	-- Felguard Destroyer's Legplates
+							},
+							["crs"] = {
+								118704, -- Dul'zak
+								118723, -- Gazerax
+								118705, -- Nal'asha
+								120713, -- Wa'glur
+								120716, -- Dreadspeaker Serilis
+								120715, -- Raga'yut
+								120717, -- Mistress Dominix
+							},
+						}),
+						cr(117193, e(1905, {	-- Agronox
+							ach(11768),	-- Boom Boom
+							i(144469),	-- Caretaker's Budding Mantle
+							i(144491),	-- Choking Vine Bindings
+							i(144490),	-- Corruption-Fused Stompers
+							i(144468),	-- Felpruner's Shroud
+							i(144470),	-- Grovetender's Handwraps
+							i(144497),	-- Petrified Hust Girdle
+							i(144477),	-- Splinters of Agronox
+							i(144483),	-- Spore-Coated Vest
+							i(144498),	-- Vambraces of Fel Crust
+						})),
+						cr(117194, e(1906, {	-- Thrashbite the Scornful
+							ach(11769),	-- A Steamy Romance Saga
+							i(144492),	-- Chain-Draped Gauntlets
+							i(144482),	-- Fel-Oiled Infernal Machine
+							i(144493),	-- Link-Lashed Spaulders
+							i(144479),	-- Master Thrasher's Lockcollar
+							i(144485),	-- Mo'arg Heavyweight's Wristwraps
+							i(144471),	-- Pulverizing Felsoles
+							i(144484),	-- Tattered Scornful Leggings
+							i(144499),	-- Tenderized Breastplate
+							i(144472),	-- Thrashbite's Cummerbund
+						})),
+						cr(119542, e(1904, {	-- Domatrax
+							i(144496),	-- Burning Chain Waistguard
+							i(144476),	-- Cloak of the Unending Assault
+							i(144475),	-- Dark Mistress' Cuffs
+							i(144495),	-- Domatrax's Chain Hauberk
+							i(144488),	-- Feaster Hide Gloves
+							i(144503),	-- Helm of Domatrax
+							i(144481),	-- Ring of Fel Domination
+							i(144474),	-- Sheer Felwoven Hosiery
+							i(144489),	-- Shoulderguards of Legion Domination
+							i(144500),	-- Two-Toed Trashstompers
+						})),
+						cr(120793, e(1878, {	-- Mephistroth
+							ach(11703),	-- Master of Shadows
+							ach(11702),	-- Mythic: Cathedral of Eternal Night
+							i(141591),	-- Technique: Codex of the Tranquil Mind (Rank 2)
+							i(141592),	-- Technique: Codex of the Tranquil Mind (Rank 3)
+							i(144478),	-- Band of Dark Solitude
+							i(144486),	-- Cerulean Shadowfade Belt
+							i(144501),	-- Dread Commander's Pauldrons
+							i(144480),	-- Dreadstone of Endless Shadows
+							i(144494),	-- Imposing Dreadsoul Headgear
+							i(144473),	-- Nathrezim Acolyte's Robes
+							i(144502),	-- Shadowed Grips of the Nathrezim
+							i(144487),	-- Treads of Creeping Shadows
+						})),
 					}),
 				},
 				["lvl"] = 110,
--- contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Deadwind Pass.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Deadwind Pass.lua
@@ -33,10 +33,25 @@ _.Zones =
 						["classes"] = { 4 },	-- Rogue
 						["sourceQuests"] = { 30108 },	-- Our Man in Karazhan
 					})),
+					{
+						["questID"] = 44865,	-- Butler to the Great
+						["sourceQuest"] = 44803,	-- Return to Karazhan: Master of the House
+						["qg"] = 115551,	-- Phantom Valet
+						["groups"] = {
+							follower(733),	-- Moroes
+						},
+					},
 --[[
 					qg(114550, qa(40717)),	-- Calling of the Council
 					qg(114631, qa(44684)),	-- Corruption Runs Deep
-					qg(114631, qa(44764)),	-- Demon in Disguise
+					]]--
+					a({
+						["questID"] = 44764,	-- Demon in Disguise
+						["sourceQuest"] = 44686,	-- Thought Collection
+						["qg"] = 114631,	-- Archmage Karlain <Kirin Tor>
+						["lvl"] = 110,
+					}),
+					--[[
 					qart(qg(100729, q(40604, {	-- Disturbing the Past
 						["classes"] = {9},
 					}))),
@@ -56,7 +71,17 @@ _.Zones =
 					qart(qg(100323, q(40834, {	-- Following the Curse
 						["classes"] = {11},
 					}))),
-					qg(114310, q(44734)),	-- Fragments of the Past
+					]]--
+					{
+						["questID"] = 44734,	-- Fragments of the Past
+						["sourceQuest"] = 44733,	-- The Power of Corruption
+						["qg"] = 114310,	-- Archmage Khadgar
+						["lvl"] = 110,
+						["groups"] = {
+							i(142460),	-- Violet Seal of the Archmage
+						},
+					},
+					--[[
 					qg(101547, q(40755)),	-- Hiding in the Stacks
 					qg(114310, q(44683)),	-- Holding the Lines
 					qg(18255, q(45296, {	-- No Bones About It
@@ -69,22 +94,53 @@ _.Zones =
 					qg(18253, q(11032)),	-- Protector No More
 					qg(114310, q(44685)),	-- Reclaiming the Ramparts
 					qg(18253, q(11034)),	-- Restorer No More
-					qg(18255, q(45291)),	-- Return to Karazhan: Book Wyrms
-					qg(18255, q(45295)),	-- Return to Karazhan: Clearing Out the Cobwebs
-					qg(18255, q(45294)),	-- Return to Karazhan: High Stress Hiatus
-					qg(114310, q(44735)),	-- Return to Karazhan: In the Eye of the Beholder					
-					n(114312, {	-- Moroes
-						["groups"] = {
-							i(142246, {	-- Broken Pocket Watch
-								q(44803, {	-- Return to Karazhan: Master of the House
-									q(44865),	-- Butler to the Great
-								}),
-							}),
+					]]--
+					{
+						["questID"] = 45291,	-- Return to Karazhan: Book Wyrms
+						["sourceQuests"] = {
+							44734,	-- Fragments of the Past
+							44735,	-- Return to Karazhan: In the Eye of the Beholder
 						},
-						["description"] = "This questline rewards Moroes as a Class Champion.|r",
-					}),
-					qg(18255, q(45293)),	-- Return to Karazhan: New Shoes
-					qg(18255, q(45292)),	-- Return to Karazhan: Rebooting the Cleaner
+						["qg"] = 18255,	-- Mage Darius
+						["lvl"] = 110,
+					},
+					{
+						["questID"] = 45295,	-- Return to Karazhan: Clearing Out the Cobwebs
+						["sourceQuest"] = 45294,	-- Return to Karazhan: High Stress Hiatus
+						["qg"] = 18255,	-- Mage Darius
+						["lvl"] = 110,
+					},
+					{
+						["questID"] = 45294,	-- Return to Karazhan: High Stress Hiatus
+						["sourceQuest"] = 45293,	-- Return to Karazhan: New Shoes
+						["qg"] = 18255,	-- Mage Darius
+						["lvl"] = 110,
+						["groups"] = {
+							i(142468),	-- Violet Seal of the Archmage
+						},
+					},
+					{
+						["questID"] = 45293,	-- Return to Karazhan: New Shoes
+						["sourceQuest"] = 45292,	-- Return to Karazhan: Rebooting the Cleaner
+						["qg"] = 18255,	-- Mage Darius
+						["lvl"] = 110,
+					},
+					{
+						["questID"] = 45292,	-- Return to Karazhan: Rebooting the Cleaner
+						["sourceQuest"] = 45291,	-- Return to Karazhan: Book Wyrms
+						["qg"] = 18255,	-- Mage Darius
+						["lvl"] = 110,
+						["groups"] = {
+							i(142461),	-- Violet Seal of the Archmage
+						},
+					},
+					{
+						["questID"] = 44735,	-- Return to Karazhan: In the Eye of the Beholder
+						["sourceQuest"] = 44733,	-- The Power of Corruption
+						["qg"] = 114310,	-- Archmage Khadgar
+						["lvl"] = 110,
+					},
+					--[[
 					qart(qg(101282, q(40900, {	-- The Burden Borne (map blank, not sure if starts in Deadwind Pass, a scenario, or elsewhere)
 						["classes"] = {11},
 					}))),
@@ -116,19 +172,28 @@ _.Zones =
 					qart(qg(100729, q(40611, {	-- The Fate of Deadwind
 						["classes"] = {9},
 					}))),
-					qg(114310, q(44733)),	-- The Power of Corruption
 					o(254007, {	-- Executioner's Altar
 						qart(q(44154, {	-- The Rite of the Executioner
 							["classes"] = {9},
 						})),
 					}),
-					qg(114631, qa(44686)),	-- Thought Collection
 					o(245793, {	-- Battered Journal
 						qart(q(40606, {	-- To Point the Way
 						["classes"] = {9},
 						})),
 					}),
---]]					
+					]]--
+					a({
+						["questID"] = 44686,	-- Thought Collection
+						["qg"] = 114631,	-- Archmage Karlain <Kirin Tor>
+						["lvl"] = 110,
+					}),
+					{
+						["questID"] = 44733,	-- The Power of Corruption
+						["sourceQuest"] = 44764,	-- Demon in Disguise
+						["qg"] = 114310,	-- Archmage Khadgar
+						["lvl"] = 110,
+					},
 					q(40712, {	-- The Power Possessed
 						["qg"] = 101282, -- Revil Kost
 						["classes"] = { 9 },
@@ -156,9 +221,17 @@ _.Zones =
 						["classes"] = { 4 },	-- Rogue
 						["sourceQuests"] = { 30109 },	-- Blood of the Betrayer
 					})),
-					o(258978, 	-- Waterlogged Tome
-						q(44487)	-- Waterlogged Tome
-					),
+					{
+						["objectID"] = 258978,	-- Waterlogged Tome
+						["questID"] = 44487,	-- Waterlogged Tome
+						["lvl"] = 110,
+						["groups"] = {
+							{
+								["achievementID"] = 11240,	-- Harbinger
+								["criteriaID"] = 1,	-- Waterlogged Tome
+							},
+						},
+					},
 				}),
 				n(-77, { 	-- Special
 					n(-79, { 	-- Puzzler's Desire
@@ -176,6 +249,7 @@ _.Zones =
 						i(33165),	-- Formula: Enchant Weapon - Greater Agility
 						i(33124), 	-- Pattern: Cloak of Darkness
 						i(33205), 	-- Pattern: Shadowprowler's Chestguard
+						i(29187),	-- Inscription of Endurance
 					}),
 				}),
 				n(0,   {	-- Zone Drop
