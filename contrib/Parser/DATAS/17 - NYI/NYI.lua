ad8bc44eb7c50b39a2337fea13165d9a38a4a9d7
Gold-DieAldor <goldenshacal@gmail.com>
2018-12-30 02:41:12 +0100

    maybe fel bat (test) will be nice and remove it self from unsorted itself fixed war glaives icon (path is correct, but did not work ingame)

--- contrib/Parser/DATAS/17 - NYI/NYI.lua
+++ contrib/Parser/DATAS/17 - NYI/NYI.lua
@@ -596,6 +596,7 @@ _.NeverImplemented =
 			},
 			["u"] = 1,
 		}))),
+		un(1, spell(215545)),	-- Fel Bat (Test) (Actual name of the Item)
 		un(1, model(17890, i(25596))), 	-- Peep's Whistle	 					(Never added in Game)
 		un(1, i(8627)),	-- Reins of the Nightsaber								(Never added in Game)
 		un(1, a(model(6447, i(8628)))),	-- Reins of the Spotted Nightsaber 		(Never added in Game)
