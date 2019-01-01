e53dc685349bc70c2edf38cb6141e91ba541ce2b
Pr3vention <Pr3vention@gmail.com>
2019-01-01 09:39:16 -0600

    Added a missing map to Stormheim for Aggramar's Vault

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Stormheim.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Stormheim.lua
@@ -2199,9 +2199,12 @@ _.Zones =
 					},
 				}),
 			},
-			["lvl"] = 98,	
+			["lvl"] = 98,
+			["maps"] = {
+				639,	-- Aggramar's Vault
+			},
 			["achievementID"] = 10668,
-			["description"] = "|cff66ccffStormheim is a zone in the eastern area of the Broken Isles situated in the shadow of Highmountain to the west, and Suramar to the southwest. It's home to the mighty vrykul who left Northrend many ages ago in search of their holy land. Two titanic watchers here have warred with each other for thousands of years. The Halls of Valor and Helheim, the realms to which the vrykul journey when they die, are located in this zone.|r",				
+			["description"] = "|cff66ccffStormheim is a zone in the eastern area of the Broken Isles situated in the shadow of Highmountain to the west, and Suramar to the southwest. It's home to the mighty vrykul who left Northrend many ages ago in search of their holy land. Two titanic watchers here have warred with each other for thousands of years. The Halls of Valor and Helheim, the realms to which the vrykul journey when they die, are located in this zone.|r",
 		}),	
 	}),
 };
