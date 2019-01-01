4f505053a78118862088b081fc8f76724d2ca5e8
Pr3vention <Pr3vention@gmail.com>
2018-12-31 22:53:51 -0600

    Darkshore achieves should now show up in the warfront, minor cleanup
    - Moved the Darkshore victory file to the Darkshore Warfront directory to match the structure of Arathi
    - Reduced some of the unnecessary duplication in the Arathi warfront files

---
+++ contrib/Parser/DATAS/06 - Expansion Features/07 War Effort/Darkshore Warfront/Achievements.lua
@@ -0,0 +1,66 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+
+_.ExpansionFeatures =
+{
+	n(-10057, {	-- War Effort
+		["groups"] = {
+			n(-237, {	-- War Front: The Battle for Darkshore
+				["groups"] = {
+					n(-4, {	-- Achievements
+						a(ach(13310, {	-- By Nature's Call
+							crit(1),	-- Dryad
+							crit(2),	-- Faerie Dragon
+							crit(3),	-- Keeper of the Grove
+						})),
+						h(ach(13309, {	-- Deforester
+							crit(1),	-- Banshee
+							crit(2),	-- AzerMEK Prototype
+							crit(3),	-- Abomination
+						})),
+						a(ach(13305, {	-- Night Moves
+							crit(1),	-- Bashal'Aran
+							crit(2),	-- Gloomtide Strand
+							crit(3),	-- Cinderfall Grove
+							crit(4),	-- Lornesta Mine
+							crit(5),	-- Forlorn Crossing
+							crit(6),	-- Ashwood Depot
+							crit(7),	-- Lor'danel Landing
+							a(ach(13302)),	-- Under Cover of Darkness
+						})),
+						h(ach(13306, {	-- Night Moves
+							crit(1),	-- Bashal'Aran
+							crit(2),	-- Gloomtide Strand
+							crit(3),	-- Cinderfall Grove
+							crit(4),	-- Lornesta Mine
+							crit(5),	-- Forlorn Crossing
+							crit(6),	-- Ashwood Depot
+							crit(7),	-- Lor'danel Landing
+							h(ach(13301)),	-- Under Cover of Darkness
+						})),
+						a(ach(13308, {	-- Recruitment Officer
+							crit(1),	-- Kaldorei Archer
+							crit(2),	-- Druid of the Branch
+							crit(3),	-- Kaldorei Hunter
+							crit(4),	-- Kaldorei Sentinel
+							crit(5),	-- Glaive Thrower
+						})),
+						h(ach(13307, {	-- Recruitment Officer
+							crit(1),	-- Forsaken Alchemist
+							crit(2),	-- Forsaken Lancer
+							crit(3),	-- Forsaken Deathguard
+							crit(4),	-- Forsaken Deadshot
+							crit(5),	-- Catapult
+						})),
+						a(ach(13297)),	-- War for the Shore (Alliance)
+						h(ach(13296)),	-- War for the Shore (Horde)
+					}),
+				},
+				["maps"] = {
+					1203,
+				},
+			}),
+		},
+	}),
+};
