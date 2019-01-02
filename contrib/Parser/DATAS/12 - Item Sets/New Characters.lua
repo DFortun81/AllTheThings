672ac225f590c51839f445307c9161eec53780cc
Pr3vention <Pr3vention@gmail.com>
2019-01-01 23:14:37 -0600

    Added yet another missing mapID for dark iron dwarves. Thanks to Evelynn for info

--- contrib/Parser/DATAS/12 - Item Sets/New Characters.lua
+++ contrib/Parser/DATAS/12 - Item Sets/New Characters.lua
@@ -134,6 +134,7 @@ _.GearSets =
 			652,	-- Thunder Totem (Highmountain Taurens)
 			1186,	-- Blackrock Depths (Dark Iron Dwarves)
 			1159,	-- Blackrock Depths (Dark Iron Dwarves scenario)
+			1160,	-- Blackrock Depths (Dark Iron Dwarves scenario)
 		},
 	}),
 };
