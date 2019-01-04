ee3fb8c579ee4f35e203da902a651eb7b034ee82
Pr3vention <Pr3vention@gmail.com>
2019-01-03 18:13:47 -0600

    Added 4 missing mapIDs to Tiragarde Sound

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/04 Tiragarde Sound/0 - Metadata.lua
@@ -0,0 +1,19 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(876, { 	-- Kul'Tiras
+		m(895, {	-- Tiragarde Sound
+			["achievementID"] = 12556,	-- Explore Tiragarde Sound
+			["lvl"] = 110,
+			["maps"] = {
+				1171,	-- Gol Thovas (lower)
+				1172,	-- Gol Thovas (upper)
+				1184,	-- Winterchill Mine (lower)
+				1185,	-- Winterchill Mine (upper)
+			},
+			["description"] = "|cff66ccffTiragarde Sound sits on an inlet in the middle of Kul Tiras. It connects the Great Sea from one side of the island to the other, and is a connection point for all the major regions of Kul Tiras. The region is notably the seat of House Proudmoore. Boralus, the capital of Kul Tiras, is located here.\n\nThe people of Tiragarde Sound are renowned monster hunters, raging battle against the creatures of the sea, and struggle every day against pirates at their own shores. While the pirates were once content to stay at Freehold, in recent months, they have grown bolder, and began attacking Tiragarde Sound. During the war between the Alliance and Horde, the area served as the Alliance's staging point and home base.|r",
+		}),
+	}),
+};
