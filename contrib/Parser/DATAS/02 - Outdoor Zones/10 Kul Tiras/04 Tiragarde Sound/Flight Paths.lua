77b537456c400a645987d04d6696dd1e3845e641
Pr3vention <Pr3vention@gmail.com>
2019-01-03 21:04:21 -0600

    Removed duplication of metadata in Tiragarde Sound

--- contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/04 Tiragarde Sound/Flight Paths.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/04 Tiragarde Sound/Flight Paths.lua
@@ -8,85 +8,63 @@ _.Zones =
 			["groups"] = {
 				n(-228, {	-- Flight Paths
 					fp(2052, { -- Anglepoint Wharf, Tiragarde Sound (Ferry) [A]
-						["description"] = "Anglepoint Wharf, Tiragarde Sound (Ferry)",
 						["coord"] = { 42, 30.6 },
-						--["races"] = ALLIANCE_ONLY,
 					}),
 					fp(2074, { -- Bridgeport, Tiragarde Sound [A]
-						["description"] = "Bridgeport, Tiragarde Sound",
 						["coord"] = { 75.8, 48.6 },
 					}),
 					fp(2077, { -- Castaway Point, Tiragarde Sound [N]
-						["description"] = "Castaway Point, Tiragarde Sound",
 						["coords"] = {
 							{ 86.43, 80.81, 895 },
 						},
 					}),
 					fp(2104, { -- Eastpoint Station, Tiragarde Sound (Ferry) [A]
-						["description"] = "Eastpoint Station (Ferry)",
 						["coord"] = { 74.2, 44.2 },
-						--["races"] = ALLIANCE_ONLY,
 					}),
 					fp(2054, { -- Firebreaker Expedition, Tiragarde Sound (Ferry) [A]
-						["description"] = "Firebreaker Expedition, Tiragarde Sound (Ferry)",
 						["coord"] = { 63.8, 30.2 },
-						--["races"] = ALLIANCE_ONLY,
 					}),
 					fp(2023, { -- Freehold, Tiragarde Sound [N]
-						["description"] = "Freehold, Tiragarde Sound",
 						["coords"] = {
 							{ 77.04, 82.91, 895 },
 						},
 					}),
 					fp(2060, { -- Hatherford, Tiragarde Sound [A]
-						["description"] = "Hatherford, Tiragarde Sound",
 						["coord"] = { 66.8, 23.0 },
 					}),
 					fp(2079, { -- Kennings Lodge, Tiragarde Sound [A]
-						["description"] = "Kennings Lodge, Tiragarde Sound",
 						["coord"] = { 76.6, 65.4 },
 					}),
 					fp(2084, { -- Norwington Estate, Tiragarde Sound [A]
-						["description"] = "Norwington Estate, Tiragarde Sound",
 						["coords"] = {
 							{ 52.91, 28.81, 895 },
 						},
 					}),
 					fp(2053, { -- Old Drust Road, Tiragarde Sound (Ferry) [A]
-						["description"] = "Old Drust Road, Tiragarde Sound (Ferry)",
 						["coord"] = { 54, 53.2 },
-						--["races"] = ALLIANCE_ONLY,
 					}),
 					fp(2087, { -- Outrigger Post, Tiragarde Sound [A]
-						["description"] = "Outrigger Post, Tiragarde Sound",
 						["coords"] = {
 							{ 35.55, 24.9, 895 },
 						},
 					}),
 					fp(2140, { -- Plunder Harbor, Tiragarde Sound [H] [NOTE: Verification Required, this may be locked behind the War Campaign quests]
-						["description"] = "Plunder Harbor, Tiragarde Sound",
 						["coord"] = { 87.2, 50.6 },
-						--["races"] = HORDE_ONLY,
 					}),
 					fp(2102, { -- Roughneck Camp, Tiragarde Sound [A]
-						["description"] = "Roughneck Camp, Tiragarde Sound",
 						["coord"] = { 42.4, 23.0 },
 					}),
 					fp(2055, { -- Southwind Station, Tiragarde Sound (Ferry) [A]
-						["description"] = "Southwind Station, Tiragarde Sound (Ferry)",
 						["coord"] = { 66.6, 49.8 },
-						--["races"] = ALLIANCE_ONLY,
 					}),
 					fp(2279, { -- Stonefist Watch, Tiragarde Sound [H]
 						["description"] = "Stonefist Watch, Tiragarde Sound \n\nRequires completion of Mission Report: Stonefist Watch and related quest line to unlock.",
 						["coord"] = { 53.2, 63 },
 					}),
 					fp(2042, { -- Vigil Hill, Tiragarde Sound [A]
-						["description"] = "Vigil Hill, Tiragarde Sound",
 						["coord"] = { 57.6, 61.6 },
 					}),
 					fp(2273, { -- Waning Glacier, Tiragarde Sound [H]
-						["description"] = "Waning Glacier, Tiragarde Sound",
 						["coord"] = { 39.6, 18.4 },
 					}),
 					fp(2062, { -- Wolf's Den, Tiragarde Sound [H]
@@ -95,10 +73,6 @@ _.Zones =
 					}),
 				}),
 			},
-			["achievementID"] = 12556,	-- Explore Tiragarde Sound
-			["lvl"] = 110,
-			["description"] = "|cff66ccffTiragarde Sound sits on an inlet in the middle of Kul Tiras. It connects the Great Sea from one side of the island to the other, and is a connection point for all the major regions of Kul Tiras. The region is notably the seat of House Proudmoore. Boralus, the capital of Kul Tiras, is located here.\n\nThe people of Tiragarde Sound are renowned monster hunters, raging battle against the creatures of the sea, and struggle every day against pirates at their own shores. While the pirates were once content to stay at Freehold, in recent months, they have grown bolder, and began attacking Tiragarde Sound. During the war between the Alliance and Horde, the area served as the Alliance's staging point and home base.|r",
 		}),
 	}),
 };
---]]
--- contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/04 Tiragarde Sound/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/04 Tiragarde Sound/Quests.lua
@@ -834,10 +834,6 @@ _.Zones =
 					},
 				}),
 			},
-			["achievementID"] = 12556,	-- Explore Tiragarde Sound
-			["lvl"] = 110,
-			["description"] = "|cff66ccffTiragarde Sound sits on an inlet in the middle of Kul Tiras. It connects the Great Sea from one side of the island to the other, and is a connection point for all the major regions of Kul Tiras. The region is notably the seat of House Proudmoore. Boralus, the capital of Kul Tiras, is located here.\n\nThe people of Tiragarde Sound are renowned monster hunters, raging battle against the creatures of the sea, and struggle every day against pirates at their own shores. While the pirates were once content to stay at Freehold, in recent months, they have grown bolder, and began attacking Tiragarde Sound. During the war between the Alliance and Horde, the area served as the Alliance's staging point and home base.|r",
 		}),
 	}),
 };
---]]
