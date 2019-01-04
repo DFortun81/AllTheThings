5fe1553bff9ebaa4a2e253642de63cd4e3f2172e
Pr3vention <Pr3vention@gmail.com>
2019-01-03 21:00:49 -0600

    Added mapID for old ironforge

--- contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/1 - Ironforge/Ironforge.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/1 - Ironforge/Ironforge.lua
@@ -1453,6 +1453,9 @@ _.Zones =
 				}),
 			},
 			["lvl"] = 1,
+			["maps"] = {
+				1361,	-- Old Ironforge
+			},
 			["isRaid"] = true,
 			["icon"] = "Interface\\Icons\\Inv_misc_tournaments_symbol_dwarf",
 			["description"] = "|cff66ccffIronforge is the capital city of the dwarves, proud members of the Alliance. It is the ancestral home of the Bronzebeard dwarves. The Council of Three Hammers rules the kingdom of Khaz Modan from the throne room within the city.|r",			
