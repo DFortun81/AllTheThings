acaead0d5a5e03079d77ccd714d2e58bedea71d7
Gold-DieAldor <goldenshacal@gmail.com>
2019-01-10 09:31:22 +0100

    added shared zandalar world quest rewards under a zandalar header instead listining each reward in each zone reduces bloat by 60 items

--- contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/03 Vol'dun/World Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/03 Vol'dun/World Quests.lua
@@ -197,37 +197,6 @@ _.Zones =
 								},
 							},
 						}),
-						i(158116),	-- Bonehex Claws
-						i(158125),	-- Bonehex Warmaul
-						i(158126),	-- Dazar'alor Polearm
-						i(158124),	-- Dinobone Cracker
-						i(162458),	-- Gilded Hex-Maw
-						i(162455),	-- Golden Empire Buckler
-						i(158135),	-- Golden Empire Broadsword
-						i(158136),	-- Golden Empire Hexer
-						i(162457),	-- Loa-Bound Defender
-						i(162459),	-- Loa-Pledged Rod
-						i(158110),	-- Razorbone Carver
-						i(158118),	-- Razorbone Claws
-						i(158123),	-- Razorbone Skullcracker
-						i(158127),	-- Razorbone Spear
-						i(158120),	-- Salt Coast Headcracker
-						i(158117),	-- Salt Coast Knuckles
-						i(158134),	-- Salt Coast Piercers
-						i(158122),	-- Spirit Bog Scepter
-						i(158108),	-- Whisperfen Chopper
-						i(158111),	-- Whisperfen Hewer
-						i(158128),	-- Whisperfen Polearm
-						i(158131),	-- Whisperfen Staff
-						i(158112),	-- Zandalari Greatbow
-						i(158133),	-- Zandalari Machete
-						i(158129),	-- Zandalari Warstaff
-						i(158115),	-- Zem'lan Cutter
-						i(158109),	-- Zem'lan Hacker
-						i(158132),	-- Zem'lan Slicer
-						i(158121),	-- Zocalo Crusher
-						i(158130),	-- Zocalo Pummeler
-						i(158114),	-- Zocalo Stabber
 						i(166673),	-- Devoted Lookout's Windcloak
 						i(158155),	-- Dinobone Charm
 						i(158151),	-- Zandalari Band
