2eccee2009b156fa8650ca5535abf78998395bb9
Gold-DieAldor <goldenshacal@gmail.com>
2019-01-10 10:24:40 +0100

    same as last change

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/World Quests.lua
@@ -0,0 +1,43 @@
+---------------------------------------------------
+--          Z O N E S       M O D U L E          --
+---------------------------------------------------
+_.Zones =
+{
+	m(876, { 	-- Kul'Tiras
+		["groups"] = {
+			n(-34, {	-- World Quests
+				i(158092),	-- Colscale Cudgel
+				i(159819),	-- Coldscale Lantern
+				i(158098),	-- Coralshell Halberd
+				i(158105),	-- Coralshell Spellblade
+				i(158107),	-- Deepwarden Baton
+				i(158089),	-- Deepwarden Fangs
+				i(158091),	-- Dockyard Mace
+				i(155545),	-- Drustwrought Spellstaff
+				i(159798),	-- Gnarlwood Barrier
+				i(158104),	-- Gnarlwood Cutlass
+				i(158086),	-- Gnarlwood Dagger
+				i(158095),	-- Gnarlwood Hammer
+				i(158102),	-- Gnarlwood Staff
+				i(158087),	-- Gol Osigr Handblade
+				i(158101),	-- Gol Osigr Pillar
+				i(159804),	-- Ironcrest Bulwark
+				i(158106),	-- Ironcrest Greatblade
+				i(158090),	-- Ironcrest Longrifle
+				i(158094),	-- Shipwrecker Maul
+				i(158093),	-- Stagheart Gavel
+				i(158079),	-- Stagheart Hatcheet
+				i(158099),	-- Stagheart Poleaxe
+				i(158097),	-- Tideguard Pike
+				i(158100),	-- Tideguard Spire
+				i(159816),	-- Wavecaller Beacon
+				i(158096),	-- Wavecaller Greatmace
+				i(158084),	-- Wavecaller Speargun
+				i(158085),	-- Wintersail Dirk
+				i(158103),	-- Wintersail Saber
+				i(158088),	-- Wintersail Striker
+			}),
+		},
+	}),
+};
+--]]
