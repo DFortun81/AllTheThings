c5eb1d0e58e71618aec7404424f350fcda7c5e10
Lucetia-Detheroc <aceso2896@gmail.om>
2019-01-13 00:29:31 -0800

    - Telogrus Rift added

--- contrib/Parser/DATAS/12 - Item Sets/Allied Races/Heritage of the Ren'dorei.lua
+++ contrib/Parser/DATAS/12 - Item Sets/Allied Races/Heritage of the Ren'dorei.lua
@@ -6,19 +6,41 @@ _.GearSets =
 	gsh(1522, { -- Allied Races
 		["groups"] = {
 			gs(1524, { -- Heritage of the Ren'dorei
-				["groups"] = {
-					i(156690),	-- Ren'dorei Helm
-					i(156691),	-- Ren'dorei Pauldrons
-					i(158917),	-- Ren'dorei Cloak
-					i(156692),	-- Ren'dorei Mantle
-					i(157758),	-- Ren'dorei Tabard
-					i(156697),	-- Ren'dorei Bracers
-					i(156693),	-- Ren'dorei Gloves
-					i(156694),	-- Ren'dorei Belt
-					i(156695),	-- Ren'dorei Leggings
-					i(156696),	-- Ren'dorei Boots
+				["races"] = {
+					29,		-- Void Elf
 				},
-				["races"] = { 29 },
+				["groups"] = {
+					{	-- Ren'dorei Helm
+						["itemID"] = 156690,	-- Ren'dorei Helm
+					},
+					{	-- Ren'dorei Pauldrons
+						["itemID"] = 156691,	-- Ren'dorei Pauldrons
+					},
+					{	-- Ren'dorei Cloak
+						["itemID"] = 158917,	-- Ren'dorei Cloak
+					},
+					{	-- Ren'dorei Mantle
+						["itemID"] = 156692,	-- Ren'dorei Mantle
+					},
+					{	-- Ren'dorei Tabard
+						["itemID"] = 157758,	-- Ren'dorei Tabard
+					},
+					{	-- Ren'dorei Bracers
+						["itemID"] = 156697,	-- Ren'dorei Bracers
+					},
+					{	-- Ren'dorei Gloves
+						["itemID"] = 156693,	-- Ren'dorei Gloves
+					},
+					{	-- Ren'dorei Belt
+						["itemID"] = 156694,	-- Ren'dorei Belt
+					},
+					{	-- Ren'dorei Leggings
+						["itemID"] = 156695,	-- Ren'dorei Leggings
+					},
+					{	-- Ren'dorei Boots
+						["itemID"] = 156696,	-- Ren'dorei Boots
+					},
+				},
 			}),
 		},
 		["icon"] = "Interface\\Icons\\achievement_level_110",
