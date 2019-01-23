51174c32414ee21343a06ca429092338405084c8
Lucetia-Detheroc <aceso2896@gmail.om>
2019-01-22 19:04:35 -0800

    - Battle for Dazar'alor has it's coordinates now added - Battle for Dazar'alor has it's associated maps added

--- contrib/Parser/DATAS/01 - Dungeons Raids/8 - Battle For Azeroth/1 - Raids/04 Battle of Dazar'alor/00 - Metadata.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/8 - Battle For Azeroth/1 - Raids/04 Battle of Dazar'alor/00 - Metadata.lua
@@ -10,8 +10,18 @@ _.Instances =
 				["instanceID"] = 1176,	-- Battle of Dazar'alor
 				["isRaid"] = true,
 				["lvl"] = 120,
+				["coords"] = {
+					{ 38.81, 02.53, 1165 },
+				},
 				["maps"] = {
-
+					1352,	-- Port of Zandalar			//
+					1357,	-- The Zocalo
+					1353,	-- Halls of Opulence
+					1354,	-- Loa's Sanctum
+					1356,	-- Walk of Kings
+					1358,	-- Heart of the Empire		//
+					1364,	-- The Great Sea			//
+					1367,	-- Boralus					//
 				},
 			},
 		},
