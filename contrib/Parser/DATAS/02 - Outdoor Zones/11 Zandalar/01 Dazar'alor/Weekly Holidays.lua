5041cdd5376f8c32f4ffd3a4795b2a7cdeba8271
Gold-DieAldor <goldenshacal@gmail.com>
2019-01-25 16:53:43 +0100

    marked bfa s1 conquest gear as unobtainable (you'll probably get via emsembles later)

--- contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/01 Dazar'alor/Weekly Holidays.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/01 Dazar'alor/Weekly Holidays.lua
@@ -580,279 +580,371 @@ _.Zones =
 									["g"] = bubbleDown({["modID"] = 8}, {
 										{	-- Dread Gladiator's Barrier
 											["itemID"] = 161677,	-- Dread Gladiator's Barrier
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Bonegrinder
 											["itemID"] = 161684,	-- Dread Gladiator's Bonegrinder
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Cleaver
 											["itemID"] = 161679,	-- Dread Gladiator's Cleaver
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Decapitator
 											["itemID"] = 161690,	-- Dread Gladiator's Decapitator
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Gavel
 											["itemID"] = 161706,	-- Dread Gladiator's Gavel
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Longbow
 											["itemID"] = 161702,	-- Dread Gladiator's Longbow
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Pike
 											["itemID"] = 161685,	-- Dread Gladiator's Pike
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Reprieve
 											["itemID"] = 161678,	-- Dread Gladiator's Reprieve
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Rifle
 											["itemID"] = 161691,	-- Dread Gladiator's Rifle
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Shanker
 											["itemID"] = 162016,	-- Dread Gladiator's Shanker
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Shield Wall
 											["itemID"] = 161695,	-- Dread Gladiator's Shield Wall
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Slicer
 											["itemID"] = 161687,	-- Dread Gladiator's Slicer
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Spellblade
 											["itemID"] = 161707,	-- Dread Gladiator's Spellblade
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Staff
 											["itemID"] = 161686,	-- Dread Gladiator's Staff
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Wand
 											["itemID"] = 161689,	-- Dread Gladiator's Wand
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Warglaive
 											["itemID"] = 161693,	-- Dread Gladiator's Warglaive
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Cape
 											["itemID"] = 161907,	-- Dread Gladiator's Cape
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Drape
 											["itemID"] = 161906,	-- Dread Gladiator's Drape
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Greatcloak
 											["itemID"] = 161669,	-- Dread Gladiator's Greatcloak
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Shawl
 											["itemID"] = 161668,	-- Dread Gladiator's Shawl
-					},
+											["u"] = 2, 
+										},
 										{	-- Dread Gladiator's Silk Amice
 											["itemID"] = 161763,	-- Dread Gladiator's Silk Amice
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Bracers
 											["itemID"] = 161769,	-- Dread Gladiator's Silk Bracers
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Cord
 											["itemID"] = 161734,	-- Dread Gladiator's Silk Cord
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Cowl
 											["itemID"] = 161754,	-- Dread Gladiator's Silk Cowl
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Cuffs
 											["itemID"] = 161660,	-- Dread Gladiator's Silk Cuffs
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Gloves
 											["itemID"] = 161643,	-- Dread Gladiator's Silk Gloves
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Handguards
 											["itemID"] = 161752,	-- Dread Gladiator's Silk Handguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Hood
 											["itemID"] = 161645,	-- Dread Gladiator's Silk Hood
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Leggings
 											["itemID"] = 161649,	-- Dread Gladiator's Silk Leggings
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Mantle
 											["itemID"] = 161654,	-- Dread Gladiator's Silk Mantle
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Raiment
 											["itemID"] = 161742,	-- Dread Gladiator's Silk Raiment
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Robe
 											["itemID"] = 161633,	-- Dread Gladiator's Silk Robe
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Sash
 											["itemID"] = 161766,	-- Dread Gladiator's Silk Sash
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Slippers
 											["itemID"] = 161747,	-- Dread Gladiator's Silk Slippers
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Treads
 											["itemID"] = 161638,	-- Dread Gladiator's Silk Treads
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Silk Trousers
 											["itemID"] = 161758,	-- Dread Gladiator's Silk Trousers
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Armwraps
 											["itemID"] = 161772,	-- Dread Gladiator's Leather Armwraps
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Belt
 											["itemID"] = 161659,	-- Dread Gladiator's Leather Belt
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Bindings
 											["itemID"] = 161663,	-- Dread Gladiator's Leather Bindings
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Boots
 											["itemID"] = 161640,	-- Dread Gladiator's Leather Boots
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Footguards
 											["itemID"] = 161749,	-- Dread Gladiator's Leather Footguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Gloves
 											["itemID"] = 161641,	-- Dread Gladiator's Leather Gloves
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Handguards
 											["itemID"] = 161750,	-- Dread Gladiator's Leather Handguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Helm
 											["itemID"] = 161647,	-- Dread Gladiator's Leather Helm
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Hood
 											["itemID"] = 161756,	-- Dread Gladiator's Leather Hood
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Legguards
 											["itemID"] = 161651,	-- Dread Gladiator's Leather Legguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Mantle
 											["itemID"] = 161761,	-- Dread Gladiator's Leather Mantle
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Pants
 											["itemID"] = 161760,	-- Dread Gladiator's Leather Pants
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Spaulders
 											["itemID"] = 161652,	-- Dread Gladiator's Leather Spaulders
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Tunic
 											["itemID"] = 161636,	-- Dread Gladiator's Leather Tunic
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Vest
 											["itemID"] = 161745,	-- Dread Gladiator's Leather Vest
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Leather Waistband
 											["itemID"] = 161768,	-- Dread Gladiator's Leather Waistband
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Armbands
 											["itemID"] = 161662,	-- Dread Gladiator's Chain Armbands
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Armor
 											["itemID"] = 161635,	-- Dread Gladiator's Chain Armor
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Chestguard
 											["itemID"] = 161744,	-- Dread Gladiator's Chain Chestguard
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Clasp
 											["itemID"] = 161767,	-- Dread Gladiator's Chain Clasp
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Footguards
 											["itemID"] = 161748,	-- Dread Gladiator's Chain Footguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Gauntlets
 											["itemID"] = 161644,	-- Dread Gladiator's Chain Gauntlets
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Handguards
 											["itemID"] = 161753,	-- Dread Gladiator's Chain Handguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Headpiece
 											["itemID"] = 161755,	-- Dread Gladiator's Chain Headpiece
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Helm
 											["itemID"] = 161646,	-- Dread Gladiator's Chain Helm
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Leggings
 											["itemID"] = 161650,	-- Dread Gladiator's Chain Leggings
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Legguards
 											["itemID"] = 161759,	-- Dread Gladiator's Chain Legguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Sabatons
 											["itemID"] = 161639,	-- Dread Gladiator's Chain Sabatons
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Shoulderguards
 											["itemID"] = 161764,	-- Dread Gladiator's Chain Shoulderguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Spaulders
 											["itemID"] = 161655,	-- Dread Gladiator's Chain Spaulders
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Waistguard
 											["itemID"] = 161658,	-- Dread Gladiator's Chain Waistguard
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Chain Wristguards
 											["itemID"] = 161771,	-- Dread Gladiator's Chain Wristguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Armguards
 											["itemID"] = 161661,	-- Dread Gladiator's Plate Armguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Chestguard
 											["itemID"] = 161743,	-- Dread Gladiator's Plate Chestguard
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Chestpiece
 											["itemID"] = 161634,	-- Dread Gladiator's Plate Chestpiece
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Clasp
 											["itemID"] = 161765,	-- Dread Gladiator's Plate Clasp
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Gauntlets
 											["itemID"] = 161642,	-- Dread Gladiator's Plate Gauntlets
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Girdle
 											["itemID"] = 161656,	-- Dread Gladiator's Plate Girdle
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Gloves
 											["itemID"] = 161751,	-- Dread Gladiator's Plate Gloves
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Greaves
 											["itemID"] = 161746,	-- Dread Gladiator's Plate Greaves
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Headcover
 											["itemID"] = 161741,	-- Dread Gladiator's Plate Headcover
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Helm
 											["itemID"] = 161632,	-- Dread Gladiator's Plate Helm
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Leg Armor
 											["itemID"] = 161757,	-- Dread Gladiator's Plate Leg Armor
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Legguards
 											["itemID"] = 161648,	-- Dread Gladiator's Plate Legguards
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Pauldrons
 											["itemID"] = 161653,	-- Dread Gladiator's Plate Pauldrons
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Shoulders
 											["itemID"] = 161762,	-- Dread Gladiator's Plate Shoulders
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Vambraces
 											["itemID"] = 161770,	-- Dread Gladiator's Plate Vambraces
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Plate Warboots
 											["itemID"] = 161637,	-- Dread Gladiator's Plate Warboots
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Band
 											["itemID"] = 161672,	-- Dread Gladiator's Band
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Ring
 											["itemID"] = 161671,	-- Dread Gladiator's Ring
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Seal
 											["itemID"] = 161899,	-- Dread Gladiator's Seal
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Signet
 											["itemID"] = 161898,	-- Dread Gladiator's Signet
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Badge
 											["itemID"] = 161902,	-- Dread Gladiator's Badge
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Emblem
 											["itemID"] = 161675,	-- Dread Gladiator's Emblem
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Insignia
 											["itemID"] = 161676,	-- Dread Gladiator's Insignia
+											["u"] = 2, 
 										},
 										{	-- Dread Gladiator's Medallion
 											["itemID"] = 161674,	-- Dread Gladiator's Medallion
+											["u"] = 2, 
 										},
 									}),
 								},
