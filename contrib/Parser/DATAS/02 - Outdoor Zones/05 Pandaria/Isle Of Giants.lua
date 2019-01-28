4faa174ec30b9b4be22ba4c43efcde9f208703b0
Lucetia-Detheroc <aceso2896@gmail.co>
2019-01-28 00:11:44 -0800

    - Condensed Zandalari Warscout/Warbringer loot. [Rather than +416 in debug it brings it down to +21]  -- Yes this will hurt numbers

--- contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Isle Of Giants.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Isle Of Giants.lua
@@ -24,32 +24,9 @@ _.Zones =
 						dr(5, i(94573)),	-- Direhorn Runt
 					}),
 					n(70096, { 		-- War-God Dokah
-						dr(97, {
-							i(94158, {
-								dr(2, {
-									i(87643),	-- Fangcracker Battlemace
-									i(87652),	-- Ook-Breaker Mace
-									i(87650),	-- Fishsticker Crossbow
-									i(90721),	-- Cournith Waterstrider's Silken Finery
-									i(87646),	-- Needlefang Throatripper
-									i(90723),	-- Arness's Scaled Leggings
-									i(87642),	-- Darkstaff of Annihilation
-									i(90719),	-- Go-Han's Golden Trousers
-									i(87651),	-- Pathwalker Greatstaff
-									i(90717),	-- Qu'nas' Apocryphal Legplates
-									i(87641),	-- Yaungol Battle Barrier
-									i(87649),	-- Pool-Stirrer
-									i(90724),	-- Spriggin's Sproggin' Leggin'
-									i(90718),	-- Torik-Ethis' Bloodied Legguards
-									i(90722),	-- Torik-Ethis' Gilded Legplates
-									i(95179),	-- Necklace of the Hazillosaurus
-								}), 
-								dr(1, {
-									i(90725),	-- Gaarn's Leggings of Infestation
-									i(90720),	-- Silent Leggings of the Ghostpaw
-								}),
-							}),
-						}),							
+						{	-- Big Bag of Zandalari Supplies
+							["itemID"] = 94158,	-- Big Bag of Zandalari Supplies
+						},						
 					}),
 					n(69925, { 		-- Zandalari Dinomancer
 						dr(2, {
