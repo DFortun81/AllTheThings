a844f070273d82ddfba8f966d2566865e88c00df
DrOxlotus <alex.metz1024@gmail.com>
2019-02-08 18:53:40 -0600

    Added sub maps to Kun-Lai Summit and Suramar.

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Suramar.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Suramar.lua
@@ -2117,7 +2117,8 @@ _.Zones =
 			["lvl"] = 98,	
 			["achievementID"] = 10669,
 			["maps"] = {
-				684,	-- Shattered Locus
+				684,	-- Shattered Locus (Floor #1)
+				685,	-- Shattered Locus (Floor #2)
 				687,	-- Kel'balor
 			},
 			["description"] = "|cff66ccffSuramar is a max-level zone located in the central Broken Isles. Once a great night elven settlement � though not as glorious as the capital of Zin-Azshari � it was famously the hometown of several important night elves: Illidan Stormrage, his brother Malfurion, their friend Tyrande Whisperwind, and the siblings Jarod and Maiev Shadowsong. Today it is the home of the Nightborne. Suramar is built on ley lines, and arcane coils in the tunnel system running beneath Suramar and the surrounding areas tap into those ley lines' power. Since the creation of the Nightwell, the ley lines have become redundant and the arcane coils abandoned.|r",				
