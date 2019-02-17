2ea422e88d2b1427d006d9728a326211f1913c90
sadidorf <47722298+sadidorf@users.noreply.github.com>
2019-02-17 20:07:08 +0100

    Update deDE.lua
    Added strings to correct the raid lockouts

--- locales/deDE.lua
+++ locales/deDE.lua
@@ -3,3 +3,11 @@ if GetLocale() ~= "deDE" then return; end
 local L = AllTheThings.L;
 
 -- TODO
+L.SAVED_TO_DJ_INSTANCES["Geschmolzener Kern"] = "Der Geschmolzene Kern";
+L.SAVED_TO_DJ_INSTANCES["Echsenkessel: Höhle des Schlangenschreins"] = "Höhle des Schlangenschreins";
+L.SAVED_TO_DJ_INSTANCES["Festung der Stürme"] = "Das Auge";
+L.SAVED_TO_DJ_INSTANCES["Der Sonnenbrunnen"] = "Sonnenbrunnenplateau";
+L.SAVED_TO_DJ_INSTANCES["Eiskronenzitadelle"] = "Die Eiskronenzitadelle";
+L.SAVED_TO_DJ_INSTANCES["Rubinsanktum"] = "Das Rubinsanktum";
+L.SAVED_TO_DJ_INSTANCES["Terrasse des Endlosen Frühlings"] = "Terrasse d. Endlosen Frühlings";
+L.SAVED_TO_DJ_INSTANCES["Herz der Angst"] = "Das Herz der Angst";
