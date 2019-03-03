172aff1502d9a1d5114b5b8c78336e3506ba9799
Pr3vention <Pr3vention@gmail.com>
2019-03-03 15:51:51 -0600

    Verified and split DK starting zone, renamed EPL file

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/The Scarlet Enclave/0 - Metadata.lua
@@ -0,0 +1,21 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	{	-- Eastern Kingdoms
+		["mapID"] = 13,
+		["g"] = {
+			{	-- Eastern Plaguelands
+				["mapID"] = 23,
+				["g"] = {
+					{	-- The Scarlet Enclave
+						["mapID"] = 124,
+						["icon"] = "Interface\\Icons\\Classicon_deathknight",
+						["description"] = "|cff66ccffThe Scarlet Enclave is the name of the starting area for all Death Knight players in World of Warcraft: Wrath of the Lich King.|r",
+					},
+				},
+			}
+		},
+	},
+};
---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/The Scarlet Enclave/Zone Drops.lua
@@ -0,0 +1,28 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	{	-- Eastern Kingdoms
+		["mapID"] = 13,
+		["g"] = {
+			{	-- Eastern Plaguelands
+				["mapID"] = 23,
+				["g"] = {
+					{	-- The Scarlet Enclave
+						["mapID"] = 124,
+						["g"] = {
+							n(0, {	-- Zone Drop
+								i(16252, {	-- Formula: Enchant Weapon - Crusader
+									["crs"] = {
+										9451,	-- Scarlet Archmage
+									},
+								}),
+							}),
+						},
+					},
+				},
+			},
+		},
+	},
+};
