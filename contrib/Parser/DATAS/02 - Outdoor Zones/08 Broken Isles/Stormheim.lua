4942698d003fe5e61915c8c9446953c60ba61d9a
Boohyaka <boohyaka@mykolab.com>
2019-03-04 12:53:46 +0100

    Added map "Vault of Eyir" to Stormheim

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Stormheim.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Stormheim.lua
@@ -2478,6 +2478,7 @@ _.Zones =
 				636,	-- Stormscale Cavern
 				637,	-- Thorignir Refuge
 				639,	-- Aggramar's Vault
+				640,	-- Vault of Eyir
 			},
 			["achievementID"] = 10668,
 			["description"] = "|cff66ccffStormheim is a zone in the eastern area of the Broken Isles situated in the shadow of Highmountain to the west, and Suramar to the southwest. It's home to the mighty vrykul who left Northrend many ages ago in search of their holy land. Two titanic watchers here have warred with each other for thousands of years. The Halls of Valor and Helheim, the realms to which the vrykul journey when they die, are located in this zone.|r",
