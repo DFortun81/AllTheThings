d9e7c40ac9c144eaab01dd20a038087d81f7e1d3
ada <slumber@love.com>
2019-03-16 19:43:46 -0500

    - added missing val'sharah map

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Val'Sharah.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Val'Sharah.lua
@@ -1894,6 +1894,7 @@ _.Zones =
 			["achievementID"] = 10666,
 			["maps"] = {
 				642,	-- Darkpens
+				643,	-- Sleeper's Barrow
 				868,	-- Val'sharah Legion Invasion
 			},
 			["description"] = "|cff66ccffVal'sharah is a zone in the northwestern area of the Broken Isles. It was here where Malfurion Stormrage first learned druidism under the tutelage of Cenarius. The zone is currently contested by Xavius and his satyrs attempting to spread the corruption of the Emerald Nightmare. Ages ago, the first druids molded Val'sharah to be a mirror of the Emerald Dream. While merely an echo, it is as close to the Dream as Azeroth can come.|r",				
