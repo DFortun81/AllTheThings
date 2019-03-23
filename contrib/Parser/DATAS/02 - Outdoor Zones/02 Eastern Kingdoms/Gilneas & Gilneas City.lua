9f1161cc89e03c8d61053b75c05f4523ed07ee4a
ada <slumber@love.com>
2019-03-23 12:26:28 -0500

    - added a missing map, very exciting

--- contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Gilneas & Gilneas City.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Gilneas & Gilneas City.lua
@@ -214,6 +214,9 @@ _.Zones =
 				})
 			},
 			["lvl"] = 1,	
+			["maps"] = {
+				182,	-- Greymane Manor
+			},
 			["icon"] = "Interface\\Icons\\achievement_battleground_battleforgilneas",	
 			["description"] = "|cff66ccffGilneas, once a human kingdom, was isolated from the rest of the world ever since the Second War. It is now home of Genn Graymane's worgen who have re-joined the Alliance once again since the Forsaken invasion and the Cataclysm wrecked their kingdom. Gilneas is a worgen-only leveling zone that relies heavily upon phasing. The starting zone covers how the player transforms into a Worgen, later aiding Graymane in trying to protect his kingdom from Sylvanas.|r",				
 		}),
