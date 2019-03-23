cbe6fde1f22d9e0e32dfcd9490ec3b7ece9d6e9b
Gaulish-Bloodscalp <l2pwncomputers@hotmail.com>
2019-03-23 07:00:28 -0600

    Reverted previous change.

--- contrib/Parser/DATAS/03 - World Drops/09 Legion/02 Armor.lua
+++ contrib/Parser/DATAS/03 - World Drops/09 Legion/02 Armor.lua
@@ -10,10 +10,12 @@ _.WorldDrops =
 					i(141566),	-- Serrinne's Maleficent Habit
 					i(141575),	-- Gorrog's Serene Gaze
 					i(141579),	-- Welded Hardskin Helmet
+					i(147064),	-- Diadem of the Highborne
 				}),
 				n(-321, { -- Shoulder
 					i(141567),	-- Cyno's Mantle of Sin
 					i(141568),	-- Boughs of Archdruid Van-Yali
+					i(147038),	-- Wakening Horror Spaulders
 				}),
 				n(-322, { -- Back
 					i(121012),	-- Azurewing Defender's Drape
@@ -39,10 +41,16 @@ _.WorldDrops =
 					}),
 					i(141589),	-- Treia's Handcrafted Shroud
 					i(141590),	-- Cloak of Martayl Oceanstrider
+					i(142541),	-- Drape of the Forgotten Souls
+					i(144399),	-- Aristocrat's Winter Drape
+					i(144400),	-- Feathermane Feather Cloak
+					i(144401),	-- Cloak of Multitudinous Sheaths
+					i(144403),	-- Fashionable Autumn Cloak
 				}),
 				n(-323, { -- Chest
 					i(141570),	-- Cainen's Preeminent Chestguard
 					i(141576),	-- Aethrynn's Everwarm Chestplate
+					i(147044),	-- Soul-Rattle Ribcage
 				}),
 				n(-326, { -- Wrist
 					i(141571),	-- Mavanah's Shifting Wristguards
@@ -50,10 +58,20 @@ _.WorldDrops =
 				n(-327, { -- Hands
 					i(141565),	-- Mir's Enthralling Grasp
 					i(141577),	-- Fists of Thane Kray-Tan
+					i(141695),	-- Tarnished Dreamkeeper's Gauntlets
+					i(146989),	-- Fel-Flecked Grips
 				}),
 				n(-328, { -- Waist
 					i(141564),	-- Telubis' Binding of Patience
 					i(141573),	-- Shokell's Grim Cinch
+					i(144404),	-- Mana-Cord of Deception
+					i(144405),	-- Waistclasp of Unethical Power
+					i(144406),	-- Vintage Duskwatch Cinch
+					i(144407),	-- Gleaming Celestial Waistguard
+					i(147423),	-- Sash of the Unredeemed
+					i(147425),	-- Cord of Pilfered Rosaries
+					i(147427),	-- Pristine Moon-Wrought Clasp
+					i(147429),	-- Girdle of the Crumbling Sanctum
 				}),
 				n(-329, { -- Legs
 					i(141569),	-- Samnoh's Exceptional Leggings
@@ -82,6 +100,10 @@ _.WorldDrops =
 					}),
 					i(141572),	-- Geta of Tay'Shute
 					i(141578),	-- Claud's War-Ravaged Boots
+					i(147422),	-- Acolyte's Abandoned Footwraps
+					i(147424),	-- Treads of Violent Intrusion
+					i(147426),	-- Treads of Panicked Escape
+					i(147428),	-- Spiked Terrorwake Greatboots
 				}),
 			}),
 		},
