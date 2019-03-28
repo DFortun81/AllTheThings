ee4ebab635a752b8e4fd0ea1fda3711a6bf968a3
Gaulish-Bloodscalp <l2pwncomputers@hotmail.com>
2019-03-28 06:10:10 -0600

    Added cords to Time-Lost trader

--- contrib/Parser/DATAS/11 - Professions/Tailoring/Recipes/Zandalari Patterns - Filtered.lua
+++ contrib/Parser/DATAS/11 - Professions/Tailoring/Recipes/Zandalari Patterns - Filtered.lua
@@ -36,34 +36,37 @@ profession(197, {	-- Tailoring
 								{
 									["npcID"] = 151903,		-- Time-Lost Trader
 									["description"] = "This vendor spawns randomly when you close a time rift",
-									--[[["coords"] = {
+									["coords"] = {
 										-- Zuldazar
-										{ 45.0, 39.0 },  -- Ledge right before entrance to Atal'Dazar
-										{ 54.0, 64.0 },  -- On the beach below Mugambala
-										{ 59.0, 75.0 },  -- NW corner of Tuck Isle
-										{ 73.0, 67.0 },  -- East of Seeker's Outpost
-										{ 71.0, 35.0 },  -- Behind Savagelands Devilsaur
+										{ 45.0, 39.0, 862 },  -- Ledge right before entrance to Atal'Dazar
+										{ 54.0, 61.0, 862 },  -- On the beach below Mugambala
+										{ 59.0, 75.0, 862 },  -- NW corner of Tuck Isle
+										{ 73.0, 67.0, 862 },  -- East of Seeker's Outpost
+										{ 71.0, 35.0, 862 },  -- Behind Savagelands Devilsaur
 										-- Nazmir
-										{ 38.0, 58.0 },  -- Under arch east side of Terrace of Sorrows
-										{ 55.0, 21.0 },  -- 
-										{ 53.0, 56.0 },  -- NE corner of Heart of Darkness
-										{ 38.0, 89.0 },  -- SE corner of Necropolis
-										{ 71.0, 35.0 },  -- The Shattered River, in front of broken totem
-										{ 26.0, 78.0 },  -- In front of waterfall
+										{ 38.0, 58.0, 863 },  -- Under arch east side of Terrace of Sorrows
+										{ 55.0, 21.0, 863 },  -- 
+										{ 53.0, 56.0, 863 },  -- NE corner of Heart of Darkness
+										{ 43.0, 29.0, 863 },  -- SE corner of Necropolis
+										{ 38.0, 89.0, 863 },  -- The Shattered River, in front of broken totem
+										{ 29.0, 56.0, 863 },  -- 
+										{ 26.0, 78.0, 863 },  -- In front of waterfall
 										-- Drustvar
-										{ 36.0, 72.0 },  --
-										{ 33.0, 23.0 },  -- 
-										{ 63.0, 35.0 },  -- 
-										{ 46.0, 38.0 },  -- Highroad Pass, SE of Aroms Stand
+										{ 23.0, 43.0, 896 },  -- Just above Anyport
+										{ 36.0, 72.0, 896 },  --
+										{ 33.0, 23.0, 896 },  -- 
+										{ 63.0, 35.0, 896 },  -- 
+										{ 46.0, 38.0, 896 },  -- Highroad Pass, SE of Aroms Stand
 										-- Voldun
-										{ 54.0, 41.0 },  -- Behind a rock E of Darkwood Shoal
-										{ 50.0, 64.0 },  -- East side of Atul'aman
-										{ 49.0, 84.0 },  -- Redrock Lowlands
-										{ 41.0, 64.0 },  -- West of Goldtusk Inn
-										{ 37.0, 87.0 },  -- On the ledge, N of island off south coast
+										{ 54.0, 41.0, 864 },  -- Behind a rock E of Darkwood Shoal
+										{ 50.0, 64.0, 864 },  -- East side of Atul'aman
+										{ 49.0, 84.0, 864 },  -- Redrock Lowlands
+										{ 41.0, 64.0, 864 },  -- West of Goldtusk Inn
+										{ 37.0, 87.0, 864 },  -- On the ledge, N of island off south coast
 										-- Tiragarde Sound
-										{ 85.0, 77.0 },  -- Dock north of Freehold dungeon
-									},	]]--How do I get this to show up in each zone only?Someday I'll figure it out.
+										{ 85.0, 77.0, 895 },  -- Dock north of Freehold dungeon
+										{ 40.0, 24.0, 895 },  --
+									},
 									["groups"] = {	
 										i(167889),	-- Pattern: Azure Silk Cloak
 										i(167881),  -- Pattern: Belt of the Archmage
