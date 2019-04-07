92aadb7dc523b230b565091ee05b9186c95297b9
Lucetia <>
2019-04-06 23:06:51 -0700

    - Added "Through the Front Door" scenario map

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/Scenarios/Through the Front Door.lua
@@ -0,0 +1,23 @@
+---------------------------------------------------
+--          Z O N E S       M O D U L E          --
+---------------------------------------------------
+_.Zones =
+{
+	{	-- Kul'Tiras
+		["mapID"] = 876,	-- Kul'Tiras
+		["g"] = {
+			{	-- Scenario's
+				["npcID"] = -10069,	-- Scenario's
+				["icon"] = "Interface\\Icons\\ability_demonhunter_imprison",
+				["g"] = {
+					{	-- Tol Dagor
+						["mapID"] = 1349,	-- Tol Dagor
+						["icon"] = "Interface\\Icons\\ability_demonhunter_imprison",
+						["lvl"] = 120,
+						["description"] = "|cff66ccffWe would like the least amount of risk during your retrieval of Priscilla Ashvane. You will arrive on the docks during a shift change. From there, you will prepare and enter the prison. Find me in Zuldazar when you are successful.|r",
+					},
+				},
+			},
+		},
+	},
+};
--- contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/Scenarios/Zandalar Forever Scenario.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/Scenarios/Zandalar Forever Scenario.lua
@@ -8,11 +8,12 @@ _.Zones =
 		["g"] = {
 			{	-- Scenario's
 				["npcID"] = -10069,	-- Scenario's
-				["icon"] = "Interface\\Icons\\inv_zuldazar",				
-				["g"] = {				
+				["icon"] = "Interface\\Icons\\inv_zuldazar",
+				["g"] = {
 					{	-- Zuldazar
 						["mapID"] = 1181,	-- Zuldazar
 						["lvl"] = 110,
+						["icon"] = "Interface\\Icons\\inv_zuldazar",
 						["description"] = "|cff66ccffBwonsamdi has whispered a warning to me: the enemy is on the move. I have gathered what defenses I could in the time allowed but we are dangerously under powered. I can only hope that, thanks to your efforts, the loa will be with us this day. Are you ready to defend Zandalar?|r",
 					},
 				},
