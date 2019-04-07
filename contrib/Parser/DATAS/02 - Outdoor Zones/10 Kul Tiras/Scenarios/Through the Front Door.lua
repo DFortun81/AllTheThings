81890c6770dce5b36867118dbeebf618fe741979
Lucetia <>
2019-04-06 23:27:00 -0700

    - Added an additional map to the "Through the Front Door" scenario

--- contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/Scenarios/Through the Front Door.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/Scenarios/Through the Front Door.lua
@@ -12,6 +12,9 @@ _.Zones =
 				["g"] = {
 					{	-- Tol Dagor
 						["mapID"] = 1349,	-- Tol Dagor
+						["maps"] = {
+							1351,	-- Tol Dagor [Inside Prison]
+						},
 						["icon"] = "Interface\\Icons\\ability_demonhunter_imprison",
 						["lvl"] = 120,
 						["description"] = "|cff66ccffWe would like the least amount of risk during your retrieval of Priscilla Ashvane. You will arrive on the docks during a shift change. From there, you will prepare and enter the prison. Find me in Zuldazar when you are successful.|r",
