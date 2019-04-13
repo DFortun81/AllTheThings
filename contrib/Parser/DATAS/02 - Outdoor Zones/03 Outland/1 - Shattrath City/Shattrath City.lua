7cef094bc45bca1d7e239c3fa290b6a67481600d
mogwai316 <bryan.harden@gmail.com>
2019-04-13 14:55:54 -0400

    Removed Bolt of Soulcloth recipe from vendor Eiin

--- contrib/Parser/DATAS/02 - Outdoor Zones/03 Outland/1 - Shattrath City/Shattrath City.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/03 Outland/1 - Shattrath City/Shattrath City.lua
@@ -957,7 +957,6 @@ _.Zones =
 						["groups"] = {
 							i(21892),	-- Pattern: Bolt of Imbued Netherweave
 							i(21893),	-- Pattern: Imbued Netherweave Bag
-							i(21894),	-- Pattern: Bolt of Soulcloth
 							i(21896), 	-- Pattern: Netherweave Robe
 							i(21897), 	-- Pattern: Netherweave Tunic
 						},
