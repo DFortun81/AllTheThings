8c891113341fab2de99c84381900d00936bd1324
Pr3vention <Pr3vention@gmail.com>
2019-04-13 21:33:28 -0500

    Broke up Sunstrider Isle

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Sunstrider Isle/Quests.lua
@@ -0,0 +1,70 @@
+---------------------------------------------------
+--          Z O N E S       M O D U L E          --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(467, {	-- Sunstrider Isle
+			["groups"] = {
+				n(-17, {	-- Quests
+					{	-- A Fistful of Slivers
+						["questID"] = 37440,
+						["qg"] = 15296,	-- Arcanist Ithanas
+						["sourceQuest"] = 8326,	-- Unfortunate Measures
+					},
+					{	-- Aggression
+						["questID"] = 8334,
+						["qg"] = 15281,	-- Lanthan Perilon
+						["sourceQuest"] = 8327,	-- Report to Lanthan Perilon
+					},
+					{	-- Aiding the Outrunners
+						["questID"] = 8347,
+						["qg"] = 15281,	-- Lanthan Perilon
+						["sourceQuest"] = 8335,	-- Felendren the Banished
+					},
+					{	-- Felendren the Banished
+						["questID"] = 8335,
+						["qg"] = 15281,	-- Lanthan Perilon
+						["sourceQuest"] = 8334,	-- Aggression
+					},
+					{	-- Reclaiming Sunstrider Isle
+						["questID"] = 8325,
+						["qg"] = 15278,	-- Magistrix Erona
+					},
+					{	-- Report to Lanthan Perilon
+						["questID"] = 8327,
+						["qg"] = 15278,	-- Magistrix Erona
+						["sourceQuest"] = 8326,	-- Unfortunate Measures
+					},
+					{	-- Solanian's Belongings
+						["questID"] = 37443,
+						["qg"] = 15295,	-- Well Watcher Solanian
+						["sourceQuest"] = 8326,	-- Unfortunate Measures
+					},
+					{	-- Tainted Arcane Sliver
+						["itemID"] = 20483,
+						["questID"] = 8338,
+						["crs"] = {
+							15298,	-- Tainted Arcane Wraith
+						},
+					},
+					{	-- The Shrine of Dath'Remar
+						["questID"] = 37442,
+						["qg"] = 15295,	-- Well Watcher Solanian
+						["sourceQuest"] = 8326,	-- Unfortunate Measures
+					},
+					{	-- Thirst Unending
+						["questID"] = 37439,
+						["qg"] = 15297,	-- Arcanist Helion
+						["sourceQuest"] = 8326,	-- Unfortunate Measures
+					},
+					{	-- Unfortunate Measures
+						["questID"] = 8326,
+						["qg"] = 15278,	-- Magistrix Erona
+						["sourceQuest"] = 8325,	-- Reclaiming Sunstrider Isle
+					},
+				}),
+			},
+		}),
+	}),
+};
