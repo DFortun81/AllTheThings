564f3625e513f41cc855fbc23114de62fefe7303
Lucetia <>
2019-04-17 01:31:01 -0700

    more verification

--- contrib/Parser/DATAS/04 - Group Finder/01 Classic.lua
+++ contrib/Parser/DATAS/04 - Group Finder/01 Classic.lua
@@ -21,14 +21,14 @@ _.GroupFinder =
 					i(51967),			-- Enumerated Sandals
 					i(51974),			-- Enumerated Shoulderpads*
 					i(51968),			-- Enumerated Wrap*
-					i(51989),			-- Stalwart Bands*			— 4/11/19
+					i(51989),			-- Stalwart Bands*			— 4/16/19
 					i(143582),			-- Stalwart Belt*			— 4/16/19
 					i(143583),			-- Stalwart Grips*			- 4/16/19 
 					i(51984),			-- Stalwart Shoulderpads*
 					un(2, i(51997)),	-- Stalwart Spaulders
 					i(51990),			-- Stalwart Treads*			— 4/16/19
 					i(51994),			-- Tumultuous Cloak*
-					i(160598),			-- Tumultuous Cloak*		— 4/11/19
+					i(160598),			-- Tumultuous Cloak*		— 4/16/19
 					i(160597),			-- Tumultuous Cloak*		— 4/7/19
 					i(51996),			-- Tumultuous Necklace*
 					i(51992),			-- Tumultuous Ring [INT]*
