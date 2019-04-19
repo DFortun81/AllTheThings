f800a138523cbb6f16244b82971b6eaff8438dda
Lucetia <>
2019-04-19 04:37:18 -0700

    - Added note on achievement for OS drakes that loot isn't always guaranteed, even if you leave correct amount of drakes up

--- contrib/Parser/DATAS/01 - Dungeons Raids/3 - Wrath of the Lich King/1 - Raids/3 The Obsidian Sanctum.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/3 - Wrath of the Lich King/1 - Raids/3 The Obsidian Sanctum.lua
@@ -40,18 +40,24 @@ _.Instances = { tier(3, {	-- Wrath of the Lich King
 						},
 					},
 					ach(2049, {	-- Twilight Assist [10 player]
-						i(43992),	-- Volitant Amulet		** Confirmed Drop 4/19/19 [3 Drakes up]
-						i(43988),	-- Gale-Proof Cloak
-						i(43990),	-- Blade-Scarred Tunic
-						i(43989),	-- Remembrance Girdle	** Confirmed Drop 4/19/19 [1 Drake up]
-						i(43991),	-- Legguards of Composure
-					}),
+						["description"] = "With only 1 drake up loot from this table isn't always guaranteed.",
+						["g"] = {
+							i(43992),	-- Volitant Amulet		** Confirmed Drop 4/19/19 [3 Drakes up]
+							i(43988),	-- Gale-Proof Cloak
+							i(43990),	-- Blade-Scarred Tunic
+							i(43989),	-- Remembrance Girdle	** Confirmed Drop 4/19/19 [1 Drake up]
+							i(43991),	-- Legguards of Composure
+						},
+					}),					
 					ach(2050, {	-- Twilight Duo [10 player]
-						i(43995),	-- Enamored Cowl
-						i(43998),	-- Chestguard of Flagrant Prowess
-						i(43994),	-- Belabored Legplates
-						i(43996),	-- Sabatons of Firmament
-						i(43993),	-- Greatring of Collision
+						["description"] = "With only 2 drakes up loot from this table isn't always guaranteed.",
+						["g"] = {
+							i(43995),	-- Enamored Cowl
+							i(43998),	-- Chestguard of Flagrant Prowess
+							i(43994),	-- Belabored Legplates
+							i(43996),	-- Sabatons of Firmament
+							i(43993),	-- Greatring of Collision
+						},
 					}),
 					ach(2051, {	-- The Twilight Zone [10 player]
 						{	-- of the Nightfall
@@ -131,17 +137,23 @@ _.Instances = { tier(3, {	-- Wrath of the Lich King
 					},
 					ach(2052, {	-- Twilight Assist [25 player]
 						-- Note!! Even with 1 drake up you can still get 0 loot from the table 4/19/19
-						i(44003),	-- Upstanding Spaulders
-						i(44000),	-- Dragonstorm Breastplate
-						i(44002),	-- The Sanctum's Flowing Vestments
-						i(44004),	-- Bountiful Gauntlets
+						["description"] = "With only 1 drake up loot from this table isn't always guaranteed.",
+						["g"] = {
+							i(44003),	-- Upstanding Spaulders
+							i(44000),	-- Dragonstorm Breastplate
+							i(44002),	-- The Sanctum's Flowing Vestments
+							i(44004),	-- Bountiful Gauntlets
+						},
 					}),
 					ach(2053, {	-- Twilight Duo [25 player]
-						i(44007),	-- Headpiece of Reconciliation
-						i(44006),	-- Obsidian Greathelm
-						i(44005),	-- Pennant Cloak
-						i(44008),	-- Unsullied Cuffs
-						i(44011),	-- Leggings of the Honored
+						["description"] = "With only 2 drakes up loot from this table isn't always guaranteed.",
+						["g"] = {
+							i(44007),	-- Headpiece of Reconciliation
+							i(44006),	-- Obsidian Greathelm
+							i(44005),	-- Pennant Cloak
+							i(44008),	-- Unsullied Cuffs
+							i(44011),	-- Leggings of the Honored
+						},
 					}),
 					ach(2054, {	-- The Twilight Zone [25 player]
 						{	-- Twilight Vanquisher
