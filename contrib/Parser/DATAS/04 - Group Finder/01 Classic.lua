2236d9576fd43421ecc11467f7b0049d410f600d
Lucetia <>
2019-04-20 01:41:33 -0700

    - more verification

--- contrib/Parser/DATAS/04 - Group Finder/01 Classic.lua
+++ contrib/Parser/DATAS/04 - Group Finder/01 Classic.lua
@@ -8,38 +8,38 @@ _.GroupFinder =
 		["groups"] = {
 			i(156683, {			-- Satchel of Helpful Goods [Level 15-60]			
 				["groups"] = {
-					i(51982),			-- Earthbound Boots*
+					i(51982),			-- Earthbound Boots
 					un(2, i(51978)),	-- Earthbound Girdle [Probably unobtainable]
-					i(143580),			-- Earthbound Girdle*
+					i(143580),			-- Earthbound Girdle
 					-- un(2, i(51979)),	-- Earthbound Grips (moved to BC satchel)
-					i(143581),			-- Earthbound Grips*
+					i(143581),			-- Earthbound Grips
 					un(2, i(51980)),	-- Earthbound Handgrips [Probably unobtainable]
-					i(51976),			-- Earthbound Shoulderguards*
-					i(51981),			-- Earthbound Wristguards*
-					i(51972),			-- Enumerated Bracers*
+					i(51976),			-- Earthbound Shoulderguards
+					i(51981),			-- Earthbound Wristguards
+					i(51972),			-- Enumerated Bracers
 					i(51973),			-- Enumerated Handwraps
 					i(51967),			-- Enumerated Sandals
-					i(51974),			-- Enumerated Shoulderpads*
-					i(51968),			-- Enumerated Wrap*
-					i(51989),			-- Stalwart Bands*			— 4/16/19
-					i(143582),			-- Stalwart Belt*			— 4/16/19
-					i(143583),			-- Stalwart Grips*			- 4/16/19 
-					i(51984),			-- Stalwart Shoulderpads*
+					i(51974),			-- Enumerated Shoulderpads
+					i(51968),			-- Enumerated Wrap
+					i(51989),			-- Stalwart Bands			— 4/16/19
+					i(143582),			-- Stalwart Belt			— 4/16/19
+					i(143583),			-- Stalwart Grips			- 4/16/19 
+					i(51984),			-- Stalwart Shoulderpads
 					un(2, i(51997)),	-- Stalwart Spaulders
-					i(51990),			-- Stalwart Treads*			— 4/16/19
-					i(51994),			-- Tumultuous Cloak*
-					i(160598),			-- Tumultuous Cloak*		— 4/16/19
-					i(160597),			-- Tumultuous Cloak*		— 4/7/19
-					i(51996),			-- Tumultuous Necklace*
-					i(51992),			-- Tumultuous Ring [INT]*
-					i(160601),			-- Tumultuous Ring [AGI]* 	— 4/7/19
+					i(51990),			-- Stalwart Treads			— 4/16/19
+					i(51994),			-- Tumultuous Cloak
+					i(160598),			-- Tumultuous Cloak			— 4/16/19
+					i(160597),			-- Tumultuous Cloak			— 4/7/19
+					i(51996),			-- Tumultuous Necklace		- 4/19/19
+					i(51992),			-- Tumultuous Ring [INT]
+					i(160601),			-- Tumultuous Ring [AGI] 	— 4/7/19
 					i(160602),			-- Tumultuous Ring [STR]
-					i(51964),			-- Vigorous Belt*			— 4/7/19
-					i(51962),			-- Vigorous Bracers*		— 4/7/19
-					i(51965),			-- Vigorous Handguards*		— 4/8/19
+					i(51964),			-- Vigorous Belt			— 4/7/19
+					i(51962),			-- Vigorous Bracers			— 4/7/19
+					i(51965),			-- Vigorous Handguards		— 4/8/19
 					un(2, i(51998)),	-- Vigorous Spaulders [Probably unobtainable - maybe old leather set that got changed into mail set]
-					i(51966),			-- Vigorous Spaulders*
-					i(51963),			-- Vigorous Stompers*		— 4/8/19
+					i(51966),			-- Vigorous Spaulders
+					i(51963),			-- Vigorous Stompers		— 4/8/19
 				},		
 				["description"] = "|cff66ccffRewarded for completing a random dungeon in the level 15-60 range.|r",		
 				["lvl"] = 15,		
