761e14eaed6992494088e0f29f4f93ddf5a4ba5c
Lucetia <>
2019-04-22 03:50:32 -0700

    Noblegarden Phase 2: Achievements done

--- contrib/Parser/DATAS/21 - Holidays/03 - Noblegarden/Achievements.lua
+++ contrib/Parser/DATAS/21 - Holidays/03 - Noblegarden/Achievements.lua
@@ -3,109 +3,268 @@
 --------------------------------------------
 _.Holidays = bubbleDown({["u"] = 19}, 
 {
-	holiday(235477, {	-- Noblegarden
-		n(-4, {	-- Achievements
-			ach(2798, {	-- Noble Gardener
-				["groups"] = {
-					un(19, title(122)),	-- the Noble
-					ach(2676, {	-- I Found One!
-						o(113768, {	-- Brightly Colored Egg
-							i(45072),	-- Brightly Colored Egg
-						}),
-					}),
-					ach(2417, {	-- Chocolate Lover
-						["groups"] = {
-							ach(2418, {	-- Chocoholic
-								o(113768, {	-- Brightly Colored Egg
-									i(45072, {	-- Brightly Colored Egg
-										i(44791),	-- Noblegarden Chocolate
-									}),
-								}),
-							}),
-						},
-					}),
-					ach(2436, {	-- Desert Rose
-						["groups"] = {
-							crit(1),	-- The Badlands
-							crit(2),	-- Desolace
-							crit(3),	-- Silithus
-							crit(4),	-- Tanaris
-							crit(5),	-- Thousand Needles
-						},
-					}),
-					ach(2576, {	-- Blushing Bride
-						
-					}),
-					ach(2416, {	-- Hard Boiled
-						
-					}),
-					ach(2419, {	-- Spring Fling (Alliance)
-						["groups"] = {
-							crit(1),	-- Azure Watch
-							crit(2),	-- Dolanaar
-							crit(3),	-- Goldshire
-							crit(4),	-- Kharanos
-						},
-						["races"] = ALLIANCE_ONLY,
-					}),
-					ach(2497, {	-- Spring Fling (Horde)
-						["groups"] = {
-							crit(1),	-- Bloodhoof Village
-							crit(2),	-- Brill
-							crit(3),	-- Falconwing Square
-							crit(4),	-- Razor Hill
-						},
-						["races"] = HORDE_ONLY,
-					}),
-					ach(2421, {	-- Noble Garden (Alliance)
-						
-						["races"] = ALLIANCE_ONLY,
-					}),
-					ach(2420, {	-- Noble Garden (Horde)
-						
-						["races"] = HORDE_ONLY,
-					}),
-					ach(2422, {	-- Shake Your Bunny-Maker
-						["groups"] = {
-							crit(1),	-- Blood Elf
-							crit(2),	-- Draenei
-							crit(3),	-- Dwarf
-							crit(4),	-- Gnome
-							crit(5),	-- Goblin
-							crit(6),	-- Human
-							crit(7),	-- Night Elf
-							crit(8),	-- Orc
-							crit(9),	-- Tauren
-							crit(10),	-- Troll
-							crit(11),	-- Undead
-							crit(12),	-- Worgen
-						},
-					}),
-				},
-			}),
-			ach(248, {	-- Sunday's Finest
-				["groups"] = {
-					o(113768, {	-- Brightly Colored Egg
-						i(45072, {	-- Brightly Colored Egg
-							["groups"] = {
-								i(6833),	-- White Tuxedo Shirt
-								i(151804),	-- Black Tuxedo Pants
+	{	-- Noblegarden
+		["holidayID"] = 235477,	-- Noblegarden
+		["u"] = 19,				-- Noblegarden Filter
+		["g"] = {
+			{	-- Achievements
+				["npcID"] = -4,	-- Achievements
+				["g"] = {
+					{	-- Noble Gardener
+						["achievementID"] = 2798,	-- Noble Gardener
+						["g"] = {
+							{	-- <Name> the Noble
+								["titleID"] = 122,	-- <Name> the Noble
+								["u"] = 19,			-- Noblegarden Filter
 							},
-						}),
-					}),
-				},
-			}),
-			ach(249, {	-- Dressed for the Occasion
-				["groups"] = {
-					o(113768, {	-- Brightly Colored Egg
-						i(45072, {	-- Brightly Colored Egg
-							["groups"] = {
-								i(151806),	-- Elegant Dress
+							{	-- Blushing Bride
+								["achievementID"] = 2576,	-- Blushing Bride
+								["u"] = 19,					-- Noblegarden Filter
 							},
-						}),
-					}),
+							{	-- Chocolate Lover
+								["achievementID"] = 2417,	-- Chocolate Lover
+								["u"] = 19,					-- Noblegarden Filter
+							},
+							{	-- Desert Rose
+								["achievementID"] = 2436,	-- Desert Rose
+								["u"] = 19,					-- Noblegarden Filter
+								["g"] = {
+									{	-- Desolace
+										["achievementID"] = 2436,	-- Desert Rose
+										["criteriaID"] = 2,			-- 
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											66,	-- Desolace
+										},
+									},
+									{	-- Silithus
+										["achievementID"] = 2436,	-- Desert Rose
+										["criteriaID"] = 3,			-- 
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											81,	-- Silithius
+										},
+									},
+									{	-- Tanaris
+										["achievementID"] = 2436,	-- Desert Rose
+										["criteriaID"] = 4,			-- 
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											71,	-- Tanaris
+										},
+									},
+									{	-- The Badlands
+										["achievementID"] = 2436,	-- Desert Rose
+										["criteriaID"] = 1,			-- The Badlands
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											15,	-- The Badlands
+										},
+									},
+									{	-- Thousand Needles
+										["achievementID"] = 2436,	-- Desert Rose
+										["criteriaID"] = 5,			-- 
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											64,	-- Thousand Needles
+										},
+									},
+								},
+							},
+							{	-- Hard Boiled
+								["achievementID"] = 2416,	-- Hard Boiled
+								["u"] = 19,					-- Noblegarden Filter
+							},
+							{	-- I Found One
+								["achievementID"] = 2676,	-- I Found One
+								["u"] = 19,					-- Noblegarden Filter
+							},
+							{	-- Noble Garden [Alliance]
+								["achievementID"] = 2421,	-- Noble Garden [Alliance]
+								["u"] = 19,					-- Noblegarden Filter
+								["races"] = ALLIANCE_ONLY,
+							},
+							{	-- Noble Garden [Horde]
+								["achievementID"] = 2420,	-- Noble Garden [Horde]
+								["u"] = 19,					-- Noblegarden Filter
+								["races"] = HORDE_ONLY,
+							},
+							{	-- Shake Your Bunny-Maker
+								["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+								["g"] = {
+									{	-- Blood Elf
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 1,			-- Blood Elf
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Draenei
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 2,			-- Draenei
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Dwarf
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 3,			-- Dwarf
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Gnome
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 4,			-- Gnome
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Goblin
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 5,			-- Goblin
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Human
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 6,			-- Human
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Night Elf
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 7,			-- Night Elf
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Orc
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 8,			-- Orc
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Tauren
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 9,			-- Tauren
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Troll
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 10,		-- Troll
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Undead
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 11,		-- Undead
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									{	-- Worgen
+										["achievementID"] = 2422,	-- Shake Your Bunny-Maker
+										["criteriaID"] = 12,		-- Worgen
+										["u"] = 19,					-- Noblegarden Filter
+									},
+									
+								},
+							},
+							{	-- Spring Fling [Alliance]
+								["achievementID"] = 2419,	-- Spring Fling [Alliance]
+								["u"] = 19,					-- Noblegarden Filter
+								["races"] = ALLIANCE_ONLY,
+								["g"] = {
+									{	-- Azure Watch
+										["achievementID"] = 2419,	-- Spring Fling [Alliance]
+										["criteriaID"] = 1,			-- Azure Watch
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											97,	-- Azuremyst Isle
+										},
+										["races"] = ALLIANCE_ONLY,
+									},
+									{	-- Dolanaar
+										["achievementID"] = 2419,	-- Spring Fling [Alliance]
+										["criteriaID"] = 2,			-- Dolanaar
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											57,	-- Teladrassil
+										},
+										["races"] = ALLIANCE_ONLY,
+									},
+									{	-- Goldshire
+										["achievementID"] = 2419,	-- Spring Fling [Alliance]
+										["criteriaID"] = 3,			-- Goldshire
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											37,	-- Elwynn Forest
+										},
+										["races"] = ALLIANCE_ONLY,
+									},
+									{	-- Kharanos
+										["achievementID"] = 2419,	-- Spring Fling [Alliance]
+										["criteriaID"] = 4,			-- Kharanos
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											27,	-- Dun Morogh
+										},
+										["races"] = ALLIANCE_ONLY,
+									},
+								},
+							},
+							{	-- Spring Fling [Horde]
+								["achievementID"] = 2497,	-- Spring Fling [Alliance]
+								["u"] = 19,					-- Noblegarden Filter
+								["races"] = HORDE_ONLY,
+								["g"] = {
+									{	-- Bloodhoof Village
+										["achievementID"] = 2497,	-- Spring Fling [Alliance]
+										["criteriaID"] = 1,			-- Bloodhoof Village
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											7,	-- Mulgore
+										},
+										["races"] = HORDE_ONLY,
+									},
+									{	-- Brill
+										["achievementID"] = 2497,	-- Spring Fling [Alliance]
+										["criteriaID"] = 2,			-- Brill
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											18,	-- Tirisfal Glades
+										},
+										["races"] = HORDE_ONLY,
+									},
+									{	-- Falconwing Square
+										["achievementID"] = 2497,	-- Spring Fling [Alliance]
+										["criteriaID"] = 3,			-- Falconwing Square
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											94,	-- Eversong Woods
+										},
+										["races"] = HORDE_ONLY,
+									},
+									{	-- Razor Hill
+										["achievementID"] = 2497,	-- Spring Fling [Alliance]
+										["criteriaID"] = 4,			-- Razor Hill
+										["u"] = 19,					-- Noblegarden Filter
+										["maps"] = {
+											1,	-- Durotar
+										},
+										["races"] = HORDE_ONLY,
+									},
+								},
+							},
+						},
+					},
+					{	-- Dressed for the Occasion
+						["achievementID"] = 249,	-- Dressed for the Occasion
+						["u"] = 19,					-- Noblegarden Filter
+					},
+					{	-- Sunday's Finest
+						["achievementID"] = 248,	-- Sunday's Finest
+						["u"] = 19,					-- Noblegarden Filter
+						["g"] = {
+							{	-- Black Tuxedo Pants
+								["achievementID"] = 248,	-- Sunday's Finest
+								["criteriaID"] = 2,			-- Black Tuxedo Pants
+								["u"] = 19,					-- Noblegarden Filter
+							},
+							{	-- White Tuxedo Shirt
+								["achievementID"] = 248,	-- Sunday's Finest
+								["criteriaID"] = 1,			-- White Tuxedo Shirt
+								["u"] = 19,					-- Noblegarden Filter
+							},
+						},
+					},
 				},
-			}),
-		}),
-	}),
+			},
+		},
+	},
 });
