3fcb9bb78be3dc2f6f3a1eab0e4e0cb950fea98d
Lucetia <>
2019-04-22 04:13:42 -0700

    Noblegarden Phase 3: Zone Drops

--- contrib/Parser/DATAS/21 - Holidays/03 - Noblegarden/Zone Drops.lua
+++ contrib/Parser/DATAS/21 - Holidays/03 - Noblegarden/Zone Drops.lua
@@ -3,21 +3,70 @@
 --------------------------------------------
 _.Holidays = bubbleDown({["u"] = 19}, 
 {
-	holiday(235477, {	-- Noblegarden
-		n(0, {	-- Zone Drop
-			un(19, o(113768, {	-- Brightly Colored Egg
-				un(19, i(45072, {	-- Brightly Colored Egg
-					un(19, i(44791)),	-- Noblegarden Chocolate
-					un(19, i(151804)),	-- Black Tuxedo Pants
-					un(19, i(44792)),	-- Blossoming Branch
-					un(19, i(151806)),	-- Elegant Dress
-					un(19, i(44803)),	-- Spring Circlet
-					un(19, i(45073)),	-- Spring Flowers
-					un(19, i(44794)),	-- Spring Rabbit's Foot
-					un(19, i(44800)),	-- Spring Robes
-					un(19, i(6833)),	-- White Tuxedo Shirt
-				})),
-			})),
-		}),
-	}),
+	{	-- Noblegarden
+		["holidayID"] = 235477,	-- Noblegarden
+		["u"] = 19,				-- Noblegarden Filter
+		["g"] = {
+			{	-- Zone Drop
+				["npcID"] = 0,
+				["g"] = {
+					{	-- Brightly Colored Egg
+						["itemID"] = 45072,	-- Brightly Colored Egg
+						["u"] = 19,				-- Noblegarden Filter
+						["g"] = {
+							--Note!! Only achievementID/criteriaID's are listed on items that are otherwise ---.  Collectible items don't need them as they auto check the achievement at the same time.
+							{	-- Black Tuxedo Pants
+								["itemID"] = 151804,		-- Black Tuxedo Pants
+								["u"] = 19,					-- Noblegarden Filter
+								--Note!! Discover from egg only so we don't want to mask every appearance with the achievement
+							},
+							{	-- Blossoming Branch
+								["itemID"] = 44792,			-- Blossoming Branch
+								["achievementID"] = 2416,	-- Hard Boiled
+								["criteriaID"] = 1,			-- Hard Boiled
+								["u"] = 19,					-- Noblegarden Filter
+								["description"] = "This item is required for the achievement \"Hard Boiled\".",
+							},
+							{	-- Elegant Dress
+								["itemID"] = 151806,		-- Elegant Dress
+								["u"] = 19,					-- Noblegarden Filter
+								--Note!! Discover from egg only so we don't want to mask every appearance with the achievement
+							},
+							{	-- Noblegarden Chocolate
+								["itemID"] = 44791,			-- Noblegarden Chocolate
+								["achievementID"] = 2417,	-- Chocolate Lover
+								["criteriaID"] = 1,			-- Chocolate Lover
+								["u"] = 19,					-- Noblegarden Filter
+								["description"] = "This item is required for the achievement \"Chocolate Lover\".",
+							},
+							{	-- Spring Circlet
+								["itemID"] = 44803,			-- Spring Circlet
+								["u"] = 19,					-- Noblegarden Filter
+							},
+							{	-- Spring Flowers
+								["itemID"] = 45073,			-- Spring Flowers
+								["achievementID"] = 2798,	-- Noble Gardener
+								["criteriaID"] = 8,			-- Shake Your Bunny-Maker [Alliance/Horde]
+								["u"] = 19,					-- Noblegarden Filter
+								["description"] = "This item is required for the achievement \"Shake Your Bunny-Maker\".",
+							},
+							{	-- Spring Rabbit's Foot
+								["itemID"] = 44794,			-- Spring Rabbit's Foot
+								["u"] = 19,					-- Noblegarden Filter
+							},
+							{	-- Spring Robes
+								["itemID"] = 44800,			-- Spring Robes
+								["u"] = 19,					-- Noblegarden Filter
+							},
+							{	-- White Tuxedo Shirt
+								["itemID"] = 6833,			-- White Tuxedo Shirt
+								["u"] = 19,					-- Noblegarden Filter
+								--Note!! Discover from egg only so we don't want to mask every appearance with the achievement
+							},
+						},
+					},
+				},
+			},
+		},
+	},
 });
