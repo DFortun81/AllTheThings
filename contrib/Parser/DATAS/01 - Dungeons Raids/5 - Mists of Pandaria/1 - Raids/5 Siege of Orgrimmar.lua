f7a39e68a8f1adc2da53da9587fe8d630fd52f5c
Lucetia <>
2019-04-23 03:19:06 -0700

    8 ≠ 9

--- contrib/Parser/DATAS/01 - Dungeons Raids/5 - Mists of Pandaria/1 - Raids/5 Siege of Orgrimmar.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/5 - Mists of Pandaria/1 - Raids/5 Siege of Orgrimmar.lua
@@ -1775,7 +1775,7 @@ _.Instances = { tier(5, {	-- Mists of Pandaria
 								i(48126),	-- Razzashi Hatchling
 							},
 						})),
-						un(49, i(105714, {	-- Coalesced Turmoil
+						un(48, i(105714, {	-- Coalesced Turmoil
 							["description"] = "Since the introduction of Legacy Loot this bag is only obtainable if you queue up as a Level 91-100 for the intended raid.  If you are 101+ then you will need to seek out each item based on their original sources.  This change occured in Patch 8.0.1",
 							--[[
 							["crs"] = {
