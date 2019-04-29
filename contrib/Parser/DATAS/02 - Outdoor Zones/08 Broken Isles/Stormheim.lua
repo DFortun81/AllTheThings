598679b0f07769e9bd5aea0c3b849962317318b8
Lucetia <>
2019-04-29 04:21:33 -0700

    pet stuff

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Stormheim.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Stormheim.lua
@@ -577,24 +577,78 @@ _.Zones =
 					},
 				}),
 				n(-25, { 	-- Pet Battle
-					p(1708), 	-- Albatross Chick
-					p(1743), 	-- Black-Footed Fox Kit
-					p(380), 	-- Bucktooth Flapper
-					p(1712), 	-- Golden Eaglet
-					p(647), 	-- Grizzly Squirrel
-					p(550), 	-- Highlands Mouse
-					p(645), 	-- Highlands Turkey
-					p(1579), 	-- Ironclaw Scuttler
-					p(1583), 	-- Kelp Scuttler
-					p(1713), 	-- Long-Eared Owl
-					p(1744), 	-- Mist Fox Kit
-					p(633), 	-- Mountain Skunk
-					p(1441), 	-- Mud Jumper
-					p(743), 	-- Rapana Whelk
-					p(1749), 	-- Rose Taipan
-					p(1736), 	-- Slithering Brownscale
-					p(1917), 	-- Stormstruck Beaver
-					p(1750), 	-- Tiny Apparition
+					{	-- Albatross Chick
+						["speciesID"] = 1708,	-- Albatross Chick
+						["crs"] = { 97018 },	-- Albatross Chick
+					},
+					{	-- Black-Footed Fox Kit
+						["speciesID"] = 1743,	-- Black-Footed Fox Kit
+						["crs"] = { 97741 },	-- Black-Footed Fox Kit
+					},
+					{	-- Bucktooth Flapper
+						["speciesID"] = 380,	-- Bucktooth Flapper
+						["crs"] = { 62992 },	-- Bucktooth Flapper
+					},
+					{	-- Golden Eaglet
+						["speciesID"] = 1712,	-- Golden Eaglet
+						["crs"] = { 97080 },	-- Golden Eaglet
+					},
+					{	-- Grizzly Squirrel
+						["speciesID"] = 647,	-- Grizzly Squirrel
+						["crs"] = { 62818 },	-- Grizzly Squirrel
+					},
+					{	-- Highlands Mouse
+						["speciesID"] = 550,	-- Highlands Mouse
+						["crs"] = { 62905 },	-- Highlands Mouse
+					},
+					{	-- Highlands Turkey
+						["speciesID"] = 645,	-- Highlands Turkey
+						["crs"] = { 62906 },	-- Highlands Turkey
+					},
+					{	-- Ironclaw Scuttler
+						["speciesID"] = 1579,	-- Ironclaw Scuttler
+						["crs"] = { 88474 },	-- Ironclaw Scuttler
+					},
+					{	-- Kelp Scuttler
+						["speciesID"] = 1583,	-- Kelp Scuttler
+						["crs"] = { 88465 },	-- Kelp Scuttler
+					},
+					{	-- Long-Eared Owl
+						["speciesID"] = 1713,	-- Long-Eared Owl
+						["crs"] = { 97118 },	-- Long-Eared Owl
+					},
+					{	-- Mist Fox Kit
+						["speciesID"] = 1744,	-- Mist Fox Kit
+						["crs"] = { 97743 },	-- Mist Fox Kit
+					},
+					{	-- Mountain Skunk
+						["speciesID"] = 633,	-- Mountain Skunk
+						["crs"] = { 61677 },	-- Mountain Skunk
+					},
+					{	-- Mud Jumper
+						["speciesID"] = 1441,	-- Mud Jumper
+						["crs"] = { 83642 },	-- Mud Jumper
+					},
+					{	-- Rapana Whelk
+						["speciesID"] = 743,	-- Rapana Whelk
+						["crs"] = { 64352 },	-- Rapana Whelk
+					},
+					{	-- Rose Taipan
+						["speciesID"] = 1749,	-- Rose Taipan
+						["crs"] = { 97840 },	-- Rose Taipan
+					},
+					{	-- Slithering Brownscale
+						["speciesID"] = 1736,	-- Slithering Brownscale
+						["crs"] = { 97542 },	-- Slithering Brownscale
+					},
+					{	-- Stormstruck Beaver
+						["speciesID"] = 1917,	-- Stormstruck Beaver
+						["crs"] = { 111172 },	-- Stormstruck Beaver
+					},
+					{	-- Tiny Apparition
+						["speciesID"] = 1750,	-- Tiny Apparition
+						["crs"] = { 97952 },	-- Tiny Apparition
+					},
 					n(115742, {	-- Orphaned Direbeak
 						["g"] = {
 							p(1975), 	-- Direbeak Hatchling
