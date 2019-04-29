3d9af4789ae213f610f963a566edc810a1627aa8
Lucetia <>
2019-04-29 04:08:42 -0700

    pet stuff

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Azsuna.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Azsuna.lua
@@ -1130,26 +1130,86 @@ _.Zones =
 					}),
 				}),
 				n(-25, { 	-- Pet Battle
-					p(1708), 	-- Albatross Chick
-					p(706), 	-- Bandicoon
-					p(1914), 	-- Coastal Sandpiper
-					p(396), 	-- Dusk Spiderling
-					p(1774), 	-- Eldritch Manafiend
-					p(1773), 	-- Erudite Manafiend
-					p(1731), 	-- Felspider
-					p(1709), 	-- Fledgling Kingfeather
-					p(1710), 	-- Fledgling Oliveback
-					p(478), 	-- Forest Moth
-					p(464), 	-- Grey Moth
-					p(647), 	-- Grizzly Squirrel
-					p(699), 	-- Jumping Spider
-					p(1728),	-- Juvenile Scuttleback
-					p(1583), 	-- Kelp Scuttler
-					p(1729), 	-- Olivetail Hare
-					p(743), 	-- Rapana Whelk
-					p(1587), 	-- Royal Moth
-					p(1736), 	-- Slithering Brownscale
-					p(1935), 	-- Squirky
+					{	-- Albatross Chick
+						["speciesID"] = 1708,	-- Albatross Chick
+						["crs"] = { 97018 },	-- Albatross Chick
+					},
+					{	-- Bandicoon
+						["speciesID"] = 706,	-- Bandicoon
+						["crs"] = { 63062 },	-- Bandicoon
+					},
+					{	-- Coastal Sandpiper
+						["speciesID"] = 1914,	-- Coastal Sandpiper
+						["crs"] = { 110826 },	-- Coastal Sandpiper
+					},
+					{	-- Dusk Spiderling
+						["speciesID"] = 396,	-- Dusk Spiderling
+						["crs"] = { 61253 },	-- Dusk Spiderling
+					},
+					{	-- Eldritch Manafiend
+						["speciesID"] = 1774,	-- Eldritch Manafiend
+						["crs"] = { 98386 },	-- Eldritch Manafiend
+					},
+					{	-- Erudite Manafiend
+						["speciesID"] = 1773,	-- Erudite Manafiend
+						["crs"] = { 98385 },	-- Erudite Manafiend
+					},
+					{	-- Felspider
+						["speciesID"] = 1731,	-- Felspider
+						["crs"] = { 97323 },	-- Felspider
+					},
+					{	-- Fledgling Kingfeather
+						["speciesID"] = 1709,	-- Fledgling Kingfeather
+						["crs"] = { 97076 },	-- Fledgling Kingfeather
+					},
+					{	-- Fledgling Oliveback
+						["speciesID"] = 1710,	-- Fledgling Oliveback
+						["crs"] = { 97078 },	-- Fledgling Oliveback
+					},
+					{	-- Forest Moth
+						["speciesID"] = 478,	-- Forest Moth
+						["crs"] = { 62177 },	-- Forest Moth
+					},
+					{	-- Grey Moth
+						["speciesID"] = 464,	-- Grey Moth
+						["crs"] = { 62050 },	-- Grey Moth
+					},
+					{	-- Grizzly Squirrel
+						["speciesID"] = 647,	-- Grizzly Squirrel
+						["crs"] = { 62818 },	-- Grizzly Squirrel
+					},
+					{	-- Jumping Spider
+						["speciesID"] = 699,	-- Jumping Spider
+						["crs"] = { 63715 },	-- Jumping Spider
+					},
+					{	-- Juvenile Scuttleback
+						["speciesID"] = 1728,	-- Juvenile Scuttleback
+						["crs"] = { 97283 },	-- Juvenile Scuttleback
+					},
+					{	-- Kelp Scuttler
+						["speciesID"] = 1583,	-- Kelp Scuttler
+						["crs"] = { 88465 },	-- Kelp Scuttler
+					},
+					{	-- Olivetail Hare
+						["speciesID"] = 1729,	-- Olivetail Hare
+						["crs"] = { 97294 },	-- Olivetail Hare
+					},
+					{	-- Rapana Whelk
+						["speciesID"] = 743,	-- Rapana Whelk
+						["crs"] = { 64352 },	-- Rapana Whelk
+					},
+					{	-- Royal Moth
+						["speciesID"] = 1587,	-- Royal Moth
+						["crs"] = { 88417 },	-- Royal Moth
+					},
+					{	-- Slithering Brownscale
+						["speciesID"] = 1736,	-- Slithering Brownscale
+						["crs"] = { 97542 },	-- Slithering Brownscale
+					},
+					{	-- Squirky
+						["speciesID"] = 1935,	-- Squirky
+						["crs"] = { 113440 },	-- Squirky
+					},
 					n(115741, {	-- Orphaned Bloodgazer
 						["groups"] = {
 							p(1977),  	-- Bloodgazer Hatchling
