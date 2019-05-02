bab5b94fed120d8efec7164073cf3213db9e6578
Lucetia <>
2019-05-02 16:18:49 -0700

    - Human CW Questline Updated

---
+++ contrib/Parser/DATAS/21 - Holidays/04 - Children's Week/Quests.lua
@@ -0,0 +1,248 @@
+--------------------------------------------
+--     H O L I D A Y S  M O D U L E       --
+--------------------------------------------
+_.Holidays = 
+{
+	{	-- Children's Week
+		["holidayID"] = 235445,	-- Children's Week
+		["u"] = 20,				-- Children's Week Filter
+		["g"] = {
+			{	-- Quests
+				["npcID"] = -17,	-- Quests
+				["g"] = {
+					{	-- A Warden of the Alliance [Alliance - Human]
+						["questID"] = 171,	-- A Warden of the Alliance
+						["u"] = 20,			-- Children's Week Filter
+						["qg"] = 14305,		-- Human Orphan
+						["sourceQuests"] = {
+							29117,	-- Let's Go Fly a Kite
+							29119,	-- You Scream, I Scream...
+						},
+						["coords"] = {
+							{ 56.31, 53.99, 84 },	-- Orphan Matron Nightingale
+							{ 56.31, 68.45, 84 },	-- Craggle Wobbletop Path
+							{ 56.67, 66.57, 84 },	-- Craggle Wobbletop Path
+							{ 56.75, 70.04, 84 },	-- Craggle Wobbletop Path
+							{ 57.19, 71.63, 84 },	-- Craggle Wobbletop Path
+							{ 57.57, 65.42, 84 },	-- Craggle Wobbletop Path
+							{ 57.63, 73.23, 84 },	-- Craggle Wobbletop Path
+							{ 58.07, 74.83, 84 },	-- Craggle Wobbletop Path
+							{ 58.44, 64.31, 84 },	-- Craggle Wobbletop Path
+							{ 58.51, 76.42, 84 },	-- Craggle Wobbletop Path
+							{ 59.12, 77.98, 84 },	-- Craggle Wobbletop Path
+							{ 59.44, 63.31, 84 },	-- Craggle Wobbletop Path
+							{ 60.44, 62.44, 84 },	-- Craggle Wobbletop Path
+							{ 61.21, 61.78, 84 },	-- Craggle Wobbletop Path
+							{ 62.31, 61.34, 84 },	-- Craggle Wobbletop Path
+							{ 63.44, 61.05, 84 },	-- Craggle Wobbletop Path
+							{ 64.62, 61.54, 84 },	-- Craggle Wobbletop Path
+							{ 65.63, 62.99, 84 },	-- Craggle Wobbletop Path
+							{ 66.61, 64.43, 84 },	-- Craggle Wobbletop Path
+						},
+						["maps"] = { 
+							84,	-- Stormwind City
+						},
+						["races"] = ALLIANCE_ONLY,
+						["g"] = {
+							{	-- Piglet's Collar
+								["itemID"] = 23007,		-- Piglet's Collar
+								["u"] = 20,				-- Children's Week Filter
+							},
+							{	-- Rat Cage
+								["itemID"] = 23015,		-- Rat Cage
+								["u"] = 20,				-- Children's Week Filter
+							},
+							{	-- Snail Shell
+								["itemID"] = 66073,		-- Snail Shell
+								["u"] = 20,				-- Children's Week Filter
+							},
+							{	-- Turtle Box
+								["itemID"] = 23002,		-- Turtle Box
+								["u"] = 20,				-- Children's Week Filter
+							},
+							{	-- Pet Care Package
+								["itemID"] = 116202,	-- Pet Care Package
+								["u"] = 20,				-- Children's Week Filter
+								["g"] = {
+									{	-- "Dapper Gentleman" Costume
+										["itemID"] = 103786,	-- "Dapper Gentleman" Costume
+									},
+									{	-- "Dread Pirate" Costume
+										["itemID"] = 103795,	-- "Dread Pirate" Costume
+									},
+
+									{	-- "Little Princess" Costume
+										["itemID"] = 103789,	-- "Little Princess" Costume
+									},
+
+									{	-- Big Pink Bow
+										["itemID"] = 103797,	-- Big Pink Bow
+									},
+
+									{	-- Flawless Battle-Stone
+										["itemID"] = 92741,	-- Flawless Battle-Stone
+									},
+
+									{	-- Lesser Pet Treat
+										["itemID"] = 98112,	-- Lesser Pet Treat
+									},
+
+									{	-- Magical Mini-Treat
+										["itemID"] = 89906,	-- Magical Mini-Treat
+									},
+
+									{	-- Magical Pet Biscuit
+										["itemID"] = 71153,	-- Magical Pet Biscuit
+									},
+
+									{	-- Pet Treat
+										["itemID"] = 98114,	-- Pet Treat
+									},
+								},
+							},
+							{	-- Curmudgeon's Payoff
+								["itemID"] = 23022,		-- Curmudgeon's Payoff
+								["u"] = 20,				-- Children's Week Filter
+								["description"] = "Contains 5 gold.",
+							},
+						},
+					},
+					{	-- Children's Week [Alliance - Human]
+						["questID"] = 1468,		-- Children's Week
+						["u"] = 20,				-- Children's Week Filter
+						["qg"] = 51988,			-- Orphan Matron Nightingale
+						["maps"] = { 
+							84,	-- Stormwind City
+						},
+						["coords"] = {
+							{ 56.31, 53.99, 84 },	-- Orphan Matron Nightingale
+						},
+						["races"] = ALLIANCE_ONLY,
+					},
+					{	-- Cruisin' the Chasm [Alliance - Human]
+						["questID"] = 29093,	-- Cruisin' the Chasm
+						["u"] = 20,				-- Children's Week Filter
+						["qg"] = 14305,			-- Human Orphan
+						["sourceQuests"] = {
+							1468,	-- Children's Week
+						},
+						["coords"] = {
+							{ 57.71, 53.27, 52 },	-- Rental Chopper
+							{ 57.78, 53.06, 52 },	-- Rental Chopper
+						},
+						["maps"] = { 
+							52,	-- Westfall
+							84,	-- Stormwind City
+						},
+						["races"] = ALLIANCE_ONLY,
+					},
+					{	-- Let's Go Fly a Kite [Alliance - Human]
+						["questID"] = 29117,	-- Let's Go Fly a Kite
+						["u"] = 20,				-- Children's Week Filter
+						["qg"] = 14305,			-- Human Orphan
+						["sourceQuests"] = { 
+							29093,	-- Cruisin' the Chasm
+							54130,	-- Our New Friends
+							29106,	-- The Biggest Diamond Ever!
+						},
+						["coords"] = {
+							{ 56.31, 68.45, 84 },	-- Craggle Wobbletop Path
+							{ 56.67, 66.57, 84 },	-- Craggle Wobbletop Path
+							{ 56.75, 70.04, 84 },	-- Craggle Wobbletop Path
+							{ 57.19, 71.63, 84 },	-- Craggle Wobbletop Path
+							{ 57.57, 65.42, 84 },	-- Craggle Wobbletop Path
+							{ 57.63, 73.23, 84 },	-- Craggle Wobbletop Path
+							{ 58.07, 74.83, 84 },	-- Craggle Wobbletop Path
+							{ 58.44, 64.31, 84 },	-- Craggle Wobbletop Path
+							{ 58.51, 76.42, 84 },	-- Craggle Wobbletop Path
+							{ 59.12, 77.98, 84 },	-- Craggle Wobbletop Path
+							{ 59.44, 63.31, 84 },	-- Craggle Wobbletop Path
+							{ 60.44, 62.44, 84 },	-- Craggle Wobbletop Path
+							{ 61.21, 61.78, 84 },	-- Craggle Wobbletop Path
+							{ 62.31, 61.34, 84 },	-- Craggle Wobbletop Path
+							{ 63.44, 61.05, 84 },	-- Craggle Wobbletop Path
+							{ 64.62, 61.54, 84 },	-- Craggle Wobbletop Path
+							{ 65.63, 62.99, 84 },	-- Craggle Wobbletop Path
+							{ 66.61, 64.43, 84 },	-- Craggle Wobbletop Path
+						},
+						["maps"] = { 
+							84,	-- Stormwind City
+						},
+						["races"] = ALLIANCE_ONLY,
+					},
+					{	-- Malfurion Has Returned! [Alliance - Human] REMOVED
+						["questID"] = 29107,	-- Malfurion Has Returned!
+						["u"] = 2,				-- Unobtainable	Note!! Looks like quest was removed in BFA due to Teladrassil burning!!
+						["qg"] = 14305,			-- Human Orphan
+						["sourceQuests"] = {
+							1468,	-- Children's Week
+						},
+						["maps"] = { 
+							84,	-- Stormwind City
+							89,	-- Darnassus
+						},
+						["maps"] = { 89, },	-- Darnassus
+						["races"] = ALLIANCE_ONLY,
+					},
+					{	-- Our New Friends [Alliance - Human]
+						["questID"] = 54130,	-- Our New Friends
+						["u"] = 20,				-- Children's Week Filter
+						["qg"] = 14305,			-- Human Orphan
+						["sourceQuests"] = {
+							1468,	-- Children's Week
+						},
+						["coords"] = {
+							{ 53.28, 15.25, 84 },	-- Stormwind Embassy
+						},
+						["maps"] = { 
+							84,	-- Stormwind City
+						},
+						["races"] = ALLIANCE_ONLY,
+					},
+					{	-- The Biggest Diamond Ever! [Alliance - Human]
+						["questID"] = 29106,	-- The Biggest Diamond Ever!
+						["u"] = 20,				-- Children's Week Filter
+						["qg"] = 14305,			-- Human Orphan
+						["sourceQuests"] = {
+							1468,	-- Children's Week
+						},
+						["coords"] = {
+							{ 44.61, 49.40, 87 },	-- The High Seat [Leads to tunnel]
+							{ 44.76, 52.25, 87 },	-- Path to Old Ironforge
+							{ 37.43, 54.32, 1361 },	-- Magni [Old Ironforge]
+						},
+						["maps"] = { 
+							84,		-- Stormwind City
+							87,		-- Ironforge
+							1361,	-- Old Ironforge
+						},
+						["maps"] = { 87, },	-- Ironforge
+						["races"] = ALLIANCE_ONLY,
+					},
+					{	-- You Scream, I Scream... [Alliance - Human]
+						["questID"] = 29119,	-- You Scream, I Scream...
+						["u"] = 20,				-- Children's Week Filter
+						["qg"] = 14305,			-- Human Orphan
+						["sourceQuests"] = { 
+							29093,	-- Cruisin' the Chasm
+							54130,	-- Our New Friends
+							29106,	-- The Biggest Diamond Ever!
+						},
+						["crs"] = {
+							52420,	-- Bazzil Frostweaver
+							52421,	-- Hans Coldhearth
+						},
+						["coords"] = {
+							{ 49.01, 89.74, 84 },	-- Hans Coldhearth
+							{ 49.12, 90.07, 84 },	-- Bazzil Frostweaver
+						},
+						["maps"] = { 
+							84,	-- Stormwind City
+						},
+						["races"] = ALLIANCE_ONLY,
+					},
+				},
+			},
+		},
+	},
+};
