53502f16586e00e66054687c9c872e3777de9d43
Lucetia <>
2019-05-06 22:23:13 -0700

    - Fixed a couple of Talador achievements

--- contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Talador/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Talador/Quests.lua
@@ -749,9 +749,10 @@ _.Zones =
 							["coord"] = { 70.5, 57.3, 535 },
 							["sourceQuest"] = 33579,	-- What the Draenei Found
 							["groups"] = {
-								{
+								{	-- The Plight of the Arakkoa
 									["achievementID"] = 8919,	-- Don't Let the Tala-door Hit You on the Way Out
-									["criteriaID"] = 3,
+									["criteriaID"] = 3,			-- The Plight of the Arakkoa
+									["races"] = HORDE_ONLY,
 								},
 							},
 						},
@@ -970,9 +971,10 @@ _.Zones =
 							["coord"] = { 70.7, 56.7, 535 },
 							["sourceQuest"] = 33579,	-- What the Draenei Found
 							["groups"] = {
-								{
+								{	-- The Plight of the Arakkoa
 									["achievementID"] = 8919,	-- Don't Let the Tala-door Hit You on the Way Out
-									["criteriaID"] = 3,
+									["criteriaID"] = 3,			-- The Plight of the Arakkoa
+									["races"] = HORDE_ONLY,
 								},
 							},
 						},
@@ -1302,9 +1304,10 @@ _.Zones =
 							["races"] = HORDE_ONLY,
 							["sourceQuest"] = 34962,	-- Khadgar's Plan
 							["groups"] = {
-								{
+								{	-- The Battle for Shattrath
 									["achievementID"] = 8919,	-- Don't Let the Tala-door Hit You on the Way Out
-									["criteriaID"] = 2,
+									["criteriaID"] = 2,			-- The Battle for Shattrath
+									["races"] = HORDE_ONLY,
 								},
 								i(112518),	-- Auchenai Keeper Gloves
 								i(112519),	-- Sha'tari Deadeye Gauntlets
@@ -1368,9 +1371,10 @@ _.Zones =
 							["races"] = HORDE_ONLY,
 							["sourceQuest"] = 34870,	-- The Lord of the Gordunni
 							["groups"] = {
-								{
+								{	-- Establishing Your Post
 									["achievementID"] = 8919,	-- Don't Let the Tala-door Hit You on the Way Out
-									["criteriaID"] = 1,
+									["criteriaID"] = 1,			-- Establishing Your Post
+									["races"] = HORDE_ONLY,
 								},
 							},
 						},
