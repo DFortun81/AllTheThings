edff3fb6d4a6a23bd209b122b4a31bedd71676e2
Lucetia <>
2019-05-07 23:09:12 -0700

    - Hovering Ahn'QIraj The Fallen Kingdom pets now shows collected/not collected

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Ahn'Qiraj The Fallen Kingdom/Pet Battles.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Ahn'Qiraj The Fallen Kingdom/Pet Battles.lua
@@ -6,15 +6,25 @@ _.Zones =
 	m(12, {	-- Kalimdor
 		m(327, {	-- Ahn'Qiraj: The Fallen Kingdom
 			["groups"] = {
-				n(-25, { 	-- Pet Battle
-					p(513, {	-- Qiraji Guardling
-						["description"] = "Starts spawning March 20th. Stops spawning September 23rd.",
-					}),
-					p(512, {-- Scarab Hatchling
-						["description"] = "Can be found near the gates of Ahn'Qiraj, or in the un-instanced zone south of Silithus.",
-					}),
-					p(511), 	-- Sidewinder
-				}),
+				{	-- Pet Battle
+					["npcID"] = -25,	-- Pet Battle
+					["g"] = {
+						{	-- Qiraji Guardling
+							["speciesID"] = 513,	-- Qiraji Guardling
+							["crs"] = { 62526 },	-- Qiraji Guardling
+							["description"] = "Starts spawning March 20th. Stops spawning September 23rd.",
+						},
+						{	-- Scarab Hatchling
+							["speciesID"] = 512,	-- Scarab Hatchling
+							["crs"] = { 62524 },	-- Scarab Hatchling
+							["description"] = "Can be found near the gates of Ahn'Qiraj, or in the un-instanced zone south of Silithus.",
+						},
+						{	-- Sidewinder
+							["speciesID"] = 511,	-- Sidewinder
+							["crs"] = { 62523 },	-- Sidewinder
+						},
+					},
+				},
 			},
 		}),
 	}),
