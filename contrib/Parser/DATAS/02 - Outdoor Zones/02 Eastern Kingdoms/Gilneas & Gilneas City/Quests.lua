04e81e12000647cc6f4c95e6b1382f77b5dc13b4
Lucetia <>
2019-05-12 23:00:26 -0700

    - More worgen quests

--- contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Gilneas & Gilneas City/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Gilneas & Gilneas City/Quests.lua
@@ -25,6 +25,32 @@ _.Zones =
 									22,	-- Worgen
 								},
 							},
+							{	-- As the Land Shatters
+								["questID"] = 14396,	-- As the Land Shatters
+								["qg"] = 36290,			-- Lord Godfrey
+								["sourceQuests"] = {
+									14386,	-- Leader of the Pack
+								},
+								["coords"] = {
+									{ 28.97, 63.93, 179 },
+								},
+								["races"] = {
+									22,	-- Worgen
+								},
+							},
+							{	-- Gasping for Breath
+								["questID"] = 14395,	-- Gasping for Breath
+								["qg"] = 36451,			-- Prince Liam Greymane
+								["sourceQuests"] = {
+									14396,	-- As the Land Shatters
+								},
+								["coords"] = {
+									{ 29.03, 65.06, 179 },
+								},
+								["races"] = {
+									22,	-- Worgen
+								},
+							},
 							{	-- Hold the Line
 								["questID"] = 14347,	-- Hold the Line
 								["qg"] = 36140,			-- Prince Liam Greymane
@@ -114,6 +140,62 @@ _.Zones =
 									22,	-- Worgen
 								},
 							},
+							{	-- Leader of the Pack
+								["questID"] = 14386,	-- Leader of the Pack
+								["qg"] = 36290,			-- Lord Godfrey
+								["sourceQuests"] = {
+									14368,	-- Save the Children!
+									14382,	-- Two By Sea
+									14369,	-- Unleash the Beast
+								},
+								["coords"] = {
+									{ 28.97, 63.93, 179 },
+								},
+								["races"] = {
+									22,	-- Worgen
+								},
+								["g"] = {
+									{	-- Dark Ranger's Hauberk
+										["itemID"] = 131355,		-- Dark Ranger's Hauberk
+										["ignoreSource"] = true,	-- White Item
+									},
+									{	-- Dark Ranger's Vest
+										["itemID"] = 55007,			-- Dark Ranger's Vest
+										["ignoreSource"] = true,	-- White Item
+									},
+									{	-- Leader's Boots
+										["itemID"] = 55008,			-- Leader's Boots
+										["ignoreSource"] = true,	-- White Item
+									},
+								},
+							},
+							{	-- Save the Children!
+								["questID"] = 14368,	-- Save the Children!
+								["qg"] = 36291,			-- Melinda Hammond
+								["sourceQuests"] = {
+									14367,	-- The Allen's Storm Caller
+								},
+								["coords"] = {
+									{ 28.93, 64.04, 179 },
+								},
+								["races"] = {
+									22,	-- Worgen
+								},
+								["g"] = {
+									{	-- Caregiver's Leggings
+										["itemID"] = 54981,			-- Caregiver's Leggings
+										["ignoreSource"] = true,	-- White Item
+									},
+									{	-- Gloves of Protection
+										["itemID"] = 54982,			-- Gloves of Protection
+										["ignoreSource"] = true,	-- White Item
+									},
+									{	-- Protector's Legguards
+										["itemID"] = 131351,		-- Protector's Legguards
+										["ignoreSource"] = true,	-- White Item
+									},
+								},
+							},
 							{	-- The Allen's Storm Caller
 								["questID"] = 14367,	-- The Allen's Storm Caller
 								["qg"] = 34571,			-- Gwen Armstead
@@ -122,6 +204,32 @@ _.Zones =
 								},
 								["coords"] = {
 									{ 37.42, 63.34, 179 },
+								},
+								["races"] = {
+									22,	-- Worgen
+								},
+							},
+							{	-- Two By Sea
+								["questID"] = 14382,	-- Two By Sea
+								["qg"] = 36290,			-- Lord Godfrey
+								["sourceQuests"] = {
+									14367,	-- The Allen's Storm Caller
+								},
+								["coords"] = {
+									{ 28.97, 63.93, 179 },
+								},
+								["races"] = {
+									22,	-- Worgen
+								},
+							},
+							{	-- Unleash the Beast
+								["questID"] = 14369,	-- Unleash the Beast
+								["qg"] = 36290,			-- Lord Godfrey
+								["sourceQuests"] = {
+									14367,	-- The Allen's Storm Caller
+								},
+								["coords"] = {
+									{ 28.97, 63.93, 179 },
 								},
 								["races"] = {
 									22,	-- Worgen
@@ -148,9 +256,7 @@ _.Zones =
 						["races"] = {22},
 						["g"] = {
 							qa(14319),	-- Further Treatment (REMOVED in Beta)
-							qg(36140, qa(14347)),	-- Hold the Line
 							qg(36616, qa(14434)),	-- Rut'theran Village
-							qg(36140, qa(14348)),	-- You Can't Take 'Em Alone
 							q(14220),	-- This Is the End (REMOVED in Beta)
 						},
 					}),
