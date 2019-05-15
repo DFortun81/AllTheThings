7809339ab75d349d979b28625176c84c6460a6aa
Lucetia <>
2019-05-15 00:54:04 -0700

    -- Brewfest Achievements condensed.  In account/debug you should no longer see opposite faction achievement

--- contrib/Parser/DATAS/21 - Holidays/07 - Brewfest/Achievements.lua
+++ contrib/Parser/DATAS/21 - Holidays/07 - Brewfest/Achievements.lua
@@ -129,90 +129,58 @@ _.Holidays = bubbleDown({["u"] = 24},
 					}),
 					["description"] = "\nEach Brewfest Stein has five different filled appearances. You want to fill up the stein with one brew, log off then refresh your collection by Shift+Clicking on this achievement. Once it has registered then you move on to the next brew and repeat.\n",
 				})),
-				un(24, ach(1184, {	-- Strange Brew [Alliance]
-					["groups"] = bubbleDown({["u"] = 24}, {
-						{	-- Barleybrew Clear	
-							["itemID"] = 33030,	-- Barleybrew Clear
-							["criteriaID"] = 1,	-- Barleybrew Clear
-						},		
-						{	-- Barleybrew Dark	
-							["itemID"] = 33029,	-- Barleybrew Dark
-							["criteriaID"] = 2,	-- Barleybrew Dark
-						},		
-						{	-- Barleybrew Light	
-							["itemID"] = 33028,	-- Barleybrew Light
-							["criteriaID"] = 3,	-- Barleybrew Light
-						},		
-						{	-- Gordok Grog	
-							["itemID"] = 33034,	-- Gordok Grog
-							["criteriaID"] = 4,	-- Gordok Grog
-						},		
-						{	-- Mudder's Milk	
-							["itemID"] = 33036,	-- Mudder's Milk
-							["criteriaID"] = 5,	-- Mudder's Milk
-						},		
-						{	-- Ogre Mead	
-							["itemID"] = 33035,	-- Ogre Mead
-							["criteriaID"] = 6,	-- Ogre Mead
-						},		
-						{	-- Thunder 45	
-							["itemID"] = 33031,	-- Thunder 45
-							["criteriaID"] = 7,	-- Thunder 45
-						},		
-						{	-- Thunderbrew Ale	
-							["itemID"] = 33032,	-- Thunderbrew Ale
-							["criteriaID"] = 8,	-- Thunderbrew Ale
-						},		
-						{	-- Thunderbrew Stout	
-							["itemID"] = 33033,	-- Thunderbrew Stout
-							["criteriaID"] = 9,	-- Thunderbrew Stout
-						},		
-					}),
-					["races"] = ALLIANCE_ONLY,
-					["u"] = 24,	-- Brewfest
-				})),
-				un(24, ach(1203, {	-- Strange Brew [Horde]
-					["groups"] = bubbleDown({["u"] = 24}, {
-						{	-- Brewdoo Magic	
-							["itemID"] = 34021,	-- Brewdoo Magic
-							["criteriaID"] = 1,	-- Brewdoo Magic
-						},		
-						{	-- Gordok Grog	
-							["itemID"] = 33034,	-- Gordok Grog
-							["criteriaID"] = 2,	-- Gordok Grog
-						},		
-						{	-- Jungle River Water	
-							["itemID"] = 34020,	-- Jungle River Water
-							["criteriaID"] = 3,	-- Jungle River Water
-						},		
-						{	-- Long Stride Brew	
-							["itemID"] = 34018,	-- Long Stride Brew
-							["criteriaID"] = 4,	-- Long Stride Brew
-						},		
-						{	-- Mudder's Milk	
-							["itemID"] = 33036,	-- Mudder's Milk
-							["criteriaID"] = 5,	-- Mudder's Milk
-						},		
-						{	-- Ogre Mead	
-							["itemID"] = 33035,	-- Ogre Mead
-							["criteriaID"] = 6,	-- Ogre Mead
-						},		
-						{	-- Path of Brew	
-							["itemID"] = 34019,	-- Path of Brew
-							["criteriaID"] = 7,	-- Path of Brew
-						},		
-						{	-- Small Step Brew	
-							["itemID"] = 34017,	-- Small Step Brew
-							["criteriaID"] = 8,	-- Small Step Brew
-						},		
-						{	-- Stout Shrunken Head	
-							["itemID"] = 34022,	-- Stout Shrunken Head
-							["criteriaID"] = 9,	-- Stout Shrunken Head
-						},		
-					}),
-					["races"] = HORDE_ONLY,
-					["u"] = 24,	-- Brewfest
-				})),
+				{	-- Strange Brew
+					["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+					["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+					["u"] = 24,							-- Brewfest Filter
+					["g"] = {
+						{	-- Barleybrew Clear // Brewdoo Magic
+							["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+							["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+							["criteriaID"] = 1,					-- Barleybrew Clear // Brewdoo Magic
+						},
+						{	-- Barleybrew Dark // Gordok Grog
+							["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+							["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+							["criteriaID"] = 2,					-- Barleybrew Dark // Gordok Grog
+						},
+						{	-- Barleybrew Light // Jungle River Water
+							["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+							["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+							["criteriaID"] = 3,					-- Barleybrew Light // Jungle River Water
+						},
+						{	-- Gordok Grog // Long Stride Brew
+							["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+							["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+							["criteriaID"] = 4,					-- Gordok Grog // Long Stride Brew
+						},
+						{	-- Mudder's Milk // Mudder's Milk
+							["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+							["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+							["criteriaID"] = 5,					-- Mudder's Milk // Mudder's Milk
+						},
+						{	-- Ogre Mead // Ogre Mead
+							["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+							["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+							["criteriaID"] = 6,					-- Ogre Mead // Ogre Mead
+						},
+						{	-- Thunder 45 // Path of Brew
+							["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+							["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+							["criteriaID"] = 7,					-- Thunder 45 // Path of Brew
+						},
+						{	-- Thunderbrew Ale // Small Step Brew
+							["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+							["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+							["criteriaID"] = 8,					-- Thunderbrew Ale // Small Step Brew
+						},
+						{	-- Thunderbrew Stout // Stout Shrunken Head
+							["allianceAchievementID"] = 1184,	-- Strange Brew [Alliance]
+							["hordeAchievementID"] = 1203,		-- Strange Brew [Horde]
+							["criteriaID"] = 9,					-- Thunderbrew Stout // Stout Shrunken Head
+						},
+					},
+				},
 				un(2, ach(1292, {	-- Yellow Brewfest Stein [Removed]
 					["groups"] = bubbleDown({["u"] = 2}, {
 						i(32912, {	-- Yellow Brewfest Stein
@@ -233,3 +201,81 @@ _.Holidays = bubbleDown({["u"] = 24},
 		}),
 	}),
 });
+
+--[[ TODO: Transfer these achievement criteria to the item on the actual vendor
+-- Alliance
+{	-- Barleybrew Clear
+	["itemID"] = 33030,	-- Barleybrew Clear
+	["criteriaID"] = 1,	-- Barleybrew Clear
+},
+{	-- Barleybrew Dark
+	["itemID"] = 33029,	-- Barleybrew Dark
+	["criteriaID"] = 2,	-- Barleybrew Dark
+},
+{	-- Barleybrew Light
+	["itemID"] = 33028,	-- Barleybrew Light
+	["criteriaID"] = 3,	-- Barleybrew Light
+},
+{	-- Gordok Grog
+	["itemID"] = 33034,	-- Gordok Grog
+	["criteriaID"] = 4,	-- Gordok Grog
+},
+{	-- Mudder's Milk
+	["itemID"] = 33036,	-- Mudder's Milk
+	["criteriaID"] = 5,	-- Mudder's Milk
+},
+{	-- Ogre Mead
+	["itemID"] = 33035,	-- Ogre Mead
+	["criteriaID"] = 6,	-- Ogre Mead
+},
+{	-- Thunder 45
+	["itemID"] = 33031,	-- Thunder 45
+	["criteriaID"] = 7,	-- Thunder 45
+},
+{	-- Thunderbrew Ale
+	["itemID"] = 33032,	-- Thunderbrew Ale
+	["criteriaID"] = 8,	-- Thunderbrew Ale
+},
+{	-- Thunderbrew Stout
+	["itemID"] = 33033,	-- Thunderbrew Stout
+	["criteriaID"] = 9,	-- Thunderbrew Stout
+},
+
+-- Horde
+{	-- Brewdoo Magic
+	["itemID"] = 34021,	-- Brewdoo Magic
+	["criteriaID"] = 1,	-- Brewdoo Magic
+},
+{	-- Gordok Grog
+	["itemID"] = 33034,	-- Gordok Grog
+	["criteriaID"] = 2,	-- Gordok Grog
+},
+{	-- Jungle River Water
+	["itemID"] = 34020,	-- Jungle River Water
+	["criteriaID"] = 3,	-- Jungle River Water
+},
+{	-- Long Stride Brew
+	["itemID"] = 34018,	-- Long Stride Brew
+	["criteriaID"] = 4,	-- Long Stride Brew
+},
+{	-- Mudder's Milk
+	["itemID"] = 33036,	-- Mudder's Milk
+	["criteriaID"] = 5,	-- Mudder's Milk
+},
+{	-- Ogre Mead
+	["itemID"] = 33035,	-- Ogre Mead
+	["criteriaID"] = 6,	-- Ogre Mead
+},
+{	-- Path of Brew
+	["itemID"] = 34019,	-- Path of Brew
+	["criteriaID"] = 7,	-- Path of Brew
+},
+{	-- Small Step Brew
+	["itemID"] = 34017,	-- Small Step Brew
+	["criteriaID"] = 8,	-- Small Step Brew
+},
+{	-- Stout Shrunken Head
+	["itemID"] = 34022,	-- Stout Shrunken Head
+	["criteriaID"] = 9,	-- Stout Shrunken Head
+},
+--]]
