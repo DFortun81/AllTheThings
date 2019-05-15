90cd7138ad751db478cb6ea45ffd7ca52714f2eb
Gaulish <l2pwncomputers@hotmail.com>
2019-05-15 02:48:14 -0600

    Tailoring discovery update

---
+++ contrib/Parser/DATAS/11 - Professions/Tailoring/Recipes/Discovery.lua
@@ -0,0 +1,170 @@
+profession(197, {    -- Tailoring
+	filter(200, {
+		{
+			["categoryID"] = 950,  -- Pandaria Patterns
+			["g"] = {
+				n(-37, {    -- Discovery
+					["groups"] = {
+						spell(143011, {	-- Celestial Cloth
+							recipe(142964),	-- Belt of the Night Sky
+							recipe(142955),	-- Leggings of the Night Sky
+							recipe(142960),	-- White Cloud Belt
+							recipe(142951),	-- White Cloud Leggings
+							recipe(143053),	-- Crafted Malevolent Gladiator's Cape of Cruelty
+							recipe(143054),	-- Crafted Malevolent Gladiator's Cape of Prowess
+							recipe(143082),	-- Crafted Malevolent Gladiator's Cloak of Alacrity
+							recipe(143083),	-- Crafted Malevolent Gladiator's Cloak of Prowess
+							recipe(143056),	-- Crafted Malevolent Gladiator's Cord of Accuracy
+							recipe(143055),	-- Crafted Malevolent Gladiator's Cord of Cruelty
+							recipe(143057),	-- Crafted Malevolent Gladiator's Cord of Meditation
+							recipe(143061),	-- Crafted Malevolent Gladiator's Cuffs of Accuracy
+							recipe(143063),	-- Crafted Malevolent Gladiator's Cuffs of Meditation
+							recipe(143062),	-- Crafted Malevolent Gladiator's Cuffs of Prowess
+							recipe(143064),	-- Crafted Malevolent Gladiator's Drape of Cruelty
+							recipe(143066),	-- Crafted Malevolent Gladiator's Drape of Meditation
+							recipe(143065),	-- Crafted Malevolent Gladiator's Drape of Prowess
+							recipe(143088),	-- Crafted Malevolent Gladiator's Felweave Amice
+							recipe(143085),	-- Crafted Malevolent Gladiator's Felweave Cowl
+							recipe(143084),	-- Crafted Malevolent Gladiator's Felweave Handguards
+							recipe(143087),	-- Crafted Malevolent Gladiator's Felweave Raiment
+							recipe(143086),	-- Crafted Malevolent Gladiator's Felweave Trousers
+							recipe(143072),	-- Crafted Malevolent Gladiator's Mooncloth Gloves
+							recipe(143073),	-- Crafted Malevolent Gladiator's Mooncloth Helm
+							recipe(143074),	-- Crafted Malevolent Gladiator's Mooncloth Leggings
+							recipe(143076),	-- Crafted Malevolent Gladiator's Mooncloth Mantle
+							recipe(143075),	-- Crafted Malevolent Gladiator's Mooncloth Robe
+							recipe(143077),	-- Crafted Malevolent Gladiator's Satin Gloves
+							recipe(143078),	-- Crafted Malevolent Gladiator's Satin Hood
+							recipe(143079),	-- Crafted Malevolent Gladiator's Satin Leggings
+							recipe(143081),	-- Crafted Malevolent Gladiator's Satin Mantle
+							recipe(143080),	-- Crafted Malevolent Gladiator's Satin Robe
+							recipe(143071),	-- Crafted Malevolent Gladiator's Silk Amice
+							recipe(143068),	-- Crafted Malevolent Gladiator's Silk Cowl
+							recipe(143067),	-- Crafted Malevolent Gladiator's Silk Handguards
+							recipe(143070),	-- Crafted Malevolent Gladiator's Silk Robe
+							recipe(143069),	-- Crafted Malevolent Gladiator's Silk Trousers
+							recipe(143059),	-- Crafted Malevolent Gladiator's Treads of Alacrity
+							recipe(143058),	-- Crafted Malevolent Gladiator's Treads of Cruelty
+							recipe(143060),	-- Crafted Malevolent Gladiator's Treads of Meditation
+						}),
+						spell(125557, {	-- Imperial Silk
+							recipe(138598),	-- Falling Blossom Cowl
+							recipe(138600),	-- Falling Blossom Hood
+							recipe(138599),	-- Falling Blossom Sandals
+							recipe(138597),	-- Falling Blossom Treads
+							recipe(137907),	-- Crafted Dreadful Gladiator's Cape of Cruelty
+							recipe(137908),	-- Crafted Dreadful Gladiator's Cape of Prowess
+							recipe(137936),	-- Crafted Dreadful Gladiator's Cloak of Alacrity
+							recipe(137937),	-- Crafted Dreadful Gladiator's Cloak of Prowess
+							recipe(137910),	-- Crafted Dreadful Gladiator's Cord of Accuracy
+							recipe(137909),	-- Crafted Dreadful Gladiator's Cord of Cruelty
+							recipe(137911),	-- Crafted Dreadful Gladiator's Cord of Meditation
+							recipe(137915),	-- Crafted Dreadful Gladiator's Cuffs of Accuracy
+							recipe(137917),	-- Crafted Dreadful Gladiator's Cuffs of Meditation
+							recipe(137916),	-- Crafted Dreadful Gladiator's Cuffs of Prowess
+							recipe(137918),	-- Crafted Dreadful Gladiator's Drape of Cruelty
+							recipe(137920),	-- Crafted Dreadful Gladiator's Drape of Meditation
+							recipe(137919),	-- Crafted Dreadful Gladiator's Drape of Prowess
+							recipe(137942),	-- Crafted Dreadful Gladiator's Felweave Amice
+							recipe(137939),	-- Crafted Dreadful Gladiator's Felweave Cowl
+							recipe(137938),	-- Crafted Dreadful Gladiator's Felweave Handguards
+							recipe(137941),	-- Crafted Dreadful Gladiator's Felweave Raiment
+							recipe(137940),	-- Crafted Dreadful Gladiator's Felweave Trousers
+							recipe(137926),	-- Crafted Dreadful Gladiator's Mooncloth Gloves
+							recipe(137927),	-- Crafted Dreadful Gladiator's Mooncloth Helm
+							recipe(137928),	-- Crafted Dreadful Gladiator's Mooncloth Leggings
+							recipe(137930),	-- Crafted Dreadful Gladiator's Mooncloth Mantle
+							recipe(137929),	-- Crafted Dreadful Gladiator's Mooncloth Robe
+							recipe(137931),	-- Crafted Dreadful Gladiator's Satin Gloves
+							recipe(137932),	-- Crafted Dreadful Gladiator's Satin Hood
+							recipe(137933),	-- Crafted Dreadful Gladiator's Satin Leggings
+							recipe(137935),	-- Crafted Dreadful Gladiator's Satin Mantle
+							recipe(137934),	-- Crafted Dreadful Gladiator's Satin Robe
+							recipe(137925),	-- Crafted Dreadful Gladiator's Silk Amice
+							recipe(137922),	-- Crafted Dreadful Gladiator's Silk Cowl
+							recipe(137921),	-- Crafted Dreadful Gladiator's Silk Handguards
+							recipe(137924),	-- Crafted Dreadful Gladiator's Silk Robe
+							recipe(137923),	-- Crafted Dreadful Gladiator's Silk Trousers
+							recipe(137913),	-- Crafted Dreadful Gladiator's Treads of Alacrity
+							recipe(137912),	-- Crafted Dreadful Gladiator's Treads of Cruelty
+							recipe(137914),	-- Crafted Dreadful Gladiator's Treads of Meditation
+						}),
+					},
+				}),
+			},
+		},
+		{
+			["categoryID"] = 942,  --Zandalari/Kul Tiran Patterns
+			["g"] = {
+				n(-37, {    -- Discovery
+					["g"] = {
+						{	-- Embroidered Deep Sea Gloves
+							["spellID"] = 257118,	-- Embroidered Deep Sea Gloves
+							["g"] = {
+								{	-- Pattern: Imbued Deep Sea Gloves
+									["itemID"] = 162507,	-- Pattern: Imbued Deep Sea Gloves
+								},
+								{	-- Imbued Deep Sea Gloves
+									["spellID"] = 257120,	-- Imbued Deep Sea Gloves
+									["g"] = {
+										{	-- Pattern: Emblazoned Deep Sea Gloves
+											["itemID"] = 162508,	-- Pattern: Emblazoned Deep Sea Gloves
+										},
+									},
+								},
+							},
+						},
+						{	-- Embroidered Deep Sea Breeches
+							["spellID"] = 257122,	-- Embroidered Deep Sea Breeches
+							["g"] = {
+								{	-- Pattern: Imbued Deep Sea Breeches
+									["itemID"] = 162509,	-- Pattern: Imbued Deep Sea Breeches
+								},
+								{	-- Imbued Deep Sea Breeches
+									["spellID"] = 257123,	-- Imbued Deep Sea Breeches
+									["g"] = {
+										{	-- Pattern: Emblazoned Deep Sea Breeches
+											["itemID"] = 162510,	-- Pattern: Emblazoned Deep Sea Breeches
+										},
+									},
+								},
+							},
+						},
+						{	-- Enhanced Deep Sea Gloves
+							["spellID"] = 285078,	-- Enhanced Deep Sea Gloves
+							["g"] = {
+								{	-- Pattern: Fortified Deep Sea Gloves
+									["itemID"] = 165652,	-- Pattern: Fortified Deep Sea Gloves
+								},
+								{	-- Fortified Deep Sea Gloves
+									["spellID"] = 285086,	-- Fortified Deep Sea Gloves
+									["g"] = {
+										{	-- Pattern: Tempered Deep Sea Gloves
+											["itemID"] = 165653,	-- Pattern: Tempered Deep Sea Gloves
+										},
+									},
+								},
+							},
+						},
+						{	-- Enhanced Deep Sea Breeches
+							["spellID"] = 285077,	-- Enhanced Deep Sea Breeches
+							["g"] = {
+								{	-- Pattern: Fortified Deep Sea Breeches
+									["itemID"] = 165655,	-- Pattern: Fortified Deep Sea Breeches
+								},
+								{	-- Fortified Deep Sea Breeches
+									["spellID"] = 285085,	-- Fortified Deep Sea Breeches
+									["g"] = {
+										{	-- Pattern: Tempered Deep Sea Breeches
+											["itemID"] = 165654,	-- Pattern: Tempered Deep Sea Breeches
+										},
+									},
+								},
+							},
+						},
+					},	
+				}),
+			},
+		},
+	}),
+});
