bcf82ca55ca9357ed27416391f6c45e112474115
Pr3vention <Pr3vention@gmail.com>
2019-05-18 12:57:59 -0500

    newhome vendors and flightpath

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/12 Nazjatar/Flight paths.lua
@@ -0,0 +1,22 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	{	-- Nazjatar
+		["mapID"] = 1355,	-- Nazjatar
+		["g"] = {
+			{	-- Flight Paths
+				["npcID"] = -228,	-- Flight Paths
+				["g"] = {
+					--[[ placeholders. These coords are correct, but need to extract flightPathIDs
+					{	-- Newhome, Nazjatar
+						["flightPathID"] = ,
+						["coord"] = { 47.5, 63.3, 1355 },
+					},
+					]]--
+				},
+			},
+		},
+	},
+};
---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/12 Nazjatar/Vendors.lua
@@ -0,0 +1,156 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(1355, {	-- Nazjatar
+		["groups"] = {
+			n(-2, {	-- Vendors
+				n(154002, {	-- Atolia Seapearl
+					["coord"] = { 51.0, 65.5, 1355 },
+					["races"] = HORDE_ONLY,
+					["g"] = {
+						currency(1721, {
+							i(168846),	-- Pearl of Lucid Dreams
+							i(168847),	-- Pearl of Manifest Ambitions
+							i(168848),	-- Pearl of Perspicuous Intentions
+							i(168849),	-- Pearl of Luminous Designs
+							i(168863),	-- Unbound Azerite Slivershards
+							i(168928),	-- Tablet of the Balancing Tides
+							i(168837),	-- Depth Forged Aegis
+							i(168864),	-- Sharpened Azerite Slivershards
+							i(168929),	-- Codex of the Never-Ending Tides
+							i(168838),	-- Enduring Bulwark of the Depths
+							i(168865),	-- Polazired Azerite Slivershards
+							i(168930),	-- Tome of the Quickening Tides
+							i(168839),	-- Regnerating Barrier of the Depths
+						}),
+						i(169365),	-- Damplight Slug
+						i(169274),	-- Tabard of the Unshackled
+						i(169606),	-- Formula: Accord of Mastery (Rank 3)
+						i(169549),	-- Recipe: Contract: Unshackled
+						i(169605),	-- Formula: Accord of Haste (Rank 3)
+						i(169607),	-- Formula: Accord of Versatility (Rank 3)
+						i(169601),	-- Recipe: Potion of Reconstitution (Rank 3)
+						i(169602),	-- Recipe: Potion of Focused Resolve (Rank 3)
+						i(169604),	-- Formula: Accord of Critical Strike (Rank 3)
+						i(169600),	-- Recipe: Potion of Empowered Proximity (Rank 3)
+						i(169603),	-- Recipe: Greater Mystical Cauldron (Rank 3)
+						i(170076),	-- Recipe: Unagi Skewer
+					},
+				}),
+				n(154652, {	-- Dazzerian
+					["coord"] = { 48.8, 60.6, 1355 },
+					["races"] = HORDE_ONLY,
+					["g"] = {
+						i(168802, {	-- Nazjatar Battle Commendation
+							i(169203),	-- Inkscale Deepseeker
+							i(169357),	-- Chitterspine Devourer
+							i(169276),	-- Horde War Banner
+							i(169277),	-- Horde War Standard
+							i(168850),	-- Time-Lost Battlefield Memento
+							i(168851),	-- Enduring Battlefield Memento
+							i(168852),	-- Stalwart Battlefield Memento
+						}),
+						i(137642, {	-- Mark of Honor
+							i(169509),	-- Recipe: Notorious Combatant's Plate Waistguard (Rank 2)
+							i(169510),	-- Recipe: Notorious Combatant's Plate Waistguard (Rank 3)
+							i(169511),	-- Recipe: Notorious Combatant's Plate Boots (Rank 2)
+							i(169512),	-- Recipe: Notorious Combatant's Plate Boots (Rank 3)
+							i(169505),	-- Recipe: Notorious Combatant's Plate Gauntlets (Rank 2)
+							i(169506),	-- Recipe: Notorious Combatant's Plate Gauntlets (Rank 3)
+							i(169513),	-- Recipe: Notorious Combatant's Plate Greaves (Rank 2)
+							i(169514),	-- Recipe: Notorious Combatant's Plate Greaves (Rank 3)
+							i(169522),	-- Recipe: Notorious Combatant's Shield (Rank 2)
+							i(169523),	-- Recipe: Notorious Combatant's Shield (Rank 3)
+							i(169507),	-- Recipe: Notorious Combatant's Plate Armguards (Rank 2)
+							i(169508),	-- Recipe: Notorious Combatant's Plate Armguards (Rank 3)
+							i(169519),	-- Recipe: Notorious Combatant's Cutlass (Rank 2)
+							i(169520),	-- Recipe: Notorious Combatant's Cutlass (Rank 3)
+							i(169526),	-- Recipe: Notorious Combatant's Deckpounder (Rank 2)
+							i(169528),	-- Recipe: Notorious Combatant's Deckpounder (Rank 3)
+							i(169515),	-- Recipe: Notorious Combatant's Spellblade (Rank 2)
+							i(169516),	-- Recipe: Notorious Combatant's Spellblade (Rank 3)
+							i(169517),	-- Recipe: Notorious Combatant's Shanker (Rank 2)
+							i(169518),	-- Recipe: Notorious Combatant's Shanker (Rank 3)
+							i(169524),	-- Recipe: Notorious Combatant's Polearm (Rank 2)
+							i(169525),	-- Recipe: Notorious Combatant's Polearm (Rank 3)
+							i(169539),	-- Recipe: Notorious Combatant's Sorcerous Scepter (Rank 2)
+							i(169540),	-- Recipe: Notorious Combatant's Sorcerous Scepter (Rank 3)
+							i(169541),	-- Recipe: Notorious Combatant's Discombobulator (Rank 2)
+							i(169542),	-- Recipe: Notorious Combatant's Discombobulator (Rank 3)
+							i(169543),	-- Recipe: Notorious Combatant's Stormsteel Destroyer (Rank 2)
+							i(169544),	-- Recipe: Notorious Combatant's Stormsteel Destroyer (Rank 3)
+							i(169545),	-- Recipe: Notorious Combatant's Etched Vessel (Rank 2)
+							i(169546),	-- Recipe: Notorious Combatant's Etched Vessel (Rank 3)
+							i(169551),	-- Recipe: Notorious Combatant's Intuitive Staff (Rank 2)
+							i(169552),	-- Recipe: Notorious Combatant's Intuitive Staff (Rank 3)
+							i(169553),	-- Recipe: Notorious Combatant's Leather Treads (Rank 2)
+							i(169554),	-- Recipe: Notorious Combatant's Leather Treads (Rank 3)
+							i(169555),	-- Recipe: Notorious Combatant's Leather Gauntlets (Rank 2)
+							i(169556),	-- Recipe: Notorious Combatant's Leather Gauntlets (Rank 3)
+							i(169561),	-- Recipe: Notorious Combatant's Leather Leggings (Rank 2)
+							i(169562),	-- Recipe: Notorious Combatant's Leather Leggings (Rank 3)
+							i(169559),	-- Recipe: Notorious Combatant's Leather Waistguard (Rank 2)
+							i(169560),	-- Recipe: Notorious Combatant's Leather Waistguard (Rank 3)
+							i(169557),	-- Recipe: Notorious Combatant's Leather Armguards (Rank 2)
+							i(169558),	-- Recipe: Notorious Combatant's Leather Armguards (Rank 3)
+							i(169569),	-- Recipe: Notorious Combatant's Mail Treads (Rank 2)
+							i(169570),	-- Recipe: Notorious Combatant's Mail Treads (Rank 3)
+							i(169563),	-- Recipe: Notorious Combatant's Mail Gauntlets (Rank 2)
+							i(169564),	-- Recipe: Notorious Combatant's Mail Gauntlets (Rank 3)
+							i(169571),	-- Recipe: Notorious Combatant's Mail Leggings (Rank 2)
+							i(169572),	-- Recipe: Notorious Combatant's Mail Leggings (Rank 3)
+							i(169567),	-- Recipe: Notorious Combatant's Mail Waistguard (Rank 2)
+							i(169568),	-- Recipe: Notorious Combatant's Mail Waistguard (Rank 3)
+							i(169565),	-- Recipe: Notorious Combatant's Mail Armguards (Rank 2)
+							i(169566),	-- Recipe: Notorious Combatant's Mail Armguards (Rank 3)
+							i(169573),	-- Recipe: Notorious Combatant's Bow (Rank 2)
+							i(169574),	-- Recipe: Notorious Combatant's Bow (Rank 3)
+							i(169586),	-- Recipe: Notorious Combatant's Satin Cloak (Rank 2)
+							i(169576),	-- Recipe: Notorious Combatant's Satin Belt (Rank 2)
+							i(169577),	-- Recipe: Notorious Combatant's Satin Belt (Rank 3)
+							i(169587),	-- Recipe: Notorious Combatant's Satin Cloak (Rank 3)
+							i(169578),	-- Recipe: Notorious Combatant's Satin Boots (Rank 2)
+							i(169579),	-- Recipe: Notorious Combatant's Satin Boots (Rank 3)
+							i(169580),	-- Recipe: Notorious Combatant's Satin Mittens (Rank 2)
+							i(169581),	-- Recipe: Notorious Combatant's Satin Mittens (Rank 3)
+							i(169584),	-- Recipe: Notorious Combatant's Satin Pants (Rank 2)
+							i(169585),	-- Recipe: Notorious Combatant's Satin Pants (Rank 3)
+							i(169582),	-- Recipe: Notorious Combatant's Satin Bracers (Rank 2)
+							i(169583),	-- Recipe: Notorious Combatant's Satin Bracers (Rank 3)
+						}),
+					},
+				}),
+				n(153512, {	-- Finder Pruc
+					["coord"] = { 49.1, 62.2, 1355 },
+					["races"] = HORDE_ONLY,
+					["g"] = {
+						i(169352),	-- Pearlescent Glimmershell
+						i(167170),	-- Unshackled Waveray
+					},
+				}),
+				n(153024, {	-- Suva
+					["coord"] = { 50.2, 63.9, 1355 },
+					["races"] = HORDE_ONLY,
+					["g"] = {
+						i(168307),	-- Necklace of Most Beautiful Shells
+						i(138308),	-- Most Magnificient Small Shell of Shimmering
+						i(168824),	-- Ocean Simulator
+						i(168309),	-- Armwrap of Ocean Magic
+						i(168320),	-- Shiniest Roundiest Pearl of Goodness
+					},
+				}),
+				n(145837, {	-- Violet Shadowmend
+					["coord"] = { 48.6, 60.7, 1355 },
+					["races"] = HORDE_ONLY,
+					["g"] = {
+						i(139776),	-- Horde Fanatic
+						i(142380),	-- Dutiful Gruntling
+						i(163974),	-- Bucketshell
+					},
+				}),
+			}),
+		},
+	}),
+};
