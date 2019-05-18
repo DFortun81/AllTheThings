12257eabd74b7997697f3dc9891e0cfa28f48660
Lucetia <>
2019-05-18 03:38:44 -0700

    - Townlong got neutered

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Townlong Steppes/Rares.lua
@@ -0,0 +1,227 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	{	-- Pandaria
+		["mapID"] = 424,	-- Pandaria
+		["g"] = {
+			{	-- Townlong Steppes
+				["mapID"] = 388,	-- Townlong Steppes
+				["g"] = {
+					n(-16, { 	-- Rares
+						n(50772, {	-- Eshelon
+							dr(25, i(87624)),	-- Yaungol Mist-Shaman's Amulet
+							dr(19, {
+								i(87625),	-- Congealed Mist Amulet
+								i(87622),	-- Yoke of Niuzao
+							}),
+							dr(18, {
+								i(87626),	-- Suna's Shattered Locket
+								i(87217),	-- Small Bag of Goods
+							}),
+							dr(17, i(87623)),	-- Razor-Sharp Chitin Choker
+							dr(10, i(87222)),	-- Big Bag of Linens
+						}),
+						n(66467, {	-- G'nathus
+							dr(92, i(94595)),	-- Spawn of G'nathus
+						}),
+						n(66900, {	-- Huggalon the Heart Watcher
+							dr(100, i(90067)),	-- B. F. F. Necklace
+						}),
+						n(50355, {	-- Kah'tir
+							dr(28, {
+								i(87626),	-- Suna's Shattered Locket
+								i(87217),	-- Small Bag of Goods
+							}),
+							dr(18, i(87624)),	-- Yaungol Mist-Shaman's Amulet
+							dr(17, {
+								i(87625),	-- Congealed Mist Amulet
+								i(87623),	-- Razor-Sharp Chitin Choker
+								i(87622),	-- Yoke of Niuzao
+							}),
+							dr(12, i(87218, {	-- Big Bag of Arms
+								dr(21, i(90725)),	-- Gaarn's Leggings of Infestation
+								dr(12, {
+									i(87646),	-- Needlefang Throatripper
+									i(87652),	-- Ook-Breaker Mace
+									i(87651),	-- Pathwalker Greatstaff
+									i(87649),	-- Pool-Stirrer
+									i(90717),	-- Qu'nas' Apocryphal Legplates
+								}),
+								dr(11, {
+									i(87642),	-- Darkstaff of Annihilation
+									i(87650),	-- Fishsticker Crossbow
+									i(90719),	-- Go-Kan's Golden Trousers
+									i(87641),	-- Yaungol Battle Barrier
+								}),
+								dr(10, {
+									i(90721),	-- Cournith Waterstrider's Silken Finery
+									i(87643),	-- Fangcracker Battlemace
+									i(90722),	-- Torik-Ethis' Gilded Legplates
+								}),
+								dr(9, {
+									i(90723),	-- Arness's Scaled Leggings
+									i(90720),	-- Silent Leggings of the Ghostpaw
+									i(90724),	-- Spriggin's Sproggin' Leggin'
+								}),
+								dr(8, i(90718)),	-- Torik-Ethis' Bloodied Legguards
+							})),
+						}),
+						n(50734, {	-- Lith'ik the Stalker
+							dr(22, {
+								i(87625),	-- Congealed Mist Amulet
+								i(87217),	-- Small Bag of Goods
+							}),
+							dr(20, i(87623)),	-- Razor-Sharp Chitin Choker
+							dr(19, {
+								i(87626),	-- Suna's Shattered Locket
+								i(87624),	-- Yaungol Mist-Shaman's Amulet
+								i(87622),	-- Yoke of Niuzao
+							}),
+							dr(10, i(87221)),	-- Big Bag of Jewels
+						}),
+						n(50333, {	-- Lon the Bull
+							dr(21, i(87623)),	-- Razor-Sharp Chitin Choker
+							dr(20, {
+								i(87626),	-- Suna's Shattered Locket
+								i(87624),	-- Yaungol Mist-Shaman's Amulet
+							}),
+							dr(19, {
+								i(87625),	-- Congealed Mist Amulet
+								i(87622),	-- Yoke of Niuzao
+							}),
+							dr(18, i(87217)),	-- Small Bag of Goods
+							dr(9, i(87219)),	-- Huge Bag of Herbs
+						}),
+						n(50344, {	-- Norlaxx
+							dr(22, i(87622)),	-- Yoke of Niuzao
+							dr(20, {
+								i(87626),	-- Suna's Shattered Locket
+								i(87217),	-- Small Bag of Goods
+							}),
+							dr(19, {
+								i(87625),	-- Congealed Mist Amulet
+								i(87623),	-- Razor-Sharp Chitin Choker
+								i(87624),	-- Yaungol Mist-Shaman's Amulet
+							}),
+							dr(9, i(87220, {	-- Big Bag of Mysteries
+								dr(3, {
+									i(90721),	-- Cournith Waterstrider's Silken Finery
+									i(87646),	-- Needlefang Throatripper
+								}),
+								dr(2, {
+									i(87649),	-- Pool-Stirrer
+									i(87651),	-- Pathwalker Greatstaff
+									i(90717),	-- Qu'nas' Apocryphal Legplates
+									i(87652),	-- Ook-Breaker Mace
+									i(90718),	-- Torik-Ethis' Bloodied Legguards
+									i(90723),	-- Arness's Scaled Leggings
+									i(87642),	-- Darkstaff of Annihilation
+									i(87650),	-- Fishsticker Crossbow
+									i(90719),	-- Go-Kan's Golden Trousers
+									i(90722),	-- Torik-Ethis' Gilded Legplates
+								}),
+								dr(1, {
+									i(87641),	-- Yaungol Battle Barrier
+									i(90720),	-- Silent Leggings of the Ghostpaw
+									i(87643),	-- Fangcracker Battlemace
+									i(90725),	-- Gaarn's Leggings of Infestation
+									i(90724),	-- Spriggin's Sproggin' Leggin'
+								}),
+							})),
+
+
+						}),
+						n(66938, {	-- Odd'nirok
+							i(90171, {	-- Odd'nirok's Clamshell
+								i(90172),	-- Clamshell Band
+								i(90087),	-- Lobstmourne
+							}),
+						}),
+						n(50791, {	-- Siltriss the Sharpener
+							dr(27, i(87623)),	-- Razor-Sharp Chitin Choker
+							dr(19, i(87622)),	-- Yoke of Niuzao
+							dr(18, {
+								i(87217),	-- Small Bag of Goods
+								i(87624),	-- Yaungol Mist-Shaman's Amulet
+							}),
+							dr(17, i(87625)),	-- Congealed Mist Amulet
+							dr(16, i(87626)),	-- Suna's Shattered Locket
+							dr(8, i(87223)),	-- Big Bag of Skins
+						}),
+						n(50832, {  -- The Yowler
+							dr(20, {
+								i(87625),	-- Congealed Mist Amulet
+								i(87623),	-- Razor-Sharp Chitin Choker
+								i(87217),	-- Small Bag of Goods
+							}),
+							dr(19, {
+								i(87626),	-- Suna's Shattered Locket
+								i(87624),	-- Yaungol Mist-Shaman's Amulet
+								i(87622),	-- Yoke of Niuzao
+							}),
+							dr(10, i(87225)),	-- Big Bag of Food
+						}),
+						n(50820, {	-- Yul Wildpaw
+							dr(20, {
+								i(87623),	-- Razor-Sharp Chitin Choker
+								i(87217),	-- Small Bag of Goods
+								i(87626),	-- Suna's Shattered Locket
+								i(87624),	-- Yaungol Mist-Shaman's Amulet
+								i(87622),	-- Yoke of Niuzao
+							}),
+							dr(19, i(87625)),	-- Congealed Mist Amulet
+							dr(9, i(87224)),	-- Big Bag of Wonders
+						}),
+						{	-- Zandalari Warbringer
+							["npcID"] = 69841,	-- Zandalari Warbringer
+							["coords"] = {
+								{ 47.42, 61.54, 422 },	-- Dread Wastes
+								{ 39.83, 65.92, 418 },	-- Krasarang Wilds
+								{ 75.11, 67.47, 379 },	-- Kun-Lai Summit
+								{ 52.56, 18.85, 371 },	-- The Jade Forest
+								{ 36.58, 85.67, 388 },	-- Townlong Steppes
+							},
+							["g"] = {
+								{	-- Reins of the Amber Primordial Direhorn
+									["itemID"] = 94230,	-- Reins of the Amber Primordial Direhorn
+								},
+							},
+						},
+						{	-- Zandalari Warbringer
+							["npcID"] = 69842,	-- Zandalari Warbringer
+							["coords"] = {
+								{ 47.42, 61.54, 422 },	-- Dread Wastes
+								{ 39.83, 65.92, 418 },	-- Krasarang Wilds
+								{ 75.11, 67.47, 379 },	-- Kun-Lai Summit
+								{ 52.56, 18.85, 371 },	-- The Jade Forest
+								{ 36.58, 85.67, 388 },	-- Townlong Steppes
+							},
+							["g"] = {
+								{	-- Reins of the Jade Primordial Direhorn
+									["itemID"] = 94231,	-- Reins of the Jade Primordial Direhorn
+								},
+							},
+						},
+						{	-- Zandalari Warbringer
+							["npcID"] = 69769,	-- Zandalari Warbringer
+							["coords"] = {
+								{ 47.42, 61.54, 422 },	-- Dread Wastes
+								{ 39.83, 65.92, 418 },	-- Krasarang Wilds
+								{ 75.11, 67.47, 379 },	-- Kun-Lai Summit
+								{ 52.56, 18.85, 371 },	-- The Jade Forest
+								{ 36.58, 85.67, 388 },	-- Townlong Steppes
+							},
+							["g"] = {
+								{	-- Reins of the Slate Primordial Direhorn
+									["itemID"] = 94229,	-- Reins of the Slate Primordial Direhorn
+								},
+							},
+						},
+					}),
+				},
+			},
+		},
+	},
+};
