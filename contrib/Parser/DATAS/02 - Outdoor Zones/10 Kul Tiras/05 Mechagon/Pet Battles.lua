f8e644c46403bc9c0b28be4fd4b85a0cd39bc328
Pr3vention <Pr3vention@gmail.com>
2019-05-19 08:36:21 -0500

    Added all Mechagon capturable battle pets with meta achievement

--- contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/05 Mechagon/Pet Battles.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/05 Mechagon/Pet Battles.lua
@@ -9,6 +9,91 @@ _.Zones =
 				n(-25, {	-- Pet Battle
 					["f"] = 101,		-- Battle Pets
 					["g"] = {
+						{	-- Mighty Minions of Mechagon
+							["achievementID"] = 13625,
+							["g"] = {
+								{	-- Gnomefeaster
+									["criteriaID"] = 1,
+									["crs"] = { 154922 },	-- Gnomefeaster
+								},
+								{	-- Sputtertube
+									["criteriaID"] = 2,
+									["crs"] = { 154923 },	-- Sputtertube
+								},
+								{	-- Goldenbot XD
+									["criteriaID"] = 3,
+									["crs"] = { 154924 },	-- Goldenbot XD
+								},
+								{	-- Creakclank
+									["criteriaID"] = 4,
+									["crs"] = { 154925 },	-- Creakclank
+								},
+								{	-- CK-9 Micro-Oppression Unit
+									["criteriaID"] = 5,
+									["crs"] = { 154926 },	-- CK-9 Micro-Oppression Unit
+								},
+								{	-- Unit 35
+									["criteriaID"] = 6,
+									["crs"] = { 154927 },	-- Unit 35
+								},
+								{	-- Unit 6
+									["criteriaID"] = 7,
+									["crs"] = { 154928 },	-- Unit 6
+								},
+								{	-- Unit 17
+									["criteriaID"] = 8,
+									["crs"] = { 154929 },	-- Unit 17
+								},
+							},
+						},
+						{	-- Duskytooth Snooter
+							["speciesID"] = 2662,	-- Duskytooth Snooter
+							["crs"] = { 154769 },	-- Duskytooth Snoother
+						},
+						{	-- Experimental Roach
+							["speciesID"] = 2664,	-- Experimental Roach
+							["crs"] = { 154773 },	-- Experimental Roach
+						},
+						{	-- Fleeting Frog
+							["speciesID"] = 2665,	-- Fleeting Frog
+							["crs"] = { 154775 },	-- Fleeting Frog
+						},
+						{	-- Junkheap Roach
+							["speciesID"] = 2663,	-- Junkheap Roach
+							["crs"] = { 154771 },	-- Junkheap Roach
+						},
+						{	-- Malfunctioning Microbot
+							["speciesID"] = 2676,	-- Malfunctioning Microbot
+							["crs"] = { 154798 },	-- Malfunctioning Microbot
+						},
+						{	-- Mechagon Marmot
+							["speciesID"] = 2670,	-- Mechagon Marmot
+							["crs"] = { 154785 },	-- Mechagon Marmot
+						},
+						{	-- Motorized Croaker
+							["speciesID"] = 2667,	-- Motorized Croaker
+							["crs"] = { 154779 },	-- Motorized Croaker
+						},
+						{	-- Rustbolt Clucker
+							["speciesID"] = 2669,	-- Rustbolt Clucker
+							["crs"] = { 154783 },	-- Rustbolt Clucker
+						},
+						{	-- Rustyroot Snooter
+							["speciesID"] = 2661,	-- Rustyroot Snooter
+							["crs"] = { 154767 },	-- Rustyroot Snooter
+						},
+						{	-- Scrapyard Tunneler
+							["speciesID"] = 2673,	-- Scrapyard Tunneler
+							["crs"] = { 154791 },	-- Scrapyard Tunneler
+						},
+						{	-- Specimen 97
+							["speciesID"] = 2671,	-- Specimen 97
+							["crs"] = { 154787 },	-- Specimen 97
+						},
+						{	-- Yellow Junkhopper
+							["speciesID"] = 2666,	-- Yellow Junkhopper
+							["crs"] = { 154777 },	-- Yellow Junkhopper
+						},
 					},
 				}),
 			},
