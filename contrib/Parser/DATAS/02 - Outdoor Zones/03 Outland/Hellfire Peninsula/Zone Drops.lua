15fa0b0ba5502328ec734b8baecc4345233445ca
Lucetia <>
2019-05-23 16:38:12 -0700

    - Hellfire Peninsula Zone Drops added

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/03 Outland/Hellfire Peninsula/Zone Drops.lua
@@ -0,0 +1,171 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	{	-- Outland
+		["mapID"] = 101,	-- Outland
+		["g"] = {
+			{	-- Hellfire Peninsula
+				["mapID"] = 100,	-- Hellfire Peninsula
+				["g"] = {
+					{	-- Zone Drops
+						["npcID"] = 0,	-- Zone Drops
+						["g"] = {
+							{	-- Absorption Dagger
+								["itemID"] = 25296,	-- Absorption Dagger
+							},
+							{	-- Anglesite Choker
+								["itemID"] = 25058,	-- Anglesite Choker
+							},
+							{	-- Ango'rosh Sabatons
+								["itemID"] = 24927,	-- Ango'rosh Sabatons
+							},
+							{	-- Arelion's Journal
+								["itemID"] = 23339,	-- Arelion's Journal
+							},
+							{	-- Azurite Ring
+								["itemID"] = 25045,	-- Azurite Ring
+							},
+							{	-- Combustion Dagger
+								["itemID"] = 25298,	-- Combustion Dagger
+							},
+							{	-- Crystallized Ebony Wand
+								["itemID"] = 25283,	-- Crystallized Ebony Wand
+							},
+							{	-- Darkcrest Breastplate
+								["itemID"] = 24936,	-- Darkcrest Breastplate
+							},
+							{	-- Darkcrest Gauntlets
+								["itemID"] = 24937,	-- Darkcrest Gauntlets
+							},
+							{	-- Darkcrest Legguards
+								["itemID"] = 24939,	-- Darkcrest Legguards
+							},
+							{	-- Dementia Boots
+								["itemID"] = 24726,	-- Dementia Boots
+							},
+							{	-- Dementia Trousers
+								["itemID"] = 24730,	-- Dementia Trousers
+							},
+							{	-- Dreamseeker Dandelion
+								["itemID"] = 25086,	-- Dreamseeker Dandelion
+							},
+							{	-- Dreghood Cowl
+								["itemID"] = 24721,	-- Dreghood Cowl
+							},
+							{	-- Felblood Sample
+								["itemID"] = 23269,	-- Felblood Sample
+							},
+							{	-- Felstone Bindings
+								["itemID"] = 24821,	-- Felstone Bindings
+							},
+							{	-- Felstone Gauntlets
+								["itemID"] = 24817,	-- Felstone Gauntlets
+							},
+							{	-- Felstone Greaves
+								["itemID"] = 24815,	-- Felstone Greaves
+							},
+							{	-- Felstone Helm
+								["itemID"] = 24818,	-- Felstone Helm
+							},
+							{	-- Felstone Waistband
+								["itemID"] = 24814,	-- Felstone Waistband
+							},
+							{	-- Fireheart Bracers
+								["itemID"] = 24596,	-- Fireheart Bracers
+							},
+							{	-- Fireheart Chestpiece
+								["itemID"] = 24591,	-- Fireheart Chestpiece
+							},
+							{	-- Glorious Scepter
+								["itemID"] = 25312,	-- Glorious Scepter
+							},
+							{	-- Grimscale Belt
+								["itemID"] = 24918,	-- Grimscale Belt
+							},
+							{	-- Khan'aish Gloves
+								["itemID"] = 24961,	-- Khan'aish Gloves
+							},
+							{	-- Laughing Skull Gloves
+								["itemID"] = 24608,	-- Laughing Skull Gloves
+							},
+							{	-- Laughing Skull Pants
+								["itemID"] = 24610,	-- Laughing Skull Pants
+							},
+							{	-- Mahogany Wand
+								["itemID"] = 25282,	-- Mahogany Wand
+							},
+							{	-- Netherstalker Bracer
+								["itemID"] = 24829,	-- Netherstalker Bracer
+							},
+							{	-- Netherstalker Mantle
+								["itemID"] = 24828,	-- Netherstalker Mantle
+							},
+							{	-- Netherweave Cloth
+								["itemID"] = 21877,	-- Netherweave Cloth
+							},
+							{	-- Nexus-Strider Bracer
+								["itemID"] = 24837,	-- Nexus-Strider Bracer
+							},
+							{	-- Nexus-Strider Breastplate
+								["itemID"] = 24832,	-- Nexus-Strider Breastplate
+							},
+							{	-- Nexus-Strider Greaves
+								["itemID"] = 24831,	-- Nexus-Strider Greaves
+							},
+							{	-- Nexus-Strider Mantle
+								["itemID"] = 24836,	-- Nexus-Strider Mantle
+							},
+							{	-- Outlander's Bracers
+								["itemID"] = 24588,	-- Outlander's Bracers
+							},
+							{	-- Outlander's Leggings
+								["itemID"] = 24586,	-- Outlander's Leggings
+							},
+							{	-- Purpleheart Wand
+								["itemID"] = 25284,	-- Purpleheart Wand
+							},
+							{	-- Rubellite Ring
+								["itemID"] = 25044,	-- Rubellite Ring
+							},
+							{	-- Shroud of Frenzy
+								["itemID"] = 31143,	-- Shroud of Frenzy
+							},
+							{	-- Siphoning Dagger
+								["itemID"] = 25299,	-- Siphoning Dagger
+							},
+							{	-- Smoked Talbuk Venison
+								["itemID"] = 27854,	-- Smoked Talbuk Venison
+							},
+							{	-- Starfire Gloves
+								["itemID"] = 24600,	-- Starfire Gloves
+							},
+							{	-- Sunstone Necklace
+								["itemID"] = 25060,	-- Sunstone Necklace
+							},
+							{	-- Supplicant's Rod
+								["itemID"] = 25089,	-- Supplicant's Rod
+							},
+							{	-- Tourmaline Loop
+								["itemID"] = 25047,	-- Tourmaline Loop
+							},
+							{	-- Unyielding Chain Vest
+								["itemID"] = 24808,	-- Unyielding Chain Vest
+							},
+							{	-- Unyielding Leggings
+								["itemID"] = 24811,	-- Unyielding Leggings
+							},
+							{	-- Vengeance Bands
+								["itemID"] = 24716,	-- Vengeance Bands
+							},
+							{	-- Vengeance Legguards
+								["itemID"] = 24714,	-- Vengeance Legguards
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+};
