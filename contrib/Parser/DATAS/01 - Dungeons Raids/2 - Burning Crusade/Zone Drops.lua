aca5670edc73598c83afa18545417be98c59b5db
Lucetia <>
2019-05-23 23:19:04 -0700

    - More D&R Zone drops added for BC

--- contrib/Parser/DATAS/01 - Dungeons Raids/2 - Burning Crusade/Zone Drops.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/2 - Burning Crusade/Zone Drops.lua
@@ -1158,7 +1158,544 @@ _.Instances =
 							17963,	-- Wastewalker Slave
 						},
 					},
-					
+					{	-- Design: Brutal Earthstorm Diamond
+						["itemID"] = 25906,	-- Design: Brutal Earthstorm Diamond
+						["crs"] = {
+							17898,	-- Abomination
+							22175,	-- Apexis Flayer
+							17905,	-- Banshee
+							20728,	-- Bladespire Raptor
+							18419,	-- Bloodwarder Greenkeeper
+							23235,	-- Bonechewer Blade Fury
+							17958,	-- Coilfang Defender
+							21339,	-- Coilfang Hate-Screamer
+							17940,	-- Coilfang Technician
+							18934,	-- Durnholde Mage
+							18311,	-- Ethereal Crypt Raider
+							18331,	-- Ethereal Darkcaster
+							18317,	-- Ethereal Priest
+							18309,	-- Ethereal Scavenger
+							18312,	-- Ethereal Spellbinder
+							16481,	-- Ghastly Haunt
+							17817,	-- Greater Bogstrok
+							21230,	-- Greyheart Nether-Mage
+							23337,	-- Illidari Centurion
+							19389,	-- Lair Brute
+							17160,	-- Living Cyclone
+							18314,	-- Nexus Stalker
+							19307,	-- Nexus Terror
+							20865,	-- Protean Horror
+							21104,	-- Rift Keeper
+							18323,	-- Sethekk Guard
+							17695,	-- Shattered Hand Assassin
+							16415,	-- Skeletal Waiter
+							21649,	-- Skettis Windwalker
+							16468,	-- Spectral Patron
+							15551,	-- Spectral Stable Hand
+							25367,	-- Sunblade Arch Mage
+							24683,	-- Sunblade Mage Guard
+							24685,	-- Sunblade Magister
+							24686,	-- Sunblade Warlock
+							19486,	-- Sunseeker Chemist
+							21651,	-- Time-Lost Skettis Reaver
+						},
+					},
+					{	-- Design: Chain of the Twilight Owl
+						["itemID"] = 24171,	-- Design: Chain of the Twilight Owl
+						["crs"] = {
+							17898,	-- Abomination
+							22878,	-- Aqueous Lord
+							22883,	-- Aqueous Spawn
+							18493,	-- Auchenai Soulpriest
+							21891,	-- Avian Ripper
+							21904,	-- Avian Warhawk
+							20990,	-- Bloodwarder Physician
+							17993,	-- Bloodwarder Protector
+							17816,	-- Bogstrok
+							18641,	-- Cabal Familiar
+							18830,	-- Cabal Fanatic
+							18638,	-- Cabal Zealot
+							17962,	-- Coilfang Collaborator
+							17721,	-- Coilfang Engineer
+							17800,	-- Coilfang Myrmidon
+							21220,	-- Coilfang Priestess
+							22876,	-- Coilskar Soothsayer
+							20041,	-- Crystalcore Sentinel
+							18467,	-- Dreadfang Widow
+							18934,	-- Durnholde Mage
+							17819,	-- Durnholde Sentry
+							22016,	-- Eclipsion Soldier
+							20879,	-- Eredar Soul-Eater
+							18331,	-- Ethereal Darkcaster
+							18317,	-- Ethereal Priest
+							18312,	-- Ethereal Spellbinder
+							16545,	-- Ethereal Spellfilcher
+							17400,	-- Felguard Annihilator
+							20340,	-- Fleshfiend
+							17895,	-- Ghoul
+							19557,	-- Greater Frayer
+							22869,	-- Illidari Boneslicer
+							23337,	-- Illidari Centurion
+							16491,	-- Mana Feeder
+							16530,	-- Mana Warp
+							17154,	-- Muck Spawn
+							16425,	-- Phantom Guardsman
+							16409,	-- Phantom Guest
+							17669,	-- Rabid Warhound
+							18323,	-- Sethekk Guard
+							18322,	-- Sethekk Ravenguard
+							16540,	-- Shadow Pillager
+							22882,	-- Shadowmoon Deathshaper
+							17427,	-- Shattered Hand Archer
+							17671,	-- Shattered Hand Champion
+							17464,	-- Shattered Hand Gladiator
+							22964,	-- Sister of Pleasure
+							16468,	-- Spectral Patron
+							15548,	-- Spectral Stallion
+							16525,	-- Spell Shade
+							24684,	-- Sunblade Blood Knight
+							25371,	-- Sunblade Dawn Priest
+							25370,	-- Sunblade Dusk Priest
+							25369,	-- Sunblade Vindicator
+							24686,	-- Sunblade Warlock
+							21695,	-- Tidal Surger
+							24689,	-- Wretched Bruiser
+						},
+					},
+					{	-- Design: Deadly Noble Topaz
+						["itemID"] = 31879,	-- Design: Deadly Noble Topaz
+						["crs"] = {
+							17898,	-- Abomination
+							20908,	-- Akkiris Lightning-Waker
+							18524,	-- Angered Skeleton
+							20043,	-- Apprentice Star Scryer
+							22883,	-- Aqueous Spawn
+							20869,	-- Arcatraz Sentinel
+							22844,	-- Ashtongue Battlelord
+							18493,	-- Auchenai Soulpriest
+							18495,	-- Auchenai Vindicator
+							19429,	-- Avian Darkhawk
+							21891,	-- Avian Ripper
+							22242,	-- Bash'ir Spell-Thief
+							18419,	-- Bloodwarder Greenkeeper
+							20031,	-- Bloodwarder Legionnaire
+							19633,	-- Bloodwarder Mender
+							17993,	-- Bloodwarder Protector
+							20036,	-- Bloodwarder Squire
+							23196,	-- Bonechewer Behemoth
+							23235,	-- Bonechewer Blade Fury
+							23222,	-- Bonechewer Brawler
+							17271,	-- Bonechewer Destroyer
+							17259,	-- Bonechewer Hungerer
+							23236,	-- Bonechewer Shield Disciple
+							17135,	-- Boulderfist Mystic
+							17136,	-- Boulderfist Warrior
+							18633,	-- Cabal Acolyte
+							18635,	-- Cabal Deathsworn
+							18632,	-- Cabal Executioner
+							18794,	-- Cabal Ritualist
+							18639,	-- Cabal Spellbinder
+							22955,	-- Charming Courtesan
+							18205,	-- Clefthoof
+							19428,	-- Cobalt Serpent
+							17957,	-- Coilfang Champion
+							17962,	-- Coilfang Collaborator
+							17721,	-- Coilfang Engineer
+							17803,	-- Coilfang Oracle
+							21220,	-- Coilfang Priestess
+							21301,	-- Coilfang Shatterer
+							17940,	-- Coilfang Technician
+							17802,	-- Coilfang Warrior
+							22875,	-- Coilskar Sea-Caller
+							22876,	-- Coilskar Soothsayer
+							24696,	-- Coilskar Witch
+							22877,	-- Coilskar Wrangler
+							16170,	-- Coldmist Stalker
+							16171,	-- Coldmist Widow
+							20047,	-- Crimson Hand Battle Mage
+							20049,	-- Crimson Hand Blood Knight
+							20048,	-- Crimson Hand Centurion
+							20050,	-- Crimson Hand Inquisitor
+							22960,	-- Dragonmaw Wyrmcaller
+							17820,	-- Durnholde Rifleman
+							17819,	-- Durnholde Sentry
+							17860,	-- Durnholde Veteran
+							17833,	-- Durnholde Warden
+							21059,	-- Enraged Water Spirit
+							18311,	-- Ethereal Crypt Raider
+							18331,	-- Ethereal Darkcaster
+							18317,	-- Ethereal Priest
+							18309,	-- Ethereal Scavenger
+							18313,	-- Ethereal Sorcerer
+							18312,	-- Ethereal Spellbinder
+							18315,	-- Ethereal Theurgist
+							16544,	-- Ethereal Thief
+							20896,	-- Ethereum Slayer
+							24698,	-- Ethereum Smuggler
+							21709,	-- Eykenen
+							17895,	-- Ghoul
+							21232,	-- Greyheart Skulker
+							23172,	-- Hand of Gorefiend
+							23403,	-- Illidari Assassin
+							23402,	-- Illidari Battle-mage
+							23397,	-- Illidari Blood Lord
+							22869,	-- Illidari Boneslicer
+							23337,	-- Illidari Centurion
+							19389,	-- Lair Brute
+							17370,	-- Laughing Skull Enforcer
+							16491,	-- Mana Feeder
+							21839,	-- Mature Silkwing
+							19712,	-- Mechanar Driller
+							17728,	-- Murkblood Tribesman
+							23232,	-- Mutant War Hound
+							23169,	-- Nethermine Flayer
+							19512,	-- Nethervine Reaper
+							18314,	-- Nexus Stalker
+							19307,	-- Nexus Terror
+							16406,	-- Phantom Attendant
+							16409,	-- Phantom Guest
+							20038,	-- Phoenix-Hawk Hatchling
+							25509,	-- Priestess of Torment
+							23394,	-- Promenade Sentinel
+							18323,	-- Sethekk Guard
+							18318,	-- Sethekk Initiate
+							18328,	-- Sethekk Oracle
+							18325,	-- Sethekk Prophet
+							18322,	-- Sethekk Ravenguard
+							21302,	-- Shadow Council Warlock
+							16176,	-- Shadowbeast
+							16594,	-- Shadowmoon Acolyte
+							22880,	-- Shadowmoon Champion
+							22882,	-- Shadowmoon Deathshaper
+							23018,	-- Shadowmoon Houndmaster
+							17395,	-- Shadowmoon Summoner
+							25486,	-- Shadowsword Vanquisher
+							17899,	-- Shadowy Necromancer
+							17671,	-- Shattered Hand Champion
+							17464,	-- Shattered Hand Gladiator
+							17420,	-- Shattered Hand Heathen
+							17670,	-- Shattered Hand Houndmaster
+							16523,	-- Shattered Hand Savage
+							16507,	-- Shattered Hand Sentry
+							16471,	-- Skeletal Usher
+							21911,	-- Skettis Soulcaller
+							15547,	-- Spectral Charger
+							16468,	-- Spectral Patron
+							16407,	-- Spectral Servant
+							15551,	-- Spectral Stable Hand
+							15548,	-- Spectral Stallion
+							16525,	-- Spell Shade
+							24684,	-- Sunblade Blood Knight
+							25363,	-- Sunblade Cabalist
+							25371,	-- Sunblade Dawn Priest
+							25370,	-- Sunblade Dusk Priest
+							24683,	-- Sunblade Mage Guard
+							24685,	-- Sunblade Magister
+							24687,	-- Sunblade Physician
+							25507,	-- Sunblade Protector
+							24777,	-- Sunblade Sentinel
+							25369,	-- Sunblade Vindicator
+							24686,	-- Sunblade Warlock
+							18422,	-- Sunseeker Botanist
+							19505,	-- Sunseeker Channeler
+							20988,	-- Sunseeker Engineer
+							19507,	-- Sunseeker Gene-Splicer
+							16492,	-- Syphoner
+							18094,	-- Tarren Mill Lookout
+							19166,	-- Tempest-Forge Patroller
+							18405,	-- Tempest-Forge Peacekeeper
+							22939,	-- Temple Concubine
+							21695,	-- Tidal Surger
+							21225,	-- Tidewalker Warrior
+							18327,	-- Time-Lost Controller
+							18319,	-- Time-Lost Scryer
+							21651,	-- Time-Lost Skettis Reaver
+							17156,	-- Tortured Earth Spirit
+							23026,	-- Twilight Serpent
+							17724,	-- Underbat
+							19983,	-- Vekh'nir Stormcaller
+							16459,	-- Wanton Hostess
+							18065,	-- Warmaul Brute
+							17138,	-- Warmaul Reaver
+							24688,	-- Wretched Skulker
+							19642,	-- Zaxxis Stalker
+						},
+					},
+					{	-- Design: Delicate Eternium Ring
+						["itemID"] = 24164,	-- Design: Delicate Eternium Ring
+						["crs"] = {
+							22883,	-- Aqueous Spawn
+							22844,	-- Ashtongue Battlelord
+							21891,	-- Avian Ripper
+							20058,	-- Bloodmaul Dire Wolf
+							18419,	-- Bloodwarder Greenkeeper
+							20990,	-- Bloodwarder Physician
+							17993,	-- Bloodwarder Protector
+							20032,	-- Bloodwarder Vindicator
+							22963,	-- Bonechewer Worker
+							18205,	-- Clefthoof
+							17962,	-- Coilfang Collaborator
+							21299,	-- Coilfang Fathom-Witch
+							21298,	-- Coilfang Serpentguard
+							17722,	-- Coilfang Sorceress
+							20047,	-- Crimson Hand Battle Mage
+							20049,	-- Crimson Hand Blood Knight
+							20048,	-- Crimson Hand Centurion
+							20052,	-- Crystalcore Mechanic
+							17819,	-- Durnholde Sentry
+							20880,	-- Eredar Deathbringer
+							18309,	-- Ethereal Scavenger
+							18312,	-- Ethereal Spellbinder
+							21408,	-- Felfire Diemetradon
+							16481,	-- Ghastly Haunt
+							16470,	-- Ghostly Philanthropist
+							17895,	-- Ghoul
+							23172,	-- Hand of Gorefiend
+							18172,	-- Infinite Saboteur
+							16530,	-- Mana Warp
+							16425,	-- Phantom Guardsman
+							16409,	-- Phantom Guest
+							20038,	-- Phoenix-Hawk Hatchling
+							22962,	-- Priestess of Delight
+							22957,	-- Priestess of Dementia
+							18326,	-- Sethekk Shaman
+							17397,	-- Shadowmoon Adept
+							22945,	-- Shadowmoon Blood Mage
+							17694,	-- Shadowmoon Darkcaster
+							23147,	-- Shadowmoon Grunt
+							17414,	-- Shadowmoon Technician
+							17671,	-- Shattered Hand Champion
+							16699,	-- Shattered Hand Reaver
+							22956,	-- Sister of Pain
+							16468,	-- Spectral Patron
+							16407,	-- Spectral Servant
+							15551,	-- Spectral Stable Hand
+							15548,	-- Spectral Stallion
+							25367,	-- Sunblade Arch Mage
+							25371,	-- Sunblade Dawn Priest
+							24683,	-- Sunblade Mage Guard
+							20139,	-- Sunfury Conjurer
+							19779,	-- Sunfury Geologist
+							19168,	-- Sunseeker Astromage
+							20988,	-- Sunseeker Engineer
+							20059,	-- Sunseeker Netherbinder
+							17131,	-- Talbuk Thorngrazer
+							19166,	-- Tempest-Forge Patroller
+							22939,	-- Temple Concubine
+							18319,	-- Time-Lost Scryer
+							18499,	-- Unliving Sorcerer
+							17726,	-- Wrathfin Myrmidon
+							18875,	-- Zaxxis Raider
+						},
+					},
+					{	-- Design: Delicate Living Ruby
+						["itemID"] = 24194,	-- Design: Delicate Living Ruby
+						["crs"] = {
+							18524,	-- Angered Skeleton
+							22883,	-- Aqueous Spawn
+							16488,	-- Arcane Anomaly
+							20857,	-- Arcatraz Defender
+							18497,	-- Auchenai Monk
+							18493,	-- Auchenai Soulpriest
+							21891,	-- Avian Ripper
+							20031,	-- Bloodwarder Legionnaire
+							20990,	-- Bloodwarder Physician
+							17993,	-- Bloodwarder Protector
+							23237,	-- Bonechewer Blood Prophet
+							17259,	-- Bonechewer Hungerer
+							17137,	-- Boulderfist Mage
+							18635,	-- Cabal Deathsworn
+							18632,	-- Cabal Executioner
+							18794,	-- Cabal Ritualist
+							18637,	-- Cabal Shadow Priest
+							25599,	-- Cataclysm Hound
+							22955,	-- Charming Courtesan
+							17957,	-- Coilfang Champion
+							17962,	-- Coilfang Collaborator
+							17721,	-- Coilfang Engineer
+							21220,	-- Coilfang Priestess
+							17802,	-- Coilfang Warrior
+							16171,	-- Coldmist Widow
+							17897,	-- Crypt Fiend
+							20040,	-- Crystalcore Devastator
+							20052,	-- Crystalcore Mechanic
+							20041,	-- Crystalcore Sentinel
+							17952,	-- Darkwater Crocolisk
+							17819,	-- Durnholde Sentry
+							17860,	-- Durnholde Veteran
+							17833,	-- Durnholde Warden
+							18317,	-- Ethereal Priest
+							18309,	-- Ethereal Scavenger
+							18313,	-- Ethereal Sorcerer
+							18312,	-- Ethereal Spellbinder
+							16544,	-- Ethereal Thief
+							18894,	-- Felguard Brute
+							21124,	-- Felsworn Daggermaw
+							16595,	-- Fleshbeast
+							17906,	-- Gargoyle
+							17895,	-- Ghoul
+							17817,	-- Greater Bogstrok
+							21229,	-- Greyheart Tidecaller
+							16539,	-- Homunculus
+							23337,	-- Illidari Centurion
+							18170,	-- Infinite Slayer
+							22884,	-- Leviathan
+							16530,	-- Mana Warp
+							19712,	-- Mechanar Driller
+							17728,	-- Murkblood Tribesman
+							19511,	-- Nethervine Inciter
+							18314,	-- Nexus Stalker
+							18503,	-- Phantasmal Possessor
+							16409,	-- Phantom Guest
+							16472,	-- Phantom Stagehand
+							20038,	-- Phoenix-Hawk Hatchling
+							17839,	-- Rift Lord
+							21478,	-- Rocknail Ripper
+							18323,	-- Sethekk Guard
+							18318,	-- Sethekk Initiate
+							18328,	-- Sethekk Oracle
+							18325,	-- Sethekk Prophet
+							16173,	-- Shadowbat
+							17397,	-- Shadowmoon Adept
+							22945,	-- Shadowmoon Blood Mage
+							22882,	-- Shadowmoon Deathshaper
+							23047,	-- Shadowmoon Soldier
+							22946,	-- Shadowmoon War Hound
+							25837,	-- Shadowsword Commander
+							22180,	-- Shard-Hide Boar
+							17465,	-- Shattered Hand Centurion
+							16700,	-- Shattered Hand Legionnaire
+							16507,	-- Shattered Hand Sentry
+							20747,	-- Silkwing Larva
+							24697,	-- Sister of Torment
+							15547,	-- Spectral Charger
+							16407,	-- Spectral Servant
+							15548,	-- Spectral Stallion
+							20034,	-- Star Scryer
+							25371,	-- Sunblade Dawn Priest
+							24686,	-- Sunblade Warlock
+							19168,	-- Sunseeker Astromage
+							16492,	-- Syphoner
+							22939,	-- Temple Concubine
+							21225,	-- Tidewalker Warrior
+							18327,	-- Time-Lost Controller
+							21651,	-- Time-Lost Skettis Reaver
+							17724,	-- Underbat
+							17138,	-- Warmaul Reaver
+							18064,	-- Warmaul Shaman
+						},
+					},
+					{	-- Design: Destructive Skyfire Diamond
+						["itemID"] = 25907,	-- Design: Destructive Skyfire Diamond
+						["crs"] = {
+							22878,	-- Aqueous Lord
+							22883,	-- Aqueous Spawn
+							22845,	-- Ashtongue Mystic
+							17478,	-- Bleeding Hollow Scryer
+							20036,	-- Bloodwarder Squire
+							17259,	-- Bonechewer Hungerer
+							17281,	-- Bonechewer Ripper
+							22955,	-- Charming Courtesan
+							19428,	-- Cobalt Serpent
+							17962,	-- Coilfang Collaborator
+							17961,	-- Coilfang Enchantress
+							17938,	-- Coilfang Observer
+							17722,	-- Coilfang Sorceress
+							22877,	-- Coilskar Wrangler
+							20049,	-- Crimson Hand Blood Knight
+							17840,	-- Durnholde Tracking Hound
+							17860,	-- Durnholde Veteran
+							18313,	-- Ethereal Sorcerer
+							24698,	-- Ethereum Smuggler
+							17908,	-- Giant Infernal
+							22853,	-- Illidari Defiler
+							23326,	-- Nethermine Ravager
+							19512,	-- Nethervine Reaper
+							16460,	-- Night Mistress
+							20038,	-- Phoenix-Hawk Hatchling
+							17669,	-- Rabid Warhound
+							18323,	-- Sethekk Guard
+							18325,	-- Sethekk Prophet
+							16594,	-- Shadowmoon Acolyte
+							17397,	-- Shadowmoon Adept
+							17694,	-- Shadowmoon Darkcaster
+							23018,	-- Shadowmoon Houndmaster
+							17395,	-- Shadowmoon Summoner
+							17414,	-- Shadowmoon Technician
+							17670,	-- Shattered Hand Houndmaster
+							16523,	-- Shattered Hand Savage
+							21650,	-- Skettis Talonite
+							16468,	-- Spectral Patron
+							16525,	-- Spell Shade
+							25371,	-- Sunblade Dawn Priest
+							24686,	-- Sunblade Warlock
+							20988,	-- Sunseeker Engineer
+							17131,	-- Talbuk Thorngrazer
+							22939,	-- Temple Concubine
+							21225,	-- Tidewalker Warrior
+							18501,	-- Unliving Stalker
+							18869,	-- Unstable Voidwraith
+							18065,	-- Warmaul Brute
+							17963,	-- Wastewalker Slave
+							21382,	-- Wyrmcult Zealot
+						},
+					},
+					{	-- Design: Embrace of the Dawn
+						["itemID"] = 24170,	-- Design: Embrace of the Dawn
+						["crs"] = {
+							17898,	-- Abomination
+							22844,	-- Ashtongue Battlelord
+							22847,	-- Ashtongue Primalist
+							18493,	-- Auchenai Soulpriest
+							21891,	-- Avian Ripper
+							17905,	-- Banshee
+							20031,	-- Bloodwarder Legionnaire
+							17816,	-- Bogstrok
+							17271,	-- Bonechewer Destroyer
+							18631,	-- Cabal Cultist
+							18794,	-- Cabal Ritualist
+							22955,	-- Charming Courtesan
+							17958,	-- Coilfang Defender
+							17803,	-- Coilfang Oracle
+							21301,	-- Coilfang Shatterer
+							17897,	-- Crypt Fiend
+							17819,	-- Durnholde Sentry
+							18331,	-- Ethereal Darkcaster
+							18317,	-- Ethereal Priest
+							16545,	-- Ethereal Spellfilcher
+							18315,	-- Ethereal Theurgist
+							17429,	-- Fel Orc Neophyte
+							21230,	-- Greyheart Nether-Mage
+							21231,	-- Greyheart Shield-Bearer
+							18866,	-- Mageslayer
+							18864,	-- Mana Wraith
+							20875,	-- Negaton Screamer
+							23169,	-- Nethermine Flayer
+							18314,	-- Nexus Stalker
+							16460,	-- Night Mistress
+							16409,	-- Phantom Guest
+							18323,	-- Sethekk Guard
+							18318,	-- Sethekk Initiate
+							17414,	-- Shadowmoon Technician
+							17899,	-- Shadowy Necromancer
+							16699,	-- Shattered Hand Reaver
+							20866,	-- Soul Devourer
+							16468,	-- Spectral Patron
+							25367,	-- Sunblade Arch Mage
+							25363,	-- Sunblade Cabalist
+							24685,	-- Sunblade Magister
+							25369,	-- Sunblade Vindicator
+							24686,	-- Sunblade Warlock
+							16492,	-- Syphoner
+							23029,	-- Talonsworn Forest-Rager
+							18094,	-- Tarren Mill Lookout
+							22939,	-- Temple Concubine
+							21228,	-- Tidewalker Hydromancer
+							18327,	-- Time-Lost Controller
+							17981,	-- Voidspawn
+							18875,	-- Zaxxis Raider
+						},
+					},
 				},
 			},
 		},
