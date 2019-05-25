a8829b8311c8dee677300886f13b90be43433a5a
Gaulish <l2pwncomputers@hotmail.com>
2019-05-25 03:57:22 -0600

    Expansion Features->War Effort->Warfronts->Achives updated to new tech

--- contrib/Parser/DATAS/06 - Expansion Features/07 War Effort/Arathi Highlands Warfront/Achievements.lua
+++ contrib/Parser/DATAS/06 - Expansion Features/07 War Effort/Arathi Highlands Warfront/Achievements.lua
@@ -26,7 +26,39 @@ _.ExpansionFeatures =
 							},
 							["description"] = "Typically only 1 rare per Warfront scenario.  If it lasts long enough you could have 2.",
 						}),
-						ach(12884, {	-- Leader of Troops [Alliance]
+						{	-- Leader of Troops [Alliance] // [Horde]
+							["allianceAchievementID"] = 12884,	-- Leader of Troops (Alliance)
+							["hordeAchievementID"] = 12878,		-- Leader of Troops (Horde)
+							["description"] = "You must personally create each troop.  Demolishers/Siege Engines are limited to 3 active at a time.",
+							["g"] = {
+								{	-- Footmen [Alliance] // Grunts [Horde]
+									["allianceAchievementID"] = 12884,	-- Leader of Troops (Alliance)
+									["hordeAchievementID"] = 12878,		-- Leader of Troops (Horde)
+									["criteriaID"] = 1,					-- Footmen [Alliance] // Grunts [Horde]
+								},
+								{	-- Riflemen [A]/ Axe Throwers [H]
+									["allianceAchievementID"] = 12884,	-- Leader of Troops (Alliance)
+									["hordeAchievementID"] = 12878,		-- Leader of Troops (Horde)
+									["criteriaID"] = 2,					-- Riflemen [A]/ Axe Throwers [H]
+								},
+								{	-- Priests [A]/Shaman [H]
+									["allianceAchievementID"] = 12884,	-- Leader of Troops (Alliance)
+									["hordeAchievementID"] = 12878,		-- Leader of Troops (Horde)
+									["criteriaID"] = 3,					-- Priests [A]/Shaman [H]
+								},
+								{	-- Knights [A]/ Raiders [H]
+									["allianceAchievementID"] = 12884,	-- Leader of Troops (Alliance)
+									["hordeAchievementID"] = 12878,		-- Leader of Troops (Horde)
+									["criteriaID"] = 4,					-- Knights [A]/ Raiders [H]
+								},
+								{	-- Siege Engine [A]/ Demolishers [H]
+									["allianceAchievementID"] = 12884,	-- Leader of Troops (Alliance)
+									["hordeAchievementID"] = 12878,		-- Leader of Troops (Horde)
+									["criteriaID"] = 5,					-- Siege Engine [A]/ Demolishers [H]
+								},
+							}
+						},
+						--[[ach(12884, {	-- Leader of Troops [Alliance]
 							["groups"] = {
 								crit(1),	-- Footman
 								crit(4),	-- Knights
@@ -47,8 +79,54 @@ _.ExpansionFeatures =
 							},
 							["description"] = "You must personally create each troop.  Demolishers are limited to 3 active at a time.",
 							["races"] = HORDE_ONLY,
-						}),
-						ach(12889, {	-- Strike Fast [Alliance]
+						}),]]
+						{	-- Strike Fast [Alliance] // [Horde]
+							["allianceAchievementID"] = 12889,	-- Strike Fast (Alliance)
+							["hordeAchievementID"] = 12876,		-- Strike Fast (Horde)
+							["description"] = "You must personally capture each",
+							["g"] = {
+								{	-- Strike Hard [A]/ [H]
+									["allianceAchievementID"] = 12888,	-- Strike Hard (Alliance)
+									["hordeAchievementID"] = 12877,		-- Strike Hard (Horde)
+								},
+								{	-- Galson's Lode [Alliance] // Circle of Elements [Horde]
+									["allianceAchievementID"] = 12889,	-- Strike Fast (Alliance)
+									["hordeAchievementID"] = 12876,		-- Strike Fast (Horde)
+									["criteriaID"] = 1,					-- Galson's Lode [Alliance] // Circle of Elements [Horde]
+								},
+								{	-- Highlands Mill [A]/ Hatchet Ridge [H]
+									["allianceAchievementID"] = 12889,	-- Strike Fast (Alliance)
+									["hordeAchievementID"] = 12876,		-- Strike Fast (Horde)
+									["criteriaID"] = 2,					-- Highlands Mill [A]/ Hatchet Ridge [H]
+								},
+								{	-- Valorcall Pass [A]/ High Perch [H]
+									["allianceAchievementID"] = 12889,	-- Strike Fast (Alliance)
+									["hordeAchievementID"] = 12876,		-- Strike Fast (Horde)
+									["criteriaID"] = 3,					-- Valorcall Pass [A]/ High Perch [H]
+								},
+								{	-- High Perch [A]/ Northfold Crossing [H]
+									["allianceAchievementID"] = 12889,	-- Strike Fast (Alliance)
+									["hordeAchievementID"] = 12876,		-- Strike Fast (Horde)
+									["criteriaID"] = 4,					-- High Perch [A]/ Northfold Crossing [H]
+								},
+								{	-- Newstead [A]/ [H]
+									["allianceAchievementID"] = 12889,	-- Strike Fast (Alliance)
+									["hordeAchievementID"] = 12876,		-- Strike Fast (Horde)
+									["criteriaID"] = 5,					-- Newstead [A]/ [H]
+								},
+								{	-- Circle of Elements [A]/ Valorcall Pass [H]
+									["allianceAchievementID"] = 12889,	-- Strike Fast (Alliance)
+									["hordeAchievementID"] = 12876,		-- Strike Fast (Horde)
+									["criteriaID"] = 6,					-- Circle of Elements [A]/ Valorcall Pass [H]
+								},
+								{	-- Northfold Crossing [A]/ Drywhisker Mine [H]
+									["allianceAchievementID"] = 12889,	-- Strike Fast (Alliance)
+									["hordeAchievementID"] = 12876,		-- Strike Fast (Horde)
+									["criteriaID"] = 7,					-- Northfold Crossing [A]/ Drywhisker Mine [H]
+								},
+							}
+						},
+						--[[ach(12889, {	-- Strike Fast [Alliance]
 							["groups"] = {
 								ach(12888, {	-- Strike Hard [Alliance]
 									["groups"] = {
@@ -99,8 +177,30 @@ _.ExpansionFeatures =
 							},
 							["description"] = "You must personally capture each",
 							["races"] = HORDE_ONLY,
-						}),
-						ach(12886, {	-- Tour of War [Alliance]
+						}),]]
+						{	-- Tour of War [Alliance] // [Horde]
+							["allianceAchievementID"] = 12886,	-- Tour of War (Alliance)
+							["hordeAchievementID"] = 12879,		-- Tour of War (Horde)
+							["description"] = "Each time the scenario is up only one of these commanders will be active.",
+							["g"] = {
+								{	-- Eitrigg [Alliance] // Danath Trollbane [Horde]
+									["allianceAchievementID"] = 12886,	-- Tour of War (Alliance)
+									["hordeAchievementID"] = 12879,		-- Tour of War (Horde)
+									["criteriaID"] = 1,					-- Eitrigg [Alliance] // Danath Trollbane [Horde]
+								},
+								{	-- Lady Liadrin [A] / Turalyon [H]
+									["allianceAchievementID"] = 12886,	-- Tour of War (Alliance)
+									["hordeAchievementID"] = 12879,		-- Tour of War (Horde)
+									["criteriaID"] = 2,					-- Lady Liadrin [A] / Turalyon [H]
+								},
+								{	-- Rokhan [A] / Muradin [H]
+									["allianceAchievementID"] = 12886,	-- Tour of War (Alliance)
+									["hordeAchievementID"] = 12879,		-- Tour of War (Horde)
+									["criteriaID"] = 3,					-- Rokhan [A] / Muradin [H]
+								},
+							}
+						},
+						--[[ach(12886, {	-- Tour of War [Alliance]
 							["groups"] = {
 								crit(1),	-- Eitrigg
 								crit(2),	-- Lady Liadran
@@ -117,13 +217,17 @@ _.ExpansionFeatures =
 							},
 							["description"] = "Each time the scenario is up only one of these commanders will be active.",
 							["races"] = HORDE_ONLY,
-						}),
-						ach(12881, {	-- War is Hell [Alliance]
+						}),]]
+						{	-- War is Hell [Alliance] // [Horde]
+							["allianceAchievementID"] = 12881,	-- War is Hell (Alliance)
+							["hordeAchievementID"] = 12873,		-- War is Hell (Horde)
+						},
+						--[[ach(12881, {	-- War is Hell [Alliance]
 							["races"] = ALLIANCE_ONLY,
 						}),
 						ach(12873, {	-- War is Hell [Horde]
 							["races"] = HORDE_ONLY,
-						}),
+						}),]]
 					}),
 				},
 				["lvl"] = 120,
