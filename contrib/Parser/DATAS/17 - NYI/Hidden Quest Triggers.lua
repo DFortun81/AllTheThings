a7e79827e40c8308cd06170f2d212601c95d8a61
Lucetia <>
2019-05-25 01:34:17 -0700

    - Hidden Quest Triggers

---
+++ contrib/Parser/DATAS/17 - NYI/Hidden Quest Triggers.lua
@@ -0,0 +1,15 @@
+---------------------------------------------------------------------------
+--         N E V E R       I M P L E M E N T E D       M O D U L E      	--
+---------------------------------------------------------------------------
+_.NeverImplemented =
+{
+	-- Note!! Here is where we can put all the hidden quest triggers in the game that don't exactly fit anywhere.
+	-- When adding a quest trigger be sure to put a description of how it triggered
+	
+	{	-- Triggered by completing Hellfire Ramparts
+		["questID"] = 35572,
+	},
+	{	-- Triggered by completing The Blood Furnace
+		["questID"] = 35566,
+	},
+};
