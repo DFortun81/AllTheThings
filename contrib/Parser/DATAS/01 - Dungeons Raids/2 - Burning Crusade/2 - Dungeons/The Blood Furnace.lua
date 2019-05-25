1279b7c2b2e6187f879f97c4e1b58e30cc50a3be
Lucetia <>
2019-05-25 08:23:01 -0700

    - Adjusted location of Zone drops for Blood Furnace

--- contrib/Parser/DATAS/01 - Dungeons Raids/2 - Burning Crusade/2 - Dungeons/The Blood Furnace.lua
+++ contrib/Parser/DATAS/01 - Dungeons Raids/2 - Burning Crusade/2 - Dungeons/The Blood Furnace.lua
@@ -7,7 +7,7 @@ _.Instances = { tier(2, {	-- Burning Crusade
 		["lvl"] = 58,
 		["mapID"] = 261,
 		["coord"] = { 46.06, 51.74, 100 },	-- The Blood Furnace, Hellfire Peninsula
-		["groups"] = {
+		["g"] = {
 			{	-- Quests
 				["npcID"] = -17,	-- Quests
 				["g"] = {
@@ -34,10 +34,6 @@ _.Instances = { tier(2, {	-- Burning Crusade
 						i(25702),	-- Scaled Legs of Ruination
 					}),
 				},
-			},
-			{	-- Zone Drops
-				["npcID"] = 0,	-- Zone Drops
-				-- Note!! Blank so when map merges into the list it's in this spot rather than top
 			},
 			d(1, {	-- Normal
 				cr(17381, e(555, {	-- The Maker
@@ -92,7 +88,7 @@ _.Instances = { tier(2, {	-- Burning Crusade
 			d(2, {	-- Heroic
 				["lvl"] = 70,
 				["ignoreBonus"] = true,
-				["groups"] = {
+				["g"] = {
 					cr(17381, e(555, {	-- The Maker
 						i(24384),	-- Diamond-Core Sledgemace
 						i(24388),	-- Mage-Collar of the Firestorm
@@ -143,6 +139,10 @@ _.Instances = { tier(2, {	-- Burning Crusade
 					})),
 				}
 			}),
+			{	-- Zone Drops
+				["npcID"] = 0,	-- Zone Drops
+				-- Note!! Blank so when map merges into the list it's in this spot rather than top
+			},
 		},
 	}),
 })};
