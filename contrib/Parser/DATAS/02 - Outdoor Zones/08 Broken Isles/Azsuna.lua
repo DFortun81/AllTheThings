ea93674b427cb977def2dc13c9608524cf74911e
mogwai316 <bryan.harden@gmail.com>
2019-05-27 13:44:05 -0400

    Added coords for a couple Azsuna vendors

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Azsuna.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Azsuna.lua
@@ -2858,46 +2858,52 @@ _.Zones =
 						i(127023),	-- Pattern: Imbued Silkweave Epaulets [Rank 1]
 					}),
 					n(107379, {	-- Marin Bladewing <Wardens Emissary>
-						i(137849),	-- Design: Twisted Pandemonite Choker (Rank 3)
-						i(142333),	-- Recipe: Feathered Luffa
-						i(142331),	-- Recipe: Spiced Falcosaur Omelet
-						i(137713),	-- Schematic: Bolt-Action Headgun (Rank 3)
-						i(137714),	-- Schematic: Reinforced Headgun (Rank 3)
-						i(137716),	-- Schematic: Sawed-Off Cranial Cannon (Rank 3)
-						i(137715),	-- Schematic: Semi-Automagic Cranial Cannon (Rank 3)
-						i(137773),	-- Vantus Rune Technique: Cenarius (Rank 3)
-						i(137777),	-- Vantus Rune Technique: Spellblade Aluriel (Rank 3)
-						i(137781),	-- Vantus Rune Technique: Star Augur Etraeus (Rank 3)
-						i(136898), 	-- Fledgling Warden Owl Pet
-						i(130157), 	-- Syxsehnz Rod Toy
-						i(130191), 	-- Trapped Treasure Chest Kit Toy
-						i(140580), 	-- Warden's Tabard
-						i(139607), 	-- Drape of the Patient Hunter
-						i(139603), 	-- Vault Patroller's Warboots
+						["coord"] = { 48.2, 73.9, 630 },
+						["groups"] = {
+							i(137849),	-- Design: Twisted Pandemonite Choker (Rank 3)
+							i(142333),	-- Recipe: Feathered Luffa
+							i(142331),	-- Recipe: Spiced Falcosaur Omelet
+							i(137713),	-- Schematic: Bolt-Action Headgun (Rank 3)
+							i(137714),	-- Schematic: Reinforced Headgun (Rank 3)
+							i(137716),	-- Schematic: Sawed-Off Cranial Cannon (Rank 3)
+							i(137715),	-- Schematic: Semi-Automagic Cranial Cannon (Rank 3)
+							i(137773),	-- Vantus Rune Technique: Cenarius (Rank 3)
+							i(137777),	-- Vantus Rune Technique: Spellblade Aluriel (Rank 3)
+							i(137781),	-- Vantus Rune Technique: Star Augur Etraeus (Rank 3)
+							i(136898), 	-- Fledgling Warden Owl Pet
+							i(130157), 	-- Syxsehnz Rod Toy
+							i(130191), 	-- Trapped Treasure Chest Kit Toy
+							i(140580), 	-- Warden's Tabard
+							i(139607), 	-- Drape of the Patient Hunter
+							i(139603), 	-- Vault Patroller's Warboots
+						},
 					}),
 					n(107376, {	-- Veridis Fallon <Court of Farondis Emissary>
-						i(137978),	-- Pattern: Imbued Silkweave Gloves [Rank 3]
-						i(137977),	-- Pattern: Imbued Silkweave Hood [Rank 3]
-						i(138015),	-- Pattern: Silkweave Pantaloons [Rank 3]
-						i(137980),	-- Pattern: Imbued Silkweave Robe [Rank 3]
-						i(137780),	-- Vantus Rune Technique: Krosus [Rank 3]
-						i(137774),	-- Vantus Rune Technique: Skorpyron [Rank 3]
-						i(137779),	-- Vantus Rune Technique: High Botanist Tel'arn [Rank 3]
-						i(137776),	-- Vantus Rune Technique: Trilliax [Rank 3]
-						i(139556, {	-- Syriel Crescentfall's Notes: Ravenguard
-							artifact(224),	-- Marksmanship Hunter Hidden Appearance
-						}),
-						i(129276, {	-- Beginner's Guide to Dimensional Rifting
-							ach(11175),	-- Higher Dimensional Rifting
-						}),
-						i(140672), 	-- Court Scribe Pet
-						i(129279), 	-- Enchanted Stone Whistle Toy
-						i(140577), 	-- Court of Farondis Tabard
-						i(139606), 	-- Cadet's Gaudy Scarf
-						i(139595), 	-- Once-Fashionable Nar'thalas Leggings
-						{
-							["itemID"] = 139631,	-- Vainglorious Draught
-							["questID"] = 43514,	-- A Vainglorious Past
+						["coord"] = { 46.9, 41.4, 630 },
+						["groups"] = {
+							i(137978),	-- Pattern: Imbued Silkweave Gloves [Rank 3]
+							i(137977),	-- Pattern: Imbued Silkweave Hood [Rank 3]
+							i(138015),	-- Pattern: Silkweave Pantaloons [Rank 3]
+							i(137980),	-- Pattern: Imbued Silkweave Robe [Rank 3]
+							i(137780),	-- Vantus Rune Technique: Krosus [Rank 3]
+							i(137774),	-- Vantus Rune Technique: Skorpyron [Rank 3]
+							i(137779),	-- Vantus Rune Technique: High Botanist Tel'arn [Rank 3]
+							i(137776),	-- Vantus Rune Technique: Trilliax [Rank 3]
+							i(139556, {	-- Syriel Crescentfall's Notes: Ravenguard
+								artifact(224),	-- Marksmanship Hunter Hidden Appearance
+							}),
+							i(129276, {	-- Beginner's Guide to Dimensional Rifting
+								ach(11175),	-- Higher Dimensional Rifting
+							}),
+							i(140672), 	-- Court Scribe Pet
+							i(129279), 	-- Enchanted Stone Whistle Toy
+							i(140577), 	-- Court of Farondis Tabard
+							i(139606), 	-- Cadet's Gaudy Scarf
+							i(139595), 	-- Once-Fashionable Nar'thalas Leggings
+							{
+								["itemID"] = 139631,	-- Vainglorious Draught
+								["questID"] = 43514,	-- A Vainglorious Past
+							},
 						},
 					}),
 				}),
