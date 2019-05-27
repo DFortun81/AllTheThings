18d406192192305fdcb94b539b2ce0532e1c8c91
Lucetia <>
2019-05-27 02:20:36 -0700

    - Removed some excess metadata stuff from Shattrath City that I forgot to crop out

--- contrib/Parser/DATAS/02 - Outdoor Zones/03 Outland/1 - Shattrath City/Pet Battles.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/03 Outland/1 - Shattrath City/Pet Battles.lua
@@ -3,21 +3,21 @@
 ---------------------------------------------------
 _.Zones =
 {
-	m(101, {	-- Outland
-		m(111, {	-- Shattrath City
-			["description"] = "|cff66ccffShattrath City is a major hub in Outland situated in the northwestern portion of Terokkar Forest. It is a capital-sized sanctuary city populated by ancient heroes and naaru. It is the first capital available to both sides.|r",
-			["isRaid"] = true,
-			["lvl"] = 58,
-			["icon"] = "Interface\\Icons\\spell_arcane_teleportshattrath",
-			["g"] = {
-				n(-25,  {	-- Pet Battle
-					q(31925, {	-- Morulu The Elder
-						["qg"] = 66553,	-- Morulu The Elder
-						["repeatable"] = true,
-						["isDaily"] = true,
-						["coord"] = { 59.0, 70.0, 111 },
+	{	-- Outland
+		["mapID"] = 101,	-- Outland
+		["g"] = {
+			{	-- Shattrath City
+				["mapID"] = 111,	-- Shattrath City
+				["g"] = {
+					n(-25,  {	-- Pet Battle
+						q(31925, {	-- Morulu The Elder
+							["qg"] = 66553,	-- Morulu The Elder
+							["repeatable"] = true,
+							["isDaily"] = true,
+							["coord"] = { 59.0, 70.0, 111 },
+						}),
 					}),
-				}),
+				},
 			},
 		},
 	},
