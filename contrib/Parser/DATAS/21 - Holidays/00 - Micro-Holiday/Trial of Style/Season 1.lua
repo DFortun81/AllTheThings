c5f565c4aeb423e0165a65114b7ee1c50b15a340
Lucetia <>
2019-05-27 04:30:48 -0700

    - Restructured Trial of Style into it's appropriate seasons - Better internal organization of Trial of Style

---
+++ contrib/Parser/DATAS/21 - Holidays/00 - Micro-Holiday/Trial of Style/Season 1.lua
@@ -0,0 +1,166 @@
+--------------------------------------------
+--     H O L I D A Y S  M O D U L E       --
+--------------------------------------------
+_.Holidays =
+{
+	{	-- Trial of Style
+		["holidayID"] = 1671631,	-- Trial of Style
+		["g"] = {
+			{	-- Trial of Style Token
+				["itemID"] = 151134,	-- Trial of Style Token
+				["u"] = 22,				-- Micro-Holiday Filter
+				["g"] = {
+					{	-- Season 1
+						["setHeaderID"] = 973,	-- Season 1
+						["icon"] = "Interface\\Icons\\inv_misc_scrollrolled02c",
+						["g"] = {
+							{	-- Stylish!
+								["spellID"] = 245743,	-- Stylish!
+								["g"] = {
+									{	-- Fashionable Undershirt
+										["itemID"] = 151116,	-- Fashionable Undershirt
+										["description"] = "Cost: 1 Trial of Style Token",
+									},
+								},
+							},
+							{	-- Ensemble: Mana-Etched Regalia
+								["itemID"] = 151117,	-- Ensemble: Mana-Etched Regalia
+								["description"] = "Cost: 60 Trial of Style Tokens",
+								["classes"] = {
+									5,	-- Priest
+									8,	-- Mage
+									9,	-- Warlock
+								},
+								["g"] = {
+									{	-- Mana-Etched Crown
+										["itemID"] = 151885,	-- Mana-Etched Crown
+									},
+									{	-- Mana-Etched Spaulders
+										["itemID"] = 151886,	-- Mana-Etched Spaulders
+									},
+									{	-- Mana-Etched Vestments
+										["itemID"] = 151882,	-- Mana-Etched Vestments
+									},
+									{	-- Mana-Etched Gloves
+										["itemID"] = 151884,	-- Mana-Etched Gloves
+									},
+									{	-- Glyph-Lined Sash
+										["itemID"] = 151887,	-- Glyph-Lined Sash
+									},
+									{	-- Mana-Etched Pantaloons
+										["itemID"] = 151883,	-- Mana-Etched Pantaloons
+									},
+									{	-- Sigil-Laced Boots
+										["itemID"] = 151888,	-- Sigil-Laced Boots
+									},
+								},
+							},
+							{	-- Ensemble: Obsidian Prowler's Garb
+								["itemID"] = 151118,	-- Ensemble: Obsidian Prowler's Garb
+								["description"] = "Cost: 60 Trial of Style Tokens",
+								["classes"] = {
+									11,	-- Druid
+									4,	-- Rogue
+									10,	-- Monk
+									12,	-- Demon Hunter
+								},
+								["g"] = {
+									{	-- Savage Mask of the Lynx Lord
+										["itemID"] = 151891,	-- Savage Mask of the Lynx Lord
+									},
+									{	-- Sun-Gilded Shouldercaps
+										["itemID"] = 151889,	-- Sun-Gilded Shouldercaps
+									},
+									{	-- Chestguard of the Prowler
+										["itemID"] = 151895,	-- Chestguard of the Prowler
+									},
+									{	-- Predatory Gloves
+										["itemID"] = 151892,	-- Predatory Gloves
+									},
+									{	-- Epoch's Whispering Cinch
+										["itemID"] = 151894,	-- Epoch's Whispering Cinch
+									},
+									{	-- Mennu's Scaled Leggings
+										["itemID"] = 151893,	-- Mennu's Scaled Leggings
+									},
+									{	-- Silent-Strider Kneeboots
+										["itemID"] = 151890,	-- Silent-Strider Kneeboots
+									},
+								},
+							},
+							{	-- Ensemble: Der'izu Armor
+								["itemID"] = 151119,	-- Ensemble: Der'izu Armor
+								["description"] = "Cost: 60 Trial of Style Tokens",
+								["classes"] = {
+									3,	-- Hunter
+									7,	-- Shaman
+								},
+								["g"] = {
+									{	-- Der'izu Helm
+										["itemID"] = 151898,	-- Der'izu Helm
+									},
+									{	-- Der'izu Spaulders
+										["itemID"] = 151896,	-- Der'izu Spaulders
+									},
+									{	-- Der'izu Chestpiece
+										["itemID"] = 151901,	-- Der'izu Chestpiece
+									},
+									{	-- Der'izu Bracer
+										["itemID"] = 151902,	-- Der'izu Bracer
+									},
+									{	-- Der'izu Fists
+										["itemID"] = 151900,	-- Der'izu Fists
+									},
+									{	-- Der'izu Belt
+										["itemID"] = 151903,	-- Der'izu Belt
+									},
+									{	-- Der'izu Legguards
+										["itemID"] = 151897,	-- Der'izu Legguards
+									},
+									{	-- Der'izu Greaves
+										["itemID"] = 151899,	-- Der'izu Greaves
+									},
+								},
+							},
+							{	-- Ensemble: Righteous Battleplate
+								["itemID"] = 151120,	-- Ensemble: Righteous Battleplate
+								["description"] = "Cost: 60 Trial of Style Tokens",
+								["classes"] = {
+									2,	-- Paladin
+									1,	-- Warrior
+									6,	-- Death Knight
+								},
+								["g"] = {
+									{	-- Helm of the Righteous
+										["itemID"] = 151906,	-- Helm of the Righteous
+									},
+									{	-- Spaulders of the Righteous
+										["itemID"] = 151904,	-- Spaulders of the Righteous
+									},
+									{	-- Breastplate of the Righteous
+										["itemID"] = 151908,	-- Breastplate of the Righteous
+									},
+									{	-- Bracers of Dignity
+										["itemID"] = 151911,	-- Bracers of Dignity
+									},
+									{	-- Gauntlets of the Righteous
+										["itemID"] = 151907,	-- Gauntlets of the Righteous
+									},
+									{	-- Girdle of Valorous Deeds
+										["itemID"] = 151910,	-- Girdle of Valorous Deeds
+									},
+									{	-- Legplates of the Righteous
+										["itemID"] = 151905,	-- Legplates of the Righteous
+									},
+									{	-- Boots of the Righteous Path
+										["itemID"] = 151909,	-- Boots of the Righteous Path
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+};
