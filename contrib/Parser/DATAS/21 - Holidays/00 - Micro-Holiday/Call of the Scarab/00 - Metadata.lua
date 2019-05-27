b941916142e9bd32617841b64ea816364cb13423
Lucetia <>
2019-05-27 02:20:09 -0700

    - Didn't push readjsted files and it only pushed the old versions ><

---
+++ contrib/Parser/DATAS/21 - Holidays/00 - Micro-Holiday/Call of the Scarab/00 - Metadata.lua
@@ -0,0 +1,13 @@
+--------------------------------------------
+--     H O L I D A Y S  M O D U L E       --
+--------------------------------------------
+_.Holidays =
+{
+	{	-- Call of the Scarab
+		["holidayID"] = 1574965,	-- Call of the Scarab
+		["u"] = 22,					-- Micro-Holiday Filter
+		["maps"] = {
+			81,	-- Silithius
+		},
+	},
+};
---
+++ contrib/Parser/DATAS/21 - Holidays/00 - Micro-Holiday/Call of the Scarab/Vendors.lua
@@ -0,0 +1,216 @@
+--------------------------------------------
+--     H O L I D A Y S  M O D U L E       --
+--------------------------------------------
+_.Holidays =
+{
+	{	-- Call of the Scarab
+		["holidayID"] = 1574965,	-- Call of the Scarab
+		["u"] = 22,					-- Micro-Holiday Filter
+		["g"] = {
+			{	-- Vendors
+				["npcID"] = -2,	-- Vendors
+				["g"] = {
+					{	-- Field Marshal Snowfall
+						["npcID"] = 117435,		-- Field Marshal Snowfall
+						["coords"] = {
+							{ 34.06, 80.43, 81 },
+						},
+						["races"] = ALLIANCE_ONLY,
+						["g"] = {
+							{	-- Abyssal Crest
+								["itemID"] = 143865,	-- Abyssal Crest
+								["g"] = {
+									{	-- Sapphire Qiraji Resonating Crystal
+										["itemID"] = 151626,	-- Sapphire Qiraji Resonating Crystal
+										["u"] = 22,				-- Micro-Holiday Filter
+										["description"] = "Cost: 1 Abyssal Crest\nCall of the Scarab must be available to purchase these items.",
+										["collectible"] = false,
+										["races"] = ALLIANCE_ONLY,
+									},
+									{	-- Twilight Cultist Medallion of Station
+										["itemID"] = 143867,	-- Twilight Cultist Medallion of Station
+										["description"] = "Cost: 250 Abyssal Crest\nCall of the Scarab must be available to purchase these items.",
+										["u"] = 22,				-- Micro-Holiday Filter
+										["g"] = {
+											{	-- Cinder of Cynders
+												["itemID"] = 143880,	-- Cinder of Cynders
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45755,	-- Cinder of Cynders
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = ALLIANCE_ONLY,
+											},
+											{	-- Pearl of Fathoms
+												["itemID"] = 143881,	-- Pearl of Fathoms
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45756,	-- Pearl of Fathoms
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = ALLIANCE_ONLY,
+											},
+											{	-- Salt of Shards
+												["itemID"] = 143882,	-- Salt of Shards
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45757,	-- Salt of Shards
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = ALLIANCE_ONLY,
+											},
+											{	-- Song of Zephyrs
+												["itemID"] = 143883,	-- Song of Zephyrs
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45758,	-- Song of Zephyrs
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = ALLIANCE_ONLY,
+											},
+										},
+									},
+									{	-- Twilight Cultist Ring of Lordship
+										["itemID"] = 143866,	-- Twilight Cultist Ring of Lordship
+										["description"] = "Cost: 1000 Abyssal Crest\nCall of the Scarab must be available to purchase these items.",
+										["u"] = 22,				-- Micro-Holiday Filter
+										["g"] = {
+											{	-- Coreforged Sigil of Skaldrenox
+												["itemID"] = 143884,	-- Coreforged Sigil of Skaldrenox
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45759,	-- Coreforged Sigil of Skaldrenox
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = ALLIANCE_ONLY,
+											},
+											{	-- Diamondine Sigil of Kazum [Alliance]
+												["itemID"] = 143886,	-- Diamondine Sigil of Kazum
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45761,	-- Diamondine Sigil of Kazum
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = ALLIANCE_ONLY,
+											},
+											{	-- Glimmering Sigil of Whirlaxis
+												["itemID"] = 143885,	-- Glimmering Sigil of Whirlaxis
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45760,	-- Glimmering Sigil of Whirlaxis
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = ALLIANCE_ONLY,
+											},
+											{	-- Tidal Sigil of Skwol
+												["itemID"] = 143887,	-- Tidal Sigil of Skwol
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45762,	-- Tidal Sigil of Skwol
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = ALLIANCE_ONLY,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					{	-- Warlord Gorchuk
+						["npcID"] = 117433,		-- Warlord Gorchuk
+						--["itemID"] = 143865,	-- Abyssal Crest
+						["coords"] = {
+							{ 33.96, 81.91, 81 },
+						},
+						["races"] = HORDE_ONLY,
+						["g"] = {
+							{	-- Abyssal Crest
+								["itemID"] = 143865,	-- Abyssal Crest
+								["g"] = {
+									{	-- Ruby Qiraji Resonating Crystal
+										["itemID"] = 151625,	-- Ruby Qiraji Resonating Crystal
+										["u"] = 22,				-- Micro-Holiday Filter
+										["description"] = "Cost: 1 Abyssal Crest\nCall of the Scarab must be available to purchase these items.",
+										["collectible"] = false,
+										["races"] = HORDE_ONLY,
+									},
+									{	-- Twilight Cultist Medallion of Station
+										["itemID"] = 143867,	-- Twilight Cultist Medallion of Station
+										["u"] = 22,				-- Micro-Holiday Filter
+										["description"] = "Cost: 250 Abyssal Crest\nCall of the Scarab must be available to purchase these items.",
+										["g"] = {
+											{	-- Cinder of Cynders
+												["itemID"] = 143872,	-- Cinder of Cynders
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45747,	-- Cinder of Cynders
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = HORDE_ONLY,
+											},
+											{	-- Pearl of Fathoms
+												["itemID"] = 143873,	-- Pearl of Fathoms
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45748,	-- Pearl of Fathoms
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = HORDE_ONLY,
+											},
+											{	-- Salt of Shards
+												["itemID"] = 143874,	-- Salt of Shards
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45749,	-- Salt of Shards
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = HORDE_ONLY,
+											},
+											{	-- Song of Zephyrs
+												["itemID"] = 143875,	-- Song of Zephyrs
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45750,	-- Song of Zephyrs
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = HORDE_ONLY,
+											},
+										},
+									},
+									{	-- Twilight Cultist Ring of Lordship
+										["itemID"] = 143866,	-- Twilight Cultist Ring of Lordship
+										["u"] = 22,				-- Micro-Holiday Filter
+										["description"] = "Cost: 1000 Abyssal Crest\nCall of the Scarab must be available to purchase these items.",
+										["g"] = {
+											{	-- Coreforged Sigil of Skaldrenox
+												["itemID"] = 143876,	-- Coreforged Sigil of Skaldrenox
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45751,	-- Coreforged Sigil of Skaldrenox
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = HORDE_ONLY,
+											},
+											{	-- Diamondine Sigil of Kazum [Horde]
+												["itemID"] = 143878,	-- Diamondine Sigil of Kazum
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45753,	-- Diamondine Sigil of Kazum
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = HORDE_ONLY,
+											},
+											{	-- Glimmering Sigil of Whirlaxis
+												["itemID"] = 143877,	-- Glimmering Sigil of Whirlaxis
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45752,	-- Glimmering Sigil of Whirlaxis
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = HORDE_ONLY,
+											},
+											{	-- Tidal Sigil of Skwol
+												["itemID"] = 143879,	-- Tidal Sigil of Skwol
+												["u"] = 22,				-- Micro-Holiday Filter
+												["description"] = "Call of the Scarab must be available to use this item.",
+												["questID"] = 45754,	-- Tidal Sigil of Skwol
+												["lvl"] = 40,			-- Level needed for quest
+												["races"] = HORDE_ONLY,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+};
