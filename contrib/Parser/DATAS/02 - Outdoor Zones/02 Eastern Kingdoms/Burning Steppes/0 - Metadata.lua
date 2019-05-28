462bd3a6e302f71db0ba9d5f9cc1946e2ea4f885
Pr3vention <Pr3vention@gmail.com>
2019-05-27 22:37:17 -0500

    Burning Steppes breakup

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Burning Steppes/0 - Metadata.lua
@@ -0,0 +1,13 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(36, {	-- Burning Steppes
+			["lvl"] = 40,
+			["achievementID"] = 775,
+			["description"] = "|cff66ccffBurning Steppes is home to a large orc settlement, Twilight Hammer cultists, and the black dragonflight. Players help popular NPCs Eitrigg and John J. Keeshan stop the attacks of the hostile bloodthirsty orcs.|r",
+		}),
+	}),
+};
---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Burning Steppes/Vendors.lua
@@ -0,0 +1,20 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(36, {	-- Burning Steppes
+			["g"] = {
+				n(-2,  {	-- Vendors
+					n(115805, {	-- Hoddruc Bladebender
+						["description"]	= "Vendor only sells the recipe to those who have completed the quest 'Worth Its Weight in Steel'|r",
+						["g"]	= {
+							i(142346),	-- Plans: Bleakwood Hew
+						},
+					}),
+				}),
+			},
+		}),
+	}),
+};
