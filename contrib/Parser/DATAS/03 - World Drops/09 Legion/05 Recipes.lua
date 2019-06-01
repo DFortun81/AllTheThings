6b490ae6fb340a18dbc64102e590245fce381f07
mogwai316 <bryan.harden@gmail.com>
2019-06-01 17:32:13 -0400

    Added descriptions to Legion world drop tailoring patterns that only drop from certain types of mobs

--- contrib/Parser/DATAS/03 - World Drops/09 Legion/05 Recipes.lua
+++ contrib/Parser/DATAS/03 - World Drops/09 Legion/05 Recipes.lua
@@ -38,14 +38,24 @@ _.WorldDrops =
 					i(141918),	-- Formula: Enchant Neck - Mark of the Trained Soldier (Rank 3)
 				}),
 				n(-190, {	-- Tailoring
-					i(142077),	-- Pattern: Imbued Silkweave Bag (Rank 2)
-					i(138006),	-- Pattern: Imbued Silkweave Cover (Rank 2)
+					i(142077, {	-- Pattern: Imbued Silkweave Bag (Rank 2)
+						["description"] = "Drops from ghosts in any Broken Isles zone"
+					}),
+					i(138006, {	-- Pattern: Imbued Silkweave Cover (Rank 2)
+						["description"] = "Drops from furbolgs in any Broken Isles zone"
+					}),
 					i(138007),	-- Pattern: Imbued Silkweave Cover (Rank 3)
-					i(138009),	-- Pattern: Imbued Silkweave Drape (Rank 2)
+					i(138009, {	-- Pattern: Imbued Silkweave Drape (Rank 2)
+						["description"] = "Drops from harpies in any Broken Isles zone"
+					}),
 					i(138010),	-- Pattern: Imbued Silkweave Drape (Rank 3)
-					i(138003),	-- Pattern: Imbued Silkweave Flourish (Rank 2)
+					i(138003, {	-- Pattern: Imbued Silkweave Flourish (Rank 2)
+						["description"] = "Drops from murlocs in any Broken Isles zone"
+					}),
 					i(138004),	-- Pattern: Imbued Silkweave Flourish (Rank 3)
-					i(138000),	-- Pattern: Imbued Silkweave Shade (Rank 2)
+					i(138000, {	-- Pattern: Imbued Silkweave Shade (Rank 2)
+						["description"] = "Drops from vrykuls in any Broken Isles zone"
+					}),
 					i(138001),	-- Pattern: Imbued Silkweave Shade (Rank 3)
 				}),
 			}),
