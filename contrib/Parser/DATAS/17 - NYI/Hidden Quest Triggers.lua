307883890ed71b984f12ea70933c4c11891460a7
Pr3vention <Pr3vention@gmail.com>
2019-06-02 12:34:58 -0500

    Zul'Farrak header reduction and added some more hidden quest flags
    Reformatted the file to leverage q() for easier reading

--- contrib/Parser/DATAS/17 - NYI/Hidden Quest Triggers.lua
+++ contrib/Parser/DATAS/17 - NYI/Hidden Quest Triggers.lua
@@ -6,28 +6,15 @@ _.NeverImplemented =
 	-- Note!! Here is where we can put all the hidden quest triggers in the game that don't exactly fit anywhere.
 	-- When adding a quest trigger be sure to put a description of how it triggered
 	
-	{	-- Triggered by completing Hellfire Ramparts
-		["questID"] = 35572,
-	},
-	{	-- Triggered by completing The Blood Furnace
-		["questID"] = 35566,
-	},
-	{	-- Blasted Land 6.0:JP3 - Invasion - Tracking Quest - See Maraad
-		["questID"] = 37677,
-	},
-	{	-- Blasted Land 6.0:JP3 - Invasion - Tracking Quest - See Thrall
-		["questID"] = 37676,
-	},
-	{	-- Razorfen Downs: dungeon completion
-		["questID"] = 35598,
-	},
-	{	-- Razorfen Kraul: dungeon completion
-		["questID"] = 35615,
-	},
-	{	-- Wailing Caverns: bonus objective complete. Does not trigger when all bonus objects are completed
-		["questID"] = 35307,
-	},
-	{	-- Wailing Caverns: Naralax awakening ritual complete with all bonus objectives met
-		["questID"] = 35303,
-	},
+	q(35572),	-- Triggered by completing Hellfire Ramparts
+	q(35566),	-- Triggered by completing The Blood Furnace
+	q(37677),	-- Blasted Land 6.0:JP3 - Invasion - Tracking Quest - See Maraad
+	q(37676),	-- Blasted Land 6.0:JP3 - Invasion - Tracking Quest - See Thrall
+	q(35894),	-- Dire Maul: Bonus objective complete
+	q(35598),	-- Razorfen Downs: dungeon completion
+	q(35615),	-- Razorfen Kraul: dungeon completion
+	q(35307),	-- Wailing Caverns: bonus objective complete
+	q(35303),	-- Wailing Caverns: Naralax awakening ritual complete
+	q(35588),	-- Zul'Farrak: end-of-dungeon reward granted
+	q(35589),	-- Zul'Farrak: bonus objective completed
 };
