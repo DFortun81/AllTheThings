21e94827d1b4d30dafce9ac952898d1780932b0e
Pr3vention <Pr3vention@gmail.com>
2019-06-02 23:08:34 -0500

    Zul'drak broken up. Added some extra details to quests and mapID to coords

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/04 Northrend/Zul'Drak/Pet Battles.lua
@@ -0,0 +1,20 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(113, { 	-- Northrend
+		m(121, {	-- Zul'Drak
+			["g"] = {
+				n(-25,  {	-- Pet Battle
+					p(641),		-- Arctic Hare
+					p(234),		-- Gundrak Hatchling
+					p(387),		-- Snake
+					p(412),		-- Spider
+					p(1238),	-- Unborn Val'kyr
+					p(535),		-- Water Waveling
+				}),
+			},
+		}),
+	}),
+};
