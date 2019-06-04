17de30c74855f631ae563e93948aeea17e3b6f8b
Pr3vention <Pr3vention@gmail.com>
2019-06-04 11:20:13 -0500

    Crystalsong Forest broken up
    mapID added to coords, crs to pets, removed some holiday quests that are implemented elsewhere

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/04 Northrend/Crystalsong Forest/Crystalsong Forest.lua
@@ -0,0 +1,27 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(113, { 	-- Northrend
+		m(127, {	-- Crystalsong Forest
+			["g"] = {
+--[[
+				n(-17, { 	-- Quests
+					qa(11786),	-- Desecrate this Fire!
+					qa(11580),	-- Desecrate this Fire!
+					qa(11732),	-- Desecrate this Fire!
+					qh(11745),	-- Desecrate this Fire!
+					qh(11749),	-- Desecrate this Fire!
+					qa(11755),	-- Desecrate this Fire!
+					qa(11766),	-- Desecrate this Fire!
+					qh(11581),	-- Desecrate this Fire!
+					qa(13457),	-- Desecrate this Fire!
+					qh(13447),	-- Desecrate this Fire!
+					q(31932),	-- Nearly Headless Jacob
+				}),
+--]]
+			},
+		}),
+	}),
+};
