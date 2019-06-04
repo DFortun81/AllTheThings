7a04e5fa6047fe27951a519d6a3d35b71f873e5e
Aveza <mikkottis@hotmail.com>
2019-06-04 21:00:05 +0300

    The Veiled Stair broken up and cleaned up.

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/The Veiled Stair/0 - Metadata.lua
@@ -0,0 +1,12 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(424, {	-- Pandaria
+		m(433, { -- The Veiled Stair
+			["lvl"] = 78,
+			["icon"] = "Interface\\Icons\\inv_legendary_theblackprince",
+		}),
+	}),
+};
