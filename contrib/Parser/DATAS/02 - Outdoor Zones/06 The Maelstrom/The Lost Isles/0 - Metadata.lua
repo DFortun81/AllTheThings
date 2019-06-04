35f8a6a232074c6cbf72d3a460c353e6c9b6ae6f
Pr3vention <Pr3vention@gmail.com>
2019-06-04 16:18:44 -0500

    The Lost Isles broken up

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/06 The Maelstrom/The Lost Isles/0 - Metadata.lua
@@ -0,0 +1,19 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(948, { 	-- The Maelstrom
+		m(174, { 	-- The Lost Isles
+			["races"] = { 9 },	-- Goblin
+			["description"] = "|cff66ccffThe Lost Isles are a chain of islands to the south of Azshara that the Bilgewater Cartel goblins reached after their shipwreck. Things get interesting when the orc ship Draka's Fury also crashes. This is a phased area that only leveling goblins can reach.|r",
+			["icon"] = "Interface\\Icons\\achievement_femalegoblinhead",
+			["maps"] = {
+				175,	-- Kaja'mite Cavern
+				176,	-- Volcanoth's Lair
+				177,	-- Gallywix Labor Mine - Mine Tunnels
+				178,	-- Gallywix Labor Mine - Mine Shaft
+			},
+		}),
+	}),
+};
---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/06 The Maelstrom/The Lost Isles/Vendors.lua
@@ -0,0 +1,20 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(948, { 	-- The Maelstrom
+		m(174, { 	-- The Lost Isles
+			["g"] = {
+				n(-2, {	-- Vendors
+					n(45286, {	-- KTC Train-a-Tron Deluxe <Professions Trainer & Vendor>
+						["g"] = {
+							i(21219),	-- Recipe: Sagefish Delight
+							i(21099),	-- Recipe: Smoked Sagefish
+						},
+					}),
+				}),
+			},
+		}),
+	}),
+};
