0909c4bc1dbb50bbe3da68d90c173b30526e6b5e
Pr3vention <Pr3vention@gmail.com>
2019-06-04 17:07:41 -0500

    Broken Shore broken up

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Broken Shore/Command Center.lua
@@ -0,0 +1,22 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(619, { 	-- The Broken Isles
+		m(646, { 	-- Broken Shore
+			["g"] = {
+				n(-10049, {	-- Command Center
+					["g"] =  {
+						n(122719, {	-- Scouting Map
+							["achievementID"] = 11217,
+							["modelScale"] = 2.75,
+							["modelRotation"] = 325,
+						}),
+					},
+					["achievementID"] = 11736,	-- Assume Command
+				}),
+			},
+		}),
+	}),
+};
---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Broken Shore/Flight Paths.lua
@@ -0,0 +1,27 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(619, { 	-- The Broken Isles
+		m(646, { 	-- Broken Shore
+			["g"] = {
+				n(-228, {	-- Flight Paths
+					fp(1942, {	-- Aalgen Point, Broken Shore
+						["coord"] = { 70.6, 47.6, 646 },
+					}),
+					fp(1862, {	-- Acherus: The Ebon Hold
+						["coord"] = { 97.4, 56.0, 646 },
+						["classes"] = { 6 },	-- Death Knight
+					}),
+					fp(1941, {	-- Deliverance Point, Broken Shore
+						["coord"] = { 45.2, 64.0, 646 },
+					}),
+					fp(1856, {	-- Vengeance Point, Broken Shore
+						["coord"] = { 49.6, 21.0, 646 },
+					}),
+				}),
+			},
+		}),
+	}),
+};
