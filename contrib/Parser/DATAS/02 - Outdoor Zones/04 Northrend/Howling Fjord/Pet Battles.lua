fab05552502e860cc55b460a7a5f4ba7a2fbdfd8
Pr3vention <Pr3vention@gmail.com>
2019-06-04 15:10:21 -0500

    Howling Fjord broken up
    Added mapID to coords, crs to pets, excess header removal

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/04 Northrend/Howling Fjord/Pet Battles.lua
@@ -0,0 +1,66 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(113, { 	-- Northrend
+		m(117, { 	-- Howling Fjord
+			["g"] = {
+				n(-25, { 	-- Pet Battle
+					p(646, {	-- Chicken
+						["crs"] = { 62664 },	-- Chicken
+					}),
+					p(523, {	-- Devouring Maggot
+						["crs"] = { 62640 },	-- Devouring Maggot
+					}),
+					p(644, {	-- Fjord Rat
+						["crs"] = { 62641 },	-- Fjord Rat
+					}),
+					p(529, {	-- Fjord Worg Pup
+						["crs"] = { 62669 },	-- Fjord Worg Pup
+					}),
+					p(647, {	-- Grizzly Squirrel
+						["crs"] = { 62818 },	-- Grizzly Squirrel
+					}),
+					p(450, {	-- Maggot
+						["crs"] = { 61753 },	-- Maggot
+					}),
+					p(378, {	-- Rabbit
+						["crs"] = { 61080 },	-- Rabbit
+					}),
+					p(417, {	-- Rat
+						["crs"] = { 61366 },	-- Rat
+					}),
+					p(424, {	-- Roach
+						["crs"] = { 61169 },	-- Roach
+					}),
+					p(388, {	-- Shore Crab
+						["crs"] = { 61158 },	-- Shore Crab
+					}),
+					p(397, {	-- Skunk
+						["crs"] = { 61255 },	-- Skunk
+					}),
+					p(387, {	-- Snake
+						["crs"] = { 61142 },	-- Snake
+					}),
+					p(412, {	-- Spider
+						["crs"] = { 61327 },	-- Spider
+					}),
+					p(379, {	-- Squirrel
+						["crs"] = { 61081 },	-- Squirrel
+					}),
+					p(525, {	-- Turkey
+						["crs"] = { 62648 },	-- Turkey
+					}),
+					p(1238, {	-- Unborn Val'kyr
+						["crs"] = { 71163 },	-- Unborn Val'kyr
+					}),
+					q(31931, {	--  Beegle Blastfuse
+						["qg"] = 66635,	-- Beegle Blastfuse
+						["repeatable"] = true,
+					}),
+				}),
+			},
+		}),
+	}),
+};
