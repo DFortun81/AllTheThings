85b5cca3659e406999f2bd5e28c01f5788fd71ce
Pr3vention <Pr3vention@gmail.com>
2019-06-04 16:05:58 -0500

    Deepholm broken up
    Added crs to pets

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/06 The Maelstrom/Deepholm/0 - Metadata.lua
@@ -0,0 +1,13 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(948, { 	-- The Maelstrom
+		m(207, {	-- Deepholm
+			["lvl"] = 82,	
+			["achievementID"] = 4864,
+			["description"] = "|cff66ccffDeepholm is one of the four regions of the Elemental Plane. It serves as both home and prison for the earth elementals, ruled by Therazane the Stonemother. Besides Therazane and her elementals, the Earthen Ring has also made Deepholm its temporary home. Stationed in the Temple of Earth, they are attempting to mend the wound Deathwing opened. In World of Warcraft: Cataclysm, Deepholm became accessible to players. It is the largest visitable level of the Elemental Plane, being a new questing, leveling and exploration zone tuned for level 82-83 players. It also holds a 5-man dungeon, the Stonecore. The two main enemy forces the player fight against are the Twilight's Hammer and a large tribe of stone troggs.|r",				
+		}),		
+	}),
+};
---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/06 The Maelstrom/Deepholm/Professions.lua
@@ -0,0 +1,27 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(948, { 	-- The Maelstrom
+		m(207, {	-- Deepholm
+			["g"] = {
+				n(-38, {	-- Profession
+					n(-188, { 	-- Mining
+						["description"] = "|cff66ccffElementium Veins (0.05% Drop Rate) and Rich Elementium Veins (0.08% Drop Rate) require Cataclysm Mining.|r",
+						["g"] = {
+							i(67282), 	-- Pet Elementium Geode
+						},
+					}),
+					prof(356, {
+						o(202778, {	-- Albino Cavefish School
+							["g"] = {
+								i(22739),	-- Tome of Polymorph: Turtle
+							},
+						}),
+					}),
+				}),		
+			},
+		}),		
+	}),
+};
