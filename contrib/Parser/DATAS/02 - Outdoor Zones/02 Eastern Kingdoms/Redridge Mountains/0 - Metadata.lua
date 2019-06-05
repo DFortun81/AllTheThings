f70a0ea26211a82e1641267a1ca17119ce41438c
Pr3vention <Pr3vention@gmail.com>
2019-06-04 19:52:48 -0500

    Redridge Mountains broken up
    Added mapID to coords, crs to pets, some excess header reduction, minor formatting

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Redridge Mountains/0 - Metadata.lua
@@ -0,0 +1,13 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(49, {	-- Redridge Mountains
+			["lvl"] = 10,	
+			["achievementID"] = 780,
+			["description"] = "Redridge Mountains is a sleepy zone that is primarily Alliance. However, threats come from the Blackrock Invasion, which is revealed to be tied to the Black Dragonflight and Deathwing. The main series of quests follows John J. Keeshan, a clear tribute to Rambo.",
+		}),
+	}),
+};
---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Redridge Mountains/Flight Paths.lua
@@ -0,0 +1,23 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(49, {	-- Redridge Mountains
+			["g"] = {
+				n(-228, {	-- Flight Path
+					fp(615, {	-- Camp Everstill, Redridge
+						["coord"] = { 52.8, 54.6, 49 },
+					}),
+					fp(5, {	-- Lakeshire, Redridge
+						["coord"] = { 29.4, 53.6, 49 },
+					}),
+					fp(596, {	-- Shalewind Canyon, Redridge
+						["coord"] = { 77.8, 65.8, 49 },
+					}),
+				}),
+			},
+		}),
+	}),
+};
