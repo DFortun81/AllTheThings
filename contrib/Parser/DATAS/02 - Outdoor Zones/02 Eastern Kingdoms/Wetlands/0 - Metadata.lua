64d7da22187f52fc4f9e474a1b095e82046f9a91
Pr3vention <Pr3vention@gmail.com>
2019-06-04 22:37:55 -0500

    Wetlands break up
    mapID added to coords, crs to pets, excess header reduction, some minor formatting adjustments to clarify zone drops

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Wetlands/0 - Metadata.lua
@@ -0,0 +1,13 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(56, {	-- Wetlands
+			["lvl"] = 20,	
+			["achievementID"] = 841,
+			["description"] = "|cff66ccffWetlands is a large swamp and primarily-Alliance area that is partially flooded after the Shattering. Quests involve trying to rebuild the flooded Menethil Harbor, assist the night elves in Greenwarden's Grove, and discover dwarven dig sites.|r",
+		}),
+	}),
+};
