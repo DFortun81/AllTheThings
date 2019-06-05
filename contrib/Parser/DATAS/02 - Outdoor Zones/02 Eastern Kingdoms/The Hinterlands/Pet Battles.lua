0d2ec87a3f8ed5fc38121b876a7bc39328b5c814
Pr3vention <Pr3vention@gmail.com>
2019-06-04 20:41:00 -0500

    The Hinterlands broken up
    Added mapID to coords, crs to pets, excess header reduction, some minor formatting

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/The Hinterlands/Pet Battles.lua
@@ -0,0 +1,36 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(26, {	-- The Hinterlands
+			["g"] = {
+				n(-25, {	-- Pet Battle
+					p(449, {	-- Brown Marmot
+						["crs"] = { 61752 },	-- Brown Marmot
+					}),
+					p(393, {	-- Cockroach
+						["crs"] = { 61384 },	-- Cockroach
+					}),
+					p(448, {	-- Hare
+						["crs"] = { 61751 },	-- Hare
+					}),
+					p(446, {	-- Jade Oozeling
+						["crs"] = { 61718 },	-- Jade Oozeling
+					}),
+					p(450, {	-- Maggot
+						["crs"] = { 61753 },	-- Maggot
+					}),
+					p(417, {	-- Rat
+						["crs"] = { 61366 },	-- Rat
+					}),
+					q(31910, {	-- David Kosse
+						["qg"] = 66478,	-- David Kosse
+						["isDaily"] = true,
+					}),
+				}),
+			},
+		}),
+	}),
+};
