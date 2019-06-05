aee22f9197d3852557d013b67e36aaa8ae2de012
Pr3vention <Pr3vention@gmail.com>
2019-06-04 19:11:50 -0500

    Duskwood broken up
    mapID added to coords, crs added to pets, excess header reduction

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Duskwood/0 - Metadata.lua
@@ -0,0 +1,13 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(47, {	-- Duskwood
+			["lvl"] = 20,
+			["achievementID"] = 778,
+			["description"] = "|cff66ccffDuskwood is a zone in Southern Eastern Kingdoms, warped and cursed due to the Scythe of Elune. Players assist the Night Watch, who protect the tenacious town of Darkshire. They also learn several chilling stories about former residents--such as Abercrombie, Jitters, and Morbent Fel.|r",
+		}),
+	}),
+};
