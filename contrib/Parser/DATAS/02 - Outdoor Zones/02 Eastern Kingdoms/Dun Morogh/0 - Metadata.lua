5517444f25c33c88ccd0250ff2c7dc4bc432c4b9
Pr3vention <Pr3vention@gmail.com>
2019-06-05 22:00:40 -0500

    Dun Morogh and related maps broken up (I hate you, gnomes)
    crs added to pets, excess header reduction

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Dun Morogh/0 - Metadata.lua
@@ -0,0 +1,20 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(27, {	-- Dun Morogh
+			["lvl"] = 1,
+			["description"] = "|cff66ccffDun Morogh is the snowy starter zone for Dwarf and Gnome in central Eastern Kingdoms. New players learn the basics of questing by fighting leper gnomes from Gnomeregan and lighter topics like safely transporting ale. This zone is reached on foot via a series of elaborate connecting tunnels not found anywhere else in Azeroth.|r",
+			["achievementID"] = 627,
+			["maps"] = {
+				28,		-- Coldridge Pass
+				29,		-- The Grizzled Den
+				31,		-- Coldridge Valley
+				428,	-- Frostmane Hovel (Coldridge Valley)
+			--	523,	-- Dun Morogh (Blood in the Snow scenario — commenting out so that random Dun Morogh quests, etc., don't show up in the scenario)
+			},
+		}),
+	}),
+};
---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Dun Morogh/Flight Paths.lua
@@ -0,0 +1,20 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(27, {	-- Dun Morogh
+			["g"] = {
+				n(-228, {	-- Flight Master
+					fp(620, {	-- Gol'Bolar Quarry, Dun Morogh
+						["coord"] = { 75.8, 54.4, 27 },
+					}),
+					fp(619, {	-- Kharanos, Dun Morogh
+						["coord"] = { 53.8, 52.6, 27 },
+					}),
+				}),
+			},
+		}),
+	}),
+};
