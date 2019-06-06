b56a9ac055df938b9eec3dbdd0aa5da870143f4c
Pr3vention <Pr3vention@gmail.com>
2019-06-05 20:21:36 -0500

    Arathi Highlands break up
    Added mapID to a few coords, crs to pets, excess header reduction, some minor formatting adjustments

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Arathi Highlands/0 - Metadata.lua
@@ -0,0 +1,13 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdoms
+		m(14, {	-- Arathi Highlands
+			["lvl"] = 25,	
+			["achievementID"] = 761,
+			["description"] = "|cff66ccffArathi Highlands is located in southeastern Lordaeron. Players can queue for Arathi Basin via Refuge Pointe and Hammerfall. Main attractions are the ancient kingdom of Stromgarde and the architectural Dwarven wonder, Thandol Span.|r",
+		}),
+	}),
+};
