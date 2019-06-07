adda86806aa0a0d7ec450a1405c2ee1ae63d31d3
slumber <slumber@love.com>
2019-06-07 01:16:24 -0500

    - forgot some!

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/02 Orgrimmar/Professions.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/02 Orgrimmar/Professions.lua
@@ -5,190 +5,184 @@ _.Zones =
 {
 	m(12, {	-- Kalimdor
 		m(85, {	-- Orgrimmar
-			["groups"] = {
-				n(-38, {	-- Profession
-					prof(356, {	-- Fishing
-						["groups"] = {
-							n(-4, {	-- Achievements
-								ach(5477),	-- Fish or Cut Bait: Orgrimmar
-							}),
-							n(-17, { 	-- Quests
-								["groups"] = {
-									q(26588, {	-- A Furious Catch
-										["qg"] = 43239,	-- Razgar
-										["coord"] = { 65.7, 41.1, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5477, crit(4)),	-- Fish or Cut Bait: Orgrimmar
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-									q(26572, {	-- A Golden Opportunity
-										["qg"] = 43239,	-- Razgar
-										["coord"] = { 65.7, 41.1, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5477, crit(5)),	-- Fish or Cut Bait: Orgrimmar
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-									q(26557, {	-- A Staggering Effort
-										["qg"] = 43239,	-- Razgar
-										["coord"] = { 65.7, 41.1, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5477, crit(3)),	-- Fish or Cut Bait: Orgrimmar
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-									q(26543, {	-- Clammy Hands
-										["qg"] = 43239,	-- Razgar
-										["coord"] = { 65.7, 41.1, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5477, crit(1)),	-- Fish or Cut Bait: Orgrimmar
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-									q(26556, {	-- No Dumping Allowed
-										["qg"] = 43239,	-- Razgar
-										["coord"] = { 65.7, 41.1, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5477, crit(2)),	-- Fish or Cut Bait: Orgrimmar
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-								},
-							}),
-							i(34486),	-- Old Crafty
-						},
+			n(-38, {	-- Profession
+				prof(356, {	-- Fishing
+					n(-4, {	-- Achievements
+						ach(5477, {	-- Fish or Cut Bait: Orgrimmar
+							["races"] = HORDE_ONLY,
+						}),
 					}),
-					prof(185, {	-- Cooking
-						["groups"] = {
-							n(-4, {	-- Achievements
-								ach(5475),	-- Let's Do Lunch: Orgrimmar
-							}),
-							n(-17, { 	-- Quests
-								["groups"] = {
-									q(26227, {	-- Careful, This Fruit Bites Back
-										["qg"] = 42506,	-- Marogg
-										["coord"] = { 56.5, 62.5, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5475, crit(1)),	-- Let's Do Lunch: Orgrimmar
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-									q(26226, {	-- Crawfish Creole
-										["qg"] = 42506,	-- Marogg
-										["coord"] = { 56.5, 62.5, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5475, crit(2)),	-- Let's Do Lunch: Orgrimmar
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-									q(26235, {	-- Even Thieves Get Hungry
-										["qg"] = 42506,	-- Marogg
-										["coord"] = { 56.5, 62.5, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5475, crit(3)),	-- Let's Do Lunch: Orgrimmar
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-									q(26220, {	-- Everything Is Better with Bacon
-										["qg"] = 42506,	-- Marogg
-										["coord"] = { 56.5, 62.5, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5475, crit(4)),	-- Let's Do Lunch: Orgrimmar
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-									q(26233, {	-- Stealing From Our Own
-										["qg"] = 42506,	-- Marogg
-										["coord"] = { 56.5, 62.5, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = {
-											2,	-- Orc
-											5,	-- Undead
-											6,	-- Tauren
-											9,	-- Goblin
-											10,	-- Blood Elf
-											26,	-- Pandaren
-											27,	-- Nightborne
-											28,	-- Highmountain Tauren
-											31,	-- Zandalari Troll
-											36,	-- Mag'har Orc
-										},
-										["groups"] = {
-											ach(5475, crit(5)),	-- Let's Do Lunch: Orgrimmar
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-									q(26234, {	-- Stealing From Our Own
-										["qg"] = 42506,	-- Marogg
-										["coord"] = { 56.5, 62.5, 85 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = { 8 },	-- Troll
-										["groups"] = {
-											ach(5475, crit(5)),	-- Let's Do Lunch: Orgrimmar
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-								},
-							}),
-						},
+					n(-17, {	-- Quests
+						q(26588, {	-- A Furious Catch
+							["qg"] = 43239,	-- Razgar
+							["coord"] = { 65.7, 41.1, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5477, crit(4)),	-- Fish or Cut Bait: Orgrimmar
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+						q(26572, {	-- A Golden Opportunity
+							["qg"] = 43239,	-- Razgar
+							["coord"] = { 65.7, 41.1, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5477, crit(5)),	-- Fish or Cut Bait: Orgrimmar
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+						q(26557, {	-- A Staggering Effort
+							["qg"] = 43239,	-- Razgar
+							["coord"] = { 65.7, 41.1, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5477, crit(3)),	-- Fish or Cut Bait: Orgrimmar
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+						q(26543, {	-- Clammy Hands
+							["qg"] = 43239,	-- Razgar
+							["coord"] = { 65.7, 41.1, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5477, crit(1)),	-- Fish or Cut Bait: Orgrimmar
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+						q(26556, {	-- No Dumping Allowed
+							["qg"] = 43239,	-- Razgar
+							["coord"] = { 65.7, 41.1, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5477, crit(2)),	-- Fish or Cut Bait: Orgrimmar
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+					}),
+					i(34486),	-- Old Crafty
+				}),
+				prof(185, {	-- Cooking
+					n(-4, {	-- Achievements
+						ach(5475, {	-- Let's Do Lunch: Orgrimmar
+							["races"] = HORDE_ONLY,
+						}),
+					}),
+					n(-17, {	-- Quests
+						q(26227, {	-- Careful, This Fruit Bites Back
+							["qg"] = 42506,	-- Marogg
+							["coord"] = { 56.5, 62.5, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5475, crit(1)),	-- Let's Do Lunch: Orgrimmar
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+						q(26226, {	-- Crawfish Creole
+							["qg"] = 42506,	-- Marogg
+							["coord"] = { 56.5, 62.5, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5475, crit(2)),	-- Let's Do Lunch: Orgrimmar
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+						q(26235, {	-- Even Thieves Get Hungry
+							["qg"] = 42506,	-- Marogg
+							["coord"] = { 56.5, 62.5, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5475, crit(3)),	-- Let's Do Lunch: Orgrimmar
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+						q(26220, {	-- Everything Is Better with Bacon
+							["qg"] = 42506,	-- Marogg
+							["coord"] = { 56.5, 62.5, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5475, crit(4)),	-- Let's Do Lunch: Orgrimmar
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+						q(26233, {	-- Stealing From Our Own
+							["qg"] = 42506,	-- Marogg
+							["coord"] = { 56.5, 62.5, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = {
+								2,	-- Orc
+								5,	-- Undead
+								6,	-- Tauren
+								9,	-- Goblin
+								10,	-- Blood Elf
+								26,	-- Pandaren
+								27,	-- Nightborne
+								28,	-- Highmountain Tauren
+								31,	-- Zandalari Troll
+								36,	-- Mag'har Orc
+							},
+							["groups"] = {
+								ach(5475, crit(5)),	-- Let's Do Lunch: Orgrimmar
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+						q(26234, {	-- Stealing From Our Own
+							["qg"] = 42506,	-- Marogg
+							["coord"] = { 56.5, 62.5, 85 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = { 8 },	-- Troll
+							["groups"] = {
+								ach(5475, crit(5)),	-- Let's Do Lunch: Orgrimmar
+								currency(81),	-- Epicurean's Award
+							},
+						}),
 					}),
 				}),
-			},
+			}),
 		}),
 	}),
 };
--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/04 Thunder Bluff/Professions.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/04 Thunder Bluff/Professions.lua
@@ -5,171 +5,165 @@ _.Zones =
 {
 	m(12, {	-- Kalimdor
 		m(88, {	-- Thunder Bluff
-			["groups"] = {
-				n(-38, {	-- Professions
-					prof(356, {	-- Fishing
-						["groups"] = {
-							n(-4, {	-- Achievements
-								ach(5849),	-- Fish or Cut Bait: Thunder Bluff
-							}),
-							n(-17, { 	-- Quests
-								["groups"] = {
-									q(29349, {	-- Craving Crayfish
-										["qg"] = 3028,	-- Kah Mistrunner
-										["coord"] = { 56.12, 46.34, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5849, crit(4)),	-- Fish or Cut Bait: Thunder Bluff
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-									q(29345, {	-- Pond Predators
-										["qg"] = 3028,	-- Kah Mistrunner
-										["coord"] = { 56.12, 46.34, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5849, crit(1)),	-- Fish or Cut Bait: Thunder Bluff
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-									q(29354, {	-- Shiny Baubles
-										["qg"] = 3028,	-- Kah Mistrunner
-										["coord"] = { 56.12, 46.34, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5849, crit(5)),	-- Fish or Cut Bait: Thunder Bluff
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-									q(29348, {	-- The Race to Restock
-										["qg"] = 3028,	-- Kah Mistrunner
-										["coord"] = { 56.12, 46.34, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5849, crit(3)),	-- Fish or Cut Bait: Thunder Bluff
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-									q(29346, {	-- The Ring's the Thing
-										["qg"] = 3028,	-- Kah Mistrunner
-										["coord"] = { 56.12, 46.34, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 356,	-- Fishing
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5849, crit(2)),	-- Fish or Cut Bait: Thunder Bluff
-											i(67414, {	-- Bag of Shiny Things
-												i(44983),	-- Strand Crawler
-												i(33820),	-- Weather-Beaten Fishing Hat
-												i(45991),	-- Bone Fishing Pole
-												i(45992),	-- Jeweled Fishing Pole
-											}),
-										},
-									}),
-								},
-							}),
-							i(122214, {	-- Music Roll: Mulgore Plains
-								["description"] = "Go fishing for a bit in the Pools of Vision beneath the Spirit Rise.",
-								["coord"] = { 28.61, 24.61, 88 },
-							}),
-						},
+			n(-38, {	-- Professions
+				prof(356, {	-- Fishing
+					n(-4, {	-- Achievements
+						ach(5849, {	-- Fish or Cut Bait: Thunder Bluff
+							["races"] = HORDE_ONLY,
+						}),
 					}),
-					prof(185, {	-- Cooking
-						["groups"] = {
-							n(-4, {	-- Achievements
-								ach(5843),	-- Let's Do Lunch: Thunder Bluff
-							}),
-							n(-17, { 	-- Quests
-								["groups"] = {
-									q(29364, {	-- Corn Mash
-										["qg"] = 3026,	-- Aska Mistrunner
-										["coord"] = { 51.2, 52.2, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5843, crit(4)),	-- Let's Do Lunch: Thunder Bluff
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-									q(29362, {	-- "Magic" Mushrooms
-										["qg"] = 3026,	-- Aska Mistrunner
-										["coord"] = { 51.2, 52.2, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5843, crit(2)),	-- Let's Do Lunch: Thunder Bluff
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-									q(29363, {	-- Mulgore Spice Bread
-										["qg"] = 3026,	-- Aska Mistrunner
-										["coord"] = { 51.2, 52.2, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5843, crit(3)),	-- Let's Do Lunch: Thunder Bluff
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-									q(29365, {	-- Perfectly Picked Portions
-										["qg"] = 3026,	-- Aska Mistrunner
-										["coord"] = { 51.2, 52.2, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5843, crit(3)),	-- Let's Do Lunch: Thunder Bluff
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-									q(29358, {	-- Pining for Nuts
-										["qg"] = 3026,	-- Aska Mistrunner
-										["coord"] = { 51.2, 52.2, 88 },
-										["isDaily"] = true,
-										["requireSkill"] = 185,	-- Cooking
-										["races"] = HORDE_ONLY,
-										["groups"] = {
-											ach(5843, crit(1)),	-- Let's Do Lunch: Thunder Bluff
-											currency(81),	-- Epicurean's Award
-										},
-									}),
-								},
-							}),
-						},
+					n(-17, { 	-- Quests
+						q(29349, {	-- Craving Crayfish
+							["qg"] = 3028,	-- Kah Mistrunner
+							["coord"] = { 56.12, 46.34, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5849, crit(4)),	-- Fish or Cut Bait: Thunder Bluff
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+						q(29345, {	-- Pond Predators
+							["qg"] = 3028,	-- Kah Mistrunner
+							["coord"] = { 56.12, 46.34, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5849, crit(1)),	-- Fish or Cut Bait: Thunder Bluff
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+						q(29354, {	-- Shiny Baubles
+							["qg"] = 3028,	-- Kah Mistrunner
+							["coord"] = { 56.12, 46.34, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5849, crit(5)),	-- Fish or Cut Bait: Thunder Bluff
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+						q(29348, {	-- The Race to Restock
+							["qg"] = 3028,	-- Kah Mistrunner
+							["coord"] = { 56.12, 46.34, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5849, crit(3)),	-- Fish or Cut Bait: Thunder Bluff
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+						q(29346, {	-- The Ring's the Thing
+							["qg"] = 3028,	-- Kah Mistrunner
+							["coord"] = { 56.12, 46.34, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 356,	-- Fishing
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5849, crit(2)),	-- Fish or Cut Bait: Thunder Bluff
+								i(67414, {	-- Bag of Shiny Things
+									i(44983),	-- Strand Crawler
+									i(33820),	-- Weather-Beaten Fishing Hat
+									i(45991),	-- Bone Fishing Pole
+									i(45992),	-- Jeweled Fishing Pole
+								}),
+							},
+						}),
+					}),
+					i(122214, {	-- Music Roll: Mulgore Plains
+						["description"] = "Go fishing for a bit in the Pools of Vision beneath the Spirit Rise.",
+						["coord"] = { 28.61, 24.61, 88 },
 					}),
 				}),
-			},
+				prof(185, {	-- Cooking
+					n(-4, {	-- Achievements
+						ach(5843, {	-- Let's Do Lunch: Thunder Bluff
+							["races"] = HORDE_ONLY,
+						}),
+					}),
+					n(-17, { 	-- Quests
+						q(29364, {	-- Corn Mash
+							["qg"] = 3026,	-- Aska Mistrunner
+							["coord"] = { 51.2, 52.2, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5843, crit(4)),	-- Let's Do Lunch: Thunder Bluff
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+						q(29362, {	-- "Magic" Mushrooms
+							["qg"] = 3026,	-- Aska Mistrunner
+							["coord"] = { 51.2, 52.2, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5843, crit(2)),	-- Let's Do Lunch: Thunder Bluff
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+						q(29363, {	-- Mulgore Spice Bread
+							["qg"] = 3026,	-- Aska Mistrunner
+							["coord"] = { 51.2, 52.2, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5843, crit(3)),	-- Let's Do Lunch: Thunder Bluff
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+						q(29365, {	-- Perfectly Picked Portions
+							["qg"] = 3026,	-- Aska Mistrunner
+							["coord"] = { 51.2, 52.2, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5843, crit(3)),	-- Let's Do Lunch: Thunder Bluff
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+						q(29358, {	-- Pining for Nuts
+							["qg"] = 3026,	-- Aska Mistrunner
+							["coord"] = { 51.2, 52.2, 88 },
+							["isDaily"] = true,
+							["requireSkill"] = 185,	-- Cooking
+							["races"] = HORDE_ONLY,
+							["groups"] = {
+								ach(5843, crit(1)),	-- Let's Do Lunch: Thunder Bluff
+								currency(81),	-- Epicurean's Award
+							},
+						}),
+					}),
+				}),
+			}),
 		}),
 	}),
 };
