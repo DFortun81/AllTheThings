67482a7ef1f05e8a14eae83191213e839a226725
slumber <slumber@love.com>
2019-06-20 02:15:09 -0500

    - marked a couple flight paths as horde-only

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Silithus/Flight Paths.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Silithus/Flight Paths.lua
@@ -9,6 +9,7 @@ _.Zones =
 				fp(72, {	-- Cenarion Hold, Silithus [Horde]
 					["description"] = "Cenarion Hold, Silithus - Horde Only \n\nSpeak to Zidormi to visit the past to access this point.",
 					["coord"] = { 52.8, 34.6, 81 },
+					["races"] = HORDE_ONLY,
 				}),
 				fp(73, {	-- Cenarion Hold, Silithus [Alliance]
 					["description"] = "Cenarion Hold, Silithus - Alliance Only \n\nSpeak to Zidormi to visit the past to access this point.",
