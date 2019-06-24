3a60e3c2dafb4741d973e4547c7026b64c743a67
Pr3vention <Pr3vention@gmail.com>
2019-06-23 19:59:30 -0500

    Coldridge Valley cleaned up
    Added all coords and sourceQuests. Marked unavailable quests accordingly.

--- contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Dun Morogh/0 - Coldridge Valley/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Dun Morogh/0 - Coldridge Valley/Quests.lua
@@ -7,100 +7,161 @@ _.Zones =
 		m(27, {	-- Dun Morogh
 			m(427,  {	-- Coldridge Valley
 				n(-17, {	-- Quests
-					q(3361, {	-- A Refugee's Quandary
-						["qg"] = 8416,	-- 
+					q(3361,  {	-- A Refugee's Quandary
+						["qg"] = 8416,	-- Felix Whindlebolt
+						["coord"] = { 41.8, 63.7, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24487,	-- Whitebeard Needs Ye
 					}),
 					q(24490, {	-- A Trip to Ironforge
-						["qg"] = 786,	-- 
+						["qg"] = 786,	-- Grelin Whitebeard
+						["coord"] = { 42.7, 62.1, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 218,	-- Ice and Fire
+					}),
+					q(24471, {	-- Aid for the Wounded
+						["qg"] = 658,	-- Sten Stoutarm
+						["coord"] = { 65.5, 41.9, 427 },
+						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24469,	-- Hold the Line!
 					}),
 					q(24475, {	-- All the Other Stuff
-						["qg"] = 37087,	-- 
+						["qg"] = 37087,	-- Jona Ironstock
+						["coord"] = { 60.6, 21.0, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24474,	-- First Things First: We're Gonna Need Some Beer
 					}),
 					q(24496, {	-- Arcane Rune
-						["qg"] = 37087,	-- 
+						["u"] = 40,
+						["qg"] = 37087,	-- Jona Ironstock
+						["coord"] = { 61.4, 19.4, 427 },
 						["races"] = { 3 },	-- Dwarf
-						["classes"] = { 8 },
+						["classes"] = { 8 },	-- Mage
 					}),
 					q(24493, {	-- Don't Forget About Us
-						["qg"] = 37087,	-- 
+						["qg"] = 37087,	-- Jona Ironstock
+						["coord"] = { 61.0, 19.7, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["description"] = "Only available during |cFFFFD700Pack Your Bags|r.",
+						["sourceQuest"] = 24491,	-- Follow that Gyro-Copter!
+						["isBreadcrumb"] = true,
 					}),
 					q(24477, {	-- Dwarven Artifacts
-						["qg"] = 1104,	-- 
+						["qg"] = 1104,	-- Grundel Harkin
+						["coord"] = { 61.6, 22.0, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24473,	-- Lockdown in Anvilmar
 					}),
 					q(31150, {	-- Elegant Rune
-						["qg"] = 37087,	-- 
+						["u"] = 40,
+						["qg"] = 37087,	-- Jona Ironstock
+						["coord"] = { 61.4, 19.4, 427 },
 						["races"] = { 3 },	-- Dwarf
 						["classes"] = { 10 },	-- Monk
 					}),
-					q(3109, {	-- Encrypted Rune
-						["qg"] = 37087,	-- 
+					q(3109,  {	-- Encrypted Rune
+						["u"] = 40,
+						["qg"] = 37087,	-- Jona Ironstock
+						["coord"] = { 61.4, 19.4, 427 },
 						["races"] = { 3, 7 },	-- Dwarf, Gnome
 						["classes"] = { 4 },	-- Rogue
 					}),
 					q(24532, {	-- Evisceratin' the Enemy
-						["qg"] = 916,	-- 
+						["u"] = 40,
+						["qg"] = 916,	-- Solm Hargin
+						["coord"] = { 59.5, 20.4, 427 },
 						["races"] = { 3 },	-- Dwarf
 						["classes"] = { 4 },	-- Rogue
+						["sourceQuest"] = 3109,	-- Encrypted Rune
 					}),
 					q(24474, {	-- First Things First: We're Gonna Need Some Beer
-						["qg"] = 37087,	-- 
+						["qg"] = 37087,	-- Jona Ironstock
+						["coord"] = { 62.7, 21.4, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24473,	-- Lockdown in Anvilmar
 					}),
 					q(24491, {	-- Follow that Gyro-Copter!
-						["qg"] = 6782,	-- 
+						["qg"] = 6782,	-- Hands Springsprocket
+						["coord"] = { 87.5, 44.4, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24490,	-- A Trip to Ironforge
 					}),
 					q(24470, {	-- Give 'em What-For
-						["qg"] = 37081,	-- 
+						["qg"] = 37081,	-- Joren Ironstock
+						["coord"] = { 67.1, 41.2, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24469,	-- Hold the Line!
 					}),
 					q(24469, {	-- Hold the Line!
-						["qg"] = 37081,	-- 
+						["qg"] = 37081,	-- Joren Ironstock
+						["coord"] = { 67.1, 41.2, 427 },
 						["races"] = ALLIANCE_ONLY,
 					}),
-					q(218, {	-- Ice and Fire
-						["qg"] = 786,	-- 
+					q(218,   {	-- Ice and Fire
+						["qg"] = 786,	-- Grelin Whitebeard
+						["coord"] = { 42.7, 62.2, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuests"] = {
+							182,	-- The Troll Menace
+							24489,	-- Trolling for Information
+						},
 					}),
 					q(31151, {	-- Kick, Punch, It's All in the Mind
-						["qg"] = 63285,	-- 
+						["u"] = 40,
+						["qg"] = 63285,	-- Lo
+						["coord"] = { 63.2, 22.3, 427 },
 						["races"] = ALLIANCE_ONLY,
-						["classes"] = {10},	-- Monk
+						["classes"] = { 10 },	-- Monk
 					}),
 					q(24473, {	-- Lockdown in Anvilmar
-						["qg"] = 37081,	-- 
+						["qg"] = 37081,	-- Joren Ironstock
+						["coord"] = { 67.1, 41.3, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuests"] = {
+							24471,	-- Aid for the Wounded
+							24470,	-- Give 'em What-For
+						},
 					}),
 					q(24486, {	-- Make Hay While the Sun Shines
-						["qg"] = 1104,	-- 
+						["qg"] = 1104,	-- Grundel Harkin
+						["coord"] = { 61.7, 22.0, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24477,	-- Dwarven Artifacts
 					}),
 					q(24492, {	-- Pack Your Bags
 						["qg"] = 37113,	-- Milo Geartwinge
+						["coord"] = { 69.8, 43.9, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24491,	-- Follow that Gyro-Copter!
 						["g"] = {
-							i(57542),	-- Coldridge Mountaineer's Pouch
+							i(57540),	-- Coldridge Mountaineer's Pouch
 						},
 					}),
-					q(182, {	-- The Troll Menace
-						["qg"] = 786,	-- 
+					q(182,   {	-- The Troll Menace
+						["qg"] = 786,	-- Grelin Whitebeard
+						["coord"] = { 42.7, 62.2, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24487,	-- Whitebeard Needs Ye
 					}),
 					q(24489, {	-- Trolling for Information
-						["qg"] = 1354,	-- 
+						["qg"] = 1354,	-- Apprentice Soren
+						["coord"] = { 43.2, 63.1, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuest"] = 24487,	-- Whitebeard Needs Ye
 					}),
 					q(24487, {	-- Whitebeard Needs Ye
-						["qg"] = 37087,	-- 
+						["qg"] = 37087,	-- Jona Ironstock
+						["coord"] = { 60.6, 21.1, 427 },
 						["races"] = ALLIANCE_ONLY,
+						["sourceQuests"] = {
+							24475,	-- All the Other Stuff
+							24486,	-- Make Hay While the Sun Shines
+						},
 					}),
 					q(24533, {	-- Words of Power
-						["qg"] = 837,	-- 
+						["u"] = 40,
+						["qg"] = 837,	-- Branstock Khalder
+						["coord"] = { 60.7, 14.6, 427 },
 						["races"] = { 3 },	-- Dwarf
 						["classes"] = { 5 },	-- Priest
 					}),
