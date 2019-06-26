e4e1d85e052b5d63760c3c88a5e3d74e8e52b84f
Gold-DieAldor <goldenshacal@gmail.com>
2019-06-26 21:00:22 +0200

    1 item added

--- contrib/Parser/DATAS/09 - PvP/World.lua
+++ contrib/Parser/DATAS/09 - PvP/World.lua
@@ -7,6 +7,7 @@ _.PVP =
 		["groups"] = {
 			n(-243,	{	-- Bounty (for later). Contains every aspirant gear as well
 				i(163825),	-- Plundered Supplies
+				i(170539),	-- Plunderer Supplies (Nazjartar and maybe Mechagnom)
 			}),
 			n(-488,	{	-- War Chest
 				["description"] = "Contains Dread Aspirant Gear if you are Level 110-119 and Sinister Aspirant Gear at Level 120.",
