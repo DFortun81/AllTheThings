3182e348dfe3287952b751e3d7735feb18a00886
mogwai316 <bryan.harden@gmail.com>
2019-06-26 19:34:02 -0400

    Gnome and Tauren heritage armor sets added

---
+++ contrib/Parser/DATAS/12 - Item Sets/Heritage Armor/Heritage of Gnomeregan.lua
@@ -0,0 +1,26 @@
+-----------------------------------------------------
+--       G E A R     S E T S     M O D U L E       --
+-----------------------------------------------------
+_.GearSets = 
+{
+	{	-- Heritage Armor
+		["npcID"] = -254,	-- Heritage Armor
+		["g"] = {
+			gs(1828, {	-- Heritage of Gnomeregan
+				["groups"] = {
+					i(168286),	-- G.E.A.R. Commander's Buckle
+					i(168284),	-- G.E.A.R. Commander's Chestpiece
+					i(168290),	-- G.E.A.R. Commander's Cloak
+					i(168282),	-- G.E.A.R. Commander's Goggles
+					i(168285),	-- G.E.A.R. Commander's Handgrips
+					i(168287),	-- G.E.A.R. Commander's Legguards
+					i(168283),	-- G.E.A.R. Commander's Shoulderguards
+					i(168288),	-- G.E.A.R. Commander's Stompers
+					i(168289),	-- G.E.A.R. Commander's Wristbands
+					i(168862),	-- G.E.A.R. Tracking Beacon (not collectible, just here so it's not in Unsorted)
+				},
+				["races"] = { 7 },	-- Gnome
+			}),
+		},
+	},
+};
---
+++ contrib/Parser/DATAS/12 - Item Sets/Heritage Armor/Heritage of the Shu'halo.lua
@@ -0,0 +1,26 @@
+-----------------------------------------------------
+--       G E A R     S E T S     M O D U L E       --
+-----------------------------------------------------
+_.GearSets = 
+{
+	{	-- Heritage Armor
+		["npcID"] = -254,	-- Heritage Armor
+		["g"] = {
+			gs(1829, {	-- Heritage of the Shu'halo
+				["groups"] = {
+					i(168298),	-- Ancestral Chieftain's Armor
+					i(168297),	-- Ancestral Chieftain's Grasps
+					i(168296),	-- Ancestral Chieftain's Greatbelt
+					i(168291),	-- Ancestral Chieftain's Headdress
+					i(168292),	-- Ancestral Chieftain's Hoofbands
+					i(168293),	-- Ancestral Chieftain's Loincloth
+					i(168294),	-- Ancestral Chieftain's Mantle
+					i(170063),	-- Ancestral Chieftain's Totem
+					i(168295),	-- Ancestral Chieftain's Wristbands
+					i(167860),	-- Ancient Tauren Talisman (not collectible, just here so it's not in Unsorted)
+				},
+				["races"] = { 6 },	-- Tauren
+			}),
+		},
+	},
+};
