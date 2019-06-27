3263c3d0df02aa10150f47ffc4f1712dedeb20ce
Gaulish <l2pwncomputers@hotmail.com>
2019-06-27 10:36:06 -0600

    Added daily questid's to Mechagon rares

--- contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/05 Mechagon/Rares.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/05 Mechagon/Rares.lua
@@ -6,7 +6,7 @@ _.Zones =
 	m(876, {	-- Kul'Tiras
 		m(1462, {	-- Mechagon
 			n(-16,  {	-- Rares
-				-- TODO:: Verify all kill questIDs
+				-- TODO:: Verify all kill questIDs(Most of these are done by Gaulish 6/27/19 Remaining Dopple Ganger, OOX-Avenger/MG, Sparkqueen P'Emp
 				n(151934, {	-- Arachnoid Harvester
 					["questID"] = 55512,
 					["coord"] = { 52.6, 41.0, 1462 },
@@ -21,8 +21,8 @@ _.Zones =
 						}),
 					},
 				}),
-				n(150394, {	-- Armored Vaultbot
-					--["questID"] = ,
+				n(150394, {	-- Armored Vaultbot	-- possibly 154968
+					["questID"] = 55546,
 					["isDaily"] = true,
 					["description"] = "Kite it to the large magnet at |cFFFFD700Bondo's Scrapyard|r to make it vulnerable to kill it.",
 					["coords"] = {
@@ -53,6 +53,7 @@ _.Zones =
 					},
 				}),
 				n(151308, {	-- Boggac Skullbash
+					["questID"] = 55539,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (Boggac Skullbash)
@@ -63,7 +64,8 @@ _.Zones =
 				}),
 				n(152001, {	-- Bonepicker
 					["allianceQuestID"] = 55537,
-					["hordeQuestID"] = 55919,
+					--["hordeQuestID"] = 55919,
+					["hordeQuestID"] = 55537,
 					["coord"] = { 65.8, 22.9, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -75,6 +77,7 @@ _.Zones =
 					},
 				}),
 				n(154739, {	-- Caustic Mechaslime
+					["questID"] = 56368,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (Caustic Mechaslime)
@@ -84,7 +87,8 @@ _.Zones =
 					},
 				}),
 				n(152569, {	-- Crazed Trogg
-					["allianceQuestID"] = 55927,
+					["allianceQuestID"] = 55812,
+					--["allianceQuestID"] = 55927,
 					["hordeQuestID"] = 55812,
 					["coord"] = { 82.3, 21.0, 1462 },
 					["isDaily"] = true,
@@ -99,7 +103,8 @@ _.Zones =
 				}),
 				n(151569, {	-- Deepwater Maw
 					["allianceQuestID"] = 55514,
-					["hordeQuestID"] = 55917,
+					--["hordeQuestID"] = 55917,
+					["hordeQuestID"] = 55514,
 					["coord"] = { 35.3, 43.0, 1462 },
 					["isDaily"] = true,
 					["description"] = "Must complete the |cFFFFD7000Let's Fish!|r questline to spawn him.",
@@ -121,6 +126,7 @@ _.Zones =
 					},
 				}),
 				n(150342, {	-- Earthbreaker Gulroc
+					["questID"] = 55814,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (Earthbreaker Gulroc)
@@ -132,7 +138,8 @@ _.Zones =
 				}),
 				n(154153, {	-- Enforcer KX-T57
 					["allianceQuestID"] = 56207,
-					["hordeQuestID"] = 56994,
+					--["hordeQuestID"] = 56994,
+					["hordeQuestID"] = 56207,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (Enforcer KX-T57)
@@ -145,7 +152,8 @@ _.Zones =
 				}),
 				n(151202, {	-- Foul Manifestation
 					["allianceQuestID"] = 55513,
-					["hordeQuestID"] = 55916,
+					--["hordeQuestID"] = 55916,
+					["hordeQuestID"] = 55513,
 					["coord"] = { 65.7, 51.7, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -160,6 +168,7 @@ _.Zones =
 					},
 				}),
 				n(151884, {	-- Fungarian Furor
+					["questID"] = 55367,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (Fungarian Furor)
@@ -171,6 +180,7 @@ _.Zones =
 					},
 				}),
 				n(153228, {	-- Gear Checker Cogstar -- possibly 154184?
+					["questID"] = 55852,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (Gear Checker Cogstar)
@@ -181,6 +191,7 @@ _.Zones =
 					},
 				}),
 				n(153205, {	-- Gemicide
+					["questID"] = 55855,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (Gemicide)
@@ -190,6 +201,7 @@ _.Zones =
 					},
 				}),
 				n(154701, {	-- Gorged Gear-Cruncher
+					["questID"] = 56367,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (Gorged Gear-Cruncher)
@@ -201,7 +213,8 @@ _.Zones =
 				}),
 				n(151684, {	-- Jawbreaker
 					["allianceQuestID"] = 55399,
-					["hordeQuestID"] = 55910, -- 56089 also popped. Need to verify which one applies here
+					--["hordeQuestID"] = 55910, -- 56089 also popped.
+					["hordeQuestID"] = 55399,
 					["coord"] = { 77.3, 44.8, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -218,7 +231,8 @@ _.Zones =
 				}),
 				n(152007, {	-- Killsaw
 					["allianceQuestID"] = 55369,
-					["hordeQuestID"] = 55914, -- 56506 also triggered. verify on next kill
+					--["hordeQuestID"] = 55914, -- 56506 also triggered. verify on next kill
+					["hordeQuestID"] = 55369,
 					["coord"] = { 42.6, 48.7, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -233,7 +247,8 @@ _.Zones =
 				}),
 				n(151933, {	-- Malfunctioning Beastbot
 					["allianceQuestID"] = 55544,
-					["hordeQuestID"] = 55926,
+					--["hordeQuestID"] = 55926,
+					["hordeQuestID"] = 55544,
 					["coord"] = { 60.7, 42.2, 1462 },
 					["isDaily"] = true,
 					["description"] = "Requires a |cFFFFD700Beastbot Powerpack|r.",
@@ -246,6 +261,7 @@ _.Zones =
 					},
 				}),
 				n(151124, {	-- Mechagonian Nullifier
+					["questID"] = 55207,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest In Pistons (Mechagonian Nullifier)
@@ -257,7 +273,8 @@ _.Zones =
 					},
 				}),
 				n(151672, {	-- Mecharantula
-					["allianceQuestID"] = 55909,
+					["allianceQuestID"] = 55386,
+					--["allianceQuestID"] = 55909,
 					["hordeQuestID"] = 55386,
 					["coord"] = { 88.3, 20.6, 1462 },
 					["isDaily"] = true,
@@ -272,7 +289,8 @@ _.Zones =
 				}),
 				n(151627, {	-- Mr. Fixthis
 					["allianceQuestID"] = 55859,
-					["hordeQuestID"] = 55935,
+					--["hordeQuestID"] = 55935,
+					["hordeQuestID"] = 55859,
 					["coord"] = { 61.0, 61.4, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -288,7 +306,8 @@ _.Zones =
 				}),
 				n(153206, {	-- Ol' Big Tusk
 					["allianceQuestID"] = 55853,
-					["hordeQuestID"] = 55928,
+					--["hordeQuestID"] = 55928,
+					["hordeQuestID"] = 55853,
 					["coord"] = { 55.6, 39.5, 1462 },
 					["isDaily"] = true,
 					["description"] = "Must activate the |cFFFFD700Drill Rig|r.",
@@ -311,6 +330,7 @@ _.Zones =
 					},
 				}),
 				n(152764, {	-- Oxidized Leachbeast
+					["questID"] = 55856,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (Oxidized Leachbeast)
@@ -322,7 +342,8 @@ _.Zones =
 				}),
 				n(151702, {	-- Paol Pondwader
 					["allianceQuestID"] = 55405,
-					["hordeQuestID"] = 55911,
+					--["hordeQuestID"] = 55911,
+					["hordeQuestID"] = 55405,
 					["coord"] = { 23.0, 68.4, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -335,7 +356,8 @@ _.Zones =
 				}),
 				n(150575, {	-- Rumblerocks
 					["allianceQuestID"] = 55368,
-					["hordeQuestID"] = 55912,
+					--["hordeQuestID"] = 55912,
+					["hordeQuestID"] = 55368,
 					["coord"] = { 39.9, 53.2, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -348,7 +370,8 @@ _.Zones =
 				}),
 				n(152182, {	-- Rustfeather
 					["allianceQuestID"] = 55811,
-					["hordeQuestID"] = 55924,
+					--["hordeQuestID"] = 55924,
+					["hordeQuestID"] = 55811,
 					["coord"] = { 65.6, 78.3, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -360,8 +383,9 @@ _.Zones =
 					},
 				}),
 				n(155583, {	-- Scrapclaw
-					["allianceQuestID"] = 55907,
-					["hordeQuestID"] = 56307,
+					["questID"] = 56737,
+					--["allianceQuestID"] = 55907,
+					--["hordeQuestID"] = 56307,
 					["coord"] = { 82.3, 77.8, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -375,7 +399,8 @@ _.Zones =
 				}),
 				n(150937, {	-- Seaspit
 					["allianceQuestID"] = 55545,
-					["hordeQuestID"] = 55922,
+					--["hordeQuestID"] = 55922,
+					["hordeQuestID"] = 55545,
 					["coord"] = { 19.3, 80.4, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -399,7 +424,8 @@ _.Zones =
 				}),
 				n(153226, {	-- Steel Singer Freza
 					["allianceQuestID"] = 55854,
-					["hordeQuestID"] = 55933,
+					--["hordeQuestID"] = 55933,
+					["hordeQuestID"] = 55854,
 					["coord"] = { 25.1, 77.4, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -413,6 +439,7 @@ _.Zones =
 					},
 				}),
 				n(152113, {	-- The Kleptoboss
+					["questID"] = 55858,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (The Kleptoboss)
@@ -423,6 +450,7 @@ _.Zones =
 					},
 				}),
 				n(154225, {	-- The Rusty Prince
+					["questID"] = 56182,
 					["isDaily"] = true,
 					["g"] = {
 						{	-- Rest in Pistons (The Rusty Prince)
@@ -432,7 +460,8 @@ _.Zones =
 					},
 				}),
 				n(151625, {	-- The Scrap King
-					["allianceQuestID"] = 55908,
+					["allianceQuestID"] = 55364,
+					--["allianceQuestID"] = 55908,
 					["hordeQuestID"] = 55364,
 					["coord"] = { 72.3, 49.8, 1462 },
 					["isDaily"] = true,
@@ -448,7 +477,8 @@ _.Zones =
 				}),
 				n(151940, {	-- Uncle T'Rogg
 					["allianceQuestID"] = 55538, -- 56333 also triggered (alliance)
-					["hordeQuestID"] = 55921,
+					--["hordeQuestID"] = 55921,
+					["hordeQuestID"] = 55538,
 					["coord"] = { 57.3, 20.7, 1462 },
 					["isDaily"] = true,
 					["g"] = {
@@ -457,15 +487,6 @@ _.Zones =
 							["criteriaID"] = 15,
 						},
 						i(168749),	-- Performant Effective Logic Board
-					},
-				}),
-				n(150394, {	-- Vaultbot -- possibly 154968
-					["isDaily"] = true,
-					["g"] = {
-						{	-- Rest in Pistons (Vaultbot)
-							["achievementID"] = 13470,
-							["criteriaID"] = 30,
-						},
 					},
 				}),
 			}),
