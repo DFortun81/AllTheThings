eaf08aba99eb444b4210de4df0c265844552aa5e
sadidorf <sadidorf@gmail.com>
2019-06-28 03:22:33 +0200

    Reworked the Bloodelf Heritage Armor Questline

--- contrib/Parser/DATAS/12 - Item Sets/Heritage Armor/Heritage of the Shu'halo.lua
+++ contrib/Parser/DATAS/12 - Item Sets/Heritage Armor/Heritage of the Shu'halo.lua
@@ -5,104 +5,101 @@ _.GearSets =
 {
 	n(-254, {	-- Heritage Armor
 		gs(1829, {	-- Heritage of the Shu'halo
-			["g"] = {
-				i(168298),	-- Ancestral Chieftain's Armor
-				i(168297),	-- Ancestral Chieftain's Grasps
-				i(168296),	-- Ancestral Chieftain's Greatbelt
-				i(168291),	-- Ancestral Chieftain's Headdress
-				i(168292),	-- Ancestral Chieftain's Hoofbands
-				i(168293),	-- Ancestral Chieftain's Loincloth
-				i(168294),	-- Ancestral Chieftain's Mantle
-				i(170063),	-- Ancestral Chieftain's Totem
-				i(168295),	-- Ancestral Chieftain's Wristbands
-				i(167860),	-- Ancient Tauren Talisman (not collectible, just here so it's not in Unsorted)
-			},
-			["races"] = { 6 },	-- Tauren
-		}),
-		n(-17, {	-- Quests
-			q(54759, {	-- When Spririts Whisper	
-				["qg"] = 149088,	-- Spiritwalker Isahi
-				["coord"] = { 39.1, 79.0, 85 },
-				["sourceQuest"] = 55779,	-- Stay of Execution (after the Baine Rescue Scenario)
-				["races"] = { 6 },	-- Tauren	
-				["minReputation"] = EXALTED,
-				["maps"] = { 85 },	-- Orgrimmar
-				["lvl"] = 120,
-			}),
-			q(54760, {	-- The Spiritwalkers
-				["qg"] = 36648,	-- Baine Bloodhoof <High Chieftain>
-				["coord"] = { 60.3, 51.7, 88 },
-				["sourceQuest"] = 54759,	-- When Spririts Whisper	
-				["races"] = { 6 },	-- Tauren
-				["minReputation"] = EXALTED,
-				["maps"] = { 88 },	-- Thunder Bluff
-				["lvl"] = 120,
-			}),
-			q(54761, {	-- Spirit Guide
-				["qg"] = 149084,	-- Spiritwalker Ussoh
-				["coord"] = { 12.3, 31.3, 462 },
-				["sourceQuest"] = 54760,	-- The Spiritwalkers	
-				["races"] = { 6 },	-- Tauren
-				["minReputation"] = EXALTED,
-				["maps"] = { 462 },	-- Camp Narache
-				["lvl"] = 120,
-			}),
-			q(54762, {	-- A Small Retreat
-				["qg"] = 149084,	-- Spiritwalker Ussoh
-				["coord"] = { 12.3, 31.3, 462 },
-				["sourceQuest"] = 54761,	-- Spirit Guide	
-				["races"] = { 6 },	-- Tauren
-				["minReputation"] = EXALTED,
-				["maps"] = { 462 },	-- Camp Narache
-				["lvl"] = 120,
-			}),
-			q(54763, {	-- Crossing Over
-				["qg"] = 149529,	-- Spiritwalker Ussoh
-				["coord"] = { 49.2, 60.8, 65 },
-				["sourceQuest"] = 54762,	-- A Small Retreat
-				["races"] = { 6 },	-- Tauren
-				["minReputation"] = EXALTED,
-				["maps"] = { 65 },	-- Stonetalon Mountains
-				["lvl"] = 120,
-			}),
-			q(54764, {	-- Storm in Bloodhoof
-				["qg"] = 149529,	-- Spiritwalker Ussoh
-				["coord"] = { 49.2, 60.8, 65 },
-				["sourceQuest"] = 54763,	-- Crossing Over
-				["races"] = { 6 },	-- Tauren
-				["minReputation"] = EXALTED,
-				["maps"] = { 65 },	-- Stonetalon Mountains
-				["lvl"] = 120,
-			}),
-			q(54766, {	-- Answer the Call
-				["qg"] = 36648,	-- Baine Bloodhoof <High Chieftain>
-				["coord"] = { 58.3, 51.8, 88 },
-				["sourceQuest"] = 54764,	-- Storm in Bloodhoof
-				["races"] = { 6 },	-- Tauren
-				["minReputation"] = EXALTED,
-				["maps"] = { 88 },	-- Thunder Bluff
-				["lvl"] = 120,
-			}),
-			q(54765, {	-- Thank Your Guide
-				["qg"] = 36648,	-- Baine Bloodhoof <High Chieftain>
-				["coord"] = { 60.3, 51.7, 88 },
-				["sourceQuest"] = 54766,	-- Answer the Call	
-				["races"] = { 6 },	-- Tauren
-				["minReputation"] = EXALTED,
-				["maps"] = { 88 },	-- Thunder Bluff
-				["lvl"] = 120,		
-				["g"] = {
-					i(168298),	-- Ancestral Chieftain's Armor
-					i(168297),	-- Ancestral Chieftain's Grasps
-					i(168296),	-- Ancestral Chieftain's Greatbelt
-					i(168291),	-- Ancestral Chieftain's Headdress
-					i(168292),	-- Ancestral Chieftain's Hoofbands
-					i(168293),	-- Ancestral Chieftain's Loincloth
-					i(168294),	-- Ancestral Chieftain's Mantle
-					i(170063),	-- Ancestral Chieftain's Totem
-					i(168295),	-- Ancestral Chieftain's Wristbands
-					i(167860),	-- Ancient Tauren Talisman
-				},
+			i(168298),	-- Ancestral Chieftain's Armor
+			i(168297),	-- Ancestral Chieftain's Grasps
+			i(168296),	-- Ancestral Chieftain's Greatbelt
+			i(168291),	-- Ancestral Chieftain's Headdress
+			i(168292),	-- Ancestral Chieftain's Hoofbands
+			i(168293),	-- Ancestral Chieftain's Loincloth
+			i(168294),	-- Ancestral Chieftain's Mantle
+			i(170063),	-- Ancestral Chieftain's Totem
+			i(168295),	-- Ancestral Chieftain's Wristbands
+			i(167860),	-- Ancient Tauren Talisman (not collectible, just here so it's not in Unsorted)
+			n(-17, {	-- Quests
+				q(54759, {	-- When Spririts Whisper	
+					["qg"] = 149088,	-- Spiritwalker Isahi
+					["coord"] = { 39.1, 79.0, 85 },
+					["sourceQuest"] = 55779,	-- Stay of Execution (after the Baine Rescue Scenario)
+					["races"] = { 6 },	-- Tauren	
+					["minReputation"] = EXALTED,
+					["maps"] = { 85 },	-- Orgrimmar
+					["lvl"] = 120,
+				}),
+				q(54760, {	-- The Spiritwalkers
+					["qg"] = 36648,	-- Baine Bloodhoof <High Chieftain>
+					["coord"] = { 60.3, 51.7, 88 },
+					["sourceQuest"] = 54759,	-- When Spririts Whisper	
+					["races"] = { 6 },	-- Tauren
+					["minReputation"] = EXALTED,
+					["maps"] = { 88 },	-- Thunder Bluff
+					["lvl"] = 120,
+				}),
+				q(54761, {	-- Spirit Guide
+					["qg"] = 149084,	-- Spiritwalker Ussoh
+					["coord"] = { 12.3, 31.3, 462 },
+					["sourceQuest"] = 54760,	-- The Spiritwalkers	
+					["races"] = { 6 },	-- Tauren
+					["minReputation"] = EXALTED,
+					["maps"] = { 462 },	-- Camp Narache
+					["lvl"] = 120,
+				}),
+				q(54762, {	-- A Small Retreat
+					["qg"] = 149084,	-- Spiritwalker Ussoh
+					["coord"] = { 12.3, 31.3, 462 },
+					["sourceQuest"] = 54761,	-- Spirit Guide	
+					["races"] = { 6 },	-- Tauren
+					["minReputation"] = EXALTED,
+					["maps"] = { 462 },	-- Camp Narache
+					["lvl"] = 120,
+				}),
+				q(54763, {	-- Crossing Over
+					["qg"] = 149529,	-- Spiritwalker Ussoh
+					["coord"] = { 49.2, 60.8, 65 },
+					["sourceQuest"] = 54762,	-- A Small Retreat
+					["races"] = { 6 },	-- Tauren
+					["minReputation"] = EXALTED,
+					["maps"] = { 65 },	-- Stonetalon Mountains
+					["lvl"] = 120,
+				}),
+				q(54764, {	-- Storm in Bloodhoof
+					["qg"] = 149529,	-- Spiritwalker Ussoh
+					["coord"] = { 49.2, 60.8, 65 },
+					["sourceQuest"] = 54763,	-- Crossing Over
+					["races"] = { 6 },	-- Tauren
+					["minReputation"] = EXALTED,
+					["maps"] = { 65 },	-- Stonetalon Mountains
+					["lvl"] = 120,
+				}),
+				q(54766, {	-- Answer the Call
+					["qg"] = 36648,	-- Baine Bloodhoof <High Chieftain>
+					["coord"] = { 58.3, 51.8, 88 },
+					["sourceQuest"] = 54764,	-- Storm in Bloodhoof
+					["races"] = { 6 },	-- Tauren
+					["minReputation"] = EXALTED,
+					["maps"] = { 88 },	-- Thunder Bluff
+					["lvl"] = 120,
+				}),
+				q(54765, {	-- Thank Your Guide
+					["qg"] = 36648,	-- Baine Bloodhoof <High Chieftain>
+					["coord"] = { 60.3, 51.7, 88 },
+					["sourceQuest"] = 54766,	-- Answer the Call	
+					["races"] = { 6 },	-- Tauren
+					["minReputation"] = EXALTED,
+					["maps"] = { 88 },	-- Thunder Bluff
+					["lvl"] = 120,		
+					["g"] = {
+						i(168298),	-- Ancestral Chieftain's Armor
+						i(168297),	-- Ancestral Chieftain's Grasps
+						i(168296),	-- Ancestral Chieftain's Greatbelt
+						i(168291),	-- Ancestral Chieftain's Headdress
+						i(168292),	-- Ancestral Chieftain's Hoofbands
+						i(168293),	-- Ancestral Chieftain's Loincloth
+						i(168294),	-- Ancestral Chieftain's Mantle
+						i(170063),	-- Ancestral Chieftain's Totem
+						i(168295),	-- Ancestral Chieftain's Wristbands
+						i(167860),	-- Ancient Tauren Talisman
+					},
+				}),
 			}),
 		}),
 	}),
--- contrib/Parser/DATAS/12 - Item Sets/Heritage Armor/Heritage of the Sin'dorei.lua
+++ contrib/Parser/DATAS/12 - Item Sets/Heritage Armor/Heritage of the Sin'dorei.lua
@@ -5,134 +5,121 @@ _.GearSets =
 {
 	n(-254, {	-- Heritage Armor
 		gs(1804, { 	-- Heritage of the Sin'dorei
-			gs(1804, { 	-- Heritage of the Sin'dorei
-				["g"] = {
-					i(166348),	-- Sin'dorei Helm
-					i(166349),	-- Sin'dorei Pauldrons
-					i(166357),	-- Sin'dorei Cloak
-					i(166356),	-- Sin'dorei Raiment
-					i(166350),	-- Sin'dorei Tunic
-					i(166355),	-- Sin'dorei Bracers
-					i(166351),	-- Sin'dorei Gauntlets
-					i(166352),	-- Sin'dorei Belt
-					i(166353),	-- Sin'dorei Leggings
-					i(166354),	-- Sin'dorei Slippers
-				},
-				["races"] = { 10 },	-- Blood Elves
-			}),
-			n(-17, {
-				["maps"] = {
-					94,		-- Eversong Woods
-					95,		-- Ghostlands
-					122,	-- Isle of Quel'danas
-					85,		-- Orgrimmar City
-					86,		-- Orgrimmar City: The Drag
-					110,	-- Silvermoon City
-					973,	-- The Sunwell: Shrine of the Eclipse [Scenario]
-				},
-				["g"] = {
-					q(53791, {	-- The Pride of the Sin'dorei
-						["qg"] = 146939,	-- Ambassador Dawnsworn
-						["coord"] = { 39.2, 79.0, 85 },
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 85 },	-- Orgrimmar
-						["lvl"] = 120,
-					}),
-					q(53734, {	-- Walk Among Ghosts
-						["qg"] = 16802,	-- Lor'themar Theron
-						["coord"] = { 53.8, 20.2, 110 },
-						["sourceQuest"] = 53791,	-- The Pride of the Sin'dorei
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 110 },	-- Silvermoon City
-						["lvl"] = 120,
-					}),
-					q(53882, {	-- Writing on the Wall
-						["qg"] = 145015,	-- Lor'themar Theron
-						["coord"] = { 46.2, 31.8, 95 },
-						["sourceQuest"] = 53734,	-- Walk Among Ghosts
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 95 },	-- Ghostlands
-						["lvl"] = 120,
-					}),
-					q(53735, {	-- The First to Fall
-						["qg"] = 145005,	-- Lor'themar Theron
-						["coord"] = { 47.5, 84.1, 95 },
-						["sourceQuest"] = 53882,	-- Writing on the Wall
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 95 },	-- Ghostlands
-						["lvl"] = 120,
-					}),
-					q(53736, {	-- Lament of the Highborne
-						["qg"] = 145005,		-- Lor'themar Theron
-						["coord"] = { 37.2, 66.4, 95 },	-- Pickup Quest
-						["sourceQuest"] = 53735,	-- The First to Fall
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 95 },	-- Ghostlands
-						["lvl"] = 120,
-					}),
-					q(53737, {	-- The Day Hope Died
-						["qg"] = 145005,	-- Lor'themar Theron
-						["coord"] = { 12.7, 56.9, 95 },	-- Pickup Quest
-						["sourceQuest"] = 53736,	-- Lament of the Highborne
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 95 },	-- Ghostlands
-						["lvl"] = 120,
-					}),
-					q(53738, {	-- Defense of Qual'Danas
-						["qg"] = 145005,	-- Lor'themar Theron
-						["sourceQuest"] = 53737,	-- The Day Hope Died
-						["coord"] = { 51.1, 68.8, 94 },
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 94 },	-- Eversong Woods
-						["lvl"] = 120,
-					}),
-					q(53725, {	-- A People Shattered
-						["qg"] = 145793,	-- Lady Liadrin
-						["sourceQuest"] = 53738,	-- Defense of Qual'Danas
-						["coord"] = { 48.3, 36.0, 122 },
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 122 },	-- Isle of Quel'danas
-						["lvl"] = 120,
-					}),
-					q(53853, {	-- The Setting Sun
-						["qg"] = 145793,	-- Lady Liadrin
-						["sourceQuest"] = 53725,	-- A People Shattered
-						["coord"] = { 48.3, 36.0, 122 },
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 122 },	-- Isle of Quel'danas
-						["lvl"] = 120,
-					}),
-					q(54096, {	-- The Fall of the Sunwell
-						["qg"] = 145793,	-- Lady Liadrin
-						["sourceQuest"] = 53853,	-- The Setting Sun
-						["coord"] = { 48.3, 36.0, 122 },
-						["races"] = { 10 },	-- Blood Elves
-						["minReputation"] = EXALTED,
-						["maps"] = { 122 },	-- Isle of Quel'danas
-						["lvl"] = 120,
-						["g"] = {
-							i(166348),	-- Sin'dorei Helm
-							i(166349),	-- Sin'dorei Pauldrons
-							i(166357),	-- Sin'dorei Cloak
-							i(166356),	-- Sin'dorei Raiment
-							i(166350),	-- Sin'dorei Tunic
-							i(166355),	-- Sin'dorei Bracers
-							i(166351),	-- Sin'dorei Gauntlets
-							i(166352),	-- Sin'dorei Belt
-							i(166353),	-- Sin'dorei Leggings
-							i(166354),	-- Sin'dorei Slippers
-						},
-					}),
-				},
+			i(166348),	-- Sin'dorei Helm
+			i(166349),	-- Sin'dorei Pauldrons
+			i(166357),	-- Sin'dorei Cloak
+			i(166356),	-- Sin'dorei Raiment
+			i(166350),	-- Sin'dorei Tunic
+			i(166355),	-- Sin'dorei Bracers
+			i(166351),	-- Sin'dorei Gauntlets
+			i(166352),	-- Sin'dorei Belt
+			i(166353),	-- Sin'dorei Leggings
+			i(166354),	-- Sin'dorei Slippers
+			n(-17, {	-- Quests
+				q(53791, {	-- The Pride of the Sin'dorei
+					["qg"] = 146939,	-- Ambassador Dawnsworn
+					["coord"] = { 39.2, 79.0, 85 },
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 85 },	-- Orgrimmar
+					["lvl"] = 120,
+				}),
+				q(53734, {	-- Walk Among Ghosts
+					["qg"] = 16802,	-- Lor'themar Theron
+					["coord"] = { 53.8, 20.2, 110 },
+					["sourceQuest"] = 53791,	-- The Pride of the Sin'dorei
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 110 },	-- Silvermoon City
+					["lvl"] = 120,
+				}),
+				q(53882, {	-- Writing on the Wall
+					["qg"] = 145015,	-- Lor'themar Theron
+					["coord"] = { 46.2, 31.8, 95 },
+					["sourceQuest"] = 53734,	-- Walk Among Ghosts
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 95 },	-- Ghostlands
+					["lvl"] = 120,
+				}),
+				q(53735, {	-- The First to Fall
+					["qg"] = 145005,	-- Lor'themar Theron
+					["coord"] = { 47.5, 84.1, 95 },
+					["sourceQuest"] = 53882,	-- Writing on the Wall
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 95 },	-- Ghostlands
+					["lvl"] = 120,
+				}),
+				q(53736, {	-- Lament of the Highborne
+					["qg"] = 145005,		-- Lor'themar Theron
+					["coord"] = { 37.2, 66.4, 95 },	-- Pickup Quest
+					["sourceQuest"] = 53735,	-- The First to Fall
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 95 },	-- Ghostlands
+					["lvl"] = 120,
+				}),
+				q(53737, {	-- The Day Hope Died
+					["qg"] = 145005,	-- Lor'themar Theron
+					["coord"] = { 12.7, 56.9, 95 },	-- Pickup Quest
+					["sourceQuest"] = 53736,	-- Lament of the Highborne
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 95 },	-- Ghostlands
+					["lvl"] = 120,
+				}),
+				q(53738, {	-- Defense of Qual'Danas
+					["qg"] = 145005,	-- Lor'themar Theron
+					["sourceQuest"] = 53737,	-- The Day Hope Died
+					["coord"] = { 51.1, 68.8, 94 },
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 94 },	-- Eversong Woods
+					["lvl"] = 120,
+				}),
+				q(53725, {	-- A People Shattered
+					["qg"] = 145793,	-- Lady Liadrin
+					["sourceQuest"] = 53738,	-- Defense of Qual'Danas
+					["coord"] = { 48.3, 36.0, 122 },
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 122 },	-- Isle of Quel'danas
+					["lvl"] = 120,
+				}),
+				q(53853, {	-- The Setting Sun
+					["qg"] = 145793,	-- Lady Liadrin
+					["sourceQuest"] = 53725,	-- A People Shattered
+					["coord"] = { 48.3, 36.0, 122 },
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 122 },	-- Isle of Quel'danas
+					["lvl"] = 120,
+				}),
+				q(54096, {	-- The Fall of the Sunwell
+					["qg"] = 145793,	-- Lady Liadrin
+					["sourceQuest"] = 53853,	-- The Setting Sun
+					["coord"] = { 48.3, 36.0, 122 },
+					["races"] = { 10 },	-- Blood Elves
+					["minReputation"] = EXALTED,
+					["maps"] = { 
+						122,	-- Isle of Quel'danas
+						973,	-- The Sunwell: Shrine of the Eclipse (Scenario)
+					 },
+					["lvl"] = 120,
+					["g"] = {
+						i(166348),	-- Sin'dorei Helm
+						i(166349),	-- Sin'dorei Pauldrons
+						i(166357),	-- Sin'dorei Cloak
+						i(166356),	-- Sin'dorei Raiment
+						i(166350),	-- Sin'dorei Tunic
+						i(166355),	-- Sin'dorei Bracers
+						i(166351),	-- Sin'dorei Gauntlets
+						i(166352),	-- Sin'dorei Belt
+						i(166353),	-- Sin'dorei Leggings
+						i(166354),	-- Sin'dorei Slippers
+					},
+				}),
 			}),
 		}),
 	}),	
