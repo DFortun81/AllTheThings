27100a8f8762c67703a5444a8b8e06df121ca2ce
Gaulish <l2pwncomputers@hotmail.com>
2019-06-28 15:56:55 -0600

    Junkyard Tinkering added to professions.

---
+++ contrib/Parser/DATAS/11 - Professions/Junkyard Tinkering/Recipes/_ Automation.lua
@@ -0,0 +1,147 @@
+--[[
+DO NOT MODIFY THIS FILE, I WILL CUT YOU.
+ With love,
+    -CRIEVE
+]]--
+profession(2720, {	-- Junkyard Tinkering
+	filter(200, {
+		{
+			["name"] = "Junkyard Tinkering",
+			["categoryID"] = 1257,
+			["g"] = {
+				{
+					["name"] = "Equipment",
+					["categoryID"] = 1283,
+					["g"] = {
+						{
+							["name"] = "Advanced Adventurer Augment",
+							["recipeID"] = 299864
+						},
+						{
+							["name"] = "Experimental Adventurer Augment",
+							["recipeID"] = 299186
+						},
+						{
+							["name"] = "Re-Procedurally Generated Punchcard",
+							["recipeID"] = 300277
+						},
+						{
+							["name"] = "Scrapmaster's Blowtorch",
+							["recipeID"] = 300122
+						}
+					}
+				},
+				{
+					["name"] = "Resources",
+					["categoryID"] = 1281,
+					["g"] = {
+						{
+							["name"] = "S.P.A.R.E. Crate",
+							["recipeID"] = 300291
+						}
+					}
+				},
+				{
+					["name"] = "Explosives",
+					["categoryID"] = 1256,
+					["g"] = {
+						{
+							["name"] = "Annoy-o-Tron Gang",
+							["recipeID"] = 300274
+						},
+						{
+							["name"] = "Rustbolt Pocket Turret",
+							["recipeID"] = 300273
+						},
+						{
+							["name"] = "Scrap Grenade",
+							["recipeID"] = 295290
+						}
+					}
+				},
+				{
+					["name"] = "Gadgets",
+					["categoryID"] = 1280,
+					["g"] = {
+						{
+							["name"] = "BAWLD-371",
+							["recipeID"] = 296595
+						},
+						{
+							["name"] = "Mechano-Treat",
+							["recipeID"] = 291869
+						},
+						{
+							["name"] = "Protocol Transference Device",
+							["recipeID"] = 298134
+						}
+					}
+				},
+				{
+					["name"] = "Transportation",
+					["categoryID"] = 1285,
+					["g"] = {
+						{
+							["name"] = "Anti-Gravity Pack",
+							["recipeID"] = 300272
+						},
+						{
+							["name"] = "Emergency Rocket Chicken",
+							["recipeID"] = 298136
+						},
+						{
+							["name"] = "Personal Time Displacer",
+							["recipeID"] = 300265
+						},
+						{
+							["name"] = "Ultrasafe Transporter: Mechagon",
+							["recipeID"] = 291987
+						}
+					}
+				},
+				{
+					["name"] = "Robots",
+					["categoryID"] = 1291,
+					["g"] = {
+						{
+							["name"] = "Green Spraybot",
+							["recipeID"] = 300267
+						},
+						{
+							["name"] = "Orange Spraybot",
+							["recipeID"] = 300266
+						}
+					}
+				},
+				{
+					["name"] = "Components & Tools",
+					["categoryID"] = 1259,
+					["g"] = {
+						{
+							["name"] = "Beastbot Powerpack",
+							["recipeID"] = 295362
+						}
+					}
+				},
+				{
+					["name"] = "Other",
+					["categoryID"] = 1261,
+					["g"] = {
+						{
+							["name"] = "Canned Minnows",
+							["recipeID"] = 294360
+						},
+						{
+							["name"] = "Hundred-Fathom Lure",
+							["recipeID"] = 293394
+						},
+						{
+							["name"] = "Vaultbot Key",
+							["recipeID"] = 291788
+						}
+					}
+				}
+			}
+		}
+	}),
+});
