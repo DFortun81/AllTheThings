b092f95139e1df47449f83987458807819d904eb
Crieve-Sargeras <dylanfortune@live.com>
2019-06-27 22:11:05 -0700

    Completing the Allied Races quest chains on any character now grants you personal and account wide completion for their full quest chains.

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -981,23 +981,22 @@ end
 -- Quest Completion Lib
 local DirtyQuests = {};
 local CompletedQuests = setmetatable({}, {__newindex = function (t, key, value)
-	DirtyQuests[key] = true;
 	rawset(t, key, value);
 	if value then
+		DirtyQuests[key] = true;
 		SetDataSubMember("CollectedQuests", key, 1);
 		SetTempDataSubMember("CollectedQuests", key, 1);
-	end
-	
-	if app.Settings:GetTooltipSetting("Report:CompletedQuests") then
-		local searchResults = app.SearchForField("questID", key);
-		if searchResults and #searchResults > 0 then
+		if app.Settings:GetTooltipSetting("Report:CompletedQuests") then
 			if app.Settings:GetTooltipSetting("Report:UnsortedQuests") then
-				return true;
+				local searchResults = app.SearchForField("questID", key);
+				if searchResults and #searchResults > 0 then
+					return true;
+				else
+					key = key .. " (Missing in ATT)";
+				end
 			end
-		else
-			key = key .. " (Missing in ATT)";
+			print("Completed Quest ID #" .. key);
 		end
-		print("Completed Quest ID #" .. key);
 	end
 end});
 local IsQuestFlaggedCompleted = function(questID)
@@ -3229,7 +3228,6 @@ end
 );
 
 -- Achievement Lib
-app.AchievementFilter = 4;
 app.BaseAchievement = {
 	__index = function(t, key)
 		if key == "achievementID" then
@@ -3250,7 +3248,7 @@ app.BaseAchievement = {
 		elseif key == "collectible" then
 			return app.CollectibleAchievements;
 		elseif key == "collected" then
-			return select(app.AchievementFilter, GetAchievementInfo(t.achievementID));
+			return select(4, GetAchievementInfo(t.achievementID));
 		else
 			-- Something that isn't dynamic.
 			return table[key];
@@ -3332,7 +3330,7 @@ app.BaseAchievementCriteria = {
 		elseif key == "collectible" then
 			return app.CollectibleAchievements;
 		elseif key == "saved" or key == "collected" then
-			if select(app.AchievementFilter, GetAchievementInfo(t.achievementID)) then
+			if select(4, GetAchievementInfo(t.achievementID)) then
 				return true;
 			elseif t.criteriaID then
 				local m = GetAchievementNumCriteria(t.achievementID);
@@ -12608,6 +12606,29 @@ app.events.VARIABLES_LOADED = function()
 		myQuests = {};
 		quests[app.GUID] = myQuests;
 		SetTempDataMember("CollectedQuests", myQuests);
+	end
+	
+	-- Cache some collection states for account wide quests that aren't actually account wide. (Allied Races)
+	for i,achievementQuests in ipairs({
+		{ 12453, { 49973, 49613, 49354, 49614 } },	-- Allied Races: Nightborne
+		{ 12517, { 53466, 53467, 53353, 53355, 52942, 52943, 52945, 52955, 51479 } },	-- Allied Races: Mag'har
+		{ 13156, { 53831, 53823, 53824, 54419, 53826, 54301, 54300, 53825, 53827, 53828, 54031, 54033, 54032, 54034, 53830, 53719 } },	-- Allied Races: Zandalari Troll
+		{ 12452, { 48066, 48067, 49756, 48079, 41884, 41764, 48185, 41799, 48190, 41800, 48434, 41815, 41840, 41882, 41841, 48403, 48433 } },	-- Allied Races: Highmountain Tauren
+		{ 12450, { 49787, 48962 } },	-- Allied Races: Void Elf
+		{ 12516, { 51813, 53351, 53342, 53352, 51474, 53566 } },	-- Allied Races: Dark Iron Dwarf
+		{ 12451, { 49698, 49266, 50071 } },	-- Allied Races: Lightforged Draenei 
+		{ 13157, { 54706, 55039, 55043, 54708, 54721, 54723, 54725, 54726, 54727, 54728, 54730, 54731, 54729, 54732, 55136, 54733, 54734, 54735, 54851, 53720 } },	-- Allied Races: Kul Tiran
+	}) do
+		-- If you completed the achievement, then mark the associated quests.
+		if select(4, GetAchievementInfo(achievementQuests[1])) then
+			for j,questID in ipairs(achievementQuests[2]) do
+				rawset(CompletedQuests, questID, 2);
+				if not myQuests[questID] then
+					myQuests[questID] = 2;
+					quests[questID] = 1;
+				end
+			end
+		end
 	end
 	
 	-- Cache your character's selfie filters data.
