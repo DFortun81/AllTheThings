6632cecc6167a8e82eb49d1e8f2277d00c2e899e
Crieve-Sargeras <dylanfortune@live.com>
2019-06-30 02:10:36 -0700

    Fixed the Shop tooltip.

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -3049,7 +3049,7 @@ local function AttachTooltip(self)
 								gf = app:GetWindow("Prime").data.g[4];
 							elseif owner.tooltipText == BLIZZARD_STORE then
 								-- Shop
-								gf = app:GetWindow("Prime").data.g[15];
+								gf = app:GetWindow("Prime").data.g[16];
 							elseif string.sub(owner.tooltipText, 1, string.len(ACHIEVEMENT_BUTTON)) == ACHIEVEMENT_BUTTON then
 								-- Achievements
 								gf = app:GetWindow("Prime").data.g[5];
