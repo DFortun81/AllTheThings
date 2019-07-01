035f7583c0a3365734afc63080d698d04e819f2c
sadidorf <sadidorf@gmail.com>
2019-07-02 01:58:31 +0200

    Added ALLTHEWQ in Darkshore

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Darkshore/World Quests.lua
@@ -0,0 +1,677 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(12, {	-- Kalimdor
+		m(62, {	-- Darkshore
+			n(-34,  {	-- World Quests
+				q(54669, {	-- A Dangerous Combination(A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54497, {	-- A Dangerous Combination (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54840, {	-- Agathe Wyrmwood
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54825, {	-- Alash'anir (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54797, {	-- Alash'anir (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54823, {	-- Aman (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54795, {	-- Aman (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54678, {	-- Anything We Can Salvage (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54531, {	-- Anything We Can Salvage (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54809, {	-- Athil Dewfire (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54821, {	-- Athrikus Narassin (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54793, {	-- Athrikus Narassin (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54677, {	-- Back to the Depths (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54528, {	-- Back to the Depths (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54856, {	-- Bilgewater Bandits (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54811, {	-- Blackpaw (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54784, {	-- Blackwood Liberation (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54612, {	-- Blood and Ashes (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54836, {	-- Commander Drald (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54834, {	-- Commander Ral'esh (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54806, {	-- Commander Ral'esh (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54818, {	-- Conflagros (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54790, {	-- Conflagros (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54838, {	-- Croz Bloodrage (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54817, {	-- Cyclarus (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54789, {	-- Cyclarus (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54724, {	-- Ending the Blackwood (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54679, {	-- Fueling the Flames (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54536, {	-- Fueling the Flames (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54858, {	-- Glaive Consequences (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54831, {	-- Glimmerspine (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54803, {	-- Glimmerspine (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54832, {	-- Glrglrr (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54804, {	-- Glrglrr (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54819, {	-- Granokk (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54795, {	-- Granokk (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54827, {	-- Gren Tornfur (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54799, {	-- Gren Tornfur (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54810, {	-- Grimhorn (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54675, {	-- Hunting Season (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54816, {	-- Hydrath (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54788, {	-- Hydrath (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54895, {	-- Ivus the Decayed (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54896, {	-- Ivus the Forest Lord (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54648, {	-- Kaldorei Resurgence (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54667, {	-- Legacy in Ruins (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54467, {	-- Legacy in Ruins (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54826, {	-- Madfeather (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54798, {	-- Madfeather (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54835, {	-- Moxo the Beheader (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54830, {	-- Mrggr'marr (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54802, {	-- Mrggr'marr (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54792, {	-- Onu (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54841, {	-- Orwell Stevenson (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54668, {	-- Out of Their Mine (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54674, {	-- Prisoners of the Darkscale (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54514, {	-- Prisoners of the Darkscale (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54672, {	-- Rituals of Twilight (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54499, {	-- Rituals of Twilight (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54807, {	-- Sapper Odette (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54833, {	-- Scalefiend (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54805, {	-- Scalefiend (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54496, {	-- Scurvy Dogs! (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54812, {	-- Shadowclaw (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54676, {	-- Shards of Narassin (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54698, {	-- Shattering Spears (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54824, {	-- Shattershard (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54796, {	-- Shattershard (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54520, {	-- Singed Saplings (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54822, {	-- Soggoth the Slitherer (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54794, {	-- Soggoth the Slitherer (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54521, {	-- Stolen Sorcery (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54829, {	-- Stonebinder Ssra'vess (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54801, {	-- Stonebinder Ssra'vess (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54808, {	-- Thelar Moonstrike (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54673, {	-- Tip of the Trident (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54509, {	-- Tip of the Trident (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54857, {	-- Turning the Tides (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54722, {	-- Turning the Tides (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54828, {	-- Twilight Prophet Graeme (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+				q(54800, {	-- Twilight Prophet Graeme (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54719, {	-- Worgen in the Ore Mine (H)
+					["collectible"] = false,
+					["races"] = HORDE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1716),	-- Honorbound Service Medal
+					},
+				}),
+				q(54820, {	-- Zim'kaga (A)
+					["collectible"] = false,
+					["races"] = ALLIANCE_ONLY,
+					["lvl"] = 120,
+					["g"] = {
+						currency(1717),	-- 7th Legion Service Medal
+					},
+				}),
+			}),
+		}),
+	}),
+};
+				
