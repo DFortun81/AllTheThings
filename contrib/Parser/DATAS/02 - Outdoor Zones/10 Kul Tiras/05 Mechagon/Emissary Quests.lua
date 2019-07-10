62103e30864e3adf5ddd35258e7c998cc2df9d39
Gaulish <l2pwncomputers@hotmail.com>
2019-07-10 16:01:44 -0600

    Rustbolt paragon chest framework.

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/05 Mechagon/Emissary Quests.lua
@@ -0,0 +1,22 @@
+--------------------------------------------------
+--          Z O N E S       M O D U L E          --
+---------------------------------------------------
+_.Zones =
+{
+	m(876, {	-- Kul'Tiras
+		m(1462, {	-- Mechagon
+			n(-169, { 	-- Emissary Quests
+				q(55348, {	-- Supplies from the Rustbolt Resistance
+					--["qg"] = 
+					["lvl"] = 120,
+					--["coord"] = { 39.11, 79.47, 1462 },
+					["repeatable"] = true,
+					["g"] = {
+						i(170061, {	-- Rustbolt Supplies
+						}),
+					},
+				}),
+			}),
+		}),
+	}),
+};
