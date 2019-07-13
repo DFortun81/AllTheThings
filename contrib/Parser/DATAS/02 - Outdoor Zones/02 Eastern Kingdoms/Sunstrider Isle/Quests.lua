4e285bd72bcc5b7a83176ae728c6e7467adf0155
Pr3vention <Pr3vention@gmail.com>
2019-07-13 08:49:49 -0500

    Fixed sunstrider island showing in the minilist for non-blood elves when in the zone
    There appears to be a difference between main list and minilist display logic. Applying the races tag to a map may not be appropriate anywhere

--- contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Sunstrider Isle/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Sunstrider Isle/Quests.lua
@@ -6,73 +6,73 @@ _.Zones =
 	m(13, {	-- Eastern Kingdoms
 		m(467, {	-- Sunstrider Isle
 			n(-17, {	-- Quests
-				{	-- A Fistful of Slivers
-					["questID"] = 37440,
+				q(37440, {	-- A Fistful of Slivers
 					["qg"] = 15296,	-- Arcanist Ithanas
 					["coord"] = { 61.8, 39.3, 467 },
+					["races"] = { 10 },	-- Blood Elves
 					["sourceQuest"] = 8326,	-- Unfortunate Measures
-				},
-				{	-- Aggression
-					["questID"] = 8334,
+				}),
+				q(8334, {	-- Aggression
 					["qg"] = 15281,	-- Lanthan Perilon
 					["coord"] = { 52.8, 49.8, 467 },
+					["races"] = { 10 },	-- Blood Elves
 					["sourceQuest"] = 8326,	-- Unfortunate Measures
-				},
-				{	-- Aiding the Outrunners
-					["questID"] = 8347,
+				}),
+				q(8347, {	-- Aiding the Outrunners
 					["qg"] = 15281,	-- Lanthan Perilon
 					["coord"] = { 52.8, 49.7, 467 },
+					["races"] = { 10 },	-- Blood Elves
 					["sourceQuest"] = 8335,	-- Felendren the Banished
 					["isBreadcrumb"] = true,
-				},
-				{	-- Felendren the Banished
-					["questID"] = 8335,
+				}),
+				q(8335, {	-- Felendren the Banished
 					["qg"] = 15281,	-- Lanthan Perilon
 					["coord"] = { 52.8, 49.7, 467 },
+					["races"] = { 10 },	-- Blood Elves
 					["sourceQuest"] = 8334,	-- Aggression
-				},
-				{	-- Reclaiming Sunstrider Isle
-					["questID"] = 8325,
+				}),
+				q(8325, {	-- Reclaiming Sunstrider Isle
 					["qg"] = 15278,	-- Magistrix Erona
 					["coord"] = { 61.0, 45.1, 467 },
-				},
-				{	-- Report to Lanthan Perilon
-					["questID"] = 8327,
+					["races"] = { 10 },	-- Blood Elves
+				}),
+				q(8327, {	-- Report to Lanthan Perilon
 					["qg"] = 15278,	-- Magistrix Erona
 					["coord"] = { 61.0, 45.1, 467 },
+					["races"] = { 10 },	-- Blood Elves
 					["sourceQuest"] = 8326,	-- Unfortunate Measures
-				},
-				{	-- Solanian's Belongings
-					["questID"] = 37443,
+				}),
+				q(37443, {	-- Solanian's Belongings
 					["qg"] = 15295,	-- Well Watcher Solanian
 					["coord"] = { 63.9, 42.8, 467 },
+					["races"] = { 10 },	-- Blood Elves
 					["sourceQuest"] = 8326,	-- Unfortunate Measures
-				},
-				{	-- Tainted Arcane Sliver
-					["itemID"] = 20483,
+				}),
+				i(20483, {	-- Tainted Arcane Sliver
 					["questID"] = 8338,
+					["races"] = { 10 },	-- Blood Elves
 					["crs"] = {
 						15298,	-- Tainted Arcane Wraith
 					},
-				},
-				{	-- The Shrine of Dath'Remar
-					["questID"] = 37442,
+				}),
+				q(37442, {	-- The Shrine of Dath'Remar
 					["qg"] = 15295,	-- Well Watcher Solanian
 					["coord"] = { 63.9, 42.8, 467 },
+					["races"] = { 10 },	-- Blood Elves
 					["sourceQuest"] = 8326,	-- Unfortunate Measures
-				},
-				{	-- Thirst Unending
-					["questID"] = 37439,
+				}),
+				q(37439, {	-- Thirst Unending
 					["qg"] = 15297,	-- Arcanist Helion
 					["coord"] = { 58.4, 38.7, 467 },
+					["races"] = { 10 },	-- Blood Elves
 					["sourceQuest"] = 8326,	-- Unfortunate Measures
-				},
-				{	-- Unfortunate Measures
-					["questID"] = 8326,
+				}),
+				q(8326, {	-- Unfortunate Measures
 					["qg"] = 15278,	-- Magistrix Erona
 					["coord"] = { 61.0, 45.1, 467 },
+					["races"] = { 10 },	-- Blood Elves
 					["sourceQuest"] = 8325,	-- Reclaiming Sunstrider Isle
-				},
+				}),
 			}),
 		}),
 	}),
