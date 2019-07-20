2e21cdfa8d7e82070fc8106aab6f84eae025aa29
Gold-DieAldor <goldenshacal@gmail.com>
2019-07-20 02:05:38 +0200

    updated zandalar weapon cache once more. reduced sources by 8 for horde, 12 for alliance.

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/Emissary Quests.lua
@@ -0,0 +1,21 @@
+--------------------------------------------------
+--          Z O N E S       M O D U L E          --
+---------------------------------------------------
+_.Zones =
+{
+	m(875, {	-- Zandalar
+		n(-169, {	-- Emissary Quests
+			i(165863, {	-- Zandalari Weapons Cache
+				["lvl"] = 120,
+				["races"] = HORDE_ONLY,
+				["repeatable"] = true,
+				["g"] = {
+					i(158131),	-- Whisperfen Staff
+					i(158108),	-- Whisperfen Chopper
+					i(158111),	-- Whisperfen Hewer
+					i(158128),	-- Whisperfen Polearm
+				},
+			}),
+		}),
+	}),
+};
