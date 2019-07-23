f196ad634eaae0dc42e464e928a2e780141eabee
Crieve-Sargeras <dylanfortune@live.com>
2019-07-22 23:56:16 -0700

    Added the contents of the Unsullied Ring, Trinket, and Relic Tokens.

--- contrib/Parser/DATAS/02 - Outdoor Zones/09 Argus/1 - Zone Drops.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/09 Argus/1 - Zone Drops.lua
@@ -42,77 +42,98 @@ _.Zones =
 					i(153144, {	-- Unsullied Cloth Slippers
 						i(152774),	-- Azurelight Sage's Slippers
 					}),
-					i(153139,{ --Unsullied Leather Headgear	
+					i(153139, {	-- Unsullied Leather Headgear	
 						i(152759),	-- Arinor Keeper's Headgear
 					}),	
-					i(153145,{ --Unsullied Leather Spaulders	
+					i(153145, {	-- Unsullied Leather Spaulders	
 						i(152760),	-- Arinor Keeper's Spaulders
 					}),	
-					i(153151,{ --Unsullied Leather Tunic	
+					i(153151, {	-- Unsullied Leather Tunic	
 						i(152758),	-- Arinor Keeper's Tunic
 					}),	
-					i(153142,{ --Unsullied Leather Armbands	
+					i(153142, {	-- Unsullied Leather Armbands	
 						i(152754),	-- Arinor Keeper's Armbands
 					}),	
-					i(152739,{ --Unsullied Leather Grips	
+					i(152739, {	-- Unsullied Leather Grips	
 						i(152755),	-- Arinor Keeper's Grips
 					}),	
-					i(153148,{ --Unsullied Leather Belt	
+					i(153148, {	-- Unsullied Leather Belt	
 						i(152756),	-- Arinor Keeper's Belt
 					}),	
-					i(152737,{ --Unsullied Leather Trousers	
+					i(152737, {	-- Unsullied Leather Trousers	
 						i(152761),	-- Arinor Keeper's Trousers
 					}),	
-					i(153136,{ --Unsullied Leather Treads	
+					i(153136, {	-- Unsullied Leather Treads	
 						i(152757),	-- Arinor Keeper's Treads
 					}),	
-					i(153147,{ --Unsullied Mail Coif	
+					i(153147, {	-- Unsullied Mail Coif	
 						i(152765),	-- Oronaar Disciple's Coif
 					}),	
-					i(153137,{ --Unsullied Mail Spaulders	
+					i(153137, {	-- Unsullied Mail Spaulders	
 						i(152769),	-- Oronaar Disciple's Spaulders
 					}),	
-					i(152741,{ --Unsullied Mail Chestguard	
+					i(152741, {	-- Unsullied Mail Chestguard	
 						i(152768),	-- Oronaar Disciple's Chestguard
 					}),	
-					i(153158,{ --Unsullied Mail Bracers	
+					i(153158, {	-- Unsullied Mail Bracers	
 						i(152762),	-- Oronaar Disciple's Bracers
 					}),	
-					i(153149,{ --Unsullied Mail Gloves	
+					i(153149, {	-- Unsullied Mail Gloves	
 						i(152763),	-- Oronaar Disciple's Gloves
 					}),	
-					i(152744,{ --Unsullied Mail Girdle	
+					i(152744, {	-- Unsullied Mail Girdle	
 						i(152767),	-- Oronaar Disciple's Girdle
 					}),	
-					i(153138,{ --Unsullied Mail Legguards	
+					i(153138, {	-- Unsullied Mail Legguards	
 						i(152764),	-- Oronaar Disciple's Legguards
 					}),	
-					i(153152,{ --Unsullied Mail Boots	
+					i(153152, {	-- Unsullied Mail Boots	
 						i(152766),	-- Oronaar Disciple's Boots
 					}),	
-					i(153155,{ --Unsullied Plate Helmet	
+					i(153155, {	-- Unsullied Plate Helmet	
 						i(152747),	-- Praetorium Guard's Helmet
 					}),	
-					i(153153,{ --Unsullied Plate Pauldrons	
+					i(153153, {	-- Unsullied Plate Pauldrons	
 						i(152750),	-- Praetorium Guard's Pauldrons
 					}),	
-					i(153143,{ --Unsullied Plate Breasplate	
+					i(153143, {	-- Unsullied Plate Breasplate	
 						i(152748),	-- Praetorium Guard's Breastplate
 					}),	
-					i(153150,{ --Unsullied Plate Vambraces	
+					i(153150, {	-- Unsullied Plate Vambraces	
 						i(152752),	-- Praetorium Guard's Vambraces
 					}),	
-					i(153157,{ --Unsullied Plate Gauntlets	
+					i(153157, {	-- Unsullied Plate Gauntlets	
 						i(152751),	-- Praetorium Guard's Gauntlets
 					}),	
-					i(153140,{ --Unsullied Plate Waistplate	
+					i(153140, {	-- Unsullied Plate Waistplate	
 						i(152749),	-- Praetorium Guard's Waitsplate
 					}),	
-					i(153146,{ --Unsullied Plate Greaves	
+					i(153146, {	-- Unsullied Plate Greaves	
 						i(152746),	-- Praetorium Guard's Greaves
 					}),	
-					i(152743,{ --Unsullied Plate Sabatons	
+					i(152743, {	-- Unsullied Plate Sabatons	
 						i(152753),	-- Praetorium Guard's Sabatons
+					}),
+					i(152735, {	-- Unsullied Ring
+						i(152779),	-- Jed'hin Aspirant's Ring
+					}),
+					i(152733, {	-- Unsullied Trinket
+						i(153172),	-- Doomed Exarch's Memento
+						i(152783),	-- Mac'Aree Focusing Amethyst
+						i(152781),	-- Unblemished Sigil of Argus
+						i(152782),	-- Venerable Triad Statuette
+					}),
+					i(152799, {	-- Unsullied Relic
+						i(150996),	-- Archaic Meditation Crystal
+						i(151002),	-- Argussian Wrought Blade
+						i(151000),	-- Construct Forge Cinder
+						i(150998),	-- Cragscaler Heart
+						i(150997),	-- Crystallized Aether
+						i(151003),	-- Petrified Seedling
+						i(151001),	-- Phial of Kaarinos Frost
+						i(151015),	-- Skyfin Tail Barb
+						i(151004),	-- Voidbent Isolon Effigy
+						i(150999),	-- Wakener's Demonic Focus
 					}),
 				},
 			},
