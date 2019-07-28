f1d2cdc228b5e3a6fa2a406114caff004945e777
Crieve-Sargeras <dylanfortune@live.com>
2019-07-28 13:07:25 -0700

    Fixed the Brawler's Footlocker Symbolic Link.

--- contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/01 Boralus/Weekly Holidays.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/01 Boralus/Weekly Holidays.lua
@@ -52,7 +52,12 @@ _.Zones =
 					["isWeekly"] = true,
 					["g"] = {
 						i(154992, {	-- Brawler's Footlocker
-							["sym"] = {{"select", "npcID", -690}, {"pop"}, {"contains", "f", 1, 2, 3, 4, 5, 6, 7, 8, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 34, 35, 51, 52, 53, 54 }},
+							["sym"] = {
+								{"select", "npcID", -691},	-- BFA Season 3
+								{"pop"},	-- Discard the header and acquire the children.
+								{"where", "npcID", -661},	-- Gladiator Gear
+								{"pop"},	-- Discard the header and acquire the children.
+							},
 						}),
 					},
 				}),
