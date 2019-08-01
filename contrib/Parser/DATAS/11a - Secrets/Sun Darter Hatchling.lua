dde8368c58344d036fa0fff77ebaca2a0c414964
Gaulish <l2pwncomputers@hotmail.com>
2019-08-01 08:32:33 -0600

    Gave Sun Darter Hatchling a face lift.

--- contrib/Parser/DATAS/11a - Secrets/Sun Darter Hatchling.lua
+++ contrib/Parser/DATAS/11a - Secrets/Sun Darter Hatchling.lua
@@ -4,22 +4,210 @@
 _.Secrets = 
 {
 	n(-802, {	-- Sun Darter Hatchling
-		--["modelScale"] = .8,
 		["modelID"] = 74529,
+		["description"] = "***This secret requires you to have debug mode enabled to see the consumables needed. To enable debug mode right click the ATT icon on the minimap, navigate to the general tab and check the Debug Mode |cFFFFFFFF(Show Everything)|r box.*** \n\nIt also requires the battle pet Perky Pug from the Dungeons and Raids achievment Looking For Multitudes.\n\nEnjoy!\n\n!Note If coords are not plotting, you might need to make a standalone item plot it, and then plot again from the main list.",
 		["maps"] = {
-			12,
-			83,
 		},
 		["g"] = {
-			o(272487, {	-- Oddly-Colored Egg
+			o(9999908, {	-- |cFFFFFFFFStep 1:|r Gather Consumables.
+				["icon"] = "Interface\\Icons\\INV_Misc_Map02",
+				["description"] = "|cFFFFFFFFSTEP 1:|r Gather the following quantity of consumbables:",
+				["g"] = {
+					i(38320, {	-- Dire Brew
+						["description"] = "Qty:1\n\nObtained from Dark Iron Brewer in BRD.\n\nWalk/fly to BRD, or use the Direbrew's Remote obtained from Brewfest. Once inside the instance, take a left threw the gate, and then right threw another gate.  Go threw the door on the left, and click the Shadowforge Lock at the back of the room.  Backtrack to the door, go up the ramp and take a left down the hallway.  Continue down the hallway, until it opens up.  Take the first right into \"The Manufactory\".  Head right and threw the door at the back of the room, over the bridge and into \"The Grim Guzzler\".  Go up the stairs on the right and in the back of the room on the left is the Dark Iron Brewer. Talk to him until he passes out, and loot the Dire Brew that spawns.",
+						["g"] = {
+							n(144128, {	-- Dark Iron Brewer
+								["coords"] = {
+									{ 21.0, 38.3, 36 },	-- Blackrock Mountain
+									{ 55.0, 84.5, 35 },	-- Entrance by MC 
+									{ 39.0, 18.3, 35 },	-- BRD Entrance
+									{ 49.8, 60.9, 243 },	-- Inside Instance
+									{ 49.8, 31.6, 242 },	-- Inside Instance
+								},
+							}),
+						},
+					}),
+					i(44958, {	-- Ethereal Oil
+						["description"] = "Qty:1\n\nPurchase on the AH or make your own(Alchemy)",
+					}),
+					i(18258, {	-- Gordok Ogre Suit
+						["description"] = "Qty:1\n\nPurchase on the AH, make your own(pre Cata Tailors/Leatherworkers only), or head to Dire Maul North.\n\nOnce inside find Knot Thimblejack near Guard Slip'kik, and complete his quest for the suit.",
+						["g"] = {
+							q(27120, {	-- The Gordok Ogre Suit
+								["qg"] = 14338,	-- Knot Thimblejack
+								["repeatable"] = true,
+								["collectible"] = false,
+								["coords"] = {
+									{ 62.5, 24.9, 69 },	-- Dire Maul North entrance
+									--{ 17.07, 17.06, 235 },	-- Inside instance?
+								}
+							}),
+						},	
+					}),
+					i(103789, {	-- "Little Princess" Costume
+						["description"] = "Qty:1\n\nPurchase on the AH or from the vendors below:",
+						["g"] = {
+							n(142065, {	-- Dana Pull
+								["description"] = "Boralus",
+								["coord"] = { 50.8, 46.0, 1161 },	
+								["races"] = ALLIANCE_ONLY,
+							}),
+							n(98725, {	-- Lio the Lioness <Battle Pet Master>
+								["description"] = "Legion Dalaran or Garrison",
+								["coords"] = {
+									{ 59.2, 39.6, 627 },	-- Legion Dalaran
+									{ 29.2, 40.4, 582 },	-- Lunarfall	
+								},
+								["races"] = ALLIANCE_ONLY,
+							}),
+							n(141614, {	-- Koari <Pet Supplies>
+								["description"] = "Dazar'alor",
+								["coord"] = { 34.8, 13.1, 1165 },	
+								["races"] = HORDE_ONLY,
+							}),
+							n(115287, {	-- Serr'ah <Battle Pet Master>
+								["description"] = "Legion Dalaran or Garrison",
+								["coords"] = {
+									{ 58.6, 38.0, 627 },	-- Legion Dalaran
+									{ 32.6, 42.8, 590 },	-- Frostwall									
+								},
+								["races"] = HORDE_ONLY,
+							}),
+							n(73293, {	-- Whizzig <Merchant of Time>
+								["description"] = "Limited time vendor on Timeless Isle",
+								["coords"] = {
+									{ 41.6, 47.2, 554 },	
+									{ 35.0, 52.8, 554 },	
+									{ 42.8, 59.4, 554 },	
+									{ 40.0, 63.0, 554 },	
+								},
+							}),
+						},
+					}),
+					i(22841, {	-- Major Fire Protection Potion
+						["description"] = "Qty:1\n\nPurchase on the AH or make your own(Alchemy)",
+					}),
+					i(22845, {	-- Major Arcane Protection Potion
+						["description"] = "Qty:1\n\nPurchase on the AH or make your own(Alchemy)",
+					}),
+					i(22847, {	-- Major Holy Protection Potion
+						["description"] = "Qty:1\n\nPurchase on the AH or make your own(Alchemy)",
+					}),
+					i(22844, {	-- Major Nature Protection Potion
+						["description"] = "Qty:1\n\nPurchase on the AH or make your own(Alchemy)",
+					}),
+					i(22846, {	-- Major Shadow Protection Potion
+						["description"] = "Qty:1\n\nPurchase on the AH or make your own(Alchemy)",
+					}),
+					i(8529, {	-- Noggenfogger Elixir
+						["description"] = "Qty:20\n\nPurchase from Sprinkle Noggenfogger in Tanaris",
+						["g"] = {
+							n(98844, {	-- Sprinkle Noggenfogger
+								["coord"] = { 51.2, 29.8, 71 },
+							}),
+						},
+					}),
+					n(37865, {	-- Perky Pug
+						["description"] = "Qty:1\n\nObtained from the Dungeons & Raids achievment \"Looking for Multitudes\"",
+					}),
+					i(40195, {	-- Pygmy Oil
+						["description"] = "Qty:20\n\nPurchase on the AH or make your own(Alchemy)",
+					}),
+					i(89373, {	-- Scotty's Lucky Coin
+						["description"] = "Qty:1\n\nObtained from killing Scotty in Townlong Steppes.\n\nScotty's behind the Mantid house in the middle of the tree, so either walk around the left side of the house, or fly just above the house down into the crevice.",
+						["g"] = {
+							n(66162, {	-- Scotty
+								["coord"] = { 21.6, 46.5, 388 },
+							}),
+						},
+					}),
+					i(955, {	-- Scroll of Intellect
+						["description"] = "Qty:1\n\nPurchase on the AH, make your own(Inscription), or buy in limited quantities from the vendors below:",
+						["g"] = {
+							n(1316, {	-- Adair Gilroy <Librarian>
+								["description"] = "Stormwind",
+								["coord"] = { 51.8, 74.8, 84 },	
+								["races"] = ALLIANCE_ONLY,
+							}),
+							n(958, {	-- Dawn Brightstar <Arcane Goods>
+								["description"] = "Elwyn Forest",
+								["coord"] = { 64.8, 69.2, 37 },	
+								["races"] = ALLIANCE_ONLY,
+							}),
+							n(16224, {	-- Rathis Tomber <Trade Supplies>
+								["description"] = "Ghostlands",
+								["coord"] = { 47.2, 28.6, 95 },	
+								["races"] = HORDE_ONLY,
+							}),
+							n(4581, {	-- Rathis Tomber <Trade Supplies>
+								["description"] = "Undercity",
+								["coord"] = { 77.6, 38.6, 90 },	
+								["races"] = HORDE_ONLY,
+							}),
+						},
+					}),
+					i(12820, {	-- Winterfall Firewater
+						["description"] = "Qty:1\n\nPurchase on the AH, or farm Winterfall furbolgs in Winterspring",
+					}),
+				},
+			}),
+			o(9999909, {	-- |cFFFFFFFFStep 2:|r Cavern of Consumption
+				["icon"] = "Interface\\Icons\\Achievement_Zone_Winterspring",
+				["description"] = "|cFFFFFFFFSTEP 2:|r |cff66ccffOnce you have gathered all of the consumables go to Caverns of Consumption on the North coast of Winterspring|r, and travel threw the cavern to the fire barrier.\n\n***Before you continue, make sure you have 10-15 mins to finish the remaining steps***",
 				["coords"] = {
-					{ 50.9, 2.3, 83 },	-- Oddly-Colored Egg
 					{ 57.2, 14.0, 12 },	-- Cave Entrance
 				},
-				--["model"] = "World\\Expansion06\\Doodads\\Dungeon\\Doodads\\7du_Nightmare_Egg01.mdx",
-				["modelScale"] = 2,
+			}),
+			o(9999910, {	-- |cFFFFFFFFStep 3:|r Fire Barrier
+				["icon"] = "Interface\\Icons\\INV_potion_124",
+				["description"] = "|cFFFFFFFFSTEP 3:|r |cff66ccffUse the Major Fire Protection Potion, and pass threw the Fire Barrior|r",
+			}),
+			o(9999911, {	-- |cFFFFFFFFStep 4:|r Prismatic Barrier
+				["icon"] = "Interface\\Icons\\INV_Potion_129",
+				["description"] = "|cFFFFFFFFSTEP 4:|r |cff66ccffUse the rest of the Major Potions and pass threw the Prismatic Barrior|r",
+			}),
+			o(9999912, {	-- |cFFFFFFFFStep 5:|r Diligent Watcher
+				["icon"] = "Interface\\Icons\\INV_Potion_83",
+				["description"] = "|cFFFFFFFFSTEP 5:|r |cff66ccffUse Noggenfogger Elixir until you turn into a Skeleton, then walk past the Diligent Watcher.|r",
+			}),
+			o(9999913, {	-- |cFFFFFFFFStep 6:|r Vine Barrier
+				["icon"] = "Interface\\Icons\\INV_Misc_Coin_17",
+				["description"] = "|cFFFFFFFFSTEP 6:|r |cff66ccffOn the right will be a Wall of Vines.  Use Scotty's Lucky Coin, pass threw the vines, and loot a Water Stone from the water.|r",
+			}),
+			o(9999914, {	-- |cFFFFFFFFStep 7:|r Water Barrier
+				["icon"] = "Interface\\Icons\\INV_Elemental_Mote_Water01",
+				["description"] = "|cFFFFFFFFSTEP 7:|r |cff66ccffUse the Water Stone, backtrack to the Blue Barrier and walk threw ever so slightly.|r",
+			}),
+			o(9999915, {	-- |cFFFFFFFFStep 8:|r Stone Golem
+				["icon"] = "Interface\\Icons\\INV_Drink_03",
+				["description"] = "|cFFFFFFFFSTEP 8:|r |cff66ccffUse the Dire Brew and walk past the Stone Watcher|r",
+			}),
+			o(9999916, {	-- |cFFFFFFFFStep 9:|r Ethereal Barrier
+				["icon"] = "Interface\\Icons\\INV_Drink_11",
+				["description"] = "|cFFFFFFFFSTEP 9:|r |cff66ccffGo up the ramp on the right to the Purple Transparent Barrier. Use Ethereal Oil and walk threw the barrier to the Strange Stone in the back.|r",
+			}),
+			o(9999917, {	-- |cFFFFFFFFStep 10:|r Strange Stone
+				["icon"] = "Interface\\Icons\\Spell_Shadow_Rune",
+				["description"] = "|cFFFFFFFFSTEP 10:|r |cff66ccffAt the Strange Stone, use the Gordok Ogre Suit & Wintefall Firewater.  Select the Strange Stones dialogue \"Stick arm in stone\" and recieve Sign of the Second debuff.|r",
+			}),
+			o(9999918, {	-- |cFFFFFFFFStep 11:|r Metal Gate
+				["icon"] = "Interface\\Icons\\INV_Potion_07",
+				["description"] = "|cFFFFFFFFSTEP 11:|r |cff66ccffBacktrack to the gate and use the Pygmy Oil until you turn into a Pygmy.  Walk threw the gate to the Wisdom Cube.|r",
+			}),
+			o(9999919, {	-- |cFFFFFFFFStep 12:|r Wisdom Cube
+				["icon"] = "Interface\\Icons\\Spell_Ice_Rune",
+				["description"] = "|cFFFFFFFFSTEP 12:|r |cff66ccffAt the Wisdom Cube summon Perky Pug and use the Little Princess Costume on it. Select the Wisdom Cubes dialogue \"Place Arm Beneath Cube\" and recieve Sign of the First debuff.|r",
+			}),
+			o(9999920, {	-- |cFFFFFFFFStep 13:|r Tarnished Plaque
+				["icon"] = "Interface\\Icons\\INV_Scroll_01",
+				["description"] = "|cFFFFFFFFSTEP 13a:|r |cff66ccffBacktrack down the ramp to the now opened gate. DO NOT JUMP OFF. Enter the room and on a pillar on the right will be a Tarnished Plaque.  Click on it.\n\nThe Tarnished Plaque reads:\n\n\"The greatest of achievments begins\nwith the smallest leap of the mind.\"\n\n|cFFFFFFFFSTEP 13b:|r After reading the Tarnished Plaque, click off at least one buff and use the Scroll of Intellect.|r",
+			}),
+			o(272487, {	-- |cFFFFFFFFStep 14:|r Oddly-Colored Egg	
+				--["modelID"] = 74529,
+				--["modelScale"] = 2,
 				["icon"] = "Interface\\Icons\\INV_Egg_09",
-				["description"] = "|cff3399ffSTEP 1:|r |cff66ccffObtain Major Arcane, Fire, Frost, Holy, Nature, & Shadow Protection Potions, 20x Noggenfogger Elixir (Sprinkle Noggenfogger - Tanaris - 51.2,29.8), Scotty's Lucky Coin (Scotty - Townlong Steppes - 21.8,46), Dire Brew (Dark Iron Brewer - BRD, talk to him til he passes out, loot Mug of Dire Brew), Ethereal Oil, Gordok Ogre Suit (Dire Maul West - ogre tannin - jump down free knot), Winterfall Firewater (Winterfall mobs - Winterspring), 20x Pygmy Oil, Perky Pug, Little Princess Costume (Pet Vendor - Garrison), Scroll of Intellect (level 1 version)|r \n|cff3399ffSTEP 2:|r |cff66ccffGo to Cavern of Consumption (57,16) North Coast of Winterspring|r \n|cff3399ffSTEP 3:|r |cff66ccffFire Barrior - Use Major Fire Protection Potion |r\n|cff3399ffSTEP 4:|r |cff66ccffPrismatic Barrior - Use rest of Major Potions |r\n|cff3399ffSTEP 5:|r |cff66ccffDiligent Watcher - Use Noggenfogger Elixir - Skeleton - Walk Past |r\n|cff3399ffSTEP 6:|r |cff66ccffWall of Vines - Use Scotty's Lucky Coin - Loot Water Stone in water. |r\n|cff3399ffSTEP 7:|r |cff66ccffUse Water Stone - backtrack to Blue Barrier - Walk Past |r\n|cff3399ffSTEP 8:|r |cff66ccffStone Watcher - Use Dire Brew - Walk Past |r\n|cff3399ffSTEP 9:|r |cff66ccffThree Paths - Up ramp to Purple Transparent Barrier - Use Ethereal Oil - Walk Past |r\n|cff3399ffSTEP 10:|r |cff66ccffStrange Stone - Use Gordok Ogre Suit & Wintefall Firewater - Stick arm in stone - Sign of the Second Buff |r\n|cff3399ffSTEP 11:|r |cff66ccffBacktrack to gate - Use Pygmy oil - Pygmy - Walk Past to Wisdom Cube |r\n|cff3399ffSTEP 12:|r |cff66ccffSummon Perky Pug - Use Little Princess Costume - Place Arm Beneath Cube - Sign of the First Buff |r\n|cff3399ffSTEP 13:|r |cff66ccffBacktrack to now opened gate. DO NOT JUMP OFF - Walk Around - Click Tarnished Plague |r\n|cff3399ffSTEP 14:|r |cff66ccffClick off Winterfall Firewater Buff - use Scroll of intellect - DO NOT JUMP IN WATER - Walk to far edge of water on right to slope to walk to egg|r",
+				["questID"] = 48022,
+				["description"] = "|cFFFFFFFFSTEP 14:|r |cff66ccffDO NOT JUMP IN WATER. Walk to the edge of water on the right, behind the plaque, where the cavern slopes down into the water.  Walk across the water and loot the egg.|r\n\nCongrats on your new pet!!! \n\nDon't forget to turn off debug mode now that you are done.",  -- |cff3399ff -- blue color
 				["g"] = {
 					i(142223),	-- Sun Darter Hatchling (PET!)
 				},
