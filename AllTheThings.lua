58f35c114a80b3ef1af44a383c804df5ad8a090e
Crieve-Sargeras <dylanfortune@live.com>
2019-08-02 18:21:22 -0700

    Added the Vol'dun POI ID.

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -12041,7 +12041,7 @@ app:GetWindow("WorldQuests", UIParent, function(self)
 					875,	-- Zandalar
 					{
 						-- { 863, ???, { 54135, 54136 }},	-- Nazmir (Romp in the Swamp [H] / March on the Marsh [A])
-						-- { 864, ???, { 53885, 54134 }},	-- Voldun (Isolated Victory [H] / Many Fine Heroes [A])
+						{ 864, 5970, { 53885, 54134 }},	-- Voldun (Isolated Victory [H] / Many Fine Heroes [A])
 						-- { 862, ???, { 53883, 54138 }},	-- Zuldazar (Shores of Zuldazar [H] / Ritual Rampage [A])
 					}
 				},
