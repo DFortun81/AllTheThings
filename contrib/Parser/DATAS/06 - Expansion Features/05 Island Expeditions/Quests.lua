648a9bf40e316e4c78a4dd36943fa4fa40a52bbc
Gaulish <l2pwncomputers@hotmail.com>
2019-08-06 15:21:59 -0600

    IE quest items.

--- contrib/Parser/DATAS/06 - Expansion Features/05 Island Expeditions/Quests.lua
+++ contrib/Parser/DATAS/06 - Expansion Features/05 Island Expeditions/Quests.lua
@@ -9,11 +9,235 @@ _.ExpansionFeatures =
 		["groups"] = {
 			n(-17, {	-- Quests
 				["groups"] = {
-					{	-- Ancient Crypt Key [Both]
-						["itemID"] = 163470,	-- Ancient Crypt Key (Item)
-						--["questID"] = 53160,	-- Ancient Crypt Key (Quest)
-						["repeatable"] = true,
-					},
+					i(163470, {	 -- Ancient Crypt Key
+						["g"] = {
+							q(53160, {	-- Ancient Crypt Key
+								["qg"] = 61962,	-- Lorewalker Cho
+								["coord"] = { 83.2, 29.6, 390 },
+							}),
+							crit(19, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163478, {	 -- Battered Twilight Scale
+						["g"] = {
+							q(53171, {	-- Battered Twilight Scale
+								["qg"] = 132888,	-- Eryna
+								["coord"] = { 20.2, 54.6, 241 },
+							}),
+							crit(11, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163472, {	 -- Bloodwake Drinking Horn
+						["g"] = {
+							q(53163, {	-- Bloodwake Drinking Horn
+								["qg"] = 93231,	-- Vydhar
+								["coord"] = { 68.4, 54.2, 634 },
+							}),
+							crit(17, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163482, {	 -- Broken Compass
+						["g"] = {
+							q(53177, {	-- Broken Compass
+								["qg"] = 908,	-- Flora Silverwind <Herbalism Trainer>
+								["coord"] = { 42.0, 74.6, 210 },
+							}),
+							crit(7, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163485, {	 -- Druidic Fur
+						["g"] = {
+							q(53181, {	-- Druidic Fur
+								["qg"] = 12042,	-- Loganaar <Druid Trainer>
+								["coord"] = { 52.6, 40.6, 80 },
+							}),
+							crit(4, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163476, {	 -- Elemental Core
+						["g"] = {
+							q(53169, {	-- Elemental Core
+								["qg"] = 13278,	-- Duke Hydraxis
+								["coord"] = { 92.7, 42.0, 76 },	-- Verify Coords/npcid
+							}),
+							crit(13, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163481, {	 -- Glittering Scarab
+						["g"] = {
+							q(53176, {	-- Glittering Scarab
+								["qg"] = 46134,	-- High Commander Kamses
+								["coord"] = { 58.8, 46.2, 249 },
+							}),
+							crit(8, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163474, {	 -- Icetusk Fur Cloak
+						["g"] = {
+							q(53167, {	-- Icetusk Fur Cloak
+								["qg"] = 26245,	-- Tua'kea
+								["coord"] = { 48.0, 76.0, 115 },
+							}),
+							crit(15, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163475, {	 -- Magical Spyglass
+						["g"] = {
+							q(53168, {	-- Magical Spyglass
+								["qg"] = 2496,	-- Baron Revilgaz
+								["coord"] = { 41.0, 73.0, 210 },
+							}),
+							crit(14, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163480, {	 -- Murloc Eye
+						["g"] = {
+							q(53174, {	-- Murloc Eye
+								["qg"] = 98067,	-- King Mrgl-Mrgl <D.E.H.T.A.>
+								["coords"] = {
+									{ 42.6, 10.8, 650 },
+									{ 49.6, 15.8, 650 },
+								},
+							}),
+							crit(9, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163466, {	 -- Notched Coin
+						["g"] = {
+							q(53155, {	-- Notched Coin
+								["qg"] = 64365,	-- Mili Wanderbrew <Apprentice Brewmaster>
+								["coord"] = { 55.2, 19.2, 371 },
+							}),
+							crit(20, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163483, {	 -- Razorfin Hunting Spear
+						["g"] = {
+							q(53178, {	-- Razorfin Hunting Spear
+								["qg"] = 58278,	-- Tired Shushen <Waterspeaker>
+								["coord"] = { 29.6, 39.0, 418 },
+							}),
+							crit(6, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163471, {	 -- Ring of the Reefs
+						["g"] = {
+							q(53161, {	-- Ring of the Reefs
+								["qg"] = 106904,	-- Valdemar Stormseeker <Valarjar Emissary>
+								["coord"] = { 60.2, 51.2, 634 },
+							}),
+							crit(18, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163473, {	 -- Sonic Stone
+						["g"] = {
+							q(53164, {	-- Sonic Stone
+								["qg"] = 61584,	-- Sentinel Commander Qipan
+								["coord"] = { 39.4, 62.0, 388 },
+							}),
+							crit(16, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163486, {	 -- Strangling Root
+						["g"] = {
+							q(53182, {	-- Strangling Root
+								["qg"] = 11832,	-- Keeper Remulos
+								["coord"] = { 36.2, 41.8, 80 },
+							}),
+							crit(3, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163477, {	 -- Unscarred Black Scale
+						["g"] = {
+							q(53170, {	-- Unscarred Black Scale
+								["qg"] = 66306,	-- Blacktalon Agent <Servant of the Black Prince>
+								["coord"] = { 47.8, 68.8, 36 },	-- Verify map/npc id
+							}),
+							crit(12, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163479, {	 -- Venture Co. Business Plan
+						["g"] = {
+							q(53172, {	-- Venture Co. Business Plan
+								["description"] = "Pats along the road between The Crossroads and Ratchet",
+								["qg"] = 3658,	-- Lizzarik <Weapon Dealer>
+								["coords"] = {
+									{ 50.2, 59.2, 10 },
+									{ 66.8, 72.6, 10 },
+								},
+							}),
+							crit(10, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163484, {	 -- Vorus'arak's Carapace
+						["g"] = {
+							q(53180, {	-- Vorus'arak's Carapace
+								["qg"] = 26653,	-- Kilix the Unraveler
+								["coord"] = { 26.2, 50.6, 115 },
+							}),
+							crit(5, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163488, {	 -- Weathered Pamphlet
+						["g"] = {
+							q(53184, {	-- Weathered Pamphlet
+								["qg"] = 132832,	-- Jordle Flangebender
+								["coord"] = { 52.0, 30.0, 71 },
+							}),
+							crit(1, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					i(163487, {	 -- Wriggling Mass
+						["g"] = {
+							q(53183, {	-- Wriggling Mass
+								["description"] = "Turn in @ old or new Silithus",
+								["qg"] = 132886,	-- Twilight Hermit
+								["coord"] = { 40.0, 76.6, 81 },
+							}),
+							crit(2, {
+								["achievementID"] = 13127,	-- Tell Me A Tale
+							}),
+						},
+					}),
+					-- Rep Items
 					i(163217, {	-- Azeroth's Tear [Both]
 						["description"] = "|CFFFF0000Turn this into Magni Bronzebeard in Silithus @ 42.2, 44.2 for Champions of Azeroth rep.|r",
 						["g"] = {
@@ -35,11 +259,6 @@ _.ExpansionFeatures =
 							}),
 						},
 					}),
-					{	-- Druidic Fur [Both]
-						["itemID"] = 163485,	-- Druidic Fur (Item)
-						--["questID"] = 53181,	-- Druidic Fur (Quest)
-						["repeatable"] = true,
-					},
 					i(163614, {	-- Exotic Spices [Alliance]
 						["description"] = "|CFFFF0000Turn this into Xun Xun Sweetflower in Drustvar @ 20.6, 45.6 for Order of Embers rep.|r",
 						["g"] = {
