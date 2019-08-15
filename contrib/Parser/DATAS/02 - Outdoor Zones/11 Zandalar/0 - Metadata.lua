580fb8e7961181856d5bfe200b00af67ed8b4bf5
Gaulish <l2pwncomputers@hotmail.com>
2019-08-15 03:59:46 -0600

    Cosmetic stuff.

--- contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/0 - Metadata.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/0 - Metadata.lua
@@ -6,7 +6,7 @@ _.Zones =
 	m(875, { 	-- Zandalar   [Possibly 947]
 		
 		["achievementID"] = 12988,
-		["icon"] = "Interface\\Icons\\inv_axe_2h_zandalarquest_b_01",
+		["icon"] = "Interface\\Icons\\Spell_Arcane_PortalZandalar",
 		["description"] = "|cff66ccffWith the advent of the Cataclysm, the island was thrown into chaos, as large sections of its capital were lost to the sea and the jungle of Nazmir began to sink, transforming it into a swamp. This would lead the Zandalari themselves to attempt to forge a new nation in other parts of the world to escape their compromised home, though they were largely unsuccessful at the time.\n\nNot as strong as in times past, beset by internal troubles within Zandalar itself, and amidst another war with the Alliance, the trolls of Zandalar now seek fresh allies, setting the ancient island up as a major theater for the impending Battle for Azeroth.|r",
 	}),
 };
--- contrib/Parser/DATAS/02 - Outdoor Zones/12 Nazjatar/Zone Drop.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/12 Nazjatar/Zone Drop.lua
@@ -604,52 +604,56 @@ _.Zones =
 			i(169497),	-- Recipe: Superior Battle Potion of Intellect (Rank 3)
 			i(169498),  -- Recipe: Superior Battle Potion of Stamina (Rank 3)
 			i(169496),	-- Recipe: Superior Steelskin Potion (Rank 3)
+		}),
 		--	Gear
-			i(170116),	-- Darkened Slicer
-			i(169402),	-- Deeptide Amice
-			i(169403),	-- Deeptide Cinch
-			i(169400),	-- Deeptide Cover
-			i(169404),	-- Deeptide Cuffs
-			i(169435),	-- Deeptide Drape
-			i(169399),	-- Deeptide Gloves
-			i(169401),	-- Deeptide Legwraps
-			i(169425),	-- Deeptide Robe
-			i(170201),	-- Deeptide Scroll
-			i(169398),	-- Deeptide Slippers
-			i(170114),	-- Flanged Mace
-			i(170120),	-- Handmaiden's Longstaff
-			i(170115),	-- Massive Viridian Claw
-			i(169431),	-- Reefwalker Cape
-			i(169419),	-- Reefwalker Chain
-			i(169416),	-- Reefwalker Coif
-			i(169420),	-- Reefwalker Coils
-			i(169415),	-- Reefwalker Handlers
-			i(169413),	-- Reefwalker Hauberk
-			i(169418),	-- Reefwalker Monnion
-			i(169414),	-- Reefwalker Striders
-			i(169417),	-- Reefwalker Trousers
-			i(169412),	-- Slithershell Armwraps
-			i(169411),	-- Slithershell Belt
-			i(169406),	-- Slithershell Boots
-			i(169434),	-- Slithershell Cloak
-			i(169409),	-- Slithershell Leggings
-			i(169410),	-- Slithershell Mantle
-			i(169407),	-- Slithershell Mitts
-			i(169408),	-- Slithershell Tricorne
-			i(169405),	-- Slithershell Vest
-			i(170119),	-- Slithershell Warglaive
-			i(170117),	-- Wavebinder's Rod
-			i(169428),	-- Wavecrash Buckle
-			i(169421),	-- Wavecrash Chestplate
-			i(169423),	-- Wavecrash Crushers
-			i(169433),	-- Wavecrash Greatcloak
-			i(169426),	-- Wavecrash Greaves
-			i(169424),	-- Wavecrash Headguard
-			i(170112),	-- Wavecrash Polearm
-			i(169427),	-- Wavecrash Shoulderplates
-			i(169422),	-- Wavecrash Smashers
-			i(169429),	-- Wavecrash Wristguards
-			i(170118),  -- Centurion's Shortsword
+		n(0, {	-- Zone Drop
+			["g"] = bubbleDown({["bonusID"] = 4796, ["lvl"] = 120}, {
+				i(170116),	-- Darkened Slicer
+				i(169402),	-- Deeptide Amice
+				i(169403),	-- Deeptide Cinch
+				i(169400),	-- Deeptide Cover
+				i(169404),	-- Deeptide Cuffs
+				i(169435),	-- Deeptide Drape
+				i(169399),	-- Deeptide Gloves
+				i(169401),	-- Deeptide Legwraps
+				i(169425),	-- Deeptide Robe
+				i(170201),	-- Deeptide Scroll
+				i(169398),	-- Deeptide Slippers
+				i(170114),	-- Flanged Mace
+				i(170120),	-- Handmaiden's Longstaff
+				i(170115),	-- Massive Viridian Claw
+				i(169431),	-- Reefwalker Cape
+				i(169419),	-- Reefwalker Chain
+				i(169416),	-- Reefwalker Coif
+				i(169420),	-- Reefwalker Coils
+				i(169415),	-- Reefwalker Handlers
+				i(169413),	-- Reefwalker Hauberk
+				i(169418),	-- Reefwalker Monnion
+				i(169414),	-- Reefwalker Striders
+				i(169417),	-- Reefwalker Trousers
+				i(169412),	-- Slithershell Armwraps
+				i(169411),	-- Slithershell Belt
+				i(169406),	-- Slithershell Boots
+				i(169434),	-- Slithershell Cloak
+				i(169409),	-- Slithershell Leggings
+				i(169410),	-- Slithershell Mantle
+				i(169407),	-- Slithershell Mitts
+				i(169408),	-- Slithershell Tricorne
+				i(169405),	-- Slithershell Vest
+				i(170119),	-- Slithershell Warglaive
+				i(170117),	-- Wavebinder's Rod
+				i(169428),	-- Wavecrash Buckle
+				i(169421),	-- Wavecrash Chestplate
+				i(169423),	-- Wavecrash Crushers
+				i(169433),	-- Wavecrash Greatcloak
+				i(169426),	-- Wavecrash Greaves
+				i(169424),	-- Wavecrash Headguard
+				i(170112),	-- Wavecrash Polearm
+				i(169427),	-- Wavecrash Shoulderplates
+				i(169422),	-- Wavecrash Smashers
+				i(169429),	-- Wavecrash Wristguards
+				i(170118),  -- Centurion's Shortsword
+			}),
 		}),
 	}),
 };
