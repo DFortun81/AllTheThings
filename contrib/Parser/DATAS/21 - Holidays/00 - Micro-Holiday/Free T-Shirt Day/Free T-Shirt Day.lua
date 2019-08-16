e579ea5b6d2c4d6cbbb83daccbdd4e5d0492898d
sadidorf <sadidorf@gmail.com>
2019-08-16 05:58:46 +0200

    wrong order ...

--- contrib/Parser/DATAS/21 - Holidays/00 - Micro-Holiday/Free T-Shirt Day/Free T-Shirt Day.lua
+++ contrib/Parser/DATAS/21 - Holidays/00 - Micro-Holiday/Free T-Shirt Day/Free T-Shirt Day.lua
@@ -7,9 +7,73 @@ _.Holidays =
 		["u"] = 22,	-- Micro-Holiday Filter
 		["maps"] = {
 			85,	-- Orgrimmar
-			84,	-- Sormwind
+			84,	-- Stormwind
 		},
 		["g"] = {
+			n(-16, {	-- Rares
+				n(145826, {	-- Orgrimmar Entertainer
+					["description"] = "This NPC wanders around in Ogrimmar and shoots T-Shirts on the ground where you can pick them up.",
+					["races"] = HORDE_ONLY,
+					["u"] = 22,
+					["g"] = {
+						un(22, i(167183)),	-- Amber Filigreed Doublet
+						un(22, i(167187)),	-- Amber Filigreed Shirt
+						un(22, i(167197)),	-- Antiseptic-Soaked Dressing
+						un(22, i(167190)),	-- Blue Martial Shirt
+						un(22, i(167081)),	-- Bold Yellow Shirt
+						un(22, i(167182)),	-- Cerulean Filigreed Doublet
+						un(22, i(167186)),	-- Cerulean Filigreed Shirt
+						un(22, i(167178)),	-- Common Brown Shirt
+						un(22, i(167177)),	-- Common Gray Shirt
+						un(22, i(167179)),	-- Common White Shirt
+						un(22, i(167180)),	-- Ebon Filigreed Doublet
+						un(22, i(167185)),	-- Ebon Filigreed Shirt
+						un(22, i(167192)),	-- Embroidered Shirt
+						un(22, i(165657)),	-- Free T-Shirt
+						un(22, i(167181)),	-- Golden Filigreed Doublet
+						un(22, i(167189)),	-- Golden Filigreed Shirt
+						un(22, i(167191)),	-- Green Martial Shirt
+						un(22, i(167194)),	-- Purple Martial Shirt
+						un(22, i(167195)),	-- Red Martial Shirt
+						un(22, i(167184)),	-- Scarlet Filigreed Doublet
+						un(22, i(167188)),	-- Scarlet Filigreed Shirt
+						un(22, i(167082)),	-- Stylish Black Shirt
+						un(22, i(167196)),	-- Wound Dressing
+						un(22, i(167193)),	-- Yellow Martial Shirt
+					},
+				}),
+				n(150942, {	-- Stormwind Entertainer
+					["description"] = "This NPC wanders around in Stormwind and shoots T-Shirts on the ground where you can pick them up.",
+					["races"] = ALIIANCE_ONLY,
+					["u"] = 22,
+					["g"] = {
+						un(22, i(167183)),	-- Amber Filigreed Doublet
+						un(22, i(167187)),	-- Amber Filigreed Shirt
+						un(22, i(167197)),	-- Antiseptic-Soaked Dressing
+						un(22, i(167190)),	-- Blue Martial Shirt
+						un(22, i(167081)),	-- Bold Yellow Shirt
+						un(22, i(167182)),	-- Cerulean Filigreed Doublet
+						un(22, i(167186)),	-- Cerulean Filigreed Shirt
+						un(22, i(167178)),	-- Common Brown Shirt
+						un(22, i(167177)),	-- Common Gray Shirt
+						un(22, i(167179)),	-- Common White Shirt
+						un(22, i(167180)),	-- Ebon Filigreed Doublet
+						un(22, i(167185)),	-- Ebon Filigreed Shirt
+						un(22, i(167192)),	-- Embroidered Shirt
+						un(22, i(165657)),	-- Free T-Shirt
+						un(22, i(167181)),	-- Golden Filigreed Doublet
+						un(22, i(167189)),	-- Golden Filigreed Shirt
+						un(22, i(167191)),	-- Green Martial Shirt
+						un(22, i(167194)),	-- Purple Martial Shirt
+						un(22, i(167195)),	-- Red Martial Shirt
+						un(22, i(167184)),	-- Scarlet Filigreed Doublet
+						un(22, i(167188)),	-- Scarlet Filigreed Shirt
+						un(22, i(167082)),	-- Stylish Black Shirt
+						un(22, i(167196)),	-- Wound Dressing
+						un(22, i(167193)),	-- Yellow Martial Shirt
+					},
+				}),
+			}),
 			n(-2, {	-- Vendors
 				n(150945, {	-- Selis Silksong
 					["coord"] = { 67.7, 27.7, 111 },
@@ -65,72 +129,6 @@ _.Holidays =
 					["u"] = 22,
 					["g"] = {
 						un(22, i(167229)),	-- Shirtsi's Cloth Shirt
-					},
-				}),
-			}),
-			n(-16, {	-- Rares
-				n(145826, {	-- Orgrimmar Entertainer
-					["description"] = "This NPC wanders around in Ogrimmar and shoots T-Shirts on the ground where you can pick them up.",
-					["races"] = HORDE_ONLY,
-					["maps"] = { 85 },	-- Orgrimmar
-					["u"] = 22,
-					["g"] = {
-						un(22, i(167183)),	-- Amber Filigreed Doublet
-						un(22, i(167187)),	-- Amber Filigreed Shirt
-						un(22, i(167197)),	-- Antiseptic-Soaked Dressing
-						un(22, i(167190)),	-- Blue Martial Shirt
-						un(22, i(167081)),	-- Bold Yellow Shirt
-						un(22, i(167182)),	-- Cerulean Filigreed Doublet
-						un(22, i(167186)),	-- Cerulean Filigreed Shirt
-						un(22, i(167178)),	-- Common Brown Shirt
-						un(22, i(167177)),	-- Common Gray Shirt
-						un(22, i(167179)),	-- Common White Shirt
-						un(22, i(167180)),	-- Ebon Filigreed Doublet
-						un(22, i(167185)),	-- Ebon Filigreed Shirt
-						un(22, i(167192)),	-- Embroidered Shirt
-						un(22, i(165657)),	-- Free T-Shirt
-						un(22, i(167181)),	-- Golden Filigreed Doublet
-						un(22, i(167189)),	-- Golden Filigreed Shirt
-						un(22, i(167191)),	-- Green Martial Shirt
-						un(22, i(167194)),	-- Purple Martial Shirt
-						un(22, i(167195)),	-- Red Martial Shirt
-						un(22, i(167184)),	-- Scarlet Filigreed Doublet
-						un(22, i(167188)),	-- Scarlet Filigreed Shirt
-						un(22, i(167082)),	-- Stylish Black Shirt
-						un(22, i(167196)),	-- Wound Dressing
-						un(22, i(167193)),	-- Yellow Martial Shirt
-					},
-				}),
-				n(150942, {	-- Stormwind Entertainer
-					["description"] = "This NPC wanders around in Stormwind and shoots T-Shirts on the ground where you can pick them up.",
-					["races"] = ALIIANCE_ONLY,
-					["maps"] = { 84 },	-- Stormwind
-					["u"] = 22,
-					["g"] = {
-						un(22, i(167183)),	-- Amber Filigreed Doublet
-						un(22, i(167187)),	-- Amber Filigreed Shirt
-						un(22, i(167197)),	-- Antiseptic-Soaked Dressing
-						un(22, i(167190)),	-- Blue Martial Shirt
-						un(22, i(167081)),	-- Bold Yellow Shirt
-						un(22, i(167182)),	-- Cerulean Filigreed Doublet
-						un(22, i(167186)),	-- Cerulean Filigreed Shirt
-						un(22, i(167178)),	-- Common Brown Shirt
-						un(22, i(167177)),	-- Common Gray Shirt
-						un(22, i(167179)),	-- Common White Shirt
-						un(22, i(167180)),	-- Ebon Filigreed Doublet
-						un(22, i(167185)),	-- Ebon Filigreed Shirt
-						un(22, i(167192)),	-- Embroidered Shirt
-						un(22, i(165657)),	-- Free T-Shirt
-						un(22, i(167181)),	-- Golden Filigreed Doublet
-						un(22, i(167189)),	-- Golden Filigreed Shirt
-						un(22, i(167191)),	-- Green Martial Shirt
-						un(22, i(167194)),	-- Purple Martial Shirt
-						un(22, i(167195)),	-- Red Martial Shirt
-						un(22, i(167184)),	-- Scarlet Filigreed Doublet
-						un(22, i(167188)),	-- Scarlet Filigreed Shirt
-						un(22, i(167082)),	-- Stylish Black Shirt
-						un(22, i(167196)),	-- Wound Dressing
-						un(22, i(167193)),	-- Yellow Martial Shirt
 					},
 				}),
 			}),
