92232a51e29066880d0834081e93cdab1a332556
slumber <slumber@love.com>
2019-08-18 17:25:16 -0500

    - added 'isLimited' to ashenvale vendors

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Ashenvale/Vendors.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Ashenvale/Vendors.lua
@@ -10,24 +10,36 @@ _.Zones =
 					["coord"] = { 34.6, 71.2, 63 },
 					["races"] = ALLIANCE_ONLY,
 					["g"] = {
-						i(11305),	-- Dense Shortbow
+						i(11305, {	-- Dense Shortbow
+							["isLimited"] = true,
+						}),
 					},
 				}),
 				n(34303, {	-- Dagrun Ragehammer
 					["coord"] = { 11.6, 35.2, 63 },
 					["races"] = HORDE_ONLY,
 					["g"] = {
-						i(4778),	-- Heavy Spiked Mace
-						i(4777),	-- Ironwood Maul
+						i(4778, {	-- Heavy Spiked Mace
+							["isLimited"] = true,
+						}),
+						i(4777, {	-- Ironwood Maul
+							["isLimited"] = true,
+						}),
 					},
 				}),
 				n(3954,  {	-- Dalria
 					["coord"] = { 35.0, 52.0, 63 },
 					["races"] = ALLIANCE_ONLY,
 					["g"] = {
-						i(20855),	-- Design: Wicked Moonstone Ring
-						i(11101),	-- Formula: Enchant Bracer - Lesser Strength
-						i(11039),	-- Formula: Enchant Cloak - Minor Agility
+						i(20855, {	-- Design: Wicked Moonstone Ring
+							["isLimited"] = true,
+						}),
+						i(11101, {	-- Formula: Enchant Bracer - Lesser Strength
+							["isLimited"] = true,
+						}),
+						i(11039, {	-- Formula: Enchant Cloak - Minor Agility
+							["isLimited"] = true,
+						}),
 					},
 				}),
 				n(34601, {	-- Harlown Darkweave
@@ -35,14 +47,18 @@ _.Zones =
 					["coord"] = { 18.2, 60.0, 63 },
 					["races"] = ALLIANCE_ONLY,
 					["g"] = {
-						i(7361),	-- Pattern: Herbalist's Gloves
+						i(7361, {	-- Pattern: Herbalist's Gloves
+							["isLimited"] = true,
+						}),
 					},
 				}),
 				n(3958,  {	-- Lardan <Leatherworking Supplies>
 					["coord"] = { 34.8, 49.8, 63 },
 					["races"] = ALLIANCE_ONLY,
 					["g"] = {
-						i(5973),	-- Pattern: Barbaric Leggings
+						i(5973, {	-- Pattern: Barbaric Leggings
+							["isLimited"] = true,
+						}),
 					},
 				}),
 				n(12962, {	-- Wik'Tar <Fish Merchant & Supplies>
