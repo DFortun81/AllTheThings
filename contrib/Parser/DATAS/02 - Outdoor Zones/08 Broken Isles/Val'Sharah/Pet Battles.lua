d62b58638e3b1a631dd8ff55831145917493b621
Pr3vention <Pr3vention@gmail.com>
2019-08-30 00:50:08 -0500

    Added quest chain to raise a Sharptalon Hatchling pet into a Viridian Sharptalon mount

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Val'Sharah/Pet Battles.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Val'Sharah/Pet Battles.lua
@@ -21,14 +21,83 @@ _.Zones =
 				p(1735), 	-- Terror Larva
 				p(1737), 	-- Vale Flitter
 				n(115740, {	-- Orphaned Sharptalon
-					["description"] = "1. Buy Dried Bilberries from Fialla Sweetberry in Dalaran.\n2. Kill Sharptalon Matriarch.\n3. /target Orphaned Sharptalon\n4. Feed Orphaned Sharptalon Dried Bilberries.\n5. Enjoy new Sharptalon Hatchling|r",
+					["description"] = "1. Buy Dried Bilberries from Fialla Sweetberry in Dalaran.\n2. Kill Sharptalon Matriarch.\n3. /target Orphaned Sharptalon\n4. Feed Orphaned Sharptalon Dried Bilberries.\n5. Enjoy new Sharptalon Hatchling! Do one quest each day for a mount!|r",
 					["groups"] = {
-						p(1976), 	-- Sharptalon Hatchling
-						q(45007, {	-- Sharptalon Bonding
-							i(142496),	-- Dirty Spoon
-						}),
-						q(45019, {	-- Sharptalon Reunion
-							i(137580),	-- Viridian Sharptalon
+						p(1976, {	-- Sharptalon Hatchling
+							q(44997, {	-- Allies in Val'sharah
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 44995,	-- Hunting Lesson: Spring Strider
+							}),
+							q(45001, {	-- Destructive Prey
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 44999,	-- Sharptalon Team Up
+							}),
+							q(45009, {	-- Hunting Lesson: Auburn Ringtail
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 45007,	-- Sharptalon Bonding
+							}),
+							q(44995, {	-- Hunting Lesson: Spring Strider
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuests"] = {
+									44992,	-- The Smell of Night Elves
+									44994,	-- The Smell of Undead
+								},
+							}),
+							q(45003, {	-- Hunting Lesson: Terror Larva
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 45001,	-- Destructive Prey
+							}),
+							q(45013, {	-- Hunting Lesson: Vale Flitter
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 45011,	-- Teamwork Lesson: Dresaron
+							}),
+							q(44989, {	-- Raising Your Sharptalon
+								["qg"] = 115786,	-- Sharptalon Hatchling
+							}),
+							q(45007, {	-- Sharptalon Bonding
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 45005,	-- The Uncongenial Faction
+								["g"] = {
+									i(142496),	-- Dirty Spoon
+								},
+							}),
+							q(45019, {	-- Sharptalon Reunion
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 45017,	-- Sharptalon Team Rumble
+								["g"] = {
+									i(137580),	-- Viridian Sharptalon
+								},
+							}),
+							q(45017, {	-- Sharptalon Team Rumble
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 45015,	-- Teamwork Lesson: Xavius
+							}),
+							q(44999, {	-- Sharptalon Team Up
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 44997,	-- Allies in Val'sharah
+							}),
+							q(45011, {	-- Teamwork Lesson: Dresaron
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 45009,	-- Hunting Lesson: Auburn Ringtail
+							}),
+							q(45015, {	-- Teamwork Lesson: Xavius
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 45013,	-- Hunting Lesson: Vale Flitter
+							}),
+							q(44992, {	-- The Smell of Night Elves
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["races"] = ALLIANCE_ONLY,
+								["sourceQuest"] = 44989,	-- Raising Your Sharptalon
+							}),
+							q(44994, {	-- The Smell of Undead
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["races"] = HORDE_ONLY,
+								["sourceQuest"] = 44989,	-- Raising Your Sharptalon
+							}),
+							q(45005, {	-- The Uncongenial Faction
+								["qg"] = 115786,	-- Sharptalon Hatchling
+								["sourceQuest"] = 45003,	-- Hunting Lesson: Terror Larva
+							}),
 						}),
 					},
 				}),	
