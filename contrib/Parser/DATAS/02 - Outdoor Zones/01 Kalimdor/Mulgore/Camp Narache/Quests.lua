7ab3352a987ec6c50dbc27c6187172be20ef1477
Pr3vention <Pr3vention@gmail.com>
2019-08-30 17:29:58 -0500

    added some legacy quests in camp narache

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Mulgore/Camp Narache/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Mulgore/Camp Narache/Quests.lua
@@ -7,6 +7,17 @@ _.Zones =
 		m(7, {	-- Mulgore
 			m(462, {	-- Camp Narache
 				n(-17, {	-- Quests
+					q(752, {	-- A Humble Task
+						["u"] = 40,
+						["qg"] = 2981,	-- Chief Hawkwind
+						["races"] = HORDE_ONLY,
+					}),
+					q(753, {	-- A Humble Task
+						["u"] = 40,
+						["qg"] = 2991,	-- Greatmother Hawkwind
+						["races"] = HORDE_ONLY,
+						["sourceQuest"] = 752,	-- A Humble Task
+					}),
 					q(14461, {	-- Feed of Evil
 						["qg"] = 36694,	-- Adana Thunderhorn
 						["coord"] = { 31.0, 50.6, 462 },
@@ -52,6 +63,12 @@ _.Zones =
 							i(57792),	-- Bag of Thorns
 						},
 					}),
+					q(757, {	-- Rite of Strength
+						["u"] = 40,
+						["qg"] = 2982,	-- Seer Graytongue
+						["races"] = HORDE_ONLY,
+						["sourceQuest"] = 755,	-- Rites of the Earthmother
+					}),
 					q(14452, {	-- Rite of Strength
 						["qg"] = 2980,	-- Grull Hawkwind
 						["coord"] = { 39.4, 37.0, 462 },
@@ -63,6 +80,18 @@ _.Zones =
 						["coord"] = { 15.4, 30.4, 462 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 23733,	-- Rites of the Earthmother
+					}),
+					q(755, {	-- Rites of the Earthmother
+						["u"] = 40,
+						["qg"] = 2981,	-- Chief Hawkwind
+						["races"] = HORDE_ONLY,
+						["sourceQuest"] = 753,	-- A Humble Task
+					}),
+					q(763, {	-- Rites of the Earthmother
+						["u"] = 40,
+						["qg"] = 2981,	-- Chief Hawkwind
+						["races"] = HORDE_ONLY,
+						["sourceQuest"] = 757,	-- Rite of Strength
 					}),
 					q(23733, {	-- Rites of the Earthmother
 						["qg"] = 2981,	-- Chief Hawkwind
