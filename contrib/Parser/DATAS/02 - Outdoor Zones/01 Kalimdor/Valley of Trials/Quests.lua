291d6cd08ef04b7766e37ea2a3aeabc8404780f1
Pr3vention <Pr3vention@gmail.com>
2019-09-01 11:15:31 -0500

    Valley of Trials minor formatting

--- contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Valley of Trials/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Valley of Trials/Quests.lua
@@ -7,103 +7,89 @@ _.Zones =
 		m(1, {	-- Durotar
 			m(461, {	-- Valley of Trials
 				n(-17, {	-- Quests
-					{	-- Back to the Den
-						["questID"] = 25130,
+					q(25130, {	-- Back to the Den
 						["provider"] = { "n", 3287 },	-- Hana'zua
 						["coord"] = { 34.6, 44.2, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25129,	-- Sarkoth
-					},
-					{	-- Burning Blade Medallion
-						["questID"] = 25132,
+					}),
+					q(25132, {	-- Burning Blade Medallion
 						["provider"] = { "n", 3145 },	-- Zureetha Fargaze
 						["coord"] = { 45.8, 63.5, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25131,	-- Vile Familiars
-					},
-					{	-- Cutting Teeth
-						["questID"] = 25126,
+					}),
+					q(25126, {	-- Cutting Teeth
 						["provider"] = { "n", 3143 },	-- Gornek
 						["coord"] = { 44.9, 66.4, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25152,	-- Your Place In The World
-					},
-					{	-- Galgar's Cactus Apple Surprise
-						["questID"] = 25136,
+					}),
+					q(25136, {	-- Galgar's Cactus Apple Surprise
 						["provider"] = { "n", 9796 },	-- Galgar
 						["coord"] = { 43.0, 62.4, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25126,	-- Cutting Teeth
-					},
-					{	-- Hana'zua
-						["questID"] = 25128,
+					}),
+					q(25128, {	-- Hana'zua
 						["provider"] = { "n", 5887 },	-- Canaga Earthcaller
 						["coord"] = { 41.7, 70.0, 461 },
 						["races"] = HORDE_ONLY,
 						["isBreadcrumb"] = true,
-					},
-					{	-- Invaders In Our Home
-						["questID"] = 25172,
+					}),
+					q(25172, {	-- Invaders In Our Home
 						["provider"] = { "n", 3143 },	-- Gornek
 						["coord"] = { 44.9, 66.4, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25126,	-- Cutting Teeth
-					},
-					{	-- Lazy Peons (replaced by 37446)
-						["questID"] = 25134,
+					}),
+					q(25134, {	-- Lazy Peons (replaced by 37446)
 						["u"] = 40,
 						["provider"] = { "n", 11378 },	-- Foreman Thazz'ril
 						["races"] = HORDE_ONLY,
-					},
-					{	-- Lazy Peons
-						["questID"] = 37446,
+					}),
+					q(37446, {	-- Lazy Peons
 						["provider"] = { "n", 11378 },	-- Foreman Thazz'ril
 						["coord"] = { 46.2, 63.3, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25127,	-- Sting of the Scorpid
-					},
-					{	-- Report to Sen'jin Village
-						["questID"] = 25133,
+					}),
+					q(25133, {	-- Report to Sen'jin Village
 						["provider"] = { "n", 3145 },	-- Zureetha Fargaze
 						["coord"] = { 45.8, 63.5, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25132,	-- Burning Blade Medallion
 						["isBreadcrumb"] = true,
-					},
-					{	-- Sarkoth
-						["questID"] = 25129,
+					}),
+					q(25129, {	-- Sarkoth
 						["provider"] = { "n", 3287 },	-- Hana'zua
 						["coord"] = { 34.6, 44.2, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25128,	-- Hana'zua
-					},
-					{	-- Sting of the Scorpid
-						["questID"] = 25127,
+					}),
+					q(25127, {	-- Sting of the Scorpid
 						["provider"] = { "n", 3143 },	-- Gornek
 						["coord"] = { 44.9, 66.4, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25172,	-- Invaders In Our Home
-					},
-					{	-- Thazz'ril's Pick
-						["questID"] = 25135,
+					}),
+					q(25135, {	-- Thazz'ril's Pick
 						["provider"] = { "n", 11378 },	-- Foreman Thazz'ril
 						["coord"] = { 46.2, 63.3, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 37446,	-- Lazy Peons
-					},
-					{	-- Vile Familiars
-						["questID"] = 25131,
+					}),
+					q(25131, {	-- Vile Familiars
 						["provider"] = { "n", 3145 },	-- Zureetha Fargaze
 						["coord"] = { 45.8, 63.5, 461 },
 						["races"] = HORDE_ONLY,
 						["sourceQuest"] = 25127,	-- Sting of the Scorpid
-					},
-					{	-- Your Place In The World
-						["questID"] = 25152,
+					}),
+					q(25152, {	-- Your Place In The World
 						["provider"] = { "n", 10176 },	-- Kaltunk
 						["coord"] = { 45.2, 68.4, 461 },
 						["races"] = HORDE_ONLY,
-					},
+					}),
 				}),
 			}),
 		}),
