f07dd581019f0866762374f4c88f929f6729028b
Gaulish <l2pwncomputers@hotmail.com>
2019-09-05 14:51:34 -0600

    Started tier 15 sets.

--- contrib/Parser/DATAS/12 - Item Sets/Tier Sets.lua
+++ contrib/Parser/DATAS/12 - Item Sets/Tier Sets.lua
@@ -3086,7 +3086,429 @@ Finish adding unobtainable flags once Unobtainables files is cleaned out
 				}),					
 			},						
 			["achievementID"] = 11755,	-- Hot Couture						
-		}),							
+		}),	
+		--[[n(-3166, {	-- Tier 15 (Throne of Thunder)	-- WIP
+			--["achievementID"] = 
+			["g"] = {
+				d(17, {	-- LFR
+					gs(576, {	-- Battleplate of the All-Consuming Maw
+						i(95825),	-- Breastplate of the All-Consuming Maw
+						i(95826),	-- Gauntlets of the All-Consuming Maw
+						i(95827),	-- Helmet of the All-Consuming Maw
+						i(95828),	-- Greaves of the All-Consuming Maw
+						i(95829),	-- Pauldrons of the All-Consuming Maw
+						i(95830),	-- Chestguard of the All-Consuming Maw
+						i(95831),	-- Handguards of the All-Consuming Maw
+						i(95832),	-- Faceguard of the All-Consuming Maw
+						i(95833),	-- Legguards of the All-Consuming Maw
+						i(95834),	-- Shoulderguards of the All-Consuming Maw
+					}),
+					gs(562, {	-- Vestments of the Haunted Forest
+						i(95835),	-- Raiment of the Haunted Forest
+						i(95836),	-- Grips of the Haunted Forest
+						i(95837),	-- Headpiece of the Haunted Forest
+						i(95838),	-- Legguards of the Haunted Forest
+						i(95839),	-- Spaulders of the Haunted Forest
+						i(95840),	-- Handwraps of the Haunted Forest
+						i(95841),	-- Helm of the Haunted Forest
+						i(95842),	-- Legwraps of the Haunted Forest
+						i(95843),	-- Robes of the Haunted Forest
+						i(95844),	-- Mantle of the Haunted Forest
+						i(95845),	-- Gloves of the Haunted Forest
+						i(95846),	-- Cover of the Haunted Forest
+						i(95847),	-- Leggings of the Haunted Forest
+						i(95848),	-- Vestments of the Haunted Forest
+						i(95849),	-- Shoulderwraps of the Haunted Forest
+						i(95850),	-- Tunic of the Haunted Forest
+						i(95851),	-- Handguards of the Haunted Forest
+						i(95852),	-- Headguard of the Haunted Forest
+						i(95853),	-- Breeches of the Haunted Forest
+						i(95854),	-- Shoulderguards of the Haunted Forest
+					}),
+					gs(543, {	-- Battlegear of the Saurok Stalker
+						i(95882),	-- Saurok Stalker's Tunic
+						i(95883),	-- Saurok Stalker's Gloves
+						i(95884),	-- Saurok Stalker's Headguard
+						i(95885),	-- Saurok Stalker's Legguards
+						i(95886),	-- Saurok Stalker's Spaulders
+					}),
+					gs(528, {	-- Regalia of the Chromatic Hydra
+						i(95890),	-- Gloves of the Chromatic Hydra
+						i(95891),	-- Hood of the Chromatic Hydra
+						i(95892),	-- Leggings of the Chromatic Hydra
+						i(95893),	-- Robes of the Chromatic Hydra
+						i(95894),	-- Mantle of the Chromatic Hydra
+					}),
+					gs(512, {	-- Fire-Charm Vestments
+						i(95895),	-- Fire-Charm Tunic
+						i(95896),	-- Fire-Charm Grips
+						i(95897),	-- Fire-Charm Headpiece
+						i(95898),	-- Fire-Charm Leggings
+						i(95899),	-- Fire-Charm Spaulders
+						i(95900),	-- Fire-Charm Handwraps
+						i(95901),	-- Fire-Charm Helm
+						i(95902),	-- Fire-Charm Legwraps
+						i(95903),	-- Fire-Charm Vest
+						i(95904),	-- Fire-Charm Mantle
+						i(95905),	-- Fire-Charm Chestguard
+						i(95906),	-- Fire-Charm Gauntlets
+						i(95907),	-- Fire-Charm Crown
+						i(95908),	-- Fire-Charm Legguards
+						i(95909),	-- Fire-Charm Shoulderguards
+					}),
+					gs(492, {	-- Battlegear of the Lightning Emperor
+						i(95910),	-- Lightning Emperor's Battleplate
+						i(95911),	-- Lightning Emperor's Gauntlets
+						i(95912),	-- Lightning Emperor's Helmet
+						i(95913),	-- Lightning Emperor's Legplates
+						i(95914),	-- Lightning Emperor's Pauldron
+						i(95915),	-- Lightning Emperor's Breastplate
+						i(95916),	-- Lightning Emperor's Gloves
+						i(95917),	-- Lightning Emperor's Headguard
+						i(95918),	-- Lightning Emperor's Greaves
+						i(95919),	-- Lightning Emperor's Mantle
+						i(95920),	-- Lightning Emperor's Chestguard
+						i(95921),	-- Lightning Emperor's Handguards
+						i(95922),	-- Lightning Emperor's Faceguard
+						i(95923),	-- Lightning Emperor's Legguards
+						i(95924),	-- Lightning Emperor's Shoulderguards
+					}),
+					gs(335, {	-- Regalia of the Exorcist
+						i(95925),	-- Handwraps of the Exorcist
+						i(95926),	-- Cowl of the Exorcist
+						i(95927),	-- Legwraps of the Exorcist
+						i(95928),	-- Robes of the Exorcist
+						i(95929),	-- Mantle of the Exorcist
+						i(95930),	-- Gloves of the Exorcist
+						i(95931),	-- Hood of the Exorcist
+						i(95932),	-- Leggings of the Exorcist
+						i(95933),	-- Raiment of the Exorcist
+						i(95934),	-- Shoulderguards of the Exorcist
+					}),
+					gs(476, {	-- Nine-Tail Battlegear
+						i(95935),	-- Nine-Tailed Tunic
+						i(95936),	-- Nine-Tailed Gloves
+						i(95937),	-- Nine-Tailed Helmet
+						i(95938),	-- Nine-Tailed Legguards
+						i(95939),	-- Nine-Tailed Spaulders
+					}),
+					gs(426, {	-- Regalia of the Witch Doctor
+						i(95940),	-- Tunic of the Witch Doctor
+						i(95941),	-- Handwraps of the Witch Doctor
+						i(95942),	-- Faceguard of the Witch Doctor
+						i(95943),	-- Legwraps of the Witch Doctor
+						i(95944),	-- Mantle of the Witch Doctor
+						i(95945),	-- Cuirass of the Witch Doctor
+						i(95946),	-- Grips of the Witch Doctor
+						i(95947),	-- Helmet of the Witch Doctor
+						i(95948),	-- Legguards of the Witch Doctor
+						i(95949),	-- Spaulders of the Witch Doctor
+						i(95950),	-- Hauberk of the Witch Doctor
+						i(95951),	-- Gloves of the Witch Doctor
+						i(95952),	-- Headpiece of the Witch Doctor
+						i(95953),	-- Kilt of the Witch Doctor
+						i(95954),	-- Shoulderwraps of the Witch Doctor
+					}),
+					gs(459, {	-- Regalia of the Thousandfold Hells
+						i(95981),	-- Gloves of the Thousandfold Hells
+						i(95982),	-- Hood of the Thousandfold Hells
+						i(95983),	-- Leggings of the Thousandfold Hells
+						i(95984),	-- Robes of the Thousandfold Hells
+						i(95985),	-- Mantle of the Thousandfold Hells
+					}),
+					gs(442, {	-- Battleplate of the Last Mogu
+						i(95986),	-- Helmet of the Last Mogu
+						i(95987),	-- Battleplate of the Last Mogu
+						i(95988),	-- Gauntlets of the Last Mogu
+						i(95989),	-- Legplates of the Last Mogu
+						i(95990),	-- Pauldrons of the Last Mogu
+						i(95991),	-- Chestguard of the Last Mogu
+						i(95992),	-- Handguards of the Last Mogu
+						i(95993),	-- Faceguard of the Last Mogu
+						i(95994),	-- Legguards of the Last Mogu
+						i(95995),	-- Shoulderguards of the Last Mogu
+					}),
+				}),
+				d(14, {	-- Normal
+					gs(575, {	-- Battleplate of the All-Consuming Maw
+						i(95225),	-- Breastplate of the All-Consuming Maw
+						i(95226),	-- Gauntlets of the All-Consuming Maw
+						i(95227),	-- Helmet of the All-Consuming Maw
+						i(95228),	-- Greaves of the All-Consuming Maw
+						i(95229),	-- Pauldrons of the All-Consuming Maw
+						i(95230),	-- Chestguard of the All-Consuming Maw
+						i(95231),	-- Handguards of the All-Consuming Maw
+						i(95232),	-- Faceguard of the All-Consuming Maw
+						i(95233),	-- Legguards of the All-Consuming Maw
+						i(95234),	-- Shoulderguards of the All-Consuming Maw
+					}),
+					gs(560, {	-- Vestments of the Haunted Forest
+						i(95235),	-- Raiment of the Haunted Forest
+						i(95236),	-- Grips of the Haunted Forest
+						i(95237),	-- Headpiece of the Haunted Forest
+						i(95238),	-- Legguards of the Haunted Forest
+						i(95239),	-- Spaulders of the Haunted Forest
+						i(95240),	-- Handwraps of the Haunted Forest
+						i(95241),	-- Helm of the Haunted Forest
+						i(95242),	-- Legwraps of the Haunted Forest
+						i(95243),	-- Robes of the Haunted Forest
+						i(95244),	-- Mantle of the Haunted Forest
+						i(95245),	-- Gloves of the Haunted Forest
+						i(95246),	-- Cover of the Haunted Forest
+						i(95247),	-- Leggings of the Haunted Forest
+						i(95248),	-- Vestments of the Haunted Forest
+						i(95249),	-- Shoulderwraps of the Haunted Forest
+						i(95250),	-- Tunic of the Haunted Forest
+						i(95251),	-- Handguards of the Haunted Forest
+						i(95252),	-- Headguard of the Haunted Forest
+						i(95253),	-- Breeches of the Haunted Forest
+						i(95254),	-- Shoulderguards of the Haunted Forest
+					}),
+					gs(542, {	-- Battlegear of the Saurok Stalker
+						i(95255),	-- Saurok Stalker's Tunic
+						i(95256),	-- Saurok Stalker's Gloves
+						i(95257),	-- Saurok Stalker's Headguard
+						i(95258),	-- Saurok Stalker's Legguards
+						i(95259),	-- Saurok Stalker's Spaulders
+					}),
+					gs(526, {	-- Regalia of the Chromatic Hydra
+						i(95260),	-- Gloves of the Chromatic Hydra
+						i(95261),	-- Hood of the Chromatic Hydra
+						i(95262),	-- Leggings of the Chromatic Hydra
+						i(95263),	-- Robes of the Chromatic Hydra
+						i(95264),	-- Mantle of the Chromatic Hydra
+					}),
+					gs(510, {	-- Fire-Charm Vestments
+						i(95265),	-- Fire-Charm Tunic
+						i(95266),	-- Fire-Charm Grips
+						i(95267),	-- Fire-Charm Headpiece
+						i(95268),	-- Fire-Charm Leggings
+						i(95269),	-- Fire-Charm Spaulders
+						i(95270),	-- Fire-Charm Handwraps
+						i(95271),	-- Fire-Charm Helm
+						i(95272),	-- Fire-Charm Legwraps
+						i(95273),	-- Fire-Charm Vest
+						i(95274),	-- Fire-Charm Mantle
+						i(95275),	-- Fire-Charm Chestguard
+						i(95276),	-- Fire-Charm Gauntlets
+						i(95277),	-- Fire-Charm Crown
+						i(95278),	-- Fire-Charm Legguards
+						i(95279),	-- Fire-Charm Shoulderguards
+					}),
+					gs(490, {	-- Battlegear of the Lightning Emperor
+						i(95280),	-- Lightning Emperor's Battleplate
+						i(95281),	-- Lightning Emperor's Gauntlets
+						i(95282),	-- Lightning Emperor's Helmet
+						i(95283),	-- Lightning Emperor's Legplates
+						i(95284),	-- Lightning Emperor's Pauldron
+						i(95285),	-- Lightning Emperor's Breastplate
+						i(95286),	-- Lightning Emperor's Gloves
+						i(95287),	-- Lightning Emperor's Headguard
+						i(95288),	-- Lightning Emperor's Greaves
+						i(95289),	-- Lightning Emperor's Mantle
+						i(95290),	-- Lightning Emperor's Chestguard
+						i(95291),	-- Lightning Emperor's Handguards
+						i(95292),	-- Lightning Emperor's Faceguard
+						i(95293),	-- Lightning Emperor's Legguards
+						i(95294),	-- Lightning Emperor's Shoulderguards
+					}),
+					gs(310, {	-- Regalia of the Exorcist
+						i(95295),	-- Handwraps of the Exorcist
+						i(95296),	-- Cowl of the Exorcist
+						i(95297),	-- Legwraps of the Exorcist
+						i(95298),	-- Robes of the Exorcist
+						i(95299),	-- Mantle of the Exorcist
+						i(95300),	-- Gloves of the Exorcist
+						i(95301),	-- Hood of the Exorcist
+						i(95302),	-- Leggings of the Exorcist
+						i(95303),	-- Raiment of the Exorcist
+						i(95304),	-- Shoulderguards of the Exorcist
+					}),
+					gs(474, {	-- Nine-Tail Battlegear
+						i(95305),	-- Nine-Tailed Tunic
+						i(95306),	-- Nine-Tailed Gloves
+						i(95307),	-- Nine-Tailed Helmet
+						i(95308),	-- Nine-Tailed Legguards
+						i(95309),	-- Nine-Tailed Spaulders
+					}),
+					gs(424, {	-- Regalia of the Witch Doctor
+						i(95310),	-- Tunic of the Witch Doctor
+						i(95311),	-- Handwraps of the Witch Doctor
+						i(95312),	-- Faceguard of the Witch Doctor
+						i(95313),	-- Legwraps of the Witch Doctor
+						i(95314),	-- Mantle of the Witch Doctor
+						i(95315),	-- Cuirass of the Witch Doctor
+						i(95316),	-- Grips of the Witch Doctor
+						i(95317),	-- Helmet of the Witch Doctor
+						i(95318),	-- Legguards of the Witch Doctor
+						i(95319),	-- Spaulders of the Witch Doctor
+						i(95320),	-- Hauberk of the Witch Doctor
+						i(95321),	-- Gloves of the Witch Doctor
+						i(95322),	-- Headpiece of the Witch Doctor
+						i(95323),	-- Kilt of the Witch Doctor
+						i(95324),	-- Shoulderwraps of the Witch Doctor
+					}),
+					gs(458, {	-- Regalia of the Thousandfold Hells
+						i(95325),	-- Gloves of the Thousandfold Hells
+						i(95326),	-- Hood of the Thousandfold Hells
+						i(95327),	-- Leggings of the Thousandfold Hells
+						i(95328),	-- Robes of the Thousandfold Hells
+						i(95329),	-- Mantle of the Thousandfold Hells
+					}),
+					gs(441, {	-- Battleplate of the Last Mogu
+						i(95330),	-- Helmet of the Last Mogu
+						i(95331),	-- Battleplate of the Last Mogu
+						i(95332),	-- Gauntlets of the Last Mogu
+						i(95333),	-- Legplates of the Last Mogu
+						i(95334),	-- Pauldrons of the Last Mogu
+						i(95335),	-- Chestguard of the Last Mogu
+						i(95336),	-- Handguards of the Last Mogu
+						i(95337),	-- Faceguard of the Last Mogu
+						i(95338),	-- Legguards of the Last Mogu
+						i(95339),	-- Shoulderguards of the Last Mogu
+					}),
+				}),
+				d(15, {	-- Heroic
+					gs(577, {	-- Battleplate of the All-Consuming Maw
+						i(96569),	-- Breastplate of the All-Consuming Maw
+						i(96570),	-- Gauntlets of the All-Consuming Maw
+						i(96571),	-- Helmet of the All-Consuming Maw
+						i(96572),	-- Greaves of the All-Consuming Maw
+						i(96573),	-- Pauldrons of the All-Consuming Maw
+						i(96574),	-- Chestguard of the All-Consuming Maw
+						i(96575),	-- Handguards of the All-Consuming Maw
+						i(96576),	-- Faceguard of the All-Consuming Maw
+						i(96577),	-- Legguards of the All-Consuming Maw
+						i(96578),	-- Shoulderguards of the All-Consuming Maw
+					}),
+					gs(561, {	-- Vestments of the Haunted Forest
+						i(96579),	-- Raiment of the Haunted Forest
+						i(96580),	-- Grips of the Haunted Forest
+						i(96581),	-- Headpiece of the Haunted Forest
+						i(96582),	-- Legguards of the Haunted Forest
+						i(96583),	-- Spaulders of the Haunted Forest
+						i(96584),	-- Handwraps of the Haunted Forest
+						i(96585),	-- Helm of the Haunted Forest
+						i(96586),	-- Legwraps of the Haunted Forest
+						i(96587),	-- Robes of the Haunted Forest
+						i(96588),	-- Mantle of the Haunted Forest
+						i(96589),	-- Gloves of the Haunted Forest
+						i(96590),	-- Cover of the Haunted Forest
+						i(96591),	-- Leggings of the Haunted Forest
+						i(96592),	-- Vestments of the Haunted Forest
+						i(96593),	-- Shoulderwraps of the Haunted Forest
+						i(96594),	-- Tunic of the Haunted Forest
+						i(96595),	-- Handguards of the Haunted Forest
+						i(96596),	-- Headguard of the Haunted Forest
+						i(96597),	-- Breeches of the Haunted Forest
+						i(96598),	-- Shoulderguards of the Haunted Forest
+					}),
+					gs(544, {	-- Battlegear of the Saurok Stalker
+						i(96626),	-- Saurok Stalker's Tunic
+						i(96627),	-- Saurok Stalker's Gloves
+						i(96628),	-- Saurok Stalker's Headguard
+						i(96629),	-- Saurok Stalker's Legguards
+						i(96630),	-- Saurok Stalker's Spaulders
+					}),
+					gs(527, {	-- Regalia of the Chromatic Hydra
+						i(96634),	-- Gloves of the Chromatic Hydra
+						i(96635),	-- Hood of the Chromatic Hydra
+						i(96636),	-- Leggings of the Chromatic Hydra
+						i(96637),	-- Robes of the Chromatic Hydra
+						i(96638),	-- Mantle of the Chromatic Hydra
+					}),
+					gs(511, {	-- Fire-Charm Vestments
+						i(96639),	-- Fire-Charm Tunic
+						i(96640),	-- Fire-Charm Grips
+						i(96641),	-- Fire-Charm Headpiece
+						i(96642),	-- Fire-Charm Leggings
+						i(96643),	-- Fire-Charm Spaulders
+						i(96644),	-- Fire-Charm Handwraps
+						i(96645),	-- Fire-Charm Helm
+						i(96646),	-- Fire-Charm Legwraps
+						i(96647),	-- Fire-Charm Vest
+						i(96648),	-- Fire-Charm Mantle
+						i(96649),	-- Fire-Charm Chestguard
+						i(96650),	-- Fire-Charm Gauntlets
+						i(96651),	-- Fire-Charm Crown
+						i(96652),	-- Fire-Charm Legguards
+						i(96653),	-- Fire-Charm Shoulderguards
+					}),
+					gs(491, {	-- Battlegear of the Lightning Emperor
+						i(96654),	-- Lightning Emperor's Battleplate
+						i(96655),	-- Lightning Emperor's Gauntlets
+						i(96656),	-- Lightning Emperor's Helmet
+						i(96657),	-- Lightning Emperor's Legplates
+						i(96658),	-- Lightning Emperor's Pauldron
+						i(96659),	-- Lightning Emperor's Breastplate
+						i(96660),	-- Lightning Emperor's Gloves
+						i(96661),	-- Lightning Emperor's Headguard
+						i(96662),	-- Lightning Emperor's Greaves
+						i(96663),	-- Lightning Emperor's Mantle
+						i(96664),	-- Lightning Emperor's Chestguard
+						i(96665),	-- Lightning Emperor's Handguards
+						i(96666),	-- Lightning Emperor's Faceguard
+						i(96667),	-- Lightning Emperor's Legguards
+						i(96668),	-- Lightning Emperor's Shoulderguards
+					}),
+					gs(334, {	-- Regalia of the Exorcist
+						i(96669),	-- Handwraps of the Exorcist
+						i(96670),	-- Cowl of the Exorcist
+						i(96671),	-- Legwraps of the Exorcist
+						i(96672),	-- Robes of the Exorcist
+						i(96673),	-- Mantle of the Exorcist
+						i(96674),	-- Gloves of the Exorcist
+						i(96675),	-- Hood of the Exorcist
+						i(96676),	-- Leggings of the Exorcist
+						i(96677),	-- Raiment of the Exorcist
+						i(96678),	-- Shoulderguards of the Exorcist
+					}),
+					gs(475, {	-- Nine-Tail Battlegear
+						i(96679),	-- Nine-Tailed Tunic
+						i(96680),	-- Nine-Tailed Gloves
+						i(96681),	-- Nine-Tailed Helmet
+						i(96682),	-- Nine-Tailed Legguards
+						i(96683),	-- Nine-Tailed Spaulders
+					}),
+					gs(425, {	-- Regalia of the Witch Doctor
+						i(96684),	-- Tunic of the Witch Doctor
+						i(96685),	-- Handwraps of the Witch Doctor
+						i(96686),	-- Faceguard of the Witch Doctor
+						i(96687),	-- Legwraps of the Witch Doctor
+						i(96688),	-- Mantle of the Witch Doctor
+						i(96689),	-- Cuirass of the Witch Doctor
+						i(96690),	-- Grips of the Witch Doctor
+						i(96691),	-- Helmet of the Witch Doctor
+						i(96692),	-- Legguards of the Witch Doctor
+						i(96693),	-- Spaulders of the Witch Doctor
+						i(96694),	-- Hauberk of the Witch Doctor
+						i(96695),	-- Gloves of the Witch Doctor
+						i(96696),	-- Headpiece of the Witch Doctor
+						i(96697),	-- Kilt of the Witch Doctor
+						i(96698),	-- Shoulderwraps of the Witch Doctor
+					}),
+					gs(460, {	-- Regalia of the Thousandfold Hells
+						i(96725),	-- Gloves of the Thousandfold Hells
+						i(96726),	-- Hood of the Thousandfold Hells
+						i(96727),	-- Leggings of the Thousandfold Hells
+						i(96728),	-- Robes of the Thousandfold Hells
+						i(96729),	-- Mantle of the Thousandfold Hells
+					}),
+					gs(443, {	-- Battleplate of the Last Mogu
+						i(96730),	-- Helmet of the Last Mogu
+						i(96731),	-- Battleplate of the Last Mogu
+						i(96732),	-- Gauntlets of the Last Mogu
+						i(96733),	-- Legplates of the Last Mogu
+						i(96734),	-- Pauldrons of the Last Mogu
+						i(96735),	-- Chestguard of the Last Mogu
+						i(96736),	-- Handguards of the Last Mogu
+						i(96737),	-- Faceguard of the Last Mogu
+						i(96738),	-- Legguards of the Last Mogu
+						i(96739),	-- Shoulderguards of the Last Mogu
+					}),
+				}),
+			},
+		}),]]
 		n(-3162, {	-- Tier 19 (The Nighthold)
 			["groups"] = {
 				d(17, {  -- Raid Finder
