4a3eb85756fad900d014c64a2294057b26d2b4ab
slumber <slumber@love.com>
2019-09-08 09:38:28 -0500

    - more broken isles 'cost'

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Highmountain/Vendors.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Highmountain/Vendors.lua
@@ -5,18 +5,18 @@ _.Zones =
 {
 	m(619, {	-- The Broken Isles
 		m(650, {	-- Highmountain
-			n(-2,   {	-- Vendors
+			n(-2, {		-- Vendors
 				n(93975,  {	-- Leyweaver Yaphisteia
 					["coord"] = { 29.4, 26.2, 650 },
-					["groups"] = {
-						i(127027, {	-- Pattern: Imbued Silkweave Slippers
-							i(126996),	-- Imbued Silkweave Slippers
+					["g"] = {
+						i(127027, {	-- Pattern: Imbued Silkweave Slippers (Rank 1)
+							["cost"] = { { "i", 124437, 100 }, },	-- 100x Shal'dorei Silk
 						}),
 					},
 				}),
 				n(94100,  {	-- Lucas Sigmund <Repairs>
 					["coord"] = { 39.9, 52.3, 650 },
-					["groups"] = {
+					["g"] = {
 						i(141044),	-- Technique: Glyph of Nesingwary's Nemeses
 						i(141048, {	-- Technique: Glyph of the Headhunter
 							["u"] = 2,	-- Removed BoP/BoA item
@@ -28,48 +28,84 @@ _.Zones =
 				}),
 				n(106902, {	-- Ransa Greyfeather <Highmountain Emissary>
 					["coord"] = { 38.3, 45.8, 750 },	-- Thunder Totem
-					["groups"] = {
-						i(137855),	-- Design: Ancient Maelstrom Amulet (Rank 3)*
-						i(137846),	-- Design: Dawnlight Band (Rank 3)*
-						i(137844),	-- Design: Prophetic Band (Rank 3)*
-						i(137839),	-- Design: Skystone Loop (Rank 3)*
-						i(123948),	-- Recipe: Demonsteel Armguards (Rank 3)*
-						i(123955),	-- Recipe: Demonsteel Breastplate (Rank 3)*
-						i(123953),	-- Recipe: Demonsteel Gauntlets (Rank 3)*
-						i(142408),	-- Recipe: Drums of the Mountain (Rank 2)*
-						i(142409),	-- Recipe: Drums of the Mountain (Rank 3)*
-						i(136697),	-- Recipe: Gleaming Iron Spike*
-						i(136919), 	-- Baby Elderhorn*
-						i(131812), 	-- Darkshard Fragment*
-						i(131814), 	-- Whitewater Carp*
-						i(139601), 	-- Harpy-Hunter's Gloves*
-						i(139597), 	-- Mountainforged Chain Hauberk*
-						i(140576), 	-- Tabard of the Hightmountain*
+					["g"] = {
+						i(136919, {	-- Baby Elderhorn (PET!)
+							["cost"] = 5000000,		-- 500g
+						}),
+						i(131812, {	-- Darkshard Fragment (TOY!)
+							["cost"] = 5000000,		-- 500g
+						}),
+						i(137855, {	-- Design: Ancient Maelstrom Amulet (Rank 3)
+							["cost"] = 30000000,	-- 3,000g
+						}),
+						i(137846, {	-- Design: Dawnlight Band (Rank 3)
+							["cost"] = 30000000,	-- 3,000g
+						}),
+						i(137844, {	-- Design: Prophetic Band (Rank 3)
+							["cost"] = 30000000,	-- 3,000g
+						}),
+						i(137839, {	-- Design: Skystone Loop (Rank 3)
+							["cost"] = 5500000,		-- 550g
+						}),
+						i(139601, {	-- Harpy-Hunter's Gloves
+							["cost"] = 10000000,	-- 1,000g
+						}),
+						i(139597, {	-- Mountainforged Chain Hauberk
+							["cost"] = 50000000,	-- 5,000g
+						}),
+						i(123948, {	-- Recipe: Demonsteel Armguards (Rank 3)
+							["cost"] = 30000000,	-- 3,000g
+						}),
+						i(123955, {	-- Recipe: Demonsteel Breastplate (Rank 3)
+							["cost"] = 30000000,	-- 3,000g
+						}),
+						i(123953, {	-- Recipe: Demonsteel Gauntlets (Rank 3)
+							["cost"] = 30000000,	-- 3,000g
+						}),
+						i(142408, {	-- Recipe: Drums of the Mountain (Rank 2)
+							["cost"] = 5500000,		-- 550g
+						}),
+						i(142409, {	-- Recipe: Drums of the Mountain (Rank 3)
+							["cost"] = 30000000,	-- 3,000g
+						}),
+						i(136697, {	-- Recipe: Gleaming Iron Spike
+							["cost"] = 5500000,		-- 550g
+						}),
+						i(140576), 	-- Tabard of the Hightmountain Tribe
 						i(140731, {	-- Treasure Map: Highmountain
 							["f"] = 55,	-- Consumable
 						}),
+						i(131814), 	-- Whitewater Carp (TOY!)
 					},
 				}),
 				n(99574,  {	-- Slyhoof the Shameless Shaman <Used Totem Vendor>
 					["coord"] = { 60.9, 59.5, 750 },	-- Thunder Totem
-					["groups"] = {
-						i(140336),	-- Brulfist Idol (TOY!)
-						i(140335),	-- Totem Tote
+					["g"] = {
+						i(140336, {	-- Brulfist Idol (TOY!)
+							["cost"] = 5000000,	-- 500g
+						}),
+						i(140335, {	-- Totem Tote
+							["cost"] = 22000000,	-- 2,200g
+						}),
 					},
 				}),
 				n(115887, {	-- Trinket
 					["coord"] = { 32.4, 66.8, 650 },
-					["groups"] = {
-						i(142368), 	-- Cracked Falcosaur Beak
-						i(142361, {	-- Ivory Talon
-							i(142363),	-- Mark of Prey
-						}),
+					["g"] = {
 						i(142364), 	-- Bag of Twigs
-						i(142366), 	-- Regurgitated Leaf
+						i(142368, {	-- Cracked Falcosaur Beak
+							["cost"] = 10000000,	-- 1,000g
+						}),
 						i(142367, { 	-- Ivory Feather
 							i(142363),	-- Mark of Prey
 						}),
-						i(142369),	-- Ivory Hawkstrider Mount
+						i(142369, {	-- Ivory Hawkstrider (MOUNT!)
+							["cost"] = 100000000,	-- 10,000g
+						}),
+						i(142361, {	-- Ivory Talon
+							i(142363),	-- Mark of Prey
+						}),
+						i(142366), 	-- Regurgitated Leaf
 					},
 				}),
 			}),
