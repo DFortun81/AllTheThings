a4bea9421f53619d52d05e3b0ecd1e6743b6d400
slumber <slumber@love.com>
2019-09-15 19:09:07 -0500

    - added a couple relics to legion WQ drops

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/3 - World Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/3 - World Quests.lua
@@ -426,9 +426,8 @@ _.Zones =
 					i(140009),	-- Felsword Legplates
 					i(140017),	-- Boots of the Felsword
 				}),
-				n(-384, {	-- Neck
-					
-				}),
+			--	n(-384, {	-- Neck
+			--	}),
 				n(-385, {	-- Finger
 					i(134367),	-- Legionnaire's Band
 					i(146858),	-- Malefic Inquisitor's Ring
@@ -439,7 +438,8 @@ _.Zones =
 					i(134204),	-- Bleached Skull Talisman
 				}),
 				n(-387,  {	-- Relics
-					
+					i(143695),	-- Ambition of the Forlorn
+					i(141275),	-- Fertile Soil
 				}),
 				i(135511),	-- Thick Slab of Bacon. You get it for a split second after completing a bacon wq
 				i(137212),	-- Cut Skystone. You get it for a split second after completing the jewelcrafter daily
