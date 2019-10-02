4e0fac7b14bbc38c3e82b2883bb0c5849b7e9f0c
Sanctuari <yamikanonji@gmail.com>
2019-10-03 00:40:13 +1000

    Added Horde/Alliance tags to Talador flightpoints.

--- contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Talador/Flight Paths.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Talador/Flight Paths.lua
@@ -8,24 +8,31 @@ _.Zones =
 			n(-228, {	-- Flight Paths
 				fp(1451, {	-- Anchorite's Sojourn, Talador
 					["coord"] = { 80.0, 56.6, 535 },
+				["races"] = ALLIANCE_ONLY,
 				}),
 				fp(1445, {	-- Durotan's Grasp, Talador
 					["coord"] = { 55.4, 40.8, 535 },
+				["races"] = HORDE_ONLY,
 				}),
 				fp(1453, {	-- Exarch's Refuge, Talador
 					["coord"] = { 54.8, 68.8, 535 },
+				["races"] = ALLIANCE_ONLY,
 				}),
 				fp(1454, {	-- Exarch's Refuge, Talador
 					["coord"] = { 54.6, 67.8, 535 },
+                                                                           ["races"] = HORDE_ONLY,
 				}),
 				fp(1447, {	-- Fort Wrynn (Alliance), Talador
 					["coord"] = { 69.8, 21.4, 535 },
+				["races"] = ALLIANCE_ONLY,
 				}),
 				fp(1441, {	-- Frostwolf Overlook, Talador
 					["coord"] = { 61.4, 10.6, 535 },
+				["races"] = HORDE_ONLY,
 				}),
 				fp(1448, {	-- Redemption Rise, Talador
 					["coord"] = { 63.2, 25.8, 535 },
+				["races"] = ALLIANCE_ONLY,
 				}),
 				fp(1452, {	-- Retribution Point, Talador
 					["coord"] = { 42.0, 76.8, 535 },
@@ -38,6 +45,7 @@ _.Zones =
 				}),
 				fp(1443, {	-- Vol'jin's Pride, Talador
 					["coord"] = { 70.8, 29.4, 535 },
+				                   ["races"] = HORDE_ONLY,
 				}),
 				fp(1440, {	-- Zangarra, Talador
 					["coord"] = { 80.4, 25.4, 535 },
