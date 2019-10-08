dfab57419ca1df0494fbfc62da4ce3e026c2b9bf
slumber <slumber@love.com>
2019-10-08 18:28:51 -0500

    - cleaned up day of the dead and added a pvp quest to dazar'alor/boralus

--- contrib/Parser/DATAS/21 - Holidays/10 - Day of the Dead/Vendor.lua
+++ contrib/Parser/DATAS/21 - Holidays/10 - Day of the Dead/Vendor.lua
@@ -5,20 +5,19 @@ _.Holidays = bubbleDown({ ["u"] = 27 },	-- Day of the Dead Filter
 {
 	holiday(307365, {	-- Day of the Dead
 		n(-2, {	-- Vendors
-			{	-- Champman <Day of the Dead Vendor>
-				["npcID"] = 34382,	-- Chapman
+			n(34382, {	-- Chapman
 				["coords"] = {
-					{ 47.88, 55.95, 97 },	-- Azuremyst Isle [Exodar]
-					{ 33.92, 39.34, 627 },	-- Dalaran: Broken Isles
-					{ 34.47, 39.12, 125 },	-- Dalaran: Northrend
-					{ 69.40, 40.54, 89 },	-- Darnassus
-					{ 61.08, 36.94, 27 },	-- Dun Morogh [IF]
-					{ 47.26, 17.83, 1 },	-- Durotar [Org]
-					{ 47.72, 49.72, 94 },	-- Eversong Woods [SMC]
-					{ 46.68, 25.81, 84 },	-- Stormwind City
-					{ 56.49, 18.41, 88 },	-- Thunder Bluff
-					{ 39.96, 21.89, 108 },	-- Terokkar Forest [Shatt]
-					{ 62.27, 67.42, 18 },	-- Tirisfal Glades [UC]
+					{ 47.8, 55.9, 97 },		-- Azuremyst Isle
+					{ 33.9, 39.3, 627 },	-- Dalaran: Broken Isles
+					{ 34.4, 39.1, 125 },	-- Dalaran: Northrend
+					{ 69.4, 40.5, 89 },		-- Darnassus
+					{ 61.0, 36.9, 27 },		-- Dun Morogh
+					{ 47.2, 17.8, 1 },		-- Durotar
+					{ 47.7, 49.7, 94 },		-- Eversong Woods
+					{ 46.6, 25.8, 84 },		-- Stormwind City
+					{ 56.4, 18.4, 88 },		-- Thunder Bluff
+					{ 39.9, 21.8, 108 },	-- Terokkar Forest
+					{ 62.2, 67.4, 18 },		-- Tirisfal Glades
 				},
 				["maps"] = {
 					97,		-- Azuremyst Isle [Exodar]
@@ -34,7 +33,6 @@ _.Holidays = bubbleDown({ ["u"] = 27 },	-- Day of the Dead Filter
 					18,		-- Tirisfal Glades [UC]
 				},
 				["g"] = {
-					i(46710),	-- Recipe: Bread of the Dead
 					i(116856),	-- "Blooming Rose" Contender's Costume
 					i(116888),	-- "Night Demon" Contender's Costume
 					i(116889),	-- "Purple Phantom" Contender's Costume
@@ -43,11 +41,12 @@ _.Holidays = bubbleDown({ ["u"] = 27 },	-- Day of the Dead Filter
 					i(46861),	-- Bouquet of Orange Marigolds
 					i(46690),	-- Candy Skull
 					i(46718),	-- Orange Marigold
+					i(46710),	-- Recipe: Bread of the Dead
 					i(46711),	-- Spirit Candle
-					i(46860),	-- Whimsical Skull Mask
+					i(46860),	-- Whimsical Skull Mask (Cosmetic)
 					i(79048),	-- Whimsical Skull Mask
 				},
-			},
+			}),
 		}),
 	}),
 });
