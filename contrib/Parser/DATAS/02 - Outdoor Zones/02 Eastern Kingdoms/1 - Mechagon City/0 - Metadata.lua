b08b1514c4c1f5863af240a5751c745c2e1781b9
Pr3vention <Pr3vention@gmail.com>
2019-10-11 09:00:00 -0500

    Added mechagnome start quests

---
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/1 - Mechagon City/0 - Metadata.lua
@@ -0,0 +1,15 @@
+---------------------------------------------------
+--          Z O N E S       M O D U L E          --
+---------------------------------------------------
+
+_.Zones =
+{
+	m(13, {	-- Eastern Kingdom
+		m(1573, {	-- Mechagon City
+			["description"] = "|cff66ccffA mystery awaits that is of particular interest to the gnomes and goblins. A previously closed vault in Kul Tiras has opened and within it a radio transmitter sends a signal for help and points to a new land to explore, Mechagon. This is an ancient lost city of the gnomes. The beginning of the adventure is within the Junker Wastes where heroes will be beset by death robots, but there are potential allies nearby. Hunted by robots themselves, the mechagnomes introduce you to a whole new society and take you to where higher cast gnomes live who have dedicated themselves to their king and have embraced robotics. King Mechangon has a vision to return anyone of flesh into more pure robotic parts and it's up to you to put a stop to him.|r",
+			["isRaid"] = true,	-- Cities we mark as Raids
+			["races"] = { 37 },	-- Mechagnomes
+			["icon"] = "Interface\\Icons\\inv_eng_gearspringparts",
+		}),
+	}),
+};
