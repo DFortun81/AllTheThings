73561338af1b507badcf9a7ff91e323d314b5fbb
Pr3vention <Pr3vention@gmail.com>
2019-10-19 15:40:42 -0500

    Slight adjustment to assault files so they show up correctly on the appropriate maps

--- contrib/Parser/DATAS/06 - Expansion Features/09 N'Zoth Assaults/Uldum.lua
+++ contrib/Parser/DATAS/06 - Expansion Features/09 N'Zoth Assaults/Uldum.lua
@@ -4,99 +4,98 @@
 
 _.ExpansionFeatures =
 {
-	n(-10072, {	-- N'Zoth Assaults
+	m(1527, {	-- Uldum (under assault)
 		["lvl"] = 120,
-		["maps"] = {
-			1527,	-- Uldum (under assault)
-		},
 		["g"] = {
-			n(-17, {	-- Quests
-				q(57157, {	-- Assault: The Black Empire
-					--[[
-						refreshing after the boss fight in Uldum triggered 57736 (weekly/daily id?)
-					]]--
-					["g"] = {
-						i(173372, {	-- Cache of the Black Empire
-							currency(1755),	-- Echoes of Oblivion
+			n(-10072, {	-- N'Zoth Assaults
+				n(-17,  {	-- Quests
+					q(57157, {	-- Assault: The Black Empire
+						--[[
+							refreshing after the boss fight in Uldum triggered 57736 (weekly/daily id?)
+						]]--
+						["g"] = {
+							i(173372, {	-- Cache of the Black Empire
+								currency(1755),	-- Echoes of Oblivion
+								i(169293),	-- Coalescing Visions x50
+								i(173178),	-- Corrupted Greaves -- TODO:: almost certainly a full ilvl 415 set for all armor types. Same ones sold by blacktalon guards for visions?
+							}),
+						},
+					}),
+					q(58151, {	-- Minions of N'Zoth
+						["coord"] = { 56.6, 31.7, 1527 },
+						["provider"] = { "n", 160252 },	-- Blacktalon Agent
+						["isDaily"] = true,
+						["g"] = {
+							i(169293),	-- Coalescing Visions x10
+						},
+					}),
+					q(57874, {	-- Visions of a Dark Destiny
+						["coord"] = { 56.6, 31.7, 1527 },
+						["provider"] = { "n", 160252 },	-- Blacktalon Agent
+						["g"] = {
 							i(169293),	-- Coalescing Visions x50
-							i(173178),	-- Corrupted Greaves -- TODO:: almost certainly a full ilvl 415 set for all armor types. Same ones sold by blacktalon guards for visions?
-						}),
-					},
+						},
+					}),
 				}),
-				q(58151, {	-- Minions of N'Zoth
-					["coord"] = { 56.6, 31.7, 1527 },
-					["provider"] = { "n", 160252 },	-- Blacktalon Agent
-					["isDaily"] = true,
-					["g"] = {
-						i(169293),	-- Coalescing Visions x10
-					},
+				n(-16,  {	-- Rares
+					n(158706, {	-- Congealed Corruption -- no questID from this even though the map says its rare
+						["coords"] = {
+							{ 59.5, 32.1, 1527 },
+							{ 54.7, 32.1, 1527 },
+						},
+						["g"] = {
+							i(173178),	-- Corrupted Greaves -- TODO: potential zone drop set from rares?
+						},
+					}),
+					n(159087, {	-- Corrupted Bonestripper
+						["questID"] = 57834,
+						["coord"] = { 56.6, 21.0, 1527 },
+					}),
+					n(156709, {	-- Corrupted Sanity Despoiler
+						["questID"] = 57666,
+						["coord"] = { 51.3, 32.0, 1527 },
+					}),
+					n(158594, {	-- Doomsayer Vathiris
+						["questID"] = 57672,
+						["coord"] = { 49.4, 38.3, 1527 },
+					}),
+					n(156299, {	-- R'khuzj the Unfathomable
+						["questID"] = 57430,
+						["description"] = "Roams around the southern half of Uldum.", -- TODO:: his path is HUGE. do we want to add coords?
+					}),
+					n(156654, {	-- Shol'thoss the Doomspeaker
+						["questID"] = 57432,
+						["coord"] = { 58.6, 82.8, 1527 },
+					}),
+					n(160532, {	-- Shoth the Darkened
+						["questID"] = 58169,
+						["coord"] = { 61.3, 74.9, 1527 },
+					}),
+					--[[
+					consuming maw: 62.4, 79.3
+					]]--
 				}),
-				q(57874, {	-- Visions of a Dark Destiny
-					["coord"] = { 56.6, 31.7, 1527 },
-					["provider"] = { "n", 160252 },	-- Blacktalon Agent
-					["g"] = {
-						i(169293),	-- Coalescing Visions x50
-					},
+				n(-212, {	-- Treasures
+					--[[
+					o(, {	-- Black Empire Cache
+						["coords"] = {
+							{ 58.4, 15.4, 1527 },	-- questID 57623
+							{ 50.8, 31.4, 1527 },	-- questID 57624
+							{ 55.6, 83.5, 1527 },	-- questID 57635
+							{ 62.6, 81.9, 1527 },	-- questID 57626
+							{ 59.8, 66.1, 1527 },	-- questID 57627
+						},
+						["g"] = {
+							i(169293),	-- Coalescing Visions
+						},
+					}),
+					]]--
 				}),
-			}),
-			n(-16, {	-- Rares
-				n(158706, {	-- Congealed Corruption -- no questID from this even though the map says its rare
-					["coords"] = {
-						{ 59.5, 32.1, 1527 },
-						{ 54.7, 32.1, 1527 },
-					},
-					["g"] = {
-						i(173178),	-- Corrupted Greaves -- TODO: potential zone drop set from rares?
-					},
-				}),
-				n(159087, {	-- Corrupted Bonestripper
-					["questID"] = 57834,
-					["coord"] = { 56.6, 21.0, 1527 },
-				}),
-				n(156709, {	-- Corrupted Sanity Despoiler
-					["questID"] = 57666,
-					["coord"] = { 51.3, 32.0, 1527 },
-				}),
-				n(158594, {	-- Doomsayer Vathiris
-					["questID"] = 57672,
-					["coord"] = { 49.4, 38.3, 1527 },
-				}),
-				n(156299, {	-- R'khuzj the Unfathomable
-					["questID"] = 57430,
-					["description"] = "Roams around the southern half of Uldum.", -- TODO:: his path is HUGE. do we want to add coords?
-				}),
-				n(156654, {	-- Shol'thoss the Doomspeaker
-					["questID"] = 57432,
-					["coord"] = { 58.6, 82.8, 1527 },
-				}),
-				n(160532, {	-- Shoth the Darkened
-					["questID"] = 58169,
-					["coord"] = { 61.3, 74.9, 1527 },
-				}),
-				--[[
-				consuming maw: 62.4, 79.3
-				]]--
-			}),
-			n(-212, {	-- Treasures
-				--[[
-				o(, {	-- Black Empire Cache
-					["coords"] = {
-						{ 58.4, 15.4, 1527 },	-- questID 57623
-						{ 50.8, 31.4, 1527 },	-- questID 57624
-						{ 55.6, 83.5, 1527 },	-- questID 57635
-						{ 62.6, 81.9, 1527 },	-- questID 57626
-						{ 59.8, 66.1, 1527 },	-- questID 57627
-					},
-					["g"] = {
-						i(169293),	-- Coalescing Visions
-					},
-				}),
-				]]--
-			}),
-			n(-34, {
-				q(57592, {	-- Executor of N'Zoth
-					["lvl"] = 120,
-					["isWorldQuest"] = true,
+				n(-34,  {	-- World Quests
+					q(57592, {	-- Executor of N'Zoth
+						["lvl"] = 120,
+						["isWorldQuest"] = true,
+					}),
 				}),
 			}),
 		},
--- contrib/Parser/DATAS/06 - Expansion Features/09 N'Zoth Assaults/Vale of Eternal Blossoms.lua
+++ contrib/Parser/DATAS/06 - Expansion Features/09 N'Zoth Assaults/Vale of Eternal Blossoms.lua
@@ -4,178 +4,177 @@
 
 _.ExpansionFeatures =
 {
-	n(-10072, {	-- N'Zoth Assaults
+	m(1530, {	-- Vale of Eternal Blossoms
 		["lvl"] = 120,
-		["maps"] = {
-			1530,	-- Vale of Eternal Blossoms
-		},
 		["g"] = {
-			n(-17, {	-- Quests
-				q(57008, {	-- Assault: The Warring Clans
-					i(173372, {	-- Cache of the Black Empire
-						currency(1755),	-- Echoes of Oblivion
-						i(169293),	-- Coalescing Visions x50
-						i(173178),	-- Corrupted Greaves -- TODO:: almost certainly a full ilvl 415 set for all armor types. Same ones sold by blacktalon guards for visions?
+			n(-10072, {	-- N'Zoth Assaults
+				n(-17,  {	-- Quests
+					q(57008, {	-- Assault: The Warring Clans
+						i(173372, {	-- Cache of the Black Empire
+							currency(1755),	-- Echoes of Oblivion
+							i(169293),	-- Coalescing Visions x50
+							i(173178),	-- Corrupted Greaves -- TODO:: almost certainly a full ilvl 415 set for all armor types. Same ones sold by blacktalon guards for visions?
+						}),
+					}),
+					q(58476, {	-- Bringing the Thunder
+						["coord"] = { 38.4, 75.0, 1530 },
+						["isDaily"] = true,
+						["provider"] = { "n", 158674 },	-- Anji Autumnlight
+					}),
+					q(57720, {	-- Champions of the Mogu
+						["coord"] = { 44.7, 73.8, 1530 },
+						["isDaily"] = true,
+						["provider"] = { "n", 154444 },	-- Stormspeaker Qian
+					}),
+					q(57721, {	-- Spoils of War
+						["coord"] = { 43.9, 71.7, 1530 },
+						["isDaily"] = true,
+						["provider"] = { "n", 154677 },	-- Forgemaster Han
 					}),
 				}),
-				q(58476, {	-- Bringing the Thunder
-					["coord"] = { 38.4, 75.0, 1530 },
-					["isDaily"] = true,
-					["provider"] = { "n", 158674 },	-- Anji Autumnlight
+				n(-16,  {	-- Rares
+					n(157466, {	-- Anh-De the Loyal
+						["questID"] = 57363,
+						["coord"] = { 33.7, 68.6, 1530 },
+					}),
+					n(157183, {	-- Coagulated Anima
+						["questID"] = 58296,
+						["coord"] = { 16.8, 66.7, 1530 },
+					}),
+					n(157287, {	-- Dokani Obliterator
+						["questID"] = 57349,
+						["coord"] = { 39.2, 59.6, 1530 },
+						["g"] = {
+							i(173421, {	-- Black Empire Signet
+								i(174086),	-- Ring -- TODO: update item names
+								i(173860),	-- Ring
+							}),
+						},
+					}),
+					--[[
+					n(156041, {	-- Empty Golem -- TODO:: determine activation criteria
+						--["questID"] = ,
+						["coord"] = { 18.7, 65.8, 1530 },
+					}),
+					]]--
+					n(157153, {	-- Ha-Li
+						["questID"] = 57344,
+						["coords"] = {
+							{ 35.6, 34.1, 1530 },
+							{ 30.6, 37.8, 1530 },
+							{ 32.4, 44.0, 1530 },
+						},
+					}),
+					n(157171, {	-- Heixi the Stonelord
+						["questID"] = 57347,
+						["coord"] = { 28.3, 40.5, 1530 },
+					}),
+					n(157160, {	-- Houndlord Ren
+						["questID"] = 57345,
+						["coord"] = { 13.1, 25.5, 1530 },
+						-- TODO:: determine patrol path
+					}),
+					n(160968, {	-- Jade Colossus
+						["questID"] = 58295,
+						["coord"] = { 17.2, 11.6, 1530 },
+					}),
+					n(157290, {	-- Jade Watcher
+						["questID"] = 57350,
+						["coord"] = { 26.7, 10.7, 1530 },
+						["g"] = {
+							i(173420),	-- Black Empire Cloth Belt
+						},
+					}),
+					n(157162, {	-- Rei Lun
+						["questID"] = 57346,
+						["coord"] = { 21.9, 12.4, 1530 },
+						["g"] = {
+							i(173409),	-- Black Empire Leather Gloves -- TODO:: there must be a whole set of new catch-up gear
+						},
+					}),
+					n(156083, {	-- Sanguifang
+						["questID"] = 56954,
+						["coord"] = { 46.4, 57.2, 1530 },
+						["g"] = {	-- Black Empire Plate Chestpiece
+							i(173393, {	-- TODO: maybe move to Item Sets and reference via symlink?
+								i(173454),	-- Plate Chestplate -- TODO:: this must be a temporary name.
+							}),
+						},
+					}),
+					n(157291, {	-- Spymaster Hul'ach
+						["questID"] = 57351,
+						["coord"] = { 17.9, 37.5, 1530 },
+					}),
+					n(157279, {	-- Stormhowl
+						["questID"] = 57348,
+						["coords"] = {
+							{ 23.0, 76.7, 1530 },
+							{ 25.5, 75.8, 1530 },
+							{ 27.1, 71.5, 1530 },
+						},
+					}),
+					n(154600, {	-- Teng the Awakened
+						["questID"] = 56332,
+						["coord"] = { 47.5, 63.7, 1530 },
+						["g"] = {
+							i(173421),	-- Black Empire Signet
+						},
+					}),
+					n(157468, {	-- Tisiphon
+						["questID"] = 57364,
+						["coord"] = { 9.5, 67.4, 1530 },
+					}),
 				}),
-				q(57720, {	-- Champions of the Mogu
-					["coord"] = { 44.7, 73.8, 1530 },
-					["isDaily"] = true,
-					["provider"] = { "n", 154444 },	-- Stormspeaker Qian
-				}),
-				q(57721, {	-- Spoils of War
-					["coord"] = { 43.9, 71.7, 1530 },
-					["isDaily"] = true,
-					["provider"] = { "n", 154677 },	-- Forgemaster Han
-				}),
-			}),
-			n(-16, {	-- Rares
-				n(157466, {	-- Anh-De the Loyal
-					["questID"] = 57363,
-					["coord"] = { 33.7, 68.6, 1530 },
-				}),
-				n(157183, {	-- Coagulated Anima
-					["questID"] = 58296,
-					["coord"] = { 16.8, 66.7, 1530 },
-				}),
-				n(157287, {	-- Dokani Obliterator
-					["questID"] = 57349,
-					["coord"] = { 39.2, 59.6, 1530 },
-					["g"] = {
-						i(173421, {	-- Black Empire Signet
-							i(174086),	-- Ring -- TODO: update item names
-							i(173860),	-- Ring
-						}),
+				n(-212, {	-- Treasures
+				--[[
+				-- all treasure rewards appear to be placeholders for now
+				o(, {	-- Mogu Plunder
+					["coords"] = {
+						{ 26.0, 12.6, 1530 },	questID 57206 -- inside the guo-lai halls
+						{ 25.3, 27.6, 1530 },	questID 57208
+						{ 21.3, 13.9, 1530 },	questID 57209
+						{ 21.3, 63.0, 1530 },	questID 57211
+						{ 51.1, 63.2, 1530 },	questID 57212
+						{ 33.3, 69.8, 1530 },	questID 57213
+
 					},
 				}),
-				--[[
-				n(156041, {	-- Empty Golem -- TODO:: determine activation criteria
-					--["questID"] = ,
-					["coord"] = { 18.7, 65.8, 1530 },
+				o(, {	-- Mogu Spoils
+					["coords"] = {
+						{ 24.4, 2.8, 1530 },	questID 57214
+					},
 				}),
 				]]--
-				n(157153, {	-- Ha-Li
-					["questID"] = 57344,
-					["coords"] = {
-						{ 35.6, 34.1, 1530 },
-						{ 30.6, 37.8, 1530 },
-						{ 32.4, 44.0, 1530 },
-					},
 				}),
-				n(157171, {	-- Heixi the Stonelord
-					["questID"] = 57347,
-					["coord"] = { 28.3, 40.5, 1530 },
+				n(-2,   {	-- Vendors
+					n(160711, {	-- Zhang Ku
+						["coord"] = { 44.5, 75.4, 1530 },
+						["g"] = {
+							i(172009),	-- Recipe: Contract: Rajani
+						},
+					}),
 				}),
-				n(157160, {	-- Houndlord Ren
-					["questID"] = 57345,
-					["coord"] = { 13.1, 25.5, 1530 },
-					-- TODO:: determine patrol path
-				}),
-				n(160968, {	-- Jade Colossus
-					["questID"] = 58295,
-					["coord"] = { 17.2, 11.6, 1530 },
-				}),
-				n(157290, {	-- Jade Watcher
-					["questID"] = 57350,
-					["coord"] = { 26.7, 10.7, 1530 },
-					["g"] = {
-						i(173420),	-- Black Empire Cloth Belt
-					},
-				}),
-				n(157162, {	-- Rei Lun
-					["questID"] = 57346,
-					["coord"] = { 21.9, 12.4, 1530 },
-					["g"] = {
-						i(173409),	-- Black Empire Leather Gloves -- TODO:: there must be a whole set of new catch-up gear
-					},
-				}),
-				n(156083, {	-- Sanguifang
-					["questID"] = 56954,
-					["coord"] = { 46.4, 57.2, 1530 },
-					["g"] = {	-- Black Empire Plate Chestpiece
-						i(173393, {	-- TODO: maybe move to Item Sets and reference via symlink?
-							i(173454),	-- Plate Chestplate -- TODO:: this must be a temporary name.
-						}),
-					},
-				}),
-				n(157291, {	-- Spymaster Hul'ach
-					["questID"] = 57351,
-					["coord"] = { 17.9, 37.5, 1530 },
-				}),
-				n(157279, {	-- Stormhowl
-					["questID"] = 57348,
-					["coords"] = {
-						{ 23.0, 76.7, 1530 },
-						{ 25.5, 75.8, 1530 },
-						{ 27.1, 71.5, 1530 },
-					},
-				}),
-				n(154600, {	-- Teng the Awakened
-					["questID"] = 56332,
-					["coord"] = { 47.5, 63.7, 1530 },
-					["g"] = {
-						i(173421),	-- Black Empire Signet
-					},
-				}),
-				n(157468, {	-- Tisiphon
-					["questID"] = 57364,
-					["coord"] = { 9.5, 67.4, 1530 },
-				}),
-			}),
-			n(-212, {	-- Treasures
-			--[[
-			-- all treasure rewards appear to be placeholders for now
-			o(, {	-- Mogu Plunder
-				["coords"] = {
-					{ 26.0, 12.6, 1530 },	questID 57206 -- inside the guo-lai halls
-					{ 25.3, 27.6, 1530 },	questID 57208
-					{ 21.3, 13.9, 1530 },	questID 57209
-					{ 21.3, 63.0, 1530 },	questID 57211
-					{ 51.1, 63.2, 1530 },	questID 57212
-					{ 33.3, 69.8, 1530 },	questID 57213
-					
-				},
-			}),
-			o(, {	-- Mogu Spoils
-				["coords"] = {
-					{ 24.4, 2.8, 1530 },	questID 57214
-				},
-			}),
-			]]--
-			n(-2, {	-- Vendors
-				n(160711, {	-- Zhang Ku
-					["coord"] = { 44.5, 75.4, 1530 },
-					["g"] = {
-						i(172009),	-- Recipe: Contract: Rajani
-					},
-				}),
-			}),
-			}),
-			n(-34, {	-- World Quests
-				q(57272, {	-- Bloodbound Effigy
-					["lvl"] = 120,
-					["isWorldQuest"] = true,
-				}),
-				q(58370, {	-- Empowered Demolisher
-					["lvl"] = 120,
-					["isWorldQuest"] = true,
-				}),
-				q(58334, {	-- Misfall in Flames
-					["lvl"] = 120,
-					["isWorldQuest"] = true,
-				}),
-				q(57049, {	-- Vault of Souls
-					["lvl"] = 120,
-					["isWorldQuest"] = true,
-				}),
-				q(57101, {	-- Zan-tien serpent cage
-					["lvl"] = 120,
-					["isWorldQuest"] = true,
+				n(-34,  {	-- World Quests
+					q(57272, {	-- Bloodbound Effigy
+						["lvl"] = 120,
+						["isWorldQuest"] = true,
+					}),
+					q(58370, {	-- Empowered Demolisher
+						["lvl"] = 120,
+						["isWorldQuest"] = true,
+					}),
+					q(58334, {	-- Misfall in Flames
+						["lvl"] = 120,
+						["isWorldQuest"] = true,
+					}),
+					q(57049, {	-- Vault of Souls
+						["lvl"] = 120,
+						["isWorldQuest"] = true,
+					}),
+					q(57101, {	-- Zan-tien serpent cage
+						["lvl"] = 120,
+						["isWorldQuest"] = true,
+					}),
 				}),
 			}),
 		},
