abf840c0d0a514005b96048afa7e905e44e9a89e
Pr3vention <Pr3vention@gmail.com>
2019-10-24 22:08:30 -0500

    Added visions of n'zoth achievement section

---
+++ contrib/Parser/DATAS/05 - Achievements/11 - Expansion Features/10 - Visions of N'Zoth.lua
@@ -0,0 +1,34 @@
+--------------------------------------------------
+--   A C H I E V E M E N T S    M O D U L E    --
+--------------------------------------------------
+
+_.Achievements = 
+{
+	n(-10055, {	-- Expansion Features
+		n(-10071, {	-- Visions of N'Zoth
+			ach(14063),	-- Horrific Vision of Orgrimmar
+			ach(14062),	-- Horrific Vision of Stormwind
+			ach(13996, {	-- The Dark Horde
+				crit(1),	-- Horrific Vision of Orgrimmar
+				crit(2),	-- The Even More Horrific Vision of Orgrimmar
+				crit(3),	-- The Most Horrific Vision of Orgrimmar
+			}),
+			ach(14065),	-- The Even More Horrific Vision of Orgrimmar
+			ach(14064),	-- The Even More Horrific Vision of Stormwind
+			ach(14067),	-- The Most Horrific Vision of Orgrimmar
+			ach(14066),	-- The Most Horrific Vision of Stormwind
+			ach(13994, {	-- Through the Depths of Visions
+				crit(1),	-- Twilight over the White City
+				crit(2),	-- The Dark Horde
+				crit(3),	-- Unwavering Resolve
+			}),
+			ach(13995, {	-- Twilight over the White City
+				crit(1),	-- Horrific Vision of Stormwind
+				crit(2),	-- The Even More Horrific Vision of Stormwind
+				crit(3),	-- The Most Horrific Vision of Stormwind
+			}),
+			ach(14060),	-- Unwavering Resolve
+			ach(14061),	-- We have the Technology
+		}),
+	}),
+};
