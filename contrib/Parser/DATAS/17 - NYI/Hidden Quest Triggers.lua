df41719feee14e7aa4ce03e79e9ca0cc434d77cc
slumber <slumber@love.com>
2019-10-30 04:13:25 -0500

    - tracking quest + fixed a boss's creatureID

--- contrib/Parser/DATAS/17 - NYI/Hidden Quest Triggers.lua
+++ contrib/Parser/DATAS/17 - NYI/Hidden Quest Triggers.lua
@@ -879,6 +879,7 @@ _.NeverImplemented =
 		q(58013),	-- Alliance war campaign, triggers when speaking to Anduin while on "This Ain't Mine"
 		q(51722),	-- triggers when completing "Uniting Kul Tiras" (Alliance WQ unlock).  Completing one triggers all 4 WQ unlock quests (main/alt quests for both Alliance and Horde)
 		q(53800),	-- Battle for Darkshore — triggered after defeating Sira Moonwarden in Darkshore warfront
+		q(53122),	-- A Season for Celebration — unused version of hallow's end quest?  triggered when i turned in the alliance version along with the other horde one
 
 
 	--	HONEYBACK HIVE
