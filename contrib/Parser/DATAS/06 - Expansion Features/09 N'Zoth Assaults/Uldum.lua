f4c4ea8021824c54979c60923e9e9ab8c157363c
Pr3vention <Pr3vention@gmail.com>
2019-11-10 17:08:59 -0600

    new set of dailies and notes for Uldum

--- contrib/Parser/DATAS/06 - Expansion Features/09 N'Zoth Assaults/Uldum.lua
+++ contrib/Parser/DATAS/06 - Expansion Features/09 N'Zoth Assaults/Uldum.lua
@@ -20,6 +20,7 @@ _.ExpansionFeatures =
 						n(157170, {	-- Acolyte Taspu
 							["questID"] = 57281,
 							["coord"] = { 64.5, 25.6, 1527 },
+							["isDaily"] = true,
 							["g"] = {
 								i(173402),	-- Black Empire Mail Gloves
 							},
@@ -27,6 +28,7 @@ _.ExpansionFeatures =
 						n(151883, {	-- Anaua
 							["questID"] = 55468,
 							["description"] = "Flies around the top of the |cFFFFD700Halls of Origination|r.",
+							["isDaily"] = true,
 							["coords"] = {
 								{ 71.2, 54.0, 1527 },
 								{ 68.7, 54.0, 1527 },
@@ -37,18 +39,22 @@ _.ExpansionFeatures =
 						n(152757, {	-- Atekhramun
 							["questID"] = 55710,
 							["coord"] = { 64.0, 51.0, 1527 },
+							["isDaily"] = true,
 							["description"] = "Crush all the little scorpions by walking over them to spawn this mob.",
 						}),
 						n(157167, {	-- Champion Sen-mat
 							["questID"] = 57280,
 							["coord"] = { 75.4, 52.2, 1527 },
+							["isDaily"] = true,
 						}),
 						n(157120, {	-- Fangtaker Orsa
 							["questID"] = 57258,
 							["coord"] = { 75.0, 68.2, 1527 },
+							["isDaily"] = true,
 						}),
 						n(151995, {	-- Hik-ten the Taskmaster
 							["questID"] = 55502,
+							["isDaily"] = true,
 							["coords"] = {
 								{ 77.2, 46.0, 1527 },
 								{ 78.0, 46.0, 1527 },
@@ -61,14 +67,17 @@ _.ExpansionFeatures =
 						n(157157, {	-- Muminah the Incandescent
 							["questID"] = 57277,
 							["coord"] = { 66.8, 20.3, 1527 },
+							["isDaily"] = true,
 						}),
 						n(152677, {	-- Nebet the Ascended
 							["questID"] = 55684,
 							["coord"] = { 62.0, 24.5, 1527 },
+							["isDaily"] = true,
 						}),
 						n(157146, {	-- Rotfeaster
 							["questID"] = 57273,
 							["coord"] = { 68.3, 31.9, 1527 },
+							["isDaily"] = true,
 							["g"] = {
 								i(174753),	-- Waste Marauder
 							},
@@ -76,28 +85,34 @@ _.ExpansionFeatures =
 						n(152040, {	-- Scoutmaster Moswen
 							["questID"] = 55518,
 							["coord"] = { 69.7, 42.2, 1527 },
+							["isDaily"] = true,
 						}),
 						n(151948, {	-- Senbu the Pridefather
 							["questID"] = 55496,
 							["coord"] = { 73.5, 64.6, 1527 },
+							["isDaily"] = true,
 						}),
 						n(151878, {	-- Sun King Nahkotep
 							["questID"] = 58613,
 							["coord"] = { 79.0, 63.9, 1527 },
+							["isDaily"] = true,
 						}),
 						n(151897, {	-- Sun Priestess Nubitt
 							["questID"] = 55479,
 							["coord"] = { 84.8, 57.0, 1527 },
+							["isDaily"] = true,
 						}),
 						n(151609, {	-- Sun Prophet Epaphos
 							["questID"] = 55353,
 							["coord"] = { 73.3, 74.4, 1527 },
+							["isDaily"] = true,
 							["g"] = {
 								i(173421),	-- Black Empire Signet -- todo: move these elsewhere. they seemingly drop from all rares
 							},
 						}),
 						n(152657, {	-- Tat the Bonechewer
 							["questID"] = 55682,
+							["isDaily"] = true,
 							["modelScale"] = 1.4,
 							["coords"] = {
 								{ 67.6, 33.2, 1527 },
@@ -110,9 +125,11 @@ _.ExpansionFeatures =
 						n(157164, {	-- Zealot Tekem
 							["questID"] = 57279,
 							["coord"] = { 80.2, 57.0, 1527 },
+							["isDaily"] = true,
 						}),
 						n(151852, {	-- Watcher Rehu
 							["questID"] = 55461,
+							["isDaily"] = true,
 							["modelScale"] = 4.0,
 							["coords"] = {
 								{ 81.2, 52.1, 1527 },
@@ -164,6 +181,11 @@ _.ExpansionFeatures =
 							["coord"] = { 84.2, 55.6, 1527 },
 							["isWorldQuest"] = true,
 						}),
+						q(57243, {	-- Amathet Slave Camp
+							["lvl"] = 120,
+							["coord"] = { 76.1, 48.0, 1527 },
+							["isWorldQuest"] = true,
+						}),
 						q(55350, {	-- Assault: Amathet Advance
 							["lvl"] = 120,
 							["isWorldQuest"] = true,
@@ -187,9 +209,24 @@ _.ExpansionFeatures =
 							["coord"] = { 71.6, 45.9, 1527 },
 							["isWorldQuest"] = true,
 						}),
+						q(55355, {	-- Lightblade Training Grounds
+							["lvl"] = 120,
+							["coord"] = { 64.5, 22.7, 1527 },
+							["isWorldQuest"] = true,
+						}),
 						q(57234, {	-- Solar Collector
 							["lvl"] = 120,
 							["coord"] = { 80.3, 66.1, 1527 },
+							["isWorldQuest"] = true,
+						}),
+						q(57235, {	-- Solar Collector
+							["lvl"] = 120,
+							["coord"] = { 66.5, 50.8, 1527 },
+							["isWorldQuest"] = true,
+						}),
+						q(55360, {	-- The Unsealed Tomb
+							["lvl"] = 120,
+							["coord"] = { 69.9, 59.9, 1527 },
 							["isWorldQuest"] = true,
 						}),
 						q(55354, {	-- The Vir'naal Front
@@ -220,26 +257,32 @@ _.ExpansionFeatures =
 						n(159087, {	-- Corrupted Bonestripper
 							["questID"] = 57834,
 							["coord"] = { 56.6, 21.0, 1527 },
+							["isDaily"] = true,
 						}),
 						n(156709, {	-- Corrupted Sanity Despoiler
 							["questID"] = 57666,
 							["coord"] = { 51.3, 32.0, 1527 },
+							["isDaily"] = true,
 						}),
 						n(158594, {	-- Doomsayer Vathiris
 							["questID"] = 57672,
 							["coord"] = { 49.4, 38.3, 1527 },
+							["isDaily"] = true,
 						}),
 						n(156299, {	-- R'khuzj the Unfathomable
 							["questID"] = 57430,
+							["isDaily"] = true,
 							["description"] = "Roams around the southern half of Uldum.", -- TODO:: his path is HUGE. do we want to add coords?
 						}),
 						n(156654, {	-- Shol'thoss the Doomspeaker
 							["questID"] = 57432,
 							["coord"] = { 58.6, 82.8, 1527 },
+							["isDaily"] = true,
 						}),
 						n(160532, {	-- Shoth the Darkened
 							["questID"] = 58169,
 							["coord"] = { 61.3, 74.9, 1527 },
+							["isDaily"] = true,
 						}),
 					}),
 					n(-212, {	-- Treasures
@@ -301,6 +344,7 @@ _.ExpansionFeatures =
 				]]--
 				n(162147, {	-- Corpse Eater
 					--["questID"] = ,
+					["isDaily"] = true,
 					["g"] = {
 						i(174769),	-- Malevolent Drone
 					},
@@ -308,26 +352,32 @@ _.ExpansionFeatures =
 				n(158632, {	-- Corrupted Protoplasm
 					--["questID"] = ,
 					["coord"] = { 61.0, 83.0, 1527 },
+					["isDaily"] = true,
 				}),
 				n(158531, {	-- Corrupted Neferset Guard
 					--["questID"] = ,
 					["coord"] = { 50.0, 82.0, 1527 },
+					["isDaily"] = true,
 				}),
 				n(158633, {	-- Gaze of N'Zoth
 					--["questID"] = ,
 					["coord"] = { 55.0, 53.0, 1527 },
+					["isDaily"] = true,
 				}),
 				n(156002, {	-- Harbinger Raloam
 					--["questID"] = ,
 					["coord"] = { 35.0, 17.0, 1527 },
+					["isDaily"] = true,
 				}),
 				n(160623, {	-- Hungering Miasma
 					--["questID"] = ,
 					["coord"] = { 60.0, 39.0, 1527 },
+					["isDaily"] = true,
 				}),
 				n(157134, {	-- Ishak of the Four Winds -- TODO: Confirm if this is only available during the Amathet invasion
 					["questID"] = 57259,
 					["coord"] = { 73.9, 83.5, 1527 },
+					["isDaily"] = true,
 					["g"] = {
 						i(174641),	-- PH Storm Drake -- TODO:: update name
 					},
@@ -335,10 +385,12 @@ _.ExpansionFeatures =
 				n(158595, {	-- Thoughtstealer Vos
 					--["questID"] = ,
 					["coord"] = { 65.0, 73.0, 1527 },
+					["isDaily"] = true,
 				}),
 				n(160970, {	-- Vuk'laz the Earthbreaker
 					--["questID"] = ,
 					["coord"] = { 45.0, 16.0, 1527 },
+					["isDaily"] = true,
 				}),
 			}),
 			n(-17,  {	-- Quests
@@ -363,7 +415,7 @@ _.ExpansionFeatures =
 					["provider"] = { "n", 161031 },	-- Captain Hadan
 					["sourceQuest"] = 58643,	-- Mutually Assured Destruction
 				}),
-				q(58281, {	-- Ancient Armaments
+				q(58281, {	-- Ancient Armaments -- assault: amathet advance
 					["lvl"] = 120,
 					["coord"] = { 54.8, 32.9, 1527 },
 					["isDaily"] = true,
@@ -391,7 +443,7 @@ _.ExpansionFeatures =
 					["provider"] = { "n", 155096 },	-- High Commander Kamses
 					["sourceQuest"] = 56472,	-- The Uldum Accord
 				}),
-				q(58291, {	-- Encroaching Poachers
+				q(58291, {	-- Encroaching Poachers -- assault: amathet advance
 					["lvl"] = 120,
 					["coord"] = { 54.6, 34.0, 1527 },
 					["isDaily"] = true,
@@ -414,7 +466,7 @@ _.ExpansionFeatures =
 						i(173280),	-- Token of Death's Door (Rank 1)
 					},
 				}),
-				q(58289, {	-- Guarded Secrets
+				q(58289, {	-- Guarded Secrets -- assault: amathet advance
 					["lvl"] = 120,
 					["coord"] = { 54.9, 32.9, 1527 },
 					["isDaily"] = true,
@@ -467,7 +519,7 @@ _.ExpansionFeatures =
 					["provider"] = { "n", 154532 },	-- Magni Bronzebeard
 					["sourceQuest"] = 56374,	-- A Titanic Problem
 				}),
-				q(58288, {	-- The Sun King's Chosen
+				q(58288, {	-- The Sun King's Chosen -- assault: amathet advance
 					["lvl"] = 120,
 					["coord"] = { 54.8, 32.9, 1527 },
 					["isDaily"] = true,
@@ -488,6 +540,15 @@ _.ExpansionFeatures =
 					["coord"] = { 47.0, 44.6, 1542 },
 					["provider"] = { "n", 154533 },	-- Magni Bronzebeard
 					["sourceQuest"] = 56209,	-- The Halls of Origination
+				}),
+				q(58321, {	-- Uncommon Core -- assault: amathet advance
+					["lvl"] = 120,
+					["coord"] = { 54.9, 32.7, 1527 },
+					["isDaily"] = true,
+					["provider"] = { "n", 155095 },	-- King Phaoris
+					["g"] = {
+						currency(1755),	-- Coalescing Visions
+					},
 				}),
 				q(58285, {	-- Unearthed Artifacts
 					["lvl"] = 120,
@@ -523,6 +584,13 @@ _.ExpansionFeatures =
 						}),
 						i(173282),	-- Token of Death's Glee (Rank 3)
 					},
+				}),
+			}),
+			n(-34, {	-- World Quests
+				q(58743, {	-- Dune Buggy
+					["lvl"] = 120,
+					["coord"] = { 35.5, 31.6, 1527 },
+					["isWorldQuest"] = true,
 				}),
 			}),
 		},
