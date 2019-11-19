56ea7c07c750901657ab79c92df522b45a48d76a
slumber <slumber@love.com>
2019-11-19 16:50:46 -0600

    - added some missing island achievements
    - reorganized island achievement criteria to match in-game crit order

--- contrib/Parser/DATAS/05 - Achievements/11 - Expansion Features/07 - Island Expeditions.lua
+++ contrib/Parser/DATAS/05 - Achievements/11 - Expansion Features/07 - Island Expeditions.lua
@@ -21,114 +21,123 @@ _.Achievements =
 					1037,	-- Whispering Reef
 				},
 				["g"] = {
-					ach(13142),	-- Archipelago Explorer
-					ach(13125),	-- Azerite Admiral
-					ach(13098),	-- Bane of the Chain
-					ach(13102),	-- Clash at the Cay
+					ach(13142),		-- Archipelago Explorer
+					ach(13125),		-- Azerite Admiral
+					ach(13098),		-- Bane of the Chain
+					ach(13102),		-- Clash at the Cay
+					ach(13580),		-- Crestfallen
 					ach(13134, {	-- Expedition Leader
 						title(382),	-- Expedition Leader
-						crit(2),	-- Azerite Admiral
-						crit(9),	-- Expert Expeditioner
-						crit(3),	-- Give Me The Energy
-						crit(7),	-- Helping Hand
-						crit(6),	-- I'm Here for the Pets
 						crit(1),	-- Island Conqueror
-						crit(11),	-- Island Slayer
-						crit(5),	-- Metal Detector
-						crit(8),	-- Notorious
-						crit(10),	-- Team Deathmatch
+						crit(2),	-- Azerite Admiral
+						crit(3),	-- Give Me The Energy
 						crit(4),	-- Tell Me A Tale
+						crit(5),	-- Metal Detector
+						crit(6),	-- I'm Here for the Pets
+						crit(7),	-- Helping Hand
+						crit(8),	-- Notorious
+						crit(9),	-- Expert Expeditioner
+						crit(10),	-- Team Deathmatch
+						crit(11),	-- Island Slayer
 					}),
 					ach(12595, {	-- Expert Expeditioner
-						ach(12594),	-- Competent Captain
 						ach(12596),	-- No Tourist
+						ach(12594),	-- Competent Captain
 					}),
-					ach(13126),	-- Give Me The Energy
-					ach(13400),	-- Havenswood Hero
+					ach(13126),		-- Give Me The Energy
+					ach(13400),		-- Havenswood Hero
 					ach(13132, {	-- Helping Hand
-						crit(3),	-- Aerin Skyhammer
-						crit(11),	-- Apprentice Karyn
-						crit(25),	-- Arwan Beastheart
+						crit(1),	-- Valero
 						crit(2),	-- Brunold
-						crit(16),	-- Burgle
-						crit(15),	-- Flickerwick
-						crit(8),	-- Gimzy Trickelbank
-						crit(9),	-- Glint-Eyed Frankie
-						crit(18),	-- Gregg
-						crit(12),	-- Grubby Beard
-						crit(21),	-- Longpaws
-						crit(24),	-- Manape
-						crit(22),	-- Nizhoni
-						crit(19),	-- Old Li
-						crit(5),	-- Royston P. Crutchley III
+						crit(3),	-- Aerin Skyhammer
 						crit(4),	-- Spooner the Scalawag
-						crit(13),	-- Sylvester
-						crit(23),	-- Taz'anga
+						crit(5),	-- Royston P. Crutchley III
 						crit(6),	-- Tinny
 						crit(7),	-- Tweets Lightsprocket
+						crit(8),	-- Gimzy Trickelbank
+						crit(9),	-- Glint-Eyed Frankie
 						crit(10),	-- Two-Socks
-						crit(1),	-- Valero
-						crit(17),	-- Vizio the Cartographer
+						crit(11),	-- Apprentice Karyn
+						crit(12),	-- Grubby Beard
+						crit(13),	-- Sylvester
 						crit(14),	-- Waterspeaker Deshi
+						crit(15),	-- Flickerwick
+						crit(16),	-- Burgle
+						crit(17),	-- Vizio the Cartographer
+						crit(18),	-- Gregg
+						crit(19),	-- Old Li
 						crit(20),	-- Yuke
+						crit(21),	-- Longpaws
+						crit(22),	-- Nizhoni
+						crit(23),	-- Taz'anga
+						crit(24),	-- Manape
+						crit(25),	-- Arwan Beastheart
 					}),
 					ach(13128, {	-- I'm Here for the Pets
-						crit(3),	-- Barnaby
-						crit(22),	-- Bloodstone Tunneler
-						crit(2),	-- Captain Nibs
-						crit(27),	-- Coldlight Surfrunner
-						crit(9),	-- Craghoof Kid
-						crit(13),	-- Deathsting Scorpid
-						crit(10),	-- False Knucklebump
-						crit(15),	-- Giggling Flame
-						crit(6),	-- Inky
-						crit(25),	-- Juvenile Brineshell
-						crit(8),	-- Kindleweb Spiderling
-						crit(26),	-- Kunchong Hatchling
-						crit(16),	-- Laughing Stonekin
-						crit(19),	-- Littlehoof
-						crit(18),	-- Mischievous Zephyr
-						crit(24),	-- Muskflank Calfling
-						crit(17),	-- Playful Frostkin
-						crit(5),	-- Octopode Fry
-						crit(4),	-- Poro
-						crit(12),	-- Sandshell Chitterer
 						crit(1),	-- Scuttle
-						crit(20),	-- Snapper
-						crit(23),	-- Snort
+						crit(2),	-- Captain Nibs
+						crit(3),	-- Barnaby
+						crit(4),	-- Poro
+						crit(5),	-- Octopode Fry
+						crit(6),	-- Inky
 						crit(7),	-- Sparkleshell Sandcrawler
-						crit(21),	-- Sunscale Hatchling
-						crit(14),	-- Thistlebrush Bud
+						crit(8),	-- Kindleweb Spiderling
+						crit(9),	-- Craghoof Kid
+						crit(10),	-- False Knucklebump
 						crit(11),	-- Tinder Pup
+						crit(12),	-- Sandshell Chitterer
+						crit(13),	-- Deathsting Scorpid
+						crit(14),	-- Thistlebrush Bud
+						crit(15),	-- Giggling Flame
+						crit(16),	-- Laughing Stonekin
+						crit(17),	-- Playful Frostkin
+						crit(18),	-- Mischievous Zephyr
+						crit(19),	-- Littlehoof
+						crit(20),	-- Snapper
+						crit(21),	-- Sunscale Hatchling
+						crit(22),	-- Bloodstone Tunneler
+						crit(23),	-- Snort
+						crit(24),	-- Muskflank Calfling
+						crit(25),	-- Juvenile Brineshell
+						crit(26),	-- Kunchong Hatchling
+						crit(27),	-- Coldlight Surfrunner
 						crit(28),	-- Voru'kar Leecher
 					}),
 					ach(13122, {	-- Island Conqueror
 						crit(1),	-- Mythic: Dread Chain
-						crit(9),	-- Mythic: Havenswood
-						crit(8),	-- Mythic: Jorundall
 						crit(2),	-- Mythic: Molten Cay
 						crit(3),	-- Mythic: Rotting Mire
 						crit(4),	-- Mythic: Skittering Hollow
 						crit(5),	-- Mythic: Un'gol Ruins
 						crit(6),	-- Mythic: Verdant Wilds
 						crit(7),	-- Mythic: Whispering Reef
+						crit(8),	-- Mythic: Jorundall
+						crit(9),	-- Mythic: Havenswood
+						crit(10),	-- Mythic: Crestfall
+						crit(11),	-- Mythic: Snowblossom Village
 					}),
 					ach(13141, {	-- Island Slayer
-						crit(6),	-- Bane of the Chain
-						crit(5),	-- Clash at the Cay
-						crit(9),	-- Havenswood Hero
-						crit(8),	-- Jorundall Justice
-						crit(4),	-- Mire Marauder
 						crit(1),	-- Reef Madness
-						crit(7),	-- Ruined the Ruins
-						crit(3),	-- Skittering Smashed
 						crit(2),	-- War for the Wilds
+						crit(3),	-- Skittering Smashed
+						crit(4),	-- Mire Marauder
+						crit(5),	-- Clash at the Cay
+						crit(6),	-- Bane of the Chain
+						crit(7),	-- Ruined the Ruins
+						crit(8),	-- Jorundall Justice
+						crit(9),	-- Havenswood Hero
+						crit(10),	-- Crestfallen
+						crit(11),	-- Snowblossom Scrapper
 					}),
-					ach(13399),	-- Jorundall Justice
+					ach(13399),		-- Jorundall Justice
 					ach(13124, {	-- Metal Detector
 						ach(13123),	-- My Dubs
 					}),
-					ach(13106),	-- Mire Marauder
+					ach(13106),		-- Mire Marauder
+					ach(13579, {	-- Mythic: Crestfall
+						ach(13578),	-- Heroic: Crestfall
+						ach(13577),	-- Crestfall
+					}),
 					ach(13097, {	-- Mythic: Dread Chain
 						ach(13096),	-- Heroic: Dread Chain
 						ach(13095),	-- Dread Chain
@@ -153,6 +162,10 @@ _.Achievements =
 						ach(13108),	-- Heroic: Skittering Hollow
 						ach(13107),	-- Skittering Hollow
 					}),
+					ach(13583, {	-- Mythic: Snowblossom Village
+						ach(13582),	-- Heroic: Snowblossom Village
+						ach(13581),	-- Snowblossom Village
+					}),
 					ach(12591, {	-- Mythic: Un'gol Ruins
 						ach(12589),	-- Heroic: Un'gol Ruins
 						ach(12590),	-- Un'gol Ruins
@@ -169,102 +182,92 @@ _.Achievements =
 						ach(13120),	-- Blood in the Water
 						ach(12597),	-- Bayside Brawler
 					}),
-					ach(13115),	-- Reef Madness
-					ach(12592),	-- Ruined the Ruins
-					ach(13110),	-- Skittering Smashed
-					ach(13129),	-- Sucker Punch
-					ach(13133, 13135, {	-- Team Deathmatch [A] / [H]
-						crit(1),	-- Gazlowe [A] / Briona the Bloodthirsty [H]
-						crit(2),	-- Lady Sena [A] / "Stabby" Lottie [H]
-						crit(3),	-- Skaggit [A] / Varigg [H]
-						crit(4),	-- Captain Greenbelly [A] / Frostfencer Seraphi [H]
-						crit(5),	-- Dorp [A] / Squallshaper Auran [H]
-						crit(6),	-- Sneaky Pete [A] / Squallshaper Bryson [H]
-						crit(7),	-- Astralite Visara [A] / Anchorite Lanna [H]
-						crit(8),	-- Moonscythe Pelani [A] / Archmage Tamuura [H]
-						crit(9),	-- Rune Scribe Lusaris [A] / Vindicator Baatul [H]
-						crit(10),	-- Phoenix Mage Rhydras [A] / Dizzy Dina [H]
-						crit(11),	-- Phoenix Mage Ryleia [A] / Tally Zapnabber [H]
-						crit(12),	-- Sunbringer Firasi [A] / Razak Ironsides [H]
-						crit(13),	-- Berserker Zar'ri [A] / Duskrunner Lorinas [H]
-						crit(14),	-- Shadow Hunter Ju'loa [A] / Riftblade Kelain [H]
-						crit(15),	-- Witch Doctor Unbugu [A] / Shadeweaver Zarra [H]
-						crit(16),	-- Mahna Flamewhisper [A] / Fenrae the Cunning [H]
-						crit(17),	-- Spiritwalker Quura [A] / Gunnolf the Ferocious [H]
-						crit(18),	-- Sunwalker Ordel [A] / Raul the Tenacious [H]
-						crit(19),	-- Airyn Swiftfeet [A] / Charg "The Boisterous" [H]
-						crit(20),	-- Brother Bruen [A] / Nagtar Wolfsbane [H]
-						crit(21),	-- Lady Tamakeen [A] / Ranah Saberclaw [H]
+					ach(13115),		-- Reef Madness
+					ach(12592),		-- Ruined the Ruins
+					ach(13110),		-- Skittering Smashed
+					ach(13584),		-- Snowblossom Scrapper
+					ach(13129),		-- Sucker Punch
+					ach(13133, {	-- Team Deathmatch (A)
+						["races"] = ALLIANCE_ONLY,
+						["g"] = {
+							crit(1),	-- Gazlowe
+							crit(2),	-- Lady Sena
+							crit(3),	-- Skaggit
+							crit(4),	-- Captain Greenbelly
+							crit(5),	-- Dorp
+							crit(6),	-- Sneaky Pete
+							crit(7),	-- Astralite Visara
+							crit(8),	-- Moonscythe Pelani
+							crit(9),	-- Rune Scribe Lusaris
+							crit(10),	-- Phoenix Mage Rhydras
+							crit(11),	-- Phoenix Mage Ryleia
+							crit(12),	-- Sunbringer Firasi
+							crit(13),	-- Berserker Zar'ri
+							crit(14),	-- Shadow Hunter Ju'loa
+							crit(15),	-- Witch Doctor Unbugu
+							crit(16),	-- Mahna Flamewhisper
+							crit(17),	-- Spiritwalker Quura
+							crit(18),	-- Sunwalker Ordel
+							crit(19),	-- Charg "The Boisterous"
+							crit(20),	-- Nagtar Wolfsbane
+							crit(21),	-- Ranah Saberclaw
+							crit(22),	-- Guardian M'sheke
+							crit(23),	-- Wardruid Ko'chus
+							crit(24),	-- Mooncaller Mozo'kas
+						},
 					}),
-					--[[a(ach(13133, {	-- Team Deathmatch (Alliance)
-						crit(1),	-- Gazlowe
-						crit(2),	-- Lady Sena
-						crit(3),	-- Skaggit
-						crit(4),	-- Captain Greenbelly
-						crit(5),	-- Dorp
-						crit(6),	-- Sneaky Pete
-						crit(7),	-- Astralite Visara
-						crit(8),	-- Moonscythe Pelani
-						crit(9),	-- Rune Scribe Lusaris
-						crit(10),	-- Phoenix Mage Rhydras
-						crit(11),	-- Phoenix Mage Ryleia
-						crit(12),	-- Sunbringer Firasi
-						crit(13),	-- Berserker Zar'ri
-						crit(14),	-- Shadow Hunter Ju'loa
-						crit(15),	-- Witch Doctor Unbugu
-						crit(16),	-- Mahna Flamewhisper
-						crit(17),	-- Spiritwalker Quura
-						crit(18),	-- Sunwalker Ordel
-						crit(19),	-- Airyn Swiftfeet
-						crit(20),	-- Brother Bruen
-						crit(21),	-- Lady Tamakeen
-					})),]]
-					--[[h(ach(13135, {	-- Team Deathmatch (Horde)
-						crit(1),	-- Briona the Bloodthirsty
-						crit(2),	-- "Stabby" Lottie
-						crit(3),	-- Varigg
-						crit(4),	-- Frostfencer Seraphi
-						crit(5),	-- Squallshaper Auran
-						crit(6),	-- Squallshaper Bryson
-						crit(7),	-- Anchorite Lanna
-						crit(8),	-- Archmage Tamuura
-						crit(9),	-- Vindicator Baatul
-						crit(10),	-- Dizzy Dina
-						crit(11),	-- Tally Zapnabber
-						crit(12),	-- Razak Ironsides
-						crit(13),	-- Duskrunner Lorinas
-						crit(14),	-- Riftblade Kelain
-						crit(15),	-- Shadeweaver Zarra
-						crit(16),	-- Fenrae the Cunning
-						crit(17),	-- Gunnolf the Ferocious
-						crit(18),	-- Raul the Tenacious
-						crit(19),	-- Charg "The Boisterous"
-						crit(20),	-- Nagtar Wolfsbane
-						crit(21),	-- Ranah Saberclaw
-					})),]]
+					ach(13135, {	-- Team Deathmatch (H)
+						["races"] = HORDE_ONLY,
+						["g"] = {
+							crit(1),	-- Briona the Bloodthirsty
+							crit(2),	-- "Stabby" Lottie
+							crit(3),	-- Varigg
+							crit(4),	-- Frostfencer Seraphi
+							crit(5),	-- Squallshaper Auran
+							crit(6),	-- Squallshaper Bryson
+							crit(7),	-- Anchorite Lanna
+							crit(8),	-- Archmage Tamuura
+							crit(9),	-- Vindicator Baatul
+							crit(10),	-- Dizzy Dina
+							crit(11),	-- Tally Zapnabber
+							crit(12),	-- Razak Ironsides
+							crit(13),	-- Duskrunner Lorinas
+							crit(14),	-- Riftblade Kelain
+							crit(15),	-- Shadeweaver Zarra
+							crit(16),	-- Fenrae the Cunning
+							crit(17),	-- Gunnolf the Ferocious
+							crit(18),	-- Raul the Tenacious
+							crit(19),	-- Airyn Swiftfeet
+							crit(20),	-- Brother Bruen
+							crit(21),	-- Lady Tamakeen
+							crit(22),	-- Thornguard Burton
+							crit(23),	-- Thornstalker Nydora
+							crit(24),	-- Thornspeaker Tavery
+						},
+					}),
 					ach(13127, {	-- Tell Me A Tale
-						crit(19),	-- Ancient Crypt Key
-						crit(11),	-- Battered Twilight Scale
-						crit(17),	-- Bloodwake Drinking Horn
-						crit(7),	-- Broken Compass
-						crit(4),	-- Druidic Fur
-						crit(13),	-- Elemental Core
-						crit(8),	-- Glittering Scarab
-						crit(15),	-- Icetusk Fur Cloak
-						crit(14),	-- Magical Spyglass
-						crit(9),	-- Murloc Eye
-						crit(20),	-- Notched Coin
-						crit(6),	-- Razorfin Hunting Spear
-						crit(18),	-- Ring of the Reefs
-						crit(16),	-- Sonic Stone
-						crit(3),	-- Strangling Root
-						crit(12),	-- Unscarred Black Scale
-						crit(10),	-- Venture Co. Business Plan
-						crit(5),	-- Vorus'arak's Carapace
 						crit(1),	-- Weathered Pamphlet
 						crit(2),	-- Wriggling Mass
+						crit(3),	-- Strangling Root
+						crit(4),	-- Druidic Fur
+						crit(5),	-- Vorus'arak's Carapace
+						crit(6),	-- Razorfin Hunting Spear
+						crit(7),	-- Broken Compass
+						crit(8),	-- Glittering Scarab
+						crit(9),	-- Murloc Eye
+						crit(10),	-- Venture Co. Business Plan
+						crit(11),	-- Battered Twilight Scale
+						crit(12),	-- Unscarred Black Scale
+						crit(13),	-- Elemental Core
+						crit(14),	-- Magical Spyglass
+						crit(15),	-- Icetusk Fur Cloak
+						crit(16),	-- Sonic Stone
+						crit(17),	-- Bloodwake Drinking Horn
+						crit(18),	-- Ring of the Reefs
+						crit(19),	-- Ancient Crypt Key
+						crit(20),	-- Notched Coin
 					}),
-					ach(13114),	-- War for the Wilds
+					ach(13114),		-- War for the Wilds
 				},
 			}),
 		},
