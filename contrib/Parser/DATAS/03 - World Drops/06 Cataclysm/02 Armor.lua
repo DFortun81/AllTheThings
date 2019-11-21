5da4dc23240726437cc84a3950aba96ccc83a43a
slumber <slumber@love.com>
2019-11-21 04:15:52 -0600

    - added a missing world drop, some reformatting

--- contrib/Parser/DATAS/03 - World Drops/06 Cataclysm/02 Armor.lua
+++ contrib/Parser/DATAS/03 - World Drops/06 Cataclysm/02 Armor.lua
@@ -3,186 +3,134 @@
 -----------------------------------------------------
 _.WorldDrops = 
 {
-	{
-		["groups"] = {
-			n(-318, {	-- Armor
-				n(-320, {		-- Head
-					i(66931),	-- Mercury-Coated Hood
-					i(66955),	-- Scentsniffer Hood
-					i(66975),	-- Hood of the Crying Rogue
-					i(66983),	-- Helm of Fatal Vision
-					i(67050),	-- Ben-Son's Royal Crown
-					i(67122),	-- Helm of Holy Visions
-					i(67123),	-- Sinman's Helm of Succor
-					i(67147),	-- Je'Tze's Sparkling Tiara
-				}),
-				filter(51, {	-- Neck
-					{	-- Amulet of the Howling Watch
-						["itemID"] = 67040,	-- Amulet of the Howling Watch
-					},
-					{	-- Dorian's Lost Necklace
-						["itemID"] = 67130,	-- Dorian's Lost Necklace
-					},
-					{	-- Mirkfallon Amulet
-						["itemID"] = 55401,	-- Mirkfallon Amulet
-					},
-					{	-- Moonbrook Amulet
-						["itemID"] = 55398,	-- Moonbrook Amulet
-					},
-					{	-- Nethander Amulet
-						["itemID"] = 55402,	-- Nethander Amulet
-					},
-					{	-- Nightrend Choker
-						["itemID"] = 66974,	-- Nightrend Choker
-					},
-					{	-- Sishir Amulet
-						["itemID"] = 55400,	-- Sishir Amulet
-					},
-					{	-- Swamplight Amulet
-						["itemID"] = 55399,	-- Swamplight Amulet
-					},
-				}),
-				n(-321, {		-- Shoulder
-					i(66882),	-- Skeletalized Shoulderpads
-					i(66959),	-- Toraelian Spaulders
-					i(66976),	-- Spaulders of the Savage Beast
-					i(66977),	-- Pauldrons of Toil
-					i(67112),	-- Brittany's Ceremonial Spaulders
-					i(67117),	-- Woundsealer Spaulders
-					i(67144),	-- Pauldrons of Edward the Odd
-				}),
-				n(-322, {		-- Back
-					i(55308),	-- Moonbrook Cloak
-					i(55309),	-- Swamplight Cloak
-					i(55310),	-- Sishir Cloak
-					i(55311),	-- Mirkfallon Cloak
-					i(55312),	-- Nethander Cloak
-					i(55313),	-- Mereldar Cloak
-					i(55314),	-- Thondroril Cloak
-					i(55315),	-- Blackwolf Cloak
-					i(66883),	-- Vertigo Cloak
-					i(66965),	-- Drape of Fiery Dreams
-					i(66991),	-- Cloak of Rich Anger
-					i(67069),	-- Shade of Death
-					i(67119),	-- Thousand Bandage Drape
-					i(67131),	-- Ritssyn's Ruminous Drape
-					i(67134),	-- Dory's Finery
-					i(67140),	-- Drape of Inimitable Fate
-					i(67142),	-- Zom's Electrostatic Cloak
-				}),
-				n(-323, {		-- Chest
-					i(66919),	-- Robes of Whispering Sands
-					i(66992),	-- Harbinger's Breastplate
-					i(67024),	-- Breastplate of Solemn Vows
-					i(67106),	-- Robes of Broken Dreams
-					i(67121),	-- Chestguard of the Caring Ursine
-					i(67133),	-- Dizze's Whirling Robe
-					i(67135),	-- Morrie's Waywalker Wrap
-					i(67143),	-- Icebone Hauberk
-				}),
-				n(-326, {		-- Wrist
-					i(66954),	-- Knife-Concealing Bracers
-					i(66958),	-- Bracers of Razor Fletching
-					i(66979),	-- Bracers of Bloody Business
-					i(67088),	-- Falling Snow Bracers
-					i(67115),	-- Soulheart Bindings
-					i(67127),	-- Bracers of Caustic Purification
-				}),
-				n(-327, {		-- Hands
-					i(66641),	-- Vonica's Gauntlets
-					i(66970),	-- Safecracker's Gloves
-					i(66989),	-- Endwyn's Flickering Gauntlets
-					i(67057),	-- Oath-Bound Gauntlets
-					i(67105),	-- Elementbinder Grips
-					i(67109),	-- Gauntlets of Chattering Valves
-					i(67126),	-- Roffle's Ruffly Gloves
-					i(67132),	-- Grips of the Failed Immortal
-					i(67146),	-- Woundsplicer Handwraps
-				}),
-				n(-328, {		-- Waist
-					i(66941),	-- Triple-Loop Belt
-					i(66966),	-- Elekk-Horn Waistguard
-					i(66980),	-- Girdle of Heated Deeds
-					i(67067),	-- Belt of Guardianship
-					i(67099),	-- Waistguard of New Days
-					i(67103),	-- Belt of Redeemed Fate
-					i(67114),	-- Girdle of Chained Thunder
-				}),
-				n(-329, {		-- Legs
-					i(66904),	-- Gartic's Resplendent Leggings
-					i(66962),	-- Shadowskulk Leggings
-					i(66971),	-- Highland Padded Leggings
-					i(66982),	-- Greaves of the Outcast Knight
-					i(67043),	-- Greaves of Sullen Earth
-					i(67102),	-- Kilt of Elemental Mending
-					i(67141),	-- Corefire Legplates
-					i(67148),	-- Kilt of Trollish Dreams
-					i(67150),	-- Arrowsinger Legguards
-				}),
-				n(-330, {		-- Feet
-					i(55855),	-- Darksky Treads
-					i(66963),	-- Treads of the Wyrmhunter
-					i(66967),	-- Camelhide Treads
-					i(66986),	-- Troubled Steps
-					i(67032),	-- Firm-Set Treads
-					i(67113),	-- Medic's Bloodstained Sandals
-				}),
-				filter(52, {	-- Finger
-					{	-- Band of the Virtuous
-						["itemID"] = 66987,	-- Band of the Virtuous
-					},
-					{	-- Immaculate Br'etc Signet
-						["itemID"] = 66942,	-- Immaculate Br'etc Signet
-					},
-					{	-- Mirkfallon Ring
-						["itemID"] = 55419,	-- Mirkfallon Ring
-					},
-					{	-- Moonbrook Ring
-						["itemID"] = 55416,	-- Moonbrook Ring
-					},
-					{	-- Nethander Ring
-						["itemID"] = 55420,	-- Nethander Ring
-					},
-					{	-- Poison Vial Ring
-						["itemID"] = 66957,	-- Poison Vial Ring
-					},
-					{	-- Ring of Cursed Fate
-						["itemID"] = 67027,	-- Ring of Cursed Fate
-					},
-					{	-- Signet of High Arcanist Savor
-						["itemID"] = 67129,	-- Signet of High Arcanist Savor
-					},
-					{	-- Sishir Ring
-						["itemID"] = 55418,	-- Sishir Ring
-					},
-					{	-- Swamplight Ring
-						["itemID"] = 55417,	-- Swamplight Ring
-					},
-				}),
-				filter(53, {	-- Trinket
-					{	-- Binding Promise
-						["itemID"] = 67037,	-- Binding Promise
-					},
-					{	-- Bottled Lightning
-						["itemID"] = 66879,	-- Bottled Lightning
-					},
-					{	-- Electrospark Heartstarter
-						["itemID"] = 67118,	-- Electrospark Heartstarter
-					},
-					{	-- Heart of the Vile
-						["itemID"] = 66969,	-- Heart of the Vile
-					},
-					{	-- Soul's Anguish
-						["itemID"] = 66994,	-- Soul's Anguish
-					},
-					{	-- Unquenchable Flame
-						["itemID"] = 67101,	-- Unquenchable Flame
-					},
-				}),
+	tier(4, {	-- Cataclysm
+		n(-318, {	-- Armor
+			n(-320, {	-- Head
+				i(66931),	-- Mercury-Coated Hood
+				i(66955),	-- Scentsniffer Hood
+				i(66975),	-- Hood of the Crying Rogue
+				i(66983),	-- Helm of Fatal Vision
+				i(67050),	-- Ben-Son's Royal Crown
+				i(67122),	-- Helm of Holy Visions
+				i(67123),	-- Sinman's Helm of Succor
+				i(67147),	-- Je'Tze's Sparkling Tiara
 			}),
-		},
-		["tierID"] = 4
-	},
+			f(51,   {	-- Neck
+				i(67040),	-- Amulet of the Howling Watch
+				i(67138),	-- Buc-Zakai Choker
+				i(67130),	-- Dorian's Lost Necklace
+				i(55401),	-- Mirkfallon Amulet
+				i(55398),	-- Moonbrook Amulet
+				i(55402),	-- Nethander Amulet
+				i(66974),	-- Nightrend Choker
+				i(55400),	-- Sishir Amulet
+				i(55399),	-- Swamplight Amulet
+			}),
+			n(-321, {	-- Shoulder
+				i(66882),	-- Skeletalized Shoulderpads
+				i(66959),	-- Toraelian Spaulders
+				i(66976),	-- Spaulders of the Savage Beast
+				i(66977),	-- Pauldrons of Toil
+				i(67112),	-- Brittany's Ceremonial Spaulders
+				i(67117),	-- Woundsealer Spaulders
+				i(67144),	-- Pauldrons of Edward the Odd
+			}),
+			n(-322, {	-- Back
+				i(55308),	-- Moonbrook Cloak
+				i(55309),	-- Swamplight Cloak
+				i(55310),	-- Sishir Cloak
+				i(55311),	-- Mirkfallon Cloak
+				i(55312),	-- Nethander Cloak
+				i(55313),	-- Mereldar Cloak
+				i(55314),	-- Thondroril Cloak
+				i(55315),	-- Blackwolf Cloak
+				i(66883),	-- Vertigo Cloak
+				i(66965),	-- Drape of Fiery Dreams
+				i(66991),	-- Cloak of Rich Anger
+				i(67069),	-- Shade of Death
+				i(67119),	-- Thousand Bandage Drape
+				i(67131),	-- Ritssyn's Ruminous Drape
+				i(67134),	-- Dory's Finery
+				i(67140),	-- Drape of Inimitable Fate
+				i(67142),	-- Zom's Electrostatic Cloak
+			}),
+			n(-323, {	-- Chest
+				i(66919),	-- Robes of Whispering Sands
+				i(66992),	-- Harbinger's Breastplate
+				i(67024),	-- Breastplate of Solemn Vows
+				i(67106),	-- Robes of Broken Dreams
+				i(67121),	-- Chestguard of the Caring Ursine
+				i(67133),	-- Dizze's Whirling Robe
+				i(67135),	-- Morrie's Waywalker Wrap
+				i(67143),	-- Icebone Hauberk
+			}),
+			n(-326, {	-- Wrist
+				i(66954),	-- Knife-Concealing Bracers
+				i(66958),	-- Bracers of Razor Fletching
+				i(66979),	-- Bracers of Bloody Business
+				i(67088),	-- Falling Snow Bracers
+				i(67115),	-- Soulheart Bindings
+				i(67127),	-- Bracers of Caustic Purification
+			}),
+			n(-327, {	-- Hands
+				i(66641),	-- Vonica's Gauntlets
+				i(66970),	-- Safecracker's Gloves
+				i(66989),	-- Endwyn's Flickering Gauntlets
+				i(67057),	-- Oath-Bound Gauntlets
+				i(67105),	-- Elementbinder Grips
+				i(67109),	-- Gauntlets of Chattering Valves
+				i(67126),	-- Roffle's Ruffly Gloves
+				i(67132),	-- Grips of the Failed Immortal
+				i(67146),	-- Woundsplicer Handwraps
+			}),
+			n(-328, {	-- Waist
+				i(66941),	-- Triple-Loop Belt
+				i(66966),	-- Elekk-Horn Waistguard
+				i(66980),	-- Girdle of Heated Deeds
+				i(67067),	-- Belt of Guardianship
+				i(67099),	-- Waistguard of New Days
+				i(67103),	-- Belt of Redeemed Fate
+				i(67114),	-- Girdle of Chained Thunder
+			}),
+			n(-329, {	-- Legs
+				i(66904),	-- Gartic's Resplendent Leggings
+				i(66962),	-- Shadowskulk Leggings
+				i(66971),	-- Highland Padded Leggings
+				i(66982),	-- Greaves of the Outcast Knight
+				i(67043),	-- Greaves of Sullen Earth
+				i(67102),	-- Kilt of Elemental Mending
+				i(67141),	-- Corefire Legplates
+				i(67148),	-- Kilt of Trollish Dreams
+				i(67150),	-- Arrowsinger Legguards
+			}),
+			n(-330, {	-- Feet
+				i(55855),	-- Darksky Treads
+				i(66963),	-- Treads of the Wyrmhunter
+				i(66967),	-- Camelhide Treads
+				i(66986),	-- Troubled Steps
+				i(67032),	-- Firm-Set Treads
+				i(67113),	-- Medic's Bloodstained Sandals
+			}),
+			f(52,   {	-- Finger
+				i(66987),	-- Band of the Virtuous
+				i(66942),	-- Immaculate Br'etc Signet
+				i(55419),	-- Mirkfallon Ring
+				i(55416),	-- Moonbrook Ring
+				i(55420),	-- Nethander Ring
+				i(66957),	-- Poison Vial Ring
+				i(67027),	-- Ring of Cursed Fate
+				i(67129),	-- Signet of High Arcanist Savor
+				i(55418),	-- Sishir Ring
+				i(55417),	-- Swamplight Ring
+			}),
+			f(53,   {	-- Trinket
+				i(67037),	-- Binding Promise
+				i(66879),	-- Bottled Lightning
+				i(67118),	-- Electrospark Heartstarter
+				i(66969),	-- Heart of the Vile
+				i(66994),	-- Soul's Anguish
+				i(67101),	-- Unquenchable Flame
+			}),
+		}),
+	}),
 };	
-		
-			
