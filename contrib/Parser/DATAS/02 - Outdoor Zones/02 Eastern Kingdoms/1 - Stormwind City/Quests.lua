9f7879a78cb693309216202323ce9d378e23ee80
slumber <slumber@love.com>
2019-11-21 02:35:16 -0600

    - #errors – switched alliance for human-only 'races' tag on a couple quests

--- contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/1 - Stormwind City/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/1 - Stormwind City/Quests.lua
@@ -610,7 +610,7 @@ _.Zones =
 					["provider"] = { "n", 1323 },	-- Osric Strang
 					["coord"] = { 77.0, 61.2, 84 },
 					["sourceQuest"] = 26394,	-- Continue to Stormwind
-					["races"] = ALLIANCE_ONLY,
+					["races"] = { 1 },	-- Human
 					["lvl"] = 4,
 				}),
 				q(27361, {	-- Favored of the Light
@@ -950,7 +950,7 @@ _.Zones =
 					["provider"] = { "n", 352 },	-- Dungar Longdrink
 					["coord"] = { 71.0, 72.6, 84 },
 					["sourceQuest"] = 26395,	-- Dungar Longdrink
-					["races"] = ALLIANCE_ONLY,
+					["races"] = { 1 },	-- Human
 					["lvl"] = 4,
 				}),
 				q(27241, {	-- Return to Jaina
