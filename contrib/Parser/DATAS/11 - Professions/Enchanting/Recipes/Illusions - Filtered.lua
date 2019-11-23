f6554fb569ddc7a3da5bcfc3412c724ad27f2fe4
Pr3vention <Pr3vention@gmail.com>
2019-11-23 08:52:47 -0600

    Additional illusion filtering for class-specific illusions

--- contrib/Parser/DATAS/11 - Professions/Enchanting/Recipes/Illusions - Filtered.lua
+++ contrib/Parser/DATAS/11 - Professions/Enchanting/Recipes/Illusions - Filtered.lua
@@ -4,43 +4,46 @@ profession(333, {	-- Enchanting
 			["categoryID"] = 399--[[Illusions]],
 			["g"] = {
 				{
-					["recipeID"] = 173716--[[Illusion: Agility]]
+					["recipeID"] = 173716,--[[Illusion: Agility]]
 				},
 				{
-					["recipeID"] = 173717--[[Illusion: Battlemaster]]
+					["recipeID"] = 173717,--[[Illusion: Battlemaster]]
 				},
 				{
-					["recipeID"] = 173718--[[Illusion: Berserking]]
+					["recipeID"] = 173718,--[[Illusion: Berserking]]
 				},
 				{
-					["recipeID"] = 174979--[[Illusion: Blood Draining]]
+					["recipeID"] = 174979,--[[Illusion: Blood Draining]]
 				},
 				{
-					["recipeID"] = 173720--[[Illusion: Crusader]]
+					["recipeID"] = 173720,--[[Illusion: Crusader]]
 				},
 				{
-					["recipeID"] = 175076--[[Illusion: Earthliving]]
+					["recipeID"] = 175076,--[[Illusion: Earthliving]]
+					["classes"] = { 7 },	-- Shaman
 				},
 				{
-					["recipeID"] = 173721--[[Illusion: Elemental Force]]
+					["recipeID"] = 173721,--[[Illusion: Elemental Force]]
 				},
 				{
-					["recipeID"] = 173722--[[Illusion: Executioner]]
+					["recipeID"] = 173722,--[[Illusion: Executioner]]
 				},
 				{
-					["recipeID"] = 173723--[[Illusion: Fiery Weapon]]
+					["recipeID"] = 173723,--[[Illusion: Fiery Weapon]]
 				},
 				{
-					["recipeID"] = 175072--[[Illusion: Flametongue]]
+					["recipeID"] = 175072,--[[Illusion: Flametongue]]
+					["classes"] = { 7 },	-- Shaman
 				},
 				{
-					["recipeID"] = 175071--[[Illusion: Frostbrand]]
+					["recipeID"] = 175071,--[[Illusion: Frostbrand]]
+					["classes"] = { 7 },	-- Shaman
 				},
 				{
-					["recipeID"] = 173719--[[Illusion: Greater Spellpower]]
+					["recipeID"] = 173719,--[[Illusion: Greater Spellpower]]
 				},
 				{
-					["recipeID"] = 173724--[[Illusion: Hidden]]
+					["recipeID"] = 173724,--[[Illusion: Hidden]]
 				},
 				{
 					-- Illusion: Holy Infusion
@@ -48,43 +51,46 @@ profession(333, {	-- Enchanting
 					["u"] = 1
 				},
 				{
-					["recipeID"] = 175070--[[Illusion: Jade Spirit]]
+					["recipeID"] = 175070,--[[Illusion: Jade Spirit]]
 				},
 				{
-					["recipeID"] = 173725--[[Illusion: Landslide]]
+					["recipeID"] = 173725,--[[Illusion: Landslide]]
 				},
 				{
-					["recipeID"] = 173726--[[Illusion: Lifestealing]]
+					["recipeID"] = 173726,--[[Illusion: Lifestealing]]
 				},
 				{
-					["recipeID"] = 175085--[[Illusion: Mending]]
+					["recipeID"] = 175085,--[[Illusion: Mending]]
 				},
 				{
-					["recipeID"] = 173727--[[Illusion: Mongoose]]
+					["recipeID"] = 173727,--[[Illusion: Mongoose]]
 				},
 				{
-					["recipeID"] = 173728--[[Illusion: Poisoned]]
+					["recipeID"] = 173728,--[[Illusion: Poisoned]]
+					["classes"] = { 4 },	-- Rogue
 				},
 				{
-					["recipeID"] = 173729--[[Illusion: Power Torrent]]
+					["recipeID"] = 173729,--[[Illusion: Power Torrent]]
 				},
 				{
-					["recipeID"] = 175086--[[Illusion: River's Song]]
+					["recipeID"] = 175086,--[[Illusion: River's Song]]
 				},
 				{
-					["recipeID"] = 175078--[[Illusion: Rockbiter]]
+					["recipeID"] = 175078,--[[Illusion: Rockbiter]]
+					["classes"] = { 7 },	-- Shaman
 				},
 				{
-					["recipeID"] = 173730--[[Illusion: Spellsurge]]
+					["recipeID"] = 173730,--[[Illusion: Spellsurge]]
 				},
 				{
-					["recipeID"] = 173731--[[Illusion: Striking]]
+					["recipeID"] = 173731,--[[Illusion: Striking]]
 				},
 				{
-					["recipeID"] = 173732--[[Illusion: Unholy]]
+					["recipeID"] = 173732,--[[Illusion: Unholy]]
 				},
 				{
-					["recipeID"] = 175074--[[Illusion: Windfury]]
+					["recipeID"] = 175074,--[[Illusion: Windfury]]
+					["classes"] = { 7 },	-- Shaman
 				}
 			}
 		},
