60cf7fd6ae8bdf759e8cfef3e2035d8e16b836d1
Pr3vention <Pr3vention@gmail.com>
2019-12-08 18:54:32 -0600

    Fixed a quest on Tol Dagor island showing up for the horde

--- contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/Tol Dagor.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/Tol Dagor.lua
@@ -6,29 +6,29 @@ _.Zones =
 {
 	m(876, { 	-- Kul'Tiras
 		m(1169, {	-- Tol Dagor
+			["lvl"] = 110,
+			["description"] = "|cff66ccffA prison located on an island east of Tiragarde Sound.  Before the Ashvane Trading Company bought it, Tol Dagor used to be a real prison — now it is Priscilla Ashvane's own fortress, with scumbags on both sides of the bar.|r",
+			["icon"] = "Interface\\Icons\\ability_demonhunter_imprison",
 			["groups"] = {
 				n(-228, {	-- Flight Paths
 					fp(2096, {	-- Tol Dagor, Tiragarde Sound [Alliance]
 						["description"] = "Tol Dagor, Tiragarde Sound",
-						["coord"] = { 37.8, 92.4 },
+						["coord"] = { 37.8, 92.4, 1169 },
 					}),
 					fp(2276, {	-- Tol Dagor, Tiragarde Sound [Horde]
 						["description"] = "Tol Dagor, Tiragarde Sound",
-						["coord"] = { 23.4, 48.2 },
+						["coord"] = { 23.4, 48.2, 1169 },
 					}),
 				}),
 				n(-17, {	-- Quests
 					q(47098, {	-- Out Like Flynn
+						["races"] = ALLIANCE_ONLY,
 						["provider"] = { "n", 121239 },	-- Flynn Fairwind
 						["sourceQuests"] = { 51341 },	-- Tol Dagor
 						["coord"] = { 47.3, 46.2, 1169 },
 					}),
 				}),
 			},
-			--["achievementID"] = 12840,	-- Tol Dagor
-			["lvl"] = 110,
-			["description"] = "|cff66ccffA prison located on an island east of Tiragarde Sound.  Before the Ashvane Trading Company bought it, Tol Dagor used to be a real prison — now it is Priscilla Ashvane's own fortress, with scumbags on both sides of the bar.|r",
-			["icon"] = "Interface\\Icons\\ability_demonhunter_imprison",
 		}),
 	}),
 };
