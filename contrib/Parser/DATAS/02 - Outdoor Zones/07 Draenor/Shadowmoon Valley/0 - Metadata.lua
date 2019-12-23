8270aba2fbb84e1a95736c9aec2d6518913e4564
Pr3vention <Pr3vention@gmail.com>
2019-12-22 21:45:58 -0600

    added The Breached Ossuary in talador and Den of Secrets in SMV
    fixes #392

--- contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Shadowmoon Valley/0 - Metadata.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/07 Draenor/Shadowmoon Valley/0 - Metadata.lua
@@ -7,7 +7,10 @@ _.Zones =
 		m(539, {	-- Shadowmoon Valley
 			["achievementID"] = 8938,
 			["description"] = "|cff66ccffShadowmoon Valley is a zone in south-eastern Draenor. A lush valley engulfed in eternal night, the Pale Lady shines so brightly here that it even replaced the sun as the valley's primary light source during the day. Shadowmoon serves as one of the key centers of draenei civilization on the world of Draenor, its heart being the sacred temple of Karabor. Lately, the Iron Horde began advancing on the draenei settlements through the Shadowmoon clan and naval attacks targeting Karabor itself.|r",
-			["maps"] = { 540 },	-- Bloodthorn Cave
+			["maps"] = {
+				540,	-- Bloodthorn Cave
+				541,	-- Den of Secrets
+			},
 			["lvl"] = 90,
 		}),
 	}),
