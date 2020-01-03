ebb3618fe0d9a5055f04782aeb4d914c0954a74e
texke <jens.rogier@gmail.com>
2020-01-03 14:32:24 +0100

    Added quest 50085 -- A Message of Blood and Fire for issue #398

--- contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/02 Nazmir/Quests.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/02 Nazmir/Quests.lua
@@ -700,6 +700,11 @@ _.Zones =
 						48823,	-- Projection Destruction
 					},
 				}),
+				q(50085, {	-- A Message of Blood and Fire
+					["provider"] = { "n", 133471 }	-- Rokhan
+					["coord"] = { 45.4, 58.6, 863 },
+					["sourceQuest"] = { 50082 },	-- Target of Opportunity
+				}),
 				q(50933, {	-- An Unfortunate Event
 					["provider"] = { "n", 135784 },	-- Imperial Guard
 					["coord"] = { 29.2, 52.0, 863 },
