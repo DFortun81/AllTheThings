b7347fa4ca43eeb6abe9d4e18733b89cfdeb546a
Gold-DieAldor <goldenshacal@gmail.com>
2020-01-12 23:41:16 +0100

    finished lv120 trial sets

--- contrib/Parser/DATAS/12 - Item Sets/Class Trials/04 - Level 120/12 - Death Knight.lua
+++ contrib/Parser/DATAS/12 - Item Sets/Class Trials/04 - Level 120/12 - Death Knight.lua
@@ -4,119 +4,124 @@
 _.GearSets = 
 {
 	n(-5350, {	-- Class Trial Item Sets
-		n(-154, {	-- Level 110
+		n(-155, {	-- Level 120
 			["groups"] = {
 				n(-142, {	-- Heart-Lesion
 					["classes"] = { 6 },
 					["groups"] = {
 						{	-- Adventurer's Footlocker
-							["itemID"] = 160439,	-- Adventurer's Footlocker
+							["itemID"] = 171988,	-- Adventurer's Footlocker
 							["g"] = {
 								{	-- Heart-Lesion Blade
-									["itemID"] = 153726,	-- Heart-Lesion Blade
+									["itemID"] = 170591,	-- Heart-Lesion Blade
+								},
+								{	-- Heart-Lesion Greatsword
+									["itemID"] = 170590,	-- Heart-Lesion Greatsword
+									["u"] = 1,
 								},
 							},
 						},
 						{	-- Heart-Lesion RuneBlade
-							["itemID"] = 153747,	-- Heart-Lesion RuneBlade
+							["itemID"] = 1705604,	-- Heart-Lesion RuneBlade
 						},
 						{	-- Heart-Lesion Faceguard
-							["itemID"] = 153737,	-- Heart-Lesion Faceguard
+							["itemID"] = 170770,	-- Heart-Lesion Faceguard
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Helm
-							["itemID"] = 153721,	-- Heart-Lesion Helm
+							["itemID"] = 170762,	-- Heart-Lesion Helm
 						},
 						{	-- Heart-Lesion Pendant
-							["itemID"] = 153728,	-- Heart-Lesion Pendant
+							["itemID"] = 170593,	-- Heart-Lesion Pendant
+							["u"] = 1,
 						},
 						{	-- Heart-Lesion Amulet
-							["itemID"] = 153739,	-- Heart-Lesion Amulet
+							["itemID"] = 170599,	-- Heart-Lesion Amulet
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Pauldrons
-							["itemID"] = 153723,	-- Heart-Lesion Pauldrons
+							["itemID"] = 170764,	-- Heart-Lesion Pauldrons
 						},
 						{	-- Heart-Lesion Shoulderguards
-							["itemID"] = 153744,	-- Heart-Lesion Shoulderguards
+							["itemID"] = 170772,	-- Heart-Lesion Shoulderguards
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Cloak of Battle
-							["itemID"] = 153727,	-- Heart-Lesion Cloak of Battle
+							["itemID"] = 170592,	-- Heart-Lesion Cloak of Battle
 						},
 						{	-- Heart-Lesion Cloak of Stoicism
-							["itemID"] = 153734,	-- Heart-Lesion Cloak of Stoicism
+							["itemID"] = 170598,	-- Heart-Lesion Cloak of Stoicism
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Breastplate
-							["itemID"] = 153718,	-- Heart-Lesion Breastplate
+							["itemID"] = 170759,	-- Heart-Lesion Breastplate
 						},
 						{	-- Heart-Lesion Chestguard
-							["itemID"] = 153733,	-- Heart-Lesion Chestguard
+							["itemID"] = 170767,	-- Heart-Lesion Chestguard
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Armguards
-							["itemID"] = 153746,	-- Heart-Lesion Armguards
+							["itemID"] = 170774,	-- Heart-Lesion Armguards
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Vambraces
-							["itemID"] = 153725,	-- Heart-Lesion Vambraces
+							["itemID"] = 170766,	-- Heart-Lesion Vambraces
 						},
 						{	-- Heart-Lesion Gauntlets
-							["itemID"] = 153720,	-- Heart-Lesion Gauntlets
+							["itemID"] = 170761,	-- Heart-Lesion Gauntlets
 						},
 						{	-- Heart-Lesion Handguards
-							["itemID"] = 153736,	-- Heart-Lesion Handguards
+							["itemID"] = 170769,	-- Heart-Lesion Handguards
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Girdle
-							["itemID"] = 153724,	-- Heart-Lesion Girdle
+							["itemID"] = 170765,	-- Heart-Lesion Girdle
 						},
 						{	-- Heart-Lesion Waistband
-							["itemID"] = 153745,	-- Heart-Lesion Waistband
+							["itemID"] = 170773,	-- Heart-Lesion Waistband
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Legguards
-							["itemID"] = 153738,	-- Heart-Lesion Legguards
+							["itemID"] = 170771,	-- Heart-Lesion Legguards
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Legplates
-							["itemID"] = 153722,	-- Heart-Lesion Legplates
+							["itemID"] = 170763,	-- Heart-Lesion Legplates
 						},
 						{	-- Heart-Lesion Greaves
-							["itemID"] = 153735,	-- Heart-Lesion Greaves
+							["itemID"] = 170768,	-- Heart-Lesion Greaves
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Sabatons
-							["itemID"] = 153719,	-- Heart-Lesion Sabatons
+							["itemID"] = 170760,	-- Heart-Lesion Sabatons
 						},
 						{	-- Heart-Lesion Band of Might
-							["itemID"] = 153730,	-- Heart-Lesion Band of Might
+							["itemID"] = 170595,	-- Heart-Lesion Band of Might
 						},
 						{	-- Heart-Lesion Band of Stoicism
-							["itemID"] = 153742,	-- Heart-Lesion Band of Stoicism
+							["itemID"] = 170602,	-- Heart-Lesion Band of Stoicism
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Ring of Might
-							["itemID"] = 153729,	-- Heart-Lesion Ring of Might
+							["itemID"] = 170594,	-- Heart-Lesion Ring of Might
 						},
 						{	-- Heart-Lesion Ring of Stoicism
-							["itemID"] = 153741,	-- Heart-Lesion Ring of Stoicism
+							["itemID"] = 170601,	-- Heart-Lesion Ring of Stoicism
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Stone of Battle
-							["itemID"] = 153731,	-- Heart-Lesion Stone of Battle
+							["itemID"] = 170596,	-- Heart-Lesion Stone of Battle
 						},
 						{	-- Heart-Lesion Defender Idol
-							["itemID"] = 153740,	-- Heart-Lesion Defender Idol
+							["itemID"] = 170600,	-- Heart-Lesion Defender Idol
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Defender Stone
-							["itemID"] = 153743,	-- Heart-Lesion Defender Stone
+							["itemID"] = 170603,	-- Heart-Lesion Defender Stone
 							["u"] = 1,
 						},
 						{	-- Heart-Lesion Idol of Battle
-							["itemID"] = 153732,	-- Heart-Lesion Idol of Battle
+							["itemID"] = 170597,	-- Heart-Lesion Idol of Battle
 						},
 					},
 				}),
