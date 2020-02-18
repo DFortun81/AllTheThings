d77c7af7f3113013ecf58e78c65c2af05961e939
Pr3vention <Pr3vention@gmail.com>
2020-02-17 22:24:48 -0600

    corrected objectIDs for treasures in azsuna and added one that was missing

--- contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Azsuna/Treasure Chest.lua
+++ contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Azsuna/Treasure Chest.lua
@@ -30,60 +30,60 @@ _.Zones =
 					["questID"] = 42297,
 					["coord"] = { 43.4, 22.4, 630 },
 				}),
-				o(258690, {	-- Small Treasure Chest
+				o(269064, {	-- Small Treasure Chest (need to verify objectID)
 					["questID"] = 42285,
 					["coord"] = { 71.7, 21.7, 631 },
 				}),
-				o(240630, {	-- Small Treasure Chest
+				o(249997, {	-- Small Treasure Chest
 					["questID"] = 42272,
 					["coord"] = { 59.9, 63.2, 630 },
 				}),
-				o(240644, {	-- Small Treasure Chest
+				o(250104, {	-- Small Treasure Chest
 					["questID"] = 42294,
 					["coord"] = { 62.8, 44.8, 630 },
 				}),
-				o(246205, {	-- Small Treasure Chest
+				o(250081, {	-- Small Treasure Chest
 					["questID"] = 42278,
 					["coord"] = { 63.0, 54.2, 630 },
 				}),
-				o(246206, {	-- Small Treasure Chest
+				o(250085, {	-- Small Treasure Chest
 					["questID"] = 42283,
 					["coord"] = { 53.5, 45.5, 630 },
 				}),
-				o(249997, {	-- Small Treasure Chest
+				o(240644, {	-- Small Treasure Chest
 					["questID"] = 37596,
 					["coord"] = { 53.0, 37.3, 630 },
 				}),
-				o(250080, {	-- Small Treasure Chest
+				o(246206, {	-- Small Treasure Chest
 					["questID"] = 40752,
 					["coord"] = { 58.6, 53.4, 630 },
 				}),
-				o(250083, {	-- Small Treasure Chest
+				o(250080, {	-- Small Treasure Chest
 					["questID"] = 42273,
 					["coord"] = { 62.4, 58.4, 630 },
 				}),
-				o(250084, {	-- Small Treasure Chest
+				o(250090, {	-- Small Treasure Chest
 					["questID"] = 42287,
 					["coord"] = { 54.4, 36.3, 630 },
 				}),
-				o(250085, {	-- Small Treasure Chest
+				o(240630, {	-- Small Treasure Chest
 					["questID"] = 37831,
 					["coord"] = { 49.7, 34.5, 630 },
 				}),
-				o(250087, {	-- Small Treasure Chest
+				o(250088, {	-- Small Treasure Chest (need to verify objectID)
 					["questID"] = 44102,
 					["coord"] = { 34.6, 35.6, 630 },
 				}),
-				o(250088, {	-- Small Treasure Chest
+				o(250083, {	-- Small Treasure Chest
 					["questID"] = 42281,
 					["coord"] = { 52.0, 42.1, 630 },
 				}),
-				o(250090, {	-- Small Treasure Chest
+				o(254025, {	-- Small Treasure Chest
 					["questID"] = 44103,
 					["coord"] = { 68.9, 29.7, 630 },
 					["description"] = "In an underwater cave.",
 				}),
-				o(250091, {	-- Small Treasure Chest
+				o(246205, {	-- Small Treasure Chest
 					["questID"] = 40751,
 					["coord"] = { 66.1, 43.5, 630 },
 				}),
@@ -91,15 +91,15 @@ _.Zones =
 					["questID"] = 42290,
 					["coord"] = { 50.2, 50.3, 630 },
 				}),
-				o(250098, {	-- Small Treasure Chest
+				o(250106, {	-- Small Treasure Chest
 					["questID"] = 42295,
 					["coord"] = { 47.9, 7.7, 630 },
 				}),
-				o(250103, {	-- Small Treasure Chest
+				o(250084, {	-- Small Treasure Chest
 					["questID"] = 42282,
 					["coord"] = { 53.6, 44.2, 630 },
 				}),
-				o(250104, {	-- Small Treasure Chest
+				o(250103, {	-- Small Treasure Chest
 					["questID"] = 42293,
 					["coord"] = { 63.6, 39.2, 630 },
 				}),
@@ -107,35 +107,39 @@ _.Zones =
 					["questID"] = 42338,
 					["coord"] = { 57.2, 25.2, 630 },
 				}),
-				o(251552, {	-- Small Treasure Chest
+				o(258690, {	-- Small Treasure Chest
 					["questID"] = 44405,
 					["coord"] = { 54.9, 52.1, 630 },
 				}),
-				o(254025, {	-- Small Treasure Chest
+				o(254028, {	-- Small Treasure Chest
 					["questID"] = 44105,
 					["coord"] = { 26.2, 47.1, 630 },
 				}),
-				o(254027, {	-- Small Treasure Chest
+				o(250087, {	-- Small Treasure Chest
 					["questID"] = 42284,
-					["coord"] = { 62.0, 83.7, 630 },
+					["coord"] = { 62.0, 83.7, 631 },
 				}),
-				o(254028, {	-- Small Treasure Chest
+				o(251552, {	-- Small Treasure Chest
 					["questID"] = 42958,
 					["coord"] = { 65.5, 29.6, 630 },
 				}),
-				o(269064, {	-- Small Treasure Chest
+				o(250091, {	-- Small Treasure Chest
 					["questID"] = 42288,
 					["coord"] = { 55.4, 27.7, 630 },
 				}),
-				o(269071, {	-- Small Treasure Chest
+				o(254027, {	-- Small Treasure Chest
 					["questID"] = 44104,
 					["coord"] = { 53.6, 18.1, 630 },
 				}),
-				o(239803, {	-- Treasure Chest
+				o(250098, {	-- Small Treasure Chest
+					["questID"] = 42291,
+					["coord"] = { 45.4, 67.0, 632 },
+				}),
+				o(240629, {	-- Treasure Chest
 					["questID"] = 37829,
 					["coord"] = { 53.2, 64.5, 630 },
 				}),
-				o(240629, {	-- Treasure Chest
+				o(240641, {	-- Treasure Chest
 					["questID"] = 38370,
 					["coord"] = { 49.4, 58.0, 630 },
 					["g"] = {
@@ -146,46 +150,46 @@ _.Zones =
 					["questID"] = 38316,
 					["coord"] = { 40.6, 57.7, 630 },
 				}),
-				o(240634, {	-- Treasure Chest
+				o(240646, {	-- Treasure Chest (need to verify objectID)
 					["questID"] = 42292,
 					["coord"] = { 41.4, 30.7, 630 },
 				}),
-				o(240635, {	-- Treasure Chest
+				o(246037, {	-- Treasure Chest
 					["questID"] = 40711,
 					["coord"] = { 55.6, 18.5, 630 },
 				}),
-				o(240641, {	-- Treasure Chest
+				o(240634, {	-- Treasure Chest
 					["questID"] = 37958,
 					["coord"] = { 57.9, 12.2, 630 },
 				}),
-				o(240642, {	-- Treasure Chest
+				o(239803, {	-- Treasure Chest
 					["questID"] = 37832,
 					["coord"] = { 63.2, 15.2, 630 },
 				}),
-				o(240643, {	-- Treasure Chest
+				o(240690, {	-- Treasure Chest
 					["questID"] = 38419,
 					["coord"] = { 57.1, 31.1, 630 },
 				}),
-				o(240646, {	-- Treasure Chest
+				o(240642, {	-- Treasure Chest
 					["questID"] = 38251,
 					["coord"] = { 56.4, 34.8, 630 },
 				}),
-				o(240690, {	-- Treasure Chest
+				o(250092, {	-- Treasure Chest
 					["questID"] = 42289,
 					["coord"] = { 51.5, 24.3, 630 },
 				}),
-				o(246037, {	-- Treasure Chest
+				o(240635, {	-- Treasure Chest (need to verify objectID)
 					["questID"] = 37713,
 					["coord"] = { 44.5, 39.5, 630 },
 				}),
-				o(250092, {	-- Treasure Chest
+				o(240643, {	-- Treasure Chest
 					["questID"] = 37828,
 					["coord"] = { 49.5, 45.3, 630 },
 					["g"] = {
 						i(122681),	-- Sternfathom's Pet Journal (TOY!)
 					},
 				}),
-				o(250102, {	-- Treasure Chest
+				o(250109, {	-- Treasure Chest
 					["questID"] = 42339,
 					["coord"] = { 52.8, 20.6, 630 },
 					["description"] = "At the end of the cave full of sleeping caves. Tread lightly!",
