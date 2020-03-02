dd29393498aab52b5c62ae066671c85508799e78
mogwai316 <bryan.harden@gmail.com>
2020-03-02 08:31:42 -0500

    Added the Jewelcrafting trinket quests from Cataclysm

---
+++ contrib/Parser/DATAS/11 - Professions/Jewelcrafting/Jewelcrafting.lua
@@ -0,0 +1,35 @@
+profession(JEWELCRAFTING, {
+	n(QUESTS, {
+		-- These may be class-restricted where the quest starter item only drops for certain class/specs but we need more data to know for sure.
+		q(28778,  {	-- Broken Boar Figurine
+			["provider"] = { "i", 65895 },	-- Figurine - King of Boars
+			["g"] = {
+				i(52351),	-- Figurine - King of Boars
+			},
+		}),
+		q(28776,  {	-- Broken Earthen Figurine
+			["provider"] = { "i", 65897 },	-- Figurine - Earthen Guardian
+			["g"] = {
+				i(52352),	-- Figurine - Earthen Guardian
+			},
+		}),
+		q(28777,  {	-- Broken Owl Figurine
+			["provider"] = { "i", 65894 },	-- Figurine - Dream Owl
+			["g"] = {
+				i(52354),	-- Figurine - Dream Owl
+			},
+		}),
+		q(25047,  {	-- Broken Panther Figurine
+			["provider"] = { "i", 52197 },	-- Figurine - Demon Panther
+			["g"] = {
+				i(52199),	-- Figurine - Demon Panther
+			},
+		}),
+		q(28775,  {	-- Broken Serpent Figurine
+			["provider"] = { "i", 65896 },	-- Figurine - Jeweled Serpent
+			["g"] = {
+				i(52353),	-- Figurine - Jeweled Serpent
+			},
+		}),
+	}),
+});
