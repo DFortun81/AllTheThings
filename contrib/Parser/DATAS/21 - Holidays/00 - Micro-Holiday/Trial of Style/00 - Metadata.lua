05992310c9719b8b7a207644a592b21c9c40c13d
mogwai316 <bryan.harden@gmail.com>
2020-04-02 18:04:13 -0400

    Added Trial of Style vendors in Boralus/Dazar'alor

--- contrib/Parser/DATAS/21 - Holidays/00 - Micro-Holiday/Trial of Style/00 - Metadata.lua
+++ contrib/Parser/DATAS/21 - Holidays/00 - Micro-Holiday/Trial of Style/00 - Metadata.lua
@@ -11,9 +11,11 @@ _.Holidays =
 				["itemID"] = 151134,	-- Trial of Style Token
 				["u"] = 22,				-- Micro-Holiday Filter
 				["crs"] = {
-					54473,	-- Warpweaver Dushar (Horde / Orgrimmar)
+					54473,	-- Warpweaver Dushar <Transmogrifier> (Horde / Orgrimmar)
+					142068,	-- Warpweaver Ezil <Transmogrifier> (Alliance / Boralus)
 					85289,	-- Warpweaver Farshlah <Transmogrifier> (Horde / Frostwall)
 					54442,	-- Warpweaver Hashom <Transmogrifier> (Alliance / Stormwind)
+					142068,	-- Warpweaver Hayri <Transmogrifier> (Horde / Dazar'alor)
 					86395,	-- Warpweaver Jasor <Transmogrifier> (Horde / Warspear)
 					64573,	-- Warpweaver Ramahesh <Transmogrifier> (Alliance / Shrine of Seven Stars)
 					67014,	-- Warpweaver Shafiee <Transmogrifier> (Horde / Shrine of Two Moons)
