19e31aeda71430e8d2134d00d3cbdfd7f3fb55ab
slumber <slumber@love.com>
2020-05-06 22:52:03 -0500

    - another old SoO drop put in ordos cache

--- .contrib/Parser/DATAS/01 - Dungeons Raids/5 - Mists of Pandaria/1 - Raids/5 Siege of Orgrimmar.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/5 - Mists of Pandaria/1 - Raids/5 Siege of Orgrimmar.lua
@@ -333,7 +333,7 @@ _.Instances = { tier(5, {	-- Mists of Pandaria
 						un(2, i(105832)),	-- Crimson Gauntlets of Death Normal
 						un(38, i(105831)),	-- Siid's Silent Stranglers Normal (confirmed to drop from Ordos cache on December 29, 2019)
 						un(38, i(105833)),	-- Keengrip Arrowpullers Normal (confirmed to drop from Ordos cache on November 14, 2019)
-						un(2, i(105834)),	-- Marco's Crackling Gloves Normal
+						un(38, i(105834)),	-- Marco's Crackling Gloves Normal (confirmed to drop from Ordos cache on May 6, 2020)
 						un(2, i(105837)),	-- Gauntlets of Discarded Time Normal
 						un(2, i(105835)),	-- Romy's Reliable Grips Normal
 						un(38, i(105836)),	-- Zoid's Molten Gauntlets Normal (confirmed to drop from Ordos cache on December 31, 2019)
