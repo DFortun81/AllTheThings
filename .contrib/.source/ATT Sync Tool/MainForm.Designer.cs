b59fa898ca09979ed72499e9277a5aa254b6f87b
Crieve <dylanfortune@live.com>
2020-06-07 00:20:37 -0700

    The Sync Tool now minimizes itself in the taskbar.

--- .contrib/.source/ATT Sync Tool/MainForm.Designer.cs
+++ .contrib/.source/ATT Sync Tool/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ATT_Sync_Tool
+﻿namespace ATT
 {
     partial class MainForm
     {
@@ -29,12 +29,143 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIconATT = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripATT = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemShow = new System.Windows.Forms.ToolStripMenuItem();
+            this.syncNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerOncePerHour = new System.Windows.Forms.Timer(this.components);
+            this.checkBoxStartUp = new System.Windows.Forms.CheckBox();
+            this.textBoxATT = new System.Windows.Forms.TextBox();
+            this.buttonSyncNow = new System.Windows.Forms.Button();
+            this.buttonExportToCSV = new System.Windows.Forms.Button();
+            this.dialogExportAsCSV = new System.Windows.Forms.FolderBrowserDialog();
+            this.contextMenuStripATT.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIconATT
+            // 
+            this.notifyIconATT.ContextMenuStrip = this.contextMenuStripATT;
+            this.notifyIconATT.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconATT.Icon")));
+            this.notifyIconATT.Text = "ATT Sync Tool";
+            this.notifyIconATT.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIconATT_MouseDoubleClick);
+            // 
+            // contextMenuStripATT
+            // 
+            this.contextMenuStripATT.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemShow,
+            this.syncNowToolStripMenuItem,
+            this.toolStripMenuItemExit});
+            this.contextMenuStripATT.Name = "contextMenuStripATT";
+            this.contextMenuStripATT.Size = new System.Drawing.Size(128, 70);
+            this.contextMenuStripATT.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStripATT_ItemClicked);
+            // 
+            // toolStripMenuItemShow
+            // 
+            this.toolStripMenuItemShow.Name = "toolStripMenuItemShow";
+            this.toolStripMenuItemShow.Size = new System.Drawing.Size(127, 22);
+            this.toolStripMenuItemShow.Text = "Show";
+            // 
+            // syncNowToolStripMenuItem
+            // 
+            this.syncNowToolStripMenuItem.Name = "syncNowToolStripMenuItem";
+            this.syncNowToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.syncNowToolStripMenuItem.Text = "Sync Now";
+            // 
+            // toolStripMenuItemExit
+            // 
+            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(127, 22);
+            this.toolStripMenuItemExit.Text = "Exit";
+            // 
+            // timerOncePerHour
+            // 
+            this.timerOncePerHour.Enabled = true;
+            this.timerOncePerHour.Interval = 3600000;
+            this.timerOncePerHour.Tick += new System.EventHandler(this.timerOncePerHour_Tick);
+            // 
+            // checkBoxStartUp
+            // 
+            this.checkBoxStartUp.AutoSize = true;
+            this.checkBoxStartUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxStartUp.Location = new System.Drawing.Point(12, 13);
+            this.checkBoxStartUp.Name = "checkBoxStartUp";
+            this.checkBoxStartUp.Size = new System.Drawing.Size(163, 24);
+            this.checkBoxStartUp.TabIndex = 1;
+            this.checkBoxStartUp.Text = "Start with Windows";
+            this.checkBoxStartUp.UseVisualStyleBackColor = true;
+            this.checkBoxStartUp.CheckedChanged += new System.EventHandler(this.checkBoxStartUp_CheckedChanged);
+            // 
+            // textBoxATT
+            // 
+            this.textBoxATT.Location = new System.Drawing.Point(251, 13);
+            this.textBoxATT.Multiline = true;
+            this.textBoxATT.Name = "textBoxATT";
+            this.textBoxATT.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxATT.Size = new System.Drawing.Size(537, 425);
+            this.textBoxATT.TabIndex = 2;
+            // 
+            // buttonSyncNow
+            // 
+            this.buttonSyncNow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSyncNow.Location = new System.Drawing.Point(12, 396);
+            this.buttonSyncNow.Name = "buttonSyncNow";
+            this.buttonSyncNow.Size = new System.Drawing.Size(233, 42);
+            this.buttonSyncNow.TabIndex = 3;
+            this.buttonSyncNow.Text = "Sync Now";
+            this.buttonSyncNow.UseVisualStyleBackColor = true;
+            this.buttonSyncNow.Click += new System.EventHandler(this.buttonSyncNow_Click);
+            // 
+            // buttonExportToCSV
+            // 
+            this.buttonExportToCSV.Location = new System.Drawing.Point(13, 354);
+            this.buttonExportToCSV.Name = "buttonExportToCSV";
+            this.buttonExportToCSV.Size = new System.Drawing.Size(232, 36);
+            this.buttonExportToCSV.TabIndex = 4;
+            this.buttonExportToCSV.Text = "Export to CSV";
+            this.buttonExportToCSV.UseVisualStyleBackColor = true;
+            this.buttonExportToCSV.Click += new System.EventHandler(this.buttonExportToCSV_Click);
+            // 
+            // dialogExportAsCSV
+            // 
+            this.dialogExportAsCSV.Description = "Export all of the Account Data to a number of CSV files within a folder of your c" +
+    "hoosing.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.buttonExportToCSV);
+            this.Controls.Add(this.buttonSyncNow);
+            this.Controls.Add(this.textBoxATT);
+            this.Controls.Add(this.checkBoxStartUp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "ATT Sync Tool";
+            this.Shown += new System.EventHandler(this.FormMain_Shown);
+            this.Resize += new System.EventHandler(this.FormMain_Resize);
+            this.contextMenuStripATT.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconATT;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripATT;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemShow;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
+        private System.Windows.Forms.ToolStripMenuItem syncNowToolStripMenuItem;
+        private System.Windows.Forms.Timer timerOncePerHour;
+        private System.Windows.Forms.CheckBox checkBoxStartUp;
+        private System.Windows.Forms.TextBox textBoxATT;
+        private System.Windows.Forms.Button buttonSyncNow;
+        private System.Windows.Forms.Button buttonExportToCSV;
+        private System.Windows.Forms.FolderBrowserDialog dialogExportAsCSV;
     }
 }
 
--- .contrib/.source/ATT Sync Tool/MainForm.cs
+++ .contrib/.source/ATT Sync Tool/MainForm.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
-namespace ATT_Sync_Tool
+namespace ATT
 {
     public partial class MainForm : Form
     {
@@ -17,6 +20,191 @@ namespace ATT_Sync_Tool
         {
             InitializeComponent();
             ATT.Database.Initialize();
+        }
+
+        ~MainForm()
+        {
+            /*
+            Sentinel.StartupAutomatically.OnValueChanged -= StartupAutomatically_OnValueChanged;
+            Tracer.OnWrite -= Tracer_OnWrite;
+            */
+        }
+
+        private void StartupAutomatically_OnValueChanged(bool shouldStartupAutomatically)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { StartupAutomatically_OnValueChanged(shouldStartupAutomatically); });
+                return;
+            }
+            checkBoxStartUp.Checked = shouldStartupAutomatically;
+        }
+
+        private void Tracer_OnWrite(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { Tracer_OnWrite(message); });
+                return;
+            }
+            var utcNow = DateTime.Now;
+            message = utcNow.ToShortDateString() + " " + utcNow.ToLongTimeString() + ": " + message;
+            if (textBoxATT.Text.Length > 0 && !textBoxATT.Text.EndsWith(Environment.NewLine)) textBoxATT.Text += Environment.NewLine;
+            textBoxATT.Text += message;
+        }
+
+        
+        
+        
+
+        /// <summary>
+        /// Sync the WTF folder with the ATT Database.
+        /// </summary>
+        public async Task Sync()
+        {
+            if (buttonSyncNow.Enabled)
+            {
+                buttonSyncNow.Enabled = false;
+                if (timerOncePerHour.Enabled)
+                {
+                    timerOncePerHour.Stop();
+                    timerOncePerHour.Start();
+                }
+
+                // Report that we have started syncing with the web application.
+                Trace.WriteLine("Syncing with ATT Database...");
+
+                // Write all of the account data into separate tables.
+                /*
+                foreach(var accountPair in Sentinel.RetailWoWAccounts)
+                {
+                    var accountID = 1;
+
+                    // Record all of the appearances that are flagged in the database already.
+                    var collected_appearances = Database.QueryDatabase($"SELECT [ID] FROM [RETAIL].[COLLECTION_APPEARANCES] WHERE [ACCOUNT_ID]={accountID};", delegate (IDataReader reader)
+                    {
+                        var dict = new Dictionary<long, bool>();
+                        while (reader.Read())
+                        {
+                            dict[reader.GetInt64(0)] = true;
+                        }
+                        return dict;
+                    });
+                    int count = 0;
+                    var insert = "INSERT INTO [RETAIL].[COLLECTION_APPEARANCES]([ACCOUNT_ID],[ID]) VALUES";
+                    var builder = new StringBuilder(insert);
+                    foreach(var pair in accountPair.Value.Appearances)
+                    {
+                        if (pair.Value == CollectedStatus.Collected)
+                        {
+                            if (collected_appearances.ContainsKey(pair.Key)) continue;
+                            if (count++ > 0)
+                            {
+                                if(count > 998)
+                                {
+                                    builder.Append(';');
+                                    Database.QueryDatabase(builder.ToString());
+                                    builder.Clear().Append(insert);
+                                    count = 1;
+                                }
+                                else builder.Append(',');
+                            }
+                            builder.Append('(').Append(accountID).Append(',').Append(pair.Key).Append(')');
+                        }
+                    }
+                    if(count > 0)
+                    {
+                        Database.QueryDatabase(builder.ToString());
+                    }
+                }
+                */
+                // TODO: Sync with the ATT Database.
+                //await Task.Delay(10000);
+
+                // Report that we finished syncing with the web application successfully.
+                Trace.WriteLine("Sync complete.");
+                buttonSyncNow.Enabled = true;
+            }
+        }
+        
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            //checkBoxStartUp.Checked = Sentinel.StartupAutomatically.Value;
+            Hide();
+        }
+
+        private void FormMain_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        private void notifyIconATT_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+        }
+
+        private void contextMenuStripATT_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Text)
+            {
+                case "Show":
+                    Show();
+                    break;
+                case "Exit":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    _ = Sync();
+                    break;
+            }
+        }
+
+        private void timerOncePerHour_Tick(object sender, EventArgs e)
+        {
+            _ = Sync();
+        }
+        
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            base.OnVisibleChanged(e);
+            if (this.Visible)
+            {
+                this.WindowState = FormWindowState.Normal;
+                notifyIconATT.Visible = false;
+            }
+            else
+            {
+                notifyIconATT.Visible = true;
+            }
+        }
+
+        private void checkBoxStartUp_CheckedChanged(object sender, EventArgs e)
+        {
+            //Sentinel.StartupAutomatically.Value = checkBoxStartUp.Checked;
+        }
+
+        private void buttonSyncNow_Click(object sender, EventArgs e)
+        {
+            _ = Sync();
+        }
+
+        private void buttonExportToCSV_Click(object sender, EventArgs e)
+        {
+            /*
+            var dialogResult = dialogExportAsCSV.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                Trace.WriteLine($"Exporting Account Data to CSV Files...{Environment.NewLine}\t{dialogExportAsCSV.SelectedPath}");
+                var directoryInfo = new DirectoryInfo(dialogExportAsCSV.SelectedPath);
+                if (!directoryInfo.Exists) directoryInfo.Create();
+                Sentinel.ExportToCSV(directoryInfo);
+                Trace.WriteLine($"Finished exporting files.");
+                Process.Start("explorer.exe", dialogExportAsCSV.SelectedPath);
+            }
+            */
         }
     }
 }
