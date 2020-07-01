cebc0170c931ce23665690e28f1a74fd7a9d4bc3
Alex <lightskywrites@gmail.com>
2020-07-01 02:43:08 -0500

    Added the Imperial Quilen and Lucky Quilen Cub to the in-game shop.

--- .contrib/Parser/DATAS/13 - In-Game Shop/InGameShop.lua
+++ .contrib/Parser/DATAS/13 - In-Game Shop/InGameShop.lua
@@ -4,64 +4,66 @@
 _.InGameShop =
 {
 	n(-163, {	-- Armor
-		un(2, i(95475)),	-- Crown of Eternal Winter
-		un(2, i(97213)),	-- Hood of Hungering Darkness
-		un(2, i(95474)),	-- Jewel of the Firelord
+		un(2, i(95475)),			-- Crown of Eternal Winter
+		un(2, i(97213)),			-- Hood of Hungering Darkness
+		un(2, i(95474)),			-- Jewel of the Firelord
 	}),
 	n(-160, {	-- Mounts
-		un(2, i(95341)),	-- Armored Bloodwing
-		un(35, i(54811)),	-- Celestial Steed
-		un(35, i(97989)),	-- Enchanted Fey Dragon
-		un(2, i(112327)),	-- Grinning Reaver
-		un(35, i(78924)),	-- Heart of the Aspects
-		un(35, i(166774)),	-- Hogrus, Swine of Good Fortune
-		un(35, i(107951)),	-- Iron Skyreaver
-		un(35, i(147901)),	-- Luminous Starseeker
-		un(35, i(122469)),	-- Mystic Runesaber
-		un(35, i(156564)),	-- Shu-zen, the Divine Sentinel
-		un(35, mount(308078)),	-- Squeakers, the Trickster
-		un(2, i(92724)),	-- Swift Windsteed
-		un(35, i(166776)),	-- Sylverian Dreamer
-		un(35, i(160589)),	-- The Dreadwake
-		un(35, i(112326, {	-- Warforged Nightmare
-			un(35, i(112324)),	-- Nightmarish Hitching Post
+		un(2, i(95341)),			-- Armored Bloodwing
+		un(35, i(54811)),			-- Celestial Steed
+		un(35, i(97989)),			-- Enchanted Fey Dragon
+		un(2, i(112327)),			-- Grinning Reaver
+		un(35, i(78924)),			-- Heart of the Aspects
+		un(35, i(166774)),			-- Hogrus, Swine of Good Fortune
+		un(35, mount(124659)),		-- Imperial Quilen
+		un(35, i(107951)),			-- Iron Skyreaver
+		un(35, i(147901)),			-- Luminous Starseeker
+		un(35, i(122469)),			-- Mystic Runesaber
+		un(35, i(156564)),			-- Shu-zen, the Divine Sentinel
+		un(35, mount(308078)),		-- Squeakers, the Trickster
+		un(2, i(92724)),			-- Swift Windsteed
+		un(35, i(166776)),			-- Sylverian Dreamer
+		un(35, i(160589)),			-- The Dreadwake
+		un(35, i(112326, {			-- Warforged Nightmare
+			un(35, i(112324)),		-- Nightmarish Hitching Post
 		})),
-		un(35, i(69846)),	-- Winged Guardian
-		un(35, i(166775)),	-- Vulpine Familiar
-		un(35, mount(302361, {	-- Alabaster Stormtalon		-- No in game item
+		un(35, i(69846)),			-- Winged Guardian
+		un(35, i(166775)),			-- Vulpine Familiar
+		un(35, mount(302361, {		-- Alabaster Stormtalon		-- No in game item
 			["races"] = ALLIANCE_ONLY,
 		})),
-		un(35, mount(302362, {	-- Alabaster Thunderwing	-- No in game item
+		un(35, mount(302362, {		-- Alabaster Thunderwing	-- No in game item
 			["races"] = HORDE_ONLY,
 		})),
 	}),
 	n(-162, {	-- Pets
-		un(35, i(106240)),	-- Alterac Brew-Pup
-		un(35, i(118516)),	-- Argi
-		un(35, i(98550)),	-- Blossoming Ancient
-		un(35, i(128424)),	-- Brightpaw
-		un(35, i(160588)),	-- Cap'n Crackers
-		un(2, i(70099)),	-- Cenarion Hatchling
-		un(35, i(92707)),	-- Cinder Kitten
+		un(35, i(106240)),			-- Alterac Brew-Pup
+		un(35, i(118516)),			-- Argi
+		un(35, i(98550)),			-- Blossoming Ancient
+		un(35, i(128424)),			-- Brightpaw
+		un(35, i(160588)),			-- Cap'n Crackers
+		un(2, i(70099)),			-- Cenarion Hatchling
+		un(35, i(92707)),			-- Cinder Kitten
 		un(35, battlepet(2623)),	-- Dottie (no in-game item)
-		un(2, i(69847)),	-- Guardian Cub (Original Version which was bound for 24h)
-		un(7, i(72068)),	-- Guardian Cub
-		un(2, i(49662, {	-- Gryphon Hatchling
+		un(2, i(69847)),			-- Guardian Cub (Original Version which was bound for 24h)
+		un(7, i(72068)),			-- Guardian Cub
+		un(2, i(49662, {			-- Gryphon Hatchling
 			["description"] = "This item is available only if you purchase the Plush Toy and redeem the code.",
 		})),
-		un(35, i(49693)),	-- Lil' K.T.
-		un(35, i(68385)),	-- Lil' Ragnaros
-		un(2, i(54847)),	-- Lil' XT
-		un(35, i(141893)),	-- Mischief
-		un(2, i(68384)),	-- Moonkin egg (You had to open the Egg on both faction sides before 6.0.1 to receive your Moonkin Pet). You get both now automaticly
-		un(2, a(i(68618))),	-- Moonkin Hatchling
-		un(2, h(i(68619))),	-- Moonkin Hatchling
-		un(35, i(49665)),	-- Pandaren Monk
-		un(35, i(151234)),	-- Shadow
-		un(35, i(78916)),	-- Soul of the Aspects
-		un(35, i(147900)),	-- Twilight
-		un(35, i(160587)),	-- Whomper
-		un(2, i(49663, {	-- Wind Rider Cub
+		un(35, i(49693)),			-- Lil' K.T.
+		un(35, i(68385)),			-- Lil' Ragnaros
+		un(2, i(54847)),			-- Lil' XT
+		un(35, battlepet(671)),		-- Lucky Quilen Cub
+		un(35, i(141893)),			-- Mischief
+		un(2, i(68384)),			-- Moonkin egg (You had to open the Egg on both faction sides before 6.0.1 to receive your Moonkin Pet). You get both now automaticly
+		un(2, a(i(68618))),			-- Moonkin Hatchling
+		un(2, h(i(68619))),			-- Moonkin Hatchling
+		un(35, i(49665)),			-- Pandaren Monk
+		un(35, i(151234)),			-- Shadow
+		un(35, i(78916)),			-- Soul of the Aspects
+		un(35, i(147900)),			-- Twilight
+		un(35, i(160587)),			-- Whomper
+		un(2, i(49663, {			-- Wind Rider Cub
 			["description"] = "This item is available only if you purchase the Plush Toy and redeem the code.",
 		})),
 	}),
--- .contrib/Parser/DATAS/14 - Mounts/In-Game Shop.lua
+++ .contrib/Parser/DATAS/14 - Mounts/In-Game Shop.lua
@@ -4,23 +4,24 @@
 _.Mounts =
 {
 	n(-36, { 	-- In-Game Shop
-		un(35, i(95341)),	-- Armored Bloodwing
-		un(35, i(54811)),	-- Celestial Steed
-		un(35, i(97989)),	-- Enchanted Fey Dragon
-		un(35, i(112327)),	-- Grinning Reaver
-		un(35, i(78924)),	-- Heart of the Aspects
-		un(35, i(166774)),	-- Hogrus, Swine of Good Fortune
-		un(35, i(107951)),	-- Iron Skyreaver
-		un(35, i(147901)),	-- Luminous Starseeker
-		un(35, i(122469)),	-- Mystic Runesaber
-		un(35, i(156564)),	-- Shu-zen, the Divine Sentinel
+		un(35, i(95341)),		-- Armored Bloodwing
+		un(35, i(54811)),		-- Celestial Steed
+		un(35, i(97989)),		-- Enchanted Fey Dragon
+		un(35, i(112327)),		-- Grinning Reaver
+		un(35, i(78924)),		-- Heart of the Aspects
+		un(35, i(166774)),		-- Hogrus, Swine of Good Fortune
+		un(35, mount(124659)),	-- Imperial Quilen
+		un(35, i(107951)),		-- Iron Skyreaver
+		un(35, i(147901)),		-- Luminous Starseeker
+		un(35, i(122469)),		-- Mystic Runesaber
+		un(35, i(156564)),		-- Shu-zen, the Divine Sentinel
 		un(35, mount(308078)),	-- Squeakers, the Trickster
-		un(35, i(92724)),	-- Swift Windsteed
-		un(35, i(166776)),	-- Sylverian Dreamer
-		un(35, i(160589)),	-- The Dreadwake
-		un(35, i(166775)),	-- Vulpine Familiar
-		un(35, i(112326)),	-- Warforged Nightmare
-		un(35, i(69846)),	-- Winged Guardian
+		un(35, i(92724)),		-- Swift Windsteed
+		un(35, i(166776)),		-- Sylverian Dreamer
+		un(35, i(160589)),		-- The Dreadwake
+		un(35, i(166775)),		-- Vulpine Familiar
+		un(35, i(112326)),		-- Warforged Nightmare
+		un(35, i(69846)),		-- Winged Guardian
 		un(35, mount(302361, {	-- Alabaster Stormtalon		-- No in game item
 			["races"] = ALLIANCE_ONLY,
 		})),
