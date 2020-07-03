de295e19d24f279eb95075d8ee566ce561f2fa92
Pr3vention <Pr3vention@gmail.com>
2020-07-03 13:00:34 -0500

    Added missing details for Zandalari heritage armor
    Tabard was also removed since it isn't rewarded as part of the heritage armor quest

--- .contrib/Parser/DATAS/12 - Item Sets/Heritage Armor/Heritage of the Zandalari.lua
+++ .contrib/Parser/DATAS/12 - Item Sets/Heritage Armor/Heritage of the Zandalari.lua
@@ -3,41 +3,21 @@
 -----------------------------------------------------
 _.GearSets = 
 {
-	{	-- Heritage Armor
-		["npcID"] = -254,	-- Heritage Armor
-		["g"] = {
-			gs(1742, {	-- Heritage of the Zandalari
-				["groups"] = {
-				{	-- Zandalari Mask
-					["itemID"] = 164993,	-- Zandalari Mask
-				},
-				{	-- Zandalari Pauldrons
-					["itemID"] = 164994,	-- Zandalari Pauldrons
-				},
-				{	-- Zandalari Torque
-					["itemID"] = 164995,	-- Zandalari Torque
-				},
-				{	-- Tabard of the Zandalari
-					["itemID"] = 165001,	-- Tabard of the Zandalari
-				},
-				{	-- Zandalari Bracers
-					["itemID"] = 165000,	-- Zandalari Bracers
-				},
-				{	-- Zandalari Blade-Turners
-					["itemID"] = 164996,	-- Zandalari Blade-Turners
-				},
-				{	-- Zandalari Belt
-					["itemID"] = 164997,	-- Zandalari Belt
-				},
-				{	-- Zandalari Leggings
-					["itemID"] = 164998,	-- Zandalari Leggings
-				},
-				{	-- Zandalari Shinguards
-					["itemID"] = 164999,	-- Zandalari Shinguards
-				},
-				},
-				["races"] = { 31 },
-			}),
-		},
-	},	
+	n(-254, {	-- Heritage Armor
+		gs(1742, {	-- Heritage of the Zandalari
+			["lvl"] = 110,
+			["races"] = { ZANDALARI },
+			["questID"] = 53721, -- there isn't an questline for this heritage armor so keeping it simple attaching the questID here
+			["g"] = {
+				i(164993),	-- Zandalari Mask
+				i(164994),	-- Zandalari Pauldrons
+				i(164995),	-- Zandalari Torque
+				i(165000),	-- Zandalari Bracers
+				i(164996),	-- Zandalari Blade-Turners
+				i(164997),	-- Zandalari Belt
+				i(164998),	-- Zandalari Leggings
+				i(164999),	-- Zandalari Shinguards
+			},
+		}),
+	}),	
 };
