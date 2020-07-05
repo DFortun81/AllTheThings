c406a16a9cc144b427a450219d38e4a07adfa08f
Jens Rogier <jens.rogier@gmail.com>
2020-07-05 10:27:38 +0200

    Added cost for "Flimsy Yellow Balloon"

--- .contrib/Parser/DATAS/07 - Events/Darkmoon Faire/Vendors.lua
+++ .contrib/Parser/DATAS/07 - Events/Darkmoon Faire/Vendors.lua
@@ -499,7 +499,9 @@ _.WorldEvents =
 					}),
 					i(75040),	-- Flimsy Darkmoon Balloon
 					i(75041),	-- Flimsy Green Balloon
-					i(75042),	-- Flimsy Yellow Balloon (TOY!)
+					i(75042, {	-- Flimsy Yellow Balloon (TOY!)
+						["cost"] = 800,
+					}),
 					i(164969, {	-- Horse Balloon (PET!)
 						["cost"] = { { "c", 515, 90 }, },	-- 90x Darkmoon Prize Ticket
 					}),
