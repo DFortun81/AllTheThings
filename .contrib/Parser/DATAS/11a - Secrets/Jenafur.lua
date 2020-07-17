f02766ec48379375ed6126eb184fef9ffd6147eb
Pr3vention <Pr3vention@gmail.com>
2020-07-16 20:39:22 -0500

    Added the first two known steps for Jenafur

---
+++ .contrib/Parser/DATAS/11a - Secrets/Jenafur.lua
@@ -0,0 +1,21 @@
+---------------------------------------------
+--     S E C R E T S     M O D U L E       --
+---------------------------------------------
+_.Secrets = 
+{
+	n(159783, { 	-- Jenafur
+		["description"] = "***Quest tracking enabled is required to see all the steps.***\n\nThis secret is a work in progress!",
+		["g"] = {
+			n(159799, {	-- Amara Lunastar
+				["questID"] = 58076,	-- Step 1: Talk to Amara Lunastar about her cat, Jenafur
+				["coord"] = { 17.4, 49.3, 63 },
+				["description"] = "|cFFFFFFFFStep 1:|r Go to |cFFFFFFFF17.4, 49.3|r in Ashenvale. Speak with |cFFFFD700Amara Lunastar|r and follow her dialog about her cat.",
+			}),
+			o(339211, {	-- Empty Dish
+				["questID"] = 58098,	-- Step 2: Find the empty dish bowl
+				["coord"] = { 44.2, 53.0, 37 },
+				["description"] = "|cFFFFFFFFStep 2:|r Go to |cFFFFFFFF44.2, 53.0|r in Elwynn Forest to find the |cFFFFD700Empty Dish|r.",
+			}),
+		},
+	}),
+};
