73bba9cccc416ada0dbc0a6da96099a88f24b393
TKTalonzor <sebastian.culic1@gmail.com>
2020-07-25 18:47:24 +0300

    Removed Celestial Tournament duplicate pets, the pets and achievement criteria is under the vendor.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Timeless Isle/Vendors.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/Timeless Isle/Vendors.lua
@@ -185,15 +185,35 @@ _.Zones =
 					["g"] = {
 						i(102145, {	-- Chi-Chi, Hatchling of Chi-Ji
 							["cost"] = { { "i", 101529, 3 }, },	-- 3x Celestial Coin
+							["g"] = {
+								crit(2, {	-- Chi-Chi, Hatchling of Chi-Ji
+									["achievementID"] = 8519,	-- Celestial Family
+								}),
+							},
 						}),
 						i(101771, {	-- Xu-Fu, Cub of Xuen
 							["cost"] = { { "i", 101529, 3 }, },	-- 3x Celestial Coin
+							["g"] = {
+								crit(1, {	-- Xu-Fu, Cub of Xuen
+									["achievementID"] = 8519,	-- Celestial Family
+								}),
+							},
 						}),
 						i(102147, {	-- Yu'la, Broodling of Yu'lon
 							["cost"] = { { "i", 101529, 3 }, },	-- 3x Celestial Coin
+							["g"] = {
+								crit(3, {	-- Yu'la, Broodling of Yu'lon
+									["achievementID"] = 8519,	-- Celestial Family
+								}),
+							},
 						}),
 						i(102146, {	-- Zao, Calfling of Niuzao
 							["cost"] = { { "i", 101529, 3 }, },	-- 3x Celestial Coin
+							["g"] = {
+								crit(4, {	-- Zao, Calfling of Niuzao
+									["achievementID"] = 8519,	-- Celestial Family
+								}),
+							},
 						}),
 					},
 				}),
--- .contrib/Parser/DATAS/08 - Pet Battles/Pet Battle Dungeons.lua
+++ .contrib/Parser/DATAS/08 - Pet Battles/Pet Battle Dungeons.lua
@@ -13,43 +13,6 @@ _.PetBattles =
 			571,	-- Pet Dungeon
 		},
 		["g"] = {
-			n(73082, {	-- Master Li <Celestial Tournament Host>
-				["coord"] = { 34.8, 59.6, 554 },
-				["g"] = {
-					i(102145, {	-- Chi-Chi, Hatchling of Chi-Ji
-						["cost"] = { { "i", 101529, 3 }, },	-- 3x Celestial Coin
-						["g"] = {
-							crit(2, {	-- Chi-Chi, Hatchling of Chi-Ji
-								["achievementID"] = 8519,	-- Celestial Family
-							}),
-						},
-					}),
-					i(101771, {	-- Xu-Fu, Cub of Xuen
-						["cost"] = { { "i", 101529, 3 }, },	-- 3x Celestial Coin
-						["g"] = {
-							crit(1, {	-- Xu-Fu, Cub of Xuen
-								["achievementID"] = 8519,	-- Celestial Family
-							}),
-						},
-					}),
-					i(102147, {	-- Yu'la, Broodling of Yu'lon
-						["cost"] = { { "i", 101529, 3 }, },	-- 3x Celestial Coin
-						["g"] = {
-							crit(3, {	-- Yu'la, Broodling of Yu'lon
-								["achievementID"] = 8519,	-- Celestial Family
-							}),
-						},
-					}),
-					i(102146, {	-- Zao, Calfling of Niuzao
-						["cost"] = { { "i", 101529, 3 }, },	-- 3x Celestial Coin
-						["g"] = {
-							crit(4, {	-- Zao, Calfling of Niuzao
-								["achievementID"] = 8519,	-- Celestial Family
-							}),
-						},
-					}),
-				},
-			}),
 			q(33136, {	-- The Rainy Day is Here
 				["provider"] = { "n", 73082 },	-- Master Li
 				["coord"] = { 34.8, 59.6, 554 },
