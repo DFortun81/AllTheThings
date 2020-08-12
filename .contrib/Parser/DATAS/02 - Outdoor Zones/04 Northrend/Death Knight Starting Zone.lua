5de11dbfffbe7a5bec854d5a0b056d1742dd0bf2
Pr3vention <Pr3vention@gmail.com>
2020-08-12 18:32:53 -0500

    Added the incredibly disappointing "new" Death Knight starting experience

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/04 Northrend/Death Knight Starting Zone.lua
@@ -0,0 +1,41 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(113, {	-- Northrend
+		m(1602, {	-- Icecrown (DK starting zone)
+			n(QUESTS, {
+				q(58877, {	-- Death's Power Grows
+					["coord"] = { 49.5, 90.7, 1602 },
+					["classes"] = { DEATHKNIGHT },
+					["provider"] = { "n", 163016 },	-- The Lich King
+					["g"] = {
+						i(174488),	-- Freezing Greatsword of the Ebon Blade
+						i(174487),	-- Unholy Greatsword of the Ebon Blade
+						i(174486),	-- Crimson Greatsword of the Ebon Blade
+						i(174485),	-- Burning Greatsword of the Ebon Blade
+						i(174660),	-- Freezing Saber of the Ebon Blade
+						i(174661),	-- Unholy Saber of the Ebon Blade
+						i(174659),	-- Crimson Saber of the Ebon Blade
+						i(174662),	-- Burning Saber of the Ebon Blade
+					},
+				}),
+				q(58902, {	-- Defender of Azeroth
+					["coord"] = { 49.5, 90.7, 1602 },
+					["races"] = ALLIANCE_ONLY,
+					["classes"] = { DEATHKNIGHT },
+					["provider"] = { "n", 163016 },	-- The Lich King
+					["sourceQuest"] = 58877,	-- Death's Power Grows
+				}),
+				q(58903, {	-- Defender of Azeroth
+					["coord"] = { 49.5, 90.7, 1602 },
+					["races"] = HORDE_ONLY,
+					["classes"] = { DEATHKNIGHT },
+					["provider"] = { "n", 163016 },	-- The Lich King
+					["sourceQuest"] = 58877,	-- Death's Power Grows
+				}),
+			}),
+		}),
+	}),
+};
