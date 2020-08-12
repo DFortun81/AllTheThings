822509c38a75511312596863752cf4a1d586dfc1
Pr3vention <Pr3vention@gmail.com>
2020-08-11 21:34:03 -0500

    Fixed a couple more instances of breaking API changes

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -4061,7 +4061,7 @@ end
 					-- We only care about currencies in the addon at the moment.
 					for currencyID, _ in pairs(cache) do
 						-- Compare the name of the currency vs the name of the token
-						if select(1, GetCurrencyInfo(currencyID)) == name then
+						if select(1, C_CurrencyInfo.GetCurrencyInfo(currencyID)) == name then
 							AttachTooltipSearchResults(self, "currencyID:" .. currencyID, SearchForField, "currencyID", currencyID);
 							if app.Settings:GetTooltipSetting("currencyID") then self:AddDoubleLine(L["CURRENCY_ID"], tostring(currencyID)); end
 							self:Show();
@@ -4613,9 +4613,9 @@ app.BaseCurrencyClass = {
 		elseif key == "text" then
 			return C_CurrencyInfo.GetCurrencyLink(t.currencyID, 1) or select(1, C_CurrencyInfo.GetCurrencyInfo(t.currencyID));
 		elseif key == "icon" then
-			return select(3, GetCurrencyInfo(t.currencyID));
+			return select(3, C_CurrencyInfo.GetCurrencyInfo(t.currencyID));
 		elseif key == "icon" then
-			return select(3, GetCurrencyInfo(t.currencyID));
+			return select(3, C_CurrencyInfo.GetCurrencyInfo(t.currencyID));
 		else
 			-- Something that isn't dynamic.
 			return table[key];
@@ -9345,7 +9345,7 @@ local function RowOnEnter(self)
 						_,name,_,_,_,_,_,_,_,icon = GetItemInfo(v[2]);
 						amount = "x" .. formatNumericWithCommas(v[3]);
 					elseif _ == "c" then
-						name,_,icon = GetCurrencyInfo(v[2])
+						name,_,icon = C_CurrencyInfo.GetCurrencyInfo(v[2])
 						amount = "x" .. formatNumericWithCommas(v[3]);
 					elseif _ == "g" then
 						name = "";
