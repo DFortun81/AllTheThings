63f807f76e5fc30e9c504fa2a0938446f6dcf7d8
sadidorf <sadidorf@gmail.com>
2020-08-16 02:25:44 +0200

    Added 5/7 Storylines in Maldraxxus

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/4 Maldraxxus/0 - Metadata.lua
@@ -0,0 +1,13 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+
+_.Zones =
+{
+	m(1550, {	-- Shadowlands
+		m(1536, {	-- Maldraxxus
+			["achievementID"] = 14305,	-- Explore Maldraxxus
+			["description"] = "Within war-torn Maldraxxus, might —of all kinds— makes right. Here, in the birthplace of necromantic magic, those who master the powers of death turn legions of ambitious souls into relentless undead armies.",
+		}),
+	}),
+};
---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/4 Maldraxxus/Achievements.lua
@@ -0,0 +1,36 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+
+_.Zones =
+{
+	m(1550, {	-- Shadowlands
+		m(1536, {	-- Maldraxxus
+			n(ACHIEVEMENTS, {
+				ach(14206, {	-- Blade of the Primus
+					crit(1, {	-- Champion of Pain
+						["sourceQuest"] = 57515,	-- The House of the Chosen
+					}),
+					crit(2, {	-- 	House of the Chosen
+						["sourceQuest"] = 60886,	-- The Seat of the Primus
+					}),
+					crit(3, {	-- Matron of Spies
+						["sourceQuest"] = 59009,	-- Her Rightful Place
+					}),
+					crit(4, {	-- House of Constructs
+						["sourceQuest"] = 60733,	-- Front and Center
+					}),
+					crit(5, {	-- 	House of Plagues
+						["sourceQuest"] = 59231,	-- Fathomless Power
+					}),
+					crit(6, {	-- Ritual for the Damned
+						["sourceQuest"] = 59974,	-- A Soul Saved
+					}),
+					crit(7, {	--  The Empty Throne
+						["sourceQuest"] = 60737,	-- The Door to the Unknown
+					}),
+				}),
+			}),
+		}),
+	}),
+};
