a500014b4bd62139603ab3a5de0493640ea1b481
Alex <lightskywrites@gmail.com>
2020-10-09 05:20:10 -0500

    Several RAF rewards now appear with "Hide BoE Items" enabled while showing Blizzard Balance filtered items.

--- .contrib/Parser/DATAS/07 - Events/Blizzard Promotions/Recruit A Friend.lua
+++ .contrib/Parser/DATAS/07 - Events/Blizzard Promotions/Recruit A Friend.lua
@@ -7,31 +7,32 @@ _.WorldEvents =
 	n(-65, { -- Blizzard Promotions
 		["g"] = {
 			n(-546, { 	-- Recruit A Friend
-				un(REMOVED_FROM_GAME, ach(1436, {	-- Friends In High Places
-					un(REMOVED_FROM_GAME, i(37719)), 	-- Swift Zhevra
+				un(REMOVED_FROM_GAME, ach(1436, {			-- Friends In High Places
+					un(REMOVED_FROM_GAME, i(37719)), 		-- Swift Zhevra
 				})),
-				un(REMOVED_FROM_GAME, ach(4832, {	-- Friends In Even Higher Places
-					un(REMOVED_FROM_GAME, i(54860)), 	-- X-53 Touring Rocket
+				un(REMOVED_FROM_GAME, ach(4832, {			-- Friends In Even Higher Places
+					un(REMOVED_FROM_GAME, i(54860)), 		-- X-53 Touring Rocket
 				})),
-				un(REMOVED_FROM_GAME, ach(8213, {	-- Friends In Places Higher Yet
-					un(REMOVED_FROM_GAME, i(83086)), 	-- Heart of the Nightwing
+				un(REMOVED_FROM_GAME, ach(8213, {			-- Friends In Places Higher Yet
+					un(REMOVED_FROM_GAME, i(83086)), 		-- Heart of the Nightwing
 				})),
-				un(REMOVED_FROM_GAME, ach(8794, {	-- Friends In Places Even Higher Than That
-					un(REMOVED_FROM_GAME, i(106246)), 	-- Emerald Hippogryph
+				un(REMOVED_FROM_GAME, ach(8794, {			-- Friends In Places Even Higher Than That
+					un(REMOVED_FROM_GAME, i(106246)), 		-- Emerald Hippogryph
 				})),
-				un(REMOVED_FROM_GAME, ach(9925, {	-- Friends In Places Yet Even Higher Than That
-					un(REMOVED_FROM_GAME, i(118515)), 	-- Cindermane Charger
+				un(REMOVED_FROM_GAME, ach(9925, {			-- Friends In Places Yet Even Higher Than That
+					un(REMOVED_FROM_GAME, i(118515)), 		-- Cindermane Charger
 				})),
-				un(REMOVED_FROM_GAME, i(34518)), 	-- Golden Pig
-				un(REMOVED_FROM_GAME, i(34519)), 	-- Silver Pig
-				un(REMOVED_FROM_GAME, i(46894)),	-- Jade Tiger
+				un(REMOVED_FROM_GAME, i(34518)), 			-- Golden Pig
+				un(REMOVED_FROM_GAME, i(34519)), 			-- Silver Pig
+				un(REMOVED_FROM_GAME, i(46894)),			-- Jade Tiger
 				n(-548, {	-- China Only
 					ach(3636, {	-- Jade Tiger
 						["collectible"] = false,
 					}),
 				}),
-				un(REMOVED_FROM_GAME, i(49664)), 	-- Zipao Tiger
-				un(BLIZZARD_BALANCE, i(173300, {	-- Ensemble: Renowned Explorer's Attire
+				un(REMOVED_FROM_GAME, i(49664)), 			-- Zipao Tiger
+				un(BLIZZARD_BALANCE, i(173300, {			-- Ensemble: Renowned Explorer's Attire
+					["b"] = 1,
 					["g"] = {
 						un(BLIZZARD_BALANCE, i(171324)),	-- Renowned Explorer's Akubra
 						un(BLIZZARD_BALANCE, i(171329)),	-- Renowned Explorer's Canteen Pouch
@@ -44,16 +45,29 @@ _.WorldEvents =
 						un(BLIZZARD_BALANCE, i(171331)),	-- Renowned Explorer's Versatile Vest
 					},
 				})),
-				un(BLIZZARD_BALANCE, i(171363)),	-- Illusion: Stinging Sands
-				un(BLIZZARD_BALANCE, i(173299)),	-- Keys to the Explorer's Jungle Hopper (MOUNT!)
-				un(BLIZZARD_BALANCE, i(173297)),	-- Reins of the Explorer's Dunetrekker (MOUNT!)
-				un(BLIZZARD_BALANCE, i(173298, {	-- Explorer's Certification
-					un(BLIZZARD_BALANCE, title(410)),	-- Renowned Explorer <Name>
-				})),
-				un(BLIZZARD_BALANCE, i(171333)),	-- Renowned Explorer's Rucksack
-				un(BLIZZARD_BALANCE, i(171361)),	-- Renowned Explorer's Tabard
-				un(BLIZZARD_BALANCE, i(173296)),	-- Rikki's Pith Helmet (PET!)
-				un(BLIZZARD_BALANCE, i(173301)),	-- Game Time
+				un(BLIZZARD_BALANCE, i(171363)),			-- Illusion: Stinging Sands
+				un(BLIZZARD_BALANCE, i(173299)),			-- Keys to the Explorer's Jungle Hopper (MOUNT!)
+				un(BLIZZARD_BALANCE, i(173297)),			-- Reins of the Explorer's Dunetrekker (MOUNT!)
+				{
+					["itemID"] = 173298,					-- Explorer's Certification
+					["b"] = 1,
+					["u"] = BLIZZARD_BALANCE,
+					["g"] = {
+						un(BLIZZARD_BALANCE, title(410)),	-- Renowned Explorer <Name>
+					},
+				},
+				{
+					["itemID"] = 171333,					-- Renowned Explorer's Rucksack
+					["b"] = 1,
+					["u"] = BLIZZARD_BALANCE,
+				},
+				{
+					["itemID"] = 171361,					-- Renowned Explorer's Tabard
+					["b"] = 1,
+					["u"] = BLIZZARD_BALANCE,
+				},
+				un(BLIZZARD_BALANCE, i(173296)),			-- Rikki's Pith Helmet (PET!)
+				un(BLIZZARD_BALANCE, i(173301)),			-- Game Time
 			}),
 		},
 	}),
