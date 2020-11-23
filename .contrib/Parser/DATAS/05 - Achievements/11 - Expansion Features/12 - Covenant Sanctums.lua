07a3fcb9e60ca02c243d1b52789eec1dae6e48f5
mogwai316 <bryan.harden@gmail.com>
2020-11-23 18:02:01 -0500

    Added some missing Covenant Sanctums achieves

--- .contrib/Parser/DATAS/05 - Achievements/11 - Expansion Features/12 - Covenant Sanctums.lua
+++ .contrib/Parser/DATAS/05 - Achievements/11 - Expansion Features/12 - Covenant Sanctums.lua
@@ -6,8 +6,31 @@ _.Achievements =
 {
 	n(-10055, {	-- Expansion Features
 		n(-10104, {	-- Covenant Sanctums
+			ach(14835),	-- A Resolute Bond
 			ach(14636),	-- Adventurer in Chief
+			ach(14843),	-- Adventures: Harmony of Purpose
+			ach(14844, {	-- Adventures: Into the Breach
+				crit(1),	-- Breach the Planes
+				crit(2),	-- Ghelak's Revenge
+				crit(3),	-- Coalescing Stygia
+				crit(4),	-- Topple the Giants
+				crit(5),	-- Dark Skies, Darker Prospects
+				crit(6),	-- Dravok's Plot
+				crit(7),	-- Grandiose Designs
+				crit(8),	-- Cutting the Threads
+				crit(9),	-- Shades of Despair
+				crit(10),	-- Krala, Wings of Woe
+			}),
+			ach(14847, {	-- Adventures: Protector of the Shadowlands
+				ach(14845),	-- Adventures: A Step in the Right Direction
+				ach(14846),	-- Adventures: Leaves in the Forest
+			}),
+			ach(14842, {	-- Adventures: Strength and Resilience
+				ach(14840),	-- Adventures: Learning the Ropes
+				ach(14841),	-- Adventures: Now You're Cooking
+			}),
 			ach(14789),	-- All Spirits Great and Small
+			ach(14839),	-- Bound to Adventure
 			ach(14627),	-- Choosing your Purpose
 			ach(14631, {	-- Champion of the Covenant
 				ach(14628),	-- The Road to Renown
@@ -20,6 +43,7 @@ _.Achievements =
 			ach(14633),	-- Master Navigator
 			ach(14780),	-- Meditation Master
 			ach(14775),	-- Mush Appreciated
+			ach(14837),	-- Nexus of Bonds
 			ach(14502),	-- Pursuing Loyalty
 			ach(14749),	-- Rendle's Big Day
 			ach(14777),	-- Restoration Expert
@@ -62,6 +86,7 @@ _.Achievements =
 				ach(14751),	-- The Gang's All Here
 				ach(14753),	-- It's a Wrap
 			}),
+			ach(14836),	-- Unwavering Bond
 			ach(14637),	-- Your Covenant's Flavor
 		}),
 	}),
