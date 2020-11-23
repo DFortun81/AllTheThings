4458024ac165b93c92e7c8f0efb90658bbd53c90
Dmitry Volodin <mr.molkree@gmail.com>
2020-11-23 08:03:53 +0300

    - better way of handling icons

--- .contrib/Parser/DATAS/09 - PvP/01 Classic PvP Pre-Seasons.lua
+++ .contrib/Parser/DATAS/09 - PvP/01 Classic PvP Pre-Seasons.lua
@@ -5,10 +5,7 @@
 _.PVP =
 {
 	n(-10002, {	-- Classic
-		["icon"] = "Interface\\Addons\\AllTheThings\\assets\\Classic",
-		["g"] = {
-			n(-663, {	-- Classic Pre-Season PvP Gear
-			}),
-		},
+		n(-663, {	-- Classic Pre-Season PvP Gear
+		}),
 	}),
 };
