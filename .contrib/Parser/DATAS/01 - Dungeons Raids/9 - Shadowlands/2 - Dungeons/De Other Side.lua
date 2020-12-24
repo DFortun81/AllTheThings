9ca902d463e7ef0a8708ee1706a2f6e142b1e149
slumber <slumber@love.com>
2020-12-24 17:13:37 -0600

    - added conduits/legendary powers to de other side

--- .contrib/Parser/DATAS/01 - Dungeons Raids/9 - Shadowlands/2 - Dungeons/De Other Side.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/9 - Shadowlands/2 - Dungeons/De Other Side.lua
@@ -14,8 +14,8 @@ _.Instances = { tier(9, {	-- Shadowlands
 		["g"] = {
 			n(QUESTS, {
 				q(62456,{	-- De Other Side: Dealin' Wit' Da Boss
+					["sourceQuests"] = { 62458 },	-- De Other Side
 					["provider"] = { "n", 174327 },	-- Mask of Bwonsamdi
-					["sourceQuest"] = 62458,	-- De Other Side
 					["_drop"] = { "g" },	-- ignore Anima item that insists on being part of this quest
 				}),
 			}),
@@ -23,18 +23,28 @@ _.Instances = { tier(9, {	-- Shadowlands
 				e(2408, {	-- Hakkar the Soulflayer
 					["crs"] = { 166473 },	-- Hakkar the Soulflayer
 					["g"] = {
+						i(181737),	-- Nourishing Chi
+						i(182621),	-- One With the Beast
+						i(183479),	-- Umbral Intensity
+						i(182134),	-- Unruly Winds
+						i(183504),	-- Well-Placed Steel
 						i(179330),	-- Zin'khas, Blade of the Fallen God
 						i(179328),	-- Bloodspiller
-						i(179322),	-- Windscale Moccasins
-						i(179324),	-- Soulfeather Breeches
 						i(179325),	-- Hakkari Revenant's Grips
 						i(179326),	-- Girdle of the Soulflayer
+						i(179324),	-- Soulfeather Breeches
+						i(179322),	-- Windscale Moccasins
 						i(179331),	-- Blood-Spattered Scale
 					},
 				}),
 				e(2409, {	-- The Manastorms
 					["crs"] = { 101976 },	-- Millificent Manastorm
 					["g"] = {
+						i(181509),	-- Arcane Prodigy
+						i(182748),	-- Borne of Blood
+						i(183216),	-- Memory of a Burning Wound
+						i(183271),	-- Memory of the Infinite Arcane
+						i(183369),	-- Memory of Wilfred's Sigil of Superior Summoning
 						i(179339),	-- Whizblast Walking Stick
 						i(179340),	-- Supercollider
 						i(179335),	-- Manastorm's Magnificent Threads
@@ -47,13 +57,17 @@ _.Instances = { tier(9, {	-- Shadowlands
 				e(2398, {	-- Dealer Xy'exa
 					["crs"] = { 101976 },	-- Millificent Manastorm
 					["g"] = {
+						i(183396),	-- Flame Infusion
+						i(183265),	-- Memory of a Wildfire Cluster
+						i(182624),	-- Show of Force
+						i(183509),	-- Sleight of Hand
 						i(179347),	-- Collector's Pulse Staff
 						i(179348),	-- Xy Cartel Crossbow
-						i(179349),	-- Dealer Xy'exa's Cape
-						i(179343),	-- Sash of Exquisite Acquisitions
 						i(179344),	-- Far Traveler's Shoulderpads
-						i(179345),	-- Spatial Rift Striders
+						i(179349),	-- Dealer Xy'exa's Cape
 						i(179346),	-- Breastplate of Fatal Contrivances
+						i(179343),	-- Sash of Exquisite Acquisitions
+						i(179345),	-- Spatial Rift Striders
 						i(179350),	-- Inscrutable Quantum Device
 					},
 				}),
@@ -61,15 +75,25 @@ _.Instances = { tier(9, {	-- Shadowlands
 					["crs"] = { 169769 },	-- 	Mueh'zala
 					["g"] = {
 						ach(14373),	-- De Other Side
-						i(179351),	-- Mueh'zala's Hexthread Sarong
-						i(179352),	-- Primeval Soul's Ankleguards
-						i(179353),	-- Harness of Twisted Whims
-						i(179354),	-- Reality-Shatter Vambraces
-						i(179355),	-- Death God's Signet
-						i(179356),	-- Shadowgrasp Totem
 						i(182178, {	-- Raptor Soul
 							["questID"] = 62433,	-- Raptor Soul
 						}),
+						i(181943),	-- Eradicating Blow
+						i(183490),	-- Floral Recycling
+						i(182145),	-- Heavy Rainfall
+						i(181376),	-- Inner Fury
+						i(183329),	-- Memory of a Prism of Shadow and Fire
+						i(183282),	-- Memory of the Fatal Touch
+						i(183350),	-- Memory of the Great Sundering
+						i(182138),	-- Mind Devourer
+						i(182684),	-- Resolute Defender
+						i(182736),	-- Rolling Agony
+						i(179353),	-- Harness of Twisted Whims
+						i(179354),	-- Reality-Shatter Vambraces
+						i(179351),	-- Mueh'zala's Hexthread Sarong
+						i(179352),	-- Primeval Soul's Ankleguards
+						i(179355),	-- Death God's Signet
+						i(179356),	-- Shadowgrasp Totem
 					},
 				}),
 			}),
@@ -79,18 +103,28 @@ _.Instances = { tier(9, {	-- Shadowlands
 					e(2408, {	-- Hakkar the Soulflayer
 						["crs"] = { 166473 },	-- Hakkar the Soulflayer
 						["g"] = {
+							i(181737),	-- Nourishing Chi
+							i(182621),	-- One With the Beast
+							i(183479),	-- Umbral Intensity
+							i(182134),	-- Unruly Winds
+							i(183504),	-- Well-Placed Steel
 							i(179330),	-- Zin'khas, Blade of the Fallen God
 							i(179328),	-- Bloodspiller
-							i(179322),	-- Windscale Moccasins
-							i(179324),	-- Soulfeather Breeches
 							i(179325),	-- Hakkari Revenant's Grips
 							i(179326),	-- Girdle of the Soulflayer
+							i(179324),	-- Soulfeather Breeches
+							i(179322),	-- Windscale Moccasins
 							i(179331),	-- Blood-Spattered Scale
 						},
 					}),
 					e(2409, {	-- The Manastorms
 						["crs"] = { 101976 },	-- Millificent Manastorm
 						["g"] = {
+							i(181509),	-- Arcane Prodigy
+							i(182748),	-- Borne of Blood
+							i(183216),	-- Memory of a Burning Wound
+							i(183271),	-- Memory of the Infinite Arcane
+							i(183369),	-- Memory of Wilfred's Sigil of Superior Summoning
 							i(179339),	-- Whizblast Walking Stick
 							i(179340),	-- Supercollider
 							i(179335),	-- Manastorm's Magnificent Threads
@@ -103,13 +137,17 @@ _.Instances = { tier(9, {	-- Shadowlands
 					e(2398, {	-- Dealer Xy'exa
 						["crs"] = { 101976 },	-- Millificent Manastorm
 						["g"] = {
+							i(183396),	-- Flame Infusion
+							i(183265),	-- Memory of a Wildfire Cluster
+							i(182624),	-- Show of Force
+							i(183509),	-- Sleight of Hand
 							i(179347),	-- Collector's Pulse Staff
 							i(179348),	-- Xy Cartel Crossbow
-							i(179349),	-- Dealer Xy'exa's Cape
-							i(179343),	-- Sash of Exquisite Acquisitions
 							i(179344),	-- Far Traveler's Shoulderpads
-							i(179345),	-- Spatial Rift Striders
+							i(179349),	-- Dealer Xy'exa's Cape
 							i(179346),	-- Breastplate of Fatal Contrivances
+							i(179343),	-- Sash of Exquisite Acquisitions
+							i(179345),	-- Spatial Rift Striders
 							i(179350),	-- Inscrutable Quantum Device
 						},
 					}),
@@ -117,15 +155,25 @@ _.Instances = { tier(9, {	-- Shadowlands
 						["crs"] = { 169769 },	-- 	Mueh'zala
 						["g"] = {
 							ach(14408),	-- Heroic: De Other Side
-							i(179351),	-- Mueh'zala's Hexthread Sarong
-							i(179352),	-- Primeval Soul's Ankleguards
-							i(179353),	-- Harness of Twisted Whims
-							i(179354),	-- Reality-Shatter Vambraces
-							i(179355),	-- Death God's Signet
-							i(179356),	-- Shadowgrasp Totem
 							i(182178, {	-- Raptor Soul
 								["questID"] = 62433,	-- Raptor Soul
 							}),
+							i(181943),	-- Eradicating Blow
+							i(183490),	-- Floral Recycling
+							i(182145),	-- Heavy Rainfall
+							i(181376),	-- Inner Fury
+							i(183329),	-- Memory of a Prism of Shadow and Fire
+							i(183282),	-- Memory of the Fatal Touch
+							i(183350),	-- Memory of the Great Sundering
+							i(182138),	-- Mind Devourer
+							i(182684),	-- Resolute Defender
+							i(182736),	-- Rolling Agony
+							i(179353),	-- Harness of Twisted Whims
+							i(179354),	-- Reality-Shatter Vambraces
+							i(179351),	-- Mueh'zala's Hexthread Sarong
+							i(179352),	-- Primeval Soul's Ankleguards
+							i(179355),	-- Death God's Signet
+							i(179356),	-- Shadowgrasp Totem
 						},
 					}),
 				},
@@ -140,12 +188,17 @@ _.Instances = { tier(9, {	-- Shadowlands
 					e(2408, {	-- Hakkar the Soulflayer
 						["crs"] = { 166473 },	-- Hakkar the Soulflayer
 						["g"] = {
+							i(181737),	-- Nourishing Chi
+							i(182621),	-- One With the Beast
+							i(183479),	-- Umbral Intensity
+							i(182134),	-- Unruly Winds
+							i(183504),	-- Well-Placed Steel
 							i(179330),	-- Zin'khas, Blade of the Fallen God
 							i(179328),	-- Bloodspiller
-							i(179322),	-- Windscale Moccasins
-							i(179324),	-- Soulfeather Breeches
 							i(179325),	-- Hakkari Revenant's Grips
 							i(179326),	-- Girdle of the Soulflayer
+							i(179324),	-- Soulfeather Breeches
+							i(179322),	-- Windscale Moccasins
 							i(179331),	-- Blood-Spattered Scale
 						},
 					}),
@@ -153,6 +206,11 @@ _.Instances = { tier(9, {	-- Shadowlands
 						["crs"] = { 101976 },	-- Millificent Manastorm
 						["g"] = {
 							ach(14374),	-- Couple's Therapy
+							i(181509),	-- Arcane Prodigy
+							i(182748),	-- Borne of Blood
+							i(183216),	-- Memory of a Burning Wound
+							i(183271),	-- Memory of the Infinite Arcane
+							i(183369),	-- Memory of Wilfred's Sigil of Superior Summoning
 							i(179339),	-- Whizblast Walking Stick
 							i(179340),	-- Supercollider
 							i(179335),	-- Manastorm's Magnificent Threads
@@ -166,13 +224,17 @@ _.Instances = { tier(9, {	-- Shadowlands
 						["crs"] = { 101976 },	-- Millificent Manastorm
 						["g"] = {
 							ach(14606),	-- Thinking with...
+							i(183396),	-- Flame Infusion
+							i(183265),	-- Memory of a Wildfire Cluster
+							i(182624),	-- Show of Force
+							i(183509),	-- Sleight of Hand
 							i(179347),	-- Collector's Pulse Staff
 							i(179348),	-- Xy Cartel Crossbow
-							i(179349),	-- Dealer Xy'exa's Cape
-							i(179343),	-- Sash of Exquisite Acquisitions
 							i(179344),	-- Far Traveler's Shoulderpads
-							i(179345),	-- Spatial Rift Striders
+							i(179349),	-- Dealer Xy'exa's Cape
 							i(179346),	-- Breastplate of Fatal Contrivances
+							i(179343),	-- Sash of Exquisite Acquisitions
+							i(179345),	-- Spatial Rift Striders
 							i(179350),	-- Inscrutable Quantum Device
 						},
 					}),
@@ -180,15 +242,25 @@ _.Instances = { tier(9, {	-- Shadowlands
 						["crs"] = { 169769 },	-- 	Mueh'zala
 						["g"] = {
 							ach(14409),	-- Mythic: De Other Side
-							i(179351),	-- Mueh'zala's Hexthread Sarong
-							i(179352),	-- Primeval Soul's Ankleguards
-							i(179353),	-- Harness of Twisted Whims
-							i(179354),	-- Reality-Shatter Vambraces
-							i(179355),	-- Death God's Signet
-							i(179356),	-- Shadowgrasp Totem
 							i(182178, {	-- Raptor Soul
 								["questID"] = 62433,	-- Raptor Soul
 							}),
+							i(181943),	-- Eradicating Blow
+							i(183490),	-- Floral Recycling
+							i(182145),	-- Heavy Rainfall
+							i(181376),	-- Inner Fury
+							i(183329),	-- Memory of a Prism of Shadow and Fire
+							i(183282),	-- Memory of the Fatal Touch
+							i(183350),	-- Memory of the Great Sundering
+							i(182138),	-- Mind Devourer
+							i(182684),	-- Resolute Defender
+							i(182736),	-- Rolling Agony
+							i(179353),	-- Harness of Twisted Whims
+							i(179354),	-- Reality-Shatter Vambraces
+							i(179351),	-- Mueh'zala's Hexthread Sarong
+							i(179352),	-- Primeval Soul's Ankleguards
+							i(179355),	-- Death God's Signet
+							i(179356),	-- Shadowgrasp Totem
 						},
 					}),
 				},
