515f43e740ac02fef5d97c734e32d12e2a4e5948
slumber <slumber@love.com>
2021-01-16 13:32:21 -0600

    - added tier 3 mirror's edge quests + transmog rewards

--- .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Revendreth/Covenant - Venthyr.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Revendreth/Covenant - Venthyr.lua
@@ -1608,7 +1608,7 @@ _.Zones =
 								}),
 								n(-962, {	-- Tier 2: Homegrown Help
 									q(61766, {	-- Homegrown Help
-										-- ["sourceQuests"] = {  },	-- TODO: upgrade to Tier 2 without doing any Tier 1 quests to verify sourceQuest
+										-- ["sourceQuests"] = {  },	-- TODO: upgrade to Tier 2 without doing any Tier 1 quests to verify SQ
 										["provider"] = { "n", 164966 },	-- Temel
 										["coord"] = { 27.9, 43.0, 1525 },	-- Revendreth
 									}),
@@ -1933,34 +1933,121 @@ _.Zones =
 							}),
 							n(-953, {	-- Tier 3: Mirror's Edge
 								["description"] = "Requires Renown 19.",
-								--o(357565, {	-- Repaired Transport Mirror
-									--["description"] = "Requires Tier 3 Mirror Network to see.",
-									--["npcID"] = 173634,	-- Muckribbon
-									--["coord"] = { 69.3, 77.9, 1525 },
-									--["questID"] = 62196,
-									["g"] = sharedData({["customCollect"] = "SL_COV_VEN",}, {
-										i(183855),	-- Stony's Infused Ruby
-									--},
-								--}),
-							--[[
-							--object(xxxxxx, {	--
-								--["description"] = "Chance to loot from repairing broken mirrors.",
-							--]]
-									--["g"] = {
-										i(183798),	-- Silessa's Battle Harness
+								["g"] = {
+									n(-908, {	-- Rewards
+									--	TODO: cloaks might need to be marked with armor spec classes, need to get one and then see if cloth set cloak can be used on a druid, for example.  there's no blizz text indicating that they're locked to users of a specific armor type so it needs verification i guess
+										i(183798),	-- Silessa's Battle Harness (MOUNT!)
+										i(183855),	-- Stony's Infused Ruby (PET!)
+										i(181064),	-- Burnished Death Shroud Belt
+										i(181065),	-- Burnished Death Shroud Bindings
+										i(181059),	-- Burnished Death Shroud Boots
+										i(181062),	-- Burnished Death Shroud Breeches
+										i(181066),	-- Burnished Death Shroud Cloak
+										i(181060),	-- Burnished Death Shroud Gloves
+										i(181061),	-- Burnished Death Shroud Hood
+										i(181063),	-- Burnished Death Shroud Spaulders
+										i(181058),	-- Burnished Death Shroud Vest
+										i(181091),	-- Fearstalker's Burnished Belt
+										i(181092),	-- Fearstalker's Burnished Bracers
+										i(181093),	-- Fearstalker's Burnished Cloak
+										i(181087),	-- Fearstalker's Burnished Gauntlets
+										i(181085),	-- Fearstalker's Burnished Hauberk
+										i(181088),	-- Fearstalker's Burnished Helm
+										i(181089),	-- Fearstalker's Burnished Leggings
+										i(181090),	-- Fearstalker's Burnished Monnion
+										i(181086),	-- Fearstalker's Burnished Sabatons
+										i(181023),	-- Dread Sentinel's Burnished Chestplate
+										i(181030),	-- Dread Sentinel's Burnished Cloak
+										i(181028),	-- Dread Sentinel's Burnished Girdle
+										i(181024),	-- Dread Sentinel's Burnished Greatboots
+										i(181025),	-- Dread Sentinel's Burnished Grips
+										i(181022),	-- Dread Sentinel's Burnished Headgear
+										i(181026),	-- Dread Sentinel's Burnished Legguards
+										i(181027),	-- Dread Sentinel's Burnished Spaulders
+										i(181029),	-- Dread Sentinel's Burnished Vambraces
+										i(181129),	-- Soulbreaker's Burnished Drape
+										i(181123),	-- Soulbreaker's Burnished Handwraps
+										i(181124),	-- Soulbreaker's Burnished Hood
+										i(181125),	-- Soulbreaker's Burnished Leggings
+										i(181126),	-- Soulbreaker's Burnished Mantle
+										i(181127),	-- Soulbreaker's Burnished Sash
+										i(181122),	-- Soulbreaker's Burnished Slippers
+										i(181121),	-- Soulbreaker's Burnished Vestments
+										i(181128),	-- Soulbreaker's Burnished Wraps
 									}),
-							--[[
-								Obtained from the tier 3 mirror system for Venthyr.
-								I got it from repairing the broken mirror and looting the chest inside at 20, 54 in Revendreth.
-								Although i've repair this mirror around 7 days total so far and only got it today,
-								so seems random from the broken mirror chests. Once you upgrade to tier 3 of the mirror system and
-								unlock the various mirrors for teleporting around the zone, you'll be then taught about broken mirrors.
-								After this quest you'll be able then purchase Handcrafted Mirror Repair Kit.
-								This will allow you to repair the various broken mirrors across Revendreth and enter them.
-								They despawn once entered and you loot the chest within, although currently on beta they sometimes bug and
-								send you to the Sanctuary of the Mad, if this happens I just kept returning to the repaired mirror
-								and entering it until it teleports me to the chest room.
-							--]]
+									q(60164, {	-- Mirror Attunement: Dominance Keep
+										["sourceQuests"] = {
+											60159,	-- Mirror Attunement: Halls of Atonement
+											60147,	-- Mirror Attunement: The Banewood
+										},
+										["provider"] = { "n", 167160 },	-- Laurent
+										["coord"] = { 47.3, 57.5, 1699 },	-- Sinfall Reaches
+									}),
+									q(60165, {	-- Mirror Attunement: Feeders' Thicket
+										["sourceQuests"] = {
+											60159,	-- Mirror Attunement: Halls of Atonement
+											60147,	-- Mirror Attunement: The Banewood
+										},
+										["provider"] = { "n", 167160 },	-- Laurent
+										["coord"] = { 47.3, 57.5, 1699 },	-- Sinfall Reaches
+									}),
+									q(59740, {	-- Repair and Restore
+										["sourceQuests"] = {
+											60164,	-- Mirror Attunement: Dominance Keep
+											60165,	-- Mirror Attunement: Feeders' Thicket
+										},
+										["provider"] = { "n", 167160 },	-- Laurent
+										["coord"] = { 47.3, 57.5, 1699 },	-- Sinfall Reaches
+									}),
+								--	not sure if the mirrors have discrete questIDs/locations/objects associated?
+								--	apparently there are 3 up that can be completed each day (costs 20 Infused Ruby each).  the same 3 will be up each week, and then upon reset will cycle, but unpredictably (aka the same 3 can be up the next week)
+								--	mirror should always port you to treasure.  if it takes you to Sanctuary of the Mad, go back to the same mirror, re-enter, and you should get ported to the right place
+								--[[ JAN 12 - JAN 18
+									1.	Broken Mirror: 39.1, 52.2, 1525
+										--	fixing mirror triggered 58013 / 61819
+										--	ported to Sanctuary of the Mad, re-ported and got to a room with Living Bulwark (npcID 173392) and Forgotten Chest (30.9, 47.0, 1525)
+										--	looting chest triggered 61836
+									2.	Broken Mirror: 58.8, 67.8, 1525
+										--	fixing mirror triggered 61823
+										--	ported to room with 2 Ancient Tomes (npcID 173387) and Forgotten Chest (31.8, 47.0, 1525)
+										--	looting chest triggered 61837
+									3. Broken Mirror: 70.9, 43.6, 1525
+										--	fixing mirror triggered 61827
+										--	ported to room with a bunch of Decaying Skeletons (npcID 173390) and Forgotten Chest (31.8, 48.4, 1525)
+										--	looting chest triggered 61838
+								--]]
+								--[[
+									o(357565, {	-- Repaired Transport Mirror
+									--	["description"] = "Requires Tier 3 Mirror Network to see.",
+									--	["npcID"] = 173634,	-- Muckribbon
+									--	["coord"] = { 69.3, 77.9, 1525 },
+									--	["questID"] = 62196,
+										["g"] = {
+								--]]
+											
+								--[[
+										},
+									}),
+									(xxxxxx, {	--
+									--["description"] = "Chance to loot from repairing broken mirrors.",
+									--]]
+									--["g"] = {
+								--]]
+										
+								--	}),
+									--[[
+									Obtained from the tier 3 mirror system for Venthyr.
+									I got it from repairing the broken mirror and looting the chest inside at 20, 54 in Revendreth.
+									Although i've repair this mirror around 7 days total so far and only got it today,
+									so seems random from the broken mirror chests. Once you upgrade to tier 3 of the mirror system and
+									unlock the various mirrors for teleporting around the zone, you'll be then taught about broken mirrors.
+									After this quest you'll be able then purchase Handcrafted Mirror Repair Kit.
+									This will allow you to repair the various broken mirrors across Revendreth and enter them.
+									They despawn once entered and you loot the chest within, although currently on beta they sometimes bug and
+									send you to the Sanctuary of the Mad, if this happens I just kept returning to the repaired mirror
+									and entering it until it teleports me to the chest room.
+									--]]
+								},
 							}),
 						}),
 					}),
@@ -1989,7 +2076,7 @@ _.Zones =
 									["cost"] = { { "c", 1885, 35 } },		-- 35x Grateful Offering
 								}),
 								i(183249, {	-- Memory of a Vital Sacrifice
-								--	appears on the vendor at Renown 14, not sure if this is the same or different for other source(s)
+								--	appears on the vendor at Renown 14, not sure if this is the same or different for other covenants
 									["cost"] = { { "c", 1813, 250 } },	-- 250x Reservoir Anima
 								}),
 						--[[	TODO: this mount doesn't show up on the vendor for me at Renown 13.  does it show up when you reach the Renown required (39)?  
