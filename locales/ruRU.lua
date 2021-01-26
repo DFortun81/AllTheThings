9430dee1f1eea012b3ef9ae2a294cc3bf8f9d554
henbe <holypush@yandex.ru>
2021-01-26 12:09:05 +0500

    Update ruRU.lua

--- locales/ruRU.lua
+++ locales/ruRU.lua
@@ -15,6 +15,7 @@ local L = app.L;
 	
 		-- Big new chunk from AllTheThings.lua
 	
+	L.COORDINATES_STRING = "Координаты";
 	L.FLIGHT_PATHS = "Пути Полётов";
 	L.KNOWN_BY = "Изучено ";
 	L.REQUIRES = "Требуется";
