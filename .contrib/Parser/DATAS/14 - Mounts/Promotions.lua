2aecc127501722ece501d43758ddddd21aa3f381
mogwai316 <bryan.harden@gmail.com>
2021-02-04 09:42:57 -0500

    Added Lucky Yun, Snowstorm, Moon-Touched Netherwhelp. Fixed some things that were not in alphabetic order.

--- .contrib/Parser/DATAS/14 - Mounts/Promotions.lua
+++ .contrib/Parser/DATAS/14 - Mounts/Promotions.lua
@@ -10,6 +10,8 @@ _.Mounts =
 		un(REMOVED_FROM_GAME, i(109013)),	-- Dread Raven
 		un(REMOVED_FROM_GAME, i(106246)),	-- Emerald Hippogryph
 		un(BLIZZARD_BALANCE, mount(307932)),	-- Ensorcelled Everwyrm
+		un(REMOVED_FROM_GAME, i(173297)),	-- Explorer's Dunetrekker
+		un(REMOVED_FROM_GAME, i(173299)),	-- Explorer's Jungle Hopper
 		un(REMOVED_FROM_GAME, i(153540)),	-- Gilded Ravasaur
 		i(98618),			-- Hearthsteed
 		un(REMOVED_FROM_GAME, i(83086)), 	-- Heart of the Nightwing
@@ -20,13 +22,12 @@ _.Mounts =
 		un(REMOVED_FROM_GAME, i(151617)),	-- Orgrimmar Interceptor Mount
 		un(REMOVED_FROM_GAME, i(143631)),	-- Primal Flamesaber
 		un(REMOVED_FROM_GAME, i(153539)),	-- Seabraid Stallion
+		un(BLIZZARD_BALANCE, mount(341821)),	-- Snowstorm
 		un(REMOVED_FROM_GAME, i(76889)),	-- Spectral Gryphon
 		un(REMOVED_FROM_GAME, i(76902)),	-- Spectral Wind Rider
 		un(REMOVED_FROM_GAME, i(151618)),	-- Stormwind Skychaser Mount
 		un(REMOVED_FROM_GAME, i(37719)),	-- Swift Zhevra
 		un(REMOVED_FROM_GAME, i(76755)),	-- Tyrael's Charger
 		un(REMOVED_FROM_GAME, i(54860)),	-- X-53 Touring Rocket
-		un(REMOVED_FROM_GAME, i(173297)),	-- Explorer's Dunetrekker
-		un(REMOVED_FROM_GAME, i(173299)),	-- Explorer's Jungle Hopper
 	}),
 };
