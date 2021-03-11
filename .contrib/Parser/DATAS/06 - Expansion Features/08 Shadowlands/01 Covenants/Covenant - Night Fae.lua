fc44255ccc720417232cb5452a84a70b1cc58682
Gromwind <arallen@gmail.com>
2021-03-11 07:30:12 +0000

    Violet Dredwing Pup added to Queen's Conservatory Cache

--- .contrib/Parser/DATAS/06 - Expansion Features/08 Shadowlands/01 Covenants/Covenant - Night Fae.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/08 Shadowlands/01 Covenants/Covenant - Night Fae.lua
@@ -1138,6 +1138,7 @@ _.ExpansionFeatures =
 								i(181302),	-- Spirit Tender's Branches
 								i(181306),	-- Spirit Tender's Bulb
 								i(181310),	-- Spirit Tender's Pack
+								i(180603),	-- Violet Dredwing Pup
 							}),
 							n(QUESTS, {
 								q(62447, {	-- A Rare and Unusual Spirit (1)
