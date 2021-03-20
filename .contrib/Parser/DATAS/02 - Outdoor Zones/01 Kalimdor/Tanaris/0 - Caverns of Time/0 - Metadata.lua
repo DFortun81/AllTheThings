ec8fd6cda6f8806cb8d8c9052dee0476c626a81c
Gold <goldenshacal@gmail.com>
2021-03-20 10:37:37 +0100

    icon for caverns

--- .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Tanaris/0 - Caverns of Time/0 - Metadata.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Tanaris/0 - Caverns of Time/0 - Metadata.lua
@@ -7,7 +7,8 @@ _.Zones =
 	m(12, {	-- Kalimdor
 		m(71, {	-- Tanaris
 			m(75, {	-- Caverns of Time 
-				["description"] = "|cff66ccffLocated in the eastern part of Tanaris, the Caverns of Time (or simply the Caverns) contain various portals to different key historical periods and events in Warcraft history.|r",
+				["description"] = "|cff66ccffLocated in the eastern part of Tanaris, the Caverns of Time contain various portals to different key historical periods and events in Warcraft history.|r",
+				["icon"] = 2026009,
 				["maps"] = {
 					74,	-- 	Caverns of Time Entrance
 				},
