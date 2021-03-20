5cdae137b45b006ed92b032eaeb4b314cb9f1189
Gold <goldenshacal@gmail.com>
2021-03-20 10:41:41 +0100

    ammen vale is now a subzone of azuremyst isle, similar to the other starting areas

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Azuremyst Isle/0 - Ammen Vale/0 - Metadata.lua
@@ -0,0 +1,14 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(12, {	-- Kalimdor
+		m(97, {	-- Azuremyst Isle
+			m(468, {	-- Ammen Vale
+				["description"] = "|cff66ccffAmmen Vale is a small island and subzone off the coast of Azuremyst Isle. It is a lightly forested valley with escape pods, crystals, and smoking bits of the crashed planar ship scattered about the landscape. |r",
+				["icon"] = "Interface\\Icons\\Achievement_Character_Draenei_Male",
+			}),
+		}),
+	}),
+};
