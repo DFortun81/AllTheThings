a620563946a65f23137cc5db1572a62e07d839d0
mogwai316 <bryan.harden@gmail.com>
2021-03-26 15:46:48 -0400

    Fixed all other Legion herbs to not show things that have already been completed as incomplete

--- .contrib/Parser/DATAS/11 - Professions/Herbalism/Herbalism.lua
+++ .contrib/Parser/DATAS/11 - Professions/Herbalism/Herbalism.lua
@@ -40,103 +40,94 @@ profession(HERBALISM, {
 			}),
 		}),
 		i(151565, {	-- Astral Glory
-			{
-				["itemID"] = 151856,	-- Withered Astral Glory
-				["questID"] = 48027,	-- The Glory of Argus
+			q(48027, {	-- The Glory of Argus
 				["requireSkill"] = HERBALISM,
-				["groups"] = {
-					recipe(247812),	-- Astral Glory [Rank 1]
+				["provider"] = { "i", 151856 },	-- Withered Astral Glory
+				["g"] = {
+					recipe(247812),	-- Astral Glory (Rank 1)
 				},
-			},
-			{
-				["itemID"] = 151857,	-- Adolescent Astral Glory
-				["questID"] = 48028,	-- Youthful Resistance
+			}),
+			q(48028, {	-- Youthful Resistance
 				["requireSkill"] = HERBALISM,
+				["provider"] = { "i", 151857 },	-- Adolescent Astral Glory
 				["sourceQuest"] = 48027,	-- The Glory of Argus
-				["groups"] = {
-					recipe(247813),	-- Astral Glory [Rank 2]
+				["g"] = {
+					recipe(247813),	-- Astral Glory (Rank 2)
 				},
-			},
-			{
-				["itemID"] = 151858,	-- Astral Glory Root Cluster
-				["questID"] = 48029,	-- The Heart of It
+			}),
+			q(48029, {	-- The Heart of It
 				["requireSkill"] = HERBALISM,
+				["provider"] = { "i", 151858 },	-- Astral Glory Root Cluster
 				["sourceQuest"] = 48028,	-- Youthful Resistance
-				["groups"] = {
-					recipe(247814),	-- Astral Glory [Rank 3]
+				["g"] = {
+					recipe(247814),	-- Astral Glory (Rank 3)
 				},
-			},
+			}),
 		}),
 		i(124102, {	-- Dreamleaf
-			{
-				["itemID"] = 129118,	-- Dreamleaf Sample
-				["questID"] = 40018,	-- Dreamleaf Sample
-				["groups"] = {
-					{
-						["questID"] = 40019,	-- An Empathetic Herb
-						["sourceQuest"] = 40018,	-- Dreamleaf Sample
-						["provider"] = { "n", 92464 },		-- Kuhuine Tenderstride <Herbalism Trainer>
-						["groups"] = {
-							recipe(193295),	-- Dreamleaf [Rank 1]
-						},
-					},
+			q(40018, {	-- Dreamleaf Sample
+				["requireSkill"] = HERBALISM,
+				["provider"] = { "i", 129118 },	-- Dreamleaf Sample
+			}),
+			q(40019, {	-- An Empathetic Herb
+				["requireSkill"] = HERBALISM,
+				["sourceQuest"] = 40018,	-- Dreamleaf Sample
+				["provider"] = { "n", 92464 },	-- Kuhuine Tenderstride <Herbalism Trainer>
+				["g"] = {
+					recipe(193295),	-- Dreamleaf [Rank 1]
 				},
-			},
-			{
-				["itemID"] = 129136,	-- Blight-Twisted Herb
-				["questID"] = 40020,	-- Twisted to Death
+			}),
+			q(40020, {	-- Twisted to Death
+				["requireSkill"] = HERBALISM,
 				["sourceQuest"] = 40019,	-- An Empathetic Herb
-				["groups"] = {
-					{
-						["questID"] = 40021,	-- One Dead Plant is One Too Many
-						["sourceQuest"] = 40020,	-- Twisted to Death
-						["provider"] = { "n", 98135 },		-- Wildcrafter Osme <Herbalism Trainer>
-						["groups"] = {
-							recipe(193296),	-- Dreamleaf [Rank 2]
-						},
-					},
+				["provider"] = { "i", 129136 },	-- Blight-Twisted Herb
+			}),
+			q(40021, {	-- One Dead Plant is One Too Many
+				["requireSkill"] = HERBALISM,
+				["sourceQuest"] = 40020,	-- Twisted to Death
+				["provider"] = { "n", 98135 },	-- Wildcrafter Osme <Herbalism Trainer>
+				["g"] = {
+					recipe(193296),	-- Dreamleaf [Rank 2]
 				},
-			},
-			{
-				["itemID"] = 129141,	-- Blight-Choked Herb
-				["questID"] = 40022,	-- Choked by Nightmare
+			}),
+			q(40022, {	-- Choked by Nightmare
+				["requireSkill"] = HERBALISM,
 				["sourceQuest"] = 40021,	-- One Dead Plant is One Too Many
-				["groups"] = {
-					{
-						["questID"] = 40023,	-- The Last Straw
-						["sourceQuest"] = 40022,	-- Choked by Nightmare
-						["provider"] = { "n", 98135 },		-- Wildcrafter Osme <Herbalism Trainer>
-						["groups"] = {
-							recipe(193297),	-- Dreamleaf [Rank 3]
-						},
-					},
+				["provider"] = { "i", 129141 },	-- Blight-Choked Herb
+			}),
+			q(40023, {	-- The Last Straw
+				["requireSkill"] = HERBALISM,
+				["sourceQuest"] = 40022,	-- Choked by Nightmare
+				["provider"] = { "n", 98135 },	-- Wildcrafter Osme <Herbalism Trainer>
+				["g"] = {
+					recipe(193297),	-- Dreamleaf [Rank 3]
 				},
-			},
+			}),
 		}),
 		i(124106, {	-- Felwort
-			{
-				["itemID"] = 129122,	-- Felwort Sample
-				["questID"] = 40040,	-- Felwort Sample
-				["groups"] = {
+			q(40040, {	-- Felwort Sample
+				["requireSkill"] = HERBALISM,
+				["provider"] = { "i", 129122 },	-- Felwort Sample
+				["g"] = {
 					recipe(193307),	-- Felwort [Rank 1]
 				},
-			},
-			{
-				["questID"] = 40041,	-- Felwort Analysis
+			}),
+			q(40041, {	-- Felwort Analysis
+				["requireSkill"] = HERBALISM,
 				["sourceQuest"] = 40040,	-- Felwort Sample
 				["provider"] = { "n", 92464 },		-- Kuhuine Tenderstride <Herbalism Trainer>
-				["groups"] = {
+				["g"] = {
 					recipe(193308),	-- Felwort [Rank 2]
 				},
-			},
-			{
-				["questID"] = 40042,	-- The Emerald Nightmare: Felwort Mastery
+			}),
+			q(40042, {	-- The Emerald Nightmare: Felwort Mastery
+				["requireSkill"] = HERBALISM,
 				["sourceQuest"] = 40041,	-- Felwort Analysis
 				["provider"] = { "n", 92464 },		-- Kuhuine Tenderstride <Herbalism Trainer>
-				["groups"] = {
+				["g"] = {
 					recipe(193309),	-- Felwort [Rank 3]
 				},
-			},
+			}),
 		}),
 		i(124104, {	-- Fjarnskaggl
 			q(40029, {	-- Fjarnskaggl Sample
@@ -174,82 +165,74 @@ profession(HERBALISM, {
 			}),
 		}),
 		i(124103, {	-- Foxflower
-			{
-				["itemID"] = 129119,	-- Foxflower Sample
-				["questID"] = 40024,	-- Foxflower Sample
-				["groups"] = {
+			q(40024, {	-- Foxflower Sample
+				["requireSkill"] = HERBALISM,
+				["provider"] = { "i", 129119 },	-- Foxflower Sample
+				["g"] = {
 					recipe(193298),	-- Foxflower [Rank 1]
 				},
-			},
-			{
-				["itemID"] = 129137,	-- Nibbled Foxflower Stem
-				["questID"] = 40025,	-- Teeny Bite Marks
+			}),
+			q(40025, {	-- Teeny Bite Marks
+				["requireSkill"] = HERBALISM,
 				["sourceQuest"] = 40024,	-- Foxflower Sample
-				["groups"] = {
-					{
-						["questID"] = 40026,	-- Chase the Culprit
-						["sourceQuest"] = 40025,	-- Teeny Bite Marks
-						["provider"] = { "n", 92464 },		-- Kuhuine Tenderstride <Herbalism Trainer>
-						["groups"] = {
-							recipe(193299),	-- Foxflower [Rank 2]
-						},
-					},
+				["provider"] = { "i", 129137 },	-- Nibbled Foxflower Stem
+			}),
+			q(40026, {	-- Chase the Culprit
+				["requireSkill"] = HERBALISM,
+				["sourceQuest"] = 40025,	-- Teeny Bite Marks
+				["provider"] = { "n", 92464 },		-- Kuhuine Tenderstride <Herbalism Trainer>
+				["g"] = {
+					recipe(193299),	-- Foxflower [Rank 2]
 				},
-			},
-			{
-				["itemID"] = 129278,	-- Foxflower Scent Gland
-				["questID"] = 40028,	-- The Pied Picker
+			}),
+			q(40028, {	-- The Pied Picker
+				["requireSkill"] = HERBALISM,
 				["sourceQuest"] = 40026,	-- Chase the Culprit
-				["groups"] = {
+				["provider"] = { "i", 129278 },		-- Foxflower Scent Gland
+				["g"] = {
 					recipe(193300),	-- Foxflower [Rank 3]
 				},
-			},
+			}),
 		}),
 		i(124105, {	-- Starlight Rose
-			{
-				["itemID"] = 129121,	-- Starlight Rosedust
-				["questID"] = 40034,	-- Starlight Rosedust
-				["groups"] = {
-					{
-						["questID"] = 40035,	-- The Gentlest Touch
-						["sourceQuest"] = 40034,	-- Starlight Rosedust
-						["provider"] = { "n", 92464 },		-- Kuhuine Tenderstride <Herbalism Trainer>
-						["groups"] = {
-							recipe(193304),	-- Starlight Rose [Rank 1]
-						},
-					},
+			q(40034, {	-- Starlight Rosedust
+				["requireSkill"] = HERBALISM,
+				["provider"] = { "i", 129121 },	-- Starlight Rosedust
+			}),
+			q(40035, {	-- The Gentlest Touch
+				["requireSkill"] = HERBALISM,
+				["sourceQuest"] = 40034,	-- Starlight Rosedust
+				["provider"] = { "n", 92464 },	-- Kuhuine Tenderstride <Herbalism Trainer>
+				["g"] = {
+					recipe(193304),	-- Starlight Rose [Rank 1]
 				},
-			},
-			{
-				["itemID"] = 129140,	-- Jeweled Spade Handle
-				["questID"] = 40036,	-- Jeweled Spade Handle
+			}),
+			q(40036, {	-- Jeweled Spade Handle
+				["requireSkill"] = HERBALISM,
 				["sourceQuest"] = 40035,	-- The Gentlest Touch
-				["groups"] = {
-					{
-						["questID"] = 40037,	-- The Spade's Blade
-						["sourceQuest"] = 40036,	-- Jeweled Spade Handle
-						["provider"] = { "n", 92464 },		-- Kuhuine Tenderstride <Herbalism Trainer>
-						["groups"] = {
-							recipe(193305),	-- Starlight Rose [Rank 2]
-						},
-					},
+				["provider"] = { "i", 129140 },	-- Jeweled Spade Handle
+			}),
+			q(40037, {	-- The Spade's Blade
+				["requireSkill"] = HERBALISM,
+				["sourceQuest"] = 40036,	-- Jeweled Spade Handle
+				["provider"] = { "n", 92464 },	-- Kuhuine Tenderstride <Herbalism Trainer>
+				["g"] = {
+					recipe(193305),	-- Starlight Rose [Rank 2]
 				},
-			},
-			{
-				["itemID"] = 129143,	-- Scribbled Ramblings
-				["questID"] = 40038,	-- Insane Ramblings
+			}),
+			q(40038, {	-- Insane Ramblings
+				["requireSkill"] = HERBALISM,
 				["sourceQuest"] = 40037,	-- The Spade's Blade
-				["groups"] = {
-					{
-						["questID"] = 40039,	-- Tharillon's Fall
-						["sourceQuest"] = 40038,	-- Insane Ramblings
-						["provider"] = { "n", 92464 },		-- Kuhuine Tenderstride <Herbalism Trainer>
-						["groups"] = {
-							recipe(193306),	-- Starlight Rose [Rank 3]
-						},
-					},
+				["provider"] = { "i", 129143 },	-- Scribbled Ramblings
+			}),
+			q(40039, {	-- Tharillon's Fall
+				["requireSkill"] = HERBALISM,
+				["sourceQuest"] = 40038,	-- Insane Ramblings
+				["provider"] = { "n", 92464 },	-- Kuhuine Tenderstride <Herbalism Trainer>
+				["g"] = {
+					recipe(193306),	-- Starlight Rose [Rank 3]
 				},
-			},
+			}),
 		}),
 	}),
 	filter(101, { 	-- Pet Battle
