29124eba0a6a41adfb77bdb6db478141a4e677d4
slumber <slumber@love.com>
2021-04-16 13:35:17 -0500

    structure/bones for sanctum of domination

---
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/9 - Shadowlands/1 - Raids/3 - Sanctum of Domination.lua
@@ -0,0 +1,226 @@
+-----------------------------------------------------
+--   D U N G E O N S  &  R A I D S  M O D U L E    --
+-----------------------------------------------------
+
+_.Instances = { tier(9, {	-- Shadowlands
+	inst(1193, {	-- Sanctum of Domination
+	--	TODO: add coords + maps
+		["isRaid"] = true,
+	--	["coord"] = {  },
+		["order"] = "02",
+	--	["maps"] = {
+	--	},
+		["g"] = {
+		--[[ loot is still mostly placeholder, so not filling it out yet, just getting structure in place
+			d(17, {	-- LFR
+				e(2435, {	-- The Tarragrue
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2442, {	-- The Eye of the Jailer
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2439, {	-- The Nine
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2444, {	-- Remnant of Ner'zhul
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2445, {	-- Soulrender Dormazain
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2443, {	-- Painsmith Raznal
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2446, {	-- Guardian of the First Ones
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2447, {	-- Fatescribe Roh-Kalo
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2440, {	-- Kel'Thuzad
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2441, {	-- Sylvanas Windrunner
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+			}),
+			d(14, {	-- Normal
+				e(2435, {	-- The Tarragrue
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2442, {	-- The Eye of the Jailer
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2439, {	-- The Nine
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2444, {	-- Remnant of Ner'zhul
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2445, {	-- Soulrender Dormazain
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2443, {	-- Painsmith Raznal
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2446, {	-- Guardian of the First Ones
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2447, {	-- Fatescribe Roh-Kalo
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2440, {	-- Kel'Thuzad
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2441, {	-- Sylvanas Windrunner
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+			}),
+			d(15, {	-- Heroic
+				e(2435, {	-- The Tarragrue
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2442, {	-- The Eye of the Jailer
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2439, {	-- The Nine
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2444, {	-- Remnant of Ner'zhul
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2445, {	-- Soulrender Dormazain
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2443, {	-- Painsmith Raznal
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2446, {	-- Guardian of the First Ones
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2447, {	-- Fatescribe Roh-Kalo
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2440, {	-- Kel'Thuzad
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2441, {	-- Sylvanas Windrunner
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+			}),
+			d(16, {	-- Mythic
+				e(2435, {	-- The Tarragrue
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2442, {	-- The Eye of the Jailer
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2439, {	-- The Nine
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2444, {	-- Remnant of Ner'zhul
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2445, {	-- Soulrender Dormazain
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2443, {	-- Painsmith Raznal
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2446, {	-- Guardian of the First Ones
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2447, {	-- Fatescribe Roh-Kalo
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2440, {	-- Kel'Thuzad
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+				e(2441, {	-- Sylvanas Windrunner
+				--	["crs"] = {  },	-- 
+					["g"] = {
+					},
+				}),
+			}),
+		--]]
+		},
+	}),
+})};
