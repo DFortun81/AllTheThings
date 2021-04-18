147cb69998dd00046dad50e789318f3b502413a4
Gold <goldenshacal@gmail.com>
2021-04-18 18:06:37 +0200

    cata jewelcrafting items now have class requirements

--- .contrib/Parser/DATAS/11 - Professions/Jewelcrafting/Jewelcrafting.lua
+++ .contrib/Parser/DATAS/11 - Professions/Jewelcrafting/Jewelcrafting.lua
@@ -1,32 +1,36 @@
 profession(JEWELCRAFTING, {
 	n(QUESTS, {
-		-- These may be class-restricted where the quest starter item only drops for certain class/specs but we need more data to know for sure.
 		q(28778,  {	-- Broken Boar Figurine
 			["provider"] = { "i", 65895 },	-- Figurine - King of Boars
+			["classes"] = { DEATHKNIGHT, PALADIN, WARRIOR },
 			["g"] = {
 				i(52351),	-- Figurine - King of Boars
 			},
 		}),
 		q(28776,  {	-- Broken Earthen Figurine
 			["provider"] = { "i", 65897 },	-- Figurine - Earthen Guardian
+			["classes"] = { DEATHKNIGHT, PALADIN, WARRIOR, DRUID, DEMONHUNTER, MONK },
 			["g"] = {
 				i(52352),	-- Figurine - Earthen Guardian
 			},
 		}),
 		q(28777,  {	-- Broken Owl Figurine
 			["provider"] = { "i", 65894 },	-- Figurine - Dream Owl
+			["classes"] = { PALADIN, SHAMAN, DRUID, MONK, PRIEST },
 			["g"] = {
 				i(52354),	-- Figurine - Dream Owl
 			},
 		}),
 		q(25047,  {	-- Broken Panther Figurine
 			["provider"] = { "i", 52197 },	-- Figurine - Demon Panther
+			["classes"] = { HUNTER, SHAMAN, MONK, DEMONHUNTER, DRUID, ROGUE },
 			["g"] = {
 				i(52199),	-- Figurine - Demon Panther
 			},
 		}),
 		q(28775,  {	-- Broken Serpent Figurine
 			["provider"] = { "i", 65896 },	-- Figurine - Jeweled Serpent
+			["classes"] = { SHAMAN, DRUID, MONK, PRIEST, MAGE, WARLOCK },
 			["g"] = {
 				i(52353),	-- Figurine - Jeweled Serpent
 			},
