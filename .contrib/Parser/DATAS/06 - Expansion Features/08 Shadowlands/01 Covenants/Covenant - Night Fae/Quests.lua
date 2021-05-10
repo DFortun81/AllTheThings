7168f1489fcaf78f219fb5110fe8614aa558442f
slumber <slumber@love.com>
2021-05-09 23:20:21 -0500

    added sq for a night fae quest

--- .contrib/Parser/DATAS/06 - Expansion Features/08 Shadowlands/01 Covenants/Covenant - Night Fae/Quests.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/08 Shadowlands/01 Covenants/Covenant - Night Fae/Quests.lua
@@ -55,9 +55,7 @@ _.ExpansionFeatures =
 						["coord"] = { 34.0, 37.0, 1701 },	-- The Trunk
 					}),
 					q(62546, {	-- No Wisp Left Behind (1)
-						-- TODO: figure out sourceQuests. This quest just randomly showed up for me after daily reset on a Saturday (similar to 62536 a couple weeks earlier).
-						-- possibly a Venari requirement? Currently Apprehensive + Rule 4 completed
-						["sourceQuests"] = { 62536 },	-- The Lost of Teldrassil
+						["sourceQuests"] = { 62544 },	-- From a Dark Place
 						["provider"] = { "n", 160262 },	-- Ysera
 						["coord"] = { 44.6, 39.0, 1701 },
 					}),
