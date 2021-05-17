66dd88a9aa5b0585f6b8c97a7bb1fcdf5a6e77e6
Crieve <dylanfortune@live.com>
2021-05-16 22:48:12 -0700

    Added Exploration for Eversong Woods. (to Classic, Retail is not ready!)

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Ghostlands/Exploration.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Ghostlands/Exploration.lua
@@ -9,7 +9,15 @@ _.Zones =
 		m(GHOSTLANDS, {
 			n(EXPLORATION, {
 				exploration(3493, "256:512:448:150"),	-- Sanctum of the Sun [TBC]
+				exploration(3489, "512:256:460:0"),	-- Suncrown Village [TBC]
 				exploration(3488, "256:512:365:2"),	-- Tranquillien [TBC]
+				--[[
+				exploration(, "512:256:326:0"),	--  [TBC]
+				exploration(, ""),	--  [TBC]
+				exploration(, ""),	--  [TBC]
+				exploration(, ""),	--  [TBC]
+				exploration(, ""),	--  [TBC]
+				]]--
 			}),
 		}),
 	})),
