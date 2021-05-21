be0f58de1280b7bfffc9c1ea9d80d82fb89c6096
slumber <slumber@love.com>
2021-05-21 14:27:05 -0500

    av achievements should now show up in av

--- .contrib/Parser/DATAS/05 - Achievements/04 - Player vs Player/06 - Alterac Valley.lua
+++ .contrib/Parser/DATAS/05 - Achievements/04 - Player vs Player/06 - Alterac Valley.lua
@@ -14,67 +14,70 @@ _.Achievements =
 {
 	n(-9958, {	-- Player vs Player
 		n(-9987, {	-- Alterac Valley
-			ach(13928, {	-- Alterac Valley of Olde (A)
-				["races"] = ALLIANCE_ONLY,
-				["u"] = 31,	-- WoW Anniversary
-				["g"] = {
-					i(172022),	-- Stormpike Battle Ram
-				},
-			}),
-			ach(13930, {	-- Alterac Valley of Olde (H)
-				["races"] = HORDE_ONLY,
-				["u"] = 31,	-- WoW Anniversary
-				["g"] = {
-					i(172023),	-- Frostwolf Snarler
-				},
-			}),
-			ach(708,   {	-- Hero of the Frostwolf Clan
-				["races"] = HORDE_ONLY,
-			}),
-			ach(709,   {	-- Hero of the Stormpike Guard
-				["races"] = ALLIANCE_ONLY,
-			}),
-			ach(1167,  {	-- Master of Alterac Valley
-				ach(219,  {	-- Alterac Valley Veteran
-					ach(218),	-- Alterac Valley Victory
-				}),
-				ach(221),	-- Alterac Grave Robber
-				ach(222),	-- Tower Defense
-				ach(1151, {	-- Loyal Defender (A)
+			["maps"] = { 91 },	-- Alterac Valley
+			["g"] = {
+				ach(13928, {	-- Alterac Valley of Olde (A)
 					["races"] = ALLIANCE_ONLY,
+					["u"] = 31,	-- WoW Anniversary
+					["g"] = {
+						i(172022),	-- Stormpike Battle Ram
+					},
 				}),
-				ach(224,  {	-- Loyal Defender (H)
+				ach(13930, {	-- Alterac Valley of Olde (H)
+					["races"] = HORDE_ONLY,
+					["u"] = 31,	-- WoW Anniversary
+					["g"] = {
+						i(172023),	-- Frostwolf Snarler
+					},
+				}),
+				ach(708,   {	-- Hero of the Frostwolf Clan
 					["races"] = HORDE_ONLY,
 				}),
-				ach(225,  {	-- Everything Counts (A)
+				ach(709,   {	-- Hero of the Stormpike Guard
 					["races"] = ALLIANCE_ONLY,
 				}),
-				ach(1164, {	-- Everything Counts (H)
-					["races"] = HORDE_ONLY,
+				ach(1167,  {	-- Master of Alterac Valley
+					ach(219,  {	-- Alterac Valley Veteran
+						ach(218),	-- Alterac Valley Victory
+					}),
+					ach(221),	-- Alterac Grave Robber
+					ach(222),	-- Tower Defense
+					ach(1151, {	-- Loyal Defender (A)
+						["races"] = ALLIANCE_ONLY,
+					}),
+					ach(224,  {	-- Loyal Defender (H)
+						["races"] = HORDE_ONLY,
+					}),
+					ach(225,  {	-- Everything Counts (A)
+						["races"] = ALLIANCE_ONLY,
+					}),
+					ach(1164, {	-- Everything Counts (H)
+						["races"] = HORDE_ONLY,
+					}),
+					ach(223),	-- The Sickly Gazelle
+					ach(873,  {	-- Frostwolf Perfection
+						["races"] = HORDE_ONLY,
+					}),
+					ach(220,  {	-- Stormpike Perfection
+						["races"] = ALLIANCE_ONLY,
+					}),
+					ach(582,  {	-- Alterac Valley All-Star
+						crit(1),	-- Assault a graveyard
+						crit(2),	-- Defend a graveyard
+						crit(3),	-- Assault a tower
+						crit(4),	-- Defend a tower
+						crit(5),	-- Kill someone in the Field of Strife
+					}),
+					ach(706,  {	-- Frostwolf Howler
+						["races"] = HORDE_ONLY,
+					}),
+					ach(707,  {	-- Stormpike Battle Charger
+						["races"] = ALLIANCE_ONLY,
+					}),
+					ach(1166),	-- To the Looter Go the Spoils
 				}),
-				ach(223),	-- The Sickly Gazelle
-				ach(873,  {	-- Frostwolf Perfection
-					["races"] = HORDE_ONLY,
-				}),
-				ach(220,  {	-- Stormpike Perfection
-					["races"] = ALLIANCE_ONLY,
-				}),
-				ach(582,  {	-- Alterac Valley All-Star
-					crit(1),	-- Assault a graveyard
-					crit(2),	-- Defend a graveyard
-					crit(3),	-- Assault a tower
-					crit(4),	-- Defend a tower
-					crit(5),	-- Kill someone in the Field of Strife
-				}),
-				ach(706,  {	-- Frostwolf Howler
-					["races"] = HORDE_ONLY,
-				}),
-				ach(707,  {	-- Stormpike Battle Charger
-					["races"] = ALLIANCE_ONLY,
-				}),
-				ach(1166),	-- To the Looter Go the Spoils
-			}),
-			ach(226),		-- The Alterac Blitz
+				ach(226),		-- The Alterac Blitz
+			},
 		}),
 	}),
 };
