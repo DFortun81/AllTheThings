969e34f108cd016f92dda320494a2240c029b8b1
Molkree <mr.molkree@gmail.com>
2021-05-24 01:37:56 +0300

    Drop esMX/zhTW from localization scrips and adjust for new ObjectDB logic

--- .contrib/.tools/Localization/localize_all_objects.py
+++ .contrib/.tools/Localization/localize_all_objects.py
@@ -2,13 +2,11 @@
 This will attempt to localize all objects marked with TODO in every locale.
 """
 
-from object_localization import LOCALES_DIR, copy_esES_objects_to_esMX, localize_objects
+from object_localization import LOCALES_DIR, localize_objects
 
 localize_objects(f"{LOCALES_DIR}deDE.lua", "de")
 
 localize_objects(f"{LOCALES_DIR}esES.lua", "es")
-
-copy_esES_objects_to_esMX()
 
 localize_objects(f"{LOCALES_DIR}frFR.lua", "fr")
 
