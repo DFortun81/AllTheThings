fdee78f8a1208b55d2a8cb7088a4c0e7eb16c7e3
ImUnicke <imunicke+github@gmail.com>
2021-05-29 16:50:43 -0600

    Added/re-organized some Castle Nathria achievements/criteria to both the achievements category and the dungeons & raids category

--- .contrib/Parser/DATAS/05 - Achievements/05 - Dungeons and Raids/17 - Shadowlands Raid.lua
+++ .contrib/Parser/DATAS/05 - Achievements/05 - Dungeons and Raids/17 - Shadowlands Raid.lua
@@ -8,33 +8,28 @@ _.Achievements =
 		n(-10102, {	-- Shadowlands Raid
 			ach(14293),	-- Blind as a Bat
 			ach(14608),	-- Burning Bright
-			ach(14715, {	-- Castle Nathria
-				crit(1),	-- Shriekwing
-				crit(2),	-- Huntsman Altimor
-				crit(3),	-- Sun King's Salvation
-				crit(4),	-- Artificer Xy'mox
-				crit(5),	-- Hungering Destroyer
-				crit(6),	-- Lady Inverva Darkvein
-				crit(7),	-- The Council of Blood
-				crit(8),	-- Sludgefist
-				crit(9),	-- Stone Legion Generals
-				crit(10),	-- Sire Denathrius
+			ach(14718, {	-- Mythic: Castle Nathria
+				ach(14717, {	-- Heroic: Castle Nathria
+					ach(14715),	-- Castle Nathria
+				}),
+				ach(14359),	-- Mythic: Artificer Xy'mox
+				ach(14358),	-- Mythic: Hungering Destroyer
+				ach(14357),	-- Mythic: Huntsman Altimor
+				ach(14361),	-- Mythic: Lady Inerva Darkvein
+				ach(14356),	-- Mythic: Shriekwing
+				ach(14365, {	-- Mythic: Sire Denathrius
+					title(432),	-- Sinbreaker <Name>
+				}),
+				ach(14363),	-- Mythic: Sludgefist
+				ach(14364),	-- Mythic: Stone Legion Generals
+				ach(14360),	-- Mythic: Sun King's Salvation
+				ach(14362),	-- Mythic: The Council of Blood
 			}),
 			ach(14610),	-- Clear Conscience
 			ach(14294),	-- Dirtflap's Revenge
 			ach(14525),	-- Feed Me, Seymour!
 			ach(14376),	-- Feed the Beast
 			ach(14524),	-- I Don't Know What I Expected
-			ach(14358),	-- Mythic: Hungering Destroyer
-			ach(14357),	-- Mythic: Huntsman Altimor
-			ach(14361),	-- Mythic: Lady Inerva Darkvein
-			ach(14365, {	-- Mythic: Sire Denathrius
-				title(432),	-- Sinbreaker <Name>
-			}),
-			ach(14363),	-- Mythic: Sludgefist
-			ach(14364),	-- Mythic: Stone Legion Generals
-			ach(14360),	-- Mythic: Sun King's Salvation
-			ach(14362),	-- Mythic: The Council of Blood
 			ach(14619),	-- Pour Decision Making
 			ach(14617),	-- Private Stock
 			ach(14523),	-- Taking Care of Business
