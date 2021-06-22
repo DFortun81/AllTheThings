fa22d6a2ba9e0831c7fc2f2b676a43adb2fd6989
gbrage <gbrage@kth.se>
2021-06-22 23:12:17 +0200

    Timeline Rares

--- .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Durotar/Rares.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Durotar/Rares.lua
@@ -6,21 +6,29 @@ _.Zones =
 	m(KALIMDOR, {
 		m(DUROTAR, {
 			n(RARES, {
-				n(5824, {	-- Captain Flat Tusk -- NO TRANSMOGGABLE LOOT!
+				n(5824, {	-- Captain Flat Tusk <Captain of the Battleguard> -- NO TRANSMOGGABLE LOOT!
 					["coords"] = {
 						{ 38.6, 53.8, DUROTAR },
 						{ 44.8, 50.6, DUROTAR },
 						{ 42.4, 38.6, DUROTAR },
 					},
+					["timeline"] = {
+						"added 1.11.1.7272",
+					},
 				}),
 				n(5823, {	-- Death Flayer
 					["coords"] = {
+						{ 36.8, 49.8, DUROTAR },
 						{ 34.8, 43.8, DUROTAR },
 						{ 35.6, 46.0, DUROTAR },
 						{ 37.8, 47.8, DUROTAR },
 						{ 39.6, 45.6, DUROTAR },
 						{ 38.6, 44.0, DUROTAR },
 						{ 36.6, 44.4, DUROTAR },
+
+					},
+					["timeline"] = {
+						"added 1.11.1.7272",
 					},
 				}),
 				n(5822, {	-- Felweaver Scornn
@@ -28,16 +36,44 @@ _.Zones =
 						{ 51.8, 9.6, DUROTAR },
 						{ 53.2, 7.6, DUROTAR },
 					},
+					["timeline"] = {
+						"added 1.11.1.7272",
+					},
 				}),
 				n(5826, {	-- Geolord Mottle -- NO TRANSMOGGABLE LOOT!
 					["coords"] = {
-						{ 43.2, 39.6, DUROTAR },
-						{ 43.6, 50.2, DUROTAR },
-						{ 47.0, 49.6, DUROTAR },
+						{ 43.6, 40.8, DUROTAR },
+						{ 49.6, 45.0, DUROTAR },
+						{ 47.6, 50.0, DUROTAR },
+						{ 44.6, 48.4, DUROTAR },
+						{ 43.8, 50.0, DUROTAR },
+					},
+					["timeline"] = {
+						"added 1.11.1.7272",
 					},
 				}),
 				n(5809, {	-- Sergeant Curtis -- NO TRANSMOGGABLE LOOT!
 					["coord"] = { 59.6, 58.8, DUROTAR },
+					["timeline"] = {
+						"added 1.11.1.7272",
+					},
+				}),
+				n(5808, {	-- Warlord Kolkanis
+					["coords"] = {
+						{ 46.6, 79.8, DUROTAR },
+						{ 48.2, 78.2, DUROTAR },
+						{ 49.6, 80.6, DUROTAR },
+					},
+					["timeline"] = {
+						"added 1.11.1.7272",
+						"removed 4.0.3.10000",
+					},
+				}),
+				n(5809, {	-- Watch Commander Zalaphil
+					["coord"] = { 59.6, 59.0, DUROTAR },
+					["timeline"] = {
+						"added 1.11.1.7272",
+					},
 				}),
 			}),
 		}),
