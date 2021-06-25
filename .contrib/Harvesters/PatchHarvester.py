6a3790fc61cbc8293a8fb4f9a231379ee42a5165
Dmitry Volodin <mr.molkree@gmail.com>
2021-06-25 11:41:45 +0300

    Fix timeout

--- .contrib/Harvesters/PatchHarvester.py
+++ .contrib/Harvesters/PatchHarvester.py
@@ -41,15 +41,16 @@ def harvest_things(thing_type, max_id_path, db_path):
                 int(re.search(r"i\((\d+)", str(line)).group(1)) if line != "" else 0
             )
 
-    t_end = time.monotonic() + 60 * 1
-    while time.monotonic() < t_end:
-        with open(db_path, "a") as things:
-            try:
-                for thing_id in range(max_harvested_id + 1, max_id + 1):
-                    patch = get_thing_patch(thing_type, thing_id)
-                    if patch != "":
-                        logging.info(f"{thing_type} {thing_id} added in patch {patch}")
-                        things.write(f"i({thing_id}, {patch})\n")
-            # catching all exceptions just to salvage collected items if we run for hours
-            except Exception:
-                logging.error(traceback.format_exc())
+    time_end = time.monotonic() + 60 * 1
+    with open(db_path, "a") as things:
+        try:
+            for thing_id in range(max_harvested_id + 1, max_id + 1):
+                if time.monotonic() >= time_end:
+                    break
+                patch = get_thing_patch(thing_type, thing_id)
+                if patch != "":
+                    logging.info(f"{thing_type} {thing_id} added in patch {patch}")
+                    things.write(f"i({thing_id}, {patch})\n")
+        # catching all exceptions just to salvage collected items if we run for hours
+        except Exception:
+            logging.error(traceback.format_exc())
