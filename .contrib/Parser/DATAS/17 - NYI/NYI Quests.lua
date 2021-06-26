888181abe651c656459a98b3730850f80518338a
Darkal31 <michalbert@seznam.cz>
2021-06-26 23:30:09 +0200

    Clearing unsorted quests.

--- .contrib/Parser/DATAS/17 - NYI/NYI Quests.lua
+++ .contrib/Parser/DATAS/17 - NYI/NYI Quests.lua
@@ -10,7 +10,170 @@ p = function(...) return un(NEVER_IMPLEMENTED, oldP(...)); end
 _.NeverImplemented =
 {
 	n(QUESTS, {
+		tier(7, {	-- Legion -- These Quests might be ingame, very unlikely however.
+			q(43497),	-- Legion 110 A
+			q(43498),	-- Legion 110 A
+			q(41710),	-- Legion 110 A
+			q(41712),	-- Legion 110 A
+			q(41714),	-- Legion 110 A
+			q(41716),	-- Legion 110 A
+			q(41718),	-- Legion 110 A
+			q(41720),	-- Legion 110 A
+			q(41721),	-- Legion 110 A
+			q(42457),	-- Legion 110 A
+			q(42458),	-- Legion 110 A
+			q(42462),	-- Legion 110 A
+			q(42464),	-- Legion 110 A
+			q(42466),	-- Legion 110 A
+			q(42467),	-- Legion 110 A
+			q(42468),	-- Legion 110 A
+			q(42469),	-- Legion 110 A
+			q(42470),	-- Legion 110 A
+			q(43499),	-- Legion 110 H
+			q(43500),	-- Legion 110 H
+			q(41711),	-- Legion 110 H
+			q(41713),	-- Legion 110 H
+			q(41715),	-- Legion 110 H
+			q(41717),	-- Legion 110 H
+			q(41719),	-- Legion 110 H
+			q(41722),	-- Legion 110 H
+			q(41723),	-- Legion 110 H
+			q(42459),	-- Legion 110 H
+			q(42460),	-- Legion 110 H
+			q(42463),	-- Legion 110 H
+			q(42465),	-- Legion 110 H
+			q(42471),	-- Legion 110 H
+			q(42472),	-- Legion 110 H
+			q(42473),	-- Legion 110 H
+			q(42474),	-- Legion 110 H
+			q(42475),	-- Legion 110 H
+			q(42912),	-- LFGDungeons - Legion - Dungeon Normal - 1st
+			q(42913),	-- LFGDungeons - Legion - Dungeon Normal - Nth
+			q(42897),	-- LFGDungeons - Legion - Heroic Random - 1st
+			q(42899),	-- LFGDungeons - Legion - Heroic Random - Nth
+			q(42901),	-- LFGDungeons - Legion - Normal Random - 1st
+			q(42903),	-- LFGDungeons - Legion - Normal Random - Nth
+			q(42904),	-- LFGDungeons - Legion - Raid Finder - 1st
+			q(42905),	-- LFGDungeons - Legion - Raid Finder - Nth
+			q(42461),	-- Vanilla 10-19
+			q(42554),	-- WOD 100 A		-- WoD quest with Legion ID smh
+			q(42555),	-- WOD 100 A		-- WoD quest with Legion ID smh
+			q(42561),	-- WOD 100 A		-- WoD quest with Legion ID smh
+			q(42562),	-- WOD 100 A		-- WoD quest with Legion ID smh
+			q(42563),	-- WOD 100 A		-- WoD quest with Legion ID smh
+			q(42564),	-- WOD 100 A		-- WoD quest with Legion ID smh
+			q(42565),	-- WOD 100 A		-- WoD quest with Legion ID smh
+			q(42552),	-- WOD 100 H		-- WoD quest with Legion ID smh
+			q(42553),	-- WOD 100 H		-- WoD quest with Legion ID smh
+			q(42556),	-- WOD 100 H		-- WoD quest with Legion ID smh
+			q(42557),	-- WOD 100 H		-- WoD quest with Legion ID smh
+			q(42558),	-- WOD 100 H		-- WoD quest with Legion ID smh
+			q(42559),	-- WOD 100 H		-- WoD quest with Legion ID smh
+			q(42560),	-- WOD 100 H		-- WoD quest with Legion ID smh
+		}),
+		tier(8, {	-- Battle for Azeroth -- These Quests might be ingame, very unlikely however.
+			q(53374),	-- BFA 111-119 E
+			q(53375),	-- BFA 111-119 E
+			q(53376),	-- BFA 111-119 E
+			q(53377),	-- BFA 111-119 E
+			q(53378),	-- BFA 111-119 E
+			q(53388),	-- BFA 111-119 E
+			q(53389),	-- BFA 111-119 E
+			q(53390),	-- BFA 111-119 E
+			q(53559),	-- BFA 111-119 E
+			q(53560),	-- BFA 111-119 E
+			q(53561),	-- BFA 111-119 E
+			q(53562),	-- BFA 111-119 E
+			q(53563),	-- BFA 111-119 E
+			q(53379),	-- BFA 120 E
+			q(53380),	-- BFA 120 E
+			q(53381),	-- BFA 120 E
+			q(53382),	-- BFA 120 E
+			q(53383),	-- BFA 120 E
+			q(53391),	-- BFA 120 E
+			q(53392),	-- BFA 120 E
+			q(53393),	-- BFA 120 E
+			q(53394),	-- BFA 120 E
+			q(53395),	-- BFA 120 E
+			q(53396),	-- BFA 120 E
+			q(53397),	-- BFA 120 E
+			q(53398),	-- BFA 120 E
+			q(53399),	-- BFA 120 E
+			q(53400),	-- BFA 120 E
+			q(53401),	-- BFA 120 E
+			q(53402),	-- BFA 120 E
+			q(53403),	-- BFA 120 E
+			q(53404),	-- BFA 120 E
+			q(53554),	-- BFA 120 E
+			q(53555),	-- BFA 120 E
+			q(53556),	-- BFA 120 E
+			q(53557),	-- BFA 120 E
+			q(53558),	-- BFA 120 E
+			q(53727),	-- Legion 110 H		-- Legion quest with BFA ID smh
+			q(53728),	-- Legion 110 H		-- Legion quest with BFA ID smh
+			q(53729),	-- Legion 110 H		-- Legion quest with BFA ID smh
+			q(53730),	-- Legion 110 H		-- Legion quest with BFA ID smh
+			q(53731),	-- Legion 110 A		-- Legion quest with BFA ID smh
+			q(53732),	-- Legion 110 A		-- Legion quest with BFA ID smh
+			q(53733),	-- Legion 110 A		-- Legion quest with BFA ID smh
+			q(53339),	-- LFG - BFA - Island Expedition - Heroic Win - Nth
+			q(53340),	-- LFG - BFA - Island Expedition - Mythic Win - Nth
+			q(51838),	-- LFG - BFA - Island Expedition - Normal Win - Nth
+			q(53341),	-- LFG - BFA - Island Expedition - PvP Win - Nth
+			q(50627),	-- LFGDungeons - BFA - Heroic Random - 1st
+			q(50628),	-- LFGDungeons - BFA - Heroic Random - Nth
+			q(50626),	-- LFGDungeons - BFA - Normal Random - 1st
+			q(50625),	-- LFGDungeons - BFA - Normal Random - Nth
+			q(52051),	-- LFGDungeons - BFA - Raid Finder - 1st
+			q(52052),	-- LFGDungeons - BFA - Raid Finder - Nth
+			q(53641),	-- LFGDungeons - BFA - Stromgarde (Arathi)
+		}),
 		tier(9, {	-- Shadowlands -- These Quests might be ingame, very unlikely however.
+			q(62300),	-- Classic 20-49
+			q(62301),	-- Classic 50
+			q(62302),	-- Classic 50
+			q(62303),	-- Classic 50
+			q(62351),	-- Classic 50
+			q(62352),	-- Classic 50
+			q(62642),	-- LFGDungeons - SL - Heroic Random - 1st
+			q(62643),	-- LFGDungeons - SL - Heroic Random - Nth
+			q(62644),	-- LFGDungeons - SL - Normal Random - 1st
+			q(62645),	-- LFGDungeons - SL - Normal Random - Nth
+			q(62646),	-- LFGDungeons - SL - Raid Finder - 1st
+			q(62647),	-- LFGDungeons - SL - Raid Finder - Nth
+			q(62346),	-- Shadowlands (51-59) E
+			q(62349),	-- Shadowlands (51-59) E
+			q(62350),	-- Shadowlands (51-59) E
+			q(62354),	-- Shadowlands (51-59) E
+			q(62355),	-- Shadowlands (51-59) E
+			q(62356),	-- Shadowlands (51-59) E
+			q(62358),	-- Shadowlands (51-59) E
+			q(62359),	-- Shadowlands (51-59) E
+			q(62360),	-- Shadowlands (51-59) E
+			q(60023),	-- Shadowlands (60) E
+			q(60024),	-- Shadowlands (60) E
+			q(60025),	-- Shadowlands (60) E
+			q(60031),	-- Shadowlands (60) E
+			q(60032),	-- Shadowlands (60) E
+			q(60033),	-- Shadowlands (60) E
+			q(60034),	-- Shadowlands (60) E
+			q(60035),	-- Shadowlands (60) E
+			q(60036),	-- Shadowlands (60) E
+			q(60037),	-- Shadowlands (60) E
+			q(60038),	-- Shadowlands (60) E
+			q(60039),	-- Shadowlands (60) E
+			q(60040),	-- Shadowlands (60) E
+			q(60014),	-- Shadowlands 60 E
+			q(60015),	-- Shadowlands 60 E
+			q(60016),	-- Shadowlands 60 E
+			q(60017),	-- Shadowlands 60 E
+			q(60018),	-- Shadowlands 60 E
+			q(60019),	-- Shadowlands 60 E
+			q(60026),	-- Shadowlands 60 E
+			q(60027),	-- Shadowlands 60 E
+			q(60028),	-- Shadowlands 60 E
+			q(60029),	-- Shadowlands 60 E
+			q(60030),	-- Shadowlands 60 E
 		}),
 		q(3064,  {	-- <NYI> <TXT> Pirate Hats
 			["u"] = NEVER_IMPLEMENTED,
