f1c1a1990391bbad21747ea10a90db01ee793a1b
Molkree <mr.molkree@gmail.com>
2021-07-05 00:32:14 +0300

    Move Pallid Bone Flute recipe to the SL section

--- .contrib/Parser/DATAS/11 - Professions/Leatherworking/Recipes/Shadowlands Patterns - Filtered.lua
+++ .contrib/Parser/DATAS/11 - Professions/Leatherworking/Recipes/Shadowlands Patterns - Filtered.lua
@@ -1,70 +1,24 @@
 profession(LEATHERWORKING, {
 	filter(200, {	-- Recipes
-		{
-			["categoryID"] = 1334,	-- Shadowlands Patterns
-			["g"] = {
-				{
-					-- All Quest Recipes marked unobtainable(training) because they are only learned temporarily
-					-- for world quests then disappear from the spellbook after the quest is completed.
-					["categoryID"] = 1526,	-- Quest Recipes
-					["g"] = {
-						{
-							["recipeID"] = 338260,	-- Bonestudded Fist
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338262,	-- Cleaned Hide
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338263,	-- Courtly Leather Boots
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338253,	-- Dyed Runestag Leather
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338254,	-- Runestag Leather Strap
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338265,	-- Softened Leather
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338255,	-- Stalker's Leather Quiver
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338258,	-- Steelhide Leather Belt
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338257,	-- Steelhide Leather Harness
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338259,	-- Steelhide Leather Strap
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-						{
-							["recipeID"] = 338264,	-- Tortured Sole
-							["u"] = 15,	-- Training Recipes / Unlearnable
-						},
-					},
-				},
-
-			},
-		},
-		{
-			["categoryID"] = 1335,	-- Other
-			["g"] = {
-				{
-					["recipeID"] = 354800,	-- Pallid Bone Flute
-					["name"] = "Pallid Bone Flute",
-				},
-			},
-		},
+		category(1334, {	-- Shadowlands Patterns
+			category(1526, bubbleDown({ ["u"] = 15 }, {	-- Quest Recipes
+				-- All Quest Recipes marked unobtainable (training) because they are only learned temporarily
+				-- for world quests then disappear from the spellbook after the quest is completed.
+				recipe(338260),	-- Bonestudded Fist
+				recipe(338262),	-- Cleaned Hide
+				recipe(338263),	-- Courtly Leather Boots
+				recipe(338253),	-- Dyed Runestag Leather
+				recipe(338254),	-- Runestag Leather Strap
+				recipe(338265),	-- Softened Leather
+				recipe(338255),	-- Stalker's Leather Quiver
+				recipe(338258),	-- Steelhide Leather Belt
+				recipe(338257),	-- Steelhide Leather Harness
+				recipe(338259),	-- Steelhide Leather Strap
+				recipe(338264),	-- Tortured Sole
+			})),
+			category(1335, {	-- Other
+				recipe(354800),	-- Pallid Bone Flute
+			}),
+		}),
 	}),
 });
