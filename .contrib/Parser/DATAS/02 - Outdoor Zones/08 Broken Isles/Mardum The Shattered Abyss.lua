e85bc532ae0f33916c0b646326f8ee748fd93884
ImUnicke <imunicke+github@gmail.com>
2021-07-06 13:35:21 -0600

    Re-fixed the 'way off' coord based on zone map

--- .contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Mardum The Shattered Abyss.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Mardum The Shattered Abyss.lua
@@ -170,7 +170,7 @@ _.Zones =
 				n(RARES, {
 					n(97058,  { -- Count Nefarious
 						["questID"] = 40231,	-- Nefarious Ring
-						["coord"] = { 51.6, 56.2, 672 },
+						["coord"] = { 63.7, 23.2, 672 },
 						["g"] = {
 							i(128948),	-- Nefarious Ring
 						},
