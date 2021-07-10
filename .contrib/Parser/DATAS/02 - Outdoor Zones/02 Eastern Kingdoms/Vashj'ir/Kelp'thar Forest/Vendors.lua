8e36b5796eb2715e986ffe20243c7362f79aaca3
gbrage <gbrage@kth.se>
2021-07-10 13:13:57 +0200

    Scrolls moved to Hyjal, Tournament to pvp, Warlords season 3 is already in pvp.

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Vashj'ir/Kelp'thar Forest/Vendors.lua
@@ -0,0 +1,27 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+--[[_.Zones =
+{
+	m(EASTERN_KINGDOMS, {
+		m(203, {	-- Vashj'ir
+			m(201, {	-- Kelp'thar Forest
+				n(VENDORS, {
+					Couldnt find these when the zone is completed, maybe only available for questing
+					n(36915, {	-- Erunak Stonespeaker <The Earthen Ring>
+						["coord"] = { 45.0, 23.2, 201 },
+						["sym"] = {
+							{"sub", "common_vendor", 71304},	-- Iris Moondreamer <Quartermaster>
+						},
+					}),
+					n(41618, {	-- Erunak Stonespeaker <The Earthen Ring>
+						["coord"] = { 38.8, 31.6, 201 },
+						["sym"] = {
+							{"sub", "common_vendor", 71304},	-- Iris Moondreamer <Quartermaster>
+						},
+					}),
+				}),
+			}),
+		}),
+	}),
+};]]
