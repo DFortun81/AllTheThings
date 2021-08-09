a8a6945a87d392cef2fb64df2418acb6ba5581b5
Darkal31 <michalbert@seznam.cz>
2021-08-09 22:39:34 +0200

    Sorting BFA world drops

---
+++ .contrib/Parser/DATAS/03 - World Drops/10 Battle For Azeroth/02 Armor.lua
@@ -0,0 +1,52 @@
+-----------------------------------------------------
+--       W O R L D   D R O P S   M O D U L E       --
+-----------------------------------------------------
+
+_.WorldDrops =
+{
+	tier(BFA_TIER, {
+		n(-318, {	-- Armor [Epic -- No adjustments]
+			n(-322, {	-- Back
+				i(160161),	-- Faithless Skycloak
+				i(154848),	-- Heartsbane Greatcloak
+				i(163953),	-- Jula's Jovial Drape
+				i(159229),	-- Mistfall Greatcloak
+				i(163932),	-- Wolfpelt Greatcloak
+			}),
+			n(-327, {	-- Hands
+				i(163941),	-- Hauer's Gloves of Strategy
+				i(163948),	-- Moarse's Coarse-Chain Gauntlets
+				i(163945),	-- Mystra's Magnificient Mitts
+				i(163952),	-- Oprescu's Gilded Touch
+			}),
+			n(-328, {	-- Waist
+				i(163931),	-- Chain Links of Fraying Awe
+				i(163951),	-- Cinch of the Sainted General
+				i(163940),	-- Extra-Light Light Emitting Cord
+				i(163944),	-- Waatah's Double-Knotted Cord
+			}),
+			n(-329, {	-- Legs
+				i(163950),	-- Edla's Embellished Legguards
+				i(163939),	-- Mallusof's Pantaloons of Stability
+				i(163943),	-- Neleimour's Patterned Leggings
+				i(163947),	-- Robust Legwraps of D'nusa
+			}),
+			n(-330, {	-- Feet
+				i(163949),	-- Celedhring's Unrelenting Stride
+				i(163946),	-- Gayeong's Gentle Step
+				i(163938),	-- Kalieve's Kickers
+				i(163942),	-- Slippers of the Unruly Fairies
+			}),
+			n(-385, {	-- Finger
+				i(163933),	-- Aguas' Band of Augury
+				i(163934),	-- Gnarled Mood Ring
+				i(160136),	-- Golden City Signet
+			}),
+			f(53,   {	-- Trinket
+				i(163936),	-- 'Bygone Bee' Almanac
+				i(163935),	-- Landoi's Scrutiny
+				i(163937),	-- Leyshock's Grand Compilation
+			}),
+		}),
+	}),
+};
