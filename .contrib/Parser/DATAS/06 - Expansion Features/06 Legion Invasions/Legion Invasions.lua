f6e54185e9364a1186c43faae53b9e495c194e9e
Crieve <dylanfortune@live.com>
2021-08-28 03:06:05 -0700

    Moved Legion Invasions under Expansion Features.

--- .contrib/Parser/DATAS/06 - Expansion Features/06 Legion Invasions/Legion Invasions.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/06 Legion Invasions/Legion Invasions.lua
@@ -1,8 +1,8 @@
------------------------------------------------------
---     W O R L D   E V E N T S   M O D U L E       --
------------------------------------------------------
+-------------------------------------------------------------------
+--      E X P A N S I O N   F E A T U R E S    M O D U L E       --
+-------------------------------------------------------------------
 
-_.WorldEvents =
+_.ExpansionFeatures =
 {
 	n(-543, {	-- Broken Isles: Legion Invasions
 		["achievementID"] = 11544,	-- Defender of the Broken Isles
