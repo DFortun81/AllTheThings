493bb2a9b487d4dd58be9ca94f1049791d1b1de9
Gold <goldenshacal@gmail.com>
2021-08-28 23:24:01 +0200

    superior parts update

--- .contrib/Parser/DATAS/06 - Expansion Features/08 Shadowlands/01 Covenants/Covenant - Necrolord/Sanctum Upgrades.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/08 Shadowlands/01 Covenants/Covenant - Necrolord/Sanctum Upgrades.lua
@@ -876,93 +876,96 @@ _.ExpansionFeatures =
 						}),
 					}),
 					n(-905, {	-- Command Table
-						n(-906, {	-- Tier 1: Tactical Insight
-							q(64465, {	-- Adventurer: Enceladus
-								["description"] = "Requires Renown 62.",
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-								["g"] = {
-									follower(1335),	-- Enceladus
-								},
+						["g"] = {
+							i(183744),	-- Superior Parts		
+							n(-906, {	-- Tier 1: Tactical Insight
+								q(64465, {	-- Adventurer: Enceladus
+									["description"] = "Requires Renown 62.",
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+									["g"] = {
+										follower(1335),	-- Enceladus
+									},
+								}),
+								q(64466, {	-- Adventurer: Deathfang
+									["description"] = "Requires Renown 71.",
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+									["g"] = {
+										follower(1336),	-- Deathfang
+									},
+								}),
+								q(62312, {	-- Adventurer: Gunn Gorgebone
+									["description"] = "Requires Renown 12.",
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+									["g"] = {
+										follower(1301),	-- Gunn Gorgebone
+									},
+								}),
+								q(62314, {	-- Adventurer: Khaliiq
+									["description"] = "Requires Renown 27.",
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+									["g"] = {
+										follower(1303),	-- Khaliiq
+									},
+								}),
+								q(64464, {	-- Adventurer: Lyra Hailstorm
+									["description"] = "Requires Renown 44.",
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+									["g"] = {
+										follower(1334),	-- Lyra Hailstorm
+									},
+								}),
+								q(62315, {	-- Adventurer: Plaguey
+									["description"] = "Requires Renown 33.",
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+									["g"] = {
+										follower(1304),	-- Plaguey
+									},
+								}),
+								q(62316, {	-- Adventurer: Rathan
+									["description"] = "Requires Renown 38.",
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+									["g"] = {
+										follower(1305),	-- Rathan
+									},
+								}),
+								q(62313, {	-- Adventurer: Rencissa the Dynamo
+									["description"] = "Requires Renown 17.",
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+									["g"] = {
+										follower(1302),	-- Rencissa the Dynamo
+									},
+								}),
+								q(62309, {	-- Adventurer: Secutor Mevix
+									["sourceQuests"] = { 59603 },	-- In Shadowlands Service
+									["description"] = "Requires Renown 4.",
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+									["g"] = {
+										follower(1300),	-- Secutor Mevix
+									},
+								}),
+								q(59603, {	-- In Shadowlands Service
+									["sourceQuests"] = { 63054 },	-- Strength in Numbers
+									["provider"] = { "n", 165321 },	-- Merick Feldscar
+									["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
+								}),
+								q(63054, {	-- Strength in Numbers
+									["provider"] = { "n", 161909 },	-- Arkadia Moa
+									["coord"] = { 52.4, 38.4, SEAT_OF_THE_PRIMUS },
+									["description"] = "Becomes available after you build Command Table tier 1 in your sanctum.",
+								}),
 							}),
-							q(64466, {	-- Adventurer: Deathfang
-								["description"] = "Requires Renown 71.",
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-								["g"] = {
-									follower(1336),	-- Deathfang
-								},
-							}),
-							q(62312, {	-- Adventurer: Gunn Gorgebone
-								["description"] = "Requires Renown 12.",
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-								["g"] = {
-									follower(1301),	-- Gunn Gorgebone
-								},
-							}),
-							q(62314, {	-- Adventurer: Khaliiq
-								["description"] = "Requires Renown 27.",
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-								["g"] = {
-									follower(1303),	-- Khaliiq
-								},
-							}),
-							q(64464, {	-- Adventurer: Lyra Hailstorm
-								["description"] = "Requires Renown 44.",
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-								["g"] = {
-									follower(1334),	-- Lyra Hailstorm
-								},
-							}),
-							q(62315, {	-- Adventurer: Plaguey
-								["description"] = "Requires Renown 33.",
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-								["g"] = {
-									follower(1304),	-- Plaguey
-								},
-							}),
-							q(62316, {	-- Adventurer: Rathan
-								["description"] = "Requires Renown 38.",
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-								["g"] = {
-									follower(1305),	-- Rathan
-								},
-							}),
-							q(62313, {	-- Adventurer: Rencissa the Dynamo
-								["description"] = "Requires Renown 17.",
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-								["g"] = {
-									follower(1302),	-- Rencissa the Dynamo
-								},
-							}),
-							q(62309, {	-- Adventurer: Secutor Mevix
-								["sourceQuests"] = { 59603 },	-- In Shadowlands Service
-								["description"] = "Requires Renown 4.",
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-								["g"] = {
-									follower(1300),	-- Secutor Mevix
-								},
-							}),
-							q(59603, {	-- In Shadowlands Service
-								["sourceQuests"] = { 63054 },	-- Strength in Numbers
-								["provider"] = { "n", 165321 },	-- Merick Feldscar
-								["coord"] = { 38.8, 48.2, SEAT_OF_THE_PRIMUS },
-							}),
-							q(63054, {	-- Strength in Numbers
-								["provider"] = { "n", 161909 },	-- Arkadia Moa
-								["coord"] = { 52.4, 38.4, SEAT_OF_THE_PRIMUS },
-								["description"] = "Becomes available after you build Command Table tier 1 in your sanctum.",
-							}),
-						}),
-						-- Tier 2: Recuperative Treatments
-						-- Tier 3: Forced March
+							-- Tier 2: Recuperative Treatments
+							-- Tier 3: Forced March
+						},
 					}),
 					n(-924, {	-- Transport Network
 						-- Tier 1: Ziggurat Now
