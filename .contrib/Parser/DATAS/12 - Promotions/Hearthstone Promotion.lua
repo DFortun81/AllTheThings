3ca6cff4c098d045175e876d0e47cf361158631d
Crieve <dylanfortune@live.com>
2021-08-28 16:51:21 -0700

    The Hearthstone Promotion is still available.

---
+++ .contrib/Parser/DATAS/12 - Promotions/Hearthstone Promotion.lua
@@ -0,0 +1,14 @@
+-----------------------------------------------------
+--        P R O M O T I O N S   M O D U L E        --
+-----------------------------------------------------
+_.Promotions =
+{
+	n(-533, { 	-- Hearthstone Promotion
+		["description"] = "All you need to do is win 3 games in Hearthstone to qualify for these rewards.\n\nThe game is free, go get it!\n\nNOTE: You can thank IceQ1337 for this!",
+		["timeline"] = { "added 5.3.0.16825" },
+		["groups"] = {
+			ach(8345),	-- Hearthstoned
+			i(98618), -- Hearthsteed
+		},
+	}),
+};
