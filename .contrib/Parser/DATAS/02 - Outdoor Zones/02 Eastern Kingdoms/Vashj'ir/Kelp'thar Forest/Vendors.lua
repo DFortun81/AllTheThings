f34f41455ee57db54469b7f8f00f054a43ca0899
Crieve <dylanfortune@live.com>
2021-08-28 16:52:06 -0700

    Moved the Scroll of Resurrection content back to Promotions.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Vashj'ir/Kelp'thar Forest/Vendors.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Vashj'ir/Kelp'thar Forest/Vendors.lua
@@ -1,27 +1,32 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
---[[_.Zones =
+_.Zones =
 {
 	m(EASTERN_KINGDOMS, {
 		m(203, {	-- Vashj'ir
 			m(201, {	-- Kelp'thar Forest
 				n(VENDORS, {
-					Couldnt find these when the zone is completed, maybe only available for questing
 					n(36915, {	-- Erunak Stonespeaker <The Earthen Ring>
 						["coord"] = { 45.0, 23.2, 201 },
+						["races"] = ALLIANCE_ONLY,
 						["sym"] = {
-							{"sub", "common_vendor", 71304},	-- Iris Moondreamer <Quartermaster>
+							{ "select", "headerID", -573 },	-- Scroll of Resurrection
+							{ "pop" },						-- Pop the Headers
+							{ "where", "key", "classID" },	-- Only grab class headers
 						},
 					}),
 					n(41618, {	-- Erunak Stonespeaker <The Earthen Ring>
 						["coord"] = { 38.8, 31.6, 201 },
+						["races"] = HORDE_ONLY,
 						["sym"] = {
-							{"sub", "common_vendor", 71304},	-- Iris Moondreamer <Quartermaster>
+							{ "select", "headerID", -573 },	-- Scroll of Resurrection
+							{ "pop" },						-- Pop the Headers
+							{ "where", "key", "classID" },	-- Only grab class headers
 						},
 					}),
 				}),
 			}),
 		}),
 	}),
-};]]
+};
