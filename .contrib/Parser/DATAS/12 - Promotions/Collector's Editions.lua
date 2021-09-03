4b1c122cd88dd8d2a02e19a48d6ff7a0988e4d89
Darkal31 <michalbert@seznam.cz>
2021-09-03 17:44:59 +0200

    Wording

--- .contrib/Parser/DATAS/12 - Promotions/Collector's Editions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/Collector's Editions.lua
@@ -123,7 +123,7 @@ _.Promotions =
 			},
 		})),
 		tier(WOD_TIER, {
-			["description"] = "These rewards were made available to anyone that purchased a Collector's Edition of Warlords of Draenor.\n\nThe rewards can be be purchased from the in-game shop.",
+			["description"] = "These rewards were made available to anyone that purchased a Collector's Edition of Warlords of Draenor.",
 			["timeline"] = { "added 6.0.1" },
 			["groups"] = {
 				ach(8917),	-- Collector's Edition: Dread Hatchling
@@ -155,7 +155,7 @@ _.Promotions =
 			},
 		})),
 		tier(SL_TIER, bubbleDown({ ["u"] = BLIZZARD_BALANCE }, {
-			["description"] = "These rewards were made available to anyone that purchased Shadowlands Heroic Edition.\n\nIt is currently unknown if these will be appearing in the In-Game Shop or not.",
+			["description"] = "These rewards are available to anyone that purchase Shadowlands Heroic Edition.",
 			["timeline"] = { "added 9.0.1" },
 			["groups"] = {
 				mount(307932),	-- Ensorcelled Everwyrm
@@ -254,7 +254,7 @@ _.Promotions =
 		}),
 		
 		n(-575, bubbleDown({ ["u"] = BLIZZARD_BALANCE }, {	-- Diablo III: Reaper of Souls
-			["description"] = "These rewards were made available to anyone that purchased the Diablo III: Reaper of Souls Collector's Edition.",
+			["description"] = "These rewards are available to anyone that purchases the Diablo III: Reaper of Souls Collector's Edition.",
 			["timeline"] = { "added 5.4.2.17585" },
 			["groups"] = {
 				ach(8795),	-- Collector's Edition: Treasure Goblin
@@ -262,7 +262,7 @@ _.Promotions =
 			},
 		})),
 		n(-568, bubbleDown({ ["u"] = BLIZZARD_BALANCE }, {	-- Overwatch: Origins
-			["description"] = "These rewards were made available to anyone that purchased the Overwatch: Origins Collector's Edition.",
+			["description"] = "These rewards are available to anyone that purchases the Overwatch: Origins Collector's Edition.",
 			["timeline"] = { "added 6.2.3.20716" },
 			["groups"] = {
 				ach(11064),	-- Collector's Edition: Baby Winston
@@ -270,7 +270,7 @@ _.Promotions =
 			},
 		})),
 		n(-564, bubbleDown({ ["u"] = BLIZZARD_BALANCE }, {	-- Starcraft II: Wings of Liberty
-			["description"] = "These rewards were made available to anyone that purchased the Starcraft II: Wings of Liberty Collector's Edition.",
+			["description"] = "These rewards are available to anyone that purchases the Starcraft II: Wings of Liberty Collector's Edition.",
 			["timeline"] = { "added 3.3.5.12340" },
 			["groups"] = {
 				ach(4824),	-- Collector's Edition: Mini Thor
@@ -278,7 +278,7 @@ _.Promotions =
 			},
 		})),
 		n(-565, bubbleDown({ ["u"] = BLIZZARD_BALANCE }, {	-- Starcraft II: Heart of the Swarm
-			["description"] = "These rewards were made available to anyone that purchased the Starcraft II: Heart of the Swarm Collector's Edition.",
+			["description"] = "These rewards are available to anyone that purchases the Starcraft II: Heart of the Swarm Collector's Edition.",
 			["timeline"] = { "added 5.0.4.15913" },
 			["groups"] = {
 				ach(7842),	-- Collector's Edition: Baneling
@@ -286,7 +286,7 @@ _.Promotions =
 			},
 		})),
 		n(-576, bubbleDown({ ["u"] = BLIZZARD_BALANCE }, {	-- Starcraft II: Legacy of the Void
-			["description"] = "These rewards were made available to anyone that purchased the Starcraft II: Legacy of the Void Collector's Edition.",
+			["description"] = "These rewards are available to anyone that purchases the Starcraft II: Legacy of the Void Collector's Edition.",
 			["timeline"] = { "added 6.2.2.20395" },
 			["groups"] = {
 				ach(10309),	-- Collector's Edition: Zeradar
@@ -294,7 +294,7 @@ _.Promotions =
 			},
 		})),
 		n(-552, bubbleDown({ ["u"] = BLIZZARD_BALANCE }, {	-- Warcraft 3 Reforged Spoils of War Edition
-			["description"] = "These rewards were made available to anyone that purchased the Warcraft 3 Reforged Spoils of War Edition.",
+			["description"] = "These rewards are available to anyone that purchases the Warcraft 3 Reforged Spoils of War Edition.",
 			["timeline"] = { "added 8.1.0.28724" },
 			["groups"] = {
 				ach(13196),	-- Meat Marauder
