da1d208aca44d5fcf9c69bd4da1c29be78475eff
Crieve <dylanfortune@live.com>
2021-09-19 16:46:36 -0700

    Deeprun Tram has been updated for TBC and Classic.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Deeprun Tram.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Deeprun Tram.lua
@@ -1,33 +1,53 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
-
+-- CRIEVE NOTE: Don't touch this file without asking.
+-- Burden of Truth: Classic & TBC.
 _.Zones =
 {
 	m(EASTERN_KINGDOMS, {
 		m(DEEPRUN_TRAM, {
-			["achievementID"] = 627,
 			["lore"] = "The Deeprun Tram is a long, fully enclosed, underground (and partially underwater) set of double tracks upon which rolls two sets of three wagons, all credited to the gnomes' technical engineering. The service is fast and smooth, and is provided free of charge to travelers between the Alliance-aligned cities of Ironforge and Stormwind City.",
-			["g"] = {
+			["groups"] = {
 				n(QUESTS, {
 					q(6661, {	-- Deeprun Rat Roundup
-						["provider"] = { "n", 12997 },	-- Monty
+						["qg"] = 12997,	-- Monty
 						["races"] = ALLIANCE_ONLY,
+						["lvl"] = lvlsquish(10, 1, 10),
+						["groups"] = {
+							objective(1, {	-- 0/5 Rats Captured
+								["provider"] = { "i", 17117 },	-- Rat Catcher's Flute
+								["cr"] = 13016,	-- Deeprun Rat
+							}),
+						},
 					}),
 					q(6662, {	-- Me Brother, Nipsy
-						["sourceQuests"] = { 6661 }, -- Deeprun Rat Roundup
-						["provider"] = { "n", 12997 },	-- Monty
+						["qg"] = 12997,	-- Monty
+						["sourceQuest"] = 6661, -- Deeprun Rat Roundup
 						["races"] = ALLIANCE_ONLY,
-					}),
-				}),
-				n(VENDORS, {
-					n(149806, {	-- A. Shady
-						["coord"] = { 34.2, 32.9, DEEPRUN_TRAM },
-						["g"] = {
-							i(166805),	-- Blood-Soaked Invitation (QI)
+						["cost"] = {
+							{ "i", 17118, 1 },	-- Carton of Mystery Meat
+						},
+						["lvl"] = lvlsquish(10, 1, 10),
+						["groups"] = {
+							i(17119),	-- Deeprun Rat Kabob
 						},
 					}),
 				}),
+				-- #if AFTER 8.1.5.29701
+				n(VENDORS, {
+					n(149806, {	-- A. Shady
+						["coord"] = { 34.2, 32.9, DEEPRUN_TRAM },
+						["timeline"] = { "added 8.1.5.29701" },
+						["groups"] = {
+							i(166805, {	-- Blood-Soaked Invitation
+								["cost"] = { { "g", 1000000 } },	-- 100g
+								["timeline"] = { "added 8.1.5.29701" },
+							}),
+						},
+					}),
+				}),
+				-- #endif
 			},
 		}),
 	}),
