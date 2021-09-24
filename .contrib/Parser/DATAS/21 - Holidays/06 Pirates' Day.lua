e5405863bb2fa2db277173a7b6be6cb3c036ed8a
Crieve <dylanfortune@live.com>
2021-09-24 15:47:49 -0700

    Updated Pirates' Day.

--- .contrib/Parser/DATAS/21 - Holidays/06 Pirates' Day.lua
+++ .contrib/Parser/DATAS/21 - Holidays/06 Pirates' Day.lua
@@ -1,58 +1,88 @@
 --------------------------------------------
 --     H O L I D A Y S  M O D U L E       --
 --------------------------------------------
-
-_.Holidays = bubbleDown({["u"] = PIRATES_DAY},
-{
-	holiday(235481, {	-- Pirates' Day
-		["maps"] = { THE_CAPE_OF_STRANGLETHORN },
-		["g"] = {
-			n(ACHIEVEMENTS, {
-				ach(3457, {	-- The Captain's Booty
-					["provider"] = { "n", 28048 },	-- Dread Captain DeMeza <Scourge of the South Seas>
-					["coord"] = { 40.0, 72.6, THE_CAPE_OF_STRANGLETHORN },	-- Booty Bay Pirate Day Location
-				}),
+_.Holidays = { applyholiday(PIRATES_DAY, {
+	-- #if ANYCLASSIC
+	["npcID"] = -55,
+	-- #else
+	["holidayID"] = 235481,
+	-- #endif
+	["maps"] = {
+		-- #if AFTER CATA
+		THE_CAPE_OF_STRANGLETHORN,
+		-- #else
+		STRANGLETHORN_VALE,
+		-- #endif
+	},
+	["timeline"] = { "added 2.4.3.8601" },
+	["groups"] = {
+		-- #if BEFORE 6.0.2.18816
+		n(28048, {	-- Dread Captain DeMeza <Scourge of the South Seas>
+			["description"] = "When you speak with her, she gives you a Pirate Costume buff that lasts for 12 hours.",
+			-- #if AFTER CATA
+			["coord"] = { 40.0, 72.6, THE_CAPE_OF_STRANGLETHORN },
+			-- #else
+			["coord"] = { 26.6, 76.6, STRANGLETHORN_VALE },
+			-- #endif
+			["groups"] = {
+				ach(3457),	-- The Captain's Booty
+			},
+		}),
+		-- #endif
+		-- #if AFTER 7.0.3.21570
+		n(QUESTS, {
+			q(42758, {	-- You're Gonna Need A Bigger Boat!
+				["qg"] = 28048,	-- Dread Captain DeMeza <Scourge of the South Seas>
+				["coord"] = { 40.0, 72.6, THE_CAPE_OF_STRANGLETHORN },
+				["timeline"] = { "added 7.0.3.21570" },
+				["isYearly"] = true,
+				["groups"] = {
+					i(138414, {	-- Emergency Pirate Outfit
+						["timeline"] = { "added 7.0.3.21570" },
+					}),
+				},
 			}),
-			n(QUESTS, {
-				q(42758, {	-- You're Gonna Need A Bigger Boat!
-					["isYearly"] = true,
-					["provider"] = { "n", 28048 },	-- Dread Captain DeMeza <Scourge of the South Seas>
-					["coord"] = { 40.0, 72.6, THE_CAPE_OF_STRANGLETHORN },	-- Booty Bay Pirate Day Location
-					["g"] = {
-						i(138414),	-- Emergency Pirate Outfit
-					},
-				}),
+		}),
+		n(RARES, {
+			n(108715, {  -- Ol' Eary
+				["coord"] = { 37.8, 66.2, THE_CAPE_OF_STRANGLETHORN },
+				["timeline"] = { "added 7.0.3.21570" },
+				["groups"] = {
+					i(138415, {	-- Slightly-Chewed Insult Book
+						["timeline"] = { "added 7.0.3.21570" },
+					}),
+				},
 			}),
-			n(RARES, {
-				n(108715, {  -- Ol' Eary
-					["coord"] = { 37.8, 66.2, THE_CAPE_OF_STRANGLETHORN },
-					["g"] = {
-						i(138415),	-- Slightly-Chewed Insult Book
-					},
-				}),
+		}),
+		-- #endif
+		-- #if AFTER 6.0.2.18816
+		n(VENDORS, {
+			n(28048,  {	-- Dread Captain DeMeza <Scourge of the South Seas>
+				["description"] = "When you speak with her, she gives you a Pirate Costume buff that lasts for 12 hours.",
+				["coord"] = { 40.0, 72.6, THE_CAPE_OF_STRANGLETHORN },
+				["groups"] = {
+					ach(3457),	-- The Captain's Booty
+					i(116392, {	-- Big Bag of Booty
+						["timeline"] = { "added 6.0.2.18816" },
+						["f"] = 55,	-- Consumable; Changed from "Toy"
+					}),
+					i(138400, {	-- Petey
+						["timeline"] = { "added 7.0.3.22248" },
+						["f"] = 55,	-- Consumable; Changed from nothing
+					}),
+				},
 			}),
-			n(VENDORS, {
-				n(28048,  {	-- Dread Captain DeMeza <Scourge of the South Seas>
-					["coord"] = { 40.0, 72.6, THE_CAPE_OF_STRANGLETHORN },	-- Booty Bay Pirate Day Location
-					["g"] = {
-						i(116392, {	-- Big Bag of Booty
-							["f"] = 55,	-- Consumable; Changed from "Toy"
-						}),
-						i(138400, {	-- Petey
-							["f"] = 55,	-- Consumable; Changed from nothing
-						}),
-					},
-				}),
-				n(122396, {	-- Edward Techt
-					["description"] = "To purchase the |cFFFFD700Jolly Roger|r toy, you must have earned the achievement |cFFFFD700Avast Ye, Admiral|r.",
-					["coord"] = { 39.6, 84.2, THE_CAPE_OF_STRANGLETHORN },
-					["g"] = {
-						i(150547, {	-- Jolly Roger (Toy)
-							["description"] = "To purchase this toy, you must have earned the achievement |cFFFFD700Avast Ye, Admiral|r.",
-						}),
-					},
-				}),
+			n(122396, {	-- Edward Techt <Keeper of the Black>
+				["coord"] = { 39.6, 84.2, THE_CAPE_OF_STRANGLETHORN },
+				["timeline"] = { "added 7.2.5.23910" },
+				["groups"] = {
+					i(150547, {	-- Jolly Roger (Toy)
+						["description"] = "To purchase this toy, you must have earned the achievement |cFFFFD700Avast Ye, Admiral|r.",
+						["timeline"] = { "added 7.2.5.23910" },
+					}),
+				},
 			}),
-		},
-	}),
-});
+		}),
+		-- #endif
+	},
+})};
