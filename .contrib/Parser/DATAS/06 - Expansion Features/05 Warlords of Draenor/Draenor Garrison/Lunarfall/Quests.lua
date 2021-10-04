e4aa3fcf3d4de00fd244204e6ad5a7237e4e8134
Darkal31 <michalbert@seznam.cz>
2021-10-04 17:17:07 +0200

    2 HQTs

--- .contrib/Parser/DATAS/06 - Expansion Features/05 Warlords of Draenor/Draenor Garrison/Lunarfall/Quests.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/05 Warlords of Draenor/Draenor Garrison/Lunarfall/Quests.lua
@@ -1267,6 +1267,8 @@ _.ExpansionFeatures =
 	}),
 };
 _.HiddenQuestTriggers = {
+	q(37647),	-- Tracking Quest - Hide Old Baros / Thorn / Brightstone (after picking up the rose quest item during quest "Garrison Campaign: Every Rose Has Its Thorn")
+	q(37670),	-- [Deprecated] Tracking Quest - Hide Old Baros / Thorn / Brightstone (after picking up the rose quest item during quest "Garrison Campaign: Every Rose Has Its Thorn")
 	q(39829),	-- Sleigh and Guards Shared Criteria (Winter Veil)
 	q(39610),	-- Winter Veil Active (Winter Veil)
 	q(38276),	-- 6.1 Garrison - World Boss Invasion Loot (killing Commander Dro'gan)
