9f71eec805dc8387c480d21429940f6abb012abc
gbrage <gbrage@kth.se>
2021-10-06 00:25:19 +0200

    -- Removing old files -- Moving some files to their place.

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/1 - Telogrus Rift.lua
@@ -0,0 +1,16 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+
+_.Zones =
+{
+	m(EASTERN_KINGDOM, {
+		m(971, {	-- Telogrus Rift
+			["maps"] = { 972 },	-- Telogrus Rift
+			["races"] = { VOIDELF },
+			["isRaid"] = true,
+			["icon"] = "Interface\\Icons\\ability_racial_preturnaturalcalm",
+			["lore"] = "Many have sought to harness the corruptive magic of the Void. Most who tried have fallen into madness. Determined to use this power for the good of Azeroth, Alleria Windrunner is the first mortal to succeed at defying the shadow's whispers. Coming to the aid of a group of her kin who nearly gave in to the darkness, Alleria has vowed to train these void elves to control the shadows within them and pledge their newfound powers to the Alliance. Void elves constantly hear voices from the Void that seek to corrupt them, something they have learned to fight against and keep control over. This assault is relentless and the discipline needed to resist them is extraordinary. The voices say many things, from lies, to things that are true from a certain point of view, things that sound true, and things that one would wish were true.",
+		}),
+	}),
+};
