f322030e99e5a62589dceebbce843307dd6439ae
Molkree <mr.molkree@gmail.com>
2021-10-09 18:17:37 +0300

    Fix typo

--- .contrib/Parser/DATAS/01 - Dungeons Raids/8 - Battle For Azeroth/1 - Raids/02 Uldir.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/8 - Battle For Azeroth/1 - Raids/02 Uldir.lua
@@ -24,20 +24,18 @@ _.Instances = { tier(BFA_TIER, {
 					["description"] = "As mentioned above, Reorigination Array can stack up to 10 times, and the tooltip reveals how this system works. Every week, there is a hidden mini-quest to defeat 3 bosses in Uldir, this objective can be completed in any difficulty. You do not need to have any Azerite Armor from Uldir to progress in these quests. Completing this quest will reward you with one stack of Reorigination Array while in Uldir permanently.\n\nThe track of the complete upgrade system is achieved by a dozen different flag quests, listed below. Note, however, that these quests don't have database entries as they only serve as flags, but can be actually queried in-game for their completion. The first part of the system controls how many bosses have been killed in Uldir for that week:",
 					["g"] = {
 						n(-229, {	-- Weekly
-							["g"] = {
-								n(-593, {	--  Kill Credit 1
-									["questID"] = 53568,	--  Kill Credit 1
-									["isWeekly"] = true,
-								}),
-								n(-594, {	--  Kill Credit 2
-									["questID"] = 53569,	--  Kill Credit 2
-									["isWeekly"] = true,
-								}),
-								n(-595, {	--  Kill Credit 3
-									["questID"] = 53570,	--  Kill Credit 3
-									["isWeekly"] = true,
-								}),
-							},
+							n(-593, {	--  Kill Credit 1
+								["questID"] = 53568,	--  Kill Credit 1
+								["isWeekly"] = true,
+							}),
+							n(-594, {	--  Kill Credit 2
+								["questID"] = 53569,	--  Kill Credit 2
+								["isWeekly"] = true,
+							}),
+							n(-595, {	--  Kill Credit 3
+								["questID"] = 53570,	--  Kill Credit 3
+								["isWeekly"] = true,
+							}),
 						}),
 						n(-583, {	-- Power Increased to Level 1
 							["questID"] = 53571,	-- Stack #1
