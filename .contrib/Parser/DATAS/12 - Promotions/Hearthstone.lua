f3c7cf6138e73c8d3eca2bf8b45e577dbac65f99
gbrage <gbrage@kth.se>
2021-10-23 10:24:17 +0200

    -- Just making filter consistent for Blizzard Balance until decided upon.

--- .contrib/Parser/DATAS/12 - Promotions/Hearthstone.lua
+++ .contrib/Parser/DATAS/12 - Promotions/Hearthstone.lua
@@ -3,19 +3,19 @@
 -----------------------------------------------------
 _.Promotions =
 {
-	n(-533, { 	-- Hearthstone Promotion
+	n(-533, bubbleDown({ ["u"] = BLIZZARD_BALANCE }, { 	-- Hearthstone Promotion
 		["description"] = "All you need to do is win 3 games in Hearthstone to qualify for these rewards.\n\nThe game is free, go get it!\n\nNOTE: You can thank IceQ1337 for this!",
 		["timeline"] = { "added 5.3.0.16825" },
 		["groups"] = {
 			ach(8345),	-- Hearthstoned
 			i(98618), -- Hearthsteed
 		},
-	}),
-	n(-536, { 	-- Hearthstone Mercenaries Promotion
+	})),
+	n(-536, bubbleDown({ ["u"] = BLIZZARD_BALANCE }, { 	-- Hearthstone Mercenaries Promotion
 		["description"] = "Complete Hearthstone's Mercenaries Mode tutorial and send your party on a mission in order to receive this new mount.",
 		["timeline"] = { "added 9.1.0" },
 		["groups"] = {
 			mount(356488),	-- Sarge's Tale
 		},
-	}),
+	})),
 };
