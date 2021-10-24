254ad4c334a68ac45cdd6a8b780a42db9b595715
Crieve <dylanfortune@live.com>
2021-10-24 15:04:14 -0700

    Tier 0.5 finalized, now uses root.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/0.5 Tier 0.5 Sets.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/0.5 Tier 0.5 Sets.lua
@@ -1,7 +1,7 @@
 -----------------------------------------------------
 --   D U N G E O N S  &  R A I D S  M O D U L E    --
 -----------------------------------------------------
-_.Instances = { tier(CLASSIC_TIER, applyclassicphase(PHASE_FIVE, {
+root("Instances", tier(CLASSIC_TIER, applyclassicphase(PHASE_FIVE, {
 	bubbleDown({ ["timeline"] = { "removed 4.0.3" } }, n(-420, {	-- Tier 0.5 Sets
 		["lore"] = "The Dungeon Set 2 class sets, commonly referred to as Tier 0.5, are obtained by completing a long quest chain to upgrade the first set available as drops in end game dungeons into stronger versions of themselves. In current WoW, these sets are covetted by Collectors as the quest chain was completely removed from the game with Cataclysm. In WoW Classic, you should finish this quest chain on all of your characters before then!",
 		["groups"] = {
@@ -2065,4 +2065,4 @@ _.Instances = { tier(CLASSIC_TIER, applyclassicphase(PHASE_FIVE, {
 			}),
 		},
 	})),
-}))};
+})));
