e112abc87814f9298e2bfe00bd71321462a194b2
Crieve <dylanfortune@live.com>
2021-10-24 16:59:14 -0700

    Updated Onyxia's Lair with timelines and objectives.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/1.5 Onyxia's Lair.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/1 - Classic/1.5 Onyxia's Lair.lua
@@ -16,7 +16,7 @@ local NEMESIS_SKULLCAP = i(16929);	-- Nemesis Skullcap
 local NETHERWIND_CROWN = i(16914);	-- Netherwind Crown
 local STORMRAGE_COVER = i(16900);	-- Stormrage Cover
 
-_.Instances = { tier(CLASSIC_TIER, {
+root("Instances", tier(CLASSIC_TIER, {
 	inst(760, {	-- Onyxia's Lair
 		["lore"] = "Onyxia is the daughter of the mighty dragon Deathwing, and sister of the scheming Nefarian, Lord of Blackrock Spire.\n\nIt is said that Onyxia delights in corrupting the mortal races by meddling in their political affairs. To this end it is believed that she takes on various humanoid forms and uses her charm and power to influence delicate matters between the different races. Some believe that Onyxia has even assumed an alias once used by her father - the title of the royal House Prestor. When not meddling in mortal concerns, Onyxia resides in a fiery cave below the Dragonmurk, a dismal swamp located within Dustwallow Marsh. There she is guarded by her kin, the remaining members of the insidious black dragonflight.",
 		-- #if BEFORE WRATH
@@ -32,9 +32,9 @@ _.Instances = { tier(CLASSIC_TIER, {
 		["lvl"] = lvlsquish(60, 30, 60),
 		["groups"] = {
 			n(QUESTS, {
-				-- #if AFTER WRATH
-				q(24428, {	-- A Most Puzzling Circumstance (A)
+				applyclassicphase(WRATH_PHASE_THREE, q(24428, {	-- A Most Puzzling Circumstance (A)
 					["provider"] = { "i", 49644 },	-- Head of Onyxia (A)
+					["timeline"] = { "added 3.3.0.10571" },
 					["races"] = ALLIANCE_ONLY,
 					["lvl"] = 80,
 					["groups"] = {
@@ -42,9 +42,10 @@ _.Instances = { tier(CLASSIC_TIER, {
 						i(49485),	-- Sparkling Onyxia Tooth Pendant
 						i(49487),	-- Purified Onyxia Blood Talisman
 					},
-				}),
-				q(24429, {	-- A Most Puzzling Circumstance (H)
+				})),
+				applyclassicphase(WRATH_PHASE_THREE, q(24429, {	-- A Most Puzzling Circumstance (H)
 					["provider"] = { "i", 49643 },	-- Head of Onyxia (H)
+					["timeline"] = { "added 3.3.0.10571" },
 					["races"] = HORDE_ONLY,
 					["lvl"] = 80,
 					["groups"] = {
@@ -52,8 +53,7 @@ _.Instances = { tier(CLASSIC_TIER, {
 						i(49485),	-- Sparkling Onyxia Tooth Pendant
 						i(49487),	-- Purified Onyxia Blood Talisman
 					},
-				}),
-				-- #endif
+				})),
 				q(7635, bubbleDown({ ["timeline"] = REMOVED_WITH_CATA }, {	-- A Proper String
 					["qg"] = 14525,	-- Stoma the Ancient
 					["sourceQuest"] = 7633,	-- An Introduction
@@ -132,42 +132,42 @@ _.Instances = { tier(CLASSIC_TIER, {
 					["lvl"] = lvlsquish(60, 30, 60),
 					["cost"] = {
 						{ "i", 18708, 1 },	-- Petrified Bark
-						{ "i", 18955, 1 },	-- Artorius's Head
-						{ "i", 18953, 1 },	-- Klinfran's Head
-						{ "i", 18952, 1 },	-- Simone's Head
-						{ "i", 18954, 1 },	-- Solenor's Head
 					},
 					["groups"] = {
-						n(14535, {	-- Artorius the Doombringer
-							["description"] = "Kite him using Aspect of the Cheetah. Keep Serpent Sting on at all times and use Concussive Shot whenever he gets close. It is important to remain beyond 30 yards of the Demonic Doom, yet within range to use Arcane Shot so that he continues chasing you. If you rely completely on Serpent Sting for damage, he will break aggro and return to where he started. Once you apply Serpent Sting, count to 4, and apply it again before it wears off (the damage does stack). You will be able to see this by a 2nd damage listing \"Stinging Trauma\", which will continue to increase each time the sting is applied.\n\nBe sure to clear the area you plan on fighting so as to not get dazed by another NPC wandering by.\n\nLocated in Winterspring.",
-							["coord"] = { 60.3, 13.2, WINTERSPRING },
-							["crs"] = { 14531 },	-- Artorius the Amiable
-							["groups"] = {
-								i(18955),	-- Artorius's Head
-							},
-						}),
-						n(14534, {	-- Klinfran the Crazed
-							["description"] = "Use Concussive Shot/Serpent Sting to kite him down the road. You should know how long serpent sting lasts, use only one per trip down the road, more if you think you can risk it. As soon as it's worn off, drop an ice trap, let him stand in it, go to the other side, max range, and start again. If for whatever reason he breaks trap, you can use scatter shot, or use concussive shot to just keep kiting him in a circle until the cooldown finishes.\n\nDo not worry about his enrage, because he won't be touching you at all if you play right.\n\nLocated in the Burning Steppes.",
-							["coord"] = { 25.3, 65.9, BURNING_STEPPES },
-							["crs"] = { 14529 },	-- Franklin the Friendly
-							["groups"] = {
-								i(18953),	-- Klinfran's Head
-							},
-						}),
-						n(14533, {	-- Simone the Seductress
+						objective(1, {	-- 0/1 Simone's Head
+							["provider"] = { "i", 18952 },	-- Simone's Head
 							["description"] = "There are two things that can make this fight difficult: her pet, a felhound named Precious and low nature resistance. Most hunters tend to CC Precious by freeze trapping it. Using ranged attacks is useless because she casts a debuff that substantially lowers ranged attack power. Your best bet is to use aspect of the wild + a greater nature protection potion (to dull and absorb her lightning bolts) and melee her until she falls.\n\nLocated in Ungoro Crater.",
 							["coord"] = { 34.5, 41.1, UNGORO_CRATER },
-							["crs"] = { 14527 },	-- Simone the Inconspicuous
-							["groups"] = {
-								i(18952),	-- Simone's Head
+							["crs"] = {
+								14527,	-- Simone the Inconspicuous
+								14533,	-- Simone the Seductress
 							},
 						}),
-						n(14530, {	-- Solenor the Slayer
+						objective(2, {	-- 0/1 Klinfran's Head
+							["provider"] = { "i", 18953 },	-- Klinfran's Head
+							["description"] = "Use Concussive Shot/Serpent Sting to kite him down the road. You should know how long serpent sting lasts, use only one per trip down the road, more if you think you can risk it. As soon as it's worn off, drop an ice trap, let him stand in it, go to the other side, max range, and start again. If for whatever reason he breaks trap, you can use scatter shot, or use concussive shot to just keep kiting him in a circle until the cooldown finishes.\n\nDo not worry about his enrage, because he won't be touching you at all if you play right.\n\nLocated in the Burning Steppes.",
+							["coord"] = { 25.3, 65.9, BURNING_STEPPES },
+							["crs"] = {
+								14529,	-- Franklin the Friendly
+								14534,	-- Klinfran the Crazed
+							},
+						}),
+						objective(3, {	-- 0/1 Solenor's Head
+							["provider"] = { "i", 18954 },	-- Solenor's Head
 							["description"] = "This is the hardest demon:\n\nStrengths:\n* He does NOT predictably chase you. He frequently stops to fear (disorienting you and causing around 800 damage also) or will stop to shoot homing beetles at you.\n* The homing beetles basically prevent you from bandaging. They move extremely slow but hit extremely hard\n* If you leave melee range (further than 2-3 range or so), he uses an instant cast fear spell that also does 700-800 damage instantly, and he can cast these quickly, almost seemingly chain casting. Shadow protection potions do absorb this damage, however, even with greater potions, he tears through them very fast. It's also likely that the fear will send you into the homing beetles\n* Seemingly unlimited mana bar, so Viper Sting is useless\n* Scorpid sting has very little/no effect on him\n* Melees extremely hard and fast (200+) when in normal form\n\nWeaknesses:\n* If you use Rank 3 Wing Clip on him, it immobilizes him completely with \"Crippling Clip\" for 30 seconds.\n* The homing missiles (creepings) are not fired when you remain in melee range\n* He does NOT cast his fear when you remain in melee range\n* Must be 'chilled' via Frost Trap prior to fully engaging\n\nLocated in Silithus.",
 							["coord"] = { 24.7, 76.0, SILITHUS },
-							["crs"] = { 14536 },	-- Nelson the Nice
-							["groups"] = {
-								i(18954),	-- Solenor's Head
+							["crs"] = {
+								14536,	-- Nelson the Nice
+								14530,	-- Solenor the Slayer
+							},
+						}),
+						objective(4, {	-- 0/1 Artorius's Head
+							["provider"] = { "i", 18955 },	-- Artorius's Head
+							["description"] = "Kite him using Aspect of the Cheetah. Keep Serpent Sting on at all times and use Concussive Shot whenever he gets close. It is important to remain beyond 30 yards of the Demonic Doom, yet within range to use Arcane Shot so that he continues chasing you. If you rely completely on Serpent Sting for damage, he will break aggro and return to where he started. Once you apply Serpent Sting, count to 4, and apply it again before it wears off (the damage does stack). You will be able to see this by a 2nd damage listing \"Stinging Trauma\", which will continue to increase each time the sting is applied.\n\nBe sure to clear the area you plan on fighting so as to not get dazed by another NPC wandering by.\n\nLocated in Winterspring.",
+							["coord"] = { 60.3, 13.2, WINTERSPRING },
+							["crs"] = {
+								14531,	-- Artorius the Amiable
+								14535,	-- Artorius the Doombringer
 							},
 						}),
 						i(18707, {	-- Ancient Rune Etched Stave
@@ -205,32 +205,25 @@ _.Instances = { tier(CLASSIC_TIER, {
 					["maps"] = { DIRE_MAUL },
 					["classes"] = { WARRIOR, PALADIN },
 					["lvl"] = lvlsquish(60, 30, 60),
-					["cost"] = {
-						{ "i", 18492, 1 },	-- Treated Ancient Blade
-					},
 					["groups"] = {
-						{
-							["itemID"] = 18489,	-- Unfired Ancient Blade
-							["description"] = "Place this in front of Onyxia's Breath attack during The Forging of Quel'Serrar quest.\n\nYou may want to delay placing this item on the ground until the breath goes on cooldown during Phase 3.\n\nAs a note you can heat the blade in Phase 1 or 3. The Heated Ancient Blade has a deteriation time of 20 minutes. This is NOT a cooldown. Once the time runs out it will not change back into the Unfired Ancient Blade. It will instead disappear from your inventory and you will have to get a replacement for it by returning to DM, abandoning the quest, and reaccepting.",
-							["classes"] = { WARRIOR, PALADIN },
-						},
+						objective(1, {	-- 0/1 Treated Ancient Blade
+							["provider"] = { "i", 18492 },	-- Treated Ancient Blade
+							["description"] = "Drops from Onyxia when you use the Heated Ancient Blade on her corpse.",
+							["cr"] = 10184,	-- Onyxia
+							["cost"] = {
+								{ "i", 18488, 1 },	-- Heated Ancient Blade
+							},
+						}),
 						{
 							["itemID"] = 18488,	-- Heated Ancient Blade
 							["description"] = "Once the boss uses her Breath attack on your Unfired Ancient Blade, loot this item from the ground and then impale the corpse of Onyxia once you defeat her.",
 							["cost"] = {
 								{ "i", 18489, 1 },	-- Unfired Ancient Blade
 							},
-							["classes"] = { WARRIOR, PALADIN },
 						},
 						{
-							["itemID"] = 18492,	-- Treated Ancient Blade
-							["questID"] = 7509,	-- The Forging of Quel'Serrar
-							["description"] = "Drops from Onyxia when you use the Heated Ancient Blade on her corpse during The Forging of Quel'Serrar quest.",
-							["classes"] = { WARRIOR, PALADIN },
-							["cost"] = {
-								{ "i", 18488, 1 },	-- Heated Ancient Blade
-							},
-							["crs"] = { 10184 },	-- Onyxia
+							["itemID"] = 18489,	-- Unfired Ancient Blade
+							["description"] = "Place this in front of Onyxia's Breath attack during The Forging of Quel'Serrar quest.\n\nYou may want to delay placing this item on the ground until the breath goes on cooldown during Phase 3.\n\nAs a note you can heat the blade in Phase 1 or 3. The Heated Ancient Blade has a deteriation time of 20 minutes. This is NOT a cooldown. Once the time runs out it will not change back into the Unfired Ancient Blade. It will instead disappear from your inventory and you will have to get a replacement for it by returning to DM, abandoning the quest, and reaccepting.",
 						},
 						i(18348),	-- Quel'Serrar
 					},
@@ -334,7 +327,7 @@ _.Instances = { tier(CLASSIC_TIER, {
 			-- #if AFTER WRATH
 				}),
 			}),
-			d(3, {	-- 10 Player
+			applyclassicphase(WRATH_PHASE_THREE, d(3, {	-- 10 Player
 				["lvl"] = 80,
 				["groups"] = {
 					e(1651, {	-- Onyxia
@@ -391,8 +384,8 @@ _.Instances = { tier(CLASSIC_TIER, {
 						},
 					}),
 				},
-			}),
-			d(4, {	-- 25 Player
+			})),
+			applyclassicphase(WRATH_PHASE_THREE, d(4, {	-- 25 Player
 				["lvl"] = 80,
 				["groups"] = {
 					e(1651, {	-- Onyxia
@@ -449,11 +442,11 @@ _.Instances = { tier(CLASSIC_TIER, {
 						},
 					})
 				},
-			}),
+			})),
 			-- #endif
 		},
 	}),
-})};
+}));
 
 -- Helms were moved to Nefarian with 3.2.2, remove the unobtainable flag.
 BLOODFANG_HOOD.timeline = nil;
