65a146131648588c6995a2d95b2b5f12e80c8a65
ImUnicke <imunicke+github@gmail.com>
2021-10-26 17:00:58 -0600

    Consolidated Garrison buildings: Alchemy Lab / Barn / Barracks

---
+++ .contrib/Parser/DATAS/06 - Expansion Features/05 Warlords of Draenor/Draenor Garrison/Common/Buildings/Alchemy Lab.lua
@@ -0,0 +1,43 @@
+-------------------------------------------------------------------
+--      E X P A N S I O N   F E A T U R E S    M O D U L E       --
+-------------------------------------------------------------------
+
+_.ExpansionFeatures =
+{
+	tier(WOD_TIER, {
+		n(GARRISONS, sharedData({["maps"] = { LUNARFALL, FROSTWALL } },	{
+			n(BUILDINGS, {
+				garrisonBuilding(120,  {	-- Alchemy Lab (rank 1: 76, rank 2: 119, rank 3: 120)
+					n(QUESTS, {
+						q(37270, {	-- Alchemy Experiment
+							["providers"] = {
+								{ "n", 88145 },	-- Alchemy Follower - Alliance
+								{ "n", 88392 },	-- Alchemy Follower - Horde
+							},
+							["isDaily"] = true,
+						}),
+						q(35342, {	-- The Mysterious Flask
+							["requireSkill"] = ALCHEMY,
+							["provider"] = { "i", 113103 },	-- Mysterious Flask
+							["races"] = ALLIANCE_ONLY,
+						}),
+						q(35343, {	-- The Young Alchemist
+							["sourceQuests"] = { 35342 },	-- The Mysterious Flask
+							["requireSkill"] = ALCHEMY,
+							["provider"] = { "n", 82466 },	-- Aenir
+							["races"] = ALLIANCE_ONLY,
+						}),
+						q(36641, {	-- Your First Alchemy Work Order
+							["provider"] = { "n", 77363 },	-- Mary Kearie
+							["races"] = ALLIANCE_ONLY,
+						}),
+						q(37568, {	-- Your First Alchemy Work Order
+							["provider"] = { "n", 79813 },	-- Albert de Hyde
+							["races"] = HORDE_ONLY,
+						}),
+					}),
+				}),
+			}),
+		})),
+	}),
+};
---
+++ .contrib/Parser/DATAS/06 - Expansion Features/05 Warlords of Draenor/Draenor Garrison/Common/Buildings/Barracks.lua
@@ -0,0 +1,116 @@
+-------------------------------------------------------------------
+--      E X P A N S I O N   F E A T U R E S    M O D U L E       --
+-------------------------------------------------------------------
+
+_.ExpansionFeatures =
+{
+	tier(WOD_TIER, {
+		n(GARRISONS, sharedData({["maps"] = { LUNARFALL, FROSTWALL } },	{
+			n(BUILDINGS, {	-- Buildings
+				garrisonBuilding(28,  {	-- Barracks (rank 1: 26, rank 2: 27, rank 3: 28)
+					n(ACHIEVEMENTS, {
+						ach(9499, {		-- Wingmen
+							crit(1),		-- Leorajh
+							crit(2),		-- Talonpriest Ishaal
+							crit(3),		-- Tormmok
+							crit(4, {["races"] = HORDE_ONLY}),		-- Aeda Brightdawn
+							crit(5, {["races"] = ALLIANCE_ONLY}),		-- Defender Illona
+							crit(6, {["races"] = ALLIANCE_ONLY}),		-- Delvar Ironfirst
+							crit(7, {["races"] = HORDE_ONLY}),		-- Vivianne
+							ach(9498),		-- Wingman
+						}),
+					}),
+					n(QUESTS, {
+						q(37126, {	-- An Ogre Without His Sword...(A)
+							["races"] = ALLIANCE_ONLY,
+							["provider"] = { "n", 86682 },	-- Retired Gorian Centurion(Tormmok)
+						}),
+						q(37180, {	-- An Ogre Without His Sword...(H)
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86682 },	-- Retired Gorian Centurion(Tormmok)
+						}),
+						q(37081, {	-- Burning Rage
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86933 },	-- Warspear Magus(Vivianne)
+						}),
+						q(37085, {	-- Fury of the Clan
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86964 },	-- Bloodmane Earthbinder(Leorajh)
+						}),
+						q(37080, {	-- Glorious Slaughter
+							["races"] = ALLIANCE_ONLY,
+							["provider"] = { "n", 86927 },	-- Stormshield Death Knight(Delvar Ironfist)
+						}),
+						q(37140, {	-- Leorajh's Prayer Beads(A)
+							["races"] = ALLIANCE_ONLY,
+							["provider"] = { "n", 86964 },	-- Bloodmane Earthbinder(Leorajh)
+						}),
+						q(37181, {	-- Leorajh's Prayer Beads(H)
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86964 },	-- Bloodmane Earthbinder(Leorajh)
+						}),
+						q(36947, {	-- New Strength
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86682 },	-- Retired Gorian Centurion(Tormmok)
+						}),
+						q(37070, {	-- Power of the Nether
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86945 },	-- Sunsworn Warlock(Aeda Brightdawn)
+						}),
+						q(37137, {	-- Scrying Hard or Hardly Scrying(A)
+							["races"] = ALLIANCE_ONLY,
+							["provider"] = { "n", 86946 },	-- Outcast Talonpriest(Talonpriest Ishaal)
+						}),
+						q(37182, {	-- Scrying Hard or Hardly Scrying(H)
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86946 },	-- Outcast Talonpriest(Talonpriest Ishaal)
+						}),
+						q(37128, {	-- Settling Grievances
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86945 },	-- Sunsworn Warlock(Aeda Brightdawn)
+						}),
+						q(37131, {	-- The Dark Lady's Concern
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86933 },	-- Warspear Magus(Vivianne)
+						}),
+						q(37047, {	-- The Light's Gift
+							["races"] = ALLIANCE_ONLY,
+							["provider"] = { "n", 86934 },	-- Sha'tari Defender(Defender Illona)
+						}),
+						q(37127, {	-- The Light of Exodus
+							["races"] = ALLIANCE_ONLY,
+							["provider"] = { "n", 86934 },	-- Sha'tari Defender(Defender Illona)
+						}),
+						q(37084, {	-- The Shadows Call
+							["races"] = HORDE_ONLY,
+							["provider"] = { "n", 86946 },	-- Outcast Talonpriest(Talonpriest Ishaal)
+						}),
+						q(37129, {	-- Unwelcome Memories
+							["races"] = ALLIANCE_ONLY,
+							["provider"] = { "n", 86927 },	-- Stormshield Death Knight(Delvar Ironfist)
+						}),
+					}),
+					n(VENDORS, {
+						n(77379, {	-- Jonathan Stephens <Steward>
+							["races"] = ALLIANCE_ONLY,
+							["g"] = {
+								i(122298, {	-- Bodyguard Miniaturization Device (TOY!)
+									["cost"] = { { "c", 824, 250 }, },	-- 250x Garrison Resources
+								}),
+							},
+						}),
+						n(79812, {	-- Moz'def
+							["races"] = HORDE_ONLY,
+							["g"] = {
+								i(122298, {	-- Bodyguard Miniaturization Device (TOY!)
+									["cost"] = { { "c", 824, 250 }, },	-- 250x Garrison Resources
+								}),
+							},
+						}),
+					}),
+				}),
+			}),
+		})),
+	}),
+};
+
