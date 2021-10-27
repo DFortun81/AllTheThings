4f81114103b69c8c25cd443eb38c4a77ca3281b1
ImUnicke <imunicke+github@gmail.com>
2021-10-27 13:26:29 -0600

    Consolidated Garrison buildings: (Spirit Lodge & Mage Tower) / (War Mill & Dwarven Bunker) / (Goblin Workshop & Gnomish Gearworks)

--- .contrib/Parser/DATAS/06 - Expansion Features/05 Warlords of Draenor/Draenor Garrison/Common/Buildings/Barn.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/05 Warlords of Draenor/Draenor Garrison/Common/Buildings/Barn.lua
@@ -2,16 +2,6 @@
 --      E X P A N S I O N   F E A T U R E S    M O D U L E       --
 -------------------------------------------------------------------
 
--- _.ExpansionFeatures =
--- {
--- 	tier(WOD_TIER, {
--- 		n(GARRISONS, sharedData({["maps"] = { LUNARFALL, FROSTWALL } },	{
--- 			n(BUILDINGS, {
--- 			}),
--- 		})),
--- 	}),
--- };
-
 _.ExpansionFeatures =
 {
 	tier(WOD_TIER, {
