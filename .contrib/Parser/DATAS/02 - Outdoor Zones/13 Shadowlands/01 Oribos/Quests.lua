ab4049b50d0610ebec20751b3421229886946a2d
Gold <goldenshacal@gmail.com>
2021-11-06 11:11:27 +0100

    description for sl skip quest

--- .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/01 Oribos/Quests.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/01 Oribos/Quests.lua
@@ -593,6 +593,7 @@ _.Zones =
 					["sourceQuests"] = { 62704 },	-- The Threads of Fate
 					["altQuests"] = { 62713 },	-- Becoming a Skip character will exclude this Quest from being available
 					["customCollect"] = "SL_SKIP",	-- SL Skip Character
+					["description"] = "To get this Quest, you have to choose Replay Storyline and pick Skip Storyline right after.",
 					["isBreadcrumb"] = true,
 				}),
 				q(62716, {	-- Re-Introductions
