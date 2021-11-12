71bbf1e633298d9513fab69ab8831eda0ef14628
Molkree <mr.molkree@gmail.com>
2021-11-12 16:38:01 +0300

    Small clarification in comment

--- .contrib/Parser/DATAS/12 - Item Sets/Class Trials/05 - Level 50 (SL[Trial])/00 - Metadata.lua
+++ .contrib/Parser/DATAS/12 - Item Sets/Class Trials/05 - Level 50 (SL[Trial])/00 - Metadata.lua
@@ -6,7 +6,7 @@ _.GearSets =
 {
 	n(CLASS_TRIAL, {
 		n(LEVEL_FIFTY_TRIAL, {
-			-- Note: [As of November 11th 48 Trials became 50 Trials with a mix of ids from 48 Trials and 50 (SL Char Boost)]
+			-- Note: [As of November 11th, 2021 48 Trials became 50 Trials with a mix of ids from 48 Trials and 50 (SL Char Boost)]
 			["description"] = "These are gained by creating a Level 50 Trial of that particular class.  Each class has one default spec except Shamans, Druids and Hunters.",
 			["icon"] = "Interface\\Icons\\ability_bossmagistrix_timewarp1",
 			["lvl"] = { 50 },
--- .contrib/Parser/DATAS/12 - Item Sets/Class Trials/07 - Level 60 (SL[Char Boost])/00 - Metadata.lua
+++ .contrib/Parser/DATAS/12 - Item Sets/Class Trials/07 - Level 60 (SL[Char Boost])/00 - Metadata.lua
@@ -6,7 +6,7 @@ _.GearSets =
 {
 	n(CLASS_TRIAL, {
 		n(LEVEL_SIXTY, {
-			-- Note: [As of November 11th SL Character boost increased from 50 to 60. Some of the old 50 items went to the new class trial level 50. It's still unknown how much of the boosted gear is available to collect]
+			-- Note: [As of November 11th, 2021 SL Character boost increased from 50 to 60. Some of the old 50 items went to the new class trial level 50. It's still unknown how much of the boosted gear is available to collect]
 			["description"] = "These are gained by using a Boost of that particular class.  Each class has one default spec.  Shamans and Druids have two specs available and both are needed to 100% each category.",
 			["lvl"] = 60,
 		}),
