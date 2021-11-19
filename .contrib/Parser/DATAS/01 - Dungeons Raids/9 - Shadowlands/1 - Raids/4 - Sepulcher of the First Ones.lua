104942af3a261c6ade621e3241539a36b58114f8
gbrage <gbrage@kth.se>
2021-11-19 14:35:09 +0100

    -- Sepulcher of the First Ones -- Zereth Mortis

---
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/9 - Shadowlands/1 - Raids/4 - Sepulcher of the First Ones.lua
@@ -0,0 +1,355 @@
+-----------------------------------------------------
+--   D U N G E O N S  &  R A I D S  M O D U L E    --
+-----------------------------------------------------
+--[[
+root("Instances", tier(SL_TIER, {
+	inst(1195, {	-- Sanctum of Domination
+		["isRaid"] = true,
+		["coord"] = { , , },
+		["order"] = "03",
+		["maps"] = {
+			
+		},
+		["g"] = {
+			n(COMMON_BOSS_DROPS, {
+				
+			}),
+			d(17, {	-- LFR
+				n(ZONE_DROPS, {
+				
+				}),
+				ach(, {
+					["collectible"] = false,
+					["g"] = {
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+					},
+				}),
+				ach(, {
+					["collectible"] = false,
+					["g"] = {
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+					},
+				}),
+				ach(, {
+					["collectible"] = false,
+					["g"] = {
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+					},
+				}),
+				ach(, {
+					["collectible"] = false,
+					["g"] = {
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+						e(, {	-- 
+							["crs"] = { },	--
+							["g"] = {
+								crit(, {	-- 
+									["achievementID"] = ,	-- 
+								}),
+							},
+						}),
+					},
+				}),
+			}),
+			d(14, {	-- Normal
+				n(QUESTS, {
+				
+				}),
+				n(ZONE_DROPS, {
+					
+				}),
+				e(2512, {	-- Solitary Guardian
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2553, {	-- Artificer Xy'mox
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2540, {	-- Dausegne, the Fallen Oracle
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2544, {	-- Prototype Pantheon
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2539, {	-- Lihuvim, Principal Architect
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2542, {	-- Skolex, the Insatiable Ravener
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2529, {	-- Halondrus the Reclaimer
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2546, {	-- Anduin Wrynn
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2543, {	-- Lords of Dread
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2549, {	-- Rygelon
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2537, {	-- The Jailer
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+			}),
+			d(15, {	-- Heroic
+				n(QUESTS, {
+				
+				}),
+				n(ZONE_DROPS, {
+					
+				}),
+				e(2512, {	-- Solitary Guardian
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2553, {	-- Artificer Xy'mox
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2540, {	-- Dausegne, the Fallen Oracle
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2544, {	-- Prototype Pantheon
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2539, {	-- Lihuvim, Principal Architect
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2542, {	-- Skolex, the Insatiable Ravener
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2529, {	-- Halondrus the Reclaimer
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2546, {	-- Anduin Wrynn
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2543, {	-- Lords of Dread
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2549, {	-- Rygelon
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2537, {	-- The Jailer
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+			}),
+			d(16, {	-- Mythic
+				n(QUESTS, {
+				
+				}),
+				n(ZONE_DROPS, {
+					
+				}),
+				e(2512, {	-- Solitary Guardian
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2553, {	-- Artificer Xy'mox
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2540, {	-- Dausegne, the Fallen Oracle
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2544, {	-- Prototype Pantheon
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2539, {	-- Lihuvim, Principal Architect
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2542, {	-- Skolex, the Insatiable Ravener
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2529, {	-- Halondrus the Reclaimer
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2546, {	-- Anduin Wrynn
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2543, {	-- Lords of Dread
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2549, {	-- Rygelon
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+				e(2537, {	-- The Jailer
+					["crs"] = {  },	-- 
+					["g"] = {
+						
+					},
+				}),
+			}),
+		},
+	}),
+}));
+--]]
