1116c1450c6fea8f8e2801eb19c567cf26e4feed
ImUnicke <imunicke+github@gmail.com>
2021-11-30 02:16:15 -0700

    Random adjustments that don't affect anything Added a couple Inscription recipes learned from Discovery... [WIP]

--- .contrib/Parser/DATAS/11 - Professions/Inscription/Inscription.lua
+++ .contrib/Parser/DATAS/11 - Professions/Inscription/Inscription.lua
@@ -3,6 +3,25 @@ profession(INSCRIPTION, {
 		i(172450, {	-- Technique: Glyph of Lavish Servings
 			["description"] = "A Mage inscriptionist with 175 skill (BfA) has a chance to create this recipe when conjuring their refreshment table. Other classes must get it from a Mage or the auction house.",
 		}),
+		recipe(167950, {	-- Research: Warbinder's Ink
+			["collectible"] = false,
+			["g"] = {
+				-- TODO: use this ItemID as 'cost' on Glyph recipes which can ONLY be learned from using it
+				i(113992),	-- Scribe's Research Notes
+			},
+		}),
+		recipe(165466, {	-- Research: Blackfallow Ink
+			["collectible"] = false,
+			["g"] = {
+				recipe(112430),	-- Glyph of Burning Anger
+				recipe(112462),	-- Glyph of Crackling Tiger Lightning
+				recipe(56986),	-- Glyph of Crittermorph
+				recipe(64260),	-- Glyph of Disguise
+				recipe(126800),	-- Glyph of Shadowy Friends
+				recipe(124466),	-- Glyph of the Heavens
+			},
+		}),
+		-- TODO: add more research recipes and their associated learnable recipes
 	}),
 	n(DROPS, {
 		q(36239, {	-- A Mysterious Satchel
