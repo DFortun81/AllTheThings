6b79b57f75a9fabe347b5d6fc1a0160e0ca66608
ImUnicke <imunicke+github@gmail.com>
2021-12-26 02:40:40 -0700

    SL mission xp item

--- .contrib/Parser/DATAS/06 - Expansion Features/08 Shadowlands/01 Covenants/02 Covenant Sanctums.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/08 Shadowlands/01 Covenants/02 Covenant Sanctums.lua
@@ -88,6 +88,9 @@ _.ExpansionFeatures =
 						i(188650, {	-- Grimoire of Knowledge
 							["timeline"] = { "added 9.1.5" },
 						}),
+						i(188651, {	-- Grimoire of Knowledge (Drops from the early Campaign Missions) [Night Fae as noticed]
+							["timeline"] = { "added 9.1.5" },
+						}),
 						-- Mounts
 						i(183617),	-- Chittering Animite
 						i(183052),	-- Darkwarren Hardshell
