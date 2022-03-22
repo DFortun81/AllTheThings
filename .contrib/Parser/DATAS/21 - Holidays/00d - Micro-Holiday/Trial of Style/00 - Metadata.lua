03c127500242ab72498776dd64cd9a9697d122fa
Myrhial <8618272+ninaarens@users.noreply.github.com>
2022-03-22 19:09:29 +0100

    Reworked Trial of Style to use costs

--- .contrib/Parser/DATAS/21 - Holidays/00d - Micro-Holiday/Trial of Style/00 - Metadata.lua
+++ .contrib/Parser/DATAS/21 - Holidays/00d - Micro-Holiday/Trial of Style/00 - Metadata.lua
@@ -7,22 +7,25 @@ _.Holidays =
 		["u"] = MICRO_HOLIDAY,
 		["mapID"] = -777,
 		["groups"] = {
-			i(151134, {	-- Trial of Style Token
-				["u"] = MICRO_HOLIDAY,
-				["crs"] = {
-					54473,	-- Warpweaver Dushar <Transmogrifier> (Horde / Orgrimmar)
-					142068,	-- Warpweaver Ezil <Transmogrifier> (Alliance / Boralus)
-					85289,	-- Warpweaver Farshlah <Transmogrifier> (Horde / Frostwall)
-					54442,	-- Warpweaver Hashom <Transmogrifier> (Alliance / Stormwind)
-					142068,	-- Warpweaver Hayri <Transmogrifier> (Horde / Dazar'alor)
-					86395,	-- Warpweaver Jasor <Transmogrifier> (Horde / Warspear)
-					64573,	-- Warpweaver Ramahesh <Transmogrifier> (Alliance / Shrine of Seven Stars)
-					67014,	-- Warpweaver Shafiee <Transmogrifier> (Horde / Shrine of Two Moons)
-					85961,	-- Warpweaver Shal <Transmogrifier> (Alliance / Stormshield)
-					133196,	-- Warpweaver Sle'shal <Token Exchange> (Inside Trial of Style)
-					99867,	-- Warpweaver Tuviss <Transmogrifier> (Neutral / Dalaran)
-					85291,	-- Warpweaver Tuviss <Transmogrifier> (Alliance / Lunarfall)
-				},
+			n(REWARDS, {
+				i(151134, {	-- Trial of Style Token
+					["u"] = MICRO_HOLIDAY,
+					["crs"] = {
+						54473,	-- Warpweaver Dushar <Transmogrifier> (Horde / Orgrimmar)
+						142068,	-- Warpweaver Ezil <Transmogrifier> (Alliance / Boralus)
+						85289,	-- Warpweaver Farshlah <Transmogrifier> (Horde / Frostwall)
+						54442,	-- Warpweaver Hashom <Transmogrifier> (Alliance / Stormwind)
+						142068,	-- Warpweaver Hayri <Transmogrifier> (Horde / Dazar'alor)
+						86395,	-- Warpweaver Jasor <Transmogrifier> (Horde / Warspear)
+						64573,	-- Warpweaver Ramahesh <Transmogrifier> (Alliance / Shrine of Seven Stars)
+						67014,	-- Warpweaver Shafiee <Transmogrifier> (Horde / Shrine of Two Moons)
+						85961,	-- Warpweaver Shal <Transmogrifier> (Alliance / Stormshield)
+						133196,	-- Warpweaver Sle'shal <Token Exchange> (Inside Trial of Style)
+						156663,	-- Warpweaver Ta'oren <Transmogrifier> (Neutral / Oribos)
+						99867,	-- Warpweaver Tuviss <Transmogrifier> (Neutral / Dalaran)
+						85291,	-- Warpweaver Tuviss <Transmogrifier> (Alliance / Lunarfall)
+					},
+				}),
 			}),
 			n(133164, {	-- Nellie Voguesong <Shirts and Tabards>
 				["sym"] = {
--- .contrib/Parser/DATAS/21 - Holidays/00d - Micro-Holiday/Trial of Style/Season 3.lua
+++ .contrib/Parser/DATAS/21 - Holidays/00d - Micro-Holiday/Trial of Style/Season 3.lua
@@ -4,20 +4,19 @@
 _.Holidays =
 {
 	holiday(1671631, {	-- Trial of Style
-		i(151134, {	-- Trial of Style Token
-			["u"] = MICRO_HOLIDAY,
-			["groups"] = {
-				{	-- Season 3
-					["setHeaderID"] = 379,	-- Season 3
-					["icon"] = "Interface\\Icons\\inv_misc_scrollrolled02c",
-					["groups"] = {
-						i(164378, {	-- Letter from an Admirer
-							["description"] = "Cost: 100 Trial of Style Tokens\nTrial of Style must be active to use this item",
-							["u"] = MICRO_HOLIDAY,
-						}),
-					},
+		["u"] = MICRO_HOLIDAY,
+		["groups"] = {
+			{	-- Season 3
+				["setHeaderID"] = 379,	-- Season 3
+				["icon"] = "Interface\\Icons\\inv_misc_scrollrolled02c",
+				["groups"] = {
+					i(164378, {	-- Letter from an Admirer
+						["cost"] = { { "i", 151134, 100 } },	-- Trial of Style Token x100
+						["description"] = "Trial of Style must be active to use this item",
+						["u"] = MICRO_HOLIDAY,
+					}),
 				},
 			},
-		}),
+		},
 	}),
 };
