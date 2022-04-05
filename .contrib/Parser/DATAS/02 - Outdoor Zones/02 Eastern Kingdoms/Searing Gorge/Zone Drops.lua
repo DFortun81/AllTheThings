7835446bbdc8d2b48400381c70c066c580d8dabc
Gold <goldenshacal@gmail.com>
2022-04-05 18:21:24 +0200

    saering gorge recipe coords

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Searing Gorge/Zone Drops.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Searing Gorge/Zone Drops.lua
@@ -13,9 +13,11 @@ _.Zones =
 				}),
 				n(47463, {	-- Searing Flamewraith
 					["description"] = "To get the Blacksmithing plans for Blazing Rapier, kill Searing Flamewraiths until you get Lava Oil. Do not use it yet. Go to the metal bridge before the Quarry Gate. The Time Lost Chest is in the lava below in the alcove. Use the Fire Oil, jump down, open the chest, and hearth.",
+					["coord"] = { 48.0, 42.3, SEARING_GORGE },
 					["g"] = {
 						i(142359, {	-- Lava Oil
 							o(266289, {	-- Time Lost Chest
+								["coord"] = { 37.0, 48.9, SEARING_GORGE },
 								["modelScale"] = 2,
 								["modelID"] = 446679,
 								["g"] = {
@@ -28,10 +30,18 @@ _.Zones =
 				i(14476, {	-- Pattern: Cindercloth Gloves
 					-- Started dropping again in 8.2.  The new version (167887) is also available from Time-Lost Trader
 					["crs"] = { 5861 },	-- Twilight Fire Guard
+					["coord"] = { 
+						{ 19.4, 36.6, SEARING_GORGE },
+						{ 25.4, 25.4, SEARING_GORGE },
+					},
 				}),
 				i(14471, {	-- Pattern: Cindercloth Vest
 					-- Started dropping again in 8.2.  The new version (167887) is also available from Time-Lost Trader
 					["crs"] = { 5861 },	-- Twilight Fire Guard
+					["coord"] = { 
+						{ 19.4, 36.6, SEARING_GORGE },
+						{ 25.4, 25.4, SEARING_GORGE },
+					},
 				}),
 				i(21547, {	-- Recipe: Elixir of Greater Firepower
 					["crs"] = {
