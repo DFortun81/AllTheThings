47c2a250e82cff05cacb2a51a738e4924fd92d72
gbrage <gbrage@kth.se>
2022-04-09 02:23:56 +0200

    -- Just for testing

--- .contrib/Parser/DATAS/17 - NYI/NYI Quests.lua
+++ .contrib/Parser/DATAS/17 - NYI/NYI Quests.lua
@@ -2943,7 +2943,7 @@ root("NeverImplemented", bubbleDown({["u"] = NEVER_IMPLEMENTED}, {
 			})),
 
 			-- 9.2.5
-			tier(SL_TIER, 2.5, bubbleDown({ ["timeline"] = { "created 9.2.5" } }, {
+			tier(SL_TIER, 2.5, bubbleDown({ ["timeline"] = { "created 9.2.0" } }, {
 				-- 9.2.5.42850
 				q(63479),	-- Summons from the Matriarch
 				q(63505),	--
