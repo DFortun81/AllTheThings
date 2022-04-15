b668785055fa5f4b20c58ab20598d153a33564a9
ImUnicke <imunicke+github@gmail.com>
2022-04-15 03:11:24 -0600

    Dynamic Profession symlink now works in Retail and excludes the main Profession header which gets pulled in from the symlink

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -11621,11 +11621,10 @@ local fields = {
 	["requireSkill"] = function(t)
 		return t.professionID;
 	end,
-	--[[
 	["sym"] = function(t)
-		return {{"selectprofession", t.professionID}};
-	end
-	]]--
+		return {{"selectprofession", t.professionID},
+				{"not","headerID",-38}};	-- Ignore the Main Professions header that will get pulled in
+	end,
 };
 app.BaseProfession = app.BaseObjectFields(fields, "BaseProfession");
 app.CreateProfession = function(id, t)
