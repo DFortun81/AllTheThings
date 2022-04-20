e2e4fb47f6e21ed20f0d457ea3fb62e953d3f3dd
Jens <jens.rogier@gmail.com>
2022-04-20 03:16:48 +0200

    Some minor Un'Goro & Ashenvale fixes (#962)
    * Reverse Blacksmithing is not alliance only
    
    * NPC 'Harlown Darkweave has 2 id's. One while he's bound before doing the quests 6731) and one when he's free (34601).
    ID hasn't been removed in 4.0.3.
    
    * Some changes suggested in https://github.com/DFortun81/AllTheThings/pull/962#issuecomment-1086967503

--- .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Ashenvale/Ashenvale.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Ashenvale/Ashenvale.lua
@@ -2416,17 +2416,21 @@ root("Zones", m(KALIMDOR, {
 						i(6054),	-- Recipe: Shadow Protection Potion
 					},
 				})),
-				n(6731, bubbleDownSelf({ ["timeline"] = { "added 1.11.1.7318", "removed 4.0.3.10000" } }, {	-- Harlown Darkweave <Leatherworking Supplies>
-					["coord"] = { 18.2, 60.0, ASHENVALE },
-					["races"] = ALLIANCE_ONLY,
-					["groups"] = {
-						i(7361),	-- Pattern: Herbalist's Gloves
-					},
-				})),
-				n(34601, bubbleDownSelf({ ["timeline"] = { "added 4.0.3.13277" } }, {	-- Harlown Darkweave
+				n(6731, bubbleDownSelf({ ["timeline"] = { "added 1.11.1.7318" } }, {	-- Harlown Darkweave <Leatherworking Supplies>
 					["description"] = "Vendor will only sell to those who have rescued him. Go to Benjari Edune and pick up the quest |cFFFFD700Three Friends of the Forest|r to start the quest chain.",
 					["coord"] = { 18.2, 60.0, ASHENVALE },
 					["races"] = ALLIANCE_ONLY,
+					-- #IF BEFORE CATA
+					["groups"] = {
+						i(7361),	-- Pattern: Herbalist's Gloves
+					},
+					-- #ENDIF
+				})),
+				n(34601, bubbleDownSelf({ ["timeline"] = { "added 4.0.3.13277" } }, {	-- Harlown Darkweave <Leatherworking Supplies>
+					["sourceQuest"] = 13982,	-- in a Bind
+					["coord"] = { 18.2, 60.0, ASHENVALE },
+					["races"] = ALLIANCE_ONLY,
+					["crs"] = { 6731 }, -- Harlown Darkweave
 					["g"] = {
 						i(7361, {	-- Pattern: Herbalist's Gloves
 							["isLimited"] = true,
