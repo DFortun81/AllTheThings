4c0aaee41810d22bac761d9a922a3e6d6eaeaccd
ImUnicke <imunicke+github@gmail.com>
2022-05-03 14:06:50 -0600

    Added current TW weekly Cache reward

--- .contrib/Parser/DATAS/17 - NYI/NYI.lua
+++ .contrib/Parser/DATAS/17 - NYI/NYI.lua
@@ -3235,7 +3235,6 @@ root("NeverImplemented", bubbleDown({["u"] = NEVER_IMPLEMENTED}, {
 			tier(SL_TIER, 2.0, {
 				i(189452),	-- 9.2 Mount Crafting Bag of Goodies
 				i(189428),	-- 9.2 Pet Battle Playtest Bag of Goodies
-				i(191040),	-- Cache of Sepulcher Treasures
 				i(190654),	-- Cache of Sepulcher Treasures
 				i(188796),	-- Cypher Test Item
 				i(190823),	-- Firim's Mysterious Cache
