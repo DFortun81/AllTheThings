17bd645b575301421f56d7871362feb5c4db9937
Crieve <dylanfortune@live.com>
2022-06-01 18:02:22 -0700

    Reparse!

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Blasted Lands.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Blasted Lands.lua
@@ -8,7 +8,7 @@ root("Zones", m(EASTERN_KINGDOMS, {
 		-- #endif
 		["lore"] = "The Blasted Lands bear that name for a reason: they were twisted by the magic that brought the Dark Portal into being, spewing the rampaging Horde into Azeroth.\n\nThis wasted desert of red clay is home to the original Dark Portal. The portal still stands, and leads to Outland — the remains of the orcs’ sundered homeworld, Draenor. Nethergarde Keep, a Stormwind fortress of dour mages and paladins, keeps watch over the portal and the demons and ogres that would abuse its power.\n\nNow, the wretched land hosts few inhabitants and no significant settlements. Magic that leaked through the Dark Portal warped the Blasted Lands, leaving an infertile desert landscape. Hardy desert species like Basilisks, Scorpions and Hyenas have since colonized the area. The red rocks of the region do not come by their color naturally. The original tan color was transmuted into the unearthly crimson by searing heat and chaotic energies when the Dark Portal was destroyed.",
 		-- #if AFTER WRATH
-		["achievementID"] = "Interface\\Icons\\achievement_zone_blastedlands_01",
+		["icon"] = "Interface\\Icons\\achievement_zone_blastedlands_01",
 		-- #endif
 		["groups"] = {
 			n(ACHIEVEMENTS, {
