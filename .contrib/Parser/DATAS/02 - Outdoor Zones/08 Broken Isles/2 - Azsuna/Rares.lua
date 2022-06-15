68a4efcc3f6949974cebf4284aae7aa805eec6d5
gbrage <gbrage@kth.se>
2022-06-15 15:50:49 +0200

    -- Small fix

--- .contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/2 - Azsuna/Rares.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/2 - Azsuna/Rares.lua
@@ -8,11 +8,11 @@ _.Zones =
 		m(AZSUNA, {
 			n(RARES, {
 				n(SPECIAL, sharedData({["description"] = "Can be made hostile by Demon Hunters using 'Spectral Sight' or Paladins wielding 'Truthguard' allowing anyone to get credit."},{
-                    n(109028, { -- Horkus
-                        ["questID"] = 42825,
-                        ["coord"] = { 56.2, 59.6, AZSUNA },
-                        ["crs"] = { 109029 },    -- Horkus
-                    }),
+					n(109028, { -- Horkus
+						["questID"] = 42825,
+						["coord"] = { 56.2, 59.6, AZSUNA },
+						["crs"] = { 109029 },    -- Horkus
+					}),
                 })),
 				n(107657, {	-- Arcanist Shal'iman
 					["questID"] = 42505,
