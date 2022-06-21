a321e67b3ff149d505b98bfb5a978ed9babf85fe
Crieve <dylanfortune@live.com>
2022-06-21 00:02:59 -0700

    Updated the Kalimdor metafile for classic.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/0 - Metadata.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/0 - Metadata.lua
@@ -1,11 +1,14 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
-
-_.Zones = {
+root("Zones", {
 	m(KALIMDOR, {
-		["achievementID"] = 43,
 		["lore"] = "Kalimdor is home to the night elves, orcs, tauren, trolls, and draenei. Other races present include the ogres, centaur, naga, demons, and other, more minor races.",
+		-- #if AFTER WRATH
 		["icon"] = "Interface\\Icons\\achievement_zone_kalimdor_01",
+		-- #endif
+		-- #if ANYCLASSIC
+		["maps"] = { 1464 },
+		-- #endif
 	}),
-};
+});
