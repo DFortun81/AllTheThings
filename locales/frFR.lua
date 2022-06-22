9958056878bc88a7e219c4b26825d653ef422e70
Machou <Machou@users.noreply.github.com>
2022-06-22 18:01:21 +0200

    Update frFR.lua (#1031)

--- locales/frFR.lua
+++ locales/frFR.lua
@@ -7,17 +7,17 @@ local L = app.L;
 	--TODO: L.DESCRIPTION = "\"Foolishly you have sought your own demise. Brazenly you have disregarded powers beyond your understanding. You have fought hard to invade the realm of the Collector. Now there is only one way out - To walk the lonely path... of the damned.\"";
 	--TODO: L.THINGS_UNTIL = " THINGS UNTIL ";
 	--TODO: L.THING_UNTIL = " THING UNTIL ";
-	--TODO: L.YOU_DID_IT = "YOU DID IT!|r";
+	L.YOU_DID_IT = "VOUS L’AVEZ FAIT !|r";
 
 -- Big new chunk from AllTheThings.lua
-	--TODO: L.PROGRESS = "Progress";
+	L.PROGRESS = "Progression";
 	--TODO: L.TRACKING_PROGRESS = "Tracking Progress";
 	--TODO: L.COLLECTED_STRING = " Collected";
-	--TODO: L.PROVIDERS = "Provider(s)";
+	L.PROVIDERS = "Fournisseur(s)";
 	--TODO: L.COLLECTION_PROGRESS = "Collection Progress";
 	--TODO: L.CONTAINS = "Contains:";
-	--TODO: L.FACTIONS = "Factions";
-	--TODO: L.COORDINATES_STRING = "Coordinates";
+	L.FACTIONS = "Factions";
+	L.COORDINATES_STRING = "Coordonnées";
 	--TODO: L.FLIGHT_PATHS = "Flight Paths";
 	--TODO: L.KNOWN_BY = "Known by ";
 	--TODO: L.REQUIRES = "Requires";
@@ -27,7 +27,7 @@ local L = app.L;
 	--TODO: L.NOT_AVAILABLE_IN_PL = "Not available in Personal Loot.";
 	--TODO: L.MARKS_OF_HONOR_DESC = "Marks of Honor must be viewed in a Popout window to see all of the normal 'Contains' content.\n(Type '/att ' in chat then Shift-Click to link the item)\n\n|cFFfe040fAfter purchasing and using an ensemble, relogging & a forced ATT refresh (in this order)\nmay be required to register all the items correctly.|r";
 	--TODO: L.ITEM_GIVES_REP = "Provides Reputation with '";
-	--TODO: L.COST = "Cost";
+	L.COST = "Coût";
 	--TODO: L.COST_DESC = "This contains the visual breakdown of what is required to obtain or purchase this Thing";
 	--TODO: L.SOURCES = "Source(s)";
 	--TODO: L.SOURCES_DESC = "Shows the Source of this Thing.\n\nParticularly, a specific Vendor/NPC, Quest, Encounter, etc.";
@@ -56,7 +56,7 @@ local L = app.L;
 	--TODO: L.LEGION_TIER_DESC = "Legion is the sixth expansion. Gul'dan is expelled into Azeroth to reopen the Tomb of Sargeras & the gateway to Argus, commencing the third invasion of the Burning Legion. After the defeat at the Broken Shore, the defenders of Azeroth search for the Pillars of Creation, which were Azeroth's only hope for closing the massive demonic portal at the heart of the Tomb. However, the Broken Isles came with their own perils to overcome, from Xavius, to God-King Skovald, to the nightborne, & to Tidemistress Athissa. Khadgar moved Dalaran to the shores of this land, the city serves as a central hub for the heroes. The death knights of Acherus also took their floating necropolis to the Isles. The heroes of Azeroth sought out legendary artifact weapons to wield in battle, but also found unexpected allies in the form of the Illidari. Ongoing conflict between the Alliance & the Horde led to the formation of the class orders, with exceptional commanders putting aside faction to lead their classes in the fight against the Legion.";
 	--TODO: L.BFA_TIER_DESC = "Battle for Azeroth is the seventh expansion. Azeroth paid a terrible price to end the apocalyptic march of the Legion's crusade—but even as the world's wounds are tended, it is the shattered trust between the Alliance and Horde that may prove the hardest to mend. In Battle for Azeroth, the fall of the Burning Legion sets off a series of disastrous incidents that reignites the conflict at the heart of the Warcraft saga. As a new age of warfare begins, Azeroth's heroes must set out on a journey to recruit new allies, race to claim the world's mightiest resources, and fight on several fronts to determine whether the Horde or Alliance will lead Azeroth into its uncertain future.";
 	--TODO: L.SL_TIER_DESC = "Shadowlands is the eighth expansion. What lies beyond the world you know? The Shadowlands, resting place for every mortal soul—virtuous or vile—that has ever lived.";
-	L.DF_TIER_DESC = "Dragonflight is the ninth expansion. Les Vols draconiques d’Azeroth sont de retour pour défendre leur foyer ancestral, les îles aux Dragons. Débordantes de magie élémentaire et de l’essence vitale d’Azeroth, les îles s’éveillent de nouveau et vous invitent à découvrir leurs merveilles primordiales et leurs secrets longtemps oubliés.";	--TODO: First sentence
+	L.DF_TIER_DESC = "Dragonflight est la 9ème extension. Les Vols draconiques d’Azeroth sont de retour pour défendre leur foyer ancestral, les îles aux Dragons. Débordantes de magie élémentaire et de l’essence vitale d’Azeroth, les îles s’éveillent de nouveau et vous invitent à découvrir leurs merveilles primordiales et leurs secrets longtemps oubliés.";
 	--TODO: L.TITLES_DESC = "Titles are tracked across your account, however, your individual character must qualify for certain titles to be usable on that character.";
 	--TODO: L.SHARED_APPEARANCES_LABEL = "Shared Appearances";
 	--TODO: L.SHARED_APPEARANCES_LABEL_DESC = "The items in this list are shared appearances for the above item. In Unique Appearance Mode, this list can help you understand why or why not a specific item would be marked Collected.";
@@ -80,9 +80,9 @@ local L = app.L;
 	--TODO: L._WITH_ = " with ";
 	--TODO: L.MAXIMUM_STANDING = "Requires a standing lower than";
 	--TODO: L.MIN_MAX_STANDING = "Requires a standing between";
-	--TODO: L.AND_ = "And ";
-	--TODO: L._AND = " and";
-	--TODO: L._MORE = " more";
+	L.AND_ = "Et ";
+	L._AND = " et";
+	L._MORE = " plus";
 	--TODO: L._OTHER_SOURCES = " other sources";
 	--TODO: L.DURING_WQ_ONLY = "This can be completed when the world quest is active.";
 	--TODO: L.COMPLETED_DAILY = "This can be completed daily.";
@@ -740,10 +740,10 @@ for key,value in pairs({
 	-- Operation: Mechagon
 		--TODO: [-379] = "Junkyard",								-- Junkyard
 	-- Icecrown Citadel
-		--TODO: [-393] = "Storming the Citadel",					-- Storming the Citadel
-		--TODO: [-394] = "The Plagueworks",							-- The Plagueworks
+		[-393] = "La prise de la citadelle",						-- Storming the Citadel
+		[-394] = "La Pesterie",										-- The Plagueworks
 	-- BFA Outposts
-		--TODO: [-397] = "Outposts",								-- Outposts
+		[-397] = "Avant-postes",									-- Outposts
 	-- 5.3
 		[-398] = "Champ de bataille : les Tarides",					-- Battlefield: Barrens
 	-- T0.5 & Classic
@@ -757,7 +757,7 @@ for key,value in pairs({
 	-- Misc
 		--TODO: [-493] = "Broken Mission Loot",						-- Broken Mission Loot
 	-- Blizzard Events and Anniversaries
-		--TODO: [-519] = "World Events",							-- World Events
+		[-519] = "Événements mondiaux",								-- World Events
 		--TODO: [-520] = "Expansion Pre-Launch",					-- Expansion Pre-Launch
 		--TODO: [-522] = EXPANSION_NAME2..": Zombie Infestation",	-- Wrath of the Lich King: Zombie Infestation
 		--TODO: [-523] = EXPANSION_NAME3..": Elemental Unrest",		-- Cataclysm: Elemental Unrest
@@ -813,15 +813,15 @@ for key,value in pairs({
 	-- Chests
 		[-851] = "Cache de l’Empire noir",							-- Black Empire Cache
 	-- Heart of Azeroth
-		--TODO: [-853] = "All Roles",								-- All Roles
+		[-853] = "Tous les Rôles",									-- All Roles
 		--TODO: [-854] = "DPS",										-- DPS
-		--TODO: [-855] = "Healers",									-- Healers
+		[-855] = "Soigneurs",										-- Healers
 		--TODO: [-856] = "Tanks",									-- Tanks
-		--TODO: [-857] = "Mythic+",									-- Mythic+
-		--TODO: [-858] = "Rank 1",									-- Rank 1
-		--TODO: [-859] = "Rank 2",									-- Rank 2
-		--TODO: [-860] = "Rank 3",									-- Rank 3
-		--TODO: [-861] = "Rank 4",									-- Rank 4
+		[-857] = "Mythique+",										-- Mythic+
+		[-858] = "Rang 1",											-- Rank 1
+		[-859] = "Rang 2",											-- Rank 2
+		[-860] = "Rang 3",											-- Rank 3
+		[-861] = "Rang 4",											-- Rank 4
 	-- Shadowlands Header
 		--TODO: [-903] = "Zone Rewards",							-- Zone Rewards
 		[-907] = "Feu Blanchette",									-- Dead Blanchy
@@ -882,15 +882,15 @@ for key,value in pairs({
 			--TODO: [-5201] = "Class Hall Campaign",
 			[-5202] = "L’équilibre de la puissance",				-- Balance of Power
 			--TODO: [-5203] = "Prestige Rewards",
-			--TODO: [-5204] = "Challenge Appearance",
-			--TODO: [-5205] = "Hidden Appearance",
+			[-5204] = "Apparence de Challenge",						-- Challenge Appearance
+			[-5205] = "Apparence Cachée",							-- Hidden Appearance
 		-- Class Sets
 			--TODO: [-5350] = "Class Trial Item Sets",				-- Class Trial
 
 		--TODO: [-7776] = "Winter Revelers",						-- Winter Revelers (for Winter Veil)
 	------ ACHIEVEMENT HEADERS SECTION ------
-		--TODO: [-10071] = "Visions of N'Zoth",
-		--TODO: [-10072] = "N'Zoth Assault",
+		[-10071] = "Visions de N'Zoth",								-- Visions of N'Zoth
+		[-10072] = "Assaut de N'Zoth Assault",						-- N'Zoth Assault
 		[-10075] = "Assaut : Avancée amathet",						-- Assault: Amathet Advance
 		[-10076] = "Assaut : L’Empire noir",						-- Assault: The Black Empire
 		[-10077] = "Assaut : La guerre des clans",					-- Assault: The Warring Clans
@@ -1082,7 +1082,7 @@ for key,value in pairs({
 	[179832] = "Oreiller brodé de Pincetaie",	-- Pillaclencher's Ornate Pillow
 	[180229] = "Pile de déchets",	-- Jinxed Hoodoo Pile
 	[180248] = "Banc de courbines",	-- School of Tastyfish
-	--TODO: [180327] = "Brazier of Madness",	-- Brazier of Madness
+	[180327] = "Brasero de la Folie",	-- Brazier of Madness
 	[180366] = "Boîte d'appâts abîmée",	-- Battered Tackle Box
 	[180368] = "Tablette de la folie",	-- Tablet of Madness
 	[180448] = "Avis de recherche : Poigne-de-mort",	-- Wanted Poster: Deathclasp
@@ -2687,7 +2687,7 @@ for key,value in pairs({
 	[329805] = "Cristal étrange",	-- Strange Crystal
 	[329918] = "Trésors hurans",	-- Quilboar Treasures
 	[329919] = "Fournitures volées",	-- Stolen Supplies
-	--TODO: [330168] = "Contained Alemental",	-- Contained Alemental
+	[330168] = "Elémenbière contenu",	-- Contained Alemental	--TODO: This was manually translated
 	[330169] = "Rouage navrebière",	-- Direbrew Cog
 	[330170] = "Totem hozen",	-- Hozen Totem
 	[330183] = "Coffret des Lamineurs",	-- Irontide Lockbox
@@ -3160,7 +3160,7 @@ for key,value in pairs({
 	--TODO: [9999890] = "Corrupted Loot",	-- Corrupted Loot
 	--TODO: [9999891] = "Main Objective Only",	-- Main Objective Only
 	--TODO: [9999892] = "Main Objective + 2 Bonus Areas",	-- Main Objective + 2 Bonus Areas
-	--TODO: [9999893] = "Full Clear",	-- Full Clear
+	 [9999893] = "Nettoyage complet",	-- Full Clear
 	--TODO: [9999894] = "Full Clear + 1 Masks",	-- Full Clear + 1 Masks
 	--TODO: [9999895] = "Full Clear + 2 Masks",	-- Full Clear + 2 Masks
 	--TODO: [9999896] = "Full Clear + 3 Masks",	-- Full Clear + 3 Masks
@@ -3179,9 +3179,9 @@ for key,value in pairs({
 	--TODO: [9999918] = "|cFFFFFFFFStep 11:|r Metal Gate",	-- |cFFFFFFFFStep 11:|r Metal Gate
 	--TODO: [9999919] = "|cFFFFFFFFStep 12:|r Wisdom Cube",	-- |cFFFFFFFFStep 12:|r Wisdom Cube
 	--TODO: [9999920] = "|cFFFFFFFFStep 13:|r Tarnished Plaque",	-- |cFFFFFFFFStep 13:|r Tarnished Plaque
-	--TODO: [9999921] = "Placement",	-- Placement
-	--TODO: [9999938] = "Uuna's World Tour",	-- Uuna's World Tour
-	--TODO: [9999946] = "Summon Baa'l",	-- Summon Baa'l
+	[9999921] = "Placement",	-- Placement
+	[9999938] = "Tour du monde d’Uuna",	-- Uuna's World Tour
+	[9999946] = "Invoquer Baa'l",	-- Summon Baa'l
 	--TODO: [13000000] = "|cFFFFFFFFStep 1:|r Purchase Talisman of True Treasure Tracking",	-- |cFFFFFFFFStep 1:|r Purchase Talisman of True Treasure Tracking
 	--TODO: [13000001] = "|cFFFFFFFFStep 2:|r Equip Talisman",	-- |cFFFFFFFFStep 2:|r Equip Talisman
 	--TODO: [13000002] = "|cFFFFFFFFItem 1:|r Scintillating Murloc Skin Lotion",	-- |cFFFFFFFFItem 1:|r Scintillating Murloc Skin Lotion
@@ -3212,17 +3212,17 @@ for key,value in pairs({
 	--TODO: [13000027] = "|cFFFFFFFFStep 3:|r Iridescent Shimmerray Skin",	-- |cFFFFFFFFStep 3:|r Iridescent Shimmerray Skin
 	--TODO: [13000028] = "|cFFFFFFFFStep 4:|r Luxurous Luxscale Scale",	-- |cFFFFFFFFStep 4:|r Luxurous Luxscale Scale
 	--TODO: [13000029] = "|cFFFFFFFFStep 5:|r Captured Cavitation Bubble",	-- |cFFFFFFFFStep 5:|r Captured Cavitation Bubble
-	--TODO: [13000030] = "Purchase Red Crystal Monocle",	-- Purchase Red Crystal Monocle
-	--TODO: [13000031] = "Red Crystal Monocle",	-- Red Crystal Monocle
+	[13000030] = "Acheter un Monocle de cristal rouge",	-- Purchase Red Crystal Monocle
+	[13000031] = "Monocle de cristal rouge",	-- Red Crystal Monocle
 	--TODO: [13000032] = "|cFFFFFFFFStep 3:|r Pick a Monocle (Or Don't!)",	-- |cFFFFFFFFStep 3:|r Pick a Monocle (Or Don't!)
 	--TODO: [13000033] = "|cFFFFFFFFStep 4:|r Suramar Beams",	-- |cFFFFFFFFStep 4:|r Suramar Beams
 	--TODO: [13000034] = "|cFFFFFFFFStep 5:|r Cat Code",	-- |cFFFFFFFFStep 5:|r Cat Code
 	--TODO: [13000035] = "|cFFFFFFFFStep 6:|r Jumping Puzzle",	-- |cFFFFFFFFStep 6:|r Jumping Puzzle
 	--TODO: [13000036] = "|cFFFFFFFFStep 7:|r Arcane Lava",	-- |cFFFFFFFFStep 7:|r Arcane Lava
 	--TODO: [13000037] = "|cFFFFFFFFStep 8:|r Hivemind",	-- |cFFFFFFFFStep 8:|r Hivemind
-	--TODO: [1278968766] = "Rune",	-- Rune
-	--TODO: [1278968767] = "Rune",	-- Rune
-	--TODO: [1278968768] = "Rune",	-- Rune
+	[1278968766] = "Rune",	-- Rune
+	[1278968767] = "Rune",	-- Rune
+	[1278968768] = "Rune",	-- Rune
 })
 do a[key] = value; end
 
