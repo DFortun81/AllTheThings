803de89db115d309c97c38dc851fa0bd03ed221b
Crieve <dylanfortune@live.com>
2022-08-08 03:05:31 -0700

    The new death knight starting area in icecrown no longer appears in the Wrath Classic build.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/04 Northrend/Death Knight Starting Zone.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/04 Northrend/Death Knight Starting Zone.lua
@@ -1,15 +1,16 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
+-- #if AFTER 8.3.0.33062
 root("Zones", {
 	m(NORTHREND, {
 		m(1602, {	-- Icecrown (DK starting zone)
 			n(QUESTS, {
-				q(58877, {	-- Death's Power Grows
+				q(58877, bubbleDown({ ["timeline"] = { "added 8.3.0.33062" } }, {	-- Death's Power Grows
+					["qg"] = 163016,	-- The Lich King
 					["coord"] = { 49.5, 90.7, 1602 },
 					["classes"] = { DEATHKNIGHT },
-					["qg"] = 163016,	-- The Lich King
-					["g"] = {
+					["groups"] = {
 						i(174488),	-- Freezing Greatsword of the Ebon Blade
 						i(174487),	-- Unholy Greatsword of the Ebon Blade
 						i(174486),	-- Crimson Greatsword of the Ebon Blade
@@ -19,30 +20,36 @@ root("Zones", {
 						i(174659),	-- Crimson Saber of the Ebon Blade
 						i(174662),	-- Burning Saber of the Ebon Blade
 					},
-				}),
-				q(58902, {	-- Defender of Azeroth
-					["coord"] = { 49.5, 90.7, 1602 },
-					["classes"] = { DEATHKNIGHT },
+				})),
+				q(58902, {	-- Defender of Azeroth (A)
 					["qg"] = 163016,	-- The Lich King
 					["sourceQuest"] = 58877,	-- Death's Power Grows
-				}),
-				q(58903, {	-- Defender of Azeroth
 					["coord"] = { 49.5, 90.7, 1602 },
+					["timeline"] = { "added 8.3.0.33062" },
 					["classes"] = { DEATHKNIGHT },
+					["races"] = ALLIANCE_ONLY,
+				}),
+				q(58903, {	-- Defender of Azeroth (H)
 					["qg"] = 163016,	-- The Lich King
 					["sourceQuest"] = 58877,	-- Death's Power Grows
+					["coord"] = { 49.5, 90.7, 1602 },
+					["timeline"] = { "added 8.3.0.33062" },
+					["classes"] = { DEATHKNIGHT },
+					["races"] = HORDE_ONLY,
 				}),
 				q(58989, {	-- Runeforging 101
-					["coord"] = { 82.0, 46.3, 648 },	-- Acherus: The Ebon Hold (Lower Level)
-					["classes"] = { DEATHKNIGHT },
 					["qg"] = 29246,	-- Highlord Darion Morgraine
-					["description"] = "Use your Death Gate after finishing previous quest, Darion will be in Acherus.",
 					["sourceQuests"] = {
 						58902,	-- Defender of Azeroth (A)
 						58903,	-- Defender of Azeroth (H)
 					},
+					["coord"] = { 82.0, 46.3, 648 },	-- Acherus: The Ebon Hold (Lower Level)
+					["description"] = "Use your Death Gate after finishing previous quest, Darion will be in Acherus.",
+					["timeline"] = { "added 8.3.0.33062" },
+					["classes"] = { DEATHKNIGHT },
 				}),
 			}),
 		}),
 	}),
 });
+-- #endif
