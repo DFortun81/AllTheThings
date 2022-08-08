8f5fbb885893d92bfe4fad6ec4e1c91493d98fb7
Crieve <dylanfortune@live.com>
2022-08-08 02:40:27 -0700

    Bad format for embedding.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/04 Northrend/0 - Metadata.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/04 Northrend/0 - Metadata.lua
@@ -1,12 +1,10 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
-
-_.Zones =
-{
+root("Zones", {
 	m(NORTHREND, {
-		["achievementID"] = 45,
 		["lore"] = "Northrend is the northern, icy continent of the world of Azeroth, and the source of the evil Scourge. It is also the home of Icecrown Citadel, the seat of the malevolent Lich King.",
 		["icon"] = "Interface\\Icons\\Achievement_Zone_Northrend_01",
+		["achievementID"] = 45,
 	}),
-};
+});
