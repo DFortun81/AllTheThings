1334c8265b0be7e52debd334db07504b49eb066f
Gold <goldenshacal@gmail.com>
2022-11-01 00:06:09 +0100

    new twitch drops

--- .contrib/Parser/DATAS/12 - Promotions/10 Dragonflight Seasonal Promotions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/10 Dragonflight Seasonal Promotions.lua
@@ -3,17 +3,44 @@
 -----------------------------------------------------
 root("Promotions", {
 	n(-584, {	-- Dragonflight Seasonal Promotions
-		["timeline"] = { "added 10.0.1" },
+		["timeline"] = { "added 10.0.2" },
 		["description"] = "These promotions happened during the time Dragonflight was the most recent content between 25th October 2022 & 12nd November 2024.\n\nThey are listed in the order of their first appearance.",
 		["groups"] = {
-		--[[	i(187957, {		-- Encrypted Banner of the Opportune (TOY!)
-				["description"] = "Mythic Dungeon International: SL Season 3\n\nThe Mythic Dungeon International (MDI) returns with its global competitions for its 6th year, pitting the best Mythic Dungeon teams in a head-to-head race to the finish line.\n\nAll registered teams that complete under time the two dungeons within the Proving Grounds will receive the exclusive Encrypted Banner of the Opportune to use in-game!\nSign-ups close 28 March 2022 and The Proving Grounds are on 30 March - 5 April (US).",
+			-- "Pre" Season
+			i(34493, {		-- Dragon Kite
+				["description"] = "Obtained through watching Twitch Streamers with Drops enabled for at least 4 hours between November 15th, 10:00 a.m. & November 17th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your B.Net Account & you have to redeem the drop on Twitch before receiving it in your B.Net Launcher as gift.",
 				["timeline"] = { 
-					"added 9.2.0", 
-					"removed 9.2.0.43114", -- 8th April 2022, 1 day after the event ended.
+					"added 10.0.2", 
+				--	"removed 9.2.0.43114", 		--
 				},	
+				-- ["u"] = REMOVED_FROM_GAME, 	-- After November 17th, 11:59 p.m. PST
 			}),
-		--]]
+			i(79771, {		-- Fel Drake
+				["description"] = "Obtained through watching Twitch Streamers with Drops enabled for at least 4 hours between November 28th, 10:00 a.m. & November 30th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your B.Net Account & you have to redeem the drop on Twitch before receiving it in your B.Net Launcher as gift.",
+				["timeline"] = { 
+					"added 10.0.2", 
+				--	"removed 9.2.0.43114", 		--
+				},	
+				-- ["u"] = REMOVED_FROM_GAME, 	-- After November 30th, 11:59 p.m. PST
+			}),
+			--[[
+			i(34493, {		-- Ichabod	-- no ID yet. Dont forget to add the pets spellID
+				["description"] = "Obtained by gifting an eligible creator’s channel two Twitch subscriptions between November 28th, 10:00 a.m. & December 12th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your B.Net Account & you have to redeem the drop on Twitch before receiving it in your B.Net Launcher as gift.",
+				["timeline"] = { 
+					"added 10.0.2", 
+				--	"removed 9.2.0.43114", 		--
+				},	
+				-- ["u"] = REMOVED_FROM_GAME, 	-- After December 12th, 11:59 p.m. PST
+			}),
+			--]]
+			i(49703, {		-- Perpetual Purple Firework
+				["description"] = "Obtained through watching select Twitch Streamers with Drops enabled for at least 2 hours between December 13th, 10:00 a.m. & December 28th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your B.Net Account & you have to redeem the drop on Twitch before receiving it in your B.Net Launcher as gift.",
+				["timeline"] = { 
+					"added 10.0.2", 
+				--	"removed 9.2.0.43114", 		--
+				},	
+				-- ["u"] = REMOVED_FROM_GAME, 	-- After December 28th, 11:59 p.m. PST
+			}),
 		},
 	}),
 });
