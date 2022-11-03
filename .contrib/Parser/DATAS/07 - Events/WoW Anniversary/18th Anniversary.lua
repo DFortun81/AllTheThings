59a2f9f445196c16d3571738c12029be784fbac2
gbrage <gbrage@kth.se>
2022-11-03 21:25:18 +0100

    WoW Anniversary 18th

--- .contrib/Parser/DATAS/07 - Events/WoW Anniversary/18th Anniversary.lua
+++ .contrib/Parser/DATAS/07 - Events/WoW Anniversary/18th Anniversary.lua
@@ -1,15 +1,14 @@
 -----------------------------------------------------
 --     W O R L D   E V E N T S   M O D U L E       --
 -----------------------------------------------------
---[[
--- One-Time Anniversary Content -- TODO change to REMOVED_FROM_GAME after anniversay ends
+
 root("WorldEvents", n(EIGHTEENTH_ANNIVERSARY, bubbleDown({ ["u"] = WOW_ANNIVERSARY, },{
 	ach(15218),	-- WoW's 18th Anniversary
 	n(MAILBOX, {
 		i(187710),	-- Anniversary Gift
 	}),
 })));
-
+--[[
 -- Yearly Anniversary Content -- TODO verify this after most recent anniversary starts
 root("WorldEvents", n(EIGHTEENTH_ANNIVERSARY, bubbleDownSelf({ ["u"] = WOW_ANNIVERSARY, }, {
 })));
