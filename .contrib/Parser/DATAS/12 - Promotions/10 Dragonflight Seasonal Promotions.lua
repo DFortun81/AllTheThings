a630ae5ba3438f41ff044fa4ec0a8f1f065cf411
Gold <goldenshacal@gmail.com>
2022-11-03 09:30:31 +0100

    until the time comes, the 12month sub mounts only show under promo. also updated some dates

--- .contrib/Parser/DATAS/12 - Promotions/10 Dragonflight Seasonal Promotions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/10 Dragonflight Seasonal Promotions.lua
@@ -3,12 +3,22 @@
 -----------------------------------------------------
 root("Promotions", {
 	n(-584, {	-- Dragonflight Seasonal Promotions
-		["timeline"] = { "added 10.0.2" },
+		["timeline"] = { "added 10.0.0" },
 		["description"] = "These promotions happened during the time Dragonflight was the most recent content between 25th October 2022 & 12nd November 2024.\n\nThey are listed in the order of their first appearance.",
 		["groups"] = {
 			-- "Pre" Season
+			mount(315132, {	-- Gargantuan Grrloc
+				["description"] = "Obtained if you set up a 12-Month WoW Subscription. Promotion valid through January 15, 2023.",
+				["u"] = BLIZZARD_BALANCE,
+				["timeline"] = { "added 10.0.0.46366" },
+			}),
+			mount(381529, {	-- Telix the Stormhorn
+				["description"] = "Obtained if you set up a 12-Month WoW Subscription. Promotion valid through January 15, 2023.",
+				["u"] = BLIZZARD_BALANCE,
+				["timeline"] = { "added 10.0.0.46366" },
+			}),
 			i(34493, {	-- Dragon Kite
-				["description"] = "Obtained through watching Twitch Streamers with Drops enabled for at least 4 hours between November 15th, 10:00 a.m. & November 17th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your Battle.net Launcher as gift.",
+				["description"] = "Obtained through watching Twitch Streamers with Drops enabled for at least 4 hours between November 15th, 03:00 p.m. & November 18th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your Battle.net Launcher as gift.",
 				["timeline"] = {
 					"added 10.0.2",
 					-- "removed 10.0.2.XXXXX",	--
@@ -16,7 +26,7 @@ root("Promotions", {
 				-- ["u"] = REMOVED_FROM_GAME,	-- After November 17th, 11:59 p.m. PST
 			}),
 			i(79771, {	-- Fel Drake
-				["description"] = "Obtained through watching Twitch Streamers with Drops enabled for at least 4 hours between November 28th, 10:00 a.m. & November 30th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your Battle.net Launcher as gift.",
+				["description"] = "Obtained through watching Twitch Streamers with Drops enabled for at least 4 hours between November 28th, 03:00 p.m. & December 1st, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your Battle.net Launcher as gift.",
 				["timeline"] = {
 					"added 10.0.2",
 					-- "removed 10.0.2.XXXXX",	--
@@ -25,7 +35,7 @@ root("Promotions", {
 			}),
 			--[[
 			i(XXXXX, {	-- Ichabod	-- no ID yet. Don't forget to add the pet's spellID
-				["description"] = "Obtained by gifting an eligible creator’s channel two Twitch subscriptions between November 28th, 10:00 a.m. & December 12th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your Battle.net Launcher as gift.",
+				["description"] = "Obtained by gifting an eligible creator’s channel two Twitch subscriptions between November 28th, 03:00 p.m. & December 12th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your Battle.net Launcher as gift.",
 				["timeline"] = {
 					"added 10.0.2",
 					-- "removed 10.0.2.XXXXX",	--
@@ -34,7 +44,7 @@ root("Promotions", {
 			}),
 			--]]
 			i(49703, {	-- Perpetual Purple Firework
-				["description"] = "Obtained through watching select Twitch Streamers with Drops enabled for at least 2 hours between December 13th, 10:00 a.m. & December 28th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your Battle.net Launcher as gift.",
+				["description"] = "Obtained through watching select Twitch Streamers with Drops enabled for at least 2 hours between December 13th, 03:00 p.m. & December 28th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your Battle.net Launcher as gift.",
 				["timeline"] = {
 					"added 10.0.2",
 					-- "removed 10.0.2.XXXXX",	--
