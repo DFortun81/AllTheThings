ea0312bff0f7faaac27fa1088ab98c02f15c2a55
gbrage <gbrage@kth.se>
2022-11-11 23:15:18 +0100

    -- Fix Molkree Mistake? Hmm

--- .contrib/Parser/DATAS/06 - Expansion Features/09 Dragonflight/Primal Storms.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/09 Dragonflight/Primal Storms.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------
 --      E X P A N S I O N   F E A T U R E S    M O D U L E       --
 -------------------------------------------------------------------
-root("ExpansionFeatures", tier(DF_TIER, bubbleDownSelf({ ["timeline"] = ADDED_DFPRE2 }, {
+root("ExpansionFeatures", tier(DF_TIER, bubbleDownSelf({ ["timeline"] = ADDED_DFREL }, {
 	n(PRIMAL_STORMS, {
 		n(ACHIEVEMENTS, {
 			ach(16492),	-- Into the Storm [meta meta]
