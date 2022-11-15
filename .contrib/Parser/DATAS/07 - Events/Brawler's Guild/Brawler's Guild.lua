d0836df0f60c950c67a320674eb33298ac3d8fa6
ImUnicke <imunicke+github@gmail.com>
2022-11-15 11:41:57 -0700

    Stubbed a tiny bit of a start to merging Brawler's Guild into one source, then gave up since right now Alliance can't do Brawler's Guild and Horde can, it would make filtering really weird

---
+++ .contrib/Parser/DATAS/07 - Events/Brawler's Guild/Brawler's Guild.lua
@@ -0,0 +1,37 @@
+-----------------------------------------------------
+--     W O R L D   E V E N T S   M O D U L E       --
+-----------------------------------------------------
+
+-- Some day figure this out... maybe once both are actually fully available again to make filtering easier
+-- Add to enUS.lus
+--         [-78] = SPLASH_BATTLEFORAZEROTH_8_1_5_FEATURE2_TITLE,                    -- Brawler's Guild
+--[[
+root("WorldEvents", n(-78,	-- Brawler's Guild
+	-- bubbleDown({ ["u"] = REMOVED_FROM_GAME },
+	{
+	["description"] = "Fight queueing currently only available to Horde.\n2022-11-14",
+	["icon"] = "Interface\\Icons\\INV_MISC_HEAD_ORC2_BRN",
+	["maps"] = {
+		500,	-- Bizmo's Brawlpub
+		503,	-- Brawlgar Arena
+		DEEPRUN_TRAM,
+	},
+	["g"] = {
+		n(REWARDS, {
+			--	attaching questIDs and sourceQuests here makes the SQ list not display correctly (the items display instead of the quests).
+			i(92718, {	-- Brawler's Purse
+				["description"] = "\nAwarded for winning a Brawl. If you haven't already finished the quest chain for Bruce, save these for the 'Soul Splinters' quest.\n",
+			}),
+			i(92719, {	-- Bulging Brawler's Purse
+				["description"] = "\nAwarded for winning a Brawl. If you haven't already finished the quest chain for Bruce, save these for the 'Soul Splinters' quest.\n",
+			}),
+			i(164938, {	-- G.G. Gearbox
+				["description"] = "\nAwarded for beating the G.G. Engineering Challenge Card encounter. You need this to complete the 'The Precious 13-Tooth Gogglegear' quest.\n",
+			}),
+			i(164931, {	-- Rumbler's Purse
+				["description"] = "\nAwarded for winning a Rumble. If you haven't already finished the quest chain for Bruce, save these for the 'Super Soul Splinters' quest.\n",
+			}),
+		}),
+	},
+}));
+--]]
