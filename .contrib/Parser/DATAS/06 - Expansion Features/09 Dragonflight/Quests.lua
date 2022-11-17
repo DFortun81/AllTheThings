31be9d5b7cba1872141c0cb50d4179d7abfa1fcf
Gromwind <arallen@gmail.com>
2022-11-17 15:23:05 +0000

    Coordinate correction

--- .contrib/Parser/DATAS/06 - Expansion Features/09 Dragonflight/Quests.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/09 Dragonflight/Quests.lua
@@ -10,13 +10,13 @@ root("ExpansionFeatures", tier(DF_TIER, bubbleDown({ ["timeline"] = TIMELINE_DFP
 		}),
 		q(72288, {	-- Stay awhile and listen: Baine and Mayla
 			["name"] = "Stay awhile and listen: Baine and Mayla",
-			["coord"] = { 55.9, 12.1, ORGRIMMAR },
+			["coord"] = { 55.9, 12.1, DUROTAR },
 			["races"] = HORDE_ONLY,
 		}),
 		q(72290, {	-- Stay awhile and listen: Cindrethresh and Emberthal
 			["name"] = "Stay awhile and listen: Cindrethresh and Emberthal",
 			["provider"] = { "n", 184858 },	-- Cindrethresh
-			["coord"] = { 55.6, 12.3, ORGRIMMAR },
+			["coord"] = { 55.6, 12.3, DUROTAR },
 			["races"] = HORDE_ONLY,
 		}),
 		q(72286, {	-- Stay awhile and listen: Genn Greymane
@@ -32,7 +32,7 @@ root("ExpansionFeatures", tier(DF_TIER, bubbleDown({ ["timeline"] = TIMELINE_DFP
 		q(72289, {	-- Stay awhile and listen: Lor'themar and Thalyssra
 			["name"] = "Stay awhile and listen: Lor'themar and Thalyssra",
 			["provider"] = { "n", 184665 },	-- Lor'themar Theron
-			["coord"] = { 55.7, 12.2, ORGRIMMAR },
+			["coord"] = { 55.7, 12.2, DUROTAR },
 			["races"] = HORDE_ONLY,
 		}),
 	}),
--- .contrib/Parser/DATAS/17 - NYI/Hidden Triggers/Hidden Quest Triggers.lua
+++ .contrib/Parser/DATAS/17 - NYI/Hidden Triggers/Hidden Quest Triggers.lua
@@ -271,6 +271,7 @@ root("HiddenQuestTriggers", {
 		-- also triggered when looting the "Grimoire of the Other Side" as horde during "Spirits' belongings"
 		-- also triggers when repairing the mirror by Charred Ramparts for the Venthyr mirror repair dailies...
 		-- 58013 ALSO triggered after my first time killing Honey Smasher, but that also popped for me while doing the Alliance war campaign "This Ain't Mine."  so wtf lol
+		-- also triggered while killing mobs for Dog Bone's Bone in Maldraxxus (05/11/22)
 		q(51722),	-- triggers when completing "Uniting Kul Tiras" (Alliance WQ unlock).  Completing one triggers all 4 WQ unlock quests (main/alt quests for both Alliance and Horde)
 		q(52212),	-- Battle for Stormgarde - triggered after defeating Rokhan on Heroic difficulty
 		q(51973),	-- looting chest with resources after Lost Caravan event in Battle for Stormgarde
