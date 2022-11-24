fe96b5e2e4eb03609696e8b9ccafb65fb3a4e69b
Dmitry Volodin <mr.molkree@gmail.com>
2022-11-24 20:37:23 +0400

    Fix broken globals

--- locales/enUS.lua
+++ locales/enUS.lua
@@ -10,6 +10,10 @@
 --				This script is called directly after the Core.lua file. 	  --
 --------------------------------------------------------------------------------
 
+-- These globals somehow are not available immediately after loading the game
+CHARACTER_TYPE_FRAME_TRIAL_BOOST_CHARACTER = "Class Trial";
+CHARACTER_UPGRADE_CHARACTER_LIST_LABEL = "Character Boost";
+
 local name, app = ...;
 app.L = {
 	["TAB_SPACING"] = -10;	-- This is to control spacing between tab names in Settings
