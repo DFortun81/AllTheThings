c82e134a1c13af0a0528bf2722712f17665b3df6
Gromwind <arallen@gmail.com>
2022-11-30 22:44:02 +0000

    Removed a quest with the incorrect quest id

--- .contrib/Parser/DATAS/06 - Expansion Features/09 Dragonflight/Professions.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/09 Dragonflight/Professions.lua
@@ -347,11 +347,13 @@ root("ExpansionFeatures", tier(DF_TIER, bubbleDown({ ["timeline"] = TIMELINE_DF_
 					r(391089),	-- Refine Herbs+++
 				},
 			}),
-			q(70360, {	-- Dragon Isles Herbalism
+			--[[
+			q(, {	-- Dragon Isles Herbalism
 				--["sourceQuests"] = { },	--
 				["provider"] = { "n", 192549 },	-- Feilin Kuan
 				["coord"] = { 76.8, 34.0, THE_WAKING_SHORES },
 			}),
+			]]--
 		}),
 		prof(INSCRIPTION, {
 			filter(RECIPES, {
