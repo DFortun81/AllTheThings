66b8d30c45646c315fbd54e91b4865c65ee72896
Dmitry Volodin <mr.molkree@gmail.com>
2022-12-08 23:56:10 +0400

    Made new Twitch Drops visible

--- .contrib/Parser/DATAS/12 - Promotions/10 Dragonflight Seasonal Promotions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/10 Dragonflight Seasonal Promotions.lua
@@ -21,49 +21,43 @@ root("Promotions", {
 				["description"] = "Obtained through watching Twitch Streamers with Drops enabled for at least 4 hours between November 15th, 03:00 p.m. & November 18th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
 				["timeline"] = {
 					"added 10.0.2",
-					"removed 10.0.2.46702",
+					"removed 10.0.2.46702",	-- After November 17th, 11:59 p.m. PST
 				},
-				["u"] = REMOVED_FROM_GAME,	-- After November 17th, 11:59 p.m. PST
 			}),
 			i(79771, {	-- Fel Drake
 				["description"] = "Obtained through watching Twitch Streamers with Drops enabled for at least 4 hours between November 28th, 03:00 p.m. & December 1st, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
 				["timeline"] = {
 					"added 10.0.2.46801",
-					"removed 10.0.2.46879",
+					"removed 10.0.2.46879",	-- After December 1st 2022
 				},
-				["u"] = REMOVED_FROM_GAME,	-- After December 1st 2022
 			}),
 			i(190583, {	-- Ichabod
 				["description"] = "Obtained by gifting an eligible creator's channel two Twitch subscriptions between November 28th, 03:00 p.m. & December 12th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
 				["timeline"] = {
 					"added 10.0.2.46801",
-					-- "removed 10.0.2.XXXXX",
+					-- "removed 10.0.2.XXXXX",	-- After December 12th, 11:59 p.m. PST
 				},
-				-- ["u"] = REMOVED_FROM_GAME,	-- After December 12th, 11:59 p.m. PST
 			}),
 			i(70099, {	-- Cenarion Hatchling
 				["description"] = "Obtained through watching the Race to World First streams with Drops enabled for at least 4 hours between December 9th, 12:00 a.m. & December 13th, 02:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
 				["timeline"] = {
-					"added 10.0.2.99999",
-					-- "removed 10.0.2.XXXXX",
+					"added 10.0.2.46924",
+					-- "removed 10.0.2.XXXXX",	-- After December 13th, 02:59 p.m. PST
 				},
-				-- ["u"] = REMOVED_FROM_GAME,	-- After December 13th, 02:59 p.m. PST
 			}),
 			i(92724, {	-- Swift Windsteed
 				["description"] = "Obtained through watching the Race to World First streams with Drops enabled for at least 8 hours between December 9th, 12:00 a.m. & December 13th, 02:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
 				["timeline"] = {
-					"added 10.0.2.99999",
-					-- "removed 10.0.2.XXXXX",
+					"added 10.0.2.46924",
+					-- "removed 10.0.2.XXXXX",	-- After December 13th, 02:59 p.m. PST
 				},
-				-- ["u"] = REMOVED_FROM_GAME,	-- After December 13th, 02:59 p.m. PST
 			}),
 			i(49703, {	-- Perpetual Purple Firework
 				["description"] = "Obtained through watching select Twitch Streamers with Drops enabled for at least 2 hours between December 13th, 03:00 p.m. & December 28th, 11:59 p.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
 				["timeline"] = {
 					"added 10.0.2.99999",
-					-- "removed 10.0.2.XXXXX",	--
+					-- "removed 10.0.2.XXXXX",	-- After December 28th, 11:59 p.m. PST
 				},
-				-- ["u"] = REMOVED_FROM_GAME,	-- After December 28th, 11:59 p.m. PST
 			}),
 		},
 	}),
