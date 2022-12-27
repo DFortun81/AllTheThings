266cda3b751882a9e7ce01ed44491e0060eaa379
ImUnicke <imunicke+github@gmail.com>
2022-12-27 13:19:12 -0700

    Possible tooltip for attaching, but doesn't seem useful so far...

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -22553,6 +22553,7 @@ local HookableTooltips = {
 	["ItemRefTooltip"]=1,
 	["ShoppingTooltip1"]=1,
 	["ShoppingTooltip2"]=1,
+	-- ["EncounterJournalTooltipItem1Tooltip"]=1,	-- various tooltips in Adventure Guide, not sure any are actually useful to attach ATT data
 	-- other addons which create user-visible tooltips that ATT should attach into
 	["SilverDragonLootTooltip"]=1,
 };
