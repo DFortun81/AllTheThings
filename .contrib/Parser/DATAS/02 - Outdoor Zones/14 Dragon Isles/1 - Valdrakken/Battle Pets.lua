9998240c80a97c208bbf1e8dc03d0ec2fda03edc
gbrage123 <gbrage@kth.se>
2022-12-27 18:11:24 +0100

    Pets

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Valdrakken/Battle Pets.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Valdrakken/Battle Pets.lua
@@ -4,17 +4,17 @@
 root("Zones", m(DRAGON_ISLES, bubbleDown({ ["timeline"] = TIMELINE_DF_REL }, {
 	m(VALDRAKKEN, {
 		petbattle(filter(BATTLE_PETS, {
-			pet(3301, {	-- Wild Duckling
-				["cr"] = 189136,	-- Wild Duckling
-				["coords"] = {
-					{ 60.4, 57.0, VALDRAKKEN, },
-				},
-			}),
-			pet(3307, {	-- Plucky Duckling
+			pet(3307, {	-- Plucky Duckling (PET!)
 				["cr"] = 189144,	-- Plucky Duckling
 				["coords"] = {
 					{ 60.8, 57.2, VALDRAKKEN, },
 					{ 57.0, 71.0, THE_WAKING_SHORES, },
+				},
+			}),
+			pet(3301, {	-- Wild Duckling (PET!)
+				["cr"] = 189136,	-- Wild Duckling
+				["coords"] = {
+					{ 60.4, 57.0, VALDRAKKEN, },
 				},
 			}),
 		})),
--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Ohn'ahran Plains/Battle Pets.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Ohn'ahran Plains/Battle Pets.lua
@@ -1,23 +1,24 @@
+
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
 root("Zones", m(DRAGON_ISLES, bubbleDown({ ["timeline"] = TIMELINE_DF_REL }, {
 	m(OHNAHRAN_PLAINS, {
 		petbattle(filter(BATTLE_PETS, {
-			pet(3266, {	-- Black Slyvern Pup
+			pet(3266, {	-- Black Slyvern Pup (PET!)
 				["cr"] = 189160,	-- Black Slyvern Pup
 				["coords"] = {
 					{ 64.0, 26.0, OHNAHRAN_PLAINS, },
 				},
 			}),
-			pet(3327, {	-- Dusky Timbertooth
+			pet(3327, {	-- Dusky Timbertooth (PET!)
 				["description"] = "You have to click on the |cFFefc400Beaver Dam|r before you can battle this pet.",
 				["cr"] = 189657,	-- Dusky Timbertooth
 				["coords"] = {
 					{ 71.6, 74.0, OHNAHRAN_PLAINS, },
 				},
 			}),
-			pet(3313, {	-- Grassland Stomper
+			pet(3313, {	-- Grassland Stomper (PET!)
 				["cr"] = 189153,	-- Grassland Stomper
 				["coords"] = {
 					{ 54.0, 50.2, OHNAHRAN_PLAINS, },
@@ -25,38 +26,38 @@ root("Zones", m(DRAGON_ISLES, bubbleDown({ ["timeline"] = TIMELINE_DF_REL }, {
 					{ 41.0, 61.8, THE_WAKING_SHORES, },
 				},
 			}),
-			pet(3300, {	-- Ironbeak Duck
+			pet(3300, {	-- Ironbeak Duck (PET!)
 				["cr"] = 189131,	-- Ironbeak Duck
 			}),
-			pet(3296, {	-- Palamanther
+			pet(3296, {	-- Palamanther (PET!)
 				["cr"] = 189122,	-- Palamanther
 			}),
-			pet(3281, {	-- Scruffy Ottuk
+			pet(3281, {	-- Scruffy Ottuk (PET!)
 				["cr"] = 189103,	-- Scruffy Ottuk
 			}),
-			pet(3353, {	-- Stoneshell
+			pet(3353, {	-- Stoneshell (PET!)
 				["cr"] = 192254,	-- Stoneshell
 			}),
-			pet(3282, {	-- Swoglet
+			pet(3282, {	-- Swoglet (PET!)
 				["cr"] = 189104,	-- Swoglet
 			}),
-			pet(3328, {	-- Tiny Timbertooth
+			pet(3328, {	-- Tiny Timbertooth (PET!)
 				["cr"] = 189658,	-- Tiny Timbertooth
 				["coords"] = {
 					{ 79.0, 48.4, OHNAHRAN_PLAINS, },
 					{ 71.8, 73.8, OHNAHRAN_PLAINS, },
 				},
 			}),
-			pet(3276, {	-- Treeflitter
+			pet(3276, {	-- Treeflitter (PET!)
 				["cr"] = 189097,	-- Treeflitter
 			}),
-			pet(3288, {	-- Trunkalumpf
+			pet(3288, {	-- Trunkalumpf (PET!)
 				["cr"] = 189110,	-- Trunkalumpf
 			}),
-			pet(3322, {	-- Woodbiter Piculet
+			pet(3322, {	-- Woodbiter Piculet (PET!)
 				["cr"] = 189157,	-- Woodbiter Piculet
 			}),
-			pet(3389, {	-- The Quackcestor
+			pet(3389, {	-- The Quackcestor (PET!)
 				["minReputation"] = { 2503, 7 },	-- Maruuk Centaur Renown 7
 				["description"] = "Buy the |cFFefc400Essence of Awakening|r from |cFFefc400Quartermaster Huseng|r and use it to be able to see this pet.",
 				["cr"] = 197063,	-- The Quackcestor
