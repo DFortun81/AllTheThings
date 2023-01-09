bd733df3d8d730fa99fc6e9944569e8c44566314
gbrage <gbrage@kth.se>
2023-01-09 18:14:04 +0100

    new Achievement Category

---
+++ .contrib/Parser/DATAS/05 - Achievements/10 - Collections/05 - Dragon Isles Drake Cosmetics.lua
@@ -0,0 +1,7 @@
+--------------------------------------------------
+--    A C H I E V E M E N T S    M O D U L E    --
+--------------------------------------------------
+
+root("Achievements", achcat(ACHIEVEMENT_CATEGORY_COLLECTIONS, {
+	achcat(ACHIEVEMENT_CATEGORY_DRAGON_ISLES_DRAKE_COSMETICS)
+}));
