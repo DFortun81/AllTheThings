79f1d34cdd3ef0b43b0e387ec8d2ccc12560dc17
Dmitry Volodin <mr.molkree@gmail.com>
2023-01-10 07:11:41 +0400

    Tiny formatting fixes

--- locales/cn.lua
+++ locales/cn.lua
@@ -872,7 +872,7 @@ for key,value in pairs({
 		[-1131] = "伊斯卡拉渔具",										-- Iskaara Fishing
 		[-1140] = "瓦德拉肯联军",											-- Valdrakken Accord
 		[-1142] = "天际挑战",											-- Aerial Challenges
-		[-1143] = "每2小时稀有",						-- Every 2 Hours Rare
+		[-1143] = "每2小时稀有",											-- Every 2 Hours Rare
 	-- Warrior order hall lore items
 		[-2200] = "伟大的奥丁与炎魔之王",
 		[-2201] = "流浪者与蛇",
