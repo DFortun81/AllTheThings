6b05d6ab5efd6eb6dcfda745ed088580298948cc
gbrage <gbrage@kth.se>
2023-01-14 17:43:20 +0100

    -- Dragon Racing got to be a own header

--- .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Ardenweald/Emissary Quests.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Ardenweald/Emissary Quests.lua
@@ -4,20 +4,19 @@
 root("Zones", m(SHADOWLANDS, bubbleDown({ ["timeline"] = { ADDED_SL_REL } }, {
 	m(ARDENWEALD, {
 		n(EMISSARY_QUESTS, {
-			q(61098, {	-- Supplies from The Wild Hunt
+			q(61098, bubbleDownSelf({ ["minReputation"] = { THE_WILD_HUNT, EXALTED } }, {	-- Supplies from The Wild Hunt
 				["provider"] = { "n", 158556 },	-- Aithlyn <Wild Hunt Quartermaster>
 				["coord"] = { 48.4, 50.4, ARDENWEALD },
 				["repeatable"] = true,
-				["minReputation"] = { THE_WILD_HUNT, EXALTED },
-				["g"] = bubbleDown({ ["minReputation"] = { THE_WILD_HUNT, EXALTED } }, {
+				["g"] = {
 					i(180649, {	-- Wild Hunt Supplies
 						i(180635),	-- Hungry Burrower (PET!)
 						i(183800),	-- Amber Ardenmoth (MOUNT!)
 						i(187870, {["timeline"] = { ADDED_SL_1_5 }}),	-- Porcupine Soul (SS!)
 						i(185054, {["timeline"] = { ADDED_SL_1_5 }}),	-- Mammoth Soul (SS!)
 					}),
-				}),
-			}),
+				},
+			})),
 		}),
 	}),
 })));
--- .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Ardenweald/Special.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Ardenweald/Special.lua
@@ -7,6 +7,14 @@ root("Zones", m(SHADOWLANDS, bubbleDown({ ["timeline"] = { ADDED_SL_REL } }, {
 			i(182599, {	-- Bucket of Clean Water
 				["cost"] = { { "i", 182620, 1 } },	-- 1x Empty Water Bucket
 			}),
+			i(180652, {	-- Fae Dreamcatcher
+				["description"] = "Used to dispel the barrier at |cFFFFFFFF36.1, 65.2|r.",
+				["cost"] = {
+					{ "i", 180656, 1 },	-- Enchanted Bough
+					{ "i", 180654, 1 },	-- Fae Ornament
+					{ "i", 180655, 1 },	-- Raw Dream Fibers
+				},
+			}),
 			n(171206, {	-- Playful Vulpin
 				["description"] = "You need to find the Playful Vulpin five times and use the following emotes.\n\n 1. Playful Vulpin begins to dig curiously. |cFFFFFFFF/curious|r\n 2. Playful Vulpin wanders around unable to sit still. |cFFFFFFFF/sit|r\n 3. Playful Vulpin sings all alone. |cFFFFFFFF/sing|r\n 4. Playful Vulpin dances with joy. |cFFFFFFFF/dance|r\n 5. Playful Vulpin sits down lonely and sad. |cFFFFFFFF/pet|r\n\nIt only counts if the Playful Vulpin reacts to your emote and runs away.",
 				["coords"] = {
@@ -52,7 +60,7 @@ root("Zones", m(SHADOWLANDS, bubbleDown({ ["timeline"] = { ADDED_SL_REL } }, {
 					i(180645),	-- Dodger (PET!)
 				},
 			}),
-			n(168135, {	-- Night Mare
+			header(HEADERS.NPC, 168135, {	-- Night Mare
 				["description"] = "Enable Debug Mode to view all the steps.\n\nYou will need at least 2 |cFFFFFFFFGoblin Gliders|r and 10 |cff16bf0dLightless Silk|r.  (I encountered a bug where, until I had more than 10 cloth in my bags, I could not progress to the next step, so you may want to bring a few extra.)\n\nYou will also need to have completed the 'Trouble at the Gormling Corral' and 'Tricky Spriggans' criteria of the |cffffff00Sojourner of Ardenweald|r achievement.  You must also fight a 62 rare elite, so bringing a couple friends along is a good idea.",
 				["g"] = {
 					i(181243, {	-- Broken Soulweb
