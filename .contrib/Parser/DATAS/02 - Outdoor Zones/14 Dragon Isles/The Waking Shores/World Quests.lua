3d4ac32be559606c5f4d60ae3dc4a1223eda4ace
jezartroz <tereliten@gmail.com>
2023-01-14 13:11:28 -0500

    Waking Shores WQs

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/The Waking Shores/World Quests.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/The Waking Shores/World Quests.lua
@@ -19,6 +19,9 @@ root("Zones", m(DRAGON_ISLES, bubbleDown({ ["timeline"] = TIMELINE_DF_REL }, {
 				q(66902, {	-- Bee-Gone
 					["coord"] = { 56.8, 68.0, THE_WAKING_SHORES }
 				}),
+				q(67010, {	-- Boiling Over
+					["coord"] = { 48.9, 45.9, THE_WAKING_SHORES },
+				}),
 				q(67062, {	-- Cascading Conflict
 					["coord"] = { 48.0, 52.2, THE_WAKING_SHORES },
 				}),
--- .contrib/Parser/DATAS/17 - NYI/NYI Quests.lua
+++ .contrib/Parser/DATAS/17 - NYI/NYI Quests.lua
@@ -6987,7 +6987,6 @@ root("NeverImplemented", bubbleDown({["u"] = NEVER_IMPLEMENTED}, {
 			q(66986),	--
 			q(67000),	--
 			q(67001),	--
-			q(67010),	--
 			q(67016),	--
 			q(67019),	--
 			q(67020),	--
