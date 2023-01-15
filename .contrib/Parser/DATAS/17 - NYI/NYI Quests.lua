f1d35f092124f344090a1883441c907f524ecd73
Dmitry Volodin <mr.molkree@gmail.com>
2023-01-16 02:12:10 +0400

    Updated changelog for 3.1.14

--- .contrib/Parser/DATAS/17 - NYI/NYI Quests.lua
+++ .contrib/Parser/DATAS/17 - NYI/NYI Quests.lua
@@ -7500,8 +7500,8 @@ root("NeverImplemented", bubbleDown({["u"] = NEVER_IMPLEMENTED}, {
 			q(72632),	--
 			q(72633),	-- Dragon Isles Supplies The Azure Span
 			q(72634),	--
-			q(72635),	--
-			q(72636),	--
+			q(72635),	-- Dragon Isles Supplies renown 20 tuskarr (2nd time)
+			q(72636),	-- Dragon Isles Supplies renown 21 tuskarr (2nd time)
 			q(72643),	-- Dragon Isles Supplies
 			q(72683),	--
 			q(72684),	--
