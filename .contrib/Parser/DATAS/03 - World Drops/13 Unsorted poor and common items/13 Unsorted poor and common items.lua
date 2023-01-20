6ce45cb2160d24aa9115c58defeb8d873aed57be
Darkal <michalbert@seznam.cz>
2023-01-20 22:05:34 +0100

    Removed classic from this temporary file.

--- .contrib/Parser/DATAS/03 - World Drops/13 Unsorted poor and common items/13 Unsorted poor and common items.lua
+++ .contrib/Parser/DATAS/03 - World Drops/13 Unsorted poor and common items/13 Unsorted poor and common items.lua
@@ -6,6 +6,7 @@
 
 -- DO NOT REMOVE OR I WILL FIND YOU! -Darkal
 
+-- #if NOT ANYCLASSIC
 root(ROOTS.WorldDrops, bubbleDown({ ["timeline"] = "added 10.0.5", ["description"] = "This is a temporary group of unsorted items.\n\nThey will be sorted soon™.\n\nYou don't need to report any of them." }, {
 	n(-123456, {	-- Poor
 		n(ARMOR, {
@@ -24,3 +25,4 @@ root(ROOTS.WorldDrops, bubbleDown({ ["timeline"] = "added 10.0.5", ["description
 		}),
 	}),
 }));
+-- #endif
