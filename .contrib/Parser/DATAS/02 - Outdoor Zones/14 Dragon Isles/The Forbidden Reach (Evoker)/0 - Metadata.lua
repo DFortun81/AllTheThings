5818bf78c0c756c534a259839386b9d28f78c106
gbrage <gbrage@kth.se>
2023-02-04 09:40:23 +0100

    -- Seperated Forbidden Reach into two branches

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/The Forbidden Reach (Evoker)/0 - Metadata.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/The Forbidden Reach (Evoker)/0 - Metadata.lua
@@ -1,16 +1,17 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
-root(ROOTS.Zones, m(DRAGON_ISLES, {
-	m(THE_FORBIDDEN_REACH, {
+root("Zones", m(DRAGON_ISLES, {
+	m(THE_FORBIDDEN_REACH_EVOKER, {
 		["icon"] = 4672496,
 		["lore"] = "The Forbidden Reach is an island located off the coast of the Dragon Isles. It was fashioned by Neltharion the Earth-Warder to serve as a training ground for his ultimate soldiers, the dracthyr. But in the aftermath of a terrible battle, the island has lain dormant and abandoned for ages until the return of an ancient enemy causes the dracthyr to stir.\n\nThe zone is the starting zone of the new playable race-hero class combo dracthyr evoker starting at level 58.",
-		["timeline"] = { ADDED_DF_0_7 },
+		["classes"] = { EVOKER },
+		["races"] = { DRACTHYR_ALLIANCE, DRACTHYR_HORDE },
+		["timeline"] = TIMELINE_DF_PRE2,
 		["maps"] = {
-			2101,	-- The Support Creche
-			2102,	-- The War Creche
-			2150,	-- ForbiddenReach_Micro_C
-			2154,	-- The Forbidden Reach
+			2109,	-- The War Creche
+			2110,	-- The Support Creche
+			2111,	-- The Siege Creche
 		},
 	}),
 }));
---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/The Forbidden Reach (Evoker)/Rares.lua
@@ -0,0 +1,59 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+root("Zones", m(DRAGON_ISLES, bubbleDown({ ["timeline"] = TIMELINE_DF_PRE2 }, {
+	m(THE_FORBIDDEN_REACH_EVOKER, bubbleDownSelf({ ["classes"] = { EVOKER }, ["races"] = { DRACTHYR_ALLIANCE, DRACTHYR_HORDE } }, {
+		n(RARES, {
+			n(191729, {	-- Deathrip
+				["coord"] = { 32.9, 41.2, THE_FORBIDDEN_REACH_EVOKER },
+				["questID"] = 66966,
+				["g"] = {
+					i(197725),	-- Deathrip's Curled Claw
+				},
+			}),
+			n(191746, {	-- Ketess the Pilager
+				["coord"] = { 56.7, 65.6, THE_FORBIDDEN_REACH_EVOKER },
+				["questID"] = 66975,
+				["g"] = {
+					i(194741),	-- Earthbound Tome
+				},
+			}),
+			n(191713, {	-- Scytherin
+				["coord"] = { 28.9, 36.6, THE_FORBIDDEN_REACH_EVOKER },
+				["questID"] = 66967,
+				["g"] = {
+					i(196435),	-- Scytherin's Barbed Necklace
+				},
+			}),
+			n(181833, {	-- Shimmermaw
+				["coord"] = { 71.0, 37.4, THE_FORBIDDEN_REACH_EVOKER },
+				["questID"] = 64971,
+				["g"] = {
+					i(194074),	-- Gilded Key
+				},
+			}),
+			n(181427, {	-- Stormspine
+				["coord"] = { 54.9, 43.1, THE_FORBIDDEN_REACH_EVOKER },
+				["questID"] = 64859,
+				["g"] = {
+					i(194084),	-- Charged Storm Crystal
+				},
+			}),
+			n(182280, {	-- Tazenrath
+				["coord"] = { 79.7, 74.3, THE_FORBIDDEN_REACH_EVOKER },
+				["questID"] = 66973,
+				["g"] = {
+					i(194883),	-- Runic Wing
+				},
+			}),
+			n(186355, {	-- Tripletath the Lost
+				["description"] = "Cave entrance is at 59.9, 59.0.",
+				["coord"] = { 57.4, 60.2, THE_FORBIDDEN_REACH_EVOKER },
+				["questID"] = 65910,
+				["g"] = {
+					i(197727),	-- Mystical Water Vial
+				},
+			}),
+		}),
+	})),
+})));
