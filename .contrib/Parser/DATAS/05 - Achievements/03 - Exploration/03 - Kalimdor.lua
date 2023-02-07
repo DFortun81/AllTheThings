1f7f56d938a634a6d69bedee1a89ef13dcffc712
Dmitry Volodin <mr.molkree@gmail.com>
2023-02-07 04:07:27 +0400

    Pruned unneeded files
    Already listed in `00 - Exploration.lua`

--- .contrib/Parser/DATAS/05 - Achievements/03 - Exploration/03 - Kalimdor.lua
+++
@@ -1,9 +0,0 @@
---------------------------------------------------
---    A C H I E V E M E N T S    M O D U L E    --
---------------------------------------------------
-
-root(ROOTS.Achievements, achcat(ACHIEVEMENT_CATEGORY_EXPLORATION, {
-	achcat(ACHIEVEMENT_CATEGORY_KALIMDOR_EXP, {
-		-- Leave Empty, Parser Fills
-	}),
-}));
--- .contrib/Parser/DATAS/05 - Achievements/03 - Exploration/10 - Battle for Azeroth.lua
+++
@@ -1,8 +0,0 @@
---------------------------------------------------
---    A C H I E V E M E N T S    M O D U L E    --
---------------------------------------------------
-root(ROOTS.Achievements, achcat(ACHIEVEMENT_CATEGORY_EXPLORATION, {
-	achcat(ACHIEVEMENT_CATEGORY_BATTLE_FOR_AZEROTH_EXP, {
-		-- Leave Empty, Parser Fills
-	}),
-}));
