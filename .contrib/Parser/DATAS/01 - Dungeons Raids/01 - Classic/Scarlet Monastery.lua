167d265b15bcc5b657187ef6be2c65e26556017b
Crieve <dylanfortune@live.com>
2023-02-16 17:03:38 -0700

    Ironspine's Ribcage becomes a Plate transmog later on.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/Scarlet Monastery.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/Scarlet Monastery.lua
@@ -354,6 +354,15 @@ root(ROOTS.Instances, tier(CLASSIC_TIER, {
 									}),
 									i(7688, {	-- Ironspine's Ribcage
 										["timeline"] = { "removed 5.0.4" },
+										-- #if ANYCLASSIC
+										["OnUpdate"] = [[function(t)
+											if _.Level >= 40 and _.ClassIndex ~= ]] .. HUNTER .. [[ and _.ClassIndex ~= ]] .. SHAMAN .. [[ then
+												t.f = ]] .. PLATE .. [[;
+											else
+												t.f = ]] .. MAIL .. [[;
+											end
+										end]],
+										-- #endif
 									}),
 									i(7686, {	-- Ironspine's Eye
 										["timeline"] = { "removed 5.0.4" },
