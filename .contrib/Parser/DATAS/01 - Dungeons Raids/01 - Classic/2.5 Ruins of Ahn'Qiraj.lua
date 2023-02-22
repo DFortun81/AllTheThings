345d5396111ae02c3da673d44c9fe26b4203ed68
Crieve <dylanfortune@live.com>
2023-02-22 13:52:59 -0700

    Alphabetical order for recipes in AQ20.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/2.5 Ruins of Ahn'Qiraj.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/2.5 Ruins of Ahn'Qiraj.lua
@@ -769,13 +769,13 @@ root(ROOTS.Instances, tier(CLASSIC_TIER, applyclassicphase(PHASE_FIVE, {
 					})),
 
 					-- Enchants
+					i(20736),	-- Formula: Enchant Cloak - Dodge (RECIPE!)
+					i(20734),	-- Formula: Enchant Cloak - Stealth (RECIPE!)
 					i(20729),	-- Formula: Enchant Gloves - Fire Power (RECIPE!)
 					i(20728),	-- Formula: Enchant Gloves - Frost Power (RECIPE!)
 					i(20730),	-- Formula: Enchant Gloves - Healing Power (RECIPE!)
 					i(20727),	-- Formula: Enchant Gloves - Shadow Power (RECIPE!)
 					i(20731),	-- Formula: Enchant Gloves - Superior Agility (RECIPE!)
-					i(20736),	-- Formula: Enchant Cloak - Dodge (RECIPE!)
-					i(20734),	-- Formula: Enchant Cloak - Stealth (RECIPE!)
 				},
 			}),
 			n(COMMON_BOSS_DROPS, {
