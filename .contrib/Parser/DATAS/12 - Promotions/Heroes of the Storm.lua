42acec8015a09508b18a6cdfe3c09ac964dceae2
Crieve <dylanfortune@live.com>
2023-02-23 11:26:54 -0700

    Heroes of the Storm no longer shows up before 6.0.1.

--- .contrib/Parser/DATAS/12 - Promotions/Heroes of the Storm.lua
+++ .contrib/Parser/DATAS/12 - Promotions/Heroes of the Storm.lua
@@ -1,6 +1,7 @@
 -----------------------------------------------------
 --        P R O M O T I O N S   M O D U L E        --
 -----------------------------------------------------
+-- #if AFTER 6.0.1.18711
 root(ROOTS.Promotions, n(-532, {	-- Heroes of the Storm
 	ach(10657, {	-- Fledgling Hero of Warcraft
 		["description"] = "Cross-Game Reward: Lady Liadrin Paladin Hero in Hearthstone.",
@@ -11,7 +12,7 @@ root(ROOTS.Promotions, n(-532, {	-- Heroes of the Storm
 		["description"] = "Obtained by playing Heroes of the Storm and reaching level 20.",
 		["timeline"] = { "added 6.0.1.18711" },
 	}, {
-		ach(9926),	-- Hero of the Storm
+		classicAch(9926),	-- Hero of the Storm
 		i(118518),	-- Graves
 	})),
 	n(REWARDS, bubbleDown({
@@ -19,7 +20,8 @@ root(ROOTS.Promotions, n(-532, {	-- Heroes of the Storm
 		["description"] = "Obtained by winning 15 games of Heroes of the Storm.\n\nSadly, this is no longer available.",
 		["timeline"] = { "added 7.2.0.23801" },
 	}, {
-		ach(11425),	-- Herald of Flames
+		classicAch(11425),	-- Herald of Flames
 		i(143631),	-- Primal Flamesaber (MOUNT!)
 	})),
 }));
+-- #endif
