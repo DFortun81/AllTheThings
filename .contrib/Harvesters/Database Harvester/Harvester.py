0a787c2a6e009c5874c32b90318e8b2aa96f4f50
gbrage <gbrage@kth.se>
2023-03-21 20:35:34 +0100

    Missing Things

--- .contrib/Harvesters/Database Harvester/Harvester.py
+++ .contrib/Harvesters/Database Harvester/Harvester.py
@@ -525,11 +525,11 @@ def give_name_quest() -> None:
 
 """Step 1: Load New CSVs inside of Latests/dbfilesclient. """
 """Step 2: Run add_latest_data(build: str) (You have to uncomment) with the build as a string ex. add_latest_data("10.0.2.43010"). """
-# add_latest_data("3.4.0.46158", "Classic Beta")
+# add_latest_data("3.4.1.48632", "Classic")
 """Step 3: If new SkillLines have has been added they need to be sorted manually. Ex. Language:Furbolg is not a real profession so it has to be added into Exclusion/SkillLines.txt. If its an interesting SkillLine it can be added to Exclusion/SkillLineOther.txt. If its a new profession just let it be"""
 """Step 4: Run sort_raw_file_recipes() (you have to uncomment it) this will sort raw recipes into respective profession."""
 # sort_raw_file_recipes()
 """Step 5: Run create_missing_files() (you have to uncomment it)"""
 # create_missing_files()
 """Step 6: Run available_post_process() (you have to uncomment it) This is still underwork and currently only Achievements, Factions, Flight Paths, Illusions, Mounts, Pets, Titles and Toys can be Post Processed"""
-available_post_process()
+# available_post_process()
