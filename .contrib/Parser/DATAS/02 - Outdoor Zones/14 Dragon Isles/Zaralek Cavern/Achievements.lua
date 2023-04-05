b586c910cb2a2edae82d6b653b13eabfd7d55248
gbrage <gbrage@kth.se>
2023-04-05 02:48:05 +0200

    Small change

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Zaralek Cavern/Achievements.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Zaralek Cavern/Achievements.lua
@@ -1,5 +1,5 @@
 ---------------------------------------------------
---		  Z O N E S		M O D U L E		 --
+--          Z O N E S        M O D U L E         --
 ---------------------------------------------------
 root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_1_0 } }, {
 	m(ZARALEK_CAVERN, {
