10d4f9dc21f832e426e8dec5b575fe94ca990be6
gbrage <gbrage@kth.se>
2023-04-05 02:43:43 +0200

    10.1 Dragon Glyphs. Thanks a lot Pr3vention!

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Zaralek Cavern/Achievements.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Zaralek Cavern/Achievements.lua
@@ -1,9 +1,45 @@
 ---------------------------------------------------
---          Z O N E S        M O D U L E         --
+--		  Z O N E S		M O D U L E		 --
 ---------------------------------------------------
 root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_1_0 } }, {
 	m(ZARALEK_CAVERN, {
 		n(ACHIEVEMENTS, {
+			ach(17517, {	-- Dragon Glyphs: Aberrus Approach
+				["coord"] = { 48.4, 7.0, ZARALEK_CAVERN },
+			}),
+			ach(17516, {	-- Dragon Glyphs: Acidbite Ravine
+				["coord"] = { 47.5, 36.9, ZARALEK_CAVERN },
+			}),
+			ach(17510, {	-- Dragon Glyphs: Glimmerogg
+				["coord"] = { 41.7, 80.9, ZARALEK_CAVERN },
+			}),
+			ach(17512, {	-- Dragon Glyphs: Loamm
+				["coord"] = { 54.7, 54.7, ZARALEK_CAVERN },
+			}),
+			ach(17511, {	-- Dragon Glyphs: Nal ks'kol
+				["coord"] = { 62.6, 73.3, ZARALEK_CAVERN },
+			}),
+			ach(17514, {	-- Dragon Glyphs: Slitherdrake Roost
+				["coord"] = { 55.3, 27.9, ZARALEK_CAVERN },
+			}),
+			ach(17515, {	-- Dragon Glyphs: The Throughway
+				["coord"] = { 72.0, 48.4, ZARALEK_CAVERN },
+			}),
+			ach(17513, {	-- Dragon Glyphs: Zaqali Caldera
+				["coord"] = { 30.5, 45.3, ZARALEK_CAVERN },
+			}),
+			ach(18150, {	-- Zaralek Caverns Glyph Hunter
+				["sym"] = {{"meta_achievement",
+					17517,	-- Dragon Glyphs: Aberrus Approach
+					17516,	-- Dragon Glyphs: Acidbite Ravine
+					17512,	-- Dragon Glyphs: Loamm
+					17510,	-- Dragon Glyphs: Glimmerogg
+					17511,	-- Dragon Glyphs: Nal ks'kol
+					17514,	-- Dragon Glyphs: Slitherdrake Roost
+					17515,	-- Dragon Glyphs: The Throughway
+					17513,	-- Dragon Glyphs: Zaqali Caldera
+				}},
+			}),
 			ach(17934, {	-- Family Battler of Zaralek Cavern
 				i(193377),	-- Gerald (PET!)
 			}),
