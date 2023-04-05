9ac7818258811ad1710195de7dc9abb120d9b36a
Goldenshacal <goldenshacal@gmail.com>
2023-04-05 02:06:00 +0200

    prime #sellout. feel free to add your name here fellow att contributors

--- .contrib/Parser/DATAS/12 - Promotions/Amazon Prime Gaming.lua
+++ .contrib/Parser/DATAS/12 - Promotions/Amazon Prime Gaming.lua
@@ -4,7 +4,7 @@
 root(ROOTS.Promotions, {
 	n(-586, {	-- Amazon Prime Gaming
 		["timeline"] = { "added 10.0.7" },
-		["description"] = "Amazon Prime Gaming offers in-game content for World of Warcraft Retail. Depending on your country, Amazon Prime (which includes Gaming) costs between 4-16 $/€ per month with the occasional monthly free trial available.\n\nCreate a Twitch account & connect it with your Prime Account. Next connect your B.Net Account with your Twitch Account. Lastly claim your monthly reward under 'gaming.amazon.com/loot/wow'.\n\nAmazon Prime also comes with a free Twitch subscription which you can use to support AllTheThings.\n\n<put names here>",
+		["description"] = "Amazon Prime Gaming offers in-game content for World of Warcraft Retail. Depending on your country, Amazon Prime (which includes Gaming) costs between 4-16 $/€ per month with the occasional monthly free trial available.\n\nCreate a Twitch account & connect it with your Prime Account. Next connect your B.Net Account with your Twitch Account. Lastly claim your monthly reward under 'gaming.amazon.com/loot/wow'.\n\nAmazon Prime also comes with a free Twitch subscription which you can use to support AllTheThings.\n\nTwitch.tv/Crieve\nTwitch.tv/Goldenshacal",
 		["groups"] = {
 			i(49282, {	-- Big Battle Bear
 				["description"] = "Amazon Prime Gaming reward from March 30th 2023 till April 27th 2023.",
