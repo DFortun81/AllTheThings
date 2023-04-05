d3007f5b5b7f22e2f2b078349117a5afb3215fa0
gbrage <gbrage@kth.se>
2023-04-05 02:38:51 +0200

    -- Some More Secrets update

---
+++ .contrib/Parser/DATAS/11a - Secrets/09 - Shadowlands/Glimr.lua
@@ -0,0 +1,53 @@
+-------------------------------------------
+--     S E C R E T S     M O D U L E     --
+-------------------------------------------
+root(ROOTS.Secrets, tier(SL_TIER, bubbleDown({ ["timeline"] = { ADDED_9_0_1 } }, {
+	header(HEADERS.Spell, 329900, {	-- Glimr
+		["modelID"] = 96259,
+		["modelScale"] = 1.1,
+		["g"] = {
+			q(60605, {	-- Glimmerfin Scale
+				["provider"] = { "o", 352909 },  -- Glimmerfin Scale
+				["coord"] = { 18.4, 88.2, GRIZZLY_HILLS },
+			}),
+			q(60606, {	-- Glimmerfin Welcome
+				["sourceQuests"] = { 60605 },   -- Glimmerfin Scale
+				["provider"] = { "n", 25197 },  -- King Mrgl-Mrgl
+				["coord"] = { 43.5, 13.9, BOREAN_TUNDRA },
+			}),
+			q(60614, {	-- A Big Horkin' Task
+				["sourceQuests"] = { 60606 },   -- Glimmerfin Welcome
+				["provider"] = { "n", 169414 },  -- Glimmergut
+				["coord"] = { 17.8, 93.2, GRIZZLY_HILLS },
+			}),
+			q(60615, {	-- Seer of the Waves
+				["sourceQuests"] = { 60614 },   -- A Big Horkin' Task
+				["provider"] = { "n", 169414 },  -- Glimmergut
+				["coord"] = { 17.8, 93.2, GRIZZLY_HILLS },
+			}),
+			q(60616, {	-- Pearl in the Deeps
+				["sourceQuests"] = { 60615 },   -- Seer of the Waves
+				["provider"] = { "n", 169415 },  -- Glimmerfin Oracle
+				["coord"] = { 17.1, 93.8, GRIZZLY_HILLS },
+			}),
+			q(60617, {	-- Trainer's Test
+				["sourceQuests"] = { 60616 },   -- Pearl in the Deeps
+				["provider"] = { "n", 169415 },  -- Glimmerfin Oracle
+				["coord"] = { 17.1, 93.8, GRIZZLY_HILLS },
+			}),
+			q(60619, {	-- Wrap it Up
+				["sourceQuests"] = { 60617 },   -- Trainer's Test
+				["provider"] = { "n", 169415 },  -- Glimmerfin Oracle
+				["coord"] = { 17.1, 93.8, GRIZZLY_HILLS },
+			}),
+			q(60620, {	-- Guardian of the Smallest
+				["sourceQuests"] = { 60619 },   -- Wrap it Up
+				["provider"] = { "n", 169415 },  -- Glimmerfin Oracle
+				["coord"] = { 17.1, 93.8, GRIZZLY_HILLS },
+				["g"] = {
+					i(180034),  -- Glimr's Cracked Egg
+				},
+			}),
+		},
+	})
+})));
