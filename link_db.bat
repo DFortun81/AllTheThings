9fb9575769027208499486224866495423be4fea
Myrhial <8618272+ninaarens@users.noreply.github.com>
2023-04-14 12:36:23 +0200

    Updated link_db so that ptr and ptr2 folder can work without needing additional Git clones. Tested and verified on my machine. Please backup your own stuff before testing! But hopefully this really simplifies stuff :)

--- link_db.bat
+++ link_db.bat
@@ -1,3 +1,50 @@
 :: Remove real /db folder in PTR WoW and hard link the one from here
+:: This needs to be execute from the _retail_ folder!
+
 @RD /S /Q "..\..\..\..\_ptr_\Interface\Addons\AllTheThings\db"
 mklink /J ..\..\..\..\_ptr_\Interface\Addons\AllTheThings\db ptr_db
+
+@RD /S /Q "..\..\..\..\_ptr_\Interface\Addons\AllTheThings\assets"
+mklink /J ..\..\..\..\_ptr_\Interface\Addons\AllTheThings\assets assets
+
+@RD /S /Q "..\..\..\..\_ptr_\Interface\Addons\AllTheThings\lib"
+mklink /J ..\..\..\..\_ptr_\Interface\Addons\AllTheThings\lib lib
+
+@RD /S /Q "..\..\..\..\_ptr_\Interface\Addons\AllTheThings\locales"
+mklink /J ..\..\..\..\_ptr_\Interface\Addons\AllTheThings\locales locales
+
+@RD /S /Q "..\..\..\..\_ptr_\Interface\Addons\AllTheThings\src"
+mklink /J ..\..\..\..\_ptr_\Interface\Addons\AllTheThings\src src
+
+@del /Q "..\..\..\..\_ptr_\Interface\Addons\AllTheThings\AllTheThings.lua"
+mklink /H ..\..\..\..\_ptr_\Interface\Addons\AllTheThings\AllTheThings.lua AllTheThings.lua
+
+@del /Q "..\..\..\..\_ptr_\Interface\Addons\AllTheThings\AllTheThings.toc"
+mklink /H ..\..\..\..\_ptr_\Interface\Addons\AllTheThings\AllTheThings.toc AllTheThings.toc
+
+@del /Q "..\..\..\..\_ptr_\Interface\Addons\AllTheThings\Settings.lua"
+mklink /H ..\..\..\..\_ptr_\Interface\Addons\AllTheThings\Settings.lua Settings.lua
+
+@RD /S /Q "..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\db"
+mklink /J ..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\db ptr_db
+
+@RD /S /Q "..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\assets"
+mklink /J ..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\assets assets
+
+@RD /S /Q "..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\lib"
+mklink /J ..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\lib lib
+
+@RD /S /Q "..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\locales"
+mklink /J ..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\locales locales
+
+@RD /S /Q "..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\src"
+mklink /J ..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\src src
+
+@del /Q "..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\AllTheThings.lua"
+mklink /H ..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\AllTheThings.lua AllTheThings.lua
+
+@del /Q "..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\AllTheThings.toc"
+mklink /H ..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\AllTheThings.toc AllTheThings.toc
+
+@del /Q "..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\Settings.lua"
+mklink /H ..\..\..\..\_ptr2_\Interface\Addons\AllTheThings\Settings.lua Settings.lua
