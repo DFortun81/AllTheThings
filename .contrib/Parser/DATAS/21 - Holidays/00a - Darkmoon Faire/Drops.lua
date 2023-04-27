9c2a0ec839ee60144d5c1972609703ba4fc43b01
Darkal31 <michalbert@seznam.cz>
2023-04-27 17:12:13 +0200

    Added Darkmoon Drop items.

---
+++ .contrib/Parser/DATAS/21 - Holidays/00a - Darkmoon Faire/Drops.lua
@@ -0,0 +1,65 @@
+--------------------------------------------
+--     H O L I D A Y S  M O D U L E       --
+--------------------------------------------
+
+root(ROOTS.Holidays, m(407, bubbleDown({ ["u"] = DARKMOON_FAIRE }, {	-- Darkmoon Island
+	n(DROPS, bubbleDownSelf({ ["isMonthly"] = true, }, {
+		-- PvE
+		i(71635, {	-- Imbued Crystal
+			["description"] = "Rewards 10x Darkmoon Prize Ticket",
+			["g"] = {
+				currency(515),	-- Darkmoon Prize Ticket
+			},
+		}),
+		i(71636, {	-- Monstrous Egg
+			["description"] = "Rewards 10x Darkmoon Prize Ticket",
+			["g"] = {
+				currency(515),	-- Darkmoon Prize Ticket
+			},
+		}),
+		i(71637, {	-- Mysterious Grimoire
+			["description"] = "Rewards 10x Darkmoon Prize Ticket",
+			["g"] = {
+				currency(515),	-- Darkmoon Prize Ticket
+			},
+		}),
+		i(71638, {	-- Ornate Weapon
+			["description"] = "Rewards 10x Darkmoon Prize Ticket",
+			["g"] = {
+				currency(515),	-- Darkmoon Prize Ticket
+			},
+		}),
+		i(71716, {	-- Soothsayer's Runes
+			["description"] = "Rewards 10x Darkmoon Prize Ticket",
+			["g"] = {
+				currency(515),	-- Darkmoon Prize Ticket
+			},
+		}),
+		i(71715, {	-- A Treatise on Strategy
+			["description"] = "Rewards 15x Darkmoon Prize Ticket",
+			["g"] = {
+				currency(515),	-- Darkmoon Prize Ticket
+			},
+		}),
+		
+		-- PvP
+		i(71951, {	-- Banner of the Fallen
+			["description"] = "Rewards 5x Darkmoon Prize Ticket",
+			["g"] = {
+				currency(515),	-- Darkmoon Prize Ticket
+			},
+		}),
+		i(71952, {	-- Captured Insignia
+			["description"] = "Rewards 5x Darkmoon Prize Ticket",
+			["g"] = {
+				currency(515),	-- Darkmoon Prize Ticket
+			},
+		}),
+		i(71953, {	-- Fallen Adventurer's Journal
+			["description"] = "Rewards 5x Darkmoon Prize Ticket",
+			["g"] = {
+				currency(515),	-- Darkmoon Prize Ticket
+			},
+		})
+	})),
+})));
