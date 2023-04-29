c687be98fe4653d27e4c6923cd69ffef40aee703
Goldenshacal <goldenshacal@gmail.com>
2023-04-29 22:36:24 +0200

    some work on the rated pvp file

--- .contrib/Parser/DATAS/09 - PvP/Rated.lua
+++ .contrib/Parser/DATAS/09 - PvP/Rated.lua
@@ -3,12 +3,9 @@
 -----------------------------------------------
 
 root(ROOTS.PVP, pvp(n(-240, {	-- Rated
-	filter(RECIPES, bubbleDown(
-	{
-		["timeline"] = { "removed 8.0.1" },
-		["description"] = "This had a chance to drop from any rated battleground win during Legion, but has not been seen since Legion ended.",
-	},
-	{
+	filter(RECIPES, bubbleDown({ ["timeline"] = { "removed 8.0.1" }, }, {
+		-- ["description"] = "This had a chance to drop from any rated battleground win during Legion, but has not been seen since Legion ended.",
+		-- Added to Vendor in 10.0.7
 		i(137975, {	-- Pattern: Imbued Silkweave Epaulets [Rank 3] (RECIPE!)
 			["requireSkill"] = TAILORING,
 		}),
@@ -23,12 +20,12 @@ root(ROOTS.PVP, pvp(n(-240, {	-- Rated
 		}),
 	})),
 	i(103533, {	-- Vicious Saddle
-		-- #if AFTER 10.0.2
+		-- #if AFTER DF
 		["description"] = "Offered as Season Reward Cap after you received your Seasonal Mount, up to 10 times per Season." ..
-			" Any win (3 or more rounds for Solo Shuffle) above 1000 rating will reward:\n\n10 Points in 2v2 (0.42%)\n\n30 Points in 3v3 (1.25%)\n\n50 Points in Solo Shuffle (2.08%)\n\n60 Points in RBG (2.5%)",
+			"Any win (3 round wins or more for Solo Shuffle) above 1000 rating will reward:\n\n10 Points in 2v2 (0.42%)\n\n30 Points in 3v3 (1.25%)\n\n50 Points in Solo Shuffle (2.08%)\n\n60 Points in RBG (2.5%)",
 		-- else
 		["description"] = "Offered as Season Reward Cap after you received your Seasonal Mount, up to 10 times per Season." ..
-			" Any win above 1000 rating will reward:\n\n10 Points in 2v2 (0.42%)\n\n30 Points in 3v3 (1.25%)\n\n60 Points in RBG (2.5%)",
+			"Any win above 1000 rating will reward:\n\n10 Points in 2v2 (0.42%)\n\n30 Points in 3v3 (1.25%)\n\n60 Points in RBG (2.5%)",
 		-- #endif
 	}),
 	i(165717),	-- Steel Strong Box, 2v2/3v3/RBG sometimes on Lose, contains only BFA Mats - Ally
