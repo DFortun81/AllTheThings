890e226ec4d420b42ac0cc014792508649cafb37
NameXFace <margomilo015@gmail.com>
2023-05-06 00:27:35 -0400

    more specific directions added (pets)

--- .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Maldraxxus/Battle Pet.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Maldraxxus/Battle Pet.lua
@@ -5,7 +5,12 @@ root(ROOTS.Zones, m(SHADOWLANDS, bubbleDown({ ["timeline"] = { ADDED_9_0_2 } }, 
 	m(MALDRAXXUS, {
 		petbattle(filter(BATTLE_PETS, {
 			pet(3051,	{	-- Animated Cruor (PET!)
-				["description"] = "Found in the House of Constructs and northeast of Theatre of Pain.",
+				["coords"] = {
+					{ 31.8, 28.6, MALDRAXXUS },
+					{ 56.2, 41.8, MALDRAXXUS },
+					{ 32.0, 23.4, MALDRAXXUS },
+				},
+				["description"] = "Found commonly in the House of Constructs and northeast of Theatre of Pain.",
 			}),
 			pet(3050,	{	-- Bleak Skitterer (PET!)
 				["coords"] = {
@@ -20,13 +25,17 @@ root(ROOTS.Zones, m(SHADOWLANDS, bubbleDown({ ["timeline"] = { ADDED_9_0_2 } }, 
 				["description"] = "Normally found only around this coord.",
 			}),
 			pet(3083,	{	-- Crawbat (PET!)
-				["description"] = "Found around the ToP outside of the arena.",
+				["description"] = "Found commonly around the outside of the ToP arena.",
 			}),
 			pet(3052,	{	-- Necroray Spawnling (PET!)
+				["coords"] = {
+					{ 73.6, 51.2, MALDRAXXUS },
+					{ 45.0, 30.4, MALDRAXXUS },
+				},
 				["description"] = "Found around the green slime pools (1) above the House of Plagues and (2) SW of the House of Eyes.",
 			}),
 			pet(3049,	{	-- Pulsating Maggot (PET!)
-				["description"] = "Found commonly around the outside of the ToP",
+				["description"] = "Found commonly around the outside of the ToP arena.",
 			}),
 		})),
 	}),
