6b1259c291c1d414e0506c3329353bb1247b19ec
Sluimerstand <Ruben.Sluimer@gmail.com>
2023-05-06 15:32:33 +0200

    Moved Zoom! out of the Family Familiar achievement

--- locales/en_auto.lua
+++ locales/en_auto.lua
@@ -2753,7 +2753,8 @@ local keys = {
 [75526]="First Rock Mouse",
 [75527]="Second Rock Mouse",
 [75534]="Third Rock Mouse",
-[75535]="Fourth Rock Mouse"}}
+[75535]="Fourth Rock Mouse",
+[76118]="Accept Book from Wrathion"}}
 
 local L = app.L;
 for k,t in pairs(keys) do
