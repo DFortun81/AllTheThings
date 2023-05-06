2974a6c184998143ee7a3b5a02803906a84ccf22
NameXFace <margomilo015@gmail.com>
2023-05-05 22:40:14 -0400

    Ardenweald pets

--- .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Ardenweald/Battle Pet.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/Ardenweald/Battle Pet.lua
@@ -4,12 +4,28 @@
 root(ROOTS.Zones, m(SHADOWLANDS, bubbleDown({ ["timeline"] = { ADDED_9_0_2 } }, {
 	m(ARDENWEALD, {
 		petbattle(filter(BATTLE_PETS, {
-			pet(3081),	-- Decay Grub (PET!)
-			pet(3021),	-- Deepwood Leaper (PET!)
-			pet(2919),	-- Gorm Rootstinger (PET!)
-			pet(3082),	-- Starmoth (PET!)
-			pet(2924),	-- Tranquil Wader (PET!)
-			pet(3080),	-- Verdant Kit (PET!)
+			pet(3081,	{	-- Decay Grub (PET!)
+				["description"] = "Found around Heartwood Grove.",
+			}),
+			pet(3021,	{	-- Deepwood Leaper (PET!)
+				["description"] = "Found in most of southeast Ardenweald.",
+			}),
+			pet(2919,	{	-- Gorm Rootstinger (PET!)
+				["coords"] = {
+					{ 68.8, 56.6, ARDENWEALD },
+					{ 30.0, 50.6, ARDENWEALD },
+				},
+				["description"] = "Found in the areas around these coords.",
+			}),
+			pet(3082,	{	-- Starmoth (PET!)
+				["description"] = "Found south of Glitterfall Basin.",
+			}),
+			pet(2924,	{	-- Tranquil Wader (PET!)
+				["description"] = "Found between Hibernal Hollow and Heart of the Forest.",
+			}),
+			pet(3080,	{	-- Verdant Kit (PET!)
+				["description"] = "Found west of Heart of the Forest.",
+			}),
 		})),
 	}),
 })));
