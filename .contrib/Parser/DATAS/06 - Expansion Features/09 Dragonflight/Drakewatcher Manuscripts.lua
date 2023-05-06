b6f36f2abe175747a5fd195f5224fd6cecaab0b2
Joshua Nelson <JoshuaAndrewN@gmail.com>
2023-05-06 07:16:30 -0500

    Added back removed Pronged Tail DM.

--- .contrib/Parser/DATAS/06 - Expansion Features/09 Dragonflight/Drakewatcher Manuscripts.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/09 Dragonflight/Drakewatcher Manuscripts.lua
@@ -964,6 +964,7 @@ root(ROOTS.ExpansionFeatures, tier(DF_TIER, bubbleDown({ ["timeline"] = TIMELINE
 		header(HEADERS.Item, 194034, {	-- Renewed Proto-Drake
 			i(197364),	-- Renewed Proto-Drake: Short Spiked Crest (DM!)
 			i(202275),	-- Renewed Proto-Drake: Plated Jaw (DM!)
+			i(202280),	-- Renewed Proto-Drake: Pronged Tail (DM!)
 		}),
 		header(HEADERS.Item, 204361, {	-- Winding Slitherdrake
 			i(203338),	-- Winding Slitherdrake: Antler Horns
