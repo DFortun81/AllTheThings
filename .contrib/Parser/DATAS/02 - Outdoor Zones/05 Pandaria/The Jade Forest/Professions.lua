325fed3d3a93297d02c5014fba4044fa044d65f5
Sluimerstand <Ruben.Sluimer@gmail.com>
2023-05-19 17:52:30 +0200

    Minor skillReq tweak

--- .contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/The Jade Forest/Professions.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/The Jade Forest/Professions.lua
@@ -7,7 +7,7 @@ root(ROOTS.Zones, m(PANDARIA, {
 		n(PROFESSIONS, {
 			prof(ALCHEMY, {
 				o(211424, {	-- Alchemy Scroll
-					["description"] = "It will only appear to alchemists who don't yet know the recipe. Might require a Pandaria Alchemy skill level of 75, but this is unconfirmed.",
+					["description"] = "It will only appear to alchemists who don't yet know the recipe. May require Pandaria Alchemy 50.",
 					["coord"] = { 49.7, 54.4, THE_JADE_FOREST },
 					["g"] = {
 						recipe(114769),	-- Flask of Spring Blossoms
