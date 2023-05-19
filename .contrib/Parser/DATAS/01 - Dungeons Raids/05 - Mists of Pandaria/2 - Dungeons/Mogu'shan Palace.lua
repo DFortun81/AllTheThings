045707d3ef6f7e2f163605a664db506755fc708c
NameXFace <margomilo015@gmail.com>
2023-05-19 16:31:40 -0400

    finishing MSP :)

--- .contrib/Parser/DATAS/01 - Dungeons Raids/05 - Mists of Pandaria/2 - Dungeons/Mogu'shan Palace.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/05 - Mists of Pandaria/2 - Dungeons/Mogu'shan Palace.lua
@@ -2,7 +2,7 @@
 --   D U N G E O N S  &  R A I D S  M O D U L E    --
 -----------------------------------------------------
 
-_.Instances = { tier(MOP_TIER, {
+_.Instances = { tier(MOP_TIER, bubbleDown({ ["timeline"] = ADDED_5_0_4 }, {
 	inst(321, {	-- Mogu'shan Palace
 		["coord"] = { 80.5, 33.0, VALE_OF_ETERNAL_BLOSSOMS },
 		["maps"] = { 453, 454, 455 },
@@ -27,29 +27,29 @@ _.Instances = { tier(MOP_TIER, {
 				q(31360, {	-- A New Lesson for the Master
 					["provider"] = { "n", 64432 },	-- Sinan the Dreamer
 					["g"] = {
-						i(87333),	-- Watchful Dreamer's Trousers
+						i(87337),	-- Deposer's Leggings
 						i(87334),	-- Dreamer's Vigil Leggings
 						i(87335),	-- Leggings of Clever Entrapment
-						i(87336),	-- Legplates of the Scattered Tribes
-						i(87337),	-- Deposer's Leggings
-						i(87338),	-- Leggings of the Empty Throne
 						i(87339),	-- Leggings of Forceful Instruction
+						i(87338),	-- Leggings of the Empty Throne
+						i(87336),	-- Legplates of the Scattered Tribes
 						i(87340),	-- Mogu Warlord Legguards
+						i(87333),	-- Watchful Dreamer's Trousers
 						i(87341),	-- Weaponmaster's Legplates
 					},
 				}),
 				q(31357, {	-- Relics of the Four Kings
 					["provider"] = { "n", 64432 },	-- Sinan the Dreamer
 					["g"] = {
-						i(87342),	-- Subjugation Bracers
+						i(87346),	-- Armbands of the Reawakened
+						i(87349),	-- Armplates of Martial Artistry
+						i(87345),	-- Bindings of Impeccable Strategy
+						i(87348),	-- Bracers of Inner Knowledge
 						i(87343),	-- Bracers of Swift Wrath
 						i(87344),	-- Cuffs of the Endless Shadow
-						i(87345),	-- Bindings of Impeccable Strategy
-						i(87346),	-- Armbands of the Reawakened
 						i(87347),	-- Lost Heritage Bracers
-						i(87348),	-- Bracers of Inner Knowledge
-						i(87349),	-- Armplates of Martial Artistry
 						i(87350),	-- Poet-King's Vambraces
+						i(87342),	-- Subjugation Bracers
 					},
 				}),
 			}),
@@ -80,61 +80,122 @@ _.Instances = { tier(MOP_TIER, {
 
 						-- Removed
 						i(85178, { -- Conflagrating Gloves
-							["timeline"] = { ADDED_5_0_1, REMOVED_7_1_5 },
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
 						}),
 						i(85175, { -- Crest of the Clan Lords
-							["timeline"] = { ADDED_5_0_1, REMOVED_7_1_5 },
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
 						}),
 						i(85176, { -- Hurricane Belt
-							["timeline"] = { ADDED_5_0_1, REMOVED_7_1_5 },
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
 						}),
 						i(85179, { -- Meteoric Greathelm
-							["timeline"] = { ADDED_5_0_1, REMOVED_7_1_5 },
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
 						}),
 					},
 				}),
 				e(690, {	-- Gekkan
 					["crs"] = { 61243 },	-- Gekkan
 					["g"] = {
-						i(144148),	-- Claws of Gekkan
-						i(144147),	-- Cloak of Cleansing Flames
-						i(144149),	-- Hexxer's Lethargic Gloves
-						i(143992),	-- Glintrok Sollerets
-						i(144146),	-- Iron Protector Talisman
-						un(REMOVED_FROM_GAME, i(85183)),	-- Claws of Gekkan (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85182)),	-- Cloak of Cleansing Flames (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85184)),	-- Hexxer's Lethargic Gloves (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85180)),	-- Glintrok Sollerets (7.1 - Removed from Game!)
+						-- Available
+						i(144148, { -- Claws of Gekkan
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(144147, { -- Cloak of Cleansing Flames
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(143992, { -- Glintrok Sollerets
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(144149, { -- Hexxer's Lethargic Gloves
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(144146, { -- Iron Protector Talisman
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+
+						-- Removed
+						i(85183, { -- Claws of Gekkan
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85182, { -- Cloak of Cleansing Flames
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85184, { -- Hexxer's Lethargic Gloves
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85180, { -- Glintrok Sollerets
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
 					},
 				}),
 				e(698, {	-- Xin the Weaponmaster
 					["crs"] = { 61398 },	-- Xin the Weaponmaster
 					["g"] = {
+						-- Avalable
 						ach(6755),	-- Mogu'shan Palace
-						i(144150),	-- Ghostheart
-						i(144214),	-- Mogu'Dar, Blade of the Thousand Slaves
-						i(144154),	-- Firescribe Dagger
-						i(144155),	-- Mindcapture Pendant
-						i(143995),	-- Regal Silk Shoulderpads
-						i(143956),	-- Mind's Eye Breastplate
-						i(144153),	-- Groundshaker Bracer
-						i(144151),	-- Axebreaker Gauntlets
-						i(143993),	-- Boots of Plummeting Death
-						i(143994),	-- Soulbinder Treads
-						i(144152),	-- Blade Trap Signet
-						un(REMOVED_FROM_GAME, i(85185)),	-- Ghostheart (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85190)),	-- Firescribe Dagger (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85194)),	-- Regal Silk Shoulderpads (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85193)),	-- Mind's Eye Breastplate (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85189)),	-- Groundshaker Bracer (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85186)),	-- Axebreaker Gauntlets (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85187)),	-- Boots of Plummeting Death (7.1 - Removed from Game!)
-						un(REMOVED_FROM_GAME, i(85192)),	-- Soulbinder Treads (7.1 - Removed from Game!)
+						i(144151, { -- Axebreaker Gauntlets
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(144152, { -- Blade Trap Signet
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(143993, { -- Boots of Plummeting Death
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(144154, { -- Firescribe Dagger
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(144150, { -- Ghostheart
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(144153, { -- Groundshaker Bracer
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(144155, { -- Mindcapture Pendant
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(143956, { -- Mind's Eye Breastplate
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(144214, { -- Mogu'Dar, Blade of the Thousand Slaves
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(143995, { -- Regal Silk Shoulderpads
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+						i(143994, { -- Soulbinder Treads
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+
+						-- Removed
+						i(85186, { -- Axebreaker Gauntlets
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85187, { -- Boots of Plummeting Death
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85190, { -- Firescribe Dagger
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85185, { -- Ghostheart
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85189, { -- Groundshaker Bracer
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85193, { -- Mind's Eye Breastplate
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85194, { -- Regal Silk Shoulderpads
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
+						i(85192, { -- Soulbinder Treads
+							["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+						}),
 					},
 				}),
 			}),
 			d(HEROIC_DUNGEON, {
-				["lvl"] = 90,
 				["g"] = {
 					ach(6713),	-- Quarrelsome Quilen Quintet
 					e(708, {	-- Trial of the King
@@ -144,58 +205,143 @@ _.Instances = { tier(MOP_TIER, {
 							61444,	-- Ming the Cunning
 						},
 						["g"] = {
-							i(144020),	-- Crest of the Clan Lords
-							i(144021),	-- Meteoric Greathelm
-							i(144144),	-- Whirling Dervish Choker
-							i(144145),	-- Conflagrating Gloves
-							i(144143),	-- Hurricane Belt
-							un(REMOVED_FROM_GAME, i(81237)),	-- Crest of the Clan Lords (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81241)),	-- Meteoric Greathelm (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81240)),	-- Conflagrating Gloves (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81238)),	-- Hurricane Belt (7.1 - Removed from Game!)
+							-- Available
+							i(144145, { -- Conflagrating Gloves
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144020, { -- Crest of the Clan Lords
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144143, { -- Hurricane Belt
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144021, { -- Meteoric Greathelm
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144144, { -- Whirling Dervish Choker
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+
+							-- Removed
+							i(81240, { -- Conflagrating Gloves
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81237, { -- Crest of the Clan Lords
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81238, { -- Hurricane Belt
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81241, { -- Meteoric Greathelm
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
 						},
 					}),
 					e(690, {	-- Gekkan
 						["crs"] = { 61243 },	-- Gekkan
 						["g"] = {
+							-- Available
 							ach(6478),	-- Glintrok N' Roll
-							i(144148),	-- Claws of Gekkan
-							i(144147),	-- Cloak of Cleansing Flames
-							i(144149),	-- Hexxer's Lethargic Gloves
-							i(143992),	-- Glintrok Sollerets
-							i(144146),	-- Iron Protector Talisman
-							un(REMOVED_FROM_GAME, i(81245)),	-- Claws of Gekkan (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81244)),	-- Cloak of Cleansing Flames (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81246)),	-- Hexxer's Lethargic Gloves (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81242)),	-- Glintrok Sollerets (7.1 - Removed from Game!)
+							i(144148, { -- Claws of Gekkan
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144147, { -- Cloak of Cleansing Flames
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(143992, { -- Glintrok Sollerets
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144149, { -- Hexxer's Lethargic Gloves
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144146, { -- Iron Protector Talisman
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+
+							-- Removed
+							i(81245, { -- Claws of Gekkan
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81244, { -- Cloak of Cleansing Flames
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81242, { -- Glintrok Sollerets
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81246, { -- Hexxer's Lethargic Gloves
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
 						},
 					}),
 					e(698, {	-- Xin the Weaponmaster
 						["crs"] = { 61398 },	-- Xin the Weaponmaster
 						["g"] = {
+							-- Available
 							ach(6756),	-- Heroic: Mogu'shan Palace
 							ach(6766),	-- Heroic: Mogu'shan Palace Guild Run
 							ach(6736),	-- What Does This Button Do?
-							i(144150),	-- Ghostheart
-							i(144214),	-- Mogu'Dar, Blade of the Thousand Slaves
-							i(144154),	-- Firescribe Dagger
-							i(144155),	-- Mindcapture Pendant
-							i(143995),	-- Regal Silk Shoulderpads
-							i(143956),	-- Mind's Eye Breastplate
-							i(144153),	-- Groundshaker Bracer
-							i(144151),	-- Axebreaker Gauntlets
-							i(143993),	-- Boots of Plummeting Death
-							i(143994),	-- Soulbinder Treads
-							i(144152),	-- Blade Trap Signet
-							un(REMOVED_FROM_GAME, i(81247)),	-- Ghostheart (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(87542)),	-- Mogu'Dar, Blade of the Thousand Slaves (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81253)),	-- Firescribe Dagger (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81257)),	-- Regal Silk Shoulderpads (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81256)),	-- Mind's Eye Breastplate (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81252)),	-- Groundshaker Bracer (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81248)),	-- Axebreaker Gauntlets (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81249)),	-- Boots of Plummeting Death (7.1 - Removed from Game!)
-							un(REMOVED_FROM_GAME, i(81255)),	-- Soulbinder Treads (7.1 - Removed from Game!)
+							i(144151, { -- Axebreaker Gauntlets
+							["timeline"] = { ADDED_7_1_5 },
+						}),
+							i(144152, { -- Blade Trap Signet
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(143993, { -- Boots of Plummeting Death
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144154, { -- Firescribe Dagger
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144150, { -- Ghostheart
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144153, { -- Groundshaker Bracer
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(143956, { -- Mind's Eye Breastplate
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144155, { -- Mindcapture Pendant
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(144214, { -- Mogu'Dar, Blade of the Thousand Slaves
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(143995, { -- Regal Silk Shoulderpads
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+							i(143994, { -- Soulbinder Treads
+								["timeline"] = { ADDED_7_1_5 },
+							}),
+
+							-- Removed
+							i(81248, { -- Axebreaker Gauntlets
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81249, { -- Boots of Plummeting Death
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81253, { -- Firescribe Dagger
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81247, { -- Ghostheart
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81252, { -- Groundshaker Bracer
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81256, { -- Mind's Eye Breastplate
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(87542, { -- Mogu'Dar, Blade of the Thousand Slaves
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81257, { -- Regal Silk Shoulderpads
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
+							i(81255, { -- Soulbinder Treads
+								["timeline"] = { ADDED_5_0_4, REMOVED_7_1_5 },
+							}),
 						},
 					}),
 				},
@@ -209,7 +355,8 @@ _.Instances = { tier(MOP_TIER, {
 			-- #endif
 		},
 	}),
-})};
+}))};
+
 root(ROOTS.HiddenQuestTriggers, {
 	tier(MOP_TIER, {
 		q(31634),	-- Mogu'shan Palace Challenge Mode - Bronze Addition (Nth)
