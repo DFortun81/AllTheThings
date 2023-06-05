b64046cfc25b3f330435703543d05d68ee944f6d
Crieve <dylanfortune@live.com>
2023-06-05 13:53:31 -0700

    Moved the last bit of main.lua to Miscellaneous.

--- .contrib/Parser/_main.lua
+++ .contrib/Parser/_main.lua
@@ -1,14 +1,6 @@
 AllTheThings = {};
 _ = AllTheThings;
 
--- A unique value which indicates the actual field within a group should not actually be Parsed (useful to bypass groups within sharedData/bubbleDown)
-IGNORED_VALUE = 'E9B4EEAE-88EA-44F8-B7DF-0831B5F99A44'
-TIMEWALKING_DUNGEON_CREATURE_IDS = {};
-POST_PROCESSING_FUNCTIONS = {};
-
--- Special Values
-DF_ACCOUNT_CAMPAIGN_QUEST = 72366;	-- Account Dragonflight Campaign Completed
-
 -- #IMPORT: ..\\lib\\Constants
 -- #IMPORT: ..\\lib\\Functions
 -- #IMPORT: ..\\lib\\Templates
