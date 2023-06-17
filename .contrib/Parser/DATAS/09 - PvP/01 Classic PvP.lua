2492cc1af8cb40c89299ea228674112b7ab2c4b4
Crieve <dylanfortune@live.com>
2023-06-17 11:30:37 -0700

    Honor Titles now have their timelines attached directly.

--- .contrib/Parser/DATAS/09 - PvP/01 Classic PvP.lua
+++ .contrib/Parser/DATAS/09 - PvP/01 Classic PvP.lua
@@ -26,28 +26,22 @@ end
 root(ROOTS.PVP, run(MarkOfWHOOOWHATNow, pvp(tier(CLASSIC_TIER, {
 	["description"] = "In order to collect these, you need to have the original title associated with the gear.",
 	["groups"] = {
-		n(HONOR_TITLES, {
-			["timeline"] = {
-				"added 1.11.1.6692",
-				"removed 2.0.0.13287"
-			},
-			["groups"] = {
-				pvprank(1),		-- Private / Scout
-				pvprank(2),		-- Corporal / Grunt
-				pvprank(3),		-- Sergeant / Sergeant
-				pvprank(4),		-- Master Sergeant / Senior Sergeant
-				pvprank(5),		-- Sergeant Major / First Sergeant
-				pvprank(6),		-- Knight / Stone Guard
-				pvprank(7),		-- Knight-Lieutenant / Blood Guard
-				pvprank(8),		-- Knight-Captain / Legionnaire
-				pvprank(9),		-- Knight-Champion / Centurion
-				pvprank(10),	-- Lieutenant Commander / Champion
-				pvprank(11),	-- Commander / Lieutenant General
-				pvprank(12),	-- Marshal / General
-				pvprank(13),	-- Field Marshal / Warlord
-				pvprank(14),	-- Grand Marshal / High Warlord
-			},
-		}),
+		n(HONOR_TITLES, bubbleDownSelf({ ["timeline"] = { "added 1.11.1.6692", "removed 2.0.0.13287" }}, {
+			pvprank(1),		-- Private / Scout
+			pvprank(2),		-- Corporal / Grunt
+			pvprank(3),		-- Sergeant / Sergeant
+			pvprank(4),		-- Master Sergeant / Senior Sergeant
+			pvprank(5),		-- Sergeant Major / First Sergeant
+			pvprank(6),		-- Knight / Stone Guard
+			pvprank(7),		-- Knight-Lieutenant / Blood Guard
+			pvprank(8),		-- Knight-Captain / Legionnaire
+			pvprank(9),		-- Knight-Champion / Centurion
+			pvprank(10),	-- Lieutenant Commander / Champion
+			pvprank(11),	-- Commander / Lieutenant General
+			pvprank(12),	-- Marshal / General
+			pvprank(13),	-- Field Marshal / Warlord
+			pvprank(14),	-- Grand Marshal / High Warlord
+		})),
 		n(FACTION_HEADER_ALLIANCE, {
 			["races"] = ALLIANCE_ONLY,
 			["groups"] = {
