41377c89497e23868803a6618b882f2dfad5b8e5
Goldenshacal <goldenshacal@gmail.com>
2023-06-19 03:11:43 +0200

    specific upgrade stones cant come from wq's, only neutral stones can

--- .contrib/Parser/DATAS/15 - Pet Battles/World Quests.lua
+++ .contrib/Parser/DATAS/15 - Pet Battles/World Quests.lua
@@ -6,15 +6,5 @@ root(ROOTS.PetBattles, petbattle({
 		i(86143),	-- Battle Pet Bandage
 		i(98715),	-- Marked Flawless Battle Stone
 		i(163036),	-- Polished Pet Charm
-		--
-		i(92679),	-- Flawless Aquatic Battle-Stone
-		i(92675),	-- Flawless Beast Battle-Stone
-		i(92676),	-- Flawless Critter Battle-Stone
-		i(92665),	-- Flawless Elemental Battle-Stone
-		i(92677),	-- Flawless Flying Battle-Stone
-		i(92682),	-- Flawless Humanoid Battle-Stone
-		i(92678),	-- Flawless Magic Battle-Stone
-		i(92680),	-- Flawless Mechanical Battle-Stone
-		i(92681),	-- Flawless Undead Battle-Stone
 	}),
 }));
