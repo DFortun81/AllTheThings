99afeda775171079ced73217284dfde013ec227c
Goldenshacal <goldenshacal@gmail.com>
2023-06-19 04:09:29 +0200

    moved pet charm wq rewards to their specific expansion. shiny pet charms are now under legion>wq & not pet battle>wq>legion

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/8 - World Quests.lua
@@ -0,0 +1,10 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+root(ROOTS.Zones, bubbleDown({ ["timeline"] = { "added 8.0.1" } }, {
+	m(ZANDALAR, {
+		n(WORLD_QUESTS, {
+			i(163036),	-- Polished Pet Charm
+		}),
+	}),
+}));
--- .contrib/Parser/DATAS/15 - Pet Battles/World Quests.lua
+++ .contrib/Parser/DATAS/15 - Pet Battles/World Quests.lua
@@ -5,14 +5,5 @@ root(ROOTS.PetBattles, petbattle({
 	n(WORLD_QUESTS, {
 		i(86143),	-- Battle Pet Bandage
 		i(98715),	-- Marked Flawless Battle Stone
-		tier(LEGION_TIER, {
-			i(116415),	-- Shiny Pet Charm
-		}),
-		tier(BFA_TIER, {
-			i(163036),	-- Polished Pet Charm
-		}),
-		tier(DF_TIER, {
-			i(163036),	-- Polished Pet Charm
-		}),
 	}),
 }));
