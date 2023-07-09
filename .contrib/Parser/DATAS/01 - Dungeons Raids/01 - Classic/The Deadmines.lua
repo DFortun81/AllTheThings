0d735e96ac581a24ec4ac5e6af72c1e44e2150a5
Crieve <dylanfortune@live.com>
2023-07-09 03:53:30 -0700

    Deadmines is now properly sorted with the rest of the dungeons in Classic.

---
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/The Deadmines.lua
@@ -0,0 +1,11 @@
+-----------------------------------------------------
+--   D U N G E O N S  &  R A I D S  M O D U L E    --
+-----------------------------------------------------
+-- #if BEFORE 6.0.1
+-- With how we sort instances, this is actually "Deadmines" after 6.0.1
+-- See the "Deadmines" file for the actual content. -Crieve
+-- My OCD was really hating that "The Deadmines" was before "Dire Maul" in Classic. :)
+root(ROOTS.Instances, tier(CLASSIC_TIER, {
+	DEADMINES_INSTANCE,
+}));
+-- #endif
