acff672219ee2507a6daa185d05891037cac559c
Alexandr Soft <37008350+AlexSoft1911@users.noreply.github.com>
2023-07-14 21:23:48 +0500

    ATA hqt

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Valdrakken/Aiding the Accord.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Valdrakken/Aiding the Accord.lua
@@ -87,5 +87,6 @@ root(ROOTS.HiddenQuestTriggers, m(DRAGON_ISLES, {
 		q(75862),	-- Aiding the Accord - Researchers - Quest Complete (spellID 410069)
 		q(75864),	-- Aiding the Accord - Suffusion - Quest Complete (spellID 410068)
 		q(75863),	-- Aiding the Accord - Cave - Quest Complete (spellID 410067)
+		q(77253),	-- Aiding the Accord - Time Rifts - Quest Complete (spellID 417862)
 	}),
 }));
