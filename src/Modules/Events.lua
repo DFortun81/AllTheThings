54fad426cb129e2b5394b60a3c01799830d5a915
Crieve <dylanfortune@live.com>
2023-07-26 16:31:52 -0700

    Added a couple more timewalking remaps for EU.

--- src/Modules/Events.lua
+++ src/Modules/Events.lua
@@ -22,7 +22,12 @@ local RemappedEventToMapID = {
 -- Event ID Remapping by Region
 local remapping = L.EVENT_REMAPPING;
 if GetCVar("portal") == "EU" then
-	remapping[1265] = 1263;	-- Legion Timewalking Event is different in EU.
+	remapping[622] = 559; -- EU BC Timewalking
+	remapping[616] = 562; -- EU Wrath Timewalking
+	remapping[628] = 587; -- EU Cata Timewalking
+	remapping[652] = 643; -- EU MoP Timewalking
+	remapping[1063] = 1056; -- EU WoD Timewalking
+	remapping[1265] = 1263;	-- EU Legion Timewalking
 end
 
 -- Event Cache
