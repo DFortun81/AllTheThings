38712c58b58b4fbb6938bfa6f6b27738b554f248
Crieve <dylanfortune@live.com>
2023-07-26 19:08:10 -0700

    The first entry in NYI Maps is in Legion, so let's not create the header until then.

--- .contrib/Parser/DATAS/17 - NYI/NYI Maps.lua
+++ .contrib/Parser/DATAS/17 - NYI/NYI Maps.lua
@@ -2,10 +2,15 @@
 --     N E V E R    I M P L E M E N T E D    M O D U L E     --
 ---------------------------------------------------------------
 -- Everything in this file hasn't been implemented yet.
-
+-- #if AFTER 7.2.0
 root(ROOTS.NeverImplemented, {
 	n(MAPS, {
+		-- #if AFTER 7.2.0
 		m(843),		-- Shado-Pan Showdown
+		-- #endif
+		-- #if AFTER 8.1.0
 		m(1335),	-- Cooking: Impossible
+		-- #endif
 	}),
 });
+-- #endif
