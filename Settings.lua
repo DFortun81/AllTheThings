76f6834991f1c73bfbbd0723a1324d1c88425162
gbrage <gbrage@kth.se>
2023-07-30 23:33:45 +0200

    -- Added Jezartroz, AlexSoft and Jenstonedart to the About Section. -- Added Hall of Section

--- Settings.lua
+++ Settings.lua
@@ -4342,7 +4342,7 @@ local child = settings:CreateOptionsPage(L["ABOUT_PAGE"], true)
 local textAbout = child:CreateTextLabel(L["TITLE"] .. L["ABOUT_1"])
 textAbout:SetPoint("TOPLEFT", child, 0, 0)
 
-local textShoutout = child:CreateTextLabel(L["ABOUT_2"] .. L["COLLECTED_ICON"] .. " " .. L["COLLECTED_APPEARANCE_ICON"] .. " " ..L["NOT_COLLECTED_ICON"] .. L["ABOUT_3"])
+local textShoutout = child:CreateTextLabel(L["ABOUT_2"] .. L["ABOUT_3"] .. L["ABOUT_4"] .. L["COLLECTED_ICON"] .. " " .. L["COLLECTED_APPEARANCE_ICON"] .. " " ..L["NOT_COLLECTED_ICON"] .. L["ABOUT_5"])
 textShoutout:SetPoint("BOTTOMLEFT", child:GetParent(), 0, 10)
 
 end)();
