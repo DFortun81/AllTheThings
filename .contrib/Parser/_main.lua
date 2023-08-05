6aa11ee5b79778436d948b9471efeb74e2f82e13
ImUnicke <imunicke+github@gmail.com>
2023-08-05 12:41:26 -0600

    Moved ItemDBConditional definition into earlier file to fix Source Harvesting in Parser

--- .contrib/Parser/_main.lua
+++ .contrib/Parser/_main.lua
@@ -2,6 +2,7 @@ AllTheThings = {};
 _ = AllTheThings;
 
 -- #IMPORT: ..\\lib\\Constants
+-- #IMPORT: ..\\lib\\DB
 -- #IMPORT: ..\\lib\\Functions
 -- #IMPORT: ..\\lib\\Headers
 -- #IMPORT: ..\\lib\\Structures
