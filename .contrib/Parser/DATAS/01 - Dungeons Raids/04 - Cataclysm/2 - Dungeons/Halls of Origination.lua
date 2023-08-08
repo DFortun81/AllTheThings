9bebf2a576c637114a8917b17ce39f27fca44e65
Sanya_de <marc-fetting@gmx.net>
2023-08-08 19:34:29 +0200

    updated cata dungeons to new (normal/heroic) layout, sorted loot alphabetically, cleaned up some structure

--- .contrib/Parser/DATAS/01 - Dungeons Raids/04 - Cataclysm/2 - Dungeons/Halls of Origination.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/04 - Cataclysm/2 - Dungeons/Halls of Origination.lua
@@ -1,15 +1,11 @@
 -----------------------------------------------------
 --   D U N G E O N S  &  R A I D S  M O D U L E    --
 -----------------------------------------------------
-
-root(ROOTS.Instances, tier(CATA_TIER, {
+local NormalPlus = {NORMAL_DUNGEON,HEROIC_DUNGEON};
+root(ROOTS.Instances, tier(CATA_TIER, bubbleDown({ ["timeline"] = ADDED_4_0_3 }, {
 	inst(70, {	-- Halls of Origination
-		["lvl"] = 83,
-		["maps"] = {
-			297,	-- The Vault of Lights
-			298,	-- Tomb of the Earthrager
-			299,	-- The Four Seats
-		},
+		["mapID"] = 297,
+ 		["maps"] = { 298, 299 },
 		["coord"] = { 71.81, 52.16, ULDUM },	-- Halls of Origination
 		["groups"] = {
 			n(QUESTS, {
@@ -32,185 +28,299 @@ root(ROOTS.Instances, tier(CATA_TIER, {
 					},
 				}),
 			}),
-			d(NORMAL_DUNGEON, {
-				n(ZONE_DROPS, {
-					i(56109),	-- Book of Origination
-					i(56110),	-- Charm of the Muse
-					i(56111),	-- Temple Band
-				}),
-				cr(39425, e(124, {	-- Temple Guardian Anhuur
-					i(56411),	-- Darkhowl Amulet
-					i(56409),	-- Poison Fang Bracers
-					i(56410),	-- Belt of Petrified Tears
-					i(56408),	-- Awakening Footfalls
-					i(56407),	-- Anhuur's Hymnal
-					un(REMOVED_FROM_GAME, i(55888)),	-- Darkhowl Amulet (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55886)),	-- Poison Fang Bracers (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55887)),	-- Belt of Petrified Tears (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55890)),	-- Awakening Footfalls (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55889)),	-- Anhuur's Hymnal (7.3.5 - Removed from Game!)
-				})),
-				cr(39428, e(125, {	-- Earthrager Ptah
-					i(56424),	-- Soul Releaser
-					i(56426),	-- Bulwark of the Primordial Mound
-					i(56422),	-- Mouth of the Earth
-					i(56425),	-- Breastplate of the Risen Land
-					i(56423),	-- Underworld Cord
-					un(REMOVED_FROM_GAME, i(56097)),	-- Soul Releaser (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56096)),	-- Bulwark of the Primordial Mound (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56095)),	-- Mouth of the Earth (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56093)),	-- Breastplate of the Risen Land (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56094)),	-- Underworld Cord (7.3.5 - Removed from Game!)
-				})),
-				cr(39788, e(126, {	-- Anraphet
-					i(157611),	-- Crown of Patient Vigil
-					i(57866),	-- Mantle of Soft Shadows
-					i(57868),	-- Anraphet's Regalia
-					i(57869),	-- Omega Breastplate
-					i(57870),	-- Alpha Bracers
-					i(57867),	-- Boots of Crumbling Ruin
-					un(REMOVED_FROM_GAME, i(57858)),	-- Mantle of Soft Shadows (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(57860)),	-- Anraphet's Regalia (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(57856)),	-- Omega Breastplate (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(57855)),	-- Alpha Bracers (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(57857)),	-- Boots of Crumbling Ruin (7.3.5 - Removed from Game!)
-				})),
-				cr(39587, e(127, {	-- Isiset, Construct of Magic
-					i(157609),	-- Scepter of Stargazing
-					i(56416),	-- Armguards of Unearthly Light
-					i(56413),	-- Legwraps of Astral Rain
-					i(56415),	-- Nova Band
-					i(56412),	-- Ring of Blinding Stars
-					i(56414),	-- Blood of Isiset
-					un(REMOVED_FROM_GAME, i(55992)),	-- Armguards of Unearthly Light (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55993)),	-- Legwraps of Astral Rain (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55996)),	-- Nova Band (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55994)),	-- Ring of Blinding Stars (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55995)),	-- Blood of Isiset (7.3.5 - Removed from Game!)
-				})),
-				cr(39731, e(128, {	-- Ammunae, Construct of Life
-					i(56421),	-- Seedling Pod
-					i(56419),	-- Bloodpetal Mantle
-					i(56417),	-- Robes of Rampant Growth
-					i(56418),	-- Band of Life Energy
-					un(REMOVED_FROM_GAME, i(55999)),	-- Seedling Pod (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55997)),	-- Bloodpetal Mantle (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(55998)),	-- Robes of Rampant Growth (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56000)),	-- Band of Life Energy (7.3.5 - Removed from Game!)
-				})),
-				cr(39732, e(129, {	-- Setesh, Construct of Destruction
-					i(57872),	-- Scepter of Power
-					i(57873),	-- Helm of Setesh
-					i(57871),	-- Helm of the Typhonic Beast
-					i(57874),	-- Hieroglyphic Vest
-					i(57875),	-- Chaotic Wrappings
-					un(REMOVED_FROM_GAME, i(57865)),	-- Scepter of Power (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(57861)),	-- Helm of Setesh (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(57864)),	-- Helm of the Typhonic Beast (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(57863)),	-- Hieroglyphic Vest (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(57862)),	-- Chaotic Wrappings (7.3.5 - Removed from Game!)
-				})),
-				cr(39378, e(130, {	-- Rajh, Construct of Sun
-					ach(4841),	-- Halls of Origination
-					i(56433),	-- Blade of the Burning Sun
-					i(56430),	-- Sun Strike
-					i(56434),	-- Solar Wind Cloak
-					i(56428),	-- Fingers of Light
-					i(56429),	-- Red Beam Cord
-					i(56435),	-- Legguards of Noon
-					i(56436),	-- Hekatic Slippers
-					i(56432),	-- Band of Rays
-					i(56427),	-- Left Eye of Rajh
-					i(56431),	-- Right Eye of Rajh
-					un(REMOVED_FROM_GAME, i(56108)),	-- Blade of the Burning Sun (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56101)),	-- Sun Strike (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56107)),	-- Solar Wind Cloak (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56099)),	-- Fingers of Light (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56098)),	-- Red Beam Cord (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56104)),	-- Legguards of Noon (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56105)),	-- Hekatic Slippers (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56106)),	-- Band of Rays (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56102)),	-- Left Eye of Rajh (7.3.5 - Removed from Game!)
-					un(REMOVED_FROM_GAME, i(56100)),	-- Right Eye of Rajh (7.3.5 - Removed from Game!)
-				})),
+			n(ZONE_DROPS, {
+				i(56109),	-- Book of Origination
+				i(56110),	-- Charm of the Muse
+				i(56111),	-- Temple Band
 			}),
+			d(NORMAL_DUNGEON, bubbleDownSelf({ ["timeline"] = REMOVED_7_3_5 }, {
+				e(124, {	-- Temple Guardian Anhuur
+					["crs"] = { 39425 },	-- Temple Guardian Anhuur
+					["groups"] = {				
+						i(55889), -- Anhuur's Hymnal
+						i(55890), -- Awakening Footfalls
+						i(55887), -- Belt of Petrified Tears
+						i(55888), -- Darkhowl Amulet
+						i(55886), -- Poison Fang Bracers
+					},
+				}),
+				e(125, {	-- Earthrager Ptah
+					["crs"] = { 39428 },	-- Earthrager Ptah
+					["groups"] = {	
+						i(56093), -- Breastplate of the Risen Land
+						i(56096), -- Bulwark of the Primordial Mound
+						i(56095), -- Mouth of the Earth
+						i(56097), -- Soul Releaser
+						i(56094), -- Underworld Cord
+					},
+				}),
+				e(126, {	-- Anraphet
+					["crs"] = { 39788 },	-- Anraphet
+					["groups"] = {	
+						i(57855), -- Alpha Bracers
+						i(57860), -- Anraphet's Regalia
+						i(57857), -- Boots of Crumbling Ruin
+						i(57858), -- Mantle of Soft Shadows
+						i(57856), -- Omega Breastplate
+					},
+				}),
+				e(127, {	-- Isiset, Construct of Magic
+					["crs"] = { 39587 },	-- Isiset, Construct of Magic
+					["groups"] = {	
+						i(55992), -- Armguards of Unearthly Light
+						i(55995), -- Blood of Isiset
+						i(55993), -- Legwraps of Astral Rain
+						i(55996), -- Nova Band
+						i(55994), -- Ring of Blinding Stars
+					},
+				}),
+				e(128, {	-- Ammunae, Construct of Life
+					["crs"] = { 39731 },	-- Ammunae, Construct of Life
+					["groups"] = {				
+						i(56000), -- Band of Life Energy
+						i(55997), -- Bloodpetal Mantle
+						i(55998), -- Robes of Rampant Growth
+						i(55999), -- Seedling Pod
+					},
+				}),
+				e(129, {	-- Setesh, Construct of Destruction
+					["crs"] = { 39732 },	-- Setesh, Construct of Destruction
+					["groups"] = {					
+						i(57862), -- Chaotic Wrappings
+						i(57861), -- Helm of Setesh
+						i(57864), -- Helm of the Typhonic Beast
+						i(57863), -- Hieroglyphic Vest
+						i(57865), -- Scepter of Power
+					},
+				}),
+				e(130, {	-- Rajh, Construct of Sun
+					["crs"] = { 39378 },	-- Rajh, Construct of Sun
+					["groups"] = {					
+						-- #if BEFORE 7.3.5
+						ach(4841),	-- Halls of Origination
+						-- #endif
+						i(56106), -- Band of Rays
+						i(56108), -- Blade of the Burning Sun
+						i(56099), -- Fingers of Light
+						i(56105), -- Hekatic Slippers
+						i(56102), -- Left Eye of Rajh
+						i(56104), -- Legguards of Noon
+						i(56098), -- Red Beam Cord
+						i(56100), -- Right Eye of Rajh
+						i(56107), -- Solar Wind Cloak
+						i(56101), -- Sun Strike
+					},
+				}),
+			})),
+			-- #if BEFORE 7.3.5
 			d(HEROIC_DUNGEON, {
-				["lvl"] = 85,
 				["groups"] = {
 					n(ACHIEVEMENTS, {
 						ach(5296),	-- Faster Than the Speed of Light
 					}),
 					n(ZONE_DROPS, {
-						i(56109),	-- Book of Origination
-						un(REMOVED_FROM_GAME, i(56438)),	-- Book of Origination
-						i(56110),	-- Charm of the Muse
-						i(56111),	-- Temple Band
+							i(56438),	-- Book of Origination
 					}),
-					cr(39425, e(124, {	-- Temple Guardian Anhuur
-						ach(5293),	-- I Hate That Song
-						i(56411),	-- Darkhowl Amulet
-						i(56409),	-- Poison Fang Bracers
-						i(56410),	-- Belt of Petrified Tears
-						i(56408),	-- Awakening Footfalls
-						i(56407),	-- Anhuur's Hymnal
-					})),
-					cr(39428, e(125, {	-- Earthrager Ptah
-						ach(5294),	-- Straw That Broke the Camel's Back
-						i(56424),	-- Soul Releaser
-						i(56426),	-- Bulwark of the Primordial Mound
-						i(56422),	-- Mouth of the Earth
-						i(56425),	-- Breastplate of the Risen Land
-						i(56423),	-- Underworld Cord
-					})),
-					cr(39788, e(126, {	-- Anraphet
-						i(157611),	-- Crown of Patient Vigil
-						i(57866),	-- Mantle of Soft Shadows
-						i(57868),	-- Anraphet's Regalia
-						i(57869),	-- Omega Breastplate
-						i(57870),	-- Alpha Bracers
-						i(57867),	-- Boots of Crumbling Ruin
-					})),
-					cr(39587, e(127, {	-- Isiset, Construct of Magic
-						i(157609),	-- Scepter of Stargazing
-						i(56416),	-- Armguards of Unearthly Light
-						i(56413),	-- Legwraps of Astral Rain
-						i(56415),	-- Nova Band
-						i(56412),	-- Ring of Blinding Stars
-						i(56414),	-- Blood of Isiset
-					})),
-					cr(39731, e(128, {	-- Ammunae, Construct of Life
-						i(56421),	-- Seedling Pod
-						i(56419),	-- Bloodpetal Mantle
-						i(56417),	-- Robes of Rampant Growth
-						i(56418),	-- Band of Life Energy
-					})),
-					cr(39732, e(129, {	-- Setesh, Construct of Destruction
-						i(57872),	-- Scepter of Power
-						i(57873),	-- Helm of Setesh
-						i(57871),	-- Helm of the Typhonic Beast
-						i(57874),	-- Hieroglyphic Vest
-						i(57875),	-- Chaotic Wrappings
-					})),
-					cr(39378, e(130, {	-- Rajh, Construct of Sun
-						ach(5065),	-- Heroic: Halls of Origination
-						ach(5139),	-- Heroic: Halls of Origination Guild Run
-						ach(5295),	-- Sun of a...
-						i(56433),	-- Blade of the Burning Sun
-						i(56430),	-- Sun Strike
-						i(56434),	-- Solar Wind Cloak
-						i(56428),	-- Fingers of Light
-						i(56429),	-- Red Beam Cord
-						i(56435),	-- Legguards of Noon
-						i(56436),	-- Hekatic Slippers
-						i(56432),	-- Band of Rays
-						i(56427),	-- Left Eye of Rajh
-						i(56431),	-- Right Eye of Rajh
-					})),
+					e(124, {	-- Temple Guardian Anhuur
+						["crs"] = { 39425 },	-- Temple Guardian Anhuur
+						["groups"] = {						
+							ach(5293),	-- I Hate That Song
+							i(56407), -- Anhuur's Hymnal
+							i(56408), -- Awakening Footfalls
+							i(56410), -- Belt of Petrified Tears
+							i(56411), -- Darkhowl Amulet
+							i(56409), -- Poison Fang Bracers
+						},
+					}),
+					e(125, {	-- Earthrager Ptah
+						["crs"] = { 39428 },	-- Earthrager Ptah
+						["groups"] = {						
+							ach(5294),	-- Straw That Broke the Camel's Back
+							i(56425), -- Breastplate of the Risen Land
+							i(56426), -- Bulwark of the Primordial Mound
+							i(56422), -- Mouth of the Earth
+							i(56424), -- Soul Releaser
+							i(56423), -- Underworld Cord
+						},
+					}),
+					e(126, {	-- Anraphet
+						["crs"] = { 39788 },	-- Anraphet
+						["groups"] = {	
+							i(57870), -- Alpha Bracers
+							i(57868), -- Anraphet's Regalia
+							i(57867), -- Boots of Crumbling Ruin
+							i(57866), -- Mantle of Soft Shadows
+							i(57869), -- Omega Breastplate
+						},
+					}),
+					e(127, {	-- Isiset, Construct of Magic
+						["crs"] = { 39587 },	-- Isiset, Construct of Magic
+						["groups"] = {						
+							i(56416), -- Armguards of Unearthly Light
+							i(56414), -- Blood of Isiset
+							i(56413), -- Legwraps of Astral Rain
+							i(56415), -- Nova Band
+							i(56412), -- Ring of Blinding Stars
+						},
+					}),
+					e(128, {	-- Ammunae, Construct of Life
+						["crs"] = { 39731 },	-- Ammunae, Construct of Life
+						["groups"] = {						
+							i(56418), -- Band of Life Energy
+							i(56419), -- Bloodpetal Mantle
+							i(56417), -- Robes of Rampant Growth
+							i(56421), -- Seedling Pod
+						},
+					}),
+					e(129, {	-- Setesh, Construct of Destruction
+						["crs"] = { 39732 },	-- Setesh, Construct of Destruction
+						["groups"] = {						
+							i(57875), -- Chaotic Wrappings
+							i(57871), -- Helm of the Typhonic Beast
+							i(57873), -- Helm of Setesh
+							i(57874), -- Hieroglyphic Vest
+							i(57872), -- Scepter of Power
+						},
+					}),
+					e(130, {	-- Rajh, Construct of Sun
+						["crs"] = { 39378 },	-- Rajh, Construct of Sun
+						["groups"] = {						
+							ach(5065),	-- Heroic: Halls of Origination
+							ach(5139),	-- Heroic: Halls of Origination Guild Run
+							ach(5295),	-- Sun of a...
+							i(56432), -- Band of Rays
+							i(56433), -- Blade of the Burning Sun
+							i(56428), -- Fingers of Light
+							i(56436), -- Hekatic Slippers
+							i(56427), -- Left Eye of Rajh
+							i(56435), -- Legguards of Noon
+							i(56429), -- Red Beam Cord
+							i(56431), -- Right Eye of Rajh
+							i(56434), -- Solar Wind Cloak
+							i(56430), -- Sun Strike
+						},
+					}),
 				},
 			}),
+			-- #else
+			d(NormalPlus, {
+				e(124, {	-- Temple Guardian Anhuur
+					["crs"] = { 39425 },	-- Temple Guardian Anhuur
+					["groups"] = {	
+						i(56407), -- Anhuur's Hymnal
+						i(56408), -- Awakening Footfalls
+						i(56410), -- Belt of Petrified Tears
+						i(56411), -- Darkhowl Amulet
+						i(56409), -- Poison Fang Bracers
+					},
+				}),
+				e(125, {	-- Earthrager Ptah
+					["crs"] = { 39428 },	-- Earthrager Ptah
+					["groups"] = {					
+						i(56425), -- Breastplate of the Risen Land
+						i(56426), -- Bulwark of the Primordial Mound
+						i(56422), -- Mouth of the Earth
+						i(56424), -- Soul Releaser
+						i(56423), -- Underworld Cord
+					},
+				}),
+				e(126, {	-- Anraphet
+					["crs"] = { 39788 },	-- Anraphet
+					["groups"] = {					
+						i(57870), -- Alpha Bracers
+						i(57868), -- Anraphet's Regalia
+						i(57867), -- Boots of Crumbling Ruin
+						i(157611, { -- Crown of Patient Vigil
+							["timeline"] = { ADDED_7_3_5 },
+						}),						
+						i(57866), -- Mantle of Soft Shadows
+						i(57869), -- Omega Breastplate
+					},
+				}),
+				e(127, {	-- Isiset, Construct of Magic
+					["crs"] = { 39587 },	-- Isiset, Construct of Magic
+					["groups"] = {					
+						i(56416), -- Armguards of Unearthly Light
+						i(56414), -- Blood of Isiset
+						i(56413), -- Legwraps of Astral Rain
+						i(56415), -- Nova Band
+						i(56412), -- Ring of Blinding Stars
+						i(157609, { -- Scepter of Stargazing
+							["timeline"] = { ADDED_7_3_5 },
+						}),
+					},
+				}),
+				e(128, {	-- Ammunae, Construct of Life
+					["crs"] = { 39731 },	-- Ammunae, Construct of Life
+					["groups"] = {					
+						i(56418), -- Band of Life Energy
+						i(56419), -- Bloodpetal Mantle
+						i(56417), -- Robes of Rampant Growth
+						i(56421), -- Seedling Pod
+					},
+				}),
+				e(129, {	-- Setesh, Construct of Destruction
+					["crs"] = { 39732 },	-- Setesh, Construct of Destruction
+					["groups"] = {	
+						i(57875), -- Chaotic Wrappings
+						i(57871), -- Helm of the Typhonic Beast
+						i(57873), -- Helm of Setesh
+						i(57874), -- Hieroglyphic Vest
+						i(57872), -- Scepter of Power
+					},
+				}),
+				e(130, {	-- Rajh, Construct of Sun
+					["crs"] = { 39378 },	-- Rajh, Construct of Sun
+					["groups"] = {			
+						ach(4841),	-- Halls of Origination		
+						i(56432), -- Band of Rays
+						i(56433), -- Blade of the Burning Sun
+						i(56428), -- Fingers of Light
+						i(56436), -- Hekatic Slippers
+						i(56427), -- Left Eye of Rajh
+						i(56435), -- Legguards of Noon
+						i(56429), -- Red Beam Cord
+						i(56431), -- Right Eye of Rajh
+						i(56434), -- Solar Wind Cloak
+						i(56430), -- Sun Strike
+					},
+				}),
+			}),
+			d(HEROIC_DUNGEON, {
+				["groups"] = {
+					n(ACHIEVEMENTS, {
+						ach(5296),	-- Faster Than the Speed of Light
+					}),
+					n(ZONE_DROPS, bubbleDownSelf({ ["timeline"] = REMOVED_7_3_5 }, {
+						i(56438),	-- Book of Origination
+					})),
+					e(124, {	-- Temple Guardian Anhuur
+						["crs"] = { 39425 },	-- Temple Guardian Anhuur
+						["groups"] = {	
+							ach(5293),	-- I Hate That Song
+						},
+					}),
+					e(125, {	-- Earthrager Ptah
+						["crs"] = { 39428 },	-- Earthrager Ptah
+						["groups"] = {	
+							ach(5294),	-- Straw That Broke the Camel's Back
+						},
+					}),
+					e(130, {	-- Rajh, Construct of Sun
+						["crs"] = { 39378 },	-- Rajh, Construct of Sun
+						["groups"] = {						
+							ach(5065),	-- Heroic: Halls of Origination
+							ach(5139),	-- Heroic: Halls of Origination Guild Run
+							ach(5295),	-- Sun of a...
+						},
+					}),
+				},
+			}),
+			-- #endif
 		},
 	}),
-}));
+})));
+
 root(ROOTS.HiddenQuestTriggers, {
 	tier(WOD_TIER, {
 		q(35415),	-- Halls of Origination Reward Quest
--- .contrib/Parser/DATAS/01 - Dungeons Raids/04 - Cataclysm/2 - Dungeons/Well of Eternity.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/04 - Cataclysm/2 - Dungeons/Well of Eternity.lua
@@ -2,9 +2,8 @@
 --   D U N G E O N S  &  R A I D S  M O D U L E    --
 -----------------------------------------------------
 
-root(ROOTS.Instances, tier(CATA_TIER, {
+root(ROOTS.Instances, tier(CATA_TIER, bubbleDown({ ["timeline"] = ADDED_4_3_0 }, {
 	inst(185, {	-- Well of Eternity
-		["lvl"] = 85,
 		["mapID"] = 398,
 		["coords"] = {
 			{ 22.9, 64.4, CAVERNS_OF_TIME },	-- dungeon entrance
@@ -49,62 +48,99 @@ root(ROOTS.Instances, tier(CATA_TIER, {
 					["sourceQuest"] = 30099,	-- In Unending Numbers
 				}),
 			}),
-			n(ZONE_DROPS, {
-				["crs"] = {
-					55654,	-- Corrupted Arcanist
-					55656,	-- Dreadlord Defender
-					56579,	-- Enchanted Highmistress
-					54589,	-- Enchanted Highmistress
-					54612,	-- Eternal Champion
-					54747,	-- Eye of the Legion
-					55426,	-- Highguard Elite
-					55503,	-- Legion Demon
-					54645,	-- Royal Handmaiden
-					55453,	-- Shadowbat
-				},
-				["groups"] = {
-					i(76158),	-- Courtier's Slippers
-					i(76157),	-- Waterworn Handguards
-					i(76159),	-- Legion Bindings
-				},
+			n(ZONE_DROPS, { 
+				i(76158, {	-- Courtier's Slippers
+					["crs"] = {
+						55654,	-- Corrupted Arcanist
+						55656,	-- Dreadlord Defender
+						56579,	-- Enchanted Highmistress
+						54589,	-- Enchanted Highmistress
+						54612,	-- Eternal Champion
+						54747,	-- Eye of the Legion
+						55426,	-- Highguard Elite
+						55503,	-- Legion Demon
+						54645,	-- Royal Handmaiden
+						55453,	-- Shadowbat
+					},
+				}),
+				i(76157, {	-- Waterworn Handguards
+					["crs"] = {
+						55654,	-- Corrupted Arcanist
+						55656,	-- Dreadlord Defender
+						56579,	-- Enchanted Highmistress
+						54589,	-- Enchanted Highmistress
+						54612,	-- Eternal Champion
+						54747,	-- Eye of the Legion
+						55426,	-- Highguard Elite
+						55503,	-- Legion Demon
+						54645,	-- Royal Handmaiden
+						55453,	-- Shadowbat
+					},	
+				}),			
+				i(76159, {	-- Legion Bindings
+					["crs"] = {
+						55654,	-- Corrupted Arcanist
+						55656,	-- Dreadlord Defender
+						56579,	-- Enchanted Highmistress
+						54589,	-- Enchanted Highmistress
+						54612,	-- Eternal Champion
+						54747,	-- Eye of the Legion
+						55426,	-- Highguard Elite
+						55503,	-- Legion Demon
+						54645,	-- Royal Handmaiden
+						55453,	-- Shadowbat
+					},
+				}),
 			}),
-			cr(55085, e(290, {	-- Peroth'arn
-				ach(6127),	-- Lazy Eye
-				i(72827),	-- Gavel of Peroth'arn
-				i(72828),	-- Trickster's Edge
-				i(72829),	-- Orb of the First Satyrs
-				i(72832),	-- Girdle of the Queen's Champion
-				i(72830),	-- Peroth'arn's Belt
-				i(72831),	-- Horned Band
-			})),
-			cr(54853, e(291, {	-- Queen Azshara
-				i(72833),	-- Scepter of Azshara
-				i(72838),	-- Cloak of the Royal Protector
-				i(72834),	-- Breastplate of the Queen's Guard
-				i(72835),	-- Puppeteer's Pantaloons
-				i(72836),	-- Slippers of Wizardry
-				i(72837),	-- Queen's Boon
-			})),
-			cr(54969, e(292, {	-- Mannoroth
-				ach(6070),	-- That's Not Canon!
-				ach(6118),	-- Heroic: Well of Eternity
-				ach(6121),	-- Heroic: Well of Eternity Guild Run
-				i(72844),	-- Pit Lord's Destroyer
-				i(72846),	-- Thornwood Staff
-				i(72842),	-- Annihilan Helm
-				i(72839),	-- Cowl of Highborne Sorcerers
-				i(72843),	-- Helm of Power
-				i(72847),	-- Helm of Thorns
-				i(72840),	-- Spaulders of Eternity
-				i(72841),	-- Demonsbane Chestguard
-				i(72848),	-- Leggings of the Legion
-				i(72845),	-- Mannoroth's Signet
-				i(72898),	-- Foul Gift of the Demon Lord
-				i(72899),	-- Varo'then's Brooch
+			d(HEROIC_DUNGEON, sharedData({ ["modID"] = 0, }, {
+				e(290, {	-- Peroth'arn
+					["crs"] = { 55085 },	-- Peroth'arn
+					["groups"] = {
+						ach(6127),	-- Lazy Eye
+						i(72827), -- Gavel of Peroth'arn
+						i(72832), -- Girdle of the Queen's Champion
+						i(72831), -- Horned Band
+						i(72829), -- Orb of the First Satyrs
+						i(72830), -- Peroth'arn's Belt
+						i(72828), -- Trickster's Edge
+					},
+				}),
+				e(291, {	-- Queen Azshara
+					["crs"] = { 54853 },	-- Queen Azshara
+					["groups"] = {
+						i(72834), -- Breastplate of the Queen's Guard
+						i(72838), -- Cloak of the Royal Protector
+						i(72835), -- Puppeteer's Pantaloons
+						i(72837), -- Queen's Boon
+						i(72833), -- Scepter of Azshara
+						i(72836), -- Slippers of Wizardry
+					},
+				}),
+				e(292, {	-- Mannoroth
+					["crs"] = { 54969 },	-- Mannoroth
+					["groups"] = {
+						ach(6070),	-- That's Not Canon!
+						ach(6118),	-- Heroic: Well of Eternity
+						ach(6121),	-- Heroic: Well of Eternity Guild Run
+						i(72842), -- Annihilan Helm
+						i(72839), -- Cowl of Highborne Sorcerers
+						i(72841), -- Demonsbane Chestguard
+						i(72898), -- Foul Gift of the Demon Lord
+						i(72843), -- Helm of Power
+						i(72847), -- Helm of Thorns
+						i(72848), -- Leggings of the Legion
+						i(72845), -- Mannoroth's Signet
+						i(72844), -- Pit Lord's Destroyer
+						i(72840), -- Spaulders of Eternity
+						i(72846), -- Thornwood Staff
+						i(72899), -- Varo'then's Brooch
+					},
+				}),
 			})),
 		},
 	}),
-}));
+})));
+
 root(ROOTS.HiddenQuestTriggers, {
 	tier(WOD_TIER, {
 		q(35435),	-- Well of Eternity Reward Quest - Heroic completion
