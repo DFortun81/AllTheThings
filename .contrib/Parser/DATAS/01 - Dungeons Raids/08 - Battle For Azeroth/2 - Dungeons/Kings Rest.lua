348d91673b7e259a11267d8cb3347da73b87a797
Goldenshacal <goldenshacal@gmail.com>
2023-08-10 21:21:16 +0200

    tool of the trade inscription items

--- .contrib/Parser/DATAS/01 - Dungeons Raids/08 - Battle For Azeroth/2 - Dungeons/Kings Rest.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/08 - Battle For Azeroth/2 - Dungeons/Kings Rest.lua
@@ -114,6 +114,7 @@ root(ROOTS.Instances, tier(BFA_TIER, {
 							i(159412),	-- Auric Puddle Stompers
 							i(159304),	-- Goldfeather Boots
 							i(159617),	-- Lustrous Golden Plumage
+							i(168168),	-- Gilded Plume
 						},
 					}),
 					e(2171, {	-- Mchimba the Embalmer
