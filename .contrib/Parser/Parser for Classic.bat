58a151ecffb4c3c6dbcd95d24d8b997581d35e6c
Crieve <dylanfortune@live.com>
2023-08-17 09:38:33 -0700

    Added a batch file for Cataclysm and another for all Classic builds.

---
+++ .contrib/Parser/Parser for Classic.bat
@@ -0,0 +1,4 @@
+"Parser.exe" config=parser.classic.config
+"Parser.exe" config=parser.classic.tbc.config
+"Parser.exe" config=parser.classic.wrath.config
+"Parser.exe" config=parser.classic.cata.config
