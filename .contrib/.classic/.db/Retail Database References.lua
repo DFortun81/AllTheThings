6fd93a90556e3a9aa2e9086438e400f9002f2bf9
Crieve <dylanfortune@live.com>
2023-08-20 08:07:58 -0700

    Added references to timelined Cataclysm content.

--- .contrib/.classic/.db/Retail Database References.lua
+++ .contrib/.classic/.db/Retail Database References.lua
@@ -5,12 +5,14 @@
 -- #IMPORT: 01 - Dungeons Raids\\01 - Classic\\
 -- #IMPORT: 01 - Dungeons Raids\\02 - Burning Crusade\\
 -- #IMPORT: 01 - Dungeons Raids\\03 - Wrath of the Lich King\\
+-- #IMPORT: 01 - Dungeons Raids\\04 - Cataclysm\\
 
 -- Outdoor Zones
 -- #IMPORT: 02 - Outdoor Zones\\01 Kalimdor\\
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\
 -- #IMPORT: 02 - Outdoor Zones\\03 Outland\\
 -- #IMPORT: 02 - Outdoor Zones\\04 Northrend\\
+-- #IMPORT: 02 - Outdoor Zones\\06 The Maelstrom\\
 
 -- World Drops
 -- #IMPORT: 03 - World Drops\\01 Rooted\\
@@ -23,6 +25,7 @@
 -- Expansion Features
 -- #IMPORT: 06 - Expansion Features\\02 Wrath of the Lich King\\Achievements.lua
 -- #IMPORT: 06 - Expansion Features\\02 Wrath of the Lich King\\Argent Tournament.lua
+-- #IMPORT: 06 - Expansion Features\\03 Cataclysm\\
 
 -- World Events
 -- #IMPORT: 07 - Events\\Elemental Unrest\\
@@ -38,6 +41,7 @@
 -- #IMPORT: 10 - Craftables\\01 - Classic.lua
 -- #IMPORT: 10 - Craftables\\02 - Burning Crusade.lua
 -- #IMPORT: 10 - Craftables\\03 - Wrath of the Lich King.lua
+-- #IMPORT: 10 - Craftables\\04 - Cataclysm.lua
 
 -- Professions
 -- #IMPORT: 11 - Professions\\
@@ -52,6 +56,7 @@
 -- #IMPORT: 09 - PvP\\01 Classic PvP.lua
 -- #IMPORT: 09 - PvP\\02 The Burning Crusade PvP Seasons.lua
 -- #IMPORT: 09 - PvP\\03 Wrath of the Lich King PvP Seasons.lua
+-- #IMPORT: 09 - PvP\\04 Cataclysm PvP Seasons.lua
 
 -- NYI
 -- #IMPORT: 17 - NYI\\
--- .contrib/Parser/DATAS/02 - Outdoor Zones/06 The Maelstrom/0 - Metadata.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/06 The Maelstrom/0 - Metadata.lua
@@ -6,6 +6,7 @@ root(ROOTS.Zones, {
 	m(THE_MAELSTROM, {
 		["lore"] = "The Maelstrom is the gigantic rift that was created when the Well of Eternity collapsed into itself during the Great Sundering. Located in the middle of the Great Sea - which the Great Sundering created - the Maelstrom appears as a massive, fierce vortex of water.",
 		["icon"] = "Interface\\Icons\\Achievement_Zone_Cataclysm",
+		["timeline"] = { "added 4.0.1" },
 		-- ["maps"] = {
 			-- 276,	-- The Maelstrom
 			-- 839,	-- Priest: Legion Class Mount Map
