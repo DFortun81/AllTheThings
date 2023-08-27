84c84a31b7479afd89ad932db8452b79b9d4baa0
Crieve <dylanfortune@live.com>
2023-08-26 22:47:14 -0700

    Most TBC raids confirmed not needing zone-text-areaID for anything after TBC.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/02 - Burning Crusade/2 Gruul's Lair.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/02 - Burning Crusade/2 Gruul's Lair.lua
@@ -4,7 +4,9 @@
 root(ROOTS.Instances, tier(TBC_TIER, applyclassicphase(TBC_PHASE_ONE, {
 	inst(746, {	-- Gruul's Lair
 		["lore"] = "When the infamous Dragon Aspect, Deathwing, discovered the Dark Portal and Draenor, he felt sure that few of Draenor's denizens would dare challenge the black dragonflight, so he scattered clutches of his eggs throughout the world.\n\nDeathwing made a serious miscalculation when he chose to leave his eggs under meager guardianship in Blade's Edge Mountains. The guardian dragons were torn from the sky by the gronn living in Blade's Edge, and the leader of the gronn, Gruul, impaled the wyrms he fought on the stone spires of the mountains.\n\nToday Gruul the Dragonkiller is worshipped as a deity by the ogres of Blade's Edge Mountains. His powerful sons ravage both the spires of their home and the plains of Nagrand. Gruul's unparalleled strength and experience in battle would pose a serious threat if he ever chose to attack Horde or Alliance forces in Outland.",
+		-- #if BEFORE WRATH
 		["zone-text-areaID"] = 3618,	-- Gruul's Lair
+		-- #endif
 		["coord"] = { 69.31, 23.67, BLADES_EDGE_MOUNTAINS },	-- Gruul's Lair, Blade's Edge Mountain
 		["mapID"] = GRUULS_LAIR,
 		["sharedLockout"] = 1,
--- .contrib/Parser/DATAS/01 - Dungeons Raids/02 - Burning Crusade/4 Serpentshrine Cavern.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/02 - Burning Crusade/4 Serpentshrine Cavern.lua
@@ -5,7 +5,9 @@ local COILFANG_ARMAMENTS = i(24368);	-- Coilfang Armaments
 root(ROOTS.Instances, tier(TBC_TIER, applyclassicphase(TBC_PHASE_TWO, {
 	inst(748, {	-- Serpentshrine Cavern
 		["lore"] = "Whoever controls the water controls Outland.\n\nThese are the words of Lord Illidan Stormrage to his most trusted lieutenant, Lady Vashj. Shortly after the Third War, when Illidan requested the aid of the naga, a group of naga led by Lady Vashj answered his call. Ever since, Vashj has displayed a fierce dedication to Illidan.\n\nNow the naga witch carefully monitors Coilfang's operations from her seat of power within Serpentshrine Cavern. Far more at home here among the waters of the reservoir than locked within the cold stone walls of Black Temple, Vashj maintains a close eye on her minions and personally oversees the draining of Zangarmarsh.\n\nWhether her loyalties belong to Illidan alone, however, remains to be seen.",
+		-- #if BEFORE WRATH
 		["zone-text-areaID"] = 3607,	-- Serpentshrine Cavern
+		-- #endif
 		["sins"] = {
 			"Coilfang: Serpentshrine Cavern",
 			"Echsenkessel: Höhle des Schlangenschreins",
