a90167749c23a77b68137f3a3ec18f9f33cfeb30
Crieve <dylanfortune@live.com>
2023-08-26 21:37:08 -0700

    Added the Link ATT to WoW batch file. (this replaces all symlinks in the folder with the current one)

---
+++ Link ATT to WoW.bat
@@ -0,0 +1,32 @@
+:: Run this batch script to link the AllTheThings addon with all non-PTR versions of the game.
+@echo off
+
+call :link_wowfolder "..\World of Warcraft"
+call :report_taskcomplete
+goto :EOF
+
+:link_wowfolder
+if exist "%~1\" (
+	echo Checking "%~1\"
+	call :link_expansion "%~1\_classic_"
+	call :link_expansion "%~1\_classic_era_"
+	call :link_expansion "%~1\_retail_"
+)
+EXIT /B 0
+
+:link_expansion
+if exist "%~1\" (
+	echo Checking "%~1\"
+	if exist "%~1\Interface\AddOns\AllTheThings" (
+		rmdir "%~1\Interface\AddOns\AllTheThings"
+	)
+	if NOT exist "%~1\Interface\AddOns\AllTheThings" (
+		mklink /J "%~1\Interface\AddOns\AllTheThings" "%cd%"
+	)
+)
+EXIT /B 0
+
+:report_taskcomplete
+echo Task Complete!
+set /p DUMMY=Hit ENTER to close...
+EXIT /B 0
