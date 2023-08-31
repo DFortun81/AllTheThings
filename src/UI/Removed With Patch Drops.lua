f86b468dcd5ca476d132760ac1aad9474f6b4e65
Crieve <dylanfortune@live.com>
2023-08-31 16:03:02 -0700

    Classic: Added the ability to choose to Exclude Recipes, Removed Maps, and Removed Rares in /attrwpdrops

--- src/UI/Removed With Patch Drops.lua
+++ src/UI/Removed With Patch Drops.lua
@@ -3,6 +3,40 @@ local appName, app = ...;
 local tinsert = tinsert;
 local GetRelativeValue = app.GetRelativeValue;
 
+-- Local functions
+local ExcludeRecipes, ExcludeRemovedMaps, ExcludeRemovedRares;
+local AllowedHeaders = {
+	[app.HeaderConstants.RARES] = true,
+	[app.HeaderConstants.ZONE_DROPS] = true,
+};
+local function ExpandGroupsRecursively(group, expanded, manual)
+	if group.g and (not group.itemID or manual) then
+		group.expanded = expanded;
+		for i, subgroup in ipairs(group.g) do
+			ExpandGroupsRecursively(subgroup, expanded, manual);
+		end
+	end
+end
+local function ReapplyExpand(g, g2)
+	for j,p in ipairs(g2) do
+		local found = false;
+		local key = p.key;
+		local id = p[key];
+		for i,o in ipairs(g) do
+			if o[key] == id then
+				found = true;
+				if o.expanded then
+					if not p.expanded then
+						p.expanded = true;
+						if o.g and p.g then ReapplyExpand(o.g, p.g); end
+					end
+				end
+				break;
+			end
+		end
+	end
+end
+
 -- Implementation
 app:GetWindow("RWPD", {
 	parent = UIParent,
@@ -13,12 +47,84 @@ app:GetWindow("RWPD", {
 			self:Toggle();
 		end
 	end,
+	OnLoad = function(self, settings)
+		ExcludeRecipes = settings.ExcludeRecipes;
+		ExcludeRemovedMaps = settings.ExcludeRemovedMaps;
+		ExcludeRemovedRares = settings.ExcludeRemovedRares;
+		if ExcludeRecipes == nil then ExcludeRecipes = true; end
+		if ExcludeRemovedMaps == nil then ExcludeRemovedMaps = true; end
+		if ExcludeRemovedRares == nil then ExcludeRemovedRares = true; end
+	end,
+	OnSave = function(self, settings)
+		settings.ExcludeRecipes = ExcludeRecipes;
+		settings.ExcludeRemovedMaps = ExcludeRemovedMaps;
+		settings.ExcludeRemovedRares = ExcludeRemovedRares;
+	end,
 	OnRebuild = function(self)
 		if self.data then return true; end
-		local allowedHeaders = {
-			[app.HeaderConstants.RARES] = true,
-			[app.HeaderConstants.ZONE_DROPS] = true,
+		local options = {
+			{	-- Exclude Recipes Button
+				text = "Exclude Recipes",
+				icon = "Interface/Icons/inv_scroll_05",
+				description = "Press this button to toggle excluding Recipes.",
+				visible = true,
+				priority = 6,
+				OnClick = function(row, button)
+					ExcludeRecipes = not ExcludeRecipes;
+					wipe(self.data.g);
+					self:Rebuild();
+					return true;
+				end,
+				OnUpdate = function(data)
+					data.saved = ExcludeRecipes;
+					return true;
+				end,
+			},
+			{	-- Exclude Removed Maps Button
+				text = "Exclude Removed Maps",
+				icon = "Interface\\Icons\\Inv_misc_map02",
+				description = "Press this button to toggle excluding Maps that get Removed in the future.",
+				visible = true,
+				priority = 6,
+				OnClick = function(row, button)
+					ExcludeRemovedMaps = not ExcludeRemovedMaps;
+					wipe(self.data.g);
+					self:Rebuild();
+					return true;
+				end,
+				OnUpdate = function(data)
+					data.saved = ExcludeRemovedMaps;
+					return true;
+				end,
+			},
+			{	-- Exclude Removed Rares Button
+				text = "Exclude Removed Rares",
+				icon = app.asset("Interface_Rare"),
+				description = "Press this button to toggle excluding Rares that get Removed in the future.",
+				visible = true,
+				priority = 6,
+				OnClick = function(row, button)
+					ExcludeRemovedRares = not ExcludeRemovedRares;
+					wipe(self.data.g);
+					self:Rebuild();
+					return true;
+				end,
+				OnUpdate = function(data)
+					data.saved = ExcludeRemovedRares;
+					return true;
+				end,
+			},
 		};
+		
+		local filteredData = app:BuildSearchFilteredResponse(app:GetDataCache().g, function(group)
+			if group.rwp and group.itemID then
+				local headerID = GetRelativeValue(group, "headerID");
+				if headerID and AllowedHeaders[headerID] then
+					return true;
+				end
+			end
+		end);
+		
 		self.data = {
 			text = "Removed With Patch Drops",
 			icon = app.asset("WindowIcon_RWP"), 
@@ -31,11 +137,17 @@ app:GetWindow("RWPD", {
 			OnUpdate = function(data)
 				local g = data.g;
 				if #g < 1 then
-					local results = app:BuildSearchFilteredResponse(app:GetDataCache().g, function(group)
-						if group.rwp and group.itemID and (not group.f or group.f ~= 200) then	-- Exclude recipes.
-							local headerID = GetRelativeValue(group, "headerID");
-							if headerID and allowedHeaders[headerID] and (not (group.parent.rwp or group.parent.u == 1) or group.parent.awp) then
-								return true;
+					for i,option in ipairs(options) do
+						option.parent = data;
+						tinsert(g, option);
+					end
+					local results = app:BuildSearchFilteredResponse(filteredData, function(group)
+						if group.rwp and group.itemID and (not ExcludeRecipes or (not group.f or group.f ~= 200)) then
+							local removed = not group.parent.awp and (group.parent.rwp or group.parent.u == 1);
+							if group.parent.npcID then
+								return not ExcludeRemovedRares or not removed;
+							else
+								return not ExcludeRemovedMaps or not removed;
 							end
 						end
 					end);
@@ -44,6 +156,8 @@ app:GetWindow("RWPD", {
 							tinsert(g, result);
 						end
 					end
+					if self.oldG then ReapplyExpand(self.oldG, results); end
+					self.oldG = results;
 				end
 			end,
 		};
