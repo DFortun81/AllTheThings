f441768609919d2b2658117abdf3d568195e9f6a
Crieve <dylanfortune@live.com>
2023-08-31 14:52:17 -0700

    Classic: Added the Removed With Patch Drops UI window.

---
+++ src/UI/Removed With Patch Drops.lua
@@ -0,0 +1,52 @@
+-- App locals
+local appName, app = ...;
+local tinsert = tinsert;
+local GetRelativeValue = app.GetRelativeValue;
+
+-- Implementation
+app:GetWindow("RWPD", {
+	parent = UIParent,
+	Silent = true,
+	OnInit = function(self, handlers)
+		SLASH_ATTRWPDROPS1 = "/attrwpdrops";
+		SlashCmdList["ATTRWPDROPS"] = function()
+			self:Toggle();
+		end
+	end,
+	OnRebuild = function(self)
+		if self.data then return true; end
+		local allowedHeaders = {
+			[app.HeaderConstants.RARES] = true,
+			[app.HeaderConstants.ZONE_DROPS] = true,
+		};
+		self.data = {
+			text = "Removed With Patch Drops",
+			icon = app.asset("WindowIcon_RWP"), 
+			description = "This window shows you all of the things that get removed in a future patch from zone drop, rare, and world drop sources.",
+			visible = true, 
+			expanded = true,
+			back = 1,
+			indent = 0,
+			g = { },
+			OnUpdate = function(data)
+				local g = data.g;
+				if #g < 1 then
+					local results = app:BuildSearchFilteredResponse(app:GetDataCache().g, function(group)
+						if group.rwp and group.itemID then
+							local headerID = GetRelativeValue(group, "headerID");
+							if headerID and allowedHeaders[headerID] and not (group.parent.rwp or group.parent.u == 1) then
+								return true;
+							end
+						end
+					end);
+					if #results > 0 then
+						for i,result in ipairs(results) do
+							tinsert(g, result);
+						end
+					end
+				end
+			end,
+		};
+		return true;
+	end,
+});
