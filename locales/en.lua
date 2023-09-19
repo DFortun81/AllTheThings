d366db6b21e05f3c87d8756e021d9852815271f9
ImUnicke <imunicke+github@gmail.com>
2023-09-19 17:20:59 -0600

    Retail: Further clarification on added/removed texts

--- locales/en.lua
+++ locales/en.lua
@@ -717,11 +717,11 @@ local L = {
 	["SECRETS_HEADER"] = "Secrets";
 	["LIMITED_QUANTITY"] = "This has a limited quantity and may not always be present on the vendor.";
 	["SOURCE_ID_MISSING"] = "Please report this Item and where it was acquired to the ATT Discord in #retail-errors!";
-	["ADDED_WITH_PATCH_FORMAT"] = "Added in patch %s";
-	["WAS_ADDED_WITH_PATCH_FORMAT"] = "NEW: Added in patch %s";
-	["ADDED_BACK_WITH_PATCH_FORMAT"] = "Returned in patch %s";
-	["WAS_ADDED_BACK_WITH_PATCH_FORMAT"] = "NEW: Returned in patch %s";
-	["REMOVED_WITH_PATCH_FORMAT"] = "Will be removed in patch %s";
+	["ADDED_WITH_PATCH_FORMAT"] = "Added with patch %s";
+	["WAS_ADDED_WITH_PATCH_FORMAT"] = "Newly added with patch %s";
+	["ADDED_BACK_WITH_PATCH_FORMAT"] = "Added back with patch %s";
+	["WAS_ADDED_BACK_WITH_PATCH_FORMAT"] = "Newly added back with patch %s";
+	["REMOVED_WITH_PATCH_FORMAT"] = "Will be removed with patch %s";
 
 	-- Filter Text
 	["ACHIEVEMENT_ID"] = "Achievement ID";
