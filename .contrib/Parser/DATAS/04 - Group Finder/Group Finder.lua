20653e7f9ea1f132549380ec30e0635eb684a831
Goldenshacal <goldenshacal@gmail.com>
2023-10-27 15:51:52 +0200

    removed 4 items from wotlk lvl bag

--- .contrib/Parser/DATAS/04 - Group Finder/Group Finder.lua
+++ .contrib/Parser/DATAS/04 - Group Finder/Group Finder.lua
@@ -259,27 +259,23 @@ applyclassicphase(WRATH_PHASE_FOUR, root(ROOTS.GroupFinder, {
 				i(113686),	-- Nerubian Prince's Pendant			— confirmed by user on discord on April 22, 2020
 				i(113685),	-- Coldblood Longcloak*					— 01/17/22
 				i(113683),	-- Coldwrap Cloak						— confirmed by user on discord on April 22, 2020
-				i(113684),	-- Icecrust Cloak
+				i(113684),	-- Icecrust Cloak						— 10/25/23
 				i(112346),	-- Ice-Encrusted Plate Headguard*		— 01/17/22
 				i(113692),	-- Ice-Encrusted Gauntlets*				— 01/17/22
-				i(112361),	-- Ice-Encrusted Plate Belt*
-				i(112354),	-- Ice-Encrusted Plate Legguards*
+				i(112361),	-- Ice-Encrusted Plate Belt*			— 10/25/23
+				i(112354),	-- Ice-Encrusted Plate Legguards*		— 10/25/23
 				i(112350),	-- Icefall Mail Helm*					— 01/15/22
 				i(113693),	-- Icefall Mail Gloves					— 01/15/22
 				i(112364),	-- Icefall Mail Girdle*					— 01/15/22
 				i(112357),	-- Icefall Mail Leggings*				— 01/15/22
-				i(112348),	-- Snow-Coated Cloth Cap*
-				i(113758),	-- Snow-Coated Cloth Gloves*
-				i(112367),	-- Snow-Coated Cloth Belt*
-				i(112355),	-- Snow-Coated Cloth Leggings*
-				i(112352),	-- Winterfrost Leather Cap
-				i(112351),	-- Winterfrost Leather Helm*
-				i(113690),	-- Winterfrost Leather Gloves
+				i(112348),	-- Snow-Coated Cloth Cap*				— 10/25/23
+				i(113758),	-- Snow-Coated Cloth Gloves*			— 10/25/23
+				i(112367),	-- Snow-Coated Cloth Belt*				— 10/25/23
+				i(112355),	-- Snow-Coated Cloth Leggings*			— 10/25/23
+				i(112351),	-- Winterfrost Leather Helm*			— 10/25/23
 				i(113689),	-- Winterfrost Leather Knuckledusters	— 01/16/22
 				i(112365),	-- Winterfrost Leather Belt				— 01/17/22
-				i(112366),	-- Winterfrost Leather Girdle
 				i(112360),	-- Winterfrost Leather Leggings			— confirmed by user on discord on April 22, 2020
-				i(112359),	-- Winterfrost Leather Waders
 			},
 		}),
 		i(114634, bubbleDownSelf({ ["timeline"] = { "added 6.0.1.18500", "removed 7.3.5.25600" } }, {	-- Icy Satchel of Helpful Goods 70-74
