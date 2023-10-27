75100c9d013c22dca3d79ec4cd591c8acdeffc93
Myrhial <8618272+ninaarens@users.noreply.github.com>
2023-10-27 13:59:48 +0200

    Moved all other battlegrounds out of achievements as they can be dynamically generated

---
+++ .contrib/Parser/DATAS/08 - PvP/Battlegrounds/Deepwind Gorge.lua
@@ -0,0 +1,25 @@
+-----------------------------------------------
+--      P L A Y E R   V S   P L A Y E R      --
+-----------------------------------------------
+root(ROOTS.PVP, pvp(n(BATTLEGROUNDS, {
+	m(519, {	-- Deepwind Gorge
+		["icon"] = "Interface\\Icons\\achievement_bg_dg_master_of_the_deepwind_gorge",
+		["timeline"] = { "added 5.2.0" },
+		["maps"] = {
+			1576,	-- Deepwind Gorge (revamped)
+		},
+		["g"] = {
+			n(ACHIEVEMENTS, {
+				ach(14175, {	-- Master of Deepwind Gorge
+					ach(8331),	-- Deepwind Gorge Victory
+					ach(8332),	-- Deepwind Gorge Veteran
+					ach(8333),	-- Deepwind Gorge Perfection
+					ach(14187),	-- Cruisin' for a Brewsin'
+					ach(14186),	-- Market Dominance
+					ach(14188),	-- Well Gorged
+					title(229),	-- Gorgeous
+				}),
+			}),
+		},
+	}),
+})));
---
+++ .contrib/Parser/DATAS/08 - PvP/Battlegrounds/Temple of Kotmogu.lua
@@ -0,0 +1,33 @@
+-----------------------------------------------
+--      P L A Y E R   V S   P L A Y E R      --
+-----------------------------------------------
+root(ROOTS.PVP, pvp(n(BATTLEGROUNDS, {
+	m(417, {	-- Temple of Kotmogu
+		["icon"] = "Interface\\Icons\\Achievement_battleground_templeofkotmogu",
+		["timeline"] = { "added 5.0.4" },
+		["g"] = {
+			n(ACHIEVEMENTS, {
+				ach(6981, {	-- Master of Temple of Kotmogu
+					ach(6882, {	-- Temple of Kotmogu Veteran
+						ach(6740),	-- Temple of Kotmogu Victory
+					}),
+					ach(6947, {	-- Four Square
+						crit(20064),	-- Cyan Orb
+						crit(20065),	-- Purple Orb
+						crit(20066),	-- Green Orb
+						crit(20067),	-- Orange Orb
+					}),
+					ach(6950),	-- Powerball
+					ach(6970),	-- Blackout
+					ach(6973),	-- Can't Stop Won't Stop
+					ach(6971),	-- I've Got the Power
+					ach(6972),	-- What is Best in Life?
+					ach(6980, {	-- Temple of Kotmogu All-Star
+						crit(21027),	-- Kill four Orb carriers.
+						crit(21028),	-- Four Orbs Controlled
+					}),
+				}),
+			}),
+		},
+	}),
+})));
