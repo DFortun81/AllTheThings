efbde6722ed31912d02fbc93b15ac01653a01789
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2023-10-31 00:04:23 -0700

    Rogue Class hall QI missing

--- .contrib/Parser/DATAS/06 - Expansion Features/06 Legion/Legion Class Halls/The Hall Of Shadows (Rogue).lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/06 Legion/Legion Class Halls/The Hall Of Shadows (Rogue).lua
@@ -345,6 +345,9 @@ root(ROOTS.ExpansionFeatures, tier(LEGION_TIER, bubbleDown({ ["timeline"] = { "a
 						["sourceQuest"] = 43829,	-- Spy vs. Spy
 						["provider"] = { "n", 101513 },	-- Lord Jorach Ravenholdt
 						["coord"] = { 41.3, 78.1, THE_HALL_OF_SHADOWS },
+						["groups"] = {
+							i(140395),	-- Stylish Spy Glasses (QI!)
+						},
 					}),
 					q(44116, {	-- Mystery at Citrine Bay
 						["sourceQuest"] = 44041,	-- The Bloody Truth
