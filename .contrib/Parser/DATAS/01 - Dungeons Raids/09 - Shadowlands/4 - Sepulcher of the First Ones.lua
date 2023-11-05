208bc647bbf553cd569f3ee4c9da49adf057ce8a
ImUnicke <imunicke+github@gmail.com>
2023-11-05 04:06:01 -0700

    Fixed SOFO LFR being removed (not sure why timeline merge is weird here)

--- .contrib/Parser/DATAS/01 - Dungeons Raids/09 - Shadowlands/4 - Sepulcher of the First Ones.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/09 - Shadowlands/4 - Sepulcher of the First Ones.lua
@@ -824,10 +824,12 @@ root(ROOTS.Instances, tier(SL_TIER, bubbleDown({ ["timeline"] = { ADDED_9_2_0 } 
 					i(182318),	-- Viscous Ink (CONDUIT!)
 				}),
 			}),
-			d(LFR_RAID, bubbleDownSelf({ ["timeline"] = { ADDED_10_1_5 } }, {	-- Queue NPC
+			-- #IF AFTER 10.1.5
+			d(LFR_RAID, {	-- Queue NPC
 				["crs"] = { 205959 },	-- Ta'elfar <Trader of Histories>
 				["coord"] = { 41.3, 71.0, ORIBOS },
-			})),
+			}),
+			-- #ENDIF
 			d(LFR_RAID, bubbleDownSelf({ ["timeline"] = { ADDED_9_2_0, REMOVED_10_0_2_LAUNCH, ADDED_10_1_5 } }, {
 				n(ZONE_DROPS, ZONE_DROPS_GROUP),
 				header(HEADERS.Achievement, 15493, {	-- Ephemeral Plains
