6560d95b8ca64d6404f22b52e8917eff76faad63
Goldenshacal <goldenshacal@gmail.com>
2023-11-07 03:22:22 +0100

    fix for dashing set

--- .contrib/Parser/DATAS/12 - Promotions/10 Dragonflight Seasonal Promotions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/10 Dragonflight Seasonal Promotions.lua
@@ -116,17 +116,16 @@ root(ROOTS.Promotions, {
 				["timeline"] = { "added 10.1.5." }, -- "removed 10.1.0.49570" },
 				["u"] = REMOVED_FROM_GAME,	-- 18th July 2023
 			}),
-			i(190923, {	-- Ensemble: Dashing Buccaneer's Slops
+			i(190923,	-- Ensemble: Dashing Buccaneer's Slops
+				bubbleDownSelf({ ["timeline"] = { "added 10.1.5.50401", "removed 10.1.5.50401" } }, {
 				["description"] = "Obtained through watching select Twitch Streamers with Drops enabled for at least 4 hours between September 5th, 10:00 a.m. & September 12th, 01:00 a.m. PDT.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
-				["timeline"] = { "added 10.1.5." }, -- "removed 10.1.0.49570" },
 				["g"] = {
 					i(190906),	-- Dashing Buccaneer's Breeches
 					i(190907),	-- Dashing Buccaneer's Boots
 					i(190905),	-- Dashing Buccaneer's Sash
 					i(190904),	-- Dashing Buccaneer's Tunic
 				},
-				["u"] = REMOVED_FROM_GAME,	-- 12th September 2023
-			}),
+			})),
 			i(208057, {	-- Smoldering Banner of the Aspects (TOY!)
 				["description"] = "Break the Meta: DF Season 2\n\nInstead of teams fighting to beat their opponent's time, Break the Meta is focused on teams pushing keys as high as they can with Season's 2 off-meta specs and classes.\n\nRegister for the event on |cFFFFFFFFraider.io/break-the-meta-df-season-2/signups|r and complete at least 2 BTM-Eligible timed keystones at level +15 or higher during the Competition Period, and the Smoldering Banner of the Aspects will be automatically added to your collection in-game after the conclusion of the event.\n\nThe Event starts on October 3rd for US, October 4th for EU & October 5th for KR/TW & lasts for 1 reset of your region.",
 				["timeline"] = { "added 10.1.7", "removed 10.1.7.51536" },
