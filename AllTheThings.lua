72e12bdb97cb98c73aa11a3a7eeef18d378ed36c
ImUnicke <imunicke+github@gmail.com>
2023-11-09 01:02:48 -0700

    Headers which have a mapID matching the current map are again allowed to be the root of a minilist even if they aren't a 'fake' map (i.e Darkmoon Faire)

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -17945,8 +17945,8 @@ customWindowUpdates["CurrentInstance"] = function(self, force, got)
 				for _,group in ipairs(results) do
 					-- do not use any raw Source groups in the final list
 					group = CreateObject(group);
-					-- Instance/Map/Class groups are allowed as root of minilist
-					if (group.instanceID or (group.mapID and (group.key == "mapID" or (group.key == "headerID" and group.mapID < 0 and group.mapID == self.mapID))) or group.key == "classID")
+					-- Instance/Map/Class/Header(of current map) groups are allowed as root of minilist
+					if (group.instanceID or (group.mapID and (group.key == "mapID" or (group.key == "headerID" and group.mapID == self.mapID))) or group.key == "classID")
 						-- and actually match this minilist...
 						-- only if this group mapID matches the minilist mapID directly or by maps
 						and (group.mapID == self.mapID or (group.maps and contains(group.maps, self.mapID))) then
