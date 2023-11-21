d95f7222f9becbd1e844efe56e31647860592104
Darkal31 <michalbert@seznam.cz>
2023-11-21 17:21:33 +0100

    typo

--- .contrib/Parser/DATAS/10 - Professions/Engineering.lua
+++ .contrib/Parser/DATAS/10 - Professions/Engineering.lua
@@ -830,7 +830,7 @@ root(ROOTS.Professions, prof(ENGINEERING, bubbleDownSelf({ ["requireSkill"] = EN
 			i(198156, {	-- Wyrmhole Generator: Dragon Isles (TOY!)
 				["description"] = "You can collect additional portal options.\nOHN’AHRAN PLAINS = Clomp your hooves, imaginary or otherwise...\nRANDOM LOCATION = Carelessly leap into the portal...\nTHALDRASZUS = Audibly spell out 'Thaldraszus'...\nTHE AZURE SPAN = Ensure your arrival at the Azure Span...\nTHE WAKING SHORES = Pray return to the Waking Shores.",
 				-- Object IDs and quest IDs are put at random to each other.
-				-- There seems to be no way of getting the correct object ID for the flags in game. (does not matter for ATT purpose)
+				-- There seems to be no way of getting the correct object ID for the Deactivated Signal Transmitter in game. (does not matter for ATT purpose)
 				["g"] = {
 					-- The Waking Shores
 					o(381964, {	-- Deactivated Signal Transmitter
