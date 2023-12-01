752f0be4e5c29cf01107fb058436060be4d50c02
Crieve <dylanfortune@live.com>
2023-12-01 13:55:07 -0700

    Classic: Reverted change to SetCollected.

--- classic/ATT-Classic.lua
+++ classic/ATT-Classic.lua
@@ -14503,16 +14503,18 @@ app.events.ADDON_LOADED = function(addonName)
 		local container = currentCharacter[field];
 		local oldstate = container[id];
 		if collected then
-			accountWideData[field][id] = 1;
 			if not oldstate then
 				if t and not (accountWideSettings[field] and accountWideData[field][id]) then
 					--print("SetCollected", field, id, accountWideSettings[field], accountWideData[field][id]);
 					AddToCollection(t);
 				end
 				container[id] = 1;
+				accountWideData[field][id] = 1;
 				local now = time();
 				timeStamps[field] = now;
 				currentCharacter.lastPlayed = now;
+			else
+				accountWideData[field][id] = 1;
 			end
 			return 1;
 		elseif oldstate then
@@ -14539,16 +14541,18 @@ app.events.ADDON_LOADED = function(addonName)
 		local container = currentCharacter[field];
 		local oldstate = container[id];
 		if collected then
-			accountWideData[field][id] = 1;
 			if not oldstate then
 				if t and not (accountWideSettings[subtype] and accountWideData[field][id]) then
 					--print("SetCollectedForSubType", field, subtype, id, accountWideSettings[subtype], accountWideData[field][id]);
 					AddToCollection(t);
 				end
 				container[id] = 1;
+				accountWideData[field][id] = 1;
 				local now = time();
 				timeStamps[field] = now;
 				currentCharacter.lastPlayed = now;
+			else
+				accountWideData[field][id] = 1;
 			end
 			return 1;
 		elseif oldstate then
