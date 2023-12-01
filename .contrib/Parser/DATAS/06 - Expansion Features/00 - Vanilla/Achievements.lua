b6fdd3fdd14aaf02ca59664064672f937b6f9645
Myrhial <8618272+ninaarens@users.noreply.github.com>
2023-12-01 19:20:17 +0100

    Header fix

--- .contrib/Parser/DATAS/06 - Expansion Features/00 - Vanilla/Achievements.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/00 - Vanilla/Achievements.lua
@@ -2,7 +2,7 @@
 --      E X P A N S I O N   F E A T U R E S    M O D U L E       --
 -------------------------------------------------------------------
 root(ROOTS.ExpansionFeatures, tier(CLASSIC_TIER, {
-	n(PROFESSIONS, {
+	n(ACHIEVEMENTS, {
 		ach(879, {	-- Old School Ride
 			["providers"] = {
 				{ "i", 13328 },	-- Black Ram
