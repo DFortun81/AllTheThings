1a66522fce8de4186779caacc5fd2e04a8eb1d2f
Goldenshacal <goldenshacal@gmail.com>
2024-01-01 18:36:59 +0100

    added creation catalyst into sepulcher for n+h+m

--- .contrib/Parser/DATAS/01 - Dungeons Raids/09 - Shadowlands/4 - Sepulcher of the First Ones.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/09 - Shadowlands/4 - Sepulcher of the First Ones.lua
@@ -897,118 +897,116 @@ root(ROOTS.Instances, tier(SL_TIER, bubbleDown({ ["timeline"] = { ADDED_9_2_0 } 
 					boss(THE_JAILER),
 				}),
 				o(375368, {	-- Creation Catalyst Console
-				--	["description"] = "Help us gather information of what is/isnt available via doing reports in ATT discord. Especially the alternative sets and if the pvp transmog is available somewhere else",
-					["g"] = bubbleDown({ ["modID"] = 4 },
-							sharedData({["cost"] = {{"i",190189,2070}}},{	-- 2070 Sandworn Relic, Full set cost from Vendor
-								cl(DEATHKNIGHT, {
-									["sym"] = SymRaid(DEATHKNIGHT, 4),
-									["g"] = {
-										i(188873),	-- Cloak of the First Eidolon
-										i(188870),	-- Girdle of the First Eidolon
-										i(188865),	-- Greaves of the First Eidolon
-										i(188869),	-- Vambraces of the First Eidolon
-									},
-								}),
-								cl(DEMONHUNTER, {
-									["sym"] = SymRaid(DEMONHUNTER, 4),
-									["g"] = {
-										i(188897),	-- Mercurial Punisher's Belt
-										i(188899),	-- Mercurial Punisher's Boots
-										i(188900),	-- Mercurial Punisher's Mantle
-										i(188895),	-- Mercurial Punisher's Wristguards
-									},
-								}),
-								cl(DRUID, {
-									["sym"] = SymRaid(DRUID, 4),
-									["g"] = {
-										i(188850),	-- Bracers of the Fixed Stars
-										i(188852),	-- Cincture of the Fixed Stars
-										i(188854),	-- Footwraps of the Fixed Stars
-										i(188871),	-- Wrap of the Fixed Stars
-									},
-								}),
-								cl(HUNTER, {
-									["sym"] = SymRaid(HUNTER, 4),
-									["g"] = {
-										i(188872),	-- Godstalker's Camouflage
-										i(188857),	-- Godstalker's Fauld
-										i(188862),	-- Godstalker's Sabatons
-										i(188855),	-- Godstalker's Wristwraps
-									},
-								}),
-								cl(MAGE, {
-									["sym"] = SymRaid(MAGE, 4),
-									["g"] = {
-										i(188840),	-- Erudite Occultist's Bracers
-										i(188841),	-- Erudite Occultist's Cord
-										i(188846),	-- Erudite Occultist's Shroud
-										i(188838),	-- Erudite Occultist's Treads
-									},
-								}),
-								cl(MONK, {
-									["sym"] = SymRaid(MONK, 4),
-									["g"] = {
-										i(188913),	-- Demigaunts of the Grand Upwelling
-										i(188918),	-- Drape of the Grand Upwelling
-										i(188917),	-- Footwraps of the Grand Upwelling
-										i(188915),	-- Waistwrap of the Grand Upwelling
-									},
-								}),
-								cl(PALADIN, {
-									["sym"] = SymRaid(PALADIN, 4),
-									["g"] = {
-										i(188936),	-- Luminous Chevalier's Drape
-										i(188935),	-- Luminous Chevalier's Girdle
-										i(188930),	-- Luminous Chevalier's Spurs
-										i(188934),	-- Luminous Chevalier's Vambraces
-									},
-								}),
-								cl(PRIEST, {
-									["sym"] = SymRaid(PRIEST, 4),
-									["g"] = {
-										i(188876),	-- Bracelets of the Empyrean
-										i(188882),	-- Drape of the Empyrean
-										i(188877),	-- Sash of the Empyrean
-										i(188874),	-- Slippers of the Empyrean
-									},
-								}),
-								cl(ROGUE, {
-									["sym"] = SymRaid(ROGUE, 4),
-									["g"] = {
-										i(188906),	-- Soulblade Baldric
-										i(188909),	-- Soulblade Cloak
-										i(188908),	-- Soulblade Footpads
-										i(188904),	-- Soulblade Wristguard
-									},
-								}),
-								cl(SHAMAN, {
-									["sym"] = SymRaid(SHAMAN, 4),
-									["g"] = {
-										i(188921),	-- Theurgic Starspeaker's Belt
-										i(188919),	-- Theurgic Starspeaker's Bracers
-										i(188926),	-- Theurgic Starspeaker's Sabatons
-										i(188927),	-- Theurgic Starspeaker's Shawl
-									},
-								}),
-								cl(WARLOCK, {
-									["sym"] = SymRaid(WARLOCK, 4),
-									["g"] = {
-										i(188885),	-- Bangles of the Demon Star
-										i(188883),	-- Boots of the Demon Star
-										i(188891),	-- Cape of the Demon Star
-										i(188886),	-- Waistwrap of the Demon Star
-									},
-								}),
-								cl(WARRIOR, {
-									["sym"] = SymRaid(WARRIOR, 4),
-									["g"] = {
-										i(188945),	-- Favor of the Infinite Infantry
-										i(188944),	-- Greatbelt of the Infinite Infantry
-										i(188939),	-- March of the Infinite Infantry
-										i(188943),	-- Vambraces of the Infinite Infantry
-									},
-								}),
-					})),
+					["g"] = bubbleDown({ ["modID"] = 4 }, {
+						cl(DEATHKNIGHT, {
+							["sym"] = SymRaid(DEATHKNIGHT, 4),
+							["g"] = {
+								i(188873),	-- Cloak of the First Eidolon
+								i(188870),	-- Girdle of the First Eidolon
+								i(188865),	-- Greaves of the First Eidolon
+								i(188869),	-- Vambraces of the First Eidolon
+							},
+						}),
+						cl(DEMONHUNTER, {
+							["sym"] = SymRaid(DEMONHUNTER, 4),
+							["g"] = {
+								i(188897),	-- Mercurial Punisher's Belt
+								i(188899),	-- Mercurial Punisher's Boots
+								i(188900),	-- Mercurial Punisher's Mantle
+								i(188895),	-- Mercurial Punisher's Wristguards
+							},
+						}),
+						cl(DRUID, {
+							["sym"] = SymRaid(DRUID, 4),
+							["g"] = {
+								i(188850),	-- Bracers of the Fixed Stars
+								i(188852),	-- Cincture of the Fixed Stars
+								i(188854),	-- Footwraps of the Fixed Stars
+								i(188871),	-- Wrap of the Fixed Stars
+							},
+						}),
+						cl(HUNTER, {
+							["sym"] = SymRaid(HUNTER, 4),
+							["g"] = {
+								i(188872),	-- Godstalker's Camouflage
+								i(188857),	-- Godstalker's Fauld
+								i(188862),	-- Godstalker's Sabatons
+								i(188855),	-- Godstalker's Wristwraps
+							},
+						}),
+						cl(MAGE, {
+							["sym"] = SymRaid(MAGE, 4),
+							["g"] = {
+								i(188840),	-- Erudite Occultist's Bracers
+								i(188841),	-- Erudite Occultist's Cord
+								i(188846),	-- Erudite Occultist's Shroud
+								i(188838),	-- Erudite Occultist's Treads
+							},
+						}),
+						cl(MONK, {
+							["sym"] = SymRaid(MONK, 4),
+							["g"] = {
+								i(188913),	-- Demigaunts of the Grand Upwelling
+								i(188918),	-- Drape of the Grand Upwelling
+								i(188917),	-- Footwraps of the Grand Upwelling
+								i(188915),	-- Waistwrap of the Grand Upwelling
+							},
+						}),
+						cl(PALADIN, {
+							["sym"] = SymRaid(PALADIN, 4),
+							["g"] = {
+								i(188936),	-- Luminous Chevalier's Drape
+								i(188935),	-- Luminous Chevalier's Girdle
+								i(188930),	-- Luminous Chevalier's Spurs
+								i(188934),	-- Luminous Chevalier's Vambraces
+							},
+						}),
+						cl(PRIEST, {
+							["sym"] = SymRaid(PRIEST, 4),
+							["g"] = {
+								i(188876),	-- Bracelets of the Empyrean
+								i(188882),	-- Drape of the Empyrean
+								i(188877),	-- Sash of the Empyrean
+								i(188874),	-- Slippers of the Empyrean
+							},
+						}),
+						cl(ROGUE, {
+							["sym"] = SymRaid(ROGUE, 4),
+							["g"] = {
+								i(188906),	-- Soulblade Baldric
+								i(188909),	-- Soulblade Cloak
+								i(188908),	-- Soulblade Footpads
+								i(188904),	-- Soulblade Wristguard
+							},
+						}),
+						cl(SHAMAN, {
+							["sym"] = SymRaid(SHAMAN, 4),
+							["g"] = {
+								i(188921),	-- Theurgic Starspeaker's Belt
+								i(188919),	-- Theurgic Starspeaker's Bracers
+								i(188926),	-- Theurgic Starspeaker's Sabatons
+								i(188927),	-- Theurgic Starspeaker's Shawl
+							},
+						}),
+						cl(WARLOCK, {
+							["sym"] = SymRaid(WARLOCK, 4),
+							["g"] = {
+								i(188885),	-- Bangles of the Demon Star
+								i(188883),	-- Boots of the Demon Star
+								i(188891),	-- Cape of the Demon Star
+								i(188886),	-- Waistwrap of the Demon Star
+							},
+						}),
+						cl(WARRIOR, {
+							["sym"] = SymRaid(WARRIOR, 4),
+							["g"] = {
+								i(188945),	-- Favor of the Infinite Infantry
+								i(188944),	-- Greatbelt of the Infinite Infantry
+								i(188939),	-- March of the Infinite Infantry
+								i(188943),	-- Vambraces of the Infinite Infantry
+							},
+						}),
+					}),
 				}),					
 			})),
 			d(NORMAL_PLUS_RAID, {
@@ -1067,6 +1065,118 @@ root(ROOTS.Instances, tier(SL_TIER, bubbleDown({ ["timeline"] = { ADDED_9_2_0 } 
 				boss(LORDS_OF_DREAD),
 				boss(RYGELON),
 				boss(THE_JAILER),
+				o(375368, {	-- Creation Catalyst Console
+					["g"] = bubbleDown({ ["modID"] = 5 }, {
+						cl(DEATHKNIGHT, {
+							["sym"] = SymRaid(DEATHKNIGHT, 3),
+							["g"] = {
+								i(188873),	-- Cloak of the First Eidolon
+								i(188870),	-- Girdle of the First Eidolon
+								i(188865),	-- Greaves of the First Eidolon
+								i(188869),	-- Vambraces of the First Eidolon
+							},
+						}),
+						cl(DEMONHUNTER, {
+							["sym"] = SymRaid(DEMONHUNTER, 3),
+							["g"] = {
+								i(188897),	-- Mercurial Punisher's Belt
+								i(188899),	-- Mercurial Punisher's Boots
+								i(188900),	-- Mercurial Punisher's Mantle
+								i(188895),	-- Mercurial Punisher's Wristguards
+							},
+						}),
+						cl(DRUID, {
+							["sym"] = SymRaid(DRUID, 3),
+							["g"] = {
+								i(188850),	-- Bracers of the Fixed Stars
+								i(188852),	-- Cincture of the Fixed Stars
+								i(188854),	-- Footwraps of the Fixed Stars
+								i(188871),	-- Wrap of the Fixed Stars
+							},
+						}),
+						cl(HUNTER, {
+							["sym"] = SymRaid(HUNTER, 3),
+							["g"] = {
+								i(188872),	-- Godstalker's Camouflage
+								i(188857),	-- Godstalker's Fauld
+								i(188862),	-- Godstalker's Sabatons
+								i(188855),	-- Godstalker's Wristwraps
+							},
+						}),
+						cl(MAGE, {
+							["sym"] = SymRaid(MAGE, 3),
+							["g"] = {
+								i(188840),	-- Erudite Occultist's Bracers
+								i(188841),	-- Erudite Occultist's Cord
+								i(188846),	-- Erudite Occultist's Shroud
+								i(188838),	-- Erudite Occultist's Treads
+							},
+						}),
+						cl(MONK, {
+							["sym"] = SymRaid(MONK, 3),
+							["g"] = {
+								i(188913),	-- Demigaunts of the Grand Upwelling
+								i(188918),	-- Drape of the Grand Upwelling
+								i(188917),	-- Footwraps of the Grand Upwelling
+								i(188915),	-- Waistwrap of the Grand Upwelling
+							},
+						}),
+						cl(PALADIN, {
+							["sym"] = SymRaid(PALADIN, 3),
+							["g"] = {
+								i(188936),	-- Luminous Chevalier's Drape
+								i(188935),	-- Luminous Chevalier's Girdle
+								i(188930),	-- Luminous Chevalier's Spurs
+								i(188934),	-- Luminous Chevalier's Vambraces
+							},
+						}),
+						cl(PRIEST, {
+							["sym"] = SymRaid(PRIEST, 3),
+							["g"] = {
+								i(188876),	-- Bracelets of the Empyrean
+								i(188882),	-- Drape of the Empyrean
+								i(188877),	-- Sash of the Empyrean
+								i(188874),	-- Slippers of the Empyrean
+							},
+						}),
+						cl(ROGUE, {
+							["sym"] = SymRaid(ROGUE, 3),
+							["g"] = {
+								i(188906),	-- Soulblade Baldric
+								i(188909),	-- Soulblade Cloak
+								i(188908),	-- Soulblade Footpads
+								i(188904),	-- Soulblade Wristguard
+							},
+						}),
+						cl(SHAMAN, {
+							["sym"] = SymRaid(SHAMAN, 3),
+							["g"] = {
+								i(188921),	-- Theurgic Starspeaker's Belt
+								i(188919),	-- Theurgic Starspeaker's Bracers
+								i(188926),	-- Theurgic Starspeaker's Sabatons
+								i(188927),	-- Theurgic Starspeaker's Shawl
+							},
+						}),
+						cl(WARLOCK, {
+							["sym"] = SymRaid(WARLOCK, 3),
+							["g"] = {
+								i(188885),	-- Bangles of the Demon Star
+								i(188883),	-- Boots of the Demon Star
+								i(188891),	-- Cape of the Demon Star
+								i(188886),	-- Waistwrap of the Demon Star
+							},
+						}),
+						cl(WARRIOR, {
+							["sym"] = SymRaid(WARRIOR, 3),
+							["g"] = {
+								i(188945),	-- Favor of the Infinite Infantry
+								i(188944),	-- Greatbelt of the Infinite Infantry
+								i(188939),	-- March of the Infinite Infantry
+								i(188943),	-- Vambraces of the Infinite Infantry
+							},
+						}),
+					}),
+				}),		
 			}),
 			d(HEROIC_PLUS_RAID, {
 				n(QUESTS, {
@@ -1120,6 +1230,118 @@ root(ROOTS.Instances, tier(SL_TIER, bubbleDown({ ["timeline"] = { ADDED_9_2_0 } 
 				boss(LORDS_OF_DREAD),
 				boss(RYGELON),
 				boss(THE_JAILER),
+				o(375368, {	-- Creation Catalyst Console
+					["g"] = bubbleDown({ ["modID"] = 5 }, {
+						cl(DEATHKNIGHT, {
+							["sym"] = SymRaid(DEATHKNIGHT, 5),
+							["g"] = {
+								i(188873),	-- Cloak of the First Eidolon
+								i(188870),	-- Girdle of the First Eidolon
+								i(188865),	-- Greaves of the First Eidolon
+								i(188869),	-- Vambraces of the First Eidolon
+							},
+						}),
+						cl(DEMONHUNTER, {
+							["sym"] = SymRaid(DEMONHUNTER, 5),
+							["g"] = {
+								i(188897),	-- Mercurial Punisher's Belt
+								i(188899),	-- Mercurial Punisher's Boots
+								i(188900),	-- Mercurial Punisher's Mantle
+								i(188895),	-- Mercurial Punisher's Wristguards
+							},
+						}),
+						cl(DRUID, {
+							["sym"] = SymRaid(DRUID, 5),
+							["g"] = {
+								i(188850),	-- Bracers of the Fixed Stars
+								i(188852),	-- Cincture of the Fixed Stars
+								i(188854),	-- Footwraps of the Fixed Stars
+								i(188871),	-- Wrap of the Fixed Stars
+							},
+						}),
+						cl(HUNTER, {
+							["sym"] = SymRaid(HUNTER, 5),
+							["g"] = {
+								i(188872),	-- Godstalker's Camouflage
+								i(188857),	-- Godstalker's Fauld
+								i(188862),	-- Godstalker's Sabatons
+								i(188855),	-- Godstalker's Wristwraps
+							},
+						}),
+						cl(MAGE, {
+							["sym"] = SymRaid(MAGE, 5),
+							["g"] = {
+								i(188840),	-- Erudite Occultist's Bracers
+								i(188841),	-- Erudite Occultist's Cord
+								i(188846),	-- Erudite Occultist's Shroud
+								i(188838),	-- Erudite Occultist's Treads
+							},
+						}),
+						cl(MONK, {
+							["sym"] = SymRaid(MONK, 5),
+							["g"] = {
+								i(188913),	-- Demigaunts of the Grand Upwelling
+								i(188918),	-- Drape of the Grand Upwelling
+								i(188917),	-- Footwraps of the Grand Upwelling
+								i(188915),	-- Waistwrap of the Grand Upwelling
+							},
+						}),
+						cl(PALADIN, {
+							["sym"] = SymRaid(PALADIN, 5),
+							["g"] = {
+								i(188936),	-- Luminous Chevalier's Drape
+								i(188935),	-- Luminous Chevalier's Girdle
+								i(188930),	-- Luminous Chevalier's Spurs
+								i(188934),	-- Luminous Chevalier's Vambraces
+							},
+						}),
+						cl(PRIEST, {
+							["sym"] = SymRaid(PRIEST, 5),
+							["g"] = {
+								i(188876),	-- Bracelets of the Empyrean
+								i(188882),	-- Drape of the Empyrean
+								i(188877),	-- Sash of the Empyrean
+								i(188874),	-- Slippers of the Empyrean
+							},
+						}),
+						cl(ROGUE, {
+							["sym"] = SymRaid(ROGUE, 5),
+							["g"] = {
+								i(188906),	-- Soulblade Baldric
+								i(188909),	-- Soulblade Cloak
+								i(188908),	-- Soulblade Footpads
+								i(188904),	-- Soulblade Wristguard
+							},
+						}),
+						cl(SHAMAN, {
+							["sym"] = SymRaid(SHAMAN, 5),
+							["g"] = {
+								i(188921),	-- Theurgic Starspeaker's Belt
+								i(188919),	-- Theurgic Starspeaker's Bracers
+								i(188926),	-- Theurgic Starspeaker's Sabatons
+								i(188927),	-- Theurgic Starspeaker's Shawl
+							},
+						}),
+						cl(WARLOCK, {
+							["sym"] = SymRaid(WARLOCK, 5),
+							["g"] = {
+								i(188885),	-- Bangles of the Demon Star
+								i(188883),	-- Boots of the Demon Star
+								i(188891),	-- Cape of the Demon Star
+								i(188886),	-- Waistwrap of the Demon Star
+							},
+						}),
+						cl(WARRIOR, {
+							["sym"] = SymRaid(WARRIOR, 5),
+							["g"] = {
+								i(188945),	-- Favor of the Infinite Infantry
+								i(188944),	-- Greatbelt of the Infinite Infantry
+								i(188939),	-- March of the Infinite Infantry
+								i(188943),	-- Vambraces of the Infinite Infantry
+							},
+						}),
+					}),
+				}),
 			}),
 			d(MYTHIC_RAID, {
 				n(QUESTS, {
@@ -1184,6 +1406,118 @@ root(ROOTS.Instances, tier(SL_TIER, bubbleDown({ ["timeline"] = { ADDED_9_2_0 } 
 					}),
 					ach(15471),	-- Cutting Edge: The Jailer
 				})),
+				o(375368, {	-- Creation Catalyst Console
+					["g"] = bubbleDown({ ["modID"] = 6 }, {
+						cl(DEATHKNIGHT, {
+							["sym"] = SymRaid(DEATHKNIGHT, 6),
+							["g"] = {
+								i(188873),	-- Cloak of the First Eidolon
+								i(188870),	-- Girdle of the First Eidolon
+								i(188865),	-- Greaves of the First Eidolon
+								i(188869),	-- Vambraces of the First Eidolon
+							},
+						}),
+						cl(DEMONHUNTER, {
+							["sym"] = SymRaid(DEMONHUNTER, 6),
+							["g"] = {
+								i(188897),	-- Mercurial Punisher's Belt
+								i(188899),	-- Mercurial Punisher's Boots
+								i(188900),	-- Mercurial Punisher's Mantle
+								i(188895),	-- Mercurial Punisher's Wristguards
+							},
+						}),
+						cl(DRUID, {
+							["sym"] = SymRaid(DRUID, 6),
+							["g"] = {
+								i(188850),	-- Bracers of the Fixed Stars
+								i(188852),	-- Cincture of the Fixed Stars
+								i(188854),	-- Footwraps of the Fixed Stars
+								i(188871),	-- Wrap of the Fixed Stars
+							},
+						}),
+						cl(HUNTER, {
+							["sym"] = SymRaid(HUNTER, 6),
+							["g"] = {
+								i(188872),	-- Godstalker's Camouflage
+								i(188857),	-- Godstalker's Fauld
+								i(188862),	-- Godstalker's Sabatons
+								i(188855),	-- Godstalker's Wristwraps
+							},
+						}),
+						cl(MAGE, {
+							["sym"] = SymRaid(MAGE, 6),
+							["g"] = {
+								i(188840),	-- Erudite Occultist's Bracers
+								i(188841),	-- Erudite Occultist's Cord
+								i(188846),	-- Erudite Occultist's Shroud
+								i(188838),	-- Erudite Occultist's Treads
+							},
+						}),
+						cl(MONK, {
+							["sym"] = SymRaid(MONK, 6),
+							["g"] = {
+								i(188913),	-- Demigaunts of the Grand Upwelling
+								i(188918),	-- Drape of the Grand Upwelling
+								i(188917),	-- Footwraps of the Grand Upwelling
+								i(188915),	-- Waistwrap of the Grand Upwelling
+							},
+						}),
+						cl(PALADIN, {
+							["sym"] = SymRaid(PALADIN, 6),
+							["g"] = {
+								i(188936),	-- Luminous Chevalier's Drape
+								i(188935),	-- Luminous Chevalier's Girdle
+								i(188930),	-- Luminous Chevalier's Spurs
+								i(188934),	-- Luminous Chevalier's Vambraces
+							},
+						}),
+						cl(PRIEST, {
+							["sym"] = SymRaid(PRIEST, 6),
+							["g"] = {
+								i(188876),	-- Bracelets of the Empyrean
+								i(188882),	-- Drape of the Empyrean
+								i(188877),	-- Sash of the Empyrean
+								i(188874),	-- Slippers of the Empyrean
+							},
+						}),
+						cl(ROGUE, {
+							["sym"] = SymRaid(ROGUE, 6),
+							["g"] = {
+								i(188906),	-- Soulblade Baldric
+								i(188909),	-- Soulblade Cloak
+								i(188908),	-- Soulblade Footpads
+								i(188904),	-- Soulblade Wristguard
+							},
+						}),
+						cl(SHAMAN, {
+							["sym"] = SymRaid(SHAMAN, 6),
+							["g"] = {
+								i(188921),	-- Theurgic Starspeaker's Belt
+								i(188919),	-- Theurgic Starspeaker's Bracers
+								i(188926),	-- Theurgic Starspeaker's Sabatons
+								i(188927),	-- Theurgic Starspeaker's Shawl
+							},
+						}),
+						cl(WARLOCK, {
+							["sym"] = SymRaid(WARLOCK, 6),
+							["g"] = {
+								i(188885),	-- Bangles of the Demon Star
+								i(188883),	-- Boots of the Demon Star
+								i(188891),	-- Cape of the Demon Star
+								i(188886),	-- Waistwrap of the Demon Star
+							},
+						}),
+						cl(WARRIOR, {
+							["sym"] = SymRaid(WARRIOR, 6),
+							["g"] = {
+								i(188945),	-- Favor of the Infinite Infantry
+								i(188944),	-- Greatbelt of the Infinite Infantry
+								i(188939),	-- March of the Infinite Infantry
+								i(188943),	-- Vambraces of the Infinite Infantry
+							},
+						}),
+					}),
+				}),	
 			}),
 		},
 	}),
