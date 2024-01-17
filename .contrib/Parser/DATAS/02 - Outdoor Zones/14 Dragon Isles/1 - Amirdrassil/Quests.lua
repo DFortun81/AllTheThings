29718d8cdd69d93e7e3a00cfaad8d089b99f1cf2
Alexandr Soft <37008350+AlexSoft1911@users.noreply.github.com>
2024-01-17 16:34:02 +0500

    10.2.5 quests update

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Amirdrassil/Quests.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Amirdrassil/Quests.lua
@@ -5,16 +5,21 @@ root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_2_5 } }
 	m(AMIRDRASSIL, {
 		n(QUESTS, {
             q(78643, {  -- Fair Skies and Strong Winds
-                ["provider"] = { "n", 189842 },	-- Captain Drine
+                ["provider"] = { "n", 187678 },	-- Alexstrasza the Life-Binder
                 ["coord"] = { 58, 35.7, VALDRAKKEN },
             }),
             q(78863, {  -- A Place Beneath the Boughs
-                ["sourceQuest"] = 78643,  -- Fair Skies and Strong Winds
-                ["provider"] = { "n", 189842 },	-- Captain Drine
+                ["sourceQuests"] = { 78643 },  -- Fair Skies and Strong Winds
+                ["provider"] = { "n", 187678 },	-- Alexstrasza the Life-Binder
                 ["coord"] = { 58, 35.7, VALDRAKKEN },
             }),
+            q(78865, {  -- Siblings Until the End
+                ["sourceQuests"] = { 78643 },  -- Fair Skies and Strong Winds
+                ["provider"] = { "n", 214051 },	-- Vyranoth
+                ["coord"] = { 57.1, 37.6, VALDRAKKEN },
+            }),
             q(78864, {  -- The Returning
-                ["sourceQuest"] = 78863,  -- A Place Beneath the Boughs
+                ["sourceQuests"] = { 78863 },  -- A Place Beneath the Boughs
                 ["provider"] = { "n", 216067 },	-- Shandris Feathermoon
                 ["coord"] = { 51, 60.6, AMIRDRASSIL },
                 ["groups"] = {
