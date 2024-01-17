0cd257b526cf659c2f497613921ab5175fa4abb2
Darkal31 <michalbert@seznam.cz>
2024-01-17 18:40:02 +0100

    Fixed vendors to show under Amirdrassil (was in Dragon Isles instead).

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Amirdrassil/Vendors.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Amirdrassil/Vendors.lua
@@ -1,33 +1,33 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
-root(ROOTS.Zones, m(DRAGON_ISLES, m(AMIRDRASSIL, bubbleDown({
-		["timeline"] = { ADDED_10_2_5 },
-		["races"] = ALLIANCE_ONLY }, {
-	n(VENDORS, {
-		n(216286, {	-- Moon Priestess Lasara
-			["coord"] = { 46.5, 70.6, AMIRDRASSIL },
-			["sym"] = {
-				{"select", "npcID", 50305 },	-- Select Moon Priestess Lasara <Darnassus Quartermaster>
-				{"pop"},						-- Discard Header and aquire their children
-			},
-			["g"] = {
-				i(210424, {	-- Darnassian Tabard
-					["cost"] = { { "c", DRAGON_SUPPLIES, 250 } },
-				}),
-				i(210418, {	-- Darnassian Cloak (COSMETIC!)
-					["cost"] = { { "c", DRAGON_SUPPLIES, 250 } },
-				}),
-				i(210415, {	-- Darnassian Moonsilver Spaulders (COSMETIC!)
-					["cost"] = { { "c", DRAGON_SUPPLIES, 250 } },
-				}),
-			},
-		}),
-		n(216284, {	-- Mythrin'dir
-			["coord"] = { 54.1, 60.8, AMIRDRASSIL },
-			["g"] = {
-				i(211399),	-- Technique: Glyph of the Lunar Chameleon
-			},
-		}),
+root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_2_5 } }, {
+	m(AMIRDRASSIL, {
+		n(VENDORS, bubbleDown({ ["races"] = ALLIANCE_ONLY }, {
+			n(216286, {	-- Moon Priestess Lasara
+				["coord"] = { 46.5, 70.6, AMIRDRASSIL },
+				["sym"] = {
+					{"select", "npcID", 50305 },	-- Select Moon Priestess Lasara <Darnassus Quartermaster>
+					{"pop"},						-- Discard Header and aquire their children
+				},
+				["g"] = {
+					i(210424, {	-- Darnassian Tabard
+						["cost"] = { { "c", DRAGON_SUPPLIES, 250 } },
+					}),
+					i(210418, {	-- Darnassian Cloak (COSMETIC!)
+						["cost"] = { { "c", DRAGON_SUPPLIES, 250 } },
+					}),
+					i(210415, {	-- Darnassian Moonsilver Spaulders (COSMETIC!)
+						["cost"] = { { "c", DRAGON_SUPPLIES, 250 } },
+					}),
+				},
+			}),
+			n(216284, {	-- Mythrin'dir
+				["coord"] = { 54.1, 60.8, AMIRDRASSIL },
+				["g"] = {
+					i(211399),	-- Technique: Glyph of the Lunar Chameleon
+				},
+			}),
+		})),
 	}),
-}))))
+})));
