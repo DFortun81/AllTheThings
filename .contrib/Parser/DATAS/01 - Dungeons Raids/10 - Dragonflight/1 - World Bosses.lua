d6d8240f746a5a4e3c9abbdf330666c00531bd45
Aldori <m.baxter1122@yahoo.com>
2024-01-24 11:10:18 -0500

    Aurostor, The Hibernator coord fix

--- .contrib/Parser/DATAS/01 - Dungeons Raids/10 - Dragonflight/1 - World Bosses.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/10 - Dragonflight/1 - World Bosses.lua
@@ -122,7 +122,7 @@ root(ROOTS.Instances, tier(DF_TIER, bubbleDown({ ["timeline"] = { ADDED_10_0_2_L
 			e(2562,	-- Aurostor, The Hibernator
 			bubbleDownSelf({ ["timeline"] = { ADDED_10_2_0 } }, {
 				["crs"] = { 209574 },
-				["coord"] = { 37.1, 55.2, EMERALD_DREAM },
+				["coord"] = { 39.6, 54.1, EMERALD_DREAM },
 				--["questID"] = ,
 				["g"] = sharedData({
 					["modID"] = 3,
@@ -145,7 +145,7 @@ root(ROOTS.Instances, tier(DF_TIER, bubbleDown({ ["timeline"] = { ADDED_10_0_2_L
 			})),
 			q(76367,	-- Hibernation Heroes
 			bubbleDownSelf({ ["timeline"] = { ADDED_10_2_0 } }, {
-				["coord"] = { 37.1, 55.2, EMERALD_DREAM },
+				["coord"] = { 39.6, 54.1, EMERALD_DREAM },
 				["isWorldQuest"] = true,
 				["sym"] = {
 					{"select","encounterID",2562,},{"pop"},
