ec779111dadeba14de2c8bcfdd0827000f9f380d
Alexandr Soft <37008350+AlexSoft1911@users.noreply.github.com>
2024-01-25 19:10:09 +0500

    Fair Skies and Strong Winds seems like require previous quest and updated reward

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Amirdrassil/Quests.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Amirdrassil/Quests.lua
@@ -5,13 +5,19 @@ root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_2_5 } }
 	m(AMIRDRASSIL, {
 		n(QUESTS, {
             q(78643, {  -- Fair Skies and Strong Winds
+                ["sourceQuests"] = { 76283 },  -- Andu-falah-dor
                 ["provider"] = { "n", 187678 },	-- Alexstrasza the Life-Binder
-                ["coord"] = { 58, 35.7, VALDRAKKEN },
+                ["coord"] = { 58.0, 35.7, VALDRAKKEN },
+                ["g"] = {
+                    ach(20206, {    -- Champion of the Dragonflights
+                        title(547),    -- <Name>, Champion of the Dragonflights
+                    }),
+                },
             }),
             q(78863, {  -- A Place Beneath the Boughs
                 ["sourceQuests"] = { 78643 },  -- Fair Skies and Strong Winds
                 ["provider"] = { "n", 187678 },	-- Alexstrasza the Life-Binder
-                ["coord"] = { 58, 35.7, VALDRAKKEN },
+                ["coord"] = { 58.0, 35.7, VALDRAKKEN },
             }),
             q(78865, {  -- Siblings Until the End
                 ["sourceQuests"] = { 78643 },  -- Fair Skies and Strong Winds
