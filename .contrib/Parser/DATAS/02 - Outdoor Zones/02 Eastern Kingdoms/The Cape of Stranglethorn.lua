a5446d73f69f57ec8a33df717865653511545ef2
Crieve <dylanfortune@live.com>
2024-01-29 12:09:47 -0700

    SOD: Expert Fishing - The Bass and You is now marked as level 26.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/The Cape of Stranglethorn.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/The Cape of Stranglethorn.lua
@@ -2081,6 +2081,15 @@ root(ROOTS.Zones, m(EASTERN_KINGDOMS, {
 					["groups"] = {
 						i(16083, {	-- Expert Fishing - The Bass and You
 							["timeline"] = { "removed 3.1.0" },
+							-- #if ANYCLASSIC
+							-- #if SEASON_OF_DISCOVERY
+							["OnUpdate"] = [[function(t)
+								t.lvl = _.Settings:GetUnobtainableFilter(]] .. SOD_PHASE_ONE .. [[) and 26 or 20;
+							end]],
+							-- #else
+							["lvl"] = 20,
+							-- #endif
+							-- #endif
 							["rank"] = 3,
 						}),
 					},
