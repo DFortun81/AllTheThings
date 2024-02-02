5515e52fbddeddf3377ae5ed6c3fa90a9699a527
Myrhial <8618272+ninaarens@users.noreply.github.com>
2024-02-02 13:23:42 +0100

    BFA factions

--- .contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/Zuldazar/Factions.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/11 Zandalar/Zuldazar/Factions.lua
@@ -4,6 +4,12 @@
 root(ROOTS.Zones, m(ZANDALAR, bubbleDown({ ["timeline"] = { "added 8.0.1" } }, {
 	m(ZULDAZAR, {
 		n(FACTIONS, {
+			faction(2370, {	-- Dino Training - Direhorn
+				["races"] = HORDE_ONLY,
+			}),
+			faction(2233, {	-- Dino Training - Pterrodax
+				["races"] = HORDE_ONLY,
+			}),
 			faction(FACTION_TORTOLLAN_SEEKERS),	-- Tortollan Seekers
 		}),
 	}),
