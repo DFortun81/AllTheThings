cc850a7196a4b48518a0861d5a84596c0591ae35
Crieve <dylanfortune@live.com>
2024-02-03 16:09:27 -0700

    Moved Prime to its own window definition file.

--- src/UI/Classic Window Logic.lua
+++ src/UI/Classic Window Logic.lua
@@ -20,6 +20,7 @@ local GetCompletionText = app.GetCompletionText;
 local GetProgressTextForRow = app.GetProgressTextForRow;
 local GetProgressTextForTooltip = app.GetProgressTextForTooltip;
 local GetRelativeValue = app.GetRelativeValue;
+local ResolveSymbolicLink = app.ResolveSymbolicLink;
 local SearchForField = app.SearchForField;
 local MergeObject = app.MergeObject;
 local MergeObjects = app.MergeObjects;
@@ -2978,63 +2979,3 @@ function app:CreateMiniListFromSource(key, id, sourcePath)
 		end
 	end
 end
-
--- Create the Primary Collection Window (this allows you to save the size and location)
-app:GetWindow("Prime", {
-	parent = UIParent,
-	Silent = true,
-	AllowCompleteSound = true,
-	SettingsName = "Main List",
-	IsTopLevel = true,
-	Defaults = {
-		["y"] = 20,
-		["x"] = 0,
-		["scale"] = 1.2,
-		["width"] = 360,
-		["height"] = 600,
-		["visible"] = false,
-		["point"] = "CENTER",
-		["relativePoint"] = "CENTER",
-	},
-	Commands = {
-		"att",
-		"allthethings",
-		"attc",
-	},
-	RootCommandIndex = 2,
-	OnCommand = function(self, cmd)
-		if cmd and strlen(cmd) > 0 then
-			-- Search for the Link in the database
-			cmd = string.lower(cmd);
-			local group = app.GetCachedSearchResults(cmd, app.SearchForLink, cmd);
-			if group then app:CreateMiniListForGroup(group); end
-			return true;
-		end
-	end,
-	OnInit = function(self)
-		app.ToggleMainList = function()
-			self:Toggle();
-		end
-	end,
-	OnLoad = function(self, settings)
-		if not settings.visible then
-			self:ForceRebuild();
-		end
-	end,
-	OnRebuild = function(self)
-		-- Prime's data is built elsewhere.
-		self.data = app:GetDataCache();
-		return false;
-	end,
-	OnUpdate = function(self, ...)
-		self.DefaultUpdate(self, ...);
-
-		-- Write the current character's progress.
-		local rootData = self.data;
-		app.CurrentCharacter.PrimeData = {
-			progress = rootData.progress,
-			total = rootData.total,
-			modeString = rootData.modeString,
-		};
-	end
-});
---
+++ src/UI/Windows/Prime.lua
@@ -0,0 +1,62 @@
+-- App locals
+local appName, app = ...;
+
+-- Implementation
+app:GetWindow("Prime", {
+	parent = UIParent,
+	Silent = true,
+	AllowCompleteSound = true,
+	SettingsName = "Main List",
+	IsTopLevel = true,
+	Defaults = {
+		["y"] = 20,
+		["x"] = 0,
+		["scale"] = 1.2,
+		["width"] = 360,
+		["height"] = 600,
+		["visible"] = false,
+		["point"] = "CENTER",
+		["relativePoint"] = "CENTER",
+	},
+	Commands = {
+		"att",
+		"allthethings",
+		"attc",
+	},
+	RootCommandIndex = 2,
+	OnCommand = function(self, cmd)
+		if cmd and cmd:len() > 0 then
+			-- Search for the Link in the database
+			cmd = cmd:lower();
+			local group = app.GetCachedSearchResults(cmd, app.SearchForLink, cmd);
+			if group then app:CreateMiniListForGroup(group); end
+			return true;
+		end
+	end,
+	OnInit = function(self)
+		app.ToggleMainList = function()
+			self:Toggle();
+		end
+	end,
+	OnLoad = function(self, settings)
+		if not settings.visible then
+			self:ForceRebuild();
+		end
+	end,
+	OnRebuild = function(self)
+		-- Prime's data is built elsewhere.
+		self.data = app:GetDataCache();
+		return false;
+	end,
+	OnUpdate = function(self, ...)
+		self.DefaultUpdate(self, ...);
+
+		-- Write the current character's progress.
+		local rootData = self.data;
+		app.CurrentCharacter.PrimeData = {
+			progress = rootData.progress,
+			total = rootData.total,
+			modeString = rootData.modeString,
+		};
+	end
+});
