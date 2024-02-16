87db28d8580816075b359f964a68cfa075dc4715
Crieve <dylanfortune@live.com>
2024-02-15 17:14:12 -0700

    Slightly updated the About Page for Retail's Settings Menu.

--- src/Settings/Pages/About.lua
+++ src/Settings/Pages/About.lua
@@ -2,10 +2,10 @@ local _, app = ...;
 local L, settings = app.L, app.Settings;
 
 -- Settings: About Page
-local child = settings:CreateOptionsPage(L["ABOUT_PAGE"], true)
+local child = settings:CreateOptionsPage(L.ABOUT_PAGE, true)
 
-local textAbout = child:CreateTextLabel(L["TITLE"] .. L["ABOUT_1"])
+local textAbout = child:CreateTextLabel(L.TITLE .. L.ABOUT_TOP)
 textAbout:SetPoint("TOPLEFT", child, 0, 0)
 
-local textShoutout = child:CreateTextLabel(L["ABOUT_2"] .. L["ABOUT_3"] .. L["ABOUT_4"] .. L["COLLECTED_ICON"] .. " " .. L["COLLECTED_APPEARANCE_ICON"] .. " " ..L["NOT_COLLECTED_ICON"] .. L["ABOUT_5"])
+local textShoutout = child:CreateTextLabel(L.ABOUT_BOTTOM:format(L.COLLECTED_ICON, L.COLLECTED_APPEARANCE_ICON, L.NOT_COLLECTED_ICON))
 textShoutout:SetPoint("BOTTOMLEFT", child:GetParent(), 0, 10)
