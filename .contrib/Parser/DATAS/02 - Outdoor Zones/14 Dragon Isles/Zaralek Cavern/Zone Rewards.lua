f490e8f9548f856b49fbf0ae9a37b475bd58dc09
ImUnicke <ImUnicke+github@gmail.com>
2024-02-26 09:46:51 -0700

    Revert "clarifaction for neltharic & survveyors items"
    This reverts commit 56714eb14b8ada49fc3796bacb26c4d23f644611.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Zaralek Cavern/Zone Rewards.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Zaralek Cavern/Zone Rewards.lua
@@ -3,9 +3,70 @@
 ---------------------------------------------------
 root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_1_0 } }, {
 	m(ZARALEK_CAVERN, {
-		n(ZONE_DROPS, {
+		n(ZONE_REWARDS, {
+			i(205340),	-- Formula: Enchanted Aspect's Shadowflame Crest (RECIPE!)
+			i(205338),	-- Formula: Enchanted Whelpling's Shadowflame Crest (RECIPE!)
+			i(205339),	-- Formula: Enchanted Wyrm's Shadowflame Crest (RECIPE!)
+			i(205337),	-- Formula: Titan Training Matrix V (RECIPE!)
+			i(204075, {	-- Whelpling's Shadowflame Crest Fragment
+				["timeline"] = { ADDED_10_1_0, REMOVED_10_2_0 },
+			}),
 			i(204460),	-- Zaralek Glowspores
 			i(204911),	-- Propagated Spore
+			n(BACK, {
+				i(205056),	-- Zaralek Surveyor's Cloak
+				i(205055),	-- Zaralek Surveyor's Drape
+				i(205057),	-- Zaralek Surveyor's Greatcloak
+				i(205112),	-- Zaralek Surveyor's Shawl
+			}),
+			filter(CLOTH, {
+				i(205063),	-- Zaralek Surveyor's Cord
+				i(205061),	-- Zaralek Surveyor's Cowl
+				i(205064),	-- Zaralek Surveyor's Cuffs
+				i(205060),	-- Zaralek Surveyor's Handwraps
+				i(205062),	-- Zaralek Surveyor's Leggings
+				i(205059),	-- Zaralek Surveyor's Sandals
+				i(205089),	-- Zaralek Surveyor's Shoulderpads
+				i(205058),	-- Zaralek Surveyor's Vestment
+			}),
+			filter(FINGER_F, {
+				i(205111),	-- Zaralek Surveyor's Band
+				i(205101),	-- Zaralek Surveyor's Loop
+				i(205102),	-- Zaralek Surveyor's Signet
+			}),
+			filter(LEATHER, {
+				i(205070),	-- Zaralek Surveyor's Bindings
+				i(205068),	-- Zaralek Surveyor's Breeches
+				i(205088),	-- Zaralek Surveyor's Epaulets
+				i(205066),	-- Zaralek Surveyor's Grips
+				i(205067),	-- Zaralek Surveyor's Helm
+				i(205069),	-- Zaralek Surveyor's Sash
+				i(205087),	-- Zaralek Surveyor's Vest
+				i(205065),	-- Zaralek Surveyor's Waders
+			}),
+			filter(MAIL, {
+				i(205078),	-- Zaralek Surveyor's Bracers
+				i(205077),	-- Zaralek Surveyor's Cinch
+				i(205071),	-- Zaralek Surveyor's Chainmail
+				i(205074),	-- Zaralek Surveyor's Coif
+				i(205075),	-- Zaralek Surveyor's Greaves
+				i(205073),	-- Zaralek Surveyor's Grips
+				i(205076),	-- Zaralek Surveyor's Shoulderguards
+				i(205072),	-- Zaralek Surveyor's Striders
+			}),
+			filter(NECK_F, {
+				i(205113),	-- Zaralek Surveyor's Choker
+			}),
+			filter(PLATE, {
+				i(205086),	-- Zaralek Surveyor's Armplates
+				i(205080),	-- Zaralek Surveyor's Breastplate
+				i(205082),	-- Zaralek Surveyor's Gauntlets
+				i(205085),	-- Zaralek Surveyor's Girdle
+				i(205079),	-- Zaralek Surveyor's Helm
+				i(205083),	-- Zaralek Surveyor's Legguards
+				i(205084),	-- Zaralek Surveyor's Mantle
+				i(205081),	-- Zaralek Surveyor's Sabatons
+			}),
 			filter(TRINKET_F, {
 				i(204714),	-- Satchel of Healing Spores
 				i(204728),	-- Friendship Censer
@@ -15,24 +76,25 @@ root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_1_0 } }
 				i(204811),	-- Drogbar Stones
 			}),
 			n(WEAPONS, {
-				i(204955),	-- Neltharic Beatstick
-				i(204958),	-- Neltharic Blade
-				i(204959),	-- Neltharic Blaster
-				i(204967),	-- Neltharic Greatsword
-				i(204961),	-- Neltharic Partisan
-				i(204954),	-- Neltharic Ritual Knife
-				i(204957),	-- Neltharic Saber
-				i(204965),	-- Neltharic Shield
-				i(204953),	-- Neltharic Shiv
-				i(204956),	-- Neltharic Smasher
-				i(204960),	-- Neltharic Spear
-				i(204963),	-- Neltharic Staff
-				i(204964),	-- Neltharic Torch
-				i(204981),	-- Neltharic Wand
-				i(204966),	-- Neltharic Warglaive
-				i(204962),	-- Neltharic Warstaff
-				i(204979),	-- Neltharic Wingaxe
-				i(204980),	-- Neltharic Wingsplitter
+				i(205090),	-- Zaralek Surveyor's Barrier
+				i(205099),	-- Zaralek Surveyor's Blade
+				i(205105),	-- Zaralek Surveyor's Censer
+				i(205104),	-- Zaralek Surveyor's Cleaver
+				i(205107),	-- Zaralek Surveyor's Cudgel
+				i(205100),	-- Zaralek Surveyor's Greatsword
+				i(205103),	-- Zaralek Surveyor's Hacker
+				i(205108),	-- Zaralek Surveyor's Halberd
+				i(205097),	-- Zaralek Surveyor's Kris
+				i(205106),	-- Zaralek Surveyor's Mallet
+				i(205093),	-- Zaralek Surveyor's Polearm
+				i(205094),	-- Zaralek Surveyor's Rifle
+				i(205091),	-- Zaralek Surveyor's Scepter
+				i(205096),	-- Zaralek Surveyor's Shank
+				i(205098),	-- Zaralek Surveyor's Sword
+				i(205095),	-- Zaralek Surveyor's Spellblade
+				i(205109),	-- Zaralek Surveyor's Staff
+				i(205092),	-- Zaralek Surveyor's Wand
+				i(205110),	-- Zaralek Surveyor's Warglaive
 			}),
 		}),
 	}),
