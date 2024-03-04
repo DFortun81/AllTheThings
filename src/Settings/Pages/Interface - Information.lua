d7f583fc77602f2c6a9a992d9a7d190a2bc53933
Crieve <dylanfortune@live.com>
2024-03-04 11:21:38 -0700

    Completed By should no longer appear on Quest Objectives for a Quest.

--- src/Settings/Pages/Interface - Information.lua
+++ src/Settings/Pages/Interface - Information.lua
@@ -128,7 +128,7 @@ local function BuildKnownByInfoForKind(tooltipInfo, kind)
 end
 local function ProcessForCompletedBy(t, reference, tooltipInfo)
 	-- If the item is a recipe, then show which characters know this recipe.
-	if reference.collectible then
+	if reference.collectible and not reference.objectiveID then
 		-- Completed By for Quests
 		local id = reference.questID;
 		if id then
