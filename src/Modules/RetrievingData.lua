4f526394f91c8ff36c6650a320a6683803256b64
ImUnicke <ImUnicke+github@gmail.com>
2024-03-04 18:33:24 -0700

    Consolidate some Retrieving Data logic and removed once check which caused any Thing with 'Retrieving' in the name to never actually load

--- src/Modules/RetrievingData.lua
+++ src/Modules/RetrievingData.lua
@@ -9,13 +9,18 @@ local _, app = ...;
 -- Encapsulates the functionality for determining whether a value is to be considered 'retrieving'
 
 -- Global locals
-local RETRIEVING_DATA, RETRIEVING_ITEM_INFO
-	= RETRIEVING_DATA, RETRIEVING_ITEM_INFO;
 
 -- App locals
 
 -- Module locals
-local RETRIEVING = (" "):split(RETRIEVING_DATA);
+local RetrievingTexts = {
+	[""] = true,
+	-- not sure Classic builds have all of these strings
+	[RETRIEVING_DATA or "Retrieving data"] = true,
+	[RETRIEVING_INVITE_LIST or "Retrieving invite list"] = true,
+	[RETRIEVING_ITEM_INFO or "Retrieving item information"] = true,
+	[RETRIEVING_TRADESKILL_INFO or "Retrieving information"] = true,
+}
 
 -- Retrieving Data API Implementation
 -- Access via AllTheThings.Modules.RetrievingData
@@ -24,19 +29,14 @@ app.Modules.RetrievingData = api;
 -- Returns whether the provided string matches a string which indicates the data is not yet loaded in the Client
 api.IsRetrieving = function(text)
 	return (not text
-		or text == ""
-		or text == RETRIEVING_DATA
-		or text == RETRIEVING_ITEM_INFO
-		or text:find(RETRIEVING)
+		or RetrievingTexts[text]
 		or text:find("^%[%]"))
 		-- make sure regardless of conditional return we return a true here for consistency
 		and true;
 end
 -- Returns whether the provided string is empty or equals RETRIEVING_DATA which indicates the data is not yet loaded in the Client (not used for Items or in general)
 api.IsRetrievingData = function(text)
-	return (not text
-		or text == ""
-		or text == RETRIEVING_DATA)
+	return (not text or RetrievingTexts[text])
 		-- make sure regardless of conditional return we return a true here for consistency
 		and true;
 end
