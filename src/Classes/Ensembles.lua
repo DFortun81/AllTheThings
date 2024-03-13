f59ba8fe2cb21c9f21e7519b3d4c9b8438875a83
ImUnicke <ImUnicke+github@gmail.com>
2024-03-12 22:06:36 -0600

    Prepped a little Ensembles Item class for future work

---
+++ src/Classes/Ensembles.lua
@@ -0,0 +1,31 @@
+
+local _, app = ...
+
+-- App locals
+-- local AssignChildren, GetRelativeField, GetRelativeValue, SearchForField =
+-- 	app.AssignChildren, app.GetRelativeField, app.GetRelativeValue, app.SearchForField;
+-- local IsRetrieving = app.Modules.RetrievingData.IsRetrieving;
+-- local Colorize = app.Modules.Color.Colorize;
+-- local Search = app.SearchForObject
+
+-- Global locals
+-- local ipairs, pairs, rawset, rawget, tinsert, math_floor, RETRIEVING_DATA, wipe, select, tonumber
+-- 	= ipairs, pairs, rawset, rawget, tinsert, math.floor, RETRIEVING_DATA, wipe, select, tonumber;
+-- local C_QuestLog_GetAllCompletedQuestIDs, C_QuestLog_GetQuestObjectives = C_QuestLog.GetAllCompletedQuestIDs, C_QuestLog.GetQuestObjectives;
+-- local GetQuestLogIndexByID = C_QuestLog.GetLogIndexForQuestID or GetQuestLogIndexByID;
+-- local C_QuestLog_IsQuestFlaggedCompleted = C_QuestLog.IsQuestFlaggedCompleted;
+-- local C_QuestLog_ReadyForTurnIn = C_QuestLog.ReadyForTurnIn or IsQuestComplete;
+-- local C_QuestLog_IsOnQuest = C_QuestLog.IsOnQuest;
+-- local GetFactionInfoByID, GetNumQuestLogRewardCurrencies, GetQuestLogRewardInfo, GetSpellInfo =
+-- 	  GetFactionInfoByID, GetNumQuestLogRewardCurrencies, GetQuestLogRewardInfo, GetSpellInfo;
+-- local ALLIANCE_FACTION_ID = Enum.FlightPathFaction.Alliance;
+-- local HORDE_FACTION_ID = Enum.FlightPathFaction.Horde;
+
+-- Class locals
+
+-- Module locals
+
+
+app.CreateEnsemble = app.ExtendClass("Item", "Ensemble", "itemID", {
+
+});
