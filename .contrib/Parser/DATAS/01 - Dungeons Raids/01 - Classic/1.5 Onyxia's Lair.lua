65586dbd92d1b9f78bff8cfe0fe901683b355b2b
Crieve <dylanfortune@live.com>
2024-03-16 13:03:12 -0700

    Fixed a preprocessor in Onyxia's Lair.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/1.5 Onyxia's Lair.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/1.5 Onyxia's Lair.lua
@@ -287,9 +287,9 @@ root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, {
 					},
 				})),
 				q(7497, {	-- The Journey Has Just Begun (A)
-					--#if BEFORE 8.0.1
+					-- #if BEFORE 8.0.1
 					["description"] = "This quest requires skill level 300 in Leatherworking.",
-					--#if AFTER 8.0.1
+					-- #elseif AFTER 8.0.1
 					["description"] = "This quest requires skill level 300 in Classic Leatherworking.",
 					-- #endif
 					["qg"] = 14394,	-- Major Mattingly
@@ -313,9 +313,9 @@ root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, {
 					},
 				}),
 				q(7493, {	-- The Journey Has Just Begun (H)
-					--#if BEFORE 8.0.1
+					-- #if BEFORE 8.0.1
 					["description"] = "This quest requires skill level 300 in Leatherworking.",
-					--#if AFTER 8.0.1
+					-- #elseif AFTER 8.0.1
 					["description"] = "This quest requires skill level 300 in Classic Leatherworking.",
 					-- #endif
 					["qg"] = 14392,		-- Overlord Runthak / Overlord Natoj
