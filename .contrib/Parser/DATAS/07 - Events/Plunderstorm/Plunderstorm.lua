dce148e6c30bb3e32d78ca712b8bd9498c16fbde
Goldenshacal <goldenshacal@gmail.com>
2024-03-20 06:30:49 +0100

    plunderstorm renown rows, txt file for s4 pvp

---
+++ .contrib/Parser/DATAS/07 - Events/Plunderstorm/Plunderstorm.lua
@@ -0,0 +1,54 @@
+-----------------------------------------------------
+--     W O R L D   E V E N T S   M O D U L E       --
+-----------------------------------------------------
+PLUNDERSTORM = createHeader({
+	readable = "Plunderstorm",
+	--icon = ,
+	text = {
+		en = "Plunderstorm",
+	},
+});
+root(ROOTS.WorldEvents, bubbleDown({ ["timeline"] = { ADDED_10_2_6, REMOVED_10_2_6_SEASON_FOUR } }, {
+	n(PLUNDERSTORM, {
+		-- RENOWN 1 --
+		-- RENOWN 2 --
+		-- RENOWN 3 --
+		-- RENOWN 4 --
+		-- RENOWN 5 --
+		-- RENOWN 6 --
+		-- RENOWN 7 --
+		-- RENOWN 8 --
+		-- RENOWN 9 --
+		-- RENOWN 10 --
+		-- RENOWN 11 --
+		-- RENOWN 12 --
+		-- RENOWN 13 --
+		-- RENOWN 14 --
+		-- RENOWN 15 --
+		-- RENOWN 16 --
+		-- RENOWN 17 --
+		-- RENOWN 18 --
+		-- RENOWN 19 --
+		-- RENOWN 20 --
+		-- RENOWN 21 --
+		-- RENOWN 22 --
+		-- RENOWN 23 --
+		-- RENOWN 24 --
+		-- RENOWN 25 --
+		-- RENOWN 26 --
+		-- RENOWN 27 --
+		-- RENOWN 28 --
+		-- RENOWN 29 --
+		-- RENOWN 30 --
+		-- RENOWN 31 --
+		-- RENOWN 32 --
+		-- RENOWN 33 --
+		-- RENOWN 34 --
+		-- RENOWN 35 --
+		-- RENOWN 36 --
+		-- RENOWN 37 --
+		-- RENOWN 38 --
+		-- RENOWN 39 --
+		-- RENOWN 40 --
+	}),
+}));
