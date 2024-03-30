b8de58bd215db96071cbbff97383ff01f51831a3
Crieve <dylanfortune@live.com>
2024-03-29 19:36:17 -0700

    Cata: Added Drywhisker Gorge and Galen's Fall to Arathi Highlands' exploration.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Arathi Highlands.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Arathi Highlands.lua
@@ -104,24 +104,31 @@ root(ROOTS.Zones, m(EASTERN_KINGDOMS, {
 				exploration(336, "170:155:419:293"),	-- Circle of Outer Binding
 				exploration(334, "190:210:138:54"),		-- Circle of West Binding
 				exploration(315, "180:210:472:165"),	-- Dabyrie's Farmstead
+				-- #if AFTER CATA
+				exploration(318),	-- Drywhisker Gorge
+				-- #endif
 				exploration(327, "256:215:171:424"),	-- Faldir's Cove
+				-- #if AFTER CATA
+				exploration(5121),	-- Galen's Fall
+				-- #endif
 				exploration(314, "230:195:531:276"),	-- Go'Shek Farm
 				exploration(321, "205:250:655:120"),	-- Hammerfall
 				exploration(313, "230:240:192:90"),		-- Northfold Manor
 				exploration(320, "175:225:370:186"),	-- Refuge Pointe
 				exploration(324, "240:230:108:287"),	-- Stromgarde Keep
 				exploration(880, "200:220:355:412"),	-- Thandol Span
+				-- #if BEFORE CATA
 				exploration(1857, "190:240:87:138"),	-- Thoradin's Wall
+				-- #endif
 				exploration(317, "215:210:559:333"),	-- Witherbark Village
 				--[[
-				exploration(318),	-- Drywhisker Gorge
 				exploration(322),	-- Blackwater Shipwrecks
 				exploration(323),	-- O'Breen's Camp
 				exploration(325),	-- The Tower of Arathor
 				exploration(326),	-- The Sanctum
 				exploration(328),	-- The Drowned Reef
-				exploration(1837),	-- Witherbark Caverns
 				exploration(2401),	-- The Forbidding Sea
+				exploration(1837),	-- Witherbark Caverns
 				]]--
 			}),
 			n(FLIGHT_PATHS, {
