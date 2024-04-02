f185f72230750aa18ff407589eb8c36951e95852
ImUnicke <ImUnicke+github@gmail.com>
2024-04-02 10:30:41 -0600

    Reverted wrong quest changes

--- .contrib/Parser/DATAS/21 - Holidays/Noblegarden.lua
+++ .contrib/Parser/DATAS/21 - Holidays/Noblegarden.lua
@@ -417,6 +417,7 @@ root(ROOTS.Holidays, applyevent(EVENTS.NOBLEGARDEN, n(NOBLEGARDEN_HEADER, {
 			["coord"] = { 31.6, 63.1, VALDRAKKEN },
 			["timeline"] = { ADDED_10_0_7, REMOVED_10_2_6 },
 			["races"] = ALLIANCE_ONLY,
+			["isBreadcrumb"] = true,
 			["isYearly"] = true,
 		}),
 		q(74875, {	-- A Spring Awakening (H)
@@ -424,6 +425,7 @@ root(ROOTS.Holidays, applyevent(EVENTS.NOBLEGARDEN, n(NOBLEGARDEN_HEADER, {
 			["coord"] = { 31.6, 63.1, VALDRAKKEN },
 			["timeline"] = { ADDED_10_0_7, REMOVED_10_2_6 },
 			["races"] = HORDE_ONLY,
+			["isBreadcrumb"] = true,
 			["isYearly"] = true,
 		}),
 		q(13502, {	-- A Tisket, a Tasket, a Noblegarden Basket (A)
