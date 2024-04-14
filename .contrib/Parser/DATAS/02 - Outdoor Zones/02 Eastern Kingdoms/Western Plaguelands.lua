4cb86ad8fc1eb058bf07cc034841053a12058faa
Crieve <dylanfortune@live.com>
2024-04-14 12:11:15 -0700

    Classic: Fixed the tooltip on the Dalson Outhouse.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Western Plaguelands.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Western Plaguelands.lua
@@ -1354,17 +1354,15 @@ root(ROOTS.Zones, m(EASTERN_KINGDOMS, {
 					["lvl"] = 52,
 					["groups"] = {
 						q(5059, {	-- Locked Away
-							["provider"] = { "o", 175925 },	-- Outhouse
+							["providers"] = {
+								{ "o", 175925 },	-- Outhouse
+								{ "i",  12738 },	-- Dalson Outhouse Key
+							},
 							["coord"] = { 48.2, 49.6, WESTERN_PLAGUELANDS },
 							["timeline"] = { "removed 4.0.3" },
 							["repeatable"] = true,
 							["lvl"] = 52,
 							["groups"] = {
-								objective(1, {	-- 0/1 Dalson Outhouse Key
-									["provider"] = { "i", 12738 },	-- Dalson Outhouse Key
-									["coord"] = { 48.0, 49.8, WESTERN_PLAGUELANDS },
-									["cr"] = 10816,	-- Wandering Skeleton
-								}),
 								objective(1, {	-- 0/1 Dalson Cabinet Key
 									["questID"] = 5060,	-- Locked Away
 									["provider"] = { "i", 12739 },	-- Dalson Cabinet Key
@@ -1379,6 +1377,12 @@ root(ROOTS.Zones, m(EASTERN_KINGDOMS, {
 						i(13475, {	-- Dalson Family Wedding Ring
 							["timeline"] = { "removed 4.0.3" },
 						}),
+						-- #if OBJECTIVES
+						i(12738, {	-- Dalson Outhouse Key
+							["coord"] = { 48.0, 49.8, WESTERN_PLAGUELANDS },
+							["cr"] = 10816,	-- Wandering Skeleton
+						}),
+						-- #endif
 					},
 				}),
 				q(26936, {	-- Lower the Boom
