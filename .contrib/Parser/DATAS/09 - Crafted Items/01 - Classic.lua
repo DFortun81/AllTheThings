26d74ffcfb78aa42580e1aa49d254b9a7cc6bb43
Myrhial <8618272+ninaarens@users.noreply.github.com>
2024-04-18 11:25:48 +0200

    Better solution, we don't need to see items turned to junk in the professions list

--- .contrib/Parser/DATAS/09 - Crafted Items/01 - Classic.lua
+++ .contrib/Parser/DATAS/09 - Crafted Items/01 - Classic.lua
@@ -1259,8 +1259,10 @@ root(ROOTS.Craftables, expansion(EXPANSION.CLASSIC, {
 			i(4382),	-- Bronze Framework
 			i(4371),	-- Bronze Tube
 			i(4364),	-- Coarse Blasting Powder
+			-- #if BEFORE 4.3.0
 			i(4363),	-- Copper Modulator
 			i(4361),	-- Copper Tube
+			-- #endif
 			i(16006),	-- Delicate Arcanite Converter
 			i(15992),	-- Dense Blasting Powder
 			i(10558),	-- Gold Power Core
