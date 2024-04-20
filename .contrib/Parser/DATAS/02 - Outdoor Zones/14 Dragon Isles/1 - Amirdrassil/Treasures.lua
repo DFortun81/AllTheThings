d1580f0864ee2741e156430f8f2a51fa32693a97
ImUnicke <ImUnicke+github@gmail.com>
2024-04-20 01:56:11 -0600

    Fixed current questID's for Amirdrassil Kaldorei treasures

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Amirdrassil/Treasures.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/1 - Amirdrassil/Treasures.lua
@@ -6,77 +6,77 @@ root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_2_5 } }
 		n(TREASURES, {
 			o(421180, {	-- Kaldorei Backpack
 				["coord"] = { 52.5, 17.7, AMIRDRASSIL },
-				["questID"] = 79494,
+				["questID"] = 82825,	-- old: 79494,
 				["g"] = {
 					i(213005),	-- Blue Kaldorei Backpack
 				},
 			}),
 			o(421183, {	-- Kaldorei Bag
 				["coord"] = { 55.33, 64.28, AMIRDRASSIL },
-				["questID"] = 79472,
+				["questID"] = 82821,	-- old: 79472,
 				["g"] = {
 					i(213010),	-- Blue Kaldorei Pouch
 				},
 			}),
 			o(421338, {	-- Kaldorei Bag
 				["coord"] = { 48.3, 76.4, AMIRDRASSIL },
-				["questID"] = 79493,
+				["questID"] = 82824,	-- old: 79493,
 				["g"] = {
 					i(213009),	-- Violet Kaldorei Pouch
 				},
 			}),
 			o(421179, {	-- Kaldorei Bedroll
 				["coord"] = { 54.7, 77.2, AMIRDRASSIL },
-				["questID"] = 79473,
+				["questID"] = 82822,	-- old: 79473,
 				["g"] = {
 					i(213003),	-- Blue Kaldorei Bedroll
 				},
 			}),
 			o(421181, {	-- Kaldorei Dagger
 				["coord"] = { 31.3, 16.1, AMIRDRASSIL },
-				["questID"] = 79496,
+				["questID"] = 82826,	-- old: 79496,
 				["g"] = {
 					i(213008),	-- Kaldorei Bow Carver
 				},
 			}),
 			o(421339, {	-- Kaldorei Horn
 				["coord"] = { 58.5, 51.9, AMIRDRASSIL },
-				["questID"] = 79498,
+				["questID"] = 82828,	-- old:79498,
 				["g"] = {
 					i(213006),	-- Night Elven Horn
 				},
 			}),
 			o(421182, {	-- Kaldorei Horn
 				["coord"] = { 49.1, 70.3, AMIRDRASSIL },
-				["questID"] = 79497,
+				["questID"] = 82827,	-- old: 79497,
 				["g"] = {
 					i(213007),	-- Night Elven Signal
 				},
 			}),
 			o(421555, {	-- Kaldorei Moon Bow
 				["coord"] = { 29.0, 28.9, AMIRDRASSIL },
-				["questID"] = 79520,
+				["questID"] = 82831,	-- old: 79520,
 				["g"] = {
 					i(213160),	-- Kaldorei Moon Bow
 				},
 			}),
 			o(421184, {	-- Kaldorei Shield
 				["coord"] = { 47.9, 56.9, AMIRDRASSIL },
-				["questID"] = 79499,
+				["questID"] = 82829,	-- old: 79499,
 				["g"] = {
 					i(213011),	-- Night Elven Shield
 				},
 			}),
 			o(421185, {	-- Kaldorei Spear
 				["coord"] = { 53.5, 55.7, AMIRDRASSIL },
-				["questID"] = 79490,
+				["questID"] = 82823,	-- old: 79490,
 				["g"] = {
 					i(213012),	-- Night Elven Spear
 				},
 			}),
 			o(421186, {	-- Kaldorei Spyglass
 				["coord"] = { 51.9, 5.9, AMIRDRASSIL },
-				["questID"] = 79500,
+				["questID"] = 82830,	-- old:79500,
 				["g"] = {
 					i(213013),	-- Kaldorei Sentinel's Spyglass
 				},
