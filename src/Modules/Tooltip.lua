DFortun81/AllTheThings

commit d1d7e78a68f0daf47efcc1374812654353f29a81

VSCode source warnings!

src/Modules/Tooltip.lua
@@ -9,6 +9,8 @@ local L = app.L;
 -- Global locals
 local ipairs, pairs, InCombatLockdown, pcall, tinsert, tostring, tonumber, C_Map_GetPlayerMapPosition, math_sqrt, GameTooltip
 	= ipairs, pairs, InCombatLockdown, pcall, tinsert, tostring, tonumber, C_Map.GetPlayerMapPosition, math.sqrt, GameTooltip
+---@diagnostic disable-next-line: deprecated
+local GetItemInfo = ((C_Item and C_Item.GetItemInfo) or GetItemInfo);
 
 local timeFormatter = CreateFromMixins(SecondsFormatterMixin);
 timeFormatter:Init(1, SecondsFormatter.Abbreviation.Truncate);
@@ -171,7 +173,7 @@ local PLAYER_TOOLTIPS = {
 		local leftSide = _G[self:GetName() .. "TextLeft1"];
 		if leftSide then leftSide:SetText("|cff665a2c" .. name .. " the Time-Loser|r"); end
 		local rightSide = _G[self:GetName() .. "TextRight2"];
-		if rightSide then rightSide:SetText(GetCollectionIcon(0)); end
+		if rightSide then rightSide:SetText(app.GetCollectionIcon(0)); end
 		self:AddLine("This scumbag abused an auto-invite addon to steal the Time-Lost Proto Drake from a person that had them on their friends list. ATT has deemed this unacceptable behaviour and will forever stain this player's reputation so long as they remain on the server.", 0.4, 0.8, 1, true);
 	end,
 };
@@ -186,6 +188,7 @@ local tooltipFunction = function(self, locClass, engClass, locRace, engRace, gen
 	end
 	local rightSide = _G[self:GetName() .. "TextRight2"];
 	leftSide = _G[self:GetName() .. "TextLeft2"];
+---@diagnostic disable-next-line: undefined-global
 	if leftSide and rightSide and not ElvUI then
 		leftSide:SetText(L.TITLE);
 		leftSide:Show();
@@ -215,6 +218,7 @@ tooltipFunction = function(self, locClass, engClass, locRace, engRace, gender, n
 	end
 	local rightSide = _G[self:GetName() .. "TextRight2"];
 	leftSide = _G[self:GetName() .. "TextLeft2"];
+	---@diagnostic disable-next-line: undefined-global
 	if leftSide and rightSide and not ElvUI then
 		leftSide:SetText(L.TITLE);
 		leftSide:Show();
@@ -561,7 +565,7 @@ local function StripColorAndTextureData(txt)
 		c = txt:sub(i,i);
 		if c == "|" then
 			local foundCommand, j = FindCommandEnd(txt, i, l)
-			if foundCommand then
+			if foundCommand and j then
 				i = j;
 			else
 				str = str .. "\\" .. c;
@@ -711,7 +715,8 @@ if TooltipDataProcessor then
 	-- 10.0.2
 	-- https://wowpedia.fandom.com/wiki/Patch_10.0.2/API_changes#Tooltip_Changes
 	-- many of these don't include an ID in-game so they don't attach results. maybe someday they will...
-	local Enum_TooltipDataType, GetItemInfoInstant, TooltipUtil = Enum.TooltipDataType, GetItemInfoInstant, TooltipUtil;
+	---@diagnostic disable-next-line: deprecated
+	local Enum_TooltipDataType, GetItemInfoInstant, TooltipUtil = Enum.TooltipDataType, ((C_Item and C_Item.GetItemInfoInstant) or GetItemInfoInstant), TooltipUtil;
 	local TooltipTypes = {
 		[Enum_TooltipDataType.Toy] = "itemID",
 		[Enum_TooltipDataType.Item] = "itemID",
@@ -744,13 +749,14 @@ if TooltipDataProcessor then
 
 	local function RerenderCurrency(self, currencyID)
 		if self:IsVisible() then
+			---@diagnostic disable-next-line: redundant-parameter
 			GameTooltip.SetCurrencyByID(self, currencyID, 1);
 		end
 	end
 	local function AttachTooltip(self, ttdata)
 		if self.AllTheThingsIgnored or not CanAttachTooltips() then return; end
 
-		local ttType, ttId = ttdata and ttdata.type;
+		local ttType, ttId = ttdata and ttdata.type, nil;
 		if ttType then
 			ttId = ttdata.id;
 			-- Debugging without ATT exclusions
@@ -924,7 +930,7 @@ if TooltipDataProcessor then
 		end
 
 		-- Check the extra data to see if there's an alternate search for the data
-		if ttType then
+		if ttType and ttId then
 			local knownSearchField = TooltipTypes[ttType];
 			if not knownSearchField then
 				-- other ways to search
@@ -975,6 +981,7 @@ if TooltipDataProcessor then
 		if GameTooltip and GameTooltip:IsVisible() then
 			-- app.PrintDebug("Auto-refresh tooltip")
 			-- Make sure the tooltip will try to re-attach the data if it's from an ATT row
+			---@diagnostic disable-next-line: inject-field
 			GameTooltip.ATT_AttachComplete = nil;
 			GameTooltip:Show();
 		end
@@ -1124,6 +1131,8 @@ else
 	ItemRefShoppingTooltip1:HookScript("OnShow", AttachTooltip);
 	ItemRefShoppingTooltip2:HookScript("OnShow", AttachTooltip);
 
+	---@diagnostic disable-next-line: undefined-global
+	local WorldMapTooltip = WorldMapTooltip;
 	if WorldMapTooltip then
 		WorldMapTooltip.ItemTooltip.Tooltip:HookScript("OnTooltipSetQuest", AttachTooltip);
 		WorldMapTooltip.ItemTooltip.Tooltip:HookScript("OnTooltipSetItem", AttachTooltip);
@@ -1135,6 +1144,7 @@ else
 
 	local GameTooltip_SetLFGDungeonReward = GameTooltip.SetLFGDungeonReward;
 	if GameTooltip_SetLFGDungeonReward then
+		---@diagnostic disable-next-line: duplicate-set-field
 		GameTooltip.SetLFGDungeonReward = function(self, dungeonID, rewardIndex)
 			GameTooltip_SetLFGDungeonReward(self, dungeonID, rewardIndex);
 			if CanAttachTooltips() then
@@ -1154,6 +1164,7 @@ else
 
 	local GameTooltip_SetLFGDungeonShortageReward = GameTooltip.SetLFGDungeonShortageReward;
 	if GameTooltip_SetLFGDungeonShortageReward then
+		---@diagnostic disable-next-line: duplicate-set-field
 		GameTooltip.SetLFGDungeonShortageReward = function(self, dungeonID, shortageSeverity, lootIndex)
 			--app.PrintDebug("GameTooltip.SetLFGDungeonShortageReward",dungeonID, shortageSeverity, lootIndex );
 			GameTooltip_SetLFGDungeonShortageReward(self, dungeonID, shortageSeverity, lootIndex);
@@ -1173,8 +1184,10 @@ else
 	end
 
 	local GameTooltip_SetCurrencyByID = GameTooltip.SetCurrencyByID;
+	---@diagnostic disable-next-line: duplicate-set-field
 	GameTooltip.SetCurrencyByID = function(self, currencyID, count)
 		if GameTooltip_SetCurrencyByID then
+			---@diagnostic disable-next-line: redundant-parameter
 			GameTooltip_SetCurrencyByID(self, currencyID, count);
 		end
 		if CanAttachTooltips() then
@@ -1185,6 +1198,9 @@ else
 
 	local GameTooltip_SetCurrencyToken = GameTooltip.SetCurrencyToken;
 	if GameTooltip_SetCurrencyToken then
+		---@diagnostic disable-next-line: undefined-global
+		local GetCurrencyListInfo = GetCurrencyListInfo;
+		---@diagnostic disable-next-line: duplicate-set-field
 		GameTooltip.SetCurrencyToken = function(self, tokenID)
 			GameTooltip_SetCurrencyToken(self, tokenID);
 			if CanAttachTooltips() then
@@ -1232,6 +1248,7 @@ local function ShowItemCompareTooltips(...)
 	local count = #items;
 	if count > 0 then
 		for i,item in ipairs(items) do
+			---@diagnostic disable-next-line: undefined-field
 			local shoppingTooltip = GameTooltip.shoppingTooltips[i];
 			if shoppingTooltip then
 				shoppingTooltip.attItem = type(item) == "number" and select(2, GetItemInfo(item)) or item;
@@ -1243,6 +1260,7 @@ local function ShowItemCompareTooltips(...)
 
 		-- Quick maths
 		-- Taken from https://github.com/Ennie/wow-ui-source/blob/master/FrameXML/GameTooltip.lua
+		---@diagnostic disable-next-line: undefined-field
 		local shoppingTooltip1, shoppingTooltip2, shoppingTooltip3 = unpack(GameTooltip.shoppingTooltips);
 		local leftPos, rightPos = (GameTooltip:GetLeft() or 0), (GameTooltip:GetRight() or 0);
 		if GetScreenWidth() - rightPos < leftPos then

lib/EventRegistration.lua
@@ -14,6 +14,7 @@ local events = setmetatable({}, {
 	end
 });
 local frame = CreateFrame("FRAME", nil, UIParent, BackdropTemplateMixin and "BackdropTemplate");
+---@diagnostic disable-next-line: inject-field
 frame.Suffix = "ATTFRAME";
 if app.DebuggingEvents then
 frame:SetScript("OnEvent", function(self, e, ...)
@@ -38,8 +39,8 @@ app.RegisterFuncEvent = function(self, event, func)
 		app.events[event] = func
 	end
 end
-app.UnregisterEvent = function(self, event, ...)
-	frame:UnregisterEvent(event, ...);
+app.UnregisterEvent = function(self, event)
+	frame:UnregisterEvent(event);
 end
 app.UnregisterEventClean = function(self, event)
 	frame:UnregisterEvent(event);

lib/Sort.lua
@@ -6,8 +6,8 @@ local _, app = ...;
 -- Encapsulates the functionality for performing sort logic against sets of ATT groups
 
 -- Global locals
-local ipairs, pairs, tostring, type, table_sort, pcall
-	= ipairs, pairs, tostring, type, table.sort, pcall;
+local ipairs, tostring, type, table_sort, pcall
+	= ipairs, tostring, type, table.sort, pcall;
 
 -- Module locals
 
@@ -16,7 +16,7 @@ local sortA, sortB;
 local function calculateSourceQuestDepth(group, text)
 	if group.sourceQuestDepth then return group.sourceQuestDepth; end
 	if group.sourceQuests then
-		local maxDepth, results, depth = 0;
+		local maxDepth, results, depth = 0, nil, nil;
 		for i,sourceQuestID in ipairs(group.sourceQuests) do
 			results = app.SearchForField("questID", sourceQuestID);
 			if results and #results > 0 then

lib/Table.lua
@@ -62,7 +62,7 @@ app.ArrayAppend = function(a1, ...)
 	local arrs = select("#", ...);
 	if arrs > 0 then
 		a1 = a1 or {};
-		local i, a = #a1 + 1;
+		local i, a = #a1 + 1, nil;
 		for n=1,arrs do
 			a = select(n, ...);
 			if a then

locales/de.lua
@@ -496,7 +496,6 @@ for key,value in pairs({
 		["Shadowlands"] = "SL",
 		["Spieler gegen Spieler"] = "PvP",
 		["Schlachtzugbrowser"] = "LFR",
-		["Schlachtzugbrowser"] = "LFR",
 		["Normal"] = "N",
 		["Heroisch"] = "H",
 		["Mythisch"] = "M",

src/Cache.lua
@@ -315,7 +315,7 @@ local function zoneTextAreasRunner(group, value)
 	if name then app.L.MAP_ID_TO_ZONE_TEXT[mapID] = name; end
 
 	-- Remap the original mapID to the new mapID when it encounters any of these artIDs.
-	local mapIDs, parentMapID, info = {};
+	local mapIDs, parentMapID, info = {}, nil, nil;
 	if group.coords then
 		parentMapID = group.coords[1][3];
 		if parentMapID then
@@ -624,7 +624,7 @@ local fieldConverters = {
 local _converter;
 local function _CacheFields(group)
 	local n = 0;
-	local clone, mapKeys, key, value, hasG = {};
+	local clone, mapKeys, key, value, hasG = {}, nil, nil, nil, nil;
 	for key,value in pairs(group) do
 		if key == "g" then
 			hasG = true;

src/Classes/BattlePet.lua
@@ -8,7 +8,8 @@ local wipe, setmetatable, rawget, select
 
 -- Blizzard
 local GetItemInfo
-	= GetItemInfo
+---@diagnostic disable-next-line: deprecated
+	= ((C_Item and C_Item.GetItemInfo) or GetItemInfo)
 
 -- Module
 

src/Classes/CharacterClass.lua
@@ -27,7 +27,7 @@ local ClassIcons = {
 	[13] = app.asset("ClassIcon_Evoker"),
 };
 local ClassInfoByID, ClassInfoByClassFile, ClassInfoByClassName = {}, {}, {};
-local GetSpecializationInfoByID, SpecInfoMetatable = GetSpecializationInfoByID;
+local GetSpecializationInfoByID, SpecInfoMetatable = GetSpecializationInfoByID, nil;
 if GetSpecializationInfoByID then
 	SpecInfoMetatable = { __index = function(t, key)
 		local specID = math_floor(key);
@@ -122,6 +122,7 @@ local ClassInfoMetatable = { __index = function(t, key)
 				isValid = not not colors,
 				c = { classID },
 			};
+			---@diagnostic disable-next-line: inject-field
 			info.icontext = "|T" .. info.icon .. ":0|t " .. info.text;
 			
 			rawset(ClassInfoByID, classID, info);
@@ -219,7 +220,7 @@ app.CreateUnit = app.CreateClass("Unit", "unit", {
 	["title"] = function(t)
 		if IsInGroup() then
 			if rawget(t, "isML") then return MASTER_LOOTER; end
-			if UnitIsGroupLeader(t.unit, "raid") then return RAID_LEADER; end
+			if UnitIsGroupLeader(t.unit) then return RAID_LEADER; end
 		end
 	end,
 	["lvl"] = function(t)

commit 85ec4626d31a6c24fcb21cc82fdf767359dbe8ca

Fixed object and other tooltips broken by d1d7e78a68f0daf47efcc1374812654353f29a81

src/Modules/Tooltip.lua
@@ -842,6 +842,7 @@ if TooltipDataProcessor then
 		end
 
 		-- app.PrintDebug(self:GetName(),link,target,spellID,id,ttType,ttId)
+		-- app.PrintTable(ttdata)
 
 		--[[--]
 		-- Debug all of the available fields on the tooltip.
@@ -928,11 +929,11 @@ if TooltipDataProcessor then
 		end
 
 		-- Check the extra data to see if there's an alternate search for the data
-		if ttType and ttId then
+		if ttType then
 			local knownSearchField = TooltipTypes[ttType];
 			if not knownSearchField then
 				-- other ways to search
-				if app.ActiveRowReference and ttType == Enum_TooltipDataType.Spell then
+				if ttId and app.ActiveRowReference and ttType == Enum_TooltipDataType.Spell then
 					-- allow spell tooltip information when it's an ATT row
 					knownSearchField = "spellID";
 				end
@@ -949,7 +950,7 @@ if TooltipDataProcessor then
 						ttId = objectID;
 					end
 				end
-				if ttType == 21 then	-- Minimap mouseover
+				if ttType == Enum_TooltipDataType.MinimapMouseover then
 					local content = ttdata.lines;
 					if content and #content > 0 then
 						local text = content[1].leftText;
@@ -972,7 +973,7 @@ if TooltipDataProcessor then
 				return true;
 			end
 		end
-		-- print("AttachTooltip-Return");
+		-- app.PrintDebug("AttachTooltip-Return");
 	end
 
 	app.AddEventRegistration("TOOLTIP_DATA_UPDATE", function(...)