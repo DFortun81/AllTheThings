375584aef09e0be7f26e357ecf17c7cd4b57cdd1
DannyDonkey <145834650+DannyDonkey@users.noreply.github.com>
2024-04-23 02:16:01 +0200

    TBC Karazhan: Koran minReputation

--- .contrib/Parser/DATAS/01 - Dungeons Raids/02 - Burning Crusade/1 Karazhan (Raid).lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/02 - Burning Crusade/1 Karazhan (Raid).lua
@@ -635,6 +635,7 @@ root(ROOTS.Instances, expansion(EXPANSION.TBC, applyclassicphase(TBC_PHASE_ONE, 
 			n(VENDORS, {
 				n(16388, {	-- Koren
 					["description"] = "This vendor is located in the Livery Stables, but will only serve characters that are at least Honoured with The Violet Eye.",
+					["minReputation"] = { 967, HONORED },	-- The Violet Eye, Honored
 					["groups"] = {
 						i(31395),	-- Plans: Iceguard Helm (RECIPE!)
 						i(31393),	-- Plans: Iceguard Breastplate (RECIPE!)
