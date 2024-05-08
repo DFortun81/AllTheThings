2e832b543d367892525460e7e05b1571cb5a38f7
Crieve <dylanfortune@live.com>
2024-05-07 20:51:09 -0700

    Cata: STV mapID 224 on Cata Classic.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/The Cape of Stranglethorn.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/The Cape of Stranglethorn.lua
@@ -6,6 +6,11 @@ root(ROOTS.Zones, m(EASTERN_KINGDOMS, {
 	m(THE_CAPE_OF_STRANGLETHORN, {
 		["lore"] = "The Cape of Stranglethorn is the southern part of Stranglethorn Vale, now split from the Shattering. It traces the escalating conflict between the goblin pirates of Booty Bay and their rivals, the Bloodsail Buccaneers. The rest of the zone is dotted with troll ruins and sandy beaches.",
 		["icon"] = "Interface\\Icons\\achievement_zone_stranglethorn_01",
+		-- #if ANYCLASSIC
+		-- CRIEVE NOTE: This is super weird. In Cata Classic they used the retail mapID for STV as the combined STV map.
+		-- The only way to get it on the mini list is to fly south from blasted lands or to use the world map button.
+		["maps"] = { 224 },
+		-- #endif
 		["groups"] = {
 			n(ACHIEVEMENTS, {
 				applyclassicphase(PHASE_ONE, ach(871, {	-- Avast Ye, Admiral!
