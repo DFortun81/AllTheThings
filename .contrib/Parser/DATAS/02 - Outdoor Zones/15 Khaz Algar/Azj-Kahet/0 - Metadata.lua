d3ba7123c7d7775d2192e0c231723f949ce528f4
Darkal31 <michalbert@seznam.cz>
2024-05-14 19:01:29 +0200

    It's wrong in more places.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Azj-Kahet/0 - Metadata.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Azj-Kahet/0 - Metadata.lua
@@ -2,16 +2,15 @@
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
 --[[
-root(ROOTS.Zones, m(KHAZ_ALGAR, {
+root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_1_LAUNCH } }, {
 	m(AZJ_KAHET, {
 		["icon"] = ,
 		["lore"] = "",
-		["timeline"] = { ADDED_11_0_1_LAUNCH },
 		["maps"] = {
 			2256,	-- Azj-Kahet Lower
 			2213,	-- Nerub'ar
 			2216,	-- Nerub'ar Lower
 		},
 	}),
-}));
+})));
 --]]
--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Isle of Dorn/0 - Metadata.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Isle of Dorn/0 - Metadata.lua
@@ -1,10 +1,9 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
-root(ROOTS.Zones, m(KHAZ_ALGAR, {
+root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_1_LAUNCH } }, {
 	m(ISLE_OF_DORN, {
 		--["icon"] = ,
 		--["lore"] = "",
-		["timeline"] = { ADDED_11_0_1_LAUNCH },
 	}),
-}));
+})));
