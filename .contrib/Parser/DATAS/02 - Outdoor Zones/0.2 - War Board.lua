219511ac931a72e9a479be960a8830ef10461d86
Crieve <dylanfortune@live.com>
2024-05-14 08:53:53 -0700

    Cata: Added base level and max level (guessing, mostly with the max level) requirements and preformatting to the Hero's Call quests introduced with Cataclysm. (these will be moved to their respective zones)

--- .contrib/Parser/DATAS/02 - Outdoor Zones/0.2 - War Board.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/0.2 - War Board.lua
@@ -161,113 +161,256 @@ root(ROOTS.Zones, {
 	},{
 		["description"] = "These quests can be obtained from any city or town to lead the Character to a specific Zone.",
 		["g"] = {
-			q(28579, {	-- Hero's Call: Badlands!
+			heroscall(q(28579, {	-- Hero's Call: Badlands!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { IRONFORGE, STORMWIND_CITY },	-- Only found in Ironforge & Stormwind City in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28673, {	-- Hero's Call: Blasted Lands!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 44. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 50 (TODO: Test max level between 48 and 55, 50 is based on Wowpedia info)
+				["lvl"] = { 44, 50 },
+				-- #endif
+			})),
+			heroscall(q(28673, {	-- Hero's Call: Blasted Lands!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { IRONFORGE, STORMWIND_CITY },	-- Only found in Ironforge & Stormwind City in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28666, {	-- Hero's Call: Burning Steppes!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 54. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 60 (TODO: Test max level between 58 and 65, 60 is based on Wowpedia info)
+				["lvl"] = { 54, 60 },
+				-- #endif
+			})),
+			heroscall(q(28666, {	-- Hero's Call: Burning Steppes!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { IRONFORGE, STORMWIND_CITY },	-- Only found in Ironforge & Stormwind City in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28564, {	-- Hero's Call: Duskwood!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 49. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 56 (TODO: Test max level between 53 and 60, 56 is based on Wowpedia info)
+				["lvl"] = { 49, 56 },
+				-- #endif
+			})),
+			heroscall(q(28564, {	-- Hero's Call: Duskwood!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { STORMWIND_CITY },	-- Only found in Stormwind City in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 20
-			}),
-			q(28552, {	-- Hero's Call: Dustwallow Marsh! (breadcrumb quest for 27210, not available if 26702 is completed) (max level 38)
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 19. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 25 (TODO: Test max level between 22 and 32, 25 is based on Wowpedia info)
+				["lvl"] = { 19, 25 },
+				-- #endif
+			})),
+			heroscall(q(28552, {	-- Hero's Call: Dustwallow Marsh! (breadcrumb quest for 27210, not available if 26702 is completed) (max level 38)
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { DARNASSUS, STORMWIND_CITY, THE_EXODAR },	-- Only found in Darnassus & Stormwind City & The Exodar in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 35,
-			}),
-			q(28578, {	-- Hero's Call: Eastern Plaguelands!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 34. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 40 (TODO: Test max level between 36 and 44, 40 is based on Wowpedia info)
+				["lvl"] = { 34, 40 },
+				-- #endif
+			})),
+			heroscall(q(28578, {	-- Hero's Call: Eastern Plaguelands!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { IRONFORGE, STORMWIND_CITY },	-- Only found in Ironforge & Stormwind City in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28543, {	-- Hero's Call: Felwood! (breadcrumb quest for 27997) (max level 48)
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 39. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 45 (TODO: Test max level between 42 and 52, 45 is based on Wowpedia info)
+				["lvl"] = { 39, 45 },
+				-- #endif
+			})),
+			heroscall(q(28543, {	-- Hero's Call: Felwood! (breadcrumb quest for 27997) (max level 48)
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { DARNASSUS, THE_EXODAR },	-- Only found in Darnassus & The Exodar in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28511, {	-- Hero's Call: Feralas! (breadcrumb quest for 25447, not available if 14410 is completed) (max level 38)
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 44. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 50 (TODO: Test max level between 48 and 55, 50 is based on Wowpedia info)
+				["lvl"] = { 44, 50 },
+				-- #endif
+			})),
+			heroscall(q(28511, {	-- Hero's Call: Feralas! (breadcrumb quest for 25447, not available if 14410 is completed) (max level 38)
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { DARNASSUS, THE_EXODAR },	-- Only found in Darnassus & The Exodar in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 35,
-			}),
-			
-			q(28699, {	-- Hero's Call: Northern Stranglethorn!
-				["maps"] = { NORTHERN_STRANGLETHORN },
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 34. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 40 (TODO: Test max level between 36 and 44, 40 is based on Wowpedia info)
+				["lvl"] = { 34, 40 },
+				-- #endif
+			})),
+			heroscall(q(28699, {	-- Hero's Call: Northern Stranglethorn!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { STORMWIND_CITY },	-- Only found in Stormwind City in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 25,
-			}),
-			q(28708, {	-- Hero's Call: Outland!(breadcrumb quest for 10288) (max level 68)
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 24. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 30 (TODO: Test max level between 28 and 35, 30 is based on Wowpedia info)
+				["lvl"] = { 24, 30 },
+				-- #endif
+			})),
+			heroscall(q(28708, {	-- Hero's Call: Outland!(breadcrumb quest for 10288) (max level 68)
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { DARNASSUS, IRONFORGE, STORMWIND_CITY, THE_EXODAR },	-- Only found in Darnassus & Ironforge & Stormwind City & The Exodar in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 58,
-			}),
-			q(28582, {	-- Hero's Call: Searing Gorge!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 58. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 66 (TODO: Test max level between 60 and 70, 66 is based on Wowpedia info)
+				["lvl"] = { 58, 66 },
+				-- #endif
+			})),
+			heroscall(q(28582, {	-- Hero's Call: Searing Gorge!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { IRONFORGE, STORMWIND_CITY },	-- Only found in Ironforge & Stormwind City in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28528, {	-- Hero's Call: Silithus! (breadcrumb quest for 8280, not available if 28859,28856,28527 are completed) (max level 58)
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 47. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 54 (TODO: Test max level between 49 and 58, 54 is based on Wowpedia info)
+				["lvl"] = { 47, 54 },
+				-- #endif
+			})),
+			heroscall(q(28528, {	-- Hero's Call: Silithus! (breadcrumb quest for 8280, not available if 28859,28856,28527 are completed) (max level 58)
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { DARNASSUS, THE_EXODAR },	-- Only found in Darnassus & The Exodar in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28550, {	-- Hero's Call: Southern Barrens! (breadcrumb quest for 24862) (max level 33)
-				["isBreadcrumb"] = true,
-				["lvl"] = 25,
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 54. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 60 (TODO: Test max level between 58 and 65, 60 is based on Wowpedia info)
+				["lvl"] = { 54, 60 },
+				-- #endif
+			})),
+			heroscall(q(28550, {	-- Hero's Call: Southern Barrens! (breadcrumb quest for 24862) (max level 33)
+				-- #if NOT ANYCLASSIC
 				["description"] = "This quest needs to be accepted from the 'Adventure Guide' and not the 'Hero's Call Board'.",
-			}),
-			q(28551, {	-- Hero's Call: Southern Barrens!
+				-- #endif
+				["maps"] = { DARNASSUS, THE_EXODAR },	-- Only found in Darnassus & The Exodar in Cataclysm.
+				["timeline"] = { ADDED_4_0_3 },
 				["isBreadcrumb"] = true,
-				["lvl"] = 25,
-			}),
-			q(28675, {	-- Hero's Call: Swamp of Sorrows!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 29. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 35 (TODO: Test max level between 32 and 39, 35 is based on Wowpedia info)
+				["lvl"] = { 29, 35 },
+				-- #endif
+			})),
+			heroscall(q(28551, {	-- Hero's Call: Southern Barrens!
+				["maps"] = { STORMWIND_CITY },	-- Only found in Stormwind City in Cataclysm.
+				["timeline"] = { ADDED_4_0_3 },
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28507, {	-- Hero's Call: Tanaris! (breadcrumb quest for 25048, not available if 27446 is completed) (max level 48)
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 29. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 35 (TODO: Test max level between 32 and 39, 35 is based on Wowpedia info)
+				["lvl"] = { 29, 35 },
+				-- #endif
+			})),
+			heroscall(q(28675, {	-- Hero's Call: Swamp of Sorrows!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { IRONFORGE, STORMWIND_CITY },	-- Only found in Ironforge & Stormwind City in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28702, {	-- Hero's Call: The Cape of Stranglethorn!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 51. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 58 (TODO: Test max level between 51 and 60, 58 is based on Wowpedia info)
+				["lvl"] = { 51, 58 },
+				-- #endif
+			})),
+			heroscall(q(28507, {	-- Hero's Call: Tanaris! (breadcrumb quest for 25048, not available if 27446 is completed) (max level 48)
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { DARNASSUS, THE_EXODAR },	-- Only found in Darnassus & The Exodar in Cataclysm.
 				["isBreadcrumb"] = true,
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 44. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 50 (TODO: Test max level between 48 and 55, 50 is based on Wowpedia info)
+				["lvl"] = { 44, 50 },
+				-- #endif
+			})),
+			heroscall(q(28702, {	-- Hero's Call: The Cape of Stranglethorn!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { STORMWIND_CITY },	-- Only found in Stormwind City in Cataclysm.
 				["races"] = ALLIANCE_ONLY,
-				["lvl"] = 30,
-			}),
-			q(26542, {	-- Hero's Call: The Hinterlands!
 				["isBreadcrumb"] = true,
-				["lvl"] = 30,
-			}),
-			q(38931, {	-- Hero's Call: The Hinterlands!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 29. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 35 (TODO: Test max level between 32 and 39, 35 is based on Wowpedia info)
+				["lvl"] = { 29, 35 },
+				-- #endif
+			})),
+			heroscall(q(26542, {	-- Hero's Call: The Hinterlands!
+				["qg"] = 2700,	-- Captain Nials
+				["coord"] = { 40, 48.8, ARATHI_HIGHLANDS },
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { IRONFORGE },	-- Only found in Ironforge in Cataclysm.
+				["isBreadcrumb"] = true,
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 29. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 35 (TODO: Test max level between 32 and 39, 35 is based on Wowpedia info)
+				["lvl"] = { 29, 35 },
+				-- #endif
+			})),
+			heroscall(q(38931, {	-- Hero's Call: The Hinterlands!
 				["timeline"] = { CREATED_6_2_0 },
 				["isBreadcrumb"] = true,
-				["lvl"] = 30,
-				["u"] = REMOVED_FROM_GAME,
-			}),
-			q(28503, {	-- Hero's Call: Thousand Needles! (breadcrumb quest for 25486, not available if 25479,25481 are completed) (max level 43)
-				["isBreadcrumb"] = true,
-				["lvl"] = 40,
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 29. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 35 (TODO: Test max level between 32 and 39, 35 is based on Wowpedia info)
+				["lvl"] = { 29, 35 },
+				-- #endif
+			})),
+			heroscall(q(28503, {	-- Hero's Call: Thousand Needles! (breadcrumb quest for 25486, not available if 25479,25481 are completed) (max level 43)
 				["altQuests"] = {
 					-- 28503,	-- Hero's Call: Thousand Needles!
 					25479,	-- To New Thalanaar [Dustwallow Marsh]
 					25481,	-- To New Thalanaar [Feralas]
 				},
-			}),
-			q(28525, {	-- Hero's Call: Un'Goro Crater! (breadcrumb quest for 24740, not available if 24911 is completed) (max level 53)
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { DARNASSUS, THE_EXODAR },	-- Only found in Darnassus & The Exodar in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
-			q(28576, {	-- Hero's Call: Western Plaguelands!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 39. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 46 (TODO: Test max level between 42 and 50, 46 is based on Wowpedia info)
+				["lvl"] = { 39, 46 },
+				-- #endif
+			})),
+			heroscall(q(28525, {	-- Hero's Call: Un'Goro Crater! (breadcrumb quest for 24740, not available if 24911 is completed) (max level 53)
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { DARNASSUS, THE_EXODAR },	-- Only found in Darnassus & The Exodar in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 35,
-			}),
-			q(28565, {	-- Hero's Call: Wetlands!
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 49. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 60 (TODO: Test max level between 54 and 60, 60 is based on Wowpedia info)
+				["lvl"] = { 49, 60 },
+				-- #endif
+			})),
+			heroscall(q(28576, {	-- Hero's Call: Western Plaguelands!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { IRONFORGE, STORMWIND_CITY },	-- Only found in Ironforge & Stormwind City in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 20,
-			}),
-			q(28544, {	-- Hero's Call: Winterspring! (breadcrumb quest for ???, not available if 28524,28545,28768 are completed) (max level 53)
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 34. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 40 (TODO: Test max level between 36 and 44, 40 is based on Wowpedia info)
+				["lvl"] = { 34, 40 },
+				-- #endif
+			})),
+			heroscall(q(28565, {	-- Hero's Call: Wetlands!
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { IRONFORGE },	-- Only found in Ironforge in Cataclysm.
 				["isBreadcrumb"] = true,
-				["lvl"] = 40,
-			}),
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 19. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 25 (TODO: Test max level between 22 and 32, 25 is based on Wowpedia info)
+				["lvl"] = { 19, 25 },
+				-- #endif
+			})),
+			heroscall(q(28544, {	-- Hero's Call: Winterspring! (breadcrumb quest for ???, not available if 28524,28545,28768 are completed) (max level 53)
+				["timeline"] = { ADDED_4_0_3 },
+				["maps"] = { DARNASSUS, THE_EXODAR },	-- Only found in Darnassus & The Exodar in Cataclysm.
+				["isBreadcrumb"] = true,
+				-- #if BEFORE 7.3.5
+				-- Cataclysm: Minimum is level 49. (TODO: Confirm this.)
+				-- Cataclysm: Maximum is level 60 (TODO: Test max level between 54 and 60, 60 is based on Wowpedia info)
+				["lvl"] = { 49, 60 },
+				-- #endif
+			})),
 			
 			--
 			
