33b6c98f41d264bf32f1ab8d2de4ea720a6d80bf
Crieve <dylanfortune@live.com>
2024-05-22 10:10:46 -0700

    Remix: Mists of Pandaria is timerunningSeasonID 1. (TODO: Use this in the mini list and make parser export it!)

--- .contrib/Parser/lib/Headers/Remix/Mists of Pandaria.lua
+++ .contrib/Parser/lib/Headers/Remix/Mists of Pandaria.lua
@@ -2,6 +2,7 @@ REMIX_MOP = createHeader({
 	readable = "Remix: Mists of Pandaria",
 	icon = "Interface\\Icons\\achievement_character_pandaren_female",
 	eventID = EVENTS.REMIX_MOP,
+	timerunningSeasonID = 1,
 	text = {
 		en = "Remix: Mists of Pandaria",
 	},
