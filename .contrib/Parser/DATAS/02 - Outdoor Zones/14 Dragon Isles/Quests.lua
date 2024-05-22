b856062e7a261ed95bea48afb3f794c0417858e5
ImUnicke <ImUnicke+github@gmail.com>
2024-05-21 23:54:42 -0600

    Adjusted organization of some still-available DF quests which were under pre-launch section and showing 'removed' in tooltips

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Quests.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/Quests.lua
@@ -3,6 +3,98 @@
 ---------------------------------------------------
 root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_0_2_LAUNCH } }, {
 	n(QUESTS, {
+		q(65436, {	-- The Dragon Isles Await [A]
+			["maps"] = { STORMWIND_CITY },
+			["races"] = ALLIANCE_ONLY,
+			["isBreadcrumb"] = true,
+			["g"] = {
+				i(205456, {	-- Lost Dragonscale
+					["timeline"] = { ADDED_10_1_0 },
+				}),
+			},
+		}),
+		q(65435, {	-- The Dragon Isles Await [H]
+			["maps"] = { ORGRIMMAR },
+			["races"] = HORDE_ONLY,
+			["isBreadcrumb"] = true,
+			["g"] = {
+				i(205458, {	-- Lost Dragonscale
+					["timeline"] = { ADDED_10_1_0 },
+				}),
+			},
+		}),
+		q(66577, {	-- Aspectral Invitation [A]
+			["sourceQuests"] = {
+				66513,	-- Ground Leave [A]
+				65436,	-- The Dragon Isles Await [A]
+			},
+			["provider"] = { "n", 189569 },	-- Wrathion
+			["coord"] = { 79.8, 27.1, STORMWIND_CITY },
+			["races"] = ALLIANCE_ONLY,
+		}),
+		q(65437, {	-- Aspectral Invitation [H]
+			["sourceQuests"] = {
+				66534,	-- Ground Leave [H]
+				65435,	-- The Dragon Isles Await [H]
+			},
+			["provider"] = { "n", 190239 },	-- Ebyssian
+			["coord"] = { 44.1, 38.0, ORGRIMMAR },
+			["races"] = HORDE_ONLY,
+		}),
+		q(66589, {	-- Expeditionary Coordination [A]
+			["sourceQuests"] = {
+				65101,	-- An Iconic, Draconic, Look [A]
+				66577,	-- Aspectral Invitation [A]
+			},
+			["provider"] = { "n", 189602 },	-- Toddy Whiskers
+			["coord"] = { 79.7, 27.3, STORMWIND_CITY },
+			["races"] = ALLIANCE_ONLY,
+		}),
+		q(65443, {	-- Expeditionary Coordination [H]
+			["sourceQuests"] = {
+				65613,	-- An Iconic, Draconic, Look [H]
+				65437,	-- Aspectral Invitation [H]
+			},
+			["provider"] = { "n", 184793 },	-- Naleidea Rivergleam
+			["coord"] = { 44.2, 37.8, ORGRIMMAR },
+			["races"] = HORDE_ONLY,
+		}),
+		q(72240, {	-- The Obsidian Warders [A]
+			["sourceQuests"] = {
+				65101,	-- An Iconic, Draconic, Look [A]
+				66577,	-- Aspectral Invitation [A]
+			},
+			["provider"] = { "n", 189603 },	-- Scalecommander Azurathel
+			["coord"] = { 79.9, 27.1, STORMWIND_CITY },
+			["races"] = ALLIANCE_ONLY,
+		}),
+		q(72256, {	-- The Dark Talons [H]
+			["sourceQuests"] = {
+				65613,	-- An Iconic, Draconic, Look [H]
+				65437,	-- Aspectral Invitation [H]
+			},
+			["provider"] = { "n", 184786 },	-- Scalecommander Cindrethresh
+			["coord"] = { 44.0, 38.3, ORGRIMMAR },
+			["races"] = HORDE_ONLY,
+		}),
+		q(66596, {	-- Whispers in the Winds [A]
+			["sourceQuests"] = {
+				66589,	-- Expeditionary Coordination [A]
+				72240,	-- The Obsidian Warders [A]
+			},
+			["provider"] = { "n", 193450 },	-- Archmage Khadgar
+			["coord"] = { 23.0, 56.1, STORMWIND_CITY },
+			["races"] = ALLIANCE_ONLY,
+		}),
+		q(65439, {	-- Whispers in the Winds [H]
+			["sourceQuests"] = {
+				65443,	-- Expeditionary Coordination [H]
+				72256,	-- The Dark Talons [H]
+			},
+			["provider"] = { "n", 193450 },	-- Archmage Khadgar
+			["coord"] = { 55.9, 12.6, DUROTAR },
+			["races"] = HORDE_ONLY,
+		}),
 		q(72481, {	-- The Waking Shore Tour
 			["sourceQuests"] = { 68798 },	-- Dragon Glyphs and You
 			["provider"] = { "n", 193411 },	-- Celormu
--- .contrib/Parser/DATAS/07 - Events/Expansion PreLaunch Events/09 Dragonflight.lua
+++ .contrib/Parser/DATAS/07 - Events/Expansion PreLaunch Events/09 Dragonflight.lua
@@ -99,6 +99,38 @@ root(ROOTS.WorldEvents, n(EXPANSION_PRELAUNCH, bubbleDown({ ["timeline"] = { ADD
 					i(192493),	-- Mote of Primal Energy (QI!)
 				},
 			}),
+			q(66458, {	-- Legacy of Tyr: Secrets of the Past [A]
+				["sourceQuests"] = { 66596 },	-- Whispers on the Winds [A]
+				["provider"] = { "n", 189569 },	-- Wrathion <The Black Prince>
+				["coord"] = { 22.9, 56.0, STORMWIND_CITY },
+				["timeline"] = { REMOVED_10_0_2_LAUNCH },
+				["g"] = {
+					i(201303),	-- Beheader's Poleaxe
+					i(201304),	-- Dunesplitter
+					i(201305),	-- Fist of the Treasure Seeker
+					i(201306),	-- Knowledge Seeker's Cudgel
+					i(201307),	-- Raider's Lost Spire
+					i(201302),	-- Scarab Blaster
+					i(201308),	-- Scorpion Slicer
+					i(201309),	-- Tomb Raider's Shank
+				},
+			}),
+			q(66586, {	-- Legacy of Tyr: Secrets of the Past [H]
+				["sourceQuests"] = { 65439 },	-- Whispers in the Winds [H]
+				["provider"] = { "n", 190239 },	-- Ebyssian
+				["coord"] = { 55.8, 12.1, DUROTAR },
+				["timeline"] = { REMOVED_10_0_2_LAUNCH },
+				["g"] = {
+					i(201303),	-- Beheader's Poleaxe
+					i(201304),	-- Dunesplitter
+					i(201305),	-- Fist of the Treasure Seeker
+					i(201306),	-- Knowledge Seeker's Cudgel
+					i(201307),	-- Raider's Lost Spire
+					i(201302),	-- Scarab Blaster
+					i(201308),	-- Scorpion Slicer
+					i(201309),	-- Tomb Raider's Shank
+				},
+			}),
 		}),
 		n(VENDORS, {
 			["crs"] = {
@@ -368,132 +400,6 @@ root(ROOTS.WorldEvents, n(EXPANSION_PRELAUNCH, bubbleDown({ ["timeline"] = { ADD
 				["provider"] = { "n", 184665 },	-- Lor'themar Theron
 				["coord"] = { 55.7, 12.2, DUROTAR },
 				["races"] = HORDE_ONLY,
-			}),
-		}),
-		n(QUESTS, {
-			q(65436, {	-- The Dragon Isles Await [A]
-				["maps"] = { STORMWIND_CITY },
-				["races"] = ALLIANCE_ONLY,
-				["isBreadcrumb"] = true,
-				["g"] = {
-					i(205456, {	-- Lost Dragonscale
-						["timeline"] = { ADDED_10_1_0 },
-					}),
-				},
-			}),
-			q(65435, {	-- The Dragon Isles Await [H]
-				["maps"] = { ORGRIMMAR },
-				["races"] = HORDE_ONLY,
-				["isBreadcrumb"] = true,
-				["g"] = {
-					i(205458, {	-- Lost Dragonscale
-						["timeline"] = { ADDED_10_1_0 },
-					}),
-				},
-			}),
-			q(66577, {	-- Aspectral Invitation [A]
-				["sourceQuests"] = {
-					66513,	-- Ground Leave [A]
-					65436,	-- The Dragon Isles Await [A]
-				},
-				["provider"] = { "n", 189569 },	-- Wrathion
-				["coord"] = { 79.8, 27.1, STORMWIND_CITY },
-				["races"] = ALLIANCE_ONLY,
-			}),
-			q(65437, {	-- Aspectral Invitation [H]
-				["sourceQuests"] = {
-					66534,	-- Ground Leave [H]
-					65435,	-- The Dragon Isles Await [H]
-				},
-				["provider"] = { "n", 190239 },	-- Ebyssian
-				["coord"] = { 44.1, 38.0, ORGRIMMAR },
-				["races"] = HORDE_ONLY,
-			}),
-			q(66589, {	-- Expeditionary Coordination [A]
-				["sourceQuests"] = {
-					65101,	-- An Iconic, Draconic, Look [A]
-					66577,	-- Aspectral Invitation [A]
-				},
-				["provider"] = { "n", 189602 },	-- Toddy Whiskers
-				["coord"] = { 79.7, 27.3, STORMWIND_CITY },
-				["races"] = ALLIANCE_ONLY,
-			}),
-			q(65443, {	-- Expeditionary Coordination [H]
-				["sourceQuests"] = {
-					65613,	-- An Iconic, Draconic, Look [H]
-					65437,	-- Aspectral Invitation [H]
-				},
-				["provider"] = { "n", 184793 },	-- Naleidea Rivergleam
-				["coord"] = { 44.2, 37.8, ORGRIMMAR },
-				["races"] = HORDE_ONLY,
-			}),
-			q(72240, {	-- The Obsidian Warders [A]
-				["sourceQuests"] = {
-					65101,	-- An Iconic, Draconic, Look [A]
-					66577,	-- Aspectral Invitation [A]
-				},
-				["provider"] = { "n", 189603 },	-- Scalecommander Azurathel
-				["coord"] = { 79.9, 27.1, STORMWIND_CITY },
-				["races"] = ALLIANCE_ONLY,
-			}),
-			q(72256, {	-- The Dark Talons [H]
-				["sourceQuests"] = {
-					65613,	-- An Iconic, Draconic, Look [H]
-					65437,	-- Aspectral Invitation [H]
-				},
-				["provider"] = { "n", 184786 },	-- Scalecommander Cindrethresh
-				["coord"] = { 44.0, 38.3, ORGRIMMAR },
-				["races"] = HORDE_ONLY,
-			}),
-			q(66596, {	-- Whispers in the Winds [A]
-				["sourceQuests"] = {
-					66589,	-- Expeditionary Coordination [A]
-					72240,	-- The Obsidian Warders [A]
-				},
-				["provider"] = { "n", 193450 },	-- Archmage Khadgar
-				["coord"] = { 23.0, 56.1, STORMWIND_CITY },
-				["races"] = ALLIANCE_ONLY,
-			}),
-			q(65439, {	-- Whispers in the Winds [H]
-				["sourceQuests"] = {
-					65443,	-- Expeditionary Coordination [H]
-					72256,	-- The Dark Talons [H]
-				},
-				["provider"] = { "n", 193450 },	-- Archmage Khadgar
-				["coord"] = { 55.9, 12.6, DUROTAR },
-				["races"] = HORDE_ONLY,
-			}),
-			q(66458, {	-- Legacy of Tyr: Secrets of the Past [A]
-				["sourceQuests"] = { 66596 },	-- Whispers on the Winds [A]
-				["provider"] = { "n", 189569 },	-- Wrathion <The Black Prince>
-				["coord"] = { 22.9, 56.0, STORMWIND_CITY },
-				["timeline"] = { REMOVED_10_0_2_LAUNCH },
-				["g"] = {
-					i(201303),	-- Beheader's Poleaxe
-					i(201304),	-- Dunesplitter
-					i(201305),	-- Fist of the Treasure Seeker
-					i(201306),	-- Knowledge Seeker's Cudgel
-					i(201307),	-- Raider's Lost Spire
-					i(201302),	-- Scarab Blaster
-					i(201308),	-- Scorpion Slicer
-					i(201309),	-- Tomb Raider's Shank
-				},
-			}),
-			q(66586, {	-- Legacy of Tyr: Secrets of the Past [H]
-				["sourceQuests"] = { 65439 },	-- Whispers in the Winds [H]
-				["provider"] = { "n", 190239 },	-- Ebyssian
-				["coord"] = { 55.8, 12.1, DUROTAR },
-				["timeline"] = { REMOVED_10_0_2_LAUNCH },
-				["g"] = {
-					i(201303),	-- Beheader's Poleaxe
-					i(201304),	-- Dunesplitter
-					i(201305),	-- Fist of the Treasure Seeker
-					i(201306),	-- Knowledge Seeker's Cudgel
-					i(201307),	-- Raider's Lost Spire
-					i(201302),	-- Scarab Blaster
-					i(201308),	-- Scorpion Slicer
-					i(201309),	-- Tomb Raider's Shank
-				},
 			}),
 		}),
 	}),
