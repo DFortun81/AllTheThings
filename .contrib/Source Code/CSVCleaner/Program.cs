a566550493a592f560260b7dbfa8071a5c48c029
ImUnicke <ImUnicke+github@gmail.com>
2024-05-23 18:30:37 -0600

    Threw together a quick CSV cleaner app to reduce the SpellEffect CSV from 42MB to under 1MB since we will only use a small portion of the data

---
+++ .contrib/Source Code/CSVCleaner/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("[0] => CSV File to Clean | [1] => File containing Regex line(s) for allowed CSV lines");
+    Console.ReadKey();
+    return -1;
+}
+
+foreach (var arg in args)
+{
+    Console.WriteLine($"Argument={arg}");
+}
+
+string filepath = args[0];
+string regexFile = args[1];
+
+if (!File.Exists(filepath) || !File.Exists(regexFile))
+{
+    Console.WriteLine("[0] => CSV File to Clean | [1] => File containing Regex line(s) for allowed CSV lines");
+    Console.ReadKey();
+    return -1;
+}
+
+string[] regexPatterns = File.ReadAllLines(regexFile);
+//string[] csvLines = File.ReadAllLines(filepath);
+StringBuilder sb = new();
+using StreamReader sr = new(filepath);
+
+// header columns
+sb.AppendLine(sr.ReadLine());
+
+string? line;
+while ((line = sr.ReadLine()) != null)
+{
+    foreach (string regex in regexPatterns)
+    {
+        if (Regex.IsMatch(line, regex))
+        {
+            sb.AppendLine(line); break;
+        }
+    }
+}
+sr.Dispose();
+
+File.WriteAllText(filepath, sb.ToString());
+
+return 0;
