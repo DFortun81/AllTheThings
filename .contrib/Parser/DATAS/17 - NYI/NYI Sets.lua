da506b51afa98c0b1cb09a902e53a59167a73451
Darkal <michalbert@seznam.cz>
2024-05-24 22:54:34 +0200

    Removed NYI items from Remix ToT Ensembles, it's not inside of them.

--- .contrib/Parser/DATAS/17 - NYI/NYI Sets.lua
+++ .contrib/Parser/DATAS/17 - NYI/NYI Sets.lua
@@ -953,7 +953,6 @@ root(ROOTS.NeverImplemented, n(SETS, {
 		})),
 
 		-- 5.2.0
-		-- #if BEFORE 10.2.7
 		expansion(EXPANSION.MOP, 2.0, bubbleDown({ ["timeline"] = { CREATED_5_2_0 } }, {
 			-- Throne of Thunder (Thunderforged & Heroic Thunderforged)
 			cl(DEATHKNIGHT, {
@@ -1209,7 +1208,6 @@ root(ROOTS.NeverImplemented, n(SETS, {
 				i(96367), -- Shoulderguards of the Last Mogu
 			}),
 		})),
-		-- #endif
 
 		-- 5.3.0
 		expansion(EXPANSION.MOP, 3.0, bubbleDown({ ["timeline"] = { CREATED_5_3_0 } }, {
