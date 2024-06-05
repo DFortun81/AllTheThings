e041be2459d34fc765575eed966c938d7baae54f
ImUnicke <ImUnicke+github@gmail.com>
2024-06-04 22:12:19 -0600

    Don't need this preprocessor since parser is a bit smarter now

--- .contrib/Parser/lib/Headers/Expansion Features/The War Within/Delves.lua
+++ .contrib/Parser/lib/Headers/Expansion Features/The War Within/Delves.lua
@@ -2,9 +2,7 @@ DELVES = createHeader({
 	readable = "Delves",
 	icon = "Interface\\Icons\\Inv_misc_questionmark",
 	constant = "DELVES",
-	-- #if AFTER TWW
 	export = true,
-	-- #endif
 	text = {
 		en = "Delves",
 	},
