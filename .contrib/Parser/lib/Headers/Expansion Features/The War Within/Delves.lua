e4404898cd45c1381073936b44c2babed156f7de
Crieve <dylanfortune@live.com>
2024-06-06 10:01:44 -0700

    Revert "Don't need this preprocessor since parser is a bit smarter now"
    This reverts commit e041be2459d34fc765575eed966c938d7baae54f.

--- .contrib/Parser/lib/Headers/Expansion Features/The War Within/Delves.lua
+++ .contrib/Parser/lib/Headers/Expansion Features/The War Within/Delves.lua
@@ -2,7 +2,9 @@ DELVES = createHeader({
 	readable = "Delves",
 	icon = "Interface\\Icons\\Inv_misc_questionmark",
 	constant = "DELVES",
+	-- #if AFTER TWW
 	export = true,
+	-- #endif
 	text = {
 		en = "Delves",
 	},
