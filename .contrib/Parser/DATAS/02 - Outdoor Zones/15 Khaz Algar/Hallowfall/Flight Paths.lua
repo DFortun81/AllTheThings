fb7122e13ed694665295d028e8a2fa1a8445f5d0
gbrage <gbrage@kth.se>
2024-06-13 00:38:42 +0200

    Some Flight Paths

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Hallowfall/Flight Paths.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Hallowfall/Flight Paths.lua
@@ -4,38 +4,38 @@
 root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_1_LAUNCH } }, {
 	m(HALLOWFALL, {
 		n(FLIGHT_PATHS, {
-			--fp(xx, {	-- Dunelle's Kindness, Hallowfall
-			--	["coord"] = { 67.5, 44.6, HALLOWFALL },
-			--	["cr"] = 217639,	-- Auro Flitter
-			--}),
-			--fp(xx, {	-- Hillhelm Family Farm, Hallowfall
-			--	["coord"] = { 61.3, 31.0, HALLOWFALL },
-			--	["cr"] = 206537,	-- Rubaen Hillhelm
-			--}),
-			--fp(xx, {	-- Light's Redoubt, Hallowfall
-			--	["coord"] = { 40.5, 71.3, HALLOWFALL },
-			--	["cr"] = 218304,	-- 'Red' Fearon
-			--}),
-			--fp(xx, {	-- Lightspark, Hallowfall
-			--	["coord"] = { 52.9, 61.3, HALLOWFALL },
-			--	["cr"] = 218300,	-- Gear Flamepierce
-			--}),
-			--fp(xx, {	-- Lorel's Crossing, Hallowfall
-			--	["coord"] = { 48.4, 40.7, HALLOWFALL },
-			--	["cr"] = 218295,	-- Bastian Wynfall
-			--}),
-			--fp(xx, {	-- Mereldar, Hallowfall
-			--	["coord"] = { 41.6, 52.6, HALLOWFALL },
-			--	["cr"] = 218305,	-- Anje Noon
-			--}),
-			--fp(xx, {	-- Priory of the Sacred Flame, Hallowfall
-			--	["coord"] = { 41.1, 33.7, HALLOWFALL },
-			--	["cr"] = 218323,	-- Tristan Lyre
-			--}),
-			--fp(xx, {	-- The Aegis Wall, Hallowfall
-			--	["coord"] = { 71.4, 56.5, HALLOWFALL },
-			--	["cr"] = 218298,	-- Vellas Tearen
-			--}),
+			fp(2944, {	-- Dunelle's Kindness, Hallowfall
+				["coord"] = { 67.5, 44.6, HALLOWFALL },
+				["cr"] = 217639,	-- Auro Flitter
+			}),
+			fp(xx, {	-- Hillhelm Family Farm, Hallowfall
+				["coord"] = { 61.3, 31.0, HALLOWFALL },
+				["cr"] = 206537,	-- Rubaen Hillhelm
+			}),
+			fp(2924, {	-- Light's Redoubt, Hallowfall
+				["coord"] = { 40.5, 71.3, HALLOWFALL },
+				["cr"] = 218304,	-- 'Red' Fearon
+			}),
+			fp(2923, {	-- Lightspark, Hallowfall
+				["coord"] = { 52.9, 61.3, HALLOWFALL },
+				["cr"] = 218300,	-- Gear Flamepierce
+			}),
+			fp(2943, {	-- Lorel's Crossing, Hallowfall
+				["coord"] = { 48.4, 40.7, HALLOWFALL },
+				["cr"] = 218295,	-- Bastian Wynfall
+			}),
+			fp(2941, {	-- Mereldar, Hallowfall
+				["coord"] = { 41.6, 52.6, HALLOWFALL },
+				["cr"] = 218305,	-- Anje Noon
+			}),
+			fp(2942, {	-- Priory of the Sacred Flame, Hallowfall
+				["coord"] = { 41.1, 33.7, HALLOWFALL },
+				["cr"] = 218323,	-- Tristan Lyre
+			}),
+			fp(2943, {	-- The Aegis Wall, Hallowfall
+				["coord"] = { 71.4, 56.5, HALLOWFALL },
+				["cr"] = 218298,	-- Vellas Tearen
+			}),
 		}),
 	}),
 })));
