288eb31e8444baa9bec3333236984a9ab0e64b9c
Myrhial <8618272+ninaarens@users.noreply.github.com>
2024-06-14 20:51:14 +0200

    Isle of Dorn glyphs and meta (most still need coord)

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Isle of Dorn/Achievements.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Isle of Dorn/Achievements.lua
@@ -4,6 +4,21 @@
 root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_1_LAUNCH } }, {
 	m(ISLE_OF_DORN, {
 		n(ACHIEVEMENTS, {
+			ach(40166, {	-- Isle of Dorn Glyph Hunter
+				-- Meta Achievement
+				["sym"] = {{"meta_achievement",
+					40669,	-- Skyriding Glyphs: Cinderbrew Meadery
+					40665,	-- Skyriding Glyphs: Dhar Durgaz
+					40663,	-- Skyriding Glyphs: Dhar Oztan
+					40671,	-- Skyriding Glyphs: Ironwold
+					40666,	-- Skyriding Glyphs: Sunken Shield
+					40664,	-- Skyriding Glyphs: Storm's Watch
+					40667,	-- Skyriding Glyphs: Thul Medran
+					40668,	-- Skyriding Glyphs: Thunderhead Peak
+					40670,	-- Skyriding Glyphs: Mourning Rise
+					40152,	-- Skyriding Glyphs: The Three Shields
+				}},
+			}),
 			ach(20118, {	-- The Isle of Dorn
 				crit(66214, {	-- Breaking Point
 					["sourceQuest"] = 78536,	-- Impossible Odds
@@ -53,17 +68,41 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_1_LAUNC
 				}),
 			}),
 			pvp(ach(40083)),	-- Tour of Duty: Isle of Dorn
+			ach(40665, {	-- Skyriding Glyphs: Dhar Durgaz
+				-- TODO
+				--["coord"] = { X, Y, ISLE_OF_DORN },
+			}),
+			ach(40669, {	-- Skyriding Glyphs: Cinderbrew Meadery
+				-- TODO
+				--["coord"] = { X, Y, ISLE_OF_DORN },
+			}),
 			ach(40663, {	-- Skyriding Glyphs: Dhar Oztan
 				["coord"] = { 23.1, 58.5, ISLE_OF_DORN },
 			}),
 			ach(40671, {	-- Skyriding Glyphs: Ironwold
 				["coord"] = { 71.9, 47.2, ISLE_OF_DORN },
 			}),
+			ach(40666, {	-- Skyriding Glyphs: Sunken Shield
+				-- TODO
+				--["coord"] = { X, Y, ISLE_OF_DORN },
+			}),
+			ach(40664, {	-- Skyriding Glyphs: Storm's Watch
+				-- TODO
+				--["coord"] = { X, Y, ISLE_OF_DORN },
+			}),
+			ach(40668, {	-- Skyriding Glyphs: Thunderhead Peak
+				-- TODO
+				--["coord"] = { X, Y, ISLE_OF_DORN },
+			}),
 			ach(40667, {	-- Skyriding Glyphs: Thul Medran
 				["coord"] = { 47.9, 26.6, ISLE_OF_DORN },
 			}),
 			ach(40670, {	-- Skyriding Glyphs: Mourning Rise
 				["coord"] = { 62.1, 44.9, ISLE_OF_DORN },
+			}),
+			ach(40152, {	-- Skyriding Glyphs: The Three Shields
+				-- TODO
+				--["coord"] = { X, Y, ISLE_OF_DORN },
 			}),
 		}),
 	}),
