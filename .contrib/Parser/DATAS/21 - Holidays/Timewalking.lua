0d28685f41e183386ec7a3b5790b1a4db1c5914e
Darkal <michalbert@seznam.cz>
2024-06-25 20:16:43 +0200

    Added 2 Anniversary / Timewalking items.

--- .contrib/Parser/DATAS/21 - Holidays/Timewalking.lua
+++ .contrib/Parser/DATAS/21 - Holidays/Timewalking.lua
@@ -1470,6 +1470,12 @@ root(ROOTS.Holidays, n(TIMEWALKING_HEADER, applyevent(EVENTS.TIMEWALKING_NORTHRE
 							i(171600, {	-- Pride of the Kor'kron
 								["races"] = HORDE_ONLY,
 							}),
+							i(171528, {	-- Reign of the Dead
+								["races"] = HORDE_ONLY,
+							}),
+							i(171549, {	-- Reign of the Unliving
+								["races"] = ALLIANCE_ONLY,
+							}),
 							i(171611, {	-- Robes of the Sleepless
 								["races"] = HORDE_ONLY,
 							}),
