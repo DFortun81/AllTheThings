9a091e04fbbe2fc6ecb2462efba52296b5e52b50
Crieve <dylanfortune@live.com>
2024-06-30 12:03:46 -0700

    Cataclysm: World Boss loot now properly marks itself available / removed from game based on your cata classic phase.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/04 - Cataclysm/1 - Raids/0.1 World Bosses.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/04 - Cataclysm/1 - Raids/0.1 World Bosses.lua
@@ -1,7 +1,18 @@
 -----------------------------------------------------
 --   D U N G E O N S  &  R A I D S  M O D U L E    --
 -----------------------------------------------------
-
+-- Blizzard added the original drops for ONLY the first phase of Cataclysm Classic.
+-- #if ANYCLASSIC
+local CATA_CLASSIC_WORLD_BOSS_ONUPDATE = [[function(t)
+	if _.Settings:GetUnobtainableFilter(]] .. CATA_PHASE_TWO .. [[) then
+		t.u = ]] .. REMOVED_FROM_GAME .. [[;
+		t.rwp = nil;
+	else
+		t.u = ]] .. CATA_PHASE_ONE .. [[;
+		t.rwp = 40100;
+	end
+end]];
+-- #endif
 root(ROOTS.Instances, expansion(EXPANSION.CATA, {
 	applyclassicphase(CATA_PHASE_ONE, n(WORLD_BOSSES, bubbleDownSelf({ ["timeline"] = { ADDED_4_0_3 }, }, {
 		["isRaid"] = true,
@@ -40,11 +51,14 @@ root(ROOTS.Instances, expansion(EXPANSION.CATA, {
 				["coord"] = { 38.0, 60.6, ULDUM },
 				["isRaid"] = true,
 				["groups"] = {
-					i(69877, {	-- Belt of a Thousand Gaping Mouths
+					applyclassicphase(CATA_PHASE_TWO, i(69877, {	-- Belt of a Thousand Gaping Mouths
 						["timeline"] = { ADDED_4_1_0 },
-					}),
+					})),
 					i(67240, {	-- Belt of A Thousand Mouths
 						["timeline"] = { ADDED_4_0_3, REMOVED_4_1_0 },
+						-- #if ANYCLASSIC
+						["OnUpdate"] = CATA_CLASSIC_WORLD_BOSS_ONUPDATE,
+						-- #endif
 					}),
 				},
 			}),
@@ -55,11 +69,14 @@ root(ROOTS.Instances, expansion(EXPANSION.CATA, {
 				},
 				["isRaid"] = true,
 				["groups"] = {
-					i(69842, {	-- Garr's Reinforced Girdle of Memories
+					applyclassicphase(CATA_PHASE_TWO, i(69842, {	-- Garr's Reinforced Girdle of Memories
 						["timeline"] = { ADDED_4_1_0 },
-					}),
+					})),
 					i(67235, {	-- Garr's Girdle of Memories
 						["timeline"] = { ADDED_4_0_3, REMOVED_4_1_0 },
+						-- #if ANYCLASSIC
+						["OnUpdate"] = CATA_CLASSIC_WORLD_BOSS_ONUPDATE,
+						-- #endif
 					}),
 				},
 			}),
@@ -73,11 +90,14 @@ root(ROOTS.Instances, expansion(EXPANSION.CATA, {
 				},
 				["isRaid"] = true,
 				["groups"] = {
-					i(69844, {	-- Vitreous Beak of Julak-Doom
+					applyclassicphase(CATA_PHASE_TWO, i(69844, {	-- Vitreous Beak of Julak-Doom
 						["timeline"] = { ADDED_4_1_0 },
-					}),
+					})),
 					i(67246, {	-- Beak of Julak-Doom
 						["timeline"] = { ADDED_4_0_3, REMOVED_4_1_0 },
+						-- #if ANYCLASSIC
+						["OnUpdate"] = CATA_CLASSIC_WORLD_BOSS_ONUPDATE,
+						-- #endif
 					}),
 				},
 			}),
@@ -91,11 +111,14 @@ root(ROOTS.Instances, expansion(EXPANSION.CATA, {
 				},
 				["isRaid"] = true,
 				["groups"] = {
-					i(69843, {	-- Mobus's Dripping Halberd
+					applyclassicphase(CATA_PHASE_TWO, i(69843, {	-- Mobus's Dripping Halberd
 						["timeline"] = { ADDED_4_1_0 },
-					}),
+					})),
 					i(67153, {	-- Mobus's Vile Halberd
 						["timeline"] = { ADDED_4_0_3, REMOVED_4_1_0 },
+						-- #if ANYCLASSIC
+						["OnUpdate"] = CATA_CLASSIC_WORLD_BOSS_ONUPDATE,
+						-- #endif
 					}),
 				},
 			}),
@@ -109,11 +132,14 @@ root(ROOTS.Instances, expansion(EXPANSION.CATA, {
 				},
 				["isRaid"] = true,
 				["groups"] = {
-					i(69876, {	-- Xariona's Spectral Gauntlets
+					applyclassicphase(CATA_PHASE_TWO, i(69876, {	-- Xariona's Spectral Gauntlets
 						["timeline"] = { ADDED_4_1_0 },
-					}),
+					})),
 					i(67239, {	-- Xariona's Spectral Claws
 						["timeline"] = { ADDED_4_0_3, REMOVED_4_1_0 },
+						-- #if ANYCLASSIC
+						["OnUpdate"] = CATA_CLASSIC_WORLD_BOSS_ONUPDATE,
+						-- #endif
 					}),
 				},
 			}),
