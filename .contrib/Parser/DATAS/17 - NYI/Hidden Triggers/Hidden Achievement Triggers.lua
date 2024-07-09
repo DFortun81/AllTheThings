79b9d976f384579cf95cd3cda018b7bdafaf658d
Myrhial <8618272+ninaarens@users.noreply.github.com>
2024-07-09 19:39:45 +0200

    Numbers difficult

--- .contrib/Parser/DATAS/17 - NYI/Hidden Triggers/Hidden Achievement Triggers.lua
+++ .contrib/Parser/DATAS/17 - NYI/Hidden Triggers/Hidden Achievement Triggers.lua
@@ -3568,7 +3568,7 @@ root(ROOTS.HiddenAchievementTriggers, {
 		})),
 
 		-- 11.0.2
-		expansion(EXPANSION.TWW, 0.02, bubbleDownSelf({ ["timeline"] = { CREATED_11_0_2 } }, {
+		expansion(EXPANSION.TWW, 0.2, bubbleDownSelf({ ["timeline"] = { CREATED_11_0_2 } }, {
 			-- 11.0.2.55399
 			ach(40857),	-- Warbands: Bank Quest Unlock
 			ach(40861),	-- Mislaid Curiosities looted
