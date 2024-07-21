1b258566d5d1cd72d15042d37a310ad285c866cb
gbrage <gbrage@kth.se>
2024-07-21 23:05:23 +0200

    Reparsed all flavors

---
+++ .contrib/.classic/.db/00 - DB/Dynamic/DynamicObjectDB_638571926954459269.lua
@@ -0,0 +1,63 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[206638] = {
+		readable = "Refrigeration Pipe",
+		model = 197736,
+		text = {
+			en = "Refrigeration Pipe",
+			es = "Conducto de refrigeración",
+			de = "Kühlleitung",
+			fr = "Conduit de réfrigération",
+			it = "Condotto di Refrigerazione",
+			pt = "Cano de Refrigeração",
+			ru = "Холодильная труба",
+			ko = "냉각 수송관",
+			cn = "冷凝管",
+		},
+	},
+	[206639] = {
+		readable = "Refrigeration Pipe",
+		model = 197737,
+		text = {
+			en = "Refrigeration Pipe",
+			es = "Conducto de refrigeración",
+			de = "Kühlleitung",
+			fr = "Conduit de réfrigération",
+			it = "Condotto di Refrigerazione",
+			pt = "Cano de Refrigeração",
+			ru = "Холодильная труба",
+			ko = "냉각 수송관",
+			cn = "冷凝管",
+		},
+	},
+	[206640] = {
+		readable = "Refrigeration Pipe",
+		model = 197738,
+		text = {
+			en = "Refrigeration Pipe",
+			es = "Conducto de refrigeración",
+			de = "Kühlleitung",
+			fr = "Conduit de réfrigération",
+			it = "Condotto di Refrigerazione",
+			pt = "Cano de Refrigeração",
+			ru = "Холодильная труба",
+			ko = "냉각 수송관",
+			cn = "冷凝管",
+		},
+	},
+	[206641] = {
+		readable = "Refrigeration Pipe",
+		model = 197739,
+		text = {
+			en = "Refrigeration Pipe",
+			es = "Conducto de refrigeración",
+			de = "Kühlleitung",
+			fr = "Conduit de réfrigération",
+			it = "Condotto di Refrigerazione",
+			pt = "Cano de Refrigeração",
+			ru = "Холодильная труба",
+			ko = "냉각 수송관",
+			cn = "冷凝管",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
