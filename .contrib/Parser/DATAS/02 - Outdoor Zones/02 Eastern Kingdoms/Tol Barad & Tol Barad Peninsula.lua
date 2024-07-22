36981d562557e1f7f7f0f532398bb0b702f82442
funjoker <funjoker109@gmail.com>
2024-07-22 05:10:55 +0200

    CATA: Add more objective for Tol Barad pvp quests

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Tol Barad & Tol Barad Peninsula.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Tol Barad & Tol Barad Peninsula.lua
@@ -646,121 +646,319 @@ root(ROOTS.Zones, m(EASTERN_KINGDOMS, bubbleDown({ ["timeline"] = { ADDED_4_0_3_
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/1 Problim slain
+									["provider"] = { "n", 47593 },	-- Problim
+								}),
+							},
 						}),
 						q(28657, {	-- A Huge Problem [H]
 							["timeline"] = { ADDED_4_0_3 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/1 Problim slain
+									["provider"] = { "n", 47593 },	-- Problim
+								}),
+							},
 						}),
 						q(28117, {	-- Clearing the Depths [A]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/9 Ghosts Slain
+									["providers"] = {
+										{ "n", 47531},	-- Captive Spirit
+										{ "n", 47534},	-- Cellblock Ooze
+										{ "n", 47590},	-- Ghastly Convict
+									},
+								}),
+							},
 						}),
 						q(28660, {	-- Clearing the Depths [H]
 							["timeline"] = { ADDED_4_0_3 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/9 Ghosts Slain
+									["providers"] = {
+										{ "n", 47531},	-- Captive Spirit
+										{ "n", 47534},	-- Cellblock Ooze
+										{ "n", 47590},	-- Ghastly Convict
+									},
+								}),
+							},
 						}),
 						q(28665, {	-- Cursed Shackles [H]
 							["timeline"] = { ADDED_8_0_1 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/8 Cursed Shackles
+									["providers"] = {
+										{ "i", 63149 },	-- Cursed Shackles
+										{ "o", 206905 },	-- Cursed Shackles
+									},
+								}),
+							},
 						}),
 						q(28186, {	-- Cursed Shackles [A]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/8 Cursed Shackles
+									["providers"] = {
+										{ "i", 63149 },	-- Cursed Shackles
+										{ "o", 206905 },	-- Cursed Shackles
+									},
+								}),
+							},
 						}),
 						q(28165, {	-- D-Block [A]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/10 Demons Slain
+									["providers"] = {
+										{ "n", 47540},	-- Shivan Destroyer
+										{ "n", 47542},	-- Cell Watcher
+										{ "n", 47544},	-- Svarnos
+										{ "n", 47548},	-- Jailed Wrathguard
+										{ "n", 47549},	-- Imprisoned Imp
+										{ "n", 56350},	-- Disciple of Hate
+									},
+								}),
+							},
 						}),
 						q(28663, {	-- D-Block [H]
 							["timeline"] = { ADDED_4_0_3 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/10 Demons Slain
+									["providers"] = {
+										{ "n", 47540},	-- Shivan Destroyer
+										{ "n", 47542},	-- Cell Watcher
+										{ "n", 47544},	-- Svarnos
+										{ "n", 47548},	-- Jailed Wrathguard
+										{ "n", 47549},	-- Imprisoned Imp
+										{ "n", 56350},	-- Disciple of Hate
+									},
+								}),
+							},
 						}),
 						q(28670, {	-- Food From Below [H]
 							["timeline"] = { ADDED_8_0_1 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/12 Cellblock Rations
+									["providers"] = {
+										{ "i", 63315 },	-- Cellblock Rations
+										{ "o", 206996 },	-- Crate of Cellblock Rations
+									},
+								}),
+							},
 						}),
 						q(28232, {	-- Food From Below [A]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/12 Cellblock Rations
+									["providers"] = {
+										{ "i", 63315 },	-- Cellblock Rations
+										{ "o", 206996 },	-- Crate of Cellblock Rations
+									},
+								}),
+							},
 						}),
 						q(28662, {	-- Learning From The Past [H]
 							["timeline"] = { ADDED_4_0_3 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/4 Dusty Prison Journal
+									["providers"] = {
+										{ "i", 63034 },	-- Dusty Prison Journal
+										{ "o", 206890 },	-- Dusty Prison Journal
+									},
+								}),
+							},
 						}),
 						q(28120, {	-- Learning From The Past [A]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/4 Dusty Prison Journal
+									["providers"] = {
+										{ "i", 63034 },	-- Dusty Prison Journal
+										{ "o", 206890 },	-- Dusty Prison Journal
+									},
+								}),
+							},
 						}),
 						q(28188, {	-- Prison Revolt [A]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/10 Prisoners Slain
+									["providers"] = {
+										{ "n", 47550},	-- Imprisoned Worker
+										{ "n", 47552},	-- Exiled Mage
+										{ "n", 47561},	-- Warden Guard
+										{ "n", 48036},	-- Warden Silva
+										{ "n", 51023},	-- Demented Prisoner
+									},
+								}),
+							},
 						}),
 						q(28668, {	-- Prison Revolt [H]
 							["timeline"] = { ADDED_4_0_3 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/10 Prisoners Slain
+									["providers"] = {
+										{ "n", 47550},	-- Imprisoned Worker
+										{ "n", 47552},	-- Exiled Mage
+										{ "n", 47561},	-- Warden Guard
+										{ "n", 48036},	-- Warden Silva
+										{ "n", 51023},	-- Demented Prisoner
+									},
+								}),
+							},
 						}),
 						q(28185, {	-- Svarnos [A]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/1 Svarnos' Cursed Collar
+									["provider"] = { "i", 63143 },	-- Svarnos' Cursed Collar
+									["cr"] = 47544,	-- Svarnos
+								}),
+							},
 						}),
 						q(28664, {	-- Svarnos [H]
 							["timeline"] = { ADDED_8_0_1 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/1 Svarnos' Cursed Collar
+									["provider"] = { "i", 63143 },	-- Svarnos' Cursed Collar
+									["cr"] = 47544,	-- Svarnos
+								}),
+							},
 						}),
 						q(28658, {	-- Swamp Bait [H]
 							["timeline"] = { ADDED_8_0_1 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/8 Crocolisk Hide
+									["provider"] = { "i", 63103 },	-- Crocolisk Hide
+									["cr"] = 47591,	-- Baradin Crocolisk
+								}),
+							},
 						}),
 						q(28162, {	-- Swamp Bait [A]
 							["timeline"] = { ADDED_8_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/8 Crocolisk Hide
+									["provider"] = { "i", 63103 },	-- Crocolisk Hide
+									["cr"] = 47591,	-- Baradin Crocolisk
+								}),
+							},
 						}),
 						q(28661, {	-- The Imprisoned Archmage [A]
 							["timeline"] = { ADDED_8_0_1 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/1 Archmage Galus' Staff
+									["provider"] = { "i", 63033 },	-- Archmage Galus' Staff
+									["cr"] = 47537,	-- Archmage Galus
+								}),
+							},
 						}),
 						q(28118, {	-- The Imprisoned Archmage [H]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/1 Archmage Galus' Staff
+									["provider"] = { "i", 63033 },	-- Archmage Galus' Staff
+									["cr"] = 47537,	-- Archmage Galus
+								}),
+							},
 						}),
 						q(28163, {	-- The Leftovers [A]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/12 Horde Infantry
+									["providers"] = {
+										{ "n", 47595},	-- Alliance Hunter Infantry
+										{ "n", 47598},	-- Alliance Mage Infantry
+										{ "n", 47599},	-- Alliance Warrior Infantry
+										{ "n", 47600},	-- Alliance Paladin Infantry
+										{ "n", 47607},	-- Horde Druid Infantry
+										{ "n", 47608},	-- Horde Mage Infantry
+										{ "n", 47609},	-- Horde Rogue Infantry
+										{ "n", 47610},	-- Horde Shaman Infantry
+									},
+								}),
+							},
 						}),
 						q(28659, {	-- The Leftovers [H]
 							["timeline"] = { ADDED_4_0_3 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/12 Alliance Infantry
+									["providers"] = {
+										{ "n", 47595},	-- Alliance Hunter Infantry
+										{ "n", 47598},	-- Alliance Mage Infantry
+										{ "n", 47599},	-- Alliance Warrior Infantry
+										{ "n", 47600},	-- Alliance Paladin Infantry
+										{ "n", 47607},	-- Horde Druid Infantry
+										{ "n", 47608},	-- Horde Mage Infantry
+										{ "n", 47609},	-- Horde Rogue Infantry
+										{ "n", 47610},	-- Horde Shaman Infantry
+									},
+								}),
+							},
 						}),
 						q(28223, {	-- The Warden [A]
 							["timeline"] = { ADDED_4_0_1 },
 							["races"] = ALLIANCE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/1 Warden's Keys
+									["provider"] = { "i", 63309 },	-- Warden's Keys
+									["cr"] = 48036,	-- Warden Silva
+								}),
+							},
 						}),
 						q(28669, {	-- The Warden [H]
 							["timeline"] = { ADDED_8_0_1 },
 							["races"] = HORDE_ONLY,
 							["isDaily"] = true,
+							["groups"] = {
+								objective(1, {	-- 0/1 Warden's Keys
+									["provider"] = { "i", 63309 },	-- Warden's Keys
+									["cr"] = 48036,	-- Warden Silva
+								}),
+							},
 						}),
 						q(28882, {	-- Victory in Tol Barad [A]
 							["qg"] = 51288,	-- Major Marsden
