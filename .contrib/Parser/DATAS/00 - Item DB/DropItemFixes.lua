816b45eeddd337d51b1cf74d33749cc8831c87dd
ImUnicke <ImUnicke+github@gmail.com>
2024-07-23 20:41:54 -0600

    No longer need these manual drops for BoE Reagents

--- .contrib/Parser/DATAS/00 - Item DB/DropItemFixes.lua
+++ .contrib/Parser/DATAS/00 - Item DB/DropItemFixes.lua
@@ -11,22 +11,6 @@ local i = function(itemID, drop)
 end
 
 for _,itemID in ipairs({
-	  2770,	-- Copper Ore
-	  2771,	-- Tin Ore
-	  2772,	-- Iron Ore
-	  3858,	-- Mithril Ore
-	 10620,	-- Thorium Ore
-	 23424,	-- Fel Iron Ore
-	 23425,	-- Adamantite Ore
-	 36909,	-- Cobalt Ore
-	 36910,	-- Titanium Ore
-	 36912,	-- Saronite Ore
-	 52183,	-- Pyrite Ore
-	 52185,	-- Elementium Ore
-	 53038,	-- Obsidium Ore
-	 72092,	-- Ghost Iron Ore
-	 72094,	-- Black Trillium Ore
-	 72103,	-- White Trillium Ore
 	 ARTISANS_METTLE,
 	 ARTISANS_ACUITY,
 }) do
