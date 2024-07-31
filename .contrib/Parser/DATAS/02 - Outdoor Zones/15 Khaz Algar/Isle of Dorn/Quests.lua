5897997ce96b938a2727818237e717e7f59ca406
Myrhial <8618272+ninaarens@users.noreply.github.com>
2024-07-31 20:27:03 +0200

    Moved quests. May need a better home later if these are permanent story quests. But this will easily make them available to players for now, so we can release.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Isle of Dorn/Quests.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Isle of Dorn/Quests.lua
@@ -10,44 +10,6 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_PH_LAUNCH
 			q(81966),	-- Slept like a Rock
 			q(83543),	-- could be some pre-patch/xpac quest intro quest chain for phase?
 			--
-			-- Story quest chain START
-			q(81930, {	-- The War Within [A]
-				["races"] = ALLIANCE_ONLY,
-				["g"] = {
-					i(227669),	-- Teleportation Scroll (QI!)
-				},
-			}),
-			q(78713, {	-- The War Within [H]
-				["races"] = HORDE_ONLY,
-				["g"] = {
-					i(227669),	-- Teleportation Scroll (QI!)
-				},
-			}),
-			q(78714, {	-- Oh, THAT Sword
-				["sourceQuests"] = {
-					81930,	-- The War Within [A]
-					78713,	-- The War Within [H]
-				},
-				["provider"] = { "n", 213620 },	-- Thrall
-				["coord"] = { 41.9, 45.0, SILITHUS_THE_WOUND },
-				["maps"] = { 2321 },	-- Chamber of Heart
-				["sourceQuestNumRequired"] = 1,
-			}),
-			q(78715, {	-- Azeroth's Voice
-				["sourceQuests"] = { 78714 },	-- Oh, THAT Sword
-				["provider"] = { "n", 213624 },	-- Magni Bronzebeard
-				["coord"] = { 50.1, 64.4, 2321 },	-- Chamber of Heart
-			}),
-			q(78716, {	-- Painful Lessons
-				["sourceQuests"] = { 78715 },	-- Azeroth's Voice
-				["provider"] = { "n", 213625 },	-- Lady Jaina Proudmoore
-				["coord"] = { 49.5, 58.6, 2321 },	-- Chamber of Heart
-			}),
-			q(80500, {	-- The Bronzebeard Family
-				["sourceQuests"] = { 78716 },	-- Painful Lessons
-				["provider"] = { "n", 214651 },	-- Moira Thaurissan
-				["coord"] = { 40.5, 57.7, 629 },	-- Aegwynn's Gallery
-			}),
 			header(HEADERS.Achievement, 20118, {	-- The Isle of Dorn
 				------ Chapter 1 ------
 				q(78529, {	-- Violet Impact
