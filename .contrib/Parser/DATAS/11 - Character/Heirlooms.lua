dc1aad4a43a810d70f991365cff41f2f9082206d
gbrage <gbrage@kth.se>
2024-08-02 16:17:23 +0200

    New Heirloom

--- .contrib/Parser/DATAS/11 - Character/Heirlooms.lua
+++ .contrib/Parser/DATAS/11 - Character/Heirlooms.lua
@@ -72,6 +72,9 @@ root(ROOTS.Character, n(HEIRLOOMS, {
 		}),
 		n(FINGER, {
 			i(128173),	-- Admiral Taylor's Loyalty Ring
+			i(219325, {	-- Band of Radiant Echoes
+				["timeline"] = { ADDED_11_PH_PREPATCH },
+			}),
 			i(128172),	-- Captain Sander's Returned Band
 			un(REMOVED_FROM_GAME, i(50255)),	-- Dread Pirate Ring
 			i(122529),	-- Dread Pirate Ring
