61717224bc9fcc39b353bd6419f1604434afb108
Myrhial <8618272+ninaarens@users.noreply.github.com>
2024-08-04 17:26:00 +0200

    Refined TWW world drops to only greys for 11.0.0

--- .contrib/Parser/DATAS/03 - World Drops/11 The War Within/The War Within.lua
+++ .contrib/Parser/DATAS/03 - World Drops/11 The War Within/The War Within.lua
@@ -3,9 +3,6 @@
 -----------------------------------------------------
 root(ROOTS.WorldDrops, expansion(EXPANSION.TWW, bubbleDown({ ["timeline"] = { ADDED_11_PH_LAUNCH } }, {
 	n(ARMOR, bubbleDownSelf({ ["timeline"] = { ADDED_11_0_0 } }, {
-		filter(BACK_F, {
-			i(224624),	-- Spinner's Shawl
-		}),
 		filter(CLOTH, {
 			i(220314),	-- Moth-Ridden Armlets
 			i(220310),	-- Moth-Ridden Hood
@@ -15,6 +12,43 @@ root(ROOTS.WorldDrops, expansion(EXPANSION.TWW, bubbleDown({ ["timeline"] = { AD
 			i(220308),	-- Moth-Ridden Slippers
 			i(220312),	-- Moth-Ridden Stole
 			i(220313),	-- Moth-Ridden Waistwrap
+		}),
+		filter(LEATHER,  {
+			i(220321),	-- Distressed Belt
+			i(220316),	-- Distressed Boots
+			i(220322),	-- Distressed Bracers
+			i(220319),	-- Distressed Breeches
+			i(220317),	-- Distressed Grips
+			i(220315),	-- Distressed Jerkin
+			i(220320),	-- Distressed Shoulderpads
+			i(220318),	-- Distressed Visor
+		}),
+		filter(MAIL, {
+			i(220330),	-- Pierced Armguards
+			i(220329),	-- Pierced Clasp
+			i(220325),	-- Pierced Gloves
+			i(220323),	-- Pierced Hauberk
+			i(220326),	-- Pierced Helm
+			i(220327),	-- Pierced Jambeaux
+			i(220324),	-- Pierced Sabatons
+			i(220328),	-- Pierced Spaulders
+		}),
+		filter(PLATE, {
+			i(220331),	-- Begrimed Breastplate
+			i(220333),	-- Begrimed Gauntlets
+			i(220337),	-- Begrimed Greatbelt
+			i(220334),	-- Begrimed Greathelm
+			i(220335),	-- Begrimed Legguards
+			i(220332),	-- Begrimed Sabatons
+			i(220336),	-- Begrimed Spaulders
+			i(220338),	-- Begrimed Vambraces
+		}),
+	})),
+	n(ARMOR, {
+		filter(BACK_F, {
+			i(224624),	-- Spinner's Shawl
+		}),
+		filter(CLOTH, {
 			i(224589),	-- Deep-Dweller's Cincture
 			i(224590),	-- Deep-Dweller's Cowl
 			i(224597),	-- Deep-Dweller's Kaftan
@@ -25,14 +59,6 @@ root(ROOTS.WorldDrops, expansion(EXPANSION.TWW, bubbleDown({ ["timeline"] = { AD
 			i(224591),	-- Deep-Dweller's Wraps
 		}),
 		filter(LEATHER, {
-			i(220321),	-- Distressed Belt
-			i(220316),	-- Distressed Boots
-			i(220322),	-- Distressed Bracers
-			i(220319),	-- Distressed Breeches
-			i(220317),	-- Distressed Grips
-			i(220315),	-- Distressed Jerkin
-			i(220320),	-- Distressed Shoulderpads
-			i(220318),	-- Distressed Visor
 			i(224605),	-- Underscout's Bracers
 			i(224602),	-- Underscout's Cap
 			i(224604),	-- Underscout's Cord
@@ -43,14 +69,6 @@ root(ROOTS.WorldDrops, expansion(EXPANSION.TWW, bubbleDown({ ["timeline"] = { AD
 			i(224603),	-- Underscout's Trousers
 		}),
 		filter(MAIL, {
-			i(220330),	-- Pierced Armguards
-			i(220329),	-- Pierced Clasp
-			i(220325),	-- Pierced Gloves
-			i(220323),	-- Pierced Hauberk
-			i(220326),	-- Pierced Helm
-			i(220327),	-- Pierced Jambeaux
-			i(220328),	-- Pierced Spaulders
-			i(220336),	-- Pierced Spaulders
 			i(224614),	-- Expeditionary Belt
 			i(224609),	-- Expeditionary Boots
 			i(224615),	-- Expeditionary Bracers
@@ -61,24 +79,16 @@ root(ROOTS.WorldDrops, expansion(EXPANSION.TWW, bubbleDown({ ["timeline"] = { AD
 			i(224612),	-- Expeditionary Shinguards
 		}),
 		filter(PLATE, {
-			i(220331),	-- Begrimed Breastplate
-			i(220333),	-- Begrimed Gauntlets
-			i(220337),	-- Begrimed Greatbelt
-			i(220334),	-- Begrimed Greathelm
-			i(220335),	-- Begrimed Legguards
-			i(220324),	-- Begrimed Sabatons
-			i(220332),	-- Begrimed Sabatons
-			i(220338),	-- Begrimed Vambraces
 			i(224617),	-- Earthforged Haubergeon
 			i(224623),	-- Earthforged Cuffs
 			i(224620),	-- Earthforged Faulds
 			i(224622),	-- Stalwart Girdle (TODO: re-check name in future build, probably mistypo)
 			i(224616),	-- Earthforged Greathelm
 			i(224619),	-- Earthforged Handguards
-			i(224618),	-- Earthforged Sabatons
 			i(224621),	-- Earthforged Shoulder Scales
+			i(224618),	-- Earthforged Sabatons
 		}),
-	})),
+	}),
 	n(WEAPONS, {
 		--TODO: uncomment & remove from unsorted if it drop
 		i(224627),	-- Arathi Arquebus
