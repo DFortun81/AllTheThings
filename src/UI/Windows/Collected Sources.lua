a5bfeecbeb4e83b00eb28fa87dd5616b64ee918a
Crieve <dylanfortune@live.com>
2024-08-05 13:28:32 -0700

    SOD: Added /attcollectedsources for checking sources you own according to Blizzard on SOD by means of collecting it on SOD or Era.

---
+++ src/UI/Windows/Collected Sources.lua
@@ -0,0 +1,46 @@
+-- App locals
+local appName, app = ...;
+
+-- Global locals
+if C_TransmogCollection then
+local ipairs, tinsert, C_TransmogCollection_GetSourceInfo
+	= ipairs, tinsert, C_TransmogCollection.GetSourceInfo;
+
+-- Implementation
+app:CreateWindow("Collected Sources", {
+	Commands = { "attcollectedsources" },
+	OnRebuild = function(self, ...)
+		if not self.data then
+			self.data = {
+				text = "Collected Sources",
+				icon = "Interface/Icons/inv_scroll_05", 
+				description = "This window shows you all of the collected sources (according to Blizzard) tracked by ATT. Go get 'em!",
+				visible = true, 
+				expanded = true,
+				back = 1,
+				indent = 0,
+				g = { },
+				OnUpdate = function(t)
+					local g = t.g;
+					if #g < 1 then
+						local results = app:BuildSearchFilteredResponse(app:GetDataCache().g, function(group)
+							if group.sourceID then
+								local sourceInfo = C_TransmogCollection_GetSourceInfo(group.sourceID);
+								return sourceInfo and sourceInfo.isCollected;
+							end
+						end);
+						if results and #results > 0 then
+							for i,result in ipairs(results) do
+								tinsert(g, result);
+							end
+							t.OnUpdate = nil;
+							self:AssignChildren();
+							self:ExpandData(true);
+						end
+					end
+				end,
+			}
+		end
+	end
+});
+end
