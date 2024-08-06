d70089ee576d5e9eb119ca4db84de045b8badbc6
Geoff <geoff.w@arcadious.com>
2024-08-06 13:10:18 +0800

    [Cata] Urga: Fixed Windslicer not being detected as a 1H axe.

--- .contrib/Parser/lib/Structures/Tier Sets/Tier 11.lua
+++ .contrib/Parser/lib/Structures/Tier Sets/Tier 11.lua
@@ -775,7 +775,9 @@ TIER_ELEVEN_COMMON_GROUPS = {
 	i(58126),	-- Vest of the Waking Dream
 	i(58191),	-- Viewless Wings
 	i(58123),	-- Willow Mask
-	i(57928),	-- Windslicer
+	i(57928, {
+		["classes"] = { DEATHKNIGHT, PALADIN, SHAMAN, WARRIOR, HUNTER, ROGUE },
+	}),	-- Windslicer
 	i(58195),	-- Woe Breeder's Boots
 	i(58124),	-- Wrap of the Valley Glades
 };
