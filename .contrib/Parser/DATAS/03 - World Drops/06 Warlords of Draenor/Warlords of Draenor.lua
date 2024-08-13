073fbf2c1ad812dc8c25cb5d85c812c9bdba3d0e
Eremeir <20992519+Eremeir@users.noreply.github.com>
2024-08-13 12:29:40 -0500

    Source WoD ring

--- .contrib/Parser/DATAS/03 - World Drops/06 Warlords of Draenor/Warlords of Draenor.lua
+++ .contrib/Parser/DATAS/03 - World Drops/06 Warlords of Draenor/Warlords of Draenor.lua
@@ -571,6 +571,7 @@ root(ROOTS.WorldDrops, expansion(EXPANSION.WOD, bubbleDown({ ["timeline"] = { AD
 			i(116716),	-- Shadowsage Ring
 			i(116710),	-- Soulkeeper Ring
 			i(116713),	-- Sunsworn Ring
+			i(118869),	-- Unbreakable Band of Zoronos
 			i(116707),	-- Zangarra Ring
 		}),
 		filter(TRINKET_F, {
