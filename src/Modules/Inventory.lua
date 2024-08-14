744f73054d74d833623697dcacc7bc7d6efc12d6
ImUnicke <ImUnicke+github@gmail.com>
2024-08-14 17:45:04 -0600

    Fixed a Lua error when inventory scan fails to retrieve an item's info after 5 attempts

--- src/Modules/Inventory.lua
+++ src/Modules/Inventory.lua
@@ -44,7 +44,7 @@ local function RunHandlerWhenLinkAvailable(bag, slot)
 	local itemCache = ContainerCache[bag][slot]
 	local positionRetries = itemCache.Retries or 0
 	if positionRetries > 5 then
-		app.PrintDebug("Retry limit",bag,slot,iteminfo.hyperlink)
+		-- app.PrintDebug("Retry limit",bag,slot,iteminfo and iteminfo.hyperlink or "NO LINK")
 		return
 	end
 
