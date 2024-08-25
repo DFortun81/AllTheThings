05aa5c89be0404d08224caaaba85aabb593c5811
gbrage <gbrage@kth.se>
2024-08-26 00:37:33 +0200

    Reparsed

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638602218862065077.lua
@@ -0,0 +1,18 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[455972] = {
+		readable = "Librarian's Magnifiers",
+		model = 5202153,
+		text = {
+			en = "Librarian's Magnifiers",
+			es = "Librarian's Magnifiers",
+			de = "Librarian's Magnifiers",
+			fr = "Librarian's Magnifiers",
+			it = "Librarian's Magnifiers",
+			pt = "Librarian's Magnifiers",
+			ru = "Librarian's Magnifiers",
+			ko = "Librarian's Magnifiers",
+			cn = "Librarian's Magnifiers",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
