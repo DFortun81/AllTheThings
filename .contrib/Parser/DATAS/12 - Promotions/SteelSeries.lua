05f2b3380982d12689e2e74e4022a7778365be79
Goldenshacal <goldenshacal@gmail.com>
2024-08-28 01:32:43 +0200

    added steelpromo prices

--- .contrib/Parser/DATAS/12 - Promotions/SteelSeries.lua
+++ .contrib/Parser/DATAS/12 - Promotions/SteelSeries.lua
@@ -17,12 +17,12 @@ root(ROOTS.Promotions, n(STEELSERIES, sharedDataSelf({
 	["u"] = BLIZZARD_BALANCE,
 }, {
 	i(112327, {	-- Grinning Reaver (MOUNT!)
-		["description"] = "Aquired alongside the purchase of an Arctis Nova 7 Headset as part of the World of Warcraft SteelSeries Limited Editon Collection",
+		["description"] = "Aquired alongside the purchase of a 200$ Arctis Nova 7 Headset as part of the World of Warcraft SteelSeries Limited Editon Collection.",
 	}),
 	i(224576, {	-- Lil' Flameo (PET!)
-		["description"] = "Aquired alongside the purchase of an Aerox 9 Mouse as part of the World of Warcraft SteelSeries Limited Editon Collection",
+		["description"] = "Aquired alongside the purchase of a 160$ Aerox 9 Mouse as part of the World of Warcraft SteelSeries Limited Editon Collection.",
 	}),
 	i(224574, {	-- Savage Ebony Battle Turtle (MOUNT!)
-		["description"] = "Aquired alongside the purchase of an Artistan Keycap, QcK XXL Mousepad, or an Alliance/Horde Booster Pack Headset as part of the World of Warcraft SteelSeries Limited Editon Collection",
+		["description"] = "Aquired alongside the purchase of a 80$ Artistan Keycap, 40$QcK XXL Mousepad, or a 40$ Alliance/Horde Booster Pack Headset as part of the World of Warcraft SteelSeries Limited Editon Collection.",
 	}),
 })));
