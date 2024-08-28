c30c0242bfa0f4b1a900503cff4f18a33cc470f8
Goldenshacal <goldenshacal@gmail.com>
2024-08-28 05:08:06 +0200

    another parse an correct ed local note

--- .contrib/Parser/DATAS/10 - Professions/Alchemy.lua
+++ .contrib/Parser/DATAS/10 - Professions/Alchemy.lua
@@ -1194,11 +1194,11 @@ root(ROOTS.Professions, prof(ALCHEMY, bubbleDownSelf({ ["requireSkill"] = ALCHEM
 				["description"] = "Requires a crafting order from Inscription.",
 				["provider"] = { "i", 222546 },	-- Algari Treatise on Alchemy
 			}),
-			q(83253, {	-- Weekly DF Alchemy Knowledgepoint #1
+			q(83253, {	-- Weekly TWW Alchemy Knowledgepoint #1
 				["name"] = "TWW Alchemy Treasure #1",
 				["provider"] =  { "i", 225234 },	-- Alchemical Sediment
 			}),
-			q(83255, {	-- Weekly DF Alchemy Knowledgepoint #2
+			q(83255, {	-- Weekly TWW Alchemy Knowledgepoint #2
 				["name"] = "TWW Alchemy Treasure #2",
 				["provider"] = { "i", 225235} ,		-- Deepstone Crucible
 			}),
