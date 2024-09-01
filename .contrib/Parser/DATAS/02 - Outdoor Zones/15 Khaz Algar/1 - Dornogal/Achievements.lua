24aacdc458a009364c8c524f2a92f662b6a40040
Eremeir <20992519+Eremeir@users.noreply.github.com>
2024-08-31 19:38:38 -0500

    Fix parsing error

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/1 - Dornogal/Achievements.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/1 - Dornogal/Achievements.lua
@@ -4,10 +4,10 @@
 root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_2 } }, {
 	m(DORNOGAL, {
 		n(ACHIEVEMENTS, {
-			ach(40606), {	-- Flat Earthen
+			ach(40606, {	-- Flat Earthen
 				["description"] = "Stand at the coordinates and let the machine crush you.",
 				["coord"] = { 50.0, 62.0, DORNOGAL },	-- The Forgegrounds
-			}},
+			}),
 		}),
 	}),
 })));
