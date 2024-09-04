b0d15d7b0f3e97c0f643c75dfd2fbe69e0878bc4
Eremeir <20992519+Eremeir@users.noreply.github.com>
2024-09-03 23:17:53 -0500

    Add Phoenix Wishwing fake objects to DB

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/PhoenixWishwing.lua
@@ -0,0 +1,45 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+    [13000040] = {
+        readable = "|cFFFFFFFFStep 1:|r Phoenix Ash Talisman",
+        icon = 2125665,
+        text = {
+            en = "|cFFFFFFFFStep 1:|r Phoenix Ash Talisman",
+        },
+    },
+	[13000041] = {
+        readable = "|cFFFFFFFFStep 1A:|r Glittering Phoenix Ember",
+        icon = 514016,
+        text = {
+            en = "|cFFFFFFFFStep 1A:|r Glittering Phoenix Ember",
+        },
+    },
+	[13000042] = {
+        readable = "|cFFFFFFFFStep 1B:|r Inert Phoenix Ash",
+        icon = 133849,
+        text = {
+            en = "|cFFFFFFFFStep 1B:|r Inert Phoenix Ash",
+        },
+    },
+	[13000043] = {
+        readable = "|cFFFFFFFFStep 1C:|r Sacred Phoenix Ash",
+        icon = 1003597,
+        text = {
+            en = "|cFFFFFFFFStep 1C:|r Sacred Phoenix Ash",
+        },
+    },
+	[13000044] = {
+        readable = "|cFFFFFFFFStep 2:|r Ash Feather",
+        icon = 2103819,
+        text = {
+            en = "|cFFFFFFFFStep 2:|r Ash Feather",
+        },
+    },
+	[13000045] = {
+        readable = "|cFFFFFFFFStep 3:|r Smoldering Phoenix Ash",
+        icon = 443368,
+        text = {
+            en = "|cFFFFFFFFStep 3:|r Smoldering Phoenix Ash",
+        },
+    },
+})
+do ObjectDB[objectID] = objectData; end
