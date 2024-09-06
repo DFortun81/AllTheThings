cb913dc29112d2d57f76530469c1b7fc430d8a3c
ImUnicke <ImUnicke+github@gmail.com>
2024-09-06 01:48:47 -0600

    Retail: When toggling /att contribute, additional event hooks will now be attached/removed as needed

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -13625,10 +13625,23 @@ app.AddEventHandler("OnStartupDone", function() app.OnStartupDone = true end)
 app.Contribute = function(contrib)
 	app.Contributor = contrib == 1 and true or nil
 	AllTheThingsSavedVariables.Contributor = app.Contributor and 1 or 0
+	local contribModule = app.Modules.Contributor or app.EmptyTable
 	if app.Contributor then
 		app.print("Thanks for helping to contribute to ATT! There will be additional chat and report sounds to help with finding additional discrepancies in ATT data.")
+		if contribModule.Events then
+			for event,func in pairs(contribModule.Events) do
+				-- app.PrintDebug("Contribute.RegisterFuncEvent",event)
+				app:RegisterFuncEvent(event,func)
+			end
+		end
 	elseif app.IsReady then
 		app.print("Not showing ATT contribution information.")
+		if contribModule.Events then
+			for event,func in pairs(contribModule.Events) do
+				-- app.PrintDebug("Contribute.UnregisterEventClean",event)
+				app:UnregisterEventClean(event)
+			end
+		end
 	end
 end
 app.AddEventHandler("OnReady", function()
