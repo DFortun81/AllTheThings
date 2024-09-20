4499665f1dad364da98b738aefa259a51af794fc
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2024-09-20 16:57:42 -0700

    Parsed with bar tab barrel's

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/Bar Tab Barrels.lua
@@ -0,0 +1,73 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[457168] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+	[457170] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+	[457171] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+	[457172] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+	[457173] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+	[457174] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+	[457176] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+	[457180] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+	[457182] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+	[457183] = {
+		readable = "Bar Tab Barrel",
+		model = 526102,
+		text = {
+			en = "Bar Tab Barrel",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
