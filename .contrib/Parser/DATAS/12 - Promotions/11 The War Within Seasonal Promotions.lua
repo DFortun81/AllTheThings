411c2686d49d97fe470f265be571805e3689682d
Goldenshacal <goldenshacal@gmail.com>
2024-09-22 18:00:30 +0200

    updated some promo stuff for release

--- .contrib/Parser/DATAS/12 - Promotions/11 The War Within Seasonal Promotions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/11 The War Within Seasonal Promotions.lua
@@ -16,23 +16,22 @@ root(ROOTS.Promotions, {
 		["timeline"] = { ADDED_11_0_0 },
 		["groups"] = {
 			-- "Pre Season"
-			-- Seems a bit confusing to have NYI showing for EU Realms... Ask Darkal/Runaway? // Braghe
 			chinaONLY(i(190231, {	-- Ash'adar, Harbinger of Dawn (MOUNT!)
-				-- ["description"] = "Rewarded for returning to World of Warcraft. China Only.",
+				["description"] = "Rewarded for returning to World of Warcraft in 2024. China Only.",
 				["timeline"] = { ADDED_11_0_0 },
 			})),
 			chinaONLY(i(210409, {	-- Aura (PET!)
-				-- ["description"] = "Rewarded for returning to World of Warcraft. China Only.",
+				["description"] = "Rewarded for returning to World of Warcraft in 2024. China Only.",
 				["timeline"] = { ADDED_11_0_0 },
 			})),
-			--]]
 			chinaONLY(i(229128, {	-- Harmonious Salutations Bear (MOUNT!)
-				-- ["description"] = "Rewarded for returning to World of Warcraft. China Only.",
+				["description"] = "Rewarded for returning to World of Warcraft in 2024. China Only.",
 				["timeline"] = { ADDED_11_0_0 },
 			})),
 			i(93671, {	-- Ghastly Charger's Skull (MOUNT!)
 				["description"] = "Obtained through watching Twitch Streamers with Drops enabled for at least 4 hours between August 26th, 03:00 p.m. & September 19th, 10:00 a.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
 				["timeline"] = { ADDED_11_0_2, },
+				["u"] = REMOVED_FROM_GAME,	-- 19th September 2024
 			}),
 			i(190609, {	-- Watcher of the Huntress (PET!)
 				["description"] = "Obtained by gifting an eligible creator's channel two Twitch subscriptions between August 26th, 03:00 p.m. & September 26th, 10:00 a.m. PST.\n\nYour Twitch account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.\n\nSpecial Note: If you buy a sub for yourself and gift one more, that will also reward the pet!",
@@ -40,13 +39,38 @@ root(ROOTS.Promotions, {
 				["u"] = BLIZZARD_BALANCE,
 			}),
 			-- Season 1
-			i(232305, {	-- Forged Champion's Prestigious Banner (TOY!)
+			i(232305, {	-- Forged Champion's Prestigious Banner (TOY!)(PVP)
 				["description"] = "...",
 				["timeline"] = { ADDED_11_0_5 },
 			}),
 			i(232301, {	-- Tempered Banner of the Algari (TOY!)
-				["description"] = "...",
+				["description"] = "Mythic Dungeon International: TWW Season 1\n\nThe Mythic Dungeon International (MDI) returns with its global competitions, pitting the best Mythic Dungeon teams in a head-to-head race to the finish line.\n\nAll registered teams that complete under time the two dungeons within the Proving Grounds will receive the exclusive Tempered Banner of the Algari to use in-game!\nSign-ups close 14 October 2024 1PM PDT and The Time Trials are on 16 October 1PM PDT - 21 October (US) 1PM PDT.",
 				["timeline"] = { ADDED_11_0_5 },
+			}),
+			usONLY(i(228765, {	-- Gummi (PET!)
+				["description"] = "Trolli + Xbox promotional pet. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in any physical retail store, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
+				["timeline"] = { ADDED_11_0_2 },
+				["u"] = BLIZZARD_BALANCE,
+			})),
+			usONLY(i(228761, {	-- Classic Brick Tabard
+				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Walgreens|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
+				["timeline"] = { ADDED_11_0_2 },
+				["u"] = BLIZZARD_BALANCE,
+			})),
+			usONLY(i(228763, {	-- Classic Crimson Tabard
+				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Circle K|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
+				["timeline"] = { ADDED_11_0_2 },
+				["u"] = BLIZZARD_BALANCE,
+			})),
+			usONLY(i(228762, {	-- Classic Lively Tabard
+				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Doller General|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
+				["timeline"] = { ADDED_11_0_2 },
+				["u"] = BLIZZARD_BALANCE,
+			})),
+			i(228764, {	-- Classic Sunny Tabard
+				["description"] = "Krogers promotional item. Available for an unknown period. Can be redeemed for 1500 rewards points from pointsrewardsplus.com. With a referral link, just signing up will earn you enough points to redeem the tabard for free. Access the code from the account page and then redeem on battle.net or in the launcher.",
+				["timeline"] = { ADDED_11_0_2 },
+				["u"] = BLIZZARD_BALANCE,
 			}),
 		},
 	}),
--- .contrib/Parser/DATAS/12 - Promotions/Miscellaneous.lua
+++ .contrib/Parser/DATAS/12 - Promotions/Miscellaneous.lua
@@ -106,29 +106,4 @@ root(ROOTS.Promotions, n(MISCELLANEOUS_PROMOTIONS, bubbleDown({ ["u"] = REMOVED_
 		["description"] = "Azeroth Choppers promotional mount. You had to have logged in on a Horde character between the 24th of July and the 30th of September 2014 in order for your account to receive this mount.",
 		["timeline"] = { ADDED_6_0_2 },
 	}),
-	usONLY(i(228765, {	-- Gummi (PET!)
-		["description"] = "Trolli + Xbox promotional pet. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in any physical retail store, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
-		["timeline"] = { ADDED_11_0_2 },
-		["u"] = BLIZZARD_BALANCE,
-	})),
-	usONLY(i(228761, {	-- Classic Brick Tabard
-		["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Walgreens|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
-		["timeline"] = { ADDED_11_0_2 },
-		["u"] = BLIZZARD_BALANCE,
-	})),
-	usONLY(i(228763, {	-- Classic Crimson Tabard
-		["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Circle K|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
-		["timeline"] = { ADDED_11_0_2 },
-		["u"] = BLIZZARD_BALANCE,
-	})),
-	usONLY(i(228762, {	-- Classic Lively Tabard
-		["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Doller General|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
-		["timeline"] = { ADDED_11_0_2 },
-		["u"] = BLIZZARD_BALANCE,
-	})),
-	i(228764, {	-- Classic Sunny Tabard
-		["description"] = "Krogers promotional item. Available for an unknown period. Can be redeemed for 1500 rewards points from pointsrewardsplus.com. With a referral link, just signing up will earn you enough points to redeem the tabard for free. Access the code from the account page and then redeem on battle.net or in the launcher.",
-		["timeline"] = { ADDED_11_0_2 },
-		["u"] = BLIZZARD_BALANCE,
-	}),
 })));
