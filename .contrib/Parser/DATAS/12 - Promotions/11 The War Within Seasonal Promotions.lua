26972ab381f073bc7360818ec650c18d625947ab
Goldenshacal <goldenshacal@gmail.com>
2024-09-30 17:35:51 +0200

    updated promo stuff

--- .contrib/Parser/DATAS/12 - Promotions/11 The War Within Seasonal Promotions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/11 The War Within Seasonal Promotions.lua
@@ -40,33 +40,47 @@ root(ROOTS.Promotions, {
 			}),
 			-- Season 1
 			i(232305, {	-- Forged Champion's Prestigious Banner (TOY!)(PVP)
-				["description"] = "...",
+				["description"] = "Arena World Championship: TWW Season 1. Sign up on Raider.io for any of the 4 Cups, available until October 13th, and play in atleast two game series (best of 5). Winning not required, for more details & requirements checkout: Raider.io/tournaments/AWC",
 				["timeline"] = { ADDED_11_0_5 },
+			}),
+			i(500000, {    -- The PVP Banner temporary item
+				["sourceID"] = 500000,
+				["description"] = "Arena World Championship: TWW Season 1. Sign up on Raider.io for any of the 4 Cups, available until October 13th, and play in atleast two game series (best of 5). Winning not required, for more details & requirements checkout: Raider.io/tournaments/AWC",
+				["timeline"] = { ADDED_11_0_5 },
+				["icon"] = "Interface\\Icons\\Inv_mdi_awc_bannerreward_icons_blue",
+				["name"] = "Forged Champion's Prestigious Banner (TOY!)(PVP)"
 			}),
 			i(232301, {	-- Tempered Banner of the Algari (TOY!)
 				["description"] = "Mythic Dungeon International: TWW Season 1\n\nThe Mythic Dungeon International (MDI) returns with its global competitions, pitting the best Mythic Dungeon teams in a head-to-head race to the finish line.\n\nAll registered teams that complete under time the two dungeons within the Proving Grounds will receive the exclusive Tempered Banner of the Algari to use in-game!\nSign-ups close 14 October 2024 1PM PDT and The Time Trials are on 16 October 1PM PDT - 21 October (US) 1PM PDT.",
 				["timeline"] = { ADDED_11_0_5 },
 			}),
-			usONLY(i(228765, {	-- Gummi (PET!)
-				["description"] = "Trolli + Xbox promotional pet. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in any physical retail store, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
+			i(500001, {    -- The PVE Banner temporary item
+				["sourceID"] = 500001,
+				["description"] = "Mythic Dungeon International: TWW Season 1\n\nThe Mythic Dungeon International (MDI) returns with its global competitions, pitting the best Mythic Dungeon teams in a head-to-head race to the finish line.\n\nAll registered teams that complete under time the two dungeons within the Proving Grounds will receive the exclusive Tempered Banner of the Algari to use in-game!\nSign-ups close 14 October 2024 1PM PDT and The Time Trials are on 16 October 1PM PDT - 21 October (US) 1PM PDT.",
+				["timeline"] = { ADDED_11_0_5 },
+				["icon"] = "Interface\\Icons\\Inv_mdi_awc_bannerreward_icons_blue",
+				["name"] = "Tempered Banner of the Algari (TOY!)(PVE)"
+			}),
+			i(228765, {	-- Gummi (PET!)
+				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Circle K|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should receive a code to your email to redeem on battle.net or in the launcher.\n\nYou must have a U.S. address and phone number to participate.\n\nThe code is usable in any region.",
 				["timeline"] = { ADDED_11_0_2 },
 				["u"] = BLIZZARD_BALANCE,
-			})),
-			usONLY(i(228761, {	-- Classic Brick Tabard
-				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Walgreens|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
+			}),
+			i(228761, {	-- Classic Brick Tabard
+				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Circle K|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should receive a code to your email to redeem on battle.net or in the launcher.\n\nYou must have a U.S. address and phone number to participate.\n\nThe code is usable in any region.",
 				["timeline"] = { ADDED_11_0_2 },
 				["u"] = BLIZZARD_BALANCE,
-			})),
-			usONLY(i(228763, {	-- Classic Crimson Tabard
-				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Circle K|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
+			}),
+			i(228763, {	-- Classic Crimson Tabard
+				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Circle K|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should receive a code to your email to redeem on battle.net or in the launcher.\n\nYou must have a U.S. address and phone number to participate.\n\nThe code is usable in any region.",
 				["timeline"] = { ADDED_11_0_2 },
 				["u"] = BLIZZARD_BALANCE,
-			})),
-			usONLY(i(228762, {	-- Classic Lively Tabard
-				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Doller General|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should recieve a code to your email to redeem on battle.net or in the launcher.",
+			}),
+			i(228762, {	-- Classic Lively Tabard
+				["description"] = "Trolli + Xbox promotional item. Available between 9/1/24 - 2/28/25 by purchasing Trolli Candy products in |CFFFF0000Circle K|r, photoing your receipt and uploading it as confirmation to trolli.com/xbox. Once processed, you should receive a code to your email to redeem on battle.net or in the launcher.\n\nYou must have a U.S. address and phone number to participate.\n\nThe code is usable in any region.",
 				["timeline"] = { ADDED_11_0_2 },
 				["u"] = BLIZZARD_BALANCE,
-			})),
+			}),
 			i(228764, {	-- Classic Sunny Tabard
 				["description"] = "Krogers promotional item. Available for an unknown period. Can be redeemed for 1500 rewards points from pointsrewardsplus.com. With a referral link, just signing up will earn you enough points to redeem the tabard for free. Access the code from the account page and then redeem on battle.net or in the launcher.",
 				["timeline"] = { ADDED_11_0_2 },
