78e704006fe439393ef48107faad2cf224e7006e
Goldenshacal <goldenshacal@gmail.com>
2024-10-02 11:17:37 +0200

    profession knowledge only comes from treasures

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Zone Rewards.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Zone Rewards.lua
@@ -164,34 +164,5 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_2 } }, 
 				224706,	-- Ancient Forged Warhammer
 			}},
 		}),
-		n(WEEKLY_PROFESSION_KNOWLEDGE, {
-			["sharedDescription"] = "Rewarded from many different treasures and activities.",
-			["g"] = {
-				-- Alchemy
-				i(225234),	-- Alchemical Sediment
-				i(225235),	-- Deepstone Crucible
-				-- Blacksmithing
-				i(225232),	-- Coreway Billet
-				i(225233),	-- Dense Bladestone
-				-- Enchanting
-				i(225230),	-- Crystalline Repository
-				i(225231),	-- Powdered Fulgurance
-				-- Engineering
-				i(225229),	-- Earthen Induction Coil
-				i(225228),	-- Rust-Locked Mechanism
-				-- Inscription
-				i(225226),	-- Striated Inkstone
-				i(225227),	-- Wax-Sealed Records
-				-- Jewelcrafting
-				i(225225),	-- Deepstone Fragment
-				i(225224),	-- Diaphanous Gem Shards
-				-- Leatherworking
-				i(225222),	-- Stone-Leather Swatch
-				i(225223),	-- Sturdy Nerubian Carapace
-				-- Tailoring
-				i(225220),	-- Chitin Needle
-				i(225221),	-- Spool of Webweave
-			},
-		}),
 	}),
 })));
