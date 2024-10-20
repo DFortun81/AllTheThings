02381820451a9092468662287e8b68250aa1771e
Goldenshacal <goldenshacal@gmail.com>
2024-10-20 21:14:36 +0200

    updated rewards header for weekly events & gave the caches a source (weekly events>rewards with nothing attached is as good as keeping them in unsorted)

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/1 - Dornogal/Weekly Holidays.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/1 - Dornogal/Weekly Holidays.lua
@@ -4,37 +4,55 @@
 root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_2 } }, {
 	m(DORNOGAL, {
 		n(WEEKLY_HOLIDAYS, {
-			n(REWARDS, {
-				i(229129, {	-- Cache of Delver's Spoils
-					["timeline"] = { ADDED_11_0_2, REMOVED_11_1_0 },
-				}),
-				i(226193, {	-- Cache of Nerubian Treasures (N)
-					["timeline"] = { ADDED_11_0_2, REMOVED_11_1_0 },
-				}),
-				i(226194, {	-- Cache of Nerubian Treasures (H)
-					["timeline"] = { ADDED_11_0_2, REMOVED_11_1_0 },
-				}),
-			}),
-			n(QUESTS, sharedData({
+			n(QUESTS, sharedData({			-- Normal Cache Quests
 				["provider"] = { "n", 226919 },	-- Archivist Frithrun
 				["coord"] = { 46.5, 49.3, DORNOGAL },
 				["isWeekly"] = true,
+				["groups"] = {
+					i(226193, {	-- Cache of Nerubian Treasures (N)
+						["timeline"] = { ADDED_11_0_2, REMOVED_11_1_0 },
+					}),
+				},
 			}, {
 				q(83363),					-- A Burning Path Through Time
-				pvp(q(83345)),				-- A Call to Battle
-				q(84776),					-- A A Call to Delves
 				--q(TODO),					-- A Fel Path Through Time
 				q(83365),					-- A Frozen Path Through Time
 				q(83364),					-- A Savage Path Through Time
 				q(83359),					-- A Shattered Path Through Time
 				q(83362),					-- A Shrouded Path Through Time
-				q(83347),					-- Emissary of War
+			})),
+			n(QUESTS, sharedData({			-- No Cache Quests
+				["provider"] = { "n", 226919 },	-- Archivist Frithrun
+				["coord"] = { 46.5, 49.3, DORNOGAL },
+				["isWeekly"] = true,
+			}, {
+				pvp(q(83345)),				-- A Call to Battle
 				pvp(q(83358)),				-- The Arena Calls
 				petbattle(pvp(q(83357, {	-- The Very Best
 					["_drop"] = { "g" },	-- Drop Ultimate Battle-Training Stone
 				}))),
-				q(83366),					-- The World Awaits
+				q(83366),					-- The World Awaits			
 			})),
+			q(84776, {						-- A Call to Delves
+				["provider"] = { "n", 226919 },	-- Archivist Frithrun
+				["coord"] = { 46.5, 49.3, DORNOGAL },
+				["isWeekly"] = true,
+				["groups"] = {
+					i(229129, {	-- Cache of Delver's Spoils
+						["timeline"] = { ADDED_11_0_2, REMOVED_11_1_0 },
+					}),
+				},
+			}),
+			q(83347, {						-- Emissary of War
+				["provider"] = { "n", 226919 },	-- Archivist Frithrun
+				["coord"] = { 46.5, 49.3, DORNOGAL },
+				["isWeekly"] = true,
+				["groups"] = {
+					i(226194, {	-- Cache of Nerubian Treasures (H)
+						["timeline"] = { ADDED_11_0_2, REMOVED_11_1_0 },
+					}),
+				},
+			}),
 		}),
 	}),
 })));
